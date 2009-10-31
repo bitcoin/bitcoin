99142385daea73bb2c8fadf44b6d36df31813fb0
sirius-m <sirius-m@1a98c847-1fd6-4fd8-948a-caf3550aa51b>
2009-10-31 09:52:24 +0000

    added wx/stdpaths.h

--- headers.h
+++ headers.h
@@ -21,6 +21,7 @@
 #include <wx/clipbrd.h>
 #include <wx/snglinst.h>
 #include <wx/taskbar.h>
+#include <wx/stdpaths.h>
 #include <openssl/ecdsa.h>
 #include <openssl/evp.h>
 #include <openssl/rand.h>
