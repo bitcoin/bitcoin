6970bd96d16137fae5c52449137941be5f9f71b5
s_nakamoto <s_nakamoto@1a98c847-1fd6-4fd8-948a-caf3550aa51b>
2010-06-22 17:59:00 +0000

    version 0.3 rc1

--- serialize.h
+++ serialize.h
@@ -19,8 +19,8 @@ class CScript;
 class CDataStream;
 class CAutoFile;
 
-static const int VERSION = 213;
-static const char* pszSubVer = "";
+static const int VERSION = 300;
+static const char* pszSubVer = " rc1";
 
 
 
