fdbf76d4f49c220e2ed4412a3d8d8cd6efd74826
s_nakamoto <s_nakamoto@1a98c847-1fd6-4fd8-948a-caf3550aa51b>
2010-09-12 17:30:15 +0000

    dropped DB_PRIVATE from dbenv.open
    git-svn-id: https://bitcoin.svn.sourceforge.net/svnroot/bitcoin/trunk@153 1a98c847-1fd6-4fd8-948a-caf3550aa51b

--- serialize.h
+++ serialize.h
@@ -23,7 +23,7 @@ class CAutoFile;
 static const unsigned int MAX_SIZE = 0x02000000;
 
 static const int VERSION = 312;
-static const char* pszSubVer = ".3";
+static const char* pszSubVer = ".4";
 
 
 
