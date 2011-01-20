97822e339a917180fbcec501028cf37fef50d85a
Tully Foote <tfoote@willowgarage.com>
2011-01-20 02:22:21 -0800

    fixing variable names to be consistent

--- Modules/FindBerkeleyDB.cmake
+++ Modules/FindBerkeleyDB.cmake
@@ -6,12 +6,12 @@
 
 # This module uses:
 #
-# DB_ROOT_DIR - set to BerkeleyDB's root directory
+# BerkeleyDB_ROOT_DIR - set to BerkeleyDB's root directory
 
 # This module defines:
-# DB_FOUND - True if BerkleyDB is found
-# DB_INCLUDE_DIR - BerkeleyDB's include directory
-# DB_LIBRARIES - Libraries needed to use Berkeley DB
+# BerkeleyDB_FOUND - True if BerkleyDB is found
+# BerkeleyDB_INCLUDE_DIR - BerkeleyDB's include directory
+# BerkeleyDB_LIBRARIES - Libraries needed to use Berkeley DB
 
 if (BerkeleyDB_FIND_VERSION_MAJOR AND BerkeleyDB_FIND_VERSION_MINOR)
   if (MSVC)
@@ -39,7 +39,7 @@ if (NAME_EXTENSION)
   set (BerkeleyDB_NAMES ${NEW_NAMES})
 endif (NAME_EXTENSION)
 
-find_path(DB_INCLUDE_DIR NAMES db_cxx.h
+find_path(BerkeleyDB_INCLUDE_DIR NAMES db_cxx.h
           PATHS ${DB_ROOT_DIR} $ENV{DBROOTDIR}
           PATH_SUFFIXES include db${NAME_EXTENSION}
           )
@@ -53,32 +53,32 @@ if (MSVC)
         set(_db_lib_path_SUFFIXES_RELEASE Release)
     endif (CMAKE_CL_64)
     find_library(DBLIB_STATIC_RELEASE libdb${NAME_EXTENSION}s
-                 PATHS ${DB_ROOT_DIR} $ENV{DBROOTDIR} ${DB_INCLUDE_DIR}
+                 PATHS ${DB_ROOT_DIR} $ENV{DBROOTDIR} ${BerkeleyDB_INCLUDE_DIR}
                  PATH_SUFFIXES ${_db_lib_path_SUFFIXES_RELEASE} lib)
     find_library(DBLIB_STATIC_DEBUG libdb${NAME_EXTENSION}sd
-                 PATHS ${DB_ROOT_DIR} $ENV{DBROOTDIR} ${DB_INCLUDE_DIR}
+                 PATHS ${DB_ROOT_DIR} $ENV{DBROOTDIR} ${BerkeleyDB_INCLUDE_DIR}
                  PATH_SUFFIXES ${_db_lib_path_SUFFIXES_DEBUG} lib)
-    set(DB_LIBRARIES optimized ${DBLIB_STATIC_RELEASE}
+    set(BerkeleyDB_LIBRARIES optimized ${DBLIB_STATIC_RELEASE}
                      debug ${DBLIB_STATIC_DEBUG})
 
 else(MSVC)
-    find_library(DB_LIB db${NAME_EXTENSION}
-                 PATHS ${DB_ROOT_DIR} $ENV{DBROOTDIR} ${DB_INCLUDE_DIR}
+    find_library(BerkeleyDB_LIB db${NAME_EXTENSION}
+                 PATHS ${DB_ROOT_DIR} $ENV{DBROOTDIR} ${BerkeleyDB_INCLUDE_DIR}
                        /usr/local/lib
                  PATH_SUFFIXES lib
                                db${NAME_EXTENSION})
-    find_library(DB_LIBCXX db_cxx${NAME_EXTENSION}
-                 PATHS ${DB_ROOT_DIR} $ENV{DBROOTDIR} ${DB_INCLUDE_DIR}
+    find_library(BerkeleyDB_LIBCXX db_cxx${NAME_EXTENSION}
+                 PATHS ${DB_ROOT_DIR} $ENV{DBROOTDIR} ${BerkeleyDB_INCLUDE_DIR}
                        /usr/local/lib
                  PATH_SUFFIXES lib db${NAME_EXTENSION})
-    set(DB_LIBRARIES ${DB_LIB})
-    if (DB_LIBCXX)
-        list(APPEND DB_LIBRARIES ${DB_LIBCXX})
-    endif (DB_LIBCXX)
+    set(BerkeleyDB_LIBRARIES ${BerkeleyDB_LIB})
+    if (BerkeleyDB_LIBCXX)
+        list(APPEND BerkeleyDB_LIBRARIES ${BerkeleyDB_LIBCXX})
+    endif (BerkeleyDB_LIBCXX)
 endif (MSVC)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(DB DEFAULT_MSG DB_INCLUDE_DIR DB_LIBRARIES)
+find_package_handle_standard_args(BerkeleyDB DEFAULT_MSG BerkeleyDB_INCLUDE_DIR BerkeleyDB_LIBRARIES)
 
 MARK_AS_ADVANCED(
   BerkeleyDB_LIBRARIES
