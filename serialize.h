84e7ad922bc6b59219d0b5f2220b531eb549c8f5
Gavin Andresen <gavinandresen@gmail.com>
2011-01-24 11:13:25 -0500

    Bump version number to 0.3.20

--- serialize.h
+++ serialize.h
@@ -25,7 +25,7 @@ class CDataStream;
 class CAutoFile;
 static const unsigned int MAX_SIZE = 0x02000000;
 
-static const int VERSION = 31902;
+static const int VERSION = 32000;
 static const char* pszSubVer = "";
 static const bool VERSION_IS_BETA = true;
 
