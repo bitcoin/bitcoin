fe460d47b06d10095b882d7a9ea8a98ad0079bbd
Matt Corallo <matt@bluematt.me>
2011-03-09 19:53:40 +0100

    Add timestamp prefix to each line in the debug log (this time with human-readable times)

--- util.cpp
+++ util.cpp
@@ -169,8 +169,8 @@ inline int OutputDebugStringF(const char* pszFormat, ...)
         }
         if (fileout)
         {
-            //// Debug print useful for profiling
-            //fprintf(fileout, " %"PRI64d" ", GetTimeMillis());
+            // Debug print useful for profiling
+            fprintf(fileout, "%s ", DateTimeStrFormat("%x %H:%M:%S", GetTime()).c_str());
             va_list arg_ptr;
             va_start(arg_ptr, pszFormat);
             ret = vfprintf(fileout, pszFormat, arg_ptr);
