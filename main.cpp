c59b6f704cd8f42e1943f124f435363bfff244f5
Pieter Wuille <pieter.wuille@gmail.com>
2011-04-12 21:01:11 +0200

    Bugfix for spentpertxout: slow startup
    When starting the client, ReacceptWalletTransaction didn't skip spent transactions in its loop, and processed all old spent transactions again.

--- main.cpp
+++ main.cpp
@@ -950,6 +950,8 @@ void ReacceptWalletTransactions()
                 }
                 for (int i = 0; i < txindex.vSpent.size(); i++)
                 {
+                    if (wtx.IsSpent(i))
+                        continue;
                     if (!txindex.vSpent[i].IsNull() && wtx.vout[i].IsMine())
                     {
                         wtx.MarkSpent(i);
