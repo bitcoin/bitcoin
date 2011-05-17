b201c1f60020763b102d259f6346305dd668c7eb
Pieter Wuille <pieter.wuille@gmail.com>
2011-05-17 17:54:06 +0200

    Bugfix for dnslookup: irc.cpp still used old CAddress constructor

--- src/irc.cpp
+++ src/irc.cpp
@@ -265,11 +265,11 @@ void ThreadIRCSeed2(void* parg)
     while (!fShutdown)
     {
         //CAddress addrConnect("216.155.130.130:6667"); // chat.freenode.net
-        CAddress addrConnect("92.243.23.21:6667"); // irc.lfnet.org
+        CAddress addrConnect("92.243.23.21", 6667); // irc.lfnet.org
         if (!fTOR)
         {
             //struct hostent* phostent = gethostbyname("chat.freenode.net");
-            CAddress addrIRC("irc.lfnet.org:6667", 0, true);
+            CAddress addrIRC("irc.lfnet.org", 6667, true);
             if (addrIRC.IsValid())
                 addrConnect = addrIRC;
         }
