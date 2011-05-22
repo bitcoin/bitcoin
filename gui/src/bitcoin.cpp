ad88e7626ba5ac2274d7ec9c218537ec7df0ad50
Wladimir van der Laan <laanwj@gmail.com>
2011-05-22 14:54:13 +0200

    go on testnet for now

--- gui/src/bitcoin.cpp
+++ gui/src/bitcoin.cpp
@@ -2,12 +2,16 @@
  * W.J. van der Laan 2011
  */
 #include "bitcoingui.h"
+#include "util.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+
+    /* Testing on testnet */
+    fTestNet = true;
 
     BitcoinGUI window;
     window.setBalance(1234.567890);
