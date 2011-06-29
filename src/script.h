5f6a654d3eb01cd1d324df4cbe2c7fe51dc3e752
Vegard Nossum <vegard.nossum@gmail.com>
2011-06-29 12:33:21 +0200

    Add prototype for EvalScript() to script.h
    This is needed for the script unit tests.

--- src/script.h
+++ src/script.h
@@ -691,6 +691,7 @@ public:
 
 
 
+bool EvalScript(std::vector<std::vector<unsigned char> >& stack, const CScript& script, const CTransaction& txTo, unsigned int nIn, int nHashType);
 
 bool IsStandard(const CScript& scriptPubKey);
 bool IsMine(const CKeyStore& keystore, const CScript& scriptPubKey);
