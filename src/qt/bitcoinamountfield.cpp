e1f3d64c4a95893ec623fd082c82f443ae868fcd
Wladimir J. van der Laan <laanwj@gmail.com>
2011-07-06 20:41:13 +0200

    Add "BTC" to all amount widgets, to make clear what the unit is

--- src/qt/bitcoinamountfield.cpp
+++ src/qt/bitcoinamountfield.cpp
@@ -25,6 +25,7 @@ BitcoinAmountField::BitcoinAmountField(QWidget *parent):
     layout->addWidget(amount);
     layout->addWidget(new QLabel(QString(".")));
     layout->addWidget(decimals);
+    layout->addWidget(new QLabel(QString(" BTC")));
     layout->addStretch(1);
     layout->setContentsMargins(0,0,0,0);
 
