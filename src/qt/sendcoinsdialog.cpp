d8aeb8dd2af508febafd9f7aa23526b67b97185f
Wladimir J. van der Laan <laanwj@gmail.com>
2011-07-08 19:00:08 +0200

    Reorganize "send coins" tab

--- src/qt/sendcoinsdialog.cpp
+++ src/qt/sendcoinsdialog.cpp
@@ -20,6 +20,7 @@ SendCoinsDialog::SendCoinsDialog(QWidget *parent, const QString &address) :
 {
     ui->setupUi(this);
 #if QT_VERSION >= 0x040700
+    ui->payTo->setPlaceholderText(tr("Enter a Bitcoin address (e.g. 1NS17iag9jJgTHD1VXjvLCEnZuQ3rJDE9L)"));
     ui->addAsLabel->setPlaceholderText(tr("Enter a label for this address to add it to your address book"));
 #endif
     GUIUtil::setupAddressWidget(ui->payTo, this);
