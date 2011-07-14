3a10d1c49390f26daed14d6362f72c154ac191e7
Matt Corallo <matt@bluematt.me>
2011-07-14 18:15:50 +0200

    Fix bad return values in LoadWallet.

--- src/db.cpp
+++ src/db.cpp
@@ -777,7 +777,7 @@ int CWalletDB::LoadWallet(CWallet* pwallet)
                     key.SetPrivKey(wkey.vchPrivKey);
                 }
                 if (!pwallet->LoadKey(key))
-                    return false;
+                    return DB_CORRUPT;
             }
             else if (strType == "mkey")
             {
@@ -786,7 +786,7 @@ int CWalletDB::LoadWallet(CWallet* pwallet)
                 CMasterKey kMasterKey;
                 ssValue >> kMasterKey;
                 if(pwallet->mapMasterKeys.count(nID) != 0)
-                    return false;
+                    return DB_CORRUPT;
                 pwallet->mapMasterKeys[nID] = kMasterKey;
                 if (pwallet->nMasterKeyMaxID < nID)
                     pwallet->nMasterKeyMaxID = nID;
@@ -798,7 +798,7 @@ int CWalletDB::LoadWallet(CWallet* pwallet)
                 vector<unsigned char> vchPrivKey;
                 ssValue >> vchPrivKey;
                 if (!pwallet->LoadCryptedKey(vchPubKey, vchPrivKey))
-                    return false;
+                    return DB_CORRUPT;
             }
             else if (strType == "defaultkey")
             {
