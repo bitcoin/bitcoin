27da267be493c410b39c8670649754fd1d8f80ad
JoelKatz <DavidJoelSchwartz@GMail.com>
2011-07-25 15:13:55 -0700

    Faster Base64 decoder.

--- src/util.h
+++ src/util.h
@@ -209,6 +209,7 @@ int64 GetTime();
 int64 GetAdjustedTime();
 void AddTimeData(unsigned int ip, int64 nTime);
 std::string FormatFullVersion();
+std::string DecodeBase64(const std::string &s);
 
 
 
