01a509fa2616537a4b444966d6e4e451822b2abd
Matt Corallo <matt@bluematt.me>
2011-08-03 21:02:37 +0200

    Don't listen if on TOR (resolves #441).

--- src/init.cpp
+++ src/init.cpp
@@ -247,7 +247,8 @@ bool AppInit2(int argc, char* argv[])
     fPrintToDebugger = GetBoolArg("-printtodebugger");
 
     fTestNet = GetBoolArg("-testnet");
-    fNoListen = GetBoolArg("-nolisten");
+    bool fTOR = (fUseProxy && addrProxy.port == htons(9050));
+    fNoListen = GetBoolArg("-nolisten") || fTOR;
     fLogTimestamps = GetBoolArg("-logtimestamps");
 
     for (int i = 1; i < argc; i++)
