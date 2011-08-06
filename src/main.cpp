514b18722aa2bd0c591428f91b4ba167013ecbc1
David Joel Schwartz <davidjoelschwartz@gmail.com>
2011-08-06 05:15:00 -0800

    Remove 2 second sleep from CheckWork

--- src/main.cpp
+++ src/main.cpp
@@ -2894,7 +2894,6 @@ bool CheckWork(CBlock* pblock, CWallet& wallet, CReserveKey& reservekey)
             return error("BitcoinMiner : ProcessBlock, block not accepted");
     }
 
-    Sleep(2000);
     return true;
 }
 
