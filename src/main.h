5435a8988f557d5a49c0c2a7def8b1e0de5299ad
Giel van Schijndel <me@mortis.eu>
2011-08-11 14:06:08 +0200

    Use "#if USE_UPNP" instead of "#ifdef USE_UPNP" to work properly with the makefiles
    Additionally, don't try to link with libminiupnc when USE_UPNP=0.
    
    Signed-off-by: Giel van Schijndel <me@mortis.eu>

--- src/main.h
+++ src/main.h
@@ -39,7 +39,7 @@ inline bool MoneyRange(int64 nValue) { return (nValue >= 0 && nValue <= MAX_MONE
 static const int COINBASE_MATURITY = 100;
 // Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp.
 static const int LOCKTIME_THRESHOLD = 500000000; // Tue Nov  5 00:53:20 1985 UTC
-#ifdef USE_UPNP
+#if USE_UPNP
 static const int fHaveUPnP = true;
 #else
 static const int fHaveUPnP = false;
