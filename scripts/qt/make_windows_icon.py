3c66913cd2d7a37f57c656c282faf7d160e88ea8
Wladimir J. van der Laan <laanwj@gmail.com>
2011-09-18 12:04:03 +0200

    move qt-specific scripts to qt-specific directory in scripts/

--- scripts/qt/make_windows_icon.py
+++ scripts/qt/make_windows_icon.py
@@ -1,7 +1,7 @@
 #!/bin/bash
 # create multiresolution windows icon
-ICON_SRC=../src/qt/res/icons/bitcoin.png
-ICON_DST=../src/qt/res/icons/bitcoin.ico
+ICON_SRC=../../src/qt/res/icons/bitcoin.png
+ICON_DST=../../src/qt/res/icons/bitcoin.ico
 convert ${ICON_SRC} -resize 16x16 bitcoin-16.png
 convert ${ICON_SRC} -resize 32x32 bitcoin-32.png
 convert ${ICON_SRC} -resize 48x48 bitcoin-48.png
