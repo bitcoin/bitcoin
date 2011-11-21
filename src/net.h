ba56a88ca5fae52ce5690553d97e4e8ac1338dfd
Matt Corallo <matt@bluematt.me>
2011-11-21 12:25:00 -0500

    Move DNS Seed lookup to a new thread.

--- src/net.h
+++ src/net.h
@@ -40,7 +40,6 @@ CNode* ConnectNode(CAddress addrConnect, int64 nTimeout=0);
 void AbandonRequests(void (*fn)(void*, CDataStream&), void* param1);
 bool AnySubscribed(unsigned int nChannel);
 void MapPort(bool fMapPort);
-void DNSAddressSeed();
 bool BindListenPort(std::string& strError=REF(std::string()));
 void StartNode(void* parg);
 bool StopNode();
