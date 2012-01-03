70bb34d9beb219b6ebcc1636f96d6e8125c0c230
Wladimir J. van der Laan <laanwj@gmail.com>
2012-01-03 11:55:58 +0100

    Fix typo (#734)

--- src/qt/locale/bitcoin_it.ts
+++ src/qt/locale/bitcoin_it.ts
@@ -917,7 +917,7 @@ p, li { white-space: pre-wrap; }⏎
     </message>
     <message>
         <location filename="../forms/sendcoinsentry.ui" line="103"/>
-        <source>Choose adress from address book</source>
+        <source>Choose address from address book</source>
         <translation>Scegli l&apos;indirizzo dalla rubrica</translation>
     </message>
     <message>
