82705af1eb521beefd63f81e3c5e39616fcf2076
Wladimir J. van der Laan <laanwj@gmail.com>
2012-02-17 18:00:41 +0100

    Change #ifdef GUI to #ifdef QT_GUI, GUI is not defined anymore...

--- src/init.cpp
+++ src/init.cpp
@@ -206,10 +206,10 @@ bool AppInit2(int argc, char* argv[])
 #endif
 #endif
             "  -paytxfee=<amt>  \t  "   + _("Fee per kB to add to transactions you send\n") +
-#ifdef GUI
+#ifdef QT_GUI
             "  -server          \t\t  " + _("Accept command line and JSON-RPC commands\n") +
 #endif
-#if !defined(WIN32) && !defined(GUI)
+#if !defined(WIN32) && !defined(QT_GUI)
             "  -daemon          \t\t  " + _("Run in the background as a daemon and accept commands\n") +
 #endif
             "  -testnet         \t\t  " + _("Use the test network\n") +
@@ -248,7 +248,7 @@ bool AppInit2(int argc, char* argv[])
     fTestNet = GetBoolArg("-testnet");
     fDebug = GetBoolArg("-debug");
 
-#if !defined(WIN32) && !defined(GUI)
+#if !defined(WIN32) && !defined(QT_GUI)
     fDaemon = GetBoolArg("-daemon");
 #else
     fDaemon = false;
@@ -279,7 +279,7 @@ bool AppInit2(int argc, char* argv[])
     }
 #endif
 
-#if !defined(WIN32) && !defined(GUI)
+#if !defined(WIN32) && !defined(QT_GUI)
     if (fDaemon)
     {
         // Daemonize
