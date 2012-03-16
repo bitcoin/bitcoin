169247766f7c7432417bfa0c00c42b9e137cd619
Luke Dashjr <luke-jr+git@utopios.org>
2012-03-16 16:12:16 -0400

    Bump version to 0.5.4

--- src/serialize.h
+++ src/serialize.h
@@ -60,7 +60,7 @@ class CDataStream;
 class CAutoFile;
 static const unsigned int MAX_SIZE = 0x02000000;
 
-static const int VERSION = 50300;
+static const int VERSION = 50400;
 static const char* pszSubVer = "";
 static const bool VERSION_IS_BETA = true;
 
