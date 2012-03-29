7de8abb6bf458944a2bd397c058ad9a2c05d3b03
Gavin Andresen <gavinandresen@gmail.com>
2012-03-29 09:03:11 -0400

    Auto-remove logs, and decrease logfile size to 10M

--- src/db.cpp
+++ src/db.cpp
@@ -87,12 +87,13 @@ CDB::CDB(const char* pszFile, const char* pszMode) : pdb(NULL)
             int nDbCache = GetArg("-dbcache", 25);
             dbenv.set_lg_dir(strLogDir.c_str());
             dbenv.set_cachesize(nDbCache / 1024, (nDbCache % 1024)*1048576, 1);
-            dbenv.set_lg_bsize(10485760);
-            dbenv.set_lg_max(104857600);
+            dbenv.set_lg_bsize(1048576);
+            dbenv.set_lg_max(10485760);
             dbenv.set_lk_max_locks(10000);
             dbenv.set_lk_max_objects(10000);
             dbenv.set_errfile(fopen(strErrorFile.c_str(), "a")); /// debug
             dbenv.set_flags(DB_AUTO_COMMIT, 1);
+            dbenv.log_set_config(DB_LOG_AUTO_REMOVE, 1);
             ret = dbenv.open(strDataDir.c_str(),
                              DB_CREATE     |
                              DB_INIT_LOCK  |
