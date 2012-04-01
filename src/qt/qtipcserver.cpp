6e2fb937f76fecbe0db397ef206d60e292c97024
Philip Kaufmann <phil.kaufmann@t-online.de>
2012-04-01 14:07:29 +0200

    massive URI-handling / IPC server re-work

--- src/qt/qtipcserver.cpp
+++ src/qt/qtipcserver.cpp
@@ -2,88 +2,115 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file license.txt or http://www.opensource.org/licenses/mit-license.php.
 
-#include <boost/algorithm/string.hpp>
+#include <boost/filesystem.hpp>
 #include <boost/interprocess/ipc/message_queue.hpp>
-#include <boost/tokenizer.hpp>
-#include <boost/date_time/posix_time/posix_time.hpp>
 
 #include "ui_interface.h"
+#include "util.h"
 #include "qtipcserver.h"
 
-using namespace boost::interprocess;
-using namespace boost::posix_time;
 using namespace boost;
-using namespace std;
 
-void ipcShutdown()
+/** global state definition */
+ipcState globalIpcState = IPC_NOT_INITIALIZED;
+
+void ipcThread(void* pArg)
 {
-    message_queue::remove(BITCOINURI_QUEUE_NAME);
+    IMPLEMENT_RANDOMIZE_STACK(ipcThread(pArg));
+    try
+    {
+        ipcThread2(pArg);
+    }
+    catch (std::exception& e) {
+        PrintExceptionContinue(&e, "ipcThread()");
+    } catch (...) {
+        PrintExceptionContinue(NULL, "ipcThread()");
+    }
+    printf("ipcThread exited\n");
 }
 
-void ipcThread(void* parg)
+void ipcThread2(void* pArg)
 {
-    message_queue* mq = (message_queue*)parg;
-    char strBuf[257];
-    size_t nSize;
-    unsigned int nPriority;
+    printf("ipcThread started\n");
+
+    interprocess::message_queue* mq = (interprocess::message_queue*)pArg;
+    char buffer[IPC_MQ_MAX_MESSAGE_SIZE + 1] = "";
+    size_t nSize = 0;
+    unsigned int nPriority = 0;
+
     loop
     {
-        ptime d = boost::posix_time::microsec_clock::universal_time() + millisec(100);
-        if(mq->timed_receive(&strBuf, sizeof(strBuf), nSize, nPriority, d))
+        if (mq->try_receive(&buffer, sizeof(buffer), nSize, nPriority))
         {
-            uiInterface.ThreadSafeHandleURI(std::string(strBuf, nSize));
+            uiInterface.ThreadSafeHandleURI(std::string(buffer, nSize));
             Sleep(1000);
         }
+        else
+            /** needs to be here to stop this thread from utilizing 100% of a CPU core */
+            Sleep(100);
+
         if (fShutdown)
-        {
-            ipcShutdown();
             break;
-        }
     }
-    ipcShutdown();
+
+    /** cleanup allocated memory and set global IPC state to not initialized */
+    delete mq;
+    globalIpcState = IPC_NOT_INITIALIZED;
 }
 
-void ipcInit()
+void ipcInit(bool fUseMQModeOpenOnly, bool fInitCalledAfterRecovery)
 {
-#ifdef MAC_OSX
-    // TODO: implement bitcoin: URI handling the Mac Way
-    return;
-#endif
-#ifdef WIN32
-    // TODO: THOROUGHLY test boost::interprocess fix,
-    // and make sure there are no Windows argument-handling exploitable
-    // problems.
-    return;
-#endif
+    /** set global IPC state variable to not initialized */
+    globalIpcState = IPC_NOT_INITIALIZED;
 
-    message_queue* mq;
-    char strBuf[257];
-    size_t nSize;
-    unsigned int nPriority;
+    interprocess::message_queue* mq = NULL;
+
     try {
-        mq = new message_queue(open_or_create, BITCOINURI_QUEUE_NAME, 2, 256);
-
-        // Make sure we don't lose any bitcoin: URIs
-        for (int i = 0; i < 2; i++)
-        {
-            ptime d = boost::posix_time::microsec_clock::universal_time() + millisec(1);
-            if(mq->timed_receive(&strBuf, sizeof(strBuf), nSize, nPriority, d))
-            {
-                uiInterface.ThreadSafeHandleURI(std::string(strBuf, nSize));
-            }
-            else
-                break;
-        }
-
-        // Make sure only one bitcoin instance is listening
-        message_queue::remove(BITCOINURI_QUEUE_NAME);
-        mq = new message_queue(open_or_create, BITCOINURI_QUEUE_NAME, 2, 256);
+        if (fUseMQModeOpenOnly)
+            mq = new interprocess::message_queue(interprocess::open_only, IPC_MQ_NAME);
+        else
+            mq = new interprocess::message_queue(interprocess::create_only, IPC_MQ_NAME, IPC_MQ_MAX_MESSAGES, IPC_MQ_MAX_MESSAGE_SIZE);
     }
-    catch (interprocess_exception &ex) {
+    catch (interprocess::interprocess_exception &ex) {
+#ifdef WIN32
+        /** check if the exception is a "file not found" error */
+        if(ex.get_error_code() == interprocess::not_found_error)
+        {
+            if (!fInitCalledAfterRecovery)
+            {
+                printf("ipcInit - trying to create new message queue...\n");
+
+                /** try init once more (false - create_only mode / true - avoid an infinite recursion)
+                 * create_only: create new message queue
+                 */
+                ipcInit(false, true);
+            }
+        }
+        /** check if the exception is a "file already exists" error */
+        else if (ex.get_error_code() == interprocess::already_exists_error)
+        {
+            if (!fInitCalledAfterRecovery)
+            {
+                printf("ipcInit - trying to open current message queue...\n");
+
+                /** try init once more (true - open_only mode / true - avoid an infinite recursion)
+                 * open_only: try to open the existing queue
+                 */
+                ipcInit(true, true);
+            }
+        }
+        else
+            printf("ipcInit - boost interprocess exception #%d: %s\n", ex.get_error_code(), ex.what());
+#endif
         return;
     }
+
     if (!CreateThread(ipcThread, mq))
     {
         delete mq;
+        return;
     }
+
+    /** if we reach this, set global IPC state to initialized */
+    globalIpcState = IPC_INITIALIZED;
 }
