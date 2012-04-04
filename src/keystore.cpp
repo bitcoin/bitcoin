c7057326eaba2d50410016a9825f0052dd0248b5
Pieter Wuille <pieter.wuille@gmail.com>
2012-04-05 01:02:49 +0200

    Verify status of encrypt/decrypt calls to detect failed padding

--- src/keystore.cpp
+++ src/keystore.cpp
@@ -71,6 +71,8 @@ bool CCryptoKeyStore::Unlock(const CKeyingMaterial& vMasterKeyIn)
             CSecret vchSecret;
             if(!DecryptSecret(vMasterKeyIn, vchCryptedSecret, Hash(vchPubKey.begin(), vchPubKey.end()), vchSecret))
                 return false;
+            if (vchSecret.size() != 32)
+                return false;
             CKey key;
             key.SetSecret(vchSecret);
             if (key.GetPubKey() == vchPubKey)
@@ -130,6 +132,8 @@ bool CCryptoKeyStore::GetKey(const CBitcoinAddress &address, CKey& keyOut) const
             const std::vector<unsigned char> &vchCryptedSecret = (*mi).second.second;
             CSecret vchSecret;
             if (!DecryptSecret(vMasterKey, vchCryptedSecret, Hash(vchPubKey.begin(), vchPubKey.end()), vchSecret))
+                return false;
+            if (vchSecret.size() != 32)
                 return false;
             keyOut.SetSecret(vchSecret);
             return true;
