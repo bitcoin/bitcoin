ca3069fec701d1422f1fd8f498bc800a7d509438
Jeff Garzik <jeff@garzik.org>
2012-04-12 12:23:58 -0400

    version.h: separate client, net sections.  Move more constants to this file.
    * move PROTOCOL_VERSION to version.h
    
    * move CLIENT_VERSION* to version.h, make available past cpp stage
    
    * clearly separate client, network version portions of version.h

--- src/version.h
+++ src/version.h
@@ -6,12 +6,32 @@
 
 #include <string>
 
+//
+// client versioning
+//
+
+static const int CLIENT_VERSION_MAJOR       =  0;
+static const int CLIENT_VERSION_MINOR       =  6;
+static const int CLIENT_VERSION_REVISION    =  0;
+static const int CLIENT_VERSION_BUILD       = 99;
+
+static const int CLIENT_VERSION =
+                           1000000 * CLIENT_VERSION_MAJOR
+                         +   10000 * CLIENT_VERSION_MINOR 
+                         +     100 * CLIENT_VERSION_REVISION
+                         +       1 * CLIENT_VERSION_BUILD;
+
 extern const std::string CLIENT_NAME;
 extern const std::string CLIENT_BUILD;
 extern const std::string CLIENT_DATE;
-extern const int         CLIENT_VERSION;
+
+//
+// network protocol versioning
+//
+
+static const int PROTOCOL_VERSION = 60000;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
-const int BIP0031_VERSION = 60000;
+static const int BIP0031_VERSION = 60000;
 
 #endif
