8b09cd3a4db2d712e91826d44a0e777478b450c6
Jeff Garzik <jeff@garzik.org>
2012-04-12 20:07:49 -0400

    Replace several network protocol version numbers with named constants stored in version.h.
    Also, a minor CAddress code reformat while we're in there, fixing
    some incorrect indentation.

--- src/version.h
+++ src/version.h
@@ -31,6 +31,17 @@ extern const std::string CLIENT_DATE;
 
 static const int PROTOCOL_VERSION = 60000;
 
+// earlier versions not supported as of Feb 2012, and are disconnected
+static const int MIN_PROTO_VERSION = 209;
+
+// nTime field added to CAddress, starting with this version;
+// if possible, avoid requesting addresses nodes older than this
+static const int CADDR_TIME_VERSION = 31402;
+
+// only request blocks from nodes outside this range of versions
+static const int NOBLKS_VERSION_START = 32000;
+static const int NOBLKS_VERSION_END = 32400;
+
 // BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
 
