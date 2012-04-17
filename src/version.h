3414a7827588097776d909c243e61464c525a0d7
Jeff Garzik <jeff@garzik.org>
2012-04-17 12:16:46 -0400

    Bump PROTOCOL_VERSION to 60001, thereby enabling BIP31

--- src/version.h
+++ src/version.h
@@ -29,7 +29,7 @@ extern const std::string CLIENT_DATE;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 60000;
+static const int PROTOCOL_VERSION = 60001;
 
 // earlier versions not supported as of Feb 2012, and are disconnected
 static const int MIN_PROTO_VERSION = 209;
