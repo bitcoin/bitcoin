767c7e63b4a125aac97c03a8ad5bf4b0b7daa83a
Philip Kaufmann <phil.kaufmann@t-online.de>
2012-05-04 18:19:06 +0200

    move label placeholdertext from sendcoinsentry.cpp to ui-file

--- src/qt/sendcoinsentry.cpp
+++ src/qt/sendcoinsentry.cpp
@@ -21,10 +21,6 @@ SendCoinsEntry::SendCoinsEntry(QWidget *parent) :
     ui->payToLayout->setSpacing(4);
 #endif
 
-#if QT_VERSION >= 0x040700
-    ui->payTo->setPlaceholderText(tr("Enter a Bitcoin address (e.g. 1NS17iag9jJgTHD1VXjvLCEnZuQ3rJDE9L)"));
-    ui->addAsLabel->setPlaceholderText(tr("Enter a label for this address to add it to your address book"));
-#endif
     setFocusPolicy(Qt::TabFocus);
     setFocusProxy(ui->payTo);
 
