fd1a899efcc2061043e81f443176e8525633287c
Wladimir J. van der Laan <laanwj@gmail.com>
2012-05-07 00:19:22 +0200

    Move back setPlaceholderText to code, from xml, as this broke building with Qt 4.6

--- src/qt/sendcoinsentry.cpp
+++ src/qt/sendcoinsentry.cpp
@@ -20,7 +20,11 @@ SendCoinsEntry::SendCoinsEntry(QWidget *parent) :
 #ifdef Q_WS_MAC
     ui->payToLayout->setSpacing(4);
 #endif
-
+#if QT_VERSION >= 0x040700
+    /* Do not move this to the XML file, Qt before 4.7 will choke on it */
+    ui->addAsLabel->setPlaceholderText(tr("Enter a label for this address to add it to your address book"));
+    ui->payTo->setPlaceholderText(tr("Enter a Bitcoin address (e.g. 1NS17iag9jJgTHD1VXjvLCEnZuQ3rJDE9L)"));
+#endif
     setFocusPolicy(Qt::TabFocus);
     setFocusProxy(ui->payTo);
 
