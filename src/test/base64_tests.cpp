f621326c24791be14aa61221f7e35530378b84d8
Pieter Wuille <pieter.wuille@gmail.com>
2012-05-09 03:48:14 +0200

    Clean up warnings
    * Use -Wall -Wextra -Wformat -Wformat-security -Wno-unused-parameters
    * Remove xCXXFLAGS usage in makefile.unix
    * Fix several recent and older sign-compare warnings

--- src/test/base64_tests.cpp
+++ src/test/base64_tests.cpp
@@ -10,7 +10,7 @@ BOOST_AUTO_TEST_CASE(base64_testvectors)
 {
     static const std::string vstrIn[]  = {"","f","fo","foo","foob","fooba","foobar"};
     static const std::string vstrOut[] = {"","Zg==","Zm8=","Zm9v","Zm9vYg==","Zm9vYmE=","Zm9vYmFy"};
-    for (int i=0; i<sizeof(vstrIn)/sizeof(vstrIn[0]); i++)
+    for (unsigned int i=0; i<sizeof(vstrIn)/sizeof(vstrIn[0]); i++)
     {
         std::string strEnc = EncodeBase64(vstrIn[i]);
         BOOST_CHECK(strEnc == vstrOut[i]);
