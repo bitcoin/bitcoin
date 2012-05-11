b4537e68b9b388ebc1c8027d69b77c9df8af30b8
Gavin Andresen <gavinandresen@gmail.com>
2012-05-11 10:09:45 -0400

    Bump version to 0.6.2.2 for osx-special build

--- src/version.h
+++ src/version.h
@@ -13,7 +13,7 @@
 static const int CLIENT_VERSION_MAJOR       =  0;
 static const int CLIENT_VERSION_MINOR       =  6;
 static const int CLIENT_VERSION_REVISION    =  2;
-static const int CLIENT_VERSION_BUILD       = 0;
+static const int CLIENT_VERSION_BUILD       = 2;
 
 static const int CLIENT_VERSION =
                            1000000 * CLIENT_VERSION_MAJOR
