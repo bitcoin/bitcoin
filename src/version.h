7532c476fe5ce004112dd501b4707578ab5f3432
Pieter Wuille <pieter.wuille@gmail.com>
2012-05-11 04:11:22 +0200

    Fix version numbers of archive builds

--- src/version.h
+++ src/version.h
@@ -10,10 +10,11 @@
 // client versioning
 //
 
-static const int CLIENT_VERSION_MAJOR       =  0;
-static const int CLIENT_VERSION_MINOR       =  6;
-static const int CLIENT_VERSION_REVISION    =  2;
-static const int CLIENT_VERSION_BUILD       = 2;
+// These need to be macro's, as version.cpp's voodoo requires it
+#define CLIENT_VERSION_MAJOR       0
+#define CLIENT_VERSION_MINOR       6
+#define CLIENT_VERSION_REVISION    2
+#define CLIENT_VERSION_BUILD       2
 
 static const int CLIENT_VERSION =
                            1000000 * CLIENT_VERSION_MAJOR
