65ab3267021fb2e6da17e68ec8a9b5673967d9cb
Fordy <fanquake@gmail.com>
2012-05-17 18:51:23 +0800

    update license

--- src/crypter.h
+++ src/crypter.h
@@ -1,6 +1,6 @@
 // Copyright (c) 2009-2012 The Bitcoin Developers
 // Distributed under the MIT/X11 software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// file license.txt or http://www.opensource.org/licenses/mit-license.php.
 #ifndef __CRYPTER_H__
 #define __CRYPTER_H__
 
