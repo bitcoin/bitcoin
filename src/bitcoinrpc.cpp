1d764d631f1e2249e5206165a1e690e0ffe44515
R E Broadley <rebroad+github@gmail.com>
2012-05-17 18:52:38 +0100

    Corrected grammar. As per Principle Of Least Surprise.

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -2621,7 +2621,7 @@ void ThreadRPCServer(void* parg)
         vnThreadsRunning[THREAD_RPCLISTENER]--;
         PrintException(NULL, "ThreadRPCServer()");
     }
-    printf("ThreadRPCServer exiting\n");
+    printf("ThreadRPCServer exited\n");
 }
 
 void ThreadRPCServer2(void* parg)
