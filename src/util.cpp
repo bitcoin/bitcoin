3a25a2b9b0877e3276d9057752a4362211717b09
Fordy <fanquake@gmail.com>
2012-05-18 22:02:28 +0800

    Update License in File Headers
    I originally created a pull to replace the "COPYING" in crypter.cpp and
    crypter.h, but it turned out that COPYING was actually the correct
    file.

--- src/util.cpp
+++ src/util.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2012 The Bitcoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
-// file license.txt or http://www.opensource.org/licenses/mit-license.php.
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "util.h"
 #include "sync.h"
