b2de28c74040595fa3fe5353ea063a8c3874f6b1
Fordy <fanquake@gmail.com>
2012-05-18 22:02:28 +0800

    Update License in File Headers
    I originally created a pull to replace the "COPYING" in crypter.cpp and
    crypter.h, but it turned out that COPYING was actually the correct
    file.

--- src/qtui.h
+++ src/qtui.h
@@ -1,6 +1,6 @@
 // Copyright (c) 2010 Satoshi Nakamoto
 // Distributed under the MIT/X11 software license, see the accompanying
-// file license.txt or http://www.opensource.org/licenses/mit-license.php.
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #ifndef BITCOIN_EXTERNUI_H
 #define BITCOIN_EXTERNUI_H
 
