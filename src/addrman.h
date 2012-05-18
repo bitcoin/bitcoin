d11488abd05cb39a9f481e7c4c35f780197a3d28
Fordy <fanquake@gmail.com>
2012-05-18 22:02:28 +0800

    Update License in File Headers
    I originally created a pull to replace the "COPYING" in crypter.cpp and
    crypter.h, but it turned out that COPYING was actually the correct
    file.

--- src/addrman.h
+++ src/addrman.h
@@ -1,6 +1,6 @@
 // Copyright (c) 2012 Pieter Wuille
 // Distributed under the MIT/X11 software license, see the accompanying
-// file license.txt or http://www.opensource.org/licenses/mit-license.php.
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #ifndef _BITCOIN_ADDRMAN
 #define _BITCOIN_ADDRMAN 1
 
--- src/netbase.h
+++ src/netbase.h
@@ -1,6 +1,6 @@
 // Copyright (c) 2009-2012 The Bitcoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
-// file license.txt or http://www.opensource.org/licenses/mit-license.php.
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #ifndef BITCOIN_NETBASE_H
 #define BITCOIN_NETBASE_H
 
