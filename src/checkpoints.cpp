940ff1934de8079fb502f56e7d10de304123adec
Fordy <fanquake@gmail.com>
2012-05-18 22:02:28 +0800

    Update License in File Headers
    I originally created a pull to replace the "COPYING" in crypter.cpp and
    crypter.h, but it turned out that COPYING was actually the correct
    file.

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -1,6 +1,6 @@
 // Copyright (c) 2009-2012 The Bitcoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
-// file license.txt or http://www.opensource.org/licenses/mit-license.php.
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include <boost/assign/list_of.hpp> // for 'map_list_of()'
 #include <boost/foreach.hpp>
