07f23fcdef96c0bfb1e502ed0e836fab5948b828
Philip Kaufmann <phil.kaufmann@t-online.de>
2012-05-21 23:05:54 +0200

    add client startup time as an entry to debug.log (note: logged time in debug.log differs by a few seconds from the one displayed in the Debug window) / make ClientModel::formatClientStartupTime() return a QString

--- src/qt/clientmodel.h
+++ src/qt/clientmodel.h
@@ -41,7 +41,7 @@ public:
     QString formatFullVersion() const;
     QString formatBuildDate() const;
     QString clientName() const;
-    QDateTime formatClientStartupTime() const;
+    QString formatClientStartupTime() const;
 
 private:
     OptionsModel *optionsModel;
