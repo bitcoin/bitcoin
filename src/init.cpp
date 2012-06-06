cecc5fff6cbf8f507bbc221c527cd8dcbadd3c0e
R E Broadley <rebroad+github@gmail.com>
2012-06-06 12:30:09 +0100

    No need to log start time if it's already being done on every line.

--- src/init.cpp
+++ src/init.cpp
@@ -470,6 +470,8 @@ bool AppInit2()
     printf("Bitcoin version %s (%s)\n", FormatFullVersion().c_str(), CLIENT_DATE.c_str());
     printf("Using OpenSSL version %s\n", SSLeay_version(SSLEAY_VERSION));
     printf("Startup time: %s\n", DateTimeStrFormat("%x %H:%M:%S", GetTime()).c_str());
+    if (!fLogTimeStamps)
+        printf("Startup time: %s\n", DateTimeStrFormat("%x %H:%M:%S", GetTime()).c_str());
     printf("Default data directory %s\n", GetDefaultDataDir().string().c_str());
     printf("Used data directory %s\n", GetDataDir().string().c_str());
     std::ostringstream strErrors;
