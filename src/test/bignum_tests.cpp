8616daea003445d8c3d2e9f5261db00d4e184aa1
Ricardo M. Correia <rcorreia@wizy.org>
2012-06-07 19:14:18 +0200

    Move NOINLINE definition to test where it's used.

--- src/test/bignum_tests.cpp
+++ src/test/bignum_tests.cpp
@@ -6,6 +6,22 @@
 
 BOOST_AUTO_TEST_SUITE(bignum_tests)
 
+// Unfortunately there's no standard way of preventing a function from being
+// inlined, so we define a macro for it.
+//
+// You should use it like this:
+//   NOINLINE void function() {...}
+#if defined(__GNUC__)
+// This also works and will be defined for any compiler implementing gcc
+// extensions, such as clang and icc.
+#define NOINLINE __attribute__((noinline))
+#elif defined(_MSC_VER)
+#define NOINLINE __declspec(noinline)
+#else
+// We give out a warning because it impacts the correctness of one bignum test.
+#warning You should define NOINLINE for your compiler.
+#define NOINLINE
+#endif
 
 // For the following test case, it is useful to use additional tools.
 //
