000f217369eda29a0d520932b27e2b0d792152b0
Michael Ford <fanquake@gmail.com>
2012-06-25 12:28:29 +0800

    Add Bitcoin dev Copyright

--- src/ui_interface.h
+++ src/ui_interface.h
@@ -1,4 +1,5 @@
 // Copyright (c) 2010 Satoshi Nakamoto
+// Copyright (c) 2012 The Bitcoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #ifndef BITCOIN_UI_INTERFACE_H
