1c644b58f913a31d0c4f45ef4130c34ec1807415
Sunny King <sunnyking9999@gmail.com>
2012-06-26 17:14:15 +0100

    PPCoin: Matching prefix of public key for RPC command 'makekeypair'

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -1889,10 +1889,23 @@ Value repairwallet(const Array& params, bool fHelp)
 // ppcoin: make a public-private key pair
 Value makekeypair(const Array& params, bool fHelp)
 {
-    CKey key;
-    key.MakeNewKey();
-    CPrivKey vchPrivKey = key.GetPrivKey();
+    if (fHelp || params.size() > 1)
+        throw runtime_error(
+            "makekeypair [prefix]\n"
+            "Make a public/private key pair.\n"
+            "[prefix] is optional preferred prefix for the public key.\n");
 
+    string strPrefix = "";
+    if (params.size() > 0)
+        strPrefix = params[0].get_str();
+ 
+    CKey key;
+    do
+    {
+        key.MakeNewKey();
+    } while (strPrefix != HexStr(key.GetPubKey()).substr(0, strPrefix.size()));
+
+    CPrivKey vchPrivKey = key.GetPrivKey();
     Object result;
     result.push_back(Pair("PrivateKey", HexStr<CPrivKey::iterator>(vchPrivKey.begin(), vchPrivKey.end())));
     result.push_back(Pair("PublicKey", HexStr(key.GetPubKey())));
