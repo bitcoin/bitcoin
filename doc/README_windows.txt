b9f4be3a4d6cb6886f9055e429bde7e8f4179acd
coblee <chocobo@alum.mit.edu>
2012-06-29 13:29:22 -0700

    Fix litecoin ports to 9333

--- doc/README_windows.txt
+++ doc/README_windows.txt
@@ -27,7 +27,7 @@ select Excluded processes, press Add, select bitcoin.exe, OK, Save changes.
 
 The software automatically finds other nodes to connect to.  You can
 enable Universal Plug and Play using a menu entry or set your firewall
-to forward port 8333 (TCP) to your computer so you can receive
+to forward port 9333 (TCP) to your computer so you can receive
 incoming connections.  Litecoin works without incoming connections,
 but allowing incoming connections helps the Litecoin network.
 
--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -3039,7 +3039,7 @@ void ThreadRPCServer2(void* parg)
     // Try a dual IPv6/IPv4 socket, falling back to separate IPv4 and IPv6 sockets
     const bool loopback = !mapArgs.count("-rpcallowip");
     asio::ip::address bindAddress = loopback ? asio::ip::address_v6::loopback() : asio::ip::address_v6::any();
-    ip::tcp::endpoint endpoint(bindAddress, GetArg("-rpcport", 8332));
+    ip::tcp::endpoint endpoint(bindAddress, GetArg("-rpcport", 9332));
 
     boost::signals2::signal<void ()> StopRequests;
 
@@ -3248,7 +3248,7 @@ Object CallRPC(const string& strMethod, const Array& params)
     asio::ssl::stream<asio::ip::tcp::socket> sslStream(io_service, context);
     SSLIOStreamDevice<asio::ip::tcp> d(sslStream, fUseSSL);
     iostreams::stream< SSLIOStreamDevice<asio::ip::tcp> > stream(d);
-    if (!d.connect(GetArg("-rpcconnect", "127.0.0.1"), GetArg("-rpcport", "8332")))
+    if (!d.connect(GetArg("-rpcconnect", "127.0.0.1"), GetArg("-rpcport", "9332")))
         throw runtime_error("couldn't connect to server");
 
     // HTTP basic authentication
