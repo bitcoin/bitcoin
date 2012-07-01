dfed574b88d9bfcf95f743a65ab4e14c48800388
Jeff Garzik <jgarzik@exmulti.com>
2012-06-30 23:40:26 -0400

    RPC getpeerinfo: s/height/startingheight/ for returned nStartingHeight value

--- src/rpcnet.cpp
+++ src/rpcnet.cpp
@@ -56,7 +56,7 @@ Value getpeerinfo(const Array& params, bool fHelp)
         obj.push_back(Pair("subver", stats.strSubVer));
         obj.push_back(Pair("inbound", stats.fInbound));
         obj.push_back(Pair("releasetime", (boost::int64_t)stats.nReleaseTime));
-        obj.push_back(Pair("height", stats.nStartingHeight));
+        obj.push_back(Pair("startingheight", stats.nStartingHeight));
         obj.push_back(Pair("banscore", stats.nMisbehavior));
 
         ret.push_back(obj);
