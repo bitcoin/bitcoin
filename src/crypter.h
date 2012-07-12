b2526f88c3c2daca68916f2fcedcfcb9b8c6f042
fanquake <fanquake@gmail.com>
2012-07-12 18:45:56 +0800

    Add LItecoin Dev Copyright to source files.

--- src/crypter.h
+++ src/crypter.h
@@ -1,4 +1,5 @@
 // Copyright (c) 2009-2012 The Bitcoin Developers
+// Copyright (c) 2011-2012 Litecoin Developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #ifndef __CRYPTER_H__
