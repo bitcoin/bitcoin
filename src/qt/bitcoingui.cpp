bb583e3c11aa630726bf57f21f10d60c85dbca47
Philip Kaufmann <phil.kaufmann@t-online.de>
2012-07-13 08:46:09 +0200

    when on testnet, set testnet icon for about dialog
    - add a comment

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -285,6 +285,7 @@ void BitcoinGUI::setClientModel(ClientModel *clientModel)
     this->clientModel = clientModel;
     if(clientModel)
     {
+        // Replace some strings and icons, when using the testnet
         if(clientModel->isTestNet())
         {
             QString title_testnet = windowTitle() + QString(" ") + tr("[testnet]");
@@ -299,6 +300,8 @@ void BitcoinGUI::setClientModel(ClientModel *clientModel)
                 trayIcon->setToolTip(title_testnet);
                 trayIcon->setIcon(QIcon(":/icons/toolbar_testnet"));
             }
+
+            aboutAction->setIcon(QIcon(":/icons/toolbar_testnet"));
         }
 
         // Keep up to date with client
