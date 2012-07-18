17ff76ab52d9777d4ff3425d1e61de24d2fac9e4
fanquake <fanquake@gmail.com>
2012-07-18 08:53:15 +0800

    Use litecoin.org for unit tests

--- src/test/netbase_tests.cpp
+++ src/test/netbase_tests.cpp
@@ -52,10 +52,10 @@ bool static TestSplitHost(string test, string host, int port)
 
 BOOST_AUTO_TEST_CASE(netbase_splithost)
 {
-    BOOST_CHECK(TestSplitHost("www.bitcoin.org", "www.bitcoin.org", -1));
-    BOOST_CHECK(TestSplitHost("[www.bitcoin.org]", "www.bitcoin.org", -1));
-    BOOST_CHECK(TestSplitHost("www.bitcoin.org:80", "www.bitcoin.org", 80));
-    BOOST_CHECK(TestSplitHost("[www.bitcoin.org]:80", "www.bitcoin.org", 80));
+    BOOST_CHECK(TestSplitHost("www.litecoin.org", "www.litecoin.org", -1));
+    BOOST_CHECK(TestSplitHost("[www.litecoin.org]", "www.litecoin.org", -1));
+    BOOST_CHECK(TestSplitHost("www.litecoin.org:80", "www.litecoin.org", 80));
+    BOOST_CHECK(TestSplitHost("[www.litecoin.org]:80", "www.litecoin.org", 80));
     BOOST_CHECK(TestSplitHost("127.0.0.1", "127.0.0.1", -1));
     BOOST_CHECK(TestSplitHost("127.0.0.1:8333", "127.0.0.1", 8333));
     BOOST_CHECK(TestSplitHost("[127.0.0.1]", "127.0.0.1", -1));
