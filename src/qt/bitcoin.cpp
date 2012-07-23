b8dcb38b4ec674282a730c0eef214794502d9f4e
fanquake <fanquake@gmail.com>
2012-07-23 11:59:02 +0800

    Fix Typo

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -130,7 +130,7 @@ std::string _(const char* psz)
 static void handleRunawayException(std::exception *e)
 {
     PrintExceptionContinue(e, "Runaway exception");
-    QMessageBox::critical(0, "Runaway exception", BitcoinGUI::tr("A fatal error occured. Bitcoin can no longer continue safely and will quit.") + QString("\n\n") + QString::fromStdString(strMiscWarning));
+    QMessageBox::critical(0, "Runaway exception", BitcoinGUI::tr("A fatal error occurred. Bitcoin can no longer continue safely and will quit.") + QString("\n\n") + QString::fromStdString(strMiscWarning));
     exit(1);
 }
 
