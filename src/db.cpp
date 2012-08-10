4fea06db25108e7f72710bf22c3d1896707eeb74
Pieter Wuille <pieter.wuille@gmail.com>
2012-08-10 15:13:57 +0200

    Automatically reorganize at startup to best known block
    Given that the block tree database (chain.dat) and the active chain
    database (coins.dat) are entirely separate now, it becomes legal to
    swap one with another instance without affecting the other.
    
    This commit introduces a check in the startup code that detects the
    presence of a better chain in chain.dat that has not been activated
    yet, and does so efficiently (in batch, while reusing the blk???.dat
    files).

--- src/db.cpp
+++ src/db.cpp
@@ -636,7 +636,6 @@ bool LoadBlockIndex(CChainDB &chaindb)
     {
         if (pindexGenesisBlock == NULL)
             return true;
-        return error("CTxDB::LoadBlockIndex() : hashBestChain not loaded");
     }
     hashBestChain = pindexBest->GetBlockHash();
     nBestHeight = pindexBest->nHeight;
