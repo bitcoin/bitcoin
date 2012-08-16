da2e56f7d2d391ff15e1cd9a99a3ecd25d7bf90a
Philip Kaufmann <phil.kaufmann@t-online.de>
2012-08-17 00:14:40 +0200

    log how long LoadExternalBlockFile() takes in ms (Benchmark)

--- src/main.cpp
+++ src/main.cpp
@@ -2146,6 +2146,8 @@ void PrintBlockTree()
 
 bool LoadExternalBlockFile(FILE* fileIn)
 {
+    int64 nStart = GetTimeMillis();
+
     int nLoaded = 0;
     {
         LOCK(cs_main);
@@ -2198,7 +2200,7 @@ bool LoadExternalBlockFile(FILE* fileIn)
                    __PRETTY_FUNCTION__);
         }
     }
-    printf("Loaded %i blocks from external file\n", nLoaded);
+    printf("Loaded %i blocks from external file in %"PRI64d"ms\n", nLoaded, GetTimeMillis() - nStart);
     return nLoaded > 0;
 }
 
