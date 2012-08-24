bfe7cc638d1f6a11a030730130262e49eb6e7940
Wladimir J. van der Laan <laanwj@gmail.com>
2012-08-24 14:47:54 +0200

    Override progress bar on platforms with segmented progress bars
    Windows & WindowsXP style have a problem with displaying the block progress.
    Add a custom stylesheet as workaround, but only when one of those renderers is active,
    otherwise leave the theme alone (issue #1071).

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -52,6 +52,7 @@
 
 #include <QDragEnterEvent>
 #include <QUrl>
+#include <QStyle>
 
 #include <iostream>
 
@@ -140,6 +141,15 @@ BitcoinGUI::BitcoinGUI(QWidget *parent):
     progressBar = new QProgressBar();
     progressBar->setToolTip(tr("Block chain synchronization in progress"));
     progressBar->setVisible(false);
+
+    // Override style sheet for progress bar for styles that have a segmented progress bar,
+    // as they make the text unreadable (workaround for issue #1071)
+    // See https://qt-project.org/doc/qt-4.8/gallery.html
+    QString curStyle = qApp->style()->metaObject()->className();
+    if(curStyle == "QWindowsStyle" || curStyle == "QWindowsXPStyle")
+    {
+        progressBar->setStyleSheet("QProgressBar { background-color: #e8e8e8; border: 1px solid grey; border-radius: 7px; padding: 1px; text-align: center; } QProgressBar::chunk { background: QLinearGradient(x1: 0, y1: 0, x2: 1, y2: 0, stop: 0 #FF8000, stop: 1 orange); border-radius: 7px; margin: 0px; }");
+    }
 
     statusBar()->addWidget(progressBarLabel);
     statusBar()->addWidget(progressBar);
