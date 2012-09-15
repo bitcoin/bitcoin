1d892253201258dab8db9c6254fa75bdb5b09927
Wladimir J. van der Laan <laanwj@gmail.com>
2012-09-15 12:10:00 +0200

    update comment, secure_allocator is defined in allocators.h

--- src/key.h
+++ src/key.h
@@ -43,7 +43,7 @@ public:
 };
 
 
-// secure_allocator is defined in serialize.h
+// secure_allocator is defined in allocators.h
 // CPrivKey is a serialized private key, with all parameters included (279 bytes)
 typedef std::vector<unsigned char, secure_allocator<unsigned char> > CPrivKey;
 // CSecret is a serialization of just the secret parameter (32 bytes)
