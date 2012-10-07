3f2ebf20d31fa0fc64ffa9834a85d64a0de71a94
Pieter Wuille <pieter.wuille@gmail.com>
2012-10-07 14:53:17 +0200

    Remove I2P support from netbase
    I2P apparently needs 256 bits to store a fully routable address. Garlicat
    requires a centralized lookup service to map the 80-bit addresses to fully
    routable ones (as far as I understood), so that's not really usable in our
    situation.
    
    To support I2P routing and peer exchange for it, another solution is needed.
    This will most likely imply a network protocol change, and extension of the
    'addr' message.

--- src/netbase.h
+++ src/netbase.h
@@ -23,7 +23,6 @@ enum Network
     NET_IPV4,
     NET_IPV6,
     NET_TOR,
-    NET_I2P,
 
     NET_MAX,
 };
@@ -44,9 +43,9 @@ class CNetAddr
         explicit CNetAddr(const std::string &strIp, bool fAllowLookup = false);
         void Init();
         void SetIP(const CNetAddr& ip);
-        bool SetSpecial(const std::string &strName); // for Tor and I2P addresses
+        bool SetSpecial(const std::string &strName); // for Tor addresses
         bool IsIPv4() const;    // IPv4 mapped address (::FFFF:0:0/96, 0.0.0.0/0)
-        bool IsIPv6() const;    // IPv6 address (not mapped IPv4, not Tor/I2P)
+        bool IsIPv6() const;    // IPv6 address (not mapped IPv4, not Tor)
         bool IsRFC1918() const; // IPv4 private networks (10.0.0.0/8, 192.168.0.0/16, 172.16.0.0/12)
         bool IsRFC3849() const; // IPv6 documentation address (2001:0DB8::/32)
         bool IsRFC3927() const; // IPv4 autoconfig (169.254.0.0/16)
@@ -58,7 +57,6 @@ class CNetAddr
         bool IsRFC6052() const; // IPv6 well-known prefix (64:FF9B::/96)
         bool IsRFC6145() const; // IPv6 IPv4-translated address (::FFFF:0:0:0/96)
         bool IsTor() const;
-        bool IsI2P() const;
         bool IsLocal() const;
         bool IsRoutable() const;
         bool IsValid() const;
