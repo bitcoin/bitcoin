f057fcfcfba2d0331906177efd9e48908edb7366
tucenaber <tucenaber@gmail.com>
2012-10-23 22:41:47 +0200

    Added checks for null pointers in Shutdown

--- src/init.cpp
+++ src/init.cpp
@@ -78,8 +78,14 @@ void Shutdown(void* parg)
         StopNode();
         {
             LOCK(cs_main);
-            pcoinsTip->Flush();
-            pblocktree->Flush();
+            if (pcoinsTip)
+            {
+                pcoinsTip->Flush();
+            }
+            if (pblocktree)
+            {
+                pblocktree->Flush();
+            }
             delete pcoinsTip;
             delete pcoinsdbview;
             delete pblocktree;
