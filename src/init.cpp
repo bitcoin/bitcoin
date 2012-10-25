1533233a34b00559bf8c1314354f9501d4d1c336
Pieter Wuille <pieter.wuille@gmail.com>
2012-10-25 20:47:11 +0200

    First flush block tree, then coin set
    As the coinset data refers to the best block, stored in the block
    tree. Flushing the coin set first can cause inconsistencies if
    the process gets killed in between.

--- src/init.cpp
+++ src/init.cpp
@@ -78,10 +78,10 @@ void Shutdown(void* parg)
         StopNode();
         {
             LOCK(cs_main);
-            if (pcoinsTip)
-                pcoinsTip->Flush();
             if (pblocktree)
                 pblocktree->Flush();
+            if (pcoinsTip)
+                pcoinsTip->Flush();
             delete pcoinsTip;
             delete pcoinsdbview;
             delete pblocktree;
