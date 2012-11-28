2fa4d8b6726c6e344574a604c7715bc434bd2ea7
Pieter Wuille <pieter.wuille@gmail.com>
2012-11-28 21:58:41 +0100

    Make test_bitcoin run in a temp datadir

--- src/test/test_bitcoin.cpp
+++ src/test/test_bitcoin.cpp
@@ -1,10 +1,12 @@
 #define BOOST_TEST_MODULE Bitcoin Test Suite
 #include <boost/test/unit_test.hpp>
+#include <boost/filesystem.hpp>
 
 #include "db.h"
 #include "txdb.h"
 #include "main.h"
 #include "wallet.h"
+#include "util.h"
 
 CWallet* pwalletMain;
 CClientUIInterface uiInterface;
@@ -14,11 +16,15 @@ extern void noui_connect();
 
 struct TestingSetup {
     CCoinsViewDB *pcoinsdbview;
+    boost::filesystem::path pathTemp;
 
     TestingSetup() {
         fPrintToDebugger = true; // don't want to write to debug.log file
         noui_connect();
         bitdb.MakeMock();
+        pathTemp = GetTempPath() / strprintf("test_bitcoin_%lu_%i", (unsigned long)GetTime(), (int)(GetRand(100000)));
+        boost::filesystem::create_directories(pathTemp);
+        mapArgs["-datadir"] = pathTemp.string();
         pblocktree = new CBlockTreeDB(1 << 20, true);
         pcoinsdbview = new CCoinsViewDB(1 << 23, true);
         pcoinsTip = new CCoinsViewCache(*pcoinsdbview);
@@ -36,6 +42,7 @@ struct TestingSetup {
         delete pcoinsdbview;
         delete pblocktree;
         bitdb.Flush(true);
+        boost::filesystem::remove_all(pathTemp);
     }
 };
 
