acda14866a6ea2c4695f8bbc583bff8a63d2dcc7
Mike Gogulski <mike@gogulski.com>
2012-12-03 16:44:32 +0100

    remove commented-out extern declaration for ValueFromAmount, now inlined in wallet.h

--- src/bitcoinrpc.h
+++ src/bitcoinrpc.h
@@ -124,7 +124,6 @@ extern const CRPCTable tableRPC;
 
 extern int64 nWalletUnlockTime;
 extern int64 AmountFromValue(const json_spirit::Value& value);
-//extern json_spirit::Value ValueFromAmount(int64 amount);
 extern double GetDifficulty(const CBlockIndex* blockindex = NULL);
 extern std::string HexBits(unsigned int nBits);
 extern std::string HelpRequiringPassphrase();
