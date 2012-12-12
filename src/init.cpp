303b0009dc4ef3dbe4d9caabfe94798ee51579d4
Richard Schwab <mail@w.tf-w.tf>
2012-12-12 16:32:22 +0100

    Change timestamps to use ISO8601 formatting

--- src/init.cpp
+++ src/init.cpp
@@ -569,7 +569,7 @@ bool AppInit2()
     printf("Bitcoin version %s (%s)\n", FormatFullVersion().c_str(), CLIENT_DATE.c_str());
     printf("Using OpenSSL version %s\n", SSLeay_version(SSLEAY_VERSION));
     if (!fLogTimestamps)
-        printf("Startup time: %s\n", DateTimeStrFormat("%x %H:%M:%S", GetTime()).c_str());
+        printf("Startup time: %s\n", DateTimeStrFormat("%Y-%m-%dT%H:%M:%S", GetTime()).c_str());
     printf("Default data directory %s\n", GetDefaultDataDir().string().c_str());
     printf("Used data directory %s\n", strDataDir.c_str());
     std::ostringstream strErrors;
