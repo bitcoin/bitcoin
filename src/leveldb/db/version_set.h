3a33b30a47c7f36318c527175430e33535365a54
David Grogan <dgrogan@chromium.org>
2013-01-07 13:17:43 -0800

    Fix corruption bug found and analyzed by dhruba@gmail.com
    https://groups.google.com/d/msg/leveldb/Kc9JxuIUu5A/9P0N9RL4ar8J

--- src/leveldb/db/version_set.h
+++ src/leveldb/db/version_set.h
@@ -277,6 +277,8 @@ class VersionSet {
 
   void AppendVersion(Version* v);
 
+  bool ManifestContains(const std::string& record) const;
+
   Env* const env_;
   const std::string dbname_;
   const Options* const options_;
