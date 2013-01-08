6b89299853719c38f0adee8b1430a8cbb6f98bb1
Eric Lombrozo <elombrozo@gmail.com>
2013-01-08 03:02:51 -0800

    Created core.h/core.cpp, added to makefiles. Started moving core structures from main to core beginning with COutPoint.

---
+++ src/core.cpp
@@ -0,0 +1,6 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2013 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "core.h"
