bb41a87d57ac885dcd951a11827ca57329224802
Philip Kaufmann <phil.kaufmann@t-online.de>
2013-01-11 22:57:22 +0100

    add InitMessage() to noui and use debug.log for GUI
    - this pull adds an InitMessage() function to noui.cpp, which outputs init
      messages to debug.log (this allows to remove some printf() calls from
      init.cpp)
    - change InitMessage() in bitcoin.cpp to also write init messages to
      debug.log to ensure nothting is missing in the log because of the
      removal of printf() calls in init.cpp

--- src/init.cpp
+++ src/init.cpp
@@ -742,7 +742,7 @@ bool AppInit2()
     nCoinCacheSize = nTotalCache / 300; // coins in memory require around 300 bytes
 
     uiInterface.InitMessage(_("Loading block index..."));
-    printf("Loading block index...\n");
+
     nStart = GetTimeMillis();
     pblocktree = new CBlockTreeDB(nBlockTreeDBCache, false, fReindex);
     pcoinsdbview = new CCoinsViewDB(nCoinDBCache, false, fReindex);
@@ -799,7 +799,7 @@ bool AppInit2()
     // ********************************************************* Step 8: load wallet
 
     uiInterface.InitMessage(_("Loading wallet..."));
-    printf("Loading wallet...\n");
+
     nStart = GetTimeMillis();
     bool fFirstRun = true;
     pwalletMain = new CWallet("wallet.dat");
@@ -897,7 +897,7 @@ bool AppInit2()
     // ********************************************************* Step 10: load peers
 
     uiInterface.InitMessage(_("Loading addresses..."));
-    printf("Loading addresses...\n");
+
     nStart = GetTimeMillis();
 
     {
@@ -932,7 +932,6 @@ bool AppInit2()
     // ********************************************************* Step 12: finished
 
     uiInterface.InitMessage(_("Done loading"));
-    printf("Done loading\n");
 
     if (!strErrors.str().empty())
         return InitError(strErrors.str());
