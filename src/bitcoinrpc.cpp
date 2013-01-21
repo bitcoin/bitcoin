4579bbc8263d25537b624d0e7ef25bb5bef001f1
Sunny King <sunnyking9999@gmail.com>
2013-01-21 05:25:01 +0000

    PPCoin: Show proof-hash in RPC call 'getblock'

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -154,6 +154,7 @@ Object blockToJSON(const CBlock& block, const CBlockIndex* blockindex, bool fPri
     if (blockindex->pnext)
         result.push_back(Pair("nextblockhash", blockindex->pnext->GetBlockHash().GetHex()));
     result.push_back(Pair("flags", strprintf("%s%s", blockindex->IsProofOfStake()? "proof-of-stake" : "proof-of-work", blockindex->GeneratedStakeModifier()? " stake-modifier": "")));
+    result.push_back(Pair("proofhash", blockindex->IsProofOfStake()? blockindex->hashProofOfStake.GetHex() : blockindex->GetBlockHash().GetHex()));
     result.push_back(Pair("entropybit", (int)blockindex->GetStakeEntropyBit()));
     result.push_back(Pair("modifier", strprintf("%016"PRI64x, blockindex->nStakeModifier)));
     Array txinfo;
