af0308895e525fb0cffe51828d80649c464a2992
Sunny King <sunnyking9999@gmail.com>
2013-02-05 00:41:56 +0000

    PPCoin: Update pnSeed

--- src/net.cpp
+++ src/net.cpp
@@ -1,6 +1,6 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2012 The Bitcoin developers
-// Copyright (c) 2011-2012 The PPCoin developers
+// Copyright (c) 2011-2013 The PPCoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -1050,7 +1050,7 @@ void ThreadDNSAddressSeed2(void* parg)
 
 unsigned int pnSeed[] =
 {
-    0xd749a5bc, 0xf649a5bc,
+    0x231621b2,
 };
 
 void DumpAddresses()
