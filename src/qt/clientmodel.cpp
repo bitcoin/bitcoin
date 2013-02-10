f0ee9af43cb0c8ab64b5dccdafae9f997bbcdf3b
Pieter Wuille <pieter.wuille@gmail.com>
2013-02-10 19:46:42 +0100

    Transactions-based verification progress

--- src/qt/clientmodel.cpp
+++ src/qt/clientmodel.cpp
@@ -6,6 +6,7 @@
 
 #include "alert.h"
 #include "main.h"
+#include "checkpoints.h"
 #include "ui_interface.h"
 
 #include <QDateTime>
@@ -54,6 +55,10 @@ QDateTime ClientModel::getLastBlockDate() const
         return QDateTime::fromTime_t(1231006505); // Genesis block's time
 }
 
+double ClientModel::getVerificationProgress() const
+{
+    return Checkpoints::GuessVerificationProgress(pindexBest);
+}
 
 void ClientModel::updateTimer()
 {
