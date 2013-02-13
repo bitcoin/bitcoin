100d0c570f2c07c8c8b0993f5c5f69ae1e528270
Pieter Wuille <pieter.wuille@gmail.com>
2013-02-13 12:37:08 +0100

    Refactor testnet checkpoints

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -14,14 +14,21 @@ namespace Checkpoints
 {
     typedef std::map<int, uint256> MapCheckpoints;
 
-    //
+    static const double fSigcheckVerificationFactor = 15.0;
+
+    struct CCheckpointData {
+        MapCheckpoints mapCheckpoints;
+        int64 nTimeLastCheckpoint;
+        int64 nTransactionsLastCheckpoint;
+        double fTransactionsPerDay;
+    };
+
     // What makes a good checkpoint block?
     // + Is surrounded by blocks with reasonable timestamps
     //   (no blocks before with a timestamp after, none after with
     //    timestamp before)
     // + Contains no strange transactions
-    //
-    static MapCheckpoints mapCheckpoints =
+    static const CCheckpointData checkpoints = {
         boost::assign::map_list_of
         ( 11111, uint256("0x0000000069e244f73d78e8fd29ba2fd2ed618bd6fa2ee92559f542fdb26e7c1d"))
         ( 33333, uint256("0x000000002dd5588a74784eaa7ab0507a18ad16a236e7b1ce69f00d7ddfb5d0a6"))
@@ -31,25 +38,33 @@ namespace Checkpoints
         (168000, uint256("0x000000000000099e61ea72015e79632f216fe6cb33d7899acb35b75c8303b763"))
         (193000, uint256("0x000000000000059f452a5f7340de6682a977387c17010ff6e6c3bd83ca8b1317"))
         (210000, uint256("0x000000000000048b95347e83192f69cf0366076336c639f9b7228e9ba171342e"))
-        (216116, uint256("0x00000000000001b4f4b433e81ee46494af945cf96014816a4e2370f11b23df4e"))
-        ;
+        (216116, uint256("0x00000000000001b4f4b433e81ee46494af945cf96014816a4e2370f11b23df4e")),
+        1357902690, // UNIX timestamp of last checkpoint
+        11011160, // total number of transactions between genesis and last checkpoint
+        50500.0 // estimated number of transactions per day after checkpoint
+    };
 
-    static const int64 nTimeLastCheckpoint = 1357902690;
-    static const int64 nTransactionsLastCheckpoint = 11011160;
-    static const double fTransactionsPerDay = 50500;
-    static const double fSigcheckVerificationFactor = 15.0;
-
-    static MapCheckpoints mapCheckpointsTestnet =
+    static const CCheckpointData checkpointsTestnet = {
         boost::assign::map_list_of
-        ( 546, uint256("000000002a936ca763904c3c35fce2f3556c559c0214345d31b1bcebf76acb70"))
-        ;
+        ( 546, uint256("000000002a936ca763904c3c35fce2f3556c559c0214345d31b1bcebf76acb70")),
+        1338180505,
+        16341,
+        300
+    };
+
+    const CCheckpointData &Checkpoints() {
+        if (fTestNet)
+            return checkpointsTestnet;
+        else
+            return checkpoints;
+    }
 
     bool CheckBlock(int nHeight, const uint256& hash)
     {
         if (!GetBoolArg("-checkpoints", true))
             return true;
 
-        MapCheckpoints& checkpoints = (fTestNet ? mapCheckpointsTestnet : mapCheckpoints);
+        const MapCheckpoints& checkpoints = Checkpoints().mapCheckpoints;
 
         MapCheckpoints::const_iterator i = checkpoints.find(nHeight);
         if (i == checkpoints.end()) return true;
@@ -62,17 +77,19 @@ namespace Checkpoints
         double fWorkBefore = 0.0;
         double fWorkAfter = 0.0;
 
-        if (pindex->nTime <= nTimeLastCheckpoint) {
+        const CCheckpointData &data = Checkpoints();
+
+        if (pindex->nTime <= data.nTimeLastCheckpoint) {
             double nCheapBefore = pindex->nChainTx;
-            double nCheapAfter = nTransactionsLastCheckpoint - pindex->nChainTx;
-            double nExpensiveAfter = (nNow - nTimeLastCheckpoint)/86400.0*fTransactionsPerDay;
+            double nCheapAfter = data.nTransactionsLastCheckpoint - pindex->nChainTx;
+            double nExpensiveAfter = (nNow - data.nTimeLastCheckpoint)/86400.0*data.fTransactionsPerDay;
             fWorkBefore = nCheapBefore;
             fWorkAfter = nCheapAfter + nExpensiveAfter*fSigcheckVerificationFactor;
             // printf("before checkpoint: nCheapBefore=%f nCheapAfter=%f nExpensiveAfter=%f\n", nCheapBefore, nCheapAfter, nExpensiveAfter);
         } else {
-            double nCheapBefore = nTransactionsLastCheckpoint;
-            double nExpensiveBefore = pindex->nChainTx - nTransactionsLastCheckpoint;
-            double nExpensiveAfter = (nNow - pindex->nTime)/86400.0*fTransactionsPerDay;
+            double nCheapBefore = data.nTransactionsLastCheckpoint;
+            double nExpensiveBefore = pindex->nChainTx - data.nTransactionsLastCheckpoint;
+            double nExpensiveAfter = (nNow - pindex->nTime)/86400.0*data.fTransactionsPerDay;
             fWorkBefore = nCheapBefore + nExpensiveBefore*fSigcheckVerificationFactor;
             fWorkAfter = nExpensiveAfter*fSigcheckVerificationFactor;
             // printf("after checkpoint: nCheapBefore=%f nExpensiveBefore=%f nExpensiveAfter=%f\n", nCheapBefore, nExpensiveBefore, nExpensiveAfter);
@@ -87,7 +104,7 @@ namespace Checkpoints
         if (!GetBoolArg("-checkpoints", true))
             return 0;
 
-        MapCheckpoints& checkpoints = (fTestNet ? mapCheckpointsTestnet : mapCheckpoints);
+        const MapCheckpoints& checkpoints = Checkpoints().mapCheckpoints;
 
         return checkpoints.rbegin()->first;
     }
@@ -97,7 +114,7 @@ namespace Checkpoints
         if (!GetBoolArg("-checkpoints", true))
             return NULL;
 
-        MapCheckpoints& checkpoints = (fTestNet ? mapCheckpointsTestnet : mapCheckpoints);
+        const MapCheckpoints& checkpoints = Checkpoints().mapCheckpoints;
 
         BOOST_REVERSE_FOREACH(const MapCheckpoints::value_type& i, checkpoints)
         {
