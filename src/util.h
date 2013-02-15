907a2aa4c78833ce93455567ae10ff2f506e752e
Gregory Maxwell <greg@xiph.org>
2013-02-15 15:27:57 -0800

    Internal RNG for approximateBestSubset to prevent degenerate behavior.
    This fixes test_bitcoin failures on openbsd reported by dhill on IRC.
    
      On some systems rand() is a simple LCG over 2^31 and so it produces
    an even-odd sequence.  ApproximateBestSubset was only using the least
    significant bit and so every run of the iterative solver would be the
    same for some inputs, resulting in some pretty dumb decisions.
    
    Using something other than the least significant bit would paper over
    the issue but who knows what other way a system's rand() might get us
    here.  Instead we use an internal RNG with a period of something like
    2^60 which is well behaved.  This also makes it possible to make the
    selection deterministic for the tests, if we wanted to implement that.

--- src/util.h
+++ src/util.h
@@ -403,13 +403,27 @@ bool SoftSetArg(const std::string& strArg, const std::string& strValue);
  */
 bool SoftSetBoolArg(const std::string& strArg, bool fValue);
 
+/**
+ * MWC RNG of George Marsaglia
+ * This is intended to be fast. It has a period of 2^59.3, though the
+ * least significant 16 bits only have a period of about 2^30.1.
+ *
+ * @return random value
+ */
+extern uint32_t insecure_rand_Rz;
+extern uint32_t insecure_rand_Rw;
+static inline uint32_t insecure_rand(void)
+{
+  insecure_rand_Rz=36969*(insecure_rand_Rz&65535)+(insecure_rand_Rz>>16);
+  insecure_rand_Rw=18000*(insecure_rand_Rw&65535)+(insecure_rand_Rw>>16);
+  return (insecure_rand_Rw<<16)+insecure_rand_Rz;
+}
 
-
-
-
-
-
-
+/**
+ * Seed insecure_rand using the random pool.
+ * @param Deterministic Use a determinstic seed
+ */
+void seed_insecure_rand(bool fDeterministic=false);
 
 /** Median filter over a stream of values.
  * Returns the median of the last N numbers
