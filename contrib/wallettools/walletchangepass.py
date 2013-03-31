636141b2cfa3129cc9e8076faa56ff33ec634818
Aviad Reich <aviad.reich@intucellsystems.com>
2013-03-31 10:21:57 +0300

    update wallettools to use bitcoinrpc 0.3

--- contrib/wallettools/walletchangepass.py
+++ contrib/wallettools/walletchangepass.py
@@ -1,5 +1,13 @@
-from jsonrpc import ServiceProxy
-access = ServiceProxy("http://127.0.0.1:8332")
-pwd = raw_input("Enter old wallet passphrase: ")
-pwd2 = raw_input("Enter new wallet passphrase: ")
-access.walletpassphrasechange(pwd, pwd2)
+from bitcoinrpc import connect_to_local
+from getpass import getpass
+
+conn = connect_to_local()
+old_pass = getpass("Enter old wallet passphrase: ")
+new_pass = getpass("Enter new wallet passphrase: ")
+
+while not conn.walletpassphrasechange(old_pass, new_pass, dont_raise=True):
+    print 'Error: The wallet passphrase entered was incorrect. Try again.\n'
+    old_pass = getpass("Enter old wallet passphrase: ")
+    new_pass = getpass("Enter new wallet passphrase: ")
+
+print 'Password changed successfully'
--- contrib/wallettools/walletunlock.py
+++ contrib/wallettools/walletunlock.py
@@ -1,4 +1,11 @@
-from jsonrpc import ServiceProxy
-access = ServiceProxy("http://127.0.0.1:8332")
-pwd = raw_input("Enter wallet passphrase: ")
-access.walletpassphrase(pwd, 60)
+from bitcoinrpc import connect_to_local
+from getpass import getpass
+
+conn = connect_to_local()
+pwd = getpass("Enter wallet passphrase: ")
+
+while not conn.walletpassphrase(pwd, 60, dont_raise=True):
+    print "Wrong password. Try again."
+    pwd = getpass("Enter wallet passphrase: ")
+
+print "Wallet unlocked."
