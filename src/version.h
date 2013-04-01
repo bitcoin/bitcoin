e6d475149e9828cf8526fcd9f6a9c105c62e8dab
alexhz <balthazar@yandex.ru>
2013-04-01 02:43:58 +0000

    add checkpoint and version rules

--- src/version.h
+++ src/version.h
@@ -49,8 +49,8 @@ static const int MIN_PROTO_VERSION = 209;
 static const int CADDR_TIME_VERSION = 31402;
 
 // only request blocks from nodes outside this range of versions
-static const int NOBLKS_VERSION_START = 32000;
-static const int NOBLKS_VERSION_END = 32400;
+static const int NOBLKS_VERSION_START = 60002;
+static const int NOBLKS_VERSION_END = 60003;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
