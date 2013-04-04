8d432cd66d545c70189f8eb720b3a0b6ca02bbd0
Philip Kaufmann <phil.kaufmann@t-online.de>
2013-04-04 22:15:47 +0200

    Bitcoin-Qt: add Genesis blocks time for testnet
    - add the Genesis blocks time for the testnet in
      ClientModel::getLastBlockDate()

--- src/qt/clientmodel.cpp
+++ src/qt/clientmodel.cpp
@@ -52,8 +52,10 @@ QDateTime ClientModel::getLastBlockDate() const
 {
     if (pindexBest)
         return QDateTime::fromTime_t(pindexBest->GetBlockTime());
-    else
+    else if(!isTestNet())
         return QDateTime::fromTime_t(1231006505); // Genesis block's time
+    else
+        return QDateTime::fromTime_t(1296688602); // Genesis block's time (testnet)
 }
 
 double ClientModel::getVerificationProgress() const
