70412e06eda1f1619565c119bbffc0cc7070f854
Philip Kaufmann <phil.kaufmann@t-online.de>
2013-04-14 19:42:15 +0200

    aboutdialog: use just "The Bitcoin developers" as tr()-string
    - this ensures our new splash screen and this will share a translatable
      string an remove the need for an additional translation

--- src/qt/aboutdialog.cpp
+++ src/qt/aboutdialog.cpp
@@ -11,7 +11,7 @@ AboutDialog::AboutDialog(QWidget *parent) :
     ui->setupUi(this);
 
     // Set current copyright year
-    ui->copyrightLabel->setText(tr("Copyright") + QString(" &copy; ") + tr("2009-%1 The Bitcoin developers").arg(COPYRIGHT_YEAR));
+    ui->copyrightLabel->setText(tr("Copyright") + QString(" &copy; 2009-%1 ").arg(COPYRIGHT_YEAR) + tr("The Bitcoin developers"));
 }
 
 void AboutDialog::setModel(ClientModel *model)
