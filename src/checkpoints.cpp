45dace5143f9c60e3f98df9a9a869de7a26ff5d5
pocopoco <pocopocoemail@gmail.com>
2013-05-09 02:02:57 +0400

    hard checkpoint added at height 15000

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -24,7 +24,8 @@ namespace Checkpoints
     //
     static MapCheckpoints mapCheckpoints =
         boost::assign::map_list_of
-        ( 0, hashGenesisBlockOfficial )
+        (     0, hashGenesisBlockOfficial )
+        ( 15000, uint256("0x00000082cab82d04354692fac3b83d19cbe3c3ab4b73610d0e73397545eb012e"))
 //        ( 6000, uint256("0x000000000945e3c9d8e15df834e802521eb79f9ceb4191a27bdfadad4b777f4a"))
 //        ( 8700, uint256("0x00000000014270724837789c9a69859290f6bdee38556bc4561c21f17935a178"))
 //        ( 13560, uint256("0xa1591a0fcbf11f282d671581edb9f0aadcd06fee69761081e0a3245914c13729"))
