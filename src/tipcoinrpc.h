5f85d02593d576ddb7042c5c554374de362063bf
Nameshar <nameshar@yahoo.com>
2013-05-11 16:32:52 +0400

    Update and rename bitcoinrpc.h to tipcoinrpc.h

--- src/tipcoinrpc.h
+++ src/tipcoinrpc.h
@@ -3,8 +3,8 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef _BITCOINRPC_H_
-#define _BITCOINRPC_H_ 1
+#ifndef _TIPCOINRPC_H_
+#define _TIPCOINRPC_H_ 1
 
 #include <string>
 #include <list>
