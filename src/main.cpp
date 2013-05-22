80cad5d310d0a3abaa3fb4dcb40dc2234944cc99
Pieter Wuille <sipa@ulyssis.org>
2013-05-22 20:58:53 +0200

    More bestblock records in wallets
    Write bestblock records in wallets:
    * Every 20160 blocks synced, no matter what (before: none during IBD)
    * Every 144 blocks after IBD (before: for every block, slow)
    * When creating a new wallet
    * At shutdown
    
    This should result in far fewer spurious rescans.

--- src/main.cpp
+++ src/main.cpp
@@ -1870,7 +1870,7 @@ bool SetBestChain(CValidationState &state, CBlockIndex* pindexNew)
     }
 
     // Update best block in wallet (so we can detect restored wallets)
-    if (!fIsInitialDownload)
+    if ((pindexNew->nHeight % 20160) == 0 || (!fIsInitialDownload && (pindexNew->nHeight % 144) == 0))
     {
         const CBlockLocator locator(pindexNew);
         ::SetBestChain(locator);
