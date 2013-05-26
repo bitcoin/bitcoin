e45dcb266a2eb4878ec1cf72f367f53089babe7c
Cory Fields <theuni-nospam-@xbmc.org>
2013-05-26 14:00:16 -0400

    build: add global var for whether or not the gui is enabled

--- src/main.h
+++ src/main.h
@@ -94,6 +94,7 @@ extern bool fBenchmark;
 extern int nScriptCheckThreads;
 extern bool fTxIndex;
 extern unsigned int nCoinCacheSize;
+extern bool fHaveGUI;
 
 // Settings
 extern int64 nTransactionFee;
