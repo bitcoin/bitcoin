378ff026d0fc5205b2e15d2c2b05534d013ef5c5
Warren Togami <wtogami@gmail.com>
2013-06-17 21:36:26 -1000

    Litecoin: Reduce MinTxFee to 0.02 LTC

--- src/main.cpp
+++ src/main.cpp
@@ -49,9 +49,9 @@ bool fTxIndex = false;
 unsigned int nCoinCacheSize = 5000;
 
 /** Fees smaller than this (in satoshi) are considered zero fee (for transaction creation) */
-int64 CTransaction::nMinTxFee = 10000;  // Override with -mintxfee
+int64 CTransaction::nMinTxFee = 2000000;
 /** Fees smaller than this (in satoshi) are considered zero fee (for relaying) */
-int64 CTransaction::nMinRelayTxFee = 10000;
+int64 CTransaction::nMinRelayTxFee = 2000000;
 
 CMedianFilter<int> cPeerBlockCounts(8, 0); // Amount of blocks that other nodes claim to have
 
