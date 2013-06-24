ec84e81e8383b3b1e1ef4a6dbcb088193d8de5d7
Pieter Wuille <sipa@ulyssis.org>
2013-06-24 20:51:08 +0200

    Prune provably-unspendable outputs

--- src/script.h
+++ src/script.h
@@ -548,6 +548,13 @@ public:
         return true;
     }
 
+    // Returns whether the script is guaranteed to fail at execution,
+    // regardless of the initial stack. This allows outputs to be pruned
+    // instantly when entering the UTXO set.
+    bool IsUnspendable() const
+    {
+        return (size() > 0 && *begin() == OP_RETURN);
+    }
 
     void SetDestination(const CTxDestination& address);
     void SetMultisig(int nRequired, const std::vector<CPubKey>& keys);
