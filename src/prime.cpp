ea73df9798c247d19ea1f375e67d5d9ab206fc0b
Sunny King <sunnyking9999@gmail.com>
2013-07-04 03:21:53 +0100

    Primecoin: Update minting model with slower drop of inflation

--- src/prime.cpp
+++ src/prime.cpp
@@ -209,19 +209,19 @@ unsigned int TargetFromInt(unsigned int nLength)
 
 // Get mint value from target
 // Primecoin mint rate is determined by target
-//   mint = 9999 / (length ** 3)
+//   mint = 999 / (target length ** 2)
 // Inflation is controlled via Moore's Law
 bool TargetGetMint(unsigned int nBits, uint64& nMint)
 {
-    static uint64 nMintLimit = 9999llu * COIN;
+    static uint64 nMintLimit = 999llu * COIN;
     CBigNum bnMint = nMintLimit;
     if (TargetGetLength(nBits) < nTargetMinLength)
         return error("TargetGetMint() : length below minimum required, nBits=%08x", nBits);
     bnMint = (bnMint << nFractionalBits) / nBits;
     bnMint = (bnMint << nFractionalBits) / nBits;
-    bnMint = (bnMint << nFractionalBits) / nBits;
     bnMint = (bnMint / CENT) * CENT;  // mint value rounded to cent
     nMint = bnMint.getuint256().Get64();
+    nMint = std::min(nMint, nMintLimit);  // should never exceed mint limit
     return true;
 }
 
