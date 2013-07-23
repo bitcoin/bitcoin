351229cfc0d0f6c54af8e4d7ac7426c16e776fb1
grayleonard <gl7039a@american.edu>
2013-07-23 10:38:02 -0400

    (Hopefully) the last update to this pull request
    Simplified time-leaking solution, using min() to make sure only valid indexes are called.

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -479,20 +479,15 @@ bool HTTPAuthorized(map<string, string>& mapHeaders)
     string strUserPass = DecodeBase64(strUserPass64);
 
     //Begin constant-time comparison
-    if (strUserPass.length() != strRPCUserColonPass.length()) 
-    {
-        for (size_t i = 0; i < strUserPass.length(); i++) //Stops timing-leaks for the length of the password
-        { 
-            nResult |= strUserPass.at(i) ^ strUserPass.at(i); 
-        }
-        return ++nResult == 0;
-    }
-
-    //XOR's chars in each password together, and then adds either 0 or 1 (0 if the chars match) to nResult
-    for (size_t i = 0; i < strUserPass.length(); i++)
+    //XOR chars in each password together, and then adds either 0 or 1 (0 if they match) to nResult
+    for(size_t i = 0; i < min(strUserPass.length(), strRPCUserColonPass.length()); i++)
     { 
         nResult |= strUserPass.at(i) ^ strRPCUserColonPass.at(i); 
     }
+
+    if(strUserPass.length() != strRPCUserColonPass.length())
+        nResult++;
+
     return nResult == 0;
 }
 
