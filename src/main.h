1877fb9aaeb9e68b29bf3c88caf031048acd21f6
Warren Togami <wtogami@gmail.com>
2013-07-28 01:47:35 -1000

    Litecoin: Unit test for scrypt_1024_1_1_256() hash
    Uncomment the block.print(); in main.cpp below printf("received block ..."
    if you want to capture raw input bytes and PoW hashes for testing.

--- src/main.h
+++ src/main.h
@@ -1482,8 +1482,9 @@ public:
 
     void print() const
     {
-        printf("CBlock(hash=%s, PoW=%s, ver=%d, hashPrevBlock=%s, hashMerkleRoot=%s, nTime=%u, nBits=%08x, nNonce=%u, vtx=%"PRIszu")\n",
+        printf("CBlock(hash=%s, input=%s, PoW=%s, ver=%d, hashPrevBlock=%s, hashMerkleRoot=%s, nTime=%u, nBits=%08x, nNonce=%u, vtx=%"PRIszu")\n",
             GetHash().ToString().c_str(),
+            HexStr(BEGIN(nVersion),BEGIN(nVersion)+80,false).c_str(),
             GetPoWHash().ToString().c_str(),
             nVersion,
             hashPrevBlock.ToString().c_str(),
