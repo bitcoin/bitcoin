325cc024c94064d0b750d47cf681913d70bb7e2a
alex <alex@alex-VirtualBox.(none)>
2013-07-30 03:30:46 +0400

    BUGFIX: prevent Qt client crash at startup with an empty directory

--- src/qt/clientmodel.cpp
+++ src/qt/clientmodel.cpp
@@ -50,7 +50,10 @@ int ClientModel::getNumBlocksAtStartup()
 
 QDateTime ClientModel::getLastBlockDate() const
 {
-    return QDateTime::fromTime_t(pindexBest->GetBlockTime());
+    if (pindexBest)
+        return QDateTime::fromTime_t(pindexBest->GetBlockTime());
+    else
+        return QDateTime::fromTime_t(1360105017); // Genesis block's time
 }
 
 void ClientModel::updateTimer()
