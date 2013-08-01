237103eb808c7bbf85f5724bbf5f51208fa59cdd
alex <alex@alex-VirtualBox.(none)>
2013-08-01 22:14:01 +0400

    Remove legacy block schema version checking

--- src/main.cpp
+++ src/main.cpp
@@ -2250,10 +2250,6 @@ bool CBlock::AcceptBlock()
         }
     }
 
-    // Reject block.nVersion < 3 blocks since 95% threshold on mainNet and always on testNet:
-    if (nVersion < 3 && ((!fTestNet && nHeight > 14060) || (fTestNet && nHeight > 0)))
-        return error("CheckBlock() : rejected nVersion < 3 block");
-
     // Enforce rule that the coinbase starts with serialized block height
     CScript expect = CScript() << nHeight;
     if (!std::equal(expect.begin(), expect.end(), vtx[0].vin[0].scriptSig.begin()))
