5ee73ff86cf9fbe8c23d7ed42ba1a085ea9faa6d
Gavin Andresen <gavinandresen@gmail.com>
2013-08-08 19:58:57 +1000

    Make RPC password resistant to timing attacks
    Fixes issue#2838; this is a tweaked version of pull#2845 that
    should not leak the length of the password and is more generic,
    in case we run into other situations where we need
    timing-attack-resistant comparisons.

--- src/util.h
+++ src/util.h
@@ -438,6 +438,21 @@ static inline uint32_t insecure_rand(void)
  */
 void seed_insecure_rand(bool fDeterministic=false);
 
+/**
+ * Timing-attack-resistant comparison.
+ * Takes time proportional to length
+ * of first argument.
+ */
+template <typename T>
+bool TimingResistantEqual(const T& a, const T& b)
+{
+    if (b.size() == 0) return a.size() == 0;
+    size_t accumulator = a.size() ^ b.size();
+    for (size_t i = 0; i < a.size(); i++)
+        accumulator |= a[i] ^ b[i%b.size()];
+    return accumulator == 0;
+}
+
 /** Median filter over a stream of values.
  * Returns the median of the last N numbers
  */
