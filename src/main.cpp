d3f27c5b81a4ff2d099eb004e2c4204b654894ba
Gregory Maxwell <greg@xiph.org>
2013-08-20 17:41:42 -0700

    Update the bloom state on the real object, not the temporary one.
    This resulted in just passing all transactions to filtered wallets
    which worked surprisingly well, except where it didn't.

--- src/main.cpp
+++ src/main.cpp
@@ -3699,7 +3699,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv)
             LOCK(pfrom->cs_filter);
             delete pfrom->pfilter;
             pfrom->pfilter = new CBloomFilter(filter);
-            filter.UpdateEmptyFull();
+            pfrom->pfilter->UpdateEmptyFull();
         }
         pfrom->fRelayTxes = true;
     }
