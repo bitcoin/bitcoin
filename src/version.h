a2f39a76898531df7127782fd8a61582f7a44a13
alex <alex@alex-VirtualBox.(none)>
2013-08-27 23:30:41 +0400

    Increase minimum protocol version

--- src/version.h
+++ src/version.h
@@ -28,15 +28,15 @@ extern const std::string CLIENT_DATE;
 static const int PROTOCOL_VERSION = 60009;
 
 // earlier versions not supported as of Feb 2012, and are disconnected
-static const int MIN_PROTO_VERSION = 209;
+static const int MIN_PROTO_VERSION = 60007;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
 static const int CADDR_TIME_VERSION = 31402;
 
 // only request blocks from nodes outside this range of versions
-static const int NOBLKS_VERSION_START = 60002;
-static const int NOBLKS_VERSION_END = 60006;
+static const int NOBLKS_VERSION_START = 60000;
+static const int NOBLKS_VERSION_END = 60007;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
