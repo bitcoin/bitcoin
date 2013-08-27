ee90bada80076568d0722b5f2b18d9dd22c3997f
alex <alex@alex-VirtualBox.(none)>
2013-08-27 23:51:11 +0400

    Fix coinbase out-of-bounds check

--- src/main.cpp
+++ src/main.cpp
@@ -2262,7 +2262,8 @@ bool CBlock::AcceptBlock()
 
     // Enforce rule that the coinbase starts with serialized block height
     CScript expect = CScript() << nHeight;
-    if (!std::equal(expect.begin(), expect.end(), vtx[0].vin[0].scriptSig.begin()))
+    if (vtx[0].vin[0].scriptSig.size() < expect.size() ||
+        !std::equal(expect.begin(), expect.end(), vtx[0].vin[0].scriptSig.begin()))
         return DoS(100, error("AcceptBlock() : block height mismatch in coinbase"));
 
     // Write block to history file
