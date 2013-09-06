f80351c3215014935c5836f7d90cc21af5fc0cdf
Gregory Maxwell <greg@xiph.org>
2013-09-05 17:54:31 -0700

    Make settxfee clear that units are btc/kb.
    Rebased-from: fffab7e45061aba3a5f345e0fc22a8584bcb012d 0.8.x

--- src/rpcblockchain.cpp
+++ src/rpcblockchain.cpp
@@ -106,8 +106,8 @@ Value settxfee(const Array& params, bool fHelp)
 {
     if (fHelp || params.size() < 1 || params.size() > 1)
         throw runtime_error(
-            "settxfee <amount>\n"
-            "<amount> is a real and is rounded to the nearest 0.00000001");
+            "settxfee <amount btc/kb>\n"
+            "<amount> is a real and is rounded to the nearest 0.00000001 btc per kb");
 
     // Amount
     int64 nAmount = 0;
