af6cd34e896382a271a11d88d6a38f9fedc6f48b
Pieter Wuille <pieter.wuille@gmail.com>
2013-10-19 18:34:06 +0200

    Use boost signals for callbacks from main to wallet

--- src/rpcrawtransaction.cpp
+++ src/rpcrawtransaction.cpp
@@ -560,7 +560,7 @@ Value sendrawtransaction(const Array& params, bool fHelp)
         // Not in block, but already in the memory pool; will drop
         // through to re-relay it.
     } else {
-        SyncWithWallets(hashTx, tx, NULL, true);
+        SyncWithWallets(hashTx, tx, NULL);
     }
     RelayTransaction(tx, hashTx);
 
