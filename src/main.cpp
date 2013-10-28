59548305426564673c3fad93bee4e21f052f37e5
Patrick Strateman <patrick.strateman@gmail.com>
2013-10-28 13:20:21 -0700

    process received messages one at a time without sleeping between messages

--- src/main.cpp
+++ src/main.cpp
@@ -3155,6 +3155,9 @@ void static ProcessGetData(CNode* pfrom)
 
             // Track requests for our stuff.
             g_signals.Inventory(inv.hash);
+            
+            if (inv.type == MSG_BLOCK || inv.type == MSG_FILTERED_BLOCK)
+                break;
         }
     }
 
@@ -3841,7 +3844,10 @@ bool ProcessMessages(CNode* pfrom)
 
     if (!pfrom->vRecvGetData.empty())
         ProcessGetData(pfrom);
-
+    
+    // this maintains the order of responses
+    if (!pfrom->vRecvGetData.empty()) return fOk;
+    
     std::deque<CNetMessage>::iterator it = pfrom->vRecvMsg.begin();
     while (!pfrom->fDisconnect && it != pfrom->vRecvMsg.end()) {
         // Don't bother if send buffer is too full to respond anyway
@@ -3929,6 +3935,8 @@ bool ProcessMessages(CNode* pfrom)
 
         if (!fRet)
             LogPrintf("ProcessMessage(%s, %u bytes) FAILED\n", strCommand.c_str(), nMessageSize);
+        
+        break;
     }
 
     // In case the connection got shut down, its receive buffer was wiped
