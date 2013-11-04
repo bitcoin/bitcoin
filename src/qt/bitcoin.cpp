fcbab53a8453d12851f19bc73f417e5c77f6526b
Wladimir J. van der Laan <laanwj@gmail.com>
2013-11-04 14:36:49 +0100

    No more fHaveGUI
    No more specific handling of GUI in bitcoin core.
    Replace the last usage of fHaveGUI with a fForceServer
    parameter on AppInit2.

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -173,8 +173,6 @@ int main(int argc, char *argv[])
     bool fMissingDatadir = false;
     bool fSelParFromCLFailed = false;
 
-    fHaveGUI = true;
-
     // Command-line options take precedence:
     ParseParameters(argc, argv);
     // ... then bitcoin.conf:
@@ -293,7 +291,7 @@ int main(int argc, char *argv[])
         QObject::connect(pollShutdownTimer, SIGNAL(timeout()), guiref, SLOT(detectShutdown()));
         pollShutdownTimer->start(200);
 
-        if(AppInit2(threadGroup))
+        if(AppInit2(threadGroup, false))
         {
             {
                 // Put this in a block, so that the Model objects are cleaned up before
