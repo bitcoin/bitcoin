a44a30afb77e5ed358a4731f8be6f534cde7401b
Brandon Dahler <brandon.dahler@gmail.com>
2013-11-05 19:58:43 -0600

    Rename leveldb.{h,cpp} to leveldbwrapper.{h,cpp}.

--- src/txdb.h
+++ src/txdb.h
@@ -6,13 +6,13 @@
 #define BITCOIN_TXDB_LEVELDB_H
 
 #include "main.h"
-#include "leveldb.h"
+#include "leveldbwrapper.h"
 
 /** CCoinsView backed by the LevelDB coin database (chainstate/) */
 class CCoinsViewDB : public CCoinsView
 {
 protected:
-    CLevelDB db;
+    CLevelDBWrapper db;
 public:
     CCoinsViewDB(size_t nCacheSize, bool fMemory = false, bool fWipe = false);
 
@@ -26,7 +26,7 @@ public:
 };
 
 /** Access to the block database (blocks/index/) */
-class CBlockTreeDB : public CLevelDB
+class CBlockTreeDB : public CLevelDBWrapper
 {
 public:
     CBlockTreeDB(size_t nCacheSize, bool fMemory = false, bool fWipe = false);
