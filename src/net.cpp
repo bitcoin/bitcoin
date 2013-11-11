2aa7ca56081530d71de7535f5a2fc8d787577be7
Henrik Nordstrom <henrik@henriknordstrom.net>
2013-11-11 18:54:17 +0100

    Make addnode nodes more insisting on being connected
     - Bypass outgoing connections limit for addnode nodes

--- src/net.cpp
+++ src/net.cpp
@@ -1382,7 +1382,7 @@ void ThreadOpenAddedConnections()
             }
             BOOST_FOREACH(string& strAddNode, lAddresses) {
                 CAddress addr;
-                CSemaphoreGrant grant(*semOutbound);
+                CSemaphoreGrant grant(*semOutbound, true);
                 OpenNetworkConnection(addr, &grant, strAddNode.c_str());
                 MilliSleep(500);
             }
@@ -1429,7 +1429,7 @@ void ThreadOpenAddedConnections()
         }
         BOOST_FOREACH(vector<CService>& vserv, lservAddressesToAdd)
         {
-            CSemaphoreGrant grant(*semOutbound);
+            CSemaphoreGrant grant(*semOutbound, true);
             OpenNetworkConnection(CAddress(vserv[i % vserv.size()]), &grant);
             MilliSleep(500);
         }
