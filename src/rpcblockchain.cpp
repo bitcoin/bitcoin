fb78cc23784b2fa478324aac35ca76c7cfe683a4
Wladimir J. van der Laan <laanwj@gmail.com>
2013-11-20 14:18:57 +0100

    Split up bitcoinrpc (code movement only)
    Split bitcoinrpc up into
    
    - rpcserver: bitcoind RPC server
    - rpcclient: bitcoin-cli RPC client
    - rpcprotocol: shared common HTTP/JSON-RPC protocol code
    
    One step towards making bitcoin-cli independent from the rest
    of the code, and thus a smaller executable that doesn't have to
    be linked against leveldb.
    
    This commit only does code movement, there are no functional changes.

--- src/rpcblockchain.cpp
+++ src/rpcblockchain.cpp
@@ -3,9 +3,7 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-
-
-#include "bitcoinrpc.h"
+#include "rpcserver.h"
 #include "main.h"
 #include "sync.h"
 
