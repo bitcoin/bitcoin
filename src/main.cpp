7cc3c0a9c018a6647d16937c0cc69375c25e3d4a
Warren Togami <wtogami@gmail.com>
2013-11-26 14:45:34 -1000

    Litecoin: Reduce default mintxfee to 0.001

--- src/main.cpp
+++ src/main.cpp
@@ -49,9 +49,9 @@ bool fTxIndex = false;
 unsigned int nCoinCacheSize = 5000;
 
 /** Fees smaller than this (in satoshi) are considered zero fee (for transaction creation) */
-int64 CTransaction::nMinTxFee = 2000000;
+int64 CTransaction::nMinTxFee = 100000;
 /** Fees smaller than this (in satoshi) are considered zero fee (for relaying) */
-int64 CTransaction::nMinRelayTxFee = 2000000;
+int64 CTransaction::nMinRelayTxFee = 100000;
 
 CMedianFilter<int> cPeerBlockCounts(8, 0); // Amount of blocks that other nodes claim to have
 
