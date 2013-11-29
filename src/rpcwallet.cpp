8aa6bbbca6d5e291f05b1275dba64b6b6ca6f535
Wladimir J. van der Laan <laanwj@gmail.com>
2013-11-29 15:48:07 +0100

    Move HelpExample* from rpcwallet to rpcserver
    General functions used throughout the RPC framework
    don't belong in rpcwallet.

--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -33,15 +33,6 @@ std::string HelpRequiringPassphrase()
         : "";
 }
 
-std::string HelpExampleCli(string methodname, string args){
-    return "> bitcoin-cli " + methodname + " " + args + "\n";
-}
-
-std::string HelpExampleRpc(string methodname, string args){
-    return "> curl --user myusername --data-binary '{\"jsonrpc\": \"1.0\", \"id\":\"curltest\", "
-        "\"method\": \"" + methodname + "\", \"params\": [" + args + "] }' -H 'content-type: text/plain;' http://127.0.0.1:8332/\n";
-}
-
 void EnsureWalletIsUnlocked()
 {
     if (pwalletMain->IsLocked())
