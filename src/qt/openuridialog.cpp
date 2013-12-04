1e01f7c506af60012bb99ced053d98eb529eda61
Gavin Andresen <gavinandresen@gmail.com>
2013-12-04 13:18:09 +1000

    Payment request URI syntax changed, from request=... to r=...
    BIP 72 was changed to save six bytes in bitcoin: URIs.

--- src/qt/openuridialog.cpp
+++ src/qt/openuridialog.cpp
@@ -48,5 +48,5 @@ void OpenURIDialog::on_selectFileButton_clicked()
     if(filename.isEmpty())
         return;
     QUrl fileUri = QUrl::fromLocalFile(filename);
-    ui->uriEdit->setText("bitcoin:?request=" + QUrl::toPercentEncoding(fileUri.toString()));
+    ui->uriEdit->setText("bitcoin:?r=" + QUrl::toPercentEncoding(fileUri.toString()));
 }
