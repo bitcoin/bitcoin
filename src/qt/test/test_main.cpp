3783ec31ea5d0d69cfe05df02170fcc917d7910c
Wladimir J. van der Laan <laanwj@gmail.com>
2013-12-11 15:00:56 +0100

    GUI for --disable-wallet compiles and -disablewallet mode
    There is not much in the GUI to be done without wallet,
    though it's possible to change options, watch the sync process,
    and use the debug console.
    
    So embed the debug console in the main window.

--- src/qt/test/test_main.cpp
+++ src/qt/test/test_main.cpp
@@ -1,5 +1,11 @@
 #include "bitcoin-config.h"
+#if defined(HAVE_CONFIG_H)
+#include "bitcoin-config.h"
+#endif
+
+#ifdef ENABLE_WALLET
 #include "paymentservertests.h"
+#endif
 #include "uritests.h"
 
 #include <QCoreApplication>
@@ -27,10 +33,11 @@ int main(int argc, char *argv[])
     URITests test1;
     if (QTest::qExec(&test1) != 0)
         fInvalid = true;
-
+#ifdef ENABLE_WALLET
     PaymentServerTests test2;
     if (QTest::qExec(&test2) != 0)
         fInvalid = true;
+#endif
 
     return fInvalid;
 }
