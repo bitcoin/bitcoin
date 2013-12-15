afbb2a32c9dc1096fce4a39ab61fce6e2ba41ebd
rolf vreijdenberger <rolf@vreijdenberger.nl>
2013-12-15 23:20:27 +0100

    Litecoin: bitrpc.py cosmetic s/Bitcoin/Litecoin/

--- contrib/bitrpc/bitrpc.py
+++ contrib/bitrpc/bitrpc.py
@@ -24,7 +24,7 @@ if cmd == "backupwallet":
 
 elif cmd == "getaccount":
 	try:
-		addr = raw_input("Enter a Bitcoin address: ")
+		addr = raw_input("Enter a Litecoin address: ")
 		print access.getaccount(addr)
 	except:
 		print "\n---An error occurred---\n"
@@ -126,7 +126,7 @@ elif cmd == "getreceivedbyaccount":
 
 elif cmd == "getreceivedbyaddress":
 	try:
-		addr = raw_input("Enter a Bitcoin address (optional): ")
+		addr = raw_input("Enter a Litecoin address (optional): ")
 		mc = raw_input("Minimum confirmations (optional): ")
 		try:
 			print access.getreceivedbyaddress(addr, mc)
