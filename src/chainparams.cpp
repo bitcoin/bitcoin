60aab8690d12b7d27828726892a44e8214004817
y12studio <y12studio@users.noreply.github.com>
2013-12-28 15:52:23 +0800

    Update chainparams.cpp
    [make it easier to mine · a37bd83 · freewil/bitcoin](https://github.com/freewil/bitcoin/commit/a37bd83254dd103bf1f51d7ee6be58ce674a89e9)

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -110,7 +110,7 @@ public:
         vAlertPubKey = ParseHex("04fc9702847840aaf195de8442ebecedf5b095cdbb9bc716bda9110971b28a49e0ead8564ff0db22209e0374782c093bb899692d524e9d6a6956e7c5ecbcd68284");
         nDefaultPort = 8333;
         nRPCPort = 8332;
-        bnProofOfWorkLimit = CBigNum(~uint256(0) >> 32);
+        bnProofOfWorkLimit = CBigNum(~uint256(0) >> 15);
         nSubsidyHalvingInterval = 210000;
 
         // Build the genesis block. Note that the output of the genesis coinbase cannot
