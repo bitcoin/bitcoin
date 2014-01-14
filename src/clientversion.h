6760b082b5ed84839d114fe45787ecc17ded792d
Philip Kaufmann <phil.kaufmann@t-online.de>
2014-01-14 17:11:38 +0100

    update copyright year to 2014

--- src/clientversion.h
+++ src/clientversion.h
@@ -19,7 +19,7 @@
 
 // Copyright year (2009-this)
 // Todo: update this when changing our copyright comments in the source
-#define COPYRIGHT_YEAR 2013
+#define COPYRIGHT_YEAR 2014
 
 #endif //HAVE_CONFIG_H
 
