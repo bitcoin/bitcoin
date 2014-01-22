816949bd7e8bac9b1def3534f99ad5a37d0dbf43
Philip Kaufmann <phil.kaufmann@t-online.de>
2014-01-22 14:47:33 +0100

    [Qt] move helpmessage from debug window to main menu
    - the option to show our help message dialog resides now in main menu
      under help

--- src/qt/rpcconsole.h
+++ src/qt/rpcconsole.h
@@ -40,8 +40,6 @@ private slots:
     void on_tabWidget_currentChanged(int index);
     /** open the debug.log from the current datadir */
     void on_openDebugLogfileButton_clicked();
-    /** display messagebox with program parameters (same as bitcoin-qt --help) */
-    void on_showCLOptionsButton_clicked();
     /** change the time range of the network traffic graph */
     void on_sldGraphRange_valueChanged(int value);
     /** update traffic statistics */
