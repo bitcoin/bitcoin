c0cf5df689c0d14f57ebc1668cadcefbc4b77e3a
Sunny King <sunnyking9999@gmail.com>
2014-01-23 06:52:29 +0000

    PPCoin: Update main net client icon with the standard icon

--- share/qt/make_windows_icon.sh
+++ share/qt/make_windows_icon.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # create multiresolution windows icon
-ICON_SRC=../../src/qt/res/icons/bitcoin.png
-ICON_DST=../../src/qt/res/icons/bitcoin.ico
-convert ${ICON_SRC} -resize 16x16 bitcoin-16.png
-convert ${ICON_SRC} -resize 32x32 bitcoin-32.png
-convert ${ICON_SRC} -resize 48x48 bitcoin-48.png
-convert bitcoin-16.png bitcoin-32.png bitcoin-48.png ${ICON_DST}
+ICON_SRC=../../src/qt/res/icons/ppcoin.png
+ICON_DST=../../src/qt/res/icons/ppcoin.ico
+convert ${ICON_SRC} -resize 16x16 ppcoin-16.png
+convert ${ICON_SRC} -resize 32x32 ppcoin-32.png
+convert ${ICON_SRC} -resize 48x48 ppcoin-48.png
+convert ppcoin-48.png ppcoin-32.png ppcoin-16.png ${ICON_DST}
 
