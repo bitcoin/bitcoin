d7aa1ec8dd1edd6ba35bddbecc22426a6179a729
Philip Kaufmann <phil.kaufmann@t-online.de>
2014-01-28 10:48:41 +0100

    [Qt] fix bitcoin: URI strings (spelling fixes)

--- src/qt/recentrequeststablemodel.h
+++ src/qt/recentrequeststablemodel.h
@@ -53,7 +53,7 @@ private:
     Qt::SortOrder order;
 };
 
-/** Model for list of recently generated payment requests / bitcoin URIs.
+/** Model for list of recently generated payment requests / bitcoin: URIs.
  * Part of wallet model.
  */
 class RecentRequestsTableModel: public QAbstractTableModel
