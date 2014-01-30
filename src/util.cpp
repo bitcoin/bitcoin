1cbbca3418d76b9209493131f292f1bc9ebb25f0
Philip Kaufmann <phil.kaufmann@t-online.de>
2014-01-30 11:01:00 +0100

    remove old Boost workaround

--- src/util.cpp
+++ src/util.cpp
@@ -71,17 +71,6 @@
 #include <openssl/crypto.h>
 #include <openssl/rand.h>
 
-// Work around clang compilation problem in Boost 1.46:
-// /usr/include/boost/program_options/detail/config_file.hpp:163:17: error: call to function 'to_internal' that is neither visible in the template definition nor found by argument-dependent lookup
-// See also: http://stackoverflow.com/questions/10020179/compilation-fail-in-boost-librairies-program-options
-//           http://clang.debian.net/status.php?version=3.0&key=CANNOT_FIND_FUNCTION
-namespace boost {
-    namespace program_options {
-        std::string to_internal(const std::string&);
-    }
-}
-
-
 using namespace std;
 
 map<string, string> mapArgs;
