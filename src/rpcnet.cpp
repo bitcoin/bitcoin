6943cb9b4f917b4f3e74a70d525b5e3084a5ab59
Philip Kaufmann <phil.kaufmann@t-online.de>
2014-01-30 14:13:30 +0100

    small changes to rpc command help message strings

--- src/rpcnet.cpp
+++ src/rpcnet.cpp
@@ -47,7 +47,7 @@ Value ping(const Array& params, bool fHelp)
             "ping\n"
             "\nRequests that a ping be sent to all other nodes, to measure ping time.\n"
             "Results provided in getpeerinfo, pingtime and pingwait fields are decimal seconds.\n"
-            "Ping command is handled in queue with all other commands, so it measures processing backlog, not just network ping."
+            "Ping command is handled in queue with all other commands, so it measures processing backlog, not just network ping.\n"
             "\nExamples:\n"
             + HelpExampleCli("ping", "")
             + HelpExampleRpc("ping", "")
--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -1850,9 +1850,9 @@ Value settxfee(const Array& params, bool fHelp)
     if (fHelp || params.size() < 1 || params.size() > 1)
         throw runtime_error(
             "settxfee amount\n"
-            "\nSet the transaction fee per KB.\n"
+            "\nSet the transaction fee per kB.\n"
             "\nArguments:\n"
-            "1. amount         (numeric, required) The transaction fee in BTC/KB rounded to the nearest 0.00000001\n"
+            "1. amount         (numeric, required) The transaction fee in BTC/kB rounded to the nearest 0.00000001\n"
             "\nResult\n"
             "true|false        (boolean) Returns true if successful\n"
             "\nExamples:\n"
