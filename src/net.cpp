399334ebd86b2d04bd07528038cbf336774431d3
Julian Yap <julian@ehawaii.gov>
2014-02-12 17:58:31 -1000

    Integrate various fixes so Novacoin builds on OSX

--- src/net.cpp
+++ src/net.cpp
@@ -57,7 +57,7 @@ static bool vfLimited[NET_MAX] = {};
 static CNode* pnodeLocalHost = NULL;
 CAddress addrSeenByPeer(CService("0.0.0.0", 0), nLocalServices);
 uint64 nLocalHostNonce = 0;
-array<int, THREAD_MAX> vnThreadsRunning;
+boost::array<int, THREAD_MAX> vnThreadsRunning;
 static std::vector<SOCKET> vhListenSocket;
 CAddrMan addrman;
 
