09ec3af16687aa301a921a1a6e94d7a57d4af141
Wladimir J. van der Laan <laanwj@gmail.com>
2014-02-18 15:23:24 +0100

    AddToWallet implies BindWallet
    Now that AddToWallet is called when loading transactions from the
    wallet database, BindWallet can be integrated into that and does not
    need to be an extra step.
    
    Leaves behaviour unchanged, but makes the
    fFromLoadWallet/!fFromLoadWallet paths in AddToWallet a bit more
    symmetric.

--- src/walletdb.cpp
+++ src/walletdb.cpp
@@ -352,9 +352,7 @@ ReadKeyValue(CWallet* pwallet, CDataStream& ssKey, CDataStream& ssValue,
             CWalletTx wtx;
             ssValue >> wtx;
             CValidationState state;
-            if (CheckTransaction(wtx, state) && (wtx.GetHash() == hash) && state.IsValid())
-                wtx.BindWallet(pwallet);
-            else
+            if (!(CheckTransaction(wtx, state) && (wtx.GetHash() == hash) && state.IsValid()))
                 return false;
 
             // Undo serialize changes in 31600
