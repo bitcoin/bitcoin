5cd31f335429e8cd35273f90e92e329e96f67ff7
paveljanik <Pavel@Janik.cz>
2014-03-05 10:42:52 +0100

    Clarify the error message when unable to bind to port

--- src/net.cpp
+++ src/net.cpp
@@ -1653,7 +1653,7 @@ bool BindListenPort(const CService &addrBind, string& strError)
     {
         int nErr = WSAGetLastError();
         if (nErr == WSAEADDRINUSE)
-            strError = strprintf(_("Unable to bind to %s on this computer. Bitcoin server is probably already running."), addrBind.ToString());
+            strError = strprintf(_("Unable to bind to %s on this computer. Bitcoin Core Daemon is probably already running."), addrBind.ToString());
         else
             strError = strprintf(_("Unable to bind to %s on this computer (bind returned error %d, %s)"), addrBind.ToString(), nErr, strerror(nErr));
         LogPrintf("%s\n", strError);
