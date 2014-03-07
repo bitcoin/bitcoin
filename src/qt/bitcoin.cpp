7fe2c85e71cafb37032b2402585c0f2a15bf6b4d
jtimon <jtimon@monetize.io>
2014-03-07 11:57:22 -0800

    MainNet is the special case for the GUI

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -448,12 +448,12 @@ int main(int argc, char *argv[])
         fSelParFromCLFailed = true;
     }
 #ifdef ENABLE_WALLET
-    // Parse URIs on command line -- this can affect Params().isTestNet() / Params().isRegTest() mode
+    // Parse URIs on command line -- this can affect Params() modes
     if (!PaymentServer::ipcParseCommandLine(argc, argv))
         exit(0);
 #endif
 
-    bool isaTestNet = Params().isTestNet() || Params().isRegTest();
+    bool isaTestNet = !Params().isMainNet();
 
     // Do not refer to data directory yet, this can be overridden by Intro::pickDataDirectory
 
