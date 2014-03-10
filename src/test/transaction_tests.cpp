6380180821917c22ecfd89128ee60aae6f4cac33
Peter Todd <pete@petertodd.org>
2014-03-10 17:36:35 -0400

    Add rejection of non-null CHECKMULTISIG dummy values
    This is a source of transaction mutability as the dummy value was
    previously not checked and could be modified to something other than the
    usual OP_0 value.

--- src/test/transaction_tests.cpp
+++ src/test/transaction_tests.cpp
@@ -39,6 +39,7 @@ unsigned int ParseFlags(string strFlags){
         mapFlagNames["P2SH"] = SCRIPT_VERIFY_P2SH;
         mapFlagNames["STRICTENC"] = SCRIPT_VERIFY_STRICTENC;
         mapFlagNames["EVEN_S"] = SCRIPT_VERIFY_EVEN_S;
+        mapFlagNames["NULLDUMMY"] = SCRIPT_VERIFY_NULLDUMMY;
     }
 
     BOOST_FOREACH(string word, words)
