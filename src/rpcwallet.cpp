fd0c4606bc89ab974e39a98fd42f4a28eb874062
Peter Todd <pete@petertodd.org>
2014-03-10 22:43:15 -0400

    Check redeemScript size does not exceed 520 byte limit
    redeemScripts >520bytes can't be spent due to the
    MAX_SCRIPT_ELEMENT_SIZE limit; previously the addmultisigaddress and
    createmultisig RPC calls would let you violate that limit unknowingly.
    
    Also made the wallet code itself check the redeemScript prior to adding
    it to the wallet, which in the (rare) instance that a user has added an
    invalid oversized redeemScript to their wallet causes an error on
    startup. The affected key isn't added to the wallet; other keys are
    unaffected.

--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -871,7 +871,7 @@ Value sendmany(const Array& params, bool fHelp)
 }
 
 // Defined in rpcmisc.cpp
-extern CScript _createmultisig(const Array& params);
+extern CScript _createmultisig_redeemScript(const Array& params);
 
 Value addmultisigaddress(const Array& params, bool fHelp)
 {
@@ -908,7 +908,7 @@ Value addmultisigaddress(const Array& params, bool fHelp)
         strAccount = AccountFromValue(params[2]);
 
     // Construct using pay-to-script-hash:
-    CScript inner = _createmultisig(params);
+    CScript inner = _createmultisig_redeemScript(params);
     CScriptID innerID = inner.GetID();
     pwalletMain->AddCScript(inner);
 
