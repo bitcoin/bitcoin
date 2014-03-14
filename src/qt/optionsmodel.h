680f08cb73cb7a89e69d38470d6413ba5004bd2d
Wladimir J. van der Laan <laanwj@gmail.com>
2014-03-14 07:22:59 +0100

    qt: Show also value for options overridden on command line
    Currently only the name of the option is shown for GUI options
    overridden by command line (or configuration file). This commit
    adds the value of the options as well, which is useful for
    troubleshooting.

--- src/qt/optionsmodel.h
+++ src/qt/optionsmodel.h
@@ -75,6 +75,9 @@ private:
     /* settings that were overriden by command-line */
     QString strOverriddenByCommandLine;
 
+    /// Add option to list of GUI options overridden through command line/config file
+    void addOverriddenOption(const std::string &option);
+
 signals:
     void displayUnitChanged(int unit);
     void transactionFeeChanged(qint64);
