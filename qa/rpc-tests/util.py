771431411f35a1b529d2d727b8988ccd3ec810a3
Wladimir J. van der Laan <laanwj@gmail.com>
2014-03-18 10:11:00 +0100

    Add licenses for tests and test data
    - Add license headers to source files (years based on commit dates)
      in `src/test` as well as `qa`
    - Add `README.md` to `src/test/data` specifying MIT license
    
    Fixes #3848

--- qa/rpc-tests/util.py
+++ qa/rpc-tests/util.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2014 The Bitcoin Core developers
+# Distributed under the MIT/X11 software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #
 # Helpful routines for regression testing
 #
