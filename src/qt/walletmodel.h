fa68fc862a16e4de536aaf10898171c05e479b87
Cozz Lovan <cozzlovan@yahoo.com>
2014-03-19 00:26:14 +0100

    [Qt] rescan progress

--- src/qt/walletmodel.h
+++ src/qt/walletmodel.h
@@ -237,6 +237,9 @@ signals:
     // Coins sent: from wallet, to recipient, in (serialized) transaction:
     void coinsSent(CWallet* wallet, SendCoinsRecipient recipient, QByteArray transaction);
 
+    // Show progress dialog e.g. for rescan
+    void showProgress(const QString &title, int nProgress);
+
 public slots:
     /* Wallet status might have changed */
     void updateStatus();
