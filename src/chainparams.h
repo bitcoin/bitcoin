10dee6c0983012203a59460cfd617927c2496032
jtimon <jtimon@monetize.io>
2014-03-22 20:19:48 +0100

    Add RequireStandard chain parameter

--- src/chainparams.h
+++ src/chainparams.h
@@ -72,6 +72,8 @@ public:
     virtual bool DefaultCheckMemPool() const { return false; }
     /* Allow mining of a min-difficulty block */
     virtual bool AllowMinDifficultyBlocks() const { return false; }
+    /* Make standard checks */
+    virtual bool RequireStandard() const { return true; }
     const string& DataDir() const { return strDataDir; }
     /* Make miner stop after a block is found. In RPC, don't return
      * until nGenProcLimit blocks are generated */
