5b63af7632ea700c8b5e35140f93b70dd33bab0c
Alon Muroch <alonmuroch@gmail.com>
2014-03-25 10:14:27 +0100

    added in qa/rpc-tests a test skeleton, changed listtransactions.py accordingly and added a template for future tests named TEST_EXAMPLE.py

---
+++ qa/rpc-tests/TEST_EXAMPLE.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# Copyright (c) 2014 The Bitcoin Core developers
+# Distributed under the MIT/X11 software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+# Exercise the listtransactions API
+
+# Add python-bitcoinrpc to module search path:
+from util import *
+from skeleton import Skeleton as baseclass
+
+# 1) Change class name to your test
+class MyTest(baseclass):
+    
+    # 2) Change 'MyTest' to class name
+    def run(self):
+        super(MyTest, self).main()
+  
+    # 3) Implement tests
+    def run_test(self,nodes):
+        ''' '''
+
+# 4) Change 'MyTest' to class name
+if __name__ == '__main__':
+    MyTest().run()
