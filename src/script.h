4d7203e6f19dbae1b4e30340427220a3d00c3514
Cory Fields <cory-nospam-@coryfields.com>
2014-03-26 15:57:21 -0400

    script: remove bignum dependency

--- src/script.h
+++ src/script.h
@@ -6,7 +6,6 @@
 #ifndef H_BITCOIN_SCRIPT
 #define H_BITCOIN_SCRIPT
 
-#include "bignum.h"
 #include "key.h"
 #include "util.h"
 
@@ -441,7 +440,6 @@ public:
     explicit CScript(opcodetype b)     { operator<<(b); }
     explicit CScript(const uint256& b) { operator<<(b); }
     explicit CScript(const CScriptNum& b) { operator<<(b); }
-    explicit CScript(const CBigNum& b) { operator<<(b); }
     explicit CScript(const std::vector<unsigned char>& b) { operator<<(b); }
 
 
@@ -474,12 +472,6 @@ public:
         assert(key.size() < OP_PUSHDATA1);
         insert(end(), (unsigned char)key.size());
         insert(end(), key.begin(), key.end());
-        return *this;
-    }
-
-    CScript& operator<<(const CBigNum& b)
-    {
-        *this << b.getvch();
         return *this;
     }
 
