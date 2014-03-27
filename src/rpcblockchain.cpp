70313dd1e7518d410dac30db4906aac4846520a7
Warren Togami <wtogami@gmail.com>
2014-03-26 18:28:43 -1000

    Litecoin: settxfee label in LTC

--- src/rpcblockchain.cpp
+++ src/rpcblockchain.cpp
@@ -106,8 +106,8 @@ Value settxfee(const Array& params, bool fHelp)
 {
     if (fHelp || params.size() < 1 || params.size() > 1)
         throw runtime_error(
-            "settxfee <amount btc/kb>\n"
-            "<amount> is a real and is rounded to the nearest 0.00000001 btc per kb");
+            "settxfee <amount LTC/KB>\n"
+            "<amount> is a real and is rounded to the nearest 0.00000001 LTC per KB");
 
     // Amount
     int64 nAmount = 0;
