0c030f04759ad3e54a27b75f577c5d261ff8229b
paveljanik <Pavel@Janik.cz>
2014-03-31 11:30:36 +0200

    Use the new name Bitcoin Core in the usage string.

--- src/bitcoin-cli.cpp
+++ src/bitcoin-cli.cpp
@@ -39,7 +39,7 @@ static bool AppInitRPC(int argc, char* argv[])
         // First part of help message is specific to RPC client
         std::string strUsage = _("Bitcoin Core RPC client version") + " " + FormatFullVersion() + "\n\n" +
             _("Usage:") + "\n" +
-              "  bitcoin-cli [options] <command> [params]  " + _("Send command to Bitcoin server") + "\n" +
+              "  bitcoin-cli [options] <command> [params]  " + _("Send command to Bitcoin Core") + "\n" +
               "  bitcoin-cli [options] help                " + _("List commands") + "\n" +
               "  bitcoin-cli [options] help <command>      " + _("Get help for a command") + "\n";
 
