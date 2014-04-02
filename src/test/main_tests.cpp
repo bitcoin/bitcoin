57aa5076f7e12bd594bbecb9061b28325502a52e
ditto-b <ditto-b@users.noreply.github.com>
2014-04-02 18:00:08 -0500

    Edit subsidy_limit_test to account for BIP42
    Because no one wants 4 gold mines being discovered every mibillenium.

--- src/test/main_tests.cpp
+++ src/test/main_tests.cpp
@@ -8,7 +8,7 @@ BOOST_AUTO_TEST_SUITE(main_tests)
 BOOST_AUTO_TEST_CASE(subsidy_limit_test)
 {
     uint64_t nSum = 0;
-    for (int nHeight = 0; nHeight < 7000000; nHeight += 1000) {
+    for (int nHeight = 0; nHeight < 14000000; nHeight += 1000) {
         uint64_t nSubsidy = GetBlockValue(nHeight, 0);
         BOOST_CHECK(nSubsidy <= 50 * COIN);
         nSum += nSubsidy * 1000;
