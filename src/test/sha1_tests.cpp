f0aada083e8ea501d5f115231f13fc39d2a9a9bf
Pieter Wuille <pieter.wuille@gmail.com>
2014-04-28 12:20:15 +0200

    Move crypto implementations to src/crypto/

--- src/test/sha1_tests.cpp
+++ src/test/sha1_tests.cpp
@@ -2,7 +2,7 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include "sha1.h"
+#include "crypto/sha1.h"
 #include "util.h"
 
 #include <vector>
--- src/test/sha2_tests.cpp
+++ src/test/sha2_tests.cpp
@@ -2,7 +2,7 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include "sha2.h"
+#include "crypto/sha2.h"
 #include "util.h"
 
 #include <vector>
