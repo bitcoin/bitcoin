d387b8ec15e553db9b9c370314ee359e981fb374
Wladimir J. van der Laan <laanwj@gmail.com>
2014-05-05 13:22:28 +0200

    rpc: add `getblockchaininfo` and `getnetworkinfo`
    Adds two new info query commands that take over information from
    hodge-podge `getinfo`.
    
    Also some new information is added:
    - `getblockchaininfo`
      - `chain`: (string) current chain (main, testnet3, regtest)
      - `verificationprogress: (numeric) estimated verification progress
      - `chainwork`
    - `getnetworkinfo`
      - `localaddresses`: (array) local addresses, from mapLocalHost (fixes #1734)

--- src/rpcserver.h
+++ src/rpcserver.h
@@ -164,6 +164,8 @@ extern json_spirit::Value encryptwallet(const json_spirit::Array& params, bool f
 extern json_spirit::Value validateaddress(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value getinfo(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value getwalletinfo(const json_spirit::Array& params, bool fHelp);
+extern json_spirit::Value getblockchaininfo(const json_spirit::Array& params, bool fHelp);
+extern json_spirit::Value getnetworkinfo(const json_spirit::Array& params, bool fHelp);
 
 extern json_spirit::Value getrawtransaction(const json_spirit::Array& params, bool fHelp); // in rcprawtransaction.cpp
 extern json_spirit::Value listunspent(const json_spirit::Array& params, bool fHelp);
