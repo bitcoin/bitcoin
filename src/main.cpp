35ab9760fbb49ac87174e4887492fc7fcf5d85eb
Wladimir J. van der Laan <laanwj@gmail.com>
2014-05-07 15:12:31 +0200

    Add missing cs_main lock to VerifyDB
    Fixes issue #4139.

--- src/main.cpp
+++ src/main.cpp
@@ -2875,6 +2875,7 @@ bool static LoadBlockIndexDB()
 
 bool VerifyDB(int nCheckLevel, int nCheckDepth)
 {
+    LOCK(cs_main);
     if (chainActive.Tip() == NULL || chainActive.Tip()->pprev == NULL)
         return true;
 
