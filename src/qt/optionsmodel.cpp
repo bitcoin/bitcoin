faa26d0e9553ae7af962d52d7f35dab69e0666e2
Wladimir J. van der Laan <laanwj@gmail.com>
2014-05-08 12:22:47 +0200

    qt: fix compile issue in Qt GUI
    This was introduced in 3e1cf9b. Needs a cast to qint64.

--- src/qt/optionsmodel.cpp
+++ src/qt/optionsmodel.cpp
@@ -93,7 +93,7 @@ void OptionsModel::Init()
     // Wallet
 #ifdef ENABLE_WALLET
     if (!settings.contains("nTransactionFee"))
-        settings.setValue("nTransactionFee", DEFAULT_TRANSACTION_FEE);
+        settings.setValue("nTransactionFee", (qint64)DEFAULT_TRANSACTION_FEE);
     nTransactionFee = settings.value("nTransactionFee").toLongLong(); // if -paytxfee is set, this will be overridden later in init.cpp
     if (mapArgs.count("-paytxfee"))
         addOverriddenOption("-paytxfee");
