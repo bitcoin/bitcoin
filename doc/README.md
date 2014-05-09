c11c72cd5b73e5d4d5acf51b29aad958c55af5ee
Wladimir J. van der Laan <laanwj@gmail.com>
2014-05-09 15:52:48 +0200

    Bump version numbers for 0.9.2 branch split-off
    Show version as 0.9.1.99 until rc1.

--- doc/README.md
+++ doc/README.md
@@ -1,4 +1,4 @@
-Bitcoin 0.9.99 BETA
+Bitcoin 0.9.2 BETA
 =====================
 
 Copyright (c) 2009-2014 Bitcoin Developers
--- src/clientversion.h
+++ src/clientversion.h
@@ -11,8 +11,8 @@
 // These need to be macros, as version.cpp's and bitcoin-qt.rc's voodoo requires it
 #define CLIENT_VERSION_MAJOR       0
 #define CLIENT_VERSION_MINOR       9
-#define CLIENT_VERSION_REVISION    99
-#define CLIENT_VERSION_BUILD       0
+#define CLIENT_VERSION_REVISION    1
+#define CLIENT_VERSION_BUILD       99
 
 // Set to true for release, false for prerelease or test build
 #define CLIENT_VERSION_IS_RELEASE  false
