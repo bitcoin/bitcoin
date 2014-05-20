2585310a2245b6ebffed3fe1318674b2f8eb5b82
Pieter Wuille <pieter.wuille@gmail.com>
2014-05-20 21:45:12 +0200

    Add missing LOCK(cs_main)
    Rebased-By: Wladimir J. van der Laan <laanwj@gmail.com>
    Rebased-From: 305ccaa

--- src/main.cpp
+++ src/main.cpp
@@ -3379,7 +3379,10 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv)
         return true;
     }
 
-    State(pfrom->GetId())->nLastBlockProcess = GetTimeMicros();
+    {
+        LOCK(cs_main);
+        State(pfrom->GetId())->nLastBlockProcess = GetTimeMicros();
+    }
 
 
 
