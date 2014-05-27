44ab8df8f3cfcc0962099c86dec4375b8b462378
Cozz Lovan <cozzlovan@yahoo.com>
2014-05-28 01:38:40 +0200

    [Qt] Fix Transaction details shows wrong To:

--- src/qt/transactiontablemodel.cpp
+++ src/qt/transactiontablemodel.cpp
@@ -222,7 +222,7 @@ public:
             std::map<uint256, CWalletTx>::iterator mi = wallet->mapWallet.find(rec->hash);
             if(mi != wallet->mapWallet.end())
             {
-                return TransactionDesc::toHTML(wallet, mi->second, rec->idx, unit);
+                return TransactionDesc::toHTML(wallet, mi->second, rec, unit);
             }
         }
         return QString("");
