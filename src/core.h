5706875eed95fd8e0a3f9a9725c02c2ac4e65fea
Gavin Andresen <gavinandresen@gmail.com>
2014-05-27 15:44:57 -0400

    Use fee/priority estimates in wallet CreateTransaction
    The wallet now uses the mempool fee estimator with a new
    command-line option: -txconfirmtarget (default: 1) instead
    of using hard-coded fees or priorities.
    
    A new bitcoind that hasn't seen enough transactions to estimate
    will fall back to the old hard-coded minimum priority or
    transaction fee.
    
    -paytxfee option overrides -txconfirmtarget.
    
    Relaying and mining code isn't changed.
    
    For Qt, the coin control dialog now uses priority estimates to
    label transaction priority (instead of hard-coded constants);
    unspent outputs were consistently labeled with a much higher
    priority than is justified by the free transactions actually
    being accepted into blocks.
    
    I did not implement any GUI for setting -txconfirmtarget; I would
    suggest getting rid of the "Pay transaction fee" GUI and replace
    it with either "target number of confirmations" or maybe
    a "faster confirmation <--> lower fee" slider or select box.

--- src/core.h
+++ src/core.h
@@ -131,7 +131,8 @@ public:
     friend bool operator<(const CFeeRate& a, const CFeeRate& b) { return a.nSatoshisPerK < b.nSatoshisPerK; }
     friend bool operator>(const CFeeRate& a, const CFeeRate& b) { return a.nSatoshisPerK > b.nSatoshisPerK; }
     friend bool operator==(const CFeeRate& a, const CFeeRate& b) { return a.nSatoshisPerK == b.nSatoshisPerK; }
-
+    friend bool operator<=(const CFeeRate& a, const CFeeRate& b) { return a.nSatoshisPerK <= b.nSatoshisPerK; }
+    friend bool operator>=(const CFeeRate& a, const CFeeRate& b) { return a.nSatoshisPerK >= b.nSatoshisPerK; }
     std::string ToString() const;
 
     IMPLEMENT_SERIALIZE( READWRITE(nSatoshisPerK); )
