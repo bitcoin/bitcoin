34cb1fc5d6c7293b5c135b85c3dd3668e05ac2b8
LongShao007 <007longshao@gmail.com>
2014-05-28 15:36:49 +0800

    vector subscript out of range
    when i debug it using VC, it will report "Expression: vector subscript out of range".

--- src/script.h
+++ src/script.h
@@ -780,12 +780,12 @@ public:
     void Serialize(Stream &s, int nType, int nVersion) const {
         std::vector<unsigned char> compr;
         if (Compress(compr)) {
-            s << CFlatData(&compr[0], &compr[compr.size()]);
+            s << CFlatData(&compr[0], &compr[compr.size()-1]);
             return;
         }
         unsigned int nSize = script.size() + nSpecialScripts;
         s << VARINT(nSize);
-        s << CFlatData(&script[0], &script[script.size()]);
+        s << CFlatData(&script[0], &script[script.size()-1]);
     }
 
     template<typename Stream>
@@ -794,13 +794,13 @@ public:
         s >> VARINT(nSize);
         if (nSize < nSpecialScripts) {
             std::vector<unsigned char> vch(GetSpecialSize(nSize), 0x00);
-            s >> REF(CFlatData(&vch[0], &vch[vch.size()]));
+            s >> REF(CFlatData(&vch[0], &vch[vch.size()-1]));
             Decompress(nSize, vch);
             return;
         }
         nSize -= nSpecialScripts;
         script.resize(nSize);
-        s >> REF(CFlatData(&script[0], &script[script.size()]));
+        s >> REF(CFlatData(&script[0], &script[script.size()-1]));
     }
 };
 
