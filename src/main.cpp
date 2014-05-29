a114178ae290af7be79d765c05db2b0dee7feab3
Philip Kaufmann <phil.kaufmann@t-online.de>
2014-05-29 12:33:17 +0200

    rename fNoListen to fListen and move to net
    - better code readability and it belongs to net
    - this is a prerequisite for a pull to add -listen to the GUI

--- src/main.cpp
+++ src/main.cpp
@@ -3550,7 +3550,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv)
         if (!pfrom->fInbound)
         {
             // Advertise our address
-            if (!fNoListen && !IsInitialBlockDownload())
+            if (fListen && !IsInitialBlockDownload())
             {
                 CAddress addr = GetLocalAddress(&pfrom->addr);
                 if (addr.IsRoutable())
@@ -4318,7 +4318,7 @@ bool SendMessages(CNode* pto, bool fSendTrickle)
                         pnode->setAddrKnown.clear();
 
                     // Rebroadcast our address
-                    if (!fNoListen)
+                    if (fListen)
                     {
                         CAddress addr = GetLocalAddress(&pnode->addr);
                         if (addr.IsRoutable())
