aed951f53f0cd3d85f923210beafc2ac485d4d31
Cory Fields <cory-nospam-@coryfields.com>
2014-05-29 15:30:46 -0400

    osx: Fix missing dock menu with qt5
    Qt5 Removed the qt_mac_set_dock_menu function and left no replacement. It was
    later re-added and deprecated for backwards-compatibility.
    
    Qt5.2 adds the non-deprecated QMenu::setAsDockMenu(). Use that when possible.

--- src/qt/macdockiconhandler.mm
+++ src/qt/macdockiconhandler.mm
@@ -62,6 +62,8 @@ MacDockIconHandler::MacDockIconHandler() : QObject()
     this->setMainWindow(NULL);
 #if QT_VERSION < 0x050000
     qt_mac_set_dock_menu(this->m_dockMenu);
+#elif QT_VERSION >= 0x050200
+    this->m_dockMenu->setAsDockMenu();
 #endif
     [pool release];
 }
