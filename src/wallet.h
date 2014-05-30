3b17800139af9de34f94d9c431e00bd1074445ed
Toshiyasu Morita <tmorita@peernova.com>
2014-05-30 11:01:01 -0700

    Change nTimeFirstKey initialization to regular 0 instead of 0l

--- src/wallet.h
+++ src/wallet.h
@@ -163,7 +163,7 @@ public:
         nOrderPosNext = 0;
         nNextResend = 0;
         nLastResend = 0;
-	nTimeFirstKey = 0l;
+        nTimeFirstKey = 0;
     }
 
     std::map<uint256, CWalletTx> mapWallet;
