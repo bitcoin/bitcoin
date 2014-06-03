b6327a50652e83c792559a4d95fe6bd5e4257f30
Wladimir J. van der Laan <laanwj@gmail.com>
2014-06-03 16:12:19 +0200

    Add `-version` option to get just the version
    Adds a `-version` or `--version` option to print just the version
    of the program for bitcoind, bitcoin-cli and bitcoin-qt.
    
    Also make it that `-help` can be used to display the help (as well as
    existing `--help`). Up to now, `-help` was the only option that didn't
    work with either one or two dashes.

--- src/bitcoin-cli.cpp
+++ src/bitcoin-cli.cpp
@@ -39,16 +39,18 @@ static bool AppInitRPC(int argc, char* argv[])
         return false;
     }
 
-    if (argc<2 || mapArgs.count("-?") || mapArgs.count("--help"))
+    if (argc<2 || mapArgs.count("-?") || mapArgs.count("-help") || mapArgs.count("-version"))
     {
-        // First part of help message is specific to RPC client
-        std::string strUsage = _("Bitcoin Core RPC client version") + " " + FormatFullVersion() + "\n\n" +
-            _("Usage:") + "\n" +
-              "  bitcoin-cli [options] <command> [params]  " + _("Send command to Bitcoin Core") + "\n" +
-              "  bitcoin-cli [options] help                " + _("List commands") + "\n" +
-              "  bitcoin-cli [options] help <command>      " + _("Get help for a command") + "\n";
+        std::string strUsage = _("Bitcoin Core RPC client version") + " " + FormatFullVersion() + "\n";
+        if (!mapArgs.count("-version"))
+        {
+            strUsage += "\n" + _("Usage:") + "\n" +
+                  "  bitcoin-cli [options] <command> [params]  " + _("Send command to Bitcoin Core") + "\n" +
+                  "  bitcoin-cli [options] help                " + _("List commands") + "\n" +
+                  "  bitcoin-cli [options] help <command>      " + _("Get help for a command") + "\n";
 
-        strUsage += "\n" + HelpMessageCli(true);
+            strUsage += "\n" + HelpMessageCli(true);
+        }
 
         fprintf(stdout, "%s", strUsage.c_str());
         return false;
