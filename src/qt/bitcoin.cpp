ac547b812bbc950c3f34337e4fa8199e649dd6ce
Cory Fields <cory-nospam-@coryfields.com>
2014-06-03 23:42:53 -0400

    build: allow linux to build against static qt5

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -52,7 +52,11 @@ Q_IMPORT_PLUGIN(qkrcodecs)
 Q_IMPORT_PLUGIN(qtaccessiblewidgets)
 #else
 Q_IMPORT_PLUGIN(AccessibleFactory)
+#if defined(QT_QPA_PLATFORM_XCB)
+Q_IMPORT_PLUGIN(QXcbIntegrationPlugin);
+#elif defined(QT_QPA_PLATFORM_WINDOWS)
 Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin);
+#endif
 #endif
 #endif
 
