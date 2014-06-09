ad07eff4c110eb717519680ebdf68a1f69767398
Flavien BINET <flavien.binet@gmail.com>
2014-06-09 18:08:41 +0200

    Cleaned duplicate method from rpc client

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -69,24 +69,7 @@ double GetDifficulty(const CBlockIndex* blockindex = NULL)
         else
             blockindex = GetLastBlockIndex(pindexBest, false);
     }
-
-    int nShift = (blockindex->nBits >> 24) & 0xff;
-
-    double dDiff =
-        (double)0x0000ffff / (double)(blockindex->nBits & 0x00ffffff);
-
-    while (nShift < 29)
-    {
-        dDiff *= 256.0;
-        nShift++;
-    }
-    while (nShift > 29)
-    {
-        dDiff /= 256.0;
-        nShift--;
-    }
-
-    return dDiff;
+    return blockindex->GetBlockDifficulty();
 }
 
 
