654871d43677947d124673c9e0dd2984f0d3ca61
jtimon <jtimon@monetize.io>
2014-06-20 20:55:42 +0200

    replace ComputeMinWork with CheckMinWork

--- src/test/DoS_tests.cpp
+++ src/test/DoS_tests.cpp
@@ -106,11 +106,7 @@ static bool CheckNBits(unsigned int nbits1, int64_t time1, unsigned int nbits2, 
         return CheckNBits(nbits2, time2, nbits1, time1);
     int64_t deltaTime = time2-time1;
 
-    uint256 required;
-    required.SetCompact(ComputeMinWork(nbits1, deltaTime));
-    uint256 have;
-    have.SetCompact(nbits2);
-    return (have <= required);
+    return CheckMinWork(nbits2, nbits1, deltaTime);
 }
 
 BOOST_AUTO_TEST_CASE(DoS_checknbits)
