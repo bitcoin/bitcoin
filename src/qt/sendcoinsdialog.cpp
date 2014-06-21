46b54ee5216378bc742cea111a92af2c0ccf35b7
CryptoManiac <balthazar@yandex.ru>
2014-06-21 16:08:37 +0400

    Convert UI files to UNIX EOL format.

--- src/qt/sendcoinsdialog.cpp
+++ src/qt/sendcoinsdialog.cpp
@@ -462,7 +462,7 @@ void SendCoinsDialog::coinControlChangeEdited(const QString & text)
         else if (!CBitcoinAddress(text.toStdString()).IsValid())
         {
             ui->labelCoinControlChangeLabel->setStyleSheet("QLabel{color:red;}");
-            ui->labelCoinControlChangeLabel->setText(tr("WARNING: Invalid Bitcoin address"));
+            ui->labelCoinControlChangeLabel->setText(tr("WARNING: Invalid NovaCoin address"));
         }
         else
         {
