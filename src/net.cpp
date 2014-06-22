3dc1464f0a87e26f3aa39c446eb4d102a8c84dc2
Philip Kaufmann <phil.kaufmann@t-online.de>
2014-06-22 14:52:38 +0200

    add missing vhListenSocket.clear(); to CNetCleanup()

--- src/net.cpp
+++ src/net.cpp
@@ -1784,6 +1784,7 @@ public:
             delete pnode;
         vNodes.clear();
         vNodesDisconnected.clear();
+        vhListenSocket.clear();
         delete semOutbound;
         semOutbound = NULL;
         delete pnodeLocalHost;
