f7c999cd07f6e71af26df42787a60cb51e4df3ae
Cory Fields <cory-nospam-@coryfields.com>
2014-06-23 14:04:24 -0400

    build: fix build weirdness after b99dfe33.
    bitcoin-config.h moved, but the old file is likely to still exist when
    reconfiguring or switching branches. This would've caused files to not rebuild
    correctly, and other strange problems.
    
    Make the path explicit so that the old one cannot be found.
    
    Core libs use config/bitcoin-config.h.
    
    Libs (like crypto) which don't want access to bitcoin's headers continue
    to use -Iconfig and #include bitcoin-config.h.

--- src/net.cpp
+++ src/net.cpp
@@ -4,7 +4,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #if defined(HAVE_CONFIG_H)
-#include "bitcoin-config.h"
+#include "config/bitcoin-config.h"
 #endif
 
 #include "net.h"
