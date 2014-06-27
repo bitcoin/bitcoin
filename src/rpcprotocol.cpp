ed49917d410d8a6b4f128192fcee1ff57530dd14
Jeff Garzik <jgarzik@bitpay.com>
2014-06-27 00:10:53 -0400

    Move AcceptedConnection class to rpcserver.h.
    Also, add parens to HTTPReply() to assist readability.

--- src/rpcprotocol.cpp
+++ src/rpcprotocol.cpp
@@ -105,8 +105,8 @@ string HTTPReply(int nStatus, const string& strMsg, bool keepalive,
         strMsg.size(),
         contentType,
         FormatFullVersion(),
-        headersOnly ? "" :
-	    useInternalContent ? cStatus : strMsg.c_str());
+        (headersOnly ? "" :
+            (useInternalContent ? cStatus : strMsg.c_str())));
 }
 
 bool ReadHTTPRequestLine(std::basic_istream<char>& stream, int &proto,
