9430431f4aa3510e26c0a580d266618ab1984992
Philip Kaufmann <phil.kaufmann@t-online.de>
2014-06-27 15:09:41 +0200

    set shutdown title to main window title
    - this ensures we don't show Bitcoin-Qt as that is still our
      internal application name
    - fixes #4427

--- src/qt/utilitydialog.cpp
+++ src/qt/utilitydialog.cpp
@@ -117,6 +117,7 @@ void ShutdownWindow::showShutdownWindow(BitcoinGUI *window)
         tr("Bitcoin Core is shutting down...") + "<br /><br />" +
         tr("Do not shut down the computer until this window disappears.")));
     shutdownWindow->setLayout(layout);
+    shutdownWindow->setWindowTitle(window->windowTitle());
 
     // Center shutdown window at where main window was
     const QPoint global = window->mapToGlobal(window->rect().center());
