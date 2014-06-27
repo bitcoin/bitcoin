ed5769f536f663a8deb8e8b7a68681cebaa52bdd
Jeff Garzik <jgarzik@bitpay.com>
2014-06-27 00:10:53 -0400

    Move AcceptedConnection class to rpcserver.h.
    Also, add parens to HTTPReply() to assist readability.

--- src/rpcprotocol.h
+++ src/rpcprotocol.h
@@ -71,16 +71,6 @@ enum RPCErrorCode
     RPC_WALLET_ALREADY_UNLOCKED     = -17, // Wallet is already unlocked
 };
 
-class AcceptedConnection
-{
-public:
-    virtual ~AcceptedConnection() {}
-
-    virtual std::iostream& stream() = 0;
-    virtual std::string peer_address_to_string() const = 0;
-    virtual void close() = 0;
-};
-
 //
 // IOStream device that speaks SSL but can also speak non-SSL
 //
