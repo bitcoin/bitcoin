9393632488c124dd13ef3b67f44e26dec3ffaf60
Pieter Wuille <pieter.wuille@gmail.com>
2014-06-28 17:35:22 +0200

    Move non-trivial uint256.h methods to uint256.cpp

--- src/util.h
+++ src/util.h
@@ -156,6 +156,7 @@ bool ParseMoney(const char* pszIn, int64_t& nRet);
 std::string SanitizeString(const std::string& str);
 std::vector<unsigned char> ParseHex(const char* psz);
 std::vector<unsigned char> ParseHex(const std::string& str);
+signed char HexDigit(char c);
 bool IsHex(const std::string& str);
 std::vector<unsigned char> DecodeBase64(const char* p, bool* pfInvalid = NULL);
 std::string DecodeBase64(const std::string& str);
