a9ebd8f7e53832ef06c7926ccaccc2fb23afb596
Philip Kaufmann <phil.kaufmann@t-online.de>
2014-07-04 16:51:25 +0200

    [Qt] remove dup includes in bitcoingui

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -34,13 +34,10 @@
 #include <QDesktopWidget>
 #include <QDragEnterEvent>
 #include <QIcon>
-#include <QLabel>
 #include <QListWidget>
-#include <QMenu>
 #include <QMenuBar>
 #include <QMessageBox>
 #include <QMimeData>
-#include <QPoint>
 #include <QProgressBar>
 #include <QProgressDialog>
 #include <QSettings>
@@ -50,8 +47,6 @@
 #include <QTimer>
 #include <QToolBar>
 #include <QVBoxLayout>
-
-
 
 #if QT_VERSION < 0x050000
 #include <QUrl>
