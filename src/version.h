d6b213af7b3304d6d3d5faea9da2b7ec08728060
CryptoManiac <balthazar@yandex.ru>
2014-07-11 02:19:02 +0400

    Bump version

--- src/version.h
+++ src/version.h
@@ -30,7 +30,7 @@ static const int DATABASE_VERSION = 70507;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 60012;
+static const int PROTOCOL_VERSION = 60015;
 
 // earlier versions not supported as of Feb 2012, and are disconnected
 static const int MIN_PROTO_VERSION = 209;
