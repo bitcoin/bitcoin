d3fd120f325d710a64d0d5efe68e3fb29e33cd38
Pieter Wuille <pieter.wuille@gmail.com>
2014-07-12 00:02:35 +0200

    Headers-first synchronization
    Many changes:
    * Do not use 'getblocks', but 'getheaders', and use it to build a headers tree.
    * Blocks are fetched in parallel from all available outbound peers, using a
      limited moving window. When one peer stalls the movement of the window, it is
      disconnected.
    * No more orphan blocks. At all. We only ever request a block for which we have
      verified the headers, and store it to disk immediately. This means that a
      disk-fill attack would require PoW.
    * Require protocol version 31800 for every peer (released in december 2010).
    * No more syncnode (we sync from everyone we can, though limited to 1 during
      initial *headers* sync).
    * Introduce some extra named constants, comments and asserts.

--- src/version.h
+++ src/version.h
@@ -33,8 +33,11 @@ static const int PROTOCOL_VERSION = 70002;
 // initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
+// In this version, 'getheaders' was introduced.
+static const int GETHEADERS_VERSION = 31800;
+
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 209;
+static const int MIN_PEER_PROTO_VERSION = GETHEADERS_VERSION;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
