4d5e5da914251463d56d5ee1f42f888298be2b3b
Whit J <whitj00@users.noreply.github.com>
2014-07-14 11:22:35 -0700

    qt: Make error message for failed export a little friendlier
    Closes #4528.

--- src/qt/addressbookpage.cpp
+++ src/qt/addressbookpage.cpp
@@ -282,7 +282,7 @@ void AddressBookPage::on_exportButton_clicked()
 
     if(!writer.write()) {
         QMessageBox::critical(this, tr("Exporting Failed"),
-            tr("There was an error trying to save the address list to %1.").arg(filename));
+            tr("There was an error trying to save the address list to %1. Please try again.").arg(filename));
     }
 }
 
