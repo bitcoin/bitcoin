1107619db606db34a0ef5b8e943c936d4c98aeca
Wladimir J. van der Laan <laanwj@gmail.com>
2014-07-18 16:36:29 +0200

    qt: Remove unused functions from BitcoinUnits
    Remove two functions that are now unused.

--- src/qt/bitcoinunits.h
+++ src/qt/bitcoinunits.h
@@ -82,10 +82,6 @@ public:
     static QString description(int unit);
     //! Number of Satoshis (1e-8) per unit
     static qint64 factor(int unit);
-    //! Max amount per unit
-    static qint64 maxAmount(int unit);
-    //! Number of amount digits (to represent max number of coins)
-    static int amountDigits(int unit);
     //! Number of decimals left
     static int decimals(int unit);
     //! Format as string
