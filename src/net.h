c91a9471be7a96311a7e1452a3624aa557bc185d
Wladimir J. van der Laan <laanwj@gmail.com>
2014-07-30 15:31:36 +0200

    Add IsReachable(net) function
    Allows other parts of the program to query for reachable
    status of a network. Similar to IsLimited(net).

--- src/net.h
+++ src/net.h
@@ -106,6 +106,7 @@ bool AddLocal(const CNetAddr& addr, int nScore = LOCAL_NONE);
 bool SeenLocal(const CService& addr);
 bool IsLocal(const CService& addr);
 bool GetLocal(CService &addr, const CNetAddr *paddrPeer = NULL);
+bool IsReachable(enum Network net);
 bool IsReachable(const CNetAddr &addr);
 void SetReachable(enum Network net, bool fFlag = true);
 CAddress GetLocalAddress(const CNetAddr *paddrPeer = NULL);
