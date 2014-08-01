2b600992e8de0cc9a484e3bf25fe71ac50adadb7
Philip Kaufmann <phil.kaufmann@t-online.de>
2014-08-01 08:39:06 +0200

    add license header to core_io.h, core_read/_write.cpp

--- src/core_read.cpp
+++ src/core_read.cpp
@@ -1,3 +1,6 @@
+// Copyright (c) 2009-2014 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "core_io.h"
 #include "core.h"
