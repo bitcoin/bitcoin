8a36c327b723c616d64f5c1d190206a021504fa0
Philip Kaufmann <phil.kaufmann@t-online.de>
2014-08-01 08:41:02 +0200

    include file ordering cleanup for core_io.h, core_read/_write.cpp

--- src/core_write.cpp
+++ src/core_write.cpp
@@ -3,12 +3,14 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "core_io.h"
-#include "univalue/univalue.h"
-#include "script.h"
+
+#include "base58.h"
 #include "core.h"
+#include "script.h"
 #include "serialize.h"
 #include "util.h"
-#include "base58.h"
+
+#include "univalue/univalue.h"
 
 using namespace std;
 
@@ -88,4 +90,3 @@ void TxToUniv(const CTransaction& tx, const uint256& hashBlock, UniValue& entry)
     if (hashBlock != 0)
         entry.pushKV("blockhash", hashBlock.GetHex());
 }
-
