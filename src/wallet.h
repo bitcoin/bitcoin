7450e8fff8b5bb12fd029e824b789686b5701530
Pieter Wuille <pieter.wuille@gmail.com>
2014-08-07 23:00:01 +0200

    Add a way to limit deserialized string lengths
    and use it for most strings being serialized.
    
    Rebased-From: 216e9a4

--- src/wallet.h
+++ src/wallet.h
@@ -756,7 +756,7 @@ public:
         READWRITE(vchPrivKey);
         READWRITE(nTimeCreated);
         READWRITE(nTimeExpires);
-        READWRITE(strComment);
+        READWRITE(LIMITED_STRING(strComment, 65536));
     )
 };
 
@@ -831,7 +831,7 @@ public:
         // Note: strAccount is serialized as part of the key, not here.
         READWRITE(nCreditDebit);
         READWRITE(nTime);
-        READWRITE(strOtherAccount);
+        READWRITE(LIMITED_STRING(strOtherAccount, 65536));
 
         if (!fRead)
         {
@@ -847,7 +847,7 @@ public:
             }
         }
 
-        READWRITE(strComment);
+        READWRITE(LIMITED_STRING(strComment, 65536));
 
         size_t nSepPos = strComment.find("\0", 0, 1);
         if (fRead)
