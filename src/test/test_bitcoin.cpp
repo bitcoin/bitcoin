8afc3fe0a5b325c6eeefa52aa9b0300b945e68d9
SergioDemianLerner <Sergio.d.Lerner@gmail.com>
2014-08-12 16:33:33 -0300

    Bad ident. TAB -> space conversion made

--- src/test/test_bitcoin.cpp
+++ src/test/test_bitcoin.cpp
@@ -16,101 +16,101 @@ extern void noui_connect();
 
 bool keepTestEvidence = false;
     
-	void TestingSetupManager::SetupGenesisBlockChain()
-	{
-		if (chainActive.Tip()->GetBlockHash()!=Params().HashGenesisBlock()) {
-			DestroyBlockChain();
-			CreateBlockChain();
-		}
-	
+void TestingSetupManager::SetupGenesisBlockChain()
+{
+	if (chainActive.Tip()->GetBlockHash()!=Params().HashGenesisBlock()) {
+		DestroyBlockChain();
+		CreateBlockChain();
 	}
-	
-	void TestingSetupManager::CreateBlockChain()
-	{
-	#ifdef ENABLE_WALLET
-        bitdb.MakeMock();
-	#endif
-		fReopenDebugLog =true; // Important when switching between two test block-chains, to move the debug.log to the new location
-	    pathTemp = GetTempPath() / strprintf("test_bitcoin_%lu_%i", (unsigned long)GetTime(), (int)(GetRand(100000)));
-        boost::filesystem::create_directories(pathTemp);
-        mapArgs["-datadir"] = pathTemp.string();
-        pblocktree = new CBlockTreeDB(1 << 20, true);
-        pcoinsdbview = new CCoinsViewDB(1 << 23, true);
-        pcoinsTip = new CCoinsViewCache(*pcoinsdbview);
-        InitBlockIndex();
-		#ifdef ENABLE_WALLET
-        bool fFirstRun;
-        pwalletMain = new CWallet("wallet.dat");
-        pwalletMain->LoadWallet(fFirstRun);
-        RegisterWallet(pwalletMain);
-		#endif
-	}
-	
-	void ResetBlockIndex();
-	
-	void TestingSetupManager::DestroyBlockChain()
-	{
-		// Clear the mempool so there are no transactions with missing inputs left.
-		mempool.clear();
-		
-	#ifdef ENABLE_WALLET
-	    UnregisterWallet(pwalletMain);
-        delete pwalletMain;
-        pwalletMain = NULL;
-	#endif
-	 
-		UnloadBlockIndex();
-		ResetBlockIndex();
-	
-	#ifdef ENABLE_WALLET
-        bitdb.Flush(true);
-	#endif
-	
-	    delete pcoinsTip;
-        delete pcoinsdbview;
-        delete pblocktree;		
-		ClearDatadirCache();
-		
-		if (!keepTestEvidence)
-			boost::filesystem::remove_all(pathTemp);
-		
-	}
-	
-    void TestingSetupManager::Start() {
-        //fPrintToDebugLog = false; // don't want to write to debug.log file
-        SelectParams(CBaseChainParams::MAIN);
-        noui_connect();
 
-        CreateBlockChain();
+}
 
-        nScriptCheckThreads = 3;
-        for (int i=0; i < nScriptCheckThreads-1; i++)
-            threadGroup.create_thread(&ThreadScriptCheck);
-        RegisterNodeSignals(GetNodeSignals());		
-    }
-	
-    void TestingSetupManager::Stop()
-    {
-        threadGroup.interrupt_all();
-        threadGroup.join_all();
-        UnregisterNodeSignals(GetNodeSignals());
+void TestingSetupManager::CreateBlockChain()
+{
+#ifdef ENABLE_WALLET
+	bitdb.MakeMock();
+#endif
+	fReopenDebugLog =true; // Important when switching between two test block-chains, to move the debug.log to the new location
+	pathTemp = GetTempPath() / strprintf("test_bitcoin_%lu_%i", (unsigned long)GetTime(), (int)(GetRand(100000)));
+	boost::filesystem::create_directories(pathTemp);
+	mapArgs["-datadir"] = pathTemp.string();
+	pblocktree = new CBlockTreeDB(1 << 20, true);
+	pcoinsdbview = new CCoinsViewDB(1 << 23, true);
+	pcoinsTip = new CCoinsViewCache(*pcoinsdbview);
+	InitBlockIndex();
+	#ifdef ENABLE_WALLET
+	bool fFirstRun;
+	pwalletMain = new CWallet("wallet.dat");
+	pwalletMain->LoadWallet(fFirstRun);
+	RegisterWallet(pwalletMain);
+	#endif
+}
 
-        DestroyBlockChain();
-    }
+void ResetBlockIndex();
+
+void TestingSetupManager::DestroyBlockChain()
+{
+	// Clear the mempool so there are no transactions with missing inputs left.
+	mempool.clear();
 	
-	TestingSetupManager::TestingSetupManager() 
-	{
-        keepTestEvidence =false;
-    }
+#ifdef ENABLE_WALLET
+	UnregisterWallet(pwalletMain);
+	delete pwalletMain;
+	pwalletMain = NULL;
+#endif
+ 
+	UnloadBlockIndex();
+	ResetBlockIndex();
+
+#ifdef ENABLE_WALLET
+	bitdb.Flush(true);
+#endif
+
+	delete pcoinsTip;
+	delete pcoinsdbview;
+	delete pblocktree;      
+	ClearDatadirCache();
+	
+	if (!keepTestEvidence)
+		boost::filesystem::remove_all(pathTemp);
+	
+}
+
+void TestingSetupManager::Start() {
+	//fPrintToDebugLog = false; // don't want to write to debug.log file
+	SelectParams(CBaseChainParams::MAIN);
+	noui_connect();
+
+	CreateBlockChain();
+
+	nScriptCheckThreads = 3;
+	for (int i=0; i < nScriptCheckThreads-1; i++)
+		threadGroup.create_thread(&ThreadScriptCheck);
+	RegisterNodeSignals(GetNodeSignals());      
+}
+
+void TestingSetupManager::Stop()
+{
+	threadGroup.interrupt_all();
+	threadGroup.join_all();
+	UnregisterNodeSignals(GetNodeSignals());
+
+	DestroyBlockChain();
+}
+
+TestingSetupManager::TestingSetupManager() 
+{
+	keepTestEvidence =false;
+}
 
 
 struct TestingSetup {
     
     TestingSetup() 
-	{
+    {
         testingSetupManager.Start();
     }
-	
+    
     ~TestingSetup()
     {
         testingSetupManager.Stop();
