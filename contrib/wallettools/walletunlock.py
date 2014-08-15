db57bbdf8519854aef0b46e0f429f48fea56d5fb
CryptoManiac <balthazar@yandex.ru>
2014-08-16 02:25:09 +0400

    Set port number to 8344

--- contrib/wallettools/walletunlock.py
+++ contrib/wallettools/walletunlock.py
@@ -1,4 +1,5 @@
 from jsonrpc import ServiceProxy
-access = ServiceProxy("http://127.0.0.1:8332")
+access = ServiceProxy("http://127.0.0.1:8344")
 pwd = raw_input("Enter wallet passphrase: ")
 access.walletpassphrase(pwd, 60)
+
