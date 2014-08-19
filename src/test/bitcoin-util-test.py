044da18779300c49c2f0b37202ce0b6c73807fca
Jeff Garzik <jgarzik@bitpay.com>
2014-08-19 10:28:58 -0400

    Add "it works" test for bitcoin-tx

---
+++ src/test/bitcoin-util-test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# Copyright 2014 BitPay, Inc.
+# Distributed under the MIT/X11 software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+import os
+import bctest
+
+if __name__ == '__main__':
+	bctest.bctester(os.environ["srcdir"] + "/test/data",
+			"bitcoin-util-test.json")
+
