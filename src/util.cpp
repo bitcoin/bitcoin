9ebc66e3624f8604a73ab1c2eee5b2104ff9ef4d
Wladimir J. van der Laan <laanwj@gmail.com>
2014-08-20 17:43:56 +0200

    Move SetThreadPriority implementation to util.cpp instead of the header
    Put the THREAD_* and PRIO_ constants in compat.h.

--- src/util.cpp
+++ src/util.cpp
@@ -1286,3 +1286,16 @@ std::string FormatParagraph(const std::string in, size_t width, size_t indent)
     }
     return out.str();
 }
+
+void SetThreadPriority(int nPriority)
+{
+#ifdef WIN32
+    SetThreadPriority(GetCurrentThread(), nPriority);
+#else // WIN32
+#ifdef PRIO_THREAD
+    setpriority(PRIO_THREAD, 0, nPriority);
+#else // PRIO_THREAD
+    setpriority(PRIO_PROCESS, 0, nPriority);
+#endif // PRIO_THREAD
+#endif // WIN32
+}
