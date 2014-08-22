d7c2f4e2ef61b2276a8241f9996b30250ac8cb49
Michael Ford <fanquake@gmail.com>
2014-08-22 18:08:41 +0800

    Update linearize.py default to block 295,000

--- contrib/linearize/linearize.py
+++ contrib/linearize/linearize.py
@@ -117,7 +117,7 @@ if __name__ == '__main__':
 	if 'min_height' not in settings:
 		settings['min_height'] = 0
 	if 'max_height' not in settings:
-		settings['max_height'] = 279000
+		settings['max_height'] = 295000
 	if 'rpcuser' not in settings or 'rpcpassword' not in settings:
 		print "Missing username and/or password in cfg file"
 		sys.exit(1)
