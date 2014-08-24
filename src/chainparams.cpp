a2b7010b58936e4bad2d6ef8f00cf26a54581d56
Karel Mašek <karel.masek@4finance.com>
2014-08-24 19:50:14 +0200

    new params

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -37,34 +37,34 @@ public:
         nDefaultPort = 9340;
         nRPCPort = 9341;
         bnProofOfWorkLimit = CBigNum(~uint256(0) >> 32);
-        nSubsidyHalvingInterval = 210000;
+        nSubsidyHalvingInterval = 2100000;
 
         // Build the genesis block. Note that the output of the genesis coinbase cannot
         // be spent as it did not originally exist in the database.
         //
-        // CBlock(hash=000000000019d6, ver=1, hashPrevBlock=00000000000000, hashMerkleRoot=4a5e1e, nTime=1405338325, nBits=1d00ffff, nNonce=1678912305, vtx=1)
+        // CBlock(hash=000000000019d6, ver=1, hashPrevBlock=00000000000000, hashMerkleRoot=4a5e1e, nTime=1408638282, nBits=1d00ffff, nNonce=1408638282, vtx=1)
         //   CTransaction(hash=4a5e1e, ver=1, vin.size=1, vout.size=1, nLockTime=0)
-        //     CTxIn(COutPoint(000000, -1), coinbase 04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73)
+        //     CTxIn(COutPoint(000000, -1), coinbase 04ffff001d01042654686520696e63657074696f6e206f662043726f776e636f696e2032302f4175672f32303134)
         //     CTxOut(nValue=50.00000000, scriptPubKey=0x5F1DF16B2B704C8A578D0B)
         //   vMerkleTree: 4a5e1e
-        const char* pszTimestamp = "The inception of Crowncoin 14/Jul/2014";
+        const char* pszTimestamp = "The inception of Crowncoin 20/Aug/2014";
         CTransaction txNew;
         txNew.vin.resize(1);
         txNew.vout.resize(1);
         txNew.vin[0].scriptSig = CScript() << 486604799 << CBigNum(4) << vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
-        txNew.vout[0].nValue = 50 * COIN;
+        txNew.vout[0].nValue = 10 * COIN;
         txNew.vout[0].scriptPubKey = CScript() << ParseHex("04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f") << OP_CHECKSIG;
         genesis.vtx.push_back(txNew);
         genesis.hashPrevBlock = 0;
         genesis.hashMerkleRoot = genesis.BuildMerkleTree();
         genesis.nVersion = 1;
-        genesis.nTime    = 1405338325;
+        genesis.nTime    = 1408638282;
         genesis.nBits    = 0x1d00ffff;
-        genesis.nNonce   = 1678912305;
+        genesis.nNonce   = 2324206466;
 
         hashGenesisBlock = genesis.GetHash();
-        assert(hashGenesisBlock == uint256("0x000000005f94b67f20bf3cad62cf7cf74fd7f2878574d3e81015d38ffacd7b6e"));
-        assert(genesis.hashMerkleRoot == uint256("0xe4538881d26d67a9ed949f40880ceca47886489dff873a61d078e00b2658f0b2"));
+        assert(hashGenesisBlock == uint256("0x000000005fd263415ed590e85d8a00ecf4ac709534bb6e8b4f64f71a63a0163a"));
+        assert(genesis.hashMerkleRoot == uint256("0x870cfca6680c451f7aa42832fa1b8aea250c7daac458a2c29c7b078764852257"));
 
         // vSeeds.push_back(CDNSSeedData("bitcoin.sipa.be", "seed.bitcoin.sipa.be"));
         // vSeeds.push_back(CDNSSeedData("bluematt.me", "dnsseed.bluematt.me"));
