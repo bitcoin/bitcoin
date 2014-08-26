b144a74a08a856997f48a3c83f0bd080cf9432f9
Cory Fields <cory-nospam-@coryfields.com>
2014-08-26 15:17:43 -0400

    depends: bump miniupnpc to 1.9.20140701.
    This is the forward-port of a9c6eef915ee264ccf4169d5e8769a6be1fa101a

--- depends/packages/miniupnpc.mk
+++ depends/packages/miniupnpc.mk
@@ -1,8 +1,8 @@
 package=miniupnpc
-$(package)_version=1.9
+$(package)_version=1.9.20140701
 $(package)_download_path=http://miniupnp.free.fr/files
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=2923e453e880bb949e3d4da9f83dd3cb6f08946d35de0b864d0339cf70934464
+$(package)_sha256_hash=26f3985bad7768b8483b793448ae49414cdc4451d0ec83e7c1944367e15f9f07
 
 define $(package)_set_vars
 $(package)_build_opts=CC="$($(package)_cc)"
