4d04492bf217bb5466135f61f89eac835caac321
Philip Kaufmann <phil.kaufmann@t-online.de>
2014-08-28 22:26:56 +0200

    add missing copyright headers

--- src/utilmoneystr.cpp
+++ src/utilmoneystr.cpp
@@ -1,3 +1,8 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2014 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #include "utilmoneystr.h"
 
 #include "core.h"
