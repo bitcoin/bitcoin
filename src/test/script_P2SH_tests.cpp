65b35937fb7a51cf51a6c98473a1934645001a27
jtimon <jtimon@blockstream.io>
2014-08-29 22:07:39 +0200

    Move scriptutils.o to wallet

--- src/test/script_P2SH_tests.cpp
+++ src/test/script_P2SH_tests.cpp
@@ -7,7 +7,10 @@
 #include "main.h"
 #include "script/script.h"
 #include "script/sign.h"
+
+#ifdef ENABLE_WALLET
 #include "scriptutils.h"
+#endif
 
 #include <vector>
 
@@ -95,7 +98,9 @@ BOOST_AUTO_TEST_CASE(sign)
         txTo[i].vin[0].prevout.n = i;
         txTo[i].vin[0].prevout.hash = txFrom.GetHash();
         txTo[i].vout[0].nValue = 1;
+#ifdef ENABLE_WALLET
         BOOST_CHECK_MESSAGE(IsMine(keystore, txFrom.vout[i].scriptPubKey), strprintf("IsMine %d", i));
+#endif
     }
     for (int i = 0; i < 8; i++)
     {
@@ -189,7 +194,9 @@ BOOST_AUTO_TEST_CASE(set)
         txTo[i].vin[0].prevout.hash = txFrom.GetHash();
         txTo[i].vout[0].nValue = 1*CENT;
         txTo[i].vout[0].scriptPubKey = inner[i];
+#ifdef ENABLE_WALLET
         BOOST_CHECK_MESSAGE(IsMine(keystore, txFrom.vout[i].scriptPubKey), strprintf("IsMine %d", i));
+#endif
     }
     for (int i = 0; i < 4; i++)
     {
