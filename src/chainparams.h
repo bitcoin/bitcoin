cc972107997122cb31f015b4e70f81781dbce784
jtimon <jtimon@blockstream.io>
2014-08-31 22:32:52 +0200

    Add fTestnetToBeDeprecatedFieldRPC to CChainParams

--- src/chainparams.h
+++ src/chainparams.h
@@ -72,6 +72,8 @@ public:
     /* Make miner stop after a block is found. In RPC, don't return
      * until nGenProcLimit blocks are generated */
     bool MineBlocksOnDemand() const { return fMineBlocksOnDemand; }
+    /* In the future use NetworkIDString() for RPC fields */
+    bool TestnetToBeDeprecatedFieldRPC() const { return fTestnetToBeDeprecatedFieldRPC; }
     CBaseChainParams::Network NetworkID() const { return networkID; }
     /* Return the BIP70 network string (main, test or regtest) */
     std::string NetworkIDString() const { return strNetworkID; }
@@ -108,6 +110,7 @@ protected:
     bool fRequireStandard;
     bool fMineBlocksOnDemand;
     bool fSkipProofOfWorkCheck;
+    bool fTestnetToBeDeprecatedFieldRPC;
 };
 
 /** Modifiable parameters interface is used by test cases to adapt the parameters in order
