a94d5711f417321d3df7030b6e6ccec12bd81acb
Pieter Wuille <pieter.wuille@gmail.com>
2014-09-04 02:01:10 +0200

    checkpoints.cpp depends on main, it can use mapBlockIndex directly

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -146,7 +146,7 @@ namespace Checkpoints {
         return checkpoints.rbegin()->first;
     }
 
-    CBlockIndex* GetLastCheckpoint(const std::map<uint256, CBlockIndex*>& mapBlockIndex)
+    CBlockIndex* GetLastCheckpoint()
     {
         if (!fEnabled)
             return NULL;
