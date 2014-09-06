8d657a651735426388c5f7462e2dbf24225a26cb
ENikS <evgeni@eniks.com>
2014-09-06 15:59:59 -0400

    Fixing compiler warning C4800: 'type' forcing value to bool 'true' or 'false'

--- src/key.cpp
+++ src/key.cpp
@@ -172,9 +172,9 @@ public:
         bool ret;
         BIGNUM bn;
         BN_init(&bn);
-        ret = BN_bin2bn(vch, 32, &bn);
+        ret = BN_bin2bn(vch, 32, &bn) != NULL;
         assert(ret);
-        ret = EC_KEY_regenerate_key(pkey, &bn);
+        ret = EC_KEY_regenerate_key(pkey, &bn) != 0;
         assert(ret);
         BN_clear_free(&bn);
     }
@@ -217,7 +217,7 @@ public:
 
     bool SetPubKey(const CPubKey &pubkey) {
         const unsigned char* pbegin = pubkey.begin();
-        return o2i_ECPublicKey(&pkey, &pbegin, pubkey.size());
+        return o2i_ECPublicKey(&pkey, &pbegin, pubkey.size()) != NULL;
     }
 
     bool Sign(const uint256 &hash, std::vector<unsigned char>& vchSig) {
@@ -553,7 +553,7 @@ bool CPubKey::RecoverCompact(const uint256 &hash, const std::vector<unsigned cha
     if (vchSig.size() != 65)
         return false;
     int recid = (vchSig[0] - 27) & 3;
-    bool fComp = (vchSig[0] - 27) & 4;
+    bool fComp = ((vchSig[0] - 27) & 4) != 0;
 #ifdef USE_SECP256K1
     int pubkeylen = 65;
     if (!secp256k1_ecdsa_recover_compact((const unsigned char*)&hash, 32, &vchSig[1], (unsigned char*)begin(), &pubkeylen, fComp, recid))
