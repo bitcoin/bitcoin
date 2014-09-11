59b922b4c4bd85917bcf7442c9750b6eaf927dee
Pieter Wuille <pieter.wuille@gmail.com>
2014-09-11 19:15:29 +0200

    Move CTxDestination from script/script to script/standard

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -13,6 +13,8 @@
 #include "init.h"
 #include "main.h"
 #include "protocol.h"
+#include "script/script.h"
+#include "script/standard.h"
 #include "util.h"
 
 #ifdef WIN32
@@ -222,7 +224,7 @@ QString formatBitcoinURI(const SendCoinsRecipient &info)
 bool isDust(const QString& address, qint64 amount)
 {
     CTxDestination dest = CBitcoinAddress(address.toStdString()).Get();
-    CScript script; script.SetDestination(dest);
+    CScript script = GetScriptForDestination(dest);
     CTxOut txOut(amount, script);
     return txOut.IsDust(::minRelayTxFee);
 }
