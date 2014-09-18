a49f11d9edf2f7dbce154ca370494a8f86e013d0
Wladimir J. van der Laan <laanwj@gmail.com>
2014-09-18 13:14:38 +0200

    qt: Change splash screen to normal window
    Makes it possible to move, minimize, unminimize the window while
    Bitcoin Core is initializing.

--- src/qt/splashscreen.h
+++ src/qt/splashscreen.h
@@ -9,23 +9,34 @@
 
 /** class for the splashscreen with information of the running client
  */
-class SplashScreen : public QSplashScreen
+class SplashScreen : public QWidget
 {
     Q_OBJECT
 
 public:
-    explicit SplashScreen(const QPixmap &pixmap, Qt::WindowFlags f, bool isTestNet);
+    explicit SplashScreen(Qt::WindowFlags f, bool isTestNet);
     ~SplashScreen();
+
+protected:
+     void paintEvent(QPaintEvent *event);
 
 public slots:
     /** Slot to call finish() method as it's not defined as slot */
     void slotFinish(QWidget *mainWin);
+
+    /** Show message and progress */
+    void showMessage(const QString &message, int alignment, const QColor &color);
 
 private:
     /** Connect core signals to splash screen */
     void subscribeToCoreSignals();
     /** Disconnect core signals to splash screen */
     void unsubscribeFromCoreSignals();
+
+    QPixmap pixmap;
+    QString curMessage;
+    QColor curColor;
+    int curAlignment;
 };
 
 #endif // SPLASHSCREEN_H
