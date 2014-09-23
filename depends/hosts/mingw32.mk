cf6190e1d40d1f91afa9bd2ec01e45c2aa26b51c
Cory Fields <cory-nospam-@coryfields.com>
2014-09-23 17:00:31 -0400

    depends: add debug/release flags for linux/osx/win
    Linux and mingw enable libstdc++ debugging for extra runtime checks. OSX
    doesn't play nice, so don't enable it there.

--- depends/hosts/mingw32.mk
+++ depends/hosts/mingw32.mk
@@ -1,2 +1,10 @@
-mingw32_CFLAGS=-pipe -O2
+mingw32_CFLAGS=-pipe
 mingw32_CXXFLAGS=$(mingw32_CFLAGS)
+
+mingw32_release_CFLAGS=-O2
+mingw32_release_CXXFLAGS=$(mingw32_release_CFLAGS)
+
+mingw32_debug_CFLAGS=-O1
+mingw32_debug_CXXFLAGS=$(mingw32_debug_CFLAGS)
+
+mingw32_debug_CPPFLAGS=-D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC
