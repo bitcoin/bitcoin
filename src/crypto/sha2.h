cf42c36e9963f9cf13704d8d74a9546f0db33d40
Pieter Wuille <pieter.wuille@gmail.com>
2014-09-25 08:23:32 +0200

    Apply clang-format on crypto/* and compat/*

--- src/crypto/sha2.h
+++ src/crypto/sha2.h
@@ -9,7 +9,8 @@
 #include <stdlib.h>
 
 /** A hasher class for SHA-256. */
-class CSHA256 {
+class CSHA256
+{
 private:
     uint32_t s[8];
     unsigned char buf[64];
@@ -19,13 +20,14 @@ public:
     static const size_t OUTPUT_SIZE = 32;
 
     CSHA256();
-    CSHA256& Write(const unsigned char *data, size_t len);
+    CSHA256& Write(const unsigned char* data, size_t len);
     void Finalize(unsigned char hash[OUTPUT_SIZE]);
     CSHA256& Reset();
 };
 
 /** A hasher class for SHA-512. */
-class CSHA512 {
+class CSHA512
+{
 private:
     uint64_t s[8];
     unsigned char buf[128];
@@ -35,13 +37,14 @@ public:
     static const size_t OUTPUT_SIZE = 64;
 
     CSHA512();
-    CSHA512& Write(const unsigned char *data, size_t len);
+    CSHA512& Write(const unsigned char* data, size_t len);
     void Finalize(unsigned char hash[OUTPUT_SIZE]);
     CSHA512& Reset();
 };
 
 /** A hasher class for HMAC-SHA-512. */
-class CHMAC_SHA512 {
+class CHMAC_SHA512
+{
 private:
     CSHA512 outer;
     CSHA512 inner;
@@ -49,8 +52,9 @@ private:
 public:
     static const size_t OUTPUT_SIZE = 64;
 
-    CHMAC_SHA512(const unsigned char *key, size_t keylen);
-    CHMAC_SHA512& Write(const unsigned char *data, size_t len) {
+    CHMAC_SHA512(const unsigned char* key, size_t keylen);
+    CHMAC_SHA512& Write(const unsigned char* data, size_t len)
+    {
         inner.Write(data, len);
         return *this;
     }
