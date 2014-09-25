f35ee0fa894fda8b75ac5b4871a2ca96db0789f2
Pieter Wuille <pieter.wuille@gmail.com>
2014-09-25 08:23:32 +0200

    Apply clang-format on crypto/* and compat/*

--- src/compat/glibc_sanity.cpp
+++ src/compat/glibc_sanity.cpp
@@ -14,10 +14,11 @@
 extern "C" void* memcpy(void* a, const void* b, size_t c);
 void* memcpy_int(void* a, const void* b, size_t c)
 {
-    return memcpy(a,b,c);
+    return memcpy(a, b, c);
 }
 
-namespace {
+namespace
+{
 // trigger: Use the memcpy_int wrapper which calls our internal memcpy.
 //   A direct call to memcpy may be optimized away by the compiler.
 // test: Fill an array with a sequence of integers. memcpy to a new empty array.
@@ -31,11 +32,10 @@ bool sanity_test_memcpy()
     for (unsigned int i = 0; i != T; ++i)
         memcpy_test[i] = i;
 
-    memcpy_int(memcpy_verify,memcpy_test,sizeof(memcpy_test));
+    memcpy_int(memcpy_verify, memcpy_test, sizeof(memcpy_test));
 
-    for (unsigned int i = 0; i != T; ++i)
-    {
-        if(memcpy_verify[i] != i)
+    for (unsigned int i = 0; i != T; ++i) {
+        if (memcpy_verify[i] != i)
             return false;
     }
     return true;
@@ -51,7 +51,7 @@ bool sanity_test_fdelt()
     fd_set fds;
     FD_ZERO(&fds);
     FD_SET(0, &fds);
-    return FD_ISSET(0,&fds);
+    return FD_ISSET(0, &fds);
 }
 #endif
 
