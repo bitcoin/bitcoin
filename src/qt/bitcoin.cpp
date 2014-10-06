58943fa952d436e3a885aaba6f60a296b2792176
whitj00 <whitj00@ymail.com>
2014-10-05 22:30:07 -0700

    Makefile

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -61,6 +61,8 @@ Q_IMPORT_PLUGIN(AccessibleFactory)
 Q_IMPORT_PLUGIN(QXcbIntegrationPlugin);
 #elif defined(QT_QPA_PLATFORM_WINDOWS)
 Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin);
+Q_IMPORT_PLUGIN(QMinimalIntegrationPlugin);
+Q_IMPORT_PLUGIN(QWindowsPrinterSupportPlugin);
 #elif defined(QT_QPA_PLATFORM_COCOA)
 Q_IMPORT_PLUGIN(QCocoaIntegrationPlugin);
 #endif
