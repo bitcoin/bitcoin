9b106a1d02b9f3444c7e4d9b106d05484e5e565b
whitj00 <whitj00@ymail.com>
2014-10-06 18:39:34 -0700

    Double Declaration Fix

--- src/util.h
+++ src/util.h
@@ -38,8 +38,6 @@ extern bool fLogTimestamps;
 extern bool fLogIPs;
 extern volatile bool fReopenDebugLog;
 
-void RandAddSeed();
-void RandAddSeedPerfmon();
 void SetupEnvironment();
 
 /* Return true if log accepts specified category */
