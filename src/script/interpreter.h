4a917f4fcfb4e827ce4ee5f315c328c7caff6b8a
Pieter Wuille <pieter.wuille@gmail.com>
2014-10-08 18:48:59 -0700

    Add SCRIPT_VERIFY_MINIMALDATA (BIP62 rules 3 and 4)
    Also use the new flag as a standard rule, and replace the IsCanonicalPush
    standardness check with it (as it is more complete).

--- src/script/interpreter.h
+++ src/script/interpreter.h
@@ -49,6 +49,13 @@ enum
 
     // Using a non-push operator in the scriptSig causes script failure (softfork safe, BIP62 rule 2).
     SCRIPT_VERIFY_SIGPUSHONLY = (1U << 5),
+
+    // Require minimal encodings for all push operations (OP_0... OP_16, OP_1NEGATE where possible, direct
+    // pushes up to 75 bytes, OP_PUSHDATA up to 255 bytes, OP_PUSHDATA2 for anything larger). Evaluating
+    // any other push causes the script to fail (BIP62 rule 3).
+    // In addition, whenever a stack element is interpreted as a number, it must be of minimal length (BIP62 rule 4).
+    // (softfork safe)
+    SCRIPT_VERIFY_MINIMALDATA = (1U << 6)
 };
 
 uint256 SignatureHash(const CScript &scriptCode, const CTransaction& txTo, unsigned int nIn, int nHashType);
