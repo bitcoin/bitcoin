03aafd8cd5a246af9abf6156c29b74d92ffcb212
Luke Dashjr <luke-jr+git@utopios.org>
2014-10-16 03:50:33 +0000

    CreateNewBlock and miner_tests: Check generated template is valid by ProcessBlockChecks (and therefore also AcceptBlockChecks)

--- src/miner.cpp
+++ src/miner.cpp
@@ -323,8 +323,18 @@ CBlockTemplate* CreateNewBlock(const CScript& scriptPubKeyIn)
         indexDummy.nHeight = pindexPrev->nHeight + 1;
         CCoinsViewCache viewNew(pcoinsTip);
         CValidationState state;
+        // NOTE: CheckBlockHeader is called by CheckBlock
+        // NOTE: CtxCheckBlockHeader only assigns ppindex, which we don't need/want
+        if (!CtxCheckBlockHeader(*pblock, state, NULL))
+            throw std::runtime_error("CreateNewBlock() : CtxCheckBlockHeader failed");
+        if (!CheckBlock(*pblock, state, false, false))
+            throw std::runtime_error("CreateNewBlock() : CheckBlock failed");
+        if (!CtxCheckBlock(*pblock, state, pindexPrev))
+            throw std::runtime_error("CreateNewBlock() : CtxCheckBlock failed");
         if (!ConnectBlock(*pblock, state, &indexDummy, viewNew, true))
             throw std::runtime_error("CreateNewBlock() : ConnectBlock failed");
+        if (!(state.IsValid() && state.IsConclusive()))
+            throw std::runtime_error("CreateNewBlock() : State is not conclusively valid");
     }
 
     return pblocktemplate.release();
