a419d09fcb41ac99054b1f9eb40f9b9026beca4b
Eric Lombrozo <elombrozo@gmail.com>
2014-10-21 23:20:50 -0400

    Allow recursive locking in getmininginfo.

--- src/rpcmining.cpp
+++ src/rpcmining.cpp
@@ -235,25 +235,19 @@ Value getmininginfo(const Array& params, bool fHelp)
         );
 
 
+    LOCK(cs_main);
+
     Object obj;
-    {
-        LOCK(cs_main);
-        obj.push_back(Pair("blocks",           (int)chainActive.Height()));
-        obj.push_back(Pair("currentblocksize", (uint64_t)nLastBlockSize));
-        obj.push_back(Pair("currentblocktx",   (uint64_t)nLastBlockTx));
-        obj.push_back(Pair("difficulty",       (double)GetDifficulty()));
-        obj.push_back(Pair("errors",           GetWarnings("statusbar")));
-        obj.push_back(Pair("genproclimit",     (int)GetArg("-genproclimit", -1)));
-    }
-
+    obj.push_back(Pair("blocks",           (int)chainActive.Height()));
+    obj.push_back(Pair("currentblocksize", (uint64_t)nLastBlockSize));
+    obj.push_back(Pair("currentblocktx",   (uint64_t)nLastBlockTx));
+    obj.push_back(Pair("difficulty",       (double)GetDifficulty()));
+    obj.push_back(Pair("errors",           GetWarnings("statusbar")));
+    obj.push_back(Pair("genproclimit",     (int)GetArg("-genproclimit", -1)));
     obj.push_back(Pair("networkhashps",    getnetworkhashps(params, false)));
-
-    {
-        LOCK(cs_main);
-        obj.push_back(Pair("pooledtx",         (uint64_t)mempool.size()));
-        obj.push_back(Pair("testnet",          Params().TestnetToBeDeprecatedFieldRPC()));
-        obj.push_back(Pair("chain",            Params().NetworkIDString()));
-    }
+    obj.push_back(Pair("pooledtx",         (uint64_t)mempool.size()));
+    obj.push_back(Pair("testnet",          Params().TestnetToBeDeprecatedFieldRPC()));
+    obj.push_back(Pair("chain",            Params().NetworkIDString()));
 #ifdef ENABLE_WALLET
     obj.push_back(Pair("generate",         getgenerate(params, false)));
     obj.push_back(Pair("hashespersec",     gethashespersec(params, false)));
