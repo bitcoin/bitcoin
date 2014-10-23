214091d584506f23b1c02e9c8a280d5b04791d7f
Michael Ford <fanquake@gmail.com>
2014-10-23 09:48:19 +0800

    Update license in pull-tester and rpc-tests
    Add missing copyright/license header where necessary

--- qa/rpc-tests/smartfees.py
+++ qa/rpc-tests/smartfees.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+# Copyright (c) 2014 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #
 # Test fee estimation code
