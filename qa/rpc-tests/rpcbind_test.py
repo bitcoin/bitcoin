387cfa48af2b70196cf574b2cda7602bcac2eda4
Michael Ford <fanquake@gmail.com>
2014-10-23 09:48:19 +0800

    Update license in pull-tester and rpc-tests
    Add missing copyright/license header where necessary

--- qa/rpc-tests/rpcbind_test.py
+++ qa/rpc-tests/rpcbind_test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # Copyright (c) 2014 The Bitcoin Core developers
-# Distributed under the MIT/X11 software license, see the accompanying
+# Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 # Test for -rpcbind, as well as -rpcallowip and -rpcconnect
