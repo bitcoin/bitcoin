43e4206513589611ee3908e0ca33f1c756bf7aa7
Michael Ford <fanquake@gmail.com>
2014-10-26 16:32:04 +0800

    Update comments in client version to be doxygen compatible

--- src/clientversion.h
+++ src/clientversion.h
@@ -1,5 +1,5 @@
 // Copyright (c) 2009-2014 The Bitcoin developers
-// Distributed under the MIT/X11 software license, see the accompanying
+// Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #ifndef CLIENTVERSION_H
@@ -8,38 +8,43 @@
 #if defined(HAVE_CONFIG_H)
 #include "config/bitcoin-config.h"
 #else
-//
-// client versioning and copyright year
-//
 
-// These need to be macros, as version.cpp's and bitcoin*-res.rc's voodoo requires it
+/**
+ * client versioning and copyright year
+ */
+
+//! These need to be macros, as clientversion.cpp's and bitcoin*-res.rc's voodoo requires it
 #define CLIENT_VERSION_MAJOR 0
 #define CLIENT_VERSION_MINOR 9
 #define CLIENT_VERSION_REVISION 99
 #define CLIENT_VERSION_BUILD 0
 
-// Set to true for release, false for prerelease or test build
+//! Set to true for release, false for prerelease or test build
 #define CLIENT_VERSION_IS_RELEASE false
 
-// Copyright year (2009-this)
-// Todo: update this when changing our copyright comments in the source
+/**
+ * Copyright year (2009-this)
+ * Todo: update this when changing our copyright comments in the source
+ */
 #define COPYRIGHT_YEAR 2014
 
 #endif //HAVE_CONFIG_H
 
-// Converts the parameter X to a string after macro replacement on X has been performed.
-// Don't merge these into one macro!
+/**
+ * Converts the parameter X to a string after macro replacement on X has been performed.
+ * Don't merge these into one macro!
+ */
 #define STRINGIZE(X) DO_STRINGIZE(X)
 #define DO_STRINGIZE(X) #X
 
-// Copyright string used in Windows .rc files
+//! Copyright string used in Windows .rc files
 #define COPYRIGHT_STR "2009-" STRINGIZE(COPYRIGHT_YEAR) " The Bitcoin Core Developers"
 
-/*
-  bitcoind-res.rc includes this file, but it cannot cope with real c++ code.
-  WINDRES_PREPROC is defined to indicate that its pre-processor is running.
-  Anything other than a define should be guarded below.
-*/
+/**
+ * bitcoind-res.rc includes this file, but it cannot cope with real c++ code.
+ * WINDRES_PREPROC is defined to indicate that its pre-processor is running.
+ * Anything other than a define should be guarded below.
+ */
 
 #if !defined(WINDRES_PREPROC)
 
