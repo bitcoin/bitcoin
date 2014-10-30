3a51422980dd2f271f213c39a76013374574b23a
Matt Corallo <git@bluematt.me>
2014-10-29 17:01:18 -0700

    Fix -loadblock after shutdown during IBD

--- src/main.cpp
+++ src/main.cpp
@@ -3136,7 +3136,7 @@ bool LoadExternalBlockFile(FILE* fileIn, CDiskBlockPos *dbp)
                 }
 
                 // process in case the block isn't known yet
-                if (mapBlockIndex.count(hash) == 0) {
+                if (mapBlockIndex.count(hash) == 0 || (mapBlockIndex[hash]->nStatus & BLOCK_HAVE_DATA) == 0) {
                     CValidationState state;
                     if (ProcessBlock(state, NULL, &block, dbp))
                         nLoaded++;
