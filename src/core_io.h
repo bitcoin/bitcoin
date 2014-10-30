0da7d1709054df4e1a13f58c96a32f9cb03bd9f5
Luke Dashjr <luke-jr+git@utopios.org>
2014-10-30 02:56:33 +0000

    Abstract DecodeHexBlk and BIP22ValidationResult functions out of submitblock

--- src/core_io.h
+++ src/core_io.h
@@ -8,6 +8,7 @@
 #include <string>
 #include <vector>
 
+class CBlock;
 class CScript;
 class CTransaction;
 class uint256;
@@ -16,6 +17,7 @@ class UniValue;
 // core_read.cpp
 extern CScript ParseScript(std::string s);
 extern bool DecodeHexTx(CTransaction& tx, const std::string& strHexTx);
+extern bool DecodeHexBlk(CBlock&, const std::string& strHexBlk);
 extern uint256 ParseHashUV(const UniValue& v, const std::string& strName);
 extern std::vector<unsigned char> ParseHexUV(const UniValue& v, const std::string& strName);
 
