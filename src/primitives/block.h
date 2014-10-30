afd4b94b6dabab29dee5a12966ce217700381682
Matt Corallo <git@bluematt.me>
2014-10-30 15:50:15 -0700

    Move CMerkleBlock and CPartialMerkleTree to their own file

--- src/primitives/block.h
+++ src/primitives/block.h
@@ -10,6 +10,9 @@
 #include "serialize.h"
 #include "uint256.h"
 
+/** The maximum allowed size for a serialized block, in bytes (network rule) */
+static const unsigned int MAX_BLOCK_SIZE = 1000000;
+
 /** Nodes collect new transactions into a block, hash them into a hash tree,
  * and scan through nonce values to make the block's hash satisfy proof-of-work
  * requirements.  When they solve the proof-of-work, they broadcast the block
