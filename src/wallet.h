ed3e5e468c5b7973d71efe51c90c3a301d570e27
Cozz Lovan <cozzlovan@yahoo.com>
2014-11-02 00:47:39 +0100

    [Wallet] Add global boolean whether to pay at least the custom fee (default=true)

--- src/wallet.h
+++ src/wallet.h
@@ -33,6 +33,7 @@ extern CFeeRate payTxFee;
 extern unsigned int nTxConfirmTarget;
 extern bool bSpendZeroConfChange;
 extern bool fSendFreeTransactions;
+extern bool fPayAtLeastCustomFee;
 
 //! -paytxfee default
 static const CAmount DEFAULT_TRANSACTION_FEE = 0;
