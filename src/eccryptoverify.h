4d86c0310b15f26247be65ee135d61af5d6f96c2
Philip Kaufmann <phil.kaufmann@t-online.de>
2014-11-04 14:34:04 +0100

    minor code style cleanup after recent merges
    - add a missing license header
    - correct some header orderings etc.

--- src/eccryptoverify.h
+++ src/eccryptoverify.h
@@ -1,13 +1,14 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2013 The Bitcoin developers
-// Distributed under the MIT/X11 software license, see the accompanying
+// Copyright (c) 2009-2014 The Bitcoin developers
+// Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_EC_CRYPTO_VERIFY_H
-#define BITCOIN_EC_CRYPTO_VERIFY_H
+#ifndef BITCOIN_ECCRYPTOVERIFY_H
+#define BITCOIN_ECCRYPTOVERIFY_H
 
 #include <vector>
 #include <cstdlib>
+
 class uint256;
 
 namespace eccrypto {
@@ -16,4 +17,5 @@ bool Check(const unsigned char *vch);
 bool CheckSignatureElement(const unsigned char *vch, int len, bool half);
 
 } // eccrypto namespace
-#endif
+
+#endif // BITCOIN_ECCRYPTOVERIFY_H
