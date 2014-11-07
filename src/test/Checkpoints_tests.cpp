6ef3c61739046e3f7220598fcccaf67b0ac8168b
sandakersmann <sandakersmann@users.noreply.github.com>
2014-11-07 23:51:15 +0100

    Block chain corrected to blockchain
    Block chain corrected to blockchain

--- src/test/Checkpoints_tests.cpp
+++ src/test/Checkpoints_tests.cpp
@@ -3,7 +3,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 //
-// Unit tests for block-chain checkpoints
+// Unit tests for blockchain checkpoints
 //
 
 #include "checkpoints.h"
