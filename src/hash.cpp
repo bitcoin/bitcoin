56f9b300f69237e8cc291b3a29c5f362c2c0c1b7
sinetek <pitwuu@gmail.com>
2014-11-08 15:38:11 -0500

    Adding license.

--- src/hash.cpp
+++ src/hash.cpp
@@ -1,3 +1,7 @@
+// Copyright (c) 2013-2014 The Bitcoin developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #include "hash.h"
 
 inline uint32_t ROTL32(uint32_t x, int8_t r)
