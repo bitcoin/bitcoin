1129bed867804092d237aea5245c4bffd4f123d2
Matt Corallo <git@bluematt.me>
2014-11-11 22:50:02 -0800

    New pull tester which adds a rather simple large-reorg test

--- depends/packages/native_comparisontool.mk
+++ depends/packages/native_comparisontool.mk
@@ -1,8 +1,8 @@
 package=native_comparisontool
-$(package)_version=0f7b5d8
-$(package)_download_path=https://github.com/TheBlueMatt/test-scripts/raw/38b490a2599d422b12d5ce8f165792f63fd8f54f
+$(package)_version=f56eec3
+$(package)_download_path=https://github.com/TheBlueMatt/test-scripts/raw/89dbae28f04eae31cb05a4011d9b810440956b68
 $(package)_file_name=pull-tests-$($(package)_version).jar
-$(package)_sha256_hash=ecd43b988a8b673b483e4f69f931596360a5e90fc415c75c4c259faa690df198
+$(package)_sha256_hash=e3a5a07aa72f2706235a9ca4c68951b7983448db2f13059a6ef1da38245dae94
 $(package)_install_dirname=BitcoindComparisonTool_jar
 $(package)_install_filename=BitcoindComparisonTool.jar
 
