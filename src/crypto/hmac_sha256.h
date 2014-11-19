9d8604f36a6f94b3c065a8143952cadd7d97aef5
Pieter Wuille <pieter.wuille@gmail.com>
2014-11-19 22:17:58 +0100

    Header define style cleanups

--- src/crypto/hmac_sha256.h
+++ src/crypto/hmac_sha256.h
@@ -2,8 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_HMAC_SHA256_H
-#define BITCOIN_HMAC_SHA256_H
+#ifndef BITCOIN_CRYPTO_HMAC_SHA256_H
+#define BITCOIN_CRYPTO_HMAC_SHA256_H
 
 #include "crypto/sha256.h"
 
@@ -29,4 +29,4 @@ public:
     void Finalize(unsigned char hash[OUTPUT_SIZE]);
 };
 
-#endif // BITCOIN_SHA256_H
+#endif // BITCOIN_CRYPTO_HMAC_SHA256_H
--- src/crypto/hmac_sha512.h
+++ src/crypto/hmac_sha512.h
@@ -2,8 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_HMAC_SHA512_H
-#define BITCOIN_HMAC_SHA512_H
+#ifndef BITCOIN_CRYPTO_HMAC_SHA512_H
+#define BITCOIN_CRYPTO_HMAC_SHA512_H
 
 #include "crypto/sha512.h"
 
@@ -29,4 +29,4 @@ public:
     void Finalize(unsigned char hash[OUTPUT_SIZE]);
 };
 
-#endif // BITCOIN_HMAC_SHA512_H
+#endif // BITCOIN_CRYPTO_HMAC_SHA512_H
