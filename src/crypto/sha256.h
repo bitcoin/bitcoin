4a3e81eedd01639bcb436c453a58d2de6ddd9959
Pieter Wuille <pieter.wuille@gmail.com>
2014-11-19 22:17:58 +0100

    Header define style cleanups

--- src/crypto/sha256.h
+++ src/crypto/sha256.h
@@ -2,8 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_SHA256_H
-#define BITCOIN_SHA256_H
+#ifndef BITCOIN_CRYPTO_SHA256_H
+#define BITCOIN_CRYPTO_SHA256_H
 
 #include <stdint.h>
 #include <stdlib.h>
@@ -25,4 +25,4 @@ public:
     CSHA256& Reset();
 };
 
-#endif // BITCOIN_SHA256_H
+#endif // BITCOIN_CRYPTO_SHA256_H
--- src/crypto/sha512.h
+++ src/crypto/sha512.h
@@ -2,8 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_SHA512_H
-#define BITCOIN_SHA512_H
+#ifndef BITCOIN_CRYPTO_SHA512_H
+#define BITCOIN_CRYPTO_SHA512_H
 
 #include <stdint.h>
 #include <stdlib.h>
@@ -25,4 +25,4 @@ public:
     CSHA512& Reset();
 };
 
-#endif // BITCOIN_SHA512_H
+#endif // BITCOIN_CRYPTO_SHA512_H
