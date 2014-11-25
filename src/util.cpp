97c7f2933a588e258a1c39f6d57efb79c8acca9a
Michael Ford <fanquake@gmail.com>
2014-11-25 16:12:55 +0800

    No longer check osx compatibility in RenameThread
    10.5 support has been dropped for some time now.

--- src/util.cpp
+++ src/util.cpp
@@ -698,13 +698,8 @@ void RenameThread(const char* name)
     //       removed.
     pthread_set_name_np(pthread_self(), name);
 
-#elif defined(MAC_OSX) && defined(__MAC_OS_X_VERSION_MAX_ALLOWED)
-
-// pthread_setname_np is XCode 10.6-and-later
-#if __MAC_OS_X_VERSION_MAX_ALLOWED >= 1060
+#elif defined(MAC_OSX)
     pthread_setname_np(name);
-#endif
-
 #else
     // Prevent warnings for unused parameters...
     (void)name;
