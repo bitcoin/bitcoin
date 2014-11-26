592883a1338dba19a9176b0fc26b158d240cb2d1
Pavel Janík <Pavel@Janik.cz>
2014-11-26 08:19:07 +0100

    Include missing config/bitcoin-config.h.

--- src/compat.h
+++ src/compat.h
@@ -6,6 +6,10 @@
 #ifndef BITCOIN_COMPAT_H
 #define BITCOIN_COMPAT_H
 
+#if defined(HAVE_CONFIG_H)
+#include "config/bitcoin-config.h"
+#endif
+
 #ifdef WIN32
 #ifdef _WIN32_WINNT
 #undef _WIN32_WINNT
