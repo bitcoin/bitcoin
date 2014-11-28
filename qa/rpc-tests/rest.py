49681a2cdd78bc6634d2a5e1e40c06f013ed2f31
Jonas Schnelli <jonas.schnelli@include7.ch>
2014-11-28 20:32:52 +0100

    [REST] /rest/block response with full tx details
    - rest block request returns full unfolded tx details
    - /rest/block/notxdetails/<HASH> returns block where transactions are only represented by its hash

--- qa/rpc-tests/rest.py
+++ qa/rpc-tests/rest.py
@@ -48,7 +48,7 @@ class RESTTest (BitcoinTestFramework):
         assert_equal(json_obj['hash'], bb_hash)
         
         # do tx test
-        tx_hash = json_obj['tx'][0];
+        tx_hash = json_obj['tx'][0]['txid'];
         json_string = http_get_call(url.hostname, url.port, '/rest/tx/'+tx_hash+self.FORMAT_SEPARATOR+"json")
         json_obj = json.loads(json_string)
         assert_equal(json_obj['txid'], tx_hash)
@@ -57,6 +57,33 @@ class RESTTest (BitcoinTestFramework):
         hex_string = http_get_call(url.hostname, url.port, '/rest/tx/'+tx_hash+self.FORMAT_SEPARATOR+"hex", True)
         assert_equal(response.status, 200)
         assert_greater_than(int(response.getheader('content-length')), 10)
+        
+        # check block tx details
+        # let's make 3 tx and mine them on node 1
+        txs = []
+        txs.append(self.nodes[0].sendtoaddress(self.nodes[2].getnewaddress(), 11))
+        txs.append(self.nodes[0].sendtoaddress(self.nodes[2].getnewaddress(), 11))
+        txs.append(self.nodes[0].sendtoaddress(self.nodes[2].getnewaddress(), 11))
+        self.sync_all()
+        
+        # now mine the transactions
+        newblockhash = self.nodes[1].setgenerate(True, 1)
+        self.sync_all()
+        
+        #check if the 3 tx show up in the new block
+        json_string = http_get_call(url.hostname, url.port, '/rest/block/'+newblockhash[0]+self.FORMAT_SEPARATOR+'json')
+        json_obj = json.loads(json_string)
+        for tx in json_obj['tx']:
+            if not 'coinbase' in tx['vin'][0]: #exclude coinbase
+                assert_equal(tx['txid'] in txs, True)
+        
+        #check the same but without tx details
+        json_string = http_get_call(url.hostname, url.port, '/rest/block/notxdetails/'+newblockhash[0]+self.FORMAT_SEPARATOR+'json')
+        json_obj = json.loads(json_string)
+        for tx in txs:
+            assert_equal(tx in json_obj['tx'], True)
+                
+        
 
 if __name__ == '__main__':
     RESTTest ().main ()
--- src/rest.cpp
+++ src/rest.cpp
@@ -42,7 +42,7 @@ public:
 };
 
 extern void TxToJSON(const CTransaction& tx, const uint256 hashBlock, Object& entry);
-extern Object blockToJSON(const CBlock& block, const CBlockIndex* blockindex);
+extern Object blockToJSON(const CBlock& block, const CBlockIndex* blockindex, bool txDetails = false);
 
 static RestErr RESTERR(enum HTTPStatusCode status, string message)
 {
@@ -92,7 +92,8 @@ static bool ParseHashStr(const string& strReq, uint256& v)
 static bool rest_block(AcceptedConnection* conn,
                        string& strReq,
                        map<string, string>& mapHeaders,
-                       bool fRun)
+                       bool fRun,
+                       bool showTxDetails)
 {
     vector<string> params;
     enum RetFormat rf = ParseDataFormat(params, strReq);
@@ -131,7 +132,7 @@ static bool rest_block(AcceptedConnection* conn,
     }
 
     case RF_JSON: {
-        Object objBlock = blockToJSON(block, pblockindex);
+        Object objBlock = blockToJSON(block, pblockindex, showTxDetails);
         string strJSON = write_string(Value(objBlock), false) + "\n";
         conn->stream() << HTTPReply(HTTP_OK, strJSON, fRun) << std::flush;
         return true;
@@ -144,6 +145,22 @@ static bool rest_block(AcceptedConnection* conn,
 
     // not reached
     return true; // continue to process further HTTP reqs on this cxn
+}
+
+static bool rest_block_extended(AcceptedConnection* conn,
+                       string& strReq,
+                       map<string, string>& mapHeaders,
+                       bool fRun)
+{
+    return rest_block(conn, strReq, mapHeaders, fRun, true);
+}
+
+static bool rest_block_notxdetails(AcceptedConnection* conn,
+                       string& strReq,
+                       map<string, string>& mapHeaders,
+                       bool fRun)
+{
+    return rest_block(conn, strReq, mapHeaders, fRun, false);
 }
 
 static bool rest_tx(AcceptedConnection* conn,
@@ -205,7 +222,8 @@ static const struct {
                     bool fRun);
 } uri_prefixes[] = {
       {"/rest/tx/", rest_tx},
-      {"/rest/block/", rest_block},
+      {"/rest/block/notxdetails/", rest_block_notxdetails},
+      {"/rest/block/", rest_block_extended},
 };
 
 bool HTTPReq_REST(AcceptedConnection* conn,
