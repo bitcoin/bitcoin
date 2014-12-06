9238ab1d9cbdf1a0b5bed3bafb237a8921e8085a
Matt Corallo <git@bluematt.me>
2014-12-05 16:02:27 -0800

    Fix missing python2 in rpc-tests

--- qa/rpc-tests/rest.py
+++ qa/rpc-tests/rest.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # Copyright (c) 2014 The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
