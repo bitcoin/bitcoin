ecae2acb06d44509425a9cdad38ed9a5bce15cbe
Pieter Wuille <pieter.wuille@gmail.com>
2014-12-11 01:58:25 +0100

    Squashed 'src/secp256k1/' changes from b0210a9..bccaf86
    bccaf86 Merge pull request #150
    2a53a47 Merge pull request #151
    5f5a31f Merge pull request #149
    3907277 Merge pull request #142
    a3e0611 Enable tests in x86 travis builds
    45da235 x86 builder
    8bb0e93 Merge pull request #155
    971fe81 build: fix openssl detection for cross builds
    f22d73e Explicitly access %0..%2 as 64-bit so we use the right registers for x32 ABI
    e66d4d6 Avoid the stack in assembly and use explicit registers
    cf7b2b4 Fix ECDSA message hashes to 32 bytes
    056ad31 Really compile with -O3 by default
    74ad63a Merge pull request #146
    9000458 Merge pull request #145
    1f46b00 build: fix __builtin_expect detection for clang
    aaba2e0 Merge pull request #136
    8a0775c Merge pull request #144
    ee1eaa7 Merge pull request #141
    c88e2b8 Compile with -O3 by default
    6558a26 Make the benchmarks print out stats
    000bdf6 Rename bench_verify to bench_recovery
    7c6fed2 Add a few more additional tests.
    992e03b travis: add clang to the test matrix
    b43b79a Merge pull request #143
    e06a924 Include time.h header for time().
    8d11164 Add some additional tests.
    3545627 Merge pull request #118
    6a9901e Merge pull request #137
    376b28b Merge pull request #128
    1728806 Merge pull request #138
    a5759c5 Check return value of malloc
    39bd94d Variable time normalize
    ad86bdf Merge pull request #140
    54b768c Another redundant secp256k1_fe_normalize
    69dcaab Merge pull request #139
    1c29f2e Remove redundant secp256k1_fe_normalize from secp256k1_gej_add_ge_var.
    2b9388b Remove unused secp256k1_fe_inv_all
    f461b76 Allocate precomputation arrays on the heap
    b2c9681 Make {mul,sqr}_inner use the same argument order as {mul,sqr}
    6793505 Convert YASM code into inline assembly
    f048615 Rewrite field assembly to match the C version
    3ce74b1 Tweak precomputed table size for G
    
    git-subtree-dir: src/secp256k1
    git-subtree-split: bccaf86caa9c44166e5a66600b742c516e03c3f0

--- src/bench_inv.c
+++ src/bench_inv.c
@@ -12,30 +12,41 @@
 #include "field_impl.h"
 #include "group_impl.h"
 #include "scalar_impl.h"
+#include "bench.h"
 
-int main(void) {
+typedef struct {
+    secp256k1_scalar_t base, x;
+} bench_inv_t;
+
+void bench_inv_setup(void* arg) {
+    bench_inv_t *data = (bench_inv_t*)arg;
+
     static const unsigned char init[32] = {
         0x02, 0x03, 0x05, 0x07, 0x0b, 0x0d, 0x11, 0x13,
         0x17, 0x1d, 0x1f, 0x25, 0x29, 0x2b, 0x2f, 0x35,
         0x3b, 0x3d, 0x43, 0x47, 0x49, 0x4f, 0x53, 0x59,
         0x61, 0x65, 0x67, 0x6b, 0x6d, 0x71, 0x7f, 0x83
     };
-    static const unsigned char fini[32] = {
-        0xba, 0x28, 0x58, 0xd8, 0xaa, 0x11, 0xd6, 0xf2,
-        0xfa, 0xce, 0x50, 0xb1, 0x67, 0x19, 0xb1, 0xa6,
-        0xe0, 0xaa, 0x84, 0x53, 0xf6, 0x80, 0xfc, 0x23,
-        0x88, 0x3c, 0xd6, 0x74, 0x9f, 0x27, 0x09, 0x03
-    };
-    secp256k1_ge_start();
-    secp256k1_scalar_t base, x;
-    secp256k1_scalar_set_b32(&base, init, NULL);
-    secp256k1_scalar_set_b32(&x, init, NULL);
-    for (int i=0; i<1000000; i++) {
-        secp256k1_scalar_inverse(&x, &x);
-        secp256k1_scalar_add(&x, &x, &base);
+
+    secp256k1_scalar_set_b32(&data->base, init, NULL);
+    secp256k1_scalar_set_b32(&data->x, init, NULL);
+}
+
+void bench_inv(void* arg) {
+    bench_inv_t *data = (bench_inv_t*)arg;
+
+    for (int i=0; i<20000; i++) {
+        secp256k1_scalar_inverse(&data->x, &data->x);
+        secp256k1_scalar_add(&data->x, &data->x, &data->base);
     }
-    unsigned char res[32];
-    secp256k1_scalar_get_b32(res, &x);
-    CHECK(memcmp(res, fini, 32) == 0);
+}
+
+int main(void) {
+    secp256k1_ge_start();
+
+    bench_inv_t data;
+    run_benchmark(bench_inv, bench_inv_setup, NULL, &data, 10, 20000);
+
+    secp256k1_ge_stop();
     return 0;
 }
--- src/field_5x52_impl.h
+++ src/field_5x52_impl.h
@@ -102,6 +102,50 @@ static void secp256k1_fe_normalize(secp256k1_fe_t *r) {
 #endif
 }
 
+static void secp256k1_fe_normalize_var(secp256k1_fe_t *r) {
+    uint64_t t0 = r->n[0], t1 = r->n[1], t2 = r->n[2], t3 = r->n[3], t4 = r->n[4];
+
+    /* Reduce t4 at the start so there will be at most a single carry from the first pass */
+    uint64_t x = t4 >> 48; t4 &= 0x0FFFFFFFFFFFFULL;
+    uint64_t m;
+
+    /* The first pass ensures the magnitude is 1, ... */
+    t0 += x * 0x1000003D1ULL;
+    t1 += (t0 >> 52); t0 &= 0xFFFFFFFFFFFFFULL;
+    t2 += (t1 >> 52); t1 &= 0xFFFFFFFFFFFFFULL; m = t1;
+    t3 += (t2 >> 52); t2 &= 0xFFFFFFFFFFFFFULL; m &= t2;
+    t4 += (t3 >> 52); t3 &= 0xFFFFFFFFFFFFFULL; m &= t3;
+
+    /* ... except for a possible carry at bit 48 of t4 (i.e. bit 256 of the field element) */
+    VERIFY_CHECK(t4 >> 49 == 0);
+
+    /* At most a single final reduction is needed; check if the value is >= the field characteristic */
+    x = (t4 >> 48) | ((t4 == 0x0FFFFFFFFFFFFULL) & (m == 0xFFFFFFFFFFFFFULL)
+        & (t0 >= 0xFFFFEFFFFFC2FULL));
+
+    if (x) {
+        t0 += 0x1000003D1ULL;
+        t1 += (t0 >> 52); t0 &= 0xFFFFFFFFFFFFFULL;
+        t2 += (t1 >> 52); t1 &= 0xFFFFFFFFFFFFFULL;
+        t3 += (t2 >> 52); t2 &= 0xFFFFFFFFFFFFFULL;
+        t4 += (t3 >> 52); t3 &= 0xFFFFFFFFFFFFFULL;
+
+        /* If t4 didn't carry to bit 48 already, then it should have after any final reduction */
+        VERIFY_CHECK(t4 >> 48 == x);
+
+        /* Mask off the possible multiple of 2^256 from the final reduction */
+        t4 &= 0x0FFFFFFFFFFFFULL;
+    }
+
+    r->n[0] = t0; r->n[1] = t1; r->n[2] = t2; r->n[3] = t3; r->n[4] = t4;
+
+#ifdef VERIFY
+    r->magnitude = 1;
+    r->normalized = 1;
+    secp256k1_fe_verify(r);
+#endif
+}
+
 SECP256K1_INLINE static void secp256k1_fe_set_int(secp256k1_fe_t *r, int a) {
     r->n[0] = a;
     r->n[1] = r->n[2] = r->n[3] = r->n[4] = 0;
@@ -255,7 +299,7 @@ static void secp256k1_fe_mul(secp256k1_fe_t *r, const secp256k1_fe_t *a, const s
     secp256k1_fe_verify(b);
     VERIFY_CHECK(r != b);
 #endif
-    secp256k1_fe_mul_inner(a->n, b->n, r->n);
+    secp256k1_fe_mul_inner(r->n, a->n, b->n);
 #ifdef VERIFY
     r->magnitude = 1;
     r->normalized = 0;
@@ -268,7 +312,7 @@ static void secp256k1_fe_sqr(secp256k1_fe_t *r, const secp256k1_fe_t *a) {
     VERIFY_CHECK(a->magnitude <= 8);
     secp256k1_fe_verify(a);
 #endif
-    secp256k1_fe_sqr_inner(a->n, r->n);
+    secp256k1_fe_sqr_inner(r->n, a->n);
 #ifdef VERIFY
     r->magnitude = 1;
     r->normalized = 0;
