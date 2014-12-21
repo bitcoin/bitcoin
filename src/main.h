bad0d88c991652999e650a33b3f080da3c61716e
Jameson Lopp <jameson.lopp@gmail.com>
2014-12-21 09:10:43 -0500

    remove max orphan blocks config parameter since it is no longer functional
    Rebased-From: 4bc95c43871645c1d69a0d719c13b38151a3a13f

--- src/main.h
+++ src/main.h
@@ -64,8 +64,6 @@ static const unsigned int MAX_P2SH_SIGOPS = 15;
 static const unsigned int MAX_TX_SIGOPS = MAX_BLOCK_SIGOPS/5;
 /** Default for -maxorphantx, maximum number of orphan transactions kept in memory */
 static const unsigned int DEFAULT_MAX_ORPHAN_TRANSACTIONS = 100;
-/** Default for -maxorphanblocks, maximum number of orphan blocks kept in memory */
-static const unsigned int DEFAULT_MAX_ORPHAN_BLOCKS = 750;
 /** The maximum size of a blk?????.dat file (since 0.8) */
 static const unsigned int MAX_BLOCKFILE_SIZE = 0x8000000; // 128 MiB
 /** The pre-allocation chunk size for blk?????.dat files (since 0.8) */
