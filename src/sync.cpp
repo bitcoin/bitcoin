edf758dd1226a6d222757abe8e5b4ea3a7f437b3
Matt Corallo <git@bluematt.me>
2014-12-22 21:35:52 -0500

    Remove broken+useless lock/unlock log prints

--- src/sync.cpp
+++ src/sync.cpp
@@ -86,7 +86,6 @@ static void push_lock(void* c, const CLockLocation& locklocation, bool fTry)
     if (lockstack.get() == NULL)
         lockstack.reset(new LockStack);
 
-    LogPrint("lock", "Locking: %s\n", locklocation.ToString());
     dd_mutex.lock();
 
     (*lockstack).push_back(std::make_pair(c, locklocation));
@@ -113,10 +112,6 @@ static void push_lock(void* c, const CLockLocation& locklocation, bool fTry)
 
 static void pop_lock()
 {
-    if (fDebug) {
-        const CLockLocation& locklocation = (*lockstack).rbegin()->second;
-        LogPrint("lock", "Unlocked: %s\n", locklocation.ToString());
-    }
     dd_mutex.lock();
     (*lockstack).pop_back();
     dd_mutex.unlock();
