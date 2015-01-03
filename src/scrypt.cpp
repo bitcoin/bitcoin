18239697ba3a846574e11c206b3176b1797aa9a8
MASM fan <masmfan@gmail.com>
2015-01-02 19:46:38 -0800

    Remove scrypt_nosalt function

--- src/scrypt.cpp
+++ src/scrypt.cpp
@@ -15,22 +15,17 @@ extern "C" void scrypt_core(unsigned int *X, unsigned int *V);
    r = 1, p = 1, N = 1024
  */
 
-uint256 scrypt_nosalt(const void* input, size_t inputlen, void *scratchpad)
-{
-    unsigned int *V;
-    unsigned int X[32];
-    uint256 result = 0;
-    V = (unsigned int *)(((uintptr_t)(scratchpad) + 63) & ~ (uintptr_t)(63));
-
-    PBKDF2_SHA256((const uint8_t*)input, inputlen, (const uint8_t*)input, inputlen, 1, (uint8_t *)X, 128);
-    scrypt_core(X, V);
-    PBKDF2_SHA256((const uint8_t*)input, inputlen, (uint8_t *)X, 128, 1, (uint8_t*)&result, 32);
-
-    return result;
-}
-
 uint256 scrypt_blockhash(const void* input)
 {
-    unsigned char scratchpad[SCRYPT_BUFFER_SIZE];
-    return scrypt_nosalt(input, 80, scratchpad);
+    uint8_t scratchpad[SCRYPT_BUFFER_SIZE];
+    uint32_t X[32];
+    uint256 result = 0;
+
+    uint32_t *V = (uint32_t *)(((uintptr_t)(scratchpad) + 63) & ~ (uintptr_t)(63));
+
+    PBKDF2_SHA256((const uint8_t*)input, 80, (const uint8_t*)input, 80, 1, (uint8_t *)X, 128);
+    scrypt_core(X, V);
+    PBKDF2_SHA256((const uint8_t*)input, 80, (uint8_t *)X, 128, 1, (uint8_t*)&result, 32);
+
+    return result;
 }
