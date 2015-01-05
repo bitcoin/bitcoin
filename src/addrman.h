9d6d0e5693f7615eadfca852605538cc08016b97
MarcoFalke <falke.marco@gmail.com>
2015-01-05 21:40:24 +0100

    Add missing copyright headers

--- src/addrman.h
+++ src/addrman.h
@@ -1,4 +1,5 @@
 // Copyright (c) 2012 Pieter Wuille
+// Copyright (c) 2012-2015 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
--- src/chainparamsseeds.h
+++ src/chainparamsseeds.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2014-2015 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #ifndef BITCOIN_CHAINPARAMSSEEDS_H
 #define BITCOIN_CHAINPARAMSSEEDS_H
 /**
