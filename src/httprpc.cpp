862354eeceec699baffa30fcff5c56c2b58ba680
MarcoFalke <falke.marco@gmail.com>
2015-01-05 21:40:24 +0100

    Add missing copyright headers

--- src/httprpc.cpp
+++ src/httprpc.cpp
@@ -1,3 +1,7 @@
+// Copyright (c) 2015 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #include "httprpc.h"
 
 #include "base58.h"
