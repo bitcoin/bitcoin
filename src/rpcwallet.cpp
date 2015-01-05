184658c3cb04390b0e755b0615837c6604d0ed87
Pavel Janík <Pavel@Janik.cz>
2015-01-05 21:44:55 +0100

    Fix typo - sentence starts with capital letter

--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -82,7 +82,7 @@ Value getnewaddress(const Array& params, bool fHelp)
             "If 'account' is specified (DEPRECATED), it is added to the address book \n"
             "so payments received with the address will be credited to 'account'.\n"
             "\nArguments:\n"
-            "1. \"account\"        (string, optional) DEPRECATED. The account name for the address to be linked to. if not provided, the default account \"\" is used. It can also be set to the empty string \"\" to represent the default account. The account does not need to exist, it will be created if there is no account by the given name.\n"
+            "1. \"account\"        (string, optional) DEPRECATED. The account name for the address to be linked to. If not provided, the default account \"\" is used. It can also be set to the empty string \"\" to represent the default account. The account does not need to exist, it will be created if there is no account by the given name.\n"
             "\nResult:\n"
             "\"bitcoinaddress\"    (string) The new bitcoin address\n"
             "\nExamples:\n"
