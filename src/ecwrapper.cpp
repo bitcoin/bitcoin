41b91586bfb3d9aad10c2d4afbfff555bb7c7cb6
Cory Fields <cory-nospam-@coryfields.com>
2015-01-09 22:06:08 -0500

    fail immediately on an empty signature
    Github-Pull: #5634
    Rebased-From: 8dccba6a45db0466370726ed462b9da2eae43bce

--- src/ecwrapper.cpp
+++ src/ecwrapper.cpp
@@ -117,6 +117,9 @@ bool CECKey::SetPubKey(const unsigned char* pubkey, size_t size) {
 }
 
 bool CECKey::Verify(const uint256 &hash, const std::vector<unsigned char>& vchSig) {
+    if (vchSig.empty())
+        return false;
+
     // New versions of OpenSSL will reject non-canonical DER signatures. de/re-serialize first.
     unsigned char *norm_der = NULL;
     ECDSA_SIG *norm_sig = ECDSA_SIG_new();
