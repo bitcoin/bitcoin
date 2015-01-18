ae9eae4c0a5db9b1844b9002ac3b756f05d3df14
Evan Duffield <evan@darkcoin.io>
2015-01-18 12:49:47 -0700

    corrected getblocktemplate coinbasevalue output

--- src/rpcmining.cpp
+++ src/rpcmining.cpp
@@ -581,7 +581,7 @@ Value getblocktemplate(const Array& params, bool fHelp)
     result.push_back(Pair("previousblockhash", pblock->hashPrevBlock.GetHex()));
     result.push_back(Pair("transactions", transactions));
     result.push_back(Pair("coinbaseaux", aux));
-    result.push_back(Pair("coinbasevalue", (int64_t)pblock->vtx[0].vout[0].nValue));
+    result.push_back(Pair("coinbasevalue", (int64_t)pblock->vtx[0].GetValueOut()));
     result.push_back(Pair("target", hashTarget.GetHex()));
     result.push_back(Pair("mintime", (int64_t)pindexPrev->GetMedianTimePast()+1));
     result.push_back(Pair("mutable", aMutable));
