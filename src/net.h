4f0ffb9c405112839b7cc299cedfce6774986807
Wladimir J. van der Laan <laanwj@gmail.com>
2015-01-20 18:02:45 +0100

    Delete RecvLine function
    No longer necessary since #5161 / c64b1ac8163f6ac69aaf9a2241e86a14a4f75f98.

--- src/net.h
+++ src/net.h
@@ -61,7 +61,6 @@ unsigned int ReceiveFloodSize();
 unsigned int SendBufferSize();
 
 void AddOneShot(std::string strDest);
-bool RecvLine(SOCKET hSocket, std::string& strLine);
 void AddressCurrentlyConnected(const CService& addr);
 CNode* FindNode(const CNetAddr& ip);
 CNode* FindNode(const std::string& addrName);
