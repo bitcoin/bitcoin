c45d6b9d03fa5c49a0827f6a235398166ff61ab6
fsb4000 <fsb4000@yandex.ru>
2015-01-23 16:08:44 +0600

    Исправление падения при нажатии пробела в CC

--- src/qt/coincontroltreewidget.cpp
+++ src/qt/coincontroltreewidget.cpp
@@ -13,7 +13,8 @@ void CoinControlTreeWidget::keyPressEvent(QKeyEvent *event)
     {
         event->ignore();
         int COLUMN_CHECKBOX = 0;
-        this->currentItem()->setCheckState(COLUMN_CHECKBOX, ((this->currentItem()->checkState(COLUMN_CHECKBOX) == Qt::Checked) ? Qt::Unchecked : Qt::Checked));
+        if(this->currentItem())
+            this->currentItem()->setCheckState(COLUMN_CHECKBOX, ((this->currentItem()->checkState(COLUMN_CHECKBOX) == Qt::Checked) ? Qt::Unchecked : Qt::Checked));
     }
     else if (event->key() == Qt::Key_Escape) // press esc -> close dialog
     {
