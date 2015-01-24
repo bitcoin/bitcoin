f339fff8f1b22a5ffb1aeee98b20771e22e57195
jtimon <jtimon@jtimon.cc>
2015-01-24 15:57:12 +0100

    Consensus: MOVEONLY: Move CValidationState from main consensus/validation

--- src/test/miner_tests.cpp
+++ src/test/miner_tests.cpp
@@ -2,6 +2,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#include "consensus/validation.h"
 #include "main.h"
 #include "miner.h"
 #include "pubkey.h"
