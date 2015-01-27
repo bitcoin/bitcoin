e6a5a8801624f797c7073a2ebb09170163da91e3
Evan Duffield <evan@darkcoin.io>
2015-01-27 14:52:31 -0700

    version bump / enable IX

--- src/clientversion.h
+++ src/clientversion.h
@@ -11,8 +11,8 @@
 // These need to be macros, as version.cpp's and darkcoin-qt.rc's voodoo requires it
 #define CLIENT_VERSION_MAJOR       0
 #define CLIENT_VERSION_MINOR       11
-#define CLIENT_VERSION_REVISION    0
-#define CLIENT_VERSION_BUILD       14
+#define CLIENT_VERSION_REVISION    1
+#define CLIENT_VERSION_BUILD       0
 
 
 
