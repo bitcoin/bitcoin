0f177393b8a4d8c10c1667419d2a90ea35ad7ead
vertoe <vertoe@qhor.net>
2015-02-06 15:03:29 +0100

    Update locale strings.

--- src/qt/bitcoinstrings.cpp
+++ src/qt/bitcoinstrings.cpp
@@ -44,6 +44,9 @@ QT_TRANSLATE_NOOP("darkcoin-core", ""
 "Continuously rate-limit free transactions to <n>*1000 bytes per minute "
 "(default:15)"),
 QT_TRANSLATE_NOOP("darkcoin-core", ""
+"Darksend uses exact denominated amounts to send funds, you might simply need "
+"to anonymize some more coins."),
+QT_TRANSLATE_NOOP("darkcoin-core", ""
 "Disable all Masternode and Darksend related functionality (0-1, default: 0)"),
 QT_TRANSLATE_NOOP("darkcoin-core", ""
 "Enable instantx, show confirmations for locked transactions (bool, default: "
@@ -93,6 +96,9 @@ QT_TRANSLATE_NOOP("darkcoin-core", ""
 "In this mode -genproclimit controls how many blocks are generated "
 "immediately."),
 QT_TRANSLATE_NOOP("darkcoin-core", ""
+"InstantX requires inputs with at least 6 confirmations, you might need to "
+"wait a few minutes and try again."),
+QT_TRANSLATE_NOOP("darkcoin-core", ""
 "Listen for JSON-RPC connections on <port> (default: 9998 or testnet: 19998)"),
 QT_TRANSLATE_NOOP("darkcoin-core", ""
 "Name to construct url for KeePass entry that stores the wallet passphrase"),
@@ -128,10 +134,12 @@ QT_TRANSLATE_NOOP("darkcoin-core", ""
 "Unable to bind to %s on this computer. Darkcoin Core is probably already "
 "running."),
 QT_TRANSLATE_NOOP("darkcoin-core", ""
-"Unable to locate enough Darksend non-denominated funds for this transaction "
-"that are not equal 1000 DRK"),
+"Unable to locate enough Darksend denominated funds for this transaction."),
 QT_TRANSLATE_NOOP("darkcoin-core", ""
-"Unable to locate enough Darksend non-denominated funds for this transaction"),
+"Unable to locate enough Darksend non-denominated funds for this transaction "
+"that are not equal 1000 DRK."),
+QT_TRANSLATE_NOOP("darkcoin-core", ""
+"Unable to locate enough Darksend non-denominated funds for this transaction."),
 QT_TRANSLATE_NOOP("darkcoin-core", ""
 "Use separate SOCKS5 proxy to reach peers via Tor hidden services (default: -"
 "proxy)"),
@@ -245,6 +253,7 @@ QT_TRANSLATE_NOOP("darkcoin-core", "Initialization sanity check failed. Darkcoin
 QT_TRANSLATE_NOOP("darkcoin-core", "Input is not valid."),
 QT_TRANSLATE_NOOP("darkcoin-core", "InstantX options:"),
 QT_TRANSLATE_NOOP("darkcoin-core", "Insufficient funds"),
+QT_TRANSLATE_NOOP("darkcoin-core", "Insufficient funds."),
 QT_TRANSLATE_NOOP("darkcoin-core", "Invalid -onion address: '%s'"),
 QT_TRANSLATE_NOOP("darkcoin-core", "Invalid -proxy address: '%s'"),
 QT_TRANSLATE_NOOP("darkcoin-core", "Invalid amount for -minrelaytxfee=<amount>: '%s'"),
@@ -346,7 +355,6 @@ QT_TRANSLATE_NOOP("darkcoin-core", "Transaction fees are too high."),
 QT_TRANSLATE_NOOP("darkcoin-core", "Transaction not valid."),
 QT_TRANSLATE_NOOP("darkcoin-core", "Transaction too large"),
 QT_TRANSLATE_NOOP("darkcoin-core", "Unable to bind to %s on this computer (bind returned error %s)"),
-QT_TRANSLATE_NOOP("darkcoin-core", "Unable to locate enough Darksend denominated funds for this transaction"),
 QT_TRANSLATE_NOOP("darkcoin-core", "Unable to sign masternode payment winner, wrong key?"),
 QT_TRANSLATE_NOOP("darkcoin-core", "Unknown -socks proxy version requested: %i"),
 QT_TRANSLATE_NOOP("darkcoin-core", "Unknown network specified in -onlynet: '%s'"),
