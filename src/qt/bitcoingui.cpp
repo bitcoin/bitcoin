78cdc18b291f5dac0fafd0bc8638201e75b75a9e
Philip Kaufmann <phil.kaufmann@t-online.de>
2015-02-09 11:21:26 +0100

    [Qt] use qint64 in BitcoinGUI::setNumBlocks
    - as QDateTime.secsTo() returns a qint64 also store in a qint64 and not in
      an integer

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -682,7 +682,7 @@ void BitcoinGUI::setNumBlocks(int count)
 
     QDateTime lastBlockDate = clientModel->getLastBlockDate();
     QDateTime currentDate = QDateTime::currentDateTime();
-    int secs = lastBlockDate.secsTo(currentDate);
+    qint64 secs = lastBlockDate.secsTo(currentDate);
 
     tooltip = tr("Processed %n blocks of transaction history.", "", count);
 
@@ -722,8 +722,8 @@ void BitcoinGUI::setNumBlocks(int count)
         }
         else
         {
-            int years = secs / YEAR_IN_SECONDS;
-            int remainder = secs % YEAR_IN_SECONDS;
+            qint64 years = secs / YEAR_IN_SECONDS;
+            qint64 remainder = secs % YEAR_IN_SECONDS;
             timeBehindText = tr("%1 and %2").arg(tr("%n year(s)", "", years)).arg(tr("%n week(s)","", remainder/WEEK_IN_SECONDS));
         }
 
