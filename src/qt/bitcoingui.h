9c0898c80ea821806d16eaf33c87456a5daf2778
Philip Kaufmann <phil.kaufmann@t-online.de>
2015-02-09 11:19:01 +0100

    [Qt] rework setNumBlocks to have blockDate as parameter
    - reduces some functional overhead and simplifies the code

--- src/qt/bitcoingui.h
+++ src/qt/bitcoingui.h
@@ -143,8 +143,8 @@ signals:
 public slots:
     /** Set number of connections shown in the UI */
     void setNumConnections(int count);
-    /** Set number of blocks shown in the UI */
-    void setNumBlocks(int count);
+    /** Set number of blocks and last block date shown in the UI */
+    void setNumBlocks(int count, const QDateTime& blockDate);
 
     /** Notify the user of an event from the core network or transaction handling code.
        @param[in] title     the message box / notification title
