7c37fc1ba03268d5fe73087fbdda6f269e7b9bcc
Evan Duffield <evan@darkcoin.io>
2015-02-09 14:06:11 -0700

    fixed attribution

--- src/spork.h
+++ src/spork.h
@@ -1,6 +1,5 @@
 
-// Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2012 The Bitcoin developers
+// Copyright (c) 2009-2012 The Darkcoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #ifndef SPORK_H
