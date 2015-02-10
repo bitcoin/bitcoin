f03598846dac18110abb673425297358c2fb86c4
vertoe <vertoe@qhor.net>
2015-02-10 09:33:30 +0100

    Revert "Fix coinbase maturity"
    This reverts commit eafb791d8c3b21bfef97a87ca92a2799cf1a7965.

--- src/main.h
+++ src/main.h
@@ -64,7 +64,7 @@ static const unsigned int BLOCKFILE_CHUNK_SIZE = 0x1000000; // 16 MiB
 /** The pre-allocation chunk size for rev?????.dat files (since 0.8) */
 static const unsigned int UNDOFILE_CHUNK_SIZE = 0x100000; // 1 MiB
 /** Coinbase transaction outputs can only be spent after this number of new blocks (network rule) */
-static const int COINBASE_MATURITY = 120;
+static const int COINBASE_MATURITY = 100;
 /** Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp. */
 static const unsigned int LOCKTIME_THRESHOLD = 500000000; // Tue Nov  5 00:53:20 1985 UTC
 /** Maximum number of script-checking threads allowed */
