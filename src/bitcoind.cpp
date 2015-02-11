210e909fca49902073d4d6f517ae3b2cee4f6f86
Michael Ford <fanquake@gmail.com>
2015-02-11 12:27:21 +0800

    Use https link to bitcoin.org in Doxygen intro

--- src/bitcoind.cpp
+++ src/bitcoind.cpp
@@ -21,7 +21,7 @@
  *
  * \section intro_sec Introduction
  *
- * This is the developer documentation of the reference client for an experimental new digital currency called Bitcoin (http://www.bitcoin.org/),
+ * This is the developer documentation of the reference client for an experimental new digital currency called Bitcoin (https://www.bitcoin.org/),
  * which enables instant payments to anyone, anywhere in the world. Bitcoin uses peer-to-peer technology to operate
  * with no central authority: managing transactions and issuing money are carried out collectively by the network.
  *
