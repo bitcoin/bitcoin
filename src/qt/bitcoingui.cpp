cc9eef01a24cde4498afb0412558fd88850b34ed
UdjinM6 <UdjinM6@users.noreply.github.com>
2015-02-12 16:32:34 +0300

    fix time to switch "out of sync" message on/of to make UI more consistent (as we are 4x times faser than bitcoin)

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -670,7 +670,7 @@ void BitcoinGUI::setNumBlocks(int count)
     tooltip = tr("Processed %1 blocks of transaction history.").arg(count);
 
     // Set icon state: spinning if catching up, tick otherwise
-    if(secs < 90*60)
+    if(secs < 25*60) // 90*60 for bitcoin but we are 4x times faster
     {
         tooltip = tr("Up to date") + QString(".<br>") + tooltip;
         labelBlocksIcon->setPixmap(QIcon(":/icons/synced").pixmap(STATUSBAR_ICONSIZE, STATUSBAR_ICONSIZE));
