9823b9ebad29ff133f3281dd38bc89bfb58bab2e
UdjinM6 <UdjinM6@users.noreply.github.com>
2015-02-12 16:58:12 +0300

    Solve performance problems by caching and fix probable security issue - add number of rounds to outputs - cache rounds calculation results of GetInputDarksendRounds - select shortest rounds chain instead of the first found

--- src/core.h
+++ src/core.h
@@ -153,6 +153,7 @@ class CTxOut
 {
 public:
     int64_t nValue;
+    int nRounds;
     CScript scriptPubKey;
 
     CTxOut()
@@ -171,6 +172,7 @@ public:
     void SetNull()
     {
         nValue = -1;
+        nRounds = -10; // an initial value, should be no way to get this by calculations
         scriptPubKey.clear();
     }
 
@@ -197,6 +199,7 @@ public:
     friend bool operator==(const CTxOut& a, const CTxOut& b)
     {
         return (a.nValue       == b.nValue &&
+                a.nRounds      == b.nRounds &&
                 a.scriptPubKey == b.scriptPubKey);
     }
 
