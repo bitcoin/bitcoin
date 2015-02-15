c1d971d962595d732ff8f7f5a07ec58061cc32a2
Jorge Timón <jtimon@jtimon.cc>
2015-02-15 02:21:42 +0100

    Consensus: Refactor: Decouple pow.o from chainparams.o

--- src/txdb.cpp
+++ src/txdb.cpp
@@ -5,6 +5,8 @@
 
 #include "txdb.h"
 
+#include "chainparams.h"
+#include "hash.h"
 #include "main.h"
 #include "pow.h"
 #include "uint256.h"
@@ -223,7 +225,7 @@ bool CBlockTreeDB::LoadBlockIndexGuts()
                 pindexNew->nStatus        = diskindex.nStatus;
                 pindexNew->nTx            = diskindex.nTx;
 
-                if (!CheckProofOfWork(pindexNew->GetBlockHash(), pindexNew->nBits))
+                if (!CheckProofOfWork(pindexNew->GetBlockHash(), pindexNew->nBits, Params().GetConsensus()))
                     return error("LoadBlockIndex(): CheckProofOfWork failed: %s", pindexNew->ToString());
 
                 pcursor->Next();
