7dca775e6a571f4467b0b934ca3d6f795a8f4f35
Cory Fields <cory-nospam-@coryfields.com>
2015-02-17 16:15:01 -0500

    0.10 fix for crashes on OSX 10.6
    strnlen is available at build-time but not at runtime, causing a crash.
    
    0.11 drops support for 10.6, so this is not needed in master.

--- src/compat/strnlen.cpp
+++ src/compat/strnlen.cpp
@@ -7,12 +7,11 @@
 #endif
 
 #include <cstring>
-
-#if HAVE_DECL_STRNLEN == 0
-size_t strnlen( const char *start, size_t max_len)
+// OSX 10.6 is missing strnlen at runtime, but builds targetting it will still
+// succeed. Define our own version here to avoid a crash.
+size_t strnlen_int( const char *start, size_t max_len)
 {
     const char *end = (const char *)memchr(start, '\0', max_len);
 
     return end ? (size_t)(end - start) : max_len;
 }
-#endif // HAVE_DECL_STRNLEN
