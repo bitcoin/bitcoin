b5f6f0e7a735eb44e4062493ed6296b75c07ef79
Evan Duffield <evan@darkcoin.io>
2015-02-18 12:17:33 -0700

    minimum collateral amount
    In the case where collateral is made as COLLATERAL*5, then
    used 4 times, it will result in an input exactly COLLATERAL sized.
    When used as a collateral transaction this will result in 1 input of
    COLLATERAL and zero outputs (due to collateral being paid solely in fees).
    zero output transactions aren't supported, so it'll result in DS failure.

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -1726,7 +1726,7 @@ bool CWallet::HasCollateralInputs() const
 
 bool CWallet::IsCollateralAmount(int64_t nInputAmount) const
 {
-    return  nInputAmount != 0 && nInputAmount % DARKSEND_COLLATERAL == 0 && nInputAmount < DARKSEND_COLLATERAL * 5;
+    return  nInputAmount != 0 && nInputAmount % DARKSEND_COLLATERAL == 0 && nInputAmount < DARKSEND_COLLATERAL * 5 && nInputAmount > DARKSEND_COLLATERAL;
 }
 
 bool CWallet::SelectCoinsWithoutDenomination(int64_t nTargetValue, set<pair<const CWalletTx*,unsigned int> >& setCoinsRet, int64_t& nValueRet) const
