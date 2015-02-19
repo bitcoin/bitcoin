568be307a1f421f4bd3541b56a7bcbd9be3afe43
svost <ya.nowa@yandex.ru>
2015-02-19 17:06:35 +0300

    Small-sized corrections in assembly with msvc

--- MSVC/build-helpers/buildqt64.bat
+++ MSVC/build-helpers/buildqt64.bat
@@ -8,7 +8,7 @@ perl -pi.bak -e "s#QMAKE_CFLAGS_RELEASE    = -O2 -MD#QMAKE_CFLAGS_RELEASE    = -
 perl -pi.bak -e "s#QMAKE_CFLAGS_DEBUG      = -Zi -MDd#QMAKE_CFLAGS_DEBUG      = -Z7 -MTd#g;" qtbase\mkspecs\win32-msvc2012\qmake.conf
 REM Now do 64 bit by setting environment to MSVC 64 bit
 call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86_amd64
-call configure -debug-and-release -openssl-linked -opensource -confirm-license -platform win32-msvc2012 -nomake examples -nomake tests -static -I \MyProjects\Deps\openssl-1.0.1j\inc32 -L \MyProjects\Deps\openssl-1.0.1j\out64.dbg -L \MyProjects\Deps\openssl-1.0.1j\out64 -l gdi32 -no-opengl -qt-zlib -qt-libpng -qt-libjpeg
+call configure -debug-and-release -openssl-linked -opensource -confirm-license -platform win32-msvc2012 -nomake examples -nomake tests -static -I \MyProjects\Deps\openssl-1.0.2\inc32 -L \MyProjects\Deps\openssl-1.0.2\out64.dbg -L \MyProjects\Deps\openssl-1.0.2\out64 -l gdi32 -no-opengl -qt-zlib -qt-libpng -qt-libjpeg
 nmake
 REM put back the path
 set PATH=%OLDPATH%
