5a9453fbaf4bf1d8195fae66d19b4a81bae16992
Nicolas Benoit <nbenoit@tuxfamily.org>
2015-02-23 20:59:39 +0100

    s/updateTip/UpdateTip/

--- src/main.cpp
+++ src/main.cpp
@@ -1990,7 +1990,7 @@ void static UpdateTip(CBlockIndex *pindexNew) {
             pindex = pindex->pprev;
         }
         if (nUpgraded > 0)
-            LogPrintf("updateTip: %d of last 100 blocks above version %d\n", nUpgraded, (int)CBlock::CURRENT_VERSION);
+            LogPrintf("UpdateTip: %d of last 100 blocks above version %d\n", nUpgraded, (int)CBlock::CURRENT_VERSION);
         if (nUpgraded > 100/2)
         {
             // strMiscWarning is read by GetWarnings(), called by Qt and the JSON-RPC code to warn the user:
