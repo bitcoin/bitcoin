82f080d4139a7961c428684dd4561123286faa0b
UdjinM6 <UdjinM6@users.noreply.github.com>
2015-02-24 17:02:22 +0300

    copyright

--- src/masternode.cpp
+++ src/masternode.cpp
@@ -1,3 +1,7 @@
+// Copyright (c) 2014-2015 The Darkcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #include "masternode.h"
 #include "masternodeman.h"
 #include "darksend.h"
