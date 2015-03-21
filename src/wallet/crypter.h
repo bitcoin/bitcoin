a2c069042da8daba26a236f7120f4478a19ddbec
Philip Kaufmann <phil.kaufmann@t-online.de>
2015-03-21 18:15:31 +0100

    ensure consistent header comment naming conventions
    - BITCOIN_FOLDER_SUBFOLDER_FILENAME_H
    - also correct header groupings in core files

--- src/wallet/crypter.h
+++ src/wallet/crypter.h
@@ -2,8 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_CRYPTER_H
-#define BITCOIN_CRYPTER_H
+#ifndef BITCOIN_WALLET_CRYPTER_H
+#define BITCOIN_WALLET_CRYPTER_H
 
 #include "keystore.h"
 #include "serialize.h"
@@ -193,4 +193,4 @@ public:
     boost::signals2::signal<void (CCryptoKeyStore* wallet)> NotifyStatusChanged;
 };
 
-#endif // BITCOIN_CRYPTER_H
+#endif // BITCOIN_WALLET_CRYPTER_H
