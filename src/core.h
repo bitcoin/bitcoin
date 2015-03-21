5b9127e54204a64f8310c0cf13b0f1e99b0bcef4
Evan Duffield <evan@darkcoin.io>
2015-03-21 05:56:50 -0700

    protocol bump

--- src/core.h
+++ src/core.h
@@ -43,7 +43,7 @@ static const int64_t DARKSEND_POOL_MAX = (999.99*COIN);
 #define MASTERNODE_EXPIRATION_SECONDS          (65*60)
 #define MASTERNODE_REMOVAL_SECONDS             (70*60)
 
-static const int MIN_POOL_PEER_PROTO_VERSION = 70072; // minimum peer version accepted by DarkSendPool
+static const int MIN_POOL_PEER_PROTO_VERSION = 70073; // minimum peer version accepted by DarkSendPool
 
 class CTransaction;
 
