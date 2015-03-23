ef10824a4f8a894fe88ef6e08dbf3b6cc72bcffc
UdjinM6 <UdjinM6@users.noreply.github.com>
2015-03-23 20:28:36 +0300

    explicitly grab each piece of data from memory for hashing in CMasternode::CalculateScore

--- src/masternode.cpp
+++ src/masternode.cpp
@@ -214,7 +214,7 @@ uint256 CMasternode::CalculateScore(int mod, int64_t nBlockHeight)
     if(!GetBlockHash(hash, nBlockHeight)) return 0;
 
     uint256 hash2 = Hash(BEGIN(hash), END(hash));
-    uint256 hash3 = Hash(BEGIN(hash), END(aux));
+    uint256 hash3 = Hash(BEGIN(hash), END(hash), BEGIN(aux), END(aux));
 
     uint256 r = (hash3 > hash2 ? hash3 - hash2 : hash2 - hash3);
 
