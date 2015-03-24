04e96a2d21385e7ecb460b13d105c20043ddbb70
Ruben de Vries <ruben@rubensayshi.com>
2015-03-24 14:29:13 +0100

    don't trickle for whitelisted nodes

--- src/main.h
+++ src/main.h
@@ -177,7 +177,12 @@ bool LoadBlockIndex();
 void UnloadBlockIndex();
 /** Process protocol messages received from a given node */
 bool ProcessMessages(CNode* pfrom);
-/** Send queued protocol messages to be sent to a give node */
+/**
+ * Send queued protocol messages to be sent to a give node.
+ *
+ * @param[in]   pto             The node which we are sending messages to.
+ * @param[in]   fSendTrickle    When true send the trickled data, otherwise trickle the data until true.
+ */
 bool SendMessages(CNode* pto, bool fSendTrickle);
 /** Run an instance of the script checking thread */
 void ThreadScriptCheck();
