fd311996e877d567a4e34a19b47e94d66e07100a
Cory Fields <cory-nospam-@coryfields.com>
2015-03-25 15:00:32 -0400

    consensus: don't use arith_uint256 in consensus.h
    Requiring arith_uint256 at such a base level is not good for modularity.

--- src/consensus/params.h
+++ src/consensus/params.h
@@ -6,7 +6,6 @@
 #ifndef BITCOIN_CONSENSUS_CONSENSUS_PARAMS_H
 #define BITCOIN_CONSENSUS_CONSENSUS_PARAMS_H
 
-#include "arith_uint256.h"
 #include "uint256.h"
 
 namespace Consensus {
@@ -21,7 +20,7 @@ struct Params {
     int nMajorityRejectBlockOutdated;
     int nMajorityWindow;
     /** Proof of work parameters */
-    arith_uint256 powLimit;
+    uint256 powLimit;
     bool fPowAllowMinDifficultyBlocks;
     int64_t nPowTargetSpacing;
     int64_t nPowTargetTimespan;
