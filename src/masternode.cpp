fea26cfb89e933ec3e8142b9742c8150d5a26b64
Evan Duffield <evan@darkcoin.io>
2015-03-30 12:33:42 -0700

    updated minimum for reference node

--- src/masternode.cpp
+++ src/masternode.cpp
@@ -436,7 +436,7 @@ bool CMasternodePayments::ProcessBlock(int nBlockHeight)
     }
 
     // pay to the oldest MN that still had no payment but its input is old enough and it was active long enough
-    CMasternode *pmn = mnodeman.FindOldestNotInVec(vecLastPayments, nMinimumAge, nMinimumAge * 2.5 * 60);
+    CMasternode *pmn = mnodeman.FindOldestNotInVec(vecLastPayments, nMinimumAge, 0);
     if(pmn != NULL)
     {
         newWinner.score = 0;
