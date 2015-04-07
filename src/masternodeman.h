95826a531b74a7ff86b0e011d6ed0e87b167bcce
Evan Duffield <evan@darkcoin.io>
2015-04-07 12:59:30 -0700

    fixed vote-many

--- src/masternodeman.h
+++ src/masternodeman.h
@@ -108,6 +108,7 @@ public:
 
     /// Find an entry
     CMasternode* Find(const CTxIn& vin);
+    CMasternode* Find(const CPubKey& pubKeyMasternode);
 
     /// Find an entry thta do not match every entry provided vector
     CMasternode* FindOldestNotInVec(const std::vector<CTxIn> &vVins, int nMinimumAge, int nMinimumActiveSeconds);
