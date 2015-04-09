de52ba4c239c0d5198dcbfc637de2c5a17e02def
Jonas Schnelli <jonas.schnelli@include7.ch>
2015-04-09 22:16:04 +0200

    [Wallet] refactor and optimize init.cpp/CWallet interaction
    Reduced wallet/core coupling.
    
    - hides CWalletDB behind CWallet
    - reduces ENABLE_WALLET ifdefs
    - remove some whitespace

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -499,6 +499,9 @@ public:
     CWallet()
     {
         SetNull();
+
+        strWalletFile = GetWalletFile();
+        fFileBacked = true;
     }
 
     CWallet(const std::string& strWalletFileIn)
@@ -666,7 +669,7 @@ public:
     CAmount GetChange(const CTransaction& tx) const;
     void SetBestChain(const CBlockLocator& loc);
 
-    DBErrors LoadWallet(bool& fFirstRunRet);
+    bool LoadWallet(std::string& warningString, std::string& errorString);
     DBErrors ZapWalletTx(std::vector<CWalletTx>& vWtx);
 
     bool SetAddressBook(const CTxDestination& address, const std::string& strName, const std::string& purpose);
@@ -715,9 +718,27 @@ public:
     //! Flush wallet (bitdb flush)
     void Flush(bool shutdown=false);
 
+    //! Dump wallet infos to log
+    void LogInfos() const;
+    static void LogGeneralInfos();
+
     //! Verify the wallet database and perform salvage if required
-    static bool Verify(const std::string& walletFile, std::string& warningString, std::string& errorString);
-    
+    static bool Verify(std::string& warningString, std::string& errorString);
+
+    static bool IsDisabled();
+
+    //! Map parameters to internal vars
+    static void MapParameters(std::string& warningString, std::string& errorString);
+
+    //! Get user defined wallet file
+    static std::string GetWalletFile();
+
+    //! Performs sanity check and appends possible errors to given string
+    static void SanityCheck(std::string& errorString);
+
+    //! append help text to existing string
+    static void AppendHelpMessageString(std::string& strUsage);
+
     /** 
      * Address book entry changed.
      * @note called with lock cs_wallet held.
