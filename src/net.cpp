e70bb308ffc436f3dd006eb697e1358add5aa9a1
CryptoManiac <CryptoManiac@users.noreply.github.com>
2015-04-14 23:37:01 +0300

    Use UINT64_MAX

--- src/net.cpp
+++ src/net.cpp
@@ -308,7 +308,7 @@ extern int GetExternalIPbySTUN(uint64_t rnd, struct sockaddr_in *mapped, const c
 bool GetMyExternalIP(CNetAddr& ipRet)
 {
     struct sockaddr_in mapped;
-    uint64_t rnd = ULLONG_MAX;
+    uint64_t rnd = UINT64_MAX;
     const char *srv;
     int rc = GetExternalIPbySTUN(rnd, &mapped, &srv);
     if(rc >= 0) {
