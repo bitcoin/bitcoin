718da7154b670ba7dd7387f6ad57aab7a1cc156e
Jorge Timón <jtimon@jtimon.cc>
2015-04-17 14:40:24 +0200

    Globals: Remove a bunch of Params() calls from main.cpp:
    1) Chainparams: Explicit CChainParams arg for main:
    
    -AcceptBlock
    -AcceptBlockHeader
    -ActivateBestChain
    -ConnectTip
    -InitBlockIndex
    -LoadExternalBlockFile
    -VerifyDB parametric constructor
    
    2) Also pickup more Params()\. in main.cpp
    
    3) Pass nPruneAfterHeight explicitly to new FindFilesToPrune() in main.cpp

--- src/rpcblockchain.cpp
+++ src/rpcblockchain.cpp
@@ -562,7 +562,7 @@ UniValue verifychain(const UniValue& params, bool fHelp)
     if (params.size() > 1)
         nCheckDepth = params[1].get_int();
 
-    return CVerifyDB().VerifyDB(pcoinsTip, nCheckLevel, nCheckDepth);
+    return CVerifyDB().VerifyDB(Params(), pcoinsTip, nCheckLevel, nCheckDepth);
 }
 
 /** Implementation of IsSuperMajority with better feedback */
@@ -828,7 +828,7 @@ UniValue invalidateblock(const UniValue& params, bool fHelp)
     }
 
     if (state.IsValid()) {
-        ActivateBestChain(state);
+        ActivateBestChain(state, Params());
     }
 
     if (!state.IsValid()) {
@@ -867,7 +867,7 @@ UniValue reconsiderblock(const UniValue& params, bool fHelp)
     }
 
     if (state.IsValid()) {
-        ActivateBestChain(state);
+        ActivateBestChain(state, Params());
     }
 
     if (!state.IsValid()) {
