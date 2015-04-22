136e422e6c96ce5a9623a9528a293c6dc7803ae6
Pieter Wuille <pieter.wuille@gmail.com>
2015-04-22 14:28:26 -0700

    Update key.cpp to use new libsecp256k1
    libsecp256k1's API changed, so update key.cpp to use it.
    
    Libsecp256k1 now has explicit context objects, which makes it completely thread-safe.
    In turn, keep an explicit context object in key.cpp, which is explicitly initialized
    destroyed. This is not really pretty now, but it's more efficient than the static
    initialized object in key.cpp (which made for example bitcoin-tx slow, as for most of
    its calls, libsecp256k1 wasn't actually needed).
    
    This also brings in the new blinding support in libsecp256k1. By passing in a random
    seed, temporary variables during the elliptic curve computations are altered, in such
    a way that if an attacker does not know the blind, observing the internal operations
    leaks less information about the keys used. This was implemented by Greg Maxwell.

--- src/key.h
+++ src/key.h
@@ -173,7 +173,13 @@ struct CExtKey {
     void SetMaster(const unsigned char* seed, unsigned int nSeedLen);
 };
 
-/** Check that required EC support is available at runtime */
+/** Initialize the elliptic curve support. May not be called twice without calling ECC_Stop first. */
+void ECC_Start(void);
+
+/** Deinitialize the elliptic curve support. No-op if ECC_Start wasn't called first. */
+void ECC_Stop(void);
+
+/** Check that required EC support is available at runtime. */
 bool ECC_InitSanityCheck(void);
 
 #endif // BITCOIN_KEY_H
