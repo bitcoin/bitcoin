9dc4fde01977326e05c15e4b1bec5bf0844131be
Jonas Schnelli <jonas.schnelli@include7.ch>
2015-05-02 13:15:26 +0200

    extend rpc-tests.sh control script with non-travis tests

--- qa/pull-tester/rpc-tests.sh
+++ qa/pull-tester/rpc-tests.sh
@@ -31,6 +31,21 @@ testScripts=(
     'proxy_test.py'
     'merkle_blocks.py'
     'signrawtransactions.py'
+);
+testScriptsExt=(
+    'bipdersig-p2p.py'
+    'bipdersig.py'
+    'getblocktemplate_longpoll.py'
+    'getblocktemplate_proposals.py'
+    'prune.py'
+    'forknotify.py'
+    'invalidateblock.py'
+    'keypool.py'
+    'receivedby.py'
+    'reindex.py'
+    'rpcbind_test.py'
+    'script_test.py'
+    'smartfees.py'
     'maxblocksinflight.py'
     'invalidblockrequest.py'
     'rawtransactions.py'
@@ -39,10 +54,18 @@ testScripts=(
 if [ "x${ENABLE_BITCOIND}${ENABLE_UTILS}${ENABLE_WALLET}" = "x111" ]; then
     for (( i = 0; i < ${#testScripts[@]}; i++ ))
     do
-        if [ -z "$1" ] || [ "$1" == "${testScripts[$i]}" ] || [ "$1.py" == "${testScripts[$i]}" ]
+        if [ -z "$1" ] || [ "$1" == "-extended" ] || [ "$1" == "${testScripts[$i]}" ] || [ "$1.py" == "${testScripts[$i]}" ]
         then
             echo -e "Running testscript \033[1m${testScripts[$i]}...\033[0m"
             ${BUILDDIR}/qa/rpc-tests/${testScripts[$i]} --srcdir "${BUILDDIR}/src"
+        fi
+    done
+    for (( i = 0; i < ${#testScriptsExt[@]}; i++ ))
+    do
+        if [ "$1" == "-extended" ] || [ "$1" == "${testScriptsExt[$i]}" ] || [ "$1.py" == "${testScriptsExt[$i]}" ]
+        then
+            echo -e "Running testscript \033[1m${testScriptsExt[$i]}...\033[0m"
+            ${BUILDDIR}/qa/rpc-tests/${testScriptsExt[$i]} --srcdir "${BUILDDIR}/src"
         fi
     done
 else
