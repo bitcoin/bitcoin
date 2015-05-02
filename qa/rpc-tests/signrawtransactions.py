3e875b1befad5ed5dbd2736c9befccd32d67721c
Jonas Schnelli <jonas.schnelli@include7.ch>
2015-05-02 13:58:10 +0200

    pull-tester/rpc-tests.sh: allow passing throug of arguments

--- qa/rpc-tests/signrawtransactions.py
+++ qa/rpc-tests/signrawtransactions.py
@@ -3,8 +3,8 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-from test_framework import BitcoinTestFramework
-from util import *
+from test_framework.test_framework import BitcoinTestFramework
+from test_framework.util import *
 
 
 class SignRawTransactionsTest(BitcoinTestFramework):
