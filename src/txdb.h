32742ea75cfd36535c7242518470eae45d124a3b
Pieter Wuille <pieter.wuille@gmail.com>
2015-05-04 01:56:42 +0200

    Cache tweak and logging improvements

--- src/txdb.h
+++ src/txdb.h
@@ -22,7 +22,7 @@ class uint256;
 //! -dbcache default (MiB)
 static const int64_t nDefaultDbCache = 100;
 //! max. -dbcache in (MiB)
-static const int64_t nMaxDbCache = sizeof(void*) > 4 ? 4096 : 1024;
+static const int64_t nMaxDbCache = sizeof(void*) > 4 ? 16384 : 1024;
 //! min. -dbcache in (MiB)
 static const int64_t nMinDbCache = 4;
 
