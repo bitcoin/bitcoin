c8a89ecb81139932ab6dc16ac933f73fc181cfc7
essofluffy <d.dehueck@yahoo.com>
2015-05-04 20:31:40 -0500

    Updated "out of sync" warning labels
    Changed "(out of sync)" to "(out of sync, please allow a few days to become synchronized)." Also updated relevant comments.
    
    This will allow the user to better understand the synchronization taking place.
    
    In response to issue #6106

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -130,10 +130,10 @@ OverviewPage::OverviewPage(QWidget *parent) :
     connect(ui->listTransactions, SIGNAL(clicked(QModelIndex)), this, SLOT(handleTransactionClicked(QModelIndex)));
 
     // init "out of sync" warning labels
-    ui->labelWalletStatus->setText("(" + tr("out of sync") + ")");
-    ui->labelTransactionsStatus->setText("(" + tr("out of sync") + ")");
+    ui->labelWalletStatus->setText("(" + tr("out of sync, please allow a few days to become synchronized") + ")");
+    ui->labelTransactionsStatus->setText("(" + tr("out of sync, please allow a few days to become synchronized") + ")");
 
-    // start with displaying the "out of sync" warnings
+    // start with displaying the "out of sync" warning labels
     showOutOfSyncWarning(true);
 }
 
