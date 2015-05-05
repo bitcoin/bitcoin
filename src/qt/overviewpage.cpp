2a6b844427d097a27bd1d2de843fa8176c2d36b7
Jonas Schnelli <jonas.schnelli@include7.ch>
2015-05-05 21:55:19 +0200

    [QT] change transaction amount and height in overview page

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -18,8 +18,8 @@
 #include <QAbstractItemDelegate>
 #include <QPainter>
 
-#define DECORATION_SIZE 64
-#define NUM_ITEMS 3
+#define DECORATION_SIZE 54
+#define NUM_ITEMS 5
 
 class TxViewDelegate : public QAbstractItemDelegate
 {
