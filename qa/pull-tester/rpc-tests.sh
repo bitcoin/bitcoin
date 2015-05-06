0029aa3dcc41f49893d2c6cacb8fdeda6a665b60
Cory Fields <cory-nospam-@coryfields.com>
2015-05-05 20:30:20 -0400

    Depends: Add ZeroMQ package

--- qa/pull-tester/rpc-tests.sh
+++ qa/pull-tester/rpc-tests.sh
@@ -59,6 +59,10 @@ testScriptsExt=(
     'p2p-acceptblock.py'
 );
 
+if [ "x$ENABLE_ZMQ" = "x1" ]; then
+  testScripts=( ${testScripts[@]} 'zmq_test.py' )
+fi
+
 extArg="-extended"
 passOn=${@#$extArg}
 
