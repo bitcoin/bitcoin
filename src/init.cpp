fe9af4923c808802b9a54cdcceb2bf921388ec39
Wladimir J. van der Laan <laanwj@gmail.com>
2015-05-18 11:21:32 +0200

    Parameter interaction: disable upnp if -proxy set
    To protect privacy, do not use UPNP when a proxy is set. The user may
    still specify -listen=1 to listen locally (for a hidden service), so
    don't rely on this happening through -listen.
    
    Fixes #2927.
    
    Conflicts:
    	src/init.cpp
    
    Rebased-From: 8c35b6f3be218101630101806300cfd75be23f58
    Github-Pull: #6153

--- src/init.cpp
+++ src/init.cpp
@@ -593,7 +593,11 @@ bool AppInit2(boost::thread_group& threadGroup)
     if (mapArgs.count("-proxy")) {
         // to protect privacy, do not listen by default if a default proxy server is specified
         if (SoftSetBoolArg("-listen", false))
-            LogPrintf("AppInit2 : parameter interaction: -proxy set -> setting -listen=0\n");
+            LogPrintf("%s: parameter interaction: -proxy set -> setting -listen=0\n", __func__);
+        // to protect privacy, do not use UPNP when a proxy is set. The user may still specify -listen=1
+        // to listen locally, so don't rely on this happening through -listen below.
+        if (SoftSetBoolArg("-upnp", false))
+            LogPrintf("%s: parameter interaction: -proxy set -> setting -upnp=0\n", __func__);
         // to protect privacy, do not discover addresses by default
         if (SoftSetBoolArg("-discover", false))
             LogPrintf("AppInit2 : parameter interaction: -proxy set -> setting -discover=0\n");
