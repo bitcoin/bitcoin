4b759fb610e6666da5ab35d43e4ecb6283dbadba
Luke Dashjr <luke-jr+git@utopios.org>
2015-05-23 04:29:56 +0000

    libbitcoinconsensus: Several C bindings to use CScriptExecution

--- src/script/bitcoinconsensus.cpp
+++ src/script/bitcoinconsensus.cpp
@@ -7,6 +7,7 @@
 
 #include "primitives/transaction.h"
 #include "script/interpreter.h"
+#include "script/script_error.h"
 #include "version.h"
 
 namespace {
@@ -102,7 +103,226 @@ bool bitcoinconsensus_parse_txTo(bitcoinconsensus_txTo_sigchecker& o, const unsi
     return true;
 }
 
+typedef std::vector<unsigned char> valtype;
+typedef std::vector<valtype> bitcoinconsensus_stack_t;
+
+class bitcoinconsensus_script_execution_data {
+public:
+    CScriptExecution *sexec;
+    bitcoinconsensus_stack_t stack;
+    CScript script;
+    ScriptError serror;
+    bitcoinconsensus_txTo_sigchecker tsc;
+
+    bitcoinconsensus_script_execution_data() : sexec(NULL) { }
+
+    ~bitcoinconsensus_script_execution_data() {
+        delete sexec;
+    };
+
+    bool parse_stackPos(bitcoinconsensus_stack_t*& stackp, bitcoinconsensus_script_execution_stack stackid, int& stackPos, bool allowEnd)
+    {
+        switch (stackid)
+        {
+            case bitcoinconsensus_SEXEC_STACK:
+                stackp = &stack;
+                break;
+            case bitcoinconsensus_SEXEC_ALTSTACK:
+                if (!sexec)
+                    return false;
+                stackp = &sexec->altstack;
+                break;
+            default:
+                return false;
+        }
+
+        if (stackPos < 0)
+        {
+            stackPos = stackp->size() + stackPos + 1;
+            if (stackPos < 0)
+                return false;
+        }
+        if ((unsigned int)stackPos + (allowEnd ? 0 : 1) > stackp->size())
+            return false;
+
+        return true;
+    }
+
+    void Terminate() {
+        delete sexec;
+        sexec = NULL;
+    };
+};
+
+
 } // anon namespace
+
+bitcoinconsensus_script_execution_t *bitcoinconsensus_script_execution(const unsigned char *script, unsigned int scriptLen, const unsigned char *txTo, unsigned int txToLen, unsigned int nIn, bitcoinconsensus_error* err)
+{
+    bitcoinconsensus_script_execution_data *o = NULL;
+    try {
+        o = new bitcoinconsensus_script_execution_data();
+
+        {
+            std::vector<unsigned char> vchScript;
+            vchScript.assign(script, &script[scriptLen]);
+            o->script = CScript(vchScript);
+        }
+
+        if (txTo) {
+            if (!bitcoinconsensus_parse_txTo(o->tsc, txTo, txToLen, nIn, err))
+                return NULL;
+        } else {
+            o->tsc.checker = new BaseSignatureChecker();
+        }
+
+        return (bitcoinconsensus_script_execution_t *)o;
+    } catch (...) {
+        delete o;
+        set_error(err, bitcoinconsensus_ERR_UNKNOWN);
+        return NULL;
+    }
+}
+
+int bitcoinconsensus_script_execution_stack_insert(bitcoinconsensus_script_execution_t *po, bitcoinconsensus_script_execution_stack stackid, int stackPos, const void *datap, size_t dataLen, bitcoinconsensus_error* err)
+{
+    try {
+        bitcoinconsensus_script_execution_data *o = (bitcoinconsensus_script_execution_data *)po;
+        const unsigned char *data = (const unsigned char *)datap;
+        bitcoinconsensus_stack_t *stack;
+
+        if (!o->parse_stackPos(stack, stackid, stackPos, true))
+            return set_error(err, bitcoinconsensus_ERR_BAD_INDEX);
+
+        valtype vchData;
+        vchData.assign(data, &data[dataLen]);
+
+        stack->insert(stack->begin() + stackPos, vchData);
+
+        return 1;
+    } catch (...) {
+        return set_error(err, bitcoinconsensus_ERR_UNKNOWN);
+    }
+}
+
+int bitcoinconsensus_script_execution_stack_delete(bitcoinconsensus_script_execution_t *po, bitcoinconsensus_script_execution_stack stackid, int stackPos, bitcoinconsensus_error* err)
+{
+    try {
+        bitcoinconsensus_script_execution_data *o = (bitcoinconsensus_script_execution_data *)po;
+        bitcoinconsensus_stack_t *stack;
+
+        if (!o->parse_stackPos(stack, stackid, stackPos, false))
+            return set_error(err, bitcoinconsensus_ERR_BAD_INDEX);
+
+        stack->erase(stack->begin() + stackPos);
+
+        return 1;
+    } catch (...) {
+        return set_error(err, bitcoinconsensus_ERR_UNKNOWN);
+    }
+}
+
+int bitcoinconsensus_script_execution_stack_get(bitcoinconsensus_script_execution_t *po, bitcoinconsensus_script_execution_stack stackid, int stackPos, const void **datap, size_t *dataLen, bitcoinconsensus_error* err)
+{
+    try {
+        bitcoinconsensus_script_execution_data *o = (bitcoinconsensus_script_execution_data *)po;
+        bitcoinconsensus_stack_t *stack;
+
+        if (!o->parse_stackPos(stack, stackid, stackPos, false))
+            return set_error(err, bitcoinconsensus_ERR_BAD_INDEX);
+
+        valtype& vchStackElem = *(stack->begin() + stackPos);
+
+        *datap = vchStackElem.data();
+        *dataLen = vchStackElem.size();
+
+        return 1;
+    } catch (...) {
+        return set_error(err, bitcoinconsensus_ERR_UNKNOWN);
+    }
+}
+
+int bitcoinconsensus_script_execution_start(bitcoinconsensus_script_execution_t *po, unsigned int flags, bitcoinconsensus_error* err)
+{
+    try {
+        bitcoinconsensus_script_execution_data *o = (bitcoinconsensus_script_execution_data *)po;
+
+        if (o->sexec)
+            return set_error(err, bitcoinconsensus_ERR_UNKNOWN);
+
+        o->sexec = new CScriptExecution(o->stack, o->script, flags, *o->tsc.checker, &o->serror);
+        if (!o->sexec->Start())
+        {
+            o->Terminate();
+            return set_error(err, bitcoinconsensus_ERR_SCRIPT_EXECUTION);
+        }
+
+        return 1;
+    } catch (...) {
+        return set_error(err, bitcoinconsensus_ERR_UNKNOWN);
+    }
+}
+
+int bitcoinconsensus_script_execution_step(bitcoinconsensus_script_execution_t *po, int *fEof, bitcoinconsensus_error* err)
+{
+    try {
+        bitcoinconsensus_script_execution_data *o = (bitcoinconsensus_script_execution_data *)po;
+
+        if (!o->sexec->Step())
+            return set_error(err, bitcoinconsensus_ERR_SCRIPT_EXECUTION);
+
+        *fEof = o->sexec->fEof;
+        return 1;
+    } catch (...) {
+        return set_error(err, bitcoinconsensus_ERR_UNKNOWN);
+    }
+}
+
+int bitcoinconsensus_script_execution_terminate(bitcoinconsensus_script_execution_t *po, bitcoinconsensus_error* err)
+{
+    try {
+        bitcoinconsensus_script_execution_data *o = (bitcoinconsensus_script_execution_data *)po;
+        o->Terminate();
+        return 1;
+    } catch (...) {
+        return set_error(err, bitcoinconsensus_ERR_UNKNOWN);
+    }
+}
+
+int bitcoinconsensus_script_execution_get_pc(bitcoinconsensus_script_execution_t *po, bitcoinconsensus_error* err)
+{
+    try {
+        bitcoinconsensus_script_execution_data *o = (bitcoinconsensus_script_execution_data *)po;
+        std::vector<unsigned char>::const_iterator cbegin = o->script.begin();
+        return std::distance(cbegin, o->sexec->pc);
+    } catch (...) {
+        set_error(err, bitcoinconsensus_ERR_UNKNOWN);
+        return -1;
+    }
+}
+
+int bitcoinconsensus_script_execution_get_codehash_pos(bitcoinconsensus_script_execution_t *po, bitcoinconsensus_error* err)
+{
+    try {
+        bitcoinconsensus_script_execution_data *o = (bitcoinconsensus_script_execution_data *)po;
+        std::vector<unsigned char>::const_iterator cbegin = o->script.begin();
+        return std::distance(cbegin, o->sexec->pbegincodehash);
+    } catch (...) {
+        set_error(err, bitcoinconsensus_ERR_UNKNOWN);
+        return -1;
+    }
+}
+
+int bitcoinconsensus_script_execution_get_sigop_count(bitcoinconsensus_script_execution_t *po, bitcoinconsensus_error* err)
+{
+    try {
+        bitcoinconsensus_script_execution_data *o = (bitcoinconsensus_script_execution_data *)po;
+        return o->sexec->nOpCount;
+    } catch (...) {
+        set_error(err, bitcoinconsensus_ERR_UNKNOWN);
+        return -1;
+    }
+}
 
 int bitcoinconsensus_verify_script(const unsigned char *scriptPubKey, unsigned int scriptPubKeyLen,
                                     const unsigned char *txTo        , unsigned int txToLen,
--- src/script/bitcoinconsensus.h
+++ src/script/bitcoinconsensus.h
@@ -6,6 +6,8 @@
 #ifndef BITCOIN_BITCOINCONSENSUS_H
 #define BITCOIN_BITCOINCONSENSUS_H
 
+#include <stddef.h>
+
 #if defined(BUILD_BITCOIN_INTERNAL) && defined(HAVE_CONFIG_H)
 #include "config/bitcoin-config.h"
   #if defined(_WIN32)
@@ -40,6 +42,8 @@ typedef enum bitcoinconsensus_error_t
     bitcoinconsensus_ERR_TX_SIZE_MISMATCH,
     bitcoinconsensus_ERR_TX_DESERIALIZE,
     bitcoinconsensus_ERR_UNKNOWN,
+    bitcoinconsensus_ERR_BAD_INDEX,
+    bitcoinconsensus_ERR_SCRIPT_EXECUTION,
 } bitcoinconsensus_error;
 
 /** Script verification flags */
@@ -49,6 +53,62 @@ enum
     bitcoinconsensus_SCRIPT_FLAGS_VERIFY_P2SH      = (1U << 0), // evaluate P2SH (BIP16) subscripts
     bitcoinconsensus_SCRIPT_FLAGS_VERIFY_DERSIG    = (1U << 2), // enforce strict DER (BIP66) compliance
 };
+
+typedef enum bitcoinconsensus_script_execution_stack
+{
+    bitcoinconsensus_SEXEC_STACK,
+    bitcoinconsensus_SEXEC_ALTSTACK,
+} bitcoinconsensus_script_execution_stack;
+
+typedef struct bitcoinconsensus_script_execution_t_ bitcoinconsensus_script_execution_t;
+
+/// Returns an opaque pointer representing a context for execution of the given script for a given transaction.
+/// If txTo is NULL, all operations that access the transaction will fail.
+/// If not NULL, err will contain an error/success code for the operation
+EXPORT_SYMBOL bitcoinconsensus_script_execution_t *bitcoinconsensus_script_execution(const unsigned char *script, unsigned int scriptLen, const unsigned char *txTo, unsigned int txToLen, unsigned int nIn, bitcoinconsensus_error*);
+
+/// Inserts a blob of data at a given position on one of the stacks.
+/// To access the altstack, the execution context must already be started.
+/// A negative stackPos will insert at the end of the stack.
+/// If not NULL, err will contain an error/success code for the operation
+EXPORT_SYMBOL int bitcoinconsensus_script_execution_stack_insert(bitcoinconsensus_script_execution_t *, bitcoinconsensus_script_execution_stack, int stackPos, const void *data, size_t dataLen, bitcoinconsensus_error*);
+
+/// Deletes the element at a given position of one of the stacks.
+/// A negative stackPos will insert at the end of the stack.
+/// If not NULL, err will contain an error/success code for the operation
+EXPORT_SYMBOL int bitcoinconsensus_script_execution_stack_delete(bitcoinconsensus_script_execution_t *, bitcoinconsensus_script_execution_stack, int stackPos, bitcoinconsensus_error*);
+
+/// Accesses the element at a given position of one of the stacks.
+/// A negative stackPos will insert at the end of the stack.
+/// When successful (indicated by a non-zero return value), data will receive a pointer to the stack element, and dataLen will be set to its size.
+/// If not NULL, err will contain an error/success code for the operation
+EXPORT_SYMBOL int bitcoinconsensus_script_execution_stack_get(bitcoinconsensus_script_execution_t *, bitcoinconsensus_script_execution_stack, int stackPos, const void **data, size_t *dataLen, bitcoinconsensus_error*);
+
+/// Starts execution of the script.
+/// If not NULL, err will contain an error/success code for the operation
+EXPORT_SYMBOL int bitcoinconsensus_script_execution_start(bitcoinconsensus_script_execution_t *, unsigned int flags, bitcoinconsensus_error*);
+
+/// Executes a single instruction of the script.
+/// When successful, fEof will be assigned to false if there is more to do, or true when the script has completed.
+/// If not NULL, err will contain an error/success code for the operation
+EXPORT_SYMBOL int bitcoinconsensus_script_execution_step(bitcoinconsensus_script_execution_t *, int *fEof, bitcoinconsensus_error*);
+
+/// Immediately terminates the execution.
+/// Unless bitcoinconsensus_script_execution_start returned true, this will crash your program.
+/// If not NULL, err will contain an error/success code for the operation
+EXPORT_SYMBOL int bitcoinconsensus_script_execution_terminate(bitcoinconsensus_script_execution_t *, bitcoinconsensus_error*);
+
+/// Returns the current execution offset in the script.
+/// If not NULL, err will contain an error/success code for the operation
+EXPORT_SYMBOL int bitcoinconsensus_script_execution_get_pc(bitcoinconsensus_script_execution_t *, bitcoinconsensus_error*);
+
+/// Returns the position in the script following the last OP_CODESEPARATOR executed, or zero if none have executed.
+/// If not NULL, err will contain an error/success code for the operation
+EXPORT_SYMBOL int bitcoinconsensus_script_execution_get_codehash_pos(bitcoinconsensus_script_execution_t *, bitcoinconsensus_error*);
+
+/// Returns the number of "sigops" executed thus far.
+/// If not NULL, err will contain an error/success code for the operation
+EXPORT_SYMBOL int bitcoinconsensus_script_execution_get_sigop_count(bitcoinconsensus_script_execution_t *, bitcoinconsensus_error*);
 
 /// Returns 1 if the input nIn of the serialized transaction pointed to by
 /// txTo correctly spends the scriptPubKey pointed to by scriptPubKey under
