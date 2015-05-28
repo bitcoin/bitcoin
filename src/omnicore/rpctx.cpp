33166162d259ae1669730f42b4d34b7cf4c50815
dexX7 <dexx@bitwatch.co>
2015-05-28 17:57:18 +0200

    Handle special cases of RPC input data

--- src/omnicore/rpctx.cpp
+++ src/omnicore/rpctx.cpp
@@ -65,7 +65,7 @@ Value send_OMNI(const Array& params, bool fHelp)
     std::string toAddress = ParseAddress(params[1]);
     uint32_t propertyId = ParsePropertyId(params[2]);
     int64_t amount = ParseAmount(params[3], isPropertyDivisible(propertyId));
-    std::string redeemAddress = (params.size() > 4) ? ParseAddress(params[4]): "";
+    std::string redeemAddress = (params.size() > 4 && !ParseText(params[4]).empty()) ? ParseAddress(params[4]): "";
     int64_t referenceAmount = (params.size() > 5) ? ParseAmount(params[5], true): 0;
 
     // perform checks
@@ -451,7 +451,7 @@ Value sendsto_OMNI(const Array& params, bool fHelp)
     std::string fromAddress = ParseAddress(params[0]);
     uint32_t propertyId = ParsePropertyId(params[1]);
     int64_t amount = ParseAmount(params[2], isPropertyDivisible(propertyId));
-    std::string redeemAddress = (params.size() > 3) ? ParseAddress(params[3]): "";
+    std::string redeemAddress = (params.size() > 3 && !ParseText(params[3]).empty()) ? ParseAddress(params[3]): "";
 
     // perform checks
     RequireBalance(fromAddress, propertyId, amount);
@@ -499,7 +499,7 @@ Value sendgrant_OMNI(const Array& params, bool fHelp)
 
     // obtain parameters & info
     std::string fromAddress = ParseAddress(params[0]);
-    std::string toAddress = ParseText(params[1]).size() ? ParseAddress(params[1]): "";
+    std::string toAddress = !ParseText(params[1]).empty() ? ParseAddress(params[1]): "";
     uint32_t propertyId = ParsePropertyId(params[2]);
     int64_t amount = ParseAmount(params[3], isPropertyDivisible(propertyId));
     std::string memo = (params.size() > 4) ? ParseText(params[4]): "";
@@ -870,7 +870,7 @@ Value sendcancelalltrades_OMNI(const Array& params, bool fHelp)
 
     // obtain parameters & info
     std::string fromAddress = ParseAddress(params[0]);
-    uint8_t ecosystem = ParseEcosystem(params[1]);
+    uint8_t ecosystem = (params[1].get_int() != 0) ? ParseEcosystem(params[1]): 0;
 
     // perform checks
     // TODO: check, if there are matching offers to cancel
