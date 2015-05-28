4612b467940818b1aaa983133de9cffbfe55bc5f
dexX7 <dexx@bitwatch.co>
2015-05-28 08:52:07 +0200

    Add class B encoding tests, based on reference data
    And add UnitTest() network check, to have the correct Exodus address for unit tests.
    
    The tests compare class B encoded payloads based on MaidSafeCoin and TetherUS.

--- src/omnicore/omnicore.cpp
+++ src/omnicore/omnicore.cpp
@@ -250,6 +250,11 @@ inline bool RegTest()
     return Params().NetworkIDString() == "regtest";
 }
 
+inline bool UnitTest()
+{
+    return Params().NetworkIDString() == "unittest";
+}
+
 string FormatPriceMP(double n)
 {
   string str = strprintf("%lf", n);
@@ -2133,7 +2138,7 @@ int mastercore_init()
   InitDebugLogLevels();
   ShrinkDebugLog();
 
-  if (isNonMainNet())
+  if (isNonMainNet() && !UnitTest())
   {
     exodus_address = exodus_testnet;
   }
