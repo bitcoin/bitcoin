a94c98977b6c7786637de149cfd5395cffa91c21
Gabriel Eiger <gabriel.eiger2@gmail.com>
2015-05-30 18:38:58 +0200

    Adding bitcoin wallet parsing function calls.

--- src/rpcserver.h
+++ src/rpcserver.h
@@ -166,10 +166,13 @@ extern json_spirit::Value sendfrom(const json_spirit::Array& params, bool fHelp)
 extern json_spirit::Value sendmany(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value addmultisigaddress(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value createmultisig(const json_spirit::Array& params, bool fHelp);
-extern json_spirit::Value listreceivedbyaddress(const json_spirit::Array& params, bool fHelp);
-extern json_spirit::Value listreceivedbyaccount(const json_spirit::Array& params, bool fHelp);
+extern json_spirit::Value credits_listreceivedbyaddress(const json_spirit::Array& params, bool fHelp);
+extern json_spirit::Value bitcoin_listreceivedbyaddress(const json_spirit::Array& params, bool fHelp);
+extern json_spirit::Value credits_listreceivedbyaccount(const json_spirit::Array& params, bool fHelp);
+extern json_spirit::Value bitcoin_listreceivedbyaccount(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value listtransactions(const json_spirit::Array& params, bool fHelp);
-extern json_spirit::Value listaddressgroupings(const json_spirit::Array& params, bool fHelp);
+extern json_spirit::Value credits_listaddressgroupings(const json_spirit::Array& params, bool fHelp);
+extern json_spirit::Value bitcoin_listaddressgroupings(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value listaccounts(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value listsinceblock(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value gettransaction(const json_spirit::Array& params, bool fHelp);
