f559c1847727ce84c1e0df3535257e8068b081b8
Wladimir J. van der Laan <laanwj@gmail.com>
2015-06-04 10:31:22 +0200

    Simplify RPCclient, adapt json_parse_error test
    # Conflicts:
    #	src/test/rpc_tests.cpp

--- src/rpcclient.h
+++ src/rpcclient.h
@@ -9,5 +9,9 @@
 #include "univalue/univalue.h"
 
 UniValue RPCConvertValues(const std::string& strMethod, const std::vector<std::string>& strParams);
+/** Non-RFC4627 JSON parser, accepts internal values (such as numbers, true, false, null)
+ * as well as objects and arrays.
+ */
+UniValue ParseNonRFCJSONValue(const std::string& strVal);
 
 #endif // BITCOIN_RPCCLIENT_H
