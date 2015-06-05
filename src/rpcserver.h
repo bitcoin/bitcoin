6c261701bc1fa7062b5c6da07e68a6e3ccc8d7df
Peter Todd <pete@petertodd.org>
2015-06-05 17:07:17 -0230

    Add getblockheader RPC call
    Alternative to getblock that works even when the block itself has been
    pruned, returning all available information.

--- src/rpcserver.h
+++ src/rpcserver.h
@@ -229,6 +229,7 @@ extern UniValue settxfee(const UniValue& params, bool fHelp);
 extern UniValue getmempoolinfo(const UniValue& params, bool fHelp);
 extern UniValue getrawmempool(const UniValue& params, bool fHelp);
 extern UniValue getblockhash(const UniValue& params, bool fHelp);
+extern UniValue getblockheader(const UniValue& params, bool fHelp);
 extern UniValue getblock(const UniValue& params, bool fHelp);
 extern UniValue gettxoutsetinfo(const UniValue& params, bool fHelp);
 extern UniValue gettxout(const UniValue& params, bool fHelp);
