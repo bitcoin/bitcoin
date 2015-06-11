907a425aa5b8fd90cf1d28215712a309e934b364
Matt Corallo <git@bluematt.me>
2015-06-11 00:57:50 -0700

    Add p2sh option to importaddress to import redeemScripts

--- src/rpcclient.cpp
+++ src/rpcclient.cpp
@@ -87,6 +87,7 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "lockunspent", 1 },
     { "importprivkey", 2 },
     { "importaddress", 2 },
+    { "importaddress", 3 },
     { "verifychain", 0 },
     { "verifychain", 1 },
     { "keypoolrefill", 0 },
