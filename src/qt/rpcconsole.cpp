e2125c176540e7ee3760a1819378790d99e6f20a
Jonas Schnelli <jonas.schnelli@include7.ch>
2015-06-21 10:44:48 +0200

    [Qt] bantable overhaul
    - some code cleanups
    - fix date formatting
    - reduce header includes

--- src/qt/rpcconsole.cpp
+++ src/qt/rpcconsole.cpp
@@ -356,10 +356,10 @@ void RPCConsole::setClientModel(ClientModel *model)
 
         // create context menu actions
         QAction* disconnectAction   = new QAction(tr("&Disconnect Node"), this);
-        QAction* banAction1h        = new QAction(tr("&Ban Node for 1 hour"), this);
-        QAction* banAction24h       = new QAction(tr("&Ban Node for 24 hours"), this);
-        QAction* banAction7d        = new QAction(tr("&Ban Node for 7 days"), this);
-        QAction* banAction365d      = new QAction(tr("&Ban Node for 1 year"), this);
+        QAction* banAction1h        = new QAction(tr("&Ban Node for") + " " + tr("&1 hour"), this);
+        QAction* banAction24h       = new QAction(tr("&Ban Node for") + " " + tr("&24 hours"), this);
+        QAction* banAction7d        = new QAction(tr("&Ban Node for") + " " + tr("&7 days"), this);
+        QAction* banAction365d      = new QAction(tr("&Ban Node for") + " " + tr("&1 year"), this);
 
         // create context menu
         peersTableContextMenu = new QMenu();
@@ -798,7 +798,8 @@ void RPCConsole::banSelectedNode(int bantime)
         SplitHostPort(nStr, port, addr);
 
         CNode::Ban(CNetAddr(addr), bantime);
-        bannedNode->CloseSocketDisconnect();
+        bannedNode->fDisconnect = true;
+
         clearSelectedNode();
         ui->banlistWidget->setVisible(true);
         ui->banHeading->setVisible(true);
@@ -830,6 +831,9 @@ void RPCConsole::clearSelectedNode()
 
 void RPCConsole::showOrHideBanTableIfRequired()
 {
+    if (!clientModel)
+        return;
+    
     bool visible = clientModel->getBanTableModel()->shouldShow();
     ui->banlistWidget->setVisible(visible);
     ui->banHeading->setVisible(visible);
