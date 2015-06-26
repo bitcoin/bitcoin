5c1fd6e1abe9bf16f174c5cee2b449d099937dd0
zathras-crypto <zathrasc@gmail.com>
2015-06-26 16:18:18 +1000

    UI: MetaDEx dialog - listen for BTC balance changes & update fee warning

--- src/qt/metadexdialog.cpp
+++ src/qt/metadexdialog.cpp
@@ -114,8 +114,11 @@ void MetaDExDialog::setClientModel(ClientModel *model)
 
 void MetaDExDialog::setWalletModel(WalletModel *model)
 {
+    // use wallet model to get visibility into BTC balance changes for fees
     this->walletModel = model;
-    if (model != NULL) { } // do nothing, signals from walletModel no longer needed
+    if (model != NULL) {
+       connect(model, SIGNAL(balanceChanged(CAmount,CAmount,CAmount,CAmount,CAmount,CAmount)), this, SLOT(UpdateBalances()));
+    }
 }
 
 void MetaDExDialog::OrderRefresh()
