4cac8a533306d7e84138bd1fcd17da735b0e7c25
Alex Morcos <morcos@chaincode.com>
2015-06-30 11:14:24 -0400

    Modify variable names for entry height and priority

--- src/txmempool.cpp
+++ src/txmempool.cpp
@@ -19,9 +19,9 @@
 using namespace std;
 
 CTxMemPoolEntry::CTxMemPoolEntry(const CTransaction& _tx, const CAmount& _nFee,
-                                 int64_t _nTime, double _dPriority,
-                                 unsigned int _nHeight, bool poolHasNoInputsOf):
-    tx(_tx), nFee(_nFee), nTime(_nTime), dPriority(_dPriority), nHeight(_nHeight),
+                                 int64_t _nTime, double _entryPriority,
+                                 unsigned int _entryHeight, bool poolHasNoInputsOf):
+    tx(_tx), nFee(_nFee), nTime(_nTime), entryPriority(_entryPriority), entryHeight(_entryHeight),
     hadNoDependencies(poolHasNoInputsOf)
 {
     nTxSize = ::GetSerializeSize(tx, SER_NETWORK, PROTOCOL_VERSION);
@@ -42,8 +42,8 @@ double
 CTxMemPoolEntry::GetPriority(unsigned int currentHeight) const
 {
     CAmount nValueIn = tx.GetValueOut()+nFee;
-    double deltaPriority = ((double)(currentHeight-nHeight)*nValueIn)/nModSize;
-    double dResult = dPriority + deltaPriority;
+    double deltaPriority = ((double)(currentHeight-entryHeight)*nValueIn)/nModSize;
+    double dResult = entryPriority + deltaPriority;
     return dResult;
 }
 
