9ac33eca90f279f3ac30bc5a9f3774691ab7ed3b
Evan Duffield <evan@dashpay.io>
2015-07-02 09:53:55 -0700

    added checkpoint

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -84,6 +84,7 @@ static const Checkpoints::CCheckpointData data = {
 static Checkpoints::MapCheckpoints mapCheckpointsTestnet =
         boost::assign::map_list_of
         ( 261, uint256("00000c26026d0815a7e2ce4fa270775f61403c040647ff2c3091f99e894a4618"))
+        ( 77100, uint256("00000133e07bae4d3553fd8d86cb5a961d638cae72d179ede8ca436c60a2c3c0"))
         ;
 static const Checkpoints::CCheckpointData dataTestnet = {
         &mapCheckpointsTestnet,
