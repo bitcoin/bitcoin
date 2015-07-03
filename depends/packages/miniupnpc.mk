ca7a347c56b39a20eb4c2aa8e3dc116f1c7f52f1
Cory Fields <cory-nospam-@coryfields.com>
2015-07-03 12:56:17 -0400

    depends: bump miniupnpc to 1.9.20150609

--- depends/packages/miniupnpc.mk
+++ depends/packages/miniupnpc.mk
@@ -1,8 +1,8 @@
 package=miniupnpc
-$(package)_version=1.9.20140701
+$(package)_version=1.9.20150609
 $(package)_download_path=http://miniupnp.free.fr/files
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=26f3985bad7768b8483b793448ae49414cdc4451d0ec83e7c1944367e15f9f07
+$(package)_sha256_hash=86e6ccec5b660ba6889893d1f3fca21db087c6466b1a90f495a1f87ab1cd1c36
 
 define $(package)_set_vars
 $(package)_build_opts=CC="$($(package)_cc)"
