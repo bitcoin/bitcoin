b43da5ccba2104dc621c4ea3c8b6a30cbcb6ddbf
dexX7 <dexx@bitwatch.co>
2015-07-06 00:37:17 +0200

    Add first test for hardcoded block height restrictions

---
+++ src/omnicore/test/rules_txs_tests.cpp
@@ -0,0 +1,34 @@
+#include "omnicore/omnicore.h"
+#include "omnicore/rules.h"
+
+#include <stdint.h>
+#include <limits>
+
+#include <boost/test/unit_test.hpp>
+
+using namespace mastercore;
+
+BOOST_AUTO_TEST_SUITE(omnicore_rules_txs_tests)
+
+const int MAX_BLOCK = std::numeric_limits<int>().max();
+const int MAX_VERSION = std::numeric_limits<uint16_t>().max();
+
+BOOST_AUTO_TEST_CASE(simple_send_restrictions)
+{
+    BOOST_CHECK(!IsTransactionTypeAllowed(0,               OMNI_PROPERTY_BTC,  MSC_TYPE_SIMPLE_SEND, MP_TX_PKT_V0));
+    BOOST_CHECK(!IsTransactionTypeAllowed(GENESIS_BLOCK,   OMNI_PROPERTY_BTC,  MSC_TYPE_SIMPLE_SEND, MP_TX_PKT_V0));
+    BOOST_CHECK(!IsTransactionTypeAllowed(MAX_BLOCK,       OMNI_PROPERTY_BTC,  MSC_TYPE_SIMPLE_SEND, MAX_VERSION));
+    BOOST_CHECK(!IsTransactionTypeAllowed(0,               OMNI_PROPERTY_MSC,  MSC_TYPE_SIMPLE_SEND, MAX_VERSION));
+    BOOST_CHECK(!IsTransactionTypeAllowed(GENESIS_BLOCK-1, OMNI_PROPERTY_MSC,  MSC_TYPE_SIMPLE_SEND, MP_TX_PKT_V0));
+    BOOST_CHECK(!IsTransactionTypeAllowed(MAX_BLOCK,       OMNI_PROPERTY_MSC,  MSC_TYPE_SIMPLE_SEND, MP_TX_PKT_V1));
+    BOOST_CHECK(!IsTransactionTypeAllowed(GENESIS_BLOCK,   OMNI_PROPERTY_TMSC, MSC_TYPE_SIMPLE_SEND, MAX_VERSION));
+    BOOST_CHECK(!IsTransactionTypeAllowed(MAX_BLOCK,       OMNI_PROPERTY_TMSC, MSC_TYPE_SIMPLE_SEND, MP_TX_PKT_V1));
+
+    BOOST_CHECK(IsTransactionTypeAllowed(GENESIS_BLOCK,    OMNI_PROPERTY_MSC,  MSC_TYPE_SIMPLE_SEND, MP_TX_PKT_V0));
+    BOOST_CHECK(IsTransactionTypeAllowed(MAX_BLOCK,        OMNI_PROPERTY_MSC,  MSC_TYPE_SIMPLE_SEND, MP_TX_PKT_V0));
+    BOOST_CHECK(IsTransactionTypeAllowed(0,                OMNI_PROPERTY_TMSC, MSC_TYPE_SIMPLE_SEND, MP_TX_PKT_V0));
+    BOOST_CHECK(IsTransactionTypeAllowed(MAX_BLOCK,        OMNI_PROPERTY_TMSC, MSC_TYPE_SIMPLE_SEND, MP_TX_PKT_V0));
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
