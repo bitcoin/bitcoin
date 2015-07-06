cd7803eb1f3c933e4af67535d40dbeff902d96b6
dexX7 <dexx@bitwatch.co>
2015-07-06 22:06:46 +0200

    Leftovers of the rebranding to Omni Core

--- src/rpcserver.cpp
+++ src/rpcserver.cpp
@@ -1069,7 +1069,7 @@ json_spirit::Value CRPCTable::execute(const std::string &strMethod, const json_s
 }
 
 std::string HelpExampleCli(string methodname, string args){
-    return "> bitcoin-cli " + methodname + " " + args + "\n";
+    return "> omnicore-cli " + methodname + " " + args + "\n";
 }
 
 std::string HelpExampleRpc(string methodname, string args){
