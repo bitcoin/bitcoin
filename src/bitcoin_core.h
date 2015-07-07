7b041d6a11f3bb1853b271c187e6730f2f0262b2
Gabriel Eiger <gabriel.eiger2@gmail.com>
2015-07-08 01:20:16 +0200

    Compacting compressed block.

--- src/bitcoin_core.h
+++ src/bitcoin_core.h
@@ -184,6 +184,38 @@ public:
     }
 };
 
+/** wrapper for CTxOut that provides a more compact serialization */
+class Bitcoin_CTxOutCompressor
+{
+private:
+    Bitcoin_CTxOut &txout;
+
+public:
+    static uint64_t CompressAmount(uint64_t nAmount);
+    static uint64_t DecompressAmount(uint64_t nAmount);
+
+    Bitcoin_CTxOutCompressor(Bitcoin_CTxOut &txoutIn) : txout(txoutIn) { }
+
+    IMPLEMENT_SERIALIZE(({
+        if (!fRead) {
+            uint64_t nValOriginal = CompressAmount(txout.nValueOriginal);
+            READWRITE(VARINT(nValOriginal));
+            uint64_t nValClaimable = CompressAmount(txout.nValueClaimable);
+            READWRITE(VARINT(nValClaimable));
+        } else {
+            uint64_t nValOriginal = 0;
+            READWRITE(VARINT(nValOriginal));
+            txout.nValueOriginal = DecompressAmount(nValOriginal);
+            uint64_t nValClaimable = 0;
+            READWRITE(VARINT(nValClaimable));
+            txout.nValueClaimable = DecompressAmount(nValClaimable);
+        }
+        CScriptCompressor cscript(REF(txout.scriptPubKey));
+        READWRITE(cscript);
+        READWRITE(VARINT(txout.nValueOriginalHasBeenSpent));
+    });)
+};
+
 /** The basic transaction that is broadcasted on the network and contained in
  * blocks.  A transaction can contain multiple inputs and outputs.
  */
@@ -270,7 +302,7 @@ public:
     int64_t valueOut;
     std::vector<COutPoint> vin;
     //Only holds info whether the out is spendable or not. 0 == unspendable, 1 == spendable
-    std::vector<int> voutSpendable;
+    std::vector<bool> voutSpendable;
 
     //Memory only
     bool isCoinBase;
@@ -288,7 +320,7 @@ public:
 			vin.push_back(tx.vin[j].prevout);
 		}
 		for (unsigned int j = 0; j < tx.vout.size(); j++) {
-			voutSpendable.push_back(tx.vout[j].scriptPubKey.IsUnspendable() ? 0 : 1);
+			voutSpendable.push_back(!tx.vout[j].scriptPubKey.IsUnspendable());
 			vout.push_back(tx.vout[j]);
 		}
 		isCreatedFromBlock = true;
@@ -299,15 +331,67 @@ public:
         SetNull();
     }
 
-    IMPLEMENT_SERIALIZE
-    (
-        READWRITE(this->nVersion);
+    unsigned int GetSerializeSize(int nType, int nVersion) const {
+        unsigned int nSize = 0;
+        // version
+        nSize += ::GetSerializeSize(VARINT(this->nVersion), nType, nVersion);
         nVersion = this->nVersion;
-        READWRITE(txHash);
-        READWRITE(valueOut);
-        READWRITE(vin);
-        READWRITE(voutSpendable);
-    )
+        nSize += ::GetSerializeSize(txHash, nType, nVersion);
+        const uint64_t nValOut = Bitcoin_CTxOutCompressor::CompressAmount(valueOut);
+        nSize += ::GetSerializeSize(VARINT(nValOut), nType, nVersion);
+        nSize += ::GetSerializeSize(vin, nType, nVersion);
+    	//Size indicator
+    	unsigned int nSpendableSize = voutSpendable.size();
+        nSize += ::GetSerializeSize(VARINT(nSpendableSize), nType, nVersion);
+        //Bit array of spentness
+        nSize += nSpendableSize;
+        return nSize;
+    }
+
+    template<typename Stream>
+    void Serialize(Stream &s, int nType, int nVersion) const {
+        // version
+        ::Serialize(s, VARINT(this->nVersion), nType, nVersion);
+        nVersion = this->nVersion;
+        ::Serialize(s, txHash, nType, nVersion);
+        const uint64_t nValOut = Bitcoin_CTxOutCompressor::CompressAmount(valueOut);
+        ::Serialize(s, VARINT(nValOut), nType, nVersion);
+        ::Serialize(s, vin, nType, nVersion);
+    	//Size indicator
+    	unsigned int nSpendableSize = voutSpendable.size();
+        ::Serialize(s, VARINT(nSpendableSize), nType, nVersion);
+        //Bit array of spentness
+        for (unsigned int b = 0; b*8 < nSpendableSize; b++) {
+            unsigned char output = 0;
+            for (unsigned int i = 0; i < 8 && b*8+i < nSpendableSize; i++) {
+            	output |= (voutSpendable[b*8+i] << i);
+            }
+            ::Serialize(s, output, nType, nVersion);
+        }
+    }
+
+    template<typename Stream>
+    void Unserialize(Stream &s, int nType, int nVersion) {
+        // version
+        ::Unserialize(s, VARINT(this->nVersion), nType, nVersion);
+        nVersion = this->nVersion;
+        ::Unserialize(s, txHash, nType, nVersion);
+        uint64_t nValOut = 0;
+        ::Unserialize(s, VARINT(nValOut), nType, nVersion);
+        valueOut = Bitcoin_CTxOutCompressor::DecompressAmount(nValOut);
+        ::Unserialize(s, vin, nType, nVersion);
+    	//Size indicator
+    	unsigned int nSpendableSize = 0;
+        ::Unserialize(s, VARINT(nSpendableSize), nType, nVersion);
+        //Bit array of spentness
+        for (unsigned int b = 0; b*8 < nSpendableSize; b++) {
+            unsigned char input = 0;
+            ::Unserialize(s, input, nType, nVersion);
+		   for(int i = 0; i < 8 && b*8+i < nSpendableSize; i++) {
+			   voutSpendable.push_back(input & (1 << i));
+		   }
+        }
+    }
 
     void SetNull()
     {
@@ -338,45 +422,13 @@ public:
     }
 
     bool HasSpendable() const {
-        BOOST_FOREACH(const int& spendable, voutSpendable) {
-        	if(spendable) {
-        		return true;
-        	}
-        }
+    	for (unsigned int i = 0; i < voutSpendable.size() ; i++) {
+    		if(voutSpendable[i]) {
+    			return true;
+    		}
+    	}
         return false;
     }
-};
-
-/** wrapper for CTxOut that provides a more compact serialization */
-class Bitcoin_CTxOutCompressor
-{
-private:
-    Bitcoin_CTxOut &txout;
-
-public:
-    static uint64_t CompressAmount(uint64_t nAmount);
-    static uint64_t DecompressAmount(uint64_t nAmount);
-
-    Bitcoin_CTxOutCompressor(Bitcoin_CTxOut &txoutIn) : txout(txoutIn) { }
-
-    IMPLEMENT_SERIALIZE(({
-        if (!fRead) {
-            uint64_t nValOriginal = CompressAmount(txout.nValueOriginal);
-            READWRITE(VARINT(nValOriginal));
-            uint64_t nValClaimable = CompressAmount(txout.nValueClaimable);
-            READWRITE(VARINT(nValClaimable));
-        } else {
-            uint64_t nValOriginal = 0;
-            READWRITE(VARINT(nValOriginal));
-            txout.nValueOriginal = DecompressAmount(nValOriginal);
-            uint64_t nValClaimable = 0;
-            READWRITE(VARINT(nValClaimable));
-            txout.nValueClaimable = DecompressAmount(nValClaimable);
-        }
-        CScriptCompressor cscript(REF(txout.scriptPubKey));
-        READWRITE(cscript);
-        READWRITE(VARINT(txout.nValueOriginalHasBeenSpent));
-    });)
 };
 
 
