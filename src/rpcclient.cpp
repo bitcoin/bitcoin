15c47fd40604332af841c8b5fb916db39dad5e90
Matt Corallo <git@bluematt.me>
2015-07-09 22:47:36 -0700

    Add importpubkey method to import a watch-only pubkey

--- src/rpcclient.cpp
+++ src/rpcclient.cpp
@@ -88,6 +88,7 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "importprivkey", 2 },
     { "importaddress", 2 },
     { "importaddress", 3 },
+    { "importpubkey", 2 },
     { "verifychain", 0 },
     { "verifychain", 1 },
     { "keypoolrefill", 0 },
