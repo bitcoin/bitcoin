e5f02000a1ec5c89e57e2394e66c62c36e4510dc
Peter Todd <pete@petertodd.org>
2015-07-14 02:49:31 -0400

    Revert "Put RBF in subversion string"
    This reverts commit 3e9e86029a36b8b4574494b21098b16e8b65df16.
    
    By request from Luke-Jr

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -13,7 +13,7 @@
  * for both bitcoind and bitcoin-core, to make it harder for attackers to
  * target servers or GUI users specifically.
  */
-const std::string CLIENT_NAME("Satoshi RBF");
+const std::string CLIENT_NAME("Satoshi");
 
 /**
  * Client version number
