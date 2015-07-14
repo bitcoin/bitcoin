e57f2d40037a3ce5391bed09c2bae1862651826a
Evan Duffield <evan@dashpay.io>
2015-07-14 07:05:33 -0700

    Check IsValid when preparing fee

--- src/rpcmasternode-budget.cpp
+++ src/rpcmasternode-budget.cpp
@@ -93,6 +93,11 @@ Value mnbudget(const Array& params, bool fHelp)
         //*************************************************************************
 
         CBudgetProposalBroadcast budgetProposalBroadcast(strProposalName, strURL, nPaymentCount, scriptPubKey, nAmount, nBlockStart, 0);
+
+        std::string strError = "";
+        if(!budgetProposalBroadcast.IsValid(strError))
+            return "Proposal is not valid - " + budgetProposalBroadcast.GetHash().ToString() + " - " + strError;
+
         std::string cmd = "tx";
         bool useIX = true;
 
