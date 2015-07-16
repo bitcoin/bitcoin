55f6dd6f7bebb84b06b0af45f5cbeaf27e000262
Evan Duffield <evan@dashpay.io>
2015-07-15 19:18:19 -0700

    version / proto bump

--- src/instantx.h
+++ src/instantx.h
@@ -27,7 +27,7 @@ class CConsensusVote;
 class CTransaction;
 class CTransactionLock;
 
-static const int MIN_INSTANTX_PROTO_VERSION = 70090;
+static const int MIN_INSTANTX_PROTO_VERSION = 70091;
 
 extern map<uint256, CTransaction> mapTxLockReq;
 extern map<uint256, CTransaction> mapTxLockReqRejected;
