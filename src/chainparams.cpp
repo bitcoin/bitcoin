6bd98b4644d417e84a0fdea9a4ea667bdf5fa15f
Andreas Schildbach <andreas@schildbach.de>
2015-07-16 10:28:23 +0200

    Remove testnet-seed.bitcoin.schildbach.de.
    My provider was constantly complaining about unroutable traffic to port 18333 and finally taking down my server. I was running sipa's crawler, modified to add zonefile support.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -158,7 +158,6 @@ public:
         vSeeds.push_back(CDNSSeedData("alexykot.me", "testnet-seed.alexykot.me"));
         vSeeds.push_back(CDNSSeedData("bitcoin.petertodd.org", "testnet-seed.bitcoin.petertodd.org"));
         vSeeds.push_back(CDNSSeedData("bluematt.me", "testnet-seed.bluematt.me"));
-        vSeeds.push_back(CDNSSeedData("bitcoin.schildbach.de", "testnet-seed.bitcoin.schildbach.de"));
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,111);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,196);
