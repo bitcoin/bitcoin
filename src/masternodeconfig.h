3c8b3355948da9e36b3d876185f34f107d534826
Evan Duffield <evan@dashpay.io>
2015-07-16 03:00:16 -0700

    Force txindex when a Mastenode

--- src/masternodeconfig.h
+++ src/masternodeconfig.h
@@ -91,6 +91,14 @@ public:
         return entries;
     }
 
+    int getCount() {
+        int c = -1;
+        BOOST_FOREACH(CMasternodeEntry e, entries) {
+            if(e.getAlias() != "") c++;
+        }
+        return c;
+    }
+
 private:
     std::vector<CMasternodeEntry> entries;
 
