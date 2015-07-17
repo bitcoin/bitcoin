8767d463e2626a9233e0f7dc7a387bebcc77d205
Daniel Kraft <d@domob.eu>
2015-07-17 17:10:26 +0200

    Fix getauxblock and add testing script.

--- src/miner.cpp
+++ src/miner.cpp
@@ -465,15 +465,12 @@ CBlockTemplate* CreateNewBlockWithKey(CReserveKey& reservekey)
 
 bool CheckWork(CBlock* pblock, CWallet& wallet, CReserveKey& reservekey)
 {
-    uint256 hash = pblock->GetHash();
-    uint256 hashTarget = CBigNum().SetCompact(pblock->nBits).getuint256();
-
-    if (hash > hashTarget)
+    if (!CheckProofOfWork(*pblock))
         return false;
 
     //// debug print
     LogPrintf("CrowncoinMiner:\n");
-    LogPrintf("proof-of-work found  \n  hash: %s  \ntarget: %s\n", hash.GetHex(), hashTarget.GetHex());
+    LogPrintf("proof-of-work found\n");
     pblock->print();
     LogPrintf("generated %s\n", FormatMoney(pblock->vtx[0].vout[0].nValue));
 
