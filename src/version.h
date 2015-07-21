8d7cec5c5da95946336d945fa71f46dd31922b20
Evan Duffield <evan@dashpay.io>
2015-07-21 08:09:17 -0700

    Dont accept MNW from older masternodes

--- src/version.h
+++ src/version.h
@@ -27,6 +27,9 @@ static const int MIN_POOL_PEER_PROTO_VERSION = 70093;
 //! minimum peer version for masternode budgets
 static const int MIN_BUDGET_PEER_PROTO_VERSION = 70093;
 
+//! minimum peer version for masternode winner broadcasts
+static const int MIN_MNW_PEER_PROTO_VERSION = 70093;
+
 //! minimum peer version that can receive masternode payments
 // V1 - Last protocol version before update
 // V2 - Newest protocol version
