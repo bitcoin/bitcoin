c6844c901ac349186ddbc1a17990dc0b068efb9a
Pavel Janík <Pavel@Janik.cz>
2015-07-23 22:48:07 +0200

    Add removeaddress RPC call (remove watch-only address)

--- src/rpcserver.cpp
+++ src/rpcserver.cpp
@@ -370,6 +370,7 @@ static const CRPCCommand vRPCCommands[] =
     { "wallet",             "listunspent",            &listunspent,            false },
     { "wallet",             "lockunspent",            &lockunspent,            true  },
     { "wallet",             "move",                   &movecmd,                false },
+    { "wallet",             "removeaddress",          &removeaddress,          true  },
     { "wallet",             "sendfrom",               &sendfrom,               false },
     { "wallet",             "sendmany",               &sendmany,               false },
     { "wallet",             "sendtoaddress",          &sendtoaddress,          false },
