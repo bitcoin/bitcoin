6e182686163ce3c15b878bd78c41d8d18db344f1
Pieter Wuille <pieter.wuille@gmail.com>
2015-07-28 20:11:20 +0200

    Switch to libsecp256k1-based validation for ECDSA

--- src/test/test_bitcoin.h
+++ src/test/test_bitcoin.h
@@ -3,6 +3,7 @@
 
 #include "chainparamsbase.h"
 #include "key.h"
+#include "pubkey.h"
 #include "txdb.h"
 
 #include <boost/filesystem.hpp>
@@ -12,6 +13,8 @@
  * This just configures logging and chain parameters.
  */
 struct BasicTestingSetup {
+    ECCVerifyHandle globalVerifyHandle;
+
     BasicTestingSetup(const std::string& chainName = CBaseChainParams::MAIN);
     ~BasicTestingSetup();
 };
