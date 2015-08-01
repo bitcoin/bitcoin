3684b3f282555958ebb6013805f61bdd4ef656b8
Evan Duffield <evan@dashpay.io>
2015-08-01 02:03:15 -0700

    draft

--- src/rpcmisc.cpp
+++ src/rpcmisc.cpp
@@ -185,6 +185,7 @@ Value spork(const Array& params, bool fHelp)
 
         //broadcast new spork
         if(sporkManager.UpdateSpork(nSporkID, nValue)){
+            ExecuteSpork(nSporkID, nValue);
             return "success";
         } else {
             return "failure";
