7a26c6b88edccfb771f5d8fa353b87012bc057ae
dexX7 <dexx@bitwatch.co>
2015-08-05 17:15:32 +0200

    Use ENABLE_WALLET to wrap wallet fetch functions
    And slightly refine the rest:
    
    - use const where possible
    - reorder functions

--- src/omnicore/fetchwallettx.h
+++ src/omnicore/fetchwallettx.h
@@ -3,21 +3,16 @@
 
 class uint256;
 
-//#include "util.h"
-#include "txdb.h"
-
-#include <stdint.h>
 #include <map>
 #include <string>
 
-/** Gets the byte offset of a transaction from the tx index
-  */
-uint64_t GetTransactionByteOffset(const uint256& txid);
+namespace mastercore
+{
+/** Gets the byte offset of a transaction from the transaction index. */
+unsigned int GetTransactionByteOffset(const uint256& txid);
 
-/**
-  * Returns an ordered list of Omni transactions that are relevant to the wallet
-  * Ignores order in the wallet (which can be skewed by watch addresses) and utilizes block height and position within block
-  */
-std::map<std::string,uint256> FetchWalletOmniTransactions(int count, int startBlock = 0, int endBlock = 999999);
+/** Returns an ordered list of Omni transactions that are relevant to the wallet. */
+std::map<std::string, uint256> FetchWalletOmniTransactions(unsigned int count, int startBlock = 0, int endBlock = 999999);
+}
 
 #endif // OMNICORE_FETCHWALLETTX_H
