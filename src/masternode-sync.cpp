7198b8d1bbb3a6ed9f200f956557df8532d55dc8
Evan Duffield <evan@dashpay.io>
2015-08-11 14:54:42 -0700

    Move superblock check

--- src/masternode-sync.cpp
+++ src/masternode-sync.cpp
@@ -30,8 +30,8 @@ bool CMasternodeSync::IsBlockchainSynced()
     static bool fBlockchainSynced = false;
     static int64_t lastProcess = GetTime();
 
-    // if the last call to this function was more than 10 minutes ago (client was in sleep mode) reset the sync process
-    if(GetTime() - lastProcess > 60*10) {
+    // if the last call to this function was more than 60 minutes ago (client was in sleep mode) reset the sync process
+    if(GetTime() - lastProcess > 60*60) {
         Reset();
         fBlockchainSynced = false;
     }
