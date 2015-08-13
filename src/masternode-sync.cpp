f5acf2c5adc17b154382ca115fb2f61f0139507c
Evan Duffield <evan@dashpay.io>
2015-08-13 08:04:25 -0700

    fixed parameters for IsBlockChainSynced

--- src/masternode-sync.cpp
+++ src/masternode-sync.cpp
@@ -48,7 +48,7 @@ bool CMasternodeSync::IsBlockchainSynced()
     if(pindex == NULL) return false;
 
 
-    if(pindex->nTime + 600 < GetTime())
+    if(pindex->nTime + 60*60 < GetTime())
         return false;
 
     fBlockchainSynced = true;
