692b5c8bb05dacddbec88743b18aeaaeb0dc0b06
zathras-crypto <zathrasc@gmail.com>
2015-08-14 15:09:20 +1000

    Fix alert boost test cases

--- src/omnicore/test/create_payload_tests.cpp
+++ src/omnicore/test/create_payload_tests.cpp
@@ -381,19 +381,37 @@ BOOST_AUTO_TEST_CASE(payload_feature_activation)
     BOOST_CHECK_EQUAL(HexStr(vch), "fffffffe00010005a550000003e7");
 }
 
-/*
-BOOST_AUTO_TEST_CASE(payload_omnicore_alert)
+BOOST_AUTO_TEST_CASE(payload_omnicore_alert_block)
+{
+    // Omni Core client notification [type 65535, version 65535]
+    std::vector<unsigned char> vch = CreatePayload_OmniCoreAlert(
+        static_cast<int32_t>(1),            // alert target: by block number
+        static_cast<uint64_t>(300000),      // expiry value: 300000
+        static_cast<std::string>("test"));  // alert message: test
+
+    BOOST_CHECK_EQUAL(HexStr(vch), "ffffffff0001000493e07465737400");
+}
+
+BOOST_AUTO_TEST_CASE(payload_omnicore_alert_blockexpiry)
+{
+    // Omni Core client notification [type 65535, version 65535]
+    std::vector<unsigned char> vch = CreatePayload_OmniCoreAlert(
+        static_cast<int32_t>(2),            // alert target: by block time
+        static_cast<uint64_t>(1439528630),  // expiry value: 1439528630
+        static_cast<std::string>("test"));  // alert message: test
+
+    BOOST_CHECK_EQUAL(HexStr(vch), "ffffffff000255cd76b67465737400");
+}
+
+BOOST_AUTO_TEST_CASE(payload_omnicore_alert_minclient)
 {
     // Omni Core client notification [type 65535, version 65535]
     std::vector<unsigned char> vch = CreatePayload_OmniCoreAlert(
         static_cast<int32_t>(3),            // alert target: by client version
         static_cast<uint64_t>(900100),      // expiry value: v0.0.9.1
-        static_cast<uint16_t>(0),           // transaction type: none
-        static_cast<uint16_t>(0),           // transaction version: none
         static_cast<std::string>("test"));  // alert message: test
 
-    BOOST_CHECK_EQUAL(HexStr(vch), "ffffffff333a3930303130303a303a303a7465737400");
+    BOOST_CHECK_EQUAL(HexStr(vch), "ffffffff0003000dbc047465737400");
 }
-*/
 
 BOOST_AUTO_TEST_SUITE_END()
