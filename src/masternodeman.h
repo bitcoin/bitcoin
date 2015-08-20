6b31970f1301d1c3fa05b328aa3b2a89363634c0
Evan Duffield <evan@dashpay.io>
2015-08-20 08:36:44 -0700

    Fix payment cycle when network is in the process of updating

--- src/masternodeman.h
+++ src/masternodeman.h
@@ -116,7 +116,7 @@ public:
     CMasternode* Find(const CPubKey& pubKeyMasternode);
 
     /// Find an entry in the masternode list that is next to be paid
-    CMasternode* GetNextMasternodeInQueueForPayment(int nBlockHeight, bool fFilterSigTime=true);
+    CMasternode* GetNextMasternodeInQueueForPayment(int nBlockHeight, bool fFilterSigTime, int& nCount);
 
     /// Find a random entry
     CMasternode* FindRandomNotInVec(std::vector<CTxIn> &vecToExclude, int protocolVersion = -1);
