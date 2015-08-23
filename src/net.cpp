2ede6b7142cb57c422126722f845b36da7d3c4ef
Pavel Vasin <pavel@vasin.nl>
2015-08-23 23:53:49 +0300

    add support for miniupnpc api version 14
    The value of new arg ttl is set to 2 as it's recommended default.

--- src/net.cpp
+++ src/net.cpp
@@ -1020,10 +1020,14 @@ void ThreadMapPort()
 #ifndef UPNPDISCOVER_SUCCESS
     /* miniupnpc 1.5 */
     devlist = upnpDiscover(2000, multicastif, minissdpdpath, 0);
-#else
+#elif MINIUPNPC_API_VERSION < 14
     /* miniupnpc 1.6 */
     int error = 0;
     devlist = upnpDiscover(2000, multicastif, minissdpdpath, 0, 0, &error);
+#else
+    /* miniupnpc 1.9.20150730 */
+    int error = 0;
+    devlist = upnpDiscover(2000, multicastif, minissdpdpath, 0, 0, 2, &error);
 #endif
 
     struct UPNPUrls urls;
