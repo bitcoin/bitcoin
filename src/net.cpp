d526ece0e1d2b859c0446b78786960c683fe01e3
J Ross Nicoll <jrn@jrn.me.uk>
2015-08-29 17:40:13 +0100

    Make sure LogPrint strings are line-terminated

--- src/net.cpp
+++ src/net.cpp
@@ -659,7 +659,7 @@ bool CNode::ReceiveMsgBytes(const char *pch, unsigned int nBytes)
                 return false;
 
         if (msg.in_data && msg.hdr.nMessageSize > MAX_PROTOCOL_MESSAGE_LENGTH) {
-            LogPrint("net", "Oversized message from peer=%i, disconnecting", GetId());
+            LogPrint("net", "Oversized message from peer=%i, disconnecting\n", GetId());
             return false;
         }
 
