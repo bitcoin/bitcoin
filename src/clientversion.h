792e5b13118667b612a87bf1b2168f7b6d1f228f
CryptoManiac <balthazar@yandex.ru>
2015-09-02 20:48:03 +0300

    v0.5.3 snapshot

--- src/clientversion.h
+++ src/clientversion.h
@@ -9,7 +9,7 @@
 #define CLIENT_VERSION_MAJOR       0
 #define CLIENT_VERSION_MINOR       7
 #define CLIENT_VERSION_REVISION    6
-#define CLIENT_VERSION_BUILD       1
+#define CLIENT_VERSION_BUILD       2
 
 // Converts the parameter X to a string after macro replacement on X has been performed.
 // Don't merge these into one macro!
--- src/version.h
+++ src/version.h
@@ -51,6 +51,6 @@ static const int MEMPOOL_GD_VERSION = 60002;
 
 #define DISPLAY_VERSION_MAJOR       0
 #define DISPLAY_VERSION_MINOR       5
-#define DISPLAY_VERSION_REVISION    2
+#define DISPLAY_VERSION_REVISION    3
 
 #endif
