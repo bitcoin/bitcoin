6f60d4040b61488d185f0dbe104d15c6762ae991
svost <ya.nowa@yandex.ru>
2015-09-17 16:46:46 +0300

    Update novacoin.solution concerning to tiny ntp client update #5a73a5b

--- src/ntp.cpp
+++ src/ntp.cpp
@@ -1,9 +1,9 @@
-#include <sys/time.h>
 
 #ifdef WIN32
 #include <winsock2.h>
 #else
 #include <sys/socket.h>
+#include <sys/time.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
