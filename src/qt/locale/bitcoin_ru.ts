3a10f0777cbb244f0522d8281283cf78d0b8dae3
svost <ya.nowa@yandex.ru>
2015-09-18 22:40:11 +0300

    Fixed again - see pull request #155

--- src/qt/locale/bitcoin_ru.ts
+++ src/qt/locale/bitcoin_ru.ts
@@ -1,7 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ru_RU">
-<defaultcodec>UTF-8</defaultcodec>
+<TS version="2.1" language="ru_RU">
 <context>
     <name>AboutDialog</name>
     <message>
