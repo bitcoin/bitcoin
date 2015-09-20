2afa7bac87cd1b8cdcfb43fbc014ad90f596fc62
Jonas Schnelli <dev@jonasschnelli.ch>
2015-09-20 10:42:35 +0200

    [QT] dump banlist to disk in case of ban/unban over QT

--- src/qt/rpcconsole.cpp
+++ src/qt/rpcconsole.cpp
@@ -809,6 +809,7 @@ void RPCConsole::banSelectedNode(int bantime)
 
         CNode::Ban(CNetAddr(addr), BanReasonManuallyAdded, bantime);
         bannedNode->fDisconnect = true;
+        DumpBanlist();
 
         clearSelectedNode();
         clientModel->getBanTableModel()->refresh();
@@ -827,6 +828,7 @@ void RPCConsole::unbanSelectedNode()
     if (possibleSubnet.IsValid())
     {
         CNode::Unban(possibleSubnet);
+        DumpBanlist();
         clientModel->getBanTableModel()->refresh();
     }
 }
