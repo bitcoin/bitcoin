0b9d3d41f3a2a7fbcb26e80f7a10bc336800bd71
Matt Corallo <git@bluematt.me>
2015-10-02 14:19:55 -0700

    Implement on-the-fly mempool size limitation.
    After each transaction which is added to mempool, we first call
    Expire() to remove old transactions, then throwing away the
    lowest-feerate transactions.
    
    After throwing away transactions by feerate, we set the minimum
    relay fee to the maximum fee transaction-and-dependant-set we
    removed, plus the default minimum relay fee.
    
    After the next block is received, the minimum relay fee is allowed
    to decrease exponentially. Its halflife defaults to 12 hours, but
    is decreased to 6 hours if the mempool is smaller than half its
    maximum size, and 3 hours if the mempool is smaller than a quarter
    its maximum size.
    
    The minimum -maxmempool size is 40*-limitdescendantsize, as it is
    easy for an attacker to play games with the cheapest
    -limitdescendantsize transactions. -maxmempool defaults to 300MB.
    
    This disables high-priority transaction relay when the min relay
    fee adjustment is >0 (ie when the mempool is full). When the relay
    fee adjustment drops below the default minimum relay fee / 2 it is
    set to 0 (re-enabling priority-based free relay).

--- src/main.h
+++ src/main.h
@@ -51,6 +51,8 @@ static const unsigned int DEFAULT_ANCESTOR_SIZE_LIMIT = 900;
 static const unsigned int DEFAULT_DESCENDANT_LIMIT = 1000;
 /** Default for -limitdescendantsize, maximum kilobytes of in-mempool descendants */
 static const unsigned int DEFAULT_DESCENDANT_SIZE_LIMIT = 2500;
+/** Default for -maxmempool, maximum megabytes of mempool memory usage */
+static const unsigned int DEFAULT_MAX_MEMPOOL_SIZE = 300;
 /** Default for -mempoolexpiry, expiration time for mempool transactions in hours */
 static const unsigned int DEFAULT_MEMPOOL_EXPIRY = 72;
 /** The maximum size of a blk?????.dat file (since 0.8) */
