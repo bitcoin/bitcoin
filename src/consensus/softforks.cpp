bfbba3952feadb8643d74d4d3d2c887bb545ee34
Eric Lombrozo <elombrozo@gmail.com>
2015-10-07 09:26:49 -0400

    Created a SoftForks unit containing only IsSuperMajority() function.

---
+++ src/consensus/softforks.cpp
@@ -0,0 +1,25 @@
+// Copyright (c) 2009-2015 Eric Lombrozo, The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "chain.h"
+#include "consensus/params.h"
+#include "softforks.h"
+
+using namespace Consensus::SoftForks;
+
+/**
+ * Returns true if there are nRequired or more blocks of minVersion or above
+ * in the last Consensus::Params::nMajorityWindow blocks, starting at pstart and going backwards.
+ */
+bool Consensus::SoftForks::IsSuperMajority(int minVersion, const CBlockIndex* pstart, unsigned nRequired, const Consensus::Params& consensusParams)
+{
+    unsigned int nFound = 0;
+    for (int i = 0; i < consensusParams.nMajorityWindow && nFound < nRequired && pstart != NULL; i++)
+    {
+        if (pstart->nVersion >= minVersion)
+            ++nFound;
+        pstart = pstart->pprev;
+    }
+    return (nFound >= nRequired);
+}
