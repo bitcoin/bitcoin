004f051877bb926b5db0ddae790ed06159fdbae1
James O'Beirne <james.obeirne@gmail.com>
2015-10-10 22:41:19 -0700

    Add basic coverage reporting for RPC tests
    Thanks to @MarcoFalke @dexX7 @laanwj for review.

--- qa/rpc-tests/getblocktemplate_longpoll.py
+++ qa/rpc-tests/getblocktemplate_longpoll.py
@@ -38,7 +38,7 @@ class LongpollThread(threading.Thread):
         self.longpollid = templat['longpollid']
         # create a new connection to the node, we can't use the same
         # connection from two threads
-        self.node = AuthServiceProxy(node.url, timeout=600)
+        self.node = get_rpc_proxy(node.url, 1, timeout=600)
 
     def run(self):
         self.node.getblocktemplate({'longpollid':self.longpollid})
