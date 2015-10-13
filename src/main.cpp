17ec031caedf4870113ba66e7e8bd104bbbb4e32
Matt Corallo <git@bluematt.me>
2015-10-13 01:06:59 -0700

    Drop minRelayTxFee to 1000
    There is no exact science to setting this parameter, but 5000
    (just over 1 US cent at the time of writing) is higher than the
    cost to relay a transaction around the network (the new benchmark
    due to mempool limiting).

--- src/main.cpp
+++ src/main.cpp
@@ -75,7 +75,7 @@ uint64_t nPruneTarget = 0;
 bool fAlerts = DEFAULT_ALERTS;
 
 /** Fees smaller than this (in satoshi) are considered zero fee (for relaying and mining) */
-CFeeRate minRelayTxFee = CFeeRate(5000);
+CFeeRate minRelayTxFee = CFeeRate(1000);
 
 CTxMemPool mempool(::minRelayTxFee);
 
