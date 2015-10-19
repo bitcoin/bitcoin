e70bf468fd63138c2e0b69d6193a41b0e4a9cc6d
Suhas Daftuar <sdaftuar@gmail.com>
2015-10-19 12:43:38 -0400

    Rename CTxMemPool::remove -> removeRecursive
    remove is no longer called non-recursively, so simplify the logic
    and eliminate an unnecessary parameter

--- src/txmempool.h
+++ src/txmempool.h
@@ -428,7 +428,7 @@ public:
     bool addUnchecked(const uint256& hash, const CTxMemPoolEntry &entry, bool fCurrentEstimate = true);
     bool addUnchecked(const uint256& hash, const CTxMemPoolEntry &entry, setEntries &setAncestors, bool fCurrentEstimate = true);
 
-    void remove(const CTransaction &tx, std::list<CTransaction>& removed, bool fRecursive = false);
+    void removeRecursive(const CTransaction &tx, std::list<CTransaction>& removed);
     void removeForReorg(const CCoinsViewCache *pcoins, unsigned int nMemPoolHeight, int flags);
     void removeConflicts(const CTransaction &tx, std::list<CTransaction>& removed);
     void removeForBlock(const std::vector<CTransaction>& vtx, unsigned int nBlockHeight,
