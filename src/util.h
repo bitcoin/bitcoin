c87dbdee0c3fcb44b239170e314a0e41560252d0
Suhas Daftuar <sdaftuar@gmail.com>
2015-10-23 13:07:36 -0400

    Add option for microsecond precision in debug.log

--- src/util.h
+++ src/util.h
@@ -28,6 +28,8 @@
 #include <boost/signals2/signal.hpp>
 #include <boost/thread/exceptions.hpp>
 
+static const bool DEFAULT_LOGTIMEMICROS = false;
+
 /** Signals for translation. */
 class CTranslationInterface
 {
@@ -44,6 +46,7 @@ extern bool fPrintToDebugLog;
 extern bool fServer;
 extern std::string strMiscWarning;
 extern bool fLogTimestamps;
+extern bool fLogTimeMicros;
 extern bool fLogIPs;
 extern volatile bool fReopenDebugLog;
 extern CTranslationInterface translationInterface;
