8e4ce8ef2f795bbb4458f99f57926dd7de0b8e9e
Wladimir J. van der Laan <laanwj@gmail.com>
2015-10-23 08:31:27 +0200

    rpc: Add maxmempool and effective min fee to getmempoolinfo

--- src/rpcblockchain.cpp
+++ src/rpcblockchain.cpp
@@ -772,6 +772,9 @@ UniValue mempoolInfoToJSON()
     ret.push_back(Pair("size", (int64_t) mempool.size()));
     ret.push_back(Pair("bytes", (int64_t) mempool.GetTotalTxSize()));
     ret.push_back(Pair("usage", (int64_t) mempool.DynamicMemoryUsage()));
+    size_t maxmempool = GetArg("-maxmempool", DEFAULT_MAX_MEMPOOL_SIZE) * 1000000;
+    ret.push_back(Pair("maxmempool", (int64_t) maxmempool));
+    ret.push_back(Pair("mempoolminfee", ValueFromAmount(mempool.GetMinFee(maxmempool).GetFeePerK())));
 
     return ret;
 }
