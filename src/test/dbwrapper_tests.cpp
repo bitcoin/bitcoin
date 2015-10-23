f1ee4f5ea9ab952e3631da91c275534c4a2d9c22
Jeff Garzik <jeff@bloq.com>
2015-10-22 21:33:06 -0400

    leveldbwrapper file rename to dbwrapper.*

--- src/test/dbwrapper_tests.cpp
+++ src/test/dbwrapper_tests.cpp
@@ -2,7 +2,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include "leveldbwrapper.h"
+#include "dbwrapper.h"
 #include "uint256.h"
 #include "random.h"
 #include "test/test_bitcoin.h"
