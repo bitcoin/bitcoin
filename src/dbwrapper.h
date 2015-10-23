6858761a9b56117dce4f6781d028b18e1bf0fe9c
Jeff Garzik <jeff@bloq.com>
2015-10-22 21:33:06 -0400

    leveldbwrapper file rename to dbwrapper.*

--- src/dbwrapper.h
+++ src/dbwrapper.h
@@ -2,8 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_LEVELDBWRAPPER_H
-#define BITCOIN_LEVELDBWRAPPER_H
+#ifndef BITCOIN_DBWRAPPER_H
+#define BITCOIN_DBWRAPPER_H
 
 #include "clientversion.h"
 #include "serialize.h"
@@ -276,5 +276,5 @@ public:
 
 };
 
-#endif // BITCOIN_LEVELDBWRAPPER_H
+#endif // BITCOIN_DBWRAPPER_H
 
