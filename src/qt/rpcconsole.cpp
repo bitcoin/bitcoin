ba053de0d935f3078679227f307d786ace3ea056
crowning- <crowning-@users.noreply.github.com>
2015-10-25 20:54:46 +0100

    UI: show complete path to wallet.dat on Wallet-Repait tab (separator-fix)

--- src/qt/rpcconsole.cpp
+++ src/qt/rpcconsole.cpp
@@ -24,6 +24,7 @@
 #include <db_cxx.h>
 #endif
 
+#include <QDir>
 #include <QKeyEvent>
 #include <QScrollBar>
 #include <QThread>
@@ -244,9 +245,9 @@ RPCConsole::RPCConsole(QWidget *parent) :
     ui->openSSLVersion->setText(SSLeay_version(SSLEAY_VERSION));
 #ifdef ENABLE_WALLET
     ui->berkeleyDBVersion->setText(DbEnv::version(0, 0, 0));
-    std::string walletPath = "Wallet in use: " + GetDataDir().string();
-    walletPath += "/" + GetArg("-wallet", "wallet.dat");
-    ui->wallet_path->setText(QString::fromStdString(walletPath));
+    std::string walletPath = GetDataDir().string();
+    walletPath += "/" + GetArg("-wallet", "wallet.dat");   
+    ui->wallet_path->setText(QDir::toNativeSeparators(QString::fromStdString(walletPath)));
 #else
     ui->label_berkeleyDBVersion->hide();
     ui->berkeleyDBVersion->hide();
