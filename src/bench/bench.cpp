06b1daddedffe53463278f9290b81d77a6f82f0e
Philip Kaufmann <phil.kaufmann@t-online.de>
2015-10-27 17:44:13 +0100

    [Trivial] ensure minimal header conventions
    - ensure header namespaces and end comments are correct
    - add missing header end comments
    - ensure minimal formatting (add newlines etc.)

--- src/bench/bench.cpp
+++ src/bench/bench.cpp
@@ -1,7 +1,9 @@
 // Copyright (c) 2015 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #include "bench.h"
+
 #include <iostream>
 #include <sys/time.h>
 
