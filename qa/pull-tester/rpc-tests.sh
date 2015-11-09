312cf155cf7d94e70455fad4162eb1edae92d75a
Wladimir J. van der Laan <laanwj@gmail.com>
2015-11-09 08:40:46 +0100

    rpc-tests: Backport test of validateaddress with -disablewallet
    Github-Pull: #6970
    Rebased-From: 2980a18572dbe6173c41afc037b0cefe367d935c

--- qa/pull-tester/rpc-tests.sh
+++ qa/pull-tester/rpc-tests.sh
@@ -11,6 +11,7 @@ export BITCOIND=${REAL_BITCOIND}
 #Run the tests
 
 if [ "x${ENABLE_BITCOIND}${ENABLE_UTILS}${ENABLE_WALLET}" = "x111" ]; then
+  ${BUILDDIR}/qa/rpc-tests/disablewallet.py --srcdir "${BUILDDIR}/src"
   ${BUILDDIR}/qa/rpc-tests/wallet.py --srcdir "${BUILDDIR}/src"
   ${BUILDDIR}/qa/rpc-tests/listtransactions.py --srcdir "${BUILDDIR}/src"
   ${BUILDDIR}/qa/rpc-tests/mempool_resurrect_test.py --srcdir "${BUILDDIR}/src"
