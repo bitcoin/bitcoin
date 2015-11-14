1407c705ab94f71f9b059e052d3b3e544d66af44
MarcoFalke <falke.marco@gmail.com>
2015-11-14 21:44:35 +0100

    [qa] travis: cover *receivedby* rpcs
    Github-Pull: #7019
    Rebased-From: 33b7f83c593456c08d2be3edef785bb4a3850368

--- qa/pull-tester/rpc-tests.sh
+++ qa/pull-tester/rpc-tests.sh
@@ -14,6 +14,7 @@ if [ "x${ENABLE_BITCOIND}${ENABLE_UTILS}${ENABLE_WALLET}" = "x111" ]; then
   ${BUILDDIR}/qa/rpc-tests/disablewallet.py --srcdir "${BUILDDIR}/src"
   ${BUILDDIR}/qa/rpc-tests/wallet.py --srcdir "${BUILDDIR}/src"
   ${BUILDDIR}/qa/rpc-tests/listtransactions.py --srcdir "${BUILDDIR}/src"
+  ${BUILDDIR}/qa/rpc-tests/receivedby.py --srcdir "${BUILDDIR}/src"
   ${BUILDDIR}/qa/rpc-tests/mempool_resurrect_test.py --srcdir "${BUILDDIR}/src"
   ${BUILDDIR}/qa/rpc-tests/txn_doublespend.py --srcdir "${BUILDDIR}/src"
   ${BUILDDIR}/qa/rpc-tests/txn_doublespend.py --mineblock --srcdir "${BUILDDIR}/src"
