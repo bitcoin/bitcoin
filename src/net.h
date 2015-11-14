496fdf74eb104de3a8ecffadc6c5e7b636017468
Patick Strateman <patrick.strateman@gmail.com>
2015-11-14 05:12:31 -0800

    Fix fRelayTxs comment

--- src/net.h
+++ src/net.h
@@ -344,7 +344,7 @@ public:
     // We use fRelayTxes for two purposes -
     // a) it allows us to not relay tx invs before receiving the peer's version message
     // b) the peer may tell us in its version message that we should not relay tx invs
-    //    until it has initialized its bloom filter.
+    //    unless it loads a bloom filter.
     bool fRelayTxes;
     CSemaphoreGrant grantOutbound;
     CCriticalSection cs_filter;
