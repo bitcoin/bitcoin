40194dc84d3f7d3e9f09f459b040f74961f757fc
Patick Strateman <patrick.strateman@gmail.com>
2015-11-14 04:44:59 -0800

    Do not process tx inv's in blocksonly mode

--- src/main.cpp
+++ src/main.cpp
@@ -4218,7 +4218,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
             bool fAlreadyHave = AlreadyHave(inv);
             LogPrint("net", "got inv: %s  %s peer=%d\n", inv.ToString(), fAlreadyHave ? "have" : "new", pfrom->id);
 
-            if (!fAlreadyHave && !fImporting && !fReindex && inv.type != MSG_BLOCK)
+            if (!fAlreadyHave && !fImporting && !fReindex && inv.type != MSG_BLOCK && !GetBoolArg("-blocksonly", false))
                 pfrom->AskFor(inv);
 
             if (inv.type == MSG_BLOCK) {
