ee60e5625bf8a11c8e5509b9cea8b6465056c448
Pieter Wuille <pieter.wuille@gmail.com>
2015-11-17 17:35:40 +0100

    Add merkle.{h,cpp}, generic merkle root/branch algorithm

---
+++ src/consensus/merkle.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2015 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_MERKLE
+#define BITCOIN_MERKLE
+
+#include <stdint.h>
+#include <vector>
+
+#include "uint256.h"
+
+uint256 ComputeMerkleRoot(const std::vector<uint256>& leaves, bool* mutated = NULL);
+std::vector<uint256> ComputeMerkleBranch(const std::vector<uint256>& leaves, uint32_t position);
+uint256 ComputeMerkleRootFromBranch(const uint256& leaf, const std::vector<uint256>& branch, uint32_t position);
+
+#endif
