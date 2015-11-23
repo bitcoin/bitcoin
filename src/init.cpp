3c5d1d1b01caab7f620589c3d35f807c826130f2
MarcoFalke <falke.marco@gmail.com>
2015-11-23 18:55:26 +0100

    OpenSSL 1.1.0: Fix text variant of the version number
    Github-Pull: #7083
    Rebased-From: faf12bc2839d0a858b36c371aaf26902e49e380c

--- src/init.cpp
+++ src/init.cpp
@@ -778,7 +778,12 @@ bool AppInit2(boost::thread_group& threadGroup)
         ShrinkDebugFile();
     LogPrintf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
     LogPrintf("Bitcoin version %s (%s)\n", FormatFullVersion(), CLIENT_DATE);
+#if (OPENSSL_VERSION_NUMBER < 0x10100000L)
     LogPrintf("Using OpenSSL version %s\n", SSLeay_version(SSLEAY_VERSION));
+#else
+    LogPrintf("Using OpenSSL version %s\n", OpenSSL_version(OPENSSL_VERSION));
+#endif
+
 #ifdef ENABLE_WALLET
     LogPrintf("Using BerkeleyDB version %s\n", DbEnv::version(0, 0, 0));
 #endif
