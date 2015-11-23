8dcd29fad65f2eee798924ed07dbb74940e7e030
Gregory Maxwell <greg@xiph.org>
2015-11-23 01:54:23 +0000

    Limit setAskFor and retire requested entries only when a getdata returns.
    The setAskFor duplicate elimination was too eager and removed entries
     when we still had no getdata response, allowing the peer to keep
     INVing and not responding.

--- src/net.h
+++ src/net.h
@@ -58,6 +58,8 @@ static const bool DEFAULT_UPNP = false;
 #endif
 /** The maximum number of entries in mapAskFor */
 static const size_t MAPASKFOR_MAX_SZ = MAX_INV_SZ;
+/** The maximum number of entries in setAskFor (larger due to getdata latency)*/
+static const size_t SETASKFOR_MAX_SZ = 2 * MAX_INV_SZ;
 /** The maximum number of peer connections to maintain. */
 static const unsigned int DEFAULT_MAX_PEER_CONNECTIONS = 125;
 /** The default for -maxuploadtarget. 0 = Unlimited */
