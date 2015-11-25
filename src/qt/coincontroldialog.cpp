30497f1c32c4bb0b495eb9361522de2a50156bee
Jonas Schnelli <dev@jonasschnelli.ch>
2015-11-25 13:21:53 +0100

    [Qt] use ASYMP_UTF8 (≈) whenever we show a fee that is not absolute

--- src/qt/coincontroldialog.cpp
+++ src/qt/coincontroldialog.cpp
@@ -619,7 +619,7 @@ void CoinControlDialog::updateLabels(WalletModel *model, QDialog* dialog)
     l6->setText(sPriorityLabel);                                             // Priority
     l7->setText(fDust ? tr("yes") : tr("no"));                               // Dust
     l8->setText(BitcoinUnits::formatWithUnit(nDisplayUnit, nChange));        // Change
-    if (nPayFee > 0 && !(payTxFee.GetFeePerK() > 0 && fPayAtLeastCustomFee && nBytes < 1000))
+    if (nPayFee > 0 && (coinControl->nMinimumTotalFee < nPayFee))
     {
         l3->setText(ASYMP_UTF8 + l3->text());
         l4->setText(ASYMP_UTF8 + l4->text());
