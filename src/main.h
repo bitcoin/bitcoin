21e039cd684addc1b85acbad9f6c72b0ee2635ce
sidhujag <sidhujag@hotmail.com>
2015-12-04 13:27:49 -0800

    check level to 4

--- src/main.h
+++ src/main.h
@@ -146,7 +146,8 @@ extern uint64_t nPruneTarget;
 static const unsigned int MIN_BLOCKS_TO_KEEP = 288;
 
 static const signed int DEFAULT_CHECKBLOCKS = MIN_BLOCKS_TO_KEEP;
-static const unsigned int DEFAULT_CHECKLEVEL = 3;
+// SYSCOIN needs checklevel 4 by default to reconnect last syscoin blocks
+static const unsigned int DEFAULT_CHECKLEVEL = 4;
 
 // Require that user allocate at least 550MB for block & undo files (blk???.dat and rev???.dat)
 // At 1MB per block, 288 blocks = 288MB.
