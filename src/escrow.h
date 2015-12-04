3c4f8dc99ef031d500606f2a85ca002804298659
sidhujag <sidhujag@hotmail.com>
2015-12-04 13:04:52 -0800

    fix escrow inputs check

--- src/escrow.h
+++ src/escrow.h
@@ -13,7 +13,7 @@ class CCoinsViewCache;
 class CCoins;
 
 
-bool CheckEscrowInputs(const CTransaction &tx, CValidationState &state, const CCoinsViewCache &inputs, bool fBlock, bool fMiner, bool fJustCheck);
+bool CheckEscrowInputs(const CTransaction &tx, CValidationState &state, const CCoinsViewCache &inputs, bool fBlock, bool fMiner, bool fJustCheck, int nHeight);
 bool IsEscrowMine(const CTransaction& tx);
 bool DecodeEscrowTx(const CTransaction& tx, int& op, int& nOut, std::vector<std::vector<unsigned char> >& vvch, int nHeight);
 bool DecodeEscrowScript(const CScript& script, int& op, std::vector<std::vector<unsigned char> > &vvch);
