7d7e491e84e45d7e0793a3496d070e95b41b50e9
sidhujag <sidhujag@hotmail.com>
2015-12-03 17:51:26 -0800

    get gen

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -97,15 +97,17 @@ public:
 
         genesis = CreateGenesisBlock(1231006505, 2083236893, 0x1d00ffff, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
+		printf("hashGenesisBlock %s\n", consensus.hashGenesisBlock.ToString().c_str());
+		printf("hashMerkleRoot %s\n", genesis.hashMerkleRoot.ToString().c_str());
         assert(consensus.hashGenesisBlock == uint256S("0x000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"));
         assert(genesis.hashMerkleRoot == uint256S("0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"));
 
-        vSeeds.push_back(CDNSSeedData("syscoin.sipa.be", "seed.syscoin.sipa.be")); // Pieter Wuille
+        /*vSeeds.push_back(CDNSSeedData("syscoin.sipa.be", "seed.syscoin.sipa.be")); // Pieter Wuille
         vSeeds.push_back(CDNSSeedData("bluematt.me", "dnsseed.bluematt.me")); // Matt Corallo
         vSeeds.push_back(CDNSSeedData("dashjr.org", "dnsseed.syscoin.dashjr.org")); // Luke Dashjr
         vSeeds.push_back(CDNSSeedData("syscoinstats.com", "seed.syscoinstats.com")); // Christian Decker
         vSeeds.push_back(CDNSSeedData("xf2.org", "bitseed.xf2.org")); // Jeff Garzik
-        vSeeds.push_back(CDNSSeedData("syscoin.jonasschnelli.ch", "seed.syscoin.jonasschnelli.ch")); // Jonas Schnelli
+        vSeeds.push_back(CDNSSeedData("syscoin.jonasschnelli.ch", "seed.syscoin.jonasschnelli.ch")); // Jonas Schnelli*/
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,0);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
