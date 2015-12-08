3e167cfdddd680fec0ee248091675fd255546f63
Wladimir J. van der Laan <laanwj@gmail.com>
2015-12-08 17:10:41 +0100

    test: Add basic test for `reject` code
    Extend P2P test framework to make it possible to expect reject
    codes for transactions and blocks.
    
    Github-Pull: #7179
    Rebased-From: 20411903d7b356ebb174df2daad1dcd5d6117f79

--- qa/pull-tester/rpc-tests.sh
+++ qa/pull-tester/rpc-tests.sh
@@ -25,6 +25,8 @@ if [ "x${ENABLE_BITCOIND}${ENABLE_UTILS}${ENABLE_WALLET}" = "x111" ]; then
   ${BUILDDIR}/qa/rpc-tests/httpbasics.py --srcdir "${BUILDDIR}/src"
   ${BUILDDIR}/qa/rpc-tests/mempool_coinbase_spends.py --srcdir "${BUILDDIR}/src"
   #${BUILDDIR}/qa/rpc-tests/forknotify.py --srcdir "${BUILDDIR}/src"
+  ${BUILDDIR}/qa/rpc-tests/invalidblockrequest.py --srcdir "${BUILDDIR}/src"
+  ${BUILDDIR}/qa/rpc-tests/invalidtxrequest.py --srcdir "${BUILDDIR}/src"
 else
   echo "No rpc tests to run. Wallet, utils, and bitcoind must all be enabled"
 fi
