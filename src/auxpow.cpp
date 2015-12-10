071ca4b4a6397e5dc84b1b5ccc6141218029eba6
sidhujag <sidhujag@hotmail.com>
2015-12-09 18:42:54 -0800

    strictchainid

--- src/auxpow.cpp
+++ src/auxpow.cpp
@@ -97,7 +97,7 @@ CAuxPow::check (const uint256& hashAuxBlock, int nChainId,
     if (nIndex != 0)
         return error("AuxPow is not a generate");
 
-    if (params.fStrictChainId && parentBlock.nVersion.GetChainId () == nChainId)
+    if (parentBlock.nVersion.GetChainId () == nChainId)
         return error("Aux POW parent has our chain ID");
 
     if (vChainMerkleBranch.size() > 30)
