92612b386b56a099866fffe1df2ceb83577c881b
sidhujag <sidhujag@hotmail.com>
2015-12-09 18:09:54 -0800

    getauxhash

--- src/primitives/transaction.cpp
+++ src/primitives/transaction.cpp
@@ -74,7 +74,7 @@ void CTransaction::UpdateHash() const
 {
     *const_cast<uint256*>(&hash) = SerializeHash(*this);
 }
-const uint256& CTransaction::GetAuxHash() const
+uint256 CTransaction::GetAuxHash() const
 {
     return SerializeHash(*this, SER_GETHASHWITHOUTDATA | SER_GETHASH);
 }
