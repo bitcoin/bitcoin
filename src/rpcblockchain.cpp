0e71350a20d90f44425508961971af02611a305d
sidhujag <sidhujag@hotmail.com>
2015-12-09 19:32:21 -0800

    ditto

--- src/rpcblockchain.cpp
+++ src/rpcblockchain.cpp
@@ -110,7 +110,7 @@ UniValue blockheaderToJSON(const CBlockIndex* blockindex)
         confirmations = chainActive.Height() - blockindex->nHeight + 1;
     result.push_back(Pair("confirmations", confirmations));
     result.push_back(Pair("height", blockindex->nHeight));
-    result.push_back(Pair("version", blockindex->nVersion));
+    result.push_back(Pair("version", blockindex->nVersion.GetFullVersion()));
     result.push_back(Pair("merkleroot", blockindex->hashMerkleRoot.GetHex()));
     result.push_back(Pair("time", (int64_t)blockindex->nTime));
     result.push_back(Pair("mediantime", (int64_t)blockindex->GetMedianTimePast()));
