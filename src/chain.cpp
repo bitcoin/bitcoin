b48bbe7f5464c06e7ea6a3990ceb90bd650f3bda
sidhujag <sidhujag@hotmail.com>
2015-12-09 18:46:49 -0800

    auxpow

--- src/chain.cpp
+++ src/chain.cpp
@@ -4,7 +4,8 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "chain.h"
-
+// SYSCOIN for auxpow
+#include "main.h"
 using namespace std;
 // SYSCOIN moved and added auxpow check
 CBlockHeader GetBlockHeader() const
