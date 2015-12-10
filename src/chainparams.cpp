114c13378b81122a84a384e2155b911dc91434a6
sidhujag <sidhujag@hotmail.com>
2015-12-09 21:42:41 -0800

    genesis ver

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -30,7 +30,7 @@ static CBlock CreateGenesisBlock(const char* pszTimestamp, const CScript& genesi
     genesis.nTime    = nTime;
     genesis.nBits    = nBits;
     genesis.nNonce   = nNonce;
-    genesis.nVersion = nVersion;
+    genesis.nVersion.SetGenesisVersion(nVersion);
     genesis.vtx.push_back(txNew);
     genesis.hashPrevBlock.SetNull();
     genesis.hashMerkleRoot = BlockMerkleRoot(genesis);
