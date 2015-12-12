d812daf967ba4173bfa1c37eeb4ab7a0ccc4df25
accraze <accraze@gmail.com>
2015-12-12 10:45:53 -0800

    fix logic for error log

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -1035,7 +1035,7 @@ void CWalletTx::GetAmounts(list<COutputEntry>& listReceived,
         // In either case, we need to get the destination address
         CTxDestination address;
 
-        if (!ExtractDestination(txout.scriptPubKey, address) && txout.scriptPubKey.IsUnspendable())
+        if (!ExtractDestination(txout.scriptPubKey, address) && !txout.scriptPubKey.IsUnspendable())
         {
             LogPrintf("CWalletTx::GetAmounts: Unknown transaction type found, txid %s\n",
                      this->GetHash().ToString());
