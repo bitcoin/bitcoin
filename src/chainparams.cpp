e18378e53fb71c39236db35ab2d560b43602b1be
Elias Rohrer <rohrer@informatik.hu-berlin.de>
2015-12-15 14:53:15 +0100

    Removed offline testnet DNSSeed 'alexykot.me'.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -179,7 +179,6 @@ public:
 
         vFixedSeeds.clear();
         vSeeds.clear();
-        vSeeds.push_back(CDNSSeedData("alexykot.me", "testnet-seed.alexykot.me"));
         vSeeds.push_back(CDNSSeedData("bitcoin.petertodd.org", "testnet-seed.bitcoin.petertodd.org"));
         vSeeds.push_back(CDNSSeedData("bluematt.me", "testnet-seed.bluematt.me"));
         vSeeds.push_back(CDNSSeedData("bitcoin.schildbach.de", "testnet-seed.bitcoin.schildbach.de"));
