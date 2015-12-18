b99b7192b674a3eee884d0d7825c6e8eb18526b3
Luke Dashjr <luke-jr+git@utopios.org>
2015-12-18 12:27:26 +0000

    depends: Add mac_alias to depends

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -15,7 +15,7 @@ wallet_packages=bdb
 
 upnp_packages=miniupnpc
 
-darwin_native_packages = native_biplist native_ds_store
+darwin_native_packages = native_biplist native_ds_store native_mac_alias
 
 ifneq ($(build_os),darwin)
 darwin_native_packages += native_cctools native_cdrkit native_libdmg-hfsplus
