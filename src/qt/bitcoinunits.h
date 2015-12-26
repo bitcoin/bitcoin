5fdf32de7ed85a1a0aec7cdedb83f750f4a0f7ff
fanquake <fanquake@gmail.com>
2015-12-26 11:49:19 +0800

    Replace some instances of formatWithUnit with formatHtmlWithUnit
    Strings in a HTML context should be using formatHtmlWithUnit.

--- src/qt/bitcoinunits.h
+++ src/qt/bitcoinunits.h
@@ -88,6 +88,7 @@ public:
     static QString format(int unit, const CAmount& amount, bool plussign=false, SeparatorStyle separators=separatorStandard);
     //! Format as string (with unit)
     static QString formatWithUnit(int unit, const CAmount& amount, bool plussign=false, SeparatorStyle separators=separatorStandard);
+    //! Format as HTML string (with unit)
     static QString formatHtmlWithUnit(int unit, const CAmount& amount, bool plussign=false, SeparatorStyle separators=separatorStandard);
     //! Parse string to coin amount
     static bool parse(int unit, const QString &value, CAmount *val_out);
