76de36fd2e2bd733fcab68314f6d67704d2047d1
Pieter Wuille <pieter.wuille@gmail.com>
2016-01-03 16:50:31 +0100

    Report non-mandatory script failures correctly
    Github-Pull: #7276
    Rebased-From: 7ef8f3c072a8750c72a3a1cdc727b5c1d173bac8

--- src/main.cpp
+++ src/main.cpp
@@ -1653,9 +1653,9 @@ bool CheckInputs(const CTransaction& tx, CValidationState &state, const CCoinsVi
                         // arguments; if so, don't trigger DoS protection to
                         // avoid splitting the network between upgraded and
                         // non-upgraded nodes.
-                        CScriptCheck check(*coins, tx, i,
+                        CScriptCheck check2(*coins, tx, i,
                                 flags & ~STANDARD_NOT_MANDATORY_VERIFY_FLAGS, cacheStore);
-                        if (check())
+                        if (check2())
                             return state.Invalid(false, REJECT_NONSTANDARD, strprintf("non-mandatory-script-verify-flag (%s)", ScriptErrorString(check.GetScriptError())));
                     }
                     // Failures of other flags indicate a transaction that is
