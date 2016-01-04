39944d7a732b84332f424940aeef4fed995cd315
MarcoFalke <falke.marco@gmail.com>
2016-01-04 13:54:23 +0100

    [qa] Move create_tx() to util.py

--- qa/rpc-tests/mempool_reorg.py
+++ qa/rpc-tests/mempool_reorg.py
@@ -25,14 +25,6 @@ class MempoolCoinbaseTest(BitcoinTestFramework):
         self.is_network_split = False
         self.sync_all()
 
-    def create_tx(self, from_txid, to_address, amount):
-        inputs = [{ "txid" : from_txid, "vout" : 0}]
-        outputs = { to_address : amount }
-        rawtx = self.nodes[0].createrawtransaction(inputs, outputs)
-        signresult = self.nodes[0].signrawtransaction(rawtx)
-        assert_equal(signresult["complete"], True)
-        return signresult["hex"]
-
     def run_test(self):
         start_count = self.nodes[0].getblockcount()
 
@@ -52,9 +44,9 @@ class MempoolCoinbaseTest(BitcoinTestFramework):
         # and make sure the mempool code behaves correctly.
         b = [ self.nodes[0].getblockhash(n) for n in range(101, 105) ]
         coinbase_txids = [ self.nodes[0].getblock(h)['tx'][0] for h in b ]
-        spend_101_raw = self.create_tx(coinbase_txids[1], node1_address, 49.99)
-        spend_102_raw = self.create_tx(coinbase_txids[2], node0_address, 49.99)
-        spend_103_raw = self.create_tx(coinbase_txids[3], node0_address, 49.99)
+        spend_101_raw = create_tx(self.nodes[0], coinbase_txids[1], node1_address, 49.99)
+        spend_102_raw = create_tx(self.nodes[0], coinbase_txids[2], node0_address, 49.99)
+        spend_103_raw = create_tx(self.nodes[0], coinbase_txids[3], node0_address, 49.99)
 
         # Create a block-height-locked transaction which will be invalid after reorg
         timelock_tx = self.nodes[0].createrawtransaction([{"txid": coinbase_txids[0], "vout": 0}], {node0_address: 49.99})
@@ -71,8 +63,8 @@ class MempoolCoinbaseTest(BitcoinTestFramework):
         assert_raises(JSONRPCException, self.nodes[0].sendrawtransaction, timelock_tx)
 
         # Create 102_1 and 103_1:
-        spend_102_1_raw = self.create_tx(spend_102_id, node1_address, 49.98)
-        spend_103_1_raw = self.create_tx(spend_103_id, node1_address, 49.98)
+        spend_102_1_raw = create_tx(self.nodes[0], spend_102_id, node1_address, 49.98)
+        spend_103_1_raw = create_tx(self.nodes[0], spend_103_id, node1_address, 49.98)
 
         # Broadcast and mine 103_1:
         spend_103_1_id = self.nodes[0].sendrawtransaction(spend_103_1_raw)
