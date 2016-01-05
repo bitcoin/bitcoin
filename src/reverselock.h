71c7c9fc841621a9a340bc0d488e2b4aece734ea
Cory Fields <cory-nospam-@coryfields.com>
2016-01-05 16:10:13 -0500

    c++11: don't throw from the reverselock destructor
    noexcept is default for destructors as of c++11. By throwing in reverselock's
    destructor if it's lock has been tampered with, the likely result is
    std::terminate being called. Indeed that happened before this change.
    
    Once reverselock has taken another lock (its ctor didn't throw), it makes no
    sense to try to grab or lock the parent lock. That is be broken/undefined
    behavior depending on the parent lock's implementation, but it shouldn't cause
    the reverselock to fail to re-lock when destroyed.
    
    To avoid those problems, simply swap the parent lock's contents with a dummy
    for the duration of the lock. That will ensure that any undefined behavior is
    caught at the call-site rather than the reverse lock's destruction.
    
    Barring a failed mutex unlock which would be indicative of a larger problem,
    the destructor should now never throw.

--- src/reverselock.h
+++ src/reverselock.h
@@ -16,10 +16,12 @@ public:
 
     explicit reverse_lock(Lock& lock) : lock(lock) {
         lock.unlock();
+        lock.swap(templock);
     }
 
     ~reverse_lock() {
-        lock.lock();
+        templock.lock();
+        templock.swap(lock);
     }
 
 private:
@@ -27,6 +29,7 @@ private:
     reverse_lock& operator=(reverse_lock const&);
 
     Lock& lock;
+    Lock templock;
 };
 
 #endif // BITCOIN_REVERSELOCK_H
