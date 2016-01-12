bf26c10b28af9e8154a507ae8dd5ac42648706a1
Jorge Timón <jtimon@jtimon.cc>
2016-01-12 14:32:02 +0100

    Consensus: Decouple pow.o from chain.o

--- src/pow.h
+++ src/pow.h
@@ -11,11 +11,11 @@
 #include <stdint.h>
 
 class CBlockHeader;
-class CBlockIndex;
+class CBlockIndexView;
 class uint256;
 
-unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHeader *pblock, const Consensus::Params&);
-unsigned int CalculateNextWorkRequired(const CBlockIndex* pindexLast, int64_t nFirstBlockTime, const Consensus::Params&);
+unsigned int GetNextWorkRequired(const CBlockIndexView* pindexLast, const CBlockHeader *pblock, const Consensus::Params&);
+unsigned int CalculateNextWorkRequired(const CBlockIndexView* pindexLast, int64_t nFirstBlockTime, const Consensus::Params&);
 
 /** Check whether a block hash satisfies the proof-of-work requirement specified by nBits */
 bool CheckProofOfWork(uint256 hash, unsigned int nBits, const Consensus::Params&);
