ad8c743421a9e64fea4ede8072fe07b4d266c1a8
MarcoFalke <falke.marco@gmail.com>
2016-01-19 17:47:55 +0100

    [qa] Extend tests
    Github-Pull: #7684
    Rebased-From: fa3a81af18347a1d3fed41aa89ee643cbf0e7abc fad7dc8a6c0ca9c067a249cf8896dd2e64703e48 fad8cfb893ac0ba83c6fc2367ade55bfe4fa75f6

--- qa/rpc-tests/maxuploadtarget.py
+++ qa/rpc-tests/maxuploadtarget.py
@@ -176,7 +176,7 @@ class MaxUploadTest(BitcoinTestFramework):
         getdata_request.inv.append(CInv(2, big_old_block))
 
         max_bytes_per_day = 200*1024*1024
-        daily_buffer = 144 * 1000000
+        daily_buffer = 144 * MAX_BLOCK_SIZE
         max_bytes_available = max_bytes_per_day - daily_buffer
         success_count = max_bytes_available / old_block_size
 
