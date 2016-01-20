c40ec1421048e7ddb9eb1878e4c65ff27aa066c8
MarcoFalke <falke.marco@gmail.com>
2016-01-20 09:51:02 +0100

    [walletdb] Fix syntax error in key parser
    Github-Pull: #7381
    Rebased-From: fa6d4cc09575de30386bfbc5c8c3858cd7a2f42a

--- src/wallet/db.cpp
+++ src/wallet/db.cpp
@@ -205,7 +205,7 @@ bool CDBEnv::Salvage(const std::string& strFile, bool fAggressive, std::vector<C
     std::string keyHex, valueHex;
     while (!strDump.eof() && keyHex != "DATA=END") {
         getline(strDump, keyHex);
-        if (keyHex != "DATA_END") {
+        if (keyHex != "DATA=END") {
             getline(strDump, valueHex);
             vResult.push_back(make_pair(ParseHex(keyHex), ParseHex(valueHex)));
         }
