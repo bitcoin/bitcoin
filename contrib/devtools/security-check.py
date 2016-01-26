aa26ee010198a1cc4d3e7e62cd0ab80807ba66a4
Cory Fields <cory-nospam-@coryfields.com>
2016-01-26 14:50:50 -0500

    release: Add security/export checks to gitian and fix current failures
    - fix parsing of BIND_NOW with older readelf
    - add _IO_stdin_used to ignored exports
    
    For details see: https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=634261#109
    
    - add check-symbols and check-security make targets
    
    These are not added to the default checks because some of them depend on
    release-build configs.
    
    - always link librt for glibc back-compat builds
    
    glibc absorbed clock_gettime in 2.17. librt (its previous location) is safe to
    link in anyway for back-compat.
    
    Fixes #7420
    
    - add security/symbol checks to gitian
    
    Github-Pull: #7424
    Rebased-From: cd27bf51e06a8d79790a631696355bd05751b0aa 475813ba5b208eb9a5d027eb628a717cc123ef4f f3d3eaf78eb51238d799d8f20a585550d1567719 a8ce872118c4807465629aecb9e4f3d72d999ccb a81c87fafce43e49cc2307947e3951b84be7ca9a

--- contrib/devtools/security-check.py
+++ contrib/devtools/security-check.py
@@ -94,7 +94,7 @@ def check_ELF_RELRO(executable):
         raise IOError('Error opening file')
     for line in stdout.split('\n'):
         tokens = line.split()
-        if len(tokens)>1 and tokens[1] == '(BIND_NOW)':
+        if len(tokens)>1 and tokens[1] == '(BIND_NOW)' or (len(tokens)>2 and tokens[1] == '(FLAGS)' and 'BIND_NOW' in tokens[2]):
             have_bindnow = True
     return have_gnu_relro and have_bindnow
 
