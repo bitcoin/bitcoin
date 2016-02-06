1b97499b478331933d4958816edcb488e26b052e
svost <ya.nowa@yandex.ru>
2016-02-06 12:57:58 +0300

    Void - return mismatch

--- src/protocol.h
+++ src/protocol.h
@@ -19,7 +19,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 17777 : 7777;
+    return static_cast<unsigned short>(testnet ? 17777 : 7777);
 }
 
 
