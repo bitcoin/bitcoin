64fd0ce1d92f6dbd40f755edbbe8353a8ee3f353
jloughry <joe.loughry@gmail.com>
2016-02-12 11:35:32 -0700

    fix spelling of advertise in src and doc
    Github-Pull: #7526
    Rebased-From: 37767fd46f673a06864df6e14d3030622b1cb2c9

--- src/net.h
+++ src/net.h
@@ -134,7 +134,7 @@ enum
 };
 
 bool IsPeerAddrLocalGood(CNode *pnode);
-void AdvertizeLocal(CNode *pnode);
+void AdvertiseLocal(CNode *pnode);
 void SetLimited(enum Network net, bool fLimited = true);
 bool IsLimited(enum Network net);
 bool IsLimited(const CNetAddr& addr);
