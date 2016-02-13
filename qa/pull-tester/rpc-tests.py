caf138122decac7d96b2f53c0c894277cb0f33ca
BtcDrak <btcdrak@gmail.com>
2016-02-13 15:42:24 +0000

    Add bip68-sequence.py to extended rpc tests

--- qa/pull-tester/rpc-tests.py
+++ qa/pull-tester/rpc-tests.py
@@ -113,6 +113,7 @@ testScriptsExt = [
     'bip9-softforks.py',
     'bip65-cltv.py',
     'bip65-cltv-p2p.py',
+    'bip68-sequence.py',
     'bipdersig-p2p.py',
     'bipdersig.py',
     'getblocktemplate_longpoll.py',
