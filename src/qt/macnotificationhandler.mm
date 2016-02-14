a7583f69a8e818579a2780691595aa11e9cf3c5f
Fuzzbawls <fuzzbawls@gmail.com>
2016-02-14 14:32:56 -0800

    More rebranding from "Bitcoin" to "HOdlcoin" - Also updated the Mac icon to be the same as windows icon - Also fixed the Mac notification registry

--- src/qt/macnotificationhandler.mm
+++ src/qt/macnotificationhandler.mm
@@ -13,7 +13,7 @@
 - (NSString *)__bundleIdentifier
 {
     if (self == [NSBundle mainBundle]) {
-        return @"org.bitcoinfoundation.Bitcoin-Qt";
+        return @"org.hodlcoinfoundation.HOdlcoin-Qt";
     } else {
         return [self __bundleIdentifier];
     }
