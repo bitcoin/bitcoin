a267c7697fcc3c96781da212156ae1595289cc8e
svost <svost@users.noreply.github.com>
2016-02-15 00:35:17 +0300

    Header guard added

--- src/ntp.h
+++ src/ntp.h
@@ -1,3 +1,9 @@
+// Copyright (c) 2013-2016 The NovaCoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or https://opensource.org/licenses/mit-license.php
+#ifndef NOVACOIN_NTP_H
+#define NOVACOIN_NTP_H
+
 // Get time from random server and return server address.
 int64_t NtpGetTime(CNetAddr& ip);
 
@@ -11,3 +17,5 @@ void ThreadNtpSamples(void* parg);
 
 // NTP offset
 int64_t GetNtpOffset();
+
+#endif // NOVACOIN_NTP_H
