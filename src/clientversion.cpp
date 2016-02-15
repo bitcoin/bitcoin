e0c9121ab74227e891cba489ee0aa98637daae08
Tom Zander <tomz@freedommail.ch>
2016-02-15 10:04:42 +0000

    Report Classic in p2p version message

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -13,7 +13,7 @@
  * for both bitcoind and bitcoin-core, to make it harder for attackers to
  * target servers or GUI users specifically.
  */
-const std::string CLIENT_NAME("Satoshi");
+const std::string CLIENT_NAME("Classic");
 
 /**
  * Client version number
