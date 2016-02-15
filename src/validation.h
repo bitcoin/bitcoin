69422d49d81b516b93cd0e73869f410467aa0155
Luke Dashjr <luke-jr+git@utopios.org>
2016-02-15 08:49:26 +0000

    Simplified and public LimitMempoolSize function

--- src/validation.h
+++ src/validation.h
@@ -292,6 +292,8 @@ bool CheckSequenceLocks(CBlockIndex* tip,
                         LockPoints* lp = nullptr,
                         bool useExistingLockPoints = false);
 
+void LimitMempoolSize(CTxMemPool&, CCoinsViewCache&);
+
 /**
  * Closure representing one script verification
  * Note that this stores references to the spending transaction
