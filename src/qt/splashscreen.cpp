b90eaa98083e69c20c8c776384ab81a96949b446
Fuzzbawls <fuzzbawls@gmail.com>
2016-02-23 23:29:33 -0800

    More rebranding from "Bitcoin" to "HOdlcoin"

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -40,7 +40,7 @@ SplashScreen::SplashScreen(Qt::WindowFlags f, const NetworkStyle *networkStyle) 
     // define text to place
     QString titleText       = tr("HOdlcoin Core");
     QString versionText     = QString("Version %1").arg(QString::fromStdString(FormatFullVersion()));
-    QString copyrightText   = QChar(0xA9)+QString(" 2009-%1 ").arg(COPYRIGHT_YEAR) + QString(tr("The Bitcoin Core developers"));
+    QString copyrightText   = QChar(0xA9)+QString(" %1 ").arg(COPYRIGHT_YEAR) + QString(tr("The HOdlcoin developers"));
     QString titleAddText    = networkStyle->getTitleAddText();
 
     QString font            = QApplication::font().toString();
