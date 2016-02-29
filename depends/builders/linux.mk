f0300c6c5fad17ace733d90516cb9d76b44cd7bc
Wladimir J. van der Laan <laanwj@gmail.com>
2016-02-29 09:51:17 +0100

    Merge #7606: [depends] builders: No need to set -L and --location for curl
    fa7a5c5 [depends] builders: No need to set -L and --location for curl (MarcoFalke)

--- depends/builders/linux.mk
+++ depends/builders/linux.mk
@@ -1,2 +1,2 @@
 build_linux_SHA256SUM = sha256sum
-build_linux_DOWNLOAD = curl --location --fail --connect-timeout $(DOWNLOAD_CONNECT_TIMEOUT) --retry $(DOWNLOAD_RETRIES) -L -o
+build_linux_DOWNLOAD = curl --location --fail --connect-timeout $(DOWNLOAD_CONNECT_TIMEOUT) --retry $(DOWNLOAD_RETRIES) -o
