376b257dbcc006365dc52cbc80c745d60a4098a9
TomZ <tomz@freedommail.ch>
2016-03-02 11:32:24 +0000

    Change default value

--- src/main.h
+++ src/main.h
@@ -111,7 +111,7 @@ static const unsigned int DEFAULT_BANSCORE_THRESHOLD = 100;
 
 static const bool DEFAULT_TESTSAFEMODE = false;
 /** Default for -mempoolreplacement */
-static const bool DEFAULT_ENABLE_REPLACEMENT = true;
+static const bool DEFAULT_ENABLE_REPLACEMENT = false;
 
 /** Maximum number of headers to announce when relaying blocks with headers message.*/
 static const unsigned int MAX_BLOCKS_TO_ANNOUNCE = 8;
