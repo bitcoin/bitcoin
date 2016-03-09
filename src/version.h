23eeaeef8ff52e27f7512e2059a9816fa0483160
Pieter Wuille <pieter.wuille@gmail.com>
2016-03-09 21:39:49 +0100

    Merge #7662: remove unused NOBLKS_VERSION_{START,END} constants
    7d2f84c remove unused NOBLKS_VERSION_{START,END} constants (Pavel Vasin)

--- src/version.h
+++ src/version.h
@@ -25,10 +25,6 @@ static const int MIN_PEER_PROTO_VERSION = 70208;
 //! if possible, avoid requesting addresses nodes older than this
 static const int CADDR_TIME_VERSION = 31402;
 
-//! only request blocks from nodes outside this range of versions
-static const int NOBLKS_VERSION_START = 32000;
-static const int NOBLKS_VERSION_END = 32400;
-
 //! BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
 
