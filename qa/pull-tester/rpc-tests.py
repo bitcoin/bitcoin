697ed8c8279a1ac915b71db06a03f9ed85b9f334
Elliot Olds <elliotolds@gmail.com>
2016-03-10 03:12:40 -0800

    Check if zmq is installed in tests, update docs
    Github-Pull: #7635
    Rebased-From: 2ab835ae6c02de14264896d571c0bba230a1e9f0

--- qa/pull-tester/rpc-tests.py
+++ qa/pull-tester/rpc-tests.py
@@ -40,6 +40,15 @@ if 'ENABLE_UTILS' not in vars():
     ENABLE_UTILS=0
 if 'ENABLE_ZMQ' not in vars():
     ENABLE_ZMQ=0
+    
+# python-zmq may not be installed. Handle this gracefully and with some helpful info
+if ENABLE_ZMQ:
+    try:
+        import zmq
+    except ImportError:
+        print("WARNING: \"import zmq\" failed. Setting ENABLE_ZMQ=0. " \
+            "To run zmq tests, see dependency info in /qa/README.md.")
+        ENABLE_ZMQ=0
 
 ENABLE_COVERAGE=0
 
