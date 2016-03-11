393b22eacb8aff58aaa4da48085f3ea37424ba59
Mustafa <mus@musalbas.com>
2016-03-11 12:03:45 +0000

    Add a source file for unit test utils.

---
+++ src/test/testutil.cpp
@@ -0,0 +1,3 @@
+// Copyright (c) 2009-2016 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
