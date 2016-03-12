a6ee0caa4e5a6459f49995ac2dfd40e2444f7c04
Pavel Vasin <pavel@vasin.nl>
2016-03-12 17:11:59 +0300

    use cached block hash in blockToJSON()

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -89,7 +89,7 @@ UniValue blockheaderToJSON(const CBlockIndex* blockindex)
 UniValue blockToJSON(const CBlock& block, const CBlockIndex* blockindex, bool txDetails = false)
 {
     UniValue result(UniValue::VOBJ);
-    result.push_back(Pair("hash", block.GetHash().GetHex()));
+    result.push_back(Pair("hash", blockindex->GetBlockHash().GetHex()));
     int confirmations = -1;
     // Only report confirmations if the block is on the main chain
     if (chainActive.Contains(blockindex))
