b604ec2da000496a204b33367a1f90a720e0e8e5
sidhujag <sidhujag@hotmail.com>
2016-03-11 20:05:03 -0800

    remove decs for unused functions

--- src/qt/mywhitelistofferdialog.h
+++ src/qt/mywhitelistofferdialog.h
@@ -47,12 +47,10 @@ private Q_SLOTS:
     /** Export button clicked */
     void on_exportButton_clicked();
 	void on_refreshButton_clicked();
-    /** Set button states based on selected tab and selection */
-    void selectionChanged();
+
     /** Spawn contextual menu (right mouse menu) for alias book entry */
     void contextualMenu(const QPoint &point);
-    /** New entry/entries were added to cert table */
-    void selectNewEntry(const QModelIndex &parent, int begin, int /*end*/);
+
 
 };
 
