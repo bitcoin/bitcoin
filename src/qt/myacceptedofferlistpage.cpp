2d916f79b0055539620ebe5cadf7223601c786c7
sidhujag <sidhujag@hotmail.com>
2016-03-18 14:50:06 -0700

    add check payment button on accept page

--- src/qt/myacceptedofferlistpage.cpp
+++ src/qt/myacceptedofferlistpage.cpp
@@ -38,6 +38,7 @@ MyAcceptedOfferListPage::MyAcceptedOfferListPage(const PlatformStyle *platformSt
 		ui->detailButton->setIcon(QIcon());
 		ui->copyOffer->setIcon(QIcon());
 		ui->refreshButton->setIcon(QIcon());
+		ui->btcButton->setIcon(QIcon());
 
 	}
 	else
@@ -47,6 +48,7 @@ MyAcceptedOfferListPage::MyAcceptedOfferListPage(const PlatformStyle *platformSt
 		ui->detailButton->setIcon(platformStyle->SingleColorIcon(":/icons/" + theme + "/details"));
 		ui->copyOffer->setIcon(platformStyle->SingleColorIcon(":/icons/" + theme + "/editcopy"));
 		ui->refreshButton->setIcon(platformStyle->SingleColorIcon(":/icons/" + theme + "/refresh"));
+		ui->btcButton->setIcon(platformStyle->SingleColorIcon(":/icons/" + theme + "/search"));
 		
 	}
 
@@ -76,6 +78,183 @@ MyAcceptedOfferListPage::MyAcceptedOfferListPage(const PlatformStyle *platformSt
 
     connect(ui->tableView, SIGNAL(customContextMenuRequested(QPoint)), this, SLOT(contextualMenu(QPoint)));
 
+
+}
+	
+bool MyAcceptedOfferListPage::lookup(const QString &lookupid, QString& address, QString& price, QString& btcTxId)
+{
+	string strError;
+	string strMethod = string("offerinfo");
+	UniValue params(UniValue::VARR);
+	UniValue result;
+	params.push_back(lookupid.toStdString());
+
+    try {
+        result = tableRPC.execute(strMethod, params);
+
+		if (result.type() == UniValue::VOBJ)
+		{
+			UniValue offerAcceptsValue = find_value(result.get_obj(), "accepts");
+			if(offerAcceptsValue.type() != UniValue::VARR)
+				return false;
+			const UniValue &offerObj = result.get_obj();
+			const UniValue &offerAccepts = offerAcceptsValue.get_array();
+			COfferAccept myAccept;
+			QDateTime timestamp;
+		    for (unsigned int idx = 0; idx < offerAccepts.size(); idx++) {
+			    const UniValue& accept = offerAccepts[idx];				
+				const UniValue& acceptObj = accept.get_obj();
+				QString offerAcceptHash = QString::fromStdString(find_value(acceptObj, "id").get_str());
+				if(offerAcceptHash != offerAcceptGUID)
+					continue;
+				QString currencyStr = QString::fromStdString(find_value(acceptObj, "currency").get_str());
+				if(currencyStr == "BTC")
+				{
+					btcTxId = QString::fromStdString(find_value(acceptObj, "btctxid").get_str());
+					
+				}
+			}
+			const string &strAddress = find_value(offerObj, "address").get_str();
+			const string &strPrice = find_value(offerObj, "price").get_str();
+			address = QString::fromStdString(strAddress);
+			price = QString::fromStdString(strPrice);
+			return true;
+		}
+	}
+	catch (UniValue& objError)
+	{
+		QMessageBox::critical(this, windowTitle(),
+			tr("Could not find this offer, please check the offer ID and that it has been confirmed by the blockchain: ") + lookupid,
+				QMessageBox::Ok, QMessageBox::Ok);
+		return true;
+
+	}
+	catch(std::exception& e)
+	{
+		QMessageBox::critical(this, windowTitle(),
+			tr("There was an exception trying to locate this offer, please check the offer ID and that it has been confirmed by the blockchain: ") + QString::fromStdString(e.what()),
+				QMessageBox::Ok, QMessageBox::Ok);
+		return true;
+	}
+	return false;
+
+
+}
+bool OfferAcceptDialogBTC::CheckPaymentInBTC(const QString &strBTCTxId, const QString& address, const QString& price, int& height, long& time)
+{
+	QNetworkAccessManager *nam = new QNetworkAccessManager(this);
+	QUrl url("https://blockchain.info/tx/" + strBTCTxId + "?format=json");
+	QNetworkRequest request(url);
+	request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+	QNetworkReply* reply = nam->get(request);
+	reply->ignoreSslErrors();
+	CAmount valueAmount = 0;
+	CAmount priceAmount = 0;
+	if(!ParseMoney(price.toStdString(), priceAmount))
+		return false;
+	int totalTime = 0;
+	while(!reply->isFinished())
+	{
+		qApp->processEvents();
+		totalTime += 100;
+		MilliSleep(100);
+		if(totalTime > 30000)
+			throw runtime_error("Timeout connecting to blockchain.info!");
+	}
+	bool doubleSpend = false;
+	if(reply->error() == QNetworkReply::NoError) {
+
+		UniValue outerValue;
+		bool read = outerValue.read(reply->readAll().toStdString());
+		if (read)
+		{
+			UniValue outerObj = outerValue.get_obj();
+			UniValue heightValue = find_value(outerObj, "block_height");
+			if (heightValue.isNum())
+				height = heightValue.get_int();
+			UniValue timeValue = find_value(outerObj, "time");
+			if (timeValue.isNum())
+				time = timeValue.get_int64();
+			UniValue doubleSpendValue = find_value(outerObj, "double_spend");
+			if (doubleSpendValue.isBool())
+			{
+				doubleSpend = doubleSpendValue.get_bool();
+				if(doubleSpend)
+					return false;
+			}
+			UniValue outputsValue = find_value(outerObj, "out");
+			if (outputsValue.isArray())
+			{
+				UniValue outputs = outputsValue.get_array();
+				for (unsigned int idx = 0; idx < outputs.size(); idx++) {
+					const UniValue& output = outputs[idx];	
+					UniValue addressValue = find_value(output, "addr");
+					if(addressValue.isStr())
+					{
+						if(addressValue.get_str() == address.toStdString())
+						{
+							UniValue paymentValue = find_value(output, "value");
+							if(paymentValue.isNum())
+							{
+								valueAmount += paymentValue.get_int64();
+								if(valueAmount >= priceAmount)
+									return true;
+							}
+						}
+							
+					}
+				}
+			}
+		}
+	}
+	reply->deleteLater();
+	return false;
+
+
+}
+void MyAcceptedOfferListPage::on_btcButton_clicked()
+{
+ 	if(!model)	
+		return;
+	if(!ui->tableView->selectionModel())
+        return;
+    QModelIndexList selection = ui->tableView->selectionModel()->selectedRows();
+    if(selection.isEmpty())
+    {
+        return;
+    }
+	QString address, price;
+	QString offerid = selection.at(0).data(OfferAcceptTableModel::Name).toString();
+	QString acceptid = selection.at(0).data(OfferAcceptTableModel::GUID).toString();
+	
+	if(!lookup(offerid, acceptid, address, price, btcTxId))
+	{
+        QMessageBox::critical(this, windowTitle(),
+        tr("Could not find this offer, please check the offer ID and that it has been confirmed by the blockchain: ") + offerid,
+            QMessageBox::Ok, QMessageBox::Ok);
+        return;
+	}
+	if(btcTxId.isEmpty())
+	{
+        QMessageBox::critical(this, windowTitle(),
+        tr("This payment was not done using Bitcoin, please select an offer that was accepted by paying with Bitcoins.") + this->offer,
+            QMessageBox::Ok, QMessageBox::Ok);
+        return;
+	}
+	int height;
+	long time;
+	if(!CheckPaymentInBTC(btcTxId, address, price, height, time))
+	{
+        QMessageBox::critical(this, windowTitle(),
+			tr("Could not find a payment of %1 BTC at address: %2, please check the Transaction ID %3 has been confirmed by the Bitcoin blockchain: ").arg(price).arg(address).arg(btcTxId),
+            QMessageBox::Ok, QMessageBox::Ok);
+        return;
+	}
+	QDateTime timestamp;
+	timestamp.setTime_t(time);
+    QMessageBox::information(this, windowTitle(),
+		tr("Transaction ID %1 was found in the Bitcoin blockchain! Full payment has been detected in block %2 at %3. It is recommended that you confirm payment by opening your Bitcoin wallet and seeing the funds in your account.").arg(btcTxId).arg(height).arg(timestamp.toString(Qt::SystemLocaleShortDate)),
+        QMessageBox::Ok, QMessageBox::Ok);
 
 }
 void MyAcceptedOfferListPage::on_messageButton_clicked()
