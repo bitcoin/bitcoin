bc31d01c53832fb9288f92141dd165a0a21d2b86
sidhujag <sidhujag@hotmail.com>
2016-03-18 14:59:19 -0700

    compile
    Former-commit-id: d5a7f44a024a04155eaaab8ca1e82fcabc7db18a

--- src/qt/myacceptedofferlistpage.h
+++ src/qt/myacceptedofferlistpage.h
@@ -60,6 +60,7 @@ private Q_SLOTS:
     void on_exportButton_clicked();
 	void on_refreshButton_clicked();
 	void on_messageButton_clicked();
+	void on_btcButton_clicked();
     void selectionChanged();
     void contextualMenu(const QPoint &point);
     void selectNewOffer(const QModelIndex &parent, int begin, int /*end*/);
