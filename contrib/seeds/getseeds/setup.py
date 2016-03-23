d72572136a0c4b4939df6dd63d16e85057cbb366
CryptoManiac <balthazar@yandex.ru>
2016-03-23 03:34:50 +0300

    Create setup.py for getseeds script.

---
+++ contrib/seeds/getseeds/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+import py2exe, sys, os
+
+sys.argv.append('py2exe')
+
+setup(
+    options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
+    windows = [{'script': "getseeds.py"}],
+    zipfile = None,
+)
