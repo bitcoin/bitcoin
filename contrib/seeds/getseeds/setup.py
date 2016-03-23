78b1e58377e147a37cb06291af6bf1fe12dde5bd
CryptoManiac <balthazar@yandex.ru>
2016-03-23 03:48:07 +0300

    http://stackoverflow.com/questions/12505383/print-not-working-when-compiled-with-py2exe

--- contrib/seeds/getseeds/setup.py
+++ contrib/seeds/getseeds/setup.py
@@ -5,6 +5,6 @@ sys.argv.append('py2exe')
 
 setup(
     options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
-    windows = [{'script': "getseeds.py"}],
+    console = [{'script': "getseeds.py"}],
     zipfile = None,
 )
