dd13ed85f27315bb1d6836a32eedd21710ef61fa
Pieter Wuille <pieter.wuille@gmail.com>
2016-03-26 18:58:00 +0100

    Keep addrman's nService bits consistent with outbound observations

--- src/main.cpp
+++ src/main.cpp
@@ -4612,6 +4612,10 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
         CAddress addrFrom;
         uint64_t nNonce = 1;
         vRecv >> pfrom->nVersion >> pfrom->nServices >> nTime >> addrMe;
+        if (!pfrom->fInbound)
+        {
+            addrman.SetServices(pfrom->addr, pfrom->nServices);
+        }
         if (pfrom->nVersion < MIN_PEER_PROTO_VERSION)
         {
             // disconnect from peers older than this proto version
