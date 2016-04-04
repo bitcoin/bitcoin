7a9ac58046e906b8826427da0bd6d007e0509167
sidhujag <sidhujag@hotmail.com>
2016-04-04 12:19:51 -0700

    typo
    Former-commit-id: 15ec42adadce0cda17f83072fd31890e61a9fcbb

--- src/qt/editofferdialog.h
+++ src/qt/editofferdialog.h
@@ -38,7 +38,7 @@ public:
 public Q_SLOTS:
     void accept();
 	void certChanged(int);
-	void on_aliasPegEdit_editingFinished(const QString& text);
+	void on_aliasPegEdit_editingFinished();
 private:
     bool saveCurrentRow();
 	void loadCerts();
