94c369ff1936636a7305a57c336de300edc10196
sidhujag <sidhujag@hotmail.com>
2016-04-13 22:12:43 -0700

    fix currencyt code check for directbc
    Former-commit-id: 8e6199c66eac7071d5121fb5f8f041fd6d0a4317

--- src/offer.cpp
+++ src/offer.cpp
@@ -2838,8 +2838,10 @@ UniValue offeraccept(const UniValue& params, bool fHelp) {
 	{
 		throw runtime_error("This offer must be paid with Bitcoins as per requirements of the seller");
 	}
+	vector<unsigned char> sCurrency = theOffer.sCurrencyCode;
 	theOffer.ClearOffer();
 	theOffer.accept = txAccept;
+	theOffer.sCurrencyCode = sCurrency;
 
 	const vector<unsigned char> &data = theOffer.Serialize();
 	CScript scriptData;
@@ -3166,8 +3168,10 @@ UniValue offeraccept_nocheck(const UniValue& params, bool fHelp) {
 	{
 		throw runtime_error("This offer must be paid with Bitcoins as per requirements of the seller");
 	}
+	vector<unsigned char> sCurrency = theOffer.sCurrencyCode;
 	theOffer.ClearOffer();
 	theOffer.accept = txAccept;
+	theOffer.sCurrencyCode = sCurrency;
 
 	const vector<unsigned char> &data = theOffer.Serialize();
 	CScript scriptData;
