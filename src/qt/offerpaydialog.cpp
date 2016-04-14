91e3dffd704e57a9eb025704cc37e1d1837e2479
sidhujag <sidhujag@hotmail.com>
2016-04-14 14:54:19 -0700

    fix #57

--- src/qt/offerpaydialog.cpp
+++ src/qt/offerpaydialog.cpp
@@ -10,7 +10,7 @@ OfferPayDialog::OfferPayDialog(const PlatformStyle *platformStyle, QString title
 {
     ui->setupUi(this);
 	connect(ui->finishButton, SIGNAL(clicked()), this, SLOT(accept()));
-	ui->payMessage->setText(tr("<p>You've purchased %1 of '%2' for %3 %4!</p><p><FONT COLOR='green'><b>Your payment is complete!</b></FONT></p><p>The merchant has been sent your delivery information and your item should arrive shortly. The merchant may followup with further information through a private message (please check your inbox regularily).</p><br>").arg(quantity).arg(title).arg(price).arg(currency));			
+	ui->payMessage->setText(tr("<p>You've purchased %1 of '%2' for %3 %4!</p><p><FONT COLOR='green'><b>Your payment is complete!</b></FONT></p><p>The merchant has been sent your delivery information and your item should arrive shortly. The merchant may follow-up with further information through a private message (please check your inbox regularily).</p><br>").arg(quantity).arg(title).arg(price).arg(currency));			
 	ui->purchaseHint->setText(tr("Please click Finish"));
 	QString theme = GUIUtil::getThemeName();
     // use a SingleColorIcon for the "out of sync warning" icon
