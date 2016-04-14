0b687fe94ade77bebb7901f3839031f0fed34164
sidhujag <sidhujag@hotmail.com>
2016-04-14 14:54:19 -0700

    fix #57
    Former-commit-id: 2173fe5f7ac5ff810d9efd7c5bc360774631ddfe

--- src/qt/offerescrowdialog.cpp
+++ src/qt/offerescrowdialog.cpp
@@ -10,7 +10,7 @@ OfferEscrowDialog::OfferEscrowDialog(const PlatformStyle *platformStyle, QString
 {
     ui->setupUi(this);
 	connect(ui->finishButton, SIGNAL(clicked()), this, SLOT(accept()));
-	ui->payMessage->setText(tr("<p>You've created an escrow for %1 of '%2' for %3 SYS!</p><p><FONT COLOR='green'><b>Your payment is in escrow!</b></FONT></p><p>The merchant and arbiter have been sent an escrow notification. The merchant may followup with further information.</p><br>").arg(quantity).arg(title).arg(price));			
+	ui->payMessage->setText(tr("<p>You've created an escrow for %1 of '%2' for %3 SYS!</p><p><FONT COLOR='green'><b>Your payment is in escrow!</b></FONT></p><p>The merchant and arbiter have been sent an escrow notification. The merchant may follow-up with further information.</p><br>").arg(quantity).arg(title).arg(price));			
 	ui->purchaseHint->setText(tr("Please click Finish"));
 	QString theme = GUIUtil::getThemeName();  
 	if (!platformStyle->getImagesOnButtons())
