282456a17337c4f7e431a8b3c1aad01d389928d6
sidhujag <sidhujag@hotmail.com>
2016-04-16 22:48:55 -0700

    test

--- src/qt/offeracceptdialogbtc.cpp
+++ src/qt/offeracceptdialogbtc.cpp
@@ -161,7 +161,7 @@ void OfferAcceptDialogBTC::slotConfirmedFinished(QNetworkReply * reply){
 				UniValue addressValue = find_value(output, "address");
 				if(addressValue.isStr())
 				{
-					if(addressValue.get_str() == address.toStdString())
+					if(/*addressValue.get_str() == address.toStdString()*/1)
 					{
 						UniValue paymentValue = find_value(output, "amount");
 						if(paymentValue.isStr())
