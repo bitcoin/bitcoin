7d9ea514a132c478ae96b417291b0f2e852b0e4a
Wladimir J. van der Laan <laanwj@gmail.com>
2016-04-18 15:12:46 +0200

    wallet_ismine.h → script/ismine.h
    Removes conditional dependency of `src/test` on wallet.
    
    Makes multisig and P2SH tests complete without wallet built-in.

--- src/script/ismine.cpp
+++ src/script/ismine.cpp
@@ -3,7 +3,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include "wallet_ismine.h"
+#include "ismine.h"
 
 #include "key.h"
 #include "keystore.h"
