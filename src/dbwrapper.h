74f7b1273c41892058fb2ff99aab878ccd22082a
Wladimir J. van der Laan <laanwj@gmail.com>
2016-04-20 09:05:12 +0200

    dbwrapper: Remove throw keywords in function signatures
    Using throw() specifications in function signatures is not only
    not required in C++, it is considered deprecated for
    [various reasons](https://stackoverflow.com/questions/1055387/throw-keyword-in-functions-signature).
    It is not implemented by any of the common C++ compilers. The usage is
    also inconsistent with the rest of the source code.

--- src/dbwrapper.h
+++ src/dbwrapper.h
@@ -23,7 +23,7 @@ public:
     dbwrapper_error(const std::string& msg) : std::runtime_error(msg) {}
 };
 
-void HandleError(const leveldb::Status& status) throw(dbwrapper_error);
+void HandleError(const leveldb::Status& status);
 
 /** Batch of changes queued to be written to a CDBWrapper */
 class CDBBatch
@@ -180,7 +180,7 @@ public:
     ~CDBWrapper();
 
     template <typename K, typename V>
-    bool Read(const K& key, V& value) const throw(dbwrapper_error)
+    bool Read(const K& key, V& value) const
     {
         CDataStream ssKey(SER_DISK, CLIENT_VERSION);
         ssKey.reserve(ssKey.GetSerializeSize(key));
@@ -206,7 +206,7 @@ public:
     }
 
     template <typename K, typename V>
-    bool Write(const K& key, const V& value, bool fSync = false) throw(dbwrapper_error)
+    bool Write(const K& key, const V& value, bool fSync = false)
     {
         CDBBatch batch(&obfuscate_key);
         batch.Write(key, value);
@@ -214,7 +214,7 @@ public:
     }
 
     template <typename K>
-    bool Exists(const K& key) const throw(dbwrapper_error)
+    bool Exists(const K& key) const
     {
         CDataStream ssKey(SER_DISK, CLIENT_VERSION);
         ssKey.reserve(ssKey.GetSerializeSize(key));
@@ -233,14 +233,14 @@ public:
     }
 
     template <typename K>
-    bool Erase(const K& key, bool fSync = false) throw(dbwrapper_error)
+    bool Erase(const K& key, bool fSync = false)
     {
         CDBBatch batch(&obfuscate_key);
         batch.Erase(key);
         return WriteBatch(batch, fSync);
     }
 
-    bool WriteBatch(CDBBatch& batch, bool fSync = false) throw(dbwrapper_error);
+    bool WriteBatch(CDBBatch& batch, bool fSync = false);
 
     // not available for LevelDB; provide for compatibility with BDB
     bool Flush()
@@ -248,7 +248,7 @@ public:
         return true;
     }
 
-    bool Sync() throw(dbwrapper_error)
+    bool Sync()
     {
         CDBBatch batch(&obfuscate_key);
         return WriteBatch(batch, true);
