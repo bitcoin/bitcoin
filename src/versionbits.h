9cb2c9787673716d0924b91055c862554fd44071
Luke Dashjr <luke-jr+git@utopios.org>
2016-04-23 23:30:20 +0000

    Implement BIP 9 GBT changes
    - BIP9DeploymentInfo struct for static deployment info
    - VersionBitsDeploymentInfo: Avoid C++11ism by commenting parameter names
    - getblocktemplate: Make sure to set deployments in the version if it is LOCKED_IN
    - In this commit, all rules are considered required for clients to support

--- src/versionbits.h
+++ src/versionbits.h
@@ -30,6 +30,13 @@ enum ThresholdState {
 // will either be NULL or a block with (height + 1) % Period() == 0.
 typedef std::map<const CBlockIndex*, ThresholdState> ThresholdConditionCache;
 
+struct BIP9DeploymentInfo {
+    /** Deployment name */
+    const char *name;
+};
+
+extern const struct BIP9DeploymentInfo VersionBitsDeploymentInfo[];
+
 /**
  * Abstract class that implements BIP9-style threshold logic, and caches results.
  */
