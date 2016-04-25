9fd46e8f8cdc7ae573ddd102bea83c40d39a2363
Patrick Strateman <patrick.strateman@gmail.com>
2016-04-24 21:59:46 -0700

    CDataStream::ignore Throw exception instead of assert on negative nSize.
    Previously disk corruption would cause an assert instead of an exception.

--- src/streams.h
+++ src/streams.h
@@ -240,7 +240,9 @@ public:
     CDataStream& ignore(int nSize)
     {
         // Ignore from the beginning of the buffer
-        assert(nSize >= 0);
+        if (nSize < 0) {
+            throw std::ios_base::failure("CDataStream::ignore(): nSize negative");
+        }
         unsigned int nReadPosNext = nReadPos + nSize;
         if (nReadPosNext >= vch.size())
         {
