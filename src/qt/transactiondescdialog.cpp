ab47d268b84e139ae0d18a5ace375e8268712b2d
Wladimir J. van der Laan <laanwj@gmail.com>
2016-04-26 16:01:45 +0200

    qt: Add transaction hash to details window title

--- src/qt/transactiondescdialog.cpp
+++ src/qt/transactiondescdialog.cpp
@@ -14,6 +14,7 @@ TransactionDescDialog::TransactionDescDialog(const QModelIndex &idx, QWidget *pa
     ui(new Ui::TransactionDescDialog)
 {
     ui->setupUi(this);
+    setWindowTitle(tr("Details for %1").arg(idx.data(TransactionTableModel::TxIDRole).toString()));
     QString desc = idx.data(TransactionTableModel::LongDescriptionRole).toString();
     ui->detailText->setHtml(desc);
 }
