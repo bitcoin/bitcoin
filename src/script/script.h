952383e16cdc8d8450dbe52ac6b1b8a14afc71e7
Wladimir J. van der Laan <laanwj@gmail.com>
2016-04-26 13:23:02 +0200

    Merge #7933: Fix OOM when deserializing UTXO entries with invalid length
    1e44169 Add tests for CCoins deserialization (Pieter Wuille)
    5d0434d Fix OOM bug: UTXO entries with invalid script length (Pieter Wuille)
    4bf631e CDataStream::ignore Throw exception instead of assert on negative nSize. (Patrick Strateman)
    4f87af6 Treat overly long scriptPubKeys as unspendable (Pieter Wuille)
    f8e6fb1 Introduce constant for maximum CScript length (Pieter Wuille)

--- src/script/script.h
+++ src/script/script.h
@@ -27,6 +27,9 @@ static const int MAX_OPS_PER_SCRIPT = 201;
 // Maximum number of public keys per multisig
 static const int MAX_PUBKEYS_PER_MULTISIG = 20;
 
+// Maximum script length in bytes
+static const int MAX_SCRIPT_SIZE = 10000;
+
 // Threshold for nLockTime: below this value it is interpreted as block number,
 // otherwise as UNIX timestamp.
 static const unsigned int LOCKTIME_THRESHOLD = 500000000; // Tue Nov  5 00:53:20 1985 UTC
@@ -633,7 +636,7 @@ public:
      */
     bool IsUnspendable() const
     {
-        return (size() > 0 && *begin() == OP_RETURN);
+        return (size() > 0 && *begin() == OP_RETURN) || (size() > MAX_SCRIPT_SIZE);
     }
 
     void clear()
