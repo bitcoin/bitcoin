c730733ae42ad135c2eea851a100936ed68cea25
sidhujag <sidhujag@hotmail.com>
2016-04-29 20:57:33 -0700

    disabled tor default

--- src/torcontrol.h
+++ src/torcontrol.h
@@ -11,7 +11,8 @@
 #include "scheduler.h"
 
 extern const std::string DEFAULT_TOR_CONTROL;
-static const bool DEFAULT_LISTEN_ONION = true;
+// Syscoin, no tor default
+static const bool DEFAULT_LISTEN_ONION = false;
 
 void StartTorControl(boost::thread_group& threadGroup, CScheduler& scheduler);
 void InterruptTorControl();
