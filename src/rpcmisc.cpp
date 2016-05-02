6cb1c3fd76495dfa21e8407803eb5e4e978b677e
sidhujag <sidhujag@hotmail.com>
2016-05-01 22:39:22 -0700

    typo
    Former-commit-id: 29de23358c4912906371d0d355a52bcb9a690a98

--- src/rpcmisc.cpp
+++ src/rpcmisc.cpp
@@ -316,7 +316,7 @@ UniValue createmultisig(const UniValue& params, bool fHelp)
 	CSyscoinAddress v1addr;
 	v1addr.Set(dest, true);
 	CSyscoinAddress v2addr;
-	v2addr.Set(des);
+	v2addr.Set(dest);
     result.push_back(Pair("address", v1addr.ToString()));
 	result.push_back(Pair("v2address", v2addr.ToString()));
     result.push_back(Pair("redeemScript", HexStr(inner.begin(), inner.end())));
