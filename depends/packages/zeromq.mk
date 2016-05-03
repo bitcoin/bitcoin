bd3cbd53330e001603bfd4be4edfbdb9c952bb36
fanquake <fanquake@gmail.com>
2016-05-03 20:52:10 +0800

    [depends] ZeroMQ 4.1.4

--- depends/packages/zeromq.mk
+++ depends/packages/zeromq.mk
@@ -1,11 +1,11 @@
 package=zeromq
-$(package)_version=4.0.7
+$(package)_version=4.1.4
 $(package)_download_path=http://download.zeromq.org
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=e00b2967e074990d0538361cc79084a0a92892df2c6e7585da34e4c61ee47b03
+$(package)_sha256_hash=e99f44fde25c2e4cb84ce440f87ca7d3fe3271c2b8cfbc67d55e4de25e6fe378
 
 define $(package)_set_vars
-  $(package)_config_opts=--without-documentation --disable-shared
+  $(package)_config_opts=--without-documentation --disable-shared --without-libsodium
   $(package)_config_opts_linux=--with-pic
   $(package)_cxxflags=-std=c++11
 endef
@@ -15,11 +15,11 @@ define $(package)_config_cmds
 endef
 
 define $(package)_build_cmds
-  $(MAKE) -C src
+  $(MAKE) libzmq.la
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) -C src DESTDIR=$($(package)_staging_dir) install
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install-libLTLIBRARIES install-includeHEADERS install-pkgconfigDATA
 endef
 
 define $(package)_postprocess_cmds
