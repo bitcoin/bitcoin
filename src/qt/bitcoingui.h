276ce84fd3a9561a11ae4c5a00c71ff44d3a59a9
Jonas Schnelli <dev@jonasschnelli.ch>
2016-05-11 21:08:36 +0200

    [Qt] Disable some menu items during splashscreen/verification state

--- src/qt/bitcoingui.h
+++ src/qt/bitcoingui.h
@@ -72,6 +72,7 @@ public:
 protected:
     void changeEvent(QEvent *e);
     void closeEvent(QCloseEvent *event);
+    void showEvent(QShowEvent *event);
     void dragEnterEvent(QDragEnterEvent *event);
     void dropEvent(QDropEvent *event);
     bool eventFilter(QObject *object, QEvent *event);
