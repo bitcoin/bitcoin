87dfd136948d9834319895dacbcf87e786a16818
Braydon Fuller <braydon@bitpay.com>
2016-05-12 16:16:44 -0400

    rpc: include satoshis in verbose raw transaction

--- src/rpcrawtransaction.cpp
+++ src/rpcrawtransaction.cpp
@@ -88,6 +88,7 @@ void TxToJSON(const CTransaction& tx, const uint256 hashBlock, UniValue& entry)
         const CTxOut& txout = tx.vout[i];
         UniValue out(UniValue::VOBJ);
         out.push_back(Pair("value", ValueFromAmount(txout.nValue)));
+        out.push_back(Pair("valueSat", txout.nValue));
         out.push_back(Pair("n", (int64_t)i));
         UniValue o(UniValue::VOBJ);
         ScriptPubKeyToJSON(txout.scriptPubKey, o, true);
