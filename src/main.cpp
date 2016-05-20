7e908c7b826cedbf29560ce7a668af809ee71524
Gregory Maxwell <greg@xiph.org>
2016-05-20 16:19:26 +0000

    Do not use mempool for GETDATA for tx accepted after the last mempool req.
    The ability to GETDATA a transaction which has not (yet) been relayed
     is a privacy loss vector.
    
    The use of the mempool for this was added as part of the mempool p2p
     message and is only needed to fetch transactions returned by it.

--- src/main.cpp
+++ src/main.cpp
@@ -4503,7 +4503,10 @@ void static ProcessGetData(CNode* pfrom, const Consensus::Params& consensusParam
                 }
                 if (!pushed && inv.type == MSG_TX) {
                     CTransaction tx;
-                    if (mempool.lookup(inv.hash, tx)) {
+                    int64_t txtime;
+                    // To protect privacy, do not answer getdata using the mempool when
+                    // that TX couldn't have been INVed in reply to a MEMPOOL request.
+                    if (mempool.lookup(inv.hash, tx, txtime) && txtime <= pfrom->timeLastMempoolReq) {
                         pfrom->PushMessage(NetMsgType::TX, tx);
                         pushed = true;
                     }
@@ -5902,6 +5905,7 @@ bool SendMessages(CNode* pto)
                         vInv.clear();
                     }
                 }
+                pto->timeLastMempoolReq = GetTime();
             }
 
             // Determine transactions to relay
--- src/txmempool.h
+++ src/txmempool.h
@@ -602,6 +602,7 @@ public:
     }
 
     bool lookup(uint256 hash, CTransaction& result) const;
+    bool lookup(uint256 hash, CTransaction& result, int64_t& time) const;
     bool lookupFeeRate(const uint256& hash, CFeeRate& feeRate) const;
 
     /** Estimate fee rate needed to get into the next nBlocks
