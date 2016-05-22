5d0ca81f7422b7eac0a5129c6fbccad77e36b85d
Gregory Maxwell <greg@xiph.org>
2016-05-22 05:55:15 +0000

    Add recently accepted blocks and txn to AttemptToEvictConnection.
    This protects any not-already-protected peers who were the most
     recent four to relay transactions and most recent four to send
     blocks to us.

--- src/main.h
+++ src/main.h
@@ -215,7 +215,7 @@ void UnregisterNodeSignals(CNodeSignals& nodeSignals);
  * @param[out]  dbp     The already known disk position of pblock, or NULL if not yet stored.
  * @return True if state.IsValid()
  */
-bool ProcessNewBlock(CValidationState& state, const CChainParams& chainparams, const CNode* pfrom, const CBlock* pblock, bool fForceProcessing, const CDiskBlockPos* dbp);
+bool ProcessNewBlock(CValidationState& state, const CChainParams& chainparams, CNode* pfrom, const CBlock* pblock, bool fForceProcessing, const CDiskBlockPos* dbp);
 /** Check whether enough disk space is available for an incoming block */
 bool CheckDiskSpace(uint64_t nAdditionalBytes = 0);
 /** Open a block file (blk?????.dat) */
