baff12b2b9796bd02f5a51d5bd8baad5d8390443
Cory Fields <cory-nospam-@coryfields.com>
2016-05-27 00:00:02 -0400

    net: add nSendBufferMaxSize/nReceiveFloodSize to CConnection::Options

--- src/net.h
+++ src/net.h
@@ -117,6 +117,8 @@ public:
         int nMaxOutbound = 0;
         int nBestHeight = 0;
         CClientUIInterface* uiInterface = nullptr;
+        unsigned int nSendBufferMaxSize = 0;
+        unsigned int nReceiveFloodSize = 0;
     };
     CConnman();
     ~CConnman();
