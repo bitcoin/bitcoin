65ee3324fbd6799241acada1320b684651bad892
Luke Dashjr <luke-jr+git@utopios.org>
2016-06-01 16:47:36 +0000

    getblocktemplate: Explicitly handle the distinction between GBT-affecting softforks vs not

--- src/versionbits.cpp
+++ src/versionbits.cpp
@@ -9,9 +9,11 @@
 const struct BIP9DeploymentInfo VersionBitsDeploymentInfo[Consensus::MAX_VERSION_BITS_DEPLOYMENTS] = {
     {
         /*.name =*/ "testdummy",
+        /*.gbt_force =*/ true,
     },
     {
         /*.name =*/ "csv",
+        /*.gbt_force =*/ true,
     }
 };
 
