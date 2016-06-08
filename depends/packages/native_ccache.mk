fc7b5778f1ea4ebba2df87a45e35041711a74ee8
Wladimir J. van der Laan <laanwj@gmail.com>
2016-06-08 14:24:48 +0200

    Merge #7993: [depends] Bump Freetype, ccache, ZeroMQ, miniupnpc, expat
    6a4cf16 [depends] expat 2.1.1 (fanquake)
    3e0587b [depends] miniupnpc 2.0 (fanquake)
    87b8175 [depends] Latest config.guess & config.sub (fanquake)
    bd3cbd5 [depends] ZeroMQ 4.1.4 (fanquake)
    0385202 [depends] ccache 3.2.5 (fanquake)
    2b2d52e [depends] Freetype 2.6.3 (fanquake)

--- depends/packages/native_ccache.mk
+++ depends/packages/native_ccache.mk
@@ -1,8 +1,8 @@
 package=native_ccache
-$(package)_version=3.2.4
+$(package)_version=3.2.5
 $(package)_download_path=https://samba.org/ftp/ccache
 $(package)_file_name=ccache-$($(package)_version).tar.bz2
-$(package)_sha256_hash=ffeb967edb549e67da0bd5f44f729a2022de9fdde65dfd80d2a7204d7f75332e
+$(package)_sha256_hash=7a553809e90faf9de3a23ee9c5b5f786cfd4836bf502744bedb824a24bee1097
 
 define $(package)_set_vars
 $(package)_config_opts=
