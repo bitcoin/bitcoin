b6443393fde876fa06067d1a61fa147e0a1d8572
Suhas Daftuar <sdaftuar@gmail.com>
2016-06-13 15:23:11 -0400

    Rename deployment (witness -> segwit)

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -755,7 +755,7 @@ UniValue getblockchaininfo(const UniValue& params, bool fHelp)
     softforks.push_back(SoftForkDesc("bip66", 3, tip, consensusParams));
     softforks.push_back(SoftForkDesc("bip65", 4, tip, consensusParams));
     bip9_softforks.push_back(Pair("csv", BIP9SoftForkDesc(consensusParams, Consensus::DEPLOYMENT_CSV)));
-    bip9_softforks.push_back(Pair("witness", BIP9SoftForkDesc(consensusParams, Consensus::DEPLOYMENT_WITNESS)));
+    bip9_softforks.push_back(Pair("segwit", BIP9SoftForkDesc(consensusParams, Consensus::DEPLOYMENT_WITNESS)));
     obj.push_back(Pair("softforks",             softforks));
     obj.push_back(Pair("bip9_softforks", bip9_softforks));
 
