40f782975ed40a0c5804cbd7a2bf6b40365ac78c
Suhas Daftuar <sdaftuar@gmail.com>
2016-06-13 15:14:31 -0400

    p2p-segwit.py: more RPC coverage

--- qa/rpc-tests/p2p-segwit.py
+++ qa/rpc-tests/p2p-segwit.py
@@ -1015,6 +1015,13 @@ class SegWitTest(BitcoinTestFramework):
             assert_equal(wit_block.serialize(False), non_wit_block.serialize())
             assert_equal(wit_block.serialize(True), block.serialize(True))
 
+            # Test size, vsize, cost
+            rpc_details = self.nodes[0].getblock(block.hash, True)
+            assert_equal(rpc_details["size"], len(block.serialize(True)))
+            assert_equal(rpc_details["strippedsize"], len(block.serialize(False)))
+            cost = 3*len(block.serialize(False)) + len(block.serialize(True))
+            assert_equal(rpc_details["cost"], cost)
+
             # Upgraded node should not ask for blocks from unupgraded
             block4 = self.build_next_block(nVersion=4)
             block4.solve()
