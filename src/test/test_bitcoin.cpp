e85671ef96e55bbfde821dc364dd35f97b16abb7
Pieter Wuille <pieter.wuille@gmail.com>
2016-06-18 19:15:03 +0200

    Enable mempool consistency checks in unit tests

--- src/test/test_bitcoin.cpp
+++ src/test/test_bitcoin.cpp
@@ -55,6 +55,7 @@ TestingSetup::TestingSetup(const std::string& chainName) : BasicTestingSetup(cha
         pathTemp = GetTempPath() / strprintf("test_bitcoin_%lu_%i", (unsigned long)GetTime(), (int)(GetRand(100000)));
         boost::filesystem::create_directories(pathTemp);
         mapArgs["-datadir"] = pathTemp.string();
+        mempool.setSanityCheck(1.0);
         pblocktree = new CBlockTreeDB(1 << 20, true);
         pcoinsdbview = new CCoinsViewDB(1 << 23, true);
         pcoinsTip = new CCoinsViewCache(pcoinsdbview);
