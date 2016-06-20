68757db683f882dbefae06ac87ef52a632ff3940
Wladimir J. van der Laan <laanwj@gmail.com>
2016-06-20 13:31:17 +0200

    Merge #8210: [Qt] Bump to Qt5.6.1
    2759597 Only pass -lQt5PlatformSupport if >=Qt5.6 (Jonas Schnelli)
    59d063d Use runtime linking of QT libdbus, use custom/temp. SDK URL (Jonas Schnelli)
    6194d9a Fix bitcoin_qt.m4 and fix-xcb-include-order.patch (Jonas Schnelli)
    f6eb4e2 [depends] OpenSSL 1.0.1k - update config_opts (fanquake)
    f25209a depends: bump OSX toolchain (Cory Fields)

--- depends/hosts/darwin.mk
+++ depends/hosts/darwin.mk
@@ -1,7 +1,7 @@
 OSX_MIN_VERSION=10.7
-OSX_SDK_VERSION=10.9
+OSX_SDK_VERSION=10.11
 OSX_SDK=$(SDK_PATH)/MacOSX$(OSX_SDK_VERSION).sdk
-LD64_VERSION=241.9
+LD64_VERSION=253.9
 darwin_CC=clang -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION)
 darwin_CXX=clang++ -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION) -stdlib=libc++
 
--- src/qt/dash.cpp
+++ src/qt/dash.cpp
@@ -577,6 +577,9 @@ int main(int argc, char *argv[])
     // Generate high-dpi pixmaps
     QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
 #endif
+#if QT_VERSION >= 0x050600
+    QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
 #ifdef Q_OS_MAC
     QApplication::setAttribute(Qt::AA_DontShowIconsInMenus);
 #endif
