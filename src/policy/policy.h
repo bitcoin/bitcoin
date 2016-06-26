c3bd2bb9ac1bb32e23961c5960feeb57b019cb2c
Cannon <cannon@cannon-ciota.info>
2016-06-26 01:25:58 -0500

    Updating default max block size
    Removing artificial limit in max block size from 750KB to 1MB.

--- src/policy/policy.h
+++ src/policy/policy.h
@@ -15,7 +15,7 @@
 class CCoinsViewCache;
 
 /** Default for -blockmaxsize and -blockminsize, which control the range of sizes the mining code will create **/
-static const unsigned int DEFAULT_BLOCK_MAX_SIZE = 750000;
+static const unsigned int DEFAULT_BLOCK_MAX_SIZE = 1000000;
 static const unsigned int DEFAULT_BLOCK_MIN_SIZE = 0;
 /** Default for -blockprioritysize, maximum space for zero/low-fee transactions **/
 static const unsigned int DEFAULT_BLOCK_PRIORITY_SIZE = 0;
