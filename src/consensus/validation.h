fa27c0a2c4545a579bf339e816c3fa785252b7dc
MarcoFalke <falke.marco@gmail.com>
2016-06-29 11:48:51 +0200

    [doc] Fix typos in comments, doxygen: Fix comment syntax

--- src/consensus/validation.h
+++ src/consensus/validation.h
@@ -22,9 +22,9 @@ static const unsigned char REJECT_CHECKPOINT = 0x43;
 class CValidationState {
 private:
     enum mode_state {
-        MODE_VALID,   //! everything ok
-        MODE_INVALID, //! network rule violation (DoS value may be set)
-        MODE_ERROR,   //! run-time error
+        MODE_VALID,   //!< everything ok
+        MODE_INVALID, //!< network rule violation (DoS value may be set)
+        MODE_ERROR,   //!< run-time error
     } mode;
     int nDoS;
     std::string strRejectReason;
