50d388348303d48fe7a46c926420c0844aae90bf
FireWalkerX <firewalkerx@gmail.com>
2016-07-18 10:18:20 +0200

    change collateral to 10000

--- src/activethrone.cpp
+++ src/activethrone.cpp
@@ -1,4 +1,4 @@
-
+u
 #include "core.h"
 #include "throneconfig.h"
 #include "protocol.h"
@@ -463,7 +463,7 @@ vector<COutput> CActiveThrone::SelectCoinsThroneForPubKey(std::string collateral
     if (chainActive.Tip()->nHeight<145000) {
     BOOST_FOREACH(const COutput& out, vCoins)
     {
-        if(out.tx->vout[out.i].scriptPubKey == scriptPubKey && out.tx->vout[out.i].nValue == 250000*COIN) { //exactly
+        if(out.tx->vout[out.i].scriptPubKey == scriptPubKey && out.tx->vout[out.i].nValue == 10000*COIN) { //exactly
         	filteredCoins.push_back(out);
         }
     }
