b6bd5c905828638457dabe0e0d32ff5f7c60721e
pallas1 <pallas1@users.noreply.github.com>
2016-07-23 10:20:06 +0200

    pattern_search call updated

--- src/patternsearch.h
+++ src/patternsearch.h
@@ -2,7 +2,7 @@
 #include "uint256.h"
 
 namespace patternsearch {
-    std::vector< std::pair<uint32_t,uint32_t> > pattern_search( uint256 midHash, char* scratchpad, int totalThreads );
+    std::vector< std::pair<uint32_t,uint32_t> > pattern_search( uint256 midHash, char* scratchpad, int totalThreads, int* minerStopFlag);
     bool pattern_verify( uint256 midHash, uint32_t a, uint32_t b );
 }
 
