1f21d161a590d5f0aaf77ea01cd9d80c33be0a54
Kaz Wesley <keziahw@gmail.com>
2016-07-27 16:27:07 -0700

    prepend license statement to indirectmap
    Add statement about MIT licensing to indirectmap.h. I forgot the license
    preamble when I originally wrote the file.
    
    Github-Pull: #8414
    Rebased-From: d3af342276f29d2bd162628eb4b669599633e39e

--- src/indirectmap.h
+++ src/indirectmap.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2016 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #ifndef BITCOIN_INDIRECTMAP_H
 #define BITCOIN_INDIRECTMAP_H
 
