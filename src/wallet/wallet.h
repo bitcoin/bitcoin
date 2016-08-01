e34e2bbcca336654f92a1542649ce0152f0d1073
Pieter Wuille <pieter.wuille@gmail.com>
2016-08-01 02:07:16 +0200

    Merge #8432: Make CWallet::fFileBacked private.
    29c2d99 Make CWallet::fFileBacked private. (Patrick Strateman)

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -651,6 +651,7 @@ private:
     /* HD derive new child key (on internal or external chain) */
     void DeriveNewChildKey(const CKeyMetadata& metadata, CKey& secretRet, uint32_t nAccountIndex, bool fInternal /*= false*/);
 
+    bool fFileBacked;
 public:
     /*
      * Main wallet lock.
@@ -661,7 +662,6 @@ public:
      */
     mutable CCriticalSection cs_wallet;
 
-    bool fFileBacked;
     const std::string strWalletFile;
 
     void LoadKeyPool(int nIndex, const CKeyPool &keypool)
