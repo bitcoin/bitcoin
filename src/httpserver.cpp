9e9d644f5178da8b5743ea3ddc3b779a2c8d1cd4
Cory Fields <cory-nospam-@coryfields.com>
2016-08-12 14:21:50 -0400

    net: fixup nits

--- src/httpserver.cpp
+++ src/httpserver.cpp
@@ -201,7 +201,7 @@ static bool InitHTTPAllowList()
     CNetAddr localv6;
     LookupHost("127.0.0.1", localv4, false);
     LookupHost("::1", localv6, false);
-    rpc_allow_subnets.push_back(CSubNet(localv4, 8)); // always allow IPv4 local subnet
+    rpc_allow_subnets.push_back(CSubNet(localv4, 8));      // always allow IPv4 local subnet
     rpc_allow_subnets.push_back(CSubNet(localv6));         // always allow IPv6 localhost
     if (mapMultiArgs.count("-rpcallowip")) {
         const std::vector<std::string>& vAllow = mapMultiArgs["-rpcallowip"];
