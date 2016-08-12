df854a5e198d50047100a3a3d37ce15b0512c496
UdjinM6 <UdjinM6@users.noreply.github.com>
2016-08-12 09:57:20 +0400

    Merge #940: Adjust block download timeouts to match Dash avg block time (bitcoin/4)
    9320e5b Adjust block download timeouts to match Dash avg block time (bitcoin/4)

--- src/main.h
+++ src/main.h
@@ -103,10 +103,10 @@ static const unsigned int AVG_ADDRESS_BROADCAST_INTERVAL = 30;
 /** Average delay between trickled inventory broadcasts in seconds.
  *  Blocks, whitelisted receivers, and a random 25% of transactions bypass this. */
 static const unsigned int AVG_INVENTORY_BROADCAST_INTERVAL = 5;
-/** Block download timeout base, expressed in millionths of the block interval (i.e. 10 min) */
-static const int64_t BLOCK_DOWNLOAD_TIMEOUT_BASE = 1000000;
-/** Additional block download timeout per parallel downloading peer (i.e. 5 min) */
-static const int64_t BLOCK_DOWNLOAD_TIMEOUT_PER_PEER = 500000;
+/** Block download timeout base, expressed in millionths of the block interval (i.e. 2.5 min) */
+static const int64_t BLOCK_DOWNLOAD_TIMEOUT_BASE = 250000;
+/** Additional block download timeout per parallel downloading peer (i.e. 1.25 min) */
+static const int64_t BLOCK_DOWNLOAD_TIMEOUT_PER_PEER = 125000;
 
 static const unsigned int DEFAULT_LIMITFREERELAY = 15;
 static const bool DEFAULT_RELAYPRIORITY = true;
