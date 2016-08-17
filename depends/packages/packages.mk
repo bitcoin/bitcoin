e6dca369e642a4df3343774cfff403f0ecdc6ade
Wladimir J. van der Laan <laanwj@gmail.com>
2016-08-17 13:11:40 +0200

    Merge #8504: test: Remove java comparison tool
    eb0c52e travis: Remove hostname hack (Wladimir J. van der Laan)
    3c5251d test: Remove java comparison tool (Wladimir J. van der Laan)

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,5 +1,5 @@
 packages:=boost openssl libevent zeromq
-native_packages := native_ccache native_comparisontool
+native_packages := native_ccache
 
 qt_native_packages = native_protobuf
 qt_packages = qrencode protobuf
