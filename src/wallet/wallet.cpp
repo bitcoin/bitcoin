61a3d9aeb4c806193679a46320781d6c26b5a992
Wladimir J. van der Laan <laanwj@gmail.com>
2016-08-22 10:33:46 +0200

    Merge #8554: trivial: remove unused variable
    4207630 trivial: remove unused variable (Daniel Kraft)

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -4964,7 +4964,6 @@ CWalletKey::CWalletKey(int64_t nExpires)
 int CMerkleTx::SetMerkleBranch(const CBlockIndex* pindex, int posInBlock)
 {
     AssertLockHeld(cs_main);
-    CBlock blockTmp;
 
     // Update the tx's hashBlock
     hashBlock = pindex->GetBlockHash();
