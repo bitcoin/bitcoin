05fa823bf60d21049caebc64dd5c5add8ba4ee10
Wladimir J. van der Laan <laanwj@gmail.com>
2016-08-26 12:13:48 +0200

    wallet: Add BIP125 comment for MAXINT-1/-2 behavior

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -2358,6 +2358,12 @@ bool CWallet::CreateTransaction(const vector<CRecipient>& vecSend, CWalletTx& wt
                 //
                 // Note how the sequence number is set to non-maxint so that
                 // the nLockTime set above actually works.
+                //
+                // BIP125 defines opt-in RBF as any nSequence < maxint-1, so
+                // we use the highest possible value in that range (maxint-2)
+                // to avoid conflicting with other possible uses of nSequence,
+                // and in the spirit of "smallest posible change from prior
+                // behavior."
                 BOOST_FOREACH(const PAIRTYPE(const CWalletTx*,unsigned int)& coin, setCoins)
                     txNew.vin.push_back(CTxIn(coin.first->GetHash(),coin.second,CScript(),
                                               std::numeric_limits<unsigned int>::max() - (fOptIntoFullRbf ? 2 : 1)));
