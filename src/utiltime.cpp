3f31fe5dff631abc9bfa179a7e13e442ff3b7266
Cory Fields <cory-nospam-@coryfields.com>
2016-08-30 20:59:23 -0400

    threads: turn MilliSleep into a wrapper around std::this_thread::sleep_for

--- src/utiltime.cpp
+++ src/utiltime.cpp
@@ -12,6 +12,9 @@
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/thread.hpp>
 
+#include <thread>
+#include <chrono>
+
 using namespace std;
 
 static int64_t nMockTime = 0; //!< For unit testing
@@ -56,20 +59,7 @@ int64_t GetLogTimeMicros()
 
 void MilliSleep(int64_t n)
 {
-
-/**
- * Boost's sleep_for was uninterruptable when backed by nanosleep from 1.50
- * until fixed in 1.52. Use the deprecated sleep method for the broken case.
- * See: https://svn.boost.org/trac/boost/ticket/7238
- */
-#if defined(HAVE_WORKING_BOOST_SLEEP_FOR)
-    boost::this_thread::sleep_for(boost::chrono::milliseconds(n));
-#elif defined(HAVE_WORKING_BOOST_SLEEP)
-    boost::this_thread::sleep(boost::posix_time::milliseconds(n));
-#else
-//should never get here
-#error missing boost sleep implementation
-#endif
+    std::this_thread::sleep_for(std::chrono::milliseconds(n));
 }
 
 std::string DateTimeStrFormat(const char* pszFormat, int64_t nTime)
