702fd2ee21c17bfbc56d2fe6d3fad982fb8412a0
isle2983 <isle2983@yahoo.com>
2016-09-11 13:36:22 -0600

    [copyright] add MIT license headers to .sh scripts where missing
    Years are set according to 'git log' history
    
    Github-Pull: #8700
    Rebased-From: 0766d1cac38d7ea27a6323d7ca206154b2686f9b

--- contrib/verify-commits/gpg.sh
+++ contrib/verify-commits/gpg.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# Copyright (c) 2014-2016 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 INPUT=$(cat /dev/stdin)
 VALID=false
 REVSIG=false
