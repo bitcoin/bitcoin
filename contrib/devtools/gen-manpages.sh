5691cee8ddd15a781bf1730eb9e193101524817f
Wladimir J. van der Laan <laanwj@gmail.com>
2016-09-13 10:25:41 +0200

    Merge #8608: Install manpages via make install, also add some autogenerated manpages
    d19583f improved gen-manpages.sh, includes bitcoin-tx and strips commit tag, now also runs binaries from build dir by default, added variables for more control (nomnombtc)
    09546ca regenerated all manpages with commit tag stripped, also add bitcoin-tx (nomnombtc)
    ae6e754 change help string --enable-man to --disable-man (nomnombtc)
    a32c102 add conditional for --enable-man, default is yes (nomnombtc)
    dc84b6f add doc/man to subdir if configure flag --enable-man is set (nomnombtc)
    00dba72 add doc/man/Makefile.am to include manpages (nomnombtc)
    eb5643b add autogenerated manpages by help2man (nomnombtc)
    6edf2fd add gen-manpages.sh description to README.md (nomnombtc)
    d2cd9c0 add script to generate manpages with help2man (nomnombtc)

---
+++ contrib/devtools/gen-manpages.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+TOPDIR=${TOPDIR:-$(git rev-parse --show-toplevel)}
+SRCDIR=${SRCDIR:-$TOPDIR/src}
+MANDIR=${MANDIR:-$TOPDIR/doc/man}
+
+BITCOIND=${BITCOIND:-$SRCDIR/bitcoind}
+BITCOINCLI=${BITCOINCLI:-$SRCDIR/bitcoin-cli}
+BITCOINTX=${BITCOINTX:-$SRCDIR/bitcoin-tx}
+BITCOINQT=${BITCOINQT:-$SRCDIR/qt/bitcoin-qt}
+
+[ ! -x $BITCOIND ] && echo "$BITCOIND not found or not executable." && exit 1
+
+# The autodetected version git tag can screw up manpage output a little bit
+BTCVER=($($BITCOINCLI --version | head -n1 | awk -F'[ -]' '{ print $6, $7 }'))
+
+# Create a footer file with copyright content.
+# This gets autodetected fine for bitcoind if --version-string is not set,
+# but has different outcomes for bitcoin-qt and bitcoin-cli.
+echo "[COPYRIGHT]" > footer.h2m
+$BITCOIND --version | sed -n '1!p' >> footer.h2m
+
+for cmd in $BITCOIND $BITCOINCLI $BITCOINTX $BITCOINQT; do
+  cmdname="${cmd##*/}"
+  help2man -N --version-string=${BTCVER[0]} --include=footer.h2m -o ${MANDIR}/${cmdname}.1 ${cmd}
+  sed -i "s/\\\-${BTCVER[1]}//g" ${MANDIR}/${cmdname}.1
+done
+
+rm -f footer.h2m
