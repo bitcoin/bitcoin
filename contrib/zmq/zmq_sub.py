45fe44ed27a970ba34f806de7f845b5bbd7c15f1
Wladimir J. van der Laan <laanwj@gmail.com>
2016-09-19 16:55:00 +0200

    Merge #8701: [copyright] add MIT License copyright header to zmq_sub.py
    37a7fe9 [copyright] add MIT License copyright header to zmq_sub.py (isle2983)

--- contrib/zmq/zmq_sub.py
+++ contrib/zmq/zmq_sub.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+# Copyright (c) 2014-2016 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 import array
 import binascii
