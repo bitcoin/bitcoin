d95a9728bec8625d9a747db5557afcfae913f095
Wladimir J. van der Laan <laanwj@gmail.com>
2016-09-19 18:54:19 +0200

    Merge #8676: Add missing copyright headers
    783e930 [copyright] Add missing copyright headers (isle2983)

--- contrib/qt_translations.py
+++ contrib/qt_translations.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+# Copyright (c) 2011 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 # Helpful little script that spits out a comma-separated list of
 # language codes for Qt icons that should be included
--- contrib/spendfrom/setup.py
+++ contrib/spendfrom/setup.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2013 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
 from distutils.core import setup
 setup(name='btcspendfrom',
       version='1.0',
