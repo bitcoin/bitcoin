4daead782b3223ae1a4eaea0d4df6b586fbe1d53
Fabian Olesen <firewalkerx@gmail.com>
2016-09-20 18:31:29 +0200

    fix
    Former-commit-id: c405d6cb8efe43ad0026d7179af836b62fcd4b70

--- src/version.h
+++ src/version.h
@@ -32,7 +32,7 @@ static const int PROTOCOL_VERSION = 70020;
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 70020;
+static const int MIN_PEER_PROTO_VERSION = 70010;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
