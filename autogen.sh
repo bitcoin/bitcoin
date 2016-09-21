1f34c998904e0a3278308e34628df57c49beb567
Luke Dashjr <luke-jr+git@utopios.org>
2016-09-21 22:35:21 +0000

    Add MIT license to autogen.sh and share/genbuild.sh

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# Copyright (c) 2013-2016 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 set -e
 srcdir="$(dirname $0)"
 cd "$srcdir"
