84b0a3f2db44f8ce7093ecb53ef6f8ff1f65cf25
MarcoFalke <falke.marco@gmail.com>
2016-09-21 12:15:18 +0200

    [wallet] Introduce DEFAULT_DISABLE_WALLET

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -59,7 +59,7 @@ static const bool DEFAULT_WALLET_RBF = false;
 //! Largest (in bytes) free transaction we're willing to create
 static const unsigned int MAX_FREE_TRANSACTION_CREATE_SIZE = 1000;
 static const bool DEFAULT_WALLETBROADCAST = true;
-
+static const bool DEFAULT_DISABLE_WALLET = false;
 //! if set, all keys will be derived by using BIP32
 static const bool DEFAULT_USE_HD_WALLET = true;
 
