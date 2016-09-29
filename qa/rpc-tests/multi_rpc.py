d24bafb0648af678752cab9db3559449018e7d4b
Wladimir J. van der Laan <laanwj@gmail.com>
2016-09-29 15:34:44 +0000

    test: Explicitly set encoding to utf8 when opening text files
    These are text files but their encoding does not depend on the locale.
    Not all of them require utf8 but it is better to fix it at something
    to remove potential unpredictability.
    
    This is necessary on FreeBSD where no locale is set by default,
    and apparently Python defaults not only the terminal encoding to the locale
    but that of every text file. So without LOCALE environment it defaults text
    file encoding to ASCII. This causes problems with e.g. `bitcoin.conf`.
    
    Luckily the locale doesn't affect the default encoding for str.encode() and
    bytes.decode() on Python 3, so this is the only change necessary.

--- qa/rpc-tests/multi_rpc.py
+++ qa/rpc-tests/multi_rpc.py
@@ -26,7 +26,7 @@ class HTTPBasicsTest (BitcoinTestFramework):
         #Append rpcauth to bitcoin.conf before initialization
         rpcauth = "rpcauth=rt:93648e835a54c573682c2eb19f882535$7681e9c5b74bdd85e78166031d2058e1069b3ed7ed967c93fc63abba06f31144"
         rpcauth2 = "rpcauth=rt2:f8607b1a88861fac29dfccf9b52ff9f$ff36a0c23c8c62b4846112e50fa888416e94c17bfd4c42f88fd8f55ec6a3137e"
-        with open(os.path.join(self.options.tmpdir+"/node0", "bitcoin.conf"), 'a') as f:
+        with open(os.path.join(self.options.tmpdir+"/node0", "bitcoin.conf"), 'a', encoding='utf8') as f:
             f.write(rpcauth+"\n")
             f.write(rpcauth2+"\n")
 
