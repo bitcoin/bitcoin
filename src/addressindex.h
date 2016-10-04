017f548ea6d89423ef568117447e61dd5707ec42
Braydon Fuller <braydon@bitpay.com>
2016-10-04 14:58:32 -0400

    indexes: adds additional address, spent and timestamp indexes
    Adds new bitcoin.conf configuration options for three new indexes:
    -addressindex=1
    -spentindex=1
    -timestampindex=1
    
    The addressindex records all changes to an address for retrieving txids, balances and
    unspent outputs for addresses. Changes are stored and sorted in block order. Both p2sh
    and p2pkh address types are supported. The index records two sets of key/value pairs.
    The first records all activity and is useful for viewing transaction history and
    all changes. The second is specifically for retrieving unspent outputs by address, and
    is smaller as values are removed once they are spent.
    
    The spentindex has multiple purposes and brings closer together inputs and outputs of
    transactions. The main purpose is to efficiently determine the address and amount of
    an input's previous output. The second purpose is to be able to determine which
    input spent an output.
    
    The timestampindex keeps track of timestamps with block hashes and is useful for searching
    blocks by date instead of by height. This is useful for a block explorer that will give
    search options by date. The index uses logical time correction to make sure that the
    results are sorted in block order. The logical time of a block is actual timestamp of the
    block, unless it is less than (earlier) the previous block's logical time, and in that
    case it is one second greater than the previous block's logical time.
    
    Includes logical time fix by Chethan Krishna
    
    Conflicts:
    	src/main.cpp
    	src/main.h
    	src/txdb.cpp
    	src/txdb.h
    	src/txmempool.h

---
+++ src/addressindex.h
@@ -0,0 +1,82 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2015 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_ADDRESSINDEX_H
+#define BITCOIN_ADDRESSINDEX_H
+
+#include "uint256.h"
+#include "amount.h"
+
+struct CMempoolAddressDelta
+{
+    int64_t time;
+    CAmount amount;
+    uint256 prevhash;
+    unsigned int prevout;
+
+    CMempoolAddressDelta(int64_t t, CAmount a, uint256 hash, unsigned int out) {
+        time = t;
+        amount = a;
+        prevhash = hash;
+        prevout = out;
+    }
+
+    CMempoolAddressDelta(int64_t t, CAmount a) {
+        time = t;
+        amount = a;
+        prevhash.SetNull();
+        prevout = 0;
+    }
+};
+
+struct CMempoolAddressDeltaKey
+{
+    int type;
+    uint160 addressBytes;
+    uint256 txhash;
+    unsigned int index;
+    int spending;
+
+    CMempoolAddressDeltaKey(int addressType, uint160 addressHash, uint256 hash, unsigned int i, int s) {
+        type = addressType;
+        addressBytes = addressHash;
+        txhash = hash;
+        index = i;
+        spending = s;
+    }
+
+    CMempoolAddressDeltaKey(int addressType, uint160 addressHash) {
+        type = addressType;
+        addressBytes = addressHash;
+        txhash.SetNull();
+        index = 0;
+        spending = 0;
+    }
+};
+
+struct CMempoolAddressDeltaKeyCompare
+{
+    bool operator()(const CMempoolAddressDeltaKey& a, const CMempoolAddressDeltaKey& b) const {
+        if (a.type == b.type) {
+            if (a.addressBytes == b.addressBytes) {
+                if (a.txhash == b.txhash) {
+                    if (a.index == b.index) {
+                        return a.spending < b.spending;
+                    } else {
+                        return a.index < b.index;
+                    }
+                } else {
+                    return a.txhash < b.txhash;
+                }
+            } else {
+                return a.addressBytes < b.addressBytes;
+            }
+        } else {
+            return a.type < b.type;
+        }
+    }
+};
+
+#endif // BITCOIN_ADDRESSINDEX_H
--- src/base58.cpp
+++ src/base58.cpp
@@ -267,6 +267,23 @@ CTxDestination CBitcoinAddress::Get() const
         return CNoDestination();
 }
 
+bool CBitcoinAddress::GetIndexKey(uint160& hashBytes, int& type) const
+{
+    if (!IsValid()) {
+        return false;
+    } else if (vchVersion == Params().Base58Prefix(CChainParams::PUBKEY_ADDRESS)) {
+        memcpy(&hashBytes, &vchData[0], 20);
+        type = 1;
+        return true;
+    } else if (vchVersion == Params().Base58Prefix(CChainParams::SCRIPT_ADDRESS)) {
+        memcpy(&hashBytes, &vchData[0], 20);
+        type = 2;
+        return true;
+    }
+
+    return false;
+}
+
 bool CBitcoinAddress::GetKeyID(CKeyID& keyID) const
 {
     if (!IsValid() || vchVersion != Params().Base58Prefix(CChainParams::PUBKEY_ADDRESS))
