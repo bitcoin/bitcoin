c9e69fbf3915fe1187b4c2e77be5ae6b16121194
Jeremy Rubin <jeremy.l.rubin@gmail.com>
2016-10-05 16:58:47 -0400

    Add CuckooCache implementation and replace the sigcache map_type with it
    SQUASHME: Change cuckoocache to only work for powers of two, to avoid mod operator
    SQUASHME: Update Documentation and simplify logarithm logic
    SQUASHME: OSX Build Errors
    SQUASHME: minor Feedback from sipa + bluematt
    SQUASHME: DOCONLY: Clarify a few comments.

--- src/test/test_bitcoin.cpp
+++ src/test/test_bitcoin.cpp
@@ -19,6 +19,7 @@
 #include "ui_interface.h"
 #include "rpc/server.h"
 #include "rpc/register.h"
+#include "script/sigcache.h"
 
 #include "test/testutil.h"
 
@@ -39,6 +40,7 @@ BasicTestingSetup::BasicTestingSetup(const std::string& chainName)
         ECC_Start();
         SetupEnvironment();
         SetupNetworking();
+        InitSignatureCache();
         fPrintToDebugLog = false; // don't want to write to debug.log file
         fCheckBlockIndex = true;
         SelectParams(chainName);
