a4ad37d4ef4bcd81bc9d867b277efdebc86bc2e8
Matt Corallo <git@bluematt.me>
2016-10-15 17:51:05 -0400

    [qa] Build v4 blocks in p2p-compactblocktests
    This fixes an issue in backporting to 0.13 as 0.13 enforces SF
    activation by block version lockin instead of through a hard-coded
    block height.

--- qa/rpc-tests/p2p-compactblocks.py
+++ qa/rpc-tests/p2p-compactblocks.py
@@ -128,6 +128,7 @@ class CompactBlocksTest(BitcoinTestFramework):
         tip = node.getbestblockhash()
         mtp = node.getblockheader(tip)['mediantime']
         block = create_block(int(tip, 16), create_coinbase(height + 1), mtp + 1)
+        block.nVersion = 4
         if segwit:
             add_witness_commitment(block)
         block.solve()
