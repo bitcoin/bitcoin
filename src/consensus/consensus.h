d9f9d00fe3d01c26dc942ba5449fd1bb5ca01856
Iblis Lin <iblis@hs.ntnu.edu.tw>
2016-10-16 22:38:42 +0800

    [consensus] COINBASE_MATURITY = 1
    We now get coin immediately

--- src/consensus/consensus.h
+++ src/consensus/consensus.h
@@ -17,7 +17,7 @@ static const unsigned int MAX_BLOCK_BASE_SIZE = 1000000;
 /** The maximum allowed number of signature check operations in a block (network rule) */
 static const int64_t MAX_BLOCK_SIGOPS_COST = 80000;
 /** Coinbase transaction outputs can only be spent after this number of new blocks (network rule) */
-static const int COINBASE_MATURITY = 100;
+static const int COINBASE_MATURITY = 1;
 
 /** Flags for nSequence and nLockTime locks */
 enum {
