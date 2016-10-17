bf30762394d3965bb4b598c7e399abe5b09bd1cd
Matt Corallo <git@bluematt.me>
2016-10-17 11:25:25 -0400

    Comment that most dnsseeds only support some service bits combos

--- src/net.cpp
+++ src/net.cpp
@@ -1454,6 +1454,7 @@ static std::string GetDNSHost(const CDNSSeedData& data, ServiceFlags* requiredSe
         return data.host;
     }
 
+    // See chainparams.cpp, most dnsseeds only support one or two possible servicebits hostnames
     return strprintf("x%x.%s", *requiredServiceBits, data.host);
 }
 
