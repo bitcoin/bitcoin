685e4c78f8ed3c25043f395125d7fc57d8183fef
Matt Corallo <git@bluematt.me>
2016-10-17 16:29:03 -0400

    Remove bogus assert on number of oubound connections.
    This value can be significantly higher if the users uses addnode
    
    Github-Pull: #8944
    Rebased-From: 1ab21cf344ed0547de5ae679b7e479cb4b1a923b

--- src/net.cpp
+++ src/net.cpp
@@ -1655,7 +1655,6 @@ void ThreadOpenConnections()
                 }
             }
         }
-        assert(nOutbound <= (MAX_OUTBOUND_CONNECTIONS + MAX_FEELER_CONNECTIONS));
 
         // Feeler Connections
         //
