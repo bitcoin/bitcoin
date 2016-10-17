88b23a83843edf0a8f7b7291d2796a79a222cecb
Gregory Maxwell <greg@xiph.org>
2016-10-17 23:08:52 +0000

    Be more aggressive in connecting to peers with relevant services.
    Only allow skipping relevant services until there are four outbound
     connections up.
    
    This avoids quickly filling up with peers lacking the relevant
     services when addrman has few or none of them.

--- src/net.cpp
+++ src/net.cpp
@@ -1675,8 +1675,8 @@ void CConnman::ThreadOpenConnections()
             if (nANow - addr.nLastTry < 600 && nTries < 30)
                 continue;
 
-            // only consider nodes missing relevant services after 40 failed attempts
-            if ((addr.nServices & nRelevantServices) != nRelevantServices && nTries < 40)
+            // only consider nodes missing relevant services after 40 failed attempts and only if less than half the outbound are up.
+            if ((addr.nServices & nRelevantServices) != nRelevantServices && (nTries < 40 || nOutbound >= (nMaxOutbound >> 1)))
                 continue;
 
             // do not allow non-default ports, unless after 50 invalid addresses selected already
