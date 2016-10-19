fdd0638161968eb49084c87cf74547ccb1b2330d
Wladimir J. van der Laan <laanwj@gmail.com>
2016-10-19 18:10:52 +0200

    Merge #8928: Fix init segfault where InitLoadWallet() calls ATMP before genesis
    37aefff Fix init segfault where InitLoadWallet() calls ATMP before genesis (Matt Corallo)

--- src/init.cpp
+++ src/init.cpp
@@ -1682,6 +1682,13 @@ bool AppInit2(boost::thread_group& threadGroup, CScheduler& scheduler)
         uiInterface.NotifyBlockTip.disconnect(BlockNotifyGenesisWait);
     }
 
+#ifdef ENABLE_WALLET
+    // Add wallet transactions that aren't already in a block to mempool
+    // Do this here as mempool requires genesis block to be loaded
+    if (pwalletMain)
+        pwalletMain->ReacceptWalletTransactions();
+#endif
+
     // ********************************************************* Step 11a: setup PrivateSend
     fMasterNode = GetBoolArg("-masternode", false);
     // TODO: masternode should have no wallet
