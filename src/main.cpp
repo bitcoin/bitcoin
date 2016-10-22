ad20cddce2097c6561202777fccd257deb1a9810
Gregory Maxwell <greg@xiph.org>
2016-10-22 05:33:25 +0000

    IBD check uses minimumchain work instead of checkpoints.
    This introduces a 'minimum chain work' chainparam which is intended
     to be the known amount of work in the chain for the network at the
     time of software release.  If you don't have this much work, you're
     not yet caught up.
    
    This is used instead of the count of blocks test from checkpoints.
    
    This criteria is trivial to keep updated as there is no element of
    subjectivity, trust, or position dependence to it. It is also a more
    reliable metric of sync status than a block count.
    
    Github-Pull: #9053
    Rebased-From: fd46136dfaf68a7046cf7b8693824d73ac6b1caf

--- src/main.cpp
+++ src/main.cpp
@@ -1747,7 +1747,9 @@ bool IsInitialBlockDownload()
         return false;
     if (fImporting || fReindex)
         return true;
-    if (fCheckpointsEnabled && chainActive.Height() < Checkpoints::GetTotalBlocksEstimate(chainParams.Checkpoints()))
+    if (chainActive.Tip() == NULL)
+        return true;
+    if (chainActive.Tip()->nChainWork < UintToArith256(chainParams.GetConsensus().nMinimumChainWork))
         return true;
     bool state = (chainActive.Height() < pindexBestHeader->nHeight - 24 * 6 ||
             std::max(chainActive.Tip()->GetBlockTime(), pindexBestHeader->GetBlockTime()) < GetTime() - nMaxTipAge);
@@ -1781,7 +1783,7 @@ void CheckForkWarningConditions()
 {
     AssertLockHeld(cs_main);
     // Before we get past initial download, we cannot reliably alert about forks
-    // (we assume we don't get stuck on a fork before the last checkpoint)
+    // (we assume we don't get stuck on a fork before finishing our initial sync)
     if (IsInitialBlockDownload())
         return;
 
