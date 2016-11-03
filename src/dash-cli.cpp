5adb5b8e8a1d17f76b27f3377f6501aa68b6dac3
Wladimir J. van der Laan <laanwj@gmail.com>
2016-11-03 19:18:22 +0100

    Merge #9073: Trivial: Add common failure cases for rpc server connection failure
    8f329f9 Add common failure cases for rpc server connection failure (instagibbs)

--- src/dash-cli.cpp
+++ src/dash-cli.cpp
@@ -255,7 +255,7 @@ UniValue CallRPC(const string& strMethod, const UniValue& params)
     event_base_free(base);
 
     if (response.status == 0)
-        throw CConnectionFailed(strprintf("couldn't connect to server (%d %s)", response.error, http_errorstring(response.error)));
+        throw CConnectionFailed(strprintf("couldn't connect to server\n(make sure server is running and you are connecting to the correct RPC port: %d %s)", response.error, http_errorstring(response.error)));
     else if (response.status == HTTP_UNAUTHORIZED)
         throw runtime_error("incorrect rpcuser or rpcpassword (authorization failed)");
     else if (response.status >= 400 && response.status != HTTP_BAD_REQUEST && response.status != HTTP_NOT_FOUND && response.status != HTTP_INTERNAL_SERVER_ERROR)
