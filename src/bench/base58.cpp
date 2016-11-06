4b04e32c20b3e00ce30f3555677d9c937ca7482f
isle2983 <isle2983@yahoo.com>
2016-11-06 10:12:50 -0700

    [copyright] copyright header style uniform
    Three categories of modifications:
    
    1)
    
    1 instance of 'The Bitcoin Core developers \n',
    1 instance of 'the Bitcoin Core developers\n',
    3 instances of 'Bitcoin Core Developers\n', and
    12 instances of 'The Bitcoin developers\n'
    
    are made uniform with the 443 instances of 'The Bitcoin Core developers\n'
    
    2)
    
    3 instances of 'BitPay, Inc\.\n' are made uniform with the other 6
    instances of 'BitPay Inc\.\n'
    
    3)
    
    4 instances where there was no '(c)' between the 'Copyright' and the year
    where it deviates from the style of the local directory.

--- src/bench/base58.cpp
+++ src/bench/base58.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2016 the Bitcoin Core developers
+// Copyright (c) 2016 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
