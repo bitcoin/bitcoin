2342b2ac00b3187da27d66aee1d0f025e1cd5c4e
Jonas Schnelli <dev@jonasschnelli.ch>
2016-11-07 14:16:54 +0100

    Merge #9094: qt: Use correct conversion function for boost::path datadir
    e760b30 qt: Use correct conversion function for boost::path datadir (Wladimir J. van der Laan)

--- src/qt/clientmodel.cpp
+++ src/qt/clientmodel.cpp
@@ -7,6 +7,7 @@
 
 #include "bantablemodel.h"
 #include "guiconstants.h"
+#include "guiutil.h"
 #include "peertablemodel.h"
 
 #include "alert.h"
@@ -289,7 +290,7 @@ QString ClientModel::formatClientStartupTime() const
 
 QString ClientModel::dataDir() const
 {
-    return QString::fromStdString(GetDataDir().string());
+    return GUIUtil::boostPathToQString(GetDataDir());
 }
 
 void ClientModel::updateBanlist()
