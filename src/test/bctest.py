9d661f4337ad8a653bf1e5c17770e8f2920c8d53
Wladimir J. van der Laan <laanwj@gmail.com>
2016-11-07 12:51:36 +0100

    Merge #8675: Make copyright header lines uniform
    4b04e32 [copyright] copyright header style uniform (isle2983)

--- src/test/bctest.py
+++ src/test/bctest.py
@@ -1,4 +1,4 @@
-# Copyright 2014 BitPay, Inc.
+# Copyright 2014 BitPay Inc.
 # Copyright 2016 The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
