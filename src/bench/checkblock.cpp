4c9af4350674ee67017e07dd257c2bdf3b3d0c8c
Pavel Janík <Pavel@Janik.cz>
2016-11-10 21:38:07 +0100

    Use better name for local variable to prevent -Wshadow compiler warning

--- src/bench/checkblock.cpp
+++ src/bench/checkblock.cpp
@@ -46,8 +46,8 @@ static void DeserializeAndCheckBlockTest(benchmark::State& state)
         stream >> block;
         assert(stream.Rewind(sizeof(block_bench::block413567)));
 
-        CValidationState state;
-        assert(CheckBlock(block, state, params));
+        CValidationState validationState;
+        assert(CheckBlock(block, validationState, params));
     }
 }
 
