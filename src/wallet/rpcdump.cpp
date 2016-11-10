bdcba6d93308551788a23f75bec194433c30ebfe
Pavel Janík <Pavel@Janik.cz>
2016-11-10 08:11:51 +0100

    Initialize variable to prevent compiler warning

--- src/wallet/rpcdump.cpp
+++ src/wallet/rpcdump.cpp
@@ -1017,7 +1017,7 @@ UniValue importmulti(const JSONRPCRequest& mainRequest)
 
     bool fRunScan = false;
     const int64_t minimumTimestamp = 1;
-    int64_t nLowestTimestamp;
+    int64_t nLowestTimestamp = 0;
 
     if (fRescan && chainActive.Tip()) {
         nLowestTimestamp = chainActive.Tip()->GetBlockTime();
