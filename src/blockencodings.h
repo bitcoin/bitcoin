ffc9c44bb58a8d9739dbd847d8418f8310ec91c5
Pieter Wuille <pieter.wuille@gmail.com>
2016-11-11 13:01:27 -0800

    Make FillBlock consume txn_available to avoid shared_ptr copies

--- src/blockencodings.h
+++ src/blockencodings.h
@@ -202,7 +202,7 @@ public:
 
     ReadStatus InitData(const CBlockHeaderAndShortTxIDs& cmpctblock);
     bool IsTxAvailable(size_t index) const;
-    ReadStatus FillBlock(CBlock& block, const std::vector<CTransactionRef>& vtx_missing) const;
+    ReadStatus FillBlock(CBlock& block, const std::vector<CTransactionRef>& vtx_missing);
 };
 
 #endif
