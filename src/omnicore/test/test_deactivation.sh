d605a1e6e2cb3a8c7032e2e0068c2e2fdf7cf48b
zathras-crypto <zathrasc@gmail.com>
2016-11-12 13:05:53 +1100

    Improve the basic tests to cover MetaDEx refunds too

--- src/omnicore/test/test_deactivation.sh
+++ src/omnicore/test/test_deactivation.sh
@@ -59,7 +59,7 @@ printf "   * Sending the deactivation & checking it was valid... "
 BLOCKS=$($SRCDIR/omnicore-cli --regtest getblockcount)
 TXIDB=$($SRCDIR/omnicore-cli --regtest omni_senddeactivation $ADDR 8)
 $SRCDIR/omnicore-cli --regtest setgenerate true 1 >$NUL
-RESULT=$($SRCDIR/omnicore-cli --regtest omni_gettransaction $TXID | grep valid | cut -c15-)
+RESULT=$($SRCDIR/omnicore-cli --regtest omni_gettransaction $TXIDB | grep valid | cut -c15-)
 if [ $RESULT == "true," ]
   then
     printf "PASS\n"
@@ -68,10 +68,30 @@ if [ $RESULT == "true," ]
     printf "FAIL (result:%s)\n" $RESULT
     FAIL=$((FAIL+1))
 fi
+printf "   * Making sure the orderbook for #3/#4 is now empty... "
+ORDERS=$($SRCDIR/omnicore-cli --regtest omni_getorderbook 3 4 | grep -vF "[" | grep -vF "]" | wc -l)
+if [ $ORDERS == "0" ]
+  then
+    printf "PASS\n"
+    PASS=$((PASS+1))
+  else
+    printf "FAIL (result:%s)\n" $BALANCE
+    FAIL=$((FAIL+1))
+fi
+printf "   * Checking the first trade was refunded... "
+BALANCE=$($SRCDIR/omnicore-cli --regtest omni_getbalance $ADDR 3 | grep balance | cut -d '"' -f4)
+if [ $BALANCE == "10000000" ]
+  then
+    printf "PASS\n"
+    PASS=$((PASS+1))
+  else
+    printf "FAIL (result:%s)\n" $BALANCE
+    FAIL=$((FAIL+1))
+fi
 printf "   * Sending a trade of #3 for #4 & checking it was invalid... "
-TXIDA=$($SRCDIR/omnicore-cli --regtest omni_sendtrade $ADDR 3 2000 4 1.0)
+TXIDC=$($SRCDIR/omnicore-cli --regtest omni_sendtrade $ADDR 3 2000 4 1.0)
 $SRCDIR/omnicore-cli --regtest setgenerate true 1 >$NUL
-RESULT=$($SRCDIR/omnicore-cli --regtest omni_gettransaction $TXIDA | grep valid | cut -c15-)
+RESULT=$($SRCDIR/omnicore-cli --regtest omni_gettransaction $TXIDC | grep valid | cut -c15-)
 if [ $RESULT == "false," ]
   then
     printf "PASS\n"
@@ -81,10 +101,120 @@ if [ $RESULT == "false," ]
     FAIL=$((FAIL+1))
 fi
 
+
+
+printf "\nDeactivating the MetaDEx completely & testing it's now disabled...\n\n"
+printf "   * Sending a trade of #3 for #1 & checking it was valid... "
+TXIDD=$($SRCDIR/omnicore-cli --regtest omni_sendtrade $ADDR 3 2000 1 1.0)
+$SRCDIR/omnicore-cli --regtest setgenerate true 1 >$NUL
+RESULT=$($SRCDIR/omnicore-cli --regtest omni_gettransaction $TXIDD | grep valid | cut -c15-)
+if [ $RESULT == "true," ]
+  then
+    printf "PASS\n"
+    PASS=$((PASS+1))
+  else
+    printf "FAIL (result:%s)\n" $RESULT
+    FAIL=$((FAIL+1))
+fi
+printf "   * Sending a trade of #4 for #1 & checking it was valid... "
+TXIDE=$($SRCDIR/omnicore-cli --regtest omni_sendtrade $ADDR 4 2000 1 1.0)
+$SRCDIR/omnicore-cli --regtest setgenerate true 1 >$NUL
+RESULT=$($SRCDIR/omnicore-cli --regtest omni_gettransaction $TXIDE | grep valid | cut -c15-)
+if [ $RESULT == "true," ]
+  then
+    printf "PASS\n"
+    PASS=$((PASS+1))
+  else
+    printf "FAIL (result:%s)\n" $RESULT
+    FAIL=$((FAIL+1))
+fi
+printf "   * Sending the deactivation & checking it was valid... "
+BLOCKS=$($SRCDIR/omnicore-cli --regtest getblockcount)
+TXIDF=$($SRCDIR/omnicore-cli --regtest omni_senddeactivation $ADDR 2)
+$SRCDIR/omnicore-cli --regtest setgenerate true 1 >$NUL
+RESULT=$($SRCDIR/omnicore-cli --regtest omni_gettransaction $TXIDF | grep valid | cut -c15-)
+if [ $RESULT == "true," ]
+  then
+    printf "PASS\n"
+    PASS=$((PASS+1))
+  else
+    printf "FAIL (result:%s)\n" $RESULT
+    FAIL=$((FAIL+1))
+fi
+printf "   * Making sure the orderbook for #3/#1 is now empty... "
+ORDERSB=$($SRCDIR/omnicore-cli --regtest omni_getorderbook 3 1 | grep -vF "[" | grep -vF "]" | wc -l)
+if [ $ORDERSB == "0" ]
+  then
+    printf "PASS\n"
+    PASS=$((PASS+1))
+  else
+    printf "FAIL (result:%s)\n" $BALANCE
+    FAIL=$((FAIL+1))
+fi
+printf "   * Making sure the orderbook for #4/#1 is now empty... "
+ORDERSC=$($SRCDIR/omnicore-cli --regtest omni_getorderbook 4 1 | grep -vF "[" | grep -vF "]" | wc -l)
+if [ $ORDERSC == "0" ]
+  then
+    printf "PASS\n"
+    PASS=$((PASS+1))
+  else
+    printf "FAIL (result:%s)\n" $BALANCE
+    FAIL=$((FAIL+1))
+fi
+printf "   * Checking the trade for #3/#1 was refunded... "
+BALANCE=$($SRCDIR/omnicore-cli --regtest omni_getbalance $ADDR 3 | grep balance | cut -d '"' -f4)
+if [ $BALANCE == "10000000" ]
+  then
+    printf "PASS\n"
+    PASS=$((PASS+1))
+  else
+    printf "FAIL (result:%s)\n" $BALANCE
+    FAIL=$((FAIL+1))
+fi
+printf "   * Checking the trade for #4/#1 was refunded... "
+BALANCE=$($SRCDIR/omnicore-cli --regtest omni_getbalance $ADDR 4 | grep balance | cut -d '"' -f4)
+if [ $BALANCE == "10000.00000000" ]
+  then
+    printf "PASS\n"
+    PASS=$((PASS+1))
+  else
+    printf "FAIL (result:%s)\n" $BALANCE
+    FAIL=$((FAIL+1))
+fi
+printf "   * Sending a trade of #3 for #1 & checking it was invalid... "
+TXIDG=$($SRCDIR/omnicore-cli --regtest omni_sendtrade $ADDR 3 2000 1 1.0)
+$SRCDIR/omnicore-cli --regtest setgenerate true 1 >$NUL
+RESULT=$($SRCDIR/omnicore-cli --regtest omni_gettransaction $TXIDG | grep valid | cut -c15-)
+if [ $RESULT == "false," ]
+  then
+    printf "PASS\n"
+    PASS=$((PASS+1))
+  else
+    printf "FAIL (result:%s)\n" $RESULT
+    FAIL=$((FAIL+1))
+fi
+printf "   * Sending a trade of #1 for #4 & checking it was invalid... "
+TXIDG=$($SRCDIR/omnicore-cli --regtest omni_sendtrade $ADDR 1 1.0 4 1.0)
+$SRCDIR/omnicore-cli --regtest setgenerate true 1 >$NUL
+RESULT=$($SRCDIR/omnicore-cli --regtest omni_gettransaction $TXIDG | grep valid | cut -c15-)
+if [ $RESULT == "false," ]
+  then
+    printf "PASS\n"
+    PASS=$((PASS+1))
+  else
+    printf "FAIL (result:%s)\n" $RESULT
+    FAIL=$((FAIL+1))
+fi
+
+
+
+
+
+
 printf "\n"
 printf "####################\n"
 printf "#  Summary:        #\n"
-printf "#    Passed = %d    #\n" $PASS
+printf "#    Passed = %d   #\n" $PASS
 printf "#    Failed = %d    #\n" $FAIL
 printf "####################\n"
 printf "\n"
