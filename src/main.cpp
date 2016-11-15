8f88aa03f715788a3c517687a77086b9d142fd21
Cory Fields <cory-nospam-@coryfields.com>
2016-11-15 17:26:32 -0500

    net: Set feelers to disconnect at the end of the version message
    This way we're not relying on messages going out after fDisconnect has been
    set.
    
    This should not cause any real behavioral changes, though feelers should
    arguably disconnect earlier in the process. That can be addressed in a later
    functional change.

--- src/main.cpp
+++ src/main.cpp
@@ -5253,12 +5253,6 @@ bool ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, int64_t
 
     if (strCommand == NetMsgType::VERSION)
     {
-        // Feeler connections exist only to verify if address is online.
-        if (pfrom->fFeeler) {
-            assert(pfrom->fInbound == false);
-            pfrom->fDisconnect = true;
-        }
-
         // Each connection can only send one version message
         if (pfrom->nVersion != 0)
         {
@@ -5373,6 +5367,12 @@ bool ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, int64_t
         int64_t nTimeOffset = nTime - GetTime();
         pfrom->nTimeOffset = nTimeOffset;
         AddTimeData(pfrom->addr, nTimeOffset);
+
+        // Feeler connections exist only to verify if address is online.
+        if (pfrom->fFeeler) {
+            assert(pfrom->fInbound == false);
+            pfrom->fDisconnect = true;
+        }
     }
 
 
