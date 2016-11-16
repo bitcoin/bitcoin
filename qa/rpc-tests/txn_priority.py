bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

QA: Enable testing priority via getblocktemplate as well

qa/rpc-tests/txn_priority.py
@@ -40,10 +40,18 @@ class PriorityTest(BitcoinTestFramework):
     def __init__(self):
         self.num_nodes = 2
 
+    def add_options(self, parser):
+        parser.add_option("--gbt", dest="test_gbt", default=False, action="store_true",
+                          help="Test priorities used by GBT")
+
     def setup_nodes(self):
+        ppopt = []
+        if self.options.test_gbt:
+            ppopt.append('-printpriority')
+
         nodes = []
         nodes.append(start_node(0, self.options.tmpdir, ['-blockmaxsize=0']))
-        nodes.append(start_node(1, self.options.tmpdir, ['-blockminsize=1000000', '-blockmaxsize=1000000']))
+        nodes.append(start_node(1, self.options.tmpdir, ['-blockminsize=1000000', '-blockmaxsize=1000000'] + ppopt))
         return nodes
 
     def setup_network(self, split=False):
@@ -52,10 +60,28 @@ class PriorityTest(BitcoinTestFramework):
         self.is_network_split = False
         self.sync_all()
 
+    def assert_prio(self, txid, starting, current):
+        node = self.nodes[1]
+
+        if self.options.test_gbt:
+            tmpl = node.getblocktemplate({})
+            tmplentry = None
+            for tx in tmpl['transactions']:
+                if tx['hash'] == txid:
+                    tmplentry = tx
+                    break
+            # GBT does not expose starting priority, so we don't check that
+            assert_approximate(tmplentry['priority'], current)
+        else:
+            mempoolentry = node.getrawmempool(True)[txid]
+            assert_approximate(mempoolentry['startingpriority'], starting)
+            assert_approximate(mempoolentry['currentpriority'], current)
+
     def run_test(self):
         node = self.nodes[0]
         miner = self.nodes[1]
         node.generate(50)
+        self.sync_all()
         miner.generate(101)
         self.sync_all()
 
@@ -67,27 +93,23 @@ class PriorityTest(BitcoinTestFramework):
         txdata_b = node.signrawtransaction(txdata_b)['hex']
         txmodsize_b = get_modified_size(node, txdata_b)
         txid_b = node.sendrawtransaction(txdata_b)
+        self.sync_all()
 
-        txid_b_mempoolentry = node.getrawmempool(True)[txid_b]
-        assert_approximate(txid_b_mempoolentry['startingpriority'], txid_b_mempoolentry['currentpriority'])
-        assert_approximate(txid_b_mempoolentry['currentpriority'], 0)
+        self.assert_prio(txid_b, 0, 0)
 
         # Mine only the sendtoaddress transaction
         tmpl = node.getblocktemplate()
         rawblock = solve_template_hex(tmpl, [create_coinbase(tmpl['height']), a2b_hex(node.getrawtransaction(txid_a))])
         assert_equal(node.submitblock(rawblock), None)
+        self.sync_all()
 
-        txid_b_mempoolentry = node.getrawmempool(True)[txid_b]
-        assert_approximate(txid_b_mempoolentry['startingpriority'], 0)
-        assert_approximate(txid_b_mempoolentry['currentpriority'], amt * 1e8 / txmodsize_b)
+        self.assert_prio(txid_b, 0, amt * 1e8 / txmodsize_b)
 
         node.generate(2)
-
-        txid_b_mempoolentry = node.getrawmempool(True)[txid_b]
-        assert_approximate(txid_b_mempoolentry['startingpriority'], 0)
-        assert_approximate(txid_b_mempoolentry['currentpriority'], amt * 1e8 * 3 / txmodsize_b)
-
         self.sync_all()
+
+        self.assert_prio(txid_b, 0, amt * 1e8 * 3 / txmodsize_b)
+
         miner.generate(4)
         self.sync_all()
 
@@ -95,21 +117,20 @@ class PriorityTest(BitcoinTestFramework):
         txdata_c = node.signrawtransaction(txdata_c)['hex']
         txmodsize_c = get_modified_size(node, txdata_c)
         txid_c = node.sendrawtransaction(txdata_c)
-        txid_c_mempoolentry = node.getrawmempool(True)[txid_c]
-        assert_approximate(txid_c_mempoolentry['startingpriority'], txid_c_mempoolentry['currentpriority'])
-        assert_approximate(txid_c_mempoolentry['currentpriority'], (amt - fee) * 1e8 * 4 / txmodsize_c)
+        self.sync_all()
+
+        txid_c_starting_prio = (amt - fee) * 1e8 * 4 / txmodsize_c
+        self.assert_prio(txid_c, txid_c_starting_prio, txid_c_starting_prio)
 
         node.generate(1)
+        self.sync_all()
 
-        txid_c_mempoolentry = node.getrawmempool(True)[txid_c]
-        assert_approximate(txid_c_mempoolentry['startingpriority'], (amt - fee) * 1e8 * 4 / txmodsize_c)
-        assert_approximate(txid_c_mempoolentry['currentpriority'], (amt - fee) * 1e8 * 5 / txmodsize_c)
+        self.assert_prio(txid_c, txid_c_starting_prio, (amt - fee) * 1e8 * 5 / txmodsize_c)
 
         node.generate(2)
+        self.sync_all()
 
-        txid_c_mempoolentry = node.getrawmempool(True)[txid_c]
-        assert_approximate(txid_c_mempoolentry['startingpriority'], (amt - fee) * 1e8 * 4 / txmodsize_c)
-        assert_approximate(txid_c_mempoolentry['currentpriority'], (amt - fee) * 1e8 * 7 / txmodsize_c)
+        self.assert_prio(txid_c, txid_c_starting_prio, (amt - fee) * 1e8 * 7 / txmodsize_c)
 
 if __name__ == '__main__':
     PriorityTest().main()

bitcoin/bitcoin

qa/rpc-tests/txn_priority.py
@@ -41,10 +41,18 @@ class PriorityTest(BitcoinTestFramework):
         super().__init__()
         self.num_nodes = 2
 
+    def add_options(self, parser):
+        parser.add_option("--gbt", dest="test_gbt", default=False, action="store_true",
+                          help="Test priorities used by GBT")
+
     def setup_nodes(self):
+        ppopt = []
+        if self.options.test_gbt:
+            ppopt.append('-printpriority')
+
         nodes = []
         nodes.append(start_node(0, self.options.tmpdir, ['-blockmaxsize=0']))
-        nodes.append(start_node(1, self.options.tmpdir, ['-blockprioritysize=1000000', '-blockmaxsize=1000000']))
+        nodes.append(start_node(1, self.options.tmpdir, ['-blockprioritysize=1000000', '-blockmaxsize=1000000'] + ppopt))
         return nodes
 
     def setup_network(self, split=False):
@@ -53,10 +61,28 @@ class PriorityTest(BitcoinTestFramework):
         self.is_network_split = False
         self.sync_all()
 
+    def assert_prio(self, txid, starting, current):
+        node = self.nodes[1]
+
+        if self.options.test_gbt:
+            tmpl = node.getblocktemplate({})
+            tmplentry = None
+            for tx in tmpl['transactions']:
+                if tx['hash'] == txid:
+                    tmplentry = tx
+                    break
+            # GBT does not expose starting priority, so we don't check that
+            assert_approximate(tmplentry['priority'], current)
+        else:
+            mempoolentry = node.getrawmempool(True)[txid]
+            assert_approximate(mempoolentry['startingpriority'], starting)
+            assert_approximate(mempoolentry['currentpriority'], current)
+
     def run_test(self):
         node = self.nodes[0]
         miner = self.nodes[1]
         node.generate(50)
+        self.sync_all()
         miner.generate(101)
         self.sync_all()
 
@@ -68,27 +94,23 @@ class PriorityTest(BitcoinTestFramework):
         txdata_b = node.signrawtransaction(txdata_b)['hex']
         txmodsize_b = get_modified_size(node, txdata_b)
         txid_b = node.sendrawtransaction(txdata_b)
+        self.sync_all()
 
-        txid_b_mempoolentry = node.getrawmempool(True)[txid_b]
-        assert_approximate(txid_b_mempoolentry['startingpriority'], txid_b_mempoolentry['currentpriority'])
-        assert_approximate(txid_b_mempoolentry['currentpriority'], 0)
+        self.assert_prio(txid_b, 0, 0)
 
         # Mine only the sendtoaddress transaction
         tmpl = node.getblocktemplate()
         rawblock = solve_template_hex(tmpl, [create_coinbase(tmpl['height']), a2b_hex(node.getrawtransaction(txid_a))])
         assert_equal(node.submitblock(rawblock), None)
+        self.sync_all()
 
-        txid_b_mempoolentry = node.getrawmempool(True)[txid_b]
-        assert_approximate(txid_b_mempoolentry['startingpriority'], 0)
-        assert_approximate(txid_b_mempoolentry['currentpriority'], amt * 1e8 / txmodsize_b)
+        self.assert_prio(txid_b, 0, amt * 1e8 / txmodsize_b)
 
         node.generate(2)
-
-        txid_b_mempoolentry = node.getrawmempool(True)[txid_b]
-        assert_approximate(txid_b_mempoolentry['startingpriority'], 0)
-        assert_approximate(txid_b_mempoolentry['currentpriority'], amt * 1e8 * 3 / txmodsize_b)
-
         self.sync_all()
+
+        self.assert_prio(txid_b, 0, amt * 1e8 * 3 / txmodsize_b)
+
         miner.generate(4)
         self.sync_all()
 
@@ -96,21 +118,20 @@ class PriorityTest(BitcoinTestFramework):
         txdata_c = node.signrawtransaction(txdata_c)['hex']
         txmodsize_c = get_modified_size(node, txdata_c)
         txid_c = node.sendrawtransaction(txdata_c)
-        txid_c_mempoolentry = node.getrawmempool(True)[txid_c]
-        assert_approximate(txid_c_mempoolentry['startingpriority'], txid_c_mempoolentry['currentpriority'])
-        assert_approximate(txid_c_mempoolentry['currentpriority'], (amt - fee) * 1e8 * 4 / txmodsize_c)
+        self.sync_all()
+
+        txid_c_starting_prio = (amt - fee) * 1e8 * 4 / txmodsize_c
+        self.assert_prio(txid_c, txid_c_starting_prio, txid_c_starting_prio)
 
         node.generate(1)
+        self.sync_all()
 
-        txid_c_mempoolentry = node.getrawmempool(True)[txid_c]
-        assert_approximate(txid_c_mempoolentry['startingpriority'], (amt - fee) * 1e8 * 4 / txmodsize_c)
-        assert_approximate(txid_c_mempoolentry['currentpriority'], (amt - fee) * 1e8 * 5 / txmodsize_c)
+        self.assert_prio(txid_c, txid_c_starting_prio, (amt - fee) * 1e8 * 5 / txmodsize_c)
 
         node.generate(2)
+        self.sync_all()
 
-        txid_c_mempoolentry = node.getrawmempool(True)[txid_c]
-        assert_approximate(txid_c_mempoolentry['startingpriority'], (amt - fee) * 1e8 * 4 / txmodsize_c)
-        assert_approximate(txid_c_mempoolentry['currentpriority'], (amt - fee) * 1e8 * 7 / txmodsize_c)
+        self.assert_prio(txid_c, txid_c_starting_prio, (amt - fee) * 1e8 * 7 / txmodsize_c)
 
 if __name__ == '__main__':
     PriorityTest().main()