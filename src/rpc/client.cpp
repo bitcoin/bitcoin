a41ead7df37af0e07d884c13c94948ebab97f331
Jonas Schnelli <dev@jonasschnelli.ch>
2016-11-16 09:12:27 +0100

    Add requestblocks - request out-of-band blocks download - RPC call

--- src/rpc/client.cpp
+++ src/rpc/client.cpp
@@ -112,6 +112,8 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "setnetworkactive", 0 },
     { "getmempoolancestors", 1 },
     { "getmempooldescendants", 1 },
+    { "requestblocks", 1 },
+    { "requestblocks", 2 },
 };
 
 class CRPCConvertTable
