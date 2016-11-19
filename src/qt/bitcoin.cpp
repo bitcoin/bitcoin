5204598f8d07d7432d91e9b8781806d2f3d16415
Wladimir J. van der Laan <laanwj@gmail.com>
2016-11-19 14:28:55 +0100

    qt: Avoid shutdownwindow-related memory leak
    Store a reference to the shutdown window on BitcoinApplication,
    so that it will be deleted when exiting the main loop.

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -245,6 +245,7 @@ private:
 #endif
     int returnValue;
     const PlatformStyle *platformStyle;
+    std::unique_ptr<QWidget> shutdownWindow;
 
     void startThread();
 };
@@ -411,7 +412,7 @@ void BitcoinApplication::requestShutdown()
     // Show a simple window indicating shutdown status
     // Do this first as some of the steps may take some time below,
     // for example the RPC console may still be executing a command.
-    ShutdownWindow::showShutdownWindow(window);
+    shutdownWindow.reset(ShutdownWindow::showShutdownWindow(window));
 
     qDebug() << __func__ << ": Requesting shutdown";
     startThread();
