c1235e3f2dd5b01b63b020d1b8f7283e8badaf09
Russell Yanofsky <russ@yanofsky.org>
2016-11-21 11:47:12 -0500

    Add RecursiveDynamicUsage overload for std::shared_ptr
    This simplifies a few usage expressions.

--- src/core_memusage.h
+++ src/core_memusage.h
@@ -63,4 +63,9 @@ static inline size_t RecursiveDynamicUsage(const CBlockLocator& locator) {
     return memusage::DynamicUsage(locator.vHave);
 }
 
+template<typename X>
+static inline size_t RecursiveDynamicUsage(const std::shared_ptr<X>& p) {
+    return p ? memusage::DynamicUsage(p) + RecursiveDynamicUsage(*p) : 0;
+}
+
 #endif // BITCOIN_CORE_MEMUSAGE_H
