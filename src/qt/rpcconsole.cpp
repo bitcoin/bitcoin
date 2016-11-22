8562792095a78fdef2dacc3c5e32c41206df4c87
Luke Dashjr <luke-jr+git@utopios.org>
2016-11-22 23:37:23 +0000

    GUI/RPCConsole: Include importmulti in history sensitive-command filter

--- src/qt/rpcconsole.cpp
+++ src/qt/rpcconsole.cpp
@@ -69,6 +69,7 @@ namespace {
 // don't add private key handling cmd's to the history
 const QStringList historyFilter = QStringList()
     << "importprivkey"
+    << "importmulti"
     << "signmessagewithprivkey"
     << "signrawtransaction"
     << "walletpassphrase"
