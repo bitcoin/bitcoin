d757c895b91f5d9b9c2c02c4d3640b6908acba13
Wladimir J. van der Laan <laanwj@gmail.com>
2016-11-23 07:24:38 +0100

    Merge #9141: Remove unnecessary calls to CheckFinalTx
    4512550 Remove unnecessary calls to CheckFinalTx (Jonas Schnelli)

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -4157,7 +4157,7 @@ std::map<CTxDestination, CAmount> CWallet::GetAddressBalances()
         {
             CWalletTx *pcoin = &walletEntry.second;
 
-            if (!CheckFinalTx(*pcoin) || !pcoin->IsTrusted())
+            if (!pcoin->IsTrusted())
                 continue;
 
             if (pcoin->IsCoinBase() && pcoin->GetBlocksToMaturity() > 0)
