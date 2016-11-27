1708dc8ea334b714c502b0ad0b7e6fe5ac1f62aa
Patrick Strateman <patrick.strateman@gmail.com>
2016-11-26 19:32:30 -0800

    Make nWalletDBUpdated atomic to avoid a potential race.

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -3508,7 +3508,7 @@ bool CWallet::InitLoadWallet()
         walletInstance->ScanForWalletTransactions(pindexRescan, true);
         LogPrintf(" rescan      %15dms\n", GetTimeMillis() - nStart);
         walletInstance->SetBestChain(chainActive.GetLocator());
-        nWalletDBUpdated++;
+        CWalletDB::IncrementUpdateCounter();
 
         // Restore wallet transaction metadata after -zapwallettxes=1
         if (GetBoolArg("-zapwallettxes", false) && GetArg("-zapwallettxes", "1") != "2")
