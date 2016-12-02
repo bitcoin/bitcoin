81e24a0928f6afd9ef8cc0fad9f8b7cf62a5c6e9
Gregory Sanders <gsanders87@gmail.com>
2016-12-02 15:29:20 -0500

    SelectCoinsMinConf: Prefer coins with fewer ancestors

--- src/txmempool.cpp
+++ src/txmempool.cpp
@@ -1142,3 +1142,10 @@ void CTxMemPool::TrimToSize(size_t sizelimit, std::vector<uint256>* pvNoSpendsRe
     if (maxFeeRateRemoved > CFeeRate(0))
         LogPrint("mempool", "Removed %u txn, rolling minimum fee bumped to %s\n", nTxnRemoved, maxFeeRateRemoved.ToString());
 }
+
+bool CTxMemPool::TransactionWithinChainLimit(const uint256& txid, size_t chainLimit) const {
+    LOCK(cs);
+    if (exists(txid) && std::max(mapTx.find(txid)->GetCountWithAncestors(), mapTx.find(txid)->GetCountWithDescendants()) >= chainLimit)
+        return false;
+    return true;
+}
