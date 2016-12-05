7db1cb413589b722a425d78a71b68855d4957f76
Jorge Timón <jtimon@jtimon.cc>
2016-12-05 11:46:27 +0100

    ERROR-unit: Make sure consensus and script flags keep decoupled from each other

--- src/consensus/flags.h
+++ src/consensus/flags.h
@@ -13,15 +13,15 @@ enum
 {
     bitcoinconsensus_SCRIPT_FLAGS_VERIFY_NONE                = 0,
     bitcoinconsensus_SCRIPT_FLAGS_VERIFY_P2SH                = (1U << 0), // evaluate P2SH (BIP16) subscripts
-    bitcoinconsensus_SCRIPT_FLAGS_VERIFY_DERSIG              = (1U << 2), // enforce strict DER (BIP66) compliance
-    bitcoinconsensus_SCRIPT_FLAGS_VERIFY_NULLDUMMY           = (1U << 4), // enforce NULLDUMMY (BIP147)
-    bitcoinconsensus_SCRIPT_FLAGS_VERIFY_CHECKLOCKTIMEVERIFY = (1U << 9), // enable CHECKLOCKTIMEVERIFY (BIP65)
-    bitcoinconsensus_SCRIPT_FLAGS_VERIFY_CHECKSEQUENCEVERIFY = (1U << 10), // enable CHECKSEQUENCEVERIFY (BIP112)
-    bitcoinconsensus_SCRIPT_FLAGS_VERIFY_WITNESS             = (1U << 11), // enable WITNESS (BIP141)
-    bitcoinconsensus_LOCKTIME_VERIFY_SEQUENCE                = (1U << 16), // BIP68: Interpret sequence numbers as relative lock-time constraints
-    bitcoinconsensus_TX_VERIFY_BIP30                         = (1U << 17), // BIP30: See http://r6.ca/blog/20120206T005236Z.html for more information
-    bitcoinconsensus_TX_COINBASE_VERIFY_BIP34                = (1U << 18), // BIP34: Verify coinbase transactions commit to the current height.
-    bitcoinconsensus_LOCKTIME_MEDIAN_TIME_PAST               = (1U << 19), // BIP113: Use GetMedianTimePast() instead of nTime for end point timestamp.
+    bitcoinconsensus_SCRIPT_FLAGS_VERIFY_DERSIG              = (1U << 1), // enforce strict DER (BIP66) compliance
+    bitcoinconsensus_SCRIPT_FLAGS_VERIFY_NULLDUMMY           = (1U << 2), // enforce NULLDUMMY (BIP147)
+    bitcoinconsensus_SCRIPT_FLAGS_VERIFY_CHECKLOCKTIMEVERIFY = (1U << 3), // enable CHECKLOCKTIMEVERIFY (BIP65)
+    bitcoinconsensus_SCRIPT_FLAGS_VERIFY_CHECKSEQUENCEVERIFY = (1U << 4), // enable CHECKSEQUENCEVERIFY (BIP112)
+    bitcoinconsensus_SCRIPT_FLAGS_VERIFY_WITNESS             = (1U << 5), // enable WITNESS (BIP141)
+    bitcoinconsensus_LOCKTIME_VERIFY_SEQUENCE                = (1U << 6), // BIP68: Interpret sequence numbers as relative lock-time constraints
+    bitcoinconsensus_TX_VERIFY_BIP30                         = (1U << 7), // BIP30: See http://r6.ca/blog/20120206T005236Z.html for more information
+    bitcoinconsensus_TX_COINBASE_VERIFY_BIP34                = (1U << 8), // BIP34: Verify coinbase transactions commit to the current height.
+    bitcoinconsensus_LOCKTIME_MEDIAN_TIME_PAST               = (1U << 9), // BIP113: Use GetMedianTimePast() instead of nTime for end point timestamp.
     bitcoinconsensus_SCRIPT_FLAGS_VERIFY_ALL                 = bitcoinconsensus_SCRIPT_FLAGS_VERIFY_P2SH | bitcoinconsensus_SCRIPT_FLAGS_VERIFY_DERSIG |
                                                                bitcoinconsensus_SCRIPT_FLAGS_VERIFY_NULLDUMMY | bitcoinconsensus_SCRIPT_FLAGS_VERIFY_CHECKLOCKTIMEVERIFY |
                                                                bitcoinconsensus_SCRIPT_FLAGS_VERIFY_CHECKSEQUENCEVERIFY | bitcoinconsensus_SCRIPT_FLAGS_VERIFY_WITNESS |
