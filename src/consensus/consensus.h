9cea98e320bfc37a6df373245f916b15c68a7f01
Shitoshi Nakamoto <goku1997@yandex.com>
2016-12-12 02:52:20 -0500

    Add more Ochoness to Bitcoin.

--- src/consensus/consensus.h
+++ src/consensus/consensus.h
@@ -1,5 +1,6 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2015 The Bitcoin Core developers
+// Copyright (c) 2016 The Bitcoin Ocho developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -7,7 +8,8 @@
 #define BITCOIN_CONSENSUS_CONSENSUS_H
 
 /** The maximum allowed size for a serialized block, in bytes (network rule) */
-static const unsigned int MAX_BLOCK_SIZE = 1000000;
+/** Bitcoin Ocho, we multiply by 8 for Ocho */
+static const unsigned int MAX_BLOCK_SIZE = 1000000*8;
 /** The maximum allowed number of signature check operations in a block (network rule) */
 static const unsigned int MAX_BLOCK_SIGOPS = MAX_BLOCK_SIZE/50;
 /** Coinbase transaction outputs can only be spent after this number of new blocks (network rule) */
