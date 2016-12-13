0982d6d3c86311dcb5a317bd995b63d391be5987
Wladimir J. van der Laan <laanwj@gmail.com>
2016-12-13 12:20:26 +0100

    Merge #9305: Refactor: Removed begin/end_ptr functions.
    8c1dbc5 Refactor: Removed begin/end_ptr functions. (Karl-Johan Alm)

--- src/test/script_tests.cpp
+++ src/test/script_tests.cpp
@@ -156,7 +156,7 @@ void DoTest(const CScript& scriptPubKey, const CScript& scriptSig, int flags, co
 #if defined(HAVE_CONSENSUS_LIB)
     CDataStream stream(SER_NETWORK, PROTOCOL_VERSION);
     stream << tx2;
-    BOOST_CHECK_MESSAGE(dashconsensus_verify_script(begin_ptr(scriptPubKey), scriptPubKey.size(), (const unsigned char*)&stream[0], stream.size(), 0, flags, NULL) == expect,message);
+    BOOST_CHECK_MESSAGE(dashconsensus_verify_script(scriptPubKey.data(), scriptPubKey.size(), (const unsigned char*)&stream[0], stream.size(), 0, flags, NULL) == expect,message);
 #endif
 }
 
