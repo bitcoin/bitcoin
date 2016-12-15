ef2c2dd2e00424f178b752ad7c40442497b77d06
Laser9un <laser9un@gmail.com>
2016-12-15 21:10:14 +0200

    Add checkpoint @ block # 4200

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -31,8 +31,9 @@ typedef std::map<int, unsigned int> MapModifierCheckpoints;
 // Hard checkpoints of stake modifiers to ensure they are deterministic
 static std::map<int, unsigned int> mapStakeModifierCheckpoints =
     boost::assign::map_list_of
-        ( 0,   0xfd11f4e7 )
-		( 420, 0x9ccdddaf )
+        ( 0,     0xfd11f4e7 )
+		( 420,   0x9ccdddaf )
+		( 4200,  0x6cfc8d5b )
     ;
 
 // Hard checkpoints of stake modifiers to ensure they are deterministic (testNet)
