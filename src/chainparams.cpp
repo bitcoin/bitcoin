d49f893ebb49cc0f8c89a112eee3df9ee0f68b40
Shitoshi Nakamoto <goku1997@yandex.com>
2016-12-15 07:03:55 -0500

    MOD OCHO MINING BECAUSE U R BITCH JOIN REAL OCHO NETWORK GET OCHO GET RICH GET BITCOIN GET FUCKED

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -155,7 +155,7 @@ public:
 
         vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_main, pnSeed6_main + ARRAYLEN(pnSeed6_main));
 
-        fMiningRequiresPeers = false;
+        fMiningRequiresPeers = true;
         fDefaultConsistencyChecks = false;
         fRequireStandard = true;
         fMineBlocksOnDemand = false;
