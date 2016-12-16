7c2e134446c2300b60c45d034d5b24d59d0da42b
Jonas Schnelli <dev@jonasschnelli.ch>
2016-12-16 16:04:42 +0100

    [Wallet] don't consume non-validated transactions

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -1125,6 +1125,9 @@ void CWallet::SyncTransaction(const CTransaction& tx, const CBlockIndex *pindex,
 {
     LOCK2(cs_main, cs_wallet);
 
+    if (!validated)
+        return;
+
     if (!AddToWalletIfInvolvingMe(tx, pindex, posInBlock, true))
         return; // Not one of ours
 
