20ae9a8458b1c70196d96abfeb0beed2035f8992
Russell Yanofsky <russ@yanofsky.org>
2016-12-19 10:51:45 -0500

    Add documentation describing CWallet::nTimeSmart.
    Most of the text comes from the 2012 Luke Dashjr <luke-jr+git@utopios.org>
    c3f95ef commit message.
    
    Conflicts:
    	src/wallet/wallet.h

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -304,7 +304,16 @@ public:
     mapValue_t mapValue;
     std::vector<std::pair<std::string, std::string> > vOrderForm;
     unsigned int fTimeReceivedIsTxTime;
-    unsigned int nTimeReceived; //! time received by this node
+    unsigned int nTimeReceived; //!< time received by this node
+    /**
+     * Stable timestamp that never changes, and reflects the order a transaction
+     * was added to the wallet. Timestamp is based on the block time for a
+     * transaction added as part of a block, or else the time when the
+     * transaction was received if it wasn't part of a block, with the timestamp
+     * adjusted in both cases so timestamp order matches the order transactions
+     * were added to the wallet. More details can be found in
+     * CWallet::ComputeTimeSmart().
+     */
     unsigned int nTimeSmart;
     char fFromMe;
     std::string strFromAccount;
