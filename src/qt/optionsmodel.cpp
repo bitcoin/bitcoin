6d1600c57ada74aae9d6e46f6f86f052404fdb9e
Laser9un <laser9un@gmail.com>
2016-12-24 20:13:54 +0200

    Add cryptoid.info block explorer URL

--- src/qt/optionsmodel.cpp
+++ src/qt/optionsmodel.cpp
@@ -67,9 +67,9 @@ void OptionsModel::Init()
         if(fTestNet)
             settings.setValue("strThirdPartyTxUrls", "");
         else
-            settings.setValue("strThirdPartyTxUrls", "");
+            settings.setValue("strThirdPartyTxUrls", "https://chainz.cryptoid.info/42/tx.dws?%s");
     }
-    strThirdPartyTxUrls = settings.value("strThirdPartyTxUrls", "").toString();
+    strThirdPartyTxUrls = settings.value("strThirdPartyTxUrls", "https://chainz.cryptoid.info/42/tx.dws?%s").toString();
     fMinimizeToTray = settings.value("fMinimizeToTray", false).toBool();
     fMinimizeOnClose = settings.value("fMinimizeOnClose", false).toBool();
     fCoinControlFeatures = settings.value("fCoinControlFeatures", false).toBool();
