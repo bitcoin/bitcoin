a5d4ed049fdb0a17f225ac41e4a7b8c82a606997
Adrian Gallagher <thrasher@addictionsoftware.com>
2016-12-29 10:28:32 -0800

    Litecoin: Fix copyright year value

--- src/util.cpp
+++ src/util.cpp
@@ -889,7 +889,9 @@ std::string CopyrightHolders(const std::string& strPrefix)
 
     // Check for untranslated substitution to make sure Bitcoin Core copyright is not removed by accident
     if (strprintf(COPYRIGHT_HOLDERS, COPYRIGHT_HOLDERS_SUBSTITUTION).find("Bitcoin Core") == std::string::npos) {
-        strCopyrightHolders += "\n" + strPrefix + "The Bitcoin Core developers";
+        std::string strYear = strPrefix;
+        strYear.replace(strYear.find("2011"), sizeof("2011")-1, "2009");
+        strCopyrightHolders += "\n" + strYear + "The Bitcoin Core developers";
     }
     return strCopyrightHolders;
 }
