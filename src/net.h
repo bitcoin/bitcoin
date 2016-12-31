991955ee81034dc3fbc1c2a8e60c04fc9e0b538c
Cory Fields <cory-nospam-@coryfields.com>
2016-12-31 02:05:32 -0500

    net: add a flag to indicate when a node's send buffer is full
    Similar to the recv flag, but this one indicates whether or not the net's send
    buffer is full.
    
    The socket handler checks the send queue when a new message is added and pauses
    if necessary, and possibly unpauses after each message is drained from its buffer.

--- src/net.h
+++ src/net.h
@@ -358,6 +358,7 @@ private:
 
     NodeId GetNewNodeId();
 
+    size_t SocketSendData(CNode *pnode);
     //!check is the banlist has unwritten changes
     bool BannedSetIsDirty();
     //!set the "dirty" flag for the banlist
@@ -444,7 +445,6 @@ void Discover(boost::thread_group& threadGroup);
 void MapPort(bool fUseUPnP);
 unsigned short GetListenPort();
 bool BindListenPort(const CService &bindAddr, std::string& strError, bool fWhitelisted = false);
-size_t SocketSendData(CNode *pnode);
 
 struct CombinerAll
 {
@@ -652,6 +652,7 @@ public:
 
     const uint64_t nKeyedNetGroup;
     std::atomic_bool fPauseRecv;
+    std::atomic_bool fPauseSend;
 protected:
 
     mapMsgCmdSize mapSendBytesPerMsgCmd;
