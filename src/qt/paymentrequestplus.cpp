1fcc0b6fce71a5ac73bde657075ff4421f2ab5a2
Wladimir J. van der Laan <laanwj@gmail.com>
2017-01-05 10:26:42 +0100

    Merge #9475: Let autoconf detect presence of EVP_MD_CTX_new
    0388afe Let autoconf detect presence of EVP_MD_CTX_new (Luke Dashjr)

--- src/qt/paymentrequestplus.cpp
+++ src/qt/paymentrequestplus.cpp
@@ -160,7 +160,7 @@ bool PaymentRequestPlus::getMerchant(X509_STORE* certStore, QString& merchant) c
         std::string data_to_verify;                     // Everything but the signature
         rcopy.SerializeToString(&data_to_verify);
 
-#if OPENSSL_VERSION_NUMBER >= 0x10100000L
+#if HAVE_DECL_EVP_MD_CTX_NEW
         EVP_MD_CTX *ctx = EVP_MD_CTX_new();
         if (!ctx) throw SSLVerifyError("Error allocating OpenSSL context.");
 #else
@@ -175,7 +175,7 @@ bool PaymentRequestPlus::getMerchant(X509_STORE* certStore, QString& merchant) c
             !EVP_VerifyFinal(ctx, (const unsigned char*)paymentRequest.signature().data(), (unsigned int)paymentRequest.signature().size(), pubkey)) {
             throw SSLVerifyError("Bad signature, invalid payment request.");
         }
-#if OPENSSL_VERSION_NUMBER >= 0x10100000L
+#if HAVE_DECL_EVP_MD_CTX_NEW
         EVP_MD_CTX_free(ctx);
 #endif
 
