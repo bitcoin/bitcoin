e3a24b00a40f5a65ed19ecd889eb4f001bdc13e9
Gregory Maxwell <greg@xiph.org>
2017-01-08 04:05:14 +0000

    Replace FindLatestBefore used by importmuti with FindEarliestAtLeast.
    In spite of the name FindLatestBefore used std::lower_bound to try
     to find the earliest block with a nTime greater or equal to the
     the requested value.  But lower_bound uses bisection and requires
     the input to be ordered with respect to the comparison operation.
     Block times are not well ordered.
    
    I don't know what lower_bound is permitted to do when the data
     is not sufficiently ordered, but it's probably not good.
     (I could construct an implementation which would infinite loop...)
    
    To resolve the issue this commit introduces a maximum-so-far to the
     block indexes and searches that.
    
    For clarity the function is renamed to reflect what it actually does.
    
    An issue that remains is that there is no grace period in importmulti:
     If a address is created at time T and a send is immediately broadcast
     and included by a miner with a slow clock there may not yet have been
     any block with at least time T.
    
    The normal rescan has a grace period of 7200 seconds, but importmulti
     does not.

--- src/wallet/rpcdump.cpp
+++ src/wallet/rpcdump.cpp
@@ -1048,8 +1048,8 @@ UniValue importmulti(const JSONRPCRequest& mainRequest)
         }
     }
 
-    if (fRescan && fRunScan && requests.size() && nLowestTimestamp <= chainActive.Tip()->GetBlockTime()) {
-        CBlockIndex* pindex = nLowestTimestamp > minimumTimestamp ? chainActive.FindLatestBefore(nLowestTimestamp) : chainActive.Genesis();
+    if (fRescan && fRunScan && requests.size() && nLowestTimestamp <= chainActive.Tip()->GetBlockTimeMax()) {
+        CBlockIndex* pindex = nLowestTimestamp > minimumTimestamp ? chainActive.FindEarliestAtLeast(nLowestTimestamp) : chainActive.Genesis();
 
         if (pindex) {
             pwalletMain->ScanForWalletTransactions(pindex, true);
