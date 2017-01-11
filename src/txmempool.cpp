819ea7484e17a88a6bcb3ec602f2c69a871f2377
Pieter Wuille <pieter.wuille@gmail.com>
2017-01-11 13:41:27 -0800

    Merge #9507: Fix use-after-free in CTxMemPool::removeConflicts()
    fe7e593 Fix use-after-free in CTxMemPool::removeConflicts() (Suhas Daftuar)

--- src/txmempool.cpp
+++ src/txmempool.cpp
@@ -716,8 +716,8 @@ void CTxMemPool::removeConflicts(const CTransaction &tx)
             const CTransaction &txConflict = *it->second;
             if (txConflict != tx)
             {
-                removeRecursive(txConflict);
                 ClearPrioritisation(txConflict.GetHash());
+                removeRecursive(txConflict);
             }
         }
     }
