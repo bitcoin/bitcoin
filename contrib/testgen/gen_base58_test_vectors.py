ac4a0953060e34ddd8f4bd7aa28c719d8ed2c068
practicalswift <practicalswift@users.noreply.github.com>
2017-01-12 17:39:50 +0100

    Remove unused Python variables

--- contrib/testgen/gen_base58_test_vectors.py
+++ contrib/testgen/gen_base58_test_vectors.py
@@ -45,7 +45,6 @@ def is_valid(v):
     result = b58decode_chk(v)
     if result is None:
         return False
-    valid = False
     for template in templates:
         prefix = str(bytearray(template[0]))
         suffix = str(bytearray(template[2]))
