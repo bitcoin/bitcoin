fac4c78028d132ee7becc1dc54a0c881ae1a0287
Matt Corallo <git@bluematt.me>
2017-01-12 12:20:11 -0800

    Make PartiallyDownloadedBlock::InitData's second param const

--- src/blockencodings.h
+++ src/blockencodings.h
@@ -201,7 +201,7 @@ public:
     PartiallyDownloadedBlock(CTxMemPool* poolIn) : pool(poolIn) {}
 
     // extra_txn is a list of extra transactions to look at, in <witness hash, reference> form
-    ReadStatus InitData(const CBlockHeaderAndShortTxIDs& cmpctblock, std::vector<std::pair<uint256, CTransactionRef>>& extra_txn);
+    ReadStatus InitData(const CBlockHeaderAndShortTxIDs& cmpctblock, const std::vector<std::pair<uint256, CTransactionRef>>& extra_txn);
     bool IsTxAvailable(size_t index) const;
     ReadStatus FillBlock(CBlock& block, const std::vector<CTransactionRef>& vtx_missing);
 };
