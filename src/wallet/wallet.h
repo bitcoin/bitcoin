0024ce7c0c24add1bdbcd57926751303863c8122
Matt Corallo <git@bluematt.me>
2017-01-17 16:44:25 -0500

    Remove CValidationInterface::UpdatedTransaction
    This removes another callback from block connection logic, making it
    easier to reason about the wallet-RPCs-returns-stale-info issue.
    
    UpdatedTransaction was previously used by the GUI to display
    coinbase transactions only after they have a block built on top of
    them. This worked fine for in most cases, but only worked due to a
    corner case if the user received a coinbase payout in a block
    immediately prior to restart. In that case, the normal process of
    caching the most recent coinbase transaction's hash would not work,
    and instead it would only work because of the on-load -checkblocks
    calling DisconnectBlock and ConnectBlock on the current tip.
    
    In order to make this more robust, a full mapWallet loop after the
    first block which is connected after restart was added.

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -685,6 +685,10 @@ private:
      */
     bool AddWatchOnly(const CScript& dest) override;
 
+    // Used to NotifyTransactionChanged of the previous block's coinbase when
+    // the next block comes in
+    uint256 hashPrevBestCoinbase;
+
 public:
     /*
      * Main wallet lock.
@@ -949,8 +953,6 @@ public:
     bool SetAddressBook(const CTxDestination& address, const std::string& strName, const std::string& purpose);
 
     bool DelAddressBook(const CTxDestination& address);
-
-    void UpdatedTransaction(const uint256 &hashTx) override;
 
     void Inventory(const uint256 &hash) override
     {
