fbf6665bb4274199544cdde69254499c72888528
Johnson Lau <jl2012@xbt.hk>
2017-01-18 16:12:26 +0800

    Skip precompute sighash for transactions without witness

--- src/script/interpreter.h
+++ src/script/interpreter.h
@@ -113,6 +113,7 @@ bool CheckSignatureEncoding(const std::vector<unsigned char> &vchSig, unsigned i
 struct PrecomputedTransactionData
 {
     uint256 hashPrevouts, hashSequence, hashOutputs;
+    bool ready = false;
 
     PrecomputedTransactionData(const CTransaction& tx);
 };
