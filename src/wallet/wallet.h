ebf05db606b47a1dab6f67084bfcbc4119fdb139
Matt Corallo <git@bluematt.me>
2017-01-19 15:47:03 -0500

    Remove dead-code tracking of requests for blocks we generated
    Conflicts:
    	src/validationinterface.cpp
    	src/validationinterface.h

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -1055,11 +1055,6 @@ public:
     }
 
     void GetScriptForMining(boost::shared_ptr<CReserveScript> &script) override;
-    void ResetRequestCount(const uint256 &hash) override
-    {
-        LOCK(cs_wallet);
-        mapRequestCount[hash] = 0;
-    };
     
     unsigned int GetKeyPoolSize()
     {
