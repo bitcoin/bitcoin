dc222f8f634d5bb82d4646f9a17f0d40a60bcf60
Karl-Johan Alm <karljohan-alm@garage.co.jp>
2017-01-23 10:12:02 +0900

    Trivial: Rephrase the definition of difficulty in the code.

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -43,10 +43,15 @@ static CUpdatedBlock latestblock;
 extern void TxToJSON(const CTransaction& tx, const uint256 hashBlock, UniValue& entry);
 void ScriptPubKeyToJSON(const CScript& scriptPubKey, UniValue& out, bool fIncludeHex);
 
+/**
+ * Get the difficulty of the net wrt to the given block index, or the chain tip if
+ * not provided.
+ *
+ * @return A floating point number that is a multiple of the main net minimum
+ * difficulty (4295032833 hashes).
+ */
 double GetDifficulty(const CBlockIndex* blockindex)
 {
-    // Floating point number that is a multiple of the minimum difficulty,
-    // minimum difficulty = 1.0.
     if (blockindex == NULL)
     {
         if (chainActive.Tip() == NULL)
