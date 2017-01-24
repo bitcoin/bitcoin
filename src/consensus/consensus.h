db626ef05a72c8474d35f865d3011040e48193ae
Luke Dashjr <luke-jr+git@utopios.org>
2017-01-24 03:58:08 +0000

    Define, check, and use MIN_TRANSACTION_SIZE as a const
    Two places use the minimal transaction size to calculate a maximum transaction count.
    For compact blocks, the value used is actually incorrect: 10 (no inputs/outputs) instead of 60 (at least one input and output is required), because it is calculated on demand based on the CTransaction() constructor.
    
    This commit defines a const with the correct value (60), and uses it in both locations.
    To ensure the value is always correct, InitSanityCheck makes sure at startup that it in fact matches the size of such a minimal transaction.

--- src/consensus/consensus.h
+++ src/consensus/consensus.h
@@ -16,6 +16,8 @@ static const unsigned int MAX_BLOCK_WEIGHT = 4000000;
 static const unsigned int MAX_BLOCK_BASE_SIZE = 1000000;
 /** The maximum allowed number of signature check operations in a block (network rule) */
 static const int64_t MAX_BLOCK_SIGOPS_COST = 80000;
+/** Smallest possible transaction size */
+static const unsigned int MIN_TRANSACTION_SIZE = 60;
 /** Coinbase transaction outputs can only be spent after this number of new blocks (network rule) */
 static const int COINBASE_MATURITY = 100;
 
