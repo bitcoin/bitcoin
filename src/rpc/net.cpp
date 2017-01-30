daa8582b187585bf8ba92ee651f4e34d776bb3cd
Wladimir J. van der Laan <laanwj@gmail.com>
2017-01-30 10:07:21 +0100

    Merge #9615: Wallet incremental fee
    4b189c1 Change bumpfee result value from 'oldfee' to 'origfee'. (Alex Morcos)
    0c0c63f Introduce WALLET_INCREMENTAL_RELAY_FEE (Alex Morcos)
    e8021ec Use CWallet::GetMinimumFee in bumpfee (Alex Morcos)
    ae9719a Refactor GetMinimumFee to give option of providing targetFee (Alex Morcos)
    fe8e8ef [rpc] Add incremental relay fee to getnetworkinfo (Alex Morcos)
    6b331e6 Fix to have miner test aware of new separate block min tx fee (Alex Morcos)
    de6400d Fix missing use of dustRelayFee (Alex Morcos)
    5b15870 Use incrementalRelayFee for BIP 125 replacement (Alex Morcos)

--- src/rpc/net.cpp
+++ src/rpc/net.cpp
@@ -11,6 +11,7 @@
 #include "net.h"
 #include "net_processing.h"
 #include "netbase.h"
+#include "policy/policy.h"
 #include "protocol.h"
 #include "sync.h"
 #include "timedata.h"
@@ -418,6 +419,7 @@ UniValue getnetworkinfo(const JSONRPCRequest& request)
             "  ,...\n"
             "  ],\n"
             "  \"relayfee\": x.xxxxxxxx,                (numeric) minimum relay fee for non-free transactions in " + CURRENCY_UNIT + "/kB\n"
+            "  \"incrementalfee\": x.xxxxxxxx,          (numeric) minimum fee increment for mempool limiting or BIP 125 replacement in " + CURRENCY_UNIT + "/kB\n"
             "  \"localaddresses\": [                    (array) list of local addresses\n"
             "  {\n"
             "    \"address\": \"xxxx\",                 (string) network address\n"
@@ -448,6 +450,7 @@ UniValue getnetworkinfo(const JSONRPCRequest& request)
     }
     obj.push_back(Pair("networks",      GetNetworksInfo()));
     obj.push_back(Pair("relayfee",      ValueFromAmount(::minRelayTxFee.GetFeePerK())));
+    obj.push_back(Pair("incrementalfee", ValueFromAmount(::incrementalRelayFee.GetFeePerK())));
     UniValue localAddresses(UniValue::VARR);
     {
         LOCK(cs_mapLocalHost);
