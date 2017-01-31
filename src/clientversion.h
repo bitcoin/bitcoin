51765562b01c136572fb729484113f7085e749eb
Andrew Stone <g.andrew.stone@gmail.com>
2017-01-31 10:40:55 -0500

    set build number to 99 indicate dev code

--- src/clientversion.h
+++ src/clientversion.h
@@ -18,7 +18,7 @@
 #define CLIENT_VERSION_MAJOR 1
 #define CLIENT_VERSION_MINOR 0
 #define CLIENT_VERSION_REVISION 0
-#define CLIENT_VERSION_BUILD 0
+#define CLIENT_VERSION_BUILD 99  // BU set version 99 to indicate an unreleased version
 
 //! Set to true for release, false for prerelease or test build
 #define CLIENT_VERSION_IS_RELEASE true
