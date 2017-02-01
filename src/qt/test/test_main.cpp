6ee6532bb9a31a8f99fb2d4309a913495d7cc3e5
Douglas Roark <doug@bloq.com>
2017-01-31 16:19:38 -0800

    Backport “Fix: OSX QT compile: use built-in swap if available, or defer”
    Macs with Protobuf 3 can’t compile the 0.13 branch. Backport the fix from master.

--- src/qt/test/test_main.cpp
+++ src/qt/test/test_main.cpp
@@ -8,6 +8,7 @@
 
 #include "util.h"
 #include "uritests.h"
+#include "compattests.h"
 
 #ifdef ENABLE_WALLET
 #include "paymentservertests.h"
@@ -48,6 +49,8 @@ int main(int argc, char *argv[])
     if (QTest::qExec(&test2) != 0)
         fInvalid = true;
 #endif
-
+    CompatTests test3;
+    if (QTest::qExec(&test3) != 0)
+        fInvalid = true;
     return fInvalid;
 }
