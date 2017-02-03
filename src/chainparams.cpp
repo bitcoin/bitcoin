0ecc7c61eb3bcfb8720fb62717f542c9100f1136
Tim Flynn <tgflynn@users.noreply.github.com>
2017-02-03 12:41:23 -0500

    Adjust start of 12.1 superblocks (#1311)

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -81,7 +81,7 @@ public:
         consensus.nBudgetPaymentsCycleBlocks = 16616; // ~(60*24*30)/2.6, actual number of blocks per month is 200700 / 12 = 16725
         consensus.nBudgetPaymentsWindowBlocks = 100;
         consensus.nBudgetProposalEstablishingTime = 60*60*24;
-        consensus.nSuperblockStartBlock = 600000; // TODO, the block at which 12.1 goes live.
+        consensus.nSuperblockStartBlock = 614820; // The block at which 12.1 goes live (end of final 12.0 budget cycle)
         consensus.nSuperblockCycle = 16616; // ~(60*24*30)/2.6, actual number of blocks per month is 200700 / 12 = 16725
         consensus.nGovernanceMinQuorum = 10;
         consensus.nGovernanceFilterElements = 20000;
