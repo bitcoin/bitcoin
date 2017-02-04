41b8e529b9df9ab6306fee57e972e6b3ab96eaca
Infernoman <jjaauto@hotmail.com>
2017-02-03 20:59:15 -0500

    Update depends folder to dash 0.12.0.x

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,21 +1,23 @@
-packages:=boost openssl libevent zeromq
-native_packages := native_ccache
+packages:=boost openssl
+native_packages := native_ccache native_comparisontool
 
 qt_native_packages = native_protobuf
 qt_packages = qrencode protobuf
 
-qt_x86_64_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
-qt_i686_linux_packages:=$(qt_x86_64_linux_packages)
+qt46_linux_packages = qt46 expat dbus libxcb xcb_proto libXau xproto freetype libX11 xextproto libXext xtrans libICE libSM
+qt5_linux_packages= qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
 
 qt_darwin_packages=qt
 qt_mingw32_packages=qt
+
+qt_linux_$(USE_LINUX_STATIC_QT5):=$(qt5_linux_packages)
+qt_linux_:=$(qt46_linux_packages)
+qt_linux_packages:=$(qt_linux_$(USE_LINUX_STATIC_QT5))
 
 wallet_packages=bdb
 
 upnp_packages=miniupnpc
 
-darwin_native_packages = native_biplist native_ds_store native_mac_alias
-
 ifneq ($(build_os),darwin)
-darwin_native_packages += native_cctools native_cdrkit native_libdmg-hfsplus
+darwin_native_packages=native_libuuid native_openssl native_cctools native_cdrkit native_libdmg-hfsplus
 endif
