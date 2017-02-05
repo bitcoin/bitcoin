f8a9d7ddb7415d9b0ad78035e4b2580b73f1696b
Alastair Clark <jjaauto@hotmail.com>
2017-02-05 16:00:07 -0500

    Subsidy change fork to 1,265,000

--- src/main.cpp
+++ src/main.cpp
@@ -1630,7 +1630,7 @@ int64_t GetBlockValue(int nBits, int nHeight, const CAmount& nFees)
     if(Params().NetworkID() == CBaseChainParams::TESTNET){
         if(nHeight > 100000) nSubsidy -= nSubsidy/10;
     } else {
-        if(nHeight > 1300000) nSubsidy -= nSubsidy/10;
+        if(nHeight > 1265000) nSubsidy -= nSubsidy/10;
     }
 
     return nSubsidy + nFees;
