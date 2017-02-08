c8cee26100c4f5196aff452d75a24ea7f1135595
Luke Dashjr <luke-jr+git@utopios.org>
2017-02-08 04:07:13 +0000

    Qt/Intro: Update block chain size

--- src/qt/intro.cpp
+++ src/qt/intro.cpp
@@ -23,7 +23,7 @@
 
 static const uint64_t GB_BYTES = 1000000000LL;
 /* Minimum free space (in GB) needed for data directory */
-static const uint64_t BLOCK_CHAIN_SIZE = 80;
+static const uint64_t BLOCK_CHAIN_SIZE = 120;
 /* Minimum free space (in GB) needed for data directory when pruned; Does not include prune target */
 static const uint64_t CHAIN_STATE_SIZE = 2;
 /* Total required space (in GB) depending on user choice (prune, not prune) */
