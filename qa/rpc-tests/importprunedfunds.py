3ed4a4493f46e37f49140451b985e3b87d9df659
John Newbery <john@johnnewbery.com>
2017-02-09 16:27:28 -0500

    Return correct error codes in removeprunedfunds().
    The removeprunedfunds() RPC was returning misleading or incorrect error
    codes (for example RPC_INTERNAL_ERROR when the transaction was
    not found in the wallet). This commit fixes those error codes:
    
    - RPC_INTERNAL_ERROR should not be returned for application-level
    errors, only for genuine internal errors such as corrupted data.
    
    This error code has been replaced with RPC_WALLET_ERROR.
    
    This commit also updates the test cases to explicitly test the error code.

--- qa/rpc-tests/importprunedfunds.py
+++ qa/rpc-tests/importprunedfunds.py
@@ -76,12 +76,7 @@ class ImportPrunedFundsTest(BitcoinTestFramework):
         self.sync_all()
 
         #Import with no affiliated address
-        try:
-            self.nodes[1].importprunedfunds(rawtxn1, proof1)
-        except JSONRPCException as e:
-            assert('No addresses' in e.error['message'])
-        else:
-            assert(False)
+        assert_raises_jsonrpc(-5, "No addresses", self.nodes[1].importprunedfunds, rawtxn1, proof1)
 
         balance1 = self.nodes[1].getbalance("", 0, True)
         assert_equal(balance1, Decimal(0))
@@ -112,12 +107,7 @@ class ImportPrunedFundsTest(BitcoinTestFramework):
         assert_equal(address_info['ismine'], True)
 
         #Remove transactions
-        try:
-            self.nodes[1].removeprunedfunds(txnid1)
-        except JSONRPCException as e:
-            assert('does not exist' in e.error['message'])
-        else:
-            assert(False)
+        assert_raises_jsonrpc(-8, "Transaction does not exist in wallet.", self.nodes[1].removeprunedfunds, txnid1)
 
         balance1 = self.nodes[1].getbalance("*", 0, True)
         assert_equal(balance1, Decimal('0.075'))
