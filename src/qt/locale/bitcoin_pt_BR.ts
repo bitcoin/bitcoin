209825bc92b960013326ae1916075183efafc53c
Wladimir J. van der Laan <laanwj@gmail.com>
2017-02-10 08:56:36 +0000

    qt: Periodic translations update

--- src/qt/locale/bitcoin_pt_BR.ts
+++ src/qt/locale/bitcoin_pt_BR.ts
@@ -23,7 +23,7 @@
     </message>
     <message>
         <source>C&amp;lose</source>
-        <translation>Fechar</translation>
+        <translation>&amp;Fechar</translation>
     </message>
     <message>
         <source>Delete the currently selected address from the list</source>
@@ -39,7 +39,7 @@
     </message>
     <message>
         <source>&amp;Delete</source>
-        <translation>&amp;Excluir</translation>
+        <translation>E&amp;xcluir</translation>
     </message>
     <message>
         <source>Choose the address to send coins to</source>
@@ -102,7 +102,7 @@
     <name>AddressTableModel</name>
     <message>
         <source>Label</source>
-        <translation>Rótuo</translation>
+        <translation>Rótulo</translation>
     </message>
     <message>
         <source>Address</source>
@@ -110,7 +110,7 @@
     </message>
     <message>
         <source>(no label)</source>
-        <translation>(sem rótuo)</translation>
+        <translation>(sem rótulo)</translation>
     </message>
 </context>
 <context>
@@ -231,7 +231,7 @@
     <name>BitcoinGUI</name>
     <message>
         <source>Sign &amp;message...</source>
-        <translation>&amp;Assinar mensagem...</translation>
+        <translation>Assinar &amp;mensagem...</translation>
     </message>
     <message>
         <source>Synchronizing with network...</source>
@@ -375,7 +375,7 @@
     </message>
     <message>
         <source>&amp;Show / Hide</source>
-        <translation>&amp;Exibir/Ocultar</translation>
+        <translation>&amp;Exibir / Ocultar</translation>
     </message>
     <message>
         <source>Show or hide the main Window</source>
@@ -479,7 +479,7 @@
     </message>
     <message>
         <source>%1 client</source>
-        <translation>%1</translation>
+        <translation>%1 cliente</translation>
     </message>
     <message>
         <source>Connecting to peers...</source>
@@ -688,7 +688,7 @@
     </message>
     <message>
         <source>(no label)</source>
-        <translation>(sem rótuo)</translation>
+        <translation>(sem rótulo)</translation>
     </message>
     <message>
         <source>change from %1 (%2)</source>
@@ -1361,6 +1361,10 @@
         <translation>Nó/Serviço</translation>
     </message>
     <message>
+        <source>NodeId</source>
+        <translation>ID do nó</translation>
+    </message>
+    <message>
         <source>Ping</source>
         <translation>Ping</translation>
     </message>
@@ -1900,7 +1904,7 @@
     </message>
     <message>
         <source>Label</source>
-        <translation>Rótuo</translation>
+        <translation>Rótulo</translation>
     </message>
     <message>
         <source>Message</source>
@@ -1923,7 +1927,7 @@
     </message>
     <message>
         <source>Label</source>
-        <translation>Rótuo</translation>
+        <translation>Rótulo</translation>
     </message>
     <message>
         <source>Message</source>
@@ -1931,7 +1935,7 @@
     </message>
     <message>
         <source>(no label)</source>
-        <translation>(sem rótuo)</translation>
+        <translation>(sem rótulo)</translation>
     </message>
     <message>
         <source>(no message)</source>
@@ -2210,7 +2214,7 @@
     </message>
     <message>
         <source>(no label)</source>
-        <translation>(sem rótuo)</translation>
+        <translation>(sem rótulo)</translation>
     </message>
 </context>
 <context>
@@ -2665,7 +2669,7 @@
     </message>
     <message>
         <source>Label</source>
-        <translation>Rótuo</translation>
+        <translation>Rótulo</translation>
     </message>
     <message numerus="yes">
         <source>Open for %n more block(s)</source>
@@ -2741,7 +2745,7 @@
     </message>
     <message>
         <source>(no label)</source>
-        <translation>(sem rótuo)</translation>
+        <translation>(sem rótulo)</translation>
     </message>
     <message>
         <source>Transaction status. Hover over this field to show number of confirmations.</source>
@@ -2888,7 +2892,7 @@
     </message>
     <message>
         <source>Label</source>
-        <translation>Rótuo</translation>
+        <translation>Rótulo</translation>
     </message>
     <message>
         <source>Address</source>
@@ -3798,6 +3802,10 @@
         <translation>Retransmitir P2SH não multisig (padrão: %u)</translation>
     </message>
     <message>
+        <source>Send transactions with full-RBF opt-in enabled (default: %u)</source>
+        <translation>Ativar opção full-RBF nas transações enviadas (padrão: %u)</translation>
+    </message>
+    <message>
         <source>Set key pool size to &lt;n&gt; (default: %u)</source>
         <translation>Defina o tamanho da chave para piscina&lt;n&gt; (padrão: %u)</translation>
     </message>
@@ -3819,7 +3827,7 @@
     </message>
     <message>
         <source>Specify pid file (default: %s)</source>
-        <translation>Especificar aqrquivo pid (padrão: %s)</translation>
+        <translation>Especificar arquivo pid (padrão: %s)</translation>
     </message>
     <message>
         <source>Spend unconfirmed change when sending transactions (default: %u)</source>
