37ce6a105177c3165915d9777bfb617e056b0dcc
Luke Dashjr <luke-jr+git@utopios.org>
2017-02-13 22:49:10 +0000

    Bugfix: Qt/Options: Restore persistent "restart required" notice

--- src/qt/optionsdialog.cpp
+++ src/qt/optionsdialog.cpp
@@ -309,6 +309,9 @@ void OptionsDialog::showRestartWarning(bool fPersistent)
 void OptionsDialog::clearStatusLabel()
 {
     ui->statusLabel->clear();
+    if (model && model->isRestartRequired()) {
+        showRestartWarning(true);
+    }
 }
 
 void OptionsDialog::updateProxyValidationState()
@@ -318,7 +321,7 @@ void OptionsDialog::updateProxyValidationState()
     if (pUiProxyIp->isValid() && (!ui->proxyPort->isEnabled() || ui->proxyPort->text().toInt() > 0) && (!ui->proxyPortTor->isEnabled() || ui->proxyPortTor->text().toInt() > 0))
     {
         setOkButtonState(otherProxyWidget->isValid()); //only enable ok button if both proxys are valid
-        ui->statusLabel->clear();
+        clearStatusLabel();
     }
     else
     {
