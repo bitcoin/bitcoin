3ed828344a3e41434b41ff2627b477e9707bf034
Alex Morcos <morcos@chaincode.com>
2017-02-15 09:16:51 -0500

    Make processBlockTx private.

--- src/policy/fees.h
+++ src/policy/fees.h
@@ -210,9 +210,6 @@ public:
     void processBlock(unsigned int nBlockHeight,
                       std::vector<const CTxMemPoolEntry*>& entries);
 
-    /** Process a transaction confirmed in a block*/
-    bool processBlockTx(unsigned int nBlockHeight, const CTxMemPoolEntry* entry);
-
     /** Process a transaction accepted to the mempool*/
     void processTransaction(const CTxMemPoolEntry& entry, bool validFeeEstimate);
 
@@ -252,6 +249,10 @@ private:
 
     unsigned int trackedTxs;
     unsigned int untrackedTxs;
+
+    /** Process a transaction confirmed in a block*/
+    bool processBlockTx(unsigned int nBlockHeight, const CTxMemPoolEntry* entry);
+
 };
 
 class FeeFilterRounder
