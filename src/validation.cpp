9072395e5fddb1f2590138a179ab0868646a38a0
Russell Yanofsky <russ@yanofsky.org>
2017-02-16 10:49:03 -0500

    Return errors from importmulti if complete rescans are not successful
    Github-Pull: #9773
    Rebased-From: e2e2f4c856363bbb0e3b5ba4df225f3754c3db39

--- src/validation.cpp
+++ src/validation.cpp
@@ -3331,7 +3331,7 @@ void PruneOneBlockFile(const int fileNumber)
 }
 
 
-void UnlinkPrunedFiles(std::set<int>& setFilesToPrune)
+void UnlinkPrunedFiles(const std::set<int>& setFilesToPrune)
 {
     for (std::set<int>::iterator it = setFilesToPrune.begin(); it != setFilesToPrune.end(); ++it) {
         CDiskBlockPos pos(*it, 0);
@@ -4161,6 +4161,11 @@ void static CheckBlockIndex(const Consensus::Params& consensusParams)
 std::string CBlockFileInfo::ToString() const
 {
     return strprintf("CBlockFileInfo(blocks=%u, size=%u, heights=%u...%u, time=%s...%s)", nBlocks, nSize, nHeightFirst, nHeightLast, DateTimeStrFormat("%Y-%m-%d", nTimeFirst), DateTimeStrFormat("%Y-%m-%d", nTimeLast));
+}
+
+CBlockFileInfo* GetBlockFileInfo(size_t n)
+{
+    return &vinfoBlockFile.at(n);
 }
 
 ThresholdState VersionBitsTipState(const Consensus::Params& params, Consensus::DeploymentPos pos)
