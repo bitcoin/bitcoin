3301587dc5c7937141282f3799592d1e398495fb
Cory Fields <cory-nospam-@coryfields.com>
2017-02-17 15:04:30 -0500

    boost: remove iostreams includes
    They're unused and produce nasty deprecation warnings

--- src/rpc/server.cpp
+++ src/rpc/server.cpp
@@ -18,8 +18,6 @@
 #include <boost/bind.hpp>
 #include <boost/filesystem.hpp>
 #include <boost/foreach.hpp>
-#include <boost/iostreams/concepts.hpp>
-#include <boost/iostreams/stream.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/signals2/signal.hpp>
 #include <boost/thread.hpp>
