0eb2edebe73f971c92c9b85dc7b0234f21d3cc89
Pieter Wuille <pieter.wuille@gmail.com>
2017-02-17 15:28:28 -0800

    Avoid VLA in hash.h

--- src/hash.h
+++ src/hash.h
@@ -23,23 +23,23 @@ class CHash256 {
 private:
 	CSHA256 sha;
 public:
-	static const size_t OUTPUT_SIZE = CSHA256::OUTPUT_SIZE;
+    static const size_t OUTPUT_SIZE = CSHA256::OUTPUT_SIZE;
 
-	void Finalize(unsigned char hash[OUTPUT_SIZE]) {
-		unsigned char buf[sha.OUTPUT_SIZE];
-		sha.Finalize(buf);
-		sha.Reset().Write(buf, sha.OUTPUT_SIZE).Finalize(hash);
-	}
+    void Finalize(unsigned char hash[OUTPUT_SIZE]) {
+        unsigned char buf[CSHA256::OUTPUT_SIZE];
+        sha.Finalize(buf);
+        sha.Reset().Write(buf, CSHA256::OUTPUT_SIZE).Finalize(hash);
+    }
 
-	CHash256& Write(const unsigned char *data, size_t len) {
-		sha.Write(data, len);
-		return *this;
-	}
+    CHash256& Write(const unsigned char *data, size_t len) {
+        sha.Write(data, len);
+        return *this;
+    }
 
-	CHash256& Reset() {
-		sha.Reset();
-		return *this;
-	}
+    CHash256& Reset() {
+        sha.Reset();
+        return *this;
+    }
 };
 
 /** A hasher class for Syscoin's 160-bit hash (SHA-256 + RIPEMD-160). */
@@ -47,23 +47,23 @@ class CHash160 {
 private:
 	CSHA256 sha;
 public:
-	static const size_t OUTPUT_SIZE = CRIPEMD160::OUTPUT_SIZE;
+    static const size_t OUTPUT_SIZE = CRIPEMD160::OUTPUT_SIZE;
 
-	void Finalize(unsigned char hash[OUTPUT_SIZE]) {
-		unsigned char buf[sha.OUTPUT_SIZE];
-		sha.Finalize(buf);
-		CRIPEMD160().Write(buf, sha.OUTPUT_SIZE).Finalize(hash);
-	}
+    void Finalize(unsigned char hash[OUTPUT_SIZE]) {
+        unsigned char buf[CSHA256::OUTPUT_SIZE];
+        sha.Finalize(buf);
+        CRIPEMD160().Write(buf, CSHA256::OUTPUT_SIZE).Finalize(hash);
+    }
 
-	CHash160& Write(const unsigned char *data, size_t len) {
-		sha.Write(data, len);
-		return *this;
-	}
+    CHash160& Write(const unsigned char *data, size_t len) {
+        sha.Write(data, len);
+        return *this;
+    }
 
-	CHash160& Reset() {
-		sha.Reset();
-		return *this;
-	}
+    CHash160& Reset() {
+        sha.Reset();
+        return *this;
+    }
 };
 
 /** Compute the 256-bit hash of an object. */
