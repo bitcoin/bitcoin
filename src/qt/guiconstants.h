75c7b7fa2348a82716ed6de5250069596db2655a
Peter Tschipper <peter.tschipper@gmailcom>
2017-02-18 11:14:02 -0800

    Change QT org name and domain to reflect BitcoinUnlimited

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -48,8 +48,8 @@ static const int MAX_URI_LENGTH = 255;
 /* Number of frames in spinner animation */
 #define SPINNER_FRAMES 36
 
-#define QAPP_ORG_NAME "Bitcoin"
-#define QAPP_ORG_DOMAIN "bitcoin.org"
+#define QAPP_ORG_NAME "BitcoinUnlimited"
+#define QAPP_ORG_DOMAIN "bitcoinunlimited.info"
 #define QAPP_APP_NAME_DEFAULT "Bitcoin-Qt"
 #define QAPP_APP_NAME_TESTNET "Bitcoin-Qt-testnet"
 #define QAPP_APP_NAME_NOLNET "Bitcoin-Qt-nolimit"   // BU
