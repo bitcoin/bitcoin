3972a8efb2f8fd727ca045dcf89104c9f8a2f846
Pavel Janík <Pavel@Janik.cz>
2017-02-20 18:14:06 +0100

    Read/write mempool.dat as a binary.
    mempool.dat is a binary file and thus it should be read/written as such.
    
    Fixes #9810.
    
    Github-Pull: #9813
    Rebased-From: 171fc91f061d4a980eedfa522e302b8598408cc5

--- src/validation.cpp
+++ src/validation.cpp
@@ -4180,7 +4180,7 @@ static const uint64_t MEMPOOL_DUMP_VERSION = 1;
 bool LoadMempool(void)
 {
     int64_t nExpiryTimeout = GetArg("-mempoolexpiry", DEFAULT_MEMPOOL_EXPIRY) * 60 * 60;
-    FILE* filestr = fopen((GetDataDir() / "mempool.dat").string().c_str(), "r");
+    FILE* filestr = fopen((GetDataDir() / "mempool.dat").string().c_str(), "rb");
     CAutoFile file(filestr, SER_DISK, CLIENT_VERSION);
     if (file.IsNull()) {
         LogPrintf("Failed to open mempool file from disk. Continuing anyway.\n");
@@ -4261,7 +4261,7 @@ void DumpMempool(void)
     int64_t mid = GetTimeMicros();
 
     try {
-        FILE* filestr = fopen((GetDataDir() / "mempool.dat.new").string().c_str(), "w");
+        FILE* filestr = fopen((GetDataDir() / "mempool.dat.new").string().c_str(), "wb");
         if (!filestr) {
             return;
         }
