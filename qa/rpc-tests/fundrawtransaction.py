260c71cbb857d12540a2f53372282d11f2b401a8
MarcoFalke <falke.marco@gmail.com>
2017-02-22 11:29:43 +0100

    qa: Check return code when stopping nodes
    This includes work by jnewbery
    
    Github-Pull: #9824
    Rebased-From: fa4cd2e998e0ebebe283a6243f7f752123a105ab

--- qa/rpc-tests/fundrawtransaction.py
+++ qa/rpc-tests/fundrawtransaction.py
@@ -469,7 +469,9 @@ class RawTransactionsTest(BitcoinTestFramework):
         # locked wallet test
         self.nodes[1].encryptwallet("test")
         self.nodes.pop(1)
-        stop_nodes(self.nodes)
+        stop_node(self.nodes[0], 0)
+        stop_node(self.nodes[1], 2)
+        stop_node(self.nodes[2], 3)
 
         self.nodes = start_nodes(self.num_nodes, self.options.tmpdir)
         # This test is not meant to test fee estimation and we'd like
