85dfeb16853a2e78207251e01470c732d5eb2c79
Marko Bencun <marko.bencun@monetas.net>
2017-02-23 16:38:41 +0900

    Trivial: fix comments referencing AppInit2
    It was refactored into multiple functions in
    6874357684e527ea22b5dce85dee8db87a7500aa.

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -268,7 +268,7 @@ void BitcoinCore::initialize()
 {
     try
     {
-        qDebug() << __func__ << ": Running AppInit2 in thread";
+        qDebug() << __func__ << ": Running initialization in thread";
         if (!AppInitBasicSetup())
         {
             Q_EMIT initializeResult(false);
@@ -457,7 +457,7 @@ void BitcoinApplication::initializeResult(int retval)
     returnValue = retval ? 0 : 1;
     if(retval)
     {
-        // Log this only after AppInit2 finishes, as then logging setup is guaranteed complete
+        // Log this only after AppInitMain finishes, as then logging setup is guaranteed complete
         qWarning() << "Platform customization:" << platformStyle->getName();
 #ifdef ENABLE_WALLET
         PaymentServer::LoadRootCAs();
