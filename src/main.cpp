76ea8e3f2c3e60079f05a06a91db92d6017593d8
Peter Tschipper <peter.tschipper@gmailcom>
2017-02-24 15:33:59 -0800

    Ban node if PROCESS MESSAGESTART failure

--- src/main.cpp
+++ src/main.cpp
@@ -6270,7 +6270,8 @@ bool ProcessMessages(CNode* pfrom)
 
         // Scan for message start
         if (memcmp(msg.hdr.pchMessageStart, chainparams.MessageStart(), MESSAGE_START_SIZE) != 0) {
-            LogPrintf("PROCESSMESSAGE: INVALID MESSAGESTART %s peer=%d\n", SanitizeString(msg.hdr.GetCommand()), pfrom->id);
+            LogPrintf("PROCESSMESSAGE: INVALID MESSAGESTART %s peer=%d ip=%s\n", SanitizeString(msg.hdr.GetCommand()), pfrom->id, pfrom->addrName.c_str());
+            CNode::Ban(pfrom->addr, BanReasonNodeMisbehaving);
             fOk = false;
             break;
         }
