0643e1f46f126973e3a014f691bdb0766a321132
Luke Dashjr <luke-jr+git@utopios.org>
2017-02-24 03:54:50 +0000

    Qt: Network Watch tool
    Simple realtime log of p2p network activity (blocks and transactions only)
    
    - Doesn't begin logging until opened; limited to 0x400 entries (outputs)
    - Automatically scrolls if left at the bottom of the log; maintains position if left elsewhere
    - Memory-efficient circular buffer; CTransaction references become weak after they're 0x200 entries back in the log
    - Search function that selects all matching log entries, including ongoing

--- src/qt/bitcoingui.h
+++ src/qt/bitcoingui.h
@@ -27,6 +27,7 @@
 
 #include <memory>
 
+class GuiNetWatch;
 class NetworkStyle;
 class Notificator;
 class OptionsModel;
@@ -150,6 +151,7 @@ private:
     QAction* backupWalletAction = nullptr;
     QAction* changePassphraseAction = nullptr;
     QAction* aboutQtAction = nullptr;
+    QAction* m_show_netwatch_action = nullptr;
     QAction* openRPCConsoleAction = nullptr;
     QAction* openAction = nullptr;
     QAction* showHelpMessageAction = nullptr;
@@ -172,6 +174,7 @@ private:
     QSystemTrayIcon* trayIcon = nullptr;
     const std::unique_ptr<QMenu> trayIconMenu;
     Notificator* notificator = nullptr;
+    GuiNetWatch* NetWatch = nullptr;
     RPCConsole* rpcConsole = nullptr;
     HelpMessageDialog* helpMessageDialog = nullptr;
     ModalOverlay* modalOverlay = nullptr;
@@ -303,6 +306,7 @@ public Q_SLOTS:
     void optionsClicked();
     /** Show about dialog */
     void aboutClicked();
+    void showNetWatch();
     /** Show debug window */
     void showDebugWindow();
     /** Show debug window and set focus to the console */
