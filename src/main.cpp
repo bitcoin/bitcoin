1203fee1b219736691b7153069612eb2db0326aa
Peter Tschipper <peter.tschipper@gmailcom>
2017-02-25 13:07:18 -0800

    Set a 4 hour ban only on Invalid MessageStart

--- src/main.cpp
+++ src/main.cpp
@@ -6271,7 +6271,7 @@ bool ProcessMessages(CNode* pfrom)
         // Scan for message start
         if (memcmp(msg.hdr.pchMessageStart, chainparams.MessageStart(), MESSAGE_START_SIZE) != 0) {
             LogPrintf("PROCESSMESSAGE: INVALID MESSAGESTART %s peer=%d ip=%s\n", SanitizeString(msg.hdr.GetCommand()), pfrom->id, pfrom->addrName.c_str());
-            CNode::Ban(pfrom->addr, BanReasonNodeMisbehaving);
+            CNode::Ban(pfrom->addr, BanReasonNodeMisbehaving, 4*60*60); // ban for 4 hours
             fOk = false;
             break;
         }
