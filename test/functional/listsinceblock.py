ee97293c6833b97fb7900c86f94d2e3d39776723
Karl-Johan Alm <karljohan-alm@garage.co.jp>
2017-02-26 18:26:34 -0800

    [qa] listsinceblock.py updated to include a 3-chain version of testreorg.

--- test/functional/listsinceblock.py
+++ test/functional/listsinceblock.py
@@ -12,9 +12,9 @@ class ListSinceBlockTest (BitcoinTestFramework):
     def __init__(self):
         super().__init__()
         self.setup_clean_chain = True
-        self.num_nodes = 4
+        self.num_nodes = 6
 
-    def run_test (self):
+    def test_reorg(self):
         '''
         `listsinceblock` did not behave correctly when handed a block that was
         no longer in the main chain:
@@ -76,6 +76,51 @@ class ListSinceBlockTest (BitcoinTestFramework):
                 found = True
                 break
         assert_equal(found, True)
+
+    def test_reorg3(self):
+        '''
+        Same as above, except there are now three conflicting chains (the middle
+        one being the longest).
+        '''
+
+        assert_equal(self.is_network_split, False)
+        assert_equal(len(self.nodes), 6)
+        self.sync_all()
+
+        # Split network into three chains
+        self.split_network([2, 4])
+        assert_equal(len(self.chains), 3)
+        assert_equal(len(self.chains[0]), 2)
+        assert_equal(len(self.chains[1]), 2)
+        assert_equal(len(self.chains[2]), 2)
+        assert_equal(self.is_network_split, True)
+
+        # send to nodes[0] from nodes[2]
+        senttx = self.nodes[2].sendtoaddress(self.nodes[0].getnewaddress(), 1)
+
+        # generate on all sides
+        lastblockhash = self.nodes[1].generate(6)[5]
+        self.nodes[2].generate(8)
+        self.nodes[4].generate(7)
+        self.log.info('lastblockhash=%s' % (lastblockhash))
+
+        self.sync_all()
+
+        self.join_network()
+        assert_equal(len(self.chains), 1)
+
+        # listsinceblock(lastblockhash) should now include tx, as seen from nodes[0]
+        lsbres = self.nodes[0].listsinceblock(lastblockhash)
+        found = False
+        for tx in lsbres['transactions']:
+            if tx['txid'] == senttx:
+                found = True
+                break
+        assert_equal(found, True)
+
+    def run_test(self):
+        self.test_reorg()
+        self.test_reorg3()
 
 if __name__ == '__main__':
     ListSinceBlockTest().main()
