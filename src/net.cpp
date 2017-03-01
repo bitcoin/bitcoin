39779235530ede880d408e202d88ab89415f3924
Wladimir J. van der Laan <laanwj@gmail.com>
2017-03-01 11:16:11 +0100

    Merge #9861: Trivial: Debug log ambiguity fix for peer addrs
    6d37ee8 Trivial: Debug log ambiguity fix for peer addrs (keystrike)
    
    Tree-SHA512: 9605f12bffd067427a206655c29b0b42689e9b28c313d1f6d6b0ef71351277b98b40f2e2a2ec0ac83c095a9f6f54e7a87762da400a56d2a833f8c2d14e5ac8d5

--- src/net.cpp
+++ src/net.cpp
@@ -2323,8 +2323,9 @@ bool CConnman::Start(CScheduler& scheduler, std::string& strNodeError, Options c
     SetBestHeight(connOptions.nBestHeight);
 
     clientInterface = connOptions.uiInterface;
-    if (clientInterface)
-        clientInterface->InitMessage(_("Loading addresses..."));
+    if (clientInterface) {
+        clientInterface->InitMessage(_("Loading P2P addresses..."));
+    }
     // Load addresses from peers.dat
     int64_t nStart = GetTimeMillis();
     {
