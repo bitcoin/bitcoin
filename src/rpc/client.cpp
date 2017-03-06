ce4c176a0ace8aa86a6eabef3246e242529442c5
sidhujag <sidhujag@hotmail.com>
2017-03-06 11:29:40 -0800

    fix for multisig alias params
    Former-commit-id: a18d0983061bef7aee9c33e48cf8d4cba636793b

--- src/rpc/client.cpp
+++ src/rpc/client.cpp
@@ -104,6 +104,8 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "setban", 3 },
     { "getmempoolancestors", 1 },
     { "getmempooldescendants", 1 },
+    { "aliasnew", 9 },
+    { "aliasupdate", 10 },
 };
 
 class CRPCConvertTable
