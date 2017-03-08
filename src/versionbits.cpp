abe7b3d3abe10e3554b770f40824174b3b217490
Suhas Daftuar <sdaftuar@gmail.com>
2017-03-08 15:56:59 -0500

    Don't require segwit in getblocktemplate for segwit signalling or mining
    Segwit's version bit will be signalled for all invocations of CreateNewBlock,
    and not specifying segwit only will cause CreateNewBlock to skip transactions
    with witness from being selected.

--- src/versionbits.cpp
+++ src/versionbits.cpp
@@ -17,7 +17,7 @@ const struct BIP9DeploymentInfo VersionBitsDeploymentInfo[Consensus::MAX_VERSION
     },
     {
         /*.name =*/ "segwit",
-        /*.gbt_force =*/ false,
+        /*.gbt_force =*/ true,
     }
 };
 
