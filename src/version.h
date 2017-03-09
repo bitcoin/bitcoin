67c68df1339a96d82db9c6a61a7b6df4e46285f6
sidhujag <sidhujag@hotmail.com>
2017-03-09 13:50:30 -0800

    update proto version
    Former-commit-id: 097dd8b2b9e59c84225634f2cb8d471f6bd8eca8

--- src/version.h
+++ src/version.h
@@ -9,7 +9,7 @@
  * network protocol versioning
  */
 // SYSCOIN
-static const int PROTOCOL_VERSION = 70029;
+static const int PROTOCOL_VERSION = 70030;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -19,7 +19,7 @@ static const int GETHEADERS_VERSION = 31800;
 
 //! disconnect from peers older than this proto version
 // SYSCOIN
-static const int MIN_PEER_PROTO_VERSION = 70028;
+static const int MIN_PEER_PROTO_VERSION = 70029;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
