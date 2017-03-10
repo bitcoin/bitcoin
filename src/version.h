7710db0f2deaa5326b0d5e39ace3c45365bee5c1
sidhujag <sidhujag@hotmail.com>
2017-03-09 16:35:28 -0800

    update version
    Former-commit-id: 675ce1af18cb8571cfacdb904635f463d416f0e0

--- src/version.h
+++ src/version.h
@@ -19,7 +19,7 @@ static const int GETHEADERS_VERSION = 31800;
 
 //! disconnect from peers older than this proto version
 // SYSCOIN
-static const int MIN_PEER_PROTO_VERSION = 70029;
+static const int MIN_PEER_PROTO_VERSION = 70030;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
