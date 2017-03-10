d92c2fd1d62e3faa008e941a75811a1f508be47f
Russell Yanofsky <russ@yanofsky.org>
2017-03-10 15:58:53 -0500

    Add simple qt wallet test sending a transaction

--- src/qt/test/test_main.cpp
+++ src/qt/test/test_main.cpp
@@ -14,9 +14,10 @@
 
 #ifdef ENABLE_WALLET
 #include "paymentservertests.h"
+#include "wallettests.h"
 #endif
 
-#include <QCoreApplication>
+#include <QApplication>
 #include <QObject>
 #include <QTest>
 
@@ -43,8 +44,8 @@ int main(int argc, char *argv[])
     bool fInvalid = false;
 
     // Don't remove this, it's needed to access
-    // QCoreApplication:: in the tests
-    QCoreApplication app(argc, argv);
+    // QApplication:: and QCoreApplication:: in the tests
+    QApplication app(argc, argv);
     app.setApplicationName("Bitcoin-Qt-test");
 
     SSL_library_init();
@@ -67,6 +68,12 @@ int main(int argc, char *argv[])
     if (QTest::qExec(&test4) != 0) {
         fInvalid = true;
     }
+#ifdef ENABLE_WALLET
+    WalletTests test5;
+    if (QTest::qExec(&test5) != 0) {
+        fInvalid = true;
+    }
+#endif
 
     return fInvalid;
 }
