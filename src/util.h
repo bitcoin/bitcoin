9c01f8898471b2a71942ff0d7294fd369fb71b02
Wladimir J. van der Laan <laanwj@gmail.com>
2017-03-13 06:51:15 +0100

    util: Throw tinyformat::format_error on formatting error
    Throw tinyformat::format_error on formatting error instead of the
    `std::runtime_error`.

--- src/util.h
+++ src/util.h
@@ -80,7 +80,7 @@ template<typename... Args> std::string FormatStringFromLogArgs(const char *fmt, 
     std::string _log_msg_; /* Unlikely name to avoid shadowing variables */ \
     try { \
         _log_msg_ = tfm::format(__VA_ARGS__); \
-    } catch (std::runtime_error &e) { \
+    } catch (tinyformat::format_error &e) { \
         /* Original format string will have newline so don't add one here */ \
         _log_msg_ = "Error \"" + std::string(e.what()) + "\" while formatting log message: " + FormatStringFromLogArgs(__VA_ARGS__); \
     } \
