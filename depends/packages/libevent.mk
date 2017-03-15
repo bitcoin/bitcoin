70c872fe4ace2ff24632262cb86d71ccc61b5e20
Holger Schinzel <holger.schinzel@gmx.de>
2017-03-16 00:15:36 +0100

    [depends] Fixing download path and sha256 hash (#1398)
    * Fixing download path and sha256 hash
    * typo
    * package version variable

--- depends/packages/libevent.mk
+++ depends/packages/libevent.mk
@@ -1,8 +1,8 @@
 package=libevent
 $(package)_version=2.1.8
-$(package)_download_path=https://github.com/libevent/libevent/archive/
-$(package)_file_name=release-$($(package)_version)-stable.tar.gz
-$(package)_sha256_hash=316ddb401745ac5d222d7c529ef1eada12f58f6376a66c1118eee803cb70f83d
+$(package)_download_path=https://github.com/libevent/libevent/releases/download/release-$($(package)_version)-stable
+$(package)_file_name=$(package)-$($(package)_version)-stable.tar.gz
+$(package)_sha256_hash=965cc5a8bb46ce4199a47e9b2c9e1cae3b137e8356ffdad6d94d3b9069b71dc2
 
 define $(package)_preprocess_cmds
   ./autogen.sh
