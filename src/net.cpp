594b78fdf5dcb31b7c93b89839de066648c68cb7
Wladimir J. van der Laan <laanwj@gmail.com>
2017-03-16 12:02:54 +0100

    Merge #9921: build: Probe MSG_DONTWAIT in the same way as MSG_NOSIGNAL
    a4d1c9f compat: use `unsigned int` instead of `u_int` (Wladimir J. van der Laan)
    25da1ee build: cleanup: define MSG_DONTWAIT/MSG_NO_SIGNAL locally (Wladimir J. van der Laan)
    c459d50 build: Probe MSG_DONTWAIT in the same way as MSG_NOSIGNAL (Wladimir J. van der Laan)
    
    Tree-SHA512: 60d79d69439bb181465e4244aa5ddc28bbd84f69c0ca0c753956b3798c9022394e29d791bc085fe7ffb1268c64c789a57e24797daad63525bb776088188ff9ae
    
    fix merge error in configure.ac
    
    Signed-off-by: Pasta <Pasta@dash.org>

--- src/net.cpp
+++ src/net.cpp
@@ -52,8 +52,13 @@
 // We add a random period time (0 to 1 seconds) to feeler connections to prevent synchronization.
 #define FEELER_SLEEP_WINDOW 1
 
-#if !defined(HAVE_MSG_NOSIGNAL) && !defined(MSG_NOSIGNAL)
+#if !defined(HAVE_MSG_NOSIGNAL)
 #define MSG_NOSIGNAL 0
+#endif
+
+// MSG_DONTWAIT is not available on some platforms, if it doesn't exist define it as 0
+#if !defined(HAVE_MSG_DONTWAIT)
+#define MSG_DONTWAIT 0
 #endif
 
 // Fix for ancient MinGW versions, that don't have defined these in ws2tcpip.h.
