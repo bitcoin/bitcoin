1b5c10d85030ddfdb42affdd11ea22c39ca413fb
kobake <kobake@users.sourceforge.net>
2017-03-18 16:24:45 +0900

    utf8 bom

--- src/chainparams.h
+++ src/chainparams.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2009-2010 Satoshi Nakamoto
+﻿// Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2016 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
--- stdafx.cpp
+++ stdafx.cpp
@@ -1,1 +1,1 @@
-#include "stdafx.h"
+﻿#include "stdafx.h"
