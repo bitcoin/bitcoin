cc82dbddf2fa23386fc6a24dd844155590be1a4c
kobake <kobake@users.sourceforge.net>
2017-03-20 16:27:37 +0900

    secp256k1_ec_pubkey_serialize: specific arguments style

--- src/key.cpp
+++ src/key.cpp
@@ -221,7 +221,13 @@ CPubKey CKey::GetPubKey() const {
     CPubKey result;
     int ret = secp256k1_ec_pubkey_create(secp256k1_context_sign, &pubkey, begin());
     assert(ret);
-    secp256k1_ec_pubkey_serialize(secp256k1_context_sign, (unsigned char*)result.begin(), &clen, &pubkey, m_fCompressed ? SECP256K1_EC_COMPRESSED : SECP256K1_EC_UNCOMPRESSED);
+    secp256k1_ec_pubkey_serialize(
+        secp256k1_context_sign,
+        (unsigned char*)result.begin(),
+        &clen,
+        &pubkey,
+        m_fCompressed ? SECP256K1_EC_COMPRESSED : SECP256K1_EC_UNCOMPRESSED
+    );
     assert(result.size() == clen);
     assert(result.IsValid());
     return result;
