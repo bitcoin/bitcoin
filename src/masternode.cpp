828b639e360ee3a533d07467d597b4a8f902108e
UdjinM6 <UdjinM6@users.noreply.github.com>
2017-03-20 04:20:43 +0300

    Fix mn list sync (#1401)
    * Fix mn list sync
    
    Bump sync timeout if masternode is "initially valid" and last ping was quite a long time ago
    
    * compare to current time

--- src/masternode.cpp
+++ src/masternode.cpp
@@ -840,7 +840,17 @@ bool CMasternodePing::CheckAndUpdate(CMasternode* pmn, bool fFromNewBroadcast, i
 
     if (!CheckSignature(pmn->pubKeyMasternode, nDos)) return false;
 
-    // so, ping seems to be ok, let's store it
+    // so, ping seems to be ok
+
+    // if we are still syncing and there was no known ping for this mn for quite a while
+    // (NOTE: assuming that MASTERNODE_EXPIRATION_SECONDS/2 should be enough to finish mn list sync)
+    if(!masternodeSync.IsMasternodeListSynced() && !pmn->IsPingedWithin(MASTERNODE_EXPIRATION_SECONDS/2)) {
+        // let's bump sync timeout
+        LogPrint("masternode", "CMasternodePing::CheckAndUpdate -- bumping sync timeout, masternode=%s\n", vin.prevout.ToStringShort());
+        masternodeSync.AddedMasternodeList();
+    }
+
+    // let's store this ping as the last one
     LogPrint("masternode", "CMasternodePing::CheckAndUpdate -- Masternode ping accepted, masternode=%s\n", vin.prevout.ToStringShort());
     pmn->lastPing = *this;
 
