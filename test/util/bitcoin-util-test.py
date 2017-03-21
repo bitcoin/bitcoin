ce58e93ec0bf4ef4a503379e98bd209dd99447eb
John Newbery <john@johnnewbery.com>
2017-03-21 13:54:02 -0400

    Change bitcoin-util-test.py to use Python3

--- test/util/bitcoin-util-test.py
+++ test/util/bitcoin-util-test.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Copyright 2014 BitPay Inc.
 # Copyright 2016 The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
-from __future__ import division,print_function,unicode_literals
 import os
 import sys
 import argparse
