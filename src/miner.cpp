3b3e64ad8d6023b1574d5382c08d04efbe45c1c4
Thomas Snider <tjps636@gmail.com>
2017-03-23 15:08:54 -0700

    [trivial] Dead code removal
    Conflicts:
    	src/miner.cpp

--- src/miner.cpp
+++ src/miner.cpp
@@ -29,8 +29,7 @@
 #include <validation.h>
 #include <validationinterface.h>
 
-#include <boost/thread.hpp>
-#include <boost/tuple/tuple.hpp>
+#include <algorithm>
 #include <queue>
 
 //////////////////////////////////////////////////////////////////////////////
@@ -47,17 +46,6 @@
 uint64_t nLastBlockTx = 0;
 uint64_t nLastBlockSize = 0;
 uint64_t nLastBlockWeight = 0;
-
-class ScoreCompare
-{
-public:
-    ScoreCompare() {}
-
-    bool operator()(const CTxMemPool::txiter a, const CTxMemPool::txiter b)
-    {
-        return CompareTxMemPoolEntryByScore()(*b,*a); // Convert to less than
-    }
-};
 
 int64_t UpdateTime(CBlockHeader* pblock, const Consensus::Params& consensusParams, const CBlockIndex* pindexPrev)
 {
