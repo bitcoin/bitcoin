df370283cc765ad599f5c0254fd9a663bfdc811e
Nicolas Dorier <nicolas.dorier@gmail.com>
2017-03-24 19:10:09 +0900

    [QA] Fix typo in fundrawtransaction test
    Ping @jnewbery introduced on https://github.com/bitcoin/bitcoin/commit/1bc7f600c302e3924291de749b09801985de0150

--- test/functional/fundrawtransaction.py
+++ test/functional/fundrawtransaction.py
@@ -472,7 +472,7 @@ class RawTransactionsTest(BitcoinTestFramework):
         rawTx = self.nodes[1].createrawtransaction(inputs, outputs)
         # fund a transaction that requires a new key for the change output
         # creating the key must be impossible because the wallet is locked
-        assert_raises_jsonrpc(-4, "Insufficient funds", self.nodes[1].fundrawtransaction, rawtx)
+        assert_raises_jsonrpc(-4, "Keypool ran out, please call keypoolrefill first", self.nodes[1].fundrawtransaction, rawTx)
 
         #refill the keypool
         self.nodes[1].walletpassphrase("test", 100)
