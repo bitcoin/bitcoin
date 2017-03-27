72785377306764ab999e4bab13b821e4e5ea11eb
Jonas Schnelli <dev@jonasschnelli.ch>
2017-03-27 10:19:55 +0200

    [Qt] Don't add arguments of sensitive command to console window

--- src/qt/rpcconsole.cpp
+++ src/qt/rpcconsole.cpp
@@ -825,7 +825,7 @@ void RPCConsole::on_lineEdit_returnPressed()
 
         cmdBeforeBrowsing = QString();
 
-        message(CMD_REQUEST, cmd);
+        message(CMD_REQUEST, QString::fromStdString(strFilteredCmd));
         Q_EMIT cmdRequest(cmd);
 
         cmd = QString::fromStdString(strFilteredCmd);
