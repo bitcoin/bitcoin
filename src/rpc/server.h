8d8f28dd52fc9f78f57842f94842c8433f1811be
Wladimir J. van der Laan <laanwj@gmail.com>
2017-03-27 16:04:31 +0200

    refactor: Move RPCNotifyBlockChange out of `rpc/server.h`

--- src/rpc/server.h
+++ src/rpc/server.h
@@ -198,7 +198,6 @@ bool StartRPC();
 void InterruptRPC();
 void StopRPC();
 std::string JSONRPCExecBatch(const UniValue& vReq);
-void RPCNotifyBlockChange(bool ibd, const CBlockIndex *);
 
 // Retrieves any serialization flags requested in command line argument
 int RPCSerializationFlags();
