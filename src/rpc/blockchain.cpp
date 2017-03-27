ee12add3f9418a5ac8ca60419f89ba3f832bc3f8
Wladimir J. van der Laan <laanwj@gmail.com>
2017-03-27 16:12:02 +0200

    refactor: Make rest.cpp dependency on `*toJSON` in `blockchain.cpp` explicit

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -101,7 +101,7 @@ UniValue blockheaderToJSON(const CBlockIndex* blockindex)
     return result;
 }
 
-UniValue blockToJSON(const CBlock& block, const CBlockIndex* blockindex, bool txDetails = false)
+UniValue blockToJSON(const CBlock& block, const CBlockIndex* blockindex, bool txDetails)
 {
     UniValue result(UniValue::VOBJ);
     result.push_back(Pair("hash", blockindex->GetBlockHash().GetHex()));
@@ -378,7 +378,7 @@ void entryToJSON(UniValue &info, const CTxMemPoolEntry &e)
     info.push_back(Pair("depends", depends));
 }
 
-UniValue mempoolToJSON(bool fVerbose = false)
+UniValue mempoolToJSON(bool fVerbose)
 {
     if (fVerbose)
     {
