fdd95ec740068c0fa49caeaf0c6931785aee5f63
Allan Doensen <allan@doensen.com>
2017-03-28 12:16:20 +1100

    Made the unit test clean up after itself so as to not interfere with other test cases.

--- src/test/excessiveblock_test.cpp
+++ src/test/excessiveblock_test.cpp
@@ -147,6 +147,9 @@ BOOST_AUTO_TEST_CASE(check_validator_rule)
 
 BOOST_AUTO_TEST_CASE(check_excessive_validator)
 {
+    unsigned int c_mgb = maxGeneratedBlock;
+    unsigned int c_ebs = excessiveBlockSize;
+
     // fudge global variables....
     maxGeneratedBlock = 1000000;
     excessiveBlockSize = 888;
@@ -186,10 +189,16 @@ BOOST_AUTO_TEST_CASE(check_excessive_validator)
 
     str = ExcessiveBlockValidator(tmpExcessive, (unsigned int *) 42, true);
     BOOST_CHECK(! str.empty());
+
+    maxGeneratedBlock = c_mgb;
+    excessiveBlockSize = c_ebs;
 }
 
 BOOST_AUTO_TEST_CASE(check_generated_block_validator)
 {
+    unsigned int c_mgb = maxGeneratedBlock;
+    unsigned int c_ebs = excessiveBlockSize;
+
     // fudge global variables....
     maxGeneratedBlock = 888;
     excessiveBlockSize = 1000000;
@@ -229,6 +238,9 @@ BOOST_AUTO_TEST_CASE(check_generated_block_validator)
 
     str = MiningBlockSizeValidator(tmpMGB, (uint64_t *) 42, true);
     BOOST_CHECK(! str.empty());
+
+    maxGeneratedBlock = c_mgb;
+    excessiveBlockSize = c_ebs;
 }
 
 
