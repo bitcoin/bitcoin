4c110182a4f70c96b62f7ce9a0ef84fd23f44bf3
roconnor-blockstream <roconnor@blockstream.com>
2017-03-29 00:43:44 -0400

    Update validation.cpp
    Replaced the segwit deploy check in `ContextualCheckBlock` with `IsWitnessEnabled`.

--- src/validation.cpp
+++ src/validation.cpp
@@ -3016,7 +3016,7 @@ bool ContextualCheckBlock(const CBlock& block, CValidationState& state, const Co
     //   {0xaa, 0x21, 0xa9, 0xed}, and the following 32 bytes are SHA256^2(witness root, witness nonce). In case there are
     //   multiple, the last one is used.
     bool fHaveWitness = false;
-    if (VersionBitsState(pindexPrev, consensusParams, Consensus::DEPLOYMENT_SEGWIT, versionbitscache) == THRESHOLD_ACTIVE) {
+    if (IsWitnessEnabled(pindexPrev, consensusParams)) {
         int commitpos = GetWitnessCommitmentIndex(block);
         if (commitpos != -1) {
             bool malleated = false;
