ee2be7be151463a6359fe1a4ceb38c32fe873851
shitakee <dua@xww.ro>
2017-03-30 10:52:56 -0300

    Add missing header file
    We need to include <sys/types.h> because we use ssize_t on the file.

--- src/leveldb/db/db_iter.cc
+++ src/leveldb/db/db_iter.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file. See the AUTHORS file for names of contributors.
 
+#include <sys/types.h>
 #include "db/db_iter.h"
 
 #include "db/filename.h"
