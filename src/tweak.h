274870c95dfc0064c822a84d705330c1db811619
Neil Booth <kyuupichan@gmail.com>
2017-04-01 17:12:22 +0900

    Remove #pragma once

--- src/tweak.h
+++ src/tweak.h
@@ -1,7 +1,7 @@
 // Copyright (c) 2016-2017 The Bitcoin Unlimited developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-#pragma once
+
 #ifndef TWEAK_H
 #define TWEAK_H
 
