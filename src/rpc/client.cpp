cb8f2cb4732687674669563f7922deef0cef5d7d
John Newbery <john@johnnewbery.com>
2017-04-03 10:03:00 -0400

    Allow disconnectnode() to be called with node id
    disconnectnode() can currently only be called with the IP address/port
    of the node the user wishes to connect. This commit allows the node to
    be disconnected using the nodeid returned by getpeerinfo().
    
    Conflicts:
    	src/rpc/net.cpp

--- src/rpc/client.cpp
+++ src/rpc/client.cpp
@@ -141,6 +141,7 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "voteraw", 5, "time" },
     { "logging", 0, "include" },
     { "logging", 1, "exclude" },
+    { "disconnectnode", 1, "nodeid" },
     // Echo with conversion (For testing only)
     { "echojson", 0, "arg0" },
     { "echojson", 1, "arg1" },
