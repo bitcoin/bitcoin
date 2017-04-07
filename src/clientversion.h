1dde64aaa0913bc1663b20064fd9a03081580a4f
Wladimir J. van der Laan <laanwj@gmail.com>
2017-04-07 15:37:31 +0200

    Merge #10155: build: Deduplicate version numbers
    9ff7818 doc: Update release process for simplified version bumping (Wladimir J. van der Laan)
    08d9aee build: Remove duplicate version information from src/clientversion.h (Wladimir J. van der Laan)
    168a703 doc: Make build system insert version in Doxyfile (Wladimir J. van der Laan)
    b67eb8d doc: Remove version numbers from READMEs (Wladimir J. van der Laan)
    
    Tree-SHA512: 531e44282b1c1383a382847a5605177917dfbf78acfaa754d1cbadd2e165c7e34ddbd01790f87615083fac359571708c2551ad24b712aab1f84a2068360c3a17

--- src/clientversion.h
+++ src/clientversion.h
@@ -7,28 +7,12 @@
 
 #if defined(HAVE_CONFIG_H)
 #include "config/dash-config.h"
-#else
-
-/**
- * client versioning and copyright year
- */
-
-//! These need to be macros, as clientversion.cpp's and dash*-res.rc's voodoo requires it
-#define CLIENT_VERSION_MAJOR 0
-#define CLIENT_VERSION_MINOR 14
-#define CLIENT_VERSION_REVISION 1
-#define CLIENT_VERSION_BUILD 0
-
-//! Set to true for release, false for prerelease or test build
-#define CLIENT_VERSION_IS_RELEASE false
-
-/**
- * Copyright year (2009-this)
- * Todo: update this when changing our copyright comments in the source
- */
-#define COPYRIGHT_YEAR 2018
-
 #endif //HAVE_CONFIG_H
+
+// Check that required client information is defined
+#if !defined(CLIENT_VERSION_MAJOR) || !defined(CLIENT_VERSION_MINOR) || !defined(CLIENT_VERSION_REVISION) || !defined(CLIENT_VERSION_BUILD) || !defined(CLIENT_VERSION_IS_RELEASE) || !defined(COPYRIGHT_YEAR)
+#error Client version information missing: wasn't defined by dash-config.h nor defined any other way
+#endif
 
 /**
  * Converts the parameter X to a string after macro replacement on X has been performed.
