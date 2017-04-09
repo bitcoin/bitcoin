6f2b375b109654534d4b154c6c50197d3a638fc6
Neil Booth <kyuupichan@gmail.com>
2017-04-09 11:40:49 +0900

    Add missing copyright header

--- src/torcontrol.cpp
+++ src/torcontrol.cpp
@@ -1,3 +1,7 @@
+// Copyright (c) 2015-2016 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #include "torcontrol.h"
 #include "utilstrencodings.h"
 #include "net.h"
@@ -698,4 +702,3 @@ void StopTorControl()
         base = 0;
     }
 }
-
