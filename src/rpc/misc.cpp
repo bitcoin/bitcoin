db8c71db82b34958119a2d34417b33ab64d7513e
John Newbery <john@johnnewbery.com>
2017-04-10 10:34:23 -0400

    allow libevent logging to be updated during runtime
    Conflicts:
    	src/rpc/misc.cpp

--- src/rpc/misc.cpp
+++ src/rpc/misc.cpp
@@ -6,10 +6,11 @@
 
 #include <base58.h>
 #include <clientversion.h>
+#include <httpserver.h>
 #include <init.h>
 #include <net.h>
 #include <netbase.h>
-#include<rpc/blockchain.h>
+#include <rpc/blockchain.h>
 #include <rpc/server.h>
 #include <timedata.h>
 #include <txmempool.h>
@@ -670,6 +671,20 @@ UniValue logging(const JSONRPCRequest& request)
         logCategories &= ~getCategoryMask(request.params[1]);
     }
 
+    // Update libevent logging if BCLog::LIBEVENT has changed.
+    // If the library version doesn't allow it, UpdateHTTPServerLogging() returns false,
+    // in which case we should clear the BCLog::LIBEVENT flag.
+    // Throw an error if the user has explicitly asked to change only the libevent
+    // flag and it failed.
+    uint32_t changedLogCategories = originalLogCategories ^ logCategories;
+    if (changedLogCategories & BCLog::LIBEVENT) {
+        if (!UpdateHTTPServerLogging(logCategories & BCLog::LIBEVENT)) {
+            logCategories &= ~BCLog::LIBEVENT;
+            if (changedLogCategories == BCLog::LIBEVENT) {
+            throw JSONRPCError(RPC_INVALID_PARAMETER, "libevent logging cannot be updated when using libevent before v2.1.1.");
+            }
+        }
+    }
 
     UniValue result(UniValue::VOBJ);
     std::vector<CLogCategoryActive> vLogCatActive = ListActiveLogCategories();
