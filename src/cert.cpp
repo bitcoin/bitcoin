b0938a43e283b1678b35b27eb1e714e32f9f26d3
sidhujag <sidhujag@hotmail.com>
2017-04-12 11:56:33 -0700

    Revert "fix createaliasrecp"
    This reverts commit 02fa886abf23958f4aa9dd1ee7a7e59ba3e92e75.

--- src/cert.cpp
+++ src/cert.cpp
@@ -866,7 +866,7 @@ UniValue certnew(const UniValue& params, bool fHelp) {
 	CRecipient aliasRecipient;
 	CreateRecipient(scriptPubKeyAlias, aliasRecipient);
 	CRecipient aliasPaymentRecipient;
-	CreateAliasRecipient(scriptPubKeyOrig, theAlias.vchAlias, aliasPaymentRecipient);
+	CreateAliasRecipient(scriptPubKeyOrig, theAlias.vchAlias, theAlias.vchAliasPeg, chainActive.Tip()->nHeight, aliasPaymentRecipient);
 		
 	CScript scriptData;
 	scriptData << OP_RETURN << data;
@@ -1010,7 +1010,7 @@ UniValue certupdate(const UniValue& params, bool fHelp) {
 	CRecipient aliasRecipient;
 	CreateRecipient(scriptPubKeyAlias, aliasRecipient);
 	CRecipient aliasPaymentRecipient;
-	CreateAliasRecipient(scriptPubKeyOrig, theAlias.vchAlias, aliasPaymentRecipient);
+	CreateAliasRecipient(scriptPubKeyOrig, theAlias.vchAlias, theAlias.vchAliasPeg, chainActive.Tip()->nHeight, aliasPaymentRecipient);
 		
 	CScript scriptData;
 	scriptData << OP_RETURN << data;
@@ -1151,7 +1151,7 @@ UniValue certtransfer(const UniValue& params, bool fHelp) {
 	CRecipient aliasRecipient;
 	CreateRecipient(scriptPubKeyAlias, aliasRecipient);
 	CRecipient aliasPaymentRecipient;
-	CreateAliasRecipient(scriptPubKeyFromOrig, fromAlias.vchAlias, aliasPaymentRecipient);
+	CreateAliasRecipient(scriptPubKeyFromOrig, fromAlias.vchAlias, fromAlias.vchAliasPeg, chainActive.Tip()->nHeight, aliasPaymentRecipient);
 
 	CScript scriptData;
 	scriptData << OP_RETURN << data;
