880361f5cc241065470b53c50a028cd890b6ce49
sidhujag <sidhujag@hotmail.com>
2017-04-12 11:12:12 -0700

    fix aliasrecp

--- src/alias.cpp
+++ src/alias.cpp
@@ -1619,7 +1619,7 @@ void CreateRecipient(const CScript& scriptPubKey, CRecipient& recipient)
 	CAmount nFee = CWallet::GetMinimumFee(nSize, nTxConfirmTarget, mempool);
 	recipient.nAmount = nFee;
 }
-void CreateAliasRecipient(const CScript& scriptPubKeyDest,  const vector<unsigned char>& vchAlias, const vector<unsigned char>& vchAliasPeg, const uint64_t& nHeight, CRecipient& recipient)
+void CreateAliasRecipient(const CScript& scriptPubKeyDest, const vector<unsigned char>& vchAlias, const vector<unsigned char>& vchAliasPeg, const uint64_t& nHeight, CRecipient& recipient)
 {
 	int precision = 0;
 	CAmount nFee = 0;
@@ -1628,16 +1628,11 @@ void CreateAliasRecipient(const CScript& scriptPubKeyDest,  const vector<unsigne
 	scriptChangeOrig += scriptPubKeyDest;
 	CRecipient recp = {scriptChangeOrig, 0, false};
 	recipient = recp;
-	int nFeePerByte = getFeePerByte(vchAliasPeg, vchFromString("SYS"), nHeight, precision);
 	CTxOut txout(0,	recipient.scriptPubKey);
 	size_t nSize = txout.GetSerializeSize(SER_DISK,0)+148u;
-	// create utxo min 1500 bytes worth of fees
-	if(nSize < 1500)
-		nSize = 1500;
-	if(nFeePerByte <= 0)
-		nFee = CWallet::GetMinimumFee(nSize, nTxConfirmTarget, mempool);
-	else
-		nFee = nFeePerByte * nSize;
+	// create alias payment utxo max 1500 bytes worth of fees
+	size_t nSize = 1500;
+	CAmount nPayFee = CWallet::GetMinimumFee(nSize, nTxConfirmTarget, mempool);
 	recipient.nAmount = nFee;
 }
 void CreateFeeRecipient(CScript& scriptPubKey, const vector<unsigned char>& vchAliasPeg, const uint64_t& nHeight, const vector<unsigned char>& data, CRecipient& recipient)
@@ -2798,6 +2793,7 @@ UniValue aliashistory(const UniValue& params, bool fHelp) {
 			CAliasIndex alias(tx);
 			if(!alias.IsNull())
 			{
+				alias.txHash = tx.GetHash();
 				if(BuildAliasJson(alias, false, oName, strWalletless))
 					oUpdates.push_back(oName);
 			}
@@ -2842,6 +2838,7 @@ UniValue aliashistory(const UniValue& params, bool fHelp) {
 			CAliasIndex alias(tx);
 			if(!alias.IsNull())
 			{
+				alias.txHash = tx.GetHash();
 				if(BuildAliasJson(alias, false, oName, strWalletless))
 					oPayments.push_back(oName);
 			}
