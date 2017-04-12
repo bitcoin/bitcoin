614a7811f7e041ad3028f5a488a7d9781fe269f7
sidhujag <sidhujag@hotmail.com>
2017-04-11 21:14:32 -0700

    yup

--- src/alias.cpp
+++ src/alias.cpp
@@ -2776,7 +2776,6 @@ UniValue aliashistory(const UniValue& params, bool fHelp) {
     vector<vector<unsigned char> > vvch;
     int op, nOut;
 	string opName;
-	for(int i =0;i,vtxTx.size();i++)
 	{
 		UniValue oName(UniValue::VOBJ);
 		const CTransaction& tx = vtxTx[i];
