2f2e25ddc0b30cbe5b7a6fa5def03f37ebcfb95a
NicolasDorier <nicolas.dorier@gmail.com>
2017-04-13 13:11:57 +0000

    [Wallet] Fetch CInputCoins from CWallet before calling FundTransaction

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -2239,17 +2239,11 @@ bool CWallet::SelectCoins(const std::vector<COutput>& vAvailableCoins, const CAm
         coinControl->ListSelected(vPresetInputs);
     BOOST_FOREACH(const COutPoint& outpoint, vPresetInputs)
     {
-        std::map<uint256, CWalletTx>::const_iterator it = mapWallet.find(outpoint.hash);
-        if (it != mapWallet.end())
-        {
-            const CWalletTx* pcoin = &it->second;
-            // Clearly invalid input, fail
-            if (pcoin->tx->vout.size() <= outpoint.n)
-                return false;
-            nValueFromPresetInputs += pcoin->tx->vout[outpoint.n].nValue;
-            setPresetCoins.insert(CInputCoin(pcoin, outpoint.n));
-        } else
-            return false; // TODO: Allow non-wallet inputs
+        auto foundCoin = coinControl->FindKnownCoin(outpoint);
+        if (!foundCoin)
+            return false;
+        nValueFromPresetInputs += foundCoin->txout.nValue;
+        setPresetCoins.insert(*foundCoin);
     }
 
     // remove preset inputs from vCoins
@@ -2348,6 +2342,16 @@ bool CWallet::FundTransaction(CMutableTransaction& tx, CAmount& nFeeRet, const C
         reservekey.KeepKey();
 
     return true;
+}
+
+boost::optional<CInputCoin> CWallet::FindCoin(const COutPoint& outpoint)
+{
+    LOCK(cs_wallet);
+    boost::optional<CInputCoin> foundCoin;
+    std::map<uint256, CWalletTx>::const_iterator it = mapWallet.find(outpoint.hash);
+    if (it != mapWallet.end())
+        foundCoin = CInputCoin(&it->second, outpoint.n);
+    return foundCoin;
 }
 
 bool CWallet::CreateTransaction(const std::vector<CRecipient>& vecSend, CWalletTx& wtxNew, CReserveKey& reservekey, CAmount& nFeeRet,
--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -893,6 +893,8 @@ public:
     CAmount GetUnconfirmedWatchOnlyBalance() const;
     CAmount GetImmatureWatchOnlyBalance() const;
 
+    boost::optional<CInputCoin> FindCoin(const COutPoint& outpoint);
+
     /**
      * Insert additional inputs into the transaction by
      * calling CreateTransaction();
