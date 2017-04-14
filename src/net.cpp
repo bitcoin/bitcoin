0aee4a132b6d3be276503c517cb2b7f07d774bf6
Matt Corallo <git@bluematt.me>
2017-04-14 16:29:57 -0400

    Check interruptNet during dnsseed lookups

--- src/net.cpp
+++ src/net.cpp
@@ -1579,6 +1579,9 @@ void CConnman::ThreadDNSAddressSeed()
     LogPrintf("Loading addresses from DNS seeds (could take a while)\n");
 
     BOOST_FOREACH(const CDNSSeedData &seed, vSeeds) {
+        if (interruptNet) {
+            return;
+        }
         if (HaveNameProxy()) {
             AddOneShot(seed.host);
         } else {
@@ -1595,6 +1598,9 @@ void CConnman::ThreadDNSAddressSeed()
                     vAdd.push_back(addr);
                     found++;
                 }
+            }
+            if (interruptNet) {
+                return;
             }
             // TODO: The seed name resolve may fail, yielding an IP of [::], which results in
             // addrman assigning the same source to results from different seeds.
