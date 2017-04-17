c0f2756be517feddacd7c6b89b9faa888b3fef8e
Russell Yanofsky <russ@yanofsky.org>
2017-04-17 14:23:14 -0400

    Remove direct bitcoin calls from qt/optionsmodel.cpp

--- src/qt/optionsmodel.h
+++ src/qt/optionsmodel.h
@@ -9,6 +9,10 @@
 
 #include <QAbstractListModel>
 
+namespace interface {
+class Node;
+}
+
 QT_BEGIN_NAMESPACE
 class QNetworkProxy;
 QT_END_NAMESPACE
@@ -27,7 +31,7 @@ class OptionsModel : public QAbstractListModel
     Q_OBJECT
 
 public:
-    explicit OptionsModel(QObject *parent = 0, bool resetSettings = false);
+    explicit OptionsModel(interface::Node& node, QObject *parent = 0, bool resetSettings = false);
 
     enum OptionID {
         StartAtStartup,         // bool
@@ -76,6 +80,7 @@ public:
     bool isRestartRequired() const;
 
 private:
+    interface::Node& m_node;
     /* Qt-only settings */
     bool fHideTrayIcon;
     bool fMinimizeToTray;
