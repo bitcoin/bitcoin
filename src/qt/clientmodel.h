1b46aa1f3750e64049305b1df5fe8aa4b349a0bf
Jonas Schnelli <dev@jonasschnelli.ch>
2017-04-20 09:51:05 +0200

    Declare headers height/time cache mutable, re-set the methods const

--- src/qt/clientmodel.h
+++ src/qt/clientmodel.h
@@ -53,8 +53,8 @@ public:
     int getNumConnections(unsigned int flags = CONNECTIONS_ALL) const;
     QString getMasternodeCountString() const;
     int getNumBlocks() const;
-    int getHeaderTipHeight();
-    int64_t getHeaderTipTime();
+    int getHeaderTipHeight() const;
+    int64_t getHeaderTipTime() const;
     //! Return number of transactions in the mempool
     long getMempoolSize() const;
     //! Return the dynamic memory usage of the mempool
@@ -84,8 +84,8 @@ public:
     QString dataDir() const;
 
     // caches for the best header
-    std::atomic<int> cachedBestHeaderHeight;
-    std::atomic<int64_t> cachedBestHeaderTime;
+    mutable std::atomic<int> cachedBestHeaderHeight;
+    mutable std::atomic<int64_t> cachedBestHeaderTime;
 
 private:
     OptionsModel *optionsModel;
