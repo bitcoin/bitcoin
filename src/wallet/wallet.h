5f3a862c483f4719d9b89cb5ca4340f3f3b7012c
sidhujag <sidhujag@hotmail.com>
2017-04-21 16:00:36 -0700

    sys payment by default

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -752,7 +752,7 @@ public:
 
 	// SYSCOIN add data and input tx for syscoin service calls
     bool CreateTransaction(const std::vector<CRecipient>& vecSend, CWalletTx& wtxNew, CReserveKey& reservekey, CAmount& nFeeRet, int& nChangePosRet,
-		std::string& strFailReason, const CCoinControl *coinControl = NULL, bool sign = true, const std::vector<unsigned char> &vchAliasPeg = std::vector<unsigned char>(), const std::string &currencyCode = "", bool sysTx = false, bool bAliasPay=false);
+		std::string& strFailReason, const CCoinControl *coinControl = NULL, bool sign = true, const std::vector<unsigned char> &vchAliasPeg = std::vector<unsigned char>(), const std::string &currencyCode = "SYS", bool sysTx = false, bool bAliasPay=false);
     bool CommitTransaction(CWalletTx& wtxNew, CReserveKey& reservekey);
 
     bool AddAccountingEntry(const CAccountingEntry&, CWalletDB & pwalletdb);
