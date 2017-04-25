6afb584b90e2a3a05269fa00bf37e69d5e793207
Pieter Wuille <pieter.wuille@gmail.com>
2017-04-25 11:29:42 -0700

    Pack Coin more tightly

--- src/coins.h
+++ src/coins.h
@@ -30,18 +30,18 @@
 class Coin
 {
 public:
-    //! whether the containing transaction was a coinbase
-    bool fCoinBase;
-
     //! unspent transaction output
     CTxOut out;
 
-    //! at which height the containing transaction was included in the active block chain
-    uint32_t nHeight;
+    //! whether containing transaction was a coinbase
+    unsigned int fCoinBase : 1;
 
-    //! construct a Coin from a CTxOut and height/coinbase properties.
-    Coin(CTxOut&& outIn, int nHeightIn, bool fCoinBaseIn) : fCoinBase(fCoinBaseIn), out(std::move(outIn)), nHeight(nHeightIn) {}
-    Coin(const CTxOut& outIn, int nHeightIn, bool fCoinBaseIn) : fCoinBase(fCoinBaseIn), out(outIn), nHeight(nHeightIn) {}
+    //! at which height this containing transaction was included in the active block chain
+    uint32_t nHeight : 31;
+
+    //! construct a Coin from a CTxOut and height/coinbase information.
+    Coin(CTxOut&& outIn, int nHeightIn, bool fCoinBaseIn) : out(std::move(outIn)), fCoinBase(fCoinBaseIn), nHeight(nHeightIn) {}
+    Coin(const CTxOut& outIn, int nHeightIn, bool fCoinBaseIn) : out(outIn), fCoinBase(fCoinBaseIn),nHeight(nHeightIn) {}
 
     void Clear() {
         out.SetNull();
