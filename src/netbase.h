235dc01fc0458abd4490d0602adc8c42fe81e8ff
Cory Fields <cory-nospam-@coryfields.com>
2017-04-26 16:54:45 -0400

    net: remove now-unused functions

--- src/netbase.h
+++ src/netbase.h
@@ -59,8 +59,6 @@ bool Lookup(const char *pszName, CService& addr, int portDefault, bool fAllowLoo
 bool Lookup(const char *pszName, std::vector<CService>& vAddr, int portDefault, bool fAllowLookup, unsigned int nMaxSolutions);
 CService LookupNumeric(const char *pszName, int portDefault = 0);
 bool LookupSubNet(const char *pszName, CSubNet& subnet);
-bool ConnectSocket(const CService &addr, SOCKET& hSocketRet, int nTimeout, bool *outProxyConnectionFailed = 0);
-bool ConnectSocketByName(CService &addr, SOCKET& hSocketRet, const char *pszDest, int portDefault, int nTimeout, bool *outProxyConnectionFailed = 0);
 bool ConnectSocketDirectly(const CService &addrConnect, SOCKET& hSocket, int nTimeout);
 /** Return readable error string for a network error code */
 std::string NetworkErrorString(int err);
