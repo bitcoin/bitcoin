94195342495bfba5e28ccdd15068c335e28fee37
MarcoFalke <falke.marco@gmail.com>
2017-05-02 20:50:57 +0200

    Merge #10137: Remove unused import. Remove accidental trailing semicolons.
    73b3721 Remove accidental trailing semicolons (practicalswift)
    b8d9a86 Remove unused import (practicalswift)
    
    Tree-SHA512: 36ac38952f8a10f36d2300519c583ef77703cff761be97470be97cfda2fec9d5b7ac604e454f174cc2601160e5cef3f227021ab5d3a6febc97fcb486d78754d3

--- test/functional/wallet-hd.py
+++ test/functional/wallet-hd.py
@@ -30,7 +30,7 @@ class WalletHDTest(BitcoinTestFramework):
 
         # create an internal key
         change_addr = self.nodes[1].getrawchangeaddress()
-        change_addrV= self.nodes[1].validateaddress(change_addr);
+        change_addrV= self.nodes[1].validateaddress(change_addr)
         assert_equal(change_addrV["hdkeypath"], "m/44'/1'/0'/1/0") #first internal child key
 
         # Import a non-HD private key in the HD wallet
@@ -58,7 +58,7 @@ class WalletHDTest(BitcoinTestFramework):
 
         # create an internal key (again)
         change_addr = self.nodes[1].getrawchangeaddress()
-        change_addrV= self.nodes[1].validateaddress(change_addr);
+        change_addrV= self.nodes[1].validateaddress(change_addr)
         assert_equal(change_addrV["hdkeypath"], "m/44'/1'/0'/1/1") #second internal child key
 
         self.sync_all()
@@ -88,7 +88,7 @@ class WalletHDTest(BitcoinTestFramework):
 
         # send a tx and make sure its using the internal chain for the changeoutput
         txid = self.nodes[1].sendtoaddress(self.nodes[0].getnewaddress(), 1)
-        outs = self.nodes[1].decoderawtransaction(self.nodes[1].gettransaction(txid)['hex'])['vout'];
+        outs = self.nodes[1].decoderawtransaction(self.nodes[1].gettransaction(txid)['hex'])['vout']
         keypath = ""
         for out in outs:
             if out['value'] != 1:
