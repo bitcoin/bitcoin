50ebea09d7f430828d5339aa24ebbd4ed107c2ca
shaolinfry <shaolinfry@protonmail.ch>
2017-05-02 21:23:12 +0000

    Mark development version 0.14.99

--- src/clientversion.h
+++ src/clientversion.h
@@ -16,11 +16,11 @@
 //! These need to be macros, as clientversion.cpp's and bitcoin*-res.rc's voodoo requires it
 #define CLIENT_VERSION_MAJOR 0
 #define CLIENT_VERSION_MINOR 14
-#define CLIENT_VERSION_REVISION 2
+#define CLIENT_VERSION_REVISION 99
 #define CLIENT_VERSION_BUILD 0
 
 //! Set to true for release, false for prerelease or test build
-#define CLIENT_VERSION_IS_RELEASE true
+#define CLIENT_VERSION_IS_RELEASE false
 
 /**
  * Copyright year (2009-this)
