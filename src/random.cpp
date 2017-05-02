ace4f211d8f043fa8af3e1691e9e37b43e698ec1
Pieter Wuille <pieter.wuille@gmail.com>
2017-05-02 11:04:31 -0700

    Add FastRandomContext::rand256()

--- src/random.cpp
+++ src/random.cpp
@@ -212,6 +212,17 @@ void FastRandomContext::RandomSeed()
     requires_seed = false;
 }
 
+uint256 FastRandomContext::rand256()
+{
+    if (bytebuf_size < 32) {
+        FillByteBuffer();
+    }
+    uint256 ret;
+    memcpy(ret.begin(), bytebuf + 64 - bytebuf_size, 32);
+    bytebuf_size -= 32;
+    return ret;
+}
+
 FastRandomContext::FastRandomContext(const uint256& seed) : requires_seed(false), bytebuf_size(0), bitbuf_size(0)
 {
     rng.SetKey(seed.begin(), 32);
