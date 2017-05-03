ff7365e780624a1ef66c12a6d7b61448a3f9294c
John Newbery <john@johnnewbery.com>
2017-05-03 11:49:58 -0400

    [tests] fix flake8 warnings in zapwallettxes.py

--- test/functional/zapwallettxes.py
+++ test/functional/zapwallettxes.py
@@ -13,8 +13,12 @@
   available, but that the unconfirmed transaction has been zapped.
 """
 from test_framework.test_framework import BitcoinTestFramework
-from test_framework.util import *
-
+from test_framework.util import (assert_equal,
+                                 assert_raises,
+                                 bitcoind_processes,
+                                 connect_nodes_bi,
+                                 JSONRPCException,
+                                 )
 
 class ZapWalletTXesTest (BitcoinTestFramework):
 
@@ -25,56 +29,56 @@ class ZapWalletTXesTest (BitcoinTestFramework):
 
     def setup_network(self):
         super().setup_network()
-        connect_nodes_bi(self.nodes,0,2)
+        connect_nodes_bi(self.nodes, 0, 2)
 
-    def run_test (self):
+    def run_test(self):
         self.log.info("Mining blocks...")
         self.nodes[0].generate(1)
         self.sync_all()
         self.nodes[1].generate(101)
         self.sync_all()
-        
+
         assert_equal(self.nodes[0].getbalance(), 50)
-        
+
         txid0 = self.nodes[0].sendtoaddress(self.nodes[2].getnewaddress(), 11)
         txid1 = self.nodes[0].sendtoaddress(self.nodes[2].getnewaddress(), 10)
         self.sync_all()
         self.nodes[0].generate(1)
         self.sync_all()
-        
+
         txid2 = self.nodes[0].sendtoaddress(self.nodes[2].getnewaddress(), 11)
         txid3 = self.nodes[0].sendtoaddress(self.nodes[2].getnewaddress(), 10)
-        
+
         tx0 = self.nodes[0].gettransaction(txid0)
-        assert_equal(tx0['txid'], txid0) #tx0 must be available (confirmed)
-        
+        assert_equal(tx0['txid'], txid0)  # tx0 must be available (confirmed)
+
         tx1 = self.nodes[0].gettransaction(txid1)
-        assert_equal(tx1['txid'], txid1) #tx1 must be available (confirmed)
-        
+        assert_equal(tx1['txid'], txid1)  # tx1 must be available (confirmed)
+
         tx2 = self.nodes[0].gettransaction(txid2)
-        assert_equal(tx2['txid'], txid2) #tx2 must be available (unconfirmed)
-        
+        assert_equal(tx2['txid'], txid2)  # tx2 must be available (unconfirmed)
+
         tx3 = self.nodes[0].gettransaction(txid3)
-        assert_equal(tx3['txid'], txid3) #tx3 must be available (unconfirmed)
-        
+        assert_equal(tx3['txid'], txid3)  # tx3 must be available (unconfirmed)
+
         #restart bitcoind
         self.stop_node(0)
         self.nodes[0] = self.start_node(0,self.options.tmpdir)
         
         tx3 = self.nodes[0].gettransaction(txid3)
-        assert_equal(tx3['txid'], txid3) #tx must be available (unconfirmed)
-        
+        assert_equal(tx3['txid'], txid3)  # tx must be available (unconfirmed)
+
         self.stop_node(0)
         
         #restart bitcoind with zapwallettxes
         self.nodes[0] = self.start_node(0,self.options.tmpdir, ["-zapwallettxes=1"])
-        
+
         assert_raises(JSONRPCException, self.nodes[0].gettransaction, [txid3])
-        #there must be an exception because the unconfirmed wallettx0 must be gone by now
+        # there must be an exception because the unconfirmed wallettx0 must be gone by now
 
         tx0 = self.nodes[0].gettransaction(txid0)
-        assert_equal(tx0['txid'], txid0) #tx0 (confirmed) must still be available because it was confirmed
+        assert_equal(tx0['txid'], txid0)  # tx0 (confirmed) must still be available because it was confirmed
 
 
 if __name__ == '__main__':
-    ZapWalletTXesTest ().main ()
+    ZapWalletTXesTest().main()
