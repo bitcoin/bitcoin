e30faab6fd380f5a9eba07d776ddd7ad6c2e26da
Spencer Lievens <spencerlievens@users.noreply.github.com>
2017-05-05 13:31:25 +0200

    Make strWalletFile const (#1459)
    * Make strWalletFile const
    
    * Insert Line Space for Legibility

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -644,7 +644,7 @@ public:
     mutable CCriticalSection cs_wallet;
 
     bool fFileBacked;
-    std::string strWalletFile;
+    const std::string strWalletFile;
 
     std::set<int64_t> setKeyPool;
     std::map<CKeyID, CKeyMetadata> mapKeyMetadata;
@@ -658,11 +658,11 @@ public:
         SetNull();
     }
 
-    CWallet(const std::string& strWalletFileIn)
+    CWallet(const std::string& strWalletFileIn) 
+    : strWalletFile(strWalletFileIn)
     {
         SetNull();
 
-        strWalletFile = strWalletFileIn;
         fFileBacked = true;
     }
 
