c2a68540e9e6d46ccf29572a46e42f7ca6e10a7d
Pieter Wuille <pieter.wuille@gmail.com>
2017-05-05 11:45:37 -0700

    Use sanity check timestamps as entropy

--- src/random.cpp
+++ src/random.cpp
@@ -298,6 +298,10 @@ bool Random_SanityCheck()
     uint64_t stop = GetPerformanceCounter();
     if (stop == start) return false;
 
+    // We called GetPerformanceCounter. Use it as entropy.
+    RAND_add((const unsigned char*)&start, sizeof(start), 1);
+    RAND_add((const unsigned char*)&stop, sizeof(stop), 1);
+
     return true;
 }
 
