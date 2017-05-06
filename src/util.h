83ae8f97e543b451d6b3348ee029ad7d8e7ad37c
Jorge Timón <jtimon@jtimon.cc>
2017-05-06 21:23:21 +0200

    Util: Put mapMultiArgs inside ArgsManager
    - Set ArgsManager::mapMultiArgs in ArgsManager::SoftSetArg, ForceSetArg, SoftSetBoolArg

--- src/util.h
+++ src/util.h
@@ -42,7 +42,6 @@ public:
     boost::signals2::signal<std::string (const char* psz)> Translate;
 };
 
-extern const std::map<std::string, std::vector<std::string> >& mapMultiArgs;
 extern bool fPrintToConsole;
 extern bool fPrintToDebugLog;
 
@@ -184,6 +183,7 @@ class ArgsManager
 protected:
     CCriticalSection cs_args;
     std::map<std::string, std::string> mapArgs;
+    std::map<std::string, std::vector<std::string> > mapMultiArgs;
 public:
     void ParseParameters(int argc, const char*const argv[]);
     void ReadConfigFile(const std::string& confPath);
