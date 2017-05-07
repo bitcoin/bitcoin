78da882edd54ed62f4a0035590460a97cb9ff282
Jorge Timón <jtimon@jtimon.cc>
2017-05-07 19:37:54 +0200

    Util: Small improvements in gArgs usage
    - Don't check gArgs.IsArgSet() is greater than 0
    - Remove unneeded calls and local variables

--- src/httprpc.cpp
+++ src/httprpc.cpp
@@ -93,7 +93,7 @@ static bool multiUserAuthorized(std::string strUserPass)
     std::string strUser = strUserPass.substr(0, strUserPass.find(":"));
     std::string strPass = strUserPass.substr(strUserPass.find(":") + 1);
 
-    if (gArgs.IsArgSet("-rpcauth") > 0) {
+    if (gArgs.IsArgSet("-rpcauth")) {
         //Search for multi-user login/pass "rpcauth" from config
         BOOST_FOREACH(std::string strRPCAuth, gArgs.GetArgs("-rpcauth"))
         {
