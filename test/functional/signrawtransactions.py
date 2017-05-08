05fb6342fddf9c3285e0c24c3c76a3f2df3f0bf3
Karl-Johan Alm <karljohan-alm@garage.co.jp>
2017-05-08 19:29:31 +0900

    [test] signrawtransactions.py: remove * imports

--- test/functional/signrawtransactions.py
+++ test/functional/signrawtransactions.py
@@ -5,7 +5,7 @@
 """Test transaction signing using the signrawtransaction RPC."""
 
 from test_framework.test_framework import BitcoinTestFramework
-from test_framework.util import *
+from test_framework.util import assert_equal, assert_raises, JSONRPCException
 
 
 class SignRawTransactionsTest(BitcoinTestFramework):
