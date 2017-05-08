e7ff7a743cebbc7891ffa4fc34746694f93d4aef
sidhujag <sidhujag@hotmail.com>
2017-05-08 11:38:07 -0700

    fix scan via guids
    Former-commit-id: ae04de790db0ee06ff21603ef835b09399bdd0cf

--- src/alias.cpp
+++ src/alias.cpp
@@ -1231,7 +1231,8 @@ bool CAliasDB::ScanNames(const std::vector<unsigned char>& vchAlias, const strin
         try {
 			if (pcursor->GetKey(key) && key.first == "namei") {
             	const vector<unsigned char> &vchMyAlias = key.second;
-				
+				if(!vchAlias.empty() && vchMyAlias != vchAlias)
+					continue;				
                 
 				pcursor->GetValue(vtxPos);
 				
@@ -1270,6 +1271,8 @@ bool CAliasDB::ScanNames(const std::vector<unsigned char>& vchAlias, const strin
 					continue;
 				}
                 nameScan.push_back(txPos);
+				if(!vchAlias.empty() && vchMyAlias == vchAlias)
+					break;
             }
             if (nameScan.size() >= nMax)
                 break;
