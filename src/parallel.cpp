34acfbf70bc13a10c77e5a382327bdc212147672
Peter Tschipper <peter.tschipper@gmail.com>
2017-05-09 18:19:35 -0700

    Prevent the mapThinBlocksInflight timer from tripping
    In the event that we have received that thinblock and were
    successful in reconstructing it we need to prevent the timer
    from tripping before the block has finished processing. Set
    the time = -1 and use it as a flag to prevent the node from
    being disconnected if the time goes over limit.  We must
    do this because we still need to maintain the map entry
    to prevent us from downloading the block again.

--- src/parallel.cpp
+++ src/parallel.cpp
@@ -496,6 +496,18 @@ void HandleBlockMessageThread(CNode *pfrom, const string &strCommand, const CBlo
     CValidationState state;
     uint64_t nSizeBlock = ::GetSerializeSize(block, SER_NETWORK, PROTOCOL_VERSION);
 
+    // At this point we have either a block or a fully reconstructed thinblock but we still need to
+    // maintain a mapThinBlocksInFlight entry so that we don't re-request a full block from
+    // the same node while the block is processing. Furthermore by setting the time = -1 we prevent
+    // the timeout from triggering and inadvertently disconnecting the node in the event that the block
+    // takes a longer time to process than the THINBLOCK_DOWNLOAD_TIMEOUT interval.
+    {
+        LOCK(pfrom->cs_mapthinblocksinflight);
+        if (pfrom->mapThinBlocksInFlight.count(inv.hash))
+            pfrom->mapThinBlocksInFlight[inv.hash] = -1;
+    }
+
+
     boost::thread::id this_id(boost::this_thread::get_id());
     PV.InitThread(this_id, pfrom, block, inv); // initialize the mapBlockValidationThread entries
 
