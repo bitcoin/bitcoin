f2ee6f1aa5059b025622b1997617c4e429351e7b
Jonas Schnelli <dev@jonasschnelli.ch>
2017-05-16 08:54:29 +0200

    Merge #10362: [GUI] Add OSX keystroke to RPCConsole info
    012fa9b99 Add OSX keystroke to clear RPCConsole (Spencer Lievens)
    
    Tree-SHA512: 124e9567d633fd80ab200e53b34c821947111ebb6ebd0b2ba3feacdbe2b6ab59ab447b87473f36d221d4189d92df6d53e34a8486aacaa8eaa4d9e413db01b11f

--- src/qt/rpcconsole.cpp
+++ src/qt/rpcconsole.cpp
@@ -821,8 +821,14 @@ void RPCConsole::clear(bool clearHistory)
             ).arg(fixedFontInfo.family(), QString("%1pt").arg(consoleFontSize))
         );
 
+#ifdef Q_OS_MAC
+    QString clsKey = "(⌘)-L";
+#else
+    QString clsKey = "Ctrl-L";
+#endif
+	 
     message(CMD_REPLY, (tr("Welcome to the %1 RPC console.").arg(tr(PACKAGE_NAME)) + "<br>" +
-                        tr("Use up and down arrows to navigate history, and <b>Ctrl-L</b> to clear screen.") + "<br>" +
+                        tr("Use up and down arrows to navigate history, and %1 to clear screen.").arg("<b>"+clsKey+"</b>") + "<br>" +
                         tr("Type <b>help</b> for an overview of available commands.")) +
                         "<br><span class=\"secwarning\">" +
                         tr("WARNING: Scammers have been active, telling users to type commands here, stealing their wallet contents. Do not use this console without fully understanding the ramification of a command.") +
