081487d5ffe1203871ac3c2c41fcca44a7c8cfff
Andrew Chow <achow101-github@achow101.com>
2017-05-17 14:36:49 -0400

    Remove uasf comment string from user agent

--- src/clientversion.h
+++ src/clientversion.h
@@ -46,7 +46,7 @@ extern const std::string CLIENT_BUILD;
 
 
 std::string FormatFullVersion();
-std::string FormatSubVersion(const std::string& name, int nClientVersion, const std::vector<std::string>& comments, bool fBaseNameOnly = false);
+std::string FormatSubVersion(const std::string& name, int nClientVersion, const std::vector<std::string>& comments);
 
 #endif // WINDRES_PREPROC
 
