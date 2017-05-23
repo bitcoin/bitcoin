d335a723055515f54cba6f41ff52637ad0e00218
Pieter Wuille <pieter.wuille@gmail.com>
2017-05-23 10:04:14 -0700

    Fix: make CCoinsViewDbCursor::Seek work for missing keys
    Thanks to Suhas Daftuar for figuring this out.
    
    Conflicts:
    	src/txdb.cpp

--- src/txdb.cpp
+++ src/txdb.cpp
@@ -179,9 +179,7 @@ CCoinsViewCursor *CCoinsViewDB::Cursor() const
     i->pcursor->Seek(DB_COIN);
     // Cache key of first record
     if (i->pcursor->Valid()) {
-        CoinEntry entry(&i->keyTmp.second);
-        i->pcursor->GetKey(entry);
-        i->keyTmp.first = entry.key;
+        i->pcursor->GetKey(i->keyTmp);
     } else {
         i->keyTmp.first = 0; // Make sure Valid() and GetKey() return false
     }
