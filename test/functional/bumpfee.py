1b6602fbe66c0205acd4a33b9183080ec500fc72
Russell Yanofsky <russ@yanofsky.org>
2017-05-24 16:15:26 -0400

    Fix bumpfee rpc "errors" return value

--- test/functional/bumpfee.py
+++ test/functional/bumpfee.py
@@ -88,6 +88,7 @@ def test_simple_bumpfee_succeeds(rbf_node, peer_node, dest_address):
     sync_mempools((rbf_node, peer_node))
     assert rbfid in rbf_node.getrawmempool() and rbfid in peer_node.getrawmempool()
     bumped_tx = rbf_node.bumpfee(rbfid)
+    assert_equal(bumped_tx["errors"], [])
     assert bumped_tx["fee"] - abs(rbftx["fee"]) > 0
     # check that bumped_tx propogates, original tx was evicted and has a wallet conflict
     sync_mempools((rbf_node, peer_node))
