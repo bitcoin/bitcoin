306a77c4da49f95808b707a05caaf81759760e69
sidhujag <sidhujag@hotmail.com>
2017-05-29 10:38:11 -0700

    fix cert tests

--- src/test/syscoin_cert_tests.cpp
+++ src/test/syscoin_cert_tests.cpp
@@ -56,7 +56,17 @@ BOOST_AUTO_TEST_CASE (generate_certupdate)
 	AliasNew("node1", "jagcertupdate", "password", "data");
 	string guid = CertNew("node1", "jagcertupdate", "password", "title", "data");
 	// update an cert that isn't yours
-	BOOST_CHECK_THROW(CallRPC("node2", "certupdate " + guid + " title pubdata data"), runtime_error);
+	UniValue r;
+	BOOST_CHECK_NO_THROW(r = CallRPC("node2", "certupdate " + guid + " title pubdata data"));
+	const UniValue& resArray = r.get_array();
+	if(resArray.size() > 1)
+	{
+		const UniValue& complete_value = resArray[1];
+		bool bComplete = false;
+		if (complete_value.isStr())
+			bComplete = complete_value.get_str() == "true";
+		BOOST_CHECK(!bComplete);
+	}
 	CertUpdate("node1", guid, "pub1");
 	// shouldnt update data, just uses prev data because it hasnt changed
 	CertUpdate("node1", guid);
@@ -82,8 +92,16 @@ BOOST_AUTO_TEST_CASE (generate_certtransfer)
 	CertTransfer("node1", "node3", pvtguid, "jagcert3");
 
 	// xfer an cert that isn't yours
-	BOOST_CHECK_THROW(CallRPC("node1", "certtransfer " + guid + " jagcert2"), runtime_error);
-
+	BOOST_CHECK_NO_THROW(r = CallRPC("node1", "certtransfer " + guid + " jagcert2"));
+	const UniValue& resArray = r.get_array();
+	if(resArray.size() > 1)
+	{
+		const UniValue& complete_value = resArray[1];
+		bool bComplete = false;
+		if (complete_value.isStr())
+			bComplete = complete_value.get_str() == "true";
+		BOOST_CHECK(!bComplete);
+	}
 	// update xferred cert
 	certdata = "newdata";
 	certtitle = "newtitle";
