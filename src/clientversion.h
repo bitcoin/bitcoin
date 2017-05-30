e3459b405ad268de66f78611e3800805b07a0ed6
backpacker69 <backpacker69@protonmail.com>
2017-05-30 13:16:03 +0000

    automake fixes

--- src/clientversion.h
+++ src/clientversion.h
@@ -17,6 +17,12 @@
 // Set to true for release, false for prerelease or test build
 #define CLIENT_VERSION_IS_RELEASE  true
 
+// Copyright year (2009-this)
+// Todo: update this when changing our copyright comments in the source
+#define COPYRIGHT_YEAR 2013
+
+#endif //HAVE_CONFIG_H
+
 // ppcoin version - intended for display purpose ONLY
 #define PEERCOIN_VERSION_MAJOR       0
 #define PEERCOIN_VERSION_MINOR       6
