384007a7f2d58dfdb15a7e49831b6d7a08cc9dc0
dexX7 <dexx@bitwatch.co>
2017-05-31 10:50:30 +0200

    Run RPC tests with explicitly defined datadir
    When there is a also a configuration file in the standard datadir, using the CLI still works, but it can result in warnings like:
    
    Error: Invalid combination of -regtest and -testnet.

--- qa/pull-tester/omnicore-rpc-tests.sh
+++ qa/pull-tester/omnicore-rpc-tests.sh
@@ -20,19 +20,19 @@ echo "Omni Core RPC test dir: "$TESTDIR
 echo "Last OmniJ commit: "$(git log -n 1 --format="%H Author: %cn <%ce>")
 if [ "$@" = "true" ]; then
     echo "Debug logging level: maximum"
-    $OMNICORED -regtest -txindex -server -daemon -rpcuser=bitcoinrpc -rpcpassword=pass -debug=1 -omnidebug=all -omnialertallowsender=any -omniactivationallowsender=any -paytxfee=0.0001 -minrelaytxfee=0.00001 -limitancestorcount=750 -limitdescendantcount=750 -rpcserialversion=0 -discover=0 -listen=0 -datadir="$DATADIR" &
+    $OMNICORED -datadir="$DATADIR" -regtest -txindex -server -daemon -rpcuser=bitcoinrpc -rpcpassword=pass -debug=1 -omnidebug=all -omnialertallowsender=any -omniactivationallowsender=any -paytxfee=0.0001 -minrelaytxfee=0.00001 -limitancestorcount=750 -limitdescendantcount=750 -rpcserialversion=0 -discover=0 -listen=0 &
 else
     echo "Debug logging level: minimum"
-    $OMNICORED -regtest -txindex -server -daemon -rpcuser=bitcoinrpc -rpcpassword=pass -debug=0 -omnidebug=none -omnialertallowsender=any -omniactivationallowsender=any -paytxfee=0.0001 -minrelaytxfee=0.00001 -limitancestorcount=750 -limitdescendantcount=750 -rpcserialversion=0 -discover=0 -listen=0 -datadir="$DATADIR" &
+    $OMNICORED -datadir="$DATADIR" -regtest -txindex -server -daemon -rpcuser=bitcoinrpc -rpcpassword=pass -debug=0 -omnidebug=none -omnialertallowsender=any -omniactivationallowsender=any -paytxfee=0.0001 -minrelaytxfee=0.00001 -limitancestorcount=750 -limitdescendantcount=750 -rpcserialversion=0 -discover=0 -listen=0 &
 fi
-$OMNICORECLI -regtest -rpcuser=bitcoinrpc -rpcpassword=pass -rpcwait getinfo
-$OMNICORECLI -regtest -rpcuser=bitcoinrpc -rpcpassword=pass -rpcwait omni_getinfo
+$OMNICORECLI -datadir="$DATADIR" -regtest -rpcuser=bitcoinrpc -rpcpassword=pass -rpcwait getinfo
+$OMNICORECLI -datadir="$DATADIR" -regtest -rpcuser=bitcoinrpc -rpcpassword=pass -rpcwait omni_getinfo
 ./gradlew --console plain :omnij-rpc:regTest
 STATUS=$?
-$OMNICORECLI -regtest -rpcuser=bitcoinrpc -rpcpassword=pass -rpcwait stop
+$OMNICORECLI -datadir="$DATADIR" -regtest -rpcuser=bitcoinrpc -rpcpassword=pass -rpcwait stop
 
 # If $STATUS is not 0, the test failed.
-if [ $STATUS -ne 0 ]; then tail -500 $DATADIR/regtest/omnicore.log; fi
+if [ $STATUS -ne 0 ]; then tail -100 $DATADIR/regtest/omnicore.log; fi
 
 
 exit $STATUS
