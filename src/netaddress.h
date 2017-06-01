3193b459f90abbb8f79e63ccd310fff67f349405
Marko Bencun <marko.bencun@monetas.net>
2017-06-01 19:13:51 +0200

    remove some unused functions
    Identified with `cppcheck --enable=unusedFunction .`.
    
     - GetSendBufferSize()'s last use removed in
       991955ee81034dc3fbc1c2a8e60c04fc9e0b538c
     - SetPort()'s last use removed in
       7e195e8459ad741368db6bb574981fccb1707268
     - GetfLargeWorkInvalidChainFound() was introduced in
       e3ba0ef95636290a3bb597ddd25d13ea13b034aa and never used

--- src/netaddress.h
+++ src/netaddress.h
@@ -148,7 +148,6 @@ class CService : public CNetAddr
         CService(const struct in_addr& ipv4Addr, unsigned short port);
         CService(const struct sockaddr_in& addr);
         void Init();
-        void SetPort(unsigned short portIn);
         unsigned short GetPort() const;
         bool GetSockAddr(struct sockaddr* paddr, socklen_t *addrlen) const;
         bool SetSockAddr(const struct sockaddr* paddr);
