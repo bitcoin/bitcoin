f1405c78145d6f5f4eb13f053383e03e6e568b49
Pieter Wuille <pieter.wuille@gmail.com>
2017-06-07 12:03:17 -0700

    scripted-diff: Use new naming style for insecure_rand* functions
    -BEGIN VERIFY SCRIPT-
    sed -i 's/\<insecure_randbits(/InsecureRandBits(/g' src/test/*.cpp src/test/*.h src/wallet/test/*.cpp
    sed -i 's/\<insecure_randbool(/InsecureRandBool(/g' src/test/*.cpp src/test/*.h src/wallet/test/*.cpp
    sed -i 's/\<insecure_randrange(/InsecureRandRange(/g' src/test/*.cpp src/test/*.h src/wallet/test/*.cpp
    sed -i 's/\<insecure_randbytes(/InsecureRandBytes(/g' src/test/*.cpp src/test/*.h src/wallet/test/*.cpp
    sed -i 's/\<insecure_rand256(/InsecureRand256(/g' src/test/*.cpp src/test/*.h src/wallet/test/*.cpp
    sed -i 's/\<insecure_rand(/InsecureRand32(/g' src/test/*.cpp src/test/*.h src/wallet/test/*.cpp
    sed -i 's/\<seed_insecure_rand(/SeedInsecureRand(/g' src/test/*.cpp src/test/*.h src/wallet/test/*.cpp
    -END VERIFY SCRIPT-

--- src/test/test_bitcoin.h
+++ src/test/test_bitcoin.h
@@ -18,7 +18,7 @@
 extern uint256 insecure_rand_seed;
 extern FastRandomContext insecure_rand_ctx;
 
-static inline void seed_insecure_rand(bool fDeterministic = false)
+static inline void SeedInsecureRand(bool fDeterministic = false)
 {
     if (fDeterministic) {
         insecure_rand_seed = uint256();
@@ -28,12 +28,12 @@ static inline void seed_insecure_rand(bool fDeterministic = false)
     insecure_rand_ctx = FastRandomContext(insecure_rand_seed);
 }
 
-static inline uint32_t insecure_rand() { return insecure_rand_ctx.rand32(); }
-static inline uint256 insecure_rand256() { return insecure_rand_ctx.rand256(); }
-static inline uint64_t insecure_randbits(int bits) { return insecure_rand_ctx.randbits(bits); }
-static inline uint64_t insecure_randrange(uint64_t range) { return insecure_rand_ctx.randrange(range); }
-static inline bool insecure_randbool() { return insecure_rand_ctx.randbool(); }
-static inline std::vector<unsigned char> insecure_randbytes(size_t len) { return insecure_rand_ctx.randbytes(len); }
+static inline uint32_t InsecureRand32() { return insecure_rand_ctx.rand32(); }
+static inline uint256 InsecureRand256() { return insecure_rand_ctx.rand256(); }
+static inline uint64_t InsecureRandBits(int bits) { return insecure_rand_ctx.randbits(bits); }
+static inline uint64_t InsecureRandRange(uint64_t range) { return insecure_rand_ctx.randrange(range); }
+static inline bool InsecureRandBool() { return insecure_rand_ctx.randbool(); }
+static inline std::vector<unsigned char> InsecureRandBytes(size_t len) { return insecure_rand_ctx.randbytes(len); }
 
 /** Basic testing setup.
  * This just configures logging and chain parameters.
