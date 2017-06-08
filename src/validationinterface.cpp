411d273837d4ed76f164c6dfcce935edcc131309
Matt Corallo <git@bluematt.me>
2017-06-08 11:13:46 -0400

    Add CallFunctionInQueue to wait on validation interface queue drain
    Conflicts:
    	src/validationinterface.h

--- src/validationinterface.cpp
+++ src/validationinterface.cpp
@@ -108,6 +108,10 @@ void UnregisterAllValidationInterfaces() {
     g_signals.m_internals->NotifyTransactionLock.disconnect_all_slots();
 }
 
+void CallFunctionInValidationInterfaceQueue(std::function<void ()> func) {
+    g_signals.m_internals->m_schedulerClient.AddToProcessQueue(std::move(func));
+}
+
 void CMainSignals::MempoolEntryRemoved(CTransactionRef ptx, MemPoolRemovalReason reason) {
     if (reason != MemPoolRemovalReason::BLOCK && reason != MemPoolRemovalReason::CONFLICT) {
         m_internals->m_schedulerClient.AddToProcessQueue([ptx, this] {
