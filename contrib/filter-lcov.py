c64f6356044c90fc3468d86d3f703cd330391483
Andrew Chow <achow101-github@achow101.com>
2017-06-08 18:15:55 -0700

    Filter subtrees and and benchmarks from coverage report
    Remove leveldb baseline coverage gathering.
    
    Added filter rules to remove all of the subtress (leveldb, secp256k1, ctaes, univalue) and
    benchmarking from the coverage report. These items are unnecessary as we do not test for any
    of the subtrees and benchmark coverage is unneeded.

--- contrib/filter-lcov.py
+++ contrib/filter-lcov.py
@@ -3,7 +3,7 @@
 import argparse
 
 parser = argparse.ArgumentParser(description='Remove the coverage data from a tracefile for all files matching the pattern.')
-parser.add_argument('pattern', help='the pattern of files to remove')
+parser.add_argument('--pattern', '-p', action='append', help='the pattern of files to remove', required=True)
 parser.add_argument('tracefile', help='the tracefile to remove the coverage data from')
 parser.add_argument('outfile', help='filename for the output to be written to')
 
@@ -16,8 +16,9 @@ in_remove = False
 with open(tracefile, 'r') as f:
     with open(outfile, 'w') as wf:
         for line in f:
-            if line.startswith("SF:") and pattern in line:
-                in_remove = True
+            for p in pattern:
+                if line.startswith("SF:") and p in line:
+                    in_remove = True
             if not in_remove:
                 wf.write(line)
             if line == 'end_of_record\n':
