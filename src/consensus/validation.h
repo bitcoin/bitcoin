6a0f565783befd2ec7c0445fb31f0d32f9322f07
Wladimir J. van der Laan <laanwj@gmail.com>
2017-06-11 14:22:00 +0200

    Merge #10560: Remove unused constants
    dce8239 Comment out unused constant REJECT_DUST (practicalswift)
    3289ef4 Remove unused constant MEMPOOL_GD_VERSION (practicalswift)
    
    Tree-SHA512: 8dd772c963037cd0a1db0d5bf5f7a5cce3f5cd8fb6fa983949f9f0c1c8312ffd251a62c1e5d32f3584d2bcc562d8c8a9bf7900609dee9a4f12898caa2be9c38f

--- src/consensus/validation.h
+++ src/consensus/validation.h
@@ -14,7 +14,7 @@ static const unsigned char REJECT_INVALID = 0x10;
 static const unsigned char REJECT_OBSOLETE = 0x11;
 static const unsigned char REJECT_DUPLICATE = 0x12;
 static const unsigned char REJECT_NONSTANDARD = 0x40;
-static const unsigned char REJECT_DUST = 0x41;
+// static const unsigned char REJECT_DUST = 0x41; // part of BIP 61
 static const unsigned char REJECT_INSUFFICIENTFEE = 0x42;
 static const unsigned char REJECT_CHECKPOINT = 0x43;
 
