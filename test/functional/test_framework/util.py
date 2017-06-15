d3421f5be1b5587cdd104740c4896bf300927612
Suhas Daftuar <sdaftuar@gmail.com>
2017-06-15 17:08:48 -0400

    [qa] Test non-atomic chainstate writes
    Adds new functional test, dbcrash.py, which uses -dbcrashratio to exercise the
    logic for recovering from a crash during chainstate flush.
    
    dbcrash.py is added to the extended tests, as it may take ~10 minutes to run
    
    Use _Exit() instead of exit() for crash simulation
    
    This eliminates stderr output such as:
        terminate called without an active exception
    or
        Assertion failed: (!pthread_mutex_destroy(&m)), function ~recursive_mutex, file /usr/local/include/boost/thread/pthread/recursive_mutex.hpp, line 104.
    
    Eliminating the stderr output on crash simulation allows testing with
    test_runner.py, which reports a test as failed if stderr is produced.

--- test/functional/test_framework/util.py
+++ test/functional/test_framework/util.py
@@ -226,6 +226,8 @@ def wait_for_bitcoind_start(process, url, i):
                 raise # unknown JSON RPC exception
         time.sleep(0.25)
 
+def wait_for_node_exit(node_index, timeout):
+    bitcoind_processes[node_index].wait(timeout)
 
 def start_node(i, dirname, extra_args=None, rpchost=None, timewait=None, binary=None, stderr=None):
     """
