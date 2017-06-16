0647ea656454d2cdf107bf97e9f44026010f41be
Pieter Wuille <pieter.wuille@gmail.com>
2017-06-16 16:34:34 -0700

    Merge #10587: Net: Fix resource leak in ReadBinaryFile(...)
    f2fb132cb Net: Fix resource leak in ReadBinaryFile(...) (practicalswift)
    
    Tree-SHA512: 879b9334d8bb681fa4b6f96d8ecb54e2a8948065f7be5fe7880131479c813602fc9d4a4314f043e6591e1aed50ffafa7c247362a9cdeb049b0721170e227b89a

--- src/torcontrol.cpp
+++ src/torcontrol.cpp
@@ -376,8 +376,10 @@ static std::pair<bool,std::string> ReadBinaryFile(const fs::path &filename, size
     while ((n=fread(buffer, 1, sizeof(buffer), f)) > 0) {
         // Check for reading errors so we don't return any data if we couldn't
         // read the entire file (or up to maxsize)
-        if (ferror(f))
+        if (ferror(f)) {
+            fclose(f);
             return std::make_pair(false,"");
+        }
         retval.append(buffer, buffer+n);
         if (retval.size() > maxsize)
             break;
