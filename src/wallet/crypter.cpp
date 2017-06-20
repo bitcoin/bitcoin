d46aff53699942363364f056b1d63ef04e5caf49
Pieter Wuille <pieter.wuille@gmail.com>
2017-06-20 16:07:53 -0700

    Merge #10536: Remove unreachable or otherwise redundant code
    4265bf351 Remove unreachable or otherwise redundant code (practicalswift)
    
    Tree-SHA512: bc9666ab5d20c936d78c50c0361405aca9edd116602aa9bcd71a79a904b647ac9eca0651d1a9d530189a6ac1c4e235bfc69ec1a68f7e36cc14d6848ac2206b7b

--- src/wallet/crypter.cpp
+++ src/wallet/crypter.cpp
@@ -337,7 +337,6 @@ bool CCryptoKeyStore::GetPubKey(const CKeyID &address, CPubKey& vchPubKeyOut) co
         // Check for watch-only pubkeys
         return CBasicKeyStore::GetPubKey(address, vchPubKeyOut);
     }
-    return false;
 }
 
 bool CCryptoKeyStore::EncryptKeys(CKeyingMaterial& vMasterKeyIn)
--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -327,7 +327,6 @@ bool CWallet::AddCryptedKey(const CPubKey &vchPubKey,
                                                             vchCryptedSecret,
                                                             mapKeyMetadata[vchPubKey.GetID()]);
     }
-    return false;
 }
 
 bool CWallet::LoadKeyMetadata(const CTxDestination& keyID, const CKeyMetadata &meta)
@@ -3661,8 +3660,6 @@ bool CWallet::CreateTransaction(const std::vector<CRecipient>& vecSend, CWalletT
                     strFailReason = _("Transaction too large");
                     return false;
                 }
-
-                CTransaction txNewConst(txNew);
 
                 // Remove scriptSigs to eliminate the fee calculation dummy signatures
                 for (auto& txin : txNew.vin) {
