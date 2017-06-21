cf9a2fe4c9242f554e05314980666502a29ac997
practicalswift <practicalswift@users.noreply.github.com>
2017-06-21 14:28:26 +0200

    Remove obsolete _MSC_VER check
    * MSVC++ 8.0 _MSC_VER == 1400 (Visual Studio 2005)
    * C++11 is supported since MSVC 2010
    * Compiling without C++11 support is no longer possible
    
    Conflicts:
    	src/init.cpp

--- src/init.cpp
+++ src/init.cpp
@@ -916,9 +916,7 @@ bool AppInitBasicSetup()
 #ifdef _MSC_VER
     // Turn off Microsoft heap dump noise
     _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
-    _CrtSetReportFile(_CRT_WARN, CreateFileA("NUL", GENERIC_WRITE, 0, nullptr, OPEN_EXISTING, 0, 0));
-#endif
-#if _MSC_VER >= 1400
+    _CrtSetReportFile(_CRT_WARN, CreateFileA("NUL", GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, 0));
     // Disable confusing "helpful" text message on abort, Ctrl-C
     _set_abort_behavior(0, _WRITE_ABORT_MSG | _CALL_REPORTFAULT);
 #endif
