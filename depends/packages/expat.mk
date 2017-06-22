41302bbae055aefdad9ac4e59894af77c87bac28
Wladimir J. van der Laan <laanwj@gmail.com>
2017-06-22 16:25:50 +0200

    Merge #10628: [depends] expat 2.2.1
    2c3fc51 [depends] expat 2.2.1 (fanquake)
    
    Tree-SHA512: ad0e18f2770c0c4b378123bcbcf93ed0ee0b03dbf6360f02e88b01052ef7b0f5540a0a5cbb6d0d3a3d70db29b0413a43f17e7fa5092185fb874470a9f6be4d76

--- depends/packages/expat.mk
+++ depends/packages/expat.mk
@@ -1,8 +1,8 @@
 package=expat
-$(package)_version=2.2.0
+$(package)_version=2.2.1
 $(package)_download_path=https://downloads.sourceforge.net/project/expat/expat/$($(package)_version)
 $(package)_file_name=$(package)-$($(package)_version).tar.bz2
-$(package)_sha256_hash=d9e50ff2d19b3538bd2127902a89987474e1a4db8e43a66a4d1a712ab9a504ff
+$(package)_sha256_hash=1868cadae4c82a018e361e2b2091de103cd820aaacb0d6cfa49bd2cd83978885
 
 define $(package)_set_vars
 $(package)_config_opts=--disable-static
