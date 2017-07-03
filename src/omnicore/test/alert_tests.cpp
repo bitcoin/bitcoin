2cad5175ee4d2fd2a8af3a66f319692456416b1e
zathrasc <zathrasc@gmail.com>
2017-07-03 13:07:19 -0400

    Update the alert tests to reflect key changes (thanks @dexx7!)

--- src/omnicore/test/alert_tests.cpp
+++ src/omnicore/test/alert_tests.cpp
@@ -23,16 +23,10 @@ BOOST_FIXTURE_TEST_SUITE(omnicore_alert_tests, BasicTestingSetup)
 BOOST_AUTO_TEST_CASE(alert_positive_authorization)
 {
     // Confirm authorized sources for mainnet
-    BOOST_CHECK(CheckAlertAuthorization("16Zwbujf1h3v1DotKcn9XXt1m7FZn2o4mj")); // Craig   <craig@omni.foundation>
-    BOOST_CHECK(CheckAlertAuthorization("1MicH2Vu4YVSvREvxW1zAx2XKo2GQomeXY")); // Michael <michael@omni.foundation>
-    BOOST_CHECK(CheckAlertAuthorization("1zAtHRASgdHvZDfHs6xJquMghga4eG7gy"));  // Zathras <zathras@omni.foundation>
-    BOOST_CHECK(CheckAlertAuthorization("1dexX7zmPen1yBz2H9ZF62AK5TGGqGTZH"));  // dexX7   <dexx@bitwatch.co>
-    BOOST_CHECK(CheckAlertAuthorization("1EXoDusjGwvnjZUyKkxZ4UHEf77z6A5S4P")); // Exodus (who has access?)
-
-    // Confirm authorized sources for testnet
-    BOOST_CHECK(CheckAlertAuthorization("mpDex4kSX4iscrmiEQ8fBiPoyeTH55z23j")); // Michael <michael@omni.foundation>
-    BOOST_CHECK(CheckAlertAuthorization("mpZATHupfCLqet5N1YL48ByCM1ZBfddbGJ")); // Zathras <zathras@omni.foundation>
-    BOOST_CHECK(CheckAlertAuthorization("mk5SSx4kdexENHzLxk9FLhQdbbBexHUFTW")); // dexX7   <dexx@bitwatch.co>
+    BOOST_CHECK(CheckAlertAuthorization("17xr7sbehYY4YSZX9yuJe6gK9rrdRrZx26"));  // Craig   <craig@omni.foundation>
+    BOOST_CHECK(CheckAlertAuthorization("1883ZMsRJfzKNozUBJBTCxQ7EaiNioNDWz"));  // Zathras <zathras@omni.foundation>
+    BOOST_CHECK(CheckAlertAuthorization("1HHv91gRxqBzQ3gydMob3LU8hqXcWoLfvd"));  // dexX7   <dexx@bitwatch.co>
+    BOOST_CHECK(CheckAlertAuthorization("16oDZYCspsczfgKXVj3xyvsxH21NpEj94F"));  // Adam    <adam@omni.foundation>
 }
 
 BOOST_AUTO_TEST_CASE(alert_unauthorized_source)
@@ -55,10 +49,10 @@ BOOST_AUTO_TEST_CASE(alert_manual_sources)
 
     // Then ignore some sources explicitly
     mapMultiArgs["-omnialertignoresender"].push_back("1JwSSubhmg6iPtRjtyqhUYYH7bZg3Lfy1T");
-    mapMultiArgs["-omnialertignoresender"].push_back("1EXoDusjGwvnjZUyKkxZ4UHEf77z6A5S4P");
-    BOOST_CHECK(CheckAlertAuthorization("1zAtHRASgdHvZDfHs6xJquMghga4eG7gy")); // should still be authorized
+    mapMultiArgs["-omnialertignoresender"].push_back("16oDZYCspsczfgKXVj3xyvsxH21NpEj94F");
+    BOOST_CHECK(CheckAlertAuthorization("1HHv91gRxqBzQ3gydMob3LU8hqXcWoLfvd")); // should still be authorized
     BOOST_CHECK(!CheckAlertAuthorization("1JwSSubhmg6iPtRjtyqhUYYH7bZg3Lfy1T"));
-    BOOST_CHECK(!CheckAlertAuthorization("1EXoDusjGwvnjZUyKkxZ4UHEf77z6A5S4P"));
+    BOOST_CHECK(!CheckAlertAuthorization("16oDZYCspsczfgKXVj3xyvsxH21NpEj94F"));
 
     mapMultiArgs = mapMultiArgsOriginal;
     mapArgs = mapArgsOriginal;
@@ -75,7 +69,7 @@ BOOST_AUTO_TEST_CASE(alert_authorize_any_source)
     mapMultiArgs["-omnialertallowsender"].push_back("any");
     BOOST_CHECK(CheckAlertAuthorization("1JwSSubhmg6iPtRjtyqhUYYH7bZg3Lfy1T"));
     BOOST_CHECK(CheckAlertAuthorization("137uFtQ5EgMsreg4FVvL3xuhjkYGToVPqs"));
-    BOOST_CHECK(CheckAlertAuthorization("1EXoDusjGwvnjZUyKkxZ4UHEf77z6A5S4P"));
+    BOOST_CHECK(CheckAlertAuthorization("16oDZYCspsczfgKXVj3xyvsxH21NpEj94F"));
 
     mapMultiArgs = mapMultiArgsOriginal;
     mapArgs = mapArgsOriginal;
