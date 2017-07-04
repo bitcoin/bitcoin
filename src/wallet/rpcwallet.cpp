b363ee3c193c7cfae428ff3097334fff16f30f43
sidhujag <sidhujag@hotmail.com>
2017-07-04 14:13:07 -0700

    revert
    Former-commit-id: ea341a691e55c66e249aad91e3fea6f128f7f309

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -1726,8 +1726,6 @@ void ListTransactions(const CWalletTx& wtx, const string& strAccount, int nMinDe
 				strResponse = GetSyscoinTransactionDescription(op, vvchArgs, "send", wtx, strResponseEnglish, strResponseGUID, strResponseGUID1);
 				if (isSysScript)
 					entry.push_back(Pair("amount", ValueFromAmount(-s.amount)));
-				else if (decodedAndParsed)
-					entry.push_back(Pair("amount", ValueFromAmount(-wtx.vout[nOut].nValue)));
 				else
 					entry.push_back(Pair("amount", ValueFromAmount(-wtx.GetValueOut())));
 				entry.push_back(Pair("systx", strResponse));
@@ -1786,8 +1784,6 @@ void ListTransactions(const CWalletTx& wtx, const string& strAccount, int nMinDe
 					strResponse = GetSyscoinTransactionDescription(op, vvchArgs, "recv", wtx, strResponseEnglish, strResponseGUID, strResponseGUID1);
 					if (isSysScript)
 						entry.push_back(Pair("amount", ValueFromAmount(r.amount)));
-					else if(decodedAndParsed)
-						entry.push_back(Pair("amount", ValueFromAmount(wtx.vout[nOut].nValue)));
 					else
 						entry.push_back(Pair("amount", ValueFromAmount(wtx.GetValueOut())));
 					entry.push_back(Pair("systx", strResponse));
