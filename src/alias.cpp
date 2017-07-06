d3787dbbc28fa5047155865021cfa6095fc65420
sidhujag <sidhujag@hotmail.com>
2017-07-06 09:50:21 -0700

    typo
    Former-commit-id: 4a22506ee967a5d277b7ad0ed5656eaa8dd9e06e

--- src/alias.cpp
+++ src/alias.cpp
@@ -2547,7 +2547,7 @@ UniValue aliaslist(const UniValue& params, bool fHelp) {
 			"list my own aliases.\n"
 			"[aliasname] alias name to use as filter.\n"
 			"[count]    (numeric, optional, default=10) The number of results to return\n"
-			"[from]     (numeric, optional, default=0) The number of results to skip\n"");
+			"[from]     (numeric, optional, default=0) The number of results to skip\n");
 
 			vector<unsigned char> vchAlias;
 	if (params.size() >= 1)
