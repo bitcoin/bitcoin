7c948a02dd65f40b788f616661bb191232725312
Pieter Wuille <pieter.wuille@gmail.com>
2017-07-07 00:54:42 -0700

    Avoid dereference-of-casted-pointer

--- src/core_memusage.h
+++ src/core_memusage.h
@@ -10,7 +10,7 @@
 #include "memusage.h"
 
 static inline size_t RecursiveDynamicUsage(const CScript& script) {
-    return memusage::DynamicUsage(*static_cast<const CScriptBase*>(&script));
+    return memusage::DynamicUsage(script);
 }
 
 static inline size_t RecursiveDynamicUsage(const COutPoint& out) {
