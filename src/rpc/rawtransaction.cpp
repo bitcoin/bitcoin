397c1a75225d6c2242cecce5a36c5ee069f60765
Gregory Sanders <gsanders87@gmail.com>
2017-07-10 11:44:39 -0400

    check for null values in rpc args and handle appropriately
    Conflicts:
    	src/wallet/rpcwallet.cpp

--- src/rpc/rawtransaction.cpp
+++ src/rpc/rawtransaction.cpp
@@ -140,7 +140,7 @@ UniValue getrawtransaction(const JSONRPCRequest& request)
 
     // Accept either a bool (true) or a num (>=1) to indicate verbose output.
     bool fVerbose = false;
-    if (request.params.size() > 1) {
+    if (!request.params[1].isNull()) {
         if (request.params[1].isNum()) {
             if (request.params[1].get_int() != 0) {
                 fVerbose = true;
@@ -214,7 +214,7 @@ UniValue gettxoutproof(const JSONRPCRequest& request)
     CBlockIndex* pblockindex = nullptr;
 
     uint256 hashBlock;
-    if (request.params.size() > 1)
+    if (!request.params[1].isNull())
     {
         hashBlock = uint256S(request.params[1].get_str());
         if (!mapBlockIndex.count(hashBlock))
@@ -415,7 +415,7 @@ UniValue createrawtransaction(const JSONRPCRequest& request)
         }
     }
 
-    if (request.params.size() > 3 && rbfOptIn != SignalsOptInRBF(rawTx)) {
+    if (!request.params[3].isNull() && rbfOptIn != SignalsOptInRBF(rawTx)) {
         throw JSONRPCError(RPC_INVALID_PARAMETER, "Invalid parameter combination: Sequence number(s) contradict replaceable option");
     }
 
