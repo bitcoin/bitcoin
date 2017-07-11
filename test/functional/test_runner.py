2bedf92d60817acad9843192513e9cb7fd7cfe69
John Newbery <john@johnnewbery.com>
2017-07-11 13:01:44 -0400

    [tests] add TestNodeCLI class for calling bitcoin-cli for a node

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -279,6 +279,7 @@ def run_tests(test_list, src_dir, build_dir, exeext, tmpdir, jobs=1, enable_cove
     #Set env vars
     if "BITCOIND" not in os.environ:
         os.environ["BITCOIND"] = build_dir + '/src/bitcoind' + exeext
+        os.environ["BITCOINCLI"] = build_dir + '/src/bitcoin-cli' + exeext
 
     tests_dir = src_dir + '/test/functional/'
 
