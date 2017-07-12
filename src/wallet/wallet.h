9a1675ee5b27f8634f9917a1f80904c9319739d3
Cory Fields <cory-nospam-@coryfields.com>
2017-07-12 16:48:36 -0400

    optim: mark a few classes final

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -648,7 +648,7 @@ private:
  * A CWallet is an extension of a keystore, which also maintains a set of transactions and balances,
  * and provides the ability to create new transactions.
  */
-class CWallet : public CCryptoKeyStore, public CValidationInterface
+class CWallet final : public CCryptoKeyStore, public CValidationInterface
 {
 private:
     static std::atomic<bool> fFlushScheduled;
