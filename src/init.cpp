28197ef74280c17e0ee04a9d7f6a55a9380d16b3
Pieter Wuille <pieter.wuille@gmail.com>
2017-07-12 15:14:22 -0700

    Merge #10714: Avoid printing incorrect block indexing time due to uninitialized variable
    959dd8781 Avoid printing incorrect block indexing time due to uninitialized variable (practicalswift)
    
    Tree-SHA512: a76e43c3ffa734ed5c7eadf363f345f268aa0e6ce775aba8f856fe3bbc82f240dc7c734c5ca3ac500a12eb41fae00623413e79f484d5acf809b6e400851d771d

--- src/init.cpp
+++ src/init.cpp
@@ -1850,7 +1850,9 @@ bool AppInitMain(boost::thread_group& threadGroup, CScheduler& scheduler)
         LogPrintf("Shutdown requested. Exiting.\n");
         return false;
     }
-    LogPrintf(" block index %15dms\n", GetTimeMillis() - nStart);
+    if (fLoaded) {
+        LogPrintf(" block index %15dms\n", GetTimeMillis() - nStart);
+    }
 
     fs::path est_path = GetDataDir() / FEE_ESTIMATES_FILENAME;
     CAutoFile est_filein(fsbridge::fopen(est_path, "rb"), SER_DISK, CLIENT_VERSION);
