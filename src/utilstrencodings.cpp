1e588b4df2c9a46f087fdf4b936c4847dbeccf06
Luke Dashjr <luke-jr+git@utopios.org>
2017-07-13 08:26:56 +0000

    SanitizeString: Support optional escaping of disallowed characters

--- src/utilstrencodings.cpp
+++ src/utilstrencodings.cpp
@@ -21,13 +21,16 @@ static const std::string SAFE_CHARS[] =
     CHARS_ALPHA_NUM + ".-_", // SAFE_CHARS_FILENAME
 };
 
-std::string SanitizeString(const std::string& str, int rule)
+std::string SanitizeString(const std::string& str, int rule, bool escape)
 {
     std::string strResult;
     for (std::string::size_type i = 0; i < str.size(); i++)
     {
-        if (SAFE_CHARS[rule].find(str[i]) != std::string::npos)
+        if (SAFE_CHARS[rule].find(str[i]) != std::string::npos) {
             strResult.push_back(str[i]);
+        } else if (escape) {
+            strResult += strprintf("%%%02X", str[i]);
+        }
     }
     return strResult;
 }
