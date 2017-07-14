a1decce99b7de15add3b8a3a8c800f97aad68f75
Mark Friedenbach <mark@friedenbach.org>
2017-07-14 19:03:57 +0900

    Add new difficulty adjustment filter, which replaces the simple average over 2016 blocks with a faster-activing 36-tap Parks and McClellan FIR filter, and adjustments every 15 blocks with a gain of 0.125 and a limiter of 1.375.

--- src/pow.cpp
+++ src/pow.cpp
@@ -9,6 +9,11 @@
 #include "chain.h"
 #include "primitives/block.h"
 #include "uint256.h"
+#include "util.h"
+#include "utilstrencodings.h"
+
+#include <gmp.h>
+#include <gmpxx.h>
 
 unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHeader *pblock, const Consensus::Params& params)
 {
@@ -16,7 +21,7 @@ unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHead
     unsigned int nProofOfWorkLimit = UintToArith256(params.powLimit).GetCompact();
 
     // Only change once per difficulty adjustment interval
-    if ((pindexLast->nHeight+1) % params.DifficultyAdjustmentInterval() != 0)
+    if ((pindexLast->nHeight+1) % params.DifficultyAdjustmentInterval(pindexLast->nHeight+1) != 0)
     {
         if (params.fPowAllowMinDifficultyBlocks)
         {
@@ -29,7 +34,7 @@ unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHead
             {
                 // Return the last non-special-min-difficulty-rules-block
                 const CBlockIndex* pindex = pindexLast;
-                while (pindex->pprev && pindex->nHeight % params.DifficultyAdjustmentInterval() != 0 && pindex->nBits == nProofOfWorkLimit)
+                while (pindex->pprev && pindex->nHeight % params.DifficultyAdjustmentInterval(pindex->nHeight) != 0 && pindex->nBits == nProofOfWorkLimit)
                     pindex = pindex->pprev;
                 return pindex->nBits;
             }
@@ -38,7 +43,7 @@ unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHead
     }
 
     // Go back by what we want to be 14 days worth of blocks
-    int nHeightFirst = pindexLast->nHeight - (params.DifficultyAdjustmentInterval()-1);
+    int nHeightFirst = pindexLast->nHeight - (params.DifficultyAdjustmentInterval(pindexLast->nHeight+1)-1);
     assert(nHeightFirst >= 0);
     const CBlockIndex* pindexFirst = pindexLast->GetAncestor(nHeightFirst);
     assert(pindexFirst);
@@ -46,27 +51,118 @@ unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHead
     return CalculateNextWorkRequired(pindexLast, pindexFirst->GetBlockTime(), params);
 }
 
+mpz_class inline i64_to_mpz(int64_t nValue)
+{
+    return mpz_class(i64tostr(nValue));
+}
+
+int64_t inline mpz_to_i64(const mpz_class &zValue)
+{
+    static mpz_class MPZ_MAX_I64( "9223372036854775807");
+    static mpz_class MPZ_MIN_I64("-9223372036854775808");
+    if (zValue < MPZ_MIN_I64 || zValue > MPZ_MAX_I64)
+        throw std::runtime_error("mpz_to_i64 : input exceeds range of int64_t type");
+    int64_t result = 0;
+    mpz_class tmp(zValue);
+    bool sign = tmp < 0;
+    if ( sign ) tmp = -tmp;
+    result = atoi64(tmp.get_str());
+    return (sign ? -result : result);
+}
+
+mpz_class inline mpz_from_uint256(const uint256& bn)
+{
+    mpz_class z;
+    mpz_import(z.get_mpz_t(), 32, -1, 1, -1, 0, bn.begin());
+    return z;
+}
+
+uint256 inline uint256_from_mpz(const mpz_class& z)
+{
+    uint256 bn;
+    mpz_export(bn.begin(), NULL, -1, 32, -1, 0, z.get_mpz_t());
+    return bn;
+}
+
 unsigned int CalculateNextWorkRequired(const CBlockIndex* pindexLast, int64_t nFirstBlockTime, const Consensus::Params& params)
 {
     if (params.fPowNoRetargeting)
         return pindexLast->nBits;
 
-    // Limit adjustment step
-    int64_t nActualTimespan = pindexLast->GetBlockTime() - nFirstBlockTime;
-    if (nActualTimespan < params.nPowTargetTimespan/4)
-        nActualTimespan = params.nPowTargetTimespan/4;
-    if (nActualTimespan > params.nPowTargetTimespan*4)
-        nActualTimespan = params.nPowTargetTimespan*4;
+    const int kWindowSize = 36;
+    static mpq_class kOne = mpq_class(1);
+    static mpq_class kGain = mpq_class(1, 8);       // 0.125
+    static mpq_class kLimiterUp = mpq_class(11, 8); // 1.375
+    static mpq_class kLimiterDown = mpq_class(8, 11);
+    static mpq_class kTargetInterval = mpq_class(i64_to_mpz(params.nPowTargetSpacing));
+    static int32_t kFilterCoeff[kWindowSize] =
+        {  140111793,  105764386,  109100300,   79262945,   11957247,  -86965742,
+          -200607279, -301199508, -354940962, -328658726, -197005272,   49124369,
+           398931360,  820161125, 1263269526, 1669153929, 1979408502, 2147483647,
+          2147483647, 1979408502, 1669153929, 1263269526,  820161125,  398931360,
+            49124369, -197005272, -328658726, -354940962, -301199508, -200607279,
+           -86965742,   11957247,   79262945,  109100300,  105764386,  140111793 };
+    static mpz_class kNormFactor = mpz_class("14608703280");
+
+    const bool fUseFilter = (pindexLast->nHeight >= (params.nFIRDiffFilterThreshold-1));
+
+    LogPrintf("GetNextWorkRequired RETARGET\n");
+
+    mpq_class dAdjustmentFactor;
+
+    if (fUseFilter)
+    {
+        int64_t vTimeDelta[kWindowSize];
+
+        size_t idx = 0;
+        const CBlockIndex *pitr = pindexLast;
+        for ( ; idx!=kWindowSize && pitr && pitr->pprev; ++idx, pitr=pitr->pprev )
+            vTimeDelta[idx] = (int32_t)(pitr->GetBlockTime() - pitr->pprev->GetBlockTime());
+        for ( ; idx!=kWindowSize; ++idx )
+            vTimeDelta[idx] = (int32_t)params.nPowTargetSpacing;
+
+        int64_t vFilteredTime = 0;
+        for ( idx=0; idx<kWindowSize; ++idx )
+            vFilteredTime += (int64_t)kFilterCoeff[idx] * (int64_t)vTimeDelta[idx];
+        mpq_class dFilteredInterval(i64_to_mpz(vFilteredTime), kNormFactor);
+
+        dAdjustmentFactor = kOne - kGain * (dFilteredInterval - kTargetInterval) / kTargetInterval;
+        if (dAdjustmentFactor > kLimiterUp)
+            dAdjustmentFactor = kLimiterUp;
+        if (dAdjustmentFactor < kLimiterDown)
+            dAdjustmentFactor = kLimiterDown;
+    } else {
+        // Limit adjustment step
+        int64_t nActualTimespan = pindexLast->GetBlockTime() - nFirstBlockTime;
+        LogPrintf("  nActualTimespan = %d  before bounds\n", nActualTimespan);
+        if (nActualTimespan < params.nPowOriginalTargetTimespan/4)
+            nActualTimespan = params.nPowOriginalTargetTimespan/4;
+        if (nActualTimespan > params.nPowOriginalTargetTimespan*4)
+            nActualTimespan = params.nPowOriginalTargetTimespan*4;
+
+        dAdjustmentFactor = mpq_class(i64_to_mpz(params.nPowOriginalTargetTimespan),
+                                      i64_to_mpz(nActualTimespan));
+    }
 
     // Retarget
-    const arith_uint256 bnPowLimit = UintToArith256(params.powLimit);
-    arith_uint256 bnNew;
-    bnNew.SetCompact(pindexLast->nBits);
-    bnNew *= nActualTimespan;
-    bnNew /= params.nPowTargetTimespan;
+    arith_uint256 bnOld;
+    bnOld.SetCompact(pindexLast->nBits);
 
+    mpz_class zNew;
+    zNew  = mpz_from_uint256(ArithToUint256(bnOld));
+    zNew *= dAdjustmentFactor.get_den();
+    zNew /= dAdjustmentFactor.get_num();
+
+    arith_uint256 bnNew;
+    bnNew = UintToArith256(uint256_from_mpz(zNew));
+
+    const arith_uint256 bnPowLimit = UintToArith256(params.powLimit);
     if (bnNew > bnPowLimit)
         bnNew = bnPowLimit;
+
+    LogPrintf("dAdjustmentFactor = %g\n", dAdjustmentFactor.get_d());
+    LogPrintf("Before: %08x  %s\n", pindexLast->nBits, ArithToUint256(bnOld).ToString());
+    LogPrintf("After:  %08x  %s\n", bnNew.GetCompact(), ArithToUint256(bnNew).ToString());
 
     return bnNew.GetCompact();
 }
