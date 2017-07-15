10c31c84d881b09358b6e457649b7ce4e2f2e2a8
Gregory Maxwell <greg@xiph.org>
2017-07-15 01:09:10 +0000

    Increase wallet default keypool size to 1000.

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -40,7 +40,7 @@ extern unsigned int nTxConfirmTarget;
 extern bool bSpendZeroConfChange;
 extern bool fWalletRbf;
 
-static const unsigned int DEFAULT_KEYPOOL_SIZE = 100;
+static const unsigned int DEFAULT_KEYPOOL_SIZE = 1000;
 //! -paytxfee default
 static const CAmount DEFAULT_TRANSACTION_FEE = 0;
 //! -fallbackfee default
