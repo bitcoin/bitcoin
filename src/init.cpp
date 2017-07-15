9e6a1ead631b990d621e42ba94c2cad29e1e0084
Wladimir J. van der Laan <laanwj@gmail.com>
2017-07-15 10:46:06 +0200

    init: Factor out AppInitLockDataDirectory
    Alternative to #10818, alternative solution to #10815.
    
    After this change: All the AppInit steps before and inclusive
    AppInitLockDataDirectory must not have Shutdown() called in case of
    failure. Only when AppInitMain fails, Shutdown should be called.
    
    Changes the GUI and bitcoind code to consistently do this.

--- src/init.cpp
+++ src/init.cpp
@@ -1263,13 +1263,13 @@ bool AppInitSanityChecks()
         return InitError(_("Initialization sanity check failed. Dash Core is shutting down."));
 
     // Probe the data directory lock to give an early error message, if possible
+    // We cannot hold the data directory lock here, as the forking for daemon() hasn't yet happened,
+    // and a fork will cause weird behavior to it.
     return LockDataDirectory(true);
 }
 
-bool AppInitMain(boost::thread_group& threadGroup, CScheduler& scheduler)
+bool AppInitLockDataDirectory()
 {
-    const CChainParams& chainparams = Params();
-    // ********************************************************* Step 4a: application initialization
     // After daemonization get the data directory lock again and hold on to it until exit
     // This creates a slight window for a race condition to happen, however this condition is harmless: it
     // will at most make us exit without printing a message to console.
@@ -1277,7 +1277,13 @@ bool AppInitMain(boost::thread_group& threadGroup, CScheduler& scheduler)
         // Detailed error printed inside LockDataDirectory
         return false;
     }
+    return true;
+}
 
+bool AppInitMain(boost::thread_group& threadGroup, CScheduler& scheduler)
+{
+    const CChainParams& chainparams = Params();
+    // ********************************************************* Step 4a: application initialization
 #ifndef WIN32
     CreatePidFile(GetPidFile(), getpid());
 #endif
