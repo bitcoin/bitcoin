90ff2b4287cf1648ebc605347af5114301b1d59f
Dag Robole <dag.robole@gmail.com>
2017-07-15 21:34:52 +0200

    Fix resource leak

--- src/random.cpp
+++ src/random.cpp
@@ -186,6 +186,7 @@ void GetDevURandom(unsigned char *ent32)
     do {
         ssize_t n = read(f, ent32 + have, NUM_OS_RANDOM_BYTES - have);
         if (n <= 0 || n + have > NUM_OS_RANDOM_BYTES) {
+            close(f);
             RandFailure();
         }
         have += n;
