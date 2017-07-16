5ea3cc2366233881c53441d70989940d35a4a583
practicalswift <practicalswift@users.noreply.github.com>
2017-07-16 23:42:27 +0200

    Enable devirtualization opportunities by using the final specifier (C++11)
    * Declaring CCoinsViewErrorCatcher final enables devirtualization of two calls
    * Declaring CReserveKey final enables devirtualization of one call

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -1214,7 +1214,7 @@ public:
 };
 
 /** A key allocated from the key pool. */
-class CReserveKey : public CReserveScript
+class CReserveKey final : public CReserveScript
 {
 protected:
     CWallet* pwallet;
