7ecb554e3774a3b24c853f695fccef26051b3ba6
Jeff Garzik <jeff@bloq.com>
2017-07-20 23:10:44 -0400

    segwit2x node preferential peering policy
    1) Advertise node_segwit2x
    2) Prefer to peer with segwit2x and, for now, segwit nodes
    3) Option to disable pref peering

--- src/init.cpp
+++ src/init.cpp
@@ -71,6 +71,7 @@ static const bool DEFAULT_PROXYRANDOMIZE = true;
 static const bool DEFAULT_REST_ENABLE = false;
 static const bool DEFAULT_DISABLE_SAFEMODE = false;
 static const bool DEFAULT_STOPAFTERBLOCKIMPORT = false;
+static const bool DEFAULT_PREFPEERING = true;
 
 std::unique_ptr<CConnman> g_connman;
 std::unique_ptr<PeerLogicValidation> peerLogic;
@@ -486,6 +487,7 @@ std::string HelpMessage(HelpMessageMode mode)
     strUsage += HelpMessageOpt("-bytespersigop", strprintf(_("Equivalent bytes per sigop in transactions for relay and mining (default: %u)"), DEFAULT_BYTES_PER_SIGOP));
     strUsage += HelpMessageOpt("-datacarrier", strprintf(_("Relay and mine data carrier transactions (default: %u)"), DEFAULT_ACCEPT_DATACARRIER));
     strUsage += HelpMessageOpt("-datacarriersize", strprintf(_("Maximum size of data in data carrier transactions we relay and mine (default: %u)"), MAX_OP_RETURN_RELAY));
+    strUsage += HelpMessageOpt("-prefpeering", strprintf(_("Preferential peering with segwit2x (and segwit) nodes (default: %u)"), DEFAULT_PREFPEERING));
     strUsage += HelpMessageOpt("-mempoolreplacement", strprintf(_("Enable transaction replacement in the memory pool (default: %u)"), DEFAULT_ENABLE_REPLACEMENT));
     strUsage += HelpMessageOpt("-minrelaytxfee=<amt>", strprintf(_("Fees (in %s/kB) smaller than this are considered zero fee for relaying, mining and transaction creation (default: %s)"),
         CURRENCY_UNIT, FormatMoney(DEFAULT_MIN_RELAY_TX_FEE)));
@@ -1076,8 +1078,14 @@ bool AppInitParameterInteraction()
     fAcceptDatacarrier = GetBoolArg("-datacarrier", DEFAULT_ACCEPT_DATACARRIER);
     nMaxDatacarrierBytes = GetArg("-datacarriersize", nMaxDatacarrierBytes);
 
+    // Advertise as segwit2x node
     nLocalServices = ServiceFlags(nLocalServices | NODE_SEGWIT2X);
-    nRelevantServices = ServiceFlags(nRelevantServices | NODE_SEGWIT2X);
+
+    // Prefer peers with compatible rulesets
+    if (GetBoolArg("-prefpeering", DEFAULT_PREFPEERING)) {
+        nRelevantServices = ServiceFlags(nRelevantServices | NODE_SEGWIT2X);
+        nRelevantServices = ServiceFlags(nRelevantServices | NODE_WITNESS); // TBR
+    }
 
     // Option to startup with mocktime set (used for regression testing):
     SetMockTime(GetArg("-mocktime", 0)); // SetMockTime(0) is a no-op
