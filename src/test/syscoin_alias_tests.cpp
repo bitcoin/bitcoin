dea179de9c83d832726f61e858af2719dea373c8
sidhujag <sidhujag@hotmail.com>
2017-07-24 12:19:36 -0700

    wip multisig test

--- src/test/syscoin_alias_tests.cpp
+++ src/test/syscoin_alias_tests.cpp
@@ -504,8 +504,7 @@ BOOST_AUTO_TEST_CASE (generate_multisigalias)
 	BOOST_CHECK_EQUAL(find_value(r.get_obj(), "address").get_str(), addressStr);
 	// change the multisigs pw and public data
 	BOOST_CHECK_NO_THROW(CallRPC("node1", "aliasaddscript " + redeemScript));
-	// if updating p2sh alias and changing pw, must always pass in address aswell otherwise new address will be generated for you
-	string hex_str = AliasUpdate("node1", "jagnodemultisig1", "pubdata1", "privdata1", "newpassword", "\"\"", addressStr);
+	string hex_str = AliasUpdate("node1", "jagnodemultisig1", "pubdata1", "privdata1", "\"\"", "\"\"", addressStr);
 	BOOST_CHECK_NO_THROW(r = CallRPC("node1", "aliasinfo jagnodemultisig1"));
 	BOOST_CHECK_EQUAL(find_value(r.get_obj(), "address").get_str(), addressStr);
 	BOOST_CHECK(!hex_str.empty());
@@ -536,7 +535,7 @@ BOOST_AUTO_TEST_CASE (generate_multisigalias)
 	BOOST_CHECK(address_value.isStr());
 	redeemScript = redeemScript_value.get_str();
 	addressStr = address_value.get_str();
-	hex_str = AliasUpdate("node1", "jagnodemultisig1", "pubdata", "privdata", "password", "\"\"", addressStr);
+	hex_str = AliasUpdate("node1", "jagnodemultisig1", "pubdata", "privdata", "\"\"", "\"\"", addressStr);
 	BOOST_CHECK_NO_THROW(r = CallRPC("node2", "syscoinsignrawtransaction " + hex_str));
 	GenerateBlocks(5, "node2");
 	GenerateBlocks(5);
@@ -561,7 +560,7 @@ BOOST_AUTO_TEST_CASE (generate_multisigalias)
 	BOOST_CHECK(address_value.isStr());
 	redeemScript = redeemScript_value.get_str();
 	addressStr = address_value.get_str();
-	tmp = AliasUpdate("node1", "jagnodemultisig1", "pubdata", "privdata", "password", "\"\"", addressStr);
+	tmp = AliasUpdate("node1", "jagnodemultisig1", "pubdata", "privdata", "\"\"", "\"\"", addressStr);
 	BOOST_CHECK_EQUAL(tmp, "");
 	BOOST_CHECK_NO_THROW(r = CallRPC("node1", "aliasinfo jagnodemultisig1"));
 	BOOST_CHECK_EQUAL(find_value(r.get_obj(), "address").get_str(), addressStr);
