bf636942dd652484f93aa4f2733ae40d051d96da
practicalswift <practicalswift@users.noreply.github.com>
2017-08-01 23:40:19 +0200

    Pass SendCoinsRecipient (208 bytes) by const reference

--- src/qt/paymentserver.cpp
+++ src/qt/paymentserver.cpp
@@ -621,7 +621,7 @@ void PaymentServer::fetchRequest(const QUrl& url)
     netManager->get(netRequest);
 }
 
-void PaymentServer::fetchPaymentACK(CWallet* wallet, SendCoinsRecipient recipient, QByteArray transaction)
+void PaymentServer::fetchPaymentACK(CWallet* wallet, const SendCoinsRecipient& recipient, QByteArray transaction)
 {
     const payments::PaymentDetails& details = recipient.paymentRequest.getDetails();
     if (!details.has_payment_url())
