1888a3014d4e6798f9bd98775baa2d792f7d6ebb
Infernoman <jjaauto@hotmail.com>
2017-08-02 17:08:39 -0400

    testnet alert key

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -223,7 +223,7 @@ public:
         pchMessageStart[1] = 0x18;
         pchMessageStart[2] = 0x0e;
         pchMessageStart[3] = 0x06;
-        vAlertPubKey = ParseHex("04517d8a699cb43d3938d7b24faaff7cda448ca4ea267723ba614784de661949bf632d6304316b244646dea079735b9a6fc4af804efb4752075b9fe2245e14e412");
+        vAlertPubKey = ParseHex("04977aae0411f4e1757e8682c87ee79180ad577ef0351054e6cda5c9381fcd8c7333e88ac250d3ab3e3aafd5d1c1d946f2ca62372db7f35c84398a878aa145f09a");
         nDefaultPort = 19340;
         nEnforceBlockUpgradeMajority = 51;
         nRejectBlockOutdatedMajority = 75;
