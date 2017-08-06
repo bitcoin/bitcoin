fbab2ff34ea7189d1cebfa2711ac24fba65b3f37
Andrew Stone <g.andrew.stone@gmail.com>
2017-08-05 20:53:47 -0400

    add unit test for IsTxProbablyNewSigHash

--- src/test/buip055_test.cpp
+++ src/test/buip055_test.cpp
@@ -17,6 +17,7 @@
 #include "script/script_error.h"
 #include "utilstrencodings.h"
 #include "buip055fork.h"
+#include "script/sign.h"
 
 #include <map>
 #include <string>
@@ -30,6 +31,43 @@
 #include <univalue.h>
 
 BOOST_FIXTURE_TEST_SUITE(buip055_tests, BasicTestingSetup)
+
+// Helper: create two dummy transactions, each with
+// two outputs.  The first has 11 and 50 CENT outputs
+// paid to a TX_PUBKEY, the second 21 and 22 CENT outputs
+// paid to a TX_PUBKEYHASH.
+//
+static std::vector<CMutableTransaction>
+SetupDummyInputs(CBasicKeyStore& keystoreRet, CCoinsViewCache& coinsRet)
+{
+    std::vector<CMutableTransaction> dummyTransactions;
+    dummyTransactions.resize(2);
+
+    // Add some keys to the keystore:
+    CKey key[4];
+    for (int i = 0; i < 4; i++)
+    {
+        key[i].MakeNewKey(i % 2);
+        keystoreRet.AddKey(key[i]);
+    }
+
+    // Create some dummy input transactions
+    dummyTransactions[0].vout.resize(2);
+    dummyTransactions[0].vout[0].nValue = 11*CENT;
+    dummyTransactions[0].vout[0].scriptPubKey << ToByteVector(key[0].GetPubKey()) << OP_CHECKSIG;
+    dummyTransactions[0].vout[1].nValue = 50*CENT;
+    dummyTransactions[0].vout[1].scriptPubKey << ToByteVector(key[1].GetPubKey()) << OP_CHECKSIG;
+    coinsRet.ModifyCoins(dummyTransactions[0].GetHash())->FromTx(dummyTransactions[0], 0);
+
+    dummyTransactions[1].vout.resize(2);
+    dummyTransactions[1].vout[0].nValue = 21*CENT;
+    dummyTransactions[1].vout[0].scriptPubKey = GetScriptForDestination(key[2].GetPubKey().GetID());
+    dummyTransactions[1].vout[1].nValue = 22*CENT;
+    dummyTransactions[1].vout[1].scriptPubKey = GetScriptForDestination(key[3].GetPubKey().GetID());
+    coinsRet.ModifyCoins(dummyTransactions[1].GetHash())->FromTx(dummyTransactions[1], 0);
+
+    return dummyTransactions;
+}
 
 BOOST_AUTO_TEST_CASE(buip055_op_return)
 {
@@ -73,6 +111,43 @@ BOOST_AUTO_TEST_CASE(buip055_op_return)
 
 }
 
+BOOST_AUTO_TEST_CASE(buip055_sighash)
+{
+    LOCK(cs_main);
+    CBasicKeyStore keystore;
+    CCoinsView coinsDummy;
+    CCoinsViewCache coins(&coinsDummy);
+    std::vector<CMutableTransaction> dummyTransactions = SetupDummyInputs(keystore, coins);
 
+    CMutableTransaction t;
+    t.vin.resize(1);
+    t.vin[0].prevout.hash = dummyTransactions[0].GetHash();
+    t.vin[0].prevout.n = 1;
+    t.vout.resize(1);
+    t.vout[0].nValue = 90*CENT;
+    CKey key;
+    key.MakeNewKey(true);
+    t.vout[0].scriptPubKey = GetScriptForDestination(key.GetPubKey().GetID());
+
+    CTransaction tx(t);
+
+    {
+    TransactionSignatureCreator tsc(&keystore,&tx, 1, 90*CENT, SIGHASH_ALL);
+    const CScript& scriptPubKey = dummyTransactions[0].vout[0].scriptPubKey;
+    CScript& scriptSigRes = t.vin[0].scriptSig;
+    bool worked = ProduceSignature(tsc,scriptPubKey,scriptSigRes);
+    BOOST_CHECK(worked);
+    BOOST_CHECK(IsTxProbablyNewSigHash(t)==false);
+    }
+
+    {
+    TransactionSignatureCreator tsc(&keystore,&tx, 1, 90*CENT, SIGHASH_ALL | SIGHASH_FORKID);
+    const CScript& scriptPubKey = dummyTransactions[0].vout[0].scriptPubKey;
+    CScript& scriptSigRes = t.vin[0].scriptSig;
+    bool worked = ProduceSignature(tsc,scriptPubKey,scriptSigRes);
+    BOOST_CHECK(worked);
+    BOOST_CHECK(IsTxProbablyNewSigHash(t)==true);
+    }
+}
 
 BOOST_AUTO_TEST_SUITE_END()
