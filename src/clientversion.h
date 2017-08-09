e31e50536389a327553b1f4f28aa4d6383a8c21d
Andrew Stone <g.andrew.stone@gmail.com>
2017-08-09 18:40:51 -0400

    bump to version 1.1.1.0

--- src/clientversion.h
+++ src/clientversion.h
@@ -19,7 +19,7 @@
 //! These need to be macros, as clientversion.cpp's and bitcoin*-res.rc's voodoo requires it
 #define CLIENT_VERSION_MAJOR 1
 #define CLIENT_VERSION_MINOR 1
-#define CLIENT_VERSION_REVISION 0
+#define CLIENT_VERSION_REVISION 1
 #define CLIENT_VERSION_BUILD 0  // BU set version 99 to indicate an unreleased version
 
 //! Set to true for release, false for prerelease or test build
