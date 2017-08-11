7cf2f3725ba252213034c03b9181ab36a84ccbeb
Luke Dashjr <luke-jr+git@utopios.org>
2017-08-11 19:42:59 +0000

    ArgsManager: ForceSetArg with int64_t

--- src/util/system.h
+++ src/util/system.h
@@ -359,6 +359,7 @@ public:
     // Forces an arg setting. Called by SoftSetArg() if the arg hasn't already
     // been set. Also called directly in testing.
     void ForceSetArg(const std::string& strArg, const std::string& strValue);
+    void ForceSetArg(const std::string& strArg, int64_t nValue);
 
     /**
      * Returns the appropriate chain name from the program arguments.
