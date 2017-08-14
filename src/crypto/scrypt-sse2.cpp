d8b8d96d2301f2142438f49887b369c821ade617
Romano <romanornr@gmail.com>
2017-08-14 13:36:49 +1000

    Litecoin: enable support for SSE2 instructions in scrypt via configure --enable-sse2 (#362)

--- src/crypto/scrypt-sse2.cpp
+++ src/crypto/scrypt-sse2.cpp
@@ -27,6 +27,8 @@
  * online backup system.
  */
 
+#if defined(USE_SSE2)
+
 #include "crypto/scrypt.h"
 #include <stdlib.h>
 #include <stdint.h>
@@ -134,3 +136,5 @@ void scrypt_1024_1_1_256_sp_sse2(const char *input, char *output, char *scratchp
 
 	PBKDF2_SHA256((const uint8_t *)input, 80, B, 128, 1, (uint8_t *)output, 32);
 }
+
+#endif // USE_SSE2
