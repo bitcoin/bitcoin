c6e0912370b9232668b0598d5bac7f09574a59dd
Wladimir J. van der Laan <laanwj@gmail.com>
2017-08-15 21:22:09 +0200

    test: refactor: Use absolute include paths for test data files

--- src/test/script_tests.cpp
+++ src/test/script_tests.cpp
@@ -2,7 +2,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <data/script_tests.json.h>
+#include <test/data/script_tests.json.h>
 
 #include <core_io.h>
 #include <key.h>
