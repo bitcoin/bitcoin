714498c02f868fae1a613750f997ef009590328b
Matt Corallo <git@bluematt.me>
2017-08-16 13:56:02 -0400

    Fix -Wthread-safety-analysis warnings. Change the sync.h primitives to std from boost.
    Commit 1.
    
    This code was written by @TheBlueMatt in the following branch:
    * https://github.com/TheBlueMatt/bitcoin/commits/2017-08-test-10923
    
    This commit message was written by me (@practicalswift) who also squashed
    @TheBlueMatt's commits into one and tried to summarize the changes made.
    
    Commit 2.
    
    Remove boost include. Remove boost mentions in comments.

--- src/init.cpp
+++ src/init.cpp
@@ -605,14 +605,14 @@ static void BlockNotifyCallback(bool initialSync, const CBlockIndex *pBlockIndex
 }
 
 static bool fHaveGenesis = false;
-static boost::mutex cs_GenesisWait;
+static CWaitableCriticalSection cs_GenesisWait;
 static CConditionVariable condvar_GenesisWait;
 
 static void BlockNotifyGenesisWait(bool, const CBlockIndex *pBlockIndex)
 {
     if (pBlockIndex != nullptr) {
         {
-            boost::unique_lock<boost::mutex> lock_GenesisWait(cs_GenesisWait);
+            WaitableLock lock_GenesisWait(cs_GenesisWait);
             fHaveGenesis = true;
         }
         condvar_GenesisWait.notify_all();
@@ -1726,7 +1726,7 @@ bool AppInitMain(boost::thread_group& threadGroup, CScheduler& scheduler)
 
     // Wait for genesis block to be processed
     {
-        boost::unique_lock<boost::mutex> lock(cs_GenesisWait);
+        WaitableLock lock(cs_GenesisWait);
         while (!fHaveGenesis) {
             condvar_GenesisWait.wait(lock);
         }
