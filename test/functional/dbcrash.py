2dcdb5f184e9ec518274aa0607ff30f58c52b700
John Newbery <john@johnnewbery.com>
2017-08-16 15:46:48 -0400

    [tests] fix timeout issues from TestNode
    Fixes a couple of bugs from the introduction of TestNode:
    
    - test scripts were no longer able to specify a custom timeout for
    starting a node. Therefore tests with nodes that take a long time to
    start up (eg pruning.py) would fail.
    - the test for whether a node has failed on start up was broken
    by changing 'assert x is None' to 'assert not x'. Since
    subprocess.poll() can return None (indicating the node is still running)
    or 0 (indicating the node exited with return code 0), this was a
    regression.
    
    Conflicts:
    	test/functional/test_framework/test_node.py

--- test/functional/dbcrash.py
+++ test/functional/dbcrash.py
@@ -67,7 +67,8 @@ class ChainstateWriteCrashTest(BitcoinTestFramework):
         self.crashed_on_restart = 0     # Track count of crashes during recovery
 
     def setup_network(self):
-        self.setup_nodes()
+        # Need a bit of extra time for the nodes to start up for this test
+        self.nodes = self.start_nodes(self.num_nodes, self.options.tmpdir, self.extra_args, timewait=90)
         # Leave them unconnected, we'll use submitblock directly in this test
 
     # Starts up a given node id, waits for the tip to reach the given block
@@ -77,10 +78,10 @@ class ChainstateWriteCrashTest(BitcoinTestFramework):
     # Returns the utxo hash of the given node.
     def restart_node(self, node_index, expected_tip):
         time_start = time.time()
-        while time.time() - time_start < 60:
+        while time.time() - time_start < 120:
             try:
                 # Any of these RPC calls could throw due to node crash
-                self.nodes[node_index] = self.start_node(node_index, self.options.tmpdir, self.extra_args[node_index])
+                self.nodes[node_index] = self.start_node(node_index, self.options.tmpdir, self.extra_args[node_index], timewait=90)
                 self.nodes[node_index].waitforblock(expected_tip)
                 utxo_hash = self.nodes[node_index].gettxoutsetinfo()['hash_serialized_2']
                 return utxo_hash
