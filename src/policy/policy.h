4c82cea99b514193672d8145c7777f3e715694ec
Matt Corallo <git@bluematt.me>
2017-08-25 12:06:13 -0400

    Use a sensible default for blockmaxweight
    No sensible user will ever keep the default settings here, so not
    having sensible defaults only serves to screw users who are
    paying less attention, which makes for terrible defaults.
    
    Github-Pull: #11100
    Rebased-From: 3dc263c9b9068ee9793b6c7a0023eff16d70fb8f

--- src/policy/policy.h
+++ src/policy/policy.h
@@ -17,7 +17,7 @@ class CCoinsViewCache;
 class CTxOut;
 
 /** Default for -blockmaxweight, which controls the range of block weights the mining code will create **/
-static const unsigned int DEFAULT_BLOCK_MAX_WEIGHT = 3000000;
+static const unsigned int DEFAULT_BLOCK_MAX_WEIGHT = MAX_BLOCK_WEIGHT - 4000;
 /** Default for -blockmintxfee, which sets the minimum feerate for a transaction in blocks created by mining code **/
 static const unsigned int DEFAULT_BLOCK_MIN_TX_FEE = 1000;
 /** The maximum weight for transactions we're willing to relay/mine */
