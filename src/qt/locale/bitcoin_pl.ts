f77bc56a0dad01e0c33ee8a376241d431cab0562
Grzegorz Błaszczyk <grzegorz.blaszczyk@gmail.com>
2017-08-29 09:33:53 +0200

    FIX: typo in Polish "Unknown..." => "Nieznane..."
    Message source: "Unknown..."

--- src/qt/locale/bitcoin_pl.ts
+++ src/qt/locale/bitcoin_pl.ts
@@ -895,7 +895,7 @@
     </message>
     <message>
         <source>Unknown...</source>
-        <translation>Nienznane...</translation>
+        <translation>Nieznane...</translation>
     </message>
     <message>
         <source>Last block time</source>
