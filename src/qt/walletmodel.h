a1ea1cfbd8d4fc976f0ab2423d395e03ded6eedd
Chris Moore <dooglus@gmail.com>
2017-08-30 00:32:54 -0700

    qt: Use IsMine to validate custom change address
    (cherry picked from commit c41224dfd51c896341bbf2fa23e160bf5ffe27c3)

--- src/qt/walletmodel.h
+++ src/qt/walletmodel.h
@@ -190,7 +190,7 @@ public:
     UnlockContext requestUnlock();
 
     bool getPubKey(const CKeyID &address, CPubKey& vchPubKeyOut) const;
-    bool havePrivKey(const CKeyID &address) const;
+    bool IsSpendable(const CTxDestination& dest) const;
     bool getPrivKey(const CKeyID &address, CKey& vchPrivKeyOut) const;
     void getOutputs(const std::vector<COutPoint>& vOutpoints, std::vector<COutput>& vOutputs);
     bool isSpent(const COutPoint& outpoint) const;
