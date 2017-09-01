e9bb3dd0ff795bb6ece3df20c403889902f849fe
practicalswift <practicalswift@users.noreply.github.com>
2017-09-01 11:12:30 +0200

    Add GUARDED_BY(m_cs_callbacks_pending) annotation

--- src/scheduler.h
+++ src/scheduler.h
@@ -95,7 +95,7 @@ private:
     CScheduler *m_pscheduler;
 
     CCriticalSection m_cs_callbacks_pending;
-    std::list<std::function<void (void)>> m_callbacks_pending;
+    std::list<std::function<void (void)>> m_callbacks_pending GUARDED_BY(m_cs_callbacks_pending);
     bool m_are_callbacks_running = false;
 
     void MaybeScheduleProcessQueue();
