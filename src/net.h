0cd42f04a4543c2559fd43a36b1110c74b18138b
practicalswift <practicalswift@users.noreply.github.com>
2017-09-01 14:12:32 +0200

    Add GUARDED_BY(cs_inventory) annotation

--- src/net.h
+++ src/net.h
@@ -678,24 +678,24 @@ public:
     int64_t nNextAddrSend;
     int64_t nNextLocalAddrSend;
 
+    CCriticalSection cs_inventory;
     // inventory based relay
-    CRollingBloomFilter filterInventoryKnown;
+    CRollingBloomFilter filterInventoryKnown GUARDED_BY(cs_inventory);
     // Set of transaction ids we still have to announce.
     // They are sorted by the mempool before relay, so the order is not important.
     std::set<uint256> setInventoryTxToSend;
     // List of block ids we still have announce.
     // There is no final sorting before sending, as they are always sent immediately
     // and in the order requested.
-    std::vector<uint256> vInventoryBlockToSend;
-    CCriticalSection cs_inventory;
+    std::vector<uint256> vInventoryBlockToSend GUARDED_BY(cs_inventory);
     std::set<uint256> setAskFor;
     std::multimap<int64_t, CInv> mapAskFor;
     int64_t nNextInvSend;
     // Used for headers announcements - unfiltered blocks to relay
     // Also protected by cs_inventory
-    std::vector<uint256> vBlockHashesToAnnounce;
+    std::vector<uint256> vBlockHashesToAnnounce GUARDED_BY(cs_inventory);
     // Used for BIP35 mempool sending, also protected by cs_inventory
-    bool fSendMempool;
+    bool fSendMempool GUARDED_BY(cs_inventory);
 
     // Last time a "MEMPOOL" request was serviced.
     std::atomic<int64_t> timeLastMempoolReq;
