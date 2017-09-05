806849470b4ad024c616f073db610f17f566f66a
Wladimir J. van der Laan <laanwj@gmail.com>
2017-09-05 22:25:42 +0200

    Merge #11164: Fix boost headers included as user instead of system headers
    5ac072caa Fix boost headers included as user instead of system headers (Dan Raviv)
    
    Pull request description:
    
      In most of the project, boost headers are included as system headers.
      Fix the few inconsistent places where they aren't.
    
    Tree-SHA512: 280af33a7bdc9d68a15b729fa88e1e7627e20a054b8d52a12cc5350c1ac9e9c90fb09f0aa97a00960969f75bcf3403dc52b834c94448b814efa63bfaf3b82663

--- src/txmempool.h
+++ src/txmempool.h
@@ -26,11 +26,10 @@
 #include "bls/bls.h"
 #include "pubkey.h"
 
-#include "boost/multi_index_container.hpp"
-#include "boost/multi_index/ordered_index.hpp"
-#include "boost/multi_index/hashed_index.hpp"
+#include <boost/multi_index_container.hpp>
+#include <boost/multi_index/hashed_index.hpp>
+#include <boost/multi_index/ordered_index.hpp>
 #include <boost/multi_index/sequenced_index.hpp>
-
 #include <boost/signals2/signal.hpp>
 
 class CBlockIndex;
