a513fce7a985f9083489df6f45dce72d8cbf4ccc
Marko Bencun <marko.bencun@monetas.net>
2017-09-11 16:13:46 +0200

    add m_added_nodes to connman options

--- src/net.cpp
+++ src/net.cpp
@@ -1913,11 +1913,6 @@ std::vector<AddedNodeInfo> CConnman::GetAddedNodeInfo()
 
 void CConnman::ThreadOpenAddedConnections()
 {
-    {
-        LOCK(cs_vAddedNodes);
-        vAddedNodes = gArgs.GetArgs("-addnode");
-    }
-
     while (true)
     {
         CSemaphoreGrant grant(*semAddnode);
