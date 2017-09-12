04f78ab5b94424be9cdef86971b7a92de79effa4
Alex Morcos <morcos@chaincode.com>
2017-09-12 12:35:35 -0400

    Do not reject based on mempool min fee when bypass_limits is set.
    This should have always been the case, but we will correctly trim to size after
    a reorg which is when bypass_limits is set.

--- src/validation.cpp
+++ src/validation.cpp
@@ -620,7 +620,7 @@ static bool AcceptToMemoryPoolWorker(const CChainParams& chainparams, CTxMemPool
                 strprintf("%d", nSigOpsCost));
 
         CAmount mempoolRejectFee = pool.GetMinFee(gArgs.GetArg("-maxmempool", DEFAULT_MAX_MEMPOOL_SIZE) * 1000000).GetFee(nSize);
-        if (mempoolRejectFee > 0 && nModifiedFees < mempoolRejectFee) {
+        if (!bypass_limits && mempoolRejectFee > 0 && nModifiedFees < mempoolRejectFee) {
             return state.DoS(0, false, REJECT_INSUFFICIENTFEE, "mempool min fee not met", false, strprintf("%d < %d", nFees, mempoolRejectFee));
         }
 
