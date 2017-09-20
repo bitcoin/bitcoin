45ea6ba4dc7065d524192ceaa3b3139bdae232f9
Alexander Block <ablock84@gmail.com>
2017-09-20 23:49:58 +0200

    Fix invalid cast to CTransaction* in WalletModelTransaction
    CWalletTx/CMerkleTx does not derive from CTransaction anymore.
    This code is different in Bitcoin as it was touched by SegWit code,
    resulting in an upfront fix.

--- src/qt/walletmodeltransaction.cpp
+++ src/qt/walletmodeltransaction.cpp
@@ -33,7 +33,7 @@ CWalletTx *WalletModelTransaction::getTransaction()
 
 unsigned int WalletModelTransaction::getTransactionSize()
 {
-    return (!walletTransaction ? 0 : (::GetSerializeSize(*(CTransaction*)walletTransaction, SER_NETWORK, PROTOCOL_VERSION)));
+    return (!walletTransaction ? 0 : (::GetSerializeSize(walletTransaction->tx, SER_NETWORK, PROTOCOL_VERSION)));
 }
 
 CAmount WalletModelTransaction::getTransactionFee()
