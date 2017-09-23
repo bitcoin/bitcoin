3b08aec8b25b63b8b77d38e9a7cb9d77f72ac137
Peter Tschipper <peter.tschipper@gmail.com>
2017-09-23 08:23:45 -0700

    Create random uint64_t by adding two random 32 bit integers

--- src/random.h
+++ src/random.h
@@ -38,7 +38,10 @@ void seed_insecure_rand(bool fDeterministic = false);
  */
 class FastRandomContext {
 public:
-    explicit FastRandomContext(bool fDeterministic=false);
+    explicit FastRandomContext(bool fDeterministic = false);
+
+    uint32_t Rz;
+    uint32_t Rw;
 
     uint32_t rand32() {
         Rz = 36969 * (Rz & 65535) + (Rz >> 16);
@@ -46,12 +49,15 @@ public:
         return (Rw << 16) + Rz;
     }
 
+    uint64_t rand64() {
+        uint64_t a = rand32();
+        uint64_t b = rand32();
+        return (b << 32) + a;
+    }
+
     bool randbool() {
         return rand32() & 1;
     }
-
-    uint32_t Rz;
-    uint32_t Rw;
 };
 
 /* Number of random bytes returned by GetOSRand.
