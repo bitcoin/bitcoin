61c5fb9ef79bd14b3b4379566847e8ca12414fda
Jonas Schnelli <dev@jonasschnelli.ch>
2017-09-26 15:59:01 -0600

    Merge #11015: [Qt] Add delay before filtering transactions
    7b137aced [Qt] Add delay before filtering transactions Fixes 3141 (Lucas Betschart)
    
    Pull request description:
    
      As discussed in https://github.com/bitcoin/bitcoin/issues/3141.
    
      This adds a QTimer pause of 200ms before start to filter so it should be possible to filter big data sets easier.
    
    Tree-SHA512: ee599367794eac2c5b8bc7ecac47f44295e40c0ff543ff2f2c4860590f917b59b1cfb273fa564e6eb4c44016c0ef412d49f1a8f1b36b07e034022f51bb76653c

--- src/qt/transactionview.h
+++ src/qt/transactionview.h
@@ -117,8 +117,8 @@ public Q_SLOTS:
     void chooseType(int idx);
     void chooseWatchonly(int idx);
     void chooseInstantSend(int idx);
-    void changedPrefix(const QString &prefix);
-    void changedAmount(const QString &amount);
+    void changedAmount();
+    void changedPrefix();
     void exportClicked();
     void focusTransaction(const QModelIndex&);
     void computeSum();
