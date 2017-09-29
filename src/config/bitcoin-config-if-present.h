dfa07f8f918de4db5b5e690755789974875346bb
Dan Raviv <dan@soundradix.com>
2017-09-30 00:19:36 +0300

    Create a wrapper header file which includes config/bitcoin-config.h only if it exists (according to a macro). Files can include this wrapper header directly instead of having each including file repeat the check-if-config-exists-and-only-include-if-it-does macros.

---
+++ src/config/bitcoin-config-if-present.h
@@ -0,0 +1,7 @@
+// Copyright (c) 2017-2017 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#if defined(HAVE_CONFIG_H)
+#include "config/bitcoin-config.h"
+#endif // defined(HAVE_CONFIG_H)
