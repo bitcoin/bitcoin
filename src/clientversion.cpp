762960f64c2e7d5366f8f37c3ab82eceb95322ab
Johnson Lau <jl2012@users.noreply.github.com>
2017-09-29 14:50:30 +0800

    Correct typo in comments

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -10,7 +10,7 @@
 
 /**
  * Name of client reported in the 'version' message. Report the same name
- * for both bitcoind and bitcoin-core, to make it harder for attackers to
+ * for both bitcoind and bitcoin-qt, to make it harder for attackers to
  * target servers or GUI users specifically.
  */
 const std::string CLIENT_NAME("Satoshi");
