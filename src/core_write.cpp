81b91dd16e802901fef362f2d6fee910618d92b1
Anfauglith <amon.e@gmx.net>
2017-09-30 11:46:44 +0200

    forgot some string reps

--- src/core_write.cpp
+++ src/core_write.cpp
@@ -149,7 +149,7 @@ void ScriptPubKeyToUniv(const CScript& scriptPubKey,
 
     UniValue a(UniValue::VARR);
     for (const CTxDestination& addr : addresses)
-        a.push_back(CBitcoinAddress(addr).ToString());
+        a.push_back(CIoPAddress(addr).ToString());
     out.pushKV("addresses", a);
 }
 
