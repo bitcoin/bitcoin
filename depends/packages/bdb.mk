3cd951d2a18810769a0ec46c272256dfb8ce73ba
Anfauglith <amon.e@gmx.net>
2017-09-30 22:56:21 +0200

    fixed strange whitespace

--- depends/packages/bdb.mk
+++ depends/packages/bdb.mk
@@ -16,7 +16,7 @@ define $(package)_preprocess_cmds
   sed -i.old 's/__atomic_compare_exchange/__atomic_compare_exchange_db/' src/dbinc/atomic.h && \ 
   sed -i.old 's/atomic_init/atomic_init_db/' src/dbinc/atomic.h src/mp/mp_region.c src/mp/mp_mvcc.c src/mp/mp_fget.c src/mutex/mut_method.c src/mutex/mut_tas.c && \ 
   sed -i.old 's/WinIoCtl.h/winioctl.h/g' src/dbinc/win_db.h && \ 
-  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub dist 
+	cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub dist 
 endef
 
 define $(package)_config_cmds
