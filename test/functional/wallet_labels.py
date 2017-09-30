949e953d9343be7b667af24c01d8693053463bd4
Thomas Kerin <thomas.kerin@bitmaintech.com>
2017-09-30 14:52:55 +0200

    Add more tests to sort_multisig.py / wallet_labels.py
    sort_multisig test: check uncompressed keys are disallowed
    sort_multisig: add test demonstrating sorting
    wallet_labels: test addmultisigaddress fails if sort=true and (wallet) address is uncompressed

--- test/functional/wallet_labels.py
+++ test/functional/wallet_labels.py
@@ -95,6 +95,23 @@ class WalletLabelsTest(BitcoinTestFramework):
 
         assert_raises_rpc_error(-1, "address_type provided in both options and 4th parameter", node.addmultisigaddress, 2, addresses, {"address_type": 'legacy'}, 'bech32')
 
+    def test_sort_multisig_with_uncompressed_hash160(self, node):
+        node.importpubkey("02632b12f4ac5b1d1b72b2a3b508c19172de44f6f46bcee50ba33f3f9291e47ed0")
+        node.importpubkey("04dd4fe618a8ad14732f8172fe7c9c5e76dd18c2cc501ef7f86e0f4e285ca8b8b32d93df2f4323ebb02640fa6b975b2e63ab3c9d6979bc291193841332442cc6ad")
+        address = "2MxvEpFdXeEDbnz8MbRwS23kDZC8tzQ9NjK"
+
+        addresses = [
+            "msDoRfEfZQFaQNfAEWyqf69H99yntZoBbG",
+            "myrfasv56W7579LpepuRy7KFhVhaWsJYS8",
+        ]
+        default = self.nodes[0].addmultisigaddress(2, addresses, {'address_type': 'legacy'})
+        assert_equal(address, default['address'])
+
+        unsorted = self.nodes[0].addmultisigaddress(2, addresses, {'address_type': 'legacy', "sort": False})
+        assert_equal(address, unsorted['address'])
+
+        assert_raises_rpc_error(-1, "Compressed key required for BIP67: myrfasv56W7579LpepuRy7KFhVhaWsJYS8", node.addmultisigaddress, 2, addresses, {"sort": True})
+
     def run_test(self):
         # Check that there's no UTXO on the node
         node = self.nodes[0]
@@ -219,6 +236,7 @@ class WalletLabelsTest(BitcoinTestFramework):
 
         if not self.options.descriptors:
             self.test_sort_multisig(node)
+            self.test_sort_multisig_with_uncompressed_hash160(node)
 
         if self.options.descriptors:
             # This is a descriptor wallet test because of segwit v1+ addresses
