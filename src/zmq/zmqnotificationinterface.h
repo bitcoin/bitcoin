84fbb51efa535628df2de8a613a27eb716d3d928
Anfauglith <amon.e@gmx.net>
2017-09-30 10:40:20 +0200

    replaced BITCOIN->IOP in files
    forgot two files apparently

--- src/zmq/zmqnotificationinterface.h
+++ src/zmq/zmqnotificationinterface.h
@@ -2,8 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_ZMQ_ZMQNOTIFICATIONINTERFACE_H
-#define BITCOIN_ZMQ_ZMQNOTIFICATIONINTERFACE_H
+#ifndef IOP_ZMQ_ZMQNOTIFICATIONINTERFACE_H
+#define IOP_ZMQ_ZMQNOTIFICATIONINTERFACE_H
 
 #include "validationinterface.h"
 #include <string>
@@ -37,4 +37,4 @@ private:
     std::list<CZMQAbstractNotifier*> notifiers;
 };
 
-#endif // BITCOIN_ZMQ_ZMQNOTIFICATIONINTERFACE_H
+#endif // IOP_ZMQ_ZMQNOTIFICATIONINTERFACE_H
