c1282c9d57bb9e7a65a67e44ec64a6277f997c3a
Jonas Schnelli <dev@jonasschnelli.ch>
2017-09-30 20:33:58 -0700

    Always set NODE_NETWORK_LIMITED bit
    The current pruning implementation does ensure to always conform to BIP159

--- src/init.cpp
+++ src/init.cpp
@@ -816,7 +816,7 @@ namespace { // Variables internal to initialization process only
 int nMaxConnections;
 int nUserMaxConnections;
 int nFD;
-ServiceFlags nLocalServices = NODE_NETWORK;
+ServiceFlags nLocalServices = ServiceFlags(NODE_NETWORK | NODE_NETWORK_LIMITED);
 
 } // namespace
 
