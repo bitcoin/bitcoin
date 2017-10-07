93f86a50f1fccbc29ea3fd98a0753d92c1bf38e5
linalouise <linalouise@linalouise.com>
2017-10-07 14:36:58 -0400

    compile

--- contrib/vagrant/install.sh
+++ contrib/vagrant/install.sh
@@ -4,3 +4,8 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install -y build-essential libssl-dev libboost-all-dev libdb++-dev
 sudo apt-get install -y miniupnpc libminiupnpc-dev
+
+sudo timedatectl set-ntp on
+
+cd /vagrant/src
+make -f makefile.unix
