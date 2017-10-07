ef8816f65310347ec0f03f45e61eb9933d71958c
linalouise <linalouise@linalouise.com>
2017-10-07 14:19:15 -0400

    vagrantfile

---
+++ contrib/vagrant/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -ev
+
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y build-essential libssl-dev libboost-all-dev libdb++-dev
+sudo apt-get install -y miniupnpc libminiupnpc-dev
