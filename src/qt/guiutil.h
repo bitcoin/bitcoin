cdb560138ce9c163d06d41d1fc547029bc6d5427
sandakersmann <mkjaerstad@yahoo.no>
2017-10-08 02:21:43 +0200

    Rebranding of debug window (show command-line options)

--- src/qt/guiutil.h
+++ src/qt/guiutil.h
@@ -96,7 +96,7 @@ namespace GUIUtil
     bool GetStartOnSystemStartup();
     bool SetStartOnSystemStartup(bool fAutoStart);
 
-    /** Help message for Bitcoin-Qt, shown with --help. */
+    /** Help message for Peercoin-Qt, shown with --help. */
     class HelpMessageBox : public QMessageBox
     {
         Q_OBJECT
