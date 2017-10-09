0f14a75da2a7368f93f0a738c2546078eb387840
Johannes Kanig <kanigsson@users.noreply.github.com>
2017-10-09 19:14:47 +0200

    fix typo in comment of chain.cpp

--- src/chain.cpp
+++ src/chain.cpp
@@ -128,7 +128,7 @@ arith_uint256 GetBlockProof(const CBlockIndex& block)
     // We need to compute 2**256 / (bnTarget+1), but we can't represent 2**256
     // as it's too large for an arith_uint256. However, as 2**256 is at least as large
     // as bnTarget+1, it is equal to ((2**256 - bnTarget - 1) / (bnTarget+1)) + 1,
-    // or ~bnTarget / (nTarget+1) + 1.
+    // or ~bnTarget / (bnTarget+1) + 1.
     return (~bnTarget / (bnTarget + 1)) + 1;
 }
 
