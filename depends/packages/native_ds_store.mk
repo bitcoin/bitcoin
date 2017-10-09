a59212364094fb5856a00d9b4a20e15eb2f6a808
Jonas Schnelli <dev@jonasschnelli.ch>
2017-10-09 12:47:15 -0700

    Merge #11460: [depends] mac_alias 2.0.6, ds_store 1.1.2
    f617d1b06 [depends] native_ds_store 1.1.2 (fanquake)
    46b752ab5 [depends] native_mac_alias 2.0.6 (fanquake)
    
    Pull request description:
    
      mac_alias and ds_store have moved from Bitbucket to GitHub.
      See https://github.com/al45tair/mac_alias and https://github.com/al45tair/ds_store.
    
      mac_alias has been updated to be compatible with Python 3? ~~~so we should be able to drop our patch.~~~ I've dropped some of the patch for now.
    
      Quickly tested on macOS, because depends building is broken with latest the Xcode see #11461.
      Related #8134.
    
    Tree-SHA512: d0017883f86b29bc4ab03bade5aaad9959e4343cd66596805fae48a1804e4bd150c77652f08e3e6cfafb3193f7c0183686f1f60c83f3a4204d40f76041c13ed2

--- depends/packages/native_ds_store.mk
+++ depends/packages/native_ds_store.mk
@@ -1,9 +1,8 @@
 package=native_ds_store
-$(package)_version=1.1.0
-$(package)_download_path=https://bitbucket.org/al45tair/ds_store/get
-$(package)_download_file=v$($(package)_version).tar.bz2
-$(package)_file_name=$(package)-$($(package)_version).tar.bz2
-$(package)_sha256_hash=921596764d71d1bbd3297a90ef6d286f718794d667e4f81d91d14053525d64c1
+$(package)_version=1.1.2
+$(package)_download_path=https://github.com/al45tair/ds_store/archive/
+$(package)_file_name=v$($(package)_version).tar.gz
+$(package)_sha256_hash=3b3ecb7bf0a5157f5b6010bc3af7c141fb0ad3527084e63336220d22744bc20c
 $(package)_install_libdir=$(build_prefix)/lib/python/dist-packages
 $(package)_dependencies=native_biplist
 
