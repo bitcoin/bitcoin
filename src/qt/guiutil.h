426ad150c5162ef3f43cd1962c960d5c6e2a6424
Aaron Golliver <aaron.golliver@gmail.com>
2017-10-14 16:06:21 -0700

    move human-readable byte formatting to guiutil

--- src/qt/guiutil.h
+++ src/qt/guiutil.h
@@ -199,6 +199,8 @@ namespace GUIUtil
 
     QString formatNiceTimeOffset(qint64 secs);
 
+    QString formatBytes(uint64_t bytes);
+
     class ClickableLabel : public QLabel
     {
         Q_OBJECT
