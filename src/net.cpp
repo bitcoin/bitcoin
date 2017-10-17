4cac044d95b5077ffcaf599cc8345bc40347692a
Alexander Block <ablock84@gmail.com>
2017-10-17 18:41:57 +0200

    Merge #8944: Remove bogus assert on number of oubound connections. (#1685)
    1ab21cf Remove bogus assert on number of oubound connections. (Matt Corallo)

--- src/net.cpp
+++ src/net.cpp
@@ -1686,7 +1686,6 @@ void CConnman::ThreadOpenConnections()
                 }
             }
         }
-        assert(nOutbound <= (nMaxOutbound + nMaxFeeler));
 
         // Feeler Connections
         //
