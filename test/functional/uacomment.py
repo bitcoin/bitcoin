21514ceb50f2cbbc69c0398a50834e749bef57c6
Wladimir J. van der Laan <laanwj@gmail.com>
2017-10-18 15:48:27 +0200

    Merge #11486: [tests] Add uacomment tests
    c5dfa90 [tests] Add uacomment tests (Cristian Mircea Messel)
    
    Pull request description:
    
      Checks for setting the value, max length and reserved characters
    
    Tree-SHA512: a62e2cf8e455a3cd3987c0855f7bfc49de47504c01263e3573366e3cbff400c5678224773d4f1e4ac684fff34d987994e490a0978c4da05ff2a4bfa972c84723

---
+++ test/functional/uacomment.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# Copyright (c) 2017 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+"""Test the -uacomment option."""
+
+from test_framework.test_framework import BitcoinTestFramework
+from test_framework.util import assert_equal
+
+class UacommentTest(BitcoinTestFramework):
+    def set_test_params(self):
+        self.num_nodes = 1
+        self.setup_clean_chain = True
+
+    def run_test(self):
+        self.log.info("test multiple -uacomment")
+        test_uacomment = self.nodes[0].getnetworkinfo()["subversion"][-12:-1]
+        assert_equal(test_uacomment, "(testnode0)")
+
+        self.restart_node(0, ["-uacomment=foo"])
+        foo_uacomment = self.nodes[0].getnetworkinfo()["subversion"][-17:-1]
+        assert_equal(foo_uacomment, "(testnode0; foo)")
+
+        self.log.info("test -uacomment max length")
+        self.stop_node(0)
+        expected = "Total length of network version string (286) exceeds maximum length (256). Reduce the number or size of uacomments."
+        self.assert_start_raises_init_error(0, ["-uacomment=" + 'a' * 256], expected)
+
+        self.log.info("test -uacomment unsafe characters")
+        for unsafe_char in ['/', ':', '(', ')']:
+            expected = "User Agent comment (" + unsafe_char + ") contains unsafe characters"
+            self.assert_start_raises_init_error(0, ["-uacomment=" + unsafe_char], expected)
+
+if __name__ == '__main__':
+    UacommentTest().main()
