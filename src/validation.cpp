24739b5a8a895d9b48113b3a2abf091f3bfd37b2
sidhujag <sidhujag@hotmail.com>
2017-10-24 19:57:31 -0700

    update magic

--- src/validation.cpp
+++ src/validation.cpp
@@ -110,7 +110,7 @@ static void CheckBlockIndex(const Consensus::Params& consensusParams);
 /** Constant stuff for coinbase transactions we create: */
 CScript COINBASE_FLAGS;
 
-const string strMessageMagic = "DarkCoin Signed Message:\n";
+const string strMessageMagic = "Syscoin Signed Message:\n";
 
 // Internal stuff
 namespace {
