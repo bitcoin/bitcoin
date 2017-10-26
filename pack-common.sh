ddec5ccb82e03cc77eae20bd40b622c6758c2e85
Anfauglith <amon.e@gmx.net>
2017-10-26 11:46:33 +0200

    bumped to 6.0.1

--- pack-common.sh
+++ pack-common.sh
@@ -7,7 +7,7 @@ sudo checkinstall \
     --pkglicense=MIT \
     --pkggroup=net \
     --pkgname=iop-core \
-    --pkgversion=6.0.0 \
+    --pkgversion=6.0.1 \
     --pkgarch=$(dpkg \
     --print-architecture) \
     $@
