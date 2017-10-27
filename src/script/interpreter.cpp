46535f20edb4288da259c91181859d782add97dc
Peter Tschipper <peter.tschipper@gmail.com>
2017-10-26 19:32:55 -0700

    Remove tests not applicable to BitcoinUnlimited
    Take out tests that use Precomputedtransactiondata.  BU does not
    implement this feature.

--- src/script/interpreter.cpp
+++ src/script/interpreter.cpp
@@ -988,7 +988,8 @@ bool EvalScript(vector<vector<unsigned char> >& stack, const CScript& script, un
                     }
 
                     // Clean up stack of actual arguments
-                    while (i-- > 1) {
+                    while (i-- > 1)
+                    {
                         // If the operation failed, we require that all signatures must be empty vector
                         if (!fSuccess && (flags & SCRIPT_VERIFY_NULLFAIL) && !ikey2 && stacktop(-1).size())
                             return set_error(serror, SCRIPT_ERR_SIG_NULLFAIL);
