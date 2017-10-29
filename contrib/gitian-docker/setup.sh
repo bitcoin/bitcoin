d02f760ff8ce7ef275d1d06cb0e0c2922f41022b
backpacker69 <backpacker69@protonmail.com>
2017-10-29 13:59:03 +0300

    qt4.8.5 for windows, revisions of dependencies

--- contrib/gitian-docker/setup.sh
+++ contrib/gitian-docker/setup.sh
@@ -16,7 +16,7 @@ cp build/out/boost-linux*-1.55.0-gitian-r1.zip inputs/
 ./bin/gbuild ../peercoin/contrib/gitian-descriptors/deps-linux.yml </dev/null
 cp build/out/peercoin-deps-linux*-gitian-r1.zip inputs/
 ./bin/gbuild ../nubit/contrib/gitian-descriptors/boost-win.yml </dev/null
-cp build/out/boost-win*-1.55.0-gitian-r6.zip inputs/
+cp build/out/boost-win*-1.55.0-gitian-r1.zip inputs/
 ./bin/gbuild ../nubit/contrib/gitian-descriptors/qt-win.yml </dev/null
 cp build/out/qt-win*-5.7.1-gitian-r1.zip inputs/
 ./bin/gbuild ../nubit/contrib/gitian-descriptors/deps-win.yml </dev/null
