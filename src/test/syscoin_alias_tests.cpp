35ce2ddf695ee89d37fa791f95048ce3475cc4d7
sidhujag <sidhujag@hotmail.com>
2017-10-31 14:37:30 -0700

    return value

--- src/test/syscoin_alias_tests.cpp
+++ src/test/syscoin_alias_tests.cpp
@@ -33,7 +33,7 @@ BOOST_AUTO_TEST_CASE (generate_big_aliasdata)
 	AliasNew("node1", "jag1", gooddata);
 	BOOST_CHECK_NO_THROW(r = CallRPC("node1", "aliasnew jag2 " + baddata));
 	UniValue varray = r.get_array();
-	BOOST_CHECK_NO_THROW(CallRPC("node1", "signrawtransaction " + varray[0].get_str()));
+	BOOST_CHECK_NO_THROW(r = CallRPC("node1", "signrawtransaction " + varray[0].get_str()));
 	BOOST_CHECK_NO_THROW(CallRPC("node1", "syscoinsendrawtransaction " + find_value(r.get_obj(), "hex").get_str()));
 	BOOST_CHECK_THROW(CallRPC("node1", "aliasnew jag2"), runtime_error);
 	GenerateBlocks(5);	
@@ -49,7 +49,7 @@ BOOST_AUTO_TEST_CASE (generate_aliaswitness)
 	BOOST_CHECK_NO_THROW(r = CallRPC("node1", "aliasinfo witness1"));
 	BOOST_CHECK_EQUAL(find_value(r.get_obj(), "publicvalue").get_str(), "pub");
 	BOOST_CHECK(!hex_str.empty());
-	BOOST_CHECK_NO_THROW(CallRPC("node2", "signrawtransaction " + hex_str));
+	BOOST_CHECK_NO_THROW(r = CallRPC("node2", "signrawtransaction " + hex_str));
 	BOOST_CHECK_NO_THROW(CallRPC("node2", "syscoinsendrawtransaction " + find_value(r.get_obj(), "hex").get_str()));
 	GenerateBlocks(5, "node2");
 	GenerateBlocks(5);
