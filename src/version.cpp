da4fa9827cba62fc12b42a555e61c0670b1597e6
hrobeers <hansrobeers@gmail.com>
2017-11-02 09:48:12 +0100

    Revert "Remove -beta suffix on release-0.6 branch"
    This reverts commit 28c9522802cc5d51d82ca66608789aa452584d03.

--- src/version.cpp
+++ src/version.cpp
@@ -11,7 +11,7 @@
 const std::string CLIENT_NAME("Satoshi");
 
 // Client version number
-#define CLIENT_VERSION_SUFFIX   ""
+#define CLIENT_VERSION_SUFFIX   "-beta"
 
 
 // The following part of the code determines the CLIENT_BUILD variable.
