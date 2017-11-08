963317b78e92a81d4f4cbd7ccd16e4ee56108a31
practicalswift <practicalswift@users.noreply.github.com>
2017-11-08 10:08:18 +0100

    Clarify NO_THREAD_SAFETY_ANALYSIS comment

--- src/rpc/misc.cpp
+++ src/rpc/misc.cpp
@@ -121,7 +121,7 @@ public:
 #endif
 
 // NO_THREAD_SAFETY_ANALYSIS: The conditional cs_wallet lock confuses the
-// thread-safety analyzer.
+// Clang thread-safety analyzer.
 UniValue validateaddress(const JSONRPCRequest& request) NO_THREAD_SAFETY_ANALYSIS
 {
     if (request.fHelp || request.params.size() != 1)
