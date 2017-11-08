7721ad64f40a0c67edefaaf7353264d78df8803e
Matt Corallo <git@bluematt.me>
2017-11-08 14:50:18 -0500

    [refactor] Prep for scripted-diff by removing some \ns which annoy sed.

--- src/consensus/tx_verify.cpp
+++ src/consensus/tx_verify.cpp
@@ -172,8 +172,7 @@ bool Consensus::CheckTxInputs(const CTransaction& tx, CValidationState& state, c
 
         // If prev is coinbase, check that it's matured
         if (coin.IsCoinBase() && nSpendHeight - coin.nHeight < COINBASE_MATURITY) {
-            return state.DoS(0, ValidationInvalidReason::TX_MISSING_INPUTS, false,
-                REJECT_INVALID, "bad-txns-premature-spend-of-coinbase", false,
+            return state.DoS(0, ValidationInvalidReason::TX_MISSING_INPUTS, false, REJECT_INVALID, "bad-txns-premature-spend-of-coinbase", false,
                 strprintf("tried to spend coinbase at depth %d", nSpendHeight - coin.nHeight));
         }
 
