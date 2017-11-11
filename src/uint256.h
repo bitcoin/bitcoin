aeaf3c5ef9f2af515e620ac93f5cd4344be44c5c
MarcoFalke <falke.marco@gmail.com>
2017-11-11 18:07:23 -0500

    Merge #10749: Use compile-time constants instead of unnamed enumerations (remove "enum hack")
    1e65f0f33 Use compile-time constants instead of unnamed enumerations (remove "enum hack") (practicalswift)
    
    Pull request description:
    
      Use compile-time constants instead of unnamed enumerations (remove "enum hack").
    
    Tree-SHA512: 1b6ebb2755398c5ebab6cce125b1dfc39cbd1504d98d55136b32703fe935c4070360ab3b2f52b1da48ba9f3b01082d204f3d87c92ccb5c8c333731f7f972e128

--- src/uint256.h
+++ src/uint256.h
@@ -20,7 +20,7 @@ template<unsigned int BITS>
 class base_blob
 {
 protected:
-    enum { WIDTH=BITS/8 };
+    static constexpr int WIDTH = BITS / 8;
     uint8_t data[WIDTH];
 public:
     base_blob()
