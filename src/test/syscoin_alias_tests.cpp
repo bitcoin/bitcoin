f76947ab2ff092bb076e970085729c5480e154dd
sidhujag <sidhujag@hotmail.com>
2017-11-10 20:59:44 -0800

    wip alias test

--- src/test/syscoin_alias_tests.cpp
+++ src/test/syscoin_alias_tests.cpp
@@ -901,11 +901,11 @@ BOOST_AUTO_TEST_CASE (generate_aliasexpired)
 	GenerateBlocks(5, "node2");
 	GenerateBlocks(5, "node3");
 	GenerateBlocks(50);
-	AliasNew("node1", "aliasexpire2", "pubdata");
 	BOOST_CHECK_THROW(CallRPC("node1", "sendtoaddress aliasexpirednode2 3000"), runtime_error);
 	GenerateBlocks(10);	
 	const string &escrowguid = EscrowNewBuyItNow("node2", "node1", "aliasexpirednode2", offerguid, "1", "aliasexpire3");
 	string aliasexpire2node2address = AliasNew("node2", "aliasexpire2node2", "pubdata");
+	AliasNew("node1", "aliasexpire2", "pubdata");
 	string certgoodguid = CertNew("node1", "aliasexpire2", "certtitle", "pubdata");
 	ExpireAlias("aliasexpirednode2");
 	GenerateBlocks(5, "node2");
