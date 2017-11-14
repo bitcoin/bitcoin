a2af81caff8a72b3632c1bbbda8c389cd4367eb9
Russell Yanofsky <russ@yanofsky.org>
2017-11-14 13:32:41 -0500

    Allow wallet files not in -walletdir directory
    Remove restriction that -wallet filenames can only refer to files in the
    -walletdir directory.

--- src/wallet/db.cpp
+++ src/wallet/db.cpp
@@ -122,6 +122,7 @@ bool CDBEnv::Open(bool retry)
     boost::this_thread::interruption_point();
 
     fs::path pathIn = strPath;
+    TryCreateDirectories(pathIn);
     if (!LockDirectory(pathIn, ".walletlock")) {
         LogPrintf("Cannot obtain a lock on wallet directory %s. Another instance of bitcoin may be using it.\n", strPath);
         return false;
