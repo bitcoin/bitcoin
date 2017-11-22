73333cc15dd2b00d905b6bef4c5e6ca9e97ad754
tperkins <tperkins@mediciventures.com>
2017-11-21 17:49:24 -0700

    fixing pythong comment for copyright

--- test/functional/test_framework/mininode.py
+++ test/functional/test_framework/mininode.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2010 ArtForz -- public domain half-a-node
 # Copyright (c) 2012 Jeff Garzik
 # Copyright (c) 2010-2016 The Bitcoin Core developers
-// Copyright (c) 2017 The Raven Core developers
+# Copyright (c) 2017 The Raven Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 """Raven P2P network half-a-node.
