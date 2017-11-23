4ea5d6a7612af7360d86a1bbaf69fd17a48eebe4
Lenny Maiorani <ldm5180@gmail.com>
2017-11-23 19:52:09 +0000

    [crypto] Refactor HMAC_SHA to eliminate code duplication
    Problem:
    - HMAC_SHA256 and HMAC_SHA512 almost the same. Differences are:
      - `OUTPUT_SIZE` of the hash
      - magic number constants which can be derived from `OUTPUT_SIZE`
    - Code duplication is error prone and causes maintenance issues.
    
    Solution:
    - Eliminate magic numbers by making `OUTPUT_SIZE` a compile-time
      constant and deriving the other constants from it.
    - Compile-time constants permit container type creation, type-based
      bounds checking, and range-based iteration instead of run-time
      branching.
    - Elimination of run-time eval and magic numbers allows templatization
      to reduce code duplication.
    - Old type names and header files are preserved via `using`
      declarations to limit the scope of the change.

--- src/crypto/hmac_sha256.h
+++ src/crypto/hmac_sha256.h
@@ -5,28 +5,8 @@
 #ifndef BITCOIN_CRYPTO_HMAC_SHA256_H
 #define BITCOIN_CRYPTO_HMAC_SHA256_H
 
-#include <crypto/sha256.h>
+#include <crypto/hmac_sha.h>
 
-#include <stdint.h>
-#include <stdlib.h>
-
-/** A hasher class for HMAC-SHA-256. */
-class CHMAC_SHA256
-{
-private:
-    CSHA256 outer;
-    CSHA256 inner;
-
-public:
-    static const size_t OUTPUT_SIZE = 32;
-
-    CHMAC_SHA256(const unsigned char* key, size_t keylen);
-    CHMAC_SHA256& Write(const unsigned char* data, size_t len)
-    {
-        inner.Write(data, len);
-        return *this;
-    }
-    void Finalize(unsigned char hash[OUTPUT_SIZE]);
-};
+using CHMAC_SHA256 = CHMAC_SHA<CSHA256>;
 
 #endif // BITCOIN_CRYPTO_HMAC_SHA256_H
