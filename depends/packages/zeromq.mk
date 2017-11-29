45a8f7087e259ef6a9bfa791268503cf9a283cc5
Cory Fields <cory-nospam-@coryfields.com>
2017-11-28 20:42:39 -0500

    depends: fix zmq build with mingw < 4.0

--- depends/packages/zeromq.mk
+++ depends/packages/zeromq.mk
@@ -3,6 +3,7 @@ $(package)_version=4.2.2
 $(package)_download_path=https://github.com/zeromq/libzmq/releases/download/v$($(package)_version)/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=5b23f4ca9ef545d5bd3af55d305765e3ee06b986263b31967435d285a3e6df6b
+$(package)_patches=0001-fix-build-with-older-mingw64.patch
 
 define $(package)_set_vars
   $(package)_config_opts=--without-docs --disable-shared --without-libsodium --disable-curve --disable-curve-keygen --disable-perf
@@ -11,6 +12,7 @@ define $(package)_set_vars
 endef
 
 define $(package)_preprocess_cmds
+   patch -p1 < $($(package)_patch_dir)/0001-fix-build-with-older-mingw64.patch && \
   ./autogen.sh
 endef
 
