cfce6fdf789a3e16ea2b87f07a33fc4f903e155b
sidhujag <sidhujag@hotmail.com>
2017-11-30 17:13:30 -0800

    apply patch #1738

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -520,8 +520,18 @@ UniValue getblocktemplate(const UniValue& params, bool fHelp)
     if (IsInitialBlockDownload())
         throw JSONRPCError(RPC_CLIENT_IN_INITIAL_DOWNLOAD, "Syscoin Core is downloading blocks...");
 
-    if (!masternodeSync.IsSynced())
-        throw JSONRPCError(RPC_CLIENT_IN_INITIAL_DOWNLOAD, "Syscoin Core is syncing with network...");
+	// when enforcement is on we need information about a masternode payee or otherwise our block is going to be orphaned by the network 
+	CScript payee;
+	if (sporkManager.IsSporkActive(SPORK_8_MASTERNODE_PAYMENT_ENFORCEMENT)
+		 && !masternodeSync.IsWinnersListSynced()
+		 && !mnpayments.GetBlockPayee(chainActive.Height() + 1, payee))
+		 throw JSONRPCError(RPC_CLIENT_IN_INITIAL_DOWNLOAD, "Syscoin Core is downloading masternode winners...");
+	
+	// next bock is a superblock and we need governance info to correctly construct it
+	if (sporkManager.IsSporkActive(SPORK_9_SUPERBLOCKS_ENABLED)
+			&& !masternodeSync.IsSynced()
+			&& CSuperblock::IsValidBlockHeight(chainActive.Height() + 1))
+		throw JSONRPCError(RPC_CLIENT_IN_INITIAL_DOWNLOAD, "Syscoin Core is syncing with network...");
 
     static unsigned int nTransactionsUpdatedLast;
 
