d384755f39944f0acc6e839782ff9612b96448b7
Wladimir J. van der Laan <laanwj@gmail.com>
2017-12-01 12:08:31 +0100

    gui: Fix proxy setting options dialog crash
    This fixes a crash bug when opening the options dialog.
    
    - Check the return value of split() to avoid segmentation faults due to
      out of bounds when the user manages to enter invalid proxy settings.
      This is reported resonably often.
    
    - Move the default proxy/port to a constant instead of hardcoding magic
      values.
    
    - Factor out some common code.
    
    - Revert #11448 because this proves a more robust replacement, it is no
      longer necessary and didn't generally solve the issue.
    
    No attempt is made to do full sanity checking on the proxy, so it can
    still be rejected by the core with an InitError message.

--- src/qt/optionsmodel.h
+++ src/qt/optionsmodel.h
@@ -13,6 +13,9 @@ QT_BEGIN_NAMESPACE
 class QNetworkProxy;
 QT_END_NAMESPACE
 
+extern const char *DEFAULT_GUI_PROXY_HOST;
+static constexpr unsigned short DEFAULT_GUI_PROXY_PORT = 9050;
+
 /** Interface from Qt to configuration data structure for Bitcoin client.
    To Qt, the options are presented as a list with the different options
    laid out vertically.
