383aa999baf156000673676fd045b8e63c70840e
tperkins <tperkins@mediciventures.com>
2017-12-01 14:03:05 -0700

    fixing functional test

--- test/functional/fundrawtransaction.py
+++ test/functional/fundrawtransaction.py
@@ -55,7 +55,7 @@ class RawTransactionsTest(RavenTestFramework):
 
         watchonly_address = self.nodes[0].getnewaddress()
         watchonly_pubkey = self.nodes[0].validateaddress(watchonly_address)["pubkey"]
-        watchonly_amount = Decimal(200)
+        watchonly_amount = Decimal(20000)
         self.nodes[3].importpubkey(watchonly_pubkey, "", True)
         watchonly_txid = self.nodes[0].sendtoaddress(watchonly_address, watchonly_amount)
         self.nodes[0].sendtoaddress(self.nodes[3].getnewaddress(), watchonly_amount / 10)
