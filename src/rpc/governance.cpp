442325b072fdba269a1d75693bd2e91d4e7679bd
Ilya Savinov <ilya@dash.org>
2017-12-06 01:18:08 +0300

    add `maxgovobjdatasize` field to the output of `getgovernanceinfo` (#1757)

--- src/rpc/governance.cpp
+++ src/rpc/governance.cpp
@@ -919,6 +919,7 @@ UniValue getgovernanceinfo(const UniValue& params, bool fHelp)
             "  \"superblockcycle\": xxxxx,               (numeric) the number of blocks between superblocks\n"
             "  \"lastsuperblock\": xxxxx,                (numeric) the block number of the last superblock\n"
             "  \"nextsuperblock\": xxxxx,                (numeric) the block number of the next superblock\n"
+            "  \"maxgovobjdatasize\": xxxxx,             (numeric) maximum governance object data size in bytes\n"
             "}\n"
             "\nExamples:\n"
             + HelpExampleCli("getgovernanceinfo", "")
@@ -959,6 +960,7 @@ UniValue getgovernanceinfo(const UniValue& params, bool fHelp)
     obj.push_back(Pair("superblockcycle", Params().GetConsensus().nSuperblockCycle));
     obj.push_back(Pair("lastsuperblock", nLastSuperblock));
     obj.push_back(Pair("nextsuperblock", nNextSuperblock));
+    obj.push_back(Pair("maxgovobjdatasize", MAX_GOVERNANCE_OBJECT_DATA_SIZE));
 
     return obj;
 }
