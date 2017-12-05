a51227a622451099047827c3066fe088b605ce78
Russell Yanofsky <russ@yanofsky.org>
2017-12-05 15:57:12 -0500

    Add capnp wrapper for Handler interface

---
+++ src/ipc/capnp/handler-types.h
@@ -0,0 +1,10 @@
+// Copyright (c) The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or https://opensource.org/license/mit.
+
+#ifndef BITCOIN_IPC_CAPNP_HANDLER_TYPES_H
+#define BITCOIN_IPC_CAPNP_HANDLER_TYPES_H
+
+#include <mp/type-context.h>
+
+#endif // BITCOIN_IPC_CAPNP_HANDLER_TYPES_H
