d24722c4c7cebe207bf7dadfb60e274001fb0289
Russell Yanofsky <russ@yanofsky.org>
2017-12-05 15:57:12 -0500

    multiprocess: Make bitcoin-gui spawn a bitcoin-node process
    Spawn node subprocess instead of running node code internally

--- src/ipc/capnp/init-types.h
+++ src/ipc/capnp/init-types.h
@@ -8,5 +8,6 @@
 #include <ipc/capnp/chain.capnp.proxy-types.h>
 #include <ipc/capnp/echo.capnp.proxy-types.h>
 #include <ipc/capnp/mining.capnp.proxy-types.h>
+#include <ipc/capnp/node.capnp.proxy-types.h>
 
 #endif // BITCOIN_IPC_CAPNP_INIT_TYPES_H
