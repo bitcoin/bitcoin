c8dad0fa730516515fa6c2aa03bd02e4a2dcb77a
Jim Posen <jimpo@coinbase.com>
2017-12-08 11:29:59 -0800

    [init] Initialize and start TxIndex in init code.

--- src/index/txindex.h
+++ src/index/txindex.h
@@ -79,4 +79,7 @@ public:
     void Stop();
 };
 
+/// The global transaction index, used in GetTransaction. May be null.
+extern std::unique_ptr<TxIndex> g_txindex;
+
 #endif // BITCOIN_INDEX_TXINDEX_H
