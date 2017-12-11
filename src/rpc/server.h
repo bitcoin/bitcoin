166abbeacd0422fcea5467521771c863baf21cc6
MarcoFalke <falke.marco@gmail.com>
2017-12-11 15:24:02 -0500

    [rpc] createrawtransaction: Accept sorted outputs

--- src/rpc/server.h
+++ src/rpc/server.h
@@ -28,7 +28,7 @@ namespace RPCServer
 }
 
 /** Wrapper for UniValue::VType, which includes typeAny:
- * Used to denote don't care type. Only used by RPCTypeCheckObj */
+ * Used to denote don't care type. */
 struct UniValueType {
     UniValueType(UniValue::VType _type) : typeAny(false), type(_type) {}
     UniValueType() : typeAny(true) {}
