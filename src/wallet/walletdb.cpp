9d3c7c3ca648ec740989ad97f1b4116794e06440
Wladimir J. van der Laan <laanwj@gmail.com>
2017-12-12 19:48:24 +0100

    Merge #11854: Split up key and script metadata for better type safety
    9c8eca7 Split up key and script metadata for better type safety (Russell Yanofsky)
    
    Pull request description:
    
      Suggested by @TheBlueMatt
      https://github.com/bitcoin/bitcoin/pull/11403#discussion_r155599383
    
      Combining the maps was probably never a good arrangement but is more
      problematic now in presence of WitnessV0ScriptHash and WitnessV0KeyHash types.
    
    Tree-SHA512: 9263e9c01090fb49221e91d88a88241a9691dda3e92d86041c8e284306a64d3af5e2438249f9dcc3e6e4a5c11c1a89f975a86d55690adf95bf2636f15f99f92a

--- src/wallet/walletdb.cpp
+++ src/wallet/walletdb.cpp
@@ -425,27 +425,23 @@ ReadKeyValue(CWallet* pwallet, CDataStream& ssKey, CDataStream& ssValue,
             }
             wss.fIsEncrypted = true;
         }
-        else if (strType == "keymeta" || strType == "watchmeta")
+        else if (strType == "keymeta")
         {
-            CTxDestination keyID;
-            if (strType == "keymeta")
-            {
-              CPubKey vchPubKey;
-              ssKey >> vchPubKey;
-              keyID = vchPubKey.GetID();
-            }
-            else if (strType == "watchmeta")
-            {
-              CScript script;
-              ssKey >> script;
-              keyID = CScriptID(script);
-            }
-
+            CPubKey vchPubKey;
+            ssKey >> vchPubKey;
             CKeyMetadata keyMeta;
             ssValue >> keyMeta;
             wss.nKeyMeta++;
-
-            pwallet->LoadKeyMetadata(keyID, keyMeta);
+            pwallet->LoadKeyMetadata(vchPubKey.GetID(), keyMeta);
+        }
+        else if (strType == "watchmeta")
+        {
+            CScript script;
+            ssKey >> script;
+            CKeyMetadata keyMeta;
+            ssValue >> keyMeta;
+            wss.nKeyMeta++;
+            pwallet->LoadScriptMetadata(CScriptID(script), keyMeta);
         }
         else if (strType == "defaultkey")
         {
