60475f127218596c7fa26551e84e7f8d10626f0d
Wladimir J. van der Laan <laanwj@gmail.com>
2017-12-12 10:27:45 +0100

    Merge #11831: Always return true if AppInitMain got to the end
    07c4838 Always return true if AppInitMain got to the end (Matt Corallo)
    
    Pull request description:
    
      This should fix a rare zapwallettxes failure on travis, but also
      avoids having init operations (re-adding wallet transactions to
      mempool) running after RPC is free'd.
    
      I believe this was the failure at https://travis-ci.org/bitcoin/bitcoin/jobs/311747844 (from #11605).
    
    Tree-SHA512: f0fea8c1b9265e2eeda57043d541380a3e58e4d9388fa24628a52fd56324257fcd7df0ca02e8f77f66fadd68d951893bab0f610ed9fd0a89b2ccd6bad1efa351

--- src/init.cpp
+++ src/init.cpp
@@ -2158,5 +2158,5 @@ bool AppInitMain(boost::thread_group& threadGroup, CScheduler& scheduler)
         pwalletMain->postInitProcess(scheduler);
 #endif
 
-    return !fRequestShutdown;
+    return true;
 }
