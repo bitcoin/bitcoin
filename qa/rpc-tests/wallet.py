4d4b8310197e961e7c02550b89f00d742cebe095
Willy Ko <k.o.willy@gmail.com>
2017-12-13 14:48:41 -0800

    Fixed unexcepted copyright holders and changed python scripts copyright

--- qa/rpc-tests/wallet.py
+++ qa/rpc-tests/wallet.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 # coding=utf-8
 # ^^^^^^^^^^^^ TODO remove when supporting only Python3
-# Copyright (c) 2014-2015 The Syscoin Core developers
+# Copyright (c) 2014-2015 The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
--- src/primitives/pureheader.h
+++ src/primitives/pureheader.h
@@ -1,5 +1,5 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2013 The SYSCOIN developers
+// Copyright (c) 2009-2013 The Bitcoin Core developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
