c60ba06bd0b8eeb901c3b032a0f2546e689b2822
Wladimir J. van der Laan <laanwj@gmail.com>
2017-12-15 13:52:27 +0100

    Merge #11886: Clarify getbalance meaning a tiny bit in response to questions.
    c79d73d Clarify getbalance meaning a tiny bit in response to questions. (Matt Corallo)
    
    Pull request description:
    
      Someone was asking why getbalance "*" was more "correct" than getbalance, which should rarely be true...spendzeroconfchange was the issue.
    
    Tree-SHA512: 90201cad1acec5161aee469fb4c6d737a0eb90f8380ac93abf0e41e0f02d120afcc3e2e873e5096d3655bb63bbd16fe99e72452f308d72e69139c7f6bb2d745e

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -807,6 +807,8 @@ UniValue getbalance(const JSONRPCRequest& request)
         throw std::runtime_error(
             "getbalance ( \"account\" minconf addlocked include_watchonly )\n"
             "\nIf account is not specified, returns the server's total available balance.\n"
+            "The available balance is what the wallet considers currently spendable, and is\n"
+            "thus affected by options which limit spendability such as -spendzeroconfchange.\n"
             "If account is specified (DEPRECATED), returns the balance in the account.\n"
             "Note that the account \"\" is not the same as leaving the parameter out.\n"
             "The server total may be different to the balance in the default \"\" account.\n"
