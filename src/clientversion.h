6bde97fc96dbc08d19b64b45f1c33f012c1bb2ee
UdjinM6 <UdjinM6@users.noreply.github.com>
2017-12-19 14:35:59 +0300

    Bump to 0.12.3.0 (#1789)
    * Bump to 0.12.3.0 (again)
    
    * Mark it as a pre-release

--- src/clientversion.h
+++ src/clientversion.h
@@ -16,11 +16,11 @@
 //! These need to be macros, as clientversion.cpp's and dash*-res.rc's voodoo requires it
 #define CLIENT_VERSION_MAJOR 0
 #define CLIENT_VERSION_MINOR 12
-#define CLIENT_VERSION_REVISION 2
-#define CLIENT_VERSION_BUILD 2
+#define CLIENT_VERSION_REVISION 3
+#define CLIENT_VERSION_BUILD 0
 
 //! Set to true for release, false for prerelease or test build
-#define CLIENT_VERSION_IS_RELEASE true
+#define CLIENT_VERSION_IS_RELEASE false
 
 /**
  * Copyright year (2009-this)
