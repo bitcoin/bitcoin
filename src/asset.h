05e8d34f3c4b2ce91e2f99525e27ba3c59223995
sidhujag <sidhujag@hotmail.com>
2017-12-20 11:07:33 -0800

    compile

--- src/asset.h
+++ src/asset.h
@@ -8,6 +8,7 @@
 #include "rpc/server.h"
 #include "dbwrapper.h"
 #include "feedback.h"
+#include "transaction.h"
 class CWalletTx;
 class CTransaction;
 class CReserveKey;
@@ -15,7 +16,7 @@ class CCoinsViewCache;
 class CCoins;
 class CBlock;
 class CAliasIndex;
-class COutPoint;
+
 bool CheckAssetInputs(const CTransaction &tx, int op, int nOut, const std::vector<std::vector<unsigned char> > &vvchArgs, bool fJustCheck, int nHeight, std::string &errorMessage, bool dontaddtodb=false);
 bool DecodeAssetTx(const CTransaction& tx, int& op, int& nOut, std::vector<std::vector<unsigned char> >& vvch);
 bool DecodeAndParseAssetTx(const CTransaction& tx, int& op, int& nOut, std::vector<std::vector<unsigned char> >& vvch, char& type);
