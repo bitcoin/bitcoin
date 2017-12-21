a1111cdf65af0b827fcfea70397275ea582aee1c
Alexander Block <ablock84@gmail.com>
2017-12-21 20:37:17 +0100

    Dashify addresses/keys in signmessages.py RPC test

--- qa/rpc-tests/signmessages.py
+++ qa/rpc-tests/signmessages.py
@@ -23,8 +23,8 @@ class SignMessagesTest(BitcoinTestFramework):
         message = 'This is just a test message'
 
         # Test the signing with a privkey
-        privKey = 'cUeKHd5orzT3mz8P9pxyREHfsWtVfgsfDjiZZBcjUBAaGk1BTj7N'
-        address = 'mpLQjfK79b7CCV4VMJWEWAj5Mpx8Up5zxB'
+        privKey = "cU4zhap7nPJAWeMFu4j6jLrfPmqakDAzy8zn8Fhb3oEevdm4e5Lc"
+        address = "yeMpGzMj3rhtnz48XsfpB8itPHhHtgxLc3"
         signature = self.nodes[0].signmessagewithprivkey(privKey, message)
 
         # Verify the message
