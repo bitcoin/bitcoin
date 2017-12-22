49f9d613b0779b29206243a17eb09a4e09fbd9d9
Alexander Block <ablock84@gmail.com>
2017-12-22 19:28:05 +0100

    Revert to BLOCK_DOWNLOAD_TIMEOUT_BASE = 1000000
    This value is in "millionths of the block interval" and in Bitcoin it's set
    to exactly 1000000, meaning one full block interval. It still has to be
    1000000 for Dash, even though the block intervall is less.
    
    This fixes test failures in p2p-fullblocktest.py when large reorgs are
    tested. These take quite some time and did timeout.

--- src/validation.h
+++ src/validation.h
@@ -118,7 +118,7 @@ static const unsigned int AVG_FEEFILTER_BROADCAST_INTERVAL = 10 * 60;
 /** Maximum feefilter broadcast delay after significant change. */
 static const unsigned int MAX_FEEFILTER_CHANGE_DELAY = 5 * 60;
 /** Block download timeout base, expressed in millionths of the block interval (i.e. 2.5 min) */
-static const int64_t BLOCK_DOWNLOAD_TIMEOUT_BASE = 250000;
+static const int64_t BLOCK_DOWNLOAD_TIMEOUT_BASE = 1000000;
 /** Additional block download timeout per parallel downloading peer (i.e. 1.25 min) */
 static const int64_t BLOCK_DOWNLOAD_TIMEOUT_PER_PEER = 125000;
 
