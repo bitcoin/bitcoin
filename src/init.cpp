faa7ecf8ba458ef0e4f1f6786c87ae766fcf02da
MarcoFalke <falke.marco@gmail.com>
2017-12-23 13:54:24 -0500

    Prepare version scheme for 17.0 release

--- src/init.cpp
+++ src/init.cpp
@@ -808,7 +808,7 @@ void InitLogging()
     fLogIPs = gArgs.GetBoolArg("-logips", DEFAULT_LOGIPS);
 
     LogPrintf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-    LogPrintf("Bitcoin version %s\n", FormatFullVersion());
+    LogPrintf("Bitcoin Core version %s\n", FormatFullVersion());
 }
 
 namespace { // Variables internal to initialization process only
