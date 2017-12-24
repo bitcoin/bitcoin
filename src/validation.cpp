96cd0c014fc9626afdc9ec9b73f71bda8da5bb74
Matt Corallo <git@bluematt.me>
2017-12-24 12:13:13 -0500

    Add helper to wait for validation interface queue to catch up

--- src/validation.cpp
+++ src/validation.cpp
@@ -2717,11 +2717,7 @@ bool CChainState::ActivateBestChain(CValidationState &state, const CChainParams&
             // Block until the validation queue drains. This should largely
             // never happen in normal operation, however may happen during
             // reindex, causing memory blowup  if we run too far ahead.
-            std::promise<void> promise;
-            CallFunctionInValidationInterfaceQueue([&promise] {
-                promise.set_value();
-            });
-            promise.get_future().wait();
+            SyncWithValidationInterfaceQueue();
         }
 
         const CBlockIndex *pindexFork;
