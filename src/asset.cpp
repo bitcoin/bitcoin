cff91e31484d1e1d6053554edefa58f8d29124ab
sidhujag <sidhujag@hotmail.com>
2017-12-26 15:37:02 -0800

    compile

--- src/asset.cpp
+++ src/asset.cpp
@@ -528,7 +528,7 @@ bool CheckAssetInputs(const CTransaction &tx, int op, int nOut, const vector<vec
 				}
 				if (dbAsset.txHash != tx.GetHash())
 				{
-					vector<string> lastReceiverList = dbAsset.listReceivers;
+					//vector<string> lastReceiverList = dbAsset.listReceivers;
 					// recreate this asset tx from last known good position (last asset stored)
 					if (op != OP_ASSET_ACTIVATE && !passetdb->ReadLastAsset(vvchArgs[0], dbAsset)) {
 						errorMessage = "SYSCOIN_ASSET_CONSENSUS_ERROR: ERRCODE: 1048 - " + _("Failed to read last escrow from escrow DB");
