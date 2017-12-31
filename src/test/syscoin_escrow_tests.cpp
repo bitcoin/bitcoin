63aa2078b7e357ea551b92e3299bcd390742384b
sidhujag <sidhujag@hotmail.com>
2017-12-30 16:20:15 -0800

    fix test

--- src/test/syscoin_escrow_tests.cpp
+++ src/test/syscoin_escrow_tests.cpp
@@ -152,7 +152,7 @@ BOOST_AUTO_TEST_CASE(generate_auction_reserve)
 	bid_in_offer_currency = "0.04";
 	total_in_payment_option = strprintf("%.*f", 2, pegRates["USD"] * 0.05);
 	bid_in_payment_option = strprintf("%.*f", 2, pegRates["USD"] * 0.04);
-	query = "escrownew true buyerauction1 arbiterauction1 " + offerguid + " " + qty + " " + buyNowStr + " " + total_in_payment_option + " " + shippingFee + " " + networkFee + " " + arbiterFee + " " + witnessFee + " " + exttxid + " " + paymentoptions + " " + bid_in_payment_option + " " + bid_in_offer_currency + " " + witness + " false";
+	query = "escrownew true buyerauction1 arbiterauction1 " + offerguid + " " + qty + " " + buyNowStr + " " + total_in_payment_option + " " + shippingFee + " " + networkFee + " " + arbiterFee + " " + witnessFee + " " + exttxid + " " + paymentoptions + " " + bid_in_payment_option + " " + bid_in_offer_currency + " " + witness;
 	BOOST_CHECK_NO_THROW(r = CallRPC("node1", query));
 	float totalFees = boost::lexical_cast<float>(find_value(r.get_obj(), "fees").write());
 	string escrowaddress = find_value(r.get_obj(), "address").get_str();
