c8330d4216e9f8e31c4e910a6bdf42f09458b2cd
MarcoFalke <falke.marco@gmail.com>
2018-01-02 08:57:27 -0500

    qa: Use node.datadir instead of tmpdir in test framework

--- test/functional/wallet_multiwallet.py
+++ test/functional/wallet_multiwallet.py
@@ -10,7 +10,11 @@ import os
 import shutil
 
 from test_framework.test_framework import BitcoinTestFramework
-from test_framework.util import assert_equal, assert_raises_rpc_error
+from test_framework.util import (
+    assert_equal,
+    assert_raises_rpc_error,
+)
+
 
 class MultiWalletTest(BitcoinTestFramework):
     def set_test_params(self):
