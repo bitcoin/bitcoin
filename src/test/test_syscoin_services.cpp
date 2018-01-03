a34d12a37239e1d15190eba947851c88f9cd72a0
sidhujag <sidhujag@hotmail.com>
2018-01-03 11:28:27 -0800

    fix relay time calc

--- src/test/test_syscoin_services.cpp
+++ src/test/test_syscoin_services.cpp
@@ -798,7 +798,7 @@ string AliasUpdate(const string& node, const string& aliasname, const string& pu
 				break;
 			}
 		}
-		printf("relay to node1 to took about %d MS\n", 10 * numtries);
+		printf("relay to node1 to took about %d MS\n", 10 * (100 * 5 - numtries));
 		BOOST_CHECK(ret.read(txHistoryResult[0].get_str()));
 		historyResultObj = ret.get_obj();
 		BOOST_CHECK_EQUAL(find_value(historyResultObj, "lock_status").get_int(), LOCK_NOCONFLICT_UNCONFIRMED_STATE);
@@ -826,7 +826,7 @@ string AliasUpdate(const string& node, const string& aliasname, const string& pu
 				break;
 			}
 		}
-		printf("relay to node2 to took about %d MS\n", 10 * numtries);
+		printf("relay to node2 to took about %d MS\n", 10 * (100 * 5 - numtries));
 		BOOST_CHECK(ret.read(txHistoryResult[0].get_str()));
 		historyResultObj = ret.get_obj();
 		BOOST_CHECK_EQUAL(find_value(historyResultObj, "lock_status").get_int(), LOCK_NOCONFLICT_UNCONFIRMED_STATE);
