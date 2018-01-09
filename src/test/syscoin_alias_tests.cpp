eed243a147bd7919825056b212f87b8237ca231c
sidhujag <sidhujag@hotmail.com>
2018-01-09 13:26:36 -0800

    colmpile

--- src/test/syscoin_alias_tests.cpp
+++ src/test/syscoin_alias_tests.cpp
@@ -24,33 +24,18 @@ BOOST_GLOBAL_FIXTURE( SyscoinTestingSetup );
 BOOST_FIXTURE_TEST_SUITE (syscoin_alias_tests, BasicSyscoinTestingSetup)
 const unsigned int MAX_ALIAS_UPDATES_PER_BLOCK = 5;
 BOOST_AUTO_TEST_CASE(generate_graph_topological_sort) {
-	typedef boost::adjacency_list<  boost::listS, boost::listS, boost::directedS > Graph;
+	typedef boost::adjacency_list< boost::vecS, boost::vecS, boost::directedS, boost::color_property<> > Graph;
 	typedef boost::graph_traits<Graph>::vertex_descriptor Vertex;
-	Vertex u, v;
-	Graph G(6);
-	u = vertex(0, G);
-	v = vertex(1, G);
-	boost::add_edge(u, v, G);
-	u = vertex(2, G);
-	v = vertex(4, G);
-	boost::add_edge(u, v, G);
-	u = vertex(2, G);
-	v = vertex(5, G);
-	boost::add_edge(u, v, G);
-	u = vertex(0, G);
-	v = vertex(3, G);
-	boost::add_edge(u, v, G);
-	u = vertex(1, G);
-	v = vertex(4, G);
-	boost::add_edge(u, v, G);
-	u = vertex(4, G);
-	v = vertex(3, G);
-	boost::add_edge(u, v, G);
-
+	Pair edges[6] = { Pair(0,1), Pair(2,4),
+		Pair(2,5),
+		Pair(0,3), Pair(1,4),
+		Pair(4,3) };
+	Graph G(6, edges, edges + 6);
 
 	typedef std::vector< Vertex > container;
 	container c;
 	boost::topological_sort(G, std::back_inserter(c));
+
 
 	LogPrintf("A topological ordering: ");
 	for (container::reverse_iterator ii = c.rbegin(); ii != c.rend(); ++ii){
