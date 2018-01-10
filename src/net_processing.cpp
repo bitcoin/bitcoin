81fb931fb8a10b2f9811bdd7cc353f06267d89e6
Alexander Block <ablock84@gmail.com>
2018-01-10 13:20:08 +0100

    Don't delay GETHEADERS when no blocks have arrived yet in devnet (#1807)
    When the first mined block arrives from another node on devnet, we have to
    send GETHEADERS immediately as we otherwise will never leave the IBD mode.

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -1407,7 +1407,13 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
             if (inv.type == MSG_BLOCK) {
                 UpdateBlockAvailability(pfrom->GetId(), inv.hash);
                 if (!fAlreadyHave && !fImporting && !fReindex && !mapBlocksInFlight.count(inv.hash)) {
-                    if (chainparams.DelayGetHeadersTime() != 0 && pindexBestHeader->GetBlockTime() < GetAdjustedTime() - chainparams.DelayGetHeadersTime()) {
+                    // Always send GETHEADERS when we are still on the devnet genesis block. Otherwise we'll never sync.
+                    // This is because after startup of the node, we are in IBD mode, which will only be left when recent
+                    // blocks arrive. At the same time, we won't get any blocks from peers because we keep delaying
+                    // GETHEADERS
+                    bool fDevNetGenesis = chainparams.NetworkIDString() == CBaseChainParams::DEVNET && pindexBestHeader->GetBlockHash() == chainparams.DevNetGenesisBlock().GetHash();
+
+                    if (!fDevNetGenesis && chainparams.DelayGetHeadersTime() != 0 && pindexBestHeader->GetBlockTime() < GetAdjustedTime() - chainparams.DelayGetHeadersTime()) {
                         // We are pretty far from being completely synced at the moment. If we would initiate a new
                         // chain of GETHEADERS/HEADERS now, we may end up downnloading the full chain from multiple
                         // peers at the same time, slowing down the initial sync. At the same time, we don't know
