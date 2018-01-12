59f9e2aaf3fe73961cc02042f929b430fee59f9c
Jonas Schnelli <dev@jonasschnelli.ch>
2018-01-12 11:09:59 -1000

    Use flexible font size for QRCode image address

--- src/qt/guiutil.h
+++ src/qt/guiutil.h
@@ -201,6 +201,8 @@ namespace GUIUtil
 
     QString formatBytes(uint64_t bytes);
 
+    qreal calculateIdealFontSize(int width, const QString& text, QFont font, qreal minPointSize = 4, qreal startPointSize = 14);
+
     class ClickableLabel : public QLabel
     {
         Q_OBJECT
