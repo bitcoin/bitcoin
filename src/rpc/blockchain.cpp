e069b7a8ff14a28accdaa4600b3b851e82a60691
Jorge Timón <jtimon@jtimon.cc>
2018-01-12 21:38:48 +0100

    Bugfix: RPC: savemempool: Don't save until LoadMempool() is finished

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -1607,11 +1607,15 @@ UniValue savemempool(const JSONRPCRequest& request)
     if (request.fHelp || request.params.size() != 0) {
         throw std::runtime_error(
             "savemempool\n"
-            "\nDumps the mempool to disk.\n"
+            "\nDumps the mempool to disk. It will fail until the previous dump is fully loaded.\n"
             "\nExamples:\n"
             + HelpExampleCli("savemempool", "")
             + HelpExampleRpc("savemempool", "")
         );
+    }
+
+    if (!g_is_mempool_loaded) {
+        throw JSONRPCError(RPC_MISC_ERROR, "The mempool was not loaded yet");
     }
 
     if (!DumpMempool()) {
