611c10412edd2bf0bfb86d47346ae820c2c5ae5c
João Barbosa <joao.paulo.barbosa@gmail.com>
2018-01-15 16:10:13 +0000

    [wallet] Add change type to CCoinControl

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -965,7 +965,7 @@ public:
     CAmount GetLegacyBalance(const isminefilter& filter, int minDepth, const std::string* account) const;
     CAmount GetAvailableBalance(const CCoinControl* coinControl = nullptr) const;
 
-    OutputType TransactionChangeType(const std::vector<CRecipient>& vecSend);
+    OutputType TransactionChangeType(OutputType change_type, const std::vector<CRecipient>& vecSend);
 
     /**
      * Insert additional inputs into the transaction by
