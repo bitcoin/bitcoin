bcd491885a140a1ab243430be0845f39f647299f
sidhujag <sidhujag@hotmail.com>
2018-01-14 21:08:13 -0800

    choose first sender

--- src/graph.cpp
+++ src/graph.cpp
@@ -9,9 +9,10 @@ typedef adjacency_list< vecS, vecS, directedS > Graph;
 typedef graph_traits<Graph> Traits;
 typedef typename Traits::vertex_descriptor vertex_descriptor;
 typedef typename std::vector<int> container;
-typedef std::map<int, vector<int> > IndexMap;
-bool CreateDAGFromBlock(const CBlock*pblock, Graph &graph, std::vector<vertex_descriptor> &vertices, IndexMap &mapTxIndex) {
-	std::map<string, int> mapAliasIndex;
+typedef std::map<int, int> IndexMap;
+typedef std::map<string, int> AliasMap;
+bool CreateDAGFromBlock(const CBlock*pblock, Graph &graph, std::vector<vertex_descriptor> &vertices, IndexMap &mapTxIndex, sorted_vector<int> &vecTxIndexToRemove) {
+	AliasMap mapAliasIndex;
 	std::vector<vector<unsigned char> > vvchArgs;
 	std::vector<vector<unsigned char> > vvchAliasArgs;
 	int op;
@@ -26,18 +27,30 @@ bool CreateDAGFromBlock(const CBlock*pblock, Graph &graph, std::vector<vertex_de
 			if (DecodeAssetAllocationTx(tx, op, nOut, vvchArgs))
 			{
 				const string& sender = stringFromVch(vvchAliasArgs[0]);
-				if (mapAliasIndex.count(sender) == 0) {
+				AliasMap::iterator it = mapAliasIndex.find(sender);
+				if (it == mapAliasIndex.end()) {
 					vertices.push_back(add_vertex(graph));
 					mapAliasIndex[sender] = vertices.size() - 1;
 				}
-				mapTxIndex[mapAliasIndex[sender]].push_back(n);
+				// remove duplicate senders and avoid processing into DAG
+				else
+				{
+					IndexMap::iterator it = mapTxIndex.find(mapAliasIndex[sender]);
+					if (it != mapTxIndex.end()) {
+						vecTxIndexToRemove.insert(n);
+						continue;
+					}
+
+				}
+				mapTxIndex[mapAliasIndex[sender]] = n;
 				
 				LogPrintf("CreateDAGFromBlock: found asset allocation from sender %s, nOut %d\n", sender, n);
 				CAssetAllocation allocation(tx);
 				if (!allocation.listSendingAllocationAmounts.empty()) {
 					for (auto& allocationInstance : allocation.listSendingAllocationAmounts) {
 						const string& receiver = stringFromVch(allocationInstance.first);
-						if (mapAliasIndex.count(receiver) == 0) {
+						AliasMap::iterator it = mapAliasIndex.find(receiver);
+						if (it == mapAliasIndex.end()) {
 							vertices.push_back(add_vertex(graph));
 							mapAliasIndex[receiver] = vertices.size() - 1;
 						}
@@ -55,13 +68,13 @@ unsigned int DAGRemoveCycles(CBlock * pblock, std::unique_ptr<CBlockTemplate> &p
 	LogPrintf("DAGRemoveCycles\n");
 	std::vector<CTransaction> newVtx;
 	std::vector<vertex_descriptor> vertices;
+	sorted_vector<int> vecTxIndexToRemove;
 	IndexMap mapTxIndex;
 	Graph graph;
 
-	if (!CreateDAGFromBlock(pblock, graph, vertices, mapTxIndex)) {
+	if (!CreateDAGFromBlock(pblock, graph, vertices, mapTxIndex, vecTxIndexToRemove)) {
 		return true;
 	}
-	std::vector<int> outputsToRemove;
 	sorted_vector<int> clearedVertices;
 	cycle_visitor<sorted_vector<int> > visitor(clearedVertices);
 	hawick_circuits(graph, visitor);
@@ -71,18 +84,12 @@ unsigned int DAGRemoveCycles(CBlock * pblock, std::unique_ptr<CBlockTemplate> &p
 		IndexMap::iterator it = mapTxIndex.find(nVertex);
 		if (it == mapTxIndex.end())
 			continue;
-		std::vector<int> &vecTx = (*it).second;
-		// mapTxIndex knows of the mapping between vertices and tx vout positions
-		for (auto& nOut : vecTx) {
-			if (nOut >= pblock->vtx.size())
-				continue;
-			LogPrintf("outputsToRemove %d\n", nOut);
-			outputsToRemove.push_back(nOut);
-		}
+		const int &nIndex = (*it).second;
+		vecTxIndexToRemove.insert(nIndex);
 	}
 	// outputs were saved above and loop through them backwards to remove from back to front
-	std::sort(outputsToRemove.begin(), outputsToRemove.end(), std::greater<int>());
-	for (auto& nIndex : outputsToRemove) {
+	std::reverse(vecTxIndexToRemove.begin(), vecTxIndexToRemove.end());
+	for (auto& nIndex : vecTxIndexToRemove) {
 		LogPrintf("reversed outputsToRemove %d\n", nIndex);
 		nFees -= pblocktemplate->vTxFees[nIndex];
 		nBlockSigOps -= pblocktemplate->vTxSigOps[nIndex];
@@ -125,9 +132,8 @@ bool DAGTopologicalSort(CBlock * pblock) {
 		IndexMap::iterator it = mapTxIndex.find(nVertex);
 		if (it == mapTxIndex.end())
 			continue;
-		const std::vector<int> &vecTx = (*it).second;
-		// we only need to add the first index we find because we want to ensure that we aren't processing more than one per sender per block
-		newVtx.push_back(pblock->vtx[vecTx.front()]);
+		const int &nIndex = (*it).second;
+		newVtx.push_back(pblock->vtx[nIndex]);
 	}
 	LogPrintf("topological ordering: %s\n", ordered);
 	// add non sys tx's to end of newVtx
