1f67dcea99eee5aa4bdc3e8e12603bd0952d0e3f
sidhujag <sidhujag@hotmail.com>
2018-01-20 14:29:19 -0800

    bitcoin signed message for message utils

--- src/validation.cpp
+++ src/validation.cpp
@@ -116,7 +116,7 @@ static void CheckBlockIndex(const Consensus::Params& consensusParams);
 /** Constant stuff for coinbase transactions we create: */
 CScript COINBASE_FLAGS;
 
-const string strMessageMagic = "Syscoin Signed Message:\n";
+const string strMessageMagic = "Bitcoin Signed Message:\n";
 
 // Internal stuff
 namespace {
