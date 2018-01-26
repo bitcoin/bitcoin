f6d98422ca4655e48a2e08b0346fd09b27ebfbf2
UdjinM6 <UdjinM6@users.noreply.github.com>
2018-01-26 04:10:45 +0300

    Silence ratecheck_test (#1873)

--- src/test/ratecheck_tests.cpp
+++ src/test/ratecheck_tests.cpp
@@ -19,7 +19,7 @@ BOOST_AUTO_TEST_CASE(ratecheck_test)
 
     buffer.AddTimestamp(1);
 
-    std::cout << "buffer.GetMinTimestamp() = " << buffer.GetMinTimestamp() << std::endl;
+    BOOST_TEST_MESSAGE("buffer.GetMinTimestamp() = " << buffer.GetMinTimestamp());
 
     BOOST_CHECK(buffer.GetCount() == 1);
     BOOST_CHECK(buffer.GetMinTimestamp() == 1);
@@ -42,10 +42,10 @@ BOOST_AUTO_TEST_CASE(ratecheck_test)
     int64_t nMax = buffer.GetMaxTimestamp();
     double dRate = buffer.GetRate();
 
-    std::cout << "buffer.GetCount() = " << buffer.GetCount() << std::endl;
-    std::cout << "nMin = " << nMin << std::endl;
-    std::cout << "nMax = " << nMax << std::endl;
-    std::cout << "buffer.GetRate() = " << dRate << std::endl;
+    BOOST_TEST_MESSAGE("buffer.GetCount() = " << buffer.GetCount());
+    BOOST_TEST_MESSAGE("nMin = " << nMin);
+    BOOST_TEST_MESSAGE("nMax = " << nMax);
+    BOOST_TEST_MESSAGE("buffer.GetRate() = " << dRate);
 
     //BOOST_CHECK(fabs(buffer.GetRate() - (3.0/2.0)) < 1.0e-9);
     BOOST_CHECK(buffer.GetRate() == 0.0);
@@ -71,9 +71,9 @@ BOOST_AUTO_TEST_CASE(ratecheck_test)
 
     CRateCheckBuffer buffer2;
 
-    std::cout << "Before loop tests" << std::endl;
+    BOOST_TEST_MESSAGE("Before loop tests");
     for(int64_t i = 1; i < 11; ++i)  {
-        std::cout << "In loop: i = " << i << std::endl;
+        BOOST_TEST_MESSAGE("In loop: i = " << i);
         buffer2.AddTimestamp(i);
         BOOST_CHECK(buffer2.GetCount() == (i <= 5 ? i : 5));
         BOOST_CHECK(buffer2.GetMinTimestamp() == max(int64_t(1), i - 4));
