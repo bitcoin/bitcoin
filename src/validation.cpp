1340eda3b7b6ca2789d6ec65dad72ee4c3844661
practicalswift <practicalswift@users.noreply.github.com>
2018-01-28 13:14:54 +0100

    Fix typos

--- src/validation.cpp
+++ src/validation.cpp
@@ -2577,7 +2577,7 @@ bool CChainState::ActivateBestChain(CValidationState &state, const CChainParams&
         if (GetMainSignals().CallbacksPending() > 10) {
             // Block until the validation queue drains. This should largely
             // never happen in normal operation, however may happen during
-            // reindex, causing memory blowup  if we run too far ahead.
+            // reindex, causing memory blowup if we run too far ahead.
             SyncWithValidationInterfaceQueue();
         }
 
