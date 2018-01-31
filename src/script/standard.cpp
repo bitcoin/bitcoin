80a2c3aed77aff39dc0967d306f1d21f35c822fe
Ben Woosley <ben.woosley@gmail.com>
2018-01-31 12:06:58 +0000

    Assert CPubKey::ValidLength to the pubkey's header-relevent size
    Previously this was an inline test where the specificity was probably judged
    overly specific. As a class method it makes sense to maintain consistency.

--- src/script/standard.cpp
+++ src/script/standard.cpp
@@ -132,7 +132,7 @@ bool Solver(const CScript& scriptPubKey, txnouttype& typeRet, std::vector<std::v
             // Template matching opcodes:
             if (opcode2 == OP_PUBKEYS)
             {
-                while (vch1.size() >= 33 && vch1.size() <= 65)
+                while (CPubKey::ValidSize(vch1))
                 {
                     vSolutionsRet.push_back(vch1);
                     if (!script1.GetOp(pc1, opcode1, vch1))
@@ -146,7 +146,7 @@ bool Solver(const CScript& scriptPubKey, txnouttype& typeRet, std::vector<std::v
 
             if (opcode2 == OP_PUBKEY)
             {
-                if (vch1.size() < 33 || vch1.size() > 65)
+                if (!CPubKey::ValidSize(vch1))
                     break;
                 vSolutionsRet.push_back(vch1);
             }
