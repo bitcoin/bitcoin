3f567531a6b82f335f28fada67bb6c700baf5911
Clem Taylor <clem@ossifrage.com>
2018-02-03 04:08:04 -0500

    Extend #11583 to include the most common message generated by non-contributing peers (port scanners?) 37% of the log default log entries for a node that has been up for ~24hrs was "version handshake timeout..."

--- src/net.cpp
+++ src/net.cpp
@@ -1431,7 +1431,7 @@ void CConnman::ThreadSocketHandler()
                 }
                 else if (!pnode->fSuccessfullyConnected)
                 {
-                    LogPrintf("version handshake timeout from %d\n", pnode->GetId());
+                    LogPrint(BCLog::NET, "version handshake timeout from %d\n", pnode->GetId());
                     pnode->fDisconnect = true;
                 }
             }
