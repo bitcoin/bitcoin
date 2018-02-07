d16f1a219073766788518513be2e1abe6e3e15f0
Michael Polzer <michael@298-MBP-MIP-3.local>
2018-02-07 18:14:03 +0100

    pull back protocol version (no need to bump by 205)

--- src/version.h
+++ src/version.h
@@ -10,16 +10,16 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70208;
+static const int PROTOCOL_VERSION = 70015;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 //! In this version, 'getheaders' was introduced.
-static const int GETHEADERS_VERSION = 70077;
+static const int GETHEADERS_VERSION = 31800;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 70003;
+static const int MIN_PEER_PROTO_VERSION = GETHEADERS_VERSION;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
@@ -32,19 +32,18 @@ static const int BIP0031_VERSION = 60000;
 static const int MEMPOOL_GD_VERSION = 60002;
 
 //! "filter*" commands are disabled without NODE_BLOOM after and including this version
-static const int NO_BLOOM_VERSION = 70201;
+static const int NO_BLOOM_VERSION = 70011;
 
 //! "sendheaders" command and announcing blocks with headers starts with this version
-static const int SENDHEADERS_VERSION = 70201;
+static const int SENDHEADERS_VERSION = 70012;
 
 //! "feefilter" tells peers to filter invs to you by fee starts with this version
-static const int FEEFILTER_VERSION = 70208;
+static const int FEEFILTER_VERSION = 70013;
 
-//! shord-id-based block download starts with this version
-static const int SHORT_IDS_BLOCKS_VERSION = 70208;
+//! short-id-based block download starts with this version
+static const int SHORT_IDS_BLOCKS_VERSION = 70014;
 
 //! not banning for invalid compact blocks starts with this version
-static const int INVALID_CB_NO_BAN_VERSION = 70208;
-
+static const int INVALID_CB_NO_BAN_VERSION = 70015;
 
 #endif // BITCOIN_VERSION_H
