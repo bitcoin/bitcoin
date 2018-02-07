daaae36e15e40cc179272129578c19bd95e8b243
Wladimir J. van der Laan <laanwj@gmail.com>
2018-02-07 10:30:59 +0100

    qt: Make sure splash screen is freed on AppInitMain fail
    The `splashFinished` event was never sent if AppInitMain fails,
    causing the splash screen to stick around, causing problems
    later.
    
    This bug has existed for a while but is now trigging potential crashed
    because the splash screen subscribes to wallet events.
    
    Meant to fix #12372.
    
    Signed-off-by: Wladimir J. van der Laan <laanwj@gmail.com>
    Github-Pull: #12374
    Rebased-From: f5a4c3ddf48db2119b2b1a438b9462a6236565cd
    Tree-SHA512: 1c59633f0caec6344dce7f7d69d2e98242601fa906b1845c372a59c8ba015c3ac76389dd5d4e60b2fdb52d2878d566a0325679470075a680418cade7204069ef

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -516,6 +516,7 @@ void BitcoinApplication::initializeResult(bool success)
 #endif
         pollShutdownTimer->start(200);
     } else {
+        Q_EMIT splashFinished(window);
         quit(); // Exit main loop
     }
 }
