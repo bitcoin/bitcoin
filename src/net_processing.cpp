120893c63d6807959a1d441d2caeb2a9fb0b2356
Alexander Block <ablock84@gmail.com>
2018-02-08 06:47:27 +0100

    Update timeLastMempoolReq when responding to MEMPOOL request (#1904)
    This should have been part of the Bitcoin #8080 backporting but was missed
    due to manual conflict resolution.

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -2790,6 +2790,7 @@ bool SendMessages(CNode* pto, CConnman& connman, const std::atomic<bool>& interr
                         vInv.clear();
                     }
                 }
+                pto->timeLastMempoolReq = GetTime();
             }
 
             // Determine transactions to relay
