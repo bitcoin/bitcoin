312088b56048132d0be68ccad0b26613a25828cb
UdjinM6 <UdjinM6@users.noreply.github.com>
2018-02-12 15:46:38 +0300

    Fix connectivity check in CActiveMasternode::ManageStateInitial (#1918)

--- src/activemasternode.cpp
+++ src/activemasternode.cpp
@@ -6,6 +6,7 @@
 #include "masternode.h"
 #include "masternode-sync.h"
 #include "masternodeman.h"
+#include "netbase.h"
 #include "protocol.h"
 
 // Keep track of the active Masternode
@@ -180,12 +181,13 @@ void CActiveMasternode::ManageStateInitial(CConnman& connman)
         return;
     }
 
+    // Check socket connectivity
     LogPrintf("CActiveMasternode::ManageStateInitial -- Checking inbound connection to '%s'\n", service.ToString());
+    SOCKET hSocket;
+    bool fConnected = ConnectSocket(service, hSocket, nConnectTimeout) && IsSelectableSocket(hSocket);
+    CloseSocket(hSocket);
 
-    CAddress addr(service, NODE_NETWORK);
-    connman.OpenMasternodeConnection(addr);
-
-    if (!connman.IsConnected(addr, CConnman::AllNodes)) {
+    if (!fConnected) {
         nState = ACTIVE_MASTERNODE_NOT_CAPABLE;
         strNotCapableReason = "Could not connect to " + service.ToString();
         LogPrintf("CActiveMasternode::ManageStateInitial -- %s: %s\n", GetStateString(), strNotCapableReason);
