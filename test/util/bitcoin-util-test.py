a8da932ff0e4ed79383c0a1532382b82f4f5e006
Wladimir J. van der Laan <laanwj@gmail.com>
2018-02-12 11:30:17 +0100

    Merge #11858: qa: Prepare tests for Windows
    faefd29 qa: Prepare functional tests for Windows (MarcoFalke)
    
    Pull request description:
    
      * Pass `sys.executable` when calling a python script via the subprocess
        module
      * Don't remove the log file while it is still open and written to
      * Properly use os.pathsep and os.path.sep when modifying the PATH
        environment variable
      * util-tests: Use os.path.join for Windows compatibility
    
      Ref:  #8227
    
    Tree-SHA512: c507a536af104b3bde4366b6634099db826532bd3e7c35d694b5883c550920643b3eab79c76703ca67e1044ed09979e855088f7324321c8d52112514e334d614

--- test/util/bitcoin-util-test.py
+++ test/util/bitcoin-util-test.py
@@ -44,7 +44,7 @@ def main():
     # Add the format/level to the logger
     logging.basicConfig(format=formatter, level=level)
 
-    bctester(os.path.join(env_conf["SRCDIR"], "test/util/data"), "bitcoin-util-test.json", env_conf)
+    bctester(os.path.join(env_conf["SRCDIR"], "test", "util", "data"), "bitcoin-util-test.json", env_conf)
 
 def bctester(testDir, input_basename, buildenv):
     """ Loads and parses the input file, runs all tests and reports results"""
