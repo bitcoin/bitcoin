f40df29d96a29197f9949b63da80f9ba3152fc79
practicalswift <practicalswift@users.noreply.github.com>
2018-02-12 15:48:37 +0100

    Fix Windows build errors introduced in #10498
    Fixes #12386

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -706,7 +706,7 @@ int main(int argc, char *argv[])
         if (BitcoinCore::baseInitialize()) {
             app.requestInitialize();
 #if defined(Q_OS_WIN) && QT_VERSION >= 0x050000
-            WinShutdownMonitor::registerShutdownBlockReason(QObject::tr("%1 didn't yet exit safely...").arg(QObject::tr(PACKAGE_NAME)), static_cast<HWND>(app.getMainWinId()));
+            WinShutdownMonitor::registerShutdownBlockReason(QObject::tr("%1 didn't yet exit safely...").arg(QObject::tr(PACKAGE_NAME)), (HWND)app.getMainWinId());
 #endif
             app.exec();
             app.requestShutdown();
