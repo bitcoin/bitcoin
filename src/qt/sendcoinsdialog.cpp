1cbb767bc281abe05860b37c9613723d408967cf
Sjors Provoost <sjors@sprovoost.nl>
2018-02-14 14:23:43 +0100

    [qt] send: Clear All also resets coin control options

--- src/qt/sendcoinsdialog.cpp
+++ src/qt/sendcoinsdialog.cpp
@@ -424,6 +424,12 @@ void SendCoinsDialog::send(QList<SendCoinsRecipient> recipients, QString strFee,
 
 void SendCoinsDialog::clear()
 {
+    // Clear coin control settings
+    CoinControlDialog::coinControl()->UnSelectAll();
+    ui->checkBoxCoinControlChange->setChecked(false);
+    ui->lineEditCoinControlChange->clear();
+    coinControlUpdateLabels();
+
     // Remove entries until only one left
     while(ui->entries->count())
     {
