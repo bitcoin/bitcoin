f8c556bec5b3c391c5e29bd11fe6261f628e8c79
fanquake <fanquake@gmail.com>
2018-02-15 19:12:49 +0800

    fixme: depends: Add D_DARWIN_C_SOURCE to miniupnpc CFLAGS

--- depends/packages/miniupnpc.mk
+++ depends/packages/miniupnpc.mk
@@ -9,6 +9,7 @@ $(package)_build_opts=CC="$($(package)_cc)"
 $(package)_build_opts_darwin=OS=Darwin LIBTOOL="$($(package)_libtool)"
 $(package)_build_opts_mingw32=-f Makefile.mingw
 $(package)_build_env+=CFLAGS="$($(package)_cflags) $($(package)_cppflags)" AR="$($(package)_ar)"
+$(package)_build_env+=CFLAGS=-D_DARWIN_C_SOURCE
 endef
 
 define $(package)_preprocess_cmds
