8645d3ee1edca9a237fa38fafccb21007acb45fb
MarcoFalke <falke.marco@gmail.com>
2018-02-15 14:37:31 -0500

    devtools: Exclude patches from lint-whitespace GitHub-Pull: #12442 Rebased-From: fafbf7f

--- contrib/devtools/lint-whitespace.sh
+++ contrib/devtools/lint-whitespace.sh
@@ -16,7 +16,7 @@ if [ -z "${TRAVIS_COMMIT_RANGE}" ]; then
 fi
 
 showdiff() {
-  if ! git diff -U0 "${TRAVIS_COMMIT_RANGE}" -- "." ":(exclude)src/leveldb/" ":(exclude)src/secp256k1/" ":(exclude)src/univalue/" ":(exclude)doc/release-notes/"; then
+  if ! git diff -U0 "${TRAVIS_COMMIT_RANGE}" -- "." ":(exclude)depends/patches/" ":(exclude)src/leveldb/" ":(exclude)src/secp256k1/" ":(exclude)src/univalue/" ":(exclude)doc/release-notes/"; then
     echo "Failed to get a diff"
     exit 1
   fi
