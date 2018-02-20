6eb6c4e61270da55e7158c7f883325396337b94b
Andrew Chow <achow101-github@achow101.com>
2018-02-20 16:09:51 -0500

    No longer shutdown after encrypting the wallet
    Since the database environment is flushed, closed, and reopened during
    EncryptWallet, there is no need to shut down the software anymore.

--- test/functional/test_framework/test_node.py
+++ test/functional/test_framework/test_node.py
@@ -305,14 +305,6 @@ class TestNode():
                     assert_msg = "bitcoind should have exited with expected error " + expected_msg
                 self._raise_assertion_error(assert_msg)
 
-    def node_encrypt_wallet(self, passphrase):
-        """"Encrypts the wallet.
-
-        This causes bitcoind to shutdown, so this method takes
-        care of cleaning up resources."""
-        self.encryptwallet(passphrase)
-        self.wait_until_stopped()
-
     def add_p2p_connection(self, p2p_conn, *, wait_for_verack=True, **kwargs):
         """Add a p2p connection to the node.
 
