b90b926270fdcbd3846b6c3ae2ed397c3839dca1
Karl-Johan Alm <karljohan-alm@garage.co.jp>
2018-02-20 13:20:36 +0900

    [test] Round target fee to 8 decimals in assert_fee_amount
    The output would produce arbitrary number of decimal points, sometimes resulting in 9 decimals:
    AssertionError: Fee of 0.00000415 BTC too low! (Should be 0.000006175 BTC)
    The above looks like the expected fee is 6175 sats when in reality it's 618.
    
    Github-Pull: #12486
    Rebased-From: 42e1b5d9797b65d3ce13a7cbace15fbedbcd4018

--- test/functional/test_framework/util.py
+++ test/functional/test_framework/util.py
@@ -26,7 +26,7 @@ logger = logging.getLogger("TestFramework.utils")
 
 def assert_fee_amount(fee, tx_size, fee_per_kB):
     """Assert the fee was in range"""
-    target_fee = tx_size * fee_per_kB / 1000
+    target_fee = round(tx_size * fee_per_kB / 1000, 8)
     if fee < target_fee:
         raise AssertionError("Fee of %s BTC too low! (Should be %s BTC)" % (str(fee), str(target_fee)))
     # allow the wallet's estimation to be at most 2 bytes off
