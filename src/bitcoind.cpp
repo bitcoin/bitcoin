e4475219f7a8f551390cfe1246fbdfbad628d136
John Newbery <john@johnnewbery.com>
2018-02-21 11:38:53 -0500

    [wallet] Use global g_wallet_init_interface to init/destroy the wallet.
    This commit creates a global g_wallet_init_interface, which is created
    in bitcoind and bitcoin-qt. g_wallet_init_interface is used to init
    and destroy the wallet.
    
    This removes the dependency from init.cpp on the wallet library.

--- src/bitcoind.cpp
+++ src/bitcoind.cpp
@@ -18,6 +18,10 @@
 #include <httpserver.h>
 #include <httprpc.h>
 #include <utilstrencodings.h>
+#if ENABLE_WALLET
+#include <wallet/init.h>
+#include <walletinitinterface.h>
+#endif
 
 #include <boost/thread.hpp>
 
@@ -58,6 +62,10 @@ void WaitForShutdown()
 bool AppInit(int argc, char* argv[])
 {
     bool fRet = false;
+
+#if ENABLE_WALLET
+    g_wallet_init_interface.reset(new WalletInit);
+#endif
 
     //
     // Parameters
