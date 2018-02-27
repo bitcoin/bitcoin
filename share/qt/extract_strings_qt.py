7bb9eac1f272d19edfa4850033d72a57d3552b1e
crypt0rick <33953035+crypt0rick@users.noreply.github.com>
2018-02-27 15:21:02 +0200

    initialize liberta

--- share/qt/extract_strings_qt.py
+++ share/qt/extract_strings_qt.py
@@ -9,7 +9,7 @@ import operator
 import os
 import sys
 
-OUT_CPP="qt/bitcoinstrings.cpp"
+OUT_CPP="qt/libertastrings.cpp"
 EMPTY=['""']
 
 def parse_po(text):
@@ -69,10 +69,10 @@ f.write("""
 #define UNUSED
 #endif
 """)
-f.write('static const char UNUSED *bitcoin_strings[] = {\n')
+f.write('static const char UNUSED *liberta_strings[] = {\n')
 messages.sort(key=operator.itemgetter(0))
 for (msgid, msgstr) in messages:
     if msgid != EMPTY:
-        f.write('QT_TRANSLATE_NOOP("bitcoin-core", %s),\n' % ('\n'.join(msgid)))
+        f.write('QT_TRANSLATE_NOOP("liberta-core", %s),\n' % ('\n'.join(msgid)))
 f.write('};\n')
 f.close()
