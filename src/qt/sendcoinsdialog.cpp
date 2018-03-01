eaf79feda0c68a0db6a1dda95602cab10b984379
Wladimir J. van der Laan <laanwj@gmail.com>
2018-03-01 12:18:25 +0100

    Merge #12421: [qt] navigate to  transaction history page after send
    e7d9fc5 [qt] navigate to  transaction history page after send (Sjors Provoost)
    
    Pull request description:
    
      Before this change QT just remained on the Send tab, which I found confusing. Now it switches to the Transactions tab. This makes it more clear to the user that the send actually succeeded, and here they can monitor progress.
    
      Ideally I would like to highlight the transaction, e.g. by refactoring `TransactionView::focusTransaction(const QModelIndex &idx)` to accept a transaction hash, but I'm not sure how to do that.
    
    Tree-SHA512: 8aa93e03874de8434e18951f8aec47377814c0bcaf7eda4766fc41d5a4e32806346e12e4139e4d45468dfdf0b786f5a7faa393a31b8cd6c65ccac21fb3782c33

--- src/qt/sendcoinsdialog.cpp
+++ src/qt/sendcoinsdialog.cpp
@@ -452,6 +452,7 @@ void SendCoinsDialog::send(QList<SendCoinsRecipient> recipients)
         accept();
         CoinControlDialog::coinControl()->UnSelectAll();
         coinControlUpdateLabels();
+        Q_EMIT coinsSent(currentTransaction.getTransaction()->GetHash());
     }
     fNewRecipientAllowed = true;
 }
