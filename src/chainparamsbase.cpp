019f8ec166081e058e321a9b92adbf1ade09cf88
tecnovert <tecnovert@particl.io>
2018-03-02 14:02:42 +0200

    Fix default rpc ports. [ci skip]

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -35,11 +35,11 @@ const CBaseChainParams& BaseParams()
 std::unique_ptr<CBaseChainParams> CreateBaseChainParams(const std::string& chain)
 {
     if (chain == CBaseChainParams::MAIN)
-        return MakeUnique<CBaseChainParams>("", 8332);
+        return MakeUnique<CBaseChainParams>("", 51735);
     else if (chain == CBaseChainParams::TESTNET)
-        return MakeUnique<CBaseChainParams>("testnet", 18332);
+        return MakeUnique<CBaseChainParams>("testnet", 51935);
     else if (chain == CBaseChainParams::REGTEST)
-        return MakeUnique<CBaseChainParams>("regtest", 18443);
+        return MakeUnique<CBaseChainParams>("regtest", 51936);
     else
         throw std::runtime_error(strprintf("%s: Unknown chain %s.", __func__, chain));
 }
