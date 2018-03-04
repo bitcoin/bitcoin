abe17a6dd17fefbf4cf64d243dc0548f07593610
tecnovert <tecnovert@particl.io>
2018-03-04 17:09:13 +0200

    Fix messages.

--- src/wallet/rpcdump.cpp
+++ src/wallet/rpcdump.cpp
@@ -315,7 +315,7 @@ UniValue importaddress(const JSONRPCRequest& request)
             std::vector<unsigned char> data(ParseHex(request.params[0].get_str()));
             ImportScript(pwallet, CScript(data.begin(), data.end()), strLabel, fP2SH);
         } else {
-            throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid Bitcoin address or script");
+            throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid Particl address or script");
         }
     }
     if (fRescan)
