45db031556a24828aaa3522b945fbd0a42636fc3
Wladimir J. van der Laan <laanwj@gmail.com>
2018-03-06 19:50:43 +0100

    Merge #11986: [depends] zeromq 4.2.3
    13a399a46 depends: patch pthread_set_name_np out of zeromq (Cory Fields)
    8f7922636 depends: zeromq 4.2.3 (fanquake)
    
    Pull request description:
    
      This is a followup to #9254 and #11981. Zeromq 4.2.3 was released just after #9254 was merged, and contains a years worth of improvements/bug fixes. See the release notes [here](https://github.com/zeromq/libzmq/releases/tag/v4.2.3).
    
      Todo:
      - [ ] Add zeromq-4.2.3.tar.gz to /depends-sources on bitcoincore.org
      - [ ] Verify gitian builds are still OK
      - [ ] Check: https://github.com/zeromq/libzmq/pull/2787
    
    Tree-SHA512: 85e06f47be3e1fdedcee50ce90e3391d69df2ea1c167472ffc3126d8970d418eb75141b970e422eb2fda9a8cad00e6ba5b36afa53565171a9ebaa152a9dc9b60
    (cherry picked from commit 765a3ebb026826f3f4765f5bbc9ed77ee23d67ba)
    
    # Conflicts:
    #	doc/dependencies.md

--- depends/packages/zeromq.mk
+++ depends/packages/zeromq.mk
@@ -1,9 +1,9 @@
 package=zeromq
-$(package)_version=4.2.2
+$(package)_version=4.2.3
 $(package)_download_path=https://github.com/zeromq/libzmq/releases/download/v$($(package)_version)/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=5b23f4ca9ef545d5bd3af55d305765e3ee06b986263b31967435d285a3e6df6b
-$(package)_patches=0001-fix-build-with-older-mingw64.patch
+$(package)_sha256_hash=8f1e2b2aade4dbfde98d82366d61baef2f62e812530160d2e6d0a5bb24e40bc0
+$(package)_patches=0001-fix-build-with-older-mingw64.patch 0002-disable-pthread_set_name_np.patch
 
 define $(package)_set_vars
   $(package)_config_opts=--without-docs --disable-shared --without-libsodium --disable-curve --disable-curve-keygen --disable-perf
@@ -13,7 +13,7 @@ endef
 
 define $(package)_preprocess_cmds
    patch -p1 < $($(package)_patch_dir)/0001-fix-build-with-older-mingw64.patch && \
-  ./autogen.sh
+   patch -p1 < $($(package)_patch_dir)/0002-disable-pthread_set_name_np.patch
 endef
 
 define $(package)_config_cmds
