f461627fc70ae914d5c37aac34bd9965d2a9713c
Evan Klitzke <evan@eklitzke.org>
2018-03-06 12:50:20 -0500

    Set SCHED_BATCH priority on the loadblk thread.
    While reading another PR I saw a mention of #6358. The use case for
    SCHED_BATCH is to hint to the kernel that the thread is running a
    non-interactive workload that consumes a lot of CPU time. This is
    helpful on desktop machines where the loadblk thread can interfere with
    interactive applications. More details can be found in the sched(7) man
    page.

--- src/init.cpp
+++ src/init.cpp
@@ -690,7 +690,7 @@ void ThreadImport(std::vector<fs::path> vImportFiles)
 {
     const CChainParams& chainparams = Params();
     RenameThread("chaincoin-loadblk");
-
+    ScheduleBatchPriority();
     {
     CImportingNow imp;
 
