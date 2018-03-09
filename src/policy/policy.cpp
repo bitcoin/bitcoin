6910490b0c6b89d8b2dfcfb34721415cbbc906fb
Michael Polzer <>
2018-03-09 17:33:16 +0100

    ...this statement, but the latter is misleadingly indented as if it were guarded by the ‘if’

--- src/policy/policy.cpp
+++ src/policy/policy.cpp
@@ -160,8 +160,8 @@ bool IsStandardTx(const CTransaction& tx, std::string& reason, const bool witnes
  */
 bool AreInputsStandard(const CTransaction& tx, const CCoinsViewCache& mapInputs)
 {
-if (tx.IsCoinBase())
-    return true; // Coinbases don't use vin normally
+    if (tx.IsCoinBase())
+        return true; // Coinbases don't use vin normally
 
     for (unsigned int i = 0; i < tx.vin.size(); i++)
     {
