d2702acd50cf2ac6f4400e2a8ff00e4a7771fdd8
Luke Dashjr <luke-jr+git@utopios.org>
2018-03-11 03:14:46 +0000

    Test: Add test for coin-age priority mining

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -201,6 +201,8 @@ BASE_SCRIPTS = [
     'tool_signet_miner.py --descriptors',
     'wallet_txn_clone.py',
     'wallet_txn_clone.py --segwit',
+    'mining_coin_age_priority.py',
+    'mining_coin_age_priority.py --gbt',
     'rpc_getchaintips.py',
     'rpc_misc.py',
     'p2p_1p1c_network.py',
