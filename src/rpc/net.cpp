f947d91c3df2f9c1b6c3b5375d261f15f243b97c
Luke Dashjr <luke-jr+git@utopios.org>
2018-03-12 05:58:49 +0000

    RPC/Net: Allow changing the connection_type for addnode onetry

--- src/rpc/net.cpp
+++ src/rpc/net.cpp
@@ -33,10 +33,30 @@ const std::vector<std::string> CONNECTION_TYPE_DOC{
         "outbound-full-relay (default automatic connections)",
         "block-relay-only (does not relay transactions or addresses)",
         "inbound (initiated by the peer)",
-        "manual (added via addnode RPC or -addnode/-connect configuration options)",
+        "manual (added via addnode RPC or -addnode/-connect configuration options; protected from DoS disconnection and not required to be full nodes as other outbound peers are)",
         "addr-fetch (short-lived automatic connection for soliciting addresses)",
         "feeler (short-lived automatic connection for testing addresses)"
 };
+
+ConnectionType ConnectionTypeFromValue(const UniValue& uv)
+{
+    std::string s = uv.get_str();
+    if (s == "inbound") {
+        return ConnectionType::INBOUND;
+    } else if (s == "manual") {
+        return ConnectionType::MANUAL;
+    } else if (s == "feeler") {
+        return ConnectionType::FEELER;
+    } else if (s == "outbound-full-relay") {
+        return ConnectionType::OUTBOUND_FULL_RELAY;
+    } else if (s == "block-relay-only") {
+        return ConnectionType::BLOCK_RELAY;
+    } else if (s == "addr-fetch") {
+        return ConnectionType::ADDR_FETCH;
+    }
+
+    throw JSONRPCError(RPC_INVALID_PARAMETER, "Unknown connection type " + s);
+}
 
 static RPCHelpMan getconnectioncount()
 {
@@ -271,12 +291,11 @@ static RPCHelpMan addnode()
 {
     return RPCHelpMan{"addnode",
                 "\nAttempts to add or remove a node from the addnode list.\n"
-                "Or try a connection to a node once.\n"
-                "Nodes added using addnode (or -connect) are protected from DoS disconnection and are not required to be\n"
-                "full nodes/support SegWit as other outbound peers are (though such peers will not be synced from).\n",
+                "Or try a connection to a node once.\n",
                 {
                     {"node", RPCArg::Type::STR, RPCArg::Optional::NO, "The node (see getpeerinfo for nodes)"},
                     {"command", RPCArg::Type::STR, RPCArg::Optional::NO, "'add' to add a node to the list, 'remove' to remove a node from the list, 'onetry' to try a connection to the node once"},
+                    {"connection_type", RPCArg::Type::BOOL, /* default */ "manual", "Type of connection: \n" + Join(CONNECTION_TYPE_DOC, ",\n") + "\nOnly supported for command \"onetry\" for now."},
                 },
                 RPCResult{RPCResult::Type::NONE, "", ""},
                 RPCExamples{
@@ -288,7 +307,7 @@ static RPCHelpMan addnode()
     std::string strCommand;
     if (!request.params[1].isNull())
         strCommand = request.params[1].get_str();
-    if (request.fHelp || request.params.size() != 2 ||
+    if (
         (strCommand != "onetry" && strCommand != "add" && strCommand != "remove"))
         throw std::runtime_error(
             self.ToString());
@@ -298,16 +317,27 @@ static RPCHelpMan addnode()
         throw JSONRPCError(RPC_CLIENT_P2P_DISABLED, "Error: Peer-to-peer functionality missing or disabled");
 
     std::string strNode = request.params[0].get_str();
+    ConnectionType connection_type = ConnectionType::MANUAL;
+    if (!request.params[2].isNull()) {
+        if (strCommand == "remove") {
+            throw std::runtime_error(self.ToString());
+        }
+        connection_type = ConnectionTypeFromValue(request.params[2]);
+    }
 
     if (strCommand == "onetry")
     {
         CAddress addr;
-        node.connman->OpenNetworkConnection(addr, false, nullptr, strNode.c_str(), ConnectionType::MANUAL);
+        node.connman->OpenNetworkConnection(addr, false, nullptr, strNode.c_str(), connection_type);
         return NullUniValue;
     }
 
     if (strCommand == "add")
     {
+        if (connection_type != ConnectionType::MANUAL) {
+            throw JSONRPCError(RPC_INVALID_PARAMETER, "connection_type != manual is only supported for the \"onetry\" command for now");
+        }
+
         if(!node.connman->AddNode(strNode))
             throw JSONRPCError(RPC_CLIENT_NODE_ALREADY_ADDED, "Error: Node already added");
     }
@@ -896,7 +926,7 @@ static const CRPCCommand commands[] =
     { "network",            "getconnectioncount",     &getconnectioncount,     {} },
     { "network",            "ping",                   &ping,                   {} },
     { "network",            "getpeerinfo",            &getpeerinfo,            {} },
-    { "network",            "addnode",                &addnode,                {"node","command"} },
+    { "network",            "addnode",                &addnode,                {"node","command","connection_type"} },
     { "network",            "disconnectnode",         &disconnectnode,         {"address", "nodeid"} },
     { "network",            "getaddednodeinfo",       &getaddednodeinfo,       {"node"} },
     { "network",            "getnettotals",           &getnettotals,           {} },
