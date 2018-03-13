3ead8f8df6a2d1cbc07caf5f5408e850bd91c9d5
Michael Polzer <>
2018-03-13 06:20:45 +0100

    [contrib] no suspicious hosts yet

--- contrib/seeds/makeseeds.py
+++ contrib/seeds/makeseeds.py
@@ -14,13 +14,7 @@ MIN_BLOCKS = 337600
 
 # These are hosts that have been observed to be behaving strangely (e.g.
 # aggressively connecting to every node).
-SUSPICIOUS_HOSTS = {
-    "130.211.129.106", "178.63.107.226",
-    "83.81.130.26", "88.198.17.7", "148.251.238.178", "176.9.46.6",
-    "54.173.72.127", "54.174.10.182", "54.183.64.54", "54.194.231.211",
-    "54.66.214.167", "54.66.220.137", "54.67.33.14", "54.77.251.214",
-    "54.94.195.96", "54.94.200.247"
-}
+SUSPICIOUS_HOSTS = {}
 
 import re
 import sys
