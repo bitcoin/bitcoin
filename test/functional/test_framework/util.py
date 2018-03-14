57d82cf72247d503847d85be3f9245a60f8feada
Wladimir J. van der Laan <laanwj@gmail.com>
2018-03-14 14:18:44 +0100

    Merge #12638: qa: Cache only chain and wallet for regtest datadir
    fa23105 qa: Cache only chain and wallet for regtest datadir (MarcoFalke)
    
    Pull request description:
    
      mempool.dat should be empty and I don't see a need to copy it around when restoring from the cache.
    
    Tree-SHA512: f11ab69732db4dee0e9a0900570464e49085532b0cebc963877057112a7b985c477da3d32eb2093daabac9ada9e73b7c49881681ec5efa6101919b0af76001cf

--- test/functional/test_framework/util.py
+++ test/functional/test_framework/util.py
@@ -346,9 +346,6 @@ def copy_datadir(from_node, to_node, dirname):
         except:
             pass
 
-def log_filename(dirname, n_node, logname):
-    return os.path.join(dirname, "node" + str(n_node), "regtest", logname)
-
 def get_bip9_status(node, key):
     info = node.getblockchaininfo()
     return info['bip9_softforks'][key]
