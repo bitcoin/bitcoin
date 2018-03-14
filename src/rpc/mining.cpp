a8e661b6b2680ceb6259bbe531ac4771d65e9a63
practicalswift <practicalswift@users.noreply.github.com>
2018-03-14 23:04:25 +0100

    Simplify KeepRunning logic

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -479,13 +479,14 @@ UniValue getblocktemplate(const JSONRPCRequest& request)
             checktxtime = std::chrono::steady_clock::now() + std::chrono::minutes(1);
 
             WaitableLock lock(csBestBlock);
-            auto KeepRunning = [&]() -> bool {
-                LOCK(cs_main);
-                assert(chainActive.Tip());
-                return chainActive.Tip()->GetBlockHash() == hashWatchedChain && IsRPCRunning();
-            };
-            while (KeepRunning())
+            while (IsRPCRunning())
             {
+                {
+                    LOCK(cs_main);
+                    if (chainActive.Tip()->GetBlockHash() != hashWatchedChain) {
+                        break;
+                    }
+                }
                 if (cvBlockChange.wait_until(lock, checktxtime) == std::cv_status::timeout)
                 {
                     // Timeout: Check transactions for update
