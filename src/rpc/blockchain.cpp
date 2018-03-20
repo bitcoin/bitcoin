4d56dec88c122f24b965afa20da6c2dd34871d0c
Russell Yanofsky <russ@yanofsky.org>
2018-03-20 13:07:17 -0400

    Remove unnecessary NONNEGATIVE_SIGNED
    Switch to unsigned encoding, which is backwards compatible and avoids MSVC
    error reported https://github.com/bitcoin/bitcoin/issues/12732

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -834,7 +834,7 @@ static void ApplyStats(CCoinsStats &stats, CHashWriter& ss, const uint256& hash,
 {
     assert(!outputs.empty());
     ss << hash;
-    ss << VARINT(outputs.begin()->second.nHeight * 2 + outputs.begin()->second.fCoinBase, VarIntMode::NONNEGATIVE_SIGNED);
+    ss << VARINT(outputs.begin()->second.nHeight * 2 + outputs.begin()->second.fCoinBase ? 1u : 0u);
     stats.nTransactions++;
     for (const auto output : outputs) {
         ss << VARINT(output.first + 1);
