d27327c79a651fab77e032fa1f00ce4458db959b
practicalswift <practicalswift@users.noreply.github.com>
2018-03-21 10:54:17 +0100

    Fix typos

--- src/policy/policy.cpp
+++ src/policy/policy.cpp
@@ -261,5 +261,5 @@ int64_t GetVirtualTransactionSize(const CTransaction& tx, int64_t nSigOpCost)
 
 int64_t GetVirtualTransactionInputSize(const CTxIn& txin, int64_t nSigOpCost)
 {
-    return GetVirtualTransactionSize(GetTransationInputWeight(txin), nSigOpCost);
+    return GetVirtualTransactionSize(GetTransactionInputWeight(txin), nSigOpCost);
 }
