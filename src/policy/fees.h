46d89488577de7db8a9d14db5b2bbe4ac9d2e8c5
practicalswift <practicalswift@users.noreply.github.com>
2018-03-22 15:19:44 +0100

    Fix missing or inconsistent include guards

--- src/policy/fees.h
+++ src/policy/fees.h
@@ -2,8 +2,8 @@
 // Copyright (c) 2009-2017 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-#ifndef BITCOIN_POLICYESTIMATOR_H
-#define BITCOIN_POLICYESTIMATOR_H
+#ifndef BITCOIN_POLICY_FEES_H
+#define BITCOIN_POLICY_FEES_H
 
 #include <amount.h>
 #include <policy/feerate.h>
@@ -294,4 +294,4 @@ private:
     FastRandomContext insecure_rand;
 };
 
-#endif /*BITCOIN_POLICYESTIMATOR_H */
+#endif // BITCOIN_POLICY_FEES_H
