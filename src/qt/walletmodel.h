f0a83d4920e682971c5fc3183842dfc463200c1d
Russell Yanofsky <russ@yanofsky.org>
2018-03-23 17:14:39 -0400

    Remove direct bitcoin calls from qt/paymentserver.cpp

--- src/qt/walletmodel.h
+++ src/qt/walletmodel.h
@@ -253,7 +253,7 @@ Q_SIGNALS:
     void message(const QString &title, const QString &message, unsigned int style);
 
     // Coins sent: from wallet, to recipient, in (serialized) transaction:
-    void coinsSent(CWallet* wallet, SendCoinsRecipient recipient, QByteArray transaction);
+    void coinsSent(WalletModel* wallet, SendCoinsRecipient recipient, QByteArray transaction);
 
     // Show progress dialog e.g. for rescan
     void showProgress(const QString &title, int nProgress);
