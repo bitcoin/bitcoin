2bf6d5a1a4073a747c62940a7f46ad619bf4b750
practicalswift <practicalswift@users.noreply.github.com>
2018-03-25 22:49:33 +0200

    Reduce variable scopes

--- src/qt/recentrequeststablemodel.cpp
+++ src/qt/recentrequeststablemodel.cpp
@@ -139,10 +139,9 @@ bool RecentRequestsTableModel::removeRows(int row, int count, const QModelIndex 
 
     if(count > 0 && row >= 0 && (row+count) <= list.size())
     {
-        const RecentRequestEntry *rec;
         for (int i = 0; i < count; ++i)
         {
-            rec = &list[row+i];
+            const RecentRequestEntry* rec = &list[row+i];
             if (!walletModel->saveReceiveRequest(rec->recipient.address.toStdString(), rec->id, ""))
                 return false;
         }
