2f1661678c13d35b5b8ee9c05d67a260be1484bb
UdjinM6 <UdjinM6@users.noreply.github.com>
2018-03-29 18:07:40 +0300

    Locked txes should not expire until mined and have sufficient confirmations (#2011)

--- src/txmempool.cpp
+++ src/txmempool.cpp
@@ -8,6 +8,7 @@
 #include "clientversion.h"
 #include "consensus/consensus.h"
 #include "consensus/validation.h"
+#include "instantx.h"
 #include "validation.h"
 #include "policy/policy.h"
 #include "policy/fees.h"
@@ -1148,6 +1149,11 @@ int CTxMemPool::Expire(int64_t time) {
     indexed_transaction_set::index<entry_time>::type::iterator it = mapTx.get<entry_time>().begin();
     setEntries toremove;
     while (it != mapTx.get<entry_time>().end() && it->GetTime() < time) {
+        // locked txes do not expire until mined and have sufficient confirmations
+        if (instantsend.IsLockedInstantSendTransaction(it->GetTx().GetHash())) {
+            it++;
+            continue;
+        }
         toremove.insert(mapTx.project<0>(it));
         it++;
     }
