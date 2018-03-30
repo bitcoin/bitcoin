d894894aab78dbd5e32267313237648933248377
João Barbosa <joao.paulo.barbosa@gmail.com>
2018-03-30 09:48:29 +0100

    wallet: Refactor to WalletInitInterface* const g_wallet_init_interface

--- src/init.cpp
+++ src/init.cpp
@@ -88,7 +88,8 @@ public:
     void Close() override {}
 };
 
-std::unique_ptr<WalletInitInterface> g_wallet_init_interface(new DummyWalletInit);
+static DummyWalletInit g_dummy_wallet_init;
+WalletInitInterface* const g_wallet_init_interface = &g_dummy_wallet_init;
 #endif
 
 #if ENABLE_ZMQ
@@ -283,7 +284,6 @@ void Shutdown()
     GetMainSignals().UnregisterBackgroundSignalScheduler();
     GetMainSignals().UnregisterWithMempoolSignals(mempool);
     g_wallet_init_interface->Close();
-    g_wallet_init_interface.reset();
     globalVerifyHandle.reset();
     ECC_Stop();
     LogPrintf("%s: done\n", __func__);
