49c1dbff79925dca3f1284013290e6d8824d2a76
João Barbosa <joao.paulo.barbosa@gmail.com>
2018-03-30 17:48:20 +0100

    rpc: Prevent concurrent savemempool

--- src/validation.cpp
+++ src/validation.cpp
@@ -4748,6 +4748,9 @@ bool DumpMempool()
     std::map<uint256, CAmount> mapDeltas;
     std::vector<TxMempoolInfo> vinfo;
 
+    static Mutex dump_mutex;
+    LOCK(dump_mutex);
+
     {
         LOCK(mempool.cs);
         for (const auto &i : mempool.mapDeltas) {
