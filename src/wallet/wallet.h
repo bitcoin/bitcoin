5c50e93d52c14fc7bc41130cdb1568f2c11e45de
Andrew Chow <achow101-github@achow101.com>
2018-03-31 13:37:27 -0400

    Allow -upgradewallet to upgradewallets to HD
    Changes the maximum upgradewallet version to the latest wallet version
    number, 159900. Non-HD wallets will be upgraded to use HD derivation.
    Non HD chain split wallets will be upgraded to HD chain split.
    
    If a non-HD wallet is upgraded to HD, the keypool will be entirely
    regenerated.
    
    Since upgradewallet is effectively run during a first run, all of the
    first run initial setup stuff is combined with the upgrade to HD

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -89,7 +89,7 @@ enum WalletFeature
 
     FEATURE_NO_DEFAULT_KEY = 159900, // Wallet without a default key written
 
-    FEATURE_LATEST = FEATURE_COMPRPUBKEY // HD is optional, use FEATURE_COMPRPUBKEY as latest version
+    FEATURE_LATEST = FEATURE_NO_DEFAULT_KEY
 };
 
 enum class OutputType {
