0ea154f1c4c2add01f29654cabcfb01a3151a5b2
sidhujag <sidhujag@hotmail.com>
2018-04-03 12:02:34 -0700

    compile

--- src/alias.cpp
+++ src/alias.cpp
@@ -2003,7 +2003,7 @@ unsigned int aliasunspent(const vector<unsigned char> &vchAlias, COutPoint& outp
 	if (resUTXOs.isArray())
 		utxoArray = resUTXOs.get_array();
 	else
-		return;
+		return 0;
 	unsigned int count = 0;
 	CAmount nCurrentAmount = 0;
 	for (unsigned int i = 0; i<utxoArray.size(); i++)
