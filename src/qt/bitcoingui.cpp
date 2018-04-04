bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

Remove direct bitcoin calls from qt/utilitydialog.cpp

src/qt/bitcoingui.cpp
@@ -153,7 +153,7 @@ BitcoinGUI::BitcoinGUI(ipc::Node& _ipcNode, const PlatformStyle *_platformStyle,
 #endif
 
     rpcConsole = new RPCConsole(_platformStyle, 0);
-    helpMessageDialog = new HelpMessageDialog(this, false);
+    helpMessageDialog = new HelpMessageDialog(ipcNode, this, false);
 #ifdef ENABLE_WALLET
     if(enableWallet)
     {
@@ -646,7 +646,7 @@ void BitcoinGUI::aboutClicked()
     if(!clientModel)
         return;
 
-    HelpMessageDialog dlg(this, true);
+    HelpMessageDialog dlg(ipcNode, this, true);
     dlg.exec();
 }
 

src/qt/utilitydialog.h
@@ -11,6 +11,10 @@
 class BitcoinGUI;
 class ClientModel;
 
+namespace ipc {
+    class Node;
+}
+
 namespace Ui {
     class HelpMessageDialog;
 }
@@ -21,7 +25,7 @@ class HelpMessageDialog : public QDialog
     Q_OBJECT
 
 public:
-    explicit HelpMessageDialog(QWidget *parent, bool about);
+    explicit HelpMessageDialog(ipc::Node& ipcNode, QWidget *parent, bool about);
     ~HelpMessageDialog();
 
     void printToConsole();

bitcoin/bitcoin

src/qt/bitcoingui.cpp
@@ -153,7 +153,7 @@ BitcoinGUI::BitcoinGUI(interface::Node& node, const PlatformStyle *_platformStyl
 #endif
 
     rpcConsole = new RPCConsole(_platformStyle, 0);
-    helpMessageDialog = new HelpMessageDialog(this, false);
+    helpMessageDialog = new HelpMessageDialog(node, this, false);
 #ifdef ENABLE_WALLET
     if(enableWallet)
     {
@@ -668,7 +668,7 @@ void BitcoinGUI::aboutClicked()
     if(!clientModel)
         return;
 
-    HelpMessageDialog dlg(this, true);
+    HelpMessageDialog dlg(m_node, this, true);
     dlg.exec();
 }
 

src/qt/utilitydialog.h
@@ -10,6 +10,10 @@
 
 class BitcoinGUI;
 
+namespace interface {
+    class Node;
+}
+
 namespace Ui {
     class HelpMessageDialog;
 }
@@ -20,7 +24,7 @@ class HelpMessageDialog : public QDialog
     Q_OBJECT
 
 public:
-    explicit HelpMessageDialog(QWidget *parent, bool about);
+    explicit HelpMessageDialog(interface::Node& node, QWidget *parent, bool about);
     ~HelpMessageDialog();
 
     void printToConsole();