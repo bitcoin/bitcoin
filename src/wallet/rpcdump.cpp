86eff460781a249ccebb1e4656c3c0a00fde77c9
MarcoFalke <falke.marco@gmail.com>
2018-04-04 17:26:48 -0400

    Merge #12702: [wallet] [rpc] [doc] importprivkey: hint about importmulti
    4e05687153 [wallet] [rpc] [doc] importprivkey: hint about importmulti (Karl-Johan Alm)
    
    Pull request description:
    
      From #12701, a hint about `importmulti` inside the help for `importprivkey` seems useful.
    
    Tree-SHA512: 09ddfd384062b4365f678167076cb9f5af1eb8f083714a20c2a9bb14fef1c886d1666196272bf09862537166d15ae89c3330cdc6836eee76cb54d137e53301df

--- src/wallet/rpcdump.cpp
+++ src/wallet/rpcdump.cpp
@@ -81,6 +81,7 @@ UniValue importprivkey(const JSONRPCRequest& request)
         throw std::runtime_error(
             "importprivkey \"privkey\" ( \"label\" ) ( rescan )\n"
             "\nAdds a private key (as returned by dumpprivkey) to your wallet. Requires a new wallet backup.\n"
+            "Hint: use importmulti to import more than one private key.\n"
             "\nArguments:\n"
             "1. \"privkey\"          (string, required) The private key (see dumpprivkey)\n"
             "2. \"label\"            (string, optional, default=\"\") An optional label\n"
