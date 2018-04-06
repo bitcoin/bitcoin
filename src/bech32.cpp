eaca9bb7e69819012196c93a8e4a506a17f4400c
lachlangreenbank <lachlan.greenbank@gmail.com>
2018-04-06 12:09:28 +1000

    Comment cleanup and consistency

--- src/bech32.cpp
+++ src/bech32.cpp
@@ -1,6 +1,6 @@
-// Copyright (c) 2017 Pieter Wuille
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+/** Copyright (c) 2017 Pieter Wuille
+ * Distributed under the MIT software license, see the accompanying
+ * file COPYING or http://www.opensource.org/licenses/mit-license.php. */
 
 #include <bech32.h>
 
@@ -36,54 +36,55 @@ data Cat(data x, const data& y)
  *  bits correspond to earlier values. */
 uint32_t PolyMod(const data& v)
 {
-    // The input is interpreted as a list of coefficients of a polynomial over F = GF(32), with an
-    // implicit 1 in front. If the input is [v0,v1,v2,v3,v4], that polynomial is v(x) =
-    // 1*x^5 + v0*x^4 + v1*x^3 + v2*x^2 + v3*x + v4. The implicit 1 guarantees that
-    // [v0,v1,v2,...] has a distinct checksum from [0,v0,v1,v2,...].
+   /** The input is interpreted as a list of coefficients of a polynomial over F = GF(32), with an
+    *  implicit 1 in front. If the input is [v0,v1,v2,v3,v4], that polynomial is v(x) =
+    *  1*x^5 + v0*x^4 + v1*x^3 + v2*x^2 + v3*x + v4. The implicit 1 guarantees that
+    *  [v0,v1,v2,...] has a distinct checksum from [0,v0,v1,v2,...].
 
-    // The output is a 30-bit integer whose 5-bit groups are the coefficients of the remainder of
-    // v(x) mod g(x), where g(x) is the Bech32 generator,
-    // x^6 + {29}x^5 + {22}x^4 + {20}x^3 + {21}x^2 + {29}x + {18}. g(x) is chosen in such a way
-    // that the resulting code is a BCH code, guaranteeing detection of up to 3 errors within a
-    // window of 1023 characters. Among the various possible BCH codes, one was selected to in
-    // fact guarantee detection of up to 4 errors within a window of 89 characters.
+    *  The output is a 30-bit integer whose 5-bit groups are the coefficients of the remainder of
+    *  v(x) mod g(x), where g(x) is the Bech32 generator,
+    *  x^6 + {29}x^5 + {22}x^4 + {20}x^3 + {21}x^2 + {29}x + {18}. g(x) is chosen in such a way
+    *  that the resulting code is a BCH code, guaranteeing detection of up to 3 errors within a
+    *  window of 1023 characters. Among the various possible BCH codes, one was selected to in
+    *  fact guarantee detection of up to 4 errors within a window of 89 characters.
 
-    // Note that the coefficients are elements of GF(32), here represented as decimal numbers
-    // between {}. In this finite field, addition is just XOR of the corresponding numbers. For
-    // example, {27} + {13} = {27 ^ 13} = {22}. Multiplication is more complicated, and requires
-    // treating the bits of values themselves as coefficients of a polynomial over a smaller field,
-    // GF(2), and multiplying those polynomials mod a^5 + a^3 + 1. For example, {5} * {26} =
-    // (a^2 + 1) * (a^4 + a^3 + a) = (a^4 + a^3 + a) * a^2 + (a^4 + a^3 + a) = a^6 + a^5 + a^4 + a
-    // = a^3 + 1 (mod a^5 + a^3 + 1) = {9}.
+    *  Note that the coefficients are elements of GF(32), here represented as decimal numbers
+    *  between {}. In this finite field, addition is just XOR of the corresponding numbers. For
+    *  example, {27} + {13} = {27 ^ 13} = {22}. Multiplication is more complicated, and requires
+    *  treating the bits of values themselves as coefficients of a polynomial over a smaller field,
+    *  GF(2), and multiplying those polynomials mod a^5 + a^3 + 1. For example, {5} * {26} =
+    *  (a^2 + 1) * (a^4 + a^3 + a) = (a^4 + a^3 + a) * a^2 + (a^4 + a^3 + a) = a^6 + a^5 + a^4 + a
+    *  = a^3 + 1 (mod a^5 + a^3 + 1) = {9}.
 
-    // During the course of the loop below, `c` contains the bitpacked coefficients of the
-    // polynomial constructed from just the values of v that were processed so far, mod g(x). In
-    // the above example, `c` initially corresponds to 1 mod (x), and after processing 2 inputs of
-    // v, it corresponds to x^2 + v0*x + v1 mod g(x). As 1 mod g(x) = 1, that is the starting value
-    // for `c`.
+    *  During the course of the loop below, `c` contains the bitpacked coefficients of the
+    *  polynomial constructed from just the values of v that were processed so far, mod g(x). In
+    *  the above example, `c` initially corresponds to 1 mod (x), and after processing 2 inputs of
+    *  v, it corresponds to x^2 + v0*x + v1 mod g(x). As 1 mod g(x) = 1, that is the starting value
+    *  for `c`. */
+
     uint32_t c = 1;
     for (auto v_i : v) {
-        // We want to update `c` to correspond to a polynomial with one extra term. If the initial
-        // value of `c` consists of the coefficients of c(x) = f(x) mod g(x), we modify it to
-        // correspond to c'(x) = (f(x) * x + v_i) mod g(x), where v_i is the next input to
-        // process. Simplifying:
-        // c'(x) = (f(x) * x + v_i) mod g(x)
-        //         ((f(x) mod g(x)) * x + v_i) mod g(x)
-        //         (c(x) * x + v_i) mod g(x)
-        // If c(x) = c0*x^5 + c1*x^4 + c2*x^3 + c3*x^2 + c4*x + c5, we want to compute
-        // c'(x) = (c0*x^5 + c1*x^4 + c2*x^3 + c3*x^2 + c4*x + c5) * x + v_i mod g(x)
-        //       = c0*x^6 + c1*x^5 + c2*x^4 + c3*x^3 + c4*x^2 + c5*x + v_i mod g(x)
-        //       = c0*(x^6 mod g(x)) + c1*x^5 + c2*x^4 + c3*x^3 + c4*x^2 + c5*x + v_i
-        // If we call (x^6 mod g(x)) = k(x), this can be written as
-        // c'(x) = (c1*x^5 + c2*x^4 + c3*x^3 + c4*x^2 + c5*x + v_i) + c0*k(x)
+       /** We want to update `c` to correspond to a polynomial with one extra term. If the initial
+        *  value of `c` consists of the coefficients of c(x) = f(x) mod g(x), we modify it to
+        *  correspond to c'(x) = (f(x) * x + v_i) mod g(x), where v_i is the next input to
+        *  process. Simplifying:
+        *  c'(x) = (f(x) * x + v_i) mod g(x)
+        *          ((f(x) mod g(x)) * x + v_i) mod g(x)
+        *          (c(x) * x + v_i) mod g(x)
+        *  If c(x) = c0*x^5 + c1*x^4 + c2*x^3 + c3*x^2 + c4*x + c5, we want to compute
+        *  c'(x) = (c0*x^5 + c1*x^4 + c2*x^3 + c3*x^2 + c4*x + c5) * x + v_i mod g(x)
+        *        = c0*x^6 + c1*x^5 + c2*x^4 + c3*x^3 + c4*x^2 + c5*x + v_i mod g(x)
+        *        = c0*(x^6 mod g(x)) + c1*x^5 + c2*x^4 + c3*x^3 + c4*x^2 + c5*x + v_i
+        *  If we call (x^6 mod g(x)) = k(x), this can be written as
+        *  c'(x) = (c1*x^5 + c2*x^4 + c3*x^3 + c4*x^2 + c5*x + v_i) + c0*k(x) */
 
-        // First, determine the value of c0:
+        /** First, determine the value of c0: */
         uint8_t c0 = c >> 25;
 
-        // Then compute c1*x^5 + c2*x^4 + c3*x^3 + c4*x^2 + c5*x + v_i:
+        /** Then compute c1*x^5 + c2*x^4 + c3*x^3 + c4*x^2 + c5*x + v_i: */
         c = ((c & 0x1ffffff) << 5) ^ v_i;
 
-        // Finally, for each set bit n in c0, conditionally add {2^n}k(x):
+        /** Finally, for each set bit n in c0, conditionally add {2^n}k(x): */
         if (c0 & 1)  c ^= 0x3b6a57b2; //     k(x) = {29}x^5 + {22}x^4 + {20}x^3 + {21}x^2 + {29}x + {18}
         if (c0 & 2)  c ^= 0x26508e6d; //  {2}k(x) = {19}x^5 +  {5}x^4 +     x^3 +  {3}x^2 + {19}x + {13}
         if (c0 & 4)  c ^= 0x1ea119fa; //  {4}k(x) = {15}x^5 + {10}x^4 +  {2}x^3 +  {6}x^2 + {15}x + {26}
@@ -117,10 +118,10 @@ data ExpandHRP(const std::string& hrp)
 /** Verify a checksum. */
 bool VerifyChecksum(const std::string& hrp, const data& values)
 {
-    // PolyMod computes what value to xor into the final values to make the checksum 0. However,
-    // if we required that the checksum was 0, it would be the case that appending a 0 to a valid
-    // list of values would result in a new valid list. For that reason, Bech32 requires the
-    // resulting checksum to be 1 instead.
+   /** PolyMod computes what value to xor into the final values to make the checksum 0. However,
+     * if we required that the checksum was 0, it would be the case that appending a 0 to a valid
+     * list of values would result in a new valid list. For that reason, Bech32 requires the
+     * resulting checksum to be 1 instead. */
     return PolyMod(Cat(ExpandHRP(hrp), values)) == 1;
 }
 
