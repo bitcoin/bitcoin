782cacc5d02d50cf2be002e90aedfb121f881fb4
sidhujag <sidhujag@hotmail.com>
2018-04-06 12:02:33 -0700

    compile

--- src/test/syscoin_asset_tests.cpp
+++ src/test/syscoin_asset_tests.cpp
@@ -513,7 +513,7 @@ BOOST_AUTO_TEST_CASE(generate_assetupdate)
 	BOOST_CHECK_THROW(r = CallRPC("node1", "assetupdate assetupdatename jagassetupdate assets 5 0 ''"), runtime_error);
 	// if max supply is -1 ensure supply can goto 10b max
 	AssetNew("node1", "assetupdatemaxsupply", "jagassetupdate", "data", "8", "false", "1", "-1");
-	string negonesupply = "-1";
+	CAmount negonesupply = -1;
 	string maxstr = ValueFromAssetAmount(negonesupply, 8, false).write();
 	AssetUpdate("node1", "assetupdatemaxsupply", "pub12", maxstr);
 	// can't go above 10b max
