fa4d9cbd2982c0a9a0fc8b6e8841c48f77459d44
sidhujag <sidhujag@hotmail.com>
2018-04-08 13:22:28 -0700

    compile

--- src/util.cpp
+++ src/util.cpp
@@ -91,6 +91,7 @@
 #include <openssl/crypto.h>
 #include <openssl/rand.h>
 #include <openssl/conf.h>
+#include <thread>
 
 // Work around clang compilation problem in Boost 1.46:
 // /usr/include/boost/program_options/detail/config_file.hpp:163:17: error: call to function 'to_internal' that is neither visible in the template definition nor found by argument-dependent lookup
