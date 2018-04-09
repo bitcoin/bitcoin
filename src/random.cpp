adb143e30f92ee75471576f18b695f9829f97a41
Ben Woosley <ben.woosley@gmail.com>
2018-04-09 01:07:06 -0700

    Make use of cpuid.h bit definitions
    These are defined in cpuid.h and are self-documenting for more expressive code.
    https://clang.llvm.org/doxygen/cpuid_8h_source.html

--- src/random.cpp
+++ src/random.cpp
@@ -76,11 +76,10 @@ static inline int64_t GetPerformanceCounter()
 #if defined(__x86_64__) || defined(__amd64__) || defined(__i386__)
 static std::atomic<bool> hwrand_initialized{false};
 static bool rdrand_supported = false;
-static constexpr uint32_t CPUID_F1_ECX_RDRAND = 0x40000000;
 static void RDRandInit()
 {
     uint32_t eax, ebx, ecx, edx;
-    if (__get_cpuid(1, &eax, &ebx, &ecx, &edx) && (ecx & CPUID_F1_ECX_RDRAND)) {
+    if (__get_cpuid(1, &eax, &ebx, &ecx, &edx) && (ecx & bit_RDRND)) {
         LogPrintf("Using RdRand as an additional entropy source\n");
         rdrand_supported = true;
     }
