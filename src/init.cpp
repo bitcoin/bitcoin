8bfddc4abdf827562269676d9a3e31ec384bc4b2
sidhujag <sidhujag@hotmail.com>
2018-04-10 12:17:57 -0700

    Revert "compile"
    This reverts commit 94db48f8b02578d3ca0355f47fbdfb4ad5605d0d.

--- src/init.cpp
+++ src/init.cpp
@@ -325,8 +325,6 @@ void PrepareShutdown()
 		pcoinsdbview = NULL;
 		delete pblocktree;
 		pblocktree = NULL;
-		delete thread_pool;
-		thread_pool = NULL;
 	}
 #ifdef ENABLE_WALLET
 	if (pwalletMain)
