8cbc5c4be4be22aca228074f087a374a7ec38be8
Ben Woosley <ben.woosley@gmail.com>
2018-04-12 13:47:53 -0700

    Drop dead code CScript::Find
    Last use removed in 922e8e2929a2e78270868385aa46f96002fbcff3 (2012!)

--- src/script/script.h
+++ src/script/script.h
@@ -615,15 +615,6 @@ public:
 
         return nFound;
     }
-    int Find(opcodetype op) const
-    {
-        int nFound = 0;
-        opcodetype opcode;
-        for (const_iterator pc = begin(); pc != end() && GetOp(pc, opcode);)
-            if (opcode == op)
-                ++nFound;
-        return nFound;
-    }
 
     /**
      * Pre-version-0.6, Bitcoin always counted CHECKMULTISIGs
