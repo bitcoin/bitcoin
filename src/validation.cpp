58aea356ad0803b7909ed21f08e10e27eeeaa536
sidhujag <sidhujag@hotmail.com>
2018-04-12 13:28:57 -0700

    compile

--- src/validation.cpp
+++ src/validation.cpp
@@ -2595,8 +2595,8 @@ bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pin
 
 	if (!control.Wait())
 		return state.DoS(100, false);
-	int64_t nTime4 = GetTimeMicros(); nTimeVerify += nTime4 - nTime2;
-	LogPrint("bench", "    - Verify %u txins: %.2fms (%.3fms/txin) [%.2fs]\n", nInputs - 1, 0.001 * (nTime4 - nTime2), nInputs <= 1 ? 0 : 0.001 * (nTime4 - nTime2) / (nInputs - 1), nTimeVerify * 0.000001);
+	int64_t nTime4 = GetTimeMicros(); nTimeVerify += nTime4 - nTime1;
+	LogPrint("bench", "    - Verify %u txins: %.2fms (%.3fms/txin) [%.2fs]\n", nInputs - 1, 0.001 * (nTime4 - nTime1), nInputs <= 1 ? 0 : 0.001 * (nTime4 - nTime1) / (nInputs - 1), nTimeVerify * 0.000001);
 
 	if (fJustCheck)
 		return true;
