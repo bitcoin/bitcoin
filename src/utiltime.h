722946e3f90bfb30afa15f4701069088cc7313c0
Pieter Wuille <pieter.wuille@gmail.com>
2018-04-12 22:31:15 -0700

    Avoid std::locale/imbue in DateTimeStrFormat

--- src/utiltime.h
+++ src/utiltime.h
@@ -31,8 +31,6 @@ void MilliSleep(int64_t n);
  * ISO 8601 formatting is preferred. Use the FormatISO8601{DateTime,Date,Time}
  * helper functions if possible.
  */
-std::string DateTimeStrFormat(const char* pszFormat, int64_t nTime);
-
 std::string FormatISO8601DateTime(int64_t nTime);
 std::string FormatISO8601Date(int64_t nTime);
 std::string FormatISO8601Time(int64_t nTime);
