e480ae9fa8c812177e2240585c535e6da8fe2af1
Pieter Wuille <pieter.wuille@gmail.com>
2018-04-12 18:13:49 -0700

    Merge #12743: Fix csBestBlock/cvBlockChange waiting in rpc/mining
    4a6c0e3dcf Modernize best block mutex/cv/hash variable naming (Pieter Wuille)
    45dd135039 Fix csBestBlock/cvBlockChange waiting in rpc/mining (Pieter Wuille)
    
    Pull request description:
    
      This is an alternative to #11694.
    
      It reintroduces a uint256 variable with the best block hash, protected by csBestBlock, and only updated while holding it.
    
      Also rename the involved variable to modern guidelines, as there are very few uses.
    
    Tree-SHA512: 826a86c7d3cee7fe49f99f4398ae99e81cb0563197eaeba77306a3ca6072b67cdb932bc35720fc0f99c2a57b218efa029d0b8bdfb240591a629b2e90efa3199d

--- src/validation.cpp
+++ src/validation.cpp
@@ -216,8 +216,9 @@ BlockMap& mapBlockIndex = g_chainstate.mapBlockIndex;
 PrevBlockMap& mapPrevBlockIndex = g_chainstate.mapPrevBlockIndex;
 CChain& chainActive = g_chainstate.chainActive;
 CBlockIndex *pindexBestHeader = nullptr;
-CWaitableCriticalSection csBestBlock;
-CConditionVariable cvBlockChange;
+CWaitableCriticalSection g_best_block_mutex;
+CConditionVariable g_best_block_cv;
+uint256 g_best_block;
 int nScriptCheckThreads = 0;
 std::atomic_bool fImporting(false);
 std::atomic_bool fReindex(false);
@@ -2507,7 +2508,11 @@ void static UpdateTip(const CBlockIndex *pindexNew, const CChainParams& chainPar
     // New best block
     mempool.AddTransactionsUpdated(1);
 
-    cvBlockChange.notify_all();
+    {
+        WaitableLock lock(g_best_block_mutex);
+        g_best_block = pindexNew->GetBlockHash();
+        g_best_block_cv.notify_all();
+    }
 
     std::vector<std::string> warningMessages;
     if (!IsInitialBlockDownload())
