3c16e9835bcf0c6668660c6338cba30a563c69fb
Ben Woosley <ben.woosley@gmail.com>
2018-04-13 04:07:24 -0700

    Tidy up DoS to return false always, and not have unnecessary default args
    And removed Invalid, unused since 2 commits back
    
    This is setting the stage for the next change...

--- src/consensus/validation.h
+++ src/consensus/validation.h
@@ -37,21 +37,16 @@ private:
     std::string strDebugMessage;
 public:
     CValidationState() : mode(MODE_VALID), nDoS(0), chRejectCode(0), corruptionPossible(false) {}
-    bool DoS(int level, bool ret = false, unsigned int chRejectCodeIn=0, const std::string &strRejectReasonIn="",
-             const std::string &strDebugMessageIn="") {
-        chRejectCode = chRejectCodeIn;
-        strRejectReason = strRejectReasonIn;
-        strDebugMessage = strDebugMessageIn;
-        if (mode == MODE_ERROR)
-            return ret;
-        nDoS += level;
-        mode = MODE_INVALID;
-        return ret;
-    }
-    bool Invalid(bool ret = false,
-                 unsigned int _chRejectCode=0, const std::string &_strRejectReason="",
-                 const std::string &_strDebugMessage="") {
-        return DoS(0, ret, _chRejectCode, _strRejectReason, _strDebugMessage);
+    bool DoS(int level, bool ret, unsigned int code, const std::string& reason, const std::string& debug_message = "")
+    {
+        chRejectCode = code;
+        strRejectReason = reason;
+        strDebugMessage = debug_message;
+        if (mode != MODE_ERROR) {
+            nDoS += level;
+            mode = MODE_INVALID;
+        }
+        return false;
     }
     bool Error(const std::string& strRejectReasonIn) {
         if (mode == MODE_VALID)
