8a93d6c1f8040a8068630d9d54c3c8bf0ac15a20
sidhujag <sidhujag@hotmail.com>
2018-04-15 10:35:16 -0700

    compile

--- src/test/syscoin_asset_tests.cpp
+++ src/test/syscoin_asset_tests.cpp
@@ -298,7 +298,7 @@ BOOST_AUTO_TEST_CASE(generate_big_assetdata)
 	BOOST_CHECK_NO_THROW(r = CallRPC("node1", "assetinfo " + guid + " false"));
 	BOOST_CHECK(find_value(r.get_obj(), "_id").get_str() == guid);
 	BOOST_CHECK(find_value(r.get_obj(), "symbol").get_str() == "NEWASSET");
-	BOOST_CHECK_NO_THROW(r = CallRPC(otherNode1, "assetinfo " + guid1 + " false"));
+	BOOST_CHECK_NO_THROW(r = CallRPC("node1", "assetinfo " + guid1 + " false"));
 	BOOST_CHECK(find_value(r.get_obj(), "_id").get_str() == guid1);
 	BOOST_CHECK(find_value(r.get_obj(), "symbol").get_str() == "NEWASSET");
 }
