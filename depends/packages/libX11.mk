fa461f49c9147037fa63a141a5390f614ad2438e
MarcoFalke <falke.marco@gmail.com>
2018-04-15 14:00:46 -0400

    depends: Fix libX11 build on gcc 8

--- depends/packages/libX11.mk
+++ depends/packages/libX11.mk
@@ -4,6 +4,11 @@ $(package)_download_path=http://xorg.freedesktop.org/releases/individual/lib/
 $(package)_file_name=$(package)-$($(package)_version).tar.bz2
 $(package)_sha256_hash=2aa027e837231d2eeea90f3a4afe19948a6eb4c8b2bec0241eba7dbc8106bd16
 $(package)_dependencies=libxcb xtrans xextproto xproto
+$(package)_patches=a9dafdd57c71473fa3a2ec4887e973e4e9876d83.patch
+
+define $(package)_preprocess_cmds
+  patch -p1 < $($(package)_patch_dir)/a9dafdd57c71473fa3a2ec4887e973e4e9876d83.patch
+endef
 
 define $(package)_set_vars
 $(package)_config_opts=--disable-xkb --disable-static
