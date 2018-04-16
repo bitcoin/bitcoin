8ecc7eb2946d395a2bd484e1e3cd25bfa6bda536
John Newbery <john@johnnewbery.com>
2018-04-16 13:53:35 -0400

    [tests] Set -deprecatedrpc=accounts in tests
    Future commits will deprecate the accounts RPC methods, arguments and
    return objects. Set the -deprecatedrpc=accounts switch now so tests
    don't break in intermediate commits.

--- test/functional/wallet_import_rescan.py
+++ test/functional/wallet_import_rescan.py
@@ -119,7 +119,7 @@ class ImportRescanTest(BitcoinTestFramework):
         self.num_nodes = 2 + len(IMPORT_NODES)
 
     def setup_network(self):
-        extra_args = [["-addresstype=legacy"] for _ in range(self.num_nodes)]
+        extra_args = [["-addresstype=legacy", '-deprecatedrpc=accounts'] for _ in range(self.num_nodes)]
         for i, import_node in enumerate(IMPORT_NODES, 2):
             if import_node.prune:
                 extra_args[i] += ["-prune=1"]
