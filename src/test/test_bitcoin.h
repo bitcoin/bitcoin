11ca4f369956058c92216f861dd63670356ec62a
Jesse Cohen <jc@jc.lol>
2018-04-18 08:01:48 -0400

    Add unit tests for signals generated by ProcessNewBlock()
    After a recent bug discovered in callback ordering in MainSignals,
    this test checks invariants in ordering of
    BlockConnected / BlockDisconnected / UpdatedChainTip signals

--- src/test/test_bitcoin.h
+++ src/test/test_bitcoin.h
@@ -120,4 +120,7 @@ struct TestMemPoolEntryHelper
 
 CBlock getBlock13b8a();
 
+// define an implicit conversion here so that uint256 may be used directly in BOOST_CHECK_*
+std::ostream& operator<<(std::ostream& os, const uint256& num);
+
 #endif
