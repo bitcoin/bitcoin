5d12d0e7225e417fa6b58f3b1d4785635712cdac
Ben Woosley <ben.woosley@gmail.com>
2018-04-18 09:50:04 -0400

    Consistently use FormatStateMessage
    To output CValidationState info.
    
    This includes the reject reason and code as well as debug info.

--- src/validation.cpp
+++ src/validation.cpp
@@ -3489,7 +3489,7 @@ bool ProcessNewBlock(const CChainParams& chainparams, const std::shared_ptr<cons
         }
         if (!ret) {
             GetMainSignals().BlockChecked(*pblock, state);
-            return error("%s: AcceptBlock FAILED (%s)", __func__, state.GetDebugMessage());
+            return error("%s: AcceptBlock FAILED (%s)", __func__, FormatStateMessage(state));
         }
     }
 
