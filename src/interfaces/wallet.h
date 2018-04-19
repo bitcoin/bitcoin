9910a1269beee7d965b2a1a9b46c49222b0d2958
MarcoFalke <falke.marco@gmail.com>
2018-04-19 14:32:57 -0400

    Merge #13026: Fix include comment in src/interfaces/wallet.h
    defffb3587 trivial: Improve include comment in src/interfaces/wallet.h (João Barbosa)
    
    Pull request description:
    
    Tree-SHA512: dfb873db4be0bdaff99ee57628a64284de5ad484b4020e386ab2f5e0b478ff11d49b094d02e3e58cd2bec1d44b3b550b9c5f3da177518087cc62b4a3c9ca824e

--- src/interfaces/wallet.h
+++ src/interfaces/wallet.h
@@ -7,7 +7,7 @@
 
 #include <amount.h>                    // For CAmount
 #include <fs.h>                        // For fs::path
-#include <pubkey.h>                    // For CTxDestination (CKeyID and CScriptID)
+#include <pubkey.h>                    // For CKeyID and CScriptID (definitions needed in CTxDestination instantiation)
 #include <script/ismine.h>             // For isminefilter, isminetype
 #include <script/standard.h>           // For CTxDestination
 #include <support/allocators/secure.h> // For SecureString
