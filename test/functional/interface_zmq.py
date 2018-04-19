fa811b0b1df1e884833a0d4d849b241ca0f449aa
MarcoFalke <falke.marco@gmail.com>
2018-04-19 08:38:59 -0400

    qa: Normalize executable location

--- test/functional/interface_zmq.py
+++ test/functional/interface_zmq.py
@@ -4,7 +4,6 @@
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 """Test the ZMQ notification interface."""
 import configparser
-import os
 import struct
 
 from test_framework.test_framework import BitcoinTestFramework, SkipTest
@@ -47,8 +46,6 @@ class ZMQTest (BitcoinTestFramework):
 
         # Check that bitcoin has been built with ZMQ enabled.
         config = configparser.ConfigParser()
-        if not self.options.configfile:
-            self.options.configfile = os.path.abspath(os.path.join(os.path.dirname(__file__), "../config.ini"))
         config.read_file(open(self.options.configfile))
 
         if not config["components"].getboolean("ENABLE_ZMQ"):
