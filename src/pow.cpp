af7bb6602d6e71ba80d80deb8b3e48ffe8a92544
sidhujag <sidhujag@hotmail.com>
2018-04-19 12:05:48 -0700

    compile

--- src/pow.cpp
+++ src/pow.cpp
@@ -84,11 +84,11 @@ unsigned int CalculateNextWorkRequired(const CBlockIndex* pindexLast, int64_t nF
 
 bool CheckProofOfWork(uint256 hash, unsigned int nBits, const Consensus::Params& params)
 {
-	static unsigned int regTestBits = UintToArith256(Params(CBaseChainParams::REGTEST).GetConsensus().powLimit).GetCompact();
+	//static unsigned int regTestBits = UintToArith256(Params(CBaseChainParams::REGTEST).GetConsensus().powLimit.GetCompact();
     bool fNegative;
     bool fOverflow;
     arith_uint256 bnTarget;
-	if (ChainNameFromCommandLine() != CBaseChainParams::REGTEST && nBits == regTestBits)
+	if (Params().NetworkIDString() != CBaseChainParams::REGTEST)
 		return true;
     bnTarget.SetCompact(nBits, &fNegative, &fOverflow);
 	arith_uint256 nProofOfWorkLimit = UintToArith256(params.powLimit);
