032dca5983208960f9a69fe3ccf97a02656ad37b
Wladimir J. van der Laan <laanwj@gmail.com>
2018-04-23 21:08:42 +0200

    Merge #12885: Reduce implementation code inside CScript
    54a5a21 [MOVEONLY] Turn CScript::GetOp2 into a function and move to cpp (Pieter Wuille)
    6a7456a [MOVEONLY] Move CSCript::FindAndDelete to interpreter (Pieter Wuille)
    33a8ecf Delete unused non-const-iterator CSCript::GetOp overloads (Pieter Wuille)
    2fb168b Make iterators in CScript::FindAndDelete const (Pieter Wuille)
    
    Pull request description:
    
      This PR moves `FindAndDelete` and `GetOp2` out of CScript (the first is only used inside the interpreter and moved there, the second does not actually depend on any script specifics and works on any vector). Furthermore, all non-const-iterator versions of GetOp are replaced by const ones, removing a number of methods in the process.
    
      The longer term goal here is making the script interpreter independent from the CScript representation.
    
      Note for reviewers: both `FindAndDelete` and `GetScriptOp` are consensus critical.
    
    Tree-SHA512: c4ccf91c0b33c37cff0d474aa8dd2dab25b5b7655e2ed69a9b15e29daf0a67b21d51c23e1defb3a72ec762bd6138de96f69c6db1fb9c1fe1e976e421261aedb7
    
    Ensure change is move only
    
    Co-authored-by: UdjinM6 <UdjinM6@users.noreply.github.com>
    
    fix assign_to
    
    Signed-off-by: pasta <pasta@dashboost.org>

--- src/script/interpreter.h
+++ src/script/interpreter.h
@@ -165,4 +165,6 @@ public:
 bool EvalScript(std::vector<std::vector<unsigned char> >& stack, const CScript& script, unsigned int flags, const BaseSignatureChecker& checker, SigVersion sigversion, ScriptError* error = nullptr);
 bool VerifyScript(const CScript& scriptSig, const CScript& scriptPubKey, unsigned int flags, const BaseSignatureChecker& checker, ScriptError* error = nullptr);
 
+int FindAndDelete(CScript& script, const CScript& b);
+
 #endif // BITCOIN_SCRIPT_INTERPRETER_H
