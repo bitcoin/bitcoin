a170f7bf40fd442855d72ff4c5f4be0112534a8d
Volodymyr Shamray <shamray@outlook.com>
2018-04-27 18:42:55 +0300

    Revert C++11-related changes

--- src/rpcmasternode-budget.cpp
+++ src/rpcmasternode-budget.cpp
@@ -734,14 +734,14 @@ Value mnfinalbudget(const Array& params, bool fHelp)
         if(pfinalBudget == NULL) return "Unknown budget hash";
 
         const std::map<uint256, CFinalizedBudgetVote>& fbVotes = pfinalBudget->GetVotes();
-        for (const std::pair<uint256, CFinalizedBudgetVote>& votePair: fbVotes)
+        for (std::map<uint256, CFinalizedBudgetVote>::const_iterator i = fbVotes.begin(); i != fbVotes.end(); ++i)
         {
             Object bObj;
-            bObj.push_back(Pair("nHash", votePair.first.ToString().c_str()));
-            bObj.push_back(Pair("nTime", static_cast<int64_t>(votePair.second.nTime)));
-            bObj.push_back(Pair("fValid", votePair.second.fValid));
+            bObj.push_back(Pair("nHash", i->first.ToString().c_str()));
+            bObj.push_back(Pair("nTime", static_cast<int64_t>(i->second.nTime)));
+            bObj.push_back(Pair("fValid", i->second.fValid));
 
-            obj.push_back(Pair(votePair.second.vin.prevout.ToStringShort(), bObj));
+            obj.push_back(Pair(i->second.vin.prevout.ToStringShort(), bObj));
         }
 
         return obj;
