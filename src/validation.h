6c9cf853b70332207cdea1e10c322219a57a2fca
sidhujag <sidhujag@hotmail.com>
2018-04-29 13:23:58 -0700

    compile

--- src/validation.h
+++ src/validation.h
@@ -169,6 +169,7 @@ extern std::atomic_bool fImporting;
 extern bool fReindex;
 extern int nScriptCheckThreads;
 extern bool fTxIndex;
+extern bool fAddressIndex;
 extern bool fIsBareMultisigStd;
 extern bool fRequireStandard;
 extern unsigned int nBytesPerSigOp;
