112271c7af093ef3dee70945dad0c21a5b700fcd
sidhujag <sidhujag@hotmail.com>
2018-04-29 13:44:37 -0700

    rev up version

--- src/version.h
+++ src/version.h
@@ -11,7 +11,7 @@
  */
 
 
-static const int PROTOCOL_VERSION = 70218;
+static const int PROTOCOL_VERSION = 70219;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -20,7 +20,7 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 70077;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 70218;
+static const int MIN_PEER_PROTO_VERSION = 70219;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
@@ -39,9 +39,9 @@ static const int NO_BLOOM_VERSION = 70201;
 static const int SENDHEADERS_VERSION = 70201;
 
 //! DIP0001 was activated in this version
-static const int DIP0001_PROTOCOL_VERSION = 70218;
+static const int DIP0001_PROTOCOL_VERSION = 70219;
 
 //! short-id-based block download starts with this version
-static const int SHORT_IDS_BLOCKS_VERSION = 70218;
+static const int SHORT_IDS_BLOCKS_VERSION = 70219;
 
 #endif // SYSCOIN_VERSION_H
