c1f4fc18cc0bdb969bf271f6910275b401f95a29
sidhujag <sidhujag@hotmail.com>
2018-04-30 19:44:01 -0700

    def hd to false

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -76,7 +76,7 @@ static const bool DEFAULT_DISABLE_WALLET = false;
 extern const char * DEFAULT_WALLET_DAT;
 
 //! if set, all keys will be derived by using BIP39/BIP44
-static const bool DEFAULT_USE_HD_WALLET = true;
+static const bool DEFAULT_USE_HD_WALLET = false;
 
 bool AutoBackupWallet (CWallet* wallet, const std::string& strWalletFile_, std::string& strBackupWarningRet, std::string& strBackupErrorRet);
 
