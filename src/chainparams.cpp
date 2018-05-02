07031436aca633f9d5a2caa3ce119f5d0941eaa5
Russell Yanofsky <russ@yanofsky.org>
2018-05-02 10:02:48 -0400

    Trivial: s/SetBestChain/ChainStateFlushed in comments after #13106

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -191,7 +191,7 @@ public:
        chainTxData = ChainTxData{
             1490629503  /*1518000378*/, // * UNIX timestamp of last checkpoint block
             1652928,    // * total number of transactions between genesis and last checkpoint
-                        //   (the tx=... number in the SetBestChain debug.log lines)
+                        //   (the tx=... number in the ChainStateFlushed debug.log lines)
             0.0142373   // * estimated number of transactions per second after that timestamp
         };
 
