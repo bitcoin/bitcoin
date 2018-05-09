6cc7d935b56c03279483af767fe7c0e7a4cc8915
Grady Laksmono <grady@laksmono.com>
2018-05-09 17:22:43 +0700

    Refactoring platform-specific code in util.h/util.cpp

--- src/util/platform_windows.cpp
+++ src/util/platform_windows.cpp
@@ -3,4 +3,27 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <platform_windows.h>
+#include <platform_util.h>
+
+bool RenameOver(fs::path src, fs::path dest)
+{
+  return MoveFileExA(src.string().c_str(), dest.string().c_str(),
+                     MOVEFILE_REPLACE_EXISTING) != 0;
+}
+
+bool FileCommit(FILE *file)
+{
+    //-- This is now repeated in platform_posix.cpp
+    if (fflush(file) != 0) { // harmless if redundantly called
+        LogPrintf("%s: fflush failed: %d\n", __func__, errno);
+        return false;
+    }
+    //--
+
+    HANDLE hFile = (HANDLE)_get_osfhandle(_fileno(file));
+    if (FlushFileBuffers(hFile) == 0) {
+        LogPrintf("%s: FlushFileBuffers failed: %d\n", __func__, GetLastError());
+        return false;
+    }
+    return true;
+}
