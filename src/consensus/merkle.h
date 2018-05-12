3a20ebf8d9e684e5bd61043885750c5963ddbc59
Pieter Wuille <pieter.wuille@gmail.com>
2018-05-12 11:48:41 -0700

    [MOVEONLY] Move unused Merkle branch code to tests

--- src/consensus/merkle.h
+++ src/consensus/merkle.h
@@ -12,9 +12,7 @@
 #include <primitives/block.h>
 #include <uint256.h>
 
-uint256 ComputeMerkleRoot(std::vector<uint256> hashes, bool* mutated);
-std::vector<uint256> ComputeMerkleBranch(const std::vector<uint256>& leaves, uint32_t position);
-uint256 ComputeMerkleRootFromBranch(const uint256& leaf, const std::vector<uint256>& branch, uint32_t position);
+uint256 ComputeMerkleRoot(std::vector<uint256> hashes, bool* mutated = nullptr);
 
 /*
  * Compute the Merkle root of the transactions in a block.
@@ -27,12 +25,5 @@ uint256 BlockMerkleRoot(const CBlock& block, bool* mutated = nullptr);
  * *mutated is set to true if a duplicated subtree was found.
  */
 uint256 BlockWitnessMerkleRoot(const CBlock& block, bool* mutated = nullptr);
-
-/*
- * Compute the Merkle branch for the tree of transactions in a block, for a
- * given position.
- * This can be verified using ComputeMerkleRootFromBranch.
- */
-std::vector<uint256> BlockMerkleBranch(const CBlock& block, uint32_t position);
 
 #endif // BITCOIN_CONSENSUS_MERKLE_H
