340d6dbee746601f5a8456b006d6c5ae8014d036
Michael Polzer <>
2018-05-13 00:42:14 +0300

    missing branding

--- src/masternode-payments.cpp
+++ src/masternode-payments.cpp
@@ -550,7 +550,7 @@ bool CMasternodeBlockPayees::IsTransactionValid(const CTransactionRef& txNew) co
         }
     }
 
-    LogPrintf("CMasternodeBlockPayees::IsTransactionValid -- ERROR: Missing required payment, possible payees: '%s', amount: %f DASH\n", strPayeesPossible, (float)nMasternodePayment/COIN);
+    LogPrintf("CMasternodeBlockPayees::IsTransactionValid -- ERROR: Missing required payment, possible payees: '%s', amount: %f CHC\n", strPayeesPossible, (float)nMasternodePayment/COIN);
     return false;
 }
 
