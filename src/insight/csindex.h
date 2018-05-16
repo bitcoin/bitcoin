d3c46fa905267746cb36d0c9f56f6fa01088a669
practicalswift <practicalswift@users.noreply.github.com>
2018-05-16 09:18:16 +0200

    travis: Build with --enable-debug (x86_64-unknown-linux-gnu)

---
+++ src/insight/csindex.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2018 The Particl Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef PARTICL_INSIGHT_CSINDEX_H
+#define PARTICL_INSIGHT_CSINDEX_H
+
+
+
+#endif // PARTICL_INSIGHT_CSINDEX_H
