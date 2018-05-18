d63f2a0d4ca516b898cd0cfbfb7e72cd6e25614e
root <root@phuong.aithercoin.com>
2018-05-18 04:02:23 +0000

    build helper

---
+++ contrib/build-sh/build_unix.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+echo -ne '\n' | sudo add-apt-repository ppa:bitcoin/bitcoin
+sudo apt-get update -y
+sudo apt-get install -y build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev libboost-all-dev software-properties-common libdb4.8-dev libdb4.8++-dev libminiupnpc-dev libzmq3-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
+cd ../..
+echo '#################################################'
+echo '# Building DB 4.8.30 NC                         #'
+echo '#################################################'
+AIT_ROOT=$(pwd)
+BDB_PREFIX="${AIT_ROOT}/db4"
+mkdir -p $BDB_PREFIX
+wget 'http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz'
+echo '12edc0df75bf9abd7f82f821795bcee50f42cb2e5f76a6a281b85732798364ef  db-4.8.30.NC.tar.gz' | sha256sum -c
+tar -xzvf db-4.8.30.NC.tar.gz
+cd db-4.8.30.NC/build_unix/
+../dist/configure --enable-cxx --disable-shared --with-pic --prefix=$BDB_PREFIX
+make install
+echo '#################################################'
+echo '# Building Aither Core                          #'
+echo '#################################################'
+cd $AIT_ROOT
+./autogen.sh
+./configure LDFLAGS="-L${BDB_PREFIX}/lib/" CPPFLAGS="-I${BDB_PREFIX}/include/"
+make
+mkdir -p release
+cp src/aitherd $AIT_ROOT/release
+cp src/aither-tx $AIT_ROOT/release
+cp src/aither-cli $AIT_ROOT/release
+cp src/qt/aither-qt $AIT_ROOT/release
---
+++ contrib/build-sh/build_windows_x64.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+echo '#################################################'
+echo '# Building Aither Core                          #'
+echo '#################################################'
+cd ../..
+AIT_ROOT = $(pwd)
+sudo apt-get install g++-mingw-w64-i686 mingw-w64-i686-dev g++-mingw-w64-x86-64 mingw-w64-x86-64-dev
+cd depends
+make HOST=x86_64-w64-mingw32 -j4
+cd ..
+./autogen.sh
+./configure --prefix=`pwd`/depends/x86_64-w64-mingw32
+make
+cp $AIT_ROOT/src/aitherd.exe $AIT_ROOT/release
+cp $AIT_ROOT/src/aither-cli.exe $AIT_ROOT/release
+cp $AIT_ROOT/src/aither-tx.exe $AIT_ROOT/release
+cp $AIT_ROOT/src/qt/aither-qt.exe $AIT_ROOT/release
