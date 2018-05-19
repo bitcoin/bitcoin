45771668301594c1836314fb7065d0ece3a317c9
GreatSock <greatsock@protonmail.com>
2018-05-19 14:37:53 +0200

    [qt] Removed pay only required fee checkbox Removes the pay only required fee checkbox from the custom transaction fee section in the SendCoinsDialog. It's replaced by a label giving a more general warning about low fees. The custom fee input box now has a minimum value equal to the minimum required fee. Before a value below the minimum fee could be entered which was confusing since the minimum fee would still be paid even though a lower amount was entered.

--- src/qt/sendcoinsdialog.h
+++ src/qt/sendcoinsdialog.h
@@ -92,9 +92,7 @@ private Q_SLOTS:
     void coinControlClipboardBytes();
     void coinControlClipboardLowOutput();
     void coinControlClipboardChange();
-    void setMinimumFee();
     void updateFeeSectionControls();
-    void updateMinFeeLabel();
     void updateSmartFeeLabel();
 
 Q_SIGNALS:
