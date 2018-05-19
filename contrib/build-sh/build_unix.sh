899cf0e5af524f12054c89577e63b4e3f9a04dcf
Harry McKinnon <38744799+harrymckinnon86@users.noreply.github.com>
2018-05-19 18:35:13 +0700

    Update build_unix.sh

--- contrib/build-sh/build_unix.sh
+++ contrib/build-sh/build_unix.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 echo -ne '\n' | sudo add-apt-repository ppa:bitcoin/bitcoin
 sudo apt-get update -y
-sudo apt-get install -y build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev libboost-all-dev software-properties-common libdb4.8-dev libdb4.8++-dev libminiupnpc-dev libzmq3-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler autoconf
+sudo apt-get install -y build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils autoconf
+sudo apt-get install -y libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev
+sudo apt-get install -y libboost-all-dev
+sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
+sudo apt-get install -y libminiupnpc-dev
+sudo apt-get install -y libzmq3-dev
+sudo apt-get install -y libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
+sudo apt-get install -y libqrencode-dev
 cd ../..
 echo '#################################################'
 echo '# Building DB 4.8.30 NC                         #'
