fe5f0377506e6d022e1929e29d27bbe96da4aab9
Volodymyr Shamray <shamray@outlook.com>
2018-05-23 11:16:09 +0300

    Fix typo

--- src/masternode-payments.cpp
+++ src/masternode-payments.cpp
@@ -203,7 +203,7 @@ bool IsBlockValueValid(const CBlock& block, int64_t nExpectedValue){
 
     if(!masternodeSync.IsSynced()) { //there is no budget data to use to check anything
         //super blocks will always be on these blocks, max 100 per budgeting
-        if(nHeight % GetBudgetPaymentCycleBlocks() < 0){
+        if(nHeight % GetBudgetPaymentCycleBlocks() < 100){
             return true;
         } else {
             if(block.vtx[0].GetValueOut() > nExpectedValue) return false;
