d16b1b9fc025c77c72de8fa29e85df85cec542a8
practicalswift <practicalswift@users.noreply.github.com>
2018-05-23 14:14:58 +0200

    Do not use uppercase characters in source code filenames

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -568,7 +568,7 @@ static void FindNextBlocksToDownload(NodeId nodeid, unsigned int count, std::vec
 } // namespace
 
 // This function is used for testing the stale tip eviction logic, see
-// DoS_tests.cpp
+// denialofservice_tests.cpp
 void UpdateLastBlockAnnounceTime(NodeId node, int64_t time_in_seconds)
 {
     LOCK(cs_main);
