ae7cb12c52e2f7f5f8b8fadff5557093f26860fb
microsatoshi <contact@microbitcoin.org>
2018-05-25 03:12:23 +0300

    Updated hardfork height

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -90,7 +90,7 @@ public:
         consensus.BIP65Height = 388381; // 000000000000000004c2b624ed5d7756c508d90fd0da2c7c679febfa6c4735f0
         consensus.BIP66Height = 363725; // 00000000000000000379eaa19dce8c9b722d46ae6a57c2f1a988119488b50931
         consensus.MBCHeight = 525000;
-        consensus.premineBlocks = 100;
+        consensus.premineBlocks = 21000;
         consensus.premineAddress = "";
         consensus.MBCPowLimitWindow = 800;
         consensus.powLimit = uint256S("00000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
