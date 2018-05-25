f44a0ebfff2e11c922bba577d64f115c566131c4
Jeff Frontz <jeff.frontz@gmail.com>
2018-05-25 15:29:30 -0400

    Ensure gitian-build.sh uses bash
    If the user has some other login shell (e.g., ksh), the bashisms in gitian-build.sh don't work so well.

--- contrib/gitian-build.sh
+++ contrib/gitian-build.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Copyright (c) 2016 The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
