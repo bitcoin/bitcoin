3e79a19327630d653668b76473c46a458b60ce5b
tecnovert <tecnovert@particl.io>
2018-05-29 19:38:01 +0200

    lint fixes.

--- test/lint/lint-shell.sh
+++ test/lint/lint-shell.sh
@@ -23,5 +23,10 @@
 # SC2166: Prefer [ p ] && [ q ] as [ p -a q ] is not well defined.
 # SC2166: Prefer [ p ] || [ q ] as [ p -o q ] is not well defined.
 # SC2181: Check exit code directly with e.g. 'if mycmd;', not indirectly with $?.
-shellcheck -e SC2001,SC2004,SC2005,SC2006,SC2016,SC2028,SC2046,SC2048,SC2066,SC2086,SC2116,SC2148,SC2162,SC2166,SC2181 \
+
+# SC2164: Use 'cd ... || exit' or 'cd ... || return' in case cd fails
+# SC2207: Prefer mapfile or read -a to split command output (or quote to avoid splitting).
+# SC1117: Backslash is literal in "\n". Prefer explicit escaping: "\\n".
+
+shellcheck -e SC2001,SC2004,SC2005,SC2006,SC2016,SC2028,SC2046,SC2048,SC2066,SC2086,SC2116,SC2148,SC2162,SC2164,SC2166,SC2181,SC2207,SC1117 \
     $(git ls-files -- "*.sh" | grep -vE 'src/(secp256k1|univalue)/')
