16823ccc16e6db0e134b36affa738a844a72418d
Cory Fields <cory-nospam-@coryfields.com>
2018-06-04 14:55:00 -0400

    cli: Ignore libevent warnings

--- src/bitcoin-cli.cpp
+++ src/bitcoin-cli.cpp
@@ -56,6 +56,18 @@ static void SetupCliArgs()
     gArgs.AddArg("-help", "", false, OptionsCategory::HIDDEN);
 }
 
+/** libevent event log callback */
+static void libevent_log_cb(int severity, const char *msg)
+{
+#ifndef EVENT_LOG_ERR // EVENT_LOG_ERR was added in 2.0.19; but before then _EVENT_LOG_ERR existed.
+# define EVENT_LOG_ERR _EVENT_LOG_ERR
+#endif
+    // Ignore everything other than errors
+    if (severity >= EVENT_LOG_ERR) {
+        throw std::runtime_error(strprintf("libevent error: %s", msg));
+    }
+}
+
 //////////////////////////////////////////////////////////////////////////////
 //
 // Start
@@ -506,6 +518,7 @@ int main(int argc, char* argv[])
         fprintf(stderr, "Error: Initializing networking failed\n");
         return EXIT_FAILURE;
     }
+    event_set_log_callback(&libevent_log_cb);
 
     try {
         int ret = AppInitRPC(argc, argv);
