3fe590ad65f48de23279c748cf39d0c34b0cbbca
Jesse Cohen <jc@jc.lol>
2018-06-08 14:54:02 -0400

    Rescope g_enable_bip61 to net_processing

--- src/net_processing.h
+++ src/net_processing.h
@@ -37,8 +37,6 @@ static constexpr int64_t MINIMUM_CONNECT_TIME = 30;
 
 /** Default for BIP61 (sending reject messages) */
 static constexpr bool DEFAULT_ENABLE_BIP61 = true;
-/** Enable BIP61 (sending reject messages) */
-extern bool g_enable_bip61;
 
 class PeerLogicValidation final : public CValidationInterface, public NetEventsInterface {
 private:
