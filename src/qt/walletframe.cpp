9697b6f2c981fd3995aabe366c0ee40932ae8c35
João Barbosa <joao.paulo.barbosa@gmail.com>
2018-06-20 14:15:12 +0100

    bugfix: Delete walletView in WalletFrame::removeWallet

--- src/qt/walletframe.cpp
+++ src/qt/walletframe.cpp
@@ -95,6 +95,7 @@ bool WalletFrame::removeWallet(const QString &name)
 
     WalletView *walletView = mapWalletViews.take(name);
     walletStack->removeWidget(walletView);
+    delete walletView;
     return true;
 }
 
