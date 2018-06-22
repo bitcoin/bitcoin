da74db0940720407fafaf3582bbaf9c81a4d3b4d
Ben Woosley <ben.woosley@gmail.com>
2018-06-22 18:27:18 +0000

    Drop unused GetType() from CSizeComputer

--- src/index/txindex.cpp
+++ src/index/txindex.cpp
@@ -250,7 +250,7 @@ bool TxIndex::WriteBlock(const CBlock& block, const CBlockIndex* pindex)
     vPos.reserve(block.vtx.size());
     for (const auto& tx : block.vtx) {
         vPos.emplace_back(tx->GetHash(), pos);
-        pos.nTxOffset += ::GetSerializeSize(*tx, SER_DISK, CLIENT_VERSION);
+        pos.nTxOffset += ::GetSerializeSize(*tx, CLIENT_VERSION);
     }
     return m_db->WriteTxs(vPos);
 }
