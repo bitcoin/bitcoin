faca0a862558cf17777f36c38452ba770227cbc9
MarcoFalke <falke.marco@gmail.com>
2018-06-24 15:51:54 -0400

    doc: Clarify that mempool txiter is const_iterator

--- src/txmempool.h
+++ src/txmempool.h
@@ -486,7 +486,7 @@ public:
     mutable CCriticalSection cs;
     indexed_transaction_set mapTx GUARDED_BY(cs);
 
-    typedef indexed_transaction_set::nth_index<0>::type::iterator txiter;
+    using txiter = indexed_transaction_set::nth_index<0>::type::const_iterator;
     std::vector<std::pair<uint256, txiter> > vTxHashes; //!< All tx witness hashes/entries in mapTx, in random order
 
     struct CompareIteratorByHash {
