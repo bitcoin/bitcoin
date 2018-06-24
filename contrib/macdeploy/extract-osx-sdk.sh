be4345ce12b7f16fdb00d0dbd72aa190141112b2
Wladimir J. van der Laan <laanwj@gmail.com>
2018-06-24 16:41:58 +0200

    Merge #13510: Scripts and tools: Obsolete #!/bin/bash shebang
    000000035b20402dea3e8168165cd4eefdc97539 Obsolete #!/bin/bash shebang (DesWurstes)
    
    Pull request description:
    
      > `#!/bin/bash` assumes it is always installed to `/bin/` which can cause issues
      > `#!/usr/bin/env bash` searches the user's `PATH` to find the `bash` binary
    
      Details: https://github.com/dylanaraps/pure-bash-bible#obsolete-syntax
    
      I'm open to comments: Should I also fix `#!/bin/sh`?
    
    Tree-SHA512: b47bb4828116aa119f1899c68fee081270d51a898535490b9c616bf0f3660ad953f29c361eafc759bc64cdd54ee6eeecb2d79e9fdb5291a996a515c719805476

--- contrib/macdeploy/extract-osx-sdk.sh
+++ contrib/macdeploy/extract-osx-sdk.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Copyright (c) 2016 The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
