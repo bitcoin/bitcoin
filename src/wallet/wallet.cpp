91fa15aaebdf99fb0d1e2d811f765a5dd3fc3a93
practicalswift <practicalswift@users.noreply.github.com>
2018-06-27 18:06:44 +0200

    wallet: Avoid potential use of unitialized value bnb_used in CWallet::CreateTransaction(...)
    Github-Pull: #13546
    Rebased-From: a23a7f60aa07de52d23ff1f2034fc43926ec3520

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -2846,6 +2846,8 @@ bool CWallet::CreateTransaction(const std::vector<CRecipient>& vecSend, CTransac
                             return false;
                         }
                     }
+                } else {
+                    bnb_used = false;
                 }
 
                 const CAmount nChange = nValueIn - nValueToSelect;
