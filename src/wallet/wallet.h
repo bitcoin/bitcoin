8f544e49f152477ce28ee56f289b535a5fe87799
John Newbery <john@johnnewbery.com>
2018-06-27 13:43:25 -0400

    [wallet] Factor out GetWatchOnlyBalance()

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -1039,7 +1039,6 @@ public:
     CAmount GetBalance(const isminefilter& filter=ISMINE_SPENDABLE) const;
     CAmount GetUnconfirmedBalance() const;
     CAmount GetImmatureBalance() const;
-    CAmount GetWatchOnlyBalance() const;
     CAmount GetUnconfirmedWatchOnlyBalance() const;
     CAmount GetImmatureWatchOnlyBalance() const;
     CAmount GetLegacyBalance(const isminefilter& filter, int minDepth, const std::string* account) const;
