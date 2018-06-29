b700b45330fc1a65684ed78c21c3fa6169b98a71
Daniel Kraft <d@domob.eu>
2018-06-29 16:10:01 +0200

    RPC: Add new getzmqnotifications method.
    This adds a new RPC method "getzmqnotifications", which returns
    information about all active ZMQ notification endpoints.  This is useful
    for software that layers on top of bitcoind, so it can verify that
    ZeroMQ is enabled and also figure out where it should listen.
    
    See https://github.com/bitcoin/bitcoin/issues/13526.

--- src/zmq/zmqnotificationinterface.h
+++ src/zmq/zmqnotificationinterface.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2015-2017 The Bitcoin Core developers
+// Copyright (c) 2015-2018 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -17,6 +17,8 @@ class CZMQNotificationInterface final : public CValidationInterface
 {
 public:
     virtual ~CZMQNotificationInterface();
+
+    std::list<const CZMQAbstractNotifier*> GetActiveNotifiers() const;
 
     static CZMQNotificationInterface* Create();
 
