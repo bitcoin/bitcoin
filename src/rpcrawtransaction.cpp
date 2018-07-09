474dd8c71286ac8ca8f25b994bc08988fa9cbf27
nuggetbram <bulll15@woodleigh.vic.edu.au>
2018-07-09 23:03:28 +1000

    Update rpcrawtransaction.cpp (#24)
    Fixes building in newer linux distros
    Based on: https://github.com/bitcoin/bitcoin/issues/6113

--- src/rpcrawtransaction.cpp
+++ src/rpcrawtransaction.cpp
@@ -238,7 +238,7 @@ Value listunspent(const Array& params, bool fHelp)
             CTxDestination address;
             if (ExtractDestination(pk, address))
             {
-                const CScriptID& hash = boost::get<const CScriptID&>(address);
+                const CScriptID& hash = boost::get<CScriptID>(address);
                 CScript redeemScript;
                 if (pwalletMain->GetCScript(hash, redeemScript))
                     entry.push_back(Pair("redeemScript", HexStr(redeemScript.begin(), redeemScript.end())));
