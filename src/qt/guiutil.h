288c8e58b864c489125cfbcc6ad05afc79662153
fanquake <fanquake@gmail.com>
2018-07-09 09:33:57 +0800

    gui: remove SubstituteFonts

--- src/qt/guiutil.h
+++ src/qt/guiutil.h
@@ -127,9 +127,6 @@ namespace GUIUtil
     // Browse backup folder
     void showBackups();
 
-    // Replace invalid default fonts with known good ones
-    void SubstituteFonts(const QString& language);
-
     /** Qt event filter that intercepts ToolTipChange events, and replaces the tooltip with a rich text
       representation if needed. This assures that Qt can word-wrap long tooltip messages.
       Tooltips longer than the provided size threshold (in characters) are wrapped.
