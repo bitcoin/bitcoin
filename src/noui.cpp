419bdec9415441e7340b780a53d357d3c44fd4bb
Justin Silver <jsilver@doublesharp.com>
2018-07-13 16:38:54 -0700

    update syscoin copyright
    Former-commit-id: 69305dbb18ffa0182750d28de327fa7d3b27c9b0

--- src/noui.cpp
+++ src/noui.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) 2010 Satoshi Nakamoto
 // Copyright (c) 2009-2014 The Bitcoin Core developers
 // Copyright (c) 2014-2017 The Dash Core developers
-// Copyright (c) 2014-2016 The Syscoin Core developers
+// Copyright (c) 2014-2017 The Syscoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
