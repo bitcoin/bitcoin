526b4ede3a49215f2f42eb89b3c9d30c5ece79fb
MarcoFalke <falke.marco@gmail.com>
2018-07-14 13:08:26 -0400

    Merge #13138: [tests] Remove accounts from wallet_importprunedfunds.py
    38040c34e1 [tests] Remove accounts from wallet_importprunedfunds.py (John Newbery)
    
    Pull request description:
    
      This was split from #13075 to not block review/merge of that PR.
    
    Tree-SHA512: 631d7139ed2bda5222ec395cc75720261e2e1f741dba04723d09fe04ef6cf92222a3679d886026ec33e2db2d1e2fa1a0f36c2451581d0f733a9939a98c7118ab

--- test/functional/wallet_importprunedfunds.py
+++ test/functional/wallet_importprunedfunds.py
@@ -15,7 +15,6 @@ class ImportPrunedFundsTest(BitcoinTestFramework):
     def set_test_params(self):
         self.setup_clean_chain = True
         self.num_nodes = 2
-        self.extra_args = [['-deprecatedrpc=accounts']] * 2
 
     def run_test(self):
         self.log.info("Mining blocks...")
