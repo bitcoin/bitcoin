d65ca0cc0f4c4556ad6635d3bb37ffbd38bb6c85
Andrew Chow <achow101-github@achow101.com>
2018-07-14 19:19:44 -0700

    Use 71 byte signature for DUMMY_SIGNATURE_CREATOR
    Changes DUMMY_SIGNATURE_CREATOR to create 71 byte dummy signatures.
    
    Update comments to reflect this change

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -1540,7 +1540,7 @@ int64_t CWalletTx::GetTxTime() const
     return n ? n : nTimeReceived;
 }
 
-// Helper for producing a max-sized low-S signature (eg 72 bytes)
+// Helper for producing a max-sized low-S low-R signature (eg 71 bytes)
 bool CWallet::DummySignInput(CTxIn &tx_in, const CTxOut &txout) const
 {
     // Fill in dummy signatures for fee calculation.
@@ -1556,7 +1556,7 @@ bool CWallet::DummySignInput(CTxIn &tx_in, const CTxOut &txout) const
     return true;
 }
 
-// Helper for producing a bunch of max-sized low-S signatures (eg 72 bytes)
+// Helper for producing a bunch of max-sized low-S low-R signatures (eg 71 bytes)
 bool CWallet::DummySignTx(CMutableTransaction &txNew, const std::vector<CTxOut> &txouts) const
 {
     // Fill in dummy signatures for fee calculation.
