fe77ae46e7b4a6ae331a5fa2ebd93c52ce202e27
TheCharlatan <seb.kung@gmail.com>
2018-07-17 23:54:44 +0200

    Add aarch64 qt depends support for cross compiling bitcoin-qt

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -92,6 +92,7 @@ $(package)_config_opts_linux += -no-opengl
 $(package)_config_opts_arm_linux += -platform linux-g++ -xplatform bitcoin-linux-g++
 $(package)_config_opts_i686_linux  = -xplatform linux-g++-32
 $(package)_config_opts_x86_64_linux = -xplatform linux-g++-64
+$(package)_config_opts_aarch64_linux = -xplatform linux-aarch64-gnu-g++
 $(package)_config_opts_mingw32  = -no-opengl -xplatform win32-g++ -device-option CROSS_COMPILE="$(host)-"
 $(package)_build_env  = QT_RCC_TEST=1
 $(package)_build_env += QT_RCC_SOURCE_DATE_OVERRIDE=1
