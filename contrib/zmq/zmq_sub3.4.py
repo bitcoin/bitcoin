00258e5b930db855095e83b64c7064e23a1425bb
Karl-Johan Alm <karljohan-alm@garage.co.jp>
2018-07-18 15:36:08 +0900

    contrib: correct version check
    [ not(major >= 3 and minor >= 4) ] fails for '4.0'

--- contrib/zmq/zmq_sub3.4.py
+++ contrib/zmq/zmq_sub3.4.py
@@ -34,7 +34,7 @@ import signal
 import struct
 import sys
 
-if not (sys.version_info.major >= 3 and sys.version_info.minor >= 4):
+if (sys.version_info.major, sys.version_info.minor) < (3, 4):
     print("This example only works with Python 3.4 and greater")
     sys.exit(1)
 
