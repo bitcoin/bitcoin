0d1d601254fd2b8ef67a6fde944bc653fa6b6729
Karl-Johan Alm <kalle.alm@gmail.com>
2018-07-19 23:48:38 +0900

    add stdin helpers for password input support
    Former-commit-id: 3ec6c2fbe2dd6e1916001ba29f3d1c11978183ce

---
+++ src/compat/stdin.h
@@ -0,0 +1,18 @@
+// Copyright (c) 2018 The Syscoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef SYSCOIN_COMPAT_STDIN_H
+#define SYSCOIN_COMPAT_STDIN_H
+
+struct NoechoInst {
+    NoechoInst();
+    ~NoechoInst();
+};
+
+#define NO_STDIN_ECHO() NoechoInst _no_echo
+
+bool StdinTerminal();
+bool StdinReady();
+
+#endif // SYSCOIN_COMPAT_STDIN_H
