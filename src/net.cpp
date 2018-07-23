bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

[RPC] Add GetNodeStats() function for individual node.
GetNodeStats() can now be called for a single node.

src/net.cpp
@@ -2521,6 +2521,18 @@ size_t CConnman::GetNodeCount(NumConnections flags)
     return nNum;
 }
 
+bool CConnman::GetNodeStats(NodeId nodeid, CNodeStats& stats)
+{
+    LOCK(cs_vNodes);
+    for(CNode* pnode : vNodes) {
+        if (pnode->id == nodeid) {
+            pnode->copyStats(stats);
+            return true;
+        }
+    }
+    return false;
+}
+
 void CConnman::GetNodeStats(std::vector<CNodeStats>& vstats)
 {
     vstats.clear();

bitcoin/bitcoin

src/net.cpp
@@ -2556,6 +2556,17 @@ CNode* CConnman::GetNodeByID(NodeId id)
     return nullptr;
 }
 
+bool CConnman::GetNodeStats(NodeId id, CNodeStats& stats)
+{
+    LOCK(cs_vNodes);
+    CNode* node = GetNodeByID(id);
+    if (node) {
+        node->copyStats(stats);
+        return true;
+    }
+    return false;
+}
+
 void CConnman::GetNodeStats(std::vector<CNodeStats>& vstats)
 {
     vstats.clear();
@@ -2576,7 +2587,6 @@ bool CConnman::DisconnectNode(const std::string& strNode)
     }
     return false;
 }
-
 bool CConnman::DisconnectNode(NodeId id)
 {
     LOCK(cs_vNodes);