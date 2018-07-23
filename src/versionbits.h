7637a941328d7b6a8f52e23caf94fa855f6a6b98
blondfrogs <jeanderson@MacBook-Pro.local>
2018-07-23 15:17:42 -0600

    Update block version once assets go active

--- src/versionbits.h
+++ src/versionbits.h
@@ -13,6 +13,8 @@
 static const int32_t VERSIONBITS_LAST_OLD_BLOCK_VERSION = 4;
 /** What bits to set in version for versionbits blocks */
 static const int32_t VERSIONBITS_TOP_BITS = 0x20000000UL;
+/** What bits to set in the version for versionbits blocks after assets is active */
+static const int32_t VERSIONBITS_TOP_BITS_ASSETS = 0x30000000UL;
 /** What bitmask determines whether versionbits is in use */
 static const int32_t VERSIONBITS_TOP_MASK = 0xE0000000UL;
 /** Total bits available for versionbits */
