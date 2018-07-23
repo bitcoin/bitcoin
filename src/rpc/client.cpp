bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

[RPC] Allow getpeerinfo to be called for a single node.
The getpeerinfo RPC can now be called for a single node by specifying the
node id.

src/rpc/client.cpp
@@ -122,6 +122,7 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "prioritisetransaction", 2, "fee_delta" },
     { "setban", 2, "bantime" },
     { "setban", 3, "absolute" },
+    { "getpeerinfo", 0, "node" },
     { "setnetworkactive", 0, "state" },
     { "getmempoolancestors", 1, "verbose" },
     { "getmempooldescendants", 1, "verbose" },

bitcoin/bitcoin

src/rpc/client.cpp
@@ -151,6 +151,7 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "prioritisetransaction", 2, "fee_delta" },
     { "setban", 2, "bantime" },
     { "setban", 3, "absolute" },
+    { "getpeerinfo", 0, "id" },
     { "setnetworkactive", 0, "state" },
     { "getmempoolancestors", 1, "verbose" },
     { "getmempooldescendants", 1, "verbose" },