85c4cf2e1e1b841a11f33304525f714a350aba2e
blondfrogs <jeanderson@MacBook-Pro.local>
2018-07-24 10:06:25 -0600

    Update Protocol Version

--- src/version.h
+++ src/version.h
@@ -10,7 +10,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70015;
+static const int PROTOCOL_VERSION = 70016;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
