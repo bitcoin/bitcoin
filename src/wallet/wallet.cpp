d96d510f44abe6d9a7d4caa9efd46d6b152d3feb
practicalswift <practicalswift@users.noreply.github.com>
2018-07-27 00:07:37 +0200

    Fix accidental use of the addition assignment operator ("+="). Remove newlines from error message.

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -4109,7 +4109,7 @@ std::shared_ptr<CWallet> CWallet::CreateWalletFromFile(const std::string& name, 
         // Regenerate the keypool if upgraded to HD
         if (hd_upgrade) {
             if (!walletInstance->TopUpKeyPool()) {
-                InitError(_("Unable to generate keys") += "\n");
+                InitError(_("Unable to generate keys"));
                 return nullptr;
             }
         }
@@ -4137,7 +4137,7 @@ std::shared_ptr<CWallet> CWallet::CreateWalletFromFile(const std::string& name, 
 
         // Top up the keypool
         if (!walletInstance->IsWalletFlagSet(WALLET_FLAG_DISABLE_PRIVATE_KEYS) && !walletInstance->TopUpKeyPool()) {
-            InitError(_("Unable to generate initial keys") += "\n");
+            InitError(_("Unable to generate initial keys"));
             return nullptr;
         }
 
