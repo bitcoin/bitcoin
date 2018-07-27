3fc20632a3ad30809356a58d2cf0ea4a4ad4cec3
DrahtBot <39886733+DrahtBot@users.noreply.github.com>
2018-07-27 06:47:46 -0400

    qt: Set BLOCK_CHAIN_SIZE = 220

--- src/qt/intro.cpp
+++ src/qt/intro.cpp
@@ -23,7 +23,7 @@
 
 static const uint64_t GB_BYTES = 1000000000LL;
 /* Minimum free space (in GB) needed for data directory */
-static const uint64_t BLOCK_CHAIN_SIZE = 200;
+constexpr uint64_t BLOCK_CHAIN_SIZE = 220;
 /* Minimum free space (in GB) needed for data directory when pruned; Does not include prune target */
 static const uint64_t CHAIN_STATE_SIZE = 3;
 /* Total required space (in GB) depending on user choice (prune, not prune) */
