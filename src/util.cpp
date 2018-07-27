4723aed698f08b233705221695ecc96bb5972c35
Chun Kuan Lee <ken2812221@gmail.com>
2018-07-28 00:35:52 +0800

    Use Unicode version of MoveFileEx

--- src/util.cpp
+++ src/util.cpp
@@ -1018,7 +1018,7 @@ void CreatePidFile(const fs::path &path, pid_t pid)
 bool RenameOver(fs::path src, fs::path dest)
 {
 #ifdef WIN32
-    return MoveFileExA(src.string().c_str(), dest.string().c_str(),
+    return MoveFileExW(src.wstring().c_str(), dest.wstring().c_str(),
                        MOVEFILE_REPLACE_EXISTING) != 0;
 #else
     int rc = std::rename(src.u8string().c_str(), dest.u8string().c_str());
