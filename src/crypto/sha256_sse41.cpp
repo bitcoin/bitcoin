7df815c57c006f32ebe7b6ee21f5fdbb0b54e837
Luke Dashjr <luke-jr+git@utopios.org>
2018-07-28 19:32:41 +0000

    crypto/sha256: Use pragmas to enforce necessary intrinsics for GCC and Clang
    This avoids problems when the user specifies CXXFLAGS explicitly disabling the relevant optimisations.

--- src/crypto/sha256_sse41.cpp
+++ src/crypto/sha256_sse41.cpp
@@ -10,6 +10,12 @@
 #include <attributes.h>
 #include <crypto/common.h>
 
+#if defined(__clang__)
+#pragma clang attribute push(__attribute__((__target__("sse4.1"))), apply_to = function)
+#elif defined(__GNUC__)
+#pragma GCC target ("sse4.1")
+#endif
+
 namespace sha256d64_sse41 {
 namespace {
 
@@ -317,5 +323,9 @@ void Transform_4way(unsigned char* out, const unsigned char* in)
 }
 
 }
+
+#if defined(__clang__)
+#pragma clang attribute pop
+#endif
 
 #endif
