c2b66bbd072893673d2c86b07ef8527f7762bcf1
Michael Polzer <>
2018-07-30 18:56:14 +0200

    chainlify

--- contrib/devtools/gen-manpages.sh
+++ contrib/devtools/gen-manpages.sh
@@ -6,10 +6,10 @@ BUILDDIR=${BUILDDIR:-$TOPDIR}
 BINDIR=${BINDIR:-$BUILDDIR/src}
 MANDIR=${MANDIR:-$TOPDIR/doc/man}
 
-BITCOIND=${BITCOIND:-$BINDIR/bitcoind}
-BITCOINCLI=${BITCOINCLI:-$BINDIR/bitcoin-cli}
-BITCOINTX=${BITCOINTX:-$BINDIR/bitcoin-tx}
-BITCOINQT=${BITCOINQT:-$BINDIR/qt/bitcoin-qt}
+BITCOIND=${BITCOIND:-$BINDIR/chaincoind}
+BITCOINCLI=${BITCOINCLI:-$BINDIR/chaincoin-cli}
+BITCOINTX=${BITCOINTX:-$BINDIR/chaincoin-tx}
+BITCOINQT=${BITCOINQT:-$BINDIR/qt/chaincoin-qt}
 
 [ ! -x $BITCOIND ] && echo "$BITCOIND not found or not executable." && exit 1
 
@@ -17,8 +17,8 @@ BITCOINQT=${BITCOINQT:-$BINDIR/qt/bitcoin-qt}
 BTCVER=($($BITCOINCLI --version | head -n1 | awk -F'[ -]' '{ print $6, $7 }'))
 
 # Create a footer file with copyright content.
-# This gets autodetected fine for bitcoind if --version-string is not set,
-# but has different outcomes for bitcoin-qt and bitcoin-cli.
+# This gets autodetected fine for chaincoind if --version-string is not set,
+# but has different outcomes for chaincoin-qt and chaincoin-cli.
 echo "[COPYRIGHT]" > footer.h2m
 $BITCOIND --version | sed -n '1!p' >> footer.h2m
 
