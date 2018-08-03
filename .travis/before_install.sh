86d34f0e657e039843ff264358e77ffb2763803f
Julian Fleischer <tirednesscankill@warhog.net>
2018-08-03 16:07:53 +0200

    abort script in END_FOLD on non-zero exit code

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -18,6 +18,8 @@ BEGIN_FOLD () {
 END_FOLD () {
   RET=$?
   echo "travis_fold:end:${CURRENT_FOLD_NAME}"
-  return $RET
+  if [ $RET != 0 ]; then
+    echo "${CURRENT_FOLD_NAME} failed with status code ${RET}"
+  fi
 }
 
