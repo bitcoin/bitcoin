97db873a01aaccf6fca9d8db4426cfb59572d19d
Chun Kuan Lee <ken2812221@gmail.com>
2018-08-04 16:39:14 +0000

    Make fs::path::string() always return utf-8 string

--- test/lint/lint-includes.sh
+++ test/lint/lint-includes.sh
@@ -54,7 +54,6 @@ EXPECTED_BOOST_INCLUDES=(
     boost/chrono/chrono.hpp
     boost/date_time/posix_time/posix_time.hpp
     boost/filesystem.hpp
-    boost/filesystem/detail/utf8_codecvt_facet.hpp
     boost/filesystem/fstream.hpp
     boost/multi_index/hashed_index.hpp
     boost/multi_index/ordered_index.hpp
