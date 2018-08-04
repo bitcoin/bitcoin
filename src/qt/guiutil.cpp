2c3eade704f63b360926de9e975ce80143781679
Chun Kuan Lee <ken2812221@gmail.com>
2018-08-04 16:39:14 +0000

    Make fs::path::string() always return utf-8 string

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -60,8 +60,6 @@
 #include <QFontDatabase>
 #endif
 
-static fs::detail::utf8_codecvt_facet utf8;
-
 namespace GUIUtil {
 
 QString dateTimeStr(const QDateTime &date)
@@ -764,12 +762,12 @@ void setClipboard(const QString& str)
 
 fs::path qstringToBoostPath(const QString &path)
 {
-    return fs::path(path.toStdString(), utf8);
+    return fs::path(path.toStdString());
 }
 
 QString boostPathToQString(const fs::path &path)
 {
-    return QString::fromStdString(path.string(utf8));
+    return QString::fromStdString(path.string());
 }
 
 QString formatDurationStr(int secs)
