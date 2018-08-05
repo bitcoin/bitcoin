380c843217139b180457889699c65b37ae3b4a87
Chun Kuan Lee <ken2812221@gmail.com>
2018-08-05 16:38:25 +0000

    utils: Convert Windows args to utf-8 string

--- src/bitcoin-cli.cpp
+++ src/bitcoin-cli.cpp
@@ -17,6 +17,7 @@
 
 #include <memory>
 #include <stdio.h>
+#include <tuple>
 
 #include <event2/buffer.h>
 #include <event2/keyvalq_struct.h>
@@ -511,6 +512,10 @@ static int CommandLineRPC(int argc, char *argv[])
 
 int main(int argc, char* argv[])
 {
+#ifdef WIN32
+    util::WinCmdLineArgs winArgs;
+    std::tie(argc, argv) = winArgs.get();
+#endif
     SetupEnvironment();
     if (!SetupNetworking()) {
         fprintf(stderr, "Error: Initializing networking failed\n");
