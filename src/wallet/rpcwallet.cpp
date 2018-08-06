3f4ad16ba2388b3e6209faa4d1a35e6cc7c4a94b
practicalswift <practicalswift@users.noreply.github.com>
2018-08-06 23:52:35 +0200

    Remove redundant redeclaration of rescanblockchain(...) in same scope

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -4786,7 +4786,6 @@ extern UniValue importwallet(const JSONRPCRequest& request);
 extern UniValue importprunedfunds(const JSONRPCRequest& request);
 extern UniValue removeprunedfunds(const JSONRPCRequest& request);
 extern UniValue importmulti(const JSONRPCRequest& request);
-extern UniValue rescanblockchain(const JSONRPCRequest& request);
 
 static const CRPCCommand commands[] =
 { //  category              name                                actor (function)                argNames
