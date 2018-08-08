c4b0d42f4ed6ebe68cffc1b539b20f102e0cc165
Michael Polzer <>
2018-08-08 19:42:58 +0200

    add privatesend to pre-release warning

--- src/warnings.cpp
+++ src/warnings.cpp
@@ -47,8 +47,8 @@ std::string GetWarnings(const std::string& strFor)
     LOCK(cs_warnings);
 
     if (!CLIENT_VERSION_IS_RELEASE) {
-        strStatusBar = "This is a pre-release test build - use at your own risk - do not use for mining or merchant applications";
-        strGUI = _("This is a pre-release test build - use at your own risk - do not use for mining or merchant applications");
+        strStatusBar = "This is a pre-release test build - use at your own risk - do not use for privatesend, mining or merchant applications";
+        strGUI = _("This is a pre-release test build - use at your own risk - do not use for privatesend, mining or merchant applications");
     }
 
     if (gArgs.GetBoolArg("-testsafemode", DEFAULT_TESTSAFEMODE))
