1f30ffeafbcd33cc2317cea71e4325e285b1d0ab
sidhujag <sidhujag@hotmail.com>
2018-08-11 23:26:14 -0700

    fix test
    Former-commit-id: f02e04224954b5b47e830f783b78bc9d400ec7ef

--- src/test/test_syscoin_services.cpp
+++ src/test/test_syscoin_services.cpp
@@ -438,6 +438,7 @@ void GenerateSpendableCoins() {
 	BOOST_CHECK_NO_THROW(r = CallRPC1("node1", "getnewaddress"));
 	string newaddress = r.get_str();
 	newaddress.erase(std::remove(newaddress.begin(), newaddress.end(), '\n'), newaddress.end());
+	BOOST_CHECK_NO_THROW(r = CallRPC("node1", "getinfo"));
 	BOOST_CHECK_NO_THROW(CallRPC1("node1", "sendtoaddress", "\"" + newaddress + "\"," + find_value(r.get_obj(), "balance").write()));
 	GenerateBlocks(10, "node1");
 	BOOST_CHECK_NO_THROW(r = CallRPC1("node2", "getnewaddress"));
