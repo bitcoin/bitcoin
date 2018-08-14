3c2921779643e691611b09d73866dbcb75417513
MarcoFalke <falke.marco@gmail.com>
2018-08-14 19:19:22 -0400

    logging: Add BCLog::DANDELION

--- src/logging.cpp
+++ src/logging.cpp
@@ -119,6 +119,7 @@ const CLogCategoryDesc LogCategories[] =
     {BCLog::COINDB, "coindb"},
     {BCLog::QT, "qt"},
     {BCLog::LEVELDB, "leveldb"},
+    {BCLog::DANDELION, "dandelion"},
     {BCLog::ALL, "1"},
     {BCLog::ALL, "all"},
 };
