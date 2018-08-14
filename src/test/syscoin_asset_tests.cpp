0e37b5b739789c40ee3c3462611cc04f09fbc67f
sidhujag <sidhujag@hotmail.com>
2018-08-13 22:40:53 -0700

    num txs to 500
    Former-commit-id: e3185611c606cf3e7d8f7ad63c0fc1171dc58c0b

--- src/test/syscoin_asset_tests.cpp
+++ src/test/syscoin_asset_tests.cpp
@@ -320,7 +320,7 @@ BOOST_AUTO_TEST_CASE(generate_asset_throughput)
 	receiver.push_back("node3");
 	BOOST_CHECK(receiver.size() == 1);
 
-	int numberOfTransactionToSend = 100;
+	int numberOfTransactionToSend = 500;
 	// create 1000 addresses and assets for each asset	
 	printf("creating sender addresses/assets...\n");
 	for (int i = 0; i < numberOfTransactionToSend; i++) {
