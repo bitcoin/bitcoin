e7d3cb98c7c0492cec9360e57ca06a2366922bf1
sidhujag <sidhujag@hotmail.com>
2018-08-16 11:03:48 -0700

    fix tpstestadd
    Former-commit-id: b6cf0559f0458cb6998abbd18ef88211d3591ce4

--- src/assetallocation.cpp
+++ src/assetallocation.cpp
@@ -827,7 +827,7 @@ UniValue tpstestadd(const JSONRPCRequest& request) {
 	UniValue txs;
 	if(params.size() > 1)
 		txs = params[1].get_array();
-	if (!fTPSTestEnabled) {
+	if (fTPSTestEnabled) {
 		for (unsigned int idx = 0; idx < txs.size(); idx++) {
 			const UniValue& tx = txs[idx];
 			UniValue paramsRawTx(UniValue::VARR);
