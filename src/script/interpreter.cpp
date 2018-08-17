e195ca98ecd4bf5cfd6265bce00083c7ae4a4029
EvgenijM86 <EvgenijM86@yandex.com>
2018-08-17 09:09:02 +0300

    add nTime to signatures

--- src/script/interpreter.cpp
+++ src/script/interpreter.cpp
@@ -1135,6 +1135,8 @@ public:
     void Serialize(S &s) const {
         // Serialize nVersion
         ::Serialize(s, txTo.nVersion);
+        // Serialize nTime
+        ::Serialize(s, txTo.nTime);
         // Serialize vin
         unsigned int nInputs = fAnyoneCanPay ? 1 : txTo.vin.size();
         ::WriteCompactSize(s, nInputs);
@@ -1217,6 +1219,8 @@ uint256 SignatureHash(const CScript& scriptCode, const CTransaction& txTo, unsig
         CHashWriter ss(SER_GETHASH, 0);
         // Version
         ss << txTo.nVersion;
+        // nTime
+        ss << txTo.nTime;
         // Input prevouts/nSequence (none/all, depending on flags)
         ss << hashPrevouts;
         ss << hashSequence;
