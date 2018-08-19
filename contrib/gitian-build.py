a2a316e2659f66d8649e72c19b37230589ae3609
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2018-08-19 09:41:56 +0300

    Fix error message if Docker installed already
    Fix https://github.com/bitcoin/bitcoin/pull/13623#issuecomment-405684241

--- contrib/gitian-build.py
+++ contrib/gitian-build.py
@@ -10,14 +10,14 @@ def setup():
     programs = ['ruby', 'git', 'make', 'wget']
     if args.kvm:
         programs += ['apt-cacher-ng', 'python-vm-builder', 'qemu-kvm', 'qemu-utils']
-    elif args.docker:
+    elif args.docker and not os.path.isfile('/lib/systemd/system/docker.service'):
         dockers = ['docker.io', 'docker-ce']
         for i in dockers:
             return_code = subprocess.call(['sudo', 'apt-get', 'install', '-qq', i])
             if return_code == 0:
                 break
         if return_code != 0:
-            print('Cannot find any way to install docker', file=sys.stderr)
+            print('Cannot find any way to install Docker', file=sys.stderr)
             exit(1)
     else:
         programs += ['apt-cacher-ng', 'lxc', 'debootstrap']
