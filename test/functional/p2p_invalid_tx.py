12a0f1be270fc9e6716e317deb884fcc58a4e15f
MarcoFalke <falke.marco@gmail.com>
2018-08-21 19:23:21 -0400

    qa: Add TestNode::assert_debug_log

--- test/functional/p2p_invalid_tx.py
+++ test/functional/p2p_invalid_tx.py
@@ -136,11 +136,16 @@ class InvalidTxRequestTest(BitcoinTestFramework):
 
         # restart node with sending BIP61 messages disabled, check that it disconnects without sending the reject message
         self.log.info('Test a transaction that is rejected, with BIP61 disabled')
-        self.restart_node(0, ['-enablebip61=0','-persistmempool=0'])
+        self.restart_node(0, ['-enablebip61=0', '-persistmempool=0'])
         self.reconnect_p2p(num_connections=1)
-        node.p2p.send_txs_and_test([tx1], node, success=False, expect_disconnect=True)
+        with node.assert_debug_log(expected_msgs=[
+                "{} from peer=0 was not accepted: mandatory-script-verify-flag-failed (Invalid OP_IF construction) (code 16)".format(tx1.hash),
+                "disconnecting peer=0",
+        ]):
+            node.p2p.send_txs_and_test([tx1], node, success=False, expect_disconnect=True)
         # send_txs_and_test will have waited for disconnect, so we can safely check that no reject has been received
         assert_equal(node.p2p.reject_code_received, None)
+
 
 if __name__ == '__main__':
     InvalidTxRequestTest().main()
