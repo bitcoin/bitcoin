243ee3137a4cb7fb3811e8bb18daefb770fca834
Wladimir J. van der Laan <laanwj@gmail.com>
2018-08-22 09:59:42 +0200

    Merge #13967: [walletdb] don't report minversion wallet entry as unknown
    321159e53e800c1df2d8dfd6ac03374f1829c327 don't report minversion wallet entry as unknown (Gregory Sanders)
    
    Pull request description:
    
      It is known in WalletBatch::LoadWallet
    
    Tree-SHA512: 82f7e12f48ae7d17317074ce5b5e27c70ba8334b04adbf7cc863f8169cc1aa460b9454571e2698aa00059c8c8f669fe19c0d40c4910dcded260ddca6ce78be9d
    
    # Conflicts:
    #	src/wallet/walletdb.cpp

--- src/wallet/walletdb.cpp
+++ src/wallet/walletdb.cpp
@@ -580,7 +580,8 @@ ReadKeyValue(CWallet* pwallet, CDataStream& ssKey, CDataStream& ssValue,
                 return false;
             }
         }
-        else if (strType != "bestblock" && strType != "bestblock_nomerkle"){
+        else if (strType != "bestblock" && strType != "bestblock_nomerkle" &&
+                strType != "minversion"){
             wss.m_unknown_records++;
         }
     } catch (...)
