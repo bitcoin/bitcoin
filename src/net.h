57b0c0a93a243769beb306c89560d1eda61f54bd
Ben Woosley <ben.woosley@gmail.com>
2018-08-23 10:02:49 -0700

    Drop CADDR_TIME_VERSION checks now that MIN_PEER_PROTO_VERSION is greater

--- src/net.h
+++ src/net.h
@@ -247,7 +247,6 @@ public:
     };
 
     // Addrman functions
-    size_t GetAddressCount() const;
     void SetServices(const CService &addr, ServiceFlags nServices);
     void MarkAddressGood(const CAddress& addr);
     void AddNewAddresses(const std::vector<CAddress>& vAddr, const CAddress& addrFrom, int64_t nTimePenalty = 0);
