412162658deb2e3260a187e2eb376cac00c0d5f2
Jim Posen <jim.posen@gmail.com>
2018-08-27 12:28:35 -0700

    index: Fix for indexers skipping genesis block.

--- src/index/txindex.cpp
+++ src/index/txindex.cpp
@@ -245,6 +245,9 @@ bool TxIndex::Init()
 
 bool TxIndex::WriteBlock(const CBlock& block, const CBlockIndex* pindex)
 {
+    // Exclude genesis block transaction because outputs are not spendable.
+    if (pindex->nHeight == 0) return true;
+
     CDiskTxPos pos(pindex->GetBlockPos(), GetSizeOfCompactSize(block.vtx.size()));
     std::vector<std::pair<uint256, CDiskTxPos>> vPos;
     vPos.reserve(block.vtx.size());
