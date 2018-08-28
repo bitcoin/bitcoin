897473a999f52889b6f901accb1a734f9553d108
Volodymyr Shamray <shamray@outlook.com>
2018-08-28 18:11:52 +0300

    Backported FormatStateMessage from Dash

--- src/main.cpp
+++ src/main.cpp
@@ -5692,3 +5692,12 @@ public:
         mapOrphanTransactionsByPrev.clear();
     }
 } instance_of_cmaincleanup;
+
+/** Convert CValidationState to a human-readable message for logging */
+std::string FormatStateMessage(const CValidationState &state)
+{
+    return strprintf("%s%s (code %i)",
+                     state.GetRejectReason(),
+                     state.GetDebugMessage().empty() ? "" : ", "+state.GetDebugMessage(),
+                     state.GetRejectCode());
+}
