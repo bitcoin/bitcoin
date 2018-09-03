marcusfox0/polis

Implement GETMNLISTDIFF and MNLISTDIFF P2P messages
Also add "protx diff" RPC

src/evo/simplifiedmns.cpp
@@ -5,10 +5,12 @@
 #include "simplifiedmns.h"
 #include "specialtx.h"
 #include "deterministicmns.h"
+#include "cbtx.h"
 
 #include "validation.h"
 #include "univalue.h"
 #include "consensus/merkle.h"
+#include "chainparams.h"
 
 CSimplifiedMNListEntry::CSimplifiedMNListEntry(const CDeterministicMN& dmn) :
     proRegTxHash(dmn.proTxHash),
@@ -64,4 +66,95 @@ uint256 CSimplifiedMNList::CalcMerkleRoot(bool *pmutated) const
         leaves.emplace_back(e.CalcHash());
     }
     return ComputeMerkleRoot(leaves, pmutated);
+}
+
+void CSimplifiedMNListDiff::ToJson(UniValue& obj) const
+{
+    obj.setObject();
+
+    obj.push_back(Pair("baseBlockHash", baseBlockHash.ToString()));
+    obj.push_back(Pair("blockHash", blockHash.ToString()));
+
+    UniValue deletedMNsArr(UniValue::VARR);
+    for (const auto& h : deletedMNs) {
+        deletedMNsArr.push_back(h.ToString());
+    }
+    obj.push_back(Pair("deletedMNs", deletedMNsArr));
+
+    UniValue mnListArr(UniValue::VARR);
+    for (const auto& e : mnList) {
+        UniValue eObj;
+        e.ToJson(eObj);
+        mnListArr.push_back(eObj);
+    }
+    obj.push_back(Pair("mnList", mnListArr));
+
+    CCbTx cbTxPayload;
+    if (GetTxPayload(*cbTx, cbTxPayload)) {
+        obj.push_back(Pair("merkleRootMNList", cbTxPayload.merkleRootMNList.ToString()));
+    }
+}
+
+bool BuildSimplifiedMNListDiff(const uint256& baseBlockHash, const uint256& blockHash, CSimplifiedMNListDiff& mnListDiffRet, std::string& errorRet)
+{
+    AssertLockHeld(cs_main);
+    mnListDiffRet = CSimplifiedMNListDiff();
+
+    auto baseBlockIt = mapBlockIndex.find(baseBlockHash);
+    auto blockIt = mapBlockIndex.find(blockHash);
+    if (baseBlockIt == mapBlockIndex.end()) {
+        errorRet = strprintf("block %s not found", baseBlockHash.ToString());
+        return false;
+    }
+    if (blockIt == mapBlockIndex.end()) {
+        errorRet = strprintf("block %s not found", blockHash.ToString());
+        return false;
+    }
+
+    if (!chainActive.Contains(baseBlockIt->second) || !chainActive.Contains(blockIt->second)) {
+        errorRet = strprintf("block %s and %s are not in the same chain", baseBlockHash.ToString(), blockHash.ToString());
+        return false;
+    }
+    if (baseBlockIt->second->nHeight > blockIt->second->nHeight) {
+        errorRet = strprintf("base block %s is higher then block %s", baseBlockHash.ToString(), blockHash.ToString());
+        return false;
+    }
+
+    LOCK(deterministicMNManager->cs);
+
+    auto baseDmnList = deterministicMNManager->GetListAtHeight(baseBlockIt->second->nHeight);
+    auto dmnList = deterministicMNManager->GetListAtHeight(blockIt->second->nHeight);
+    auto dmnDiff = baseDmnList.BuildDiff(dmnList);
+
+    // TODO store coinbase TX in CBlockIndex
+    CBlock block;
+    if (!ReadBlockFromDisk(block, blockIt->second, Params().GetConsensus())) {
+        errorRet = strprintf("failed to read block %s from disk", blockHash.ToString());
+        return false;
+    }
+
+    mnListDiffRet.baseBlockHash = baseBlockHash;
+    mnListDiffRet.blockHash = blockHash;
+    mnListDiffRet.cbTx = block.vtx[0];
+
+    std::vector<uint256> vHashes;
+    std::vector<bool> vMatch(block.vtx.size(), false);
+    for (const auto& tx : block.vtx) {
+        vHashes.emplace_back(tx->GetHash());
+    }
+    vMatch[0] = true; // only coinbase matches
+    mnListDiffRet.cbTxMerkleTree = CPartialMerkleTree(vHashes, vMatch);
+    mnListDiffRet.deletedMNs.assign(dmnDiff.removedMns.begin(), dmnDiff.removedMns.end());
+
+    for (const auto& p : dmnDiff.addedMNs) {
+        mnListDiffRet.mnList.emplace_back(*p.second);
+    }
+    for (const auto& p : dmnDiff.updatedMNs) {
+        const auto& dmn = dmnList.GetMN(p.first);
+        CDeterministicMN newDmn(*dmn);
+        newDmn.state = p.second;
+        mnListDiffRet.mnList.emplace_back(newDmn);
+    }
+
+    return true;
 }

src/protocol.cpp
@@ -69,6 +69,8 @@ const char *MNGOVERNANCESYNC="govsync";
 const char *MNGOVERNANCEOBJECT="govobj";
 const char *MNGOVERNANCEOBJECTVOTE="govobjvote";
 const char *MNVERIFY="mnv";
+const char *GETMNLISTDIFF="getmnlistdiff";
+const char *MNLISTDIFF="mnlistdiff";
 };
 
 static const char* ppszTypeName[] =
@@ -153,6 +155,8 @@ const static std::string allNetMessageTypes[] = {
     NetMsgType::MNGOVERNANCEOBJECT,
     NetMsgType::MNGOVERNANCEOBJECTVOTE,
     NetMsgType::MNVERIFY,
+    NetMsgType::GETMNLISTDIFF,
+    NetMsgType::MNLISTDIFF,
 };
 const static std::vector<std::string> allNetMessageTypesVec(allNetMessageTypes, allNetMessageTypes+ARRAYLEN(allNetMessageTypes));
 

bitcoin/bitcoin

src/evo/simplifiedmns.cpp
@@ -5,10 +5,12 @@
 #include "simplifiedmns.h"
 #include "specialtx.h"
 #include "deterministicmns.h"
+#include "cbtx.h"
 
 #include "validation.h"
 #include "univalue.h"
 #include "consensus/merkle.h"
+#include "chainparams.h"
 
 CSimplifiedMNListEntry::CSimplifiedMNListEntry(const CDeterministicMN& dmn) :
     proRegTxHash(dmn.proTxHash),
@@ -64,4 +66,98 @@ uint256 CSimplifiedMNList::CalcMerkleRoot(bool *pmutated) const
         leaves.emplace_back(e.CalcHash());
     }
     return ComputeMerkleRoot(leaves, pmutated);
+}
+
+void CSimplifiedMNListDiff::ToJson(UniValue& obj) const
+{
+    obj.setObject();
+
+    obj.push_back(Pair("baseBlockHash", baseBlockHash.ToString()));
+    obj.push_back(Pair("blockHash", blockHash.ToString()));
+
+    UniValue deletedMNsArr(UniValue::VARR);
+    for (const auto& h : deletedMNs) {
+        deletedMNsArr.push_back(h.ToString());
+    }
+    obj.push_back(Pair("deletedMNs", deletedMNsArr));
+
+    UniValue mnListArr(UniValue::VARR);
+    for (const auto& e : mnList) {
+        UniValue eObj;
+        e.ToJson(eObj);
+        mnListArr.push_back(eObj);
+    }
+    obj.push_back(Pair("mnList", mnListArr));
+
+    CCbTx cbTxPayload;
+    if (GetTxPayload(*cbTx, cbTxPayload)) {
+        obj.push_back(Pair("merkleRootMNList", cbTxPayload.merkleRootMNList.ToString()));
+    }
+}
+
+bool BuildSimplifiedMNListDiff(const uint256& baseBlockHash, const uint256& blockHash, CSimplifiedMNListDiff& mnListDiffRet, std::string& errorRet)
+{
+    AssertLockHeld(cs_main);
+    mnListDiffRet = CSimplifiedMNListDiff();
+
+    BlockMap::iterator baseBlockIt = mapBlockIndex.begin();
+    if (!baseBlockHash.IsNull()) {
+        baseBlockIt = mapBlockIndex.find(baseBlockHash);
+    }
+    auto blockIt = mapBlockIndex.find(blockHash);
+    if (baseBlockIt == mapBlockIndex.end()) {
+        errorRet = strprintf("block %s not found", baseBlockHash.ToString());
+        return false;
+    }
+    if (blockIt == mapBlockIndex.end()) {
+        errorRet = strprintf("block %s not found", blockHash.ToString());
+        return false;
+    }
+
+    if (!chainActive.Contains(baseBlockIt->second) || !chainActive.Contains(blockIt->second)) {
+        errorRet = strprintf("block %s and %s are not in the same chain", baseBlockHash.ToString(), blockHash.ToString());
+        return false;
+    }
+    if (baseBlockIt->second->nHeight > blockIt->second->nHeight) {
+        errorRet = strprintf("base block %s is higher then block %s", baseBlockHash.ToString(), blockHash.ToString());
+        return false;
+    }
+
+    LOCK(deterministicMNManager->cs);
+
+    auto baseDmnList = deterministicMNManager->GetListForBlock(baseBlockHash);
+    auto dmnList = deterministicMNManager->GetListForBlock(blockHash);
+    auto dmnDiff = baseDmnList.BuildDiff(dmnList);
+
+    // TODO store coinbase TX in CBlockIndex
+    CBlock block;
+    if (!ReadBlockFromDisk(block, blockIt->second, Params().GetConsensus())) {
+        errorRet = strprintf("failed to read block %s from disk", blockHash.ToString());
+        return false;
+    }
+
+    mnListDiffRet.baseBlockHash = baseBlockHash;
+    mnListDiffRet.blockHash = blockHash;
+    mnListDiffRet.cbTx = block.vtx[0];
+
+    std::vector<uint256> vHashes;
+    std::vector<bool> vMatch(block.vtx.size(), false);
+    for (const auto& tx : block.vtx) {
+        vHashes.emplace_back(tx->GetHash());
+    }
+    vMatch[0] = true; // only coinbase matches
+    mnListDiffRet.cbTxMerkleTree = CPartialMerkleTree(vHashes, vMatch);
+    mnListDiffRet.deletedMNs.assign(dmnDiff.removedMns.begin(), dmnDiff.removedMns.end());
+
+    for (const auto& p : dmnDiff.addedMNs) {
+        mnListDiffRet.mnList.emplace_back(*p.second);
+    }
+    for (const auto& p : dmnDiff.updatedMNs) {
+        const auto& dmn = dmnList.GetMN(p.first);
+        CDeterministicMN newDmn(*dmn);
+        newDmn.pdmnState = p.second;
+        mnListDiffRet.mnList.emplace_back(newDmn);
+    }
+
+    return true;
 }

src/protocol.cpp
@@ -69,6 +69,8 @@ const char *MNGOVERNANCESYNC="govsync";
 const char *MNGOVERNANCEOBJECT="govobj";
 const char *MNGOVERNANCEOBJECTVOTE="govobjvote";
 const char *MNVERIFY="mnv";
+const char *GETMNLISTDIFF="getmnlistd";
+const char *MNLISTDIFF="mnlistdiff";
 };
 
 static const char* ppszTypeName[] =
@@ -153,6 +155,8 @@ const static std::string allNetMessageTypes[] = {
     NetMsgType::MNGOVERNANCEOBJECT,
     NetMsgType::MNGOVERNANCEOBJECTVOTE,
     NetMsgType::MNVERIFY,
+    NetMsgType::GETMNLISTDIFF,
+    NetMsgType::MNLISTDIFF,
 };
 const static std::vector<std::string> allNetMessageTypesVec(allNetMessageTypes, allNetMessageTypes+ARRAYLEN(allNetMessageTypes));
 