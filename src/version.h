77621467f2d27492631def5499269f262f78c948
sidhujag <sidhujag@hotmail.com>
2018-09-05 14:29:57 -0700

    Revert "Merge pull request #275 from syscoin/proto-upgrade"
    This reverts commit 6c8e3446deb83cc1a8f4441136d41794c7c43c41 [formerly f7f36fafc60628f8a3d3e72a644c0cdb4e123507], reversing
    changes made to aafe69527eaa19cbe67c5cd510a04f492adf925e [formerly e7bbb3d9d72b92533c1a04e2d77c6f78a4deb7a3].
    
    
    Former-commit-id: 28ccd8e115dd073cec4736d924ec6605ed065fe8

--- src/version.h
+++ src/version.h
@@ -21,7 +21,7 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 70077;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 70223;
+static const int MIN_PEER_PROTO_VERSION = 70221;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
