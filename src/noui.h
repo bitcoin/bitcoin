fd76b6c6cf59f6d99c02e13c0212d92c9f3bf690
MarcoFalke <falke.marco@gmail.com>
2018-09-06 14:49:40 -0400

    noui: Move handlers to header file

--- src/noui.h
+++ src/noui.h
@@ -5,6 +5,16 @@
 #ifndef BITCOIN_NOUI_H
 #define BITCOIN_NOUI_H
 
-extern void noui_connect();
+#include <string>
+
+/** Non-GUI handler, which logs and prints messages. */
+bool noui_ThreadSafeMessageBox(const std::string& message, const std::string& caption, unsigned int style);
+/** Non-GUI handler, which logs and prints questions. */
+bool noui_ThreadSafeQuestion(const std::string& /* ignored interactive message */, const std::string& message, const std::string& caption, unsigned int style);
+/** Non-GUI handler, which only logs a message. */
+void noui_InitMessage(const std::string& message);
+
+/** Connect all bitcoind signal handlers */
+void noui_connect();
 
 #endif // BITCOIN_NOUI_H
