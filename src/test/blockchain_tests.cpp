b3chain/b3chain

Use bracket syntax includes ("#include <foo.h>")

src/test/blockchain_tests.cpp
@@ -1,9 +1,9 @@
 #include <boost/test/unit_test.hpp>
 
-#include "stdlib.h"
+#include <stdlib.h>
 
-#include "rpc/blockchain.h"
-#include "test/test_bitcoin.h"
+#include <rpc/blockchain.h>
+#include <test/test_bitcoin.h>
 
 /* Equality between doubles is imprecise. Comparison should be done
  * with a small threshold of tolerance, rather than exact equality.

bitcoin/bitcoin

src/test/blockchain_tests.cpp
@@ -1,9 +1,9 @@
 #include <boost/test/unit_test.hpp>
 
-#include "stdlib.h"
+#include <stdlib.h>
 
-#include "rpc/blockchain.cpp"
-#include "test/test_chaincoin.h"
+#include <rpc/blockchain.h>
+#include <test/test_chaincoin.h>
 
 /* Equality between doubles is imprecise. Comparison should be done
  * with a small threshold of tolerance, rather than exact equality.