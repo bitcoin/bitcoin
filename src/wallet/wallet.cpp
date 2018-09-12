ab7a46c4cf3acbc7c1426508bb08aa89ad98e441
Michael Polzer <hashrate@pm-tech.at>
2018-09-12 08:45:29 +0200

    [wallet.cpp] replace NULL with nullptr
    came with cebefba0855cee7fbcb9474b34e6779369e8e9ce
    
    a linter would be nice to detect those

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -4007,7 +4007,7 @@ std::shared_ptr<CWallet> CWallet::CreateWalletFromFile(const std::string& name, 
     } else if (wallet_creation_flags & WALLET_FLAG_DISABLE_PRIVATE_KEYS) {
         // Make it impossible to disable private keys after creation
         InitError(strprintf(_("Error loading %s: Private keys can only be disabled during creation"), walletFile));
-        return NULL;
+        return nullptr;
     } else if (walletInstance->IsWalletFlagSet(WALLET_FLAG_DISABLE_PRIVATE_KEYS)) {
         LOCK(walletInstance->cs_KeyStore);
         if (!walletInstance->mapKeys.empty() || !walletInstance->mapCryptedKeys.empty()) {
