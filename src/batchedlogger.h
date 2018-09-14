9d25bb1d8f0d3e5acc10549d9db8fa52f52be5b2
Alexander Block <ablock84@gmail.com>
2018-09-14 13:59:02 +0200

    Add batched logger

---
+++ src/batchedlogger.h
@@ -0,0 +1,28 @@
+// Copyright (c) 2018 The Dash Core developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef DASH_BATCHEDLOGGER_H
+#define DASH_BATCHEDLOGGER_H
+
+#include "tinyformat.h"
+
+class CBatchedLogger
+{
+private:
+    std::string header;
+    std::string msg;
+public:
+    CBatchedLogger(const std::string& _header);
+    virtual ~CBatchedLogger();
+
+    template<typename... Args>
+    void Printf(const std::string& fmt, const Args&... args)
+    {
+        msg += strprintf(fmt, args...);
+    }
+
+    void Flush();
+};
+
+#endif//DASH_BATCHEDLOGGER_H
