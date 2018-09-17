fa84723e73d3d7766e7821881ac96ec203e50efc
MarcoFalke <falke.marco@gmail.com>
2018-09-17 14:33:52 -0400

    amount: Move CAmount CENT to unit test header

--- src/wallet/coinselection.h
+++ src/wallet/coinselection.h
@@ -10,7 +10,7 @@
 #include <random.h>
 
 //! target minimum change amount
-static const CAmount MIN_CHANGE = CENT;
+static constexpr CAmount MIN_CHANGE{COIN / 100};
 //! final minimum change amount after paying for fees
 static const CAmount MIN_FINAL_CHANGE = MIN_CHANGE/2;
 
