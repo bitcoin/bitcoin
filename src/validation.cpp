4b8a3f5d235f40be8102506ab26caad005cc40d6
Suhas Daftuar <sdaftuar@gmail.com>
2018-09-17 16:13:37 -0400

    Fix crash bug with duplicate inputs within a transaction
    Introduced by #9049
    
    Github-Pull: #14247
    
    Tree-SHA512: 2815312a3da8ef4a93dbc26b71d9147e34d1fed794aa7188ec12670579d2e45380212cf1e23526a7b5e339a185a73637fc2f342e0699b687c920244bc2edc124

--- src/validation.cpp
+++ src/validation.cpp
@@ -2854,7 +2854,7 @@ bool CheckBlock(const CBlock& block, CValidationState& state, const Consensus::P
 
     // Check transactions
     for (const auto& tx : block.vtx)
-        if (!CheckTransaction(*tx, state, false))
+        if (!CheckTransaction(*tx, state, true))
             return state.Invalid(false, state.GetRejectCode(), state.GetRejectReason(),
                                  strprintf("Transaction check failed (tx hash %s) %s", tx->GetHash().ToString(), state.GetDebugMessage()));
 
