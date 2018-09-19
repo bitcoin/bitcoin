42d512bd059668992ad93713a8677b2658690e2e
Chun Kuan Lee <ken2812221@gmail.com>
2018-09-19 10:36:26 +0800

    lint: use utf-8 to read python source file

--- test/lint/lint-python-shebang.sh
+++ test/lint/lint-python-shebang.sh
@@ -5,8 +5,8 @@ export LC_ALL=C
 EXIT_CODE=0
 for PYTHON_FILE in $(git ls-files -- "*.py"); do
     if [[ $(head -c 2 "${PYTHON_FILE}") == "#!" &&
-          $(head -n 1 "${PYTHON_FILE}") != "#!/usr/bin/env python3" ]]; then
-        echo "Missing shebang \"#!/usr/bin/env python3\" in ${PYTHON_FILE} (do not use python or python2)"
+          $(head -n 2 "${PYTHON_FILE}") != "#!/usr/bin/env python3"$'\n'"# -*- coding: utf-8 -*-" ]]; then
+        echo "Missing shebang \"#!/usr/bin/env python3\" or \"# -*- coding: utf-8 -*-\" in ${PYTHON_FILE} (do not use python or python2)"
         EXIT_CODE=1
     fi
 done
