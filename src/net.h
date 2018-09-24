48796b33a238090ba5498891cce35c59d122ebd3
Patrick Strateman <patrick.strateman@gmail.com>
2018-09-24 16:36:58 -0400

    Move DisconnectNodes logic to private method.

--- src/net.h
+++ src/net.h
@@ -336,6 +336,7 @@ private:
     void ThreadOpenConnections(std::vector<std::string> connect);
     void ThreadMessageHandler();
     void AcceptConnection(const ListenSocket& hListenSocket);
+    void DisconnectNodes();
     void NotifyNumConnectionsChanged();
     void ThreadSocketHandler();
     void ThreadDNSAddressSeed();
