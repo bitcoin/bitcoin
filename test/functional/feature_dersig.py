590a57fdecc2eb2e8208d82e491d09a1986e4f0e
practicalswift <practicalswift@users.noreply.github.com>
2018-09-24 22:45:58 +0200

    tests: Remove unused testing code

--- test/functional/feature_dersig.py
+++ test/functional/feature_dersig.py
@@ -22,7 +22,6 @@ DERSIG_HEIGHT = 1251
 
 # Reject codes that we might receive in this test
 REJECT_INVALID = 16
-REJECT_OBSOLETE = 17
 REJECT_NONSTANDARD = 64
 
 # A canonical signature consists of:
