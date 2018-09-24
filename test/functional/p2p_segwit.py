1d0ce94a54be17b139a052b8e7b4605fe9ac1ecd
Justin Turner Arthur <justinarthur@gmail.com>
2018-09-23 21:30:22 -0500

    Fix for incorrect version attr set on functional test segwit block.

--- test/functional/p2p_segwit.py
+++ test/functional/p2p_segwit.py
@@ -205,7 +205,7 @@ class SegWitTest(BitcoinTestFramework):
         height = self.nodes[0].getblockcount() + 1
         block_time = self.nodes[0].getblockheader(tip)["mediantime"] + 1
         block = create_block(int(tip, 16), create_coinbase(height), block_time)
-        block.version = version
+        block.nVersion = version
         block.rehash()
         return block
 
