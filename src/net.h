be51d05ff2cce532bba307e2601c3087e1823004
Patrick Strateman <patrick.strateman@gmail.com>
2018-09-24 16:30:53 -0400

    Move NotifyNumConnectionsChanged logic to private method.

--- src/net.h
+++ src/net.h
@@ -336,6 +336,7 @@ private:
     void ThreadOpenConnections(std::vector<std::string> connect);
     void ThreadMessageHandler();
     void AcceptConnection(const ListenSocket& hListenSocket);
+    void NotifyNumConnectionsChanged();
     void ThreadSocketHandler();
     void ThreadDNSAddressSeed();
 
@@ -406,6 +407,7 @@ private:
     std::list<CNode*> vNodesDisconnected;
     mutable CCriticalSection cs_vNodes;
     std::atomic<NodeId> nLastNodeId;
+    unsigned int nPrevNodeCount;
 
     /** Services this instance offers */
     ServiceFlags nLocalServices;
