c6fadc9eee84467e41c8916d86edaa2690d29fdb
DesWurstes <DesWurstes@users.noreply.github.com>
2018-09-26 18:18:23 +0300

    Add byteswap functions

--- src/compat/byteswap.h
+++ src/compat/byteswap.h
@@ -33,21 +33,41 @@
 // Non-Mac OS X / non-Darwin
 
 #if HAVE_DECL_BSWAP_16 == 0
+#if HAVE_DECL___builtin_bswap16
+inline uint16_t bswap_16(uint16_t x)
+{
+    return __builtin_bswap16(x);
+}
+#else
 inline uint16_t bswap_16(uint16_t x)
 {
     return (x >> 8) | (x << 8);
 }
+#endif
 #endif // HAVE_DECL_BSWAP16 == 0
 
 #if HAVE_DECL_BSWAP_32 == 0
+#if HAVE_DECL___builtin_bswap32
+inline uint32_t bswap_32(uin32_t x)
+{
+    return __builtin_bswap32(x);
+}
+#else
 inline uint32_t bswap_32(uint32_t x)
 {
     return (((x & 0xff000000U) >> 24) | ((x & 0x00ff0000U) >>  8) |
             ((x & 0x0000ff00U) <<  8) | ((x & 0x000000ffU) << 24));
 }
+#endif
 #endif // HAVE_DECL_BSWAP32 == 0
 
 #if HAVE_DECL_BSWAP_64 == 0
+#if HAVE_DECL___builtin_bswap64
+inline uint64_t bswap_64(uin64_t x)
+{
+    return __builtin_bswap64(x);
+}
+#else
 inline uint64_t bswap_64(uint64_t x)
 {
      return (((x & 0xff00000000000000ull) >> 56)
@@ -59,6 +79,7 @@ inline uint64_t bswap_64(uint64_t x)
           | ((x & 0x000000000000ff00ull) << 40)
           | ((x & 0x00000000000000ffull) << 56));
 }
+#endif
 #endif // HAVE_DECL_BSWAP64 == 0
 
 #endif // defined(MAC_OSX)
