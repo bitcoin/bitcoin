f57d926f320c34a7679f79e98cf6ce0bf7c9d5a7
João Barbosa <joao.paulo.barbosa@gmail.com>
2018-09-28 12:50:04 +0100

    wallet: Add WalletLocation utility class
    Github-Pull: #14350
    Rebased-From: 01a4c09
    
    Former-commit-id: 74f7fa48d71540458ce01615e7f49545562da305

--- src/wallet/walletutil.h
+++ src/wallet/walletutil.h
@@ -11,4 +11,25 @@
 //! Get the path of the wallet directory.
 fs::path GetWalletDir();
 
+//! The WalletLocation class provides wallet information.
+class WalletLocation final
+{
+    std::string m_name;
+    fs::path m_path;
+
+public:
+    explicit WalletLocation() {}
+    explicit WalletLocation(const std::string& name);
+
+    //! Get wallet name.
+    const std::string& GetName() const { return m_name; }
+
+    //! Get wallet absolute path.
+    const fs::path& GetPath() const { return m_path; }
+
+    //! Return whether the wallet exists.
+    bool Exists() const;
+};
+
 #endif // SYSCOIN_WALLET_WALLETUTIL_H
+
