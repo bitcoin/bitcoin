430fb7182f28e166c5e7508682ebe89e704219e4
João Barbosa <joao.paulo.barbosa@gmail.com>
2018-09-28 12:50:04 +0100

    wallet: Add WalletLocation utility class
    Github-Pull: #14350
    Rebased-From: 01a4c09
    
    Former-commit-id: 9abcf9470e92b8ba715b977cb9e83a15e79586fd

--- src/wallet/walletutil.cpp
+++ src/wallet/walletutil.cpp
@@ -25,3 +25,14 @@ fs::path GetWalletDir()
 
     return path;
 }
+
+WalletLocation::WalletLocation(const std::string& name)
+    : m_name(name)
+    , m_path(fs::absolute(name, GetWalletDir()))
+{
+}
+
+bool WalletLocation::Exists() const
+{
+    return fs::symlink_status(m_path).type() != fs::file_not_found;
+}
