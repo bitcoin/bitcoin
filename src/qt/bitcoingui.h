b9b71019e156aa25dcda60747eaa470c6578f2b7
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2018-10-19 10:39:25 +0300

    Fix start with the `-min` option
    When GUI starts with the `-min` option, the `Minimize to tray instead of
    the taskbar` option works as expected now.

--- src/qt/bitcoingui.h
+++ src/qt/bitcoingui.h
@@ -82,6 +82,11 @@ public:
 #endif // ENABLE_WALLET
     bool enableWallet = false;
 
+    /** Get the tray icon status.
+        Some systems have not "system tray" or "notification area" available.
+    */
+    bool hasTrayIcon() const { return trayIcon; }
+
 protected:
     void changeEvent(QEvent *e);
     void closeEvent(QCloseEvent *event);
