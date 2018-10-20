bf84aca4db05dccb4fe1676a1c379817f04aab87
Pieter Wuille <pieter.wuille@gmail.com>
2018-10-19 18:44:39 -0700

    Merge #14524: Trivial: fix typo
    7e18673f04 Fix typo (Kristaps Kaupe)
    
    Pull request description:
    
    Tree-SHA512: 3f3557d9aad007562b780e34c25584d0de6f240cb9ff56779d224bf9b8cd7bd657c92a73222fd8d7f679c282dce6a75265ca61e20d50a834caa4a9f21a286b3a

--- test/functional/interface_bitcoin_cli.py
+++ test/functional/interface_bitcoin_cli.py
@@ -18,7 +18,7 @@ class TestBitcoinCli(BitcoinTestFramework):
         cli_response = self.nodes[0].cli("-version").send_cli()
         assert("Dash Core RPC client version" in cli_response)
 
-        self.log.info("Compare responses from gewalletinfo RPC and `dash-cli getwalletinfo`")
+        self.log.info("Compare responses from getwalletinfo RPC and `dash-cli getwalletinfo`")
         cli_response = self.nodes[0].cli.getwalletinfo()
         rpc_response = self.nodes[0].getwalletinfo()
         assert_equal(cli_response, rpc_response)
