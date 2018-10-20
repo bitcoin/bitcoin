dbe9d6a018f3cfd6d5f82afd4e5a326d669ace58
Pieter Wuille <pieter.wuille@gmail.com>
2018-10-19 19:12:18 -0700

    Merge #14011: Disable wallet and address book Qt tests on macOS minimal platform
    a3197c5294 Disable wallet and address book Qt tests on macOS minimal platform (Russell Yanofsky)
    
    Pull request description:
    
      macOS minimal platform is frequently broken, and these are currently failing with Qt 5.11.1.
    
      The tests do pass when run on the full cocoa platform (with `test_bitcoin-qt -platform cocoa`).
    
      Stack trace from test crash: https://gist.github.com/ryanofsky/3401fb63c52d13d5585e7fc777361f1e
    
    Tree-SHA512: a05644ef15d75ea7d7f85ea804c6a5fe78e4e7358b189cbab639d9f7dc46163a35f77f7a2b4ae2fd6be5b9fb22898386b4d88069d5ee8d5fdbd995157c6f0846

--- src/qt/test/wallettests.cpp
+++ src/qt/test/wallettests.cpp
@@ -209,5 +209,16 @@ void TestGUI()
 
 void WalletTests::walletTests()
 {
+#ifdef Q_OS_MAC
+    if (QApplication::platformName() == "minimal") {
+        // Disable for mac on "minimal" platform to avoid crashes inside the Qt
+        // framework when it tries to look up unimplemented cocoa functions,
+        // and fails to handle returned nulls
+        // (https://bugreports.qt.io/browse/QTBUG-49686).
+        QWARN("Skipping WalletTests on mac build with 'minimal' platform set due to Qt bugs. To run AppTests, invoke "
+              "with 'test_bitcoin-qt -platform cocoa' on mac, or else use a linux or windows build.");
+        return;
+    }
+#endif
     TestGUI();
 }
