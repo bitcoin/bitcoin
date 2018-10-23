c4351fd3285c308bfe10f042fddd52ea1ca39394
PastaPastaPasta <pasta@dashboost.org>
2018-10-23 06:16:17 -0500

    revert 737, DEFAULT_TRANSACTION_MAXFEE = 0.1 * COIN (#2362)

--- src/validation.h
+++ src/validation.h
@@ -58,7 +58,7 @@ static const bool DEFAULT_WHITELISTFORCERELAY = true;
 /** Default for -minrelaytxfee, minimum relay fee for transactions */
 static const unsigned int DEFAULT_MIN_RELAY_TX_FEE = 1000;
 //! -maxtxfee default
-static const CAmount DEFAULT_TRANSACTION_MAXFEE = 0.2 * COIN; // "smallest denom" + X * "denom tails"
+static const CAmount DEFAULT_TRANSACTION_MAXFEE = 0.1 * COIN;
 //! Discourage users to set fees higher than this amount (in duffs) per kB
 static const CAmount HIGH_TX_FEE_PER_KB = 0.01 * COIN;
 //! -maxtxfee will warn if called with a higher fee than this amount (in duffs)
