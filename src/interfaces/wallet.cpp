910e036d63793f99550bd2cd6c17e5a8227e36ac
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2018-10-23 17:36:46 +0300

    Remove unused `adjustedTime` parameter
    qt: After merging #13622 the `adjustedTime` is not used any more in
    wallet related functions.

--- src/interfaces/wallet.cpp
+++ src/interfaces/wallet.cpp
@@ -293,8 +293,7 @@ public:
     }
     bool tryGetTxStatus(const uint256& txid,
         interfaces::WalletTxStatus& tx_status,
-        int& num_blocks,
-        int64_t& adjusted_time) override
+        int& num_blocks) override
     {
         TRY_LOCK(::cs_main, locked_chain);
         if (!locked_chain) {
@@ -309,7 +308,6 @@ public:
             return false;
         }
         num_blocks = ::chainActive.Height();
-        adjusted_time = GetAdjustedTime();
         tx_status = MakeWalletTxStatus(mi->second);
         return true;
     }
@@ -317,14 +315,12 @@ public:
         WalletTxStatus& tx_status,
         WalletOrderForm& order_form,
         bool& in_mempool,
-        int& num_blocks,
-        int64_t& adjusted_time) override
+        int& num_blocks) override
     {
         LOCK2(::cs_main, m_wallet.cs_wallet);
         auto mi = m_wallet.mapWallet.find(txid);
         if (mi != m_wallet.mapWallet.end()) {
             num_blocks = ::chainActive.Height();
-            adjusted_time = GetAdjustedTime();
             in_mempool = mi->second.InMempool();
             order_form = mi->second.vOrderForm;
             tx_status = MakeWalletTxStatus(mi->second);
