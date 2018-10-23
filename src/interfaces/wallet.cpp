fb3ce75807c50055a97f573fc72bf44d997ea218
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2018-10-23 22:02:20 +0300

    Don't label transactions "Open" while catching up
    Since the default `nSequence` is `0xFFFFFFFE` and locktime is enabled,
    the checking `wtx.is_final` is meaningless until the syncing has
    completed.

--- src/interfaces/wallet.cpp
+++ src/interfaces/wallet.cpp
@@ -318,7 +318,8 @@ public:
     }
     bool tryGetTxStatus(const uint256& txid,
         interfaces::WalletTxStatus& tx_status,
-        int& num_blocks) override
+        int& num_blocks,
+        int64_t& block_time) override
     {
         auto locked_chain = m_wallet.chain().lock(true /* try_lock */);
         if (!locked_chain) {
@@ -333,6 +334,7 @@ public:
             return false;
         }
         num_blocks = ::chainActive.Height();
+        block_time = ::chainActive.Tip()->GetBlockTime();
         tx_status = MakeWalletTxStatus(*locked_chain, mi->second);
         return true;
     }
