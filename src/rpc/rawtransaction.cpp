39ece4fc2807b3442759d879985146ac0de9742d
Glenn Willen <gwillen@nerdnet.org>
2018-10-26 15:28:48 -0700

    Add bool PSBTInputSigned
    Refactor out a "PSBTInputSigned" function to check if a PSBT is signed,
    for use in subsequent commits.
    
    Also improve a related comment.
    
    GitHub-Pull: #14588
    Rebased-From: 53e6fffb8f5b10f94708d33d667a67cb91c2d09d

--- src/rpc/rawtransaction.cpp
+++ src/rpc/rawtransaction.cpp
@@ -1643,13 +1643,16 @@ UniValue finalizepsbt(const JSONRPCRequest& request)
         throw JSONRPCError(RPC_DESERIALIZATION_ERROR, strprintf("TX decode failed %s", error));
     }
 
-    // Get all of the previous transactions
+    // Finalize input signatures -- in case we have partial signatures that add up to a complete
+    //   signature, but have not combined them yet (e.g. because the combiner that created this
+    //   PartiallySignedTransaction did not understand them), this will combine them into a final
+    //   script.
     bool complete = true;
     for (unsigned int i = 0; i < psbtx.tx->vin.size(); ++i) {
         PSBTInput& input = psbtx.inputs.at(i);
 
         SignatureData sigdata;
-        complete &= SignPSBTInput(DUMMY_SIGNING_PROVIDER, *psbtx.tx, input, sigdata, i, 1);
+        complete &= SignPSBTInput(DUMMY_SIGNING_PROVIDER, *psbtx.tx, input, sigdata, i, SIGHASH_ALL);
     }
 
     UniValue result(UniValue::VOBJ);
