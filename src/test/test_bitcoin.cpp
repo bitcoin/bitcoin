97b44f9cee0a81cb277146b3359d45b29bb7e242
Pieter Wuille <pieter.wuille@gmail.com>
2018-10-31 15:51:57 -0700

    Simplify testing RNG code

--- src/test/test_bitcoin.cpp
+++ src/test/test_bitcoin.cpp
@@ -36,8 +36,7 @@ void CConnmanTest::ClearNodes()
     g_connman->vNodes.clear();
 }
 
-uint256 insecure_rand_seed = GetRandHash();
-FastRandomContext insecure_rand_ctx(insecure_rand_seed);
+FastRandomContext insecure_rand_ctx;
 
 extern bool fPrintToConsole;
 extern void noui_connect();
