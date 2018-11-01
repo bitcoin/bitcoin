ab8c6f24d28ea1d1e6258cf316b4b97a0baf2377
practicalswift <practicalswift@users.noreply.github.com>
2018-11-01 17:03:32 +0100

    Add SAFE_CHARS[SAFE_CHARS_URI]: Chars allowed in URIs (RFC 3986)

--- src/util/strencodings.cpp
+++ src/util/strencodings.cpp
@@ -20,6 +20,7 @@ static const std::string SAFE_CHARS[] =
     CHARS_ALPHA_NUM + " .,;-_/:?@()", // SAFE_CHARS_DEFAULT
     CHARS_ALPHA_NUM + " .,;-_?@", // SAFE_CHARS_UA_COMMENT
     CHARS_ALPHA_NUM + ".-_", // SAFE_CHARS_FILENAME
+    CHARS_ALPHA_NUM + "!*'();:@&=+$,/?#[]-_.~%", // SAFE_CHARS_URI
 };
 
 std::string SanitizeString(const std::string& str, int rule)
