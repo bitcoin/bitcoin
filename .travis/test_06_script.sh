bd3ee8e9fcbc093f8cf5f23ee83b7aa8241f5b50
MarcoFalke <falke.marco@gmail.com>
2018-11-01 10:52:17 -0400

    test_runner: Remove travis specific code

--- .travis/test_06_script.sh
+++ .travis/test_06_script.sh
@@ -62,6 +62,6 @@ fi
 
 if [ "$RUN_FUNCTIONAL_TESTS" = "true" ]; then
   BEGIN_FOLD functional-tests
-  DOCKER_EXEC test/functional/test_runner.py --combinedlogslen=4000 --coverage --quiet --failfast ${extended}
+  DOCKER_EXEC test/functional/test_runner.py --ci --combinedlogslen=4000 --coverage --quiet --failfast ${extended}
   END_FOLD
 fi
