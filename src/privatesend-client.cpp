98bdf35f9d9cfaf22d21c9b92059dedeba39b34b
PastaPastaPasta <pasta@dashboost.org>
2018-11-05 03:29:33 -0600

    bump PS copyright (#2417)

--- src/privatesend-client.cpp
+++ src/privatesend-client.cpp
@@ -1,6 +1,7 @@
-// Copyright (c) 2014-2017 The Dash Core developers
+// Copyright (c) 2014-2018 The Dash Core developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #include "privatesend-client.h"
 
 #include "consensus/validation.h"
