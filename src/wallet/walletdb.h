996229fb99753aeae63b4ad4714d2c1fd354707b
Andrew Chow <achow101-github@achow101.com>
2018-11-06 09:23:28 -0500

    Refactor keymetadata writing to a separate method

--- src/wallet/walletdb.h
+++ src/wallet/walletdb.h
@@ -177,6 +177,7 @@ public:
     bool WriteTx(const CWalletTx& wtx);
     bool EraseTx(uint256 hash);
 
+    bool WriteKeyMetadata(const CKeyMetadata& meta, const CPubKey& pubkey, const bool overwrite);
     bool WriteKey(const CPubKey& vchPubKey, const CPrivKey& vchPrivKey, const CKeyMetadata &keyMeta);
     bool WriteCryptedKey(const CPubKey& vchPubKey, const std::vector<unsigned char>& vchCryptedSecret, const CKeyMetadata &keyMeta);
     bool WriteMasterKey(unsigned int nID, const CMasterKey& kMasterKey);
