74d118712f032d0759b2a052e23a6c2957586cf0
Andrew Chow <achow101-github@achow101.com>
2018-11-06 09:43:58 -0500

    Add a method to add a pubkey to the keypool
    Introduces AddKeypoolPubkey in order to add a pubkey to the keypool

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -1001,6 +1001,8 @@ public:
     bool NewKeyPool();
     size_t KeypoolCountExternalKeys() EXCLUSIVE_LOCKS_REQUIRED(cs_wallet);
     bool TopUpKeyPool(unsigned int kpSize = 0);
+    void AddKeypoolPubkey(const CPubKey& pubkey, const bool internal);
+    void AddKeypoolPubkeyWithDB(const CPubKey& pubkey, const bool internal, WalletBatch& batch);
 
     /**
      * Reserves a key from the keypool and sets nIndex to its index
