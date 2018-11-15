fa5e0452e875a7ca6bf6fe61fdd652d341eece40
MarcoFalke <falke.marco@gmail.com>
2018-11-15 12:19:41 -0500

    rpc: Documentation fixups

--- src/rpc/misc.cpp
+++ src/rpc/misc.cpp
@@ -461,7 +461,7 @@ static UniValue echo(const JSONRPCRequest& request)
         throw std::runtime_error(
             RPCHelpMan{"echo|echojson ...",
                 "\nSimply echo back the input arguments. This command is for testing.\n"
-                "\nThe difference between echo and echojson is that echojson has argument conversion enabled in the client-side table in"
+                "\nThe difference between echo and echojson is that echojson has argument conversion enabled in the client-side table in "
                 "bitcoin-cli and the GUI. There is no server-side difference.",
                 {}}
                 .ToString() +
