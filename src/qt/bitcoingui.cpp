042891f34caeb2fc632bca2640ba55063fa48d22
João Barbosa <joao.paulo.barbosa@gmail.com>
2018-11-22 17:40:41 +0000

    qt: Call noui_connect to prevent boost::signals2::no_slots_error in early calls to InitWarning

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -32,7 +32,6 @@
 #include <chainparams.h>
 #include <interfaces/handler.h>
 #include <interfaces/node.h>
-#include <noui.h>
 #include <ui_interface.h>
 #include <util/system.h>
 
@@ -1403,9 +1402,6 @@ void BitcoinGUI::showModalOverlay()
 
 static bool ThreadSafeMessageBox(BitcoinGUI* gui, const std::string& message, const std::string& caption, unsigned int style)
 {
-    // Redundantly log and print message in non-gui fashion
-    noui_ThreadSafeMessageBox(message, caption, style);
-
     bool modal = (style & CClientUIInterface::MODAL);
     // The SECURE flag has no effect in the Qt GUI.
     // bool secure = (style & CClientUIInterface::SECURE);
