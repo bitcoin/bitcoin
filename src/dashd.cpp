40d649da5492a5862de9a68b534cd1c945a18692
Wladimir J. van der Laan <laanwj@gmail.com>
2018-11-23 10:16:07 +0100

    Merge #14521: qt, docs: Fix `bitcoin-qt -version` output formatting
    60ae463a68206b08faaadfbb023d7ede78fff784 Fix `bitcoin-qt -version` output formatting (Hennadii Stepanov)
    
    Pull request description:
    
      This PR makes command line output of `bitcoin-qt -version` formatted in the same way as `bitcoind -version` output.
    
      Before:
      ![screenshot from 2018-10-19 20-16-42](https://user-images.githubusercontent.com/32963518/47233718-434a2a80-d3dc-11e8-90d4-84dd23e8ac3b.png)
    
      After:
      ![screenshot from 2018-10-19 20-22-09](https://user-images.githubusercontent.com/32963518/47233888-b653a100-d3dc-11e8-9155-000b517a8e7b.png)
    
    Tree-SHA512: fd8bd20e2108b9562bcbf6c094f950e867a5755994e8acc28b07e52fe73d04f783201c20bde47be2083ce89fb3ef9749f9c3757d479ad6c48ed09c633155f47e

--- src/dashd.cpp
+++ src/dashd.cpp
@@ -85,7 +85,7 @@ static bool AppInit(int argc, char* argv[])
 
         if (gArgs.IsArgSet("-version"))
         {
-            strUsage += FormatParagraph(LicenseInfo());
+            strUsage += FormatParagraph(LicenseInfo()) + "\n";
         }
         else
         {
