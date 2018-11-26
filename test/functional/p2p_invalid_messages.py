303cf99960f8948755df01f66fa8663fbb82e454
James O'Beirne <james.obeirne@gmail.com>
2018-11-26 15:14:03 -0500

    qa: fix p2p_invalid_messages on macOS

--- test/functional/p2p_invalid_messages.py
+++ test/functional/p2p_invalid_messages.py
@@ -66,7 +66,7 @@ class InvalidMessagesTest(BitcoinTestFramework):
         msg_at_size = msg_unrecognized("b" * valid_data_limit)
         assert len(msg_at_size.serialize()) == msg_limit
 
-        with node.assert_memory_usage_stable(perc_increase_allowed=0.03):
+        with node.assert_memory_usage_stable(perc_increase_allowed=0.5):
             self.log.info(
                 "Sending a bunch of large, junk messages to test "
                 "memory exhaustion. May take a bit...")
