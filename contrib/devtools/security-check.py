a177da19ad08af90fbc27c69913d76e8ffeffeb4
Craig Younkins <cyounkins@gmail.com>
2018-11-30 13:51:55 +0000

    Fix broken Gmane URLs

--- contrib/devtools/security-check.py
+++ contrib/devtools/security-check.py
@@ -86,7 +86,7 @@ def check_ELF_RELRO(executable):
         # This does not affect security: the permission flags of the GNU_RELRO program header are ignored, the PT_LOAD header determines the effective permissions.
         # However, the dynamic linker need to write to this area so these are RW.
         # Glibc itself takes care of mprotecting this area R after relocations are finished.
-        # See also http://permalink.gmane.org/gmane.comp.gnu.binutils/71347
+        # See also https://marc.info/?l=binutils&m=1498883354122353
         if typ == 'GNU_RELRO':
             have_gnu_relro = True
 
