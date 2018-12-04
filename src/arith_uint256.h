6b389aee90066f974df7d97f0ff1b4841a96b7c0
practicalswift <practicalswift@users.noreply.github.com>
2018-12-04 13:16:30 +0100

    Use std::numeric_limits<UNSIGNED>::max()) instead of (UNSIGNED)-1

--- src/arith_uint256.h
+++ src/arith_uint256.h
@@ -8,6 +8,7 @@
 
 #include <assert.h>
 #include <cstring>
+#include <limits>
 #include <stdexcept>
 #include <stdint.h>
 #include <string>
@@ -189,7 +190,7 @@ public:
     {
         // prefix operator
         int i = 0;
-        while (i < WIDTH && --pn[i] == (uint32_t)-1)
+        while (i < WIDTH && --pn[i] == std::numeric_limits<uint32_t>::max())
             i++;
         return *this;
     }
