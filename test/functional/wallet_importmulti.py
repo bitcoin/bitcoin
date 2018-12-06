ee3b21dccbeb0f9d4e99de869dbfaf625c159c7f
John Newbery <john@johnnewbery.com>
2018-12-06 13:29:32 -0500

    [tests] Add docstring for wallet_importmulti.py
    Adds a docstring describing the new importmulti test.

--- test/functional/wallet_importmulti.py
+++ test/functional/wallet_importmulti.py
@@ -2,7 +2,18 @@
 # Copyright (c) 2014-2018 The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
-"""Test the importmulti RPC."""
+"""Test the importmulti RPC.
+
+Test importmulti by generating keys on node0, importing the scriptPubKeys and
+addresses on node1 and then testing the address info for the different address
+variants.
+
+- `get_key()` and `get_multisig()` are called to generate keys on node0 and
+  return the privkeys, pubkeys and all variants of scriptPubKey and address.
+- `test_importmulti()` is called to send an importmulti call to node1, test
+  success, and (if unsuccessful) test the error code and error message returned.
+- `test_address()` is called to call getaddressinfo for an address on node1
+  and test the values returned."""
 from collections import namedtuple
 
 from test_framework.address import (
