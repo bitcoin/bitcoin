67857bce60c677dbdcc734200678f9c1b8bd7d86
Wladimir J. van der Laan <laanwj@gmail.com>
2018-12-07 17:19:28 +0100

    Merge #14890: rpc: Avoid creating non-standard raw transactions
    fa4c8679ed94f215ce895938f7c3c169a2ce101e rpc: Avoid creating non-standard raw transactions (MarcoFalke)
    
    Pull request description:
    
      Multiple OP_RETURN outputs in a transaction are not standard and unlikely to be relayed, so avoid creating them.
    
      Apart from that, the logic was broken in that it duplicated the same hex-data for each data output: Closes #14868.
    
    Tree-SHA512: b08d08062b5622e8a7b497e490ccaf53b06e844c863fda3bf3f932a98684a809e8341aeb98232059a795afb32d8770a6c5591a66f8e6ee372b672af245607887

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -4041,7 +4041,8 @@ UniValue walletcreatefundedpsbt(const JSONRPCRequest& request)
                             },
                         },
                         },
-                    {"outputs", RPCArg::Type::ARR, RPCArg::Optional::NO, "a json array with outputs (key-value pairs).\n"
+                    {"outputs", RPCArg::Type::ARR, RPCArg::Optional::NO, "a json array with outputs (key-value pairs), where none of the keys are duplicated.\n"
+                            "That is, each address can only appear once and there can only be one 'data' object.\n"
                             "For compatibility reasons, a dictionary, which holds the key-value pairs directly, is also\n"
                             "                             accepted as second parameter.",
                         {
