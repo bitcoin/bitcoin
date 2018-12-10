cbd0303529bca6c8156b2bc992aaa022c5cf0789
Solar Designer <solar@openwall.com>
2018-12-10 05:40:08 +0100

    Serialize the block header in CBlockHeader::GetHash() (#2531)
    * Serialize the block header in CBlockHeader::GetHash()
    
    * Switch from CDataStream to CVectorWriter
    
    Co-Authored-By: solardiz <solar@openwall.com>

--- src/primitives/block.cpp
+++ src/primitives/block.cpp
@@ -6,13 +6,17 @@
 #include "primitives/block.h"
 
 #include "hash.h"
+#include "streams.h"
 #include "tinyformat.h"
 #include "utilstrencodings.h"
 #include "crypto/common.h"
 
 uint256 CBlockHeader::GetHash() const
 {
-    return HashX11(BEGIN(nVersion), END(nNonce));
+    std::vector<unsigned char> vch(80);
+    CVectorWriter ss(SER_NETWORK, PROTOCOL_VERSION, vch, 0);
+    ss << *this;
+    return HashX11((const char *)vch.data(), (const char *)vch.data() + vch.size());
 }
 
 std::string CBlock::ToString() const
