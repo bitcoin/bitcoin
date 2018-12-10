8078dad339f44b544515d9e470840c9737c11cf4
marcaiaf <mmachicao@m19r.de>
2018-12-11 00:11:03 +0100

    enforced code conventions and added compat.h since arpa/inet.h not available on windows

--- src/test/timedata_tests.cpp
+++ src/test/timedata_tests.cpp
@@ -2,8 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 //
+#include <compat.h>
 #include <netaddress.h>
-#include <arpa/inet.h>
 #include <test/test_bitcoin.h>
 #include <timedata.h>
 
@@ -52,19 +52,35 @@ BOOST_AUTO_TEST_CASE(util_MedianFilterShallNotGrowBeyondSize)
 }
 
 
-CNetAddr utilBuildAddress(std::string address);
+CNetAddr UtilBuildAddress(const std::string& address)
+{
+    struct sockaddr_in sa;
+    inet_pton(AF_INET, address.c_str(), &(sa.sin_addr));
+    CNetAddr addr = CNetAddr(sa.sin_addr);
+    return addr;
+}
 
-void utilPreconditionIsAtLeastFiveEntriesRequired(std::string baseip, int basevalue);
+
+void UtilPreconditionIsAtLeastFiveEntriesRequired(const std::string& baseip, int basevalue)
+{
+    for (int i = CountOffsetSamples(); i < 5; i++) { // precondition 1: at least 5 entries required to compute any offset
+        int val = basevalue + i;
+        std::stringstream stream;
+        stream << baseip << val;
+        std::string ip = stream.str();
+        AddTimeData(UtilBuildAddress(ip), val);
+    }
+}
 
 
 BOOST_AUTO_TEST_CASE(util_AddTimeDataComputeOffsetWhenSampleCountIsUneven)
 {
-    utilPreconditionIsAtLeastFiveEntriesRequired("1.1.1.", 200); // precondition 1: at least 5 entries required to compute any offset
+    UtilPreconditionIsAtLeastFiveEntriesRequired("1.1.1.", 200); // precondition 1: at least 5 entries required to compute any offset
     BOOST_CHECK(CountOffsetSamples() >= 5);
 
 
     if ((CountOffsetSamples() % 2) == 1) { // precondition 2: start with an even number of samples
-        AddTimeData(utilBuildAddress("1.1.1.210"), 210);
+        AddTimeData(UtilBuildAddress("1.1.1.210"), 210);
     }
 
     BOOST_CHECK(CountOffsetSamples() % 2 == 0);
@@ -72,7 +88,7 @@ BOOST_AUTO_TEST_CASE(util_AddTimeDataComputeOffsetWhenSampleCountIsUneven)
 
     int64_t offset = GetTimeOffset();
     int samples = CountOffsetSamples();
-    AddTimeData(utilBuildAddress("1.1.1.211"), 211);
+    AddTimeData(UtilBuildAddress("1.1.1.211"), 211);
 
     BOOST_CHECK_EQUAL(CountOffsetSamples(), samples + 1); // sample was added
     BOOST_CHECK(GetTimeOffset() != offset);               // and new offset was computed
@@ -81,12 +97,12 @@ BOOST_AUTO_TEST_CASE(util_AddTimeDataComputeOffsetWhenSampleCountIsUneven)
 
 BOOST_AUTO_TEST_CASE(util_AddTimeDataDoNotComputeOffsetWhenSampleCountIsEven)
 {
-    utilPreconditionIsAtLeastFiveEntriesRequired("1.1.1.", 100); // precondition 1: at least 5 entries required to compute any offset
+    UtilPreconditionIsAtLeastFiveEntriesRequired("1.1.1.", 100); // precondition 1: at least 5 entries required to compute any offset
 
     BOOST_CHECK(CountOffsetSamples() >= 5);
 
     if (CountOffsetSamples() % 2 == 0) { // precondition 2: start with an uneven number of samples
-        AddTimeData(utilBuildAddress("1.1.1.110"), 110);
+        AddTimeData(UtilBuildAddress("1.1.1.110"), 110);
     }
 
     BOOST_CHECK(CountOffsetSamples() % 2 == 1);
@@ -94,7 +110,7 @@ BOOST_AUTO_TEST_CASE(util_AddTimeDataDoNotComputeOffsetWhenSampleCountIsEven)
 
     int64_t offset = GetTimeOffset();
     int samples = CountOffsetSamples();
-    AddTimeData(utilBuildAddress("1.1.1.111"), 111);
+    AddTimeData(UtilBuildAddress("1.1.1.111"), 111);
 
     BOOST_CHECK_EQUAL(CountOffsetSamples(), samples + 1); // sample was added
     BOOST_CHECK_EQUAL(GetTimeOffset(), offset);           //new offset was not computed
@@ -103,15 +119,15 @@ BOOST_AUTO_TEST_CASE(util_AddTimeDataDoNotComputeOffsetWhenSampleCountIsEven)
 
 BOOST_AUTO_TEST_CASE(util_AddTimeDataIgnoreSampleWithDuplicateIP)
 {
-    utilPreconditionIsAtLeastFiveEntriesRequired("1.1.1.", 300); // precondition 1: at least 5 entries required to compute any offset
+    UtilPreconditionIsAtLeastFiveEntriesRequired("1.1.1.", 300); // precondition 1: at least 5 entries required to compute any offset
     BOOST_CHECK(CountOffsetSamples() >= 5);
 
 
-    AddTimeData(utilBuildAddress("1.1.1.310"), 310);
+    AddTimeData(UtilBuildAddress("1.1.1.310"), 310);
 
     int64_t offset = GetTimeOffset();
     int samples = CountOffsetSamples();
-    AddTimeData(utilBuildAddress("1.1.1.310"), 311);
+    AddTimeData(UtilBuildAddress("1.1.1.310"), 311);
 
     BOOST_CHECK_EQUAL(CountOffsetSamples(), samples); // sample was ignored
     BOOST_CHECK_EQUAL(GetTimeOffset(), offset);       //new offset was not computed
@@ -130,13 +146,13 @@ BOOST_AUTO_TEST_CASE(util_AddTimeDataAlgorithmMedianIsWithinBounds)
         std::stringstream stream;
         stream << "1.1.1." << sample;
         std::string ip = stream.str();
-        AddTimeDataAlgorithm(utilBuildAddress(ip), sample, knownSet, offsetFilter, offset);
+        AddTimeDataAlgorithm(UtilBuildAddress(ip), sample, knownSet, offsetFilter, offset);
     }
 
     BOOST_CHECK_EQUAL(offset, 0);
     BOOST_CHECK_EQUAL(offsetFilter.size(), 4);
 
-    AddTimeDataAlgorithm(utilBuildAddress("1.1.1.200"), 200, knownSet, offsetFilter, offset);
+    AddTimeDataAlgorithm(UtilBuildAddress("1.1.1.200"), 200, knownSet, offsetFilter, offset);
 
     BOOST_CHECK_EQUAL(offset, offsetFilter.median());
     BOOST_CHECK_EQUAL(offsetFilter.size(), 5);
@@ -153,22 +169,22 @@ BOOST_AUTO_TEST_CASE(util_AddTimeDataAlgorithmMedianIsOutsideBounds)
         std::stringstream stream;
         stream << "1.1.1." << 1 + sample;
         std::string ip = stream.str();
-        AddTimeDataAlgorithm(utilBuildAddress(ip), 2 * DEFAULT_MAX_TIME_ADJUSTMENT, knownSet, offsetFilter, offset);
+        AddTimeDataAlgorithm(UtilBuildAddress(ip), 2 * DEFAULT_MAX_TIME_ADJUSTMENT, knownSet, offsetFilter, offset);
     } // sorted filter: 0 x x x  -- x is outside the boundaries
 
     BOOST_CHECK_EQUAL(offset, 0);
     BOOST_CHECK_EQUAL(offsetFilter.size(), 4);
 
     // offset is computed only when number of entries is uneven
-    AddTimeDataAlgorithm(utilBuildAddress("1.1.1.5"), 1, knownSet, offsetFilter, offset); // sorted filter : 0 1 (x) x x  -- median (x)
+    AddTimeDataAlgorithm(UtilBuildAddress("1.1.1.5"), 1, knownSet, offsetFilter, offset); // sorted filter : 0 1 (x) x x  -- median (x)
     BOOST_CHECK_EQUAL(offset, 0);
-    AddTimeDataAlgorithm(utilBuildAddress("1.1.1.6"), 1, knownSet, offsetFilter, offset); // sorted filter : 0 1 1 x x x
+    AddTimeDataAlgorithm(UtilBuildAddress("1.1.1.6"), 1, knownSet, offsetFilter, offset); // sorted filter : 0 1 1 x x x
     BOOST_CHECK_EQUAL(offset, 0);
-    AddTimeDataAlgorithm(utilBuildAddress("1.1.1.7"), 1, knownSet, offsetFilter, offset);                               // sorted filter : 0 1 1 (1) x x x x -- median (1)
+    AddTimeDataAlgorithm(UtilBuildAddress("1.1.1.7"), 1, knownSet, offsetFilter, offset);                               // sorted filter : 0 1 1 (1) x x x x -- median (1)
     BOOST_CHECK_EQUAL(offset, 1);                                                                                       // flip to 1
-    AddTimeDataAlgorithm(utilBuildAddress("1.1.1.8"), 2 * DEFAULT_MAX_TIME_ADJUSTMENT, knownSet, offsetFilter, offset); // sorted filter : 0 1 1 1 x x x x x
+    AddTimeDataAlgorithm(UtilBuildAddress("1.1.1.8"), 2 * DEFAULT_MAX_TIME_ADJUSTMENT, knownSet, offsetFilter, offset); // sorted filter : 0 1 1 1 x x x x x
     BOOST_CHECK_EQUAL(offset, 1);
-    AddTimeDataAlgorithm(utilBuildAddress("1.1.1.9"), 2 * DEFAULT_MAX_TIME_ADJUSTMENT, knownSet, offsetFilter, offset); // sorted filter : 0 1 1 1 (x) x x x x x -- median (x)
+    AddTimeDataAlgorithm(UtilBuildAddress("1.1.1.9"), 2 * DEFAULT_MAX_TIME_ADJUSTMENT, knownSet, offsetFilter, offset); // sorted filter : 0 1 1 1 (x) x x x x x -- median (x)
     BOOST_CHECK_EQUAL(offset, 0);                                                                                       // flip back to zero
 
     BOOST_CHECK_EQUAL(offsetFilter.size(), 9);
@@ -181,16 +197,16 @@ BOOST_AUTO_TEST_CASE(util_AddTimeDataAlgorithmAtLeastFiveSamplesToComputeOffset)
     std::set<CNetAddr> knownSet;
     CMedianFilter<int64_t> offsetFilter(limit, 0); // max size : limit , init value: 0
 
-    AddTimeDataAlgorithm(utilBuildAddress("1.1.1.1"), 1, knownSet, offsetFilter, offset);
+    AddTimeDataAlgorithm(UtilBuildAddress("1.1.1.1"), 1, knownSet, offsetFilter, offset);
     BOOST_CHECK_EQUAL(offset, 0);
 
-    AddTimeDataAlgorithm(utilBuildAddress("1.1.1.2"), 2, knownSet, offsetFilter, offset);
+    AddTimeDataAlgorithm(UtilBuildAddress("1.1.1.2"), 2, knownSet, offsetFilter, offset);
     BOOST_CHECK_EQUAL(offset, 0);
 
-    AddTimeDataAlgorithm(utilBuildAddress("1.1.1.3"), 3, knownSet, offsetFilter, offset);
+    AddTimeDataAlgorithm(UtilBuildAddress("1.1.1.3"), 3, knownSet, offsetFilter, offset);
     BOOST_CHECK_EQUAL(offset, 0);
 
-    AddTimeDataAlgorithm(utilBuildAddress("1.1.1.4"), 4, knownSet, offsetFilter, offset); // this is the fifth entry
+    AddTimeDataAlgorithm(UtilBuildAddress("1.1.1.4"), 4, knownSet, offsetFilter, offset); // this is the fifth entry
     BOOST_CHECK_EQUAL(offset, 2);
 }
 
@@ -206,7 +222,7 @@ BOOST_AUTO_TEST_CASE(util_AddTimeDataAlgorithmIgnoresSamplesBeyondInternalLimit)
         std::stringstream stream;
         stream << "1.1.1." << sample;
         std::string ip = stream.str();
-        AddTimeDataAlgorithm(utilBuildAddress(ip), sample, knownSet, offsetFilter, offset);
+        AddTimeDataAlgorithm(UtilBuildAddress(ip), sample, knownSet, offsetFilter, offset);
     }
 
     BOOST_CHECK_EQUAL(offsetFilter.size(), limit);
@@ -214,31 +230,10 @@ BOOST_AUTO_TEST_CASE(util_AddTimeDataAlgorithmIgnoresSamplesBeyondInternalLimit)
     int64_t pre = offset;
     int size = offsetFilter.size();
 
-    AddTimeDataAlgorithm(utilBuildAddress("1.1.1.200"), 200, knownSet, offsetFilter, offset);
+    AddTimeDataAlgorithm(UtilBuildAddress("1.1.1.200"), 200, knownSet, offsetFilter, offset);
 
     BOOST_CHECK_EQUAL(offset, pre);
     BOOST_CHECK_EQUAL(offsetFilter.size(), size);
-}
-
-
-CNetAddr utilBuildAddress(std::string address)
-{
-    struct sockaddr_in sa;
-    inet_pton(AF_INET, address.c_str(), &(sa.sin_addr));
-    CNetAddr addr = CNetAddr(sa.sin_addr);
-    return addr;
-}
-
-
-void utilPreconditionIsAtLeastFiveEntriesRequired(std::string baseip, int basevalue)
-{
-    for (int i = CountOffsetSamples(); i < 5; i++) { // precondition 1: at least 5 entries required to compute any offset
-        int val = basevalue + i;
-        std::stringstream stream;
-        stream << baseip << val;
-        std::string ip = stream.str();
-        AddTimeData(utilBuildAddress(ip.c_str()), val);
-    }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
