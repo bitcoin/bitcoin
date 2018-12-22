fa71b38168ae3a59f3463e0e38a6eb6c88243db7
MarcoFalke <falke.marco@gmail.com>
2018-12-22 17:22:55 +0100

    scripted-diff: Rename rpc_timewait to rpc_timeout
    -BEGIN VERIFY SCRIPT-
    sed -i -e 's/self.rpc_timewait/self.rpc_timeout/g' $(git grep -l self.rpc_timewait)
    -END VERIFY SCRIPT-

--- test/functional/wallet_groups.py
+++ test/functional/wallet_groups.py
@@ -21,7 +21,7 @@ class WalletGroupTest(BitcoinTestFramework):
         self.setup_clean_chain = True
         self.num_nodes = 3
         self.extra_args = [[], [], ['-avoidpartialspends']]
-        self.rpc_timewait = 120
+        self.rpc_timeout = 120
 
     def skip_test_if_missing_module(self):
         self.skip_if_no_wallet()
