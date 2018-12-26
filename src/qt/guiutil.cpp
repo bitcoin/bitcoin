da176cc231b9fb99acfefcca56443d75a68af995
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2018-12-26 22:19:14 +0200

    Add workaround for QProgressDialog bug on macOS
    See: QTBUG-65750, QTBUG-70357.

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -48,13 +48,15 @@
 #include <QFileDialog>
 #include <QFont>
 #include <QFontDatabase>
+#include <QFontMetrics>
 #include <QKeyEvent>
 #include <QLineEdit>
+#include <QMouseEvent>
+#include <QProgressDialog>
 #include <QSettings>
 #include <QTextDocument> // for Qt::mightBeRichText
 #include <QThread>
 #include <QUrlQuery>
-#include <QMouseEvent>
 
 #if defined(Q_OS_MAC)
 #pragma GCC diagnostic push
@@ -931,6 +933,18 @@ bool ItemDelegate::eventFilter(QObject *object, QEvent *event)
         }
     }
     return QItemDelegate::eventFilter(object, event);
+}
+
+void PolishProgressDialog(QProgressDialog* dialog)
+{
+#ifdef Q_OS_MAC
+    // Workaround for macOS-only Qt bug; see: QTBUG-65750, QTBUG-70357.
+    const int margin = dialog->fontMetrics().width("X");
+    dialog->resize(dialog->width() + 2 * margin, dialog->height());
+    dialog->show();
+#else
+    Q_UNUSED(dialog);
+#endif
 }
 
 } // namespace GUIUtil
