f9c585776b62626e33adbf3e21019aacfeccce28
Pasta <pasta@dashboost.org>
2018-12-28 16:53:13 -0600

    manual fixes on #9868

--- src/miner.h
+++ src/miner.h
@@ -165,7 +165,6 @@ private:
 public:
     struct Options {
         Options();
-        size_t nBlockMaxWeight;
         size_t nBlockMaxSize;
         CFeeRate blockMinFeeRate;
     };
--- src/test/miner_tests.cpp
+++ src/test/miner_tests.cpp
@@ -31,8 +31,7 @@ static CFeeRate blockMinFeeRate = CFeeRate(DEFAULT_BLOCK_MIN_TX_FEE);
 static BlockAssembler AssemblerForTest(const CChainParams& params) {
     BlockAssembler::Options options;
 
-    options.nBlockMaxWeight = MAX_BLOCK_WEIGHT;
-    options.nBlockMaxSize = MAX_BLOCK_SERIALIZED_SIZE;
+    options.nBlockMaxSize = DEFAULT_BLOCK_MAX_SIZE;
     options.blockMinFeeRate = blockMinFeeRate;
     return BlockAssembler(params, options);
 }
