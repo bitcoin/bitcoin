85906c99f70cfdb81a0ae49e18a2b1609ea05dfa
Graham Krizek <graham@krizek.io>
2018-12-28 08:28:38 -0600

    Increase timeout of featuer_assumevalid test to fix flaky tests

--- test/functional/feature_assumevalid.py
+++ test/functional/feature_assumevalid.py
@@ -180,7 +180,7 @@ class AssumeValidTest(BitcoinTestFramework):
         for i in range(2202):
             p2p1.send_message(msg_block(self.blocks[i]))
         # Syncing 2200 blocks can take a while on slow systems. Give it plenty of time to sync.
-        p2p1.sync_with_ping(120)
+        p2p1.sync_with_ping(150)
         assert_equal(self.nodes[1].getblock(self.nodes[1].getbestblockhash())['height'], 2202)
 
         # Send blocks to node2. Block 102 will be rejected.
