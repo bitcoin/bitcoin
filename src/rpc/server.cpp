02f4661b3c7cb0d947794cbfbf05bc02c134b96a
Pasta <pasta@dashboost.org>
2018-12-30 16:05:45 -0600

    fix #9894 backport

--- src/rpc/server.cpp
+++ src/rpc/server.cpp
@@ -240,7 +240,7 @@ std::string CRPCTable::help(const std::string& strCommand, const std::string& st
     BOOST_FOREACH(const PAIRTYPE(std::string, const CRPCCommand*)& command, vCommands)
     {
         const CRPCCommand *pcmd = command.second;
-        string strMethod = pcmd->name;
+        std::string strMethod = pcmd->name;
         if ((strCommand != "" || pcmd->category == "hidden") && strMethod != strCommand)
             continue;
         try
