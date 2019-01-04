152146e782d401aa1ce7d989d62306aabc85f22e
Pieter Wuille <pieter.wuille@gmail.com>
2019-01-04 02:00:44 -0800

    DRY: Implement GetRand using FastRandomContext::randrange

--- src/random.cpp
+++ src/random.cpp
@@ -503,17 +503,7 @@ void RandAddSeedSleep() { ProcRand(nullptr, 0, RNGLevel::SLEEP); }
 
 uint64_t GetRand(uint64_t nMax) noexcept
 {
-    if (nMax == 0)
-        return 0;
-
-    // The range of the random source must be a multiple of the modulus
-    // to give every possible output value an equal possibility
-    uint64_t nRange = (std::numeric_limits<uint64_t>::max() / nMax) * nMax;
-    uint64_t nRand = 0;
-    do {
-        GetRandBytes((unsigned char*)&nRand, sizeof(nRand));
-    } while (nRand >= nRange);
-    return (nRand % nMax);
+    return FastRandomContext().randrange(nMax);
 }
 
 int GetRandInt(int nMax) noexcept
