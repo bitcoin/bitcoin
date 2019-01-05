f5bdfcab19ef24c515e20e52477974ce1508d279
Sjors Provoost <sjors@sprovoost.nl>
2019-01-05 20:20:42 +0100

    [tests] check v0.17.1 and v0.18.1 backwards compatibility
    Former-commit-id: 1e67d413f058b411e67a7bef2d9528bef3cd55d4

--- test/functional/rpc_getaddressinfo_label_deprecation.py
+++ test/functional/rpc_getaddressinfo_label_deprecation.py
@@ -7,9 +7,9 @@ Test deprecation of the RPC getaddressinfo `label` field. It has been
 superceded by the `labels` field.
 
 """
-from test_framework.test_framework import BitcoinTestFramework
+from test_framework.test_framework import SyscoinTestFramework
 
-class GetAddressInfoLabelDeprecationTest(BitcoinTestFramework):
+class GetAddressInfoLabelDeprecationTest(SyscoinTestFramework):
     def set_test_params(self):
         self.num_nodes = 2
         self.setup_clean_chain = False
--- test/functional/test_framework/test_node.py
+++ test/functional/test_framework/test_node.py
@@ -60,7 +60,7 @@ class TestNode():
     To make things easier for the test writer, any unrecognised messages will
     be dispatched to the RPC connection."""
 
-    def __init__(self, i, datadir, *, chain, rpchost, timewait, syscoind, syscoin_cli, coverage_dir, cwd, extra_conf=None, extra_args=None, use_cli=False, start_perf=False, use_valgrind=False):
+    def __init__(self, i, datadir, *, chain, rpchost, timewait, syscoind, syscoin_cli, coverage_dir, cwd, extra_conf=None, extra_args=None, use_cli=False, start_perf=False, use_valgrind=False, version=None):
         """
         Kwargs:
             start_perf (bool): If True, begin profiling the node with `perf` as soon as
@@ -69,7 +69,7 @@ class TestNode():
 
         self.index = i
         self.datadir = datadir
-        self.syscoinconf = os.path.join(self.datadir, "bitcoin.conf")
+        self.syscoinconf = os.path.join(self.datadir, "syscoin.conf")
         self.stdout_dir = os.path.join(self.datadir, "stdout")
         self.stderr_dir = os.path.join(self.datadir, "stderr")
         self.chain = chain
@@ -84,6 +84,7 @@ class TestNode():
         # For those callers that need more flexibility, they can just set the args property directly.
         # Note that common args are set in the config file (see initialize_datadir)
         self.extra_args = extra_args
+        self.version = version
         # Configuration for logging is set as command-line args rather than in the syscoin.conf file.
         # This means that starting a syscoind using the temp dir to debug a failed test won't
         # spam debug.log.
@@ -91,7 +92,6 @@ class TestNode():
             self.binary,
             "-datadir=" + self.datadir,
             "-logtimemicros",
-            "-logthreadnames",
             "-debug",
             "-debugexclude=libevent",
             "-debugexclude=leveldb",
@@ -106,6 +106,9 @@ class TestNode():
             self.args = ["valgrind", "--suppressions={}".format(suppressions_file),
                          "--gen-suppressions=all", "--exit-on-first-error=yes",
                          "--error-exitcode=1", "--quiet"] + self.args
+
+        if self.version is None or self.version >= 190000:
+            self.args.append("-logthreadnames")
 
         self.cli = TestNodeCLI(syscoin_cli, self.datadir)
         self.use_cli = use_cli
@@ -254,7 +257,11 @@ class TestNode():
             return
         self.log.debug("Stopping node")
         try:
-            self.stop(wait=wait)
+            # Do not use wait argument when testing older nodes, e.g. in feature_backwards_compatibility.py
+            if self.version is None or self.version >= 180000:
+                self.stop(wait=wait)
+            else:
+                self.stop()
         except http.client.CannotSendRequest:
             self.log.exception("Unable to stop node.")
 
