edb5bb35000e13e001e2693fbff4e8cc9358e166
Wladimir J. van der Laan <laanwj@gmail.com>
2019-01-10 01:31:50 +0100

    util: remove unused [U](BEGIN|END) macros

--- src/util/strencodings.h
+++ src/util/strencodings.h
@@ -15,10 +15,6 @@
 #include <string>
 #include <vector>
 
-#define BEGIN(a)            ((char*)&(a))
-#define END(a)              ((char*)&((&(a))[1]))
-#define UBEGIN(a)           ((unsigned char*)&(a))
-#define UEND(a)             ((unsigned char*)&((&(a))[1]))
 #define ARRAYLEN(array)     (sizeof(array)/sizeof((array)[0]))
 
 /** Used by SanitizeString() */
