17abc0fd52510bfeb85a7b5be04001075ec78b6b
João Barbosa <joao.paulo.barbosa@gmail.com>
2019-01-12 11:47:04 +0000

    wallet: Factor out LoadWallet

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -66,6 +66,7 @@ bool RemoveWallet(const std::shared_ptr<CWallet>& wallet);
 bool HasWallets();
 std::vector<std::shared_ptr<CWallet>> GetWallets();
 std::shared_ptr<CWallet> GetWallet(const std::string& name);
+std::shared_ptr<CWallet> LoadWallet(interfaces::Chain& chain, const WalletLocation& location, std::string& error, std::string& warning);
 
 //! Default for -keypool
 static const unsigned int DEFAULT_KEYPOOL_SIZE = 1000;
