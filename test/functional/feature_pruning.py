74ae9024f748277dd5e3f10a35f5a55db53745f2
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2019-01-14 13:40:00 +0200

    Correct units for "-dbcache" and "-prune"
    All dbcache-related values in the code are measured in MiB (not in
    megabytes, MB) or in bytes.
    The GUI "-prune" values in GB are translated to the node values in MiB
    correctly. The maximum of the "-prune" QSpinBox is not limited by the
    default value of 99 (GB).
    Also, this improves log readability.

--- test/functional/feature_pruning.py
+++ test/functional/feature_pruning.py
@@ -320,7 +320,7 @@ class PruneTest(BitcoinTestFramework):
         if has_block(3):
             raise AssertionError("blk00003.dat is still there, should be pruned by now")
 
-        # stop node, start back up with auto-prune at 550MB, make sure still runs
+        # stop node, start back up with auto-prune at 550 MiB, make sure still runs
         self.stop_node(node_number)
         self.start_node(node_number, extra_args=["-prune=550"])
 
