ad6d2e09e52ead0d95249ecd45e03ce0e1f51a38
Peerchemist <dx.1178@gmail.com>
2019-01-16 00:48:42 +0100

    hrp chars

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -133,7 +133,8 @@ public:
         base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x88, 0xB2, 0x1E};          //ppcTODO: leave it as is?
         base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x88, 0xAD, 0xE4};          //ppcTODO: leave it as is?
 
-        bech32_hrp = "bc";
+        // human readable prefix to bench32 address
+        bech32_hrp = "xpc";
 
         vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_main, pnSeed6_main + ARRAYLEN(pnSeed6_main));
 
@@ -223,7 +224,8 @@ public:
         base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x35, 0x87, 0xCF};
         base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x35, 0x83, 0x94};
 
-        bech32_hrp = "tb";
+        // human readable prefix to bench32 address
+        bech32_hrp = "tpc";
 
         vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_test, pnSeed6_test + ARRAYLEN(pnSeed6_test));
 
