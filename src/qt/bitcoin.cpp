fec938a497036a913d74ddafbe169108791b665c
João Barbosa <joao.paulo.barbosa@gmail.com>
2019-01-29 00:08:48 +0000

    gui: Fix m_node.startShutdown() order
    This change forwards the shutdown request on the GUI (close the
    application for instace) to the node as soon as possible. This way the
    GUI doesn't have to wait for long operations to complete (rescan the
    wallet for instance), instead those operations detect the shutdown
    request and abort/interrupt.

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -309,13 +309,19 @@ void BitcoinApplication::requestShutdown()
     qDebug() << __func__ << ": Requesting shutdown";
     startThread();
     window->hide();
+    // Must disconnect node signals otherwise current thread can deadlock since
+    // no event loop is running.
+    window->unsubscribeFromCoreSignals();
+    // Request node shutdown, which can interrupt long operations, like
+    // rescanning a wallet.
+    m_node.startShutdown();
+    // Unsetting the client model can cause the current thread to wait for node
+    // to complete an operation, like wait for a RPC execution to complate.
     window->setClientModel(nullptr);
     pollShutdownTimer->stop();
 
     delete clientModel;
     clientModel = nullptr;
-
-    m_node.startShutdown();
 
     // Request shutdown from core thread
     Q_EMIT requestedShutdown();
