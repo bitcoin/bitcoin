b84dcecbbbe2ed18a73d6087db5e8999eaf6f990
João Barbosa <joao.paulo.barbosa@gmail.com>
2019-01-29 01:08:30 +0000

    partial bitcoin#15280 Check m_internals in UnregisterValidationInterface

--- src/validationinterface.cpp
+++ src/validationinterface.cpp
@@ -133,7 +133,9 @@ void RegisterValidationInterface(CValidationInterface* pwalletIn) {
 }
 
 void UnregisterValidationInterface(CValidationInterface* pwalletIn) {
-    g_signals.m_internals->m_connMainSignals.erase(pwalletIn);
+    if (g_signals.m_internals) {
+        g_signals.m_internals->m_connMainSignals.erase(pwalletIn);
+    }
 }
 
 void UnregisterAllValidationInterfaces() {
