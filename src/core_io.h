2d93bb7fe091f2439673ae49cf5caa495e4e7131
Glenn Willen <gwillen@nerdnet.org>
2019-01-29 21:32:38 -0800

    Split DecodePSBT into Base64 and Raw versions
    Split up DecodePSBT, which both decodes base64 and then deserializes a
    PartiallySignedTransaction, into two functions: DecodeBase64PSBT, which retains
    the old behavior, and DecodeRawPSBT, which only performs the deserialization.
    
    Add a test for base64 decoding failure.

--- src/core_io.h
+++ src/core_io.h
@@ -37,7 +37,11 @@ bool DecodeHexBlockHeader(CBlockHeader&, const std::string& hex_header);
  */
 bool ParseHashStr(const std::string& strHex, uint256& result);
 std::vector<unsigned char> ParseHexUV(const UniValue& v, const std::string& strName);
-NODISCARD bool DecodePSBT(PartiallySignedTransaction& psbt, const std::string& base64_tx, std::string& error);
+
+//! Decode a base64ed PSBT into a PartiallySignedTransaction
+NODISCARD bool DecodeBase64PSBT(PartiallySignedTransaction& decoded_psbt, const std::string& base64_psbt, std::string& error);
+//! Decode a raw (binary blob) PSBT into a PartiallySignedTransaction
+NODISCARD bool DecodeRawPSBT(PartiallySignedTransaction& decoded_psbt, const std::string& raw_psbt, std::string& error);
 int ParseSighashString(const UniValue& sighash);
 
 // core_write.cpp
