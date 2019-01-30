9584aae81a1d1245f2578a122b49c3d863c87623
Glenn Willen <gwillen@nerdnet.org>
2019-01-29 22:51:56 -0800

    Remove op== on PSBTs; check compatibility in Merge
    Remove the op== on PartiallySignedTransaction, which only checks that the
    CTransactions are equal. Instead, check this directly in Merge, and return
    false if the CTransactions are not equal (so the PSBTs cannot be merged.)

--- src/psbt.h
+++ src/psbt.h
@@ -384,21 +384,14 @@ struct PartiallySignedTransaction
     std::map<std::vector<unsigned char>, std::vector<unsigned char>> unknown;
 
     bool IsNull() const;
-    void Merge(const PartiallySignedTransaction& psbt);
+
+    /** Merge psbt into this. The two psbts must have the same underlying CTransaction (i.e. the
+      * same actual Bitcoin transaction.) Returns true if the merge succeeded, false otherwise. */
+    NODISCARD bool Merge(const PartiallySignedTransaction& psbt);
     bool IsSane() const;
     PartiallySignedTransaction() {}
     PartiallySignedTransaction(const PartiallySignedTransaction& psbt_in) : tx(psbt_in.tx), inputs(psbt_in.inputs), outputs(psbt_in.outputs), unknown(psbt_in.unknown) {}
     explicit PartiallySignedTransaction(const CMutableTransaction& tx);
-
-    // Only checks if they refer to the same transaction
-    friend bool operator==(const PartiallySignedTransaction& a, const PartiallySignedTransaction &b)
-    {
-        return a.tx->GetHash() == b.tx->GetHash();
-    }
-    friend bool operator!=(const PartiallySignedTransaction& a, const PartiallySignedTransaction &b)
-    {
-        return !(a == b);
-    }
 
     template <typename Stream>
     inline void Serialize(Stream& s) const {
