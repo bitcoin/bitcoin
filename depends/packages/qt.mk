3e430026e21dbc3bf70a532fb351ba948d47371d
practicalswift <practicalswift@users.noreply.github.com>
2019-02-01 22:33:51 +0100

    build: Enable C++14 in build, require C++14 compiler

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -23,7 +23,7 @@ define $(package)_set_vars
 $(package)_config_opts_release = -release
 $(package)_config_opts_debug = -debug
 $(package)_config_opts += -bindir $(build_prefix)/bin
-$(package)_config_opts += -c++std c++11
+$(package)_config_opts += -c++std c++14
 $(package)_config_opts += -confirm-license
 $(package)_config_opts += -dbus-runtime
 $(package)_config_opts += -hostprefix $(build_prefix)
