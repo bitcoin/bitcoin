f897dbeef19451abde18cebe587c89ed025ef7ec
Wladimir J. van der Laan <laanwj@gmail.com>
2019-02-04 10:45:10 +0100

    Merge #15280: gui: Fix shutdown order
    0dd6a8c12489ea4428b398a2328dde5d1a9fe39b Check m_internals in UnregisterValidationInterface (João Barbosa)
    fd6d499bdacfa29f25b0f675375e3feaced08667 gui: Fix m_node.startShutdown() order (João Barbosa)
    07b9aadcfc7cc72be9df344dd5715cf8fc78f0ad gui: Expose BitcoinGUI::unsubscribeFromCoreSignals (João Barbosa)
    60e190ceb3563a8102d42fdfcbefccdd1b53e812 gui: Fix WalletController deletion (João Barbosa)
    
    Pull request description:
    
      This PR consists in small fixes in order to have a clean shutdown from the GUI.
    
    Tree-SHA512: a9c641f202bc810698c4a39d5c5a1f54e54bdab098c412d65418879e00764a9db9f38383813914d591e24e097e49f177942b2ae6c57bba05dcc095e8a1d0b8f4

--- src/qt/bitcoingui.h
+++ src/qt/bitcoingui.h
@@ -95,6 +95,9 @@ public:
     */
     bool hasTrayIcon() const { return trayIcon; }
 
+    /** Disconnect core signals from GUI client */
+    void unsubscribeFromCoreSignals();
+
 protected:
     void changeEvent(QEvent *e) override;
     void closeEvent(QCloseEvent *event) override;
@@ -220,8 +223,6 @@ private:
 
     /** Connect core signals to GUI client */
     void subscribeToCoreSignals();
-    /** Disconnect core signals from GUI client */
-    void unsubscribeFromCoreSignals();
 
     /** Update UI with latest network info from model. */
     void updateNetworkState();
