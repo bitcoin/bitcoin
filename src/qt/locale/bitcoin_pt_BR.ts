05fc797c0ee79b15ff320ebc23bf078903ff2fde
sandakersmann <mkjaerstad@protonmail.com>
2019-02-10 19:10:40 +0100

    Description in about section added to locale

--- src/qt/locale/bitcoin_pt_BR.ts
+++ src/qt/locale/bitcoin_pt_BR.ts
@@ -17,9 +17,11 @@
         <source>
 This is experimental software.
 
-Distributed under the MIT/X11 software license, see the accompanying file COPYING or http://www.opensource.org/licenses/mit-license.php.
+Distributed under the MIT/X11 software license, see the accompanying file COPYING or https://www.opensource.org/licenses/mit-license.php.
 
-This product includes software developed by the OpenSSL Project for use in the OpenSSL Toolkit (http://www.openssl.org/) and cryptographic software written by Eric Young (eay@cryptsoft.com) and UPnP software written by Thomas Bernard.</source>
+This product includes software developed by the OpenSSL Project for use in the OpenSSL Toolkit (https://www.openssl.org/) and cryptographic software written by Eric Young (eay@cryptsoft.com) and UPnP software written by Thomas Bernard.
+
+More information can be found on the Peercoin official website: https://peercoin.net/.</source>
         <translation>⏎
 Este é um software experimental.⏎
 ⏎
