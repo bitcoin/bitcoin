9a8b29d3dae726a571f4c31fb5ae5aeb77afde26
backpacker69 <backpacker69@protonmail.com>
2019-02-11 12:03:14 +0300

    extra deps

--- scripts/dependencies-ubuntu.sh
+++ scripts/dependencies-ubuntu.sh
@@ -3,7 +3,7 @@
 sudo apt-get update -qq
 sudo apt-get upgrade -y -qq
 sudo apt-get install -y -qq autoconf build-essential pkg-config libssl-dev libboost-all-dev
-sudo apt-get install -y -qq miniupnpc libminiupnpc-dev gettext libevent-dev
+sudo apt-get install -y -qq miniupnpc libminiupnpc-dev gettext libevent-dev protobuf-compiler
 #for gui
 sudo apt-get install -y -qq qtbase5-dev qttools5-dev-tools
 sudo apt-get install -y -qq libdb++-dev
