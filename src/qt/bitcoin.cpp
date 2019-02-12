fd46c4c0018c41d36cd892ccb47485b572d65837
Sjors Provoost <sjors@sprovoost.nl>
2019-02-12 20:24:29 +0100

    Bump minimum Qt version to 5.5.1

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -55,9 +55,6 @@
 
 #if defined(QT_STATICPLUGIN)
 #include <QtPlugin>
-#if QT_VERSION < 0x050400
-Q_IMPORT_PLUGIN(AccessibleFactory)
-#endif
 #if defined(QT_QPA_PLATFORM_XCB)
 Q_IMPORT_PLUGIN(QXcbIntegrationPlugin);
 #elif defined(QT_QPA_PLATFORM_WINDOWS)
