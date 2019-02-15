84589e51819f8a7ef6e085541b444c8a21883bc8
Sjors Provoost <sjors@sprovoost.nl>
2019-02-15 12:54:30 +0100

    [rpc] signer: add signerprocesspsbt

--- src/wallet/externalsigner.cpp
+++ src/wallet/externalsigner.cpp
@@ -2,6 +2,10 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#include <core_io.h>
+#include <psbt.h>
+#include <util/strencodings.h>
+#include <util/system.h>
 #include <wallet/externalsigner.h>
 
 ExternalSigner::ExternalSigner(const std::string& command, const std::string& fingerprint, bool mainnet): m_command(command), m_fingerprint(fingerprint), m_mainnet(mainnet) {}
@@ -38,6 +42,47 @@ UniValue ExternalSigner::displayAddress(const std::string& descriptor)
 UniValue ExternalSigner::getDescriptors(int account)
 {
     return runCommandParseJSON(m_command + " --fingerprint \"" + m_fingerprint + "\"" + (m_mainnet ? "" : " --testnet ") + " getdescriptors --account " + std::to_string(account));
+}
+
+bool ExternalSigner::signTransaction(PartiallySignedTransaction& psbtx, std::string& error)
+{
+    // Check if signer fingerpint matches any input master key fingerprint
+    bool match = false;
+    for (unsigned int i = 0; i < psbtx.inputs.size(); ++i) {
+        const PSBTInput& input = psbtx.inputs[i];
+        for (auto entry : input.hd_keypaths) {
+            if (m_fingerprint == strprintf("%08x", ReadBE32(entry.second.fingerprint))) match = true;
+        }
+    }
+    if (!match) {
+        error = "Signer fingerprint does not match any of the inputs";
+        return false;
+    }
+
+    // Serialize the PSBT
+    CDataStream ssTx(SER_NETWORK, PROTOCOL_VERSION);
+    ssTx << psbtx;
+
+    std::string command = m_command + " --stdin --fingerprint \"" + m_fingerprint + "\"" + (m_mainnet ? "" : " --testnet ");
+    std::string stdinStr = "signtx \"" + EncodeBase64(ssTx.str()) + "\"";
+
+    const UniValue signer_result = runCommandParseJSON(command, stdinStr);
+
+    if (!find_value(signer_result, "psbt").isStr()) {
+        error = "Unexpected result from signer";
+        return false;
+    }
+
+    PartiallySignedTransaction signer_psbtx;
+    std::string signer_psbt_error;
+    if (!DecodeBase64PSBT(signer_psbtx, find_value(signer_result, "psbt").get_str(), signer_psbt_error)) {
+        error = strprintf("TX decode failed %s", signer_psbt_error);
+        return false;
+    }
+
+    psbtx = signer_psbtx;
+
+    return true;
 }
 
 #endif
--- src/wallet/rpcsigner.cpp
+++ src/wallet/rpcsigner.cpp
@@ -3,7 +3,9 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include <chainparamsbase.h>
+#include <core_io.h>
 #include <key_io.h>
+#include <psbt.h>
 #include <rpc/server.h>
 #include <rpc/util.h>
 #include <script/descriptor.h>
@@ -186,14 +188,18 @@ UniValue signerfetchkeys(const JSONRPCRequest& request)
         return NullUniValue;
     }
 
-    if (request.fHelp || request.params.size() > 2) {
+    if (request.fHelp || request.params.size() > 3) {
         throw std::runtime_error(
             RPCHelpMan{"signerfetchkeys",
                 "Obtains keys from external signer and imports them into the wallet.\n"
-                "Call enumeratesigners before using this.\n",
+                "For interoperability reasons (BIP 44, 49 and 84), it is recommended that you\n"
+                "check -addresstype and -changetype settings before calling this.\n"
+                "It is also recommended that you continue to use the same address type with this\n"
+                "wallet. Call enumeratesigners first.\n",
                 {
                     {"account",     RPCArg::Type::NUM, /* default_val */ "0", "BIP32 account to use"},
-                    {"fingerprint", RPCArg::Type::STR, /* default_val */ "", "Master key fingerprint of signer"}
+                    {"fingerprint", RPCArg::Type::STR, /* default_val */ "", "Master key fingerprint of signer"},
+                    {"range", RPCArg::Type::RANGE, /* default */ "set by -keypool", "The range of HD chain indexes to import (either end or [begin,end])"},
                 },
                 RPCResult{
                     "[{ \"success\": true }"
@@ -230,31 +236,43 @@ UniValue signerfetchkeys(const JSONRPCRequest& request)
         change_descriptors.push_back(ParseDescriptor(desc, true, true));
     }
 
+    uint64_t keypool_target_size = 0;
+    keypool_target_size = gArgs.GetArg("-keypool", DEFAULT_KEYPOOL_SIZE);
+    int64_t range_begin = 0;
+    int64_t range_end = 0;
+    if (request.params.size() >= 3 && !request.params[2].isNull()) {
+        std::tie(range_begin, range_end) = ParseDescriptorRange(request.params[2]);
+    } else {
+        range_end = keypool_target_size - 1;
+    }
+    UniValue range(UniValue::VARR);
+    range.push_back(range_begin);
+    range.push_back(range_end);
+
     // Use importmulti to process the descriptors:
     // TODO: extract reusable non-RPC code from importmulti
     UniValue importdata(UniValue::VARR);
-
-    uint64_t keypool_target_size = 0;
-    keypool_target_size = gArgs.GetArg("-keypool", DEFAULT_KEYPOOL_SIZE);
 
     if (keypool_target_size == 0) throw JSONRPCError(RPC_WALLET_ERROR, "-keypool must be > 0");
 
     UniValue receive_key_data(UniValue::VOBJ);
 
-    // Pick receive descriptor based on address_type
-    // TODO: after #15567, remove desc_prefix stuff and use desc->GetAddressType()
-    std::string desc_prefix = "";
+    // Pick receive descriptor based on -addresstype
+    AddressType address_type;
+    bool receive_segwit = false;
     switch (pwallet->m_default_address_type) {
-    case OutputType::LEGACY: {
-        desc_prefix = "pkh(";
+        case OutputType::LEGACY: {
+        address_type = AddressType::BASE58;
         break;
     }
     case OutputType::P2SH_SEGWIT: {
-        desc_prefix = "sh(wpkh(";
+        address_type = AddressType::BASE58;
+        receive_segwit = true;
         break;
     }
     case OutputType::BECH32: {
-        desc_prefix = "wpkh(";
+        address_type = AddressType::BECH32;
+        receive_segwit = true;
         break;
     }
     default:
@@ -263,7 +281,7 @@ UniValue signerfetchkeys(const JSONRPCRequest& request)
 
     std::unique_ptr<Descriptor> match_desc;
     for (auto&& desc : receive_descriptors) {
-        if (desc->ToString().find(desc_prefix) == 0) {
+        if (desc->GetAddressType() == address_type && desc->IsSegWit() == receive_segwit) {
             match_desc = std::move(desc);
             break;
         }
@@ -272,11 +290,7 @@ UniValue signerfetchkeys(const JSONRPCRequest& request)
     if (!match_desc) throw JSONRPCError(RPC_WALLET_ERROR, "No descriptor found for wallet address type");
     receive_key_data.pushKV("desc", match_desc->ToString());
 
-    UniValue receive_range(UniValue::VARR);
-    // TODO: base range start and end on what's currently in the keypool
-    receive_range.push_back(0);
-    receive_range.push_back(keypool_target_size - 1);
-    receive_key_data.pushKV("range", receive_range);
+    receive_key_data.pushKV("range", range);
     receive_key_data.pushKV("internal", false);
     receive_key_data.pushKV("keypool", true);
     receive_key_data.pushKV("watchonly", true);
@@ -284,21 +298,23 @@ UniValue signerfetchkeys(const JSONRPCRequest& request)
 
     UniValue change_key_data(UniValue::VOBJ);
 
-    // Pick change descriptor based on address_type
-    const OutputType change_type = pwallet->m_default_change_type == OutputType::CHANGE_AUTO ? pwallet->m_default_address_type : pwallet->m_default_change_type;
-
-    // TODO: after #15567, remove desc_prefix stuff and use desc->GetAddressType()
-    switch (change_type) {
-    case OutputType::LEGACY: {
-        desc_prefix = "pkh(";
+    // Pick receive descriptor based on -changetype
+    const OutputType default_change_type = pwallet->m_default_change_type == OutputType::CHANGE_AUTO ? pwallet->m_default_address_type : pwallet->m_default_change_type;
+    AddressType change_type;
+    bool change_segwit = false;
+    switch (default_change_type) {
+        case OutputType::LEGACY: {
+        change_type = AddressType::BASE58;
         break;
     }
     case OutputType::P2SH_SEGWIT: {
-        desc_prefix = "sh(wpkh(";
+        change_type = AddressType::BASE58;
+        change_segwit = true;
         break;
     }
     case OutputType::BECH32: {
-        desc_prefix = "wpkh(";
+        change_type = AddressType::BECH32;
+        change_segwit = true;
         break;
     }
     default:
@@ -307,7 +323,7 @@ UniValue signerfetchkeys(const JSONRPCRequest& request)
 
     match_desc.reset(nullptr);
     for (auto&& desc : change_descriptors) {
-        if (desc->ToString().find(desc_prefix) == 0) {
+        if (desc->GetAddressType() == change_type && desc->IsSegWit() == change_segwit) {
             match_desc = std::move(desc);
             break;
         }
@@ -316,11 +332,7 @@ UniValue signerfetchkeys(const JSONRPCRequest& request)
     if (!match_desc) throw JSONRPCError(RPC_WALLET_ERROR, "No descriptor found for wallet change address type");
     change_key_data.pushKV("desc", match_desc->ToString());
 
-    UniValue change_range(UniValue::VARR);
-    // TODO: base range start and end on what's currently in the keypool
-    change_range.push_back(0);
-    change_range.push_back(keypool_target_size - 1);
-    change_key_data.pushKV("range", change_range);
+    change_key_data.pushKV("range", range);
     change_key_data.pushKV("internal", true);
     change_key_data.pushKV("keypool", true);
     change_key_data.pushKV("watchonly", true);
@@ -349,6 +361,70 @@ UniValue signerfetchkeys(const JSONRPCRequest& request)
     return result;
 }
 
+UniValue signerprocesspsbt(const JSONRPCRequest& request)
+{
+    std::shared_ptr<CWallet> const wallet = GetWalletForJSONRPCRequest(request);
+    CWallet* const pwallet = wallet.get();
+
+    if (!EnsureWalletIsAvailable(pwallet, request.fHelp)) {
+        return NullUniValue;
+    }
+
+    if (request.fHelp || request.params.size() < 1 || request.params.size() > 2)
+        throw std::runtime_error(
+            RPCHelpMan{"signerprocesspsbt",
+                "\nSign PSBT inputs using external signer\n"
+                "that we can sign for." +
+                    HelpRequiringPassphrase(pwallet) + "\n",
+                {
+                    {"psbt", RPCArg::Type::STR, RPCArg::Optional::NO, /* default_val */ "", "The transaction base64 string"},
+                    {"fingerprint", RPCArg::Type::STR, /* default_val */ "", "master key fingerprint of signer"},
+                },
+                RPCResult{
+                    "{\n"
+                    "  \"hex\" : \"value\",           (string) The hex-encoded network transaction, if complete\n"
+                    "  \"psbt\" : \"value\",          (string) The base64-encoded partially signed transaction\n"
+                    "  \"complete\" : true|false,     (boolean) If the transaction has a complete set of signatures\n"
+                    "  ]\n"
+                    "}\n"
+                },
+                RPCExamples{
+                    HelpExampleCli("signerprocesspsbt", "\"psbt\"")
+                }
+            }.ToString()
+        );
+
+    RPCTypeCheck(request.params, {UniValue::VSTR, UniValue::VSTR});
+
+    ExternalSigner *signer = GetSignerForJSONRPCRequest(request, 1, pwallet);
+
+    // Unserialize the transaction
+    PartiallySignedTransaction psbtx;
+    std::string error;
+    if (!DecodeBase64PSBT(psbtx, request.params[0].get_str(), error)) {
+        throw JSONRPCError(RPC_DESERIALIZATION_ERROR, strprintf("PSBT decode failed %s", error));
+    }
+
+    if( !signer->signTransaction(psbtx, error)) throw JSONRPCError(RPC_WALLET_ERROR, error);
+
+    CMutableTransaction mtx;
+    bool complete = FinalizeAndExtractPSBT(psbtx, mtx);
+    CDataStream ssPsbtx(SER_NETWORK, PROTOCOL_VERSION);
+    ssPsbtx << psbtx;
+
+    UniValue result(UniValue::VOBJ);
+    if (complete) {
+        CDataStream ssTx(SER_NETWORK, PROTOCOL_VERSION);
+        std::string tx_hex;
+        ssTx << mtx;
+        tx_hex = HexStr(ssTx.str());
+        result.pushKV("hex", tx_hex);
+    }
+    result.pushKV("psbt", EncodeBase64(ssPsbtx.str()));
+    result.pushKV("complete", complete);
+    return result;
+}
+
 // clang-format off
 static const CRPCCommand commands[] =
 { //  category              name                                actor (function)                argNames
@@ -357,6 +433,7 @@ static const CRPCCommand commands[] =
     { "signer",             "signerdissociate",                 &signerdissociate,              {"fingerprint"} },
     { "signer",             "signerdisplayaddress",             &signerdisplayaddress,          {"address", "fingerprint"} },
     { "signer",             "signerfetchkeys",                  &signerfetchkeys,               {"account", "fingerprint"} },
+    { "signer",             "signerprocesspsbt",                &signerprocesspsbt,             {"psbt", "fingerprint"} },
 };
 // clang-format on
 
