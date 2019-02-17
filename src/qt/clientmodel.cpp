6cd81f358b3a1a3e400aa81a8084a890cf213a36
Luke Dashjr <luke-jr+git@utopios.org>
2019-02-17 00:19:22 +0000

    GUI: Add getTorInfo to ClientModel

--- src/qt/clientmodel.cpp
+++ src/qt/clientmodel.cpp
@@ -308,6 +308,17 @@ bool ClientModel::getProxyInfo(std::string& ip_port) const
     return false;
 }
 
+bool ClientModel::getTorInfo(QString& out_onion) const
+{
+    for (const auto& [addr, info] : m_node.getNetLocalAddresses()) {
+        if (addr.IsTor()) {
+            out_onion = QString::fromStdString(addr.ToStringAddr());
+            return true;
+        }
+    }
+    return false;
+}
+
 mempoolSamples_t ClientModel::getMempoolStatsInRange(QDateTime &from, QDateTime &to)
 {
     // get stats from the core stats model
