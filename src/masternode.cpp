118e843c9e747420de070329a57ed9ecffe8f217
sidhujag <sidhujag@gmail.com>
2019-02-21 22:05:49 -0800

    startup preenabled for 10s
    Former-commit-id: f2a165902560eb33a87596fff8b2e769d0e802d2

--- src/masternode.cpp
+++ src/masternode.cpp
@@ -198,7 +198,7 @@ void CMasternode::Check(bool fForce)
     }
     
     if (Params().NetworkIDString() != CBaseChainParams::REGTEST) {  
-        if (!masternodeSync.IsMasternodeListSynced() || !lastPing || lastPing.sigTime - sigTime < MASTERNODE_MIN_MNP_SECONDS) {  
+        if (!masternodeSync.IsMasternodeListSynced() || !lastPing || lastPing.sigTime - sigTime < MASTERNODE_MIN_MNP_SECONDS*10) {  
             nActiveState = MASTERNODE_PRE_ENABLED;  
             if (nActiveStatePrev != nActiveState) { 
                 LogPrint(BCLog::MN, "CMasternode::Check -- Masternode %s is in %s state now\n", outpoint.ToStringShort(), GetStateString());    
