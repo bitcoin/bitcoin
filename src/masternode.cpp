a4d8637c85ed74cdb05b0bfe62bdbb54c4aff078
sidhujag <sidhujag@gmail.com>
2019-02-22 10:35:26 -0800

    use adjusted time instead of lastping for preenabled check
    Former-commit-id: 6f421fc2c76c169de64632978d6a069804db5b8e

--- src/masternode.cpp
+++ src/masternode.cpp
@@ -205,7 +205,7 @@ void CMasternode::Check(bool fForce, const std::set<CScript> &payeeScripts)
     }
     
     if (Params().NetworkIDString() != CBaseChainParams::REGTEST) {  
-        if (!masternodeSync.IsMasternodeListSynced() || !lastPing || lastPing.sigTime - sigTime < MASTERNODE_MIN_MNP_SECONDS*10) {  
+        if (abs(GetAdjustedTime() - sigTime) < MASTERNODE_MIN_MNP_SECONDS*10) {  
             nActiveState = MASTERNODE_PRE_ENABLED;  
             if (nActiveStatePrev != nActiveState) { 
                 LogPrint(BCLog::MN, "CMasternode::Check -- Masternode %s is in %s state now\n", outpoint.ToStringShort(), GetStateString());    
