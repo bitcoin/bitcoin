bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

validation: Refactor OpenDiskFile into method on FlatFileSeq.

src/flatfile.h
@@ -31,6 +31,9 @@ public:
 
     /** Get the name of the file at the given position. */
     fs::path FileName(const CDiskBlockPos& pos) const;
+
+    /** Open a handle to the file at the given position. */
+    FILE* Open(const CDiskBlockPos& pos, bool read_only = false);
 };
 
 #endif // BITCOIN_FLATFILE_H

bitcoin/bitcoin

src/flatfile.h
@@ -31,6 +31,9 @@ public:
 
     /** Get the name of the file at the given position. */
     fs::path FileName(const CDiskBlockPos& pos) const;
+
+    /** Open a handle to the file at the given position. */
+    FILE* Open(const CDiskBlockPos& pos, bool fReadOnly = false);
 };
 
 #endif // BITCOIN_FLATFILE_H