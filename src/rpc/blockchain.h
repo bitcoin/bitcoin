fa5dc3534be1d58bbca641a32cd87cd97ce8882a
MarcoFalke <falke.marco@gmail.com>
2019-02-23 11:04:20 -0500

    rpc: Pass mempool into MempoolToJSON

--- src/rpc/blockchain.h
+++ src/rpc/blockchain.h
@@ -11,6 +11,7 @@
 
 class CBlock;
 class CBlockIndex;
+class CTxMemPool;
 class UniValue;
 
 static constexpr int NUM_GETBLOCKSTATS_PERCENTILES = 5;
@@ -30,10 +31,10 @@ void RPCNotifyBlockChange(bool ibd, const CBlockIndex *);
 UniValue blockToJSON(const CBlock& block, const CBlockIndex* tip, const CBlockIndex* blockindex, bool txDetails = false);
 
 /** Mempool information to JSON */
-UniValue mempoolInfoToJSON();
+UniValue MempoolInfoToJSON(const CTxMemPool& pool);
 
 /** Mempool to JSON */
-UniValue mempoolToJSON(bool fVerbose = false);
+UniValue MempoolToJSON(const CTxMemPool& pool, bool verbose = false);
 
 /** Block header to JSON */
 UniValue blockheaderToJSON(const CBlockIndex* tip, const CBlockIndex* blockindex);
