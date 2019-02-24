570eae77629f0f882c4e8983fa2b13a4e558893c
Matt Corallo <git@bluematt.me>
2019-02-24 18:01:46 -0500

    Ensure the script-fail reason is correct on mandatory flags
    Previously, if we fail both due to a
    STANDARD_NOT_MANDATORY_SCRIPT_VERIFY_FLAGS and a
    MANDATORY_SCRIPT_VERIFY_FLAGS, we would potentially return the
    non-mandatory error as the reason for
    mandatory-script-verify-flag-failed. This ensures we always return
    the correct error.

--- src/validation.cpp
+++ src/validation.cpp
@@ -1425,8 +1425,11 @@ bool CheckInputs(const CTransaction& tx, CValidationState &state, const CCoinsVi
                         // non-upgraded nodes.
                         CScriptCheck check2(coin.out, tx, i,
                                 flags & ~STANDARD_NOT_MANDATORY_VERIFY_FLAGS, cacheSigStore, &txdata);
-                        if (check2())
+                        if (check2()) {
                             return state.Invalid(false, REJECT_NONSTANDARD, strprintf("non-mandatory-script-verify-flag (%s)", ScriptErrorString(check.GetScriptError())));
+                        } else {
+                            return state.DoS(100, false, REJECT_INVALID, strprintf("mandatory-script-verify-flag-failed (%s)", ScriptErrorString(check2.GetScriptError())));
+                        }
                     }
                     // Failures of other flags indicate a transaction that is
                     // invalid in new blocks, e.g. an invalid P2SH. We DoS ban
