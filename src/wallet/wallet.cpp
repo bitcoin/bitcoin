c8ff65e8f59cc0ad4c29d4648f11c2c317d71591
Sjors Provoost <sjors@sprovoost.nl>
2019-03-01 13:14:08 +0100

    [wallet] remove unused GetScriptForMining

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -3819,17 +3819,6 @@ void CWallet::MarkReserveKeysAsUsed(int64_t keypool_id)
     }
 }
 
-void CWallet::GetScriptForMining(std::shared_ptr<CReserveScript> &script)
-{
-    std::shared_ptr<CReserveKey> rKey = std::make_shared<CReserveKey>(this);
-    CPubKey pubkey;
-    if (!rKey->GetReservedKey(pubkey))
-        return;
-
-    script = rKey;
-    script->reserveScript = CScript() << ToByteVector(pubkey) << OP_CHECKSIG;
-}
-
 void CWallet::LockCoin(const COutPoint& output)
 {
     AssertLockHeld(cs_wallet); // setLockedCoins
