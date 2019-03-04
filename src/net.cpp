1f207b8ed32b9478881c60e7ad6a1c9d1132020e
Ashot Khachatryan <ashot@crown.tech>
2019-03-04 12:58:09 +0400

    Changed 'staking' to be true by default #308

--- src/net.cpp
+++ src/net.cpp
@@ -1761,7 +1761,7 @@ void StartNode(boost::thread_group& threadGroup)
     threadGroup.create_thread(boost::bind(&LoopForever<void (*)()>, "dumpaddr", &DumpAddresses, DUMP_ADDRESSES_INTERVAL * 1000));
 
     // Stake miner thread
-    if (GetBoolArg("-staking", false))
+    if (GetBoolArg("-staking", true))
         threadGroup.create_thread(boost::bind(&TraceThread<void (*)()>, "stake-miner", &ThreadStakeMiner));
 
 }
