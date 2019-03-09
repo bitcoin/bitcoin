527f1ec69de55fbe3c78ac8fe8f73f7e622bf960
Sjors Provoost <sjors@sprovoost.nl>
2019-03-09 17:06:18 +0100

    [rpc] signer: GetSignerForJSONRPCRequest

--- src/wallet/rpcsigner.h
+++ src/wallet/rpcsigner.h
@@ -11,6 +11,21 @@
 #ifdef HAVE_BOOST_PROCESS
 
 class CRPCTable;
+class CWallet;
+class ExternalSigner;
+class JSONRPCRequest;
+
+/**
+ * Figures out what external signer to use for a JSONRPCRequest. If no fingerprint
+ * is specified and there is only one signer, it returns that.
+ *
+ * @param[in] request   JSONRPCRequest that wishes to access a signer
+ * @param[in] index     request.params[index] contains the signer fingerprint
+ * @param[in] pwallet   CWallet pointer to the wallet
+ *
+ * @return nullptr if no signer is found, or a pointer to the ExternalSigner
+ */
+ExternalSigner *GetSignerForJSONRPCRequest(const JSONRPCRequest& request, int index, CWallet* pwallet);
 
 namespace interfaces {
 class Chain;
