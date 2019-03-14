73d62d8b5c979bb3324bddd3fe8286c0d0245833
Sjors Provoost <sjors@sprovoost.nl>
2019-03-14 11:18:02 +0100

    Add IsSegWit() to Descriptor

--- src/script/descriptor.cpp
+++ src/script/descriptor.cpp
@@ -428,6 +428,14 @@ public:
         return nullopt;
     }
 
+    bool IsSegWit() const override
+    {
+        if (m_subdescriptor_arg) {
+            if (m_subdescriptor_arg->IsSegWit()) return true;
+        }
+        return false;
+    }
+
     bool ExpandHelper(int pos, const SigningProvider& arg, Span<const unsigned char>* cache_read, std::vector<CScript>& output_scripts, FlatSigningProvider& out, std::vector<unsigned char>* cache_write) const
     {
         std::vector<std::pair<CPubKey, KeyOriginInfo>> entries;
@@ -573,6 +581,8 @@ public:
     {
         return AddressType::BECH32;
     }
+
+    bool IsSegWit() const final { return true; }
 };
 
 /** A parsed combo(P) descriptor. */
@@ -643,6 +653,8 @@ public:
     {
         return AddressType::BECH32;
     }
+
+    bool IsSegWit() const final { return true; }
 };
 
 ////////////////////////////////////////////////////////////////////////////
