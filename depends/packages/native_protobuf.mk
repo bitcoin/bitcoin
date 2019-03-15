f334f63d3dbf2b134058100c2787a22bc8236f0f
MarcoFalke <falke.marco@gmail.com>
2019-03-15 09:48:21 -0400

    Merge #15580: depends: native_protobuf: avoid system zlib
    19a0c4af0f depends: native_protobuf: avoid system zlib (Carl Dong)
    
    Pull request description:
    
      I don't believe we use any zlib features in protobufs
    
    Tree-SHA512: cd09229f3fac215f58e9ddd4871f190cf2a301e25939aaa1c6ee130d1ba5bbb00d9ebe9ca012a2894bac4c2db923259f34fe43e255ad55ccd2b11ec88afc2a8f

--- depends/packages/native_protobuf.mk
+++ depends/packages/native_protobuf.mk
@@ -5,7 +5,7 @@ $(package)_file_name=protobuf-$($(package)_version).tar.bz2
 $(package)_sha256_hash=ee445612d544d885ae240ffbcbf9267faa9f593b7b101f21d58beceb92661910
 
 define $(package)_set_vars
-$(package)_config_opts=--disable-shared
+$(package)_config_opts=--disable-shared --without-zlib
 endef
 
 define $(package)_config_cmds
