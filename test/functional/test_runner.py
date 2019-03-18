1935ab79b7fef1af54457b4ebe67a9b27319ba10
MarcoFalke <falke.marco@gmail.com>
2019-03-18 11:26:03 -0400

    Merge #15466: tests: Print remaining jobs in test_runner.py
    2e5d482659 tests: Print remaining jobs in test_runner.py (Steven Roose)
    
    Pull request description:
    
      This helps finding out which tests fail to finish.
    
    Tree-SHA512: d22beb82beecd33aaa50731c83075e49577842d29fd21aa63bcb859df5da99069eba9cc16eed5d91dbba8fb0fdc317fb88b3b370c4d3917e9da1cd13b0a622dc

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -505,6 +505,11 @@ class TestHandler:
                               log_stderr))
         if not self.jobs:
             raise IndexError('pop from empty list')
+
+        # Print remaining running jobs when all jobs have been started.
+        if not self.test_list:
+            print("Remaining jobs: [{}]".format(", ".join(j[0] for j in self.jobs)))
+
         dot_count = 0
         while True:
             # Return first proc that finishes
