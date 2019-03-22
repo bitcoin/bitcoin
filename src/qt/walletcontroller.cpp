167363d4bd7e5d69f4a494305755232491abb685
Jonas Schnelli <dev@jonasschnelli.ch>
2019-03-22 11:07:35 +0100

    Merge #15614: gui: Defer removeAndDeleteWallet when no modal widget is active
    a10972bc0 gui: Defer removeAndDeleteWallet when no modal widget is active (João Barbosa)
    
    Pull request description:
    
      Fixes #15310.
    
    Tree-SHA512: ac91a4e37020d3a854830c50c0a7a45c2c0537f80be492ec5e9ba7daf90725e912f9dcc324605493599c36180e1d3bcdfa86840b7325cba208b7e93fbe7be368

--- src/qt/walletcontroller.cpp
+++ src/qt/walletcontroller.cpp
@@ -9,8 +9,10 @@
 
 #include <algorithm>
 
+#include <QApplication>
 #include <QMutexLocker>
 #include <QThread>
+#include <QWindow>
 
 WalletController::WalletController(interfaces::Node& node, OptionsModel* options_model, QObject* parent)
     : QObject(parent)
@@ -55,7 +57,17 @@ WalletModel* WalletController::getOrCreateWallet(std::unique_ptr<interfaces::Wal
     m_wallets.push_back(wallet_model);
 
     connect(wallet_model, &WalletModel::unload, [this, wallet_model] {
-        removeAndDeleteWallet(wallet_model);
+        // Defer removeAndDeleteWallet when no modal widget is active.
+        // TODO: remove this workaround by removing usage of QDiallog::exec.
+        if (QApplication::activeModalWidget()) {
+            connect(qApp, &QApplication::focusWindowChanged, wallet_model, [this, wallet_model]() {
+                if (!QApplication::activeModalWidget()) {
+                    removeAndDeleteWallet(wallet_model);
+                }
+            }, Qt::QueuedConnection);
+        } else {
+            removeAndDeleteWallet(wallet_model);
+        }
     });
 
     // Re-emit coinsSent signal from wallet model.
