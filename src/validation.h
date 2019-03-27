78ed18ca6874eb15d230d60b8993d242b2584323
James O'Beirne <james.obeirne@gmail.com>
2019-03-27 13:54:18 -0400

    refactoring: remove unused chainActive

--- src/validation.h
+++ src/validation.h
@@ -435,9 +435,6 @@ bool InvalidateBlock(CValidationState& state, const CChainParams& chainparams, C
 /** Remove invalidity status from a block and its descendants. */
 void ResetBlockFailureFlags(CBlockIndex* pindex) EXCLUSIVE_LOCKS_REQUIRED(cs_main);
 
-/** The currently-connected chain of blocks (protected by cs_main). */
-extern CChain& chainActive;
-
 /** @returns the most-work chain. */
 CChain& ChainActive();
 
