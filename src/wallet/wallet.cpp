59716ec395daaf914924fe5c1a4fbeb5d5031907
João Barbosa <joao.paulo.barbosa@gmail.com>
2019-03-27 17:00:08 +0000

    wallet: Update transactions with current mempool after load
    Github-Pull: #15652
    Rebased-From: 2ebf650b2eb7a078ab60c8c4d5c726823686f549

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -4429,6 +4429,9 @@ void CWallet::postInitProcess()
     // Add wallet transactions that aren't already in a block to mempool
     // Do this here as mempool requires genesis block to be loaded
     ReacceptWalletTransactions(*locked_chain);
+
+    // Update wallet transactions with current mempool transactions.
+    chain().requestMempoolTransactions([this](const CTransactionRef& tx) { TransactionAddedToMempool(tx); });
 }
 
 bool CWallet::BackupWallet(const std::string& strDest)
