32def8d1c29e0855fe5429687acabd2f29119316
Peter Bushnell <bushsolo@gmail.com>
2019-03-31 14:47:54 +0100

    Catch ios_base::failure specifically

--- src/wallet/walletdb.cpp
+++ src/wallet/walletdb.cpp
@@ -277,7 +277,7 @@ ReadKeyValue(CWallet* pwallet, CDataStream& ssKey, CDataStream& ssValue,
             {
                 ssValue >> hash;
             }
-            catch (...) {}
+            catch (const std::ios_base::failure&) {}
 
             bool fSkipCheck = false;
 
