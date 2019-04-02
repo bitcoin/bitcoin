b9b0d939fb4f42ece5692aa8bb00cfce2851892b
John Newbery <john@johnnewbery.com>
2019-04-02 17:11:26 -0400

    [build] Move wallet load functions to wallet/load unit
    Moves the following wallet load functions to a new wallet/load unit in
    the libbitcoin_wallet library. All other functions in wallet/init remain
    in libbitcoin_server:
    
    - `VerifyWallets`
    - `LoadWallets`
    - `StartWallets`
    - `FlushWallets`
    - `StopWallets`
    - `UnloadWallets`

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -35,26 +35,6 @@
 #include <utility>
 #include <vector>
 
-//! Responsible for reading and validating the -wallet arguments and verifying the wallet database.
-//! This function will perform salvage on the wallet if requested, as long as only one wallet is
-//! being loaded (WalletParameterInteraction forbids -salvagewallet, -zapwallettxes or -upgradewallet with multiwallet).
-bool VerifyWallets(interfaces::Chain& chain, const std::vector<std::string>& wallet_files);
-
-//! Load wallet databases.
-bool LoadWallets(interfaces::Chain& chain, const std::vector<std::string>& wallet_files);
-
-//! Complete startup of wallets.
-void StartWallets(CScheduler& scheduler);
-
-//! Flush all wallets in preparation for shutdown.
-void FlushWallets();
-
-//! Stop all wallets. Wallets will be flushed first.
-void StopWallets();
-
-//! Close all wallets.
-void UnloadWallets();
-
 //! Explicitly unload and delete the wallet.
 //! Blocks the current thread after signaling the unload intent so that all
 //! wallet clients release the wallet.
