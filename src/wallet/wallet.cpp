ccb26cf347a6fcc861768bbfbbd652e683ffd5bb
Andrew Chow <achow101-github@achow101.com>
2019-04-03 18:57:33 -0400

    Batch writes for importmulti
    When writing all of the imported data to the wallet, use a common
    WalletBatch object so that batch writes are done and the writes
    finish more quickly.
    
    AddKeypoolPubkey is no longer needed so it is also removed

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -3372,13 +3372,6 @@ bool CWallet::TopUpKeyPool(unsigned int kpSize)
     return true;
 }
 
-void CWallet::AddKeypoolPubkey(const CPubKey& pubkey, const bool internal)
-{
-    WalletBatch batch(*database);
-    AddKeypoolPubkeyWithDB(pubkey, internal, batch);
-    NotifyCanGetAddressesChanged();
-}
-
 void CWallet::AddKeypoolPubkeyWithDB(const CPubKey& pubkey, const bool internal, WalletBatch& batch)
 {
     LOCK(cs_wallet);
