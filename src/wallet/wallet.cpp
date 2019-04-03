a18d0ef7fd597452b350e3554e999b0a0f293766
John Newbery <john@johnnewbery.com>
2019-04-03 17:51:23 -0400

    [wallet] Add doxygen comment to CWallet::CommitTransaction()
    Former-commit-id: c014fcc64bfd2ea8b07a4f536266b356b5d3a4d7

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -3350,9 +3350,6 @@ bool CWallet::CreateTransaction(interfaces::Chain::Lock& locked_chain, const std
     return true;
 }
 
-/**
- * Call after CreateTransaction unless you want to abort
- */
 bool CWallet::CommitTransaction(CTransactionRef tx, mapValue_t mapValue, std::vector<std::pair<std::string, std::string>> orderForm, CValidationState& state)
 {
 	if (!CheckSyscoinLockedOutpoints(tx, state))
