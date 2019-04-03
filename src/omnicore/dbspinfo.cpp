271c2cd274ceac3529cd61d279f952bc7d445193
dexX7 <dexx@bitwatch.co>
2019-04-03 08:08:10 +0200

    Rename "(Test) Omni" to "(Test) Omni tokens"

--- src/omnicore/dbspinfo.cpp
+++ src/omnicore/dbspinfo.cpp
@@ -60,17 +60,17 @@ CMPSPInfo::CMPSPInfo(const boost::filesystem::path& path, bool fWipe)
     implied_omni.num_tokens = 700000;
     implied_omni.category = "N/A";
     implied_omni.subcategory = "N/A";
-    implied_omni.name = "Omni";
+    implied_omni.name = "Omni tokens";
     implied_omni.url = "http://www.omnilayer.org";
-    implied_omni.data = "Omni serve as the binding between Bitcoin, smart properties and contracts created on the Omni Layer.";
+    implied_omni.data = "Omni tokens serve as the binding between Bitcoin, smart properties and contracts created on the Omni Layer.";
     implied_tomni.issuer = ExodusAddress().ToString();
     implied_tomni.prop_type = MSC_PROPERTY_TYPE_DIVISIBLE;
     implied_tomni.num_tokens = 700000;
     implied_tomni.category = "N/A";
     implied_tomni.subcategory = "N/A";
-    implied_tomni.name = "Test Omni";
+    implied_tomni.name = "Test Omni tokens";
     implied_tomni.url = "http://www.omnilayer.org";
-    implied_tomni.data = "Test Omni serve as the binding between Bitcoin, smart properties and contracts created on the Omni Layer.";
+    implied_tomni.data = "Test Omni tokens serve as the binding between Bitcoin, smart properties and contracts created on the Omni Layer.";
 
     init();
 }
