b6f486a02b463ffeaf82ec11fc6f74f439c037ae
John Newbery <john@johnnewbery.com>
2019-04-03 17:51:23 -0400

    [wallet] Add doxygen comment to CWallet::CommitTransaction()

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -3286,9 +3286,6 @@ bool CWallet::CreateTransaction(interfaces::Chain::Lock& locked_chain, const std
     return true;
 }
 
-/**
- * Call after CreateTransaction unless you want to abort
- */
 bool CWallet::CommitTransaction(CTransactionRef tx, mapValue_t mapValue, std::vector<std::pair<std::string, std::string>> orderForm, CValidationState& state)
 {
     auto locked_chain = chain().lock();
