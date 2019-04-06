83ce9ef74b429ce6da13349c81dcdda4c6f3f07b
Pieter Wuille <pieter.wuille@gmail.com>
2019-04-06 08:23:19 -0700

    Take non-importing keys into account for spendability warning in descriptor import

--- test/functional/wallet_importmulti.py
+++ test/functional/wallet_importmulti.py
@@ -629,7 +629,8 @@ class ImportMultiTest(BitcoinTestFramework):
         self.log.info("Should import a 1-of-2 bare multisig from descriptor")
         self.test_importmulti({"desc": descsum_create("multi(1," + key1.pubkey + "," + key2.pubkey + ")"),
                                "timestamp": "now"},
-                              success=True)
+                              success=True,
+                              warnings=["Some private keys are missing, outputs will be considered watchonly. If this is intentional, specify the watchonly flag."])
         self.log.info("Should not treat individual keys from the imported bare multisig as watchonly")
         test_address(self.nodes[1],
                      key1.p2pkh_addr,
