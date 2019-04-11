362eb3bad6b431e907019a9cbe2e4ec1b59f9605
Antoine Riard <ariard@student.42.fr>
2019-04-11 16:04:37 +0000

    Remove unused submitToMemoryPool and relayTransactions Chain interfaces
    Former-commit-id: 45cbcaed8e1ab8ed9047834b8a7f32297d91a42d

--- src/interfaces/chain.cpp
+++ src/interfaces/chain.cpp
@@ -294,14 +294,6 @@ public:
         // that Chain clients do not need to know about.
         return TransactionError::OK == err;
     }
-    bool broadcastTransaction(const CTransactionRef& tx, std::string& err_string, const CAmount& max_tx_fee, bool relay) override
-    {
-        const TransactionError err = BroadcastTransaction(tx, err_string, max_tx_fee, relay, /*wait_callback*/ false);
-        // Chain clients only care about failures to accept the tx to the mempool. Disregard non-mempool related failures.
-        // Note: this will need to be updated if BroadcastTransactions() is updated to return other non-mempool failures
-        // that Chain clients do not need to know about.
-        return TransactionError::OK == err;
-    }
     void getTransactionAncestry(const uint256& txid, size_t& ancestors, size_t& descendants) override
     {
         ::mempool.GetTransactionAncestry(txid, ancestors, descendants);
