0e752637a26cf75187864a466db9a92540a2d3c8
Carl Dong <accounts@carldong.me>
2019-04-19 17:21:58 -0400

    depends: libXext: Bump to 1.3.3 to fix _XEatDataWords
    Compilation error for _XEatDataWords fixed in bb24f29, first included in
    libXext 1.3.3.

--- depends/packages/libXext.mk
+++ depends/packages/libXext.mk
@@ -1,8 +1,8 @@
 package=libXext
-$(package)_version=1.3.2
+$(package)_version=1.3.3
 $(package)_download_path=https://xorg.freedesktop.org/releases/individual/lib/
 $(package)_file_name=$(package)-$($(package)_version).tar.bz2
-$(package)_sha256_hash=f829075bc646cdc085fa25d98d5885d83b1759ceb355933127c257e8e50432e0
+$(package)_sha256_hash=b518d4d332231f313371fdefac59e3776f4f0823bcb23cf7c7305bfb57b16e35
 $(package)_dependencies=xproto xextproto libX11 libXau
 
 define $(package)_set_vars
