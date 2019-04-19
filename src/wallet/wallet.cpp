fadf7d13905d76ef4d2d8b9b96a6b9269f78fbba
MarcoFalke <falke.marco@gmail.com>
2019-04-19 09:53:48 -0400

    wallet: Remove unused import checkpoints.h

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -1,12 +1,11 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2018 The Bitcoin Core developers
+// Copyright (c) 2009-2019 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include <wallet/wallet.h>
 
 #include <chain.h>
-#include <checkpoints.h>
 #include <consensus/consensus.h>
 #include <consensus/validation.h>
 #include <fs.h>
