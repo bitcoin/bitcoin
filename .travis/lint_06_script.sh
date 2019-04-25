36361443fce93ff3581ed273c49ec1213073ae3a
sidhujag <sidhujag@gmail.com>
2019-04-25 12:31:09 -0700

    travis: remove subtree check
    Former-commit-id: e71aa6d4b07fc9d2421d764c2a0405183cd99ead

--- .travis/lint_06_script.sh
+++ .travis/lint_06_script.sh
@@ -10,10 +10,6 @@ if [ "$TRAVIS_EVENT_TYPE" = "pull_request" ]; then
   test/lint/commit-script-check.sh $TRAVIS_COMMIT_RANGE
 fi
 
-test/lint/git-subtree-check.sh src/crypto/ctaes
-test/lint/git-subtree-check.sh src/secp256k1
-test/lint/git-subtree-check.sh src/univalue
-test/lint/git-subtree-check.sh src/leveldb
 test/lint/check-doc.py
 test/lint/check-rpc-mappings.py .
 test/lint/lint-all.sh
