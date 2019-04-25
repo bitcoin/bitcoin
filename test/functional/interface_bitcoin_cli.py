efcb5574883e884ed8949025825185a460132ac4
Luke Dashjr <luke-jr+git@utopios.org>
2019-04-25 09:11:12 +0000

    QA: feature_filelock, interface_bitcoin_cli: Use PACKAGE_NAME in messages rather than hardcoding Bitcoin Core

--- test/functional/interface_bitcoin_cli.py
+++ test/functional/interface_bitcoin_cli.py
@@ -16,7 +16,7 @@ class TestBitcoinCli(BitcoinTestFramework):
         """Main test logic"""
 
         cli_response = self.nodes[0].cli("-version").send_cli()
-        assert "Bitcoin Core RPC client version" in cli_response
+        assert "{} RPC client version".format(self.config['environment']['PACKAGE_NAME']) in cli_response
 
         self.log.info("Compare responses from getwalletinfo RPC and `bitcoin-cli getwalletinfo`")
         if self.is_wallet_compiled():
