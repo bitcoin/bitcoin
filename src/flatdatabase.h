0daed744c15d8c257363d1daee65ed331f23af97
sidhujag <sidhujag@gmail.com>
2019-04-25 19:33:46 -0700

    fix ifdef
    Former-commit-id: 2244805641fa52a80f8d2138ba357bb3d5acef2c

--- src/flatdatabase.h
+++ src/flatdatabase.h
@@ -3,8 +3,8 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-  #ifndef SYSCOIN_FLATDATABASE_H
-  #define SYSCOIN_FLATDATABASE_H
+#ifndef SYSCOIN_FLATDATABASE_H
+#define SYSCOIN_FLATDATABASE_H
 
 #include "chainparams.h"
 #include "clientversion.h"
