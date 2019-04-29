83d5be0d7e15708850ebf3888cde3be9904e1a30
Jonas Schnelli <dev@jonasschnelli.ch>
2019-04-29 09:04:20 +0200

    Merge #15371: gui: Uppercase bech32 addresses in qr codes
    Former-commit-id: 9670f030d99e9a7771a3e93252a5bf09a49935d6

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -175,7 +175,11 @@ bool parseSyscoinURI(QString uri, SendCoinsRecipient *out)
 
 QString formatSyscoinURI(const SendCoinsRecipient &info)
 {
-    QString ret = QString("syscoin:%1").arg(info.address);
+
+    bool bech_32 = info.address.startsWith(QString::fromStdString(Params().Bech32HRP() + "1"));
+
+    QString ret = QString("syscoin:%1").arg(bech_32 ? info.address.toUpper() : info.address);
+
     int paramCount = 0;
 
     if (info.amount)
