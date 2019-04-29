96ebf40b24c2b2a42986207e737bc08cd9abd1da
MarcoFalke <falke.marco@gmail.com>
2019-04-29 08:45:44 -0400

    Merge #15913: Bugfix: dummywallet: Add -ignorepartialspends to list of ignored wallet options
    765d5890be Bugfix: dummywallet: Add -ignorepartialspends to list of ignored wallet options (Luke Dashjr)
    0f09eb779d dummywallet: Reformat ignored wallet options list (Luke Dashjr)
    
    Pull request description:
    
      When building w/o wallet support, we add all the wallet options as hidden options to avoid throwing errors/warnings that they're unknown.
    
      `-ignorepartialspends` is missing from this list. This PR adds it.
    
      (This seems like a good candidate for a linter? Or maybe we can autogenerate it?)
    
      Also reformats the dummywallet options list across multiple lines to make conflicts less often.
    
    ACKs for commit 765d58:
      meshcollider:
        utACK https://github.com/bitcoin/bitcoin/pull/15913/commits/765d5890be46ed5f5557c1d2b3cb931e6b8eb3b0
      MarcoFalke:
        utACK 765d5890be46ed5f5557c1d2b3cb931e6b8eb3b0
      promag:
        utACK 765d589.
      practicalswift:
        utACK 765d5890be46ed5f5557c1d2b3cb931e6b8eb3b0
      fanquake:
        utACK 765d589
    
    Tree-SHA512: 37c8037148bdc1b7a8bde201eff51ee6a64c042c17eb8b6c68faef490d16575348c2f22ab81f48302b8ad80a5559222af23b721a8b5acc1d89c0757fb88796a6
    Signed-off-by: pasta <pasta@dashboost.org>

--- src/dummywallet.cpp
+++ src/dummywallet.cpp
@@ -33,17 +33,49 @@ public:
 
 void DummyWalletInit::AddWalletOptions() const
 {
-    std::vector<std::string> opts = {"-createwalletbackups=<n>", "-disablewallet", "-instantsendnotify=<cmd>",
-        "-keypool=<n>", "-rescan=<mode>", "-salvagewallet", "-spendzeroconfchange", "-upgradewallet",
-        "-wallet=<path>", "-walletbackupsdir=<dir>", "-walletbroadcast", "-walletdir=<dir>",
-        "-walletnotify=<cmd>", "-zapwallettxes=<mode>", "-discardfee=<amt>", "-fallbackfee=<amt>",
-        "-mintxfee=<amt>", "-paytxfee=<amt>", "-txconfirmtarget=<n>", "-hdseed=<hex>", "-mnemonic=<text>",
-        "-mnemonicpassphrase=<text>", "-usehd", "-keepass", "-keepassid=<id>", "-keepasskey=<key>",
-        "-keepassname=<name>", "-keepassport=<port>", "-enablecoinjoin", "-coinjoinamount=<n>",
-        "-coinjoinautostart", "-coinjoindenomsgoal=<n>", "-coinjoindenomshardcap=<n>", "-coinjoinmultisession",
-        "-coinjoinrounds=<n>", "-coinjoinsessions=<n>", "-dblogsize=<n>", "-flushwallet", "-privdb",
-        "-walletrejectlongchains"};
-    gArgs.AddHiddenArgs(opts);
+    gArgs.AddHiddenArgs({
+        "-avoidpartialspends",
+        "-createwalletbackups=<n>",
+        "-disablewallet",
+        "-instantsendnotify=<cmd>",
+        "-keypool=<n>",
+        "-rescan=<mode>",
+        "-salvagewallet",
+        "-spendzeroconfchange",
+        "-upgradewallet",
+        "-wallet=<path>",
+        "-walletbackupsdir=<dir>",
+        "-walletbroadcast",
+        "-walletdir=<dir>",
+        "-walletnotify=<cmd>",
+        "-zapwallettxes=<mode>",
+        "-discardfee=<amt>",
+        "-fallbackfee=<amt>",
+        "-mintxfee=<amt>",
+        "-paytxfee=<amt>",
+        "-txconfirmtarget=<n>",
+        "-hdseed=<hex>",
+        "-mnemonic=<text>",
+        "-mnemonicpassphrase=<text>",
+        "-usehd",
+        "-keepass",
+        "-keepassid=<id>",
+        "-keepasskey=<key>",
+        "-keepassname=<name>",
+        "-keepassport=<port>",
+        "-enablecoinjoin",
+        "-coinjoinamount=<n>",
+        "-coinjoinautostart",
+        "-coinjoindenomsgoal=<n>",
+        "-coinjoindenomshardcap=<n>",
+        "-coinjoinmultisession",
+        "-coinjoinrounds=<n>",
+        "-coinjoinsessions=<n>",
+        "-dblogsize=<n>",
+        "-flushwallet",
+        "-privdb",
+        "-walletrejectlongchains"
+    });
 }
 
 const WalletInitInterface& g_wallet_init_interface = DummyWalletInit();
