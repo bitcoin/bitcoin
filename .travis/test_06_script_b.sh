d7743903239c22ba0bb3cd697a2af5802023e0c7
MarcoFalke <falke.marco@gmail.com>
2019-05-02 16:48:05 -0400

    tests: Fail if RPC has been added without tests
    Former-commit-id: cdcba4d2679c8c33a150a1a3e7e2a61eb212942c

--- .travis/test_06_script_b.sh
+++ .travis/test_06_script_b.sh
@@ -16,7 +16,7 @@ fi
 
 if [ "$RUN_FUNCTIONAL_TESTS" = "true" ]; then
   BEGIN_FOLD functional-tests
-  DOCKER_EXEC test/functional/test_runner.py --ci --combinedlogslen=4000 --coverage --quiet --failfast
+  DOCKER_EXEC test/functional/test_runner.py --ci --combinedlogslen=4000 ${TEST_RUNNER_EXTRA} --quiet --failfast
   END_FOLD
 fi
 
