7b29ec277b2f99a526c2a70f0dec7b28f1e874a4
John Newbery <john@johnnewbery.com>
2019-05-03 12:47:27 -0400

    [tests] Comment for why logging config is set as command-line args.

--- test/functional/test_framework/test_node.py
+++ test/functional/test_framework/test_node.py
@@ -80,6 +80,9 @@ class TestNode():
         # For those callers that need more flexibility, they can just set the args property directly.
         # Note that common args are set in the config file (see initialize_datadir)
         self.extra_args = extra_args
+        # Configuration for logging is set as command-line args rather than in the bitcoin.conf file.
+        # This means that starting a bitcoind using the temp dir to debug a failed test won't
+        # spam debug.log.
         self.args = [
             self.binary,
             "-datadir=" + self.datadir,
