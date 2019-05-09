7ccf9dc2bd05caa0952c6e7ed438e9b265d808c1
Gregory Sanders <gsanders87@gmail.com>
2019-05-09 16:31:57 -0400

    deprecate totalFee argument in bumpfee RPC call

--- test/functional/wallet_bumpfee.py
+++ test/functional/wallet_bumpfee.py
@@ -37,6 +37,7 @@ class BumpFeeTest(BitcoinTestFramework):
         self.extra_args = [[
             "-walletrbf={}".format(i),
             "-mintxfee=0.00002",
+            "-deprecatedrpc=totalFee",
         ] for i in range(self.num_nodes)]
 
     def skip_test_if_missing_module(self):
