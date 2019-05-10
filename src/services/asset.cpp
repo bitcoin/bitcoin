b36845add9b18d9510f2ec9f2fe39e2a29aef706
sidhujag <sidhujag@hotmail.com>
2019-05-10 15:16:59 -0700

    dbg
    Former-commit-id: b7d139bc8ce64a2bbdf76858d35cd07422db8fe1

--- src/services/asset.cpp
+++ src/services/asset.cpp
@@ -1207,9 +1207,10 @@ UniValue assetupdate(const JSONRPCRequest& request) {
     theAsset.witnessAddress = copyWitness;
 	UniValue params3 = params[3];
 	CAmount nBalance = 0;
+	LogPrintf("params3 %s\n", params3.write().c_str());
 	if((params3.isStr() && params3.get_str() != "0") || (params3.isNum() && params3.get_int64() != 0))
 		nBalance = AssetAmountFromValue(params3, theAsset.nPrecision);
-	
+	LogPrintf("after parse\n");
 	if(strPubData != stringFromVch(theAsset.vchPubData))
 		theAsset.vchPubData = vchFromString(strPubData);
     else
