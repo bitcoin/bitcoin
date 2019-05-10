34ca96e6a9ca963ac7caa42cffaf6b49a83c1fda
sidhujag <sidhujag@gmail.com>
2019-05-09 22:52:14 -0700

    Revert "refactoring: introduce unused ChainActive()"
    This reverts commit 0cbf6aff3f65c7b3e097cf44695cf0ed6b3bef92 [formerly cb154adca87b893652c98936b409c72695d3a9a3].
    
    
    Former-commit-id: 305591446b8ef5f8ff8caae162f4cd57be201981

--- src/validation.h
+++ src/validation.h
@@ -506,9 +506,6 @@ void ResetBlockFailureFlags(CBlockIndex* pindex) EXCLUSIVE_LOCKS_REQUIRED(cs_mai
 /** @returns the most-work chain. */
 CChain& ChainActive();
 
-/** @returns the most-work chain. */
-CChain& ChainActive();
-
 /** Global variable that points to the coins database (protected by cs_main) */
 extern std::unique_ptr<CCoinsViewDB> pcoinsdbview;
 
