661222fb57c2d51cee26fd9ebc725a143b539220
Wladimir J. van der Laan <laanwj@gmail.com>
2019-05-16 20:17:53 +0200

    Merge #15970: Utils and libraries: fix static_assert for macro HAVE_THREAD_LOCAL
    41ab2a892456f4f3e27128a7f9237bc45cc288dd fix static_assert for macro HAVE_THREAD_LOCAL (orient)
    
    Pull request description:
    
      Utils and libraries: fix static_assert for macro HAVE_THREAD_LOCAL if define DEBUG_LOCKCONTENTION
    
    ACKs for commit 41ab2a:
      laanwj:
        utACK 41ab2a892456f4f3e27128a7f9237bc45cc288dd
    
    Tree-SHA512: 6c1d20375a70cbdef1140c544f443106d6bf6c34b1da2ddc66739f2b662a0d6b800288f48bf451a3d5937bac7e40b8ecda3a4effcc978d0093fc497410447ea7

--- src/sync.cpp
+++ src/sync.cpp
@@ -2,6 +2,10 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#if defined(HAVE_CONFIG_H)
+#include <config/bitcoin-config.h>
+#endif
+
 #include <sync.h>
 #include <tinyformat.h>
 
