e17a9b71971d8a3d1bf98083c48fd10f8cbb46cc
Wladimir J. van der Laan <laanwj@gmail.com>
2019-05-16 19:10:13 +0200

    Merge #14047: Add HKDF_HMAC256_L32 and method to negate a private key
    8794a4b3ae4d34a4cd21a7dee9f694eef7726a4f QA: add test for HKDF HMAC_SHA256 L32 (Jonas Schnelli)
    551d489416339dae8f9d896013cd060a21406e2b Add HKDF HMAC_SHA256 L=32 implementations (Jonas Schnelli)
    3b64f852e400c552f031697d6a86829dc6e74bd6 QA: add test for CKey::Negate() (Jonas Schnelli)
    463921bb649d644f79f9d7f0f96f10aa0d165f76 CKey: add method to negate the key (Jonas Schnelli)
    
    Pull request description:
    
      This adds a limited implementation of `HKDF` (defined by rfc5869) that supports only HMAC-SHA256  and length output of 32 bytes (will be required for v2 transport protocol).
    
      This PR also includes a method to negate a private key which is useful to enforce public keys starting with 0x02 (or 0x03) (a requirement for the v2 transport protocol). The new `CKey::Negate()` method is pretty much a wrapper around `secp256k1_ec_privkey_negate()`.
    
      Including tests.
    
      This is a subset of #14032 and a pre-requirement for the v2 transport protocol.
    
    ACKs for commit 8794a4:
    
    Tree-SHA512: 5341929dfa29f5da766ec3612784baec6a3ad69972f08b5a985a8aafdae4dae36f104a2b888d1f5d1f33561456bd111f960d7e32c2cc4fd18e48358468f26c1a
    
    Former-commit-id: 0a01b31f24ccf8f461fc648ad160500b88915a86

--- src/key.h
+++ src/key.h
@@ -98,6 +98,9 @@ public:
     //! Generate a new private key using a cryptographic PRNG.
     void MakeNewKey(bool fCompressed);
 
+    //! Negate private key
+    bool Negate();
+
     /**
      * Convert the private key to a CPrivKey (serialized OpenSSL private key data).
      * This is expensive.
