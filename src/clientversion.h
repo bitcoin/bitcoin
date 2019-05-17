612a90e8c38af87d785c23f48c7ef29e528ece63
UdjinM6 <UdjinM6@users.noreply.github.com>
2019-05-17 12:16:31 +0300

    Set CLIENT_VERSION_IS_RELEASE to true (#2926)

--- src/clientversion.h
+++ src/clientversion.h
@@ -20,7 +20,7 @@
 #define CLIENT_VERSION_BUILD 0
 
 //! Set to true for release, false for prerelease or test build
-#define CLIENT_VERSION_IS_RELEASE false
+#define CLIENT_VERSION_IS_RELEASE true
 
 /**
  * Copyright year (2009-this)
