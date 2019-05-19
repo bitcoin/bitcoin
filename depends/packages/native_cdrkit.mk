8b693dfeb8b988e04201829693c7835eca5f69ef
Ulrich Kempken <Uli.Kempken@t-online.de>
2019-05-19 19:58:29 +0000

    depends: switch to secure download of all dependencies
    Some dependency sources were downloaded via http, even though https (SSL/TLS) options are available.
    Even if we potentially check the integrity of the downloaded files via hash comparison, we should make
    use of this additional security layer.
    
    bdb.mk
    fontconfig.mk
    freetype.mk
    libX11.mk
    libXau.mk
    libXext.mk
    libxcb.mk
    native_cctools.mk
    native_cdrkit.mk
    xcb_proto.mk
    xextproto.mk
    xproto.mk
    xtrans.mk
    zlib.mk
    
    miniupnp was switched to official project mirror with SSL support

--- depends/packages/native_cdrkit.mk
+++ depends/packages/native_cdrkit.mk
@@ -1,6 +1,6 @@
 package=native_cdrkit
 $(package)_version=1.1.11
-$(package)_download_path=http://distro.ibiblio.org/fatdog/source/600/c
+$(package)_download_path=https://distro.ibiblio.org/fatdog/source/600/c
 $(package)_file_name=cdrkit-$($(package)_version).tar.bz2
 $(package)_sha256_hash=b50d64c214a65b1a79afe3a964c691931a4233e2ba605d793eb85d0ac3652564
 $(package)_patches=cdrkit-deterministic.patch
