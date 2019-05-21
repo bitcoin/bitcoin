6ac67595eb96bf0205606bc1fcfec37fcf2c99cf
BtcDrak <btcdrak@gmail.com>
2019-05-21 02:51:32 +0000

    Remove unused import (fixes Lint test)

--- test/functional/feature_versionbits_warning.py
+++ test/functional/feature_versionbits_warning.py
@@ -13,7 +13,7 @@ from time import sleep
 
 from test_framework.blocktools import create_block, create_coinbase
 from test_framework.messages import msg_block
-from test_framework.mininode import P2PInterface, mininode_lock
+from test_framework.mininode import P2PInterface
 from test_framework.test_framework import BitcoinTestFramework
 from test_framework.util import wait_until
 
