376129fa39ae99053127fa770bf631d67c63ef66
sidhujag <sidhujag@gmail.com>
2019-05-21 23:05:06 -0700

    update chain param
    Former-commit-id: 2a2aab664d2269f960bd780aedeba72310b63514

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -133,7 +133,7 @@ public:
         consensus.nAuxpowStartHeight = 1;
         consensus.fStrictChainId = true;
         consensus.nLegacyBlocksBefore = 1;
-        consensus.vchSYSXContract = ParseHex("f2323264e0f7f392544f9bb209fe89cb64838e3f");
+        consensus.vchSYSXContract = ParseHex("e30d512cc0e75c94fb82dd7c56c9191ef5a2111e");
         consensus.vchSYSXBurnMethodSignature = ParseHex("285c5bc6");
         /**
          * The message start string is designed to be unlikely to occur in normal data.
