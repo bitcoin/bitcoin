764021f12bfe29df538c34edb5210c29d3443314
sidhujag <sidhujag@gmail.com>
2019-05-28 20:23:02 -0700

    fix start-all bug
    Former-commit-id: ba3193392b8e61899d3c22e4b66ed8a1bf56d010

--- src/rpc/masternode.cpp
+++ src/rpc/masternode.cpp
@@ -260,7 +260,7 @@ UniValue masternode(const JSONRPCRequest& request)
         for (const auto& mne : masternodeConfig.getEntries()) {
             std::string strError;
             uint32_t outputIndex;
-            bool fResult;
+            bool fResult = true;
             if(!ParseUInt32(mne.getOutputIndex(), &outputIndex)){
                 strError = "Could not parse output index";
                 fResult = false;
