d1de3d8411275051d0b34d13f3019a4f2c136206
João Barbosa <joao.paulo.barbosa@gmail.com>
2019-06-02 17:04:35 +0100

    gui: Set progressDialog to nullptr
    Github-Pull: #16135
    Rebased-From: d2ae6be80f6a0156021bf8c9b9d17cd4966ddffc

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -1442,6 +1442,7 @@ void BitcoinGUI::showProgress(const QString &title, int nProgress)
         if (progressDialog) {
             progressDialog->close();
             progressDialog->deleteLater();
+            progressDialog = nullptr;
         }
     } else if (progressDialog) {
         progressDialog->setValue(nProgress);
