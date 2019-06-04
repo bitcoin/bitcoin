b4ab3e83b50872b242aea3601c85b0beba8f6676
sidhujag <sidhujag@gmail.com>
2019-06-04 11:29:49 -0700

    create new console and dont close handle to dbg
    Former-commit-id: 9995cfade99189704ddea51243df12f2e6c8e87c

--- src/util/system.cpp
+++ src/util/system.cpp
@@ -125,15 +125,15 @@ ArgsManager gArgs;
         const wchar_t* app_const = app_w.c_str();
         LogPrintf("CreateProcessW app %s arg %s\n",app, arg);
         int result = CreateProcessW(app_const, arg_concat, NULL, NULL, FALSE, 
-              CREATE_NO_WINDOW, NULL, NULL, &si, &pi);
+              CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi);
         if(!result)
         {
             LogPrintf("CreateProcess failed (%d)\n", GetLastError());
             return 0;
         }
         pid_t pid = (pid_t)pi.dwProcessId;
-        CloseHandle(pi.hProcess);
-        CloseHandle(pi.hThread);
+        //CloseHandle(pi.hProcess);
+        //CloseHandle(pi.hThread);
         return pid;
     }
 #endif
