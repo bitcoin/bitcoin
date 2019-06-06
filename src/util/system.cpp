d4d14b7f480a690388c64c10b170f6d9560c9982
sidhujag <sidhujag@gmail.com>
2019-06-05 22:53:16 -0700

    compile
    Former-commit-id: bc16de7847ffdbc06c213a72e120d4ea5b9de23d

--- src/util/system.cpp
+++ src/util/system.cpp
@@ -126,7 +126,7 @@ ArgsManager gArgs;
         std::wstring input = appQuoted_w + L" " + arg_w;
         wchar_t* arg_concat = const_cast<wchar_t*>( input.c_str() );
         const wchar_t* app_const = app_w.c_str();
-        LogPrintf("CreateProcessW app %s\n",input.c_str());
+        LogPrintf("CreateProcessW app %s %s\n",app,arg);
         int result = CreateProcessW(app_const, arg_concat, NULL, NULL, FALSE, 
               CREATE_NO_WINDOW, NULL, NULL, &si, &pi);
         if(!result)
