b77bd932bd9f984e125457c1fe529f1b20e0b75e
Andrew Chow <achow101-github@achow101.com>
2019-06-06 16:49:52 +0200

    Move HaveKey static function from keystore to rpcwallet where it is used

--- src/keystore.h
+++ src/keystore.h
@@ -54,7 +54,4 @@ public:
 /** Return the CKeyID of the key involved in a script (if there is a unique one). */
 CKeyID GetKeyForDestination(const CBasicKeyStore& store, const CTxDestination& dest);
 
-/** Checks if a CKey is in the given CBasicKeyStore compressed or otherwise*/
-bool HaveKey(const CBasicKeyStore& store, const CKey& key);
-
 #endif // BITCOIN_KEYSTORE_H
