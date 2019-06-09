5e758c1996ed34e01fc68a9e13bd78abfd9688a6
Ben Woosley <ben.woosley@gmail.com>
2019-06-09 18:34:10 +0200

    lint: Fix shellcheck SC2155
    Declare and assign separately to avoid masking return values.
    
    https://github.com/koalaman/shellcheck/wiki/SC2155
    
    Former-commit-id: 9275ee5435b172106e6edcdcf824bfc668977128

--- ci/lint/04_install.sh
+++ ci/lint/04_install.sh
@@ -11,11 +11,13 @@ if [ "$TRAVIS_OS_NAME" == "osx" ]; then
   travis_retry brew update
   travis_retry brew install shellcheck
   travis_retry brew upgrade python
-  export PATH="$(brew --prefix python)/bin:$PATH"
+  PATH="$(brew --prefix python)/bin:$PATH"
+  export PATH
 else
   SHELLCHECK_VERSION=v0.6.0
   travis_retry curl --silent "https://storage.googleapis.com/shellcheck/shellcheck-${SHELLCHECK_VERSION}.linux.x86_64.tar.xz" | tar --xz -xf - --directory /tmp/
-  export PATH="/tmp/shellcheck-${SHELLCHECK_VERSION}:${PATH}"
+  PATH="/tmp/shellcheck-${SHELLCHECK_VERSION}:${PATH}"
+  export PATH
 fi
 
 travis_retry pip3 install codespell==1.15.0
