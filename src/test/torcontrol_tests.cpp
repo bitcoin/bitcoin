9a40d488d4695d9ff6fd36f31c1c2cbdae64508a
Pasta <pasta@dashboost.org>
2019-06-10 17:10:09 -0500

    change include
    Signed-off-by: Pasta <pasta@dashboost.org>

--- src/test/torcontrol_tests.cpp
+++ src/test/torcontrol_tests.cpp
@@ -2,7 +2,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 //
-#include "test/test_bitcoin.h"
+#include "test/test_dash.h"
 #include "torcontrol.cpp"
 
 #include <boost/test/unit_test.hpp>
