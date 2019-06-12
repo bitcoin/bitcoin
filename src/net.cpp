3be45ed85ce5eb02c0df2fb4556e589e80a63450
sidhujag <sidhujag@gmail.com>
2019-06-12 12:20:02 -0700

    fire acceptconnections when opennetworkconnection is called
    Former-commit-id: 5bcd0c795849ff34603d52b18f323fb925d30902

--- src/net.cpp
+++ src/net.cpp
@@ -2023,6 +2023,13 @@ void CConnman::OpenNetworkConnection(const CAddress& addrConnect, bool fCountFai
         LOCK(cs_vNodes);
         vNodes.push_back(pnode);
     }
+    // SYSCOIN
+    if(fZMQNetworkStatus){
+        UniValue oNetworkStatus(UniValue::VOBJ);
+        oNetworkStatus.pushKV("connections", (int)GetNodeCount(CConnman::CONNECTIONS_ALL));
+        oNetworkStatus.pushKV("status", "AcceptConnection");
+        GetMainSignals().NotifySyscoinUpdate(oNetworkStatus.write().c_str(), "networkstatus");
+    } 
 }
 // SYSCOIN
 void CConnman::OpenMasternodeConnection(const CAddress &addrConnect) {
