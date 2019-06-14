f3fc8e0a8effdc3f32dc2e7ee7d2b211e73104e4
Ben Woosley <ben.woosley@gmail.com>
2019-06-14 15:20:42 -0400

    lint: Run the linters against Mac OS on Travis
    This helps ensure ongoing compatibility with macOS-distributed version
    of GNU bash.

--- ci/lint/04_install.sh
+++ ci/lint/04_install.sh
@@ -6,9 +6,17 @@
 
 export LC_ALL=C
 
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+  # update first to install required ruby dependency
+  travis_retry brew update
+  travis_retry brew install shellcheck
+  travis_retry brew upgrade python
+  export PATH="$(brew --prefix python)/bin:$PATH"
+else
+  SHELLCHECK_VERSION=v0.6.0
+  travis_retry curl --silent "https://storage.googleapis.com/shellcheck/shellcheck-${SHELLCHECK_VERSION}.linux.x86_64.tar.xz" | tar --xz -xf - --directory /tmp/
+  export PATH="/tmp/shellcheck-${SHELLCHECK_VERSION}:${PATH}"
+fi
+
 travis_retry pip3 install codespell==1.15.0
 travis_retry pip3 install flake8==3.7.8
-
-SHELLCHECK_VERSION=v0.6.0
-curl -s "https://storage.googleapis.com/shellcheck/shellcheck-${SHELLCHECK_VERSION}.linux.x86_64.tar.xz" | tar --xz -xf - --directory /tmp/
-export PATH="/tmp/shellcheck-${SHELLCHECK_VERSION}:${PATH}"
