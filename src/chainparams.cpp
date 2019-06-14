018a41376de19e963d9bd7dcdf19fa713088ccb3
sidhujag <sidhujag@gmail.com>
2019-06-13 22:12:09 -0700

    remove btc key prefix as not needed
    Former-commit-id: 77074e59c38b76acbd928d10bc09b4c2ab718267

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -171,10 +171,7 @@ public:
         base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x88, 0xAD, 0xE4};
 
         bech32_hrp = "sys";
-
-        base58Prefixes[PUBKEY_ADDRESS_BTC] = std::vector<unsigned char>(1,0);
-        bech32_hrp_btc = "bc";
-
+        
         vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_main, pnSeed6_main + ARRAYLEN(pnSeed6_main));
 
         fDefaultConsistencyChecks = false;
@@ -284,8 +281,7 @@ public:
 
         bech32_hrp = "tsys";
 
-        base58Prefixes[PUBKEY_ADDRESS_BTC] = std::vector<unsigned char>(1,111);
-        bech32_hrp_btc = "tb";
+
 
         vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_test, pnSeed6_test + ARRAYLEN(pnSeed6_test));
 
@@ -407,9 +403,7 @@ public:
         base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x35, 0x83, 0x94};
 
         bech32_hrp = "sysrt";
-
-        base58Prefixes[PUBKEY_ADDRESS_BTC] = std::vector<unsigned char>(1,111);
-        bech32_hrp_btc = "bcrt";
+        
         /* enable fallback fee on regtest */
         m_fallback_fee_enabled = true;
     }
--- src/key_io.cpp
+++ src/key_io.cpp
@@ -69,58 +69,6 @@ public:
 
     std::string operator()(const CNoDestination& no) const { return {}; }
 };
-// SYSCOIN
-class DestinationEncoderBitcoin : public boost::static_visitor<std::string>
-{
-private:
-    const CChainParams& m_params;
-
-public:
-    explicit DestinationEncoderBitcoin(const CChainParams& params) : m_params(params) {}
-
-    std::string operator()(const PKHash& id) const
-    {
-        std::vector<unsigned char> data = m_params.Base58Prefix(CChainParams::PUBKEY_ADDRESS_BTC);
-        data.insert(data.end(), id.begin(), id.end());
-        return EncodeBase58Check(data);
-    }
-
-    std::string operator()(const ScriptHash& id) const
-    {
-        std::vector<unsigned char> data = m_params.Base58Prefix(CChainParams::SCRIPT_ADDRESS);
-        data.insert(data.end(), id.begin(), id.end());
-        return EncodeBase58Check(data);
-    }
-
-    std::string operator()(const WitnessV0KeyHash& id) const
-    {
-        std::vector<unsigned char> data = {0};
-        data.reserve(33);
-        ConvertBits<8, 5, true>([&](unsigned char c) { data.push_back(c); }, id.begin(), id.end());
-        return bech32::Encode(m_params.Bech32HRPBTC(), data);
-    }
-
-    std::string operator()(const WitnessV0ScriptHash& id) const
-    {
-        std::vector<unsigned char> data = {0};
-        data.reserve(53);
-        ConvertBits<8, 5, true>([&](unsigned char c) { data.push_back(c); }, id.begin(), id.end());
-        return bech32::Encode(m_params.Bech32HRPBTC(), data);
-    }
-
-    std::string operator()(const WitnessUnknown& id) const
-    {
-        if (id.version < 1 || id.version > 16 || id.length < 2 || id.length > 40) {
-            return {};
-        }
-        std::vector<unsigned char> data = {(unsigned char)id.version};
-        data.reserve(1 + (id.length * 8 + 4) / 5);
-        ConvertBits<8, 5, true>([&](unsigned char c) { data.push_back(c); }, id.program, id.program + id.length);
-        return bech32::Encode(m_params.Bech32HRPBTC(), data);
-    }
-
-    std::string operator()(const CNoDestination& no) const { return {}; }
-};
 CTxDestination DecodeDestination(const std::string& str, const CChainParams& params)
 {
     std::vector<unsigned char> data;
@@ -130,9 +78,7 @@ CTxDestination DecodeDestination(const std::string& str, const CChainParams& par
         // Public-key-hash-addresses have version 0 (or 111 testnet).
         // The data vector contains RIPEMD160(SHA256(pubkey)), where pubkey is the serialized public key.
         const std::vector<unsigned char>& pubkey_prefix = params.Base58Prefix(CChainParams::PUBKEY_ADDRESS);
-        // SYSCOIN
-        const std::vector<unsigned char>& pubkey_prefix_btc = params.Base58Prefix(CChainParams::PUBKEY_ADDRESS_BTC);
-        if (data.size() == hash.size() + pubkey_prefix.size() && (std::equal(pubkey_prefix.begin(), pubkey_prefix.end(), data.begin()) || std::equal(pubkey_prefix_btc.begin(), pubkey_prefix_btc.end(), data.begin()))) {
+        if (data.size() == hash.size() + pubkey_prefix.size() && std::equal(pubkey_prefix.begin(), pubkey_prefix.end(), data.begin())) {
             std::copy(data.begin() + pubkey_prefix.size(), data.end(), hash.begin());
             return PKHash(hash);
         }
@@ -264,11 +210,6 @@ std::string EncodeExtKey(const CExtKey& key)
 std::string EncodeDestination(const CTxDestination& dest)
 {
     return boost::apply_visitor(DestinationEncoder(Params()), dest);
-}
-// SYSCOIN
-std::string EncodeDestinationBitcoin(const CTxDestination& dest)
-{
-    return boost::apply_visitor(DestinationEncoderBitcoin(Params()), dest);
 }
 CTxDestination DecodeDestination(const std::string& str)
 {
