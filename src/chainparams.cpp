ac20646ba815ff654c670a1c125ee584d25ff0bb
Jakub Cech <jakub.cech@iota.org>
2019-06-15 17:48:04 +0200

    address prefixes for mainnet

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -129,8 +129,8 @@ public:
         vSeeds.emplace_back("seed.bitcoin.sprovoost.nl"); // Sjors Provoost
         vSeeds.emplace_back("dnsseed.emzy.de"); // Stephan Oeste
 
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,0);
-        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,4);
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(12,94);
         base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,128);
         base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x88, 0xB2, 0x1E};
         base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x88, 0xAD, 0xE4};
