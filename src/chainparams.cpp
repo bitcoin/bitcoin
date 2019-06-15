70e0d5b5dfdd7d7757a418f775cecda09869643b
Jakub Cech <jakub.cech@iota.org>
2019-06-15 17:52:37 +0200

    fix bech32 HRP

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -122,7 +122,7 @@ public:
         base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x88, 0xB2, 0x1E};
         base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x88, 0xAD, 0xE4};
 
-        bech32_hrp = "bc";
+        bech32_hrp = "2p";
 
         vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_main, pnSeed6_main + ARRAYLEN(pnSeed6_main));
 
