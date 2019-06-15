95109070c102f3b3c505fbab62c9420c338e7691
Jakub Cech <jakub.cech@iota.org>
2019-06-15 18:15:26 +0200

    default max tip age change

--- src/validation.cpp
+++ src/validation.cpp
@@ -110,7 +110,7 @@ bool fCheckBlockIndex = false;
 bool fCheckpointsEnabled = DEFAULT_CHECKPOINTS_ENABLED;
 size_t nCoinCacheUsage = 5000 * 300;
 uint64_t nPruneTarget = 0;
-int64_t nMaxTipAge = DEFAULT_MAX_TIP_AGE;
+int64_t nMaxTipAge = DEFAULT_MAX_TIP_AGE * 4300;
 bool fEnableReplacement = DEFAULT_ENABLE_REPLACEMENT;
 
 uint256 hashAssumeValid;
