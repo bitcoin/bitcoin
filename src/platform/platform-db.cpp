a5552a09d36b0b9d0df451d35bf709f62da2ce6a
Artem B <artem.bv@protonmail.com>
2019-06-20 15:44:12 +0300

    fix windows build

--- src/platform/platform-db.cpp
+++ src/platform/platform-db.cpp
@@ -2,6 +2,7 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#include <boost/thread.hpp>
 #include "platform-utils.h"
 #include "platform-db.h"
 #include "platform/specialtx.h"
