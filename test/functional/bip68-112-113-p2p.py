3dcbb2e599dda3bc08751691992993ad8b6197b4
UdjinM6 <UdjinM6@users.noreply.github.com>
2019-06-21 16:44:29 +0300

    fix bip68-112-113-p2p.py

--- test/functional/bip68-112-113-p2p.py
+++ test/functional/bip68-112-113-p2p.py
@@ -95,7 +95,13 @@ class BIP68_112_113Test(ComparisonTestFramework):
     def __init__(self):
         super().__init__()
         self.num_nodes = 1
+        # Must set the blockversion for this test
+        # Must also set '-maxtipage=600100' to allow syncing from very old blocks
+        # and '-dip3params=2000:2000' to create pre-dip3 blocks only
         self.extra_args = [['-whitelist=127.0.0.1', '-blockversion=4', '-maxtipage=600100', '-dip3params=2000:2000']]
+
+    def setup_network(self):
+        self.setup_nodes()
 
     def run_test(self):
         test = TestManager(self, self.options.tmpdir)
