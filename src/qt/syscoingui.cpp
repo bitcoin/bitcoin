033011ebdde6eb0f7873150413b2f80000bc485e
fanquake <fanquake@gmail.com>
2019-06-23 18:39:53 +0800

    Merge #16231: gui: Fix open wallet menu initialization order
    5224be5a3354e1a22ea4d7f0e40aadfccdf67064 gui: Fix open wallet menu initialization order (João Barbosa)
    
    Pull request description:
    
      Fixes #16230, the menu must be created before connecting to aboutToShow signal.
    
    ACKs for commit 5224be:
      hebasto:
        ACK 5224be5a3354e1a22ea4d7f0e40aadfccdf67064, I have tested the code on Bionic with Qt 5.12.4.
      ryanofsky:
        utACK 5224be5a3354e1a22ea4d7f0e40aadfccdf67064. Looks good, fix is simple and makes perfect sense after seeing explanation in https://github.com/bitcoin/bitcoin/pull/16118#issuecomment-503166407. Without this change (and since #16118), the menu pointer passed to `connect(m_open_wallet_action->menu(), ...)` is null and connecting has no effect. With this change, the menu is constructed earlier so the connect call can work.
      fanquake:
        ACK 5224be5a3354e1a22ea4d7f0e40aadfccdf67064 Testing included in a comment above. The segfaulting with QT_FATAL_WARNINGS is unrelated to this change.
    
    Tree-SHA512: 97b42493b37b96683058bccf39a0ee93589293d4ba8f0c60aef7f4fb9dd084cc6d5608cd5ef531cadf5e03b1f01627ef96bc2d79f784fb38cb87aa6643183d41
    
    Former-commit-id: d0a515884f6323999dd18a7ba85bc334fe4626ef

--- src/qt/syscoingui.cpp
+++ src/qt/syscoingui.cpp
@@ -355,6 +355,7 @@ void SyscoinGUI::createActions()
     m_open_wallet_action = new QAction(tr("Open Wallet"), this);
     m_open_wallet_action->setEnabled(false);
     m_open_wallet_action->setStatusTip(tr("Open a wallet"));
+    m_open_wallet_menu = new QMenu(this);
 
     m_close_wallet_action = new QAction(tr("Close Wallet..."), this);
     m_close_wallet_action->setStatusTip(tr("Close wallet"));
@@ -386,13 +387,13 @@ void SyscoinGUI::createActions()
         connect(usedSendingAddressesAction, &QAction::triggered, walletFrame, &WalletFrame::usedSendingAddresses);
         connect(usedReceivingAddressesAction, &QAction::triggered, walletFrame, &WalletFrame::usedReceivingAddresses);
         connect(openAction, &QAction::triggered, this, &SyscoinGUI::openClicked);
-        connect(m_open_wallet_action->menu(), &QMenu::aboutToShow, [this] {
-            m_open_wallet_action->menu()->clear();
+        connect(m_open_wallet_menu, &QMenu::aboutToShow, [this] {
+            m_open_wallet_menu->clear();
             std::vector<std::string> available_wallets = m_wallet_controller->getWalletsAvailableToOpen();
             std::vector<std::string> wallets = m_node.listWalletDir();
             for (const auto& path : wallets) {
                 QString name = path.empty() ? QString("["+tr("default wallet")+"]") : QString::fromStdString(path);
-                QAction* action = m_open_wallet_action->menu()->addAction(name);
+                QAction* action = m_open_wallet_menu->addAction(name);
 
                 if (std::find(available_wallets.begin(), available_wallets.end(), path) == available_wallets.end()) {
                     // This wallet is already loaded
@@ -428,7 +429,7 @@ void SyscoinGUI::createActions()
                 });
             }
             if (wallets.empty()) {
-                QAction* action = m_open_wallet_action->menu()->addAction(tr("No wallets available"));
+                QAction* action = m_open_wallet_menu->addAction(tr("No wallets available"));
                 action->setEnabled(false);
             }
         });
@@ -659,7 +660,7 @@ void SyscoinGUI::setWalletController(WalletController* wallet_controller)
     m_wallet_controller = wallet_controller;
 
     m_open_wallet_action->setEnabled(true);
-    m_open_wallet_action->setMenu(new QMenu(this));
+    m_open_wallet_action->setMenu(m_open_wallet_menu);
 
     connect(wallet_controller, &WalletController::walletAdded, this, &SyscoinGUI::addWallet);
     connect(wallet_controller, &WalletController::walletRemoved, this, &SyscoinGUI::removeWallet);
