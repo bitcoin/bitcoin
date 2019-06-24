66f4d50e375497f926d0d7868f86c77920a3a601
Antoine Riard <ariard@student.42.fr>
2019-06-24 19:07:09 -0400

    Only return early from BlockUntilSyncedToCurrentChain if current tip is exact match
    In the next commit, we start using BlockConnected/BlockDisconnected
    callbacks to establish tx depth, rather than querying the chain
    directly.
    
    Currently, BlockUntilSyncedToCurrentChain will return early if
    the best block processed by the wallet is a descendant of the node'tip.
    That means that in the case of a re-org, it won't wait for the
    BlockDisconnected callbacks that have been enqueued during the re-org
    but have not yet been triggered in the wallet.
    
    Change BlockUntilSyncedToCurrentChain to only return early if the
    wallet's m_last_block_processed matches the tip exactly. This ensures
    that there are no BlockDisconnected or BlockConnected callbacks
    in-flight.
    
    
    Former-commit-id: 68e53681f3fb06b28ac49542ad1ea1ea2227f1c8

--- src/interfaces/chain.h
+++ src/interfaces/chain.h
@@ -236,9 +236,8 @@ public:
     virtual std::unique_ptr<Handler> handleNotifications(Notifications& notifications) = 0;
 
     //! Wait for pending notifications to be processed unless block hash points to the current
-    //! chain tip, or to a possible descendant of the current chain tip that isn't currently
-    //! connected.
-    virtual void waitForNotificationsIfNewBlocksConnected(const uint256& old_tip) = 0;
+    //! chain tip.
+    virtual void waitForNotificationsIfTipChanged(const uint256& old_tip) = 0;
 
     //! Register handler for RPC. Command is not copied, so reference
     //! needs to remain valid until Handler is disconnected.
