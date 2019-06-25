490aa2ebe65b40d8df72e00e3e841df5cae0f77f
Sunny King <sunnyking9999@gmail.com>
2019-06-25 10:21:34 +0000

    Additional dnsseed

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -123,6 +123,7 @@ public:
         vSeeds.emplace_back("seed2.peercoin.net");
         vSeeds.emplace_back("seed.peercoin-library.org");
         vSeeds.emplace_back("ppcseed.ns.7server.net");
+        vSeeds.emplace_back("seed.ppcoin.info");
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,55);  // peercoin: addresses begin with 'P'
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,117); // peercoin: addresses begin with 'p'
@@ -216,6 +217,7 @@ public:
         vSeeds.emplace_back("tseed.peercoin.net");
         vSeeds.emplace_back("tseed2.peercoin.net");
         vSeeds.emplace_back("tseed.peercoin-library.org");
+        vSeeds.emplace_back("testseed.ppcoin.info");
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,111);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,196);
