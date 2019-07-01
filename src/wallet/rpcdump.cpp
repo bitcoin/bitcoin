d5d27ad19bd2c16644ce438e610e656b087f1208
Pasta <pasta@dashboost.org>
2019-07-01 07:58:31 -0500

    s/bitcoind/dashd
    Signed-off-by: Pasta <pasta@dashboost.org>

--- src/wallet/rpcdump.cpp
+++ src/wallet/rpcdump.cpp
@@ -1363,7 +1363,7 @@ UniValue importmulti(const JSONRPCRequest& mainRequest)
                                       "block from time %d, which is after or within %d seconds of key creation, and "
                                       "could contain transactions pertaining to the key. As a result, transactions "
                                       "and coins using this key may not appear in the wallet. This error could be "
-                                      "caused by pruning or data corruption (see bitcoind log for details) and could "
+                                      "caused by pruning or data corruption (see dashd log for details) and could "
                                       "be dealt with by downloading and rescanning the relevant blocks (see -reindex "
                                       "and -rescan options).",
                                 GetImportTimestamp(request, now), scanFailed->GetBlockTimeMax(), TIMESTAMP_WINDOW)));
