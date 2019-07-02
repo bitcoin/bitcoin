e4728f98056b5c41a2a63815734a2e56e30dcab7
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2019-07-02 19:48:08 +0300

    Enable ShellCheck rules
    Enabled ShellCheck rules:
      SC1087
      SC2001
      SC2004
      SC2005
      SC2006
      SC2016
      SC2028
      SC2048
      SC2066 (note that IFS already contains only a line feed)
      SC2116
      SC2166
      SC2181
      SC2206
      SC2207
      SC2230
      SC2236
    
    
    Former-commit-id: 473bcbf69e04cd8758be1c7bb479d4aa044e6927

--- test/lint/lint-logs.sh
+++ test/lint/lint-logs.sh
@@ -19,6 +19,7 @@ UNTERMINATED_LOGS=$(git grep --extended-regexp "LogPrintf?\(" -- "*.cpp" | \
     grep -v "LogPrint()" | \
     grep -v "LogPrintf()")
 if [[ ${UNTERMINATED_LOGS} != "" ]]; then
+    # shellcheck disable=SC2028
     echo "All calls to LogPrintf() and LogPrint() should be terminated with \\n"
     echo
     echo "${UNTERMINATED_LOGS}"
