4099769738266e5c789be7a2a58e06d265e54179
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2019-07-02 19:48:08 +0300

    Enable ShellCheck rules
    Enabled ShellCheck rules:
      SC1087
      SC2001
      SC2004
      SC2005
      SC2006
      SC2016
      SC2028
      SC2048
      SC2066 (note that IFS already contains only a line feed)
      SC2116
      SC2166
      SC2181
      SC2206
      SC2207
      SC2230
      SC2236
    
    
    Former-commit-id: bcf4eaad5f3bc951662805c6f8c1f60e4b2ca829

--- test/lint/commit-script-check.sh
+++ test/lint/commit-script-check.sh
@@ -18,12 +18,12 @@ if test "x$1" = "x"; then
 fi
 
 RET=0
-PREV_BRANCH=`git name-rev --name-only HEAD`
-PREV_HEAD=`git rev-parse HEAD`
-for commit in `git rev-list --reverse $1`; do
+PREV_BRANCH=$(git name-rev --name-only HEAD)
+PREV_HEAD=$(git rev-parse HEAD)
+for commit in $(git rev-list --reverse $1); do
     if git rev-list -n 1 --pretty="%s" $commit | grep -q "^scripted-diff:"; then
         git checkout --quiet $commit^ || exit
-        SCRIPT="`git rev-list --format=%b -n1 $commit | sed '/^-BEGIN VERIFY SCRIPT-$/,/^-END VERIFY SCRIPT-$/{//!b};d'`"
+        SCRIPT="$(git rev-list --format=%b -n1 $commit | sed '/^-BEGIN VERIFY SCRIPT-$/,/^-END VERIFY SCRIPT-$/{//!b};d')"
         if test "x$SCRIPT" = "x"; then
             echo "Error: missing script for: $commit"
             echo "Failed"
