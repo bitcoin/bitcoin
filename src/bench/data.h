0f60baf980b3c4c556f33b98dca06c238cc7d643
MarcoFalke <falke.marco@gmail.com>
2019-07-02 12:42:29 -0400

    rpc: Clarify that block count means height excl genesis
    Former-commit-id: bf682cc0d5c395c56a72e07e1f7e52adedfa4744

--- src/bench/data.h
+++ src/bench/data.h
@@ -2,8 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_BENCH_DATA_H
-#define BITCOIN_BENCH_DATA_H
+#ifndef SYSCOIN_BENCH_DATA_H
+#define SYSCOIN_BENCH_DATA_H
 
 #include <cstdint>
 #include <vector>
@@ -16,4 +16,4 @@ extern const std::vector<uint8_t> block413567;
 } // namespace data
 } // namespace benchmark
 
-#endif // BITCOIN_BENCH_DATA_H
+#endif // SYSCOIN_BENCH_DATA_H
