b502a15a956b8747879213059d45cc00cf46960e
Andrew Chow <achow101-github@achow101.com>
2019-07-05 18:33:35 -0400

    Add a lock cs_desc_man for DescriptorScriptPubKeyMan

--- src/wallet/scriptpubkeyman.h
+++ src/wallet/scriptpubkeyman.h
@@ -473,6 +473,8 @@ class DescriptorScriptPubKeyMan : public ScriptPubKeyMan
 public:
     using ScriptPubKeyMan::ScriptPubKeyMan;
 
+    mutable RecursiveMutex cs_desc_man;
+
     bool GetNewDestination(const OutputType type, CTxDestination& dest, std::string& error) override;
     isminetype IsMine(const CScript& script) const override;
 
