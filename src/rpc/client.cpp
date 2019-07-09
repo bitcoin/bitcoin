803f6425c6da0508b9ef12ab98139d3720cccef1
darosior <darosior@protonmail.com>
2019-07-09 15:00:40 +0200

    Add the 'upgradewallet' RPC command
    This add a new RPC command which permits to upgrade a wallet to, by default, the latest version or to an optionally passed as a parameter version

--- src/rpc/client.cpp
+++ src/rpc/client.cpp
@@ -151,6 +151,7 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "logging", 0, "include" },
     { "logging", 1, "exclude" },
     { "disconnectnode", 1, "nodeid" },
+    { "upgradewallet", 0, "wallet_version" },
     // Echo with conversion (For testing only)
     { "echojson", 0, "arg0" },
     { "echojson", 1, "arg1" },
