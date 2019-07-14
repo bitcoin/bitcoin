eb7d766dd5edf69b0f12a9513345b16165ba913a
fanquake <fanquake@gmail.com>
2019-07-14 13:31:16 +0800

    build: use macOS 10.14 SDK

--- ci/test/00_setup_env_mac.sh
+++ ci/test/00_setup_env_mac.sh
@@ -8,7 +8,7 @@ export LC_ALL=C.UTF-8
 
 export HOST=x86_64-apple-darwin16
 export PACKAGES="cmake imagemagick libcap-dev librsvg2-bin libz-dev libbz2-dev libtiff-tools python3-dev python3-setuptools"
-export OSX_SDK=10.11
+export OSX_SDK=10.14
 export RUN_UNIT_TESTS=false
 export RUN_FUNCTIONAL_TESTS=false
 export GOAL="deploy"
