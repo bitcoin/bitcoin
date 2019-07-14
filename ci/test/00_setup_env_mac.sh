87f941357635b4481afba4a2ed1c2824cfd3adea
fanquake <fanquake@gmail.com>
2019-07-14 13:15:24 +0800

    build: set minimum supported macOS to 10.12

--- ci/test/00_setup_env_mac.sh
+++ ci/test/00_setup_env_mac.sh
@@ -6,7 +6,7 @@
 
 export LC_ALL=C.UTF-8
 
-export HOST=x86_64-apple-darwin14
+export HOST=x86_64-apple-darwin16
 export PACKAGES="cmake imagemagick libcap-dev librsvg2-bin libz-dev libbz2-dev libtiff-tools python3-dev python3-setuptools"
 export OSX_SDK=10.11
 export RUN_UNIT_TESTS=false
