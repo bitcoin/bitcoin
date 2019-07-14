2ac2d328ee4045436910d7116a8c12330a6959b2
fanquake <fanquake@gmail.com>
2019-07-14 13:31:16 +0800

    build: use macOS 10.14 SDK
    Co-Authored-By: Carl Dong <accounts@carldong.me>
    
    Former-commit-id: f324130242121438e12805e4f27907ffad54c75a

--- contrib/gitian-build.py
+++ contrib/gitian-build.py
@@ -209,7 +209,7 @@ def main():
     args.macos = 'm' in args.os
 
     # Disable for MacOS if no SDK found
-    if args.macos and not os.path.isfile('gitian-builder/inputs/MacOSX10.11.sdk.tar.gz'):
+    if args.macos and not os.path.isfile('gitian-builder/inputs/MacOSX10.14.sdk.tar.gz'):
         print('Cannot build for MacOS, SDK does not exist. Will build for other OSes')
         args.macos = False
 
