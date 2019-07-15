1346e14831489f9c8f53a08f9dfed61d55d53c6f
Andrew Chow <achow101-github@achow101.com>
2019-07-15 16:12:04 -0400

    Functional tests for descriptor wallets

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -147,6 +147,7 @@ BASE_SCRIPTS = [
     'p2p_addr_relay.py',
     'rpc_net.py',
     'wallet_keypool.py',
+    'wallet_descriptor.py',
     'p2p_mempool.py',
     'p2p_filter.py',
     'rpc_setban.py',
