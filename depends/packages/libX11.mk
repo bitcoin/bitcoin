c241ba4f13720687c18b532fd82aeaaf6793c2b9
Carl Dong <contact@carldong.me>
2019-07-17 16:21:50 -0400

    depends: libX11: Make package headers-only
    We're no longer building QT with libX11/XLib, however, libX11/XLib
    headers are still required for parts of QT. In this commit we add a
    minimal configure.ac for libX11/XLib that is headers-only.
    
    This change allows us to remove all of libX11/XLib's dependencies.
    
    
    Former-commit-id: dcddd3a07bdff126ac6e6ec55a30fc0f9f0e362e

--- depends/packages/libX11.mk
+++ depends/packages/libX11.mk
@@ -3,30 +3,18 @@ $(package)_version=1.6.2
 $(package)_download_path=https://xorg.freedesktop.org/releases/individual/lib/
 $(package)_file_name=$(package)-$($(package)_version).tar.bz2
 $(package)_sha256_hash=2aa027e837231d2eeea90f3a4afe19948a6eb4c8b2bec0241eba7dbc8106bd16
-$(package)_dependencies=libxcb xtrans xextproto xproto
-
-define $(package)_set_vars
-  # See libXext for --disable-malloc0returnsnull rationale.
-  $(package)_config_opts=--disable-xkb --disable-static --disable-malloc0returnsnull
-  $(package)_config_opts_linux=--with-pic
-endef
+$(package)_patches=configure.ac
 
 define $(package)_preprocess_cmds
-  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub .
+  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub . && \
+  cp -f $($(package)_patch_dir)/configure.ac .
 endef
 
 define $(package)_config_cmds
+  autoreconf --install --force --verbose && \
   $($(package)_autoconf)
 endef
 
-define $(package)_build_cmds
-  $(MAKE)
-endef
-
 define $(package)_stage_cmds
-  $(MAKE) DESTDIR=$($(package)_staging_dir) install
-endef
-
-define $(package)_postprocess_cmds
-  rm lib/*.la
+  $(MAKE) -C include DESTDIR=$($(package)_staging_dir) install-x11includeHEADERS
 endef
