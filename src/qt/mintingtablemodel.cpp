3648642971bd1a5222ca5d1d6a61917753bda0b7
backpacker69 <backpacker69@protonmail.com>
2019-07-22 15:47:46 +0300

    extra safety during mintingtable update

--- src/qt/mintingtablemodel.cpp
+++ src/qt/mintingtablemodel.cpp
@@ -182,11 +182,21 @@ public:
                         {
                             for(int i = lowerIndex; i < upperIndex; i++)
                             {
+                                if(i>=cachedWallet.size())
+                                {
+                                    LogPrintf("updateWallet: cachedWallet is smaller than expected, access item %d not in size %d\n", i, cachedWallet.size());
+                                    break;
+                                }
                                 KernelRecord cachedRec = cachedWallet.at(i);
                                 if((rec.address == cachedRec.address)
                                    && (rec.nValue == cachedRec.nValue)
                                    && (rec.idx == cachedRec.idx))
                                 {
+                                    if(i>=cachedWallet.size())
+                                    {
+                                        LogPrintf("updateWallet: cachedWallet is smaller than expected, remove item %d not in size %d\n", i, cachedWallet.size());
+                                        break;
+                                    }
                                     parent->beginRemoveRows(QModelIndex(), i, i);
                                     cachedWallet.removeAt(i);
                                     parent->endRemoveRows();
