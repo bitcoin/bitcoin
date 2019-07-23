3a1ea8e6e7ae29de065ef90805d9d6bfb037769a
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2019-07-23 16:31:53 +0300

    Add a way to set default -datadir
    -datadir or -datadir="" in command line will set the default datadir 
    (overriding config file options).

--- src/util/system.cpp
+++ src/util/system.cpp
@@ -741,8 +741,9 @@ const fs::path &GetDataDir(bool fNetSpecific)
     // this function
     if (!path.empty()) return path;
 
-    if (gArgs.IsArgSet("-datadir")) {
-        path = fs::system_complete(gArgs.GetArg("-datadir", ""));
+    std::string datadir = gArgs.GetArg("-datadir", "");
+    if (!datadir.empty()) {
+        path = fs::system_complete(datadir);
         if (!fs::is_directory(path)) {
             path = "";
             return path;
