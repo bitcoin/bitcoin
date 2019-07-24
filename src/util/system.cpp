6a2c4fb6771def774c15696d5c8c2c19496784de
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2019-07-24 03:29:40 +0300

    Fix datadir handling in bitcoind
    This prevents premature tries to access or create the default datadir.
    This is useful when the -datadir option is specified and the default
    datadir is unreachable.

--- src/util/system.cpp
+++ src/util/system.cpp
@@ -941,7 +941,7 @@ bool ArgsManager::ReadConfigFiles(std::string& error, bool ignore_invalid_keys)
 
     // If datadir is changed in .conf file:
     ClearDatadirCache();
-    if (!fs::is_directory(GetDataDir(false))) {
+    if (!CheckDataDirOption()) {
         error = strprintf("specified data directory \"%s\" does not exist.", gArgs.GetArg("-datadir", "").c_str());
         return false;
     }
