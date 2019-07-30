fa1bb53b0d76b1bfdc0f0669e260df4768d3be29
MarcoFalke <falke.marco@gmail.com>
2019-07-30 14:48:44 -0400

    test: Add -acceptnonstdtxn to self.extra_args[3]

--- test/functional/feature_dbcrash.py
+++ test/functional/feature_dbcrash.py
@@ -64,7 +64,8 @@ class ChainstateWriteCrashTest(BitcoinTestFramework):
         self.node2_args = ["-dbcrashratio=24", "-dbcache=16"] + self.base_args
 
         # Node3 is a normal node with default args, except will mine full blocks
-        self.node3_args = ["-blockmaxweight=4000000"]
+        # and non-standard txs (e.g. txs with "dust" outputs)
+        self.node3_args = ["-blockmaxweight=4000000", "-acceptnonstdtxn"]
         self.extra_args = [self.node0_args, self.node1_args, self.node2_args, self.node3_args]
 
     def skip_test_if_missing_module(self):
