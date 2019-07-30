003178751eff0ff452d24aa1c2229efa8f9f51c6
Antoine Riard <ariard@student.42.fr>
2019-07-30 18:08:03 -0400

    Remove p2pEnabled from Chain interface
    RPC server starts in warmup mode, it can't
    process yet calls, then follows connection manager
    initialization and finally RPC server get out of
    warmup mode. RPC calls shouldn't be able to get
    P2P disabled errors because once we initialize
    g_connman it's not unset until shutdown, after
    RPC server has been stopped.
    
    
    Former-commit-id: 2892b652ced0e964eca12511f54202bf07b70b79

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -314,10 +314,6 @@ static CTransactionRef SendMoney(interfaces::Chain::Lock& locked_chain, CWallet 
     if (nValue > curBalance)
         throw JSONRPCError(RPC_WALLET_INSUFFICIENT_FUNDS, "Insufficient funds");
 
-    if (pwallet->GetBroadcastTransactions() && !pwallet->chain().p2pEnabled()) {
-        throw JSONRPCError(RPC_CLIENT_P2P_DISABLED, "Error: Peer-to-peer functionality missing or disabled");
-    }
-
     // Parse Syscoin address
     CScript scriptPubKey = GetScriptForDestination(address);
 
@@ -850,10 +846,6 @@ static UniValue sendmany(const JSONRPCRequest& request)
 
     auto locked_chain = pwallet->chain().lock();
     LOCK(pwallet->cs_wallet);
-
-    if (pwallet->GetBroadcastTransactions() && !pwallet->chain().p2pEnabled()) {
-        throw JSONRPCError(RPC_CLIENT_P2P_DISABLED, "Error: Peer-to-peer functionality missing or disabled");
-    }
 
     if (!request.params[0].isNull() && !request.params[0].get_str().empty()) {
         throw JSONRPCError(RPC_INVALID_PARAMETER, "Dummy value must be set to \"\"");
