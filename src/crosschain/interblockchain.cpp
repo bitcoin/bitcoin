10f9a3ff8d5ab5b9aaa5da6e7078a81ccaebea1b
BlockMechanic <blockmecha@gmail.com>
2019-07-31 20:15:58 +0200

    Crosschain fixes

--- src/crosschain/interblockchain.cpp
+++ src/crosschain/interblockchain.cpp
@@ -10,9 +10,9 @@
 
 void CIbtp::LoadMsgStart()
 {
-    vChains.push_back(SChain("bitcointalkcoin", "CRT", 0xf7, 0xba, 0xd4, 0xa9, 8383));
-    vChains.push_back(SChain("talk", "TALK", 0xf7, 0xba, 0xd4, 0xaa, 8373));
-    vChains.push_back(SChain("Codex", "CDX", 0xf7, 0xba, 0xd4, 0xab, 8363));
+    vChains.push_back(SChain("bitcointalkcoin", "TALK", 0xf7, 0xba, 0xd4, 0xa8, 8382));
+    vChains.push_back(SChain("cryptotalkcoin", "CRT", 0xf7, 0xba, 0xd4, 0xa9, 8383));
+    vChains.push_back(SChain("Codex", "CDX", 0xf7, 0xba, 0xd4, 0xaa, 8384));
 }
 
 bool CIbtp::IsIbtpChain(const unsigned char msgStart[], std::string& chainName)
