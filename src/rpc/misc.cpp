882cbbf628f2a9f67d496af88dab218b4f692aa7
Andrew Chow <achow101-github@achow101.com>
2019-08-02 19:19:53 -0400

    Give more errors for specific failure conditions
    Some failure conditions implicitly fail by failing some other check.
    But the error messages are more helpful if they say explicitly what
    actually caused the failure, so add those as failure conditions and
    errors.

--- src/rpc/misc.cpp
+++ src/rpc/misc.cpp
@@ -153,7 +153,7 @@ UniValue getdescriptorinfo(const JSONRPCRequest& request)
     std::string error;
     auto desc = Parse(request.params[0].get_str(), provider, error);
     if (!desc) {
-        throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, strprintf("Invalid descriptor, %s", error));
+        throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, error);
     }
 
     UniValue result(UniValue::VOBJ);
@@ -203,7 +203,7 @@ UniValue deriveaddresses(const JSONRPCRequest& request)
     std::string error;
     auto desc = Parse(desc_str, key_provider, error, /* require_checksum = */ true);
     if (!desc) {
-        throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, strprintf("Invalid descriptor, %s", error));
+        throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, error);
     }
 
     if (!desc->IsRange() && request.params.size() > 1) {
