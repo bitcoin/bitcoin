60b3ae1821494bf94f25f61ce905ae50fe73f872
Pasta <pasta@dashboost.org>
2019-08-02 12:41:37 -0500

    add gArgs
    Signed-off-by: Pasta <pasta@dashboost.org>

--- src/txdb.cpp
+++ src/txdb.cpp
@@ -89,8 +89,8 @@ bool CCoinsViewDB::BatchWrite(CCoinsMap &mapCoins, const uint256 &hashBlock) {
     CDBBatch batch(db);
     size_t count = 0;
     size_t changed = 0;
-    size_t batch_size = (size_t)GetArg("-dbbatchsize", nDefaultDbBatchSize);
-    int crash_simulate = GetArg("-dbcrashratio", 0);
+    size_t batch_size = (size_t)gArgs.GetArg("-dbbatchsize", nDefaultDbBatchSize);
+    int crash_simulate = gArgs.GetArg("-dbcrashratio", 0);
     assert(!hashBlock.IsNull());
 
     uint256 old_tip = GetBestBlock();
