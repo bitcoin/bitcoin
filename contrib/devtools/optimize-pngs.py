083c57bfa28d29424b3bdf008c04aaa5560b7f50
MarcoFalke <falke.marco@gmail.com>
2019-08-07 18:25:41 -0400

    gui: Make Bitcoin Core logo pure black

--- contrib/devtools/optimize-pngs.py
+++ contrib/devtools/optimize-pngs.py
@@ -26,7 +26,7 @@ def content_hash(filename):
 
 pngcrush = 'pngcrush'
 git = 'git'
-folders = ["src/qt/res/movies", "src/qt/res/icons", "share/pixmaps"]
+folders = ["src/qt/res/movies", "src/qt/res/icons", "share/pixmaps", "doc"]
 basePath = subprocess.check_output([git, 'rev-parse', '--show-toplevel'], universal_newlines=True, encoding='utf8').rstrip('\n')
 totalSaveBytes = 0
 noHashChange = True
