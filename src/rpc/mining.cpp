7bd8f4e4321b73a9d55926778655310de0166bac
Emil Engler <me@emilengler.com>
2019-08-13 18:25:58 +0200

    rpc: Fix getblocktemplate CLI example (#16594)
    Github-Pull: #16596
    Rebased-From: 14f7eec3bdb15a9a75f8686c0a3e879aa8ceef47

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -368,7 +368,7 @@ static UniValue getblocktemplate(const JSONRPCRequest& request)
             "}\n"
                 },
                 RPCExamples{
-                    HelpExampleCli("getblocktemplate", "{\"rules\": [\"segwit\"]}")
+                    HelpExampleCli("getblocktemplate", "'{\"rules\": [\"segwit\"]}'")
             + HelpExampleRpc("getblocktemplate", "{\"rules\": [\"segwit\"]}")
                 },
             }.ToString());
