fa27c55b0593c769b6ad87de0b59df3816d73548
MarcoFalke <falke.marco@gmail.com>
2019-08-15 10:06:23 -0400

    util: Move ResolveErrMsg to util/error

--- src/util/error.h
+++ src/util/error.h
@@ -10,7 +10,7 @@
  * string functions. Types and functions defined here should not require any
  * outside dependencies.
  *
- * Error types defined here can be used in different parts of the bitcoin
+ * Error types defined here can be used in different parts of the
  * codebase, to avoid the need to write boilerplate code catching and
  * translating errors passed across wallet/node/rpc/gui code boundaries.
  */
@@ -32,8 +32,10 @@ enum class TransactionError {
 
 std::string TransactionErrorString(const TransactionError error);
 
+std::string ResolveErrMsg(const std::string& optname, const std::string& strBind);
+
 std::string AmountHighWarn(const std::string& optname);
 
-std::string AmountErrMsg(const char* const optname, const std::string& strValue);
+std::string AmountErrMsg(const std::string& optname, const std::string& strValue);
 
 #endif // BITCOIN_UTIL_ERROR_H
