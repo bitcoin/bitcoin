8392bbcd5b38fad3e565c430371655a2390d433b
sidhujag <sidhujag@gmail.com>
2019-08-17 11:07:01 -0700

    update ifdef
    Former-commit-id: 2c344861adcfaa8dcca9bf774d76eab76672d0cd

--- src/net_permissions.h
+++ src/net_permissions.h
@@ -6,8 +6,8 @@
 #include <vector>
 #include <netaddress.h>
 
-#ifndef BITCOIN_NET_PERMISSIONS_H
-#define BITCOIN_NET_PERMISSIONS_H
+#ifndef SYSCOIN_NET_PERMISSIONS_H
+#define SYSCOIN_NET_PERMISSIONS_H
 enum NetPermissionFlags
 {
     PF_NONE = 0,
@@ -59,4 +59,4 @@ public:
     CSubNet m_subnet;
 };
 
-#endif // BITCOIN_NET_PERMISSIONS_H
+#endif // SYSCOIN_NET_PERMISSIONS_H
