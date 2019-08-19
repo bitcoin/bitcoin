b7e4be7f6187b23a9d2cf73f79fbb42b0b83e2ca
Wladimir J. van der Laan <laanwj@protonmail.com>
2019-08-19 11:22:18 +0200

    Merge #16646: qa: Run tests with UPnP disabled
    b168dd30cf71ac176e271bc610b0b1a79ceaf075 Bugfix: QA: Run tests with UPnP disabled (Luke Dashjr)
    
    Pull request description:
    
      This replaces #16560 by adding `upnp=0` to `bitcoin.conf` rather than passing it to nodes.
    
      > Needed for builds configured with --enable-upnp-default
    
      You can test this change using:
      ```bash
      ./configure --enable-upnp-default && make -j6 && test/functional/test_runner.py feature_config_args.py
      ```
    
      on master the test will fail without this change.
    
    ACKs for top commit:
      practicalswift:
        ACK b168dd30cf71ac176e271bc610b0b1a79ceaf075 -- diff looks correct
    
    Tree-SHA512: e639dd480dda2cffa19a679018c4bd7e4bd4d0f5e3001d6b407b833e3c166bde98b201063e267b8e45f8a20b0d53ec8bc028bec806b2357f9a7ba314cc4e2d07

--- test/functional/test_framework/util.py
+++ test/functional/test_framework/util.py
@@ -336,6 +336,7 @@ def initialize_datadir(dirname, n, chain):
         f.write("discover=0\n")
         f.write("listenonion=0\n")
         f.write("printtoconsole=0\n")
+        f.write("upnp=0\n")
         os.makedirs(os.path.join(datadir, 'stderr'), exist_ok=True)
         os.makedirs(os.path.join(datadir, 'stdout'), exist_ok=True)
     return datadir
