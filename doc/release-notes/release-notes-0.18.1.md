2c283389d8a71e2f2209495b1cd37b7b69d926ec
sidhujag <sidhujag@gmail.com>
2019-08-19 08:00:39 -0700

    ren
    Former-commit-id: 688efe554f3dc5c49db333e19a29d70284bbfd5f

--- doc/release-notes/release-notes-0.18.1.md
+++ doc/release-notes/release-notes-0.18.1.md
@@ -19,7 +19,7 @@ How to Upgrade
 If you are running an older version, shut it down. Wait until it has
 completely shut down (which might take a few minutes for older
 versions), then run the installer (on Windows) or just copy over
-`/Applications/Bitcoin-Qt` (on Mac) or `bitcoind`/`bitcoin-qt` (on
+`/Applications/Bitcoin-Qt` (on Mac) or `syscoind`/`bitcoin-qt` (on
 Linux).
 
 The first time you run version 0.15.0 or newer, your chainstate database
--- src/coins.h
+++ src/coins.h
@@ -319,7 +319,7 @@ const Coin& AccessByTxid(const CCoinsViewCache& cache, const uint256& txid);
 /**
  * This is a minimally invasive approach to shutdown on LevelDB read errors from the
  * chainstate, while keeping user interface out of the common library, which is shared
- * between bitcoind, and bitcoin-qt and non-server tools.
+ * between syscoind, and bitcoin-qt and non-server tools.
  *
  * Writes do not need similar protection, as failure to write is handled by the caller.
 */
