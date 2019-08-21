428ac70095253225f64462ee15c595644747f376
Jeffrey Czyz <jkczyz@gmail.com>
2019-08-21 16:19:51 -0700

    Add VALIDATION to BCLog::LogFlags
    This flag is for logging from within CValidationInterface (see #12994).
    A separate flag is desirable as the logging can be noisy and thus may
    need to be disabled without affecting other logging.

--- src/logging.cpp
+++ src/logging.cpp
@@ -162,6 +162,7 @@ const CLogCategoryDesc LogCategories[] =
     {BCLog::COINDB, "coindb"},
     {BCLog::QT, "qt"},
     {BCLog::LEVELDB, "leveldb"},
+    {BCLog::VALIDATION, "validation"},
     {BCLog::ALL, "1"},
     {BCLog::ALL, "all"},
 };
--- src/logging.h
+++ src/logging.h
@@ -54,6 +54,7 @@ namespace BCLog {
         COINDB      = (1 << 18),
         QT          = (1 << 19),
         LEVELDB     = (1 << 20),
+        VALIDATION  = (1 << 21),
         ALL         = ~(uint32_t)0,
     };
 
