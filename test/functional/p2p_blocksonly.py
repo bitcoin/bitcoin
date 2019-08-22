c21a98a63e9b97ec1919f7b6a3eceb5ad7886941
John Newbery <john@johnnewbery.com>
2019-08-22 10:43:22 -0400

    [test] Clean up testing for disconnecting blocks-only peers that send us tx INVs
    This commit restructures the p2p_blocksonly.py test case in preparation
    for adding more tests. In future commits, we'll test for disconnecting
    blocks-only peers that send us TX messages or tx GETDATA messages.

--- test/functional/p2p_blocksonly.py
+++ test/functional/p2p_blocksonly.py
@@ -4,11 +4,10 @@
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 """Test p2p blocksonly"""
 
-from test_framework.messages import msg_tx, CTransaction, FromHex
+from test_framework.messages import msg_inv, CInv, CTransaction, FromHex
 from test_framework.mininode import P2PInterface
 from test_framework.test_framework import BitcoinTestFramework
 from test_framework.util import assert_equal
-
 
 class P2PBlocksOnly(BitcoinTestFramework):
     def set_test_params(self):
@@ -17,9 +16,12 @@ class P2PBlocksOnly(BitcoinTestFramework):
         self.extra_args = [["-blocksonly"]]
 
     def run_test(self):
+        self.nodes[0].add_p2p_connection(P2PInterface())  # sends us a tx INV message
         self.nodes[0].add_p2p_connection(P2PInterface())
 
-        self.log.info('Check that txs from p2p are rejected and result in disconnect')
+        assert_equal(self.nodes[0].getnetworkinfo()['localrelay'], False)
+
+        self.log.info('Create signed transaction')
         prevtx = self.nodes[0].getblock(self.nodes[0].getblockhash(1), 2)['tx'][0]
         rawtx = self.nodes[0].createrawtransaction(
             inputs=[{
@@ -39,24 +41,20 @@ class P2PBlocksOnly(BitcoinTestFramework):
                 'scriptPubKey': prevtx['vout'][0]['scriptPubKey']['hex'],
             }],
         )['hex']
-        assert_equal(self.nodes[0].getnetworkinfo()['localrelay'], False)
-        with self.nodes[0].assert_debug_log(['transaction sent in violation of protocol peer=0']):
-            self.nodes[0].p2p.send_message(msg_tx(FromHex(CTransaction(), sigtx)))
-            self.nodes[0].p2p.wait_for_disconnect()
-            assert_equal(self.nodes[0].getmempoolinfo()['size'], 0)
+        tx = FromHex(CTransaction(), sigtx)
 
-        # Remove the disconnected peer and add a new one.
-        del self.nodes[0].p2ps[0]
-        self.nodes[0].add_p2p_connection(P2PInterface())
+        self.log.info('Check that a TX inv from a blocks-only peer results in disconnect')
+        with self.nodes[0].assert_debug_log(['in violation of protocol']):
+            self.nodes[0].p2ps[0].send_message(msg_inv([CInv(1, int(tx.rehash(), 16))]))
+            self.nodes[0].p2ps[0].wait_for_disconnect()
 
         self.log.info('Check that txs from rpc are not rejected and relayed to other peers')
         assert_equal(self.nodes[0].getpeerinfo()[0]['relaytxes'], True)
         txid = self.nodes[0].testmempoolaccept([sigtx])[0]['txid']
         with self.nodes[0].assert_debug_log(['received getdata for: tx {} peer=1'.format(txid)]):
             self.nodes[0].sendrawtransaction(sigtx)
-            self.nodes[0].p2p.wait_for_tx(txid)
+            self.nodes[0].p2ps[1].wait_for_tx(txid)
             assert_equal(self.nodes[0].getmempoolinfo()['size'], 1)
-
 
 if __name__ == '__main__':
     P2PBlocksOnly().main()
