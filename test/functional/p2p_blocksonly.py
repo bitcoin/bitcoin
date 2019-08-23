2964f78e811cfcbe55c4c27c3d79e1f571cc5c32
John Newbery <john@johnnewbery.com>
2019-08-23 16:27:58 -0400

    [test] Add testing for disconnecting blocks-only peers that send us TXs
    If a blocks-only peer sends us a TX message, we should disconnect. Add a
    test for this.

--- test/functional/p2p_blocksonly.py
+++ test/functional/p2p_blocksonly.py
@@ -4,7 +4,7 @@
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 """Test p2p blocksonly"""
 
-from test_framework.messages import msg_inv, CInv, CTransaction, FromHex
+from test_framework.messages import msg_inv, msg_tx, CInv, CTransaction, FromHex
 from test_framework.mininode import P2PInterface
 from test_framework.test_framework import BitcoinTestFramework
 from test_framework.util import assert_equal
@@ -17,6 +17,7 @@ class P2PBlocksOnly(BitcoinTestFramework):
 
     def run_test(self):
         self.nodes[0].add_p2p_connection(P2PInterface())  # sends us a tx INV message
+        self.nodes[0].add_p2p_connection(P2PInterface())  # sends us a TX message
         self.nodes[0].add_p2p_connection(P2PInterface())
 
         assert_equal(self.nodes[0].getnetworkinfo()['localrelay'], False)
@@ -48,12 +49,17 @@ class P2PBlocksOnly(BitcoinTestFramework):
             self.nodes[0].p2ps[0].send_message(msg_inv([CInv(1, int(tx.rehash(), 16))]))
             self.nodes[0].p2ps[0].wait_for_disconnect()
 
+        self.log.info('Check that a TX message from a blocks-only peer results in disconnect')
+        with self.nodes[0].assert_debug_log(['in violation of protocol']):
+            self.nodes[0].p2ps[1].send_message(msg_tx(tx))
+            self.nodes[0].p2ps[1].wait_for_disconnect()
+
         self.log.info('Check that txs from rpc are not rejected and relayed to other peers')
         assert_equal(self.nodes[0].getpeerinfo()[0]['relaytxes'], True)
         txid = self.nodes[0].testmempoolaccept([sigtx])[0]['txid']
-        with self.nodes[0].assert_debug_log(['received getdata for: tx {} peer=1'.format(txid)]):
+        with self.nodes[0].assert_debug_log(['received getdata for: tx {} peer=2'.format(txid)]):
             self.nodes[0].sendrawtransaction(sigtx)
-            self.nodes[0].p2ps[1].wait_for_tx(txid)
+            self.nodes[0].p2ps[2].wait_for_tx(txid)
             assert_equal(self.nodes[0].getmempoolinfo()['size'], 1)
 
 if __name__ == '__main__':
