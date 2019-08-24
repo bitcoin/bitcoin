7fcb8f53eda298a0e84348e16ad15d9b70e6c75d
MeshCollider <dobsonsa68@gmail.com>
2019-08-24 14:44:46 +1200

    Merge #16370: depends: cleanup package configure flags
    c295cba5a2f934e51a7c8610ab4c58b8e9d56619 depends: zeromq: disable draft classes and methods (fanquake)
    0072237b9e33e0b89f6c9f51dd0b946fa89a6134 depends: xproto: configure flags cleanup (fanquake)
    6a8ada3a4f67affcf0ef7452e206083d7b58b2bc depends: qrencode: configure flags cleanup (fanquake)
    86beb8cdc4e312bd0bed2cbb273aebb792be2747 depends: fontconfig: configure flags cleanup (fanquake)
    e656d95ec74336c2bd93bd387f67aeb6aed4dc40 depends: libxcb: configure flags cleanup (fanquake)
    e439388b352b7dfbf2e00c6ba2970fed0a4a5554 depends: libXau: configure flags cleanup (fanquake)
    
    Pull request description:
    
      Related to #16354.
    
      This PR adds additional configure flags to packages in depends to explicitly disable features we aren't using; similar to #16183. It also fixes passing `--without-tools` to `qrencode`.
    
      I've added `--disable-drafts` to `zeromq`:
      ```bash
      Build and install draft classes and methods [default=yes]
      ```
    
      I'm not entirely sure how far we want to take this. i.e in the `zeromq` package we explicitly pass `--without-libsodium`, even though it's disabled by default.
    
      Do we also want to explicitly pass all the other `--without` flags? :
      ```bash
        --with-libgssapi_krb5   require libzmq build with libgssapi_krb5
                                [default=no]
        --with-libsodium        use libsodium instead of built-in tweetnacl
                                [default=no]
        --with-pgm              build libzmq with PGM extension. Requires pkg-config
                                [default=no]
        --with-norm             build libzmq with NORM protocol extension,
                                optionally specifying norm path [default=no]
        --with-vmci             build libzmq with VMCI transport [default=no]
      ```
    
    ACKs for top commit:
      dongcarl:
        ACK c295cba5a2f934e51a7c8610ab4c58b8e9d56619
    
    Tree-SHA512: df6d38b863b4008ed2cb06c97eb0e21eaa4b5fde552876065ba7f3c87bf6e372e5b954a51bf3fde2151cfb6d2c022227d34337fc6e50ce0caa1d518abbd2412a

--- depends/packages/libxcb.mk
+++ depends/packages/libxcb.mk
@@ -6,7 +6,7 @@ $(package)_sha256_hash=98d9ab05b636dd088603b64229dd1ab2d2cc02ab807892e107d674f9c
 $(package)_dependencies=xcb_proto libXau
 
 define $(package)_set_vars
-$(package)_config_opts=--disable-static
+$(package)_config_opts=--disable-static --disable-build-docs --without-doxygen --without-launchd
 # Because we pass -qt-xcb to Qt, it will compile in a set of xcb helper libraries and extensions,
 # so we skip building all of the extensions here.
 # More info is available from: https://doc.qt.io/qt-5.9/linux-requirements.html
--- depends/packages/xproto.mk
+++ depends/packages/xproto.mk
@@ -5,7 +5,7 @@ $(package)_file_name=$(package)-$($(package)_version).tar.bz2
 $(package)_sha256_hash=636162c1759805a5a0114a369dffdeccb8af8c859ef6e1445f26a4e6e046514f
 
 define $(package)_set_vars
-$(package)_config_opts=--disable-shared
+$(package)_config_opts=--without-fop --without-xmlto --without-xsltproc --disable-specs
 endef
 
 define $(package)_config_cmds
