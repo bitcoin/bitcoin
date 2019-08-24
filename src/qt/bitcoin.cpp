c8de347a9d6c88fe67d77aba6fcce1b7fd66791c
Sjors Provoost <sjors@sprovoost.nl>
2019-08-24 19:13:04 +0200

    [gui] intro: add prune preference
    Adds a checkbox to the introduction screen letting the user enable pruning from the start.
    Disable checkbox when launched with -prune

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -492,8 +492,9 @@ int GuiMain(int argc, char* argv[])
     /// 5. Now that settings and translations are available, ask user for data directory
     // User language is set up: pick a data directory
     bool did_show_intro = false;
+    bool prune = false; // Intro dialog prune check box
     // Gracefully exit if the user cancels
-    if (!Intro::showIfNeeded(*node, did_show_intro)) return EXIT_SUCCESS;
+    if (!Intro::showIfNeeded(*node, did_show_intro, prune)) return EXIT_SUCCESS;
 
     /// 6. Determine availability of data directory and parse bitcoin.conf
     /// - Do not call GetDataDir(true) before this step finishes
@@ -566,6 +567,11 @@ int GuiMain(int argc, char* argv[])
     app.parameterSetup();
     // Load GUI settings from QSettings
     app.createOptionsModel(gArgs.GetBoolArg("-resetguisettings", false));
+
+    if (did_show_intro) {
+        // Store intro dialog settings other than datadir (network specific)
+        app.SetPrune(prune, true);
+    }
 
     if (gArgs.GetBoolArg("-splash", DEFAULT_SPLASHSCREEN) && !gArgs.GetBoolArg("-min", false))
         app.createSplashScreen(networkStyle.data());
