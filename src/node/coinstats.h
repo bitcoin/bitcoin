501f404d7b8760ad88d42ae2bec7064469e453d4
sidhujag <sidhujag@gmail.com>
2019-08-27 10:39:59 -0700

    update ifdef
    Former-commit-id: ce8050119f7a00597226ab856d51c5455b73c82a

--- src/node/coinstats.h
+++ src/node/coinstats.h
@@ -3,8 +3,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_NODE_COINSTATS_H
-#define BITCOIN_NODE_COINSTATS_H
+#ifndef SYSCOIN_NODE_COINSTATS_H
+#define SYSCOIN_NODE_COINSTATS_H
 
 #include <amount.h>
 #include <uint256.h>
@@ -30,4 +30,4 @@ struct CCoinsStats
 //! Calculate statistics about the unspent transaction output set
 bool GetUTXOStats(CCoinsView* view, CCoinsStats& stats);
 
-#endif // BITCOIN_NODE_COINSTATS_H
+#endif // SYSCOIN_NODE_COINSTATS_H
