c526c1db6184920e30ec70b3e111d3cff1e88b9c
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2019-09-03 21:09:26 +0300

    refactor: Define =default copy assignment explicitly

--- src/support/allocators/secure.h
+++ src/support/allocators/secure.h
@@ -28,6 +28,7 @@ struct secure_allocator : public std::allocator<T> {
     typedef typename base::value_type value_type;
     secure_allocator() noexcept {}
     secure_allocator(const secure_allocator& a) noexcept : base(a) {}
+    secure_allocator& operator=(const secure_allocator& a) = default;
     template <typename U>
     secure_allocator(const secure_allocator<U>& a) noexcept : base(a)
     {
