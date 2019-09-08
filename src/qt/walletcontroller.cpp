5d131df1909db54621c32e0e879f153c5c09c6ed
Andrew Chow <achow101-github@achow101.com>
2019-09-08 16:40:05 -0400

    HTML escape the wallet name in more dialogs and notifications
    Former-commit-id: 61a6b44f93f1a97037cd67e19b0afcdbe77e0db0

--- src/qt/walletcontroller.cpp
+++ src/qt/walletcontroller.cpp
@@ -75,7 +75,7 @@ void WalletController::closeWallet(WalletModel* wallet_model, QWidget* parent)
 {
     QMessageBox box(parent);
     box.setWindowTitle(tr("Close wallet"));
-    box.setText(tr("Are you sure you wish to close wallet <i>%1</i>?").arg(wallet_model->getDisplayName()));
+    box.setText(tr("Are you sure you wish to close wallet <i>%1</i>?").arg(GUIUtil::HtmlEscape(wallet_model->getDisplayName())));
     box.setInformativeText(tr("Closing the wallet for too long can result in having to resync the entire chain if pruning is enabled."));
     box.setStandardButtons(QMessageBox::Yes|QMessageBox::Cancel);
     box.setDefaultButton(QMessageBox::Yes);
