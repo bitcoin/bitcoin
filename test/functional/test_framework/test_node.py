8d79e1a365e70e32bcd409b7f03932540f627d31
MarcoFalke <falke.marco@gmail.com>
2019-09-12 13:28:37 +0300

    Merge #16551: test: Test that low difficulty chain fork is rejected
    333317ce6b67aa92f7363d48cd750712190b4b6b test: Test that low difficulty chain fork is rejected (MarcoFalke)
    fa31dc1bf4ee471c4641eef8de02702ba0619ae7 test: Pass down correct chain name in tests (MarcoFalke)
    
    Pull request description:
    
      To prevent OOM, Bitcoin Core will reject chain forks at low difficulty by default. This is the only use-case of checkpoints, so add a test for it to make sure the feature works as expected. If it didn't work, checkpoints would have no use-case and we might as well remove them
    
    ACKs for top commit:
      Sjors:
        Thanks for adding the node 1 example. Code review ACK 333317c
    
    Tree-SHA512: 90dffa540d0904f3cffb61d2382b1a26f84fe9560b7013e4461546383add31a8757b350616a6d43217c59ef7b8b2a1b62bb3bab582c679cbb2c660a782ce7be1
    
    Former-commit-id: 1fa7bebf7aeda3d1f11040d32d56dd48800d66e4

--- test/functional/test_framework/test_node.py
+++ test/functional/test_framework/test_node.py
@@ -489,7 +489,7 @@ class TestNode():
         if 'dstaddr' not in kwargs:
             kwargs['dstaddr'] = '127.0.0.1'
 
-        p2p_conn.peer_connect(**kwargs)()
+        p2p_conn.peer_connect(**kwargs, net=self.chain)()
         self.p2ps.append(p2p_conn)
         if wait_for_verack:
             p2p_conn.wait_for_verack()
