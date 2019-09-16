a61d8765df484a87e99f8d8680d7e4096a873998
Gregory Sanders <gsanders87@gmail.com>
2019-09-16 12:26:58 -0400

    Revert "gui: Generate bech32 addresses by default (take 2, fixup)"
    This reverts commit 52b615f92192cae203711404b64e4195560ed0ec.

--- src/qt/receivecoinsdialog.cpp
+++ src/qt/receivecoinsdialog.cpp
@@ -7,7 +7,6 @@
 #include <qt/receivecoinsdialog.h>
 #include <qt/forms/ui_receivecoinsdialog.h>
 
-#include <interfaces/node.h>
 #include <qt/addresstablemodel.h>
 #include <qt/optionsmodel.h>
 #include <qt/platformstyle.h>
@@ -93,16 +92,10 @@ void ReceiveCoinsDialog::setModel(WalletModel *_model)
         // Last 2 columns are set by the columnResizingFixer, when the table geometry is ready.
         columnResizingFixer = new GUIUtil::TableViewLastColumnResizingFixer(tableView, AMOUNT_MINIMUM_COLUMN_WIDTH, DATE_COLUMN_WIDTH, this);
 
-        if (model->node().isAddressTypeSet()) {
-            // user explicitly set the type, use it
-            if (model->wallet().getDefaultAddressType() == OutputType::BECH32) {
-                ui->useBech32->setCheckState(Qt::Checked);
-            } else {
-                ui->useBech32->setCheckState(Qt::Unchecked);
-            }
-        } else {
-            // Always fall back to bech32 in the gui
+        if (model->wallet().getDefaultAddressType() == OutputType::BECH32) {
             ui->useBech32->setCheckState(Qt::Checked);
+        } else {
+            ui->useBech32->setCheckState(Qt::Unchecked);
         }
 
         // Set the button to be enabled or disabled based on whether the wallet can give out new addresses.
