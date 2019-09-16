a6ee3e92911153c550e34518a2a3f5ed787bfa0c
MarcoFalke <falke.marco@gmail.com>
2019-09-16 09:22:49 -0400

    test: Use connect_nodes when connecting nodes in the test_framework

--- test/functional/rpc_net.py
+++ test/functional/rpc_net.py
@@ -54,6 +54,10 @@ class NetTest(BitcoinTestFramework):
         self.extra_args = [["-minrelaytxfee=0.00001000"],["-minrelaytxfee=0.00000500"]]
 
     def run_test(self):
+        self.log.info('Connect nodes both way')
+        connect_nodes(self.nodes[0], 1)
+        connect_nodes(self.nodes[1], 0)
+
         self._test_connection_count()
         self._test_getnettotals()
         self._test_getnetworkinfo()
@@ -105,7 +109,10 @@ class NetTest(BitcoinTestFramework):
         wait_until(lambda: self.nodes[0].getnetworkinfo()['connections'] == 0, timeout=3)
 
         self.nodes[0].setnetworkactive(state=True)
-        connect_nodes_bi(self.nodes, 0, 1)
+        self.log.info('Connect nodes both way')
+        connect_nodes(self.nodes[0], 1)
+        connect_nodes(self.nodes[1], 0)
+
         assert_equal(self.nodes[0].getnetworkinfo()['networkactive'], True)
         assert_equal(self.nodes[0].getnetworkinfo()['connections'], 2)
 
