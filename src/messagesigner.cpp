9af0784929b1220c8d2761283538a12a3e110832
konez2k <konez@protonmail.com>
2019-09-20 20:31:34 +0200

    Implement hasher and signer for ProTx transactions

---
+++ src/messagesigner.cpp
@@ -0,0 +1,73 @@
+// Copyright (c) 2014-2018 The Dash Core developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <base58.h>
+#include <hash.h>
+#include <key_io.h>
+#include <util/validation.h>
+#include <messagesigner.h>
+#include <tinyformat.h>
+#include <util/strencodings.h>
+
+bool CMessageSigner::GetKeysFromSecret(const std::string& strSecret, CKey& keyRet, CPubKey& pubkeyRet)
+{
+    keyRet = DecodeSecret(strSecret);
+    if (!keyRet.IsValid())
+        return false;
+
+    pubkeyRet = keyRet.GetPubKey();
+
+    return true;
+}
+
+bool CMessageSigner::SignMessage(const std::string& strMessage, std::vector<unsigned char>& vchSigRet, const CKey& key)
+{
+    CHashWriter ss(SER_GETHASH, 0);
+    ss << strMessageMagic;
+    ss << strMessage;
+
+    return CHashSigner::SignHash(ss.GetHash(), key, vchSigRet);
+}
+
+bool CMessageSigner::VerifyMessage(const CPubKey& pubkey, const std::vector<unsigned char>& vchSig, const std::string& strMessage, std::string& strErrorRet)
+{
+    return VerifyMessage(pubkey.GetID(), vchSig, strMessage, strErrorRet);
+}
+
+bool CMessageSigner::VerifyMessage(const CKeyID& keyID, const std::vector<unsigned char>& vchSig, const std::string& strMessage, std::string& strErrorRet)
+{
+    CHashWriter ss(SER_GETHASH, 0);
+    ss << strMessageMagic;
+    ss << strMessage;
+
+    return CHashSigner::VerifyHash(ss.GetHash(), keyID, vchSig, strErrorRet);
+}
+
+bool CHashSigner::SignHash(const uint256& hash, const CKey& key, std::vector<unsigned char>& vchSigRet)
+{
+    return key.SignCompact(hash, vchSigRet);
+}
+
+bool CHashSigner::VerifyHash(const uint256& hash, const CPubKey& pubkey, const std::vector<unsigned char>& vchSig, std::string& strErrorRet)
+{
+    return VerifyHash(hash, pubkey.GetID(), vchSig, strErrorRet);
+}
+
+bool CHashSigner::VerifyHash(const uint256& hash, const CKeyID& keyID, const std::vector<unsigned char>& vchSig, std::string& strErrorRet)
+{
+    CPubKey pubkeyFromSig;
+    if(!pubkeyFromSig.RecoverCompact(hash, vchSig)) {
+        strErrorRet = "Error recovering public key.";
+        return false;
+    }
+
+    if(pubkeyFromSig.GetID() != keyID) {
+        strErrorRet = strprintf("Keys don't match: pubkey=%s, pubkeyFromSig=%s, hash=%s, vchSig=%s",
+                    keyID.ToString(), pubkeyFromSig.GetID().ToString(), hash.ToString(),
+                    EncodeBase64(&vchSig[0], vchSig.size()));
+        return false;
+    }
+
+    return true;
+}
