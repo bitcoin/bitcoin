35121706b954d4c6edee97b00f6237acbe4e0d99
MarcoFalke <falke.marco@gmail.com>
2019-09-24 11:32:34 -0400

    Merge #16912: doc: Remove Doxygen intro from src/bitcoind.cpp
    dbdc758c27cfdda9d255742b6c6ff4d1b7be82df doc: Improve doxygen readme navigation section (Jon Layton)
    c15ac2c0aa45c59aee6d36c2d6d5210290dc601c doc: Move doxygen intro to file for USE_MDFILE_AS_MANPAGE (Jon Layton)
    
    Pull request description:
    
      With `USE_MDFILE_AS_MAINPAGE`, this moves the introductory Doxygen comment to its own file. This makes `bitcoind.cpp` cleaner.
    
      It also removes the `\mainpage` header text, which was smaller than the section titles, and improves the Navigation section.
    
    ACKs for top commit:
      promag:
        ACK dbdc758c27cfdda9d255742b6c6ff4d1b7be82df.
    
    Tree-SHA512: 9352baad655877437913b74dc8888a71d1cccf55a837657ee2630fde3f427d0f0339155b7ab3d9e63a9edb9d53512d747eafcb11987a7c26c47a6df2eca93351
    
    Former-commit-id: 7d79df057a49a20d981b22283358eca94c80b691

--- src/syscoind.cpp
+++ src/syscoind.cpp
@@ -28,24 +28,6 @@
 #include <masternodeconfig.h>
 const std::function<std::string(const char*)> G_TRANSLATION_FUN = nullptr;
 
-/* Introduction text for doxygen: */
-
-/*! \mainpage Developer documentation
- *
- * \section intro_sec Introduction
- *
- * This is the developer documentation of the reference client for an experimental new digital currency called Syscoin,
- * which enables instant payments to anyone, anywhere in the world. Syscoin uses peer-to-peer technology to operate
- * with no central authority: managing transactions and issuing money are carried out collectively by the network.
- *
- * The software is a community-driven open source project, released under the MIT license.
- *
- * See https://github.com/syscoin/syscoin and https://syscoin.org/ for further information about the project.
- *
- * \section Navigation
- * Use the buttons <code>Namespaces</code>, <code>Classes</code> or <code>Files</code> at the top of the page to start navigating the code.
- */
-
 static void WaitForShutdown()
 {
     while (!ShutdownRequested())
