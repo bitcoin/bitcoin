9ef73e6adde81245d90d472f51c56525c5a0e3b6
Alexander Block <ablock84@gmail.com>
2019-09-24 10:42:38 +0200

    Fix mining.py

--- test/functional/mining.py
+++ test/functional/mining.py
@@ -40,9 +40,8 @@ class MiningTest(BitcoinTestFramework):
         assert_equal(mining_info['chain'], 'regtest')
         assert_equal(mining_info['currentblocksize'], 0)
         assert_equal(mining_info['currentblocktx'], 0)
-        assert_equal(mining_info['currentblockweight'], 0)
         assert_equal(mining_info['difficulty'], Decimal('4.656542373906925E-10'))
-        assert_equal(mining_info['networkhashps'], Decimal('0.003333333333333334'))
+        assert_equal(mining_info['networkhashps'], Decimal('0.01282051282051282'))
         assert_equal(mining_info['pooledtx'], 0)
 
         # Mine a block to leave initial block download
