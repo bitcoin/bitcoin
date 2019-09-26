eafcea7a0ab17512f2b9e2a724685ca193920f04
João Barbosa <joao.paulo.barbosa@gmail.com>
2019-09-26 21:43:44 +0100

    gui: Fix duplicate wallet showing up
    The slot BitcoinGUI::addWallet can be invoked twice for the same
    WalletModel due to a concurrent wallet being loaded after the first `connect()`:
    
    ```cpp
     connect(wallet_controller, &WalletController::walletAdded, this, &BitcoinGUI::addWallet);
     connect(wallet_controller, &WalletController::walletRemoved, this, &BitcoinGUI::removeWallet);
    
     for (WalletModel* wallet_model : m_wallet_controller->getOpenWallets()) {
         addWallet(wallet_model);
    ```
    
    Github-Pull: #16963
    Rebased-From: 6d6a7a8403ae923f189812edebdd95761de0e7f2

--- src/qt/walletframe.cpp
+++ src/qt/walletframe.cpp
@@ -40,11 +40,11 @@ void WalletFrame::setClientModel(ClientModel *_clientModel)
     this->clientModel = _clientModel;
 }
 
-void WalletFrame::addWallet(WalletModel *walletModel)
+bool WalletFrame::addWallet(WalletModel *walletModel)
 {
-    if (!gui || !clientModel || !walletModel) return;
+    if (!gui || !clientModel || !walletModel) return false;
 
-    if (mapWalletViews.count(walletModel) > 0) return;
+    if (mapWalletViews.count(walletModel) > 0) return false;
 
     WalletView *walletView = new WalletView(platformStyle, this);
     walletView->setBitcoinGUI(gui);
@@ -68,6 +68,8 @@ void WalletFrame::addWallet(WalletModel *walletModel)
     });
 
     connect(walletView, &WalletView::outOfSyncWarningClicked, this, &WalletFrame::outOfSyncWarningClicked);
+
+    return true;
 }
 
 void WalletFrame::setCurrentWallet(WalletModel* wallet_model)
