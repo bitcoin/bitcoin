b137e4ca5e0bf5d25c28ba2387e10c9346fa64fe
Jon Atack <jon@atack.com>
2019-10-01 10:53:09 +0200

    build: 0.19 release updates on master
    as per doc/release-process.md.
    
    Note: On branch-off, these same changes should be made on the release branch, with also these additional changes to both files:
    
      - set `CLIENT_VERSION_REVISION` to `0`
      - set `CLIENT_VERSION_IS_RELEASE` to `true`
    
    
    Former-commit-id: 75039974cd3b93ca3ae2779cdd3f435bba350636

--- build_msvc/syscoin_config.h
+++ build_msvc/syscoin_config.h
@@ -14,7 +14,7 @@
 #define CLIENT_VERSION_MAJOR 4
 
 /* Minor version */
-#define CLIENT_VERSION_MINOR 18
+#define CLIENT_VERSION_MINOR 1
 
 /* Build revision */
 #define CLIENT_VERSION_REVISION 0
@@ -346,7 +346,7 @@
 #define PACKAGE_NAME "Syscoin Core"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "Syscoin Core 4.0.0"
+#define PACKAGE_STRING "Syscoin Core 4.1.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "syscoin"
@@ -355,7 +355,7 @@
 #define PACKAGE_URL "https://syscoin.org/"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "4.0.0"
+#define PACKAGE_VERSION "4.1.0"
 
 /* Define to necessary symbol if this constant uses a non-standard name on
    your system. */
