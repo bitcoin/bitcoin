9ccc72625bed9be2e8abce9f7f4b4d4e4f3feae4
sidhujag <jsidhu@blockchainfoundry.co>
2019-10-03 21:34:30 -0700

    move repeated fn
    Former-commit-id: 1297eade53030434f0ce3d02cfd5da8759c6b534

--- src/util/threadnames.cpp
+++ src/util/threadnames.cpp
@@ -65,8 +65,3 @@ void util::ThreadSetInternalName(std::string&& name)
 {
     SetInternalName(std::move(name));
 }
-
-void util::ThreadSetInternalName(std::string&& name)
-{
-    SetInternalName(std::move(name));
-}
