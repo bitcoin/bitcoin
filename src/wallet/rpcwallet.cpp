c2df9366f0a924bbdc7760c9c78bef056666222c
fanquake <fanquake@gmail.com>
2019-10-04 13:27:52 -0400

    Merge #16507: feefilter: Compute the absolute fee rather than stored rate
    eb7b78165966f2c79da71b993c4c4d793e37297f modify p2p_feefilter test to catch rounding error (Gregory Sanders)
    6a51f7951716d6d6fc0f9b56028f3a0dd02b61c8 Disallow implicit conversion for CFeeRate constructor (Gregory Sanders)
    8e59af55aaf1b196575084bce2448af02d97d745 feefilter: Compute the absolute fee rather than stored rate to match mempool acceptance logic (Gregory Sanders)
    
    Pull request description:
    
      This means we will use the rounding-down behavior in `GetFee` to match both mempool acceptance and wallet logic, with minimal changes.
    
      Fixes https://github.com/bitcoin/bitcoin/issues/16499
    
      Replacement PR for https://github.com/bitcoin/bitcoin/pull/16500
    
    ACKs for top commit:
      ajtowns:
        ACK eb7b78165966f2c79da71b993c4c4d793e37297f code review only
      naumenkogs:
        utACK eb7b78165966f2c79da71b993c4c4d793e37297f
      achow101:
        re ACK eb7b78165966f2c79da71b993c4c4d793e37297f
      promag:
        ACK eb7b78165966f2c79da71b993c4c4d793e37297f.
    
    Tree-SHA512: 484a11c8f0e825f0c983b1f7e71cf6252b1bba6858194abfe4c088da3bae8a418ec539ef6c4181bf30940e277a95c08d493595d59dfcc6ddf77c65b05563dd7e

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -2315,7 +2315,7 @@ static UniValue settxfee(const JSONRPCRequest& request)
     CAmount nAmount = AmountFromValue(request.params[0]);
     CFeeRate tx_fee_rate(nAmount, 1000);
     CFeeRate max_tx_fee_rate(pwallet->m_default_max_tx_fee, 1000);
-    if (tx_fee_rate == 0) {
+    if (tx_fee_rate == CFeeRate(0)) {
         // automatic selection
     } else if (tx_fee_rate < pwallet->chain().relayMinFee()) {
         throw JSONRPCError(RPC_INVALID_PARAMETER, strprintf("txfee cannot be less than min relay tx fee (%s)", pwallet->chain().relayMinFee().ToString()));
