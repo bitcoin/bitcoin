a0d5860f51861314b3a4bbbd250e9a4bc0c4756e
Andrew Chow <achow101-github@achow101.com>
2019-10-07 14:11:34 -0400

    Refactor: Move SetupGeneration code out of CWallet
    This commit does not change behavior.
    
    
    Former-commit-id: 92e9fc05dc5c63d02c771569b99667ac3b153529

--- src/wallet/scriptpubkeyman.cpp
+++ src/wallet/scriptpubkeyman.cpp
@@ -339,6 +339,19 @@ void LegacyScriptPubKeyMan::UpgradeKeyMetadata()
     }
 }
 
+bool LegacyScriptPubKeyMan::SetupGeneration(bool force)
+{
+    if ((CanGenerateKeys() && !force) || m_storage.IsLocked()) {
+        return false;
+    }
+
+    SetHDSeed(GenerateNewSeed());
+    if (!NewKeyPool()) {
+        return false;
+    }
+    return true;
+}
+
 bool LegacyScriptPubKeyMan::IsHDEnabled() const
 {
     return !hdChain.seed_id.IsNull();
