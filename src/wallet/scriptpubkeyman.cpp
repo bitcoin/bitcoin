ae82201145e967a67dac45e5f53355854cf8bd72
Andrew Chow <achow101-github@achow101.com>
2019-10-07 14:11:34 -0400

    Refactor: Move SetWalletFlag out of LegacyScriptPubKeyMan::UpgradeKeyMetadata
    This commit does not change behavior.

--- src/wallet/scriptpubkeyman.cpp
+++ src/wallet/scriptpubkeyman.cpp
@@ -320,8 +320,6 @@ void LegacyScriptPubKeyMan::UpgradeKeyMetadata()
             }
         }
     }
-    batch.reset(); //write before setting the flag
-    m_storage.SetWalletFlag(WALLET_FLAG_KEY_ORIGIN_METADATA);
 }
 
 bool LegacyScriptPubKeyMan::IsHDEnabled() const
