05a10689f82f67b09b34a9d8991b2321af6472be
MarcoFalke <falke.marco@gmail.com>
2019-10-08 14:42:17 -0400

    consensus: Explain why fCheckDuplicateInputs can not be skipped and remove it
    Former-commit-id: f589a2206f6d35082704633a9e4282c53389bf1c

--- src/test/fuzz/transaction.cpp
+++ src/test/fuzz/transaction.cpp
@@ -43,12 +43,7 @@ void test_one_input(const std::vector<uint8_t>& buffer)
     }
 
     CValidationState state_with_dupe_check;
-    const bool valid_with_dupe_check = CheckTransaction(tx, state_with_dupe_check, /* fCheckDuplicateInputs= */ true);
-    CValidationState state_without_dupe_check;
-    const bool valid_without_dupe_check = CheckTransaction(tx, state_without_dupe_check, /* fCheckDuplicateInputs= */ false);
-    if (valid_with_dupe_check) {
-        assert(valid_without_dupe_check);
-    }
+    (void)CheckTransaction(tx, state_with_dupe_check);
 
     const CFeeRate dust_relay_fee{DUST_RELAY_TX_FEE};
     std::string reason;
