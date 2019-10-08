0c75084694b5c5388afed3ced342f8d6309f367d
sidhujag <jsidhu@blockchainfoundry.co>
2019-10-08 08:21:42 -0700

    macros ifdef fix
    Former-commit-id: 777a86c77ac85b32d6976c318d219021973a8fa7

--- src/util/macros.h
+++ src/util/macros.h
@@ -2,10 +2,10 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_UTIL_MACROS_H
-#define BITCOIN_UTIL_MACROS_H
+#ifndef SYSCOIN_UTIL_MACROS_H
+#define SYSCOIN_UTIL_MACROS_H
 
 #define PASTE(x, y) x ## y
 #define PASTE2(x, y) PASTE(x, y)
 
-#endif // BITCOIN_UTIL_MACROS_H
+#endif // SYSCOIN_UTIL_MACROS_H
