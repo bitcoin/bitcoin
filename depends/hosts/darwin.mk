98785c341d5bee4d9902d47b2f53edfbb9c49fae
BlockMechanic <blockmecha@gmail.com>
2019-10-09 22:38:32 +0200

    depends hosts mods

--- depends/hosts/darwin.mk
+++ depends/hosts/darwin.mk
@@ -2,8 +2,16 @@ OSX_MIN_VERSION=10.10
 OSX_SDK_VERSION=10.11
 OSX_SDK=$(SDK_PATH)/MacOSX$(OSX_SDK_VERSION).sdk
 LD64_VERSION=253.9
+
+IOS_MIN_VERSION=12.4
+IOS_SDK_VERSION=12.4
+IOS_SDK=$(SDK_PATH)/iPhoneOS$(IOS_SDK_VERSION).sdk
+
 darwin_CC=clang -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION)
 darwin_CXX=clang++ -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION) -stdlib=libc++
+
+aarch64_darwin_CC=clang -target $(host) -arch arm64 -miphoneos-version-min=$(IOS_MIN_VERSION) --sysroot $(IOS_SDK) -mlinker-version=$(LD64_VERSION)
+aarch64_darwin_CXX=clang++ -target $(host) -arch arm64 -miphoneos-version-min=$(IOS_MIN_VERSION) --sysroot $(IOS_SDK) -mlinker-version=$(LD64_VERSION) -stdlib=libc++
 
 darwin_CFLAGS=-pipe
 darwin_CXXFLAGS=$(darwin_CFLAGS)
