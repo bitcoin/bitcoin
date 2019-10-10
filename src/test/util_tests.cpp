830dc498f1fb9abd7f85d9cfb3f1e0c0874278ea
Wladimir J. van der Laan <laanwj@protonmail.com>
2019-10-10 13:25:08 +0200

    util: Filter control characters out of log messages
    Belts and suspenders: make sure outgoing log messages don't contain
    potentially suspicious characters, such as terminal control codes.
    
    This escapes control characters except newline ('\n') in C syntax.
    It escapes instead of removes them to still allow for troubleshooting
    issues where they accidentally end up in strings.
    
    
    Former-commit-id: 6c571db8d2a95401caee2098775c7c29f2dda23b

--- src/test/util_tests.cpp
+++ src/test/util_tests.cpp
@@ -26,6 +26,11 @@
 
 #include <boost/test/unit_test.hpp>
 
+/* defined in logging.cpp */
+namespace BCLog {
+    std::string LogEscapeMessage(const std::string& str);
+}
+
 BOOST_FIXTURE_TEST_SUITE(util_tests, BasicTestingSetup)
 
 BOOST_AUTO_TEST_CASE(util_criticalsection)
@@ -1694,6 +1699,19 @@ BOOST_AUTO_TEST_CASE(test_spanparsing)
     BOOST_CHECK_EQUAL(SpanToStr(results[1]), "foo");
     BOOST_CHECK_EQUAL(SpanToStr(results[2]), "bar");
     BOOST_CHECK_EQUAL(SpanToStr(results[3]), "");
+}
+
+BOOST_AUTO_TEST_CASE(test_LogEscapeMessage)
+{
+    // ASCII and UTF-8 must pass through unaltered.
+    BOOST_CHECK_EQUAL(BCLog::LogEscapeMessage("Valid log message貓"), "Valid log message貓");
+    // Newlines must pass through unaltered.
+    BOOST_CHECK_EQUAL(BCLog::LogEscapeMessage("Message\n with newlines\n"), "Message\n with newlines\n");
+    // Other control characters are escaped in C syntax.
+    BOOST_CHECK_EQUAL(BCLog::LogEscapeMessage("\x01\x7f Corrupted log message\x0d"), R"(\x01\x7f Corrupted log message\x0d)");
+    // Embedded NULL characters are escaped too.
+    const std::string NUL("O\x00O", 3);
+    BOOST_CHECK_EQUAL(BCLog::LogEscapeMessage(NUL), R"(O\x00O)");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
