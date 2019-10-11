8019b6b150ff7444195a238470414c9deec5bf74
João Barbosa <joao.paulo.barbosa@gmail.com>
2019-10-11 01:50:49 +0100

    gui: Make RPCConsole::TabTypes an enum class

--- src/qt/rpcconsole.cpp
+++ src/qt/rpcconsole.cpp
@@ -1268,22 +1268,24 @@ void RPCConsole::showOrHideBanTableIfRequired()
 
 void RPCConsole::setTabFocus(enum TabTypes tabType)
 {
-    ui->tabWidget->setCurrentIndex(tabType);
+    ui->tabWidget->setCurrentIndex(int(tabType));
 }
 
 QString RPCConsole::tabTitle(TabTypes tab_type) const
 {
-    return ui->tabWidget->tabText(tab_type);
+    return ui->tabWidget->tabText(int(tab_type));
 }
 
 QKeySequence RPCConsole::tabShortcut(TabTypes tab_type) const
 {
     switch (tab_type) {
-    case TAB_INFO: return QKeySequence(Qt::CTRL + Qt::Key_I);
-    case TAB_CONSOLE: return QKeySequence(Qt::CTRL + Qt::Key_T);
-    case TAB_GRAPH: return QKeySequence(Qt::CTRL + Qt::Key_N);
-    case TAB_PEERS: return QKeySequence(Qt::CTRL + Qt::Key_P);
-    }
+    case TabTypes::INFO: return QKeySequence(Qt::CTRL + Qt::Key_I);
+    case TabTypes::CONSOLE: return QKeySequence(Qt::CTRL + Qt::Key_T);
+    case TabTypes::GRAPH: return QKeySequence(Qt::CTRL + Qt::Key_N);
+    case TabTypes::PEERS: return QKeySequence(Qt::CTRL + Qt::Key_P);
+    } // no default case, so the compiler can warn about missing cases
+
+    assert(false);
 }
 
 void RPCConsole::updateAlerts(const QString& warnings)
