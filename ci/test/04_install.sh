b19b30ba9c0548f0ecf704b2b6e283cdbb31a85c
Kittywhiskers Van Gogh <63189531+kittywhiskers@users.noreply.github.com>
2019-10-14 23:00:49 +0200

    merge bitcoin#16597: run full test suite on native macOS

--- ci/test/04_install.sh
+++ ci/test/04_install.sh
@@ -6,6 +6,32 @@
 
 export LC_ALL=C.UTF-8
 
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+  set +o errexit
+  pushd /usr/local/Homebrew || exit 1
+  git reset --hard origin/master
+  popd || exit 1
+  set -o errexit
+  ${CI_RETRY_EXE} brew update
+  # brew upgrade returns an error if any of the packages is already up to date
+  # Failure is safe to ignore, unless we really need an update.
+  brew upgrade $BREW_PACKAGES || true
+
+  # install new packages (brew install returns an error if already installed)
+  for i in $BREW_PACKAGES; do
+    if ! brew list | grep -q $i; then
+      ${CI_RETRY_EXE} brew install $i
+    fi
+  done
+
+  export PATH="/usr/local/opt/ccache/libexec:$PATH"
+  OPENSSL_PKG_CONFIG_PATH="/usr/local/opt/openssl@1.1/lib/pkgconfig"
+  export PKG_CONFIG_PATH=$OPENSSL_PKG_CONFIG_PATH:$PKG_CONFIG_PATH
+
+  ${CI_RETRY_EXE} pip3 install $PIP_PACKAGES
+
+fi
+
 mkdir -p "${BASE_SCRATCH_DIR}"
 mkdir -p "${CCACHE_DIR}"
 
@@ -41,15 +67,22 @@ else
 fi
 export -f DOCKER_EXEC
 
-DOCKER_EXEC free -m -h
-DOCKER_EXEC echo "Number of CPUs \(nproc\):" \$\(nproc\)
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+  top -l 1 -s 0 | awk ' /PhysMem/ {print}'
+  echo "Number of CPUs: $(sysctl -n hw.logicalcpu)"
+else
+  DOCKER_EXEC free -m -h
+  DOCKER_EXEC echo "Number of CPUs \(nproc\):" \$\(nproc\)
+fi
 
 if [ -n "$DPKG_ADD_ARCH" ]; then
   DOCKER_EXEC dpkg --add-architecture "$DPKG_ADD_ARCH"
 fi
 
-${CI_RETRY_EXE} DOCKER_EXEC apt-get update
-${CI_RETRY_EXE} DOCKER_EXEC apt-get install --no-install-recommends --no-upgrade -y $PACKAGES $DOCKER_PACKAGES
+if [ "$TRAVIS_OS_NAME" != "osx" ]; then
+  ${CI_RETRY_EXE} DOCKER_EXEC apt-get update
+  ${CI_RETRY_EXE} DOCKER_EXEC apt-get install --no-install-recommends --no-upgrade -y $PACKAGES $DOCKER_PACKAGES
+fi
 
 if [ ! -d ${DIR_QA_ASSETS} ]; then
   DOCKER_EXEC git clone https://github.com/bitcoin-core/qa-assets ${DIR_QA_ASSETS}
--- ci/test/05_before_script.sh
+++ ci/test/05_before_script.sh
@@ -6,21 +6,28 @@
 
 export LC_ALL=C.UTF-8
 
-DOCKER_EXEC echo \> \$HOME/.dashcore  # Make sure default datadir does not exist and is never read by creating a dummy file
-OSX_SDK_BASENAME="Xcode-${XCODE_VERSION}-${XCODE_BUILD_ID}-extracted-SDK-with-libcxx-headers.tar.gz"
-OSX_SDK_PATH="depends/sdk-sources/${OSX_SDK_BASENAME}"
+# Make sure default datadir does not exist and is never read by creating a dummy file
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+  echo > $HOME/Library/Application\ Support/DashCore
+else
+  DOCKER_EXEC echo \> \$HOME/.dashcore
+fi
 
-DOCKER_EXEC mkdir -p depends/SDKs depends/sdk-sources
+if [ "$TRAVIS_OS_NAME" != "osx" ]; then
 
-if [ -n "$XCODE_VERSION" ] && [ ! -f "$OSX_SDK_PATH" ]; then
-  DOCKER_EXEC curl --location --fail "${SDK_URL}/${OSX_SDK_BASENAME}" -o "$OSX_SDK_PATH"
-fi
-if [ -n "$XCODE_VERSION" ] && [ -f "$OSX_SDK_PATH" ]; then
-  DOCKER_EXEC tar -C "depends/SDKs" -xf "$OSX_SDK_PATH"
-fi
-if [[ $HOST = *-mingw32 ]]; then
-  DOCKER_EXEC update-alternatives --set $HOST-g++ \$\(which $HOST-g++-posix\)
-fi
-if [ -z "$NO_DEPENDS" ]; then
-  DOCKER_EXEC CONFIG_SHELL= make $MAKEJOBS -C depends HOST=$HOST $DEP_OPTS
+  DOCKER_EXEC mkdir -p depends/SDKs depends/sdk-sources
+
+  if [ -n "$XCODE_VERSION" ] && [ ! -f "$OSX_SDK_PATH" ]; then
+    DOCKER_EXEC curl --location --fail "${SDK_URL}/${OSX_SDK_BASENAME}" -o "$OSX_SDK_PATH"
+  fi
+  if [ -n "$XCODE_VERSION" ] && [ -f "$OSX_SDK_PATH" ]; then
+    DOCKER_EXEC tar -C "depends/SDKs" -xf "$OSX_SDK_PATH"
+  fi
+  if [[ $HOST = *-mingw32 ]]; then
+    DOCKER_EXEC update-alternatives --set $HOST-g++ \$\(which $HOST-g++-posix\)
+  fi
+  if [ -z "$NO_DEPENDS" ]; then
+    DOCKER_EXEC CONFIG_SHELL= make $MAKEJOBS -C depends HOST=$HOST $DEP_OPTS
+  fi
+
 fi
