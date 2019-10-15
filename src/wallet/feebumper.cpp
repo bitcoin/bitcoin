7f937aa23da1a330d60faba299d62ea537f73fad
MarcoFalke <falke.marco@gmail.com>
2019-10-15 14:31:15 -0400

    Merge #17098: refactor: Feebumper EstimateFeeRate follow-up
    66b29848c71c4b3b4dc36ca6d94de829bd533797 change wallet pointers to references in feebumper (Adam Jonas)
    9be6666a4e648782b49a6fdc458372ea521b444d typo and unneccessary parentheses (Adam Jonas)
    
    Pull request description:
    
      Picking up some of the suggestions in the comments of #16727 including:
      https://github.com/bitcoin/bitcoin/pull/16727#discussion_r330547321
      https://github.com/bitcoin/bitcoin/pull/16727#discussion_r330549766
      https://github.com/bitcoin/bitcoin/pull/16727#discussion_r333209674
    
    ACKs for top commit:
      promag:
        Code review ACK 66b29848c71c4b3b4dc36ca6d94de829bd533797.
      MarcoFalke:
        ACK 66b29848c71c4b3b4dc36ca6d94de829bd533797 (looked at the diff on GitHub)
      fjahr:
        ACK 66b2984 reviewed code
    
    Tree-SHA512: d118f7689970fe39d9f5318dc818f13283cce9194370b3ce4758f298172e4681ae119ddc809f5c0b7602677137ac0d38147b915422ff616531a76a570b766fa2
    
    Former-commit-id: 3862200af8a555f4e32ad15e1cc32b7bcf03fd84

--- src/wallet/feebumper.cpp
+++ src/wallet/feebumper.cpp
@@ -123,7 +123,7 @@ static CFeeRate EstimateFeeRate(const CWallet& wallet, const CWalletTx& wtx, CCo
     // the minimum of that and the wallet's conservative
     // WALLET_INCREMENTAL_RELAY_FEE value to future proof against changes to
     // network wide policy for incremental relay fee that our node may not be
-    // aware of. This ensures we're over the over the required relay fee rate
+    // aware of. This ensures we're over the required relay fee rate
     // (BIP 125 rule 4).  The replacement tx will be at least as large as the
     // original tx, so the total fee will be greater (BIP 125 rule 3)
     CFeeRate node_incremental_relay_fee = wallet.chain().relayIncrementalFee();
