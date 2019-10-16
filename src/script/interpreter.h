42c760cc8ded061e22370f5a947d164d64e5dae6
Jeremy Rubin <j@rubin.io>
2019-10-16 14:36:34 -0700

    Add StandardTemplateHash definition

--- src/script/interpreter.h
+++ src/script/interpreter.h
@@ -185,6 +185,13 @@ struct PrecomputedTransactionData
     explicit PrecomputedTransactionData(const T& tx);
 };
 
+/* Standard Template Hash Declarations */
+template<typename TxType>
+uint256 GetDefaultCheckTemplateVerifyHash(const TxType& tx, uint32_t input_index);
+template<typename TxType>
+uint256 GetDefaultCheckTemplateVerifyHash(const TxType& tx, const uint256& outputs_hash, const uint256& sequences_hash,
+                                const uint32_t input_index);
+
 enum class SigVersion
 {
     BASE = 0,        //!< Bare scripts and BIP16 P2SH-wrapped redeemscripts
