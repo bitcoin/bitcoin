c3e611575fe726fb33648f2f7ae9af8c6eb08838
Jeremy Rubin <j@rubin.io>
2019-10-16 14:48:23 -0700

    Add OP_CHECKTEMPLATEVERIFY Opcode as OP_NOP4
    [TESTS] modify script_tests.json to enable OP_NOP4 as OP_CHECKTEMPLATEVERIFY

--- src/policy/policy.h
+++ src/policy/policy.h
@@ -115,6 +115,7 @@ static constexpr unsigned int STANDARD_SCRIPT_VERIFY_FLAGS{MANDATORY_SCRIPT_VERI
                                                              SCRIPT_VERIFY_DISCOURAGE_OP_SUCCESS |
                                                              SCRIPT_VERIFY_DISCOURAGE_UPGRADABLE_PUBKEYTYPE};
 
+
 /** For convenience, standard but not mandatory verify flags. */
 static constexpr unsigned int STANDARD_NOT_MANDATORY_VERIFY_FLAGS{STANDARD_SCRIPT_VERIFY_FLAGS & ~MANDATORY_SCRIPT_VERIFY_FLAGS};
 
--- src/validation.cpp
+++ src/validation.cpp
@@ -2143,7 +2143,7 @@ static unsigned int GetBlockScriptFlags(const CBlockIndex& block_index, const Ch
 
     // Enforce COVTOOLS (BIPxxx)
     if (DeploymentActiveAt(block_index, chainman, Consensus::DEPLOYMENT_COVTOOLS)) {
-        // Empty for now.
+        flags |= SCRIPT_VERIFY_CHECKTEMPLATEVERIFY;
     }
 
     // Enforce BIP147 NULLDUMMY (activated simultaneously with segwit)
