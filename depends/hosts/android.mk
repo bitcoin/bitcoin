d6e4c9c1a55bf122ead786e37d49454b5a95550e
BlockMechanic <blockmecha@gmail.com>
2019-10-21 23:57:05 +0200

    Add android files

---
+++ depends/hosts/android.mk
@@ -0,0 +1,29 @@
+ANDROID_SDK=$(ANDROID_SDK)
+ANDROID_NDK=$(ANDROID_NDK)
+NDK_VERSION=4.9
+ANDROID_API_LEVEL=$(ANDROID_API_LEVEL)
+ANDROID_TARGET_ARCH=$(ANDROID_TARGET_ARCH)
+
+ifeq ($(ANDROID_TARGET_ARCH),armeabi-v7a)
+android_AR=$(ANDROID_TOOLCHAIN_BIN)/arm-linux-androideabi-ar
+android_CXX=$(ANDROID_TOOLCHAIN_BIN)/$(HOST)eabi$(ANDROID_API_LEVEL)-clang++
+android_CC=$(ANDROID_TOOLCHAIN_BIN)/$(HOST)eabi$(ANDROID_API_LEVEL)-clang
+android_RANLIB=$(ANDROID_TOOLCHAIN_BIN)/arm-linux-androideabi-ranlib
+android_NM=$(ANDROID_TOOLCHAIN_BIN)/arm-linux-androideabi-nm
+endif
+
+ifeq ($(ANDROID_TARGET_ARCH),x86)
+android_AR=$(ANDROID_TOOLCHAIN_BIN)/i686-linux-android-ar
+android_CXX=$(ANDROID_TOOLCHAIN_BIN)/i686-linux-android$(ANDROID_API_LEVEL)-clang++
+android_CC=$(ANDROID_TOOLCHAIN_BIN)/i686-linux-android$(ANDROID_API_LEVEL)-clang
+android_RANLIB=$(ANDROID_TOOLCHAIN_BIN)/i686-linux-android-ranlib
+android_NM=$(ANDROID_TOOLCHAIN_BIN)/i686-linux-android-nm
+endif
+
+ifeq ($(ANDROID_TARGET_ARCH),arm64-v8a)
+android_AR=$(ANDROID_TOOLCHAIN_BIN)/$(HOST)-ar
+android_CXX=$(ANDROID_TOOLCHAIN_BIN)/$(HOST)$(ANDROID_API_LEVEL)-clang++
+android_CC=$(ANDROID_TOOLCHAIN_BIN)/$(HOST)$(ANDROID_API_LEVEL)-clang
+android_RANLIB=$(ANDROID_TOOLCHAIN_BIN)/$(HOST)-ranlib
+android_NM=$(ANDROID_TOOLCHAIN_BIN)/$(HOST)-nm
+endif
