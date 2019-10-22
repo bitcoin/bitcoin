bcaf0f3733dbb63c8af56fdde5fabc6789f8ca2c
sadrasabouri <sabouri.sadra@gmail.com>
2019-10-22 20:03:33 +0330

    add : comments are added to scripts

--- ci/test/06_script_a.sh
+++ ci/test/06_script_a.sh
@@ -6,11 +6,13 @@
 
 export LC_ALL=C.UTF-8
 
+#cd command is misunderstanded by cd Travis built-in command in some macOS
 lcd='cd'
 if [[ "$OSTYPE" == "darwin"* ]]; then
   lcd='type cd'
 fi
 
+#Avoid repeating this block several places in code
 safe_cd()
 {
   set +o errexit
