ea946a1440583173d0d49bb3a18097c7d361b2bc
sadrasabouri <sabouri.sadra@gmail.com>
2019-10-23 01:23:01 +0330

    test : (type cd) and (which cd) command added for debug

--- ci/test/06_script_b.sh
+++ ci/test/06_script_b.sh
@@ -15,8 +15,10 @@ fi
 #Avoid repeating this block several places in code
 safe_cd()
 {
+  type cd
+  which cd
   set +o errexit
-  $lcd $1 || (echo $2; exit 1)
+  #$lcd $1 || (echo $2; exit 1)
   set -o errexit
 }
 
