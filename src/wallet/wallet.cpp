0fca6e63ea013514d7d6f936491b863639a72716
João Barbosa <joao.paulo.barbosa@gmail.com>
2019-10-24 09:45:20 +0100

    wallet: LearnRelatedScripts only if KeepDestination

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -3256,7 +3256,6 @@ bool ReserveDestination::GetReservedDestination(CTxDestination& dest, bool inter
         fInternal = keypool.fInternal;
     }
     assert(vchPubKey.IsValid());
-    m_spk_man->LearnRelatedScripts(vchPubKey, type);
     address = GetDestinationForKey(vchPubKey, type);
     dest = address;
     return true;
@@ -3264,8 +3263,10 @@ bool ReserveDestination::GetReservedDestination(CTxDestination& dest, bool inter
 
 void ReserveDestination::KeepDestination()
 {
-    if (nIndex != -1)
+    if (nIndex != -1) {
         m_spk_man->KeepDestination(nIndex);
+        m_spk_man->LearnRelatedScripts(vchPubKey, type);
+    }
     nIndex = -1;
     vchPubKey = CPubKey();
     address = CNoDestination();
