f61aae80a6e36d6f368ce6f7859ae13cdd3dda94
Wladimir J. van der Laan <laanwj@protonmail.com>
2019-10-26 13:00:01 +0200

    Merge #17120: gui: Fix start timer from non QThread
    a8f5026d6d992fd8d72908c848c5028f0f9a8cd1 gui: Fix start timer from non QThread (João Barbosa)
    
    Pull request description:
    
      Fixes #16296.
    
    ACKs for top commit:
      laanwj:
        code review ACK a8f5026d6d992fd8d72908c848c5028f0f9a8cd1
    
    Tree-SHA512: d7b05ac88e188de16cbbe80cb2f773b7976ee07ee876ac94a93f9351856c4f3a9d66a531d3f3748d2dccff8c8d77d9d8227433069ed5909c32be2efeaa32f655
    
    Former-commit-id: 780a75e8017f544c0cb13588f9404b170c901496

--- src/qt/walletmodel.cpp
+++ src/qt/walletmodel.cpp
@@ -44,17 +44,20 @@ WalletModel::WalletModel(std::unique_ptr<interfaces::Wallet> wallet, interfaces:
     transactionTableModel = new TransactionTableModel(platformStyle, this);
     recentRequestsTableModel = new RecentRequestsTableModel(this);
 
-    // This timer will be fired repeatedly to update the balance
-    pollTimer = new QTimer(this);
-    connect(pollTimer, &QTimer::timeout, this, &WalletModel::pollBalanceChanged);
-    pollTimer->start(MODEL_UPDATE_DELAY);
-
     subscribeToCoreSignals();
 }
 
 WalletModel::~WalletModel()
 {
     unsubscribeFromCoreSignals();
+}
+
+void WalletModel::startPollBalance()
+{
+    // This timer will be fired repeatedly to update the balance
+    QTimer* timer = new QTimer(this);
+    connect(timer, &QTimer::timeout, this, &WalletModel::pollBalanceChanged);
+    timer->start(MODEL_UPDATE_DELAY);
 }
 
 void WalletModel::updateStatus()
