448e0463b5c52a01c062a2c80112027065df3251
sidhujag <jsidhu@blockchainfoundry.co>
2019-10-25 17:15:52 -0700

    Update feature_loadblock.py
    Former-commit-id: c1344f0ed3bdecf03664b1e9d6eb0e3b5e0bde9a

--- test/functional/feature_loadblock.py
+++ test/functional/feature_loadblock.py
@@ -17,12 +17,12 @@ import tempfile
 import urllib
 
 from test_framework.test_framework import (
-    BitcoinTestFramework,
+    SyscoinTestFramework,
 )
 from test_framework.util import assert_equal, wait_until
 
 
-class LoadblockTest(BitcoinTestFramework):
+class LoadblockTest(SyscoinTestFramework):
     def set_test_params(self):
         self.setup_clean_chain = True
         self.num_nodes = 2
