39cc3f331e65644d237b447042bc25825f1e9a7d
Wladimir J. van der Laan <laanwj@protonmail.com>
2019-10-28 11:56:43 +0100

    Merge #17192: util: Add CHECK_NONFATAL and use it in src/rpc
    faeb6665362e35f573ad715ade0ef2db62d71839 util: Add CHECK_NONFATAL and use it in src/rpc (MarcoFalke)
    
    Pull request description:
    
      Fixes #17181
    
      Currently, we use `assert` in RPC code to document logic and code assumptions. However, it seems a bit extreme to abort all of Bitcoin Core on an assert failure in one of the RPC threads. I suggest to replace all `assert`s with a macro `CHECK_NONFATAL(condition)` that throws a runtime error when the condition evaluates to `false`. That runtime error will then be returned to the rpc caller and will include instructions to report the error to our issue tracker.
    
    ACKs for top commit:
      practicalswift:
        ACK faeb6665362e35f573ad715ade0ef2db62d71839
      laanwj:
        ACK faeb6665362e35f573ad715ade0ef2db62d71839
      ryanofsky:
        Code review ACK faeb6665362e35f573ad715ade0ef2db62d71839
    
    Tree-SHA512: 9b748715a5e0767ac11f1324a95a3a6ec672a0e0658013492219223bda83ce4b1b447fd8183bbb235f7df5ef7dddda7666ad569544b4d61cc65f232ca7a800ec
    
    Former-commit-id: a5c20ba4d9796ae82e9540bfd2f5425d1eb668d7

--- src/rpc/misc.cpp
+++ src/rpc/misc.cpp
@@ -3,15 +3,16 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <key_io.h>
 #include <httpserver.h>
+#include <key_io.h>
 #include <outputtype.h>
 #include <rpc/blockchain.h>
 #include <rpc/server.h>
 #include <rpc/util.h>
 #include <script/descriptor.h>
-#include <util/system.h>
+#include <util/check.h>
 #include <util/strencodings.h>
+#include <util/system.h>
 #include <util/validation.h>
 
 #include <stdint.h>
@@ -680,6 +681,7 @@ static UniValue echo(const JSONRPCRequest& request)
         throw std::runtime_error(
             RPCHelpMan{"echo|echojson ...",
                 "\nSimply echo back the input arguments. This command is for testing.\n"
+                "\nIt will return an internal bug report when exactly 100 arguments are passed.\n"
                 "\nThe difference between echo and echojson is that echojson has argument conversion enabled in the client-side table in "
                 "syscoin-cli and the GUI. There is no server-side difference.",
                 {},
@@ -687,6 +689,8 @@ static UniValue echo(const JSONRPCRequest& request)
                 RPCExamples{""},
             }.ToString()
         );
+
+    CHECK_NONFATAL(request.params.size() != 100);
 
     return request.params;
 }
