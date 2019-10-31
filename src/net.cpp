b6d2183858975abc961207c125c15791e531edcc
User <naumenko.gs@gmail.com>
2019-10-31 13:42:02 -0400

    Minor refactoring to remove implied m_addr_relay_peer.
    Co-authored-by: MarcoFalke <falke.marco@gmail.com>

--- src/net.cpp
+++ src/net.cpp
@@ -2655,7 +2655,7 @@ CNode::CNode(NodeId idIn, ServiceFlags nLocalServicesIn, int nMyStartingHeightIn
     // Don't relay addr messages to peers that we connect to as block-relay-only
     // peers (to prevent adversaries from inferring these links from addr
     // traffic).
-    m_addr_relay_peer(!block_relay_only),
+    m_addr_known{block_relay_only ? nullptr : MakeUnique<CRollingBloomFilter>(5000, 0.001)},
     id(idIn),
     nLocalHostNonce(nLocalHostNonceIn),
     nLocalServices(nLocalServicesIn),
@@ -2666,10 +2666,6 @@ CNode::CNode(NodeId idIn, ServiceFlags nLocalServicesIn, int nMyStartingHeightIn
     hashContinue = uint256();
     if (!block_relay_only) {
         m_tx_relay = MakeUnique<TxRelay>();
-    }
-
-    if (m_addr_relay_peer) {
-        m_addr_known = MakeUnique<CRollingBloomFilter>(5000, 0.001);
     }
 
     for (const std::string &msg : getAllNetMessageTypes())
