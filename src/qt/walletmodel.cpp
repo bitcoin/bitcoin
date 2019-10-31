bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

GUI/Wallet: Strip ".dat" from end of wallet display names

src/qt/walletmodel.cpp
@@ -571,6 +571,9 @@ QString WalletModel::getWalletName() const
 QString WalletModel::getWalletDisplayName() const
 {
     QString walletName = getWalletName();
+    if (walletName.endsWith(".dat")) {
+        walletName.truncate(walletName.size() - 4);
+    }
     if (walletName.isEmpty()) {
         walletName = "[" + tr("default wallet") + "]";
     }

bitcoin/bitcoin

src/qt/walletmodel.cpp
@@ -598,6 +598,9 @@ QString WalletModel::getWalletName() const
 
 QString WalletModel::getDisplayName(QString name)
 {
+    if (name.endsWith(".dat")) {
+        name.truncate(name.size() - 4);
+    }
     return name.isEmpty() ? "["+tr("default wallet")+"]" : name;
 }
 
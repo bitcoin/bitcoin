4cd991977d906c8f86f8184cb8bfd8044de79b21
Luke Dashjr <luke-jr+git@utopios.org>
2018-12-13 15:43:08 +0000

    GUI/Wallet: Strip ".dat" from end of wallet display names

--- src/qt/walletmodel.cpp
+++ src/qt/walletmodel.cpp
@@ -598,6 +598,9 @@ QString WalletModel::getWalletName() const
 
 QString WalletModel::getDisplayName(QString name)
 {
+    if (name.endsWith(".dat")) {
+        name.truncate(name.size() - 4);
+    }
     return name.isEmpty() ? "["+tr("default wallet")+"]" : name;
 }
 
