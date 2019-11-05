8e29d1d38e735aa82779527ed588a396ff3c8d72
Wladimir J. van der Laan <laanwj@protonmail.com>
2019-11-06 00:04:22 +0100

    Merge #17382: rpc: Remove unused boost::this_thread::interruption_point
    fa5facd3e72b6d61374b0b93b722b55e2b090020 rpc: Remove unused boost::this_thread::interruption_point (MarcoFalke)
    
    Pull request description:
    
      There are predefined interruption points for `boost::thread`: https://www.boost.org/doc/libs/1_71_0/doc/html/thread/thread_management.html#interruption_points
    
      However, the rpc threads are `std::thread`, which does not have an `std::thread::interrupt` member function to request interruption: https://dev.visucore.com/bitcoin/doxygen/httpserver_8cpp.html#ae1a63374e18b9abd348eb74e4243ea34
    
      Thus, the interruption points can be removed.
    
    ACKs for top commit:
      laanwj:
        ACK fa5facd3e72b6d61374b0b93b722b55e2b090020, this does nothing.
      practicalswift:
        ACK fa5facd3e72b6d61374b0b93b722b55e2b090020
      jamesob:
        ACK https://github.com/bitcoin/bitcoin/pull/17382/commits/fa5facd3e72b6d61374b0b93b722b55e2b090020
    
    Tree-SHA512: 4e29a44df1f2702cbd1ffdffa559440a8bb800baab64b4116e2c3d27cd64d8d1e8aafe1dc21b1a4e3988470d03be19cae294bd5669f7abf6d487685dc8fd8d7e
    
    Former-commit-id: 06ab951d15b5e36d3d9c61fe344f1de61ab1f4c8

--- test/functional/rpc_dumptxoutset.py
+++ test/functional/rpc_dumptxoutset.py
@@ -4,14 +4,14 @@
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 """Test the generation of UTXO snapshots using `dumptxoutset`.
 """
-from test_framework.test_framework import BitcoinTestFramework
+from test_framework.test_framework import SyscoinTestFramework
 from test_framework.util import assert_equal, assert_raises_rpc_error
 
 import hashlib
 from pathlib import Path
 
 
-class DumptxoutsetTest(BitcoinTestFramework):
+class DumptxoutsetTest(SyscoinTestFramework):
     def set_test_params(self):
         self.setup_clean_chain = True
         self.num_nodes = 1
