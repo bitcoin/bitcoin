d1c02775aa74a0610809ac54bb241ddad61d2d8c
Pieter Wuille <pieter.wuille@gmail.com>
2019-11-05 10:54:20 -0800

    Report amount of data gathered from environment

--- src/random.cpp
+++ src/random.cpp
@@ -525,7 +525,9 @@ static void SeedPeriodic(CSHA512& hasher, RNGState& rng)
     SeedTimestamp(hasher);
 
     // Dynamic environment data (performance monitoring, ...)
+    auto old_size = hasher.Size();
     RandAddDynamicEnv(hasher);
+    LogPrintf("Feeding %i bytes of dynamic environment data into RNG\n", hasher.Size() - old_size);
 
     // Strengthen for 10 ms
     SeedStrengthen(hasher, rng, 10000);
@@ -540,10 +542,12 @@ static void SeedStartup(CSHA512& hasher, RNGState& rng) noexcept
     SeedSlow(hasher);
 
     // Dynamic environment data (performance monitoring, ...)
+    auto old_size = hasher.Size();
     RandAddDynamicEnv(hasher);
 
     // Static environment data
     RandAddStaticEnv(hasher);
+    LogPrintf("Feeding %i bytes of environment data into RNG\n", hasher.Size() - old_size);
 
     // Strengthen for 100 ms
     SeedStrengthen(hasher, rng, 100000);
