76a8e19492d2ec34a658c4401c22d6d046997389
konez2k <konez@protonmail.com>
2019-11-07 20:21:28 +0100

    update protocol version

--- src/version.h
+++ src/version.h
@@ -9,7 +9,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70015;
+static const int PROTOCOL_VERSION = 70016;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -40,9 +40,9 @@ static const int FEEFILTER_VERSION = 70013;
 static const int SHORT_IDS_BLOCKS_VERSION = 70014;
 
 //! not banning for invalid compact blocks starts with this version
-static const int INVALID_CB_NO_BAN_VERSION = 70015;
+static const int INVALID_CB_NO_BAN_VERSION = 70016;
 
 //! introduction of LLMQs
-static const int LLMQS_PROTO_VERSION = 70015;
+static const int LLMQS_PROTO_VERSION = 70016;
 
 #endif // BITGREEN_VERSION_H
