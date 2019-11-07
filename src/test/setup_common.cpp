c599a5b19298b87c5c3c7eb8031867f0f212b166
sidhujag <jsidhu@blockchainfoundry.co>
2019-11-07 11:13:01 +0100

    fix tests
    Former-commit-id: 238d5d2e8aec5b45b7fa2a6a21dcead2b5bdf75c

--- src/test/setup_common.cpp
+++ src/test/setup_common.cpp
@@ -124,14 +124,14 @@ TestingSetup::~TestingSetup()
     pblocktree.reset();
 }
 
-TestChain100Setup::TestChain100Setup()
+TestChain100Setup::TestChain100Setup() : TestingSetup(CBaseChainParams::REGTEST)
 {
     // CreateAndProcessBlock() does not support building SegWit blocks, so don't activate in these tests.
     // TODO: fix the code to support SegWit blocks.
     gArgs.ForceSetArg("-segwitheight", "432");
-   // TurnOffSegwitForUnitTests();
+    TurnOffSegwitForUnitTests();
     // Need to recreate chainparams
-    SelectParams(CBaseChainParams::REGTEST);
+    //SelectParams(CBaseChainParams::REGTEST);
 
     // Generate a 100-block chain:
     coinbaseKey.MakeNewKey(true);
