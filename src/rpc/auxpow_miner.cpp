f44d671f7e8f53adec8b55655bace7ae6a2bff6b
sidhujag <jsidhu@blockchainfoundry.co>
2019-11-09 15:35:19 +0100

    wip travis ci
    Former-commit-id: 1e31a94ab966c8b5899ece1721b7b17b45609e25

--- src/rpc/auxpow_miner.cpp
+++ src/rpc/auxpow_miner.cpp
@@ -93,7 +93,7 @@ AuxpowMiner::getCurrentBlock (const CScript& scriptPubKey, uint256& target)
      pindexPrev == ::ChainActive().Tip().  But for that to happen, we must
      already have created a pblockCur in a previous call, as pindexPrev is
      initialised only when pblockCur is.  */
-  assert (pblockCur);
+  CHECK_NONFATAL (pblockCur);
 
   arith_uint256 arithTarget;
   bool fNegative, fOverflow;
@@ -160,7 +160,7 @@ AuxpowMiner::submitAuxBlock (const std::string& hashHex,
   std::unique_ptr<CAuxPow> pow(new CAuxPow ());
   ss >> *pow;
   shared_block->SetAuxpow (std::move (pow));
-  assert (shared_block->GetHash ().GetHex () == hashHex);
+  CHECK_NONFATAL (shared_block->GetHash ().GetHex () == hashHex);
 
   return ProcessNewBlock (Params (), shared_block, true, nullptr);
 }
