0a5587c2f7216298b8cafbecbb42a7287458ccdd
MarcoFalke <falke.marco@gmail.com>
2019-11-12 14:53:31 -0500

    Merge #17435: test: check custom ancestor limit in mempool_packages.py
    49997813a4db388b2810e5e27ef771e8aa6a1f03 test: check custom ancestor limit in mempool_packages.py (Sebastian Falbesoner)
    
    Pull request description:
    
      The functional test `mempool_packages.py` starts one node with default ancestor/descendant limit settings and one with a custom, reduced ancestor limit (currently `-limitancestorcount=5`). The effect of the latter had not been tested yet though. This is approached in this PR by checking on the expected mempool contents of node1 after the node0 ancestor tests are done, via the following three conditions:
      - the # of txs in the node1 mempool is equal to the the limit
      - all txs in node1 mempool are a subset of txs in node0 mempool
      - the node1 mempool txs match the start of the constructed tx-chain
    
      Note that this still doesn't *fully* check the expected mempool of node1 (e.g. that it isn't influenced by `prioritisetransaction` RPC on node0), hence I add another TODO. In the future it would make sense to also set a custom descendant limit when the second TODO about checking node1's mempool is approached: https://github.com/bitcoin/bitcoin/blob/89e93135aedf984f7a98771f047e2beb6cdbdb8e/test/functional/mempool_packages.py#L228
    
    ACKs for top commit:
      MarcoFalke:
        ACK 49997813a4db388b2810e5e27ef771e8aa6a1f03 👲
    
    Tree-SHA512: d3a1d19fb49731238ad08ee7c02e2fa81a227e3b4ef3340d68598de42ddb62be9161134f6b8e08fa76b8c9faa02fecfa01111159642e20e9f358292a757b7608

--- test/functional/mempool_packages.py
+++ test/functional/mempool_packages.py
@@ -10,13 +10,19 @@ from test_framework.messages import COIN
 from test_framework.test_framework import BitcoinTestFramework
 from test_framework.util import assert_equal, assert_raises_rpc_error, satoshi_round
 
+# default limits
 MAX_ANCESTORS = 25
 MAX_DESCENDANTS = 25
+# custom limits for node1
+MAX_ANCESTORS_CUSTOM = 5
 
 class MempoolPackagesTest(BitcoinTestFramework):
     def set_test_params(self):
         self.num_nodes = 2
-        self.extra_args = [["-maxorphantxsize=1000"], ["-maxorphantxsize=1000", "-limitancestorcount=5"]]
+        self.extra_args = [
+            ["-maxorphantxsize=1000"],
+            ["-maxorphantxsize=1000", "-limitancestorcount={}".format(MAX_ANCESTORS_CUSTOM)],
+        ]
 
     def skip_test_if_missing_module(self):
         self.skip_if_no_wallet()
@@ -184,7 +190,14 @@ class MempoolPackagesTest(BitcoinTestFramework):
             assert_equal(mempool[x]['descendantfees'], descendant_fees * COIN + 2000)
             assert_equal(mempool[x]['fees']['descendant'], descendant_fees+satoshi_round(0.00002))
 
-        # TODO: check that node1's mempool is as expected
+        # Check that node1's mempool is as expected (-> custom ancestor limit)
+        mempool0 = self.nodes[0].getrawmempool(False)
+        mempool1 = self.nodes[1].getrawmempool(False)
+        assert_equal(len(mempool1), MAX_ANCESTORS_CUSTOM)
+        assert set(mempool1).issubset(set(mempool0))
+        for tx in chain[:MAX_ANCESTORS_CUSTOM]:
+            assert tx in mempool1
+        # TODO: more detailed check of node1's mempool (fees etc.)
 
         # TODO: test ancestor size limits
 
