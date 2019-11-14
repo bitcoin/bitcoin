1e9816d17cb8eb32ff478df1fccc6e779dff3af6
Wladimir J. van der Laan <laanwj@protonmail.com>
2019-11-14 20:35:07 +0100

    build: bump version to 0.19.0.1
    Tree-SHA512: d448348761b7cd371b9ee4736577f6af9a141c73141ccd88a91a94593f6d21ab32754771673e8f2eb31044a624a1e1e8e4e83919a4d0100cb08eec71dcff7158

--- build_msvc/bitcoin_config.h
+++ build_msvc/bitcoin_config.h
@@ -5,7 +5,7 @@
 /* #undef AC_APPLE_UNIVERSAL_BUILD */
 
 /* Version Build */
-#define CLIENT_VERSION_BUILD 0
+#define CLIENT_VERSION_BUILD 1
 
 /* Version is release */
 #define CLIENT_VERSION_IS_RELEASE true
