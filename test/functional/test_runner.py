9d89e9e0d6acc705308fbc85e24a51b417e017f1
fanquake <fanquake@gmail.com>
2019-11-15 11:38:27 -0500

    test: fix bitcoind already running warnings on macOS
    On macOS, pidof installed via brew returns b'' rather than None.
    Account for this, to remove spurious warnings from the test_runner.
    
    
    Former-commit-id: c1556a43136ca32c98803539cac9cd1570199272

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -372,9 +372,10 @@ def main():
 def run_tests(*, test_list, src_dir, build_dir, tmpdir, jobs=1, enable_coverage=False, args=None, combined_logs_len=0, failfast=False, runs_ci, use_term_control):
     args = args or []
 
-    # Warn if syscoind is already running (unix only)
+    # Warn if syscoind is already running
+    # pidof might fail or return an empty string if syscoind is not running
     try:
-        if subprocess.check_output(["pidof", "syscoind"]) is not None:
+        if subprocess.check_output(["pidof", "syscoind"]) not in [b'']:
             print("%sWARNING!%s There is already a syscoind process running on this system. Tests may fail unexpectedly due to resource contention!" % (BOLD[1], BOLD[0]))
     except (OSError, subprocess.SubprocessError):
         pass
