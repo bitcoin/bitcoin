aab475ad5799624166ac7dcdeebffad9e6a86753
João Barbosa <joao.paulo.barbosa@gmail.com>
2019-11-18 11:36:00 +0000

    Fix unsigned integer overflow in LoadMempool

--- src/validation.cpp
+++ src/validation.cpp
@@ -4968,7 +4968,8 @@ bool LoadMempool(CTxMemPool& pool)
         }
         uint64_t num;
         file >> num;
-        while (num--) {
+        while (num > 0) {
+            --num;
             CTransactionRef tx;
             int64_t nTime;
             int64_t nFeeDelta;
