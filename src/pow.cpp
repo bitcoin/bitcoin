7ad01ac6cbe75fec32aaaf6e75e0fb1eec970bbf
sidhujag <jsidhu@blockchainfoundry.co>
2019-11-18 10:29:38 -0800

    pow fix for testnet to 20min
    Former-commit-id: 3a70ad443ec15bfb5816a8e2d57c89e4201f26d1

--- src/pow.cpp
+++ src/pow.cpp
@@ -20,10 +20,10 @@ unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHead
     {
         if (params.fPowAllowMinDifficultyBlocks)
         {
-            // Special difficulty rule for testnet:
+            // SYSCOIN Special difficulty rule for testnet:
             // If the new block's timestamp is more than 2* 10 minutes
             // then allow mining of a min-difficulty block.
-            if (pblock->GetBlockTime() > pindexLast->GetBlockTime() + params.nPowTargetSpacing*2)
+            if (pblock->GetBlockTime() > pindexLast->GetBlockTime() + params.nPowTargetSpacing*20)
                 return nProofOfWorkLimit;
             else
             {
