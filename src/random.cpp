ca2e47437277ef6851a739f247b44e73a53f21a1
Vasil Dimov <vd@FreeBSD.org>
2019-11-19 15:28:07 +0100

    Fix a compiler warning: unused GetDevURandom()
    ```
    random.cpp:255:13: error: unused function 'GetDevURandom' [-Werror,-Wunused-function]
    
    ```
    
    Clang 9.0.0, FreeBSD 12.1
    
    Silence by planting a dummy reference to the `GetDevURandom` symbol
    in the places where we don't call the function.

--- src/random.cpp
+++ src/random.cpp
@@ -314,12 +314,16 @@ void GetOSRand(unsigned char *ent32)
     if (getentropy(ent32, NUM_OS_RANDOM_BYTES) != 0) {
         RandFailure();
     }
+    // Silence a compiler warning about unused function.
+    (void)GetDevURandom;
 #elif defined(HAVE_GETENTROPY_RAND) && defined(MAC_OSX)
     /* getentropy() is available on macOS 10.12 and later.
      */
     if (getentropy(ent32, NUM_OS_RANDOM_BYTES) != 0) {
         RandFailure();
     }
+    // Silence a compiler warning about unused function.
+    (void)GetDevURandom;
 #elif defined(HAVE_SYSCTL_ARND)
     /* FreeBSD, NetBSD and similar. It is possible for the call to return less
      * bytes than requested, so need to read in a loop.
@@ -333,6 +337,8 @@ void GetOSRand(unsigned char *ent32)
         }
         have += len;
     } while (have < NUM_OS_RANDOM_BYTES);
+    // Silence a compiler warning about unused function.
+    (void)GetDevURandom;
 #else
     /* Fall back to /dev/urandom if there is no specific method implemented to
      * get system entropy for this OS.
