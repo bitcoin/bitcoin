b60e452aef735a5b972a1e9ca12b34694bfa53f1
MarcoFalke <falke.marco@gmail.com>
2019-11-19 22:34:38 -0500

    zmq: Send blocks faster by serializing PureBlock

--- src/zmq/zmqpublishnotifier.cpp
+++ src/zmq/zmqpublishnotifier.cpp
@@ -235,9 +235,8 @@ bool CZMQPublishRawBlockNotifier::NotifyBlock(const CBlockIndex *pindex)
     CDataStream ss(SER_NETWORK, PROTOCOL_VERSION | RPCSerializationFlags());
     {
         LOCK(cs_main);
-        CBlock block;
-        if(!ReadBlockFromDisk(block, pindex, consensusParams))
-        {
+        PureBlock block;
+        if (!ReadBlockFromDisk(block, pindex, consensusParams)) {
             zmqError("Can't read block from disk");
             return false;
         }
