f6e42256fe8900ed90f9644906b77f97e1600952
Wladimir J. van der Laan <laanwj@protonmail.com>
2019-11-23 10:28:02 +0100

    build: Allow export of environ symbols
    This export was introduced in #17270 which added
    ```
    //! Necessary on some platforms
    extern char** environ;
    ```

--- contrib/devtools/symbol-check.py
+++ contrib/devtools/symbol-check.py
@@ -47,7 +47,8 @@ MAX_VERSIONS = {
 
 # Ignore symbols that are exported as part of every executable
 IGNORE_EXPORTS = {
-'_edata', '_end', '__end__', '_init', '__bss_start', '__bss_start__', '_bss_end__', '__bss_end__', '_fini', '_IO_stdin_used', 'stdin', 'stdout', 'stderr'
+'_edata', '_end', '__end__', '_init', '__bss_start', '__bss_start__', '_bss_end__', '__bss_end__', '_fini', '_IO_stdin_used', 'stdin', 'stdout', 'stderr',
+'environ', '_environ', '__environ',
 }
 READELF_CMD = os.getenv('READELF', '/usr/bin/readelf')
 CPPFILT_CMD = os.getenv('CPPFILT', '/usr/bin/c++filt')
