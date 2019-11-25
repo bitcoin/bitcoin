f7536cb94a49dbdd0216a872d22fe2e9b36e0d17
sidhujag <jsidhu@blockchainfoundry.co>
2019-11-25 14:18:31 -0800

    update script
    Former-commit-id: 488fa93b1ca8de3aaa9d13cc20748f3f8e9f29a7

--- ci/test/00_setup_env_s390x.sh
+++ ci/test/00_setup_env_s390x.sh
@@ -10,9 +10,9 @@ export HOST=s390x-unknown-linux-gnu
 export DOCKER_NAME_TAG=s390x/ubuntu:18.04
 export PACKAGES="clang llvm python3-zmq qtbase5-dev qttools5-dev-tools libssl1.0-dev libevent-dev bsdmainutils libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-test-dev libboost-thread-dev libdb5.3++-dev libminiupnpc-dev libzmq3-dev libqrencode-dev"
 export NO_DEPENDS=1
-export RUN_UNIT_TESTS=true
+export RUN_UNIT_TESTS=false
 export RUN_FUNCTIONAL_TESTS=false
 export GOAL="install"
-export BITCOIN_CONFIG="--enable-reduce-exports --with-incompatible-bdb"
+export SYSCOIN_CONFIG="--enable-reduce-exports --with-incompatible-bdb"
 
 lscpu
