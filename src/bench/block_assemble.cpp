78e283e656bf1643944ffdb76185f3468eb25895
Martin Zumsande <mzumsande@gmail.com>
2019-11-25 13:57:47 +0100

    [test] move wallet helper functions into test library

--- src/bench/block_assemble.cpp
+++ src/bench/block_assemble.cpp
@@ -5,8 +5,8 @@
 #include <bench/bench.h>
 #include <consensus/validation.h>
 #include <crypto/sha256.h>
-#include <test/util.h>
 #include <test/util/mining.h>
+#include <test/util/wallet.h>
 #include <txmempool.h>
 #include <validation.h>
 
--- src/test/util/wallet.cpp
+++ src/test/util/wallet.cpp
@@ -2,7 +2,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <test/util.h>
+#include <test/util/wallet.h>
 
 #include <key_io.h>
 #include <outputtype.h>
