65833a74076cddf986037c6eb3b29a9b9dbe31c5
Andrew Chow <achow101-github@achow101.com>
2019-11-26 11:52:51 -0500

    Add OutputType and CPubKey parameters to KeepDestination
    These need to be added so that LearnRelatedScripts can be called
    from within KeepDestination later.

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -3317,7 +3317,7 @@ bool ReserveDestination::GetReservedDestination(CTxDestination& dest, bool inter
 void ReserveDestination::KeepDestination()
 {
     if (nIndex != -1) {
-        m_spk_man->KeepDestination(nIndex);
+        m_spk_man->KeepDestination(nIndex, type, vchPubKey);
         m_spk_man->LearnRelatedScripts(vchPubKey, type);
     }
     nIndex = -1;
