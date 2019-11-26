1ea20138695b38d99c7c9a4a4054b9e7672e964a
sidhujag <jsidhu@blockchainfoundry.co>
2019-11-25 20:45:23 -0800

    skip qt packages
    Former-commit-id: db4025a29b35e05b8bf1095e5388b1de205afb4e

--- ci/test/00_setup_env_s390x.sh
+++ ci/test/00_setup_env_s390x.sh
@@ -8,7 +8,7 @@ export LC_ALL=C.UTF-8
 
 export HOST=s390x-unknown-linux-gnu
 export DOCKER_NAME_TAG=s390x/ubuntu:18.04
-export PACKAGES="clang llvm python3-zmq qtbase5-dev qttools5-dev-tools libssl1.0-dev libevent-dev bsdmainutils libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-test-dev libboost-thread-dev libdb5.3++-dev libminiupnpc-dev libzmq3-dev libqrencode-dev"
+export PACKAGES="clang llvm libssl1.0-dev libevent-dev bsdmainutils libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-test-dev libboost-thread-dev libdb5.3++-dev"
 export NO_DEPENDS=1
 export RUN_UNIT_TESTS=false
 export RUN_FUNCTIONAL_TESTS=false
