1b34aee4fded8a39375e0263017e652039fbbc6f
sidhujag <jsidhu@blockchainfoundry.co>
2019-11-27 20:53:37 -0800

    fix assetallocationmint + fix getAuxFee
    these functions were not precision aware, so make them aware, assetallocationmint was actually critical as it wasn't allowing precisions other than the default from being minted. This fix was to use AssetAmountFromValue instead of AmountFromValue in these functions, which takes a precision and formats it to a target precision value.
    
    
    Former-commit-id: 89d1349add2f513fa4115ee40253c3c09c55752f

--- src/test/syscoin_asset_tests.cpp
+++ src/test/syscoin_asset_tests.cpp
@@ -21,7 +21,7 @@
 #include <services/rpc/assetrpc.h>
 using namespace std;
 extern UniValue read_json(const std::string& jsondata);
-extern CAmount getAuxFee(const std::string &publicData, const CAmount& nAmount, CWitnessAddress & address);
+extern CAmount getAuxFee(const std::string &publicData, const CAmount& nAmount, const uint8_t &nPrecision, CWitnessAddress & address);
 BOOST_FIXTURE_TEST_SUITE(syscoin_asset_tests, BasicSyscoinTestingSetup)
 
 BOOST_AUTO_TEST_CASE(generate_big_assetdata)
@@ -108,24 +108,24 @@ BOOST_AUTO_TEST_CASE(generate_auxfees)
     CWitnessAddress address;
     const std::string& pubDataStr = pubDataObj.write();
  
-    BOOST_CHECK_EQUAL(getAuxFee(pubDataStr, 5000000*COIN, address), 19456000000);
-    BOOST_CHECK_EQUAL(getAuxFee(pubDataStr, 25000000*COIN, address), 19456000000);
-    BOOST_CHECK_EQUAL(getAuxFee(pubDataStr, 10*COIN, address), 10000000);
-    BOOST_CHECK_EQUAL(getAuxFee(pubDataStr, 3000*COIN, address), 591000000);
-    BOOST_CHECK_EQUAL(getAuxFee(pubDataStr, 6*COIN, address), 6000000);
-    BOOST_CHECK_EQUAL(getAuxFee(pubDataStr, 250*COIN, address), 106000000);
-    BOOST_CHECK_EQUAL(getAuxFee(pubDataStr, 500*COIN, address), 156000000);
-    BOOST_CHECK_EQUAL(getAuxFee(pubDataStr, 1250*COIN, address), 306000000);
-    BOOST_CHECK_EQUAL(getAuxFee(pubDataStr, 2500*COIN, address), 556000000);
-    BOOST_CHECK_EQUAL(getAuxFee(pubDataStr, 7500*COIN, address), 906000000);
-    BOOST_CHECK_EQUAL(getAuxFee(pubDataStr, 20000*COIN, address), 1781000000);
-    BOOST_CHECK_EQUAL(getAuxFee(pubDataStr, 25000*COIN, address), 2131000000);
-    BOOST_CHECK_EQUAL(getAuxFee(pubDataStr, 60000*COIN, address), 2376000000);
-    BOOST_CHECK_EQUAL(getAuxFee(pubDataStr, 180000*COIN, address), 3216000000);
-    BOOST_CHECK_EQUAL(getAuxFee(pubDataStr, 700000*COIN, address), 6856000000);
-    BOOST_CHECK_EQUAL(getAuxFee(pubDataStr, 2500000*COIN, address), 19456000000);
-    BOOST_CHECK_EQUAL(getAuxFee(pubDataStr, 200.1005005*COIN, address), 86040200);
-    BOOST_CHECK_EQUAL(getAuxFee(pubDataStr, 2500001*COIN, address), 19456000000);
+    BOOST_CHECK_EQUAL(getAuxFee(pubDataStr, 5000000*COIN,8, address), 19456000000);
+    BOOST_CHECK_EQUAL(getAuxFee(pubDataStr, 25000000*COIN,8, address), 19456000000);
+    BOOST_CHECK_EQUAL(getAuxFee(pubDataStr, 10*COIN,8, address), 10000000);
+    BOOST_CHECK_EQUAL(getAuxFee(pubDataStr, 3000*COIN,8, address), 591000000);
+    BOOST_CHECK_EQUAL(getAuxFee(pubDataStr, 6*COIN,8, address), 6000000);
+    BOOST_CHECK_EQUAL(getAuxFee(pubDataStr, 250*COIN,8, address), 106000000);
+    BOOST_CHECK_EQUAL(getAuxFee(pubDataStr, 500*COIN,8, address), 156000000);
+    BOOST_CHECK_EQUAL(getAuxFee(pubDataStr, 1250*COIN,8, address), 306000000);
+    BOOST_CHECK_EQUAL(getAuxFee(pubDataStr, 2500*COIN,8, address), 556000000);
+    BOOST_CHECK_EQUAL(getAuxFee(pubDataStr, 7500*COIN,8, address), 906000000);
+    BOOST_CHECK_EQUAL(getAuxFee(pubDataStr, 20000*COIN,8, address), 1781000000);
+    BOOST_CHECK_EQUAL(getAuxFee(pubDataStr, 25000*COIN,8, address), 2131000000);
+    BOOST_CHECK_EQUAL(getAuxFee(pubDataStr, 60000*COIN,8, address), 2376000000);
+    BOOST_CHECK_EQUAL(getAuxFee(pubDataStr, 180000*COIN,8, address), 3216000000);
+    BOOST_CHECK_EQUAL(getAuxFee(pubDataStr, 700000*COIN,8, address), 6856000000);
+    BOOST_CHECK_EQUAL(getAuxFee(pubDataStr, 2500000*COIN,8, address), 19456000000);
+    BOOST_CHECK_EQUAL(getAuxFee(pubDataStr, 200.1005005*COIN,8, address), 86040200);
+    BOOST_CHECK_EQUAL(getAuxFee(pubDataStr, 2500001*COIN,8, address), 19456000000);
 }
 BOOST_AUTO_TEST_CASE(generate_asset_auxfees)
 {
