4c67742c6a6f084487d509c92187890c374f7c8c
Wladimir J. van der Laan <laanwj@protonmail.com>
2019-12-04 10:55:40 +0100

    Merge #17648: doc: rename wallet-tool references to bitcoin-wallet
    e7ad4a2f8c07a82d6424b473f0d51dbd8f897b10 doc: rename wallet-tool references to bitcoin-wallet (Wilson Ccasihue S)
    
    Pull request description:
    
      Fix. text reference to executable bitcoin-wallet instead of wallet-tool, there is not a wallet-tool at bin/ folder.
    
    ACKs for top commit:
      fanquake:
        ACK e7ad4a2f8c07a82d6424b473f0d51dbd8f897b10 - thanks for following up.
    
    Tree-SHA512: aed41b08947728a4ff3a97a62858ee7c86e2e5d57dcbbd0aab492dae3d8a548bb60541924e68cf3a0aa3d53d7db0012b489462b466919cd83f05b2aa88b7fff7
    
    Former-commit-id: c18ac2b2e431207b7ae2c08de1274d186605a2b5

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -837,7 +837,7 @@ bool CWallet::AddToWallet(const CWalletTx& wtxIn, bool fFlushOnClose)
 
 void CWallet::LoadToWallet(CWalletTx& wtxIn)
 {
-    // If wallet doesn't have a chain (e.g wallet-tool), lock can't be taken.
+    // If wallet doesn't have a chain (e.g syscoin-wallet), lock can't be taken.
     auto locked_chain = LockChain();
     if (locked_chain) {
         Optional<int> block_height = locked_chain->getBlockHeight(wtxIn.m_confirm.hashBlock);
@@ -3013,7 +3013,7 @@ DBErrors CWallet::LoadWallet(bool& fFirstRunRet)
 {
     // Even if we don't use this lock in this function, we want to preserve
     // lock order in LoadToWallet if query of chain state is needed to know
-    // tx status. If lock can't be taken (e.g wallet-tool), tx confirmation
+    // tx status. If lock can't be taken (e.g syscoin-wallet), tx confirmation
     // status may be not reliable.
     auto locked_chain = LockChain();
     LOCK(cs_wallet);
