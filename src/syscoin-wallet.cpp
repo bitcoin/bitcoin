060c64eeafd635e756890106eecc6252e4b608dd
Wladimir J. van der Laan <laanwj@protonmail.com>
2019-12-04 10:55:40 +0100

    Merge #17648: doc: rename wallet-tool references to bitcoin-wallet
    e7ad4a2f8c07a82d6424b473f0d51dbd8f897b10 doc: rename wallet-tool references to bitcoin-wallet (Wilson Ccasihue S)
    
    Pull request description:
    
      Fix. text reference to executable bitcoin-wallet instead of wallet-tool, there is not a wallet-tool at bin/ folder.
    
    ACKs for top commit:
      fanquake:
        ACK e7ad4a2f8c07a82d6424b473f0d51dbd8f897b10 - thanks for following up.
    
    Tree-SHA512: aed41b08947728a4ff3a97a62858ee7c86e2e5d57dcbbd0aab492dae3d8a548bb60541924e68cf3a0aa3d53d7db0012b489462b466919cd83f05b2aa88b7fff7
    
    Former-commit-id: 3f159b5220d8d3560e1aea7edbe6946f8bd44e9b

--- src/syscoin-wallet.cpp
+++ src/syscoin-wallet.cpp
@@ -41,8 +41,8 @@ static bool WalletAppInit(int argc, char* argv[])
     }
     if (argc < 2 || HelpRequested(gArgs)) {
         std::string usage = strprintf("%s syscoin-wallet version", PACKAGE_NAME) + " " + FormatFullVersion() + "\n\n" +
-                                      "wallet-tool is an offline tool for creating and interacting with Syscoin Core wallet files.\n" +
-                                      "By default wallet-tool will act on wallets in the default mainnet wallet directory in the datadir.\n" +
+                                      "syscoin-wallet is an offline tool for creating and interacting with Syscoin Core wallet files.\n" +
+                                      "By default syscoin-wallet will act on wallets in the default mainnet wallet directory in the datadir.\n" +
                                       "To change the target wallet, use the -datadir, -wallet and -testnet/-regtest arguments.\n\n" +
                                       "Usage:\n" +
                                      "  syscoin-wallet [options] <command>\n\n" +
