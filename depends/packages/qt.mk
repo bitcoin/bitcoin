11113247c323c5b98debcb512fb9db9fe5a8e7cf
MarcoFalke <falke.marco@gmail.com>
2019-12-05 12:15:48 -0500

    depends: Support for S390X targets

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -131,6 +131,7 @@ $(package)_config_opts_aarch64_linux = -xplatform linux-aarch64-gnu-g++
 $(package)_config_opts_powerpc64_linux = -platform linux-g++ -xplatform bitcoin-linux-g++
 $(package)_config_opts_powerpc64le_linux = -platform linux-g++ -xplatform bitcoin-linux-g++
 $(package)_config_opts_riscv64_linux = -platform linux-g++ -xplatform bitcoin-linux-g++
+$(package)_config_opts_s390x_linux = -platform linux-g++ -xplatform bitcoin-linux-g++
 
 $(package)_config_opts_mingw32 = -no-opengl
 $(package)_config_opts_mingw32 += -no-dbus
