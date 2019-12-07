fac86ac7b3ceac2f884412c7a9f4bd5bab5e3916
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2019-12-07 18:14:40 +0200

    scripted-diff: Add missed copyright headers
    -BEGIN VERIFY SCRIPT-
    s() { contrib/devtools/copyright_header.py insert "$1"; }
    
    s build_msvc/bitcoin_config.h
    s build_msvc/msvc-autogen.py
    s build_msvc/testconsensus/testconsensus.cpp
    s contrib/devtools/circular-dependencies.py
    s contrib/devtools/gen-manpages.sh
    s contrib/filter-lcov.py
    s contrib/gitian-build.py
    s contrib/install_db4.sh
    s src/crypto/sha256_avx2.cpp
    s src/crypto/sha256_sse41.cpp
    s src/fs.cpp
    s src/qt/test/addressbooktests.cpp
    s src/qt/test/addressbooktests.h
    s src/qt/test/util.cpp
    s src/qt/test/util.h
    s src/qt/test/wallettests.cpp
    s src/qt/test/wallettests.h
    s src/test/blockchain_tests.cpp
    s test/functional/combine_logs.py
    s test/lint/lint-locale-dependence.sh
    sed -i '1G' test/lint/lint-shebang.sh
    s test/lint/lint-shebang.sh
    -END VERIFY SCRIPT-

--- src/qt/test/addressbooktests.cpp
+++ src/qt/test/addressbooktests.cpp
@@ -1,3 +1,7 @@
+// Copyright (c) 2017-2019 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #include <qt/test/addressbooktests.h>
 #include <qt/test/util.h>
 #include <test/util/setup_common.h>
