ead86641b4699e4f25f402c23f4b4127acdef55b
fanquake <fanquake@gmail.com>
2019-12-07 15:32:05 -0500

    gui: disable File->CreateWallet during startup
    Former-commit-id: 21cef3d5a280d950219250f49d8a282e740bcfd0

--- src/qt/syscoingui.cpp
+++ src/qt/syscoingui.cpp
@@ -359,6 +359,7 @@ void SyscoinGUI::createActions()
     m_close_wallet_action->setStatusTip(tr("Close wallet"));
 
     m_create_wallet_action = new QAction(tr("Create Wallet..."), this);
+    m_create_wallet_action->setEnabled(false);
     m_create_wallet_action->setStatusTip(tr("Create a new wallet"));
 
     showHelpMessageAction = new QAction(tr("&Command-line options"), this);
@@ -643,6 +644,7 @@ void SyscoinGUI::setWalletController(WalletController* wallet_controller)
 
     m_wallet_controller = wallet_controller;
 
+    m_create_wallet_action->setEnabled(true);
     m_open_wallet_action->setEnabled(true);
     m_open_wallet_action->setMenu(m_open_wallet_menu);
 
