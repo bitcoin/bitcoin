d945c6f5e6f61b6e289ac7da6834c18f1b677b0f
practicalswift <practicalswift@users.noreply.github.com>
2019-12-11 11:00:52 +0000

    util: Don't allow base58-decoding of std::string:s containing non-base58 characters

--- src/util/strencodings.cpp
+++ src/util/strencodings.cpp
@@ -4,6 +4,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include <util/strencodings.h>
+#include <util/string.h>
 
 #include <tinyformat.h>
 
@@ -269,7 +270,7 @@ NODISCARD static bool ParsePrechecks(const std::string& str)
         return false;
     if (str.size() >= 1 && (IsSpace(str[0]) || IsSpace(str[str.size()-1]))) // No padding allowed
         return false;
-    if (str.size() != strlen(str.c_str())) // No embedded NUL characters allowed
+    if (!ValidAsCString(str)) // No embedded NUL characters allowed
         return false;
     return true;
 }
