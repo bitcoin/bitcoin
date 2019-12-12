f08497c93e67b762dbae229a4ae08f31b1e45a22
fanquake <fanquake@gmail.com>
2019-12-11 20:10:59 -0500

    Merge #17705: test: re-enable CLI test support by using EncodeDecimal in json.dumps()
    b6f9e3576a1ea18572e4803aeb3f39330f0cb759 test: re-enable CLI test support by using EncodeDecimal in json.dumps() (fanquake)
    
    Pull request description:
    
      As mentioned in https://github.com/bitcoin/bitcoin/pull/17675#issuecomment-563188648.
    
    ACKs for top commit:
      practicalswift:
        ACK b6f9e3576a1ea18572e4803aeb3f39330f0cb759 assuming Travis is happy too -- diff looks correct :)
      MarcoFalke:
        > ACK b6f9e35 assuming Travis is happy too -- diff looks correct :)
    
    Tree-SHA512: 79fa535cc1756c8ee610a3d6a316a1c4f036797d6990a5620e44985393a2e52f78450f8e0021d0a148c08705fd1ba765508464a365f9030ae0d2cacbd7a93e19

--- test/functional/wallet_reorgsrestore.py
+++ test/functional/wallet_reorgsrestore.py
@@ -27,7 +27,6 @@ from test_framework.util import (
 class ReorgsRestoreTest(BitcoinTestFramework):
     def set_test_params(self):
         self.num_nodes = 3
-        self.supports_cli = False
 
     def skip_test_if_missing_module(self):
         self.skip_if_no_wallet()
