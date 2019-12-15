7aab8d1024996c7c422bd34a8226df0117b813f7
John Newbery <john@johnnewbery.com>
2019-12-15 12:53:51 -0300

    [style] Code style fixups in GetWarnings()

--- src/warnings.cpp
+++ src/warnings.cpp
@@ -42,31 +42,28 @@ std::string GetWarnings(bool verbose)
 {
     std::string warnings_concise;
     std::string warnings_verbose;
-    const std::string uiAlertSeperator = "<hr />";
+    const std::string warning_separator = "<hr />";
 
     LOCK(cs_warnings);
 
+    // Pre-release build warning
     if (!CLIENT_VERSION_IS_RELEASE) {
         warnings_concise = "This is a pre-release test build - use at your own risk - do not use for mining or merchant applications";
         warnings_verbose = _("This is a pre-release test build - use at your own risk - do not use for mining or merchant applications").translated;
     }
 
     // Misc warnings like out of disk space and clock is wrong
-    if (strMiscWarning != "")
-    {
+    if (strMiscWarning != "") {
         warnings_concise = strMiscWarning;
-        warnings_verbose += (warnings_verbose.empty() ? "" : uiAlertSeperator) + strMiscWarning;
+        warnings_verbose += (warnings_verbose.empty() ? "" : warning_separator) + strMiscWarning;
     }
 
-    if (fLargeWorkForkFound)
-    {
+    if (fLargeWorkForkFound) {
         warnings_concise = "Warning: The network does not appear to fully agree! Some miners appear to be experiencing issues.";
-        warnings_verbose += (warnings_verbose.empty() ? "" : uiAlertSeperator) + _("Warning: The network does not appear to fully agree! Some miners appear to be experiencing issues.").translated;
-    }
-    else if (fLargeWorkInvalidChainFound)
-    {
+        warnings_verbose += (warnings_verbose.empty() ? "" : warning_separator) + _("Warning: The network does not appear to fully agree! Some miners appear to be experiencing issues.").translated;
+    } else if (fLargeWorkInvalidChainFound) {
         warnings_concise = "Warning: We do not appear to fully agree with our peers! You may need to upgrade, or other nodes may need to upgrade.";
-        warnings_verbose += (warnings_verbose.empty() ? "" : uiAlertSeperator) + _("Warning: We do not appear to fully agree with our peers! You may need to upgrade, or other nodes may need to upgrade.").translated;
+        warnings_verbose += (warnings_verbose.empty() ? "" : warning_separator) + _("Warning: We do not appear to fully agree with our peers! You may need to upgrade, or other nodes may need to upgrade.").translated;
     }
 
     if (verbose) return warnings_verbose;
