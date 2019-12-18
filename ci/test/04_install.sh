fa569e1a9c5ad1bf8bdf866235b21aff56112224
MarcoFalke <falke.marco@gmail.com>
2019-12-17 22:02:48 -0500

    ci: Set LC_ALL=C when running in qemu-s390x
    This avoids std::runtime_error in the unit tests of the type
    "locale::facet::_S_create_c_locale name not valid"

--- ci/test/04_install.sh
+++ ci/test/04_install.sh
@@ -9,6 +9,9 @@ export LC_ALL=C.UTF-8
 if [[ $DOCKER_NAME_TAG == centos* ]]; then
   export LC_ALL=en_US.utf8
 fi
+if [[ $QEMU_USER_CMD == qemu-s390* ]]; then
+  export LC_ALL=C
+fi
 
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
   set +o errexit
