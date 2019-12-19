19361fd81338ed9af727b27650d86d1d61ff134f
fanquake <fanquake@gmail.com>
2019-12-19 10:32:29 -0500

    lint spellcheck fixes
    Former-commit-id: 78aaaf38fd2272b633dd5578dbf11f48cc6a5dae

--- src/services/assetconsensus.cpp
+++ src/services/assetconsensus.cpp
@@ -1874,7 +1874,7 @@ bool CEthereumMintedTxDB::FlushWrite(const EthereumMintTxVec &vecMintKeys){
         batch.Write(key.first.first, key.second);
         // write the bridge transfer ID if it existed (should on mainnet, and testnet after canceltransfer feature introduced)
         if(key.first.second > 0){
-            // create link between keys for reorg compatbility because bridge transfer id isn't serialized
+            // create link between keys for reorg compatibility because bridge transfer id isn't serialized
             // we could have easily done key.first.second, key.second but that would break under reorgs
             batch.Write(key.first.second, key.first.first);
         } 
