aaaaad6ac95b402fe18d019d67897ced6b316ee0
MarcoFalke <falke.marco@gmail.com>
2019-12-30 22:39:22 +1300

    scripted-diff: Bump copyright of files changed in 2019
    -BEGIN VERIFY SCRIPT-
    ./contrib/devtools/copyright_header.py update ./
    -END VERIFY SCRIPT-

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Copyright (c) 2013-2016 The Bitcoin Core developers
+# Copyright (c) 2013-2019 The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
