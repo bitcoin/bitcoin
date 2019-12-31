2da9982e55ffb46abec4d061e35d907865863e02
fanquake <fanquake@gmail.com>
2019-12-31 20:35:41 +0300

    Merge #17829: scripted-diff: Bump copyright of files changed in 2019
    aaaaad6ac95b402fe18d019d67897ced6b316ee0 scripted-diff: Bump copyright of files changed in 2019 (MarcoFalke)
    
    Pull request description:
    
    ACKs for top commit:
      practicalswift:
        ACK aaaaad6ac95b402fe18d019d67897ced6b316ee0
      promag:
        ACK aaaaad6ac95b402fe18d019d67897ced6b316ee0 🎉
      fanquake:
        ACK aaaaad6ac95b402fe18d019d67897ced6b316ee0 - going to merge this now because the year is over and conflicts are minimal.
    
    Tree-SHA512: 58cb1f53bc4c1395b2766f36fabc7e2332e213780a802762fff0afd59468dad0c3265f553714d761c7a2c44ff90f7dc250f04458f4b2eb8eef8b94f8c9891321

--- contrib/macdeploy/detached-sig-create.sh
+++ contrib/macdeploy/detached-sig-create.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Copyright (c) 2014-2015 The Bitcoin Core developers
+# Copyright (c) 2014-2019 The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
