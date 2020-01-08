a1c2386153546cd9c347a64058ff3f3ff34d1856
Wladimir J. van der Laan <laanwj@protonmail.com>
2020-01-08 15:19:59 +0100

    Merge #17445: zmq: Fix due to invalid argument and multiple notifiers
    3e730bf90aaf53c41ff3a778f6aac15d163d1c0c zmq: Fix due to invalid argument and multiple notifiers (João Barbosa)
    
    Pull request description:
    
      ZMQ initialization is interrupted if any notifier fails, and in that case all notifiers are destroyed. The notifier shutdown assumes that the initialization had occurred. This is not valid when there are multiple notifiers and any except the last fails to initialize.
    
      Can be tested by running test/functional/interface_zmq.py from this branch with bitcoind from master.
    
      Closes #17185.
    
    ACKs for top commit:
      laanwj:
        Code review ACK 3e730bf90aaf53c41ff3a778f6aac15d163d1c0c, thanks for adding a test
    
    Tree-SHA512: 5da710e97dcbaa94896d019e75162d470f6d381ee07c60e5b3e9db93d11e8f7ca9bf2c509efa4486199e88c96c3e720cc96b4e35b62725d4c7db8e8e9bf6e09d

--- test/functional/interface_zmq.py
+++ test/functional/interface_zmq.py
@@ -64,6 +64,11 @@ class ZMQTest (BitcoinTestFramework):
         # Note that the publishing order is not defined in the documentation and
         # is subject to change.
         import zmq
+
+        # Invalid zmq arguments don't take down the node, see #17185.
+        self.restart_node(0, ["-zmqpubrawtx=foo", "-zmqpubhashtx=bar"])
+        self.zmq_context = zmq.Context()
+
         address = 'tcp://127.0.0.1:28332'
         socket = self.ctx.socket(zmq.SUB)
         socket.set(zmq.RCVTIMEO, 60000)
