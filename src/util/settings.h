90b20a9841e9b5875e5c9a3db9fe88bc534c3f51
Ben Woosley <ben.woosley@gmail.com>
2020-01-10 16:32:12 -0800

    Fix improper Doxygen inline comments
    The proper syntax is "//!<"
    http://www.doxygen.nl/manual/docblocks.html#memberdoc
    
    Former-commit-id: 0aa78c72164a8207e9fe22055e60255176eec54c

--- src/util/settings.h
+++ src/util/settings.h
@@ -71,11 +71,11 @@ struct SettingsSpan {
     explicit SettingsSpan(const SettingsValue& value) noexcept : SettingsSpan(&value, 1) {}
     explicit SettingsSpan(const SettingsValue* data, size_t size) noexcept : data(data), size(size) {}
     explicit SettingsSpan(const std::vector<SettingsValue>& vec) noexcept;
-    const SettingsValue* begin() const; //<! Pointer to first non-negated value.
-    const SettingsValue* end() const;   //<! Pointer to end of values.
-    bool empty() const;                 //<! True if there are any non-negated values.
-    bool last_negated() const;          //<! True if the last value is negated.
-    size_t negated() const;             //<! Number of negated values.
+    const SettingsValue* begin() const; //!< Pointer to first non-negated value.
+    const SettingsValue* end() const;   //!< Pointer to end of values.
+    bool empty() const;                 //!< True if there are any non-negated values.
+    bool last_negated() const;          //!< True if the last value is negated.
+    size_t negated() const;             //!< Number of negated values.
 
     const SettingsValue* data = nullptr;
     size_t size = 0;
