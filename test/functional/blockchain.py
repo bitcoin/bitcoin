8d2cf6b82ed06de764ab05343fad74109ec948c5
UdjinM6 <UdjinM6@users.noreply.github.com>
2020-01-13 01:31:45 +0300

    assert_raises_jsonrpc -> assert_raises_rpc_error

--- test/functional/blockchain.py
+++ test/functional/blockchain.py
@@ -125,7 +125,7 @@ class BlockchainTest(BitcoinTestFramework):
         assert('window_interval' not in chaintxstats)
         assert('txrate' not in chaintxstats)
 
-        assert_raises_jsonrpc(-8, "Invalid block count: should be between 0 and the block's height - 1", self.nodes[0].getchaintxstats, 201)
+        assert_raises_rpc_error(-8, "Invalid block count: should be between 0 and the block's height - 1", self.nodes[0].getchaintxstats, 201)
 
     def _test_gettxoutsetinfo(self):
         node = self.nodes[0]
