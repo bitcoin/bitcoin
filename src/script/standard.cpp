c2bb705d86b2c5d77decf5cc15972f94a7bde842
Ben Woosley <ben.woosley@gmail.com>
2020-01-15 21:48:59 +0000

    Explicitly support conversion between equivalent hash types
    ScriptHash <-> CScriptID
    CKeyID -> PKHash
    PKHash -> WitnessV0KeyHash
    
    Former-commit-id: 4c73ad9e3376a3952da1ca97e9a06fb8467f92f7

--- src/script/standard.cpp
+++ src/script/standard.cpp
@@ -17,11 +17,16 @@ bool fAcceptDatacarrier = DEFAULT_ACCEPT_DATACARRIER;
 unsigned nMaxDatacarrierBytes = MAX_OP_RETURN_RELAY;
 
 CScriptID::CScriptID(const CScript& in) : uint160(Hash160(in.begin(), in.end())) {}
+CScriptID::CScriptID(const ScriptHash& in) : uint160(static_cast<uint160>(in)) {}
 
 ScriptHash::ScriptHash(const CScript& in) : uint160(Hash160(in.begin(), in.end())) {}
+ScriptHash::ScriptHash(const CScriptID& in) : uint160(static_cast<uint160>(in)) {}
 
 PKHash::PKHash(const CPubKey& pubkey) : uint160(pubkey.GetID()) {}
+PKHash::PKHash(const CKeyID& pubkey_id) : uint160(pubkey_id) {}
+
 WitnessV0KeyHash::WitnessV0KeyHash(const CPubKey& pubkey) : uint160(pubkey.GetID()) {}
+WitnessV0KeyHash::WitnessV0KeyHash(const PKHash& pubkey_hash) : uint160(static_cast<uint160>(pubkey_hash)) {}
 
 CKeyID ToKeyID(const PKHash& key_hash)
 {
