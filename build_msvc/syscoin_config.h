f718f07456ed888aba0dc5775014738b1cdfdc44
MarcoFalke <falke.marco@gmail.com>
2020-01-16 15:57:24 -0500

    Merge #17691: doc: Add missed copyright headers
    fac86ac7b3ceac2f884412c7a9f4bd5bab5e3916 scripted-diff: Add missed copyright headers (Hennadii Stepanov)
    6fde9d5e47fc9a1042b3fb68031eab5bf55e508d script: Update EXLUDE list in copyright_header.py (Hennadii Stepanov)
    1998152f15fd2b0e83f5068c375a34feaf73db8c script: Add empty line after C++ copyright (Hennadii Stepanov)
    071f2fc204f542c5a287ca8835115a2ee0bf2f50 script: Add ability to insert copyright to *.sh (Hennadii Stepanov)
    
    Pull request description:
    
      This PR improves `contrib/devtools/copyright_header.py` script and adds copyright headers to the files in `src` and `test` directories with two exceptions:
      - [`src/reverse_iterator.h`](https://github.com/bitcoin/bitcoin/blob/master/src/reverse_iterator.h) (added to exceptions)
      - [`src/test/fuzz/FuzzedDataProvider.h`](https://github.com/bitcoin/bitcoin/blob/master/src/test/fuzz/FuzzedDataProvider.h) (added to exceptions)
    
      On master 5622d8f3156a293e61d0964c33d4b21d8c9fd5e0:
      ```
      $ ./contrib/devtools/copyright_header.py report . | grep zero
        25 with zero copyrights
      ```
    
      With this PR:
      ```
      $ ./contrib/devtools/copyright_header.py report . | grep zero
         2 with zero copyrights
      ```
    
      ~I am uncertain about our copyright policy with `build_msvc` and `contrib` directories content, so they are out of scope of this PR.~
    
    ACKs for top commit:
      MarcoFalke:
        ACK fac86ac7b3ceac2f884412c7a9f4bd5bab5e3916
    
    Tree-SHA512: d7832c4a7a1a3b7806119775b40ec35d7982f49ff0e6199b8cee4c0e0a36e68d51728b6ee9924b1c161df4bc6105bd93391b79d42914357fa522f499cb113fa8
    
    Former-commit-id: 178129465a352137330b84206582759a82746af4

--- build_msvc/syscoin_config.h
+++ build_msvc/syscoin_config.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2018-2019 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #ifndef SYSCOIN_SYSCOIN_CONFIG_H
 #define SYSCOIN_SYSCOIN_CONFIG_H
 
--- test/lint/lint-locale-dependence.sh
+++ test/lint/lint-locale-dependence.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# Copyright (c) 2018-2019 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 export LC_ALL=C
 KNOWN_VIOLATIONS=(
