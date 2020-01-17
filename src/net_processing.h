6b6e614df4ce593aab561caabb8d1812e2e6266b
Suhas Daftuar <sdaftuar@gmail.com>
2020-01-17 11:43:11 -0500

    Use rolling bloom filter of recent block tx's for AlreadyHave() check
    In order to determine whether to download or process a relayed transaction, we
    try to determine if we already have the transaction, either in the mempool, in
    our recently rejected filter, in our orphan pool, or already confirmed in the
    chain itself.
    
    Prior to this commit, the heuristic for checking the chain is based on whether
    there's an output corresponding to the 0- or 1-index vout in our coin cache.
    While that is a quick check, it is very imprecise (say if those outputs were
    already spent in a block) -- we can do better by just keeping a rolling bloom
    filter of the transactions in recent blocks, which will capture the case of a
    transaction which has been confirmed and then fully spent already.
    
    To avoid relay problems for transactions which have been included in a recent
    block but then reorged out of the chain, we clear the bloom filter whenever a
    block is disconnected.
    
    
    Former-commit-id: 962130b7d148e05b16934b0f2153b1d4b5dc0bd7

--- src/net_processing.h
+++ src/net_processing.h
@@ -33,6 +33,7 @@ public:
      * Overridden from CValidationInterface.
      */
     void BlockConnected(const std::shared_ptr<const CBlock>& pblock, const CBlockIndex* pindexConnected, const std::vector<CTransactionRef>& vtxConflicted) override;
+    void BlockDisconnected(const std::shared_ptr<const CBlock> &block, const CBlockIndex* pindex) override;
     /**
      * Overridden from CValidationInterface.
      */
