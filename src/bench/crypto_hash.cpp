3c54f652786f44e1e2b1bd5de184c57a224254cf
UdjinM6 <UdjinM6@users.noreply.github.com>
2020-01-17 17:42:55 +0300

    Bump copyright year to 2020 (#3290)
    * Bump _COPYRIGHT_YEAR
    
    * Run copyright update script
    
    ./contrib/devtools/copyright_header.py update .
    
    * Update COPYING
    
    * Bump copyright year in dash-cli/qt/tx and dashd map pages

--- src/bench/crypto_hash.cpp
+++ src/bench/crypto_hash.cpp
@@ -1,5 +1,5 @@
 // Copyright (c) 2016 The Bitcoin Core developers
-// Copyright (c) 2018 The Dash Core developers
+// Copyright (c) 2018-2020 The Dash Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
