200c0ba66a1aa6832ebc25a4e51e5ecb5560906a
Harris <brakmic@gmail.com>
2020-01-18 20:49:27 +0100

    don't ask for clean chain in test

--- test/functional/rpc_generate.py
+++ test/functional/rpc_generate.py
@@ -11,7 +11,7 @@ from test_framework.util import assert_equal, assert_raises_process_error
 class GenerateRpcTest(BitcoinTestFramework):
     def set_test_params(self):
         self.num_nodes = 1
-        self.setup_clean_chain = True
+        self.setup_clean_chain = False
 
     def skip_test_if_missing_module(self):
         self.skip_if_no_wallet()
