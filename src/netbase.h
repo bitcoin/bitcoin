abb43891e3a1cbff093d957ece02aa5d9b531256
Wladimir J. van der Laan <laanwj@protonmail.com>
2020-01-22 20:14:12 +0100

    Merge #17754: net: Don't allow resolving of std::string with embedded NUL characters. Add tests.
    7a046cdc1423963bdcbcf9bb98560af61fa90b37 tests: Avoid using C-style NUL-terminated strings as arguments (practicalswift)
    fefb9165f23fe9d10ad092ec31715f906e0d2ee7 tests: Add tests to make sure lookup methods fail on std::string parameters with embedded NUL characters (practicalswift)
    9574de86ad703ad942cdd0eca79f48c0d42b102b net: Avoid using C-style NUL-terminated strings as arguments in the netbase interface (practicalswift)
    
    Pull request description:
    
      Don't allow resolving of `std::string`:s with embedded `NUL` characters.
    
      Avoid using C-style `NUL`-terminated strings as arguments in the `netbase` interface
    
      Add tests.
    
      The only place in where C-style `NUL`-terminated strings are actually needed is here:
    
      ```diff
      +    if (!ValidAsCString(name)) {
      +        return false;
      +    }
      ...
      -    int nErr = getaddrinfo(pszName, nullptr, &aiHint, &aiRes);
      +    int nErr = getaddrinfo(name.c_str(), nullptr, &aiHint, &aiRes);
           if (nErr)
               return false;
      ```
    
      Interface changes:
    
      ```diff
      -bool LookupHost(const char *pszName, std::vector<CNetAddr>& vIP, unsigned int nMaxSolutions, bool fAllowLookup);
      +bool LookupHost(const std::string& name, std::vector<CNetAddr>& vIP, unsigned int nMaxSolutions, bool fAllowLookup);
    
      -bool LookupHost(const char *pszName, CNetAddr& addr, bool fAllowLookup);
      +bool LookupHost(const std::string& name, CNetAddr& addr, bool fAllowLookup);
    
      -bool Lookup(const char *pszName, CService& addr, int portDefault, bool fAllowLookup);
      +bool Lookup(const std::string& name, CService& addr, int portDefault, bool fAllowLookup);
    
      -bool Lookup(const char *pszName, std::vector<CService>& vAddr, int portDefault, bool fAllowLookup, unsigned int nMaxSolutions);
      +bool Lookup(const std::string& name, std::vector<CService>& vAddr, int portDefault, bool fAllowLookup, unsigned int nMaxSolutions);
    
      -bool LookupSubNet(const char *pszName, CSubNet& subnet);
      +bool LookupSubNet(const std::string& strSubnet, CSubNet& subnet);
    
      -CService LookupNumeric(const char *pszName, int portDefault = 0);
      +CService LookupNumeric(const std::string& name, int portDefault = 0);
    
      -bool ConnectThroughProxy(const proxyType &proxy, const std::string& strDest, int port, const SOCKET& hSocketRet, int nTimeout, bool *outProxyConnectionFailed);
      +bool ConnectThroughProxy(const proxyType &proxy, const std::string& strDest, int port, const SOCKET& hSocketRet, int nTimeout, bool& outProxyConnectionFailed);
      ```
    
      It should be noted that the `ConnectThroughProxy` change (from `bool *outProxyConnectionFailed` to `bool& outProxyConnectionFailed`) has nothing to do with `NUL` handling but I thought it was worth doing when touching this file :)
    
    ACKs for top commit:
      EthanHeilman:
        ACK 7a046cdc1423963bdcbcf9bb98560af61fa90b37
      laanwj:
        ACK 7a046cdc1423963bdcbcf9bb98560af61fa90b37
    
    Tree-SHA512: 66556e290db996917b54091acd591df221f72230f6b9f6b167b9195ee870ebef6e26f4cda2f6f54d00e1c362e1743bf56785d0de7cae854e6bf7d26f6caccaba
    
    Former-commit-id: fc9bd3b507a70fc19b96e98b526279d7f5fbc75c

--- src/netbase.h
+++ src/netbase.h
@@ -45,15 +45,15 @@ bool IsProxy(const CNetAddr &addr);
 bool SetNameProxy(const proxyType &addrProxy);
 bool HaveNameProxy();
 bool GetNameProxy(proxyType &nameProxyOut);
-bool LookupHost(const char *pszName, std::vector<CNetAddr>& vIP, unsigned int nMaxSolutions, bool fAllowLookup);
-bool LookupHost(const char *pszName, CNetAddr& addr, bool fAllowLookup);
-bool Lookup(const char *pszName, CService& addr, int portDefault, bool fAllowLookup);
-bool Lookup(const char *pszName, std::vector<CService>& vAddr, int portDefault, bool fAllowLookup, unsigned int nMaxSolutions);
-CService LookupNumeric(const char *pszName, int portDefault = 0);
-bool LookupSubNet(const char *pszName, CSubNet& subnet);
+bool LookupHost(const std::string& name, std::vector<CNetAddr>& vIP, unsigned int nMaxSolutions, bool fAllowLookup);
+bool LookupHost(const std::string& name, CNetAddr& addr, bool fAllowLookup);
+bool Lookup(const std::string& name, CService& addr, int portDefault, bool fAllowLookup);
+bool Lookup(const std::string& name, std::vector<CService>& vAddr, int portDefault, bool fAllowLookup, unsigned int nMaxSolutions);
+CService LookupNumeric(const std::string& name, int portDefault = 0);
+bool LookupSubNet(const std::string& strSubnet, CSubNet& subnet);
 SOCKET CreateSocket(const CService &addrConnect);
 bool ConnectSocketDirectly(const CService &addrConnect, const SOCKET& hSocketRet, int nTimeout, bool manual_connection);
-bool ConnectThroughProxy(const proxyType &proxy, const std::string& strDest, int port, const SOCKET& hSocketRet, int nTimeout, bool *outProxyConnectionFailed);
+bool ConnectThroughProxy(const proxyType &proxy, const std::string& strDest, int port, const SOCKET& hSocketRet, int nTimeout, bool& outProxyConnectionFailed);
 /** Return readable error string for a network error code */
 std::string NetworkErrorString(int err);
 /** Close socket and set hSocket to INVALID_SOCKET */
