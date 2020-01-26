1964561a3a5eacbb27139e9125859854c0e77437
fanquake <fanquake@gmail.com>
2020-01-26 12:16:44 +0800

    build: don't embed a build-id when building libdmg-hfsplus
    Github-Pull: #18004
    Rebased-From: cb9e88e73a042ff4e1c83289a6f8fa1db03fb093

--- depends/packages/native_libdmg-hfsplus.mk
+++ depends/packages/native_libdmg-hfsplus.mk
@@ -10,7 +10,7 @@ define $(package)_preprocess_cmds
 endef
 
 define $(package)_config_cmds
-  cmake -DCMAKE_INSTALL_PREFIX:PATH=$(build_prefix)/bin ..
+  cmake -DCMAKE_INSTALL_PREFIX:PATH=$(build_prefix)/bin -DCMAKE_C_FLAGS="-Wl,--build-id=none" ..
 endef
 
 define $(package)_build_cmds
