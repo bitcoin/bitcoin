a5cc88f59e7c8ed5f071d5eb8ecd466cce8b51de
Peter Bushnell <bushsolo@gmail.com>
2020-01-28 10:21:33 +0000

    GBT rule example missing single quotes

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -532,7 +532,7 @@ static UniValue getblocktemplate(const JSONRPCRequest& request)
 
     // GBT must be called with 'segwit' set in the rules
     if (setClientRules.count("segwit") != 1) {
-        throw JSONRPCError(RPC_INVALID_PARAMETER, "getblocktemplate must be called with the segwit rule set (call with {\"rules\": [\"segwit\"]})");
+        throw JSONRPCError(RPC_INVALID_PARAMETER, "getblocktemplate must be called with the segwit rule set (call with '{\"rules\": [\"segwit\"]}')");
     }
 
     // Update block
