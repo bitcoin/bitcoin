fa91a0985e29833db7a3842e50a1b0e9aef01c3a
Jakub Trnka <jak3264@gmail.com>
2020-02-01 11:55:01 +0100

    wallet: replace boost deprecated method call
    Use recursive_directory_iterator.depth() instead of
    recursive_directory_iterator.level()
    
    fixes #18049

--- src/wallet/walletutil.cpp
+++ src/wallet/walletutil.cpp
@@ -74,7 +74,7 @@ std::vector<fs::path> ListWalletDir()
         if (it->status().type() == fs::directory_file && IsBerkeleyBtree(it->path() / "wallet.dat")) {
             // Found a directory which contains wallet.dat btree file, add it as a wallet.
             paths.emplace_back(path);
-        } else if (it.level() == 0 && it->symlink_status().type() == fs::regular_file && IsBerkeleyBtree(it->path())) {
+        } else if (it.depth() == 0 && it->symlink_status().type() == fs::regular_file && IsBerkeleyBtree(it->path())) {
             if (it->path().filename() == "wallet.dat") {
                 // Found top-level wallet.dat btree file, add top level directory ""
                 // as a wallet.
