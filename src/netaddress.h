9002c1f25f8434c7fbfc77ffacad12743d9dbf6b
Jon Atack <jon@atack.com>
2020-02-05 15:49:33 +0100

    net: extract conditional to bool CNetAddr::IsHeNet
    and remove redundant public declaration
    
    
    Former-commit-id: f55e623f8564e5aff6ba17e739cc3045023dff2b

--- src/netaddress.h
+++ src/netaddress.h
@@ -45,7 +45,6 @@ class CNetAddr
          */
         void SetRaw(Network network, const uint8_t *data);
 
-    public:
         bool SetInternal(const std::string& name);
 
         bool SetSpecial(const std::string &strName); // for Tor addresses
@@ -66,6 +65,7 @@ class CNetAddr
         bool IsRFC4862() const; // IPv6 autoconfig (FE80::/64)
         bool IsRFC6052() const; // IPv6 well-known prefix for IPv4-embedded address (64:FF9B::/96)
         bool IsRFC6145() const; // IPv6 IPv4-translated address (::FFFF:0:0:0/96) (actually defined in RFC2765)
+        bool IsHeNet() const;   // IPv6 Hurricane Electric - https://he.net (2001:0470::/36)
         bool IsTor() const;
         bool IsLocal() const;
         bool IsRoutable() const;
