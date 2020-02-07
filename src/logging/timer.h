49f6178c3e5e3ad54a419da9d8523207da17fc64
Ben Woosley <ben.woosley@gmail.com>
2020-02-07 16:24:51 +0000

    Drop unused LOG_TIME_MICROS helper

--- src/logging/timer.h
+++ src/logging/timer.h
@@ -93,8 +93,6 @@ private:
 } // namespace BCLog
 
 
-#define LOG_TIME_MICROS(end_msg, ...) \
-    BCLog::Timer<std::chrono::microseconds> PASTE2(logging_timer, __COUNTER__)(__func__, end_msg, ## __VA_ARGS__)
 #define LOG_TIME_MILLIS(end_msg, ...) \
     BCLog::Timer<std::chrono::milliseconds> PASTE2(logging_timer, __COUNTER__)(__func__, end_msg, ## __VA_ARGS__)
 #define LOG_TIME_SECONDS(end_msg, ...) \
