37580b0ba35ab70bc01431351418bc71c0613d2a
Pasta <pasta@dashboost.org>
2020-02-12 20:49:34 -0600

    fix validationinterface.cpp
    Signed-off-by: Pasta <pasta@dashboost.org>

--- src/validationinterface.cpp
+++ src/validationinterface.cpp
@@ -159,7 +159,7 @@ void CMainSignals::UpdatedBlockTip(const CBlockIndex *pindexNew, const CBlockInd
 }
 
 void CMainSignals::TransactionAddedToMempool(const CTransactionRef &ptx, int64_t nAcceptTime) {
-    m_internals->m_schedulerClient.AddToProcessQueue([ptx, this] {
+    m_internals->m_schedulerClient.AddToProcessQueue([ptx, nAcceptTime, this] {
         m_internals->TransactionAddedToMempool(ptx, nAcceptTime);
     });
 }
@@ -171,7 +171,7 @@ void CMainSignals::BlockConnected(const std::shared_ptr<const CBlock> &pblock, c
 }
 
 void CMainSignals::BlockDisconnected(const std::shared_ptr<const CBlock> &pblock, const CBlockIndex* pindexDisconnected) {
-    m_internals->m_schedulerClient.AddToProcessQueue([pblock, this] {
+    m_internals->m_schedulerClient.AddToProcessQueue([pblock, pindexDisconnected, this] {
         m_internals->BlockDisconnected(pblock, pindexDisconnected);
     });
 }
