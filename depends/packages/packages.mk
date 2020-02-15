581d0e245cb7c93f2f43f5abbf57171c1ea41c86
fanquake <fanquake@gmail.com>
2020-02-15 20:41:14 +0800

    build: remove cdrkit package from depends

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -21,5 +21,5 @@ upnp_packages=miniupnpc
 darwin_native_packages = native_biplist native_ds_store native_mac_alias
 
 ifneq ($(build_os),darwin)
-darwin_native_packages += native_cctools native_cdrkit
+darwin_native_packages += native_cctools
 endif
