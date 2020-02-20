55c089ad7008098dcaadf0972df3e9ef237b3112
Jonas Schnelli <dev@jonasschnelli.ch>
2020-02-20 20:15:34 +0100

    Add --options runtime (runtime hardening) to macOS codesign Runtime hardening is necessary for app notarization https://developer.apple.com/documentation/security/hardened_runtime

--- contrib/macdeploy/detached-sig-create.sh
+++ contrib/macdeploy/detached-sig-create.sh
@@ -23,7 +23,7 @@ fi
 rm -rf ${TEMPDIR} ${TEMPLIST}
 mkdir -p ${TEMPDIR}
 
-${CODESIGN} -f --file-list ${TEMPLIST} "$@" "${BUNDLE}"
+${CODESIGN} --options runtime --timestamp -f --file-list ${TEMPLIST} "$@" "${BUNDLE}"
 
 grep -v CodeResources < "${TEMPLIST}" | while read i; do
   TARGETFILE="${BUNDLE}/$(echo "${i}" | sed "s|.*${BUNDLE}/||")"
