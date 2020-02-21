cea91a1e40e12029140ebfba969ce3ef2965029c
Luke Dashjr <luke-jr+git@utopios.org>
2020-02-21 19:09:49 +0000

    Bugfix: GUI: Use unsigned long long type to avoid implicit conversion of MSB check

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -760,7 +760,7 @@ QString formatServicesStr(quint64 mask)
     QStringList strList;
 
     for (int i = 0; i < 64; i++) {
-        uint64_t check = 1LL << i;
+        uint64_t check = 1ull << i;
         if (mask & check)
         {
             strList.append(serviceFlagToStr(check, i));
