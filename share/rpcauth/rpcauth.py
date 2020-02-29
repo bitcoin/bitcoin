131d8f5cf59fede78a0a59acc8a6f40386edda87
UdjinM6 <UdjinM6@users.noreply.github.com>
2020-02-29 22:38:24 +0300

    More of 11836

--- share/rpcauth/rpcauth.py
+++ share/rpcauth/rpcauth.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Copyright (c) 2015-2016 The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying 
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
