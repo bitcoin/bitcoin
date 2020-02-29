0d42fa00cea4beb276939618d1f85b40663c108b
UdjinM6 <UdjinM6@users.noreply.github.com>
2020-02-29 22:48:18 +0300

    Use mocktime in wallet-encryption.py

--- test/functional/wallet-encryption.py
+++ test/functional/wallet-encryption.py
@@ -65,13 +65,13 @@ class WalletEncryptionTest(BitcoinTestFramework):
         # Check the timeout
         # Check a time less than the limit
         MAX_VALUE = 100000000
-        expected_time = int(time.time()) + MAX_VALUE - 600
+        expected_time = self.mocktime + MAX_VALUE - 600
         self.nodes[0].walletpassphrase(passphrase2, MAX_VALUE - 600)
         actual_time = self.nodes[0].getwalletinfo()['unlocked_until']
         assert_greater_than_or_equal(actual_time, expected_time)
         assert_greater_than(expected_time + 5, actual_time) # 5 second buffer
         # Check a time greater than the limit
-        expected_time = int(time.time()) + MAX_VALUE - 1
+        expected_time = self.mocktime + MAX_VALUE - 1
         self.nodes[0].walletpassphrase(passphrase2, MAX_VALUE + 1000)
         actual_time = self.nodes[0].getwalletinfo()['unlocked_until']
         assert_greater_than_or_equal(actual_time, expected_time)
