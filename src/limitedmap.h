4c01ca4573875589deb47ee1e7e4d5b847a5afe4
Oleg Girko <ol+github@infoserver.lv>
2020-03-01 13:24:03 +0000

    Fix undefined behaviour in unordered_limitedmap and optimise it. (#3349)
    Switching from std::map to std::unordered_map and using the same approach
    of storing iterators to this map in reverse map for additional bookkeeping
    is incorrect and leads to undefined behaviour.
    
    Documentation on std::unordered_map::insert() method reads:
    
      If rehashing occurs due to the insertion, all iterators are invalidated.
      Otherwise iterators are not affected. References are not invalidated.
      Rehashing occurs only if the new number of elements is greater than
      max_load_factor()*bucket_count()
    
    Documentation on std::unordered_map::unordered_map() constructor mentions
    that minimal bucket count is implementation dependent if not specified.
    
    As the map is created without specifying minimum bucket count,
    it can be rehashed after any insertion, invalidating all iterators
    stored in reverse map and causing undefined behaviour.
    
    This is not just a theoretical problem: it causes assertion failures in
    test_dash utility if built with GCC 9.2 using _GLIBCXX_DEBUG define.
    
    Fortunately, the new approach of pruning the map's contents in batches
    using a sorted vector of iterators makes reverse map completely unnecessary.
    
    This change eliminates the reverse map completely, thus fixing undefined
    behaviour caused by invalidated iterators stored there.
    
    Signed-off-by: Oleg Girko <ol@infoserver.lv>

--- src/limitedmap.h
+++ src/limitedmap.h
@@ -27,8 +27,6 @@ public:
 protected:
     std::unordered_map<K, V, Hash> map;
     typedef typename std::unordered_map<K, V, Hash>::iterator iterator;
-    std::unordered_multimap<V, iterator> rmap;
-    typedef typename std::unordered_multimap<V, iterator>::iterator rmap_iterator;
     size_type nMaxSize;
     size_type nPruneAfterSize;
 
@@ -53,25 +51,12 @@ public:
     void insert(const value_type& x)
     {
         std::pair<iterator, bool> ret = map.insert(x);
-        if (ret.second) {
+        if (ret.second)
             prune();
-            rmap.insert(make_pair(x.second, ret.first));
-        }
     }
     void erase(const key_type& k)
     {
-        iterator itTarget = map.find(k);
-        if (itTarget == map.end())
-            return;
-        std::pair<rmap_iterator, rmap_iterator> itPair = rmap.equal_range(itTarget->second);
-        for (rmap_iterator it = itPair.first; it != itPair.second; ++it)
-            if (it->second == itTarget) {
-                rmap.erase(it);
-                map.erase(itTarget);
-                return;
-            }
-        // Shouldn't ever get here
-        assert(0);
+        map.erase(k);
     }
     void update(const_iterator itIn, const mapped_type& v)
     {
@@ -79,19 +64,9 @@ public:
         // since it is a constant time operation in C++11. For more details, see
         // https://stackoverflow.com/questions/765148/how-to-remove-constness-of-const-iterator
         iterator itTarget = map.erase(itIn, itIn);
-        
         if (itTarget == map.end())
             return;
-        std::pair<rmap_iterator, rmap_iterator> itPair = rmap.equal_range(itTarget->second);
-        for (rmap_iterator it = itPair.first; it != itPair.second; ++it)
-            if (it->second == itTarget) {
-                rmap.erase(it);
-                itTarget->second = v;
-                rmap.insert(make_pair(v, itTarget));
-                return;
-            }
-        // Shouldn't ever get here
-        assert(0);
+        itTarget->second = v;
     }
     size_type max_size() const { return nMaxSize; }
     size_type max_size(size_type nMaxSizeIn, size_type nPruneAfterSizeIn = 0)
@@ -113,13 +88,13 @@ public:
             return;
         }
 
-        std::vector<rmap_iterator> sortedIterators;
+        std::vector<iterator> sortedIterators;
         sortedIterators.reserve(map.size());
-        for (auto it = rmap.begin(); it != rmap.end(); ++it) {
+        for (auto it = map.begin(); it != map.end(); ++it) {
             sortedIterators.emplace_back(it);
         }
-        std::sort(sortedIterators.begin(), sortedIterators.end(), [](const rmap_iterator& it1, const rmap_iterator& it2) {
-            return it1->first < it2->first;
+        std::sort(sortedIterators.begin(), sortedIterators.end(), [](const iterator& it1, const iterator& it2) {
+            return it1->second < it2->second;
         });
 
         size_type tooMuch = map.size() - nMaxSize;
@@ -127,8 +102,7 @@ public:
         sortedIterators.resize(tooMuch);
 
         for (auto& it : sortedIterators) {
-            map.erase(it->second);
-            rmap.erase(it);
+            map.erase(it);
         }
     }
 };
