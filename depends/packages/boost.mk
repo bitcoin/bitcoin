7ac48612e2bea65bd6e2f369ab398c7bf47da190
MarcoFalke <falke.marco@gmail.com>
2020-03-02 09:59:04 -0500

    depends: Set boost to 1.66

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -1,8 +1,8 @@
 package=boost
-$(package)_version=1_67_0
-$(package)_download_path=https://dl.bintray.com/boostorg/release/1.67.0/source/
+$(package)_version=1_66_0
+$(package)_download_path=https://dl.bintray.com/boostorg/release/1.66.0/source/
 $(package)_file_name=$(package)_$($(package)_version).tar.bz2
-$(package)_sha256_hash=2684c972994ee57fc5632e03bf044746f6eb45d4920c343937a465fd67a5adba
+$(package)_sha256_hash=5721818253e6a0989583192f96782c4a98eb6204965316df9f5ad75819225ca9
 
 define $(package)_set_vars
 $(package)_config_opts_release=variant=release
