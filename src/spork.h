c2f15eec083bec8c07ff3718791ead553490fc62
Mark Brooker <mark@walkjivefly.com>
2020-03-03 12:36:13 +0100

    Update SPORK_END to include NFT spork

--- src/spork.h
+++ src/spork.h
@@ -1,5 +1,5 @@
 
-// Copyright (c) 2009-2012 The Crown developers
+// Copyright (c) 2014-2020 The Crown developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #ifndef SPORK_H
@@ -24,7 +24,7 @@ using namespace boost;
     - This would result in old clients getting confused about which spork is for what
 */
 #define SPORK_START                                           10001
-#define SPORK_END                                             10015
+#define SPORK_END                                             10016
 
 #define SPORK_2_INSTANTX                                      10001
 #define SPORK_3_INSTANTX_BLOCK_FILTERING                      10002
