88f3793377b7fdca76669b4557a8d8336f0602a6
Luke Dashjr <luke-jr+git@utopios.org>
2020-03-07 01:12:32 +0000

    depends: Patch libevent build to fix IPv6 -rpcbind on Windows
    libevent uses getaddrinfo when available, and falls back to gethostbyname
    Windows has both, but gethostbyname only supports IPv4
    libevent fails to detect Windows's getaddrinfo due to not including the right headers
    This patches libevent's configure script to check it correctly
    
    
    Former-commit-id: fbefb61ef4281144a1c70e53c99f017ece103334

--- depends/packages/libevent.mk
+++ depends/packages/libevent.mk
@@ -3,8 +3,10 @@ $(package)_version=2.1.11-stable
 $(package)_download_path=https://github.com/libevent/libevent/archive/
 $(package)_file_name=release-$($(package)_version).tar.gz
 $(package)_sha256_hash=229393ab2bf0dc94694f21836846b424f3532585bac3468738b7bf752c03901e
+$(package)_patches=0001-fix-windows-getaddrinfo.patch
 
 define $(package)_preprocess_cmds
+   patch -p1 < $($(package)_patch_dir)/0001-fix-windows-getaddrinfo.patch && \
   ./autogen.sh
 endef
 
