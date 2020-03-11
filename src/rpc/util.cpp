a33cffbeabcc42137c4a66aa19b7dd1d300e6d73
Jon Atack <jon@atack.com>
2020-03-11 16:32:06 +0100

    util: HelpExampleRpc formatting fixup
    to conform to json-rpc spec as per
    https://www.jsonrpc.org/specification#examples

--- src/rpc/util.cpp
+++ src/rpc/util.cpp
@@ -116,8 +116,8 @@ std::string HelpExampleCli(const std::string& methodname, const std::string& arg
 
 std::string HelpExampleRpc(const std::string& methodname, const std::string& args)
 {
-    return "> curl --user myusername --data-binary '{\"jsonrpc\": \"1.0\", \"id\":\"curltest\", "
-        "\"method\": \"" + methodname + "\", \"params\": [" + args + "] }' -H 'content-type: text/plain;' http://127.0.0.1:8332/\n";
+    return "> curl --user myusername --data-binary '{\"jsonrpc\": \"1.0\", \"id\": \"curltest\", "
+        "\"method\": \"" + methodname + "\", \"params\": [" + args + "]}' -H 'content-type: text/plain;' http://127.0.0.1:8332/\n";
 }
 
 // Converts a hex string to a public key if possible
