ab4df1c8e720416a1104ebfd1cf290485f30bd3c
MarcoFalke <falke.marco@gmail.com>
2020-03-11 12:42:23 -0400

    Merge #18208: rpc: Change RPCExamples to bech32
    3e32499909ca8127baaa9b40ad113b25ee151bbd Change example addresses to bech32 (Yusuf Sahin HAMZA)
    
    Pull request description:
    
      This is a follow-up PR to #18197 that fixes RPCExamples.
    
      Fixes #18185.
    
    ACKs for top commit:
      MarcoFalke:
        ACK 3e32499909ca8127baaa9b40ad113b25ee151bbd
      jonatack:
        ACK 3e32499
    
    Tree-SHA512: c7a6410ef8b6e169016c2c5eac3e6b9501caabd0e8a0871ec31e56bfc44589f056d3f5cb55b5a13bba36f6c15136c2352f883e30e4dcc0997ffd36b27f9173b9
    
    Former-commit-id: b232f23b00b1914334cd87775509b82b73084814

--- src/rpc/util.h
+++ src/rpc/util.h
@@ -29,10 +29,10 @@
 extern const std::string UNIX_EPOCH_TIME;
 
 /**
- * Example bech32 address used in multiple RPCExamples. The address is intentionally
+ * Example bech32 addresses for the RPCExamples help documentation. They are intentionally
  * invalid to prevent accidental transactions by users.
  */
-extern const std::string EXAMPLE_ADDRESS;
+extern const std::string EXAMPLE_ADDRESS[2];
 
 class FillableSigningProvider;
 class CPubKey;
