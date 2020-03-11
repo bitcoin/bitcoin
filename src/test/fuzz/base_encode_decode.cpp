09f1bcb068ba725a4ba56d6cdd5b358df2cfecd6
practicalswift <practicalswift@users.noreply.github.com>
2020-03-11 12:05:52 +0000

    tests: Fuzz DecodeBase64PSBT(...)
    Former-commit-id: 8ddf8031add8f89ad75a83766f24b03a556cdc96

--- src/test/fuzz/base_encode_decode.cpp
+++ src/test/fuzz/base_encode_decode.cpp
@@ -5,6 +5,7 @@
 #include <test/fuzz/fuzz.h>
 
 #include <base58.h>
+#include <psbt.h>
 #include <util/string.h>
 #include <util/strencodings.h>
 
@@ -44,4 +45,8 @@ void test_one_input(const std::vector<uint8_t>& buffer)
         assert(encoded_string == TrimString(encoded_string));
         assert(ToLower(encoded_string) == ToLower(TrimString(random_encoded_string)));
     }
+
+    PartiallySignedTransaction psbt;
+    std::string error;
+    (void)DecodeBase64PSBT(psbt, random_encoded_string, error);
 }
