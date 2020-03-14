d044e0ec7d37bbcdf10bbdb903b9119741c7297d
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-03-14 08:50:15 +0200

    refactor: Remove override for final overriders

--- src/interfaces/chain.cpp
+++ src/interfaces/chain.cpp
@@ -127,7 +127,7 @@ public:
         ::tableRPC.appendCommand(m_command.name, &m_command);
     }
 
-    void disconnect() override final
+    void disconnect() final
     {
         if (m_wrapped_command) {
             m_wrapped_command = nullptr;
