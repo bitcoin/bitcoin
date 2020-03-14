166b1542d74f289f430826154dac17bb5bd4f18b
Vasil Dimov <vd@FreeBSD.org>
2020-03-14 20:58:55 +0100

    util: use stronger-guarantee rename method
    Use std::filesystem::rename() instead of std::rename(). We rely on the
    destination to be overwritten if it exists, but std::rename()'s behavior
    is implementation-defined in this case.

--- src/util/system.cpp
+++ src/util/system.cpp
@@ -64,6 +64,8 @@
 #endif
 
 #include <boost/algorithm/string/replace.hpp>
+#include <filesystem>
+#include <system_error>
 #include <thread>
 #include <typeinfo>
 #include <univalue.h>
@@ -1005,15 +1007,15 @@ void ArgsManager::LogArgs() const
     logArgsPrefix("Command-line arg:", "", m_settings.command_line_options);
 }
 
+/**
+ * Rename src to dest.
+ * @return true if the rename was successful.
+ */
 bool RenameOver(fs::path src, fs::path dest)
 {
-#ifdef WIN32
-    return MoveFileExW(src.wstring().c_str(), dest.wstring().c_str(),
-                       MOVEFILE_REPLACE_EXISTING) != 0;
-#else
-    int rc = std::rename(src.string().c_str(), dest.string().c_str());
-    return (rc == 0);
-#endif /* WIN32 */
+    std::error_code error;
+    std::filesystem::rename(src.string(), dest.string(), error);
+    return !error;
 }
 
 /**
