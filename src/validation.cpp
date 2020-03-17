1fb5f7707b2ffc4199f037b78d11e5b33dbcd0bd
MarcoFalke <falke.marco@gmail.com>
2020-03-17 14:27:37 -0400

    Merge #15283: log: Fix UB with bench on genesis block
    ec30a79f1c430cc7fbda37e5d747b0b31b262fa5 Fix UB with bench on genesis block (Gregory Sanders)
    
    Pull request description:
    
      During the loading of the genesis block, the bench print lines in ConnectTip divide by zero due to early return in ConnectBlock.
    
    ACKs for top commit:
      practicalswift:
        ACK ec30a79f1c430cc7fbda37e5d747b0b31b262fa5
      sipa:
        utACK ec30a79f1c430cc7fbda37e5d747b0b31b262fa5
      promag:
        ACK ec30a79, `nBlocksTotal` is only used in logging.
    
    Tree-SHA512: b3bdbb58d10d002a2293d7f99196b227ed9f4ca8c6cd08981e95cc964be47efed98b91fad276ee6da5cf7e6684610998ace7ce9bace172dd6c51c386d985b83c
    
    Former-commit-id: ceb70cb60ad74dce254dc9a5469f61963580f8d9

--- src/validation.cpp
+++ src/validation.cpp
@@ -2166,6 +2166,8 @@ bool CChainState::ConnectBlock(const CBlock& block, BlockValidationState& state,
     uint256 hashPrevBlock = pindex->pprev == nullptr ? uint256() : pindex->pprev->GetBlockHash();
     assert(hashPrevBlock == view.GetBestBlock());
 
+    nBlocksTotal++;
+
     // Special case for the genesis block, skipping connection of its transactions
     // (its coinbase is unspendable)
     if (block.GetHash() == chainparams.GetConsensus().hashGenesisBlock) {
@@ -2173,8 +2175,6 @@ bool CChainState::ConnectBlock(const CBlock& block, BlockValidationState& state,
             view.SetBestBlock(pindex->GetBlockHash());
         return true;
     }
-
-    nBlocksTotal++;
 
     bool fScriptChecks = true;
     if (!hashAssumeValid.IsNull()) {
@@ -2811,6 +2811,7 @@ bool CChainState::ConnectTip(BlockValidationState& state, const CChainParams& ch
             return error("%s: ConnectBlock %s failed, %s", __func__, pindexNew->GetBlockHash().ToString(), state.ToString());
         }
         nTime3 = GetTimeMicros(); nTimeConnectTotal += nTime3 - nTime2;
+        assert(nBlocksTotal > 0);
         LogPrint(BCLog::BENCH, "  - Connect total: %.2fms [%.2fs (%.2fms/blk)]\n", (nTime3 - nTime2) * MILLI, nTimeConnectTotal * MICRO, nTimeConnectTotal * MILLI / nBlocksTotal);
         bool flushed = view.Flush();
         assert(flushed);
