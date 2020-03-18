feebdd3ad0c8a25c6ae5e1c1b1f32f017f552884
Willy Ko <willyk@syscoin.org>
2020-03-18 14:25:48 -0700

    Updated checkpoint and assumevalid
    Former-commit-id: 718028a66f30ef5c4d71353b26c52cbc7f342a59

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -135,7 +135,7 @@ public:
         consensus.nMinimumChainWork = uint256S("0x000000000000000000000000000000000000000000d9db446b03201ba9461642");
 
         // By default assume that the signatures in ancestors of this block are valid.
-        consensus.defaultAssumeValid = uint256S("0x76d13e8f08c2b7027251484078f734f91c485727031be6b4c21c42d5e103d0ad"); // 288481
+        consensus.defaultAssumeValid = uint256S("0x4c332acd53ca99ab78fb80a3dacffe234674674e0b682350c492d7fe839d128e"); // 419800
         consensus.nAuxpowChainId = 0x1000;
         consensus.nAuxpowStartHeight = 1;
         consensus.fStrictChainId = true;
@@ -206,6 +206,7 @@ public:
                 { 350000, uint256S("0x02501c7feba858c83e005acbf0505a892081288dcf7a8a37bd4fc47d7c24c799")},
                 { 390000, uint256S("0x8654451a7ed5286ba5c830cdf6e65cbbd7a77f650216541bfbe50af04933741b")},
                 { 391285, uint256S("76d13e8f08c2b7027251484078f734f91c485727031be6b4c21c42d5e103d0ad")},
+                { 419800, uint256S("4c332acd53ca99ab78fb80a3dacffe234674674e0b682350c492d7fe839d128e")},
             }
         };
 
