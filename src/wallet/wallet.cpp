889056286d63d71f580125020862e19bbab5fc1f
sidhujag <jsidhu@blockchainfoundry.co>
2020-03-19 10:47:37 -0700

    Merge #17477: Remove the mempool's NotifyEntryAdded and NotifyEntryRe…  …moved signals
    Former-commit-id: 3df361f60bd3f8428f652b2d6ffadfde350a50f2

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -1125,7 +1125,7 @@ void CWallet::TransactionRemovedFromMempool(const CTransactionRef &ptx) {
     }
 }
 
-void CWallet::BlockConnected(const CBlock& block, const std::vector<CTransactionRef>& vtxConflicted, int height)
+void CWallet::BlockConnected(const CBlock& block, int height)
 {
     const uint256& block_hash = block.GetHash();
     auto locked_chain = chain().lock();
@@ -1137,9 +1137,6 @@ void CWallet::BlockConnected(const CBlock& block, const std::vector<CTransaction
         CWalletTx::Confirmation confirm(CWalletTx::Status::CONFIRMED, height, block_hash, index);
         SyncTransaction(block.vtx[index], confirm);
         TransactionRemovedFromMempool(block.vtx[index]);
-    }
-    for (const CTransactionRef& ptx : vtxConflicted) {
-        TransactionRemovedFromMempool(ptx);
     }
 }
 
--- src/zmq/zmqnotificationinterface.h
+++ src/zmq/zmqnotificationinterface.h
@@ -27,7 +27,7 @@ protected:
     // CValidationInterface
     // SYSCOIN
     void TransactionAddedToMempool(const CTransactionRef& tx, bool fBlock) override;
-    void BlockConnected(const std::shared_ptr<const CBlock>& pblock, const CBlockIndex* pindexConnected, const std::vector<CTransactionRef>& vtxConflicted) override;
+    void BlockConnected(const std::shared_ptr<const CBlock>& pblock, const CBlockIndex* pindexConnected) override;
     void BlockDisconnected(const std::shared_ptr<const CBlock>& pblock, const CBlockIndex* pindexDisconnected) override;
     void UpdatedBlockTip(const CBlockIndex *pindexNew, const CBlockIndex *pindexFork, bool fInitialDownload) override;
     // SYSCOIN
