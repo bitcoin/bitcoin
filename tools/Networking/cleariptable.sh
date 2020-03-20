04a7c5a5d4a85acfc641f6edc9080d29190ea36a
SimeoW <swuthier@uccs.edu>
2020-03-20 11:45:34 -0600

    iptable modifications

---
+++ tools/Networking/cleariptable.sh
@@ -0,0 +1,10 @@
+sudo iptables -F
+
+sudo iptables -P INPUT ACCEPT
+sudo iptables -P FORWARD ACCEPT
+sudo iptables -P OUTPUT ACCEPT
+
+sudo iptables -t nat -F
+sudo iptables -t mangle -F
+sudo iptables -F
+sudo iptables -X
