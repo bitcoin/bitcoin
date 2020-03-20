48974a88392a7229174426f6a1e6dbb3000d78a0
MarcoFalke <falke.marco@gmail.com>
2020-03-20 10:48:13 -0400

    ci: Run feature_block and feature_abortnode in valgrind
    Former-commit-id: 3769e4a3ceb337f32392e1e5b0ddaa8b2f2b7f39

--- test/functional/feature_block.py
+++ test/functional/feature_block.py
@@ -1273,7 +1273,7 @@ class FullBlockTest(SyscoinTestFramework):
             self.save_spendable_output()
             spend = self.get_spendable_output()
 
-        self.send_blocks(blocks, True, timeout=1920)
+        self.send_blocks(blocks, True, timeout=2440)
         chain1_tip = i
 
         # now create alt chain of same length
@@ -1285,14 +1285,14 @@ class FullBlockTest(SyscoinTestFramework):
 
         # extend alt chain to trigger re-org
         block = self.next_block("alt" + str(chain1_tip + 1), version=4)
-        self.send_blocks([block], True, timeout=1920)
+        self.send_blocks([block], True, timeout=2440)
 
         # ... and re-org back to the first chain
         self.move_tip(chain1_tip)
         block = self.next_block(chain1_tip + 1, version=4)
         self.send_blocks([block], False, force_send=True)
         block = self.next_block(chain1_tip + 2, version=4)
-        self.send_blocks([block], True, timeout=1920)
+        self.send_blocks([block], True, timeout=2440)
 
         self.log.info("Reject a block with an invalid block header version")
         b_v1 = self.next_block('b_v1', version=1)
