40cdfe866290d7334909952a905732212ef38086
Alexander Block <ablock84@gmail.com>
2020-03-21 11:33:37 +0100

    Add peer id to "socket send error" logs (#3363)

--- src/net.cpp
+++ src/net.cpp
@@ -943,7 +943,7 @@ size_t CConnman::SocketSendData(CNode *pnode) const
                 int nErr = WSAGetLastError();
                 if (nErr != WSAEWOULDBLOCK && nErr != WSAEMSGSIZE && nErr != WSAEINTR && nErr != WSAEINPROGRESS)
                 {
-                    LogPrintf("socket send error %s\n", NetworkErrorString(nErr));
+                    LogPrintf("socket send error %s (peer=%d)\n", NetworkErrorString(nErr), pnode->GetId());
                     pnode->fDisconnect = true;
                 }
             }
