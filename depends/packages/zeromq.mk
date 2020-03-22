079df9609e6ed2a09949ebe5e98a179f61709801
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-03-22 20:26:06 +0200

    build: Drop ZeroMQ patch for Mingw-w64 < 4.0
    The Mingw-w64 5.0 (Ubuntu 18.04 Bionic) is used to build the Windows
    binaries now.

--- depends/packages/zeromq.mk
+++ depends/packages/zeromq.mk
@@ -3,7 +3,7 @@ $(package)_version=4.3.1
 $(package)_download_path=https://github.com/zeromq/libzmq/releases/download/v$($(package)_version)/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=bcbabe1e2c7d0eec4ed612e10b94b112dd5f06fcefa994a0c79a45d835cd21eb
-$(package)_patches=0001-fix-build-with-older-mingw64.patch 0002-disable-pthread_set_name_np.patch
+$(package)_patches=0002-disable-pthread_set_name_np.patch
 
 define $(package)_set_vars
   $(package)_config_opts=--without-docs --disable-shared --disable-curve --disable-curve-keygen --disable-perf
@@ -16,7 +16,6 @@ define $(package)_set_vars
 endef
 
 define $(package)_preprocess_cmds
-   patch -p1 < $($(package)_patch_dir)/0001-fix-build-with-older-mingw64.patch && \
    patch -p1 < $($(package)_patch_dir)/0002-disable-pthread_set_name_np.patch && \
    cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub config
 endef
