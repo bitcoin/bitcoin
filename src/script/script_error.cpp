41ff4992e57f8626019c0b2ab3d024db71e4c20f
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2020-03-23 17:26:36 +0100

    script: fix SCRIPT_ERR_SIG_PUSHONLY error string

--- src/script/script_error.cpp
+++ src/script/script_error.cpp
@@ -58,7 +58,7 @@ const char* ScriptErrorString(const ScriptError serror)
         case SCRIPT_ERR_MINIMALDATA:
             return "Data push larger than necessary";
         case SCRIPT_ERR_SIG_PUSHONLY:
-            return "Only non-push operators allowed in signatures";
+            return "Only push operators allowed in signatures";
         case SCRIPT_ERR_SIG_HIGH_S:
             return "Non-canonical signature: S value is unnecessarily high";
         case SCRIPT_ERR_SIG_NULLDUMMY:
