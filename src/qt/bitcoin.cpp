8e08d005989c6b5f7f05e0a1e0ba84f544a76d01
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-03-24 21:16:38 +0200

    qt: Use parent-child relation to manage lifetime of OptionsModel object
    Both BitcoinApplication and OptionsModel classes are derived from the
    QObject class, therefore a parent-child relation could be established to
    manage the lifetime of an OptionsModel object.
    This commit does not change behavior.

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -210,8 +210,6 @@ BitcoinApplication::~BitcoinApplication()
 
     delete window;
     window = nullptr;
-    delete optionsModel;
-    optionsModel = nullptr;
     delete platformStyle;
     platformStyle = nullptr;
 }
@@ -225,7 +223,7 @@ void BitcoinApplication::createPaymentServer()
 
 void BitcoinApplication::createOptionsModel(bool resetSettings)
 {
-    optionsModel = new OptionsModel(m_node, nullptr, resetSettings);
+    optionsModel = new OptionsModel(m_node, this, resetSettings);
 }
 
 void BitcoinApplication::createWindow(const NetworkStyle *networkStyle)
