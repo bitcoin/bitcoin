968aaae940b064f21eddee6bb461aa08f777544c
fanquake <fanquake@gmail.com>
2020-03-26 11:38:24 +0800

    tests: run test-security-check.py in CI

--- ci/test/00_setup_env_win64.sh
+++ ci/test/00_setup_env_win64.sh
@@ -10,5 +10,6 @@ export CONTAINER_NAME=ci_win64
 export HOST=x86_64-w64-mingw32
 export PACKAGES="python3 nsis g++-mingw-w64-x86-64 wine-binfmt wine64"
 export RUN_FUNCTIONAL_TESTS=false
+export RUN_SECURITY_TESTS="true"
 export GOAL="deploy"
 export BITCOIN_CONFIG="--enable-reduce-exports --disable-gui-tests"
