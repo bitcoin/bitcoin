1d05a9d80b1211b47af465ba6958b0ec5a8c33ab
Sjors Provoost <sjors@sprovoost.nl>
2020-03-26 11:20:15 +0100

    Move DEFAULT_MAX_RAW_TX_FEE_RATE to node/transaction.h
    So it can be used in the GUI.

--- src/node/transaction.h
+++ src/node/transaction.h
@@ -6,10 +6,18 @@
 #define BITCOIN_NODE_TRANSACTION_H
 
 #include <attributes.h>
+#include <policy/feerate.h>
 #include <primitives/transaction.h>
 #include <util/error.h>
 
 struct NodeContext;
+
+/** Maximum fee rate for sendrawtransaction and testmempoolaccept RPC calls.
+ * Also used by the GUI when broadcasting a completed PSBT.
+ * By default, a transaction with a fee rate higher than this will be rejected
+ * by these RPCs and the GUI. This can be overridden with the maxfeerate argument.
+ */
+static const CFeeRate DEFAULT_MAX_RAW_TX_FEE_RATE{COIN / 10};
 
 /**
  * Submit a transaction to the mempool and (optionally) relay it to all P2P peers.
--- src/rpc/rawtransaction.cpp
+++ src/rpc/rawtransaction.cpp
@@ -39,12 +39,6 @@
 
 #include <univalue.h>
 
-/** Maximum fee rate for sendrawtransaction and testmempoolaccept.
- * By default, a transaction with a fee rate higher than this will be rejected
- * by the RPCs. This can be overridden with the maxfeerate argument.
- */
-static const CFeeRate DEFAULT_MAX_RAW_TX_FEE_RATE{COIN / 10};
-
 static void TxToJSON(const CTransaction& tx, const uint256 hashBlock, UniValue& entry)
 {
     // Call into TxToUniv() in bitcoin-common to decode the transaction hex.
