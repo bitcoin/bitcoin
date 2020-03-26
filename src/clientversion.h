894b55368e24636fbfc9e6862cccd88908eff53f
artem-bv <abrazhnikov@aitheon.com>
2020-03-26 23:27:16 +0200

    bump version 0.14.0.1

--- src/clientversion.h
+++ src/clientversion.h
@@ -17,7 +17,7 @@
 #define CLIENT_VERSION_MAJOR 0
 #define CLIENT_VERSION_MINOR 14
 #define CLIENT_VERSION_REVISION 0
-#define CLIENT_VERSION_BUILD 0
+#define CLIENT_VERSION_BUILD 1
 
 /**
  * Copyright year (2009-this)
