7eacdc5167c8db94df84e206db85817bc64e4921
Jon Atack <jon@atack.com>
2020-03-27 17:21:14 +0100

    Shift coverage from getunconfirmedbalance to getbalances in wallet_abandonconflict tests

--- test/functional/wallet_abandonconflict.py
+++ test/functional/wallet_abandonconflict.py
@@ -110,8 +110,8 @@ class AbandonConflictTest(BitcoinTestFramework):
         assert_equal(newbalance, balance - signed3_change)
         # Unconfirmed received funds that are not in mempool, also shouldn't show
         # up in unconfirmed balance
-        unconfbalance = self.nodes[0].getunconfirmedbalance() + self.nodes[0].getbalance()
-        assert_equal(unconfbalance, newbalance)
+        balances = self.nodes[0].getbalances()['mine']
+        assert_equal(balances['untrusted_pending'] + balances['trusted'], newbalance)
         # Also shouldn't show up in listunspent
         assert not txABC2 in [utxo["txid"] for utxo in self.nodes[0].listunspent(0)]
         balance = newbalance
