aa885fe5ef1ba4c621c2adede8505e89eb875c80
SimeoW <swuthier@uccs.edu>
2020-03-28 01:55:16 -0600

    Fix wallet & mining

--- first_compile.sh
+++ first_compile.sh
@@ -1,3 +1,6 @@
+sudo add-apt-repository ppa:bitcoin/bitcoin
+sudo apt-get update
+
 # Bitcoin dependencies
 sudo apt-get install build-essential libtool autotools-dev automake pkg-config bsdmainutils python3
 # Build with self-compiled depends to re-enable wallet compatibility
@@ -6,6 +9,9 @@ sudo apt-get install libssl-dev libevent-dev libboost-system-dev libboost-filesy
 # GUI dependencies
 sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
 sudo apt-get install libqrencode-dev
+
+# Bitcoin wallet functionality
+sudo apt-get install libdb4.8-dev libdb4.8++-dev
 
 ./autogen.sh
 ./configure # --prefix=`pwd`/depends/x86_64-linux-gnu
