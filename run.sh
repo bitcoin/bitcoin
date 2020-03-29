a9d7d7638ae4f738abb7dc8cf683a35786f69858
Simeon <simeonwuthier@gmail.com>
2020-03-29 12:40:20 -0600

    Support sf_BitcoinVictim and sf_BitcoinAttacker

--- run.sh
+++ run.sh
@@ -9,6 +9,14 @@ if [ -d "/media/sf_Bitcoin/blocks/" ]
 then
 	echo "datadir = /media/sf_Bitcoin/"
     src/bitcoind -datadir=/media/sf_Bitcoin/ -debug=researcher # Virtual machine shared folder
+elif [ -d "/media/sf_BitcoinAttacker/blocks/" ]
+then
+	echo "datadir = /media/sf_BitcoinAttacker/"
+    src/bitcoind -datadir=/media/sf_BitcoinAttacker/ -debug=researcher
+elif [ -d "/media/sf_BitcoinVictim/blocks/" ]
+then
+	echo "datadir = /media/sf_BitcoinVictim/"
+    src/bitcoind -datadir=/media/sf_BitcoinVictim/ -debug=researcher
 elif [ -d "/media/sim/BITCOIN/blocks/" ]
 then
 	echo "datadir = /media/sim/BITCOIN/"
