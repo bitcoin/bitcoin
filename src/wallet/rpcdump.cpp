bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

Bugfix: RPC/Wallet: dumpwallet should include hdmasterkeyid metadata

src/wallet/rpcdump.cpp
@@ -753,6 +753,9 @@ UniValue dumpwallet(const JSONRPCRequest& request)
             }
             if (!meta.hdKeypath.empty()) {
                 file << " hdkeypath=" + meta.hdKeypath;
+                if (!(meta.hdMasterKeyID.IsNull() || (meta.hdKeypath == "m" && meta.hdMasterKeyID == keyid))) {
+                    file << " hdmasterkeyid=" + EncodeDestination(meta.hdMasterKeyID);
+                }
             }
             file << strprintf(" # addr=%s\n", strAddr);
         }

bitcoin/bitcoin

src/wallet/rpcdump.cpp
@@ -835,6 +835,9 @@ UniValue dumpwallet(const JSONRPCRequest& request)
             }
             if (meta.has_key_origin) {
                 file << " hdkeypath=" + WriteHDKeypath(meta.key_origin.path);
+                if (!(meta.hd_seed_id.IsNull() || (meta.hdKeypath == "s" && meta.hd_seed_id == keyid))) {
+                    file << " hdmasterkeyid=" + EncodeDestination(meta.hd_seed_id);
+                }
             }
             file << strprintf(" # addr=%s\n", strAddr);
         }