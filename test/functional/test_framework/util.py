4b70ee4f24b70165ae06d4a6101b3c3c92fed112
MarcoFalke <falke.marco@gmail.com>
2020-03-31 16:34:54 -0400

    Merge #18474: test: check that peer is connected when calling sync_*
    fac3716b09bb9ee121db629873d9694a95cae942 test: check that peer is connected when calling sync_* (MarcoFalke)
    
    Pull request description:
    
      Without a connection there is no way to sync, so we can fail early and don't have to wait for the timeout
    
    ACKs for top commit:
      jonatack:
        ACK fac3716b09bb9
    
    Tree-SHA512: 12f771473c23e152dae4bfb201fadb2c3530cf439de64fea07d048734614543080a5d05c9c36e6e398c6a69c8279f609d34706599571814172a11bcfbea4a3b9

--- test/functional/test_framework/util.py
+++ test/functional/test_framework/util.py
@@ -489,8 +489,11 @@ def sync_blocks(rpc_connections, *, wait=1, timeout=60):
         best_hash = [x.getbestblockhash() for x in rpc_connections]
         if best_hash.count(best_hash[0]) == len(rpc_connections):
             return
+        # Check that each peer has at least one connection
+        assert (all([len(x.getpeerinfo()) for x in rpc_connections]))
         time.sleep(wait)
     raise AssertionError("Block sync timed out:{}".format("".join("\n  {!r}".format(b) for b in best_hash)))
+
 
 def sync_mempools(rpc_connections, *, wait=1, timeout=60, flush_scheduler=True, wait_func=None):
     """
@@ -506,6 +509,8 @@ def sync_mempools(rpc_connections, *, wait=1, timeout=60, flush_scheduler=True, 
                 for r in rpc_connections:
                     r.syncwithvalidationinterfacequeue()
             return
+        # Check that each peer has at least one connection
+        assert (all([len(x.getpeerinfo()) for x in rpc_connections]))
         if wait_func is not None:
             wait_func()
         time.sleep(wait)
