639386a7a1efcdb63b4a4276314a5e77b0f8b124
MarcoFalke <falke.marco@gmail.com>
2020-03-31 15:39:08 -0400

    Merge #18472: test: Remove unsafe BOOST_TEST_MESSAGE
    fa3cc0bfc4c4fc13a384fc291403c9fd41082f18 test: Remove unsafe BOOST_TEST_MESSAGE (MarcoFalke)
    
    Pull request description:
    
      Fixes #17987
    
      Can be tested with
    
      ```
      ./src/test/test_bitcoin -l test_suite  -- DEBUG_LOG_OUT
    
    ACKs for top commit:
      fjahr:
        tested ACK fa3cc0bfc4c4fc13a384fc291403c9fd41082f18
      mzumsande:
        Tested ACK fa3cc0bfc4c4fc13a384fc291403c9fd41082f18
    
    Tree-SHA512: f63b110d77882cd7c0d7574ff6c9c948db8febb3400ecdac45164746b587b0fa223463041801271b3959267ddc1d9a4a67ba76939e242e7dd2f92a2834a400a0

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -11,12 +11,16 @@
 
 #include <test/util/setup_common.h>
 
-/** Redirect debug log to boost log */
+#include <iostream>
+
+/** Redirect debug log to unit_test.log files */
 const std::function<void(const std::string&)> G_TEST_LOG_FUN = [](const std::string& s) {
-    if (s.back() == '\n') {
-        // boost will insert the new line
-        BOOST_TEST_MESSAGE(s.substr(0, s.size() - 1));
-    } else {
-        BOOST_TEST_MESSAGE(s);
-    }
+    static const bool should_log{std::any_of(
+        &boost::unit_test::framework::master_test_suite().argv[1],
+        &boost::unit_test::framework::master_test_suite().argv[boost::unit_test::framework::master_test_suite().argc],
+        [](const char* arg) {
+            return std::string{"DEBUG_LOG_OUT"} == arg;
+        })};
+    if (!should_log) return;
+    std::cout << s;
 };
