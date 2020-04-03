a80768906f5d6bd600c76db6304ea471dc4107d0
fanquake <fanquake@gmail.com>
2020-04-03 14:58:51 +0800

    test: remove rapidcheck integration and tests
    Former-commit-id: bf4cf085a4b5f15eb74f7c4db0b758464b5342f5

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -7,8 +7,6 @@ qrencode_packages = qrencode
 qt_linux_packages:=qt expat libxcb xcb_proto libXau xproto freetype fontconfig
 qt_android_packages=qt
 
-rapidcheck_packages = rapidcheck
-
 qt_darwin_packages=qt
 qt_mingw32_packages=qt
 
