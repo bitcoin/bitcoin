e14641c34f5038e15f890eab1839c69bdddf8f16
MarcoFalke <falke.marco@gmail.com>
2020-04-06 00:21:33 +0800

    rpc: Avoid initialization-order-fiasco on static CRPCCommand tables
    Former-commit-id: afdabc36c077fabe05553e8d2ebaa06ef9296900

--- src/rpc/misc.cpp
+++ src/rpc/misc.cpp
@@ -719,6 +719,8 @@ static UniValue echo(const JSONRPCRequest& request)
     return request.params;
 }
 
+void RegisterMiscRPCCommands(CRPCTable &t)
+{
 // clang-format off
 static const CRPCCommand commands[] =
 { //  category              name                      actor (function)         argNames
@@ -740,8 +742,6 @@ static const CRPCCommand commands[] =
 };
 // clang-format on
 
-void RegisterMiscRPCCommands(CRPCTable &t)
-{
     for (unsigned int vcidx = 0; vcidx < ARRAYLEN(commands); vcidx++)
         t.appendCommand(commands[vcidx].name, &commands[vcidx]);
 }
