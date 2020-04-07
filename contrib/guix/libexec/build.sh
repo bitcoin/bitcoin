ba0b99bdd613ba7f17c6247ece3001e1b44759a3
Carl Dong <contact@carldong.me>
2020-04-07 18:42:01 -0400

    guix: Don't set MINGW_HAS_SECURE_API CFLAG in depends
    This is no longer needed after 3bef7c22 in the mingw-w64 git repository,
    which is first included in mingw-w64 v7.0.0.
    
    As of the previous bump to our Guix time machine, we now use mingw-w64
    v7.0.0.

--- contrib/guix/libexec/build.sh
+++ contrib/guix/libexec/build.sh
@@ -134,8 +134,7 @@ make -C depends --jobs="$MAX_JOBS" HOST="$HOST" \
                                    x86_64_linux_RANLIB=x86_64-linux-gnu-ranlib \
                                    x86_64_linux_NM=x86_64-linux-gnu-nm \
                                    x86_64_linux_STRIP=x86_64-linux-gnu-strip \
-                                   qt_config_opts_i686_linux='-platform linux-g++ -xplatform bitcoin-linux-g++' \
-                                   mingw32_CFLAGS='-DMINGW_HAS_SECURE_API=1 -pipe'
+                                   qt_config_opts_i686_linux='-platform linux-g++ -xplatform bitcoin-linux-g++'
 
 
 ###########################
