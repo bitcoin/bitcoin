24ead629051233c34eade4cf9c0aea23ba2cb937
Alexander Block <ablock84@gmail.com>
2020-04-07 06:43:13 +0200

    Only call SendMessages when ProcessMessages did some work or when some time passed
    Invoking SendMessages for hundreds of peers while we know that there is no
    work to do give a lot of overhead on the message handler thread.

--- src/net.cpp
+++ src/net.cpp
@@ -2436,11 +2436,20 @@ void CConnman::OpenMasternodeConnection(const CAddress &addrConnect, bool probe)
 
 void CConnman::ThreadMessageHandler()
 {
+    int64_t nLastForceSendMessages = 0;
+
     while (!flagInterruptMsgProc)
     {
         std::vector<CNode*> vNodesCopy = CopyNodeVector();
 
+        int64_t nNow = GetTimeMillis();
+
         bool fMoreWork = false;
+        bool fForceSendMessages = false;
+        if (nNow - nLastForceSendMessages >= 100) {
+            fForceSendMessages = true;
+            nLastForceSendMessages = nNow;
+        }
 
         for (CNode* pnode : vNodesCopy)
         {
@@ -2448,12 +2457,13 @@ void CConnman::ThreadMessageHandler()
                 continue;
 
             // Receive messages
-            bool fMoreNodeWork = m_msgproc->ProcessMessages(pnode, flagInterruptMsgProc);
+            bool fDidWork = false;
+            bool fMoreNodeWork = m_msgproc->ProcessMessages(pnode, flagInterruptMsgProc, fDidWork);
             fMoreWork |= (fMoreNodeWork && !pnode->fPauseSend);
             if (flagInterruptMsgProc)
                 return;
             // Send messages
-            {
+            if (fDidWork || fForceSendMessages) {
                 LOCK(pnode->cs_sendProcessing);
                 m_msgproc->SendMessages(pnode, flagInterruptMsgProc);
             }
