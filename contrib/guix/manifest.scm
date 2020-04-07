0b66d22da5f53640e22f05adf880782c613e6d0f
Carl Dong <contact@carldong.me>
2020-04-07 18:23:53 -0400

    guix: Use gcc-9 for mingw-w64 instead of 8
    The libtool unsorted 'find' determinism issue seemed to have been solved
    in gcc-9's git: d41cd173e23ebea7c758644d6ad6e0fde1c2e3a6 or SVN: r262451
    
    Furthermore, it seems that Ubuntu Focal 20.04 LTS is going to ship with
    gcc 9 and mingw-w64 7, which will match what we have now.
    
    -----
    
    A note on this:
    
    Careful observers will see that previously I stated that all released
    versions of gcc were bootstrapped with a libtool 2.2.7a, meaning that
    they all had the unsorted 'find' determinism issue first resolved in
    libtool 2.2.7b.
    
    However, I was mistaken, gcc's ltmain.sh CLAIMS it was generated by
    libtool 2.2.7a, but it was in fact edited manually. It seems that gcc
    maintains their own versions of ltmain.sh and libtool.m4, and only
    sometimes backports patches from upstream.
    
    Quite confusing.

--- contrib/guix/manifest.scm
+++ contrib/guix/manifest.scm
@@ -128,7 +128,7 @@ desirable for building Bitcoin Core release binaries."
          (pthreads-xlibc mingw-w64-x86_64-winpthreads)
          (pthreads-xgcc (make-gcc-with-pthreads
                          (cross-gcc target
-                                    #:xgcc (make-ssp-fixed-gcc gcc-8)
+                                    #:xgcc (make-ssp-fixed-gcc gcc-9)
                                     #:xbinutils xbinutils
                                     #:libc pthreads-xlibc))))
     ;; Define a meta-package that propagates the resulting XBINUTILS, XLIBC, and
