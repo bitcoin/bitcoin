f995e48712e86364b9c0bb41e7ad1894f8b5eb5b
MarcoFalke <falke.marco@gmail.com>
2020-04-10 10:10:23 -0400

    Merge #18454: net: Make addr relay mockable, add test
    fa1da3d4bfc0511a89f5b19d5a4d89e55ff7ccde test: Add basic addr relay test (MarcoFalke)
    fa1793c1c44a3f75a09f9c636467b8274c541bdd net: Pass connman const when relaying address (MarcoFalke)
    fa47a0b003f53708b6d5df1ed4e7f8a7c68aa3ac net: Make addr relay mockable (MarcoFalke)
    
    Pull request description:
    
      As usual:
    
      * Switch to std::chrono time to be type-safe and mockable
      * Add basic test that relies on mocktime to add code coverage
    
    ACKs for top commit:
      naumenkogs:
        utACK  fa1da3d
      promag:
        ACK fa1da3d4bfc0511a89f5b19d5a4d89e55ff7ccde (fabe56e44b6f683e24e37246a7a8851190947cb3 before https://github.com/bitcoin/bitcoin/pull/18454#issuecomment-607866453), fa5bf23d527a450e72c2bf13d013e5393b664ca3 was dropped since last review.
    
    Tree-SHA512: 0552bf8fcbe375baa3cab62acd8c23b2994efa47daff818ad1116d0ffaa0b9e520dc1bca2bbc68369b25584e85e54861fe6fd0968de4f503b95439c099df9bd7
    
    Former-commit-id: c472cd95312db0833da67231f5ae08244f8bfaeb

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -144,6 +144,7 @@ BASE_SCRIPTS = [
     'rpc_blockchain.py',
     'rpc_deprecated.py',
     'wallet_disable.py',
+    'p2p_addr_relay.py',
     'rpc_net.py',
     'wallet_keypool.py',
     'p2p_mempool.py',
