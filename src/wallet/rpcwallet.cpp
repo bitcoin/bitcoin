9721e0bd8d175ad86a6a6e3873d8b9d3f58267fb
João Barbosa <joao.paulo.barbosa@gmail.com>
2020-04-12 00:42:15 +0100

    wallet: Refactor WalletRescanReserver to use wallet reference
    Former-commit-id: 0daeceefceab075e7979591a4ce8528aaa7d5c7a

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -3600,7 +3600,7 @@ UniValue rescanblockchain(const JSONRPCRequest& request)
                 },
             }.Check(request);
 
-    WalletRescanReserver reserver(pwallet);
+    WalletRescanReserver reserver(*pwallet);
     if (!reserver.reserve()) {
         throw JSONRPCError(RPC_WALLET_ERROR, "Wallet is currently rescanning. Abort existing rescan or wait.");
     }
