e74a81124706d0ce4fe69df38e9b1a4162abb465
SimeoW <simeonwuthier@gmail.com>
2020-04-11 19:09:18 -0600

    More debugging

--- tools/Networking/eclipse.py
+++ tools/Networking/eclipse.py
@@ -189,7 +189,7 @@ def packet_received(packet):
 	# Relay Bitcoin packets that aren't from the victim
 	if packet[IP].src == victim_ip:
 		print(f'*** Message received ** addr={packet[IP].dst} ** cmd={msgtype}')
-		print(f'.{str(msgtype)}.{len(str(msgtype))}.{msgtype[:4]}')
+		print(f'.{str(msgtype)}.{len(msgtype[:4])}.{len(msgtype.strip())}')
 		print(msgtype is 'ping')
 		if msgtype == 'ping':
 			payload = packet[TCP].payload
