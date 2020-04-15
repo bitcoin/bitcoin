ea3871ebd769779d1c737556ee2ce9345926c63b
Pasta <pasta@dashboost.org>
2020-04-15 17:17:05 -0500

    comment out extern declaration in DoS_tests.cpp
    Signed-off-by: Pasta <pasta@dashboost.org>

--- src/test/DoS_tests.cpp
+++ src/test/DoS_tests.cpp
@@ -24,7 +24,8 @@
 extern bool AddOrphanTx(const CTransactionRef& tx, NodeId peer);
 extern void EraseOrphansFor(NodeId peer);
 extern unsigned int LimitOrphanTxSize(unsigned int nMaxOrphans);
-extern void Misbehaving(NodeId nodeid, int howmuch, const std::string& message="");
+// We don't need this, since we kept declaration in net_processing.h when backporting (#13417)
+// extern void Misbehaving(NodeId nodeid, int howmuch, const std::string& message="");
 
 struct COrphanTx {
     CTransactionRef tx;
