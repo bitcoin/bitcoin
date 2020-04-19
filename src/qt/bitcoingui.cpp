93aa640af8c7cdc944d07b2a0902f48b7115517d
10xcryptodev <63932478+10xcryptodev@users.noreply.github.com>
2020-04-19 08:05:37 -0300

    Fix #3248: use blue logo for Traditional theme (#3441)
    * #3248 use blue logo for Traditional theme
    
    * review tab space and blue logo name
    
    * change file order and spaces
    
    * Revert "review tab space and blue logo name"
    
    This reverts commit 31e0c3d0e90d79ae0f292642a473943e0c45e354.

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -246,7 +246,7 @@ BitcoinGUI::BitcoinGUI(const PlatformStyle *_platformStyle, const NetworkStyle *
 #ifndef Q_OS_MAC
     // Apply some styling to scrollbars
     QString theme = settings.value("theme", "").toString();
-    if (theme != "trad") { // No scrollbar styling for the traditional theme
+    if (theme != "Traditional") { // No scrollbar styling for the traditional theme
         QFile qFile(QString(":/css/scrollbars"));
         QString styleSheet;
         if (qFile.open(QFile::ReadOnly)) {
@@ -598,7 +598,13 @@ void BitcoinGUI::createToolBars()
         toolbar->addWidget(spacer);
 
         QLabel *logoLabel = new QLabel();
-        QPixmap logoPixmap(":/images/dash_logo_toolbar");
+        QString theme = settings.value("theme", "").toString();
+        QString logoImage = ":/images/dash_logo_toolbar";
+        if (theme == "Traditional") {
+            logoImage = ":/images/dash_logo_toolbar_blue";
+        }
+
+        QPixmap logoPixmap(logoImage);
         logoLabel->setPixmap(logoPixmap);
         toolbar->addWidget(logoLabel);
 
