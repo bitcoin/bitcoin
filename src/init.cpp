e50a6fd42a21725cb00832e76b2f1723911ae7d0
sidhujag <jsidhu@blockchainfoundry.co>
2020-04-19 19:51:57 -0700

    tests
    Former-commit-id: 1b4cc4df87760ce221ac41a51466ca78d387a0bb

--- src/init.cpp
+++ src/init.cpp
@@ -974,7 +974,7 @@ namespace { // Variables internal to initialization process only
 int nMaxConnections;
 int nUserMaxConnections;
 int nFD;
-ServiceFlags nLocalServices = ServiceFlags(NODE_NETWORK | NODE_NETWORK_LIMITED | NODE_WITNESS);
+ServiceFlags nLocalServices = ServiceFlags(NODE_NETWORK | NODE_NETWORK_LIMITED);
 int64_t peer_connect_timeout;
 std::set<BlockFilterType> g_enabled_filter_types;
 
@@ -1906,6 +1906,12 @@ bool AppInitMain(NodeContext& node)
                 chainstate->PruneAndFlush();
             }
         }
+    }
+
+    if (chainparams.GetConsensus().SegwitHeight != std::numeric_limits<int>::max()) {
+        // Advertise witness capabilities.
+        // The option to not set NODE_WITNESS is only used in the tests and should be removed.
+        nLocalServices = ServiceFlags(nLocalServices | NODE_WITNESS);
     }
 
     // ********************************************************* Step 11: import blocks
