2b7b9cb2c2b39d4ff82add57f0024cbe76d2f5b3
Simeon <simeonwuthier@gmail.com>
2020-04-21 13:59:31 -0600

    Update LogGetMsgInfo_AlternatingConnections.py

--- tools/LogGetMsgInfo_AlternatingConnections.py
+++ tools/LogGetMsgInfo_AlternatingConnections.py
@@ -512,7 +512,9 @@ def log(file, targetDateTime, count, maxConnections):
 		count += 1
 	except Exception as e:
 		print('\nLOGGER PAUSED. Hold on...')
-		print(e)
+		exc_type, exc_obj, exc_tb = sys.exc_info()
+		fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+		print(exc_type, fname, exc_tb.tb_lineno)
 
 
 	targetDateTime = targetDateTime + datetime.timedelta(seconds = numSecondsPerSample)
