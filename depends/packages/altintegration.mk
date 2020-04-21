8950cbfc0818998b442d13cab9a52f5010088c5a
Overcooked Panda <35861207+overcookedpanda@users.noreply.github.com>
2020-04-21 13:53:58 -0500

    update altintegration.mk to check for host and set mingw32 compiler a… (#53)
    * update altintegration.mk to check for host and set mingw32 compiler as cmake compiler

--- depends/packages/altintegration.mk
+++ depends/packages/altintegration.mk
@@ -4,9 +4,15 @@ $(package)_download_path=https://github.com/VeriBlock/alt-integration-cpp/archiv
 $(package)_file_name=$($(package)_version).tar.gz
 $(package)_sha256_hash=9c2288e48a47ecb1c9d94c36f3acabffb91718d9c366110c52785563e4d0a355
 
-define $(package)_config_cmds
-  cmake -DCMAKE_C_COMPILER=$(CC) -DCMAKE_CXX_COMPILER=$(CXX) -DCMAKE_INSTALL_PREFIX=$($(package)_staging_dir)$(host_prefix) -DCMAKE_BUILD_TYPE=Release -DTESTING=OFF -DWITH_ROCKSDB=OFF -DSHARED=OFF -B .
-endef
+ifeq ($(HOST), x86_64-w64-mingw32)
+  define $(package)_config_cmds
+    cmake -DCMAKE_C_COMPILER=$(HOST)-gcc -DCMAKE_CXX_COMPILER=$(HOST)-g++ -DCMAKE_INSTALL_PREFIX=$($(package)_staging_dir)$(host_prefix) -DCMAKE_BUILD_TYPE=Release -DTESTING=OFF -DWITH_ROCKSDB=OFF -DSHARED=OFF -B .
+  endef
+else
+  define $(package)_config_cmds
+    cmake -DCMAKE_INSTALL_PREFIX=$($(package)_staging_dir)$(host_prefix) -DCMAKE_BUILD_TYPE=Release -DTESTING=OFF -DWITH_ROCKSDB=OFF -DSHARED=OFF -B .
+  endef
+endif
 
 define $(package)_build_cmds
   $(MAKE)
