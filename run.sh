923bb42bae64a6c7d714cc30cfa105cfb4594001
Simeon <simeonwuthier@gmail.com>
2020-04-21 23:05:44 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
 #gnome-terminal -f ./use.sh -t "Custom Bitcoin Console"
 gnome-terminal -t "Custom Bitcoin Console" -- python3 bitcoin_console.py
-if [ ! -d src"" ]; then
-	cd ..
+if [ ! -d "src" ]; then
+	cd .. # If located in src or any other level of depth 1, return
 fi
 
 if [ -d "/media/sf_Bitcoin/blocks/" ]; then
 	echo "datadir = /media/sf_Bitcoin/"
 	if [ ! -f "/media/sf_Bitcoin/bitcoin.conf" ]; then
+		echo "Resetting configuration file"
 		echo "server = 1" > /media/sf_Bitcoin/bitcoin.conf
 		echo "rpcuser=cybersec" >> /media/sf_Bitcoin/bitcoin.conf
 		echo "rpcpassword=kZIdeN4HjZ3fp9Lge4iezt0eJrbjSi8kuSuOHeUkEUbQVdf09JZXAAGwF3R5R2qQkPgoLloW91yTFuufo7CYxM2VPT7A5lYeTrodcLWWzMMwIrOKu7ZNiwkrKOQ95KGW8kIuL1slRVFXoFpGsXXTIA55V3iUYLckn8rj8MZHBpmdGQjLxakotkj83ZlSRx1aOJ4BFxdvDNz0WHk1i2OPgXL4nsd56Ph991eKNbXVJHtzqCXUbtDELVf4shFJXame" >> /media/sf_Bitcoin/bitcoin.conf
@@ -16,6 +17,7 @@ if [ -d "/media/sf_Bitcoin/blocks/" ]; then
 elif [ -d "/media/sf_BitcoinAttacker/blocks/" ]; then
 	echo "datadir = /media/sf_BitcoinAttacker/"
 	if [ ! -f "/media/sf_BitcoinAttacker/bitcoin.conf" ]; then
+		echo "Resetting configuration file"
 		echo "server = 1" > /media/sf_BitcoinAttacker/bitcoin.conf
 		echo "rpcuser=cybersec" >> /media/sf_BitcoinAttacker/bitcoin.conf
 		echo "rpcpassword=kZIdeN4HjZ3fp9Lge4iezt0eJrbjSi8kuSuOHeUkEUbQVdf09JZXAAGwF3R5R2qQkPgoLloW91yTFuufo7CYxM2VPT7A5lYeTrodcLWWzMMwIrOKu7ZNiwkrKOQ95KGW8kIuL1slRVFXoFpGsXXTIA55V3iUYLckn8rj8MZHBpmdGQjLxakotkj83ZlSRx1aOJ4BFxdvDNz0WHk1i2OPgXL4nsd56Ph991eKNbXVJHtzqCXUbtDELVf4shFJXame" >> /media/sf_BitcoinAttacker/bitcoin.conf
@@ -25,6 +27,7 @@ elif [ -d "/media/sf_BitcoinAttacker/blocks/" ]; then
 elif [ -d "/media/sf_BitcoinVictim/blocks/" ]; then
 	echo "datadir = /media/sf_BitcoinVictim/"
 	if [ ! -f "/media/sf_BitcoinVictim/bitcoin.conf" ]; then
+		echo "Resetting configuration file"
 		echo "server = 1" > /media/sf_BitcoinVictim/bitcoin.conf
 		echo "rpcuser=cybersec" >> /media/sf_BitcoinVictim/bitcoin.conf
 		echo "rpcpassword=kZIdeN4HjZ3fp9Lge4iezt0eJrbjSi8kuSuOHeUkEUbQVdf09JZXAAGwF3R5R2qQkPgoLloW91yTFuufo7CYxM2VPT7A5lYeTrodcLWWzMMwIrOKu7ZNiwkrKOQ95KGW8kIuL1slRVFXoFpGsXXTIA55V3iUYLckn8rj8MZHBpmdGQjLxakotkj83ZlSRx1aOJ4BFxdvDNz0WHk1i2OPgXL4nsd56Ph991eKNbXVJHtzqCXUbtDELVf4shFJXame" >> /media/sf_BitcoinVictim/bitcoin.conf
@@ -34,6 +37,7 @@ elif [ -d "/media/sf_BitcoinVictim/blocks/" ]; then
 elif [ -d "/media/sim/BITCOIN/blocks/" ]; then
 	echo "datadir = /media/sim/BITCOIN/"
 	if [ ! -f "/media/sim/BITCOIN/bitcoin.conf" ]; then
+		echo "Resetting configuration file"
 		echo "server = 1" > /media/sim/BITCOIN/bitcoin.conf
 		echo "rpcuser=cybersec" >> /media/sim/BITCOIN/bitcoin.conf
 		echo "rpcpassword=kZIdeN4HjZ3fp9Lge4iezt0eJrbjSi8kuSuOHeUkEUbQVdf09JZXAAGwF3R5R2qQkPgoLloW91yTFuufo7CYxM2VPT7A5lYeTrodcLWWzMMwIrOKu7ZNiwkrKOQ95KGW8kIuL1slRVFXoFpGsXXTIA55V3iUYLckn8rj8MZHBpmdGQjLxakotkj83ZlSRx1aOJ4BFxdvDNz0WHk1i2OPgXL4nsd56Ph991eKNbXVJHtzqCXUbtDELVf4shFJXame" >> /media/sim/BITCOIN/bitcoin.conf
@@ -43,6 +47,7 @@ elif [ -d "/media/sim/BITCOIN/blocks/" ]; then
 else
 	echo "datadir = ~/.bitcoin/"
 	if [ ! -f "~/.bitcoin/bitcoin.conf" ]; then
+		echo "Resetting configuration file"
 		echo "server = 1" > ~/.bitcoin/bitcoin.conf
 		echo "rpcuser=cybersec" >> ~/.bitcoin/bitcoin.conf
 		echo "rpcpassword=kZIdeN4HjZ3fp9Lge4iezt0eJrbjSi8kuSuOHeUkEUbQVdf09JZXAAGwF3R5R2qQkPgoLloW91yTFuufo7CYxM2VPT7A5lYeTrodcLWWzMMwIrOKu7ZNiwkrKOQ95KGW8kIuL1slRVFXoFpGsXXTIA55V3iUYLckn8rj8MZHBpmdGQjLxakotkj83ZlSRx1aOJ4BFxdvDNz0WHk1i2OPgXL4nsd56Ph991eKNbXVJHtzqCXUbtDELVf4shFJXame" >> ~/.bitcoin/bitcoin.conf
