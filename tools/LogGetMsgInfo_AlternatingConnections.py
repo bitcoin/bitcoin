8886ef220f8ea6004d277387e5f35555cbfa8751
Simeon <simeonwuthier@gmail.com>
2020-04-21 20:37:53 -0600

    Update LogGetMsgInfo_AlternatingConnections.py

--- tools/LogGetMsgInfo_AlternatingConnections.py
+++ tools/LogGetMsgInfo_AlternatingConnections.py
@@ -33,7 +33,7 @@ def bitcoin(cmd):
 	return os.popen('./../src/bitcoin-cli -rpcuser=cybersec -rpcpassword=kZIdeN4HjZ3fp9Lge4iezt0eJrbjSi8kuSuOHeUkEUbQVdf09JZXAAGwF3R5R2qQkPgoLloW91yTFuufo7CYxM2VPT7A5lYeTrodcLWWzMMwIrOKu7ZNiwkrKOQ95KGW8kIuL1slRVFXoFpGsXXTIA55V3iUYLckn8rj8MZHBpmdGQjLxakotkj83ZlSRx1aOJ4BFxdvDNz0WHk1i2OPgXL4nsd56Ph991eKNbXVJHtzqCXUbtDELVf4shFJXame -rpcport=8332 ' + cmd).read()
 
 def startBitcoin():
-	subprocess.Popen(['gnome-terminal -t 'Custom Bitcoin Core Instance' -- .././run.sh'], shell=True)
+	subprocess.Popen(['gnome-terminal -t "Custom Bitcoin Core Instance" -- .././run.sh'], shell=True)
 
 def bitcoinUp():
 	return winexists('Custom Bitcoin Core Instance')
