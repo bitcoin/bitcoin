4ccc64b1e0c7855ddb05dc2d372fbae908d17433
Overcooked Panda <35861207+overcookedpanda@users.noreply.github.com>
2020-04-22 17:09:21 -0500

    update alt-integration package, fix msvc build, update ci (#62)
    * initial commit for windows ci
    
    * fix windows.yml
    
    * fix windows.yml
    
    * add windows to ci, add 'qt' to nightly linux build artifact name
    
    * temp remove upload artifacts
    
    * initial commit for mac os x
    
    * update macos tmp directory usage
    
    * fix vcpkg alt-integration install
    
    * create macos-test.yml for debugging
    
    * turn off werror
    
    * fix windows qt path
    
    * update qt version for windows
    
    * fix qt path
    
    * save vcpkg cache
    
    * updates for qt from bitcoin-core 0874a109da5463699199cf732b974635b97643dd
    
    * fix vcpkg integration
    
    * import msvs changes from bitcoin-core
    
    * fix regex
    
    * comment set variables
    
    * revert to qt Qt5.9.8, update other .vcxproj files
    
    * fix qt download hash, add vbk depends
    
    * update bitcoin.sln
    
    * rename libtest_util.vcxproj.in to libtest_util.vcxproj
    
    * fix bitcoin-qt.vcxproj
    
    * re-add libtest_util.vcxproj
    
    * temp remove un-needed workflows
    
    * remove qt test
    
    * test macos fix
    
    * test macos fix
    
    * remove bitcoin-bench
    
    * enable qt for macos
    
    * remove more tests from msvs build
    
    * add macos to ci, remove make deploy
    
    * update altintegration.mk add to nightly.yml
    
    * add macos artifacts
    
    * revert changes to bench_bitcoin.vcxproj.in, re-enable all ci

--- depends/packages/altintegration.mk
+++ depends/packages/altintegration.mk
@@ -1,8 +1,8 @@
 package=altintegration
-$(package)_version=56da423eb8c983131139a5f265fe185cfe6b083b
+$(package)_version=55945a121b9ca49becb980f8a7433b36585c3edd
 $(package)_download_path=https://github.com/VeriBlock/alt-integration-cpp/archive/
 $(package)_file_name=$($(package)_version).tar.gz
-$(package)_sha256_hash=9c2288e48a47ecb1c9d94c36f3acabffb91718d9c366110c52785563e4d0a355
+$(package)_sha256_hash=2eb173a47836c28c14a28efd8305b9c4364a3c26509229d2e125e0919322e5a7
 
 ifeq ($(HOST), x86_64-w64-mingw32)
   define $(package)_config_cmds
