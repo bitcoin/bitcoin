038cc2ec9a539766e5ae8f440dcba62067c7afce
sidhujag <jsidhu@blockchainfoundry.co>
2020-04-23 09:32:01 -0700

    rmv compressor into tx
    Former-commit-id: 6935277c85cdce727eaf8c53a822d71b69339b08

--- src/primitives/transaction.h
+++ src/primitives/transaction.h
@@ -316,16 +316,141 @@ inline void SerializeTransaction(const TxType& tx, Stream& s) {
     s << tx.nLockTime;
 }
 // SYSCOIN
+
+bool CompressScript(const CScript& script, std::vector<unsigned char> &out);
+unsigned int GetSpecialScriptSize(unsigned int nSize);
+bool DecompressScript(CScript& script, unsigned int nSize, const std::vector<unsigned char> &out);
+
+/**
+ * Compress amount.
+ *
+ * nAmount is of type uint64_t and thus cannot be negative. If you're passing in
+ * a CAmount (int64_t), make sure to properly handle the case where the amount
+ * is negative before calling CompressAmount(...).
+ *
+ * @pre Function defined only for 0 <= nAmount <= MAX_MONEY.
+ */
+uint64_t CompressAmount(uint64_t nAmount);
+
+uint64_t DecompressAmount(uint64_t nAmount);
+
+/** Compact serializer for scripts.
+ *
+ *  It detects common cases and encodes them much more efficiently.
+ *  3 special cases are defined:
+ *  * Pay to pubkey hash (encoded as 21 bytes)
+ *  * Pay to script hash (encoded as 21 bytes)
+ *  * Pay to pubkey starting with 0x02, 0x03 or 0x04 (encoded as 33 bytes)
+ *
+ *  Other scripts up to 121 bytes require 1 byte + script length. Above
+ *  that, scripts up to 16505 bytes require 2 bytes + script length.
+ */
+struct ScriptCompression
+{
+    /**
+     * make this static for now (there are only 6 special scripts defined)
+     * this can potentially be extended together with a new nVersion for
+     * transactions, in which case this value becomes dependent on nVersion
+     * and nHeight of the enclosing transaction.
+     */
+    static const unsigned int nSpecialScripts = 6;
+
+    template<typename Stream>
+    void Ser(Stream &s, const CScript& script) {
+        std::vector<unsigned char> compr;
+        if (CompressScript(script, compr)) {
+            s << MakeSpan(compr);
+            return;
+        }
+        unsigned int nSize = script.size() + nSpecialScripts;
+        s << VARINT(nSize);
+        s << MakeSpan(script);
+    }
+
+    template<typename Stream>
+    void Unser(Stream &s, CScript& script) {
+        unsigned int nSize = 0;
+        s >> VARINT(nSize);
+        if (nSize < nSpecialScripts) {
+            std::vector<unsigned char> vch(GetSpecialScriptSize(nSize), 0x00);
+            s >> MakeSpan(vch);
+            DecompressScript(script, nSize, vch);
+            return;
+        }
+        nSize -= nSpecialScripts;
+        if (nSize > MAX_SCRIPT_SIZE) {
+            // Overly long script, replace with a short invalid one
+            script << OP_RETURN;
+            s.ignore(nSize);
+        } else {
+            script.resize(nSize);
+            s >> MakeSpan(script);
+        }
+    }
+};
+
+struct AmountCompression
+{
+    template<typename Stream, typename I> void Ser(Stream& s, I val)
+    {
+        s << VARINT(CompressAmount(val));
+    }
+    template<typename Stream, typename I> void Unser(Stream& s, I& val)
+    {
+        uint64_t v;
+        s >> VARINT(v);
+        val = DecompressAmount(v);
+    }
+};
+
+struct AssetCoinInfoCompression
+{
+    template<typename Stream, typename I> void Ser(Stream& s, I val)
+    {
+        s << VARINT_MODE(val.nAsset, VarIntMode::NONNEGATIVE_SIGNED);
+        if(val.nAsset > 0) {
+            s << VARINT(CompressAmount(val.nValue));
+        }
+    }
+    template<typename Stream, typename I> void Unser(Stream& s, I& val)
+    {
+        s >> VARINT_MODE(val.nAsset, VarIntMode::NONNEGATIVE_SIGNED);
+        if(val.nAsset > 0) {
+            uint64_t v;
+            s >> VARINT(v);
+            val.nValue = DecompressAmount(v);
+        }
+    }
+};
+
+/** wrapper for CTxOut that provides a more compact serialization */
+struct TxOutCompression
+{
+    FORMATTER_METHODS(CTxOut, obj) { READWRITE(Using<AmountCompression>(obj.nValue), Using<ScriptCompression>(obj.scriptPubKey)); }
+};
+// SYSCOIN
+struct TxOutCoinCompression
+{
+    FORMATTER_METHODS(CTxOutCoin, obj) { READWRITE(Using<AmountCompression>(obj.nValue), Using<ScriptCompression>(obj.scriptPubKey), Using<AssetCoinInfoCompression>(obj.assetInfo)); }
+};
+
 class CAssetOut {
 public:
     uint32_t n;
     CAmount nValue;
 
-    template<typename Stream>
-    void Serialize(Stream &s) const;
 
     template<typename Stream>
-    void Unserialize(Stream &s);
+    inline void Serialize(Stream &s) const {
+        s << VARINT(n);
+        ::Serialize(s, Using<AmountCompression>(nValue));	
+    }
+
+    template<typename Stream>
+    inline void Unserialize(Stream &s) {
+        s >> VARINT(n);
+        ::Unserialize(s, Using<AmountCompression>(nValue));	
+    }
 
     CAssetOut(const uint32_t &nIn, const CAmount& nAmountIn): n(nIn), nValue(nAmountIn) {}
 	CAssetOut() {
@@ -531,10 +656,14 @@ public:
     assetOutputType voutAssets;
     
     template<typename Stream>
-    void Serialize(Stream &s) const;
+    inline void Serialize(Stream &s) const {
+        s << voutAssets;
+    }
 
     template<typename Stream>
-    void Unserialize(Stream &s);
+    inline void Unserialize(Stream &s)  {
+        s >> voutAssets;
+    }
 
 	CAssetAllocation() {
 		SetNull();
@@ -584,10 +713,34 @@ public:
     }
 
     template<typename Stream>
-    void Serialize(Stream &s) const;
+    inline void Serialize(Stream &s) const {
+        s << assetAllocation;
+        s << VARINT(nBridgeTransferID);
+        s << VARINT(nBlockNumber);
+        s << vchTxValue;
+        s << vchTxParentNodes;
+        s << vchTxRoot;
+        s << vchTxPath;   
+        s << vchReceiptValue;
+        s << vchReceiptParentNodes;
+        s << vchReceiptRoot;
+        s << vchReceiptPath;
+    }
 
     template<typename Stream>
-    void Unserialize(Stream &s);
+    inline void Unserialize(Stream &s) {
+        s >> assetAllocation;
+        s >> VARINT(nBridgeTransferID);
+        s >> VARINT(nBlockNumber);
+        s >> vchTxValue;
+        s >> vchTxParentNodes;
+        s >> vchTxRoot;
+        s >> vchTxPath;   
+        s >> vchReceiptValue;
+        s >> vchReceiptParentNodes;
+        s >> vchReceiptRoot;
+        s >> vchReceiptPath;
+    }
 
     inline void SetNull() { assetAllocation.SetNull(); vchTxRoot.clear(); vchTxValue.clear(); vchTxParentNodes.clear(); vchTxPath.clear(); vchReceiptRoot.clear(); vchReceiptValue.clear(); vchReceiptParentNodes.clear(); vchReceiptPath.clear(); nBridgeTransferID = 0; nBlockNumber = 0;  }
     inline bool IsNull() const { return (vchTxValue.empty() && vchReceiptValue.empty()); }
@@ -608,10 +761,16 @@ public:
     }
 
     template<typename Stream>
-    void Serialize(Stream &s) const;
+    inline void Serialize(Stream &s) const {
+        s << assetAllocation;
+        s << vchEthAddress;
+    }
 
     template<typename Stream>
-    void Unserialize(Stream &s);
+    inline void Unserialize(Stream &s) {
+        s >> assetAllocation;
+        s >> vchEthAddress;
+    }
 
     inline void SetNull() { assetAllocation.SetNull(); vchEthAddress.clear();  }
     inline bool IsNull() const { return (vchEthAddress.empty() && assetAllocation.IsNull()); }
--- src/test/compress_tests.cpp
+++ src/test/compress_tests.cpp
@@ -1,8 +1,8 @@
 // Copyright (c) 2012-2020 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-
-#include <compressor.h>
+// SYSCOIN
+#include <primitives/transaction.h>
 #include <script/standard.h>
 #include <test/util/setup_common.h>
 
