05060ee4b4935fd58c834ccee2db4fd5527584e4
sidhujag <jsidhu@blockchainfoundry.co>
2020-04-23 12:10:24 -0700

    inc dbwrapper
    Former-commit-id: 289e3cc4a5c9502e6ef40928db8310d86314c6fe

--- src/services/asset.cpp
+++ src/services/asset.cpp
@@ -6,6 +6,7 @@
 #include <services/asset.h>
 #include <services/assetconsensus.h>
 #include <consensus/validation.h>
+#include <consensus/dbwrapper.h>
 
 std::string stringFromSyscoinTx(const int &nVersion) {
     switch (nVersion) {
