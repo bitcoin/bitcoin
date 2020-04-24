fa7b885f51ff848d3f913bc6e15d24528300c210
MarcoFalke <falke.marco@gmail.com>
2020-04-24 11:23:40 -0400

    walletdb: Remove unsed boost/thread

--- src/wallet/walletdb.cpp
+++ src/wallet/walletdb.cpp
@@ -18,8 +18,6 @@
 #include <atomic>
 #include <string>
 
-#include <boost/thread.hpp>
-
 namespace DBKeys {
 const std::string ACENTRY{"acentry"};
 const std::string ACTIVEEXTERNALSPK{"activeexternalspk"};
@@ -738,11 +736,7 @@ DBErrors WalletBatch::LoadWallet(CWallet* pwallet)
                 pwallet->WalletLogPrintf("%s\n", strErr);
         }
         pcursor->close();
-    }
-    catch (const boost::thread_interrupted&) {
-        throw;
-    }
-    catch (...) {
+    } catch (...) {
         result = DBErrors::CORRUPT;
     }
 
