b9073c8f13fb0ba94c2ec6365666343e19fd9ddf
Andrew Chow <achow101-github@achow101.com>
2020-04-24 17:57:37 -0400

    rpc: createwallet warning that descriptor wallets are experimental

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -2720,6 +2720,7 @@ static UniValue createwallet(const JSONRPCRequest& request)
     }
     if (!request.params[5].isNull() && request.params[5].get_bool()) {
         flags |= WALLET_FLAG_DESCRIPTORS;
+        warnings.emplace_back(Untranslated("Wallet is an experimental descriptor wallet"));
     }
 
     bilingual_str error;
