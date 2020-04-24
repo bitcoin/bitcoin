9dd5335a8d8a1732acffb0f87568284aedc64720
Xagau <bullionxpress@gmail.com>
2020-04-24 14:32:57 -0400

    change Makefile.am, call rn.sh for renaming

---
+++ rn.sh
@@ -0,0 +1,20 @@
+cd ./release
+mv bitcoind vbitcoind
+mv bitcoin-cli vbitcoin-cli
+mv bitcoin-qt vbitcoin-qt
+mv bitcoin-tx vbitcoin-tx
+mv bitcoind.exe vbitcoind.exe
+mv bitcoin-cli.exe vbitcoin-cli.exe
+mv bitcoin-qt.exe vbitcoin-qt.exe
+mv bitcoin-tx.exe vbitcoin-tx.exe
+cd ..
+cd ./src
+mv bitcoind vbitcoind
+mv bitcoin-cli vbitcoin-cli
+mv qt/bitcoin-qt qt/vbitcoin-qt
+mv bitcoin-tx vbitcoin-tx
+mv bitcoind.exe vbitcoind.exe
+mv bitcoin-cli.exe vbitcoin-cli.exe
+mv qt/bitcoin-qt.exe qt/vbitcoin-qt.exe
+mv bitcoin-tx.exe vbitcoin-tx.exe
+
