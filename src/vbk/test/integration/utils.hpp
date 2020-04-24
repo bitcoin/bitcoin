1e63f6624665933841e57ec8ed84b8b1d1b19eda
Overcooked Panda <35861207+overcookedpanda@users.noreply.github.com>
2020-04-24 12:10:25 -0500

    Update licenses (#67)
    * update licenses

--- src/vbk/test/integration/utils.hpp
+++ src/vbk/test/integration/utils.hpp
@@ -1,3 +1,7 @@
+// Copyright (c) 2019-2020 Xenios SEZC
+// https://www.veriblock.org
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #ifndef VERIBLOCK_INTEGRATION_TEST_UTILS_H
 #define VERIBLOCK_INTEGRATION_TEST_UTILS_H
 
