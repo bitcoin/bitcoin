fac24dea00c54fe1e2dcc7625bb65ada8b97ac0c
MarcoFalke <falke.marco@gmail.com>
2020-04-26 21:03:27 -0400

    ci: Run functional tests on mac again

--- ci/test/00_setup_env_mac_host.sh
+++ ci/test/00_setup_env_mac_host.sh
@@ -8,8 +8,6 @@ export LC_ALL=C.UTF-8
 
 export HOST=x86_64-apple-darwin16
 export PIP_PACKAGES="zmq"
-export RUN_UNIT_TESTS=true
-export RUN_FUNCTIONAL_TESTS=false
 export GOAL="install"
 export BITCOIN_CONFIG="--enable-gui --enable-reduce-exports --enable-werror"
 # Run without depends
