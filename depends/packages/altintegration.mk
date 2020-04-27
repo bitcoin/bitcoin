4a4a38e5c13f706faf7156df122df3631d1b9341
Overcooked Panda <35861207+overcookedpanda@users.noreply.github.com>
2020-04-26 19:02:12 -0500

    Feature/alpine docker (#72)
    * initial commit for alpine base docker images
    
    * update default vbitcoin.conf docker image template
    
    * update cd
    
    * fix cd
    
    * update vbitcoin.conf template
    
    * edit cd
    
    * update alt-integration version, adjust templates
    
    * fix typo..

--- depends/packages/altintegration.mk
+++ depends/packages/altintegration.mk
@@ -1,8 +1,8 @@
 package=altintegration
-$(package)_version=55945a121b9ca49becb980f8a7433b36585c3edd
+$(package)_version=9c6676d424694480c18e9138ab05d550ed75e969
 $(package)_download_path=https://github.com/VeriBlock/alt-integration-cpp/archive/
 $(package)_file_name=$($(package)_version).tar.gz
-$(package)_sha256_hash=2eb173a47836c28c14a28efd8305b9c4364a3c26509229d2e125e0919322e5a7
+$(package)_sha256_hash=d9c5e9939ed085a89907f0b5131f106148096ad7698f4394e7d0a65396a8fa93
 
 ifeq ($(HOST), x86_64-w64-mingw32)
   define $(package)_config_cmds
