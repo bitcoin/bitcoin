985dee939e91c4a91a26c45ce34e0e09af4d5cea
SimeoW <swuthier@uccs.edu>
2020-04-28 01:42:57 -0600

    Fixes

--- first_compile.sh
+++ first_compile.sh
@@ -4,14 +4,17 @@ sudo apt-get update
 # Bitcoin dependencies
 sudo apt-get install build-essential libtool autotools-dev automake pkg-config bsdmainutils python3
 # Build with self-compiled depends to re-enable wallet compatibility
-sudo apt-get install libssl-dev libevent-dev libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-test-dev libboost-thread-dev
+sudo apt-get install libssl-dev libevent-dev libboost-all-dev
 
 # GUI dependencies
 sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
 sudo apt-get install libqrencode-dev
 
 # Bitcoin wallet functionality
-sudo apt-get install libdb4.8-dev libdb4.8++-dev
+sudo apt-get install libdb-dev libdb++-dev
+
+# Incoming connections
+sudo apt-get install libminiupnpc-dev
 
 ./autogen.sh
 ./configure --enable-upnp-default # --prefix=`pwd`/depends/x86_64-linux-gnu
