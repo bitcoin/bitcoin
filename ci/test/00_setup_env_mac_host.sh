fa72a751026d43e1d01ae30e26bcfd9b4cc0cf45
MarcoFalke <falke.marco@gmail.com>
2020-04-28 07:12:47 -0400

    ci: Document why tests can not be run on mac

--- ci/test/00_setup_env_mac_host.sh
+++ ci/test/00_setup_env_mac_host.sh
@@ -10,6 +10,7 @@ export HOST=x86_64-apple-darwin16
 export PIP_PACKAGES="zmq"
 export GOAL="install"
 export BITCOIN_CONFIG="--enable-gui --enable-reduce-exports --enable-werror"
+export TEST_RUNNER_EXTRA="wallet_disable"  # Only run wallet_disable as a smoke test, see https://github.com/bitcoin/bitcoin/pull/17240#issuecomment-546022121 why the other tests are disabled
 # Run without depends
 export NO_DEPENDS=1
 export OSX_SDK=""
