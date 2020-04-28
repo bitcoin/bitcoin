7497861b6a2c0e7e0877e449b5a8d61c1560d4f5
SimeoW <swuthier@uccs.edu>
2020-04-28 00:41:13 -0600

    Much smarter ./run command, supports gui

--- run.sh
+++ run.sh
@@ -1,58 +1,55 @@
-#gnome-terminal -f ./use.sh -t "Custom Bitcoin Console"
-gnome-terminal -t "Custom Bitcoin Console" -- python3 bitcoin_console.py
 if [ ! -d "src" ]; then
 	cd .. # If located in src or any other level of depth 1, return
 fi
 
+pruned="false"
 if [ -d "/media/sf_Bitcoin/blocks/" ]; then
-	echo "datadir = /media/sf_Bitcoin/"
-	if [ ! -f "/media/sf_Bitcoin/bitcoin.conf" ]; then
-		echo "Resetting configuration file"
-		echo "server = 1" > /media/sf_Bitcoin/bitcoin.conf
-		echo "rpcuser=cybersec" >> /media/sf_Bitcoin/bitcoin.conf
-		echo "rpcpassword=kZIdeN4HjZ3fp9Lge4iezt0eJrbjSi8kuSuOHeUkEUbQVdf09JZXAAGwF3R5R2qQkPgoLloW91yTFuufo7CYxM2VPT7A5lYeTrodcLWWzMMwIrOKu7ZNiwkrKOQ95KGW8kIuL1slRVFXoFpGsXXTIA55V3iUYLckn8rj8MZHBpmdGQjLxakotkj83ZlSRx1aOJ4BFxdvDNz0WHk1i2OPgXL4nsd56Ph991eKNbXVJHtzqCXUbtDELVf4shFJXame" >> /media/sf_Bitcoin/bitcoin.conf
-		echo "rpcport=8332" >> /media/sf_Bitcoin/bitcoin.conf
-	fi
-	src/bitcoind -datadir=/media/sf_Bitcoin/ -debug=researcher # Virtual machine shared folder
+	dir="/media/sf_Bitcoin"
 elif [ -d "/media/sf_BitcoinAttacker/blocks/" ]; then
-	echo "datadir = /media/sf_BitcoinAttacker/"
-	if [ ! -f "/media/sf_BitcoinAttacker/bitcoin.conf" ]; then
-		echo "Resetting configuration file"
-		echo "server = 1" > /media/sf_BitcoinAttacker/bitcoin.conf
-		echo "rpcuser=cybersec" >> /media/sf_BitcoinAttacker/bitcoin.conf
-		echo "rpcpassword=kZIdeN4HjZ3fp9Lge4iezt0eJrbjSi8kuSuOHeUkEUbQVdf09JZXAAGwF3R5R2qQkPgoLloW91yTFuufo7CYxM2VPT7A5lYeTrodcLWWzMMwIrOKu7ZNiwkrKOQ95KGW8kIuL1slRVFXoFpGsXXTIA55V3iUYLckn8rj8MZHBpmdGQjLxakotkj83ZlSRx1aOJ4BFxdvDNz0WHk1i2OPgXL4nsd56Ph991eKNbXVJHtzqCXUbtDELVf4shFJXame" >> /media/sf_BitcoinAttacker/bitcoin.conf
-		echo "rpcport=8332" >> /media/sf_BitcoinAttacker/bitcoin.conf
-	fi
-	src/bitcoind -datadir=/media/sf_BitcoinAttacker/ -debug=researcher
+	dir="/media/sf_BitcoinAttacker"
 elif [ -d "/media/sf_BitcoinVictim/blocks/" ]; then
-	echo "datadir = /media/sf_BitcoinVictim/"
-	if [ ! -f "/media/sf_BitcoinVictim/bitcoin.conf" ]; then
-		echo "Resetting configuration file"
-		echo "server = 1" > /media/sf_BitcoinVictim/bitcoin.conf
-		echo "rpcuser=cybersec" >> /media/sf_BitcoinVictim/bitcoin.conf
-		echo "rpcpassword=kZIdeN4HjZ3fp9Lge4iezt0eJrbjSi8kuSuOHeUkEUbQVdf09JZXAAGwF3R5R2qQkPgoLloW91yTFuufo7CYxM2VPT7A5lYeTrodcLWWzMMwIrOKu7ZNiwkrKOQ95KGW8kIuL1slRVFXoFpGsXXTIA55V3iUYLckn8rj8MZHBpmdGQjLxakotkj83ZlSRx1aOJ4BFxdvDNz0WHk1i2OPgXL4nsd56Ph991eKNbXVJHtzqCXUbtDELVf4shFJXame" >> /media/sf_BitcoinVictim/bitcoin.conf
-		echo "rpcport=8332" >> /media/sf_BitcoinVictim/bitcoin.conf
-	fi
-	src/bitcoind -datadir=/media/sf_BitcoinVictim/ -debug=researcher
+	dir="/media/sf_BitcoinVictim"
 elif [ -d "/media/sim/BITCOIN/blocks/" ]; then
-	echo "datadir = /media/sim/BITCOIN/"
-	if [ ! -f "/media/sim/BITCOIN/bitcoin.conf" ]; then
-		echo "Resetting configuration file"
-		echo "server = 1" > /media/sim/BITCOIN/bitcoin.conf
-		echo "rpcuser=cybersec" >> /media/sim/BITCOIN/bitcoin.conf
-		echo "rpcpassword=kZIdeN4HjZ3fp9Lge4iezt0eJrbjSi8kuSuOHeUkEUbQVdf09JZXAAGwF3R5R2qQkPgoLloW91yTFuufo7CYxM2VPT7A5lYeTrodcLWWzMMwIrOKu7ZNiwkrKOQ95KGW8kIuL1slRVFXoFpGsXXTIA55V3iUYLckn8rj8MZHBpmdGQjLxakotkj83ZlSRx1aOJ4BFxdvDNz0WHk1i2OPgXL4nsd56Ph991eKNbXVJHtzqCXUbtDELVf4shFJXame" >> /media/sim/BITCOIN/bitcoin.conf
-		echo "rpcport=8332" >> /media/sim/BITCOIN/bitcoin.conf
-	fi
-	src/bitcoind -datadir=/media/sim/BITCOIN/ -debug=researcher
+	dir="/media/sim/BITCOIN"
 else
-	echo "datadir = ~/.bitcoin/"
-	if [ ! -f "$HOME/.bitcoin/bitcoin.conf" ]; then
-		echo "Resetting configuration file"
-		echo "server = 1" > ~/.bitcoin/bitcoin.conf
-		echo "rpcuser=cybersec" >> ~/.bitcoin/bitcoin.conf
-		echo "rpcpassword=kZIdeN4HjZ3fp9Lge4iezt0eJrbjSi8kuSuOHeUkEUbQVdf09JZXAAGwF3R5R2qQkPgoLloW91yTFuufo7CYxM2VPT7A5lYeTrodcLWWzMMwIrOKu7ZNiwkrKOQ95KGW8kIuL1slRVFXoFpGsXXTIA55V3iUYLckn8rj8MZHBpmdGQjLxakotkj83ZlSRx1aOJ4BFxdvDNz0WHk1i2OPgXL4nsd56Ph991eKNbXVJHtzqCXUbtDELVf4shFJXame" >> ~/.bitcoin/bitcoin.conf
-		echo "rpcport=8332" >> ~/.bitcoin/bitcoin.conf
+	dir="$HOME/.bitcoin/"
+	pruned="true"
+fi
+
+echo "datadir = $dir"
+
+if [ ! -f "$dir/bitcoin.conf" ]; then
+	echo "Resetting configuration file"
+	echo "server = 1" > "$dir/bitcoin.conf"
+	echo "rpcuser=cybersec" >> "$dir/bitcoin.conf"
+	echo "rpcpassword=kZIdeN4HjZ3fp9Lge4iezt0eJrbjSi8kuSuOHeUkEUbQVdf09JZXAAGwF3R5R2qQkPgoLloW91yTFuufo7CYxM2VPT7A5lYeTrodcLWWzMMwIrOKu7ZNiwkrKOQ95KGW8kIuL1slRVFXoFpGsXXTIA55V3iUYLckn8rj8MZHBpmdGQjLxakotkj83ZlSRx1aOJ4BFxdvDNz0WHk1i2OPgXL4nsd56Ph991eKNbXVJHtzqCXUbtDELVf4shFJXame" >> "$dir/bitcoin.conf"
+	echo "rpcport=8332" >> "$dir/bitcoin.conf"
+fi
+
+if [ "$1" == "gui" ]; then
+	if [ "$pruned" == "true" ]; then
+		echo "Pruned mode activated, only keeping 550 block transactions"
+		echo
+		src/qt/bitcoin-qt -prune=550 -datadir="$dir" -debug=researcher
+	else
+		echo
+		src/qt/bitcoin-qt -datadir="$dir" -debug=researcher
 	fi
-	echo "Running without blockchain (keeping only 550 blocks)"
-	src/bitcoind -debug=researcher -prune=550
+else
+	if [ "$pruned" == "true" ]; then
+		echo "Pruned mode activated, only keeping 550 block transactions"
+		echo
+		# Only open the console if not already open
+		if ! wmctrl -l | grep -q "Custom Bitcoin Console"; then
+			gnome-terminal -t "Custom Bitcoin Console" -- python3 bitcoin_console.py
+		fi
+		src/bitcoind -prune=550 -datadir="$dir" -debug=researcher
+	else
+		echo
+		# Only open the console if not already open
+		if ! wmctrl -l | grep -q "Custom Bitcoin Console"; then
+			gnome-terminal -t "Custom Bitcoin Console" -- python3 bitcoin_console.py
+		fi
+		src/bitcoind -datadir="$dir" -debug=researcher
+	fi
 fi
