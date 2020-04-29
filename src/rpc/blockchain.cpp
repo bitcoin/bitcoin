fac0cf6e5513df1402068df113d496b4e03a4bdc
MarcoFalke <falke.marco@gmail.com>
2020-04-28 20:42:44 -0400

    rpc: Do not advertise dumptxoutset as a way to flush the chainstate

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -2242,8 +2242,7 @@ UniValue dumptxoutset(const JSONRPCRequest& request)
 {
     RPCHelpMan{
         "dumptxoutset",
-        "\nWrite the serialized UTXO set to disk.\n"
-        "Incidentally flushes the latest coinsdb (leveldb) to disk.\n",
+        "\nWrite the serialized UTXO set to disk.\n",
         {
             {"path",
                 RPCArg::Type::STR,
