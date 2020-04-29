8e32e1c41c995e832e643f605d35a7aa112837e6
Andrew Chow <achow101-github@achow101.com>
2020-04-29 17:07:52 -0400

    wallet: remove nWalletMaxVersion
    nWalletMaxVersion was used to allow an upgrade to a version only
    when the new feature was used. This makes sense for the old
    -upgradewallet startup option. But because upgradewallet is now a RPC,
    putting off the version bump like this does not make sense. Instead,
    immediately upgrading to the given version number makes sense.

--- src/wallet/scriptpubkeyman.h
+++ src/wallet/scriptpubkeyman.h
@@ -37,7 +37,7 @@ public:
     virtual bool IsWalletFlagSet(uint64_t) const = 0;
     virtual void UnsetBlankWalletFlag(WalletBatch&) = 0;
     virtual bool CanSupportFeature(enum WalletFeature) const = 0;
-    virtual void SetMinVersion(enum WalletFeature, WalletBatch* = nullptr, bool = false) = 0;
+    virtual void SetMinVersion(enum WalletFeature, WalletBatch* = nullptr) = 0;
     virtual const CKeyingMaterial& GetEncryptionKey() const = 0;
     virtual bool HasEncryptionKeys() const = 0;
     virtual bool IsLocked() const = 0;
