c887bea347a7f71965d5af60ddb61bd2e14f19c7
Wladimir J. van der Laan <laanwj@protonmail.com>
2020-04-30 11:16:39 +0200

    Merge #18591: Add C++17 build to Travis
    c31cbe7cfefc18123eb85ffb2ce509748435efde Add C++17 test to Travis (Pieter Wuille)
    7829685e27aae25efb32e07368175c8f664b2218 Add configure option for c++17 (Pieter Wuille)
    0fbde488b24f62b4bbbde216647941dcac65c81a Support conversion between Spans of compatible types (Pieter Wuille)
    7cbfebbf3df0d26f518811e0bfb7abf270c83e37 Update ax_cxx_compile_stdcxx.m4 (Pieter Wuille)
    
    Pull request description:
    
      This adds a `--enable-c++17` option to the configure script, fixes the only C++17 incompatibility (with a commit taken from #18468), and adds a Travis test for it.
    
      This is all off by default, and release builds remain C++11.
    
      It implements the first step of the plan in https://github.com/bitcoin/bitcoin/issues/16684.
    
    ACKs for top commit:
      elichai:
        tACK c31cbe7cfefc18123eb85ffb2ce509748435efde
      practicalswift:
        Tested ACK c31cbe7cfefc18123eb85ffb2ce509748435efde
      hebasto:
        ACK c31cbe7cfefc18123eb85ffb2ce509748435efde, tested on Linux Mint 19.3 both C++11 and C++17 modes. Compiled and passed tests locally.
    
    Tree-SHA512: a4b00776dbceef9c12abbb404c6bcd48f7916ce24c8c7a14116355f64e817578b7fcddbedd5ce435322319d1e4de43429b68553f4d96d970c308fe3e3e59b9d1
    
    Former-commit-id: c3bc34f50a97a289bdd8180ee033434f4e9c8032

---
+++ ci/test/00_setup_env_native_cxx17.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2020 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+export LC_ALL=C.UTF-8
+
+export CONTAINER_NAME=ci_native_cxx17
+export PACKAGES="python3-zmq libevent-dev bsdmainutils libboost-system-dev libboost-filesystem-dev libboost-test-dev libboost-thread-dev libdb5.3++-dev libminiupnpc-dev libzmq3-dev"
+export NO_DEPENDS=1
+export GOAL="install"
+export BITCOIN_CONFIG="--enable-zmq --with-incompatible-bdb --enable-c++17"
