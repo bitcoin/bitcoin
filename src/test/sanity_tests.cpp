1a6c26154b55e92f1ed1296e16841c2cbf79df40
Kittywhiskers Van Gogh <63189531+kittywhiskers@users.noreply.github.com>
2020-04-30 16:57:46 +0800

    merge bitcoin#18825: fix message for ECC_InitSanityCheck test

--- src/test/sanity_tests.cpp
+++ src/test/sanity_tests.cpp
@@ -14,7 +14,7 @@ BOOST_AUTO_TEST_CASE(basic_sanity)
 {
   BOOST_CHECK_MESSAGE(glibc_sanity_test() == true, "libc sanity test");
   BOOST_CHECK_MESSAGE(glibcxx_sanity_test() == true, "stdlib sanity test");
-  BOOST_CHECK_MESSAGE(ECC_InitSanityCheck() == true, "openssl ECC test");
+  BOOST_CHECK_MESSAGE(ECC_InitSanityCheck() == true, "secp256k1 sanity test");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
