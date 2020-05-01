c269e618cf53634d3cf270273ab1b0354dc3c119
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-05-02 01:07:34 +0300

    Drop unused GIT_COMMIT_DATE macro

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -24,11 +24,7 @@ const std::string CLIENT_NAME("Satoshi");
 //   - "// No build information available", if proper git information is not available
 #endif
 
-//! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. $Format:%n#define GIT_ARCHIVE 1$
-#ifdef GIT_ARCHIVE
-#define GIT_COMMIT_ID "$Format:%H$"
-#define GIT_COMMIT_DATE "$Format:%cD$"
-#endif
+//! git will put "#define GIT_COMMIT_ID ..." on the next line inside archives. $Format:%n#define GIT_COMMIT_ID "%H"$
 
 #ifdef BUILD_GIT_TAG
     #define BUILD_DESC BUILD_GIT_TAG
