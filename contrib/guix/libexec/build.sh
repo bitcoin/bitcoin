827b1b61c97ecc6d058fdbd23fb403be54e20f11
Luke Dashjr <luke-jr+git@utopios.org>
2020-05-06 13:37:02 +0000

    build: Abstract release tarball generation to a utility script make_release_tarball

--- contrib/guix/libexec/build.sh
+++ contrib/guix/libexec/build.sh
@@ -158,7 +158,7 @@ GIT_ARCHIVE="${OUTDIR}/src/${DISTNAME}.tar.gz"
 # Create the source tarball if not already there
 if [ ! -e "$GIT_ARCHIVE" ]; then
     mkdir -p "$(dirname "$GIT_ARCHIVE")"
-    git archive --prefix="${DISTNAME}/" --output="$GIT_ARCHIVE" HEAD
+    contrib/gitian-descriptors/make_release_tarball "${GIT_ARCHIVE}"
 fi
 
 ###########################
