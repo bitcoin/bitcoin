2653c8721fa228f16c9f93eea240ddedd12ab44d
Harris <brakmic@gmail.com>
2020-05-09 11:09:52 +0200

    fuzz: use std::optional for sep_pos variable
    Co-authored-by: MarcoFalke <falke.marco@gmail.com>
    
    Former-commit-id: faa0bf6a25124e702df4926c0e2ff52d33543a26

--- src/test/fuzz/asmap_direct.cpp
+++ src/test/fuzz/asmap_direct.cpp
@@ -6,6 +6,7 @@
 #include <test/fuzz/fuzz.h>
 
 #include <cstdint>
+#include <optional>
 #include <vector>
 
 #include <assert.h>
@@ -13,20 +14,19 @@
 void test_one_input(const std::vector<uint8_t>& buffer)
 {
     // Encoding: [asmap using 1 bit / byte] 0xFF [addr using 1 bit / byte]
-    bool have_sep = false;
-    size_t sep_pos;
+    std::optional<size_t> sep_pos_opt;
     for (size_t pos = 0; pos < buffer.size(); ++pos) {
         uint8_t x = buffer[pos];
         if ((x & 0xFE) == 0) continue;
         if (x == 0xFF) {
-            if (have_sep) return;
-            have_sep = true;
-            sep_pos = pos;
+            if (sep_pos_opt) return;
+            sep_pos_opt = pos;
         } else {
             return;
         }
     }
-    if (!have_sep) return; // Needs exactly 1 separator
+    if (!sep_pos_opt) return; // Needs exactly 1 separator
+    const size_t sep_pos{sep_pos_opt.value()};
     if (buffer.size() - sep_pos - 1 > 128) return; // At most 128 bits in IP address
 
     // Checks on asmap
