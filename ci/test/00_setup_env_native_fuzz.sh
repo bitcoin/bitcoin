420fa0770f37619bfa29898d59dac45b6a477abb
Harris <brakmic@gmail.com>
2020-05-09 11:09:52 +0200

    fuzz: use std::optional for sep_pos variable
    Co-authored-by: MarcoFalke <falke.marco@gmail.com>

--- ci/test/00_setup_env_native_fuzz.sh
+++ ci/test/00_setup_env_native_fuzz.sh
@@ -14,4 +14,4 @@ export RUN_UNIT_TESTS=false
 export RUN_FUNCTIONAL_TESTS=false
 export RUN_FUZZ_TESTS=true
 export GOAL="install"
-export BITCOIN_CONFIG="--enable-fuzz --with-sanitizers=fuzzer,address,undefined CC=clang CXX=clang++"
+export BITCOIN_CONFIG="--enable-fuzz --with-sanitizers=fuzzer,address,undefined --enable-c++17 CC=clang CXX=clang++"
