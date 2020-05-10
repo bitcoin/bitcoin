d4dd65ee24c1d76a0fe7c9c6dd122f2b5457a0c5
Simeon <simeonwuthier@gmail.com>
2020-05-09 18:25:07 -0600

    Update net.cpp

--- src/rpc/net.cpp
+++ src/rpc/net.cpp
@@ -1246,7 +1246,7 @@ static UniValue forcerealfake(const JSONRPCRequest& request)
 
     for (std::vector<CNodeStats>::reverse_iterator i = vstats.rbegin();
     i != vstats.rend(); ++i ) {
-        CNodeStats& stats = vstats[i];
+        CNodeStats& stats = vstats.at(i);
     //for (const CNodeStats& stats : vstats) {
         CNodeStateStats statestats;
         bool fStateStats = GetNodeStateStats(stats.nodeid, statestats);
