1b394c58e12823b9b1de45e2118ad84f580b98cb
fanquake <fanquake@gmail.com>
2020-05-12 08:34:46 +0800

    Merge #18808: [net processing] Drop unknown types in getdata
    9847e205bf7edcac4c30ce4b6d62f482aa7bc1b7 [docs] Improve commenting in ProcessGetData() (John Newbery)
    2f032556e08a04807c71eb02104ca9589eaadf1b [test] test that an invalid GETDATA doesn't prevent processing of future messages (Amiti Uttarwar)
    e257cf71c851e25e1a533bf1d4296f6b55c81332 [net processing] ignore unknown INV types in GETDATA messages (Amiti Uttarwar)
    047ceac142246b5d51056a51dbf4645b31802be4 [net processing] ignore tx GETDATA from blocks-only peers (Amiti Uttarwar)
    
    Pull request description:
    
      Currently we'll stall peers that send us an unknown INV type in a GETDATA message. Be a bit more friendly and just drop the invalid request.
    
      Ditto for blocks-relay-only peers that send us a GETDATA for a transaction.
    
      There's a test for the first part. The second is difficult to test in the functional test framework since we aren't able to make blocks-relay-only connections.
    
    ACKs for top commit:
      sipa:
        utACK 9847e205bf7edcac4c30ce4b6d62f482aa7bc1b7
      brakmic:
        ACK https://github.com/bitcoin/bitcoin/commit/9847e205bf7edcac4c30ce4b6d62f482aa7bc1b7
      luke-jr:
        utACK 9847e205bf7edcac4c30ce4b6d62f482aa7bc1b7
      naumenkogs:
        utACK 9847e20
      ajtowns:
        utACK 9847e205bf7edcac4c30ce4b6d62f482aa7bc1b7
    
    Tree-SHA512: 6007f2fd839ffe737727f6fb8e8f083b2d9e05a510748f1d40b8f9be8fdf7b5419a36d8f1039923eec1ba2983e8f6f0436ec5fc196d9f6dcb0657f2ff8ff8e4c
    
    Former-commit-id: ceac7691b22d986e1ed8e56fbe62ab1bb4df9878

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -157,6 +157,7 @@ BASE_SCRIPTS = [
     'rpc_deprecated.py',
     'wallet_disable.py',
     'p2p_addr_relay.py',
+    'p2p_getdata.py',
     'rpc_net.py',
     'wallet_keypool.py',
     'wallet_keypool.py --descriptors',
