ebdea790d4e51a9c1513e4e9469603722e208003
Antoine Riard <ariard@student.42.fr>
2020-05-13 19:10:05 -0400

    [alt_processing] Support GETHEADERS processing
    At GETHEADERS reception if peer capabilities support sending we
    reply back with headers.

--- src/alt_processing.cpp
+++ src/alt_processing.cpp
@@ -4,6 +4,9 @@
 
 #include <alt_processing.h>
 
+#include <primitives/block.h>
+#include <protocol.h>
+
 AltLogicValidation::AltLogicValidation(CAltstack* altstack)
     : m_altstack(altstack)
 {}
@@ -16,6 +19,36 @@ CAltNodeState *AltLogicValidation::State(uint32_t node_id) {
 }
 
 bool AltLogicValidation::ProcessMessage(CAltMsg& msg) {
+    if (msg.m_command == NetMsgType::GETHEADERS) {
+
+        LOCK(cs_vNodeState);
+        CAltNodeState *node = State(msg.m_node_id);
+        // Peer ask for a reply, check receiving capability
+        if (!node->caps.fReceiving)
+            return false;
+
+        CBlockLocator locator;
+        uint256 hashStop;
+        msg.m_recv >> locator >> hashStop;
+
+        //TODO: implement checks
+        const CBlockIndex* pindex = nullptr;
+        pindex = FindForkInGlobalIndex(::ChainActive(), locator);
+        if (pindex)
+            pindex = ::ChainActive().Next(pindex);
+
+        std::vector<CBlock> vHeaders;
+        for (; pindex; pindex = ::ChainActive().Next(pindex))
+        {
+            vHeaders.push_back(pindex->GetBlockHeader());
+            if (pindex->GetBlockHash() == hashStop)
+                break;
+        }
+        const CNetMsgMaker msgMaker(209);
+        if (vHeaders.size() > 0)
+            m_altstack->PushMessage(node->driver_id, msgMaker.Make(NetMsgType::HEADERS, vHeaders));
+        return true;
+    }
     return true;
 }
 
