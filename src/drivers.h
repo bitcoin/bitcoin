d599505e210816d9f8a644570e0e2ed0c7f0444e
Antoine Riard <ariard@student.42.fr>
2020-05-12 20:09:48 -0400

    [altnet] Add CDriver class
    CDriver is an abstract interface which must be implemented by each
    transport protocol driver to interface with CAltstack.
    
    It contains basic methods to separate warmup, preprocessing, flusing
    and receiving.
    
    This is used in next commit.

---
+++ src/drivers.h
@@ -0,0 +1,61 @@
+// Copyright (c) 2020 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_DRIVERS_H
+#define BITCOIN_DRIVERS_H
+
+#include <altnet.h>
+
+#include <net.h>
+
+#include <string>
+#include <vector>
+
+class CAltMsg;
+
+/**
+ * Implement a transport-specific driver to support natively its
+ * processing.
+ *
+ * Each CDriver implementation must maintain its protocol specific
+ * connnection state, control logic and peer management.
+ *
+ */
+class CDriver {
+private:
+    uint32_t m_driver_id; // used to link peer back to its hosting driver
+public:
+    /* Warmup a driver until it's ready to service connection */
+    virtual bool Warmup() = 0;
+
+    /* Flush a batch of messages pending on all peers hosted on this
+     * device
+     */
+    virtual bool Flush() = 0;
+
+    /* Receive messages for all peers hosted on this device */
+    //TODO: std::vector<std::tuple(node_id, msgs)>> ?
+    virtual bool Receive(CAltMsg& msg) = 0;
+
+    /* Establish incoming connections */
+    virtual bool Listen(uint32_t potential_node_id) = 0;
+
+    /* Send a message to given node_id, driver MUST not flush yet */
+    virtual bool Send(CSerializedNetMsg msg) = 0;
+
+    /* Return driver capabilities */
+    virtual TransportCapabilities GetCapabilities() = 0;
+
+    /* Set driver identifier. */
+    void SetId(uint32_t driver_id) {
+        m_driver_id = driver_id;
+    }
+
+    /* Return driver identifier */
+    uint32_t GetId() {
+        return m_driver_id;
+    }
+};
+
+#endif
