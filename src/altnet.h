748518006615972565ee03900dbe5dfb0aa29292
Antoine Riard <ariard@student.42.fr>
2020-05-13 03:53:58 -0400

    [altnet] Add CAltMsg class
    This new class helps to communicate identifier payload between low-level
    alternative stack drivers and message processing logic.
    
    Add vRecvMsg to CAltstack to serve as a buffer between drivers processing
    and messages processing.

--- src/altnet.h
+++ src/altnet.h
@@ -7,6 +7,20 @@
 #define BITCOIN_ALTNET_H
 
 #include <threadinterrupt.h>
+#include <streams.h>
+
+/* Data payload and its origin node */
+class CAltMsg {
+public:
+    uint32_t m_node_id;
+    CDataStream m_recv; // received message data
+
+    std::string m_command;
+
+    CAltMsg() : m_node_id(0), m_recv(0, 0) {
+        m_recv.clear();
+    }
+};
 
 class CAltstack
 {
@@ -14,6 +28,9 @@ private:
     uint32_t m_node_id;
     CThreadInterrupt interruptNet;
     std::atomic<bool> flagInterruptAltProc{false};
+
+    RecursiveMutex cs_vRecvMsg;
+    std::vector<CAltMsg> vRecvMsg;
 public:
     CAltstack();
     ~CAltstack();
