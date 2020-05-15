d70ea16e2ce258869388fe5b758fa511310214b0
Andrew Chow <achow101-github@achow101.com>
2020-05-14 21:17:01 -0400

    walletdb: Handle cursor internally
    Instead of returning a Dbc (BDB cursor object) and having the caller
    deal with the cursor, make BerkeleyBatch handle the cursor internally.
    
    This prepares BerkeleyBatch to work with other database systems as Dbc
    objects are BDB specific.
    
    
    Former-commit-id: 0f3abd329950011f19f99606623bdd64cdaf33c7

--- src/wallet/bdb.h
+++ src/wallet/bdb.h
@@ -198,6 +198,7 @@ protected:
     Db* pdb;
     std::string strFile;
     DbTxn* activeTxn;
+    Dbc* m_cursor;
     bool fReadOnly;
     bool fFlushOnClose;
     BerkeleyEnvironment *env;
@@ -284,8 +285,9 @@ public:
         return HasKey(ssKey);
     }
 
-    Dbc* GetCursor();
-    int ReadAtCursor(Dbc* pcursor, CDataStream& ssKey, CDataStream& ssValue);
+    bool StartCursor();
+    bool ReadAtCursor(CDataStream& ssKey, CDataStream& ssValue, bool& complete);
+    void CloseCursor();
     bool TxnBegin();
     bool TxnCommit();
     bool TxnAbort();
