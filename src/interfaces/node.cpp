b1af8d0f432f27106ba425c23434d8273c057942
Jonas Schnelli <dev@jonasschnelli.ch>
2020-05-19 15:13:17 +0200

    Add BlockAndHeaderTipInfo to the node interface/appInit
    Former-commit-id: 3d3922f214d96d3393e5f1b012cb2602c82b8fc6

--- src/interfaces/node.cpp
+++ src/interfaces/node.cpp
@@ -81,10 +81,10 @@ public:
         return AppInitBasicSetup(argv) && AppInitParameterInteraction() && AppInitSanityChecks() &&
                AppInitLockDataDirectory();
     }
-    bool appInitMain() override
+    bool appInitMain(interfaces::BlockAndHeaderTipInfo* tip_info) override
     {
         m_context->chain = MakeChain(*m_context);
-        return AppInitMain(m_context_ref, *m_context);
+        return AppInitMain(m_context_ref, *m_context, tip_info);
     }
     void appShutdown() override
     {
