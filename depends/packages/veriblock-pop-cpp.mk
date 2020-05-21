243210c0c357e1b4c7d17de4f750ad67c4259603
Bohdan <warchantua@gmail.com>
2020-05-21 21:42:44 +0300

    Bump alt integration (#111)

--- depends/packages/veriblock-pop-cpp.mk
+++ depends/packages/veriblock-pop-cpp.mk
@@ -1,8 +1,8 @@
 package=veriblock-pop-cpp
-$(package)_version=5101607e2052b2b35ef57af926430f44191c7472
+$(package)_version=d8964ec757a40a2c8b49658d59aa88f00303e7ca
 $(package)_download_path=https://github.com/VeriBlock/alt-integration-cpp/archive/
 $(package)_file_name=$($(package)_version).tar.gz
-$(package)_sha256_hash=b40ed1d5d35df9bf89d9a155c2078f1cec7c99eed60be3548603afc41cac80cc
+$(package)_sha256_hash=d35113b6ca55a2c9be5bc8c621135b0eff0427cabe03ee5fcd8c1d3b94409694
 $(package)_build_subdir=build
 $(package)_build_type=$(BUILD_TYPE)
 $(package)_asan=$(ASAN)
