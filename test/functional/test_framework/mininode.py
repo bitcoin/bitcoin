999922baed3a80b581ce46daa01c4cbca4fcbfd8
MarcoFalke <falke.marco@gmail.com>
2020-05-23 09:47:55 -0400

    test: Default mininode.wait_until timeout to 60s

--- test/functional/test_framework/mininode.py
+++ test/functional/test_framework/mininode.py
@@ -371,7 +371,7 @@ class P2PInterface(P2PConnection):
 
     # Connection helper methods
 
-    def wait_until(self, test_function, timeout):
+    def wait_until(self, test_function, timeout=60):
         wait_until(test_function, timeout=timeout, lock=mininode_lock, timeout_factor=self.timeout_factor)
 
     def wait_for_disconnect(self, timeout=60):
