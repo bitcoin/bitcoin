2cd788d41bf58edb39f6e949c7dbcf8518637177
Cory Fields <cory-nospam-@coryfields.com>
2020-05-26 13:43:54 -0400

    gui: change combiner for signals to optional_last_value
    optional_last_value, which does not throw, has replaced optional_value as
    boost's default combiner. Besides being better supported, it also doesn't
    trigger gcc's -Wmaybe-unitialized warning, presumably because exceptions no
    longer bubble-up out of signals:
    
    ```bash
    boost/signals2/last_value.hpp:54:36: warning: '*((void*)& value +1)' may be used uninitialized in this function [-Wmaybe-uninitialized]
    	if(value) return value.get();
    ```
    
    The change in default happened in Boost 1.39.0 (along with the
    introduction of the signals 2 library. More information is available here:
    
    https://www.boost.org/doc/libs/1_73_0/doc/html/signals2/rationale.html#id-1.3.36.9.4
    
    and here:
    
    https://www.boost.org/doc/libs/1_73_0/doc/html/boost/signals2/optional_last_value.html
    
    Co-authored-by: fanquake <fanquake@gmail.com>
    
    Former-commit-id: 37275ac0b03f43adf2a75b3f7c888ee10ed7a0f7

--- test/lint/lint-includes.sh
+++ test/lint/lint-includes.sh
@@ -64,7 +64,7 @@ EXPECTED_BOOST_INCLUDES=(
     boost/preprocessor/cat.hpp
     boost/preprocessor/stringize.hpp
     boost/signals2/connection.hpp
-    boost/signals2/last_value.hpp
+    boost/signals2/optional_last_value.hpp
     boost/signals2/signal.hpp
     boost/test/unit_test.hpp
     boost/thread/condition_variable.hpp
