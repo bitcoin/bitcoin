93825352a36456283bf87e39b5888363ee242f21
Andrew Chow <achow101-github@achow101.com>
2020-05-26 20:53:32 -0400

    Implement SQLiteDatabase::Close

--- src/wallet/sqlite.cpp
+++ src/wallet/sqlite.cpp
@@ -161,6 +161,11 @@ bool SQLiteDatabase::Backup(const std::string& dest) const
 
 void SQLiteDatabase::Close()
 {
+    int res = sqlite3_close(m_db);
+    if (res != SQLITE_OK) {
+        throw std::runtime_error(strprintf("SQLiteDatabase: Failed to close database: %s\n", sqlite3_errstr(res)));
+    }
+    m_db = nullptr;
 }
 
 std::unique_ptr<DatabaseBatch> SQLiteDatabase::MakeBatch(bool flush_on_close)
