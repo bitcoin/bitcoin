e46553fc2b70444bd589e83368fefb24e4d302de
Jon Atack <jon@atack.com>
2020-05-28 17:52:01 +0200

    cli: add -getinfo multiwallet total balance
    Github-Pull: #19092
    Rebased-From: 8e6c8d69f9f9ee1b8f118ae325c5c1575575ba17

--- src/bitcoin-cli.cpp
+++ src/bitcoin-cli.cpp
@@ -870,8 +870,9 @@ static UniValue ValueFromAmount(const CAmount& amount)
 }
 
 /**
- * GetWalletBalances calls listwallets; if more than one wallet is loaded, it then
- * fetches mine.trusted balances for each loaded wallet and pushes them to `result`.
+ * GetWalletBalances calls listwallets; if more than one wallet is loaded, it
+ * then fetches mine.trusted balances for each loaded wallet and pushes all the
+ * balances, followed by the total balance, to `result`.
  *
  * @param result  Reference to UniValue object the wallet names and balances are pushed to.
  */
@@ -884,13 +885,17 @@ static void GetWalletBalances(UniValue& result)
     if (wallets.size() <= 1) return;
 
     UniValue balances(UniValue::VOBJ);
+    CAmount total_balance{0};
     for (const UniValue& wallet : wallets.getValues()) {
         const std::string wallet_name = wallet.get_str();
         const UniValue getbalances = ConnectAndCallRPC(&rh, "getbalances", /* args=*/{}, wallet_name);
+        if (!find_value(getbalances, "error").isNull()) continue;
         const UniValue& balance = find_value(getbalances, "result")["mine"]["trusted"];
+        total_balance += AmountFromValue(balance);
         balances.pushKV(wallet_name, balance);
     }
     result.pushKV("balances", balances);
+    result.pushKV("total_balance", ValueFromAmount(total_balance));
 }
 
 /**
