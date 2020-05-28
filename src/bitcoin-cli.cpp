cc46c0a59664f866c39994d45c765b64e878e7bd
Jon Atack <jon@atack.com>
2020-05-28 17:48:34 +0200

    cli: add AmountFromValue() and ValueFromAmount()
    Github-Pull: #19092
    Rebased-From: bbf0afade89d948f7ce377b662b0f9b4050db0bc

--- src/bitcoin-cli.cpp
+++ src/bitcoin-cli.cpp
@@ -7,6 +7,7 @@
 #include <config/bitcoin-config.h>
 #endif
 
+#include <amount.h>
 #include <chainparamsbase.h>
 #include <clientversion.h>
 #include <rpc/client.h>
@@ -847,6 +848,25 @@ static void ParseError(const UniValue& error, std::string& strPrint, int& nRet)
         strPrint = "error: " + error.write();
     }
     nRet = abs(error["code"].get_int());
+}
+
+static CAmount AmountFromValue(const UniValue& value)
+{
+    CAmount amount{0};
+    if (!ParseFixedPoint(value.getValStr(), 8, &amount))
+        throw std::runtime_error("Invalid amount");
+    if (!MoneyRange(amount))
+        throw std::runtime_error("Amount out of range");
+    return amount;
+}
+
+static UniValue ValueFromAmount(const CAmount& amount)
+{
+    bool sign{amount < 0};
+    int64_t n_abs{sign ? -amount : amount};
+    int64_t quotient{n_abs / COIN};
+    int64_t remainder{n_abs % COIN};
+    return UniValue(UniValue::VNUM, strprintf("%s%d.%08d", sign ? "-" : "", quotient, remainder));
 }
 
 /**
