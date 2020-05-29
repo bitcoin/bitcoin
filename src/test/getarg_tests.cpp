40a31cfa17d1fb89f4a56117eabbdd2c926bafb8
MarcoFalke <falke.marco@gmail.com>
2020-05-29 14:23:37 -0400

    Merge #18926: test: Pass ArgsManager into getarg_tests
    f871f15c9d760b56a6a3b78b3941d3983d60810c scripted-diff: replace gArgs with argsman (glowang)
    357f02bf2942f2944a04a1ceaa687f89d5da7d28 Create a local class inherited from BasicTestingSetup with a localized args manager and put it into the getarg_tests namespace (glowang)
    
    Pull request description:
    
      Replaced the global argsManager gArgs with a locally defined one in getarg_tests. This is to avoid confusion in arg settings between the test's ArgsManager and the   #18804
    
    ACKs for top commit:
      MarcoFalke:
        ACK f871f15c9d760b56a6a3b78b3941d3983d60810c
      ryanofsky:
        Code review ACK f871f15c9d760b56a6a3b78b3941d3983d60810c. Changes look good and thanks for updating. In future would recommend using clang-format-diff and following [coding style](https://github.com/bitcoin/bitcoin/blob/master/doc/developer-notes.md#coding-style-c) notes, because it's atypical to indent namespace content, or indent protected keywords or put spaces around ::. Also it's fragile to define test setup class in a namespace, but test setup methods outside of the namespace and inside the test fixture instead. Would be simpler to just define the testing setup completely before using it without a namespace like: https://github.com/bitcoin/bitcoin/blob/8ad5f1c376fe99eeccac2696ac0e18e662323a4d/src/test/rpc_tests.cpp#L23 and it would have been a slightly smaller change too.
    
    Tree-SHA512: 016594639396d60667fadec8ea80ef7af634fbb2014c704f02406fe3251c5362757c21f1763d8bdb94ca4a3026ab9dc786a92a9a934efc8cd807655d9deee779
    
    Former-commit-id: 2f57154071901c738ad2292b9774e0d335318791

--- src/test/getarg_tests.cpp
+++ src/test/getarg_tests.cpp
@@ -13,9 +13,18 @@
 #include <boost/algorithm/string.hpp>
 #include <boost/test/unit_test.hpp>
 
-BOOST_FIXTURE_TEST_SUITE(getarg_tests, BasicTestingSetup)
+namespace getarg_tests{
+    class LocalTestingSetup : BasicTestingSetup {
+        protected:
+        void SetupArgs(const std::vector<std::pair<std::string, unsigned int>>& args);
+        void ResetArgs(const std::string& strArg);
+        ArgsManager m_args;
+    };
+}
 
-static void ResetArgs(const std::string& strArg)
+BOOST_FIXTURE_TEST_SUITE(getarg_tests, LocalTestingSetup)
+
+void LocalTestingSetup :: ResetArgs(const std::string& strArg)
 {
     std::vector<std::string> vecArg;
     if (strArg.size())
@@ -30,14 +39,14 @@ static void ResetArgs(const std::string& strArg)
         vecChar.push_back(s.c_str());
 
     std::string error;
-    BOOST_CHECK(gArgs.ParseParameters(vecChar.size(), vecChar.data(), error));
+    BOOST_CHECK(m_args.ParseParameters(vecChar.size(), vecChar.data(), error));
 }
 
-static void SetupArgs(const std::vector<std::pair<std::string, unsigned int>>& args)
+void LocalTestingSetup :: SetupArgs(const std::vector<std::pair<std::string, unsigned int>>& args)
 {
-    gArgs.ClearArgs();
+    m_args.ClearArgs();
     for (const auto& arg : args) {
-        gArgs.AddArg(arg.first, "", arg.second, OptionsCategory::OPTIONS);
+        m_args.AddArg(arg.first, "", arg.second, OptionsCategory::OPTIONS);
     }
 }
 
@@ -46,52 +55,52 @@ BOOST_AUTO_TEST_CASE(boolarg)
     const auto foo = std::make_pair("-foo", ArgsManager::ALLOW_ANY);
     SetupArgs({foo});
     ResetArgs("-foo");
-    BOOST_CHECK(gArgs.GetBoolArg("-foo", false));
-    BOOST_CHECK(gArgs.GetBoolArg("-foo", true));
+    BOOST_CHECK(m_args.GetBoolArg("-foo", false));
+    BOOST_CHECK(m_args.GetBoolArg("-foo", true));
 
-    BOOST_CHECK(!gArgs.GetBoolArg("-fo", false));
-    BOOST_CHECK(gArgs.GetBoolArg("-fo", true));
+    BOOST_CHECK(!m_args.GetBoolArg("-fo", false));
+    BOOST_CHECK(m_args.GetBoolArg("-fo", true));
 
-    BOOST_CHECK(!gArgs.GetBoolArg("-fooo", false));
-    BOOST_CHECK(gArgs.GetBoolArg("-fooo", true));
+    BOOST_CHECK(!m_args.GetBoolArg("-fooo", false));
+    BOOST_CHECK(m_args.GetBoolArg("-fooo", true));
 
     ResetArgs("-foo=0");
-    BOOST_CHECK(!gArgs.GetBoolArg("-foo", false));
-    BOOST_CHECK(!gArgs.GetBoolArg("-foo", true));
+    BOOST_CHECK(!m_args.GetBoolArg("-foo", false));
+    BOOST_CHECK(!m_args.GetBoolArg("-foo", true));
 
     ResetArgs("-foo=1");
-    BOOST_CHECK(gArgs.GetBoolArg("-foo", false));
-    BOOST_CHECK(gArgs.GetBoolArg("-foo", true));
+    BOOST_CHECK(m_args.GetBoolArg("-foo", false));
+    BOOST_CHECK(m_args.GetBoolArg("-foo", true));
 
     // New 0.6 feature: auto-map -nosomething to !-something:
     ResetArgs("-nofoo");
-    BOOST_CHECK(!gArgs.GetBoolArg("-foo", false));
-    BOOST_CHECK(!gArgs.GetBoolArg("-foo", true));
+    BOOST_CHECK(!m_args.GetBoolArg("-foo", false));
+    BOOST_CHECK(!m_args.GetBoolArg("-foo", true));
 
     ResetArgs("-nofoo=1");
-    BOOST_CHECK(!gArgs.GetBoolArg("-foo", false));
-    BOOST_CHECK(!gArgs.GetBoolArg("-foo", true));
+    BOOST_CHECK(!m_args.GetBoolArg("-foo", false));
+    BOOST_CHECK(!m_args.GetBoolArg("-foo", true));
 
     ResetArgs("-foo -nofoo");  // -nofoo should win
-    BOOST_CHECK(!gArgs.GetBoolArg("-foo", false));
-    BOOST_CHECK(!gArgs.GetBoolArg("-foo", true));
+    BOOST_CHECK(!m_args.GetBoolArg("-foo", false));
+    BOOST_CHECK(!m_args.GetBoolArg("-foo", true));
 
     ResetArgs("-foo=1 -nofoo=1");  // -nofoo should win
-    BOOST_CHECK(!gArgs.GetBoolArg("-foo", false));
-    BOOST_CHECK(!gArgs.GetBoolArg("-foo", true));
+    BOOST_CHECK(!m_args.GetBoolArg("-foo", false));
+    BOOST_CHECK(!m_args.GetBoolArg("-foo", true));
 
     ResetArgs("-foo=0 -nofoo=0");  // -nofoo=0 should win
-    BOOST_CHECK(gArgs.GetBoolArg("-foo", false));
-    BOOST_CHECK(gArgs.GetBoolArg("-foo", true));
+    BOOST_CHECK(m_args.GetBoolArg("-foo", false));
+    BOOST_CHECK(m_args.GetBoolArg("-foo", true));
 
     // New 0.6 feature: treat -- same as -:
     ResetArgs("--foo=1");
-    BOOST_CHECK(gArgs.GetBoolArg("-foo", false));
-    BOOST_CHECK(gArgs.GetBoolArg("-foo", true));
+    BOOST_CHECK(m_args.GetBoolArg("-foo", false));
+    BOOST_CHECK(m_args.GetBoolArg("-foo", true));
 
     ResetArgs("--nofoo=1");
-    BOOST_CHECK(!gArgs.GetBoolArg("-foo", false));
-    BOOST_CHECK(!gArgs.GetBoolArg("-foo", true));
+    BOOST_CHECK(!m_args.GetBoolArg("-foo", false));
+    BOOST_CHECK(!m_args.GetBoolArg("-foo", true));
 
 }
 
@@ -101,24 +110,24 @@ BOOST_AUTO_TEST_CASE(stringarg)
     const auto bar = std::make_pair("-bar", ArgsManager::ALLOW_ANY);
     SetupArgs({foo, bar});
     ResetArgs("");
-    BOOST_CHECK_EQUAL(gArgs.GetArg("-foo", ""), "");
-    BOOST_CHECK_EQUAL(gArgs.GetArg("-foo", "eleven"), "eleven");
+    BOOST_CHECK_EQUAL(m_args.GetArg("-foo", ""), "");
+    BOOST_CHECK_EQUAL(m_args.GetArg("-foo", "eleven"), "eleven");
 
     ResetArgs("-foo -bar");
-    BOOST_CHECK_EQUAL(gArgs.GetArg("-foo", ""), "");
-    BOOST_CHECK_EQUAL(gArgs.GetArg("-foo", "eleven"), "");
+    BOOST_CHECK_EQUAL(m_args.GetArg("-foo", ""), "");
+    BOOST_CHECK_EQUAL(m_args.GetArg("-foo", "eleven"), "");
 
     ResetArgs("-foo=");
-    BOOST_CHECK_EQUAL(gArgs.GetArg("-foo", ""), "");
-    BOOST_CHECK_EQUAL(gArgs.GetArg("-foo", "eleven"), "");
+    BOOST_CHECK_EQUAL(m_args.GetArg("-foo", ""), "");
+    BOOST_CHECK_EQUAL(m_args.GetArg("-foo", "eleven"), "");
 
     ResetArgs("-foo=11");
-    BOOST_CHECK_EQUAL(gArgs.GetArg("-foo", ""), "11");
-    BOOST_CHECK_EQUAL(gArgs.GetArg("-foo", "eleven"), "11");
+    BOOST_CHECK_EQUAL(m_args.GetArg("-foo", ""), "11");
+    BOOST_CHECK_EQUAL(m_args.GetArg("-foo", "eleven"), "11");
 
     ResetArgs("-foo=eleven");
-    BOOST_CHECK_EQUAL(gArgs.GetArg("-foo", ""), "eleven");
-    BOOST_CHECK_EQUAL(gArgs.GetArg("-foo", "eleven"), "eleven");
+    BOOST_CHECK_EQUAL(m_args.GetArg("-foo", ""), "eleven");
+    BOOST_CHECK_EQUAL(m_args.GetArg("-foo", "eleven"), "eleven");
 
 }
 
@@ -128,20 +137,20 @@ BOOST_AUTO_TEST_CASE(intarg)
     const auto bar = std::make_pair("-bar", ArgsManager::ALLOW_ANY);
     SetupArgs({foo, bar});
     ResetArgs("");
-    BOOST_CHECK_EQUAL(gArgs.GetArg("-foo", 11), 11);
-    BOOST_CHECK_EQUAL(gArgs.GetArg("-foo", 0), 0);
+    BOOST_CHECK_EQUAL(m_args.GetArg("-foo", 11), 11);
+    BOOST_CHECK_EQUAL(m_args.GetArg("-foo", 0), 0);
 
     ResetArgs("-foo -bar");
-    BOOST_CHECK_EQUAL(gArgs.GetArg("-foo", 11), 0);
-    BOOST_CHECK_EQUAL(gArgs.GetArg("-bar", 11), 0);
+    BOOST_CHECK_EQUAL(m_args.GetArg("-foo", 11), 0);
+    BOOST_CHECK_EQUAL(m_args.GetArg("-bar", 11), 0);
 
     ResetArgs("-foo=11 -bar=12");
-    BOOST_CHECK_EQUAL(gArgs.GetArg("-foo", 0), 11);
-    BOOST_CHECK_EQUAL(gArgs.GetArg("-bar", 11), 12);
+    BOOST_CHECK_EQUAL(m_args.GetArg("-foo", 0), 11);
+    BOOST_CHECK_EQUAL(m_args.GetArg("-bar", 11), 12);
 
     ResetArgs("-foo=NaN -bar=NotANumber");
-    BOOST_CHECK_EQUAL(gArgs.GetArg("-foo", 1), 0);
-    BOOST_CHECK_EQUAL(gArgs.GetArg("-bar", 11), 0);
+    BOOST_CHECK_EQUAL(m_args.GetArg("-foo", 1), 0);
+    BOOST_CHECK_EQUAL(m_args.GetArg("-bar", 11), 0);
 }
 
 BOOST_AUTO_TEST_CASE(doubledash)
@@ -150,11 +159,11 @@ BOOST_AUTO_TEST_CASE(doubledash)
     const auto bar = std::make_pair("-bar", ArgsManager::ALLOW_ANY);
     SetupArgs({foo, bar});
     ResetArgs("--foo");
-    BOOST_CHECK_EQUAL(gArgs.GetBoolArg("-foo", false), true);
+    BOOST_CHECK_EQUAL(m_args.GetBoolArg("-foo", false), true);
 
     ResetArgs("--foo=verbose --bar=1");
-    BOOST_CHECK_EQUAL(gArgs.GetArg("-foo", ""), "verbose");
-    BOOST_CHECK_EQUAL(gArgs.GetArg("-bar", 0), 1);
+    BOOST_CHECK_EQUAL(m_args.GetArg("-foo", ""), "verbose");
+    BOOST_CHECK_EQUAL(m_args.GetArg("-bar", 0), 1);
 }
 
 BOOST_AUTO_TEST_CASE(boolargno)
@@ -163,24 +172,24 @@ BOOST_AUTO_TEST_CASE(boolargno)
     const auto bar = std::make_pair("-bar", ArgsManager::ALLOW_ANY);
     SetupArgs({foo, bar});
     ResetArgs("-nofoo");
-    BOOST_CHECK(!gArgs.GetBoolArg("-foo", true));
-    BOOST_CHECK(!gArgs.GetBoolArg("-foo", false));
+    BOOST_CHECK(!m_args.GetBoolArg("-foo", true));
+    BOOST_CHECK(!m_args.GetBoolArg("-foo", false));
 
     ResetArgs("-nofoo=1");
-    BOOST_CHECK(!gArgs.GetBoolArg("-foo", true));
-    BOOST_CHECK(!gArgs.GetBoolArg("-foo", false));
+    BOOST_CHECK(!m_args.GetBoolArg("-foo", true));
+    BOOST_CHECK(!m_args.GetBoolArg("-foo", false));
 
     ResetArgs("-nofoo=0");
-    BOOST_CHECK(gArgs.GetBoolArg("-foo", true));
-    BOOST_CHECK(gArgs.GetBoolArg("-foo", false));
+    BOOST_CHECK(m_args.GetBoolArg("-foo", true));
+    BOOST_CHECK(m_args.GetBoolArg("-foo", false));
 
     ResetArgs("-foo --nofoo"); // --nofoo should win
-    BOOST_CHECK(!gArgs.GetBoolArg("-foo", true));
-    BOOST_CHECK(!gArgs.GetBoolArg("-foo", false));
+    BOOST_CHECK(!m_args.GetBoolArg("-foo", true));
+    BOOST_CHECK(!m_args.GetBoolArg("-foo", false));
 
     ResetArgs("-nofoo -foo"); // foo always wins:
-    BOOST_CHECK(gArgs.GetBoolArg("-foo", true));
-    BOOST_CHECK(gArgs.GetBoolArg("-foo", false));
+    BOOST_CHECK(m_args.GetBoolArg("-foo", true));
+    BOOST_CHECK(m_args.GetBoolArg("-foo", false));
 }
 
 BOOST_AUTO_TEST_CASE(logargs)
@@ -200,7 +209,7 @@ BOOST_AUTO_TEST_CASE(logargs)
         });
 
     // Log the arguments
-    gArgs.LogArgs();
+    m_args.LogArgs();
 
     LogInstance().DeleteCallback(print_connection);
     // Check that what should appear does, and what shouldn't doesn't.
