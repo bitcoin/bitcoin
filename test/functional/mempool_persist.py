e39defc3f696081b7851ab71691d0c833bfcf196
sidhujag <jsidhu@blockchainfoundry.co>
2020-05-30 18:00:03 -0700

    fix mempool persist issue
    Former-commit-id: deef57fd6176577bfd702afafb2fe81df311b4ba

--- test/functional/mempool_persist.py
+++ test/functional/mempool_persist.py
@@ -100,8 +100,7 @@ class MempoolPersistTest(SyscoinTestFramework):
         assert self.nodes[0].getmempoolinfo()["loaded"]  # start_node is blocking on the mempool being loaded
         assert self.nodes[2].getmempoolinfo()["loaded"]
         assert_equal(len(self.nodes[0].getrawmempool()), 6)
-        # SYSCOIN trickle is much faster so node 2 will get tx right away
-        assert_equal(len(self.nodes[2].getrawmempool()), 6)
+        assert_equal(len(self.nodes[2].getrawmempool()), 5)
         # The others have loaded their mempool. If node_1 loaded anything, we'd probably notice by now:
         assert_equal(len(self.nodes[1].getrawmempool()), 0)
 
