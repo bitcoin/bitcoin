c84b303eccde2099f487c1a81df9206c0717baa4
MarcoFalke <falke.marco@gmail.com>
2020-05-31 18:18:03 -0400

    Merge #19044: net processing: Add support for getcfilters
    9e36067d8cd02830c7e5a88a391dff6ac3adbe0c [test] Add test for cfilters. (Jim Posen)
    11106a4722558765a44ae45c7892724a73ce514c [net processing] Message handling for getcfilters. (Jim Posen)
    e535670726952e43483763dfca6fc6ec2f4b0691 [indexes] Fix default [de]serialization of BlockFilter. (Jim Posen)
    bb911ae7f5cbe4974ec61266d2334b95067fa49d [refactor] Pass CNode and CConnman by reference (John Newbery)
    
    Pull request description:
    
      Support `getcfilters` requests when `-peerblockfilters` is set.
    
      Does not advertise compact filter support in version messages.
    
    ACKs for top commit:
      Empact:
        re-Code Review ACK https://github.com/bitcoin/bitcoin/pull/19044/commits/9e36067d8cd02830c7e5a88a391dff6ac3adbe0c
      MarcoFalke:
        re-ACK 9e36067d8c , only change is adding commit "[refactor] Pass CNode and CConnman by reference" 🥑
      jkczyz:
        ACK 9e36067d8cd02830c7e5a88a391dff6ac3adbe0c
      fjahr:
        Code review ACK 9e36067d8cd02830c7e5a88a391dff6ac3adbe0c
    
    Tree-SHA512: b45b42a25905ef0bd9e195029185300c86856c87f78cbe17921f4a25e159ae0f6f003e61714fa43779017eb97cd89d3568419be88e47d19dc8095562939e7887
    
    Former-commit-id: abf84997fe56e4ec2402065bc69ac6a09412054f

--- src/protocol.cpp
+++ src/protocol.cpp
@@ -40,6 +40,8 @@ const char *SENDCMPCT="sendcmpct";
 const char *CMPCTBLOCK="cmpctblock";
 const char *GETBLOCKTXN="getblocktxn";
 const char *BLOCKTXN="blocktxn";
+const char *GETCFILTERS="getcfilters";
+const char *CFILTER="cfilter";
 const char *GETCFHEADERS="getcfheaders";
 const char *CFHEADERS="cfheaders";
 const char *GETCFCHECKPT="getcfcheckpt";
@@ -103,6 +105,10 @@ const static std::string allNetMessageTypes[] = {
     NetMsgType::MNGOVERNANCESYNC,
     NetMsgType::MNGOVERNANCEOBJECTVOTE,
     NetMsgType::MNVERIFY,   
+    NetMsgType::GETCFILTERS,
+    NetMsgType::CFILTER,
+    NetMsgType::GETCFHEADERS,
+    NetMsgType::CFHEADERS,
     NetMsgType::GETCFCHECKPT,
     NetMsgType::CFCHECKPT,
     NetMsgType::GETCFHEADERS,
