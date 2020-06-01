405b44e211c0c7ee03ba038644fe6b3520d7c180
Kittywhiskers Van Gogh <63189531+kittywhiskers@users.noreply.github.com>
2020-06-01 18:05:15 +1000

    merge bitcoin#19131: Fix unreachable code in init arg checks

--- src/util/translation.h
+++ src/util/translation.h
@@ -24,6 +24,11 @@ struct bilingual_str {
         translated += rhs.translated;
         return *this;
     }
+
+    bool empty() const
+    {
+        return original.empty();
+    }
 };
 
 inline bilingual_str operator+(bilingual_str lhs, const bilingual_str& rhs)
