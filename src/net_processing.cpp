dffbdc4c660826de62c2643c714aab74ae1d2bbe
gzhao408 <gzhao408@berkeley.edu>
2020-06-03 16:28:43 -0700

    [netprocessing] disconnect node that sends filterclear
    -nodes not serving bloomfilters should disconnect peers
    that send filterclear, just like filteradd and filterload
    -nodes that want to enable/disable txrelay should use
    feefilter
    
    
    Former-commit-id: 6f669174763e37bead3396f8191afbbf08b2adfd

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -3803,13 +3803,15 @@ bool ProcessMessage(CNode& pfrom, const std::string& msg_type, CDataStream& vRec
     }
 
     if (msg_type == NetMsgType::FILTERCLEAR) {
+        if (!(pfrom.GetLocalServices() & NODE_BLOOM)) {
+            pfrom.fDisconnect = true;
+            return true;
+        }
         if (pfrom.m_tx_relay == nullptr) {
             return true;
         }
         LOCK(pfrom.m_tx_relay->cs_filter);
-        if (pfrom.GetLocalServices() & NODE_BLOOM) {
-            pfrom.m_tx_relay->pfilter = nullptr;
-        }
+        pfrom.m_tx_relay->pfilter = nullptr;
         pfrom.m_tx_relay->fRelayTxes = true;
         return true;
     }
