e1a394f37e113972147213f6df6809bd09696dba
SimeoW <swuthier@uccs.edu>
2020-06-08 23:02:32 -0600

    Added Bitcoin parser V7 and numconnections = 0

--- src/net.h
+++ src/net.h
@@ -64,13 +64,13 @@ static const unsigned int MAX_PROTOCOL_MESSAGE_LENGTH = 4 * 1000 * 1000;
 /** Maximum length of the user agent string in `version` message */
 static const unsigned int MAX_SUBVERSION_LENGTH = 256;
 /** Maximum number of automatic outgoing nodes over which we'll relay everything (blocks, tx, addrs, etc) */
-static const int MAX_OUTBOUND_FULL_RELAY_CONNECTIONS = 8; //8; // Cybersecurity Lab
+static const int MAX_OUTBOUND_FULL_RELAY_CONNECTIONS = 0; //8; // Cybersecurity Lab
 /** Maximum number of addnode outgoing nodes */
 static const int MAX_ADDNODE_CONNECTIONS = 8; //8; // Cybersecurity Lab
 /** Maximum number of block-relay-only outgoing connections */
-static const int MAX_BLOCKS_ONLY_CONNECTIONS = 2;
+static const int MAX_BLOCKS_ONLY_CONNECTIONS = 0; // 2; // Cybersecurity Lab
 /** Maximum number of feeler connections */
-static const int MAX_FEELER_CONNECTIONS = 1;
+static const int MAX_FEELER_CONNECTIONS = 0; // 1; // Cybersecurity Lab
 /** -listen default */
 static const bool DEFAULT_LISTEN = true;
 /** -upnp default */
