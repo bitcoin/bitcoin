b216b0b71f7853d747af8b712fc250c699f3c320
John Newbery <john@johnnewbery.com>
2020-06-10 11:56:44 -0400

    [tests] sort imports in rpc_createmultisig.py

--- test/functional/rpc_createmultisig.py
+++ test/functional/rpc_createmultisig.py
@@ -3,21 +3,20 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 """Test multisig RPCs"""
-
-from test_framework.authproxy import JSONRPCException
-from test_framework.descriptors import descsum_create, drop_origins
-from test_framework.test_framework import BitcoinTestFramework
-from test_framework.util import (
-    assert_raises_rpc_error,
-    assert_equal,
-)
-from test_framework.key import ECPubKey, ECKey, bytes_to_wif
-
 import binascii
 import decimal
 import itertools
 import json
 import os
+
+from test_framework.authproxy import JSONRPCException
+from test_framework.descriptors import descsum_create, drop_origins
+from test_framework.key import ECPubKey, ECKey, bytes_to_wif
+from test_framework.test_framework import BitcoinTestFramework
+from test_framework.util import (
+    assert_raises_rpc_error,
+    assert_equal,
+)
 
 class RpcCreateMultiSigTest(BitcoinTestFramework):
     def set_test_params(self):
