bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

refactor: Add negative thread safety annotations to CAddrMan

src/addrman.h
@@ -357,7 +357,7 @@ public:
      * very little in common.
      */
     template<typename Stream>
-    void Serialize(Stream &s) const
+    void Serialize(Stream &s) const EXCLUSIVE_LOCKS_REQUIRED(!m_addrman_mutex)
     {
         LOCK(m_addrman_mutex);
 
@@ -414,7 +414,7 @@ public:
     }
 
     template<typename Stream>
-    void Unserialize(Stream& s)
+    void Unserialize(Stream& s) EXCLUSIVE_LOCKS_REQUIRED(!m_addrman_mutex)
     {
         LOCK(m_addrman_mutex);
 
@@ -536,7 +536,7 @@ public:
         CheckNonLockHelper();
     }
 
-    void Clear()
+    void Clear() EXCLUSIVE_LOCKS_REQUIRED(!m_addrman_mutex)
     {
         LOCK(m_addrman_mutex);
         ClearNonLockHelper();
@@ -553,14 +553,14 @@ public:
     }
 
     //! Return the number of (unique) addresses in all tables.
-    size_t size() const
+    size_t size() const EXCLUSIVE_LOCKS_REQUIRED(!m_addrman_mutex)
     {
         LOCK(m_addrman_mutex); // TODO: Cache this in an atomic to avoid this overhead
         return sizeNonLockerHelper();
     }
 
     //! Consistency check
-    void Check()
+    void Check() EXCLUSIVE_LOCKS_REQUIRED(!m_addrman_mutex)
     {
 #ifdef DEBUG_ADDRMAN
         LOCK(m_addrman_mutex);
@@ -569,7 +569,7 @@ public:
     }
 
     //! Add a single address.
-    bool Add(const CAddress &addr, const CNetAddr& source, int64_t nTimePenalty = 0)
+    bool Add(const CAddress &addr, const CNetAddr& source, int64_t nTimePenalty = 0) EXCLUSIVE_LOCKS_REQUIRED(!m_addrman_mutex)
     {
         LOCK(m_addrman_mutex);
         bool fRet = false;
@@ -583,7 +583,7 @@ public:
     }
 
     //! Add multiple addresses.
-    bool Add(const std::vector<CAddress> &vAddr, const CNetAddr& source, int64_t nTimePenalty = 0)
+    bool Add(const std::vector<CAddress> &vAddr, const CNetAddr& source, int64_t nTimePenalty = 0) EXCLUSIVE_LOCKS_REQUIRED(!m_addrman_mutex)
     {
         LOCK(m_addrman_mutex);
         int nAdd = 0;
@@ -598,7 +598,7 @@ public:
     }
 
     //! Mark an entry as accessible.
-    void Good(const CService &addr, bool test_before_evict = true, int64_t nTime = GetAdjustedTime())
+    void Good(const CService &addr, bool test_before_evict = true, int64_t nTime = GetAdjustedTime()) EXCLUSIVE_LOCKS_REQUIRED(!m_addrman_mutex)
     {
         LOCK(m_addrman_mutex);
         CheckNonLockHelper();
@@ -607,7 +607,7 @@ public:
     }
 
     //! Mark an entry as connection attempted to.
-    void Attempt(const CService &addr, bool fCountFailure, int64_t nTime = GetAdjustedTime())
+    void Attempt(const CService &addr, bool fCountFailure, int64_t nTime = GetAdjustedTime()) EXCLUSIVE_LOCKS_REQUIRED(!m_addrman_mutex)
     {
         LOCK(m_addrman_mutex);
         CheckNonLockHelper();
@@ -616,7 +616,7 @@ public:
     }
 
     //! See if any to-be-evicted tried table entries have been tested and if so resolve the collisions.
-    void ResolveCollisions()
+    void ResolveCollisions() EXCLUSIVE_LOCKS_REQUIRED(!m_addrman_mutex)
     {
         LOCK(m_addrman_mutex);
         CheckNonLockHelper();
@@ -625,7 +625,7 @@ public:
     }
 
     //! Randomly select an address in tried that another address is attempting to evict.
-    CAddrInfo SelectTriedCollision()
+    CAddrInfo SelectTriedCollision() EXCLUSIVE_LOCKS_REQUIRED(!m_addrman_mutex)
     {
         CAddrInfo ret;
         {
@@ -640,7 +640,7 @@ public:
     /**
      * Choose an address to connect to.
      */
-    CAddrInfo Select(bool newOnly = false)
+    CAddrInfo Select(bool newOnly = false) EXCLUSIVE_LOCKS_REQUIRED(!m_addrman_mutex)
     {
         CAddrInfo addrRet;
         {
@@ -653,7 +653,7 @@ public:
     }
 
     //! Return a bunch of addresses, selected at random.
-    std::vector<CAddress> GetAddr()
+    std::vector<CAddress> GetAddr() EXCLUSIVE_LOCKS_REQUIRED(!m_addrman_mutex)
     {
         Check();
         std::vector<CAddress> vAddr;
@@ -666,7 +666,7 @@ public:
     }
 
     //! Mark an entry as currently-connected-to.
-    void Connected(const CService &addr, int64_t nTime = GetAdjustedTime())
+    void Connected(const CService &addr, int64_t nTime = GetAdjustedTime()) EXCLUSIVE_LOCKS_REQUIRED(!m_addrman_mutex)
     {
         LOCK(m_addrman_mutex);
         CheckNonLockHelper();
@@ -674,7 +674,7 @@ public:
         CheckNonLockHelper();
     }
 
-    void SetServices(const CService &addr, ServiceFlags nServices)
+    void SetServices(const CService &addr, ServiceFlags nServices) EXCLUSIVE_LOCKS_REQUIRED(!m_addrman_mutex)
     {
         LOCK(m_addrman_mutex);
         CheckNonLockHelper();

bitcoin/bitcoin

src/addrman.h
@@ -320,7 +320,7 @@ public:
      * very little in common.
      */
     template<typename Stream>
-    void Serialize(Stream &s) const
+    void Serialize(Stream &s) const EXCLUSIVE_LOCKS_REQUIRED(!cs)
     {
         LOCK(cs);
 
@@ -377,7 +377,7 @@ public:
     }
 
     template<typename Stream>
-    void Unserialize(Stream& s)
+    void Unserialize(Stream& s) EXCLUSIVE_LOCKS_REQUIRED(!cs)
     {
         LOCK(cs);
 
@@ -554,7 +554,7 @@ public:
     }
 
     //! Add a single address.
-    bool Add(const CAddress &addr, const CNetAddr& source, int64_t nTimePenalty = 0)
+    bool Add(const CAddress &addr, const CNetAddr& source, int64_t nTimePenalty = 0) EXCLUSIVE_LOCKS_REQUIRED(!cs)
     {
         LOCK(cs);
         bool fRet = false;
@@ -568,7 +568,7 @@ public:
     }
 
     //! Add multiple addresses.
-    bool Add(const std::vector<CAddress> &vAddr, const CNetAddr& source, int64_t nTimePenalty = 0)
+    bool Add(const std::vector<CAddress> &vAddr, const CNetAddr& source, int64_t nTimePenalty = 0) EXCLUSIVE_LOCKS_REQUIRED(!cs)
     {
         LOCK(cs);
         int nAdd = 0;
@@ -583,7 +583,7 @@ public:
     }
 
     //! Mark an entry as accessible.
-    void Good(const CService &addr, bool test_before_evict = true, int64_t nTime = GetAdjustedTime())
+    void Good(const CService &addr, bool test_before_evict = true, int64_t nTime = GetAdjustedTime()) EXCLUSIVE_LOCKS_REQUIRED(!cs)
     {
         LOCK(cs);
         Check();
@@ -601,7 +601,7 @@ public:
     }
 
     //! See if any to-be-evicted tried table entries have been tested and if so resolve the collisions.
-    void ResolveCollisions()
+    void ResolveCollisions() EXCLUSIVE_LOCKS_REQUIRED(!cs)
     {
         LOCK(cs);
         Check();
@@ -610,7 +610,7 @@ public:
     }
 
     //! Randomly select an address in tried that another address is attempting to evict.
-    CAddrInfo SelectTriedCollision()
+    CAddrInfo SelectTriedCollision() EXCLUSIVE_LOCKS_REQUIRED(!cs)
     {
         CAddrInfo ret;
         {
@@ -625,7 +625,7 @@ public:
     /**
      * Choose an address to connect to.
      */
-    CAddrInfo Select(bool newOnly = false)
+    CAddrInfo Select(bool newOnly = false) EXCLUSIVE_LOCKS_REQUIRED(!cs)
     {
         CAddrInfo addrRet;
         {
@@ -638,7 +638,7 @@ public:
     }
 
     //! Return a bunch of addresses, selected at random.
-    std::vector<CAddress> GetAddr()
+    std::vector<CAddress> GetAddr() EXCLUSIVE_LOCKS_REQUIRED(!cs)
     {
         Check();
         std::vector<CAddress> vAddr;
@@ -651,7 +651,7 @@ public:
     }
 
     //! Mark an entry as currently-connected-to.
-    void Connected(const CService &addr, int64_t nTime = GetAdjustedTime())
+    void Connected(const CService &addr, int64_t nTime = GetAdjustedTime()) EXCLUSIVE_LOCKS_REQUIRED(!cs)
     {
         LOCK(cs);
         Check();
@@ -659,7 +659,7 @@ public:
         Check();
     }
 
-    void SetServices(const CService &addr, ServiceFlags nServices)
+    void SetServices(const CService &addr, ServiceFlags nServices) EXCLUSIVE_LOCKS_REQUIRED(!cs)
     {
         LOCK(cs);
         Check();