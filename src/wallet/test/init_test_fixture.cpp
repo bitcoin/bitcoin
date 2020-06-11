38d8432c1b9dab2ad4469a80827ae5a95e475dfe
Kiminuo <kiminuo@protonmail.com>
2020-06-11 09:57:47 +0200

    Remove `BOOST_FILESYSTEM_C_STR` as we want to use the overload with `fs::path`
    See (1) to understand what the macro `BOOST_FILESYSTEM_C_STR` does.
    
    Resources:
    
    (1) https://www.boost.org/doc/libs/1_70_0/boost/filesystem/fstream.hpp
    (2) https://stackoverflow.com/questions/821873/how-to-open-an-stdfstream-ofstream-or-ifstream-with-a-unicode-filename/#54842261
    
    (cherry picked from commit 7543252fe53f992f93d6af802a8f5f30d40cce15)
    (cherry picked from commit a02bba11c22108ecc4a88dbc106c2b7693b4838a)

--- src/wallet/test/init_test_fixture.cpp
+++ src/wallet/test/init_test_fixture.cpp
@@ -31,7 +31,7 @@ InitWalletDirTestingSetup::InitWalletDirTestingSetup(const std::string& chainNam
     fs::create_directories(m_walletdir_path_cases["default"]);
     fs::create_directories(m_walletdir_path_cases["custom"]);
     fs::create_directories(m_walletdir_path_cases["relative"]);
-    std::ofstream f(m_walletdir_path_cases["file"].BOOST_FILESYSTEM_C_STR);
+    std::ofstream f(m_walletdir_path_cases["file"]);
     f.close();
 }
 
