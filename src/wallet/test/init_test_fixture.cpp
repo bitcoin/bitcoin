ab400705d0598aff114076cfdc6b21e4ceeab9c4
Kiminuo <kiminuo@protonmail.com>
2020-06-11 17:59:28 +0000

    Fix init_tests
    (cherry picked from commit db44346266d71b716f136fe6d8febf819168ce27)
    (cherry picked from commit 62e7fb353efbbc125b1a96ef78e0331149b144ca)

--- src/wallet/test/init_test_fixture.cpp
+++ src/wallet/test/init_test_fixture.cpp
@@ -22,8 +22,8 @@ InitWalletDirTestingSetup::InitWalletDirTestingSetup(const std::string& chainNam
     m_walletdir_path_cases["custom"] = m_datadir / "my_wallets";
     m_walletdir_path_cases["nonexistent"] = m_datadir / "path_does_not_exist";
     m_walletdir_path_cases["file"] = m_datadir / "not_a_directory.dat";
-    m_walletdir_path_cases["trailing"] = m_datadir / "wallets" / sep;
-    m_walletdir_path_cases["trailing2"] = m_datadir / "wallets" / sep / sep;
+    m_walletdir_path_cases["trailing"] = m_datadir / ("wallets" + sep);
+    m_walletdir_path_cases["trailing2"] = m_datadir / ("wallets" + sep + sep);
 
     fs::current_path(m_datadir);
     m_walletdir_path_cases["relative"] = "wallets";
