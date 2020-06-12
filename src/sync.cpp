2beab95acb7e125fb9f122b11f25434af203c471
PastaPastaPasta <6443210+PastaPastaPasta@users.noreply.github.com>
2020-06-12 15:45:05 +0000

    remove /* continued */ where is is not needed
    Co-authored-by: UdjinM6 <UdjinM6@users.noreply.github.com>

--- src/sync.cpp
+++ src/sync.cpp
@@ -81,20 +81,20 @@ static void potential_deadlock_detected(const std::pair<void*, void*>& mismatch,
     strOutput += "Previous lock order was:\n";
     for (const std::pair<void*, CLockLocation> & i : s2) {
         if (i.first == mismatch.first) {
-            strOutput += " (1)"; /* Continued */
+            strOutput += " (1)";
         }
         if (i.first == mismatch.second) {
-            strOutput += " (2)"; /* Continued */
+            strOutput += " (2)";
         }
         strOutput += strprintf(" %s\n", i.second.ToString().c_str());
     }
     strOutput += "Current lock order is:\n";
     for (const std::pair<void*, CLockLocation> & i : s1) {
         if (i.first == mismatch.first) {
-            strOutput += " (1)"; /* Continued */
+            strOutput += " (1)";
         }
         if (i.first == mismatch.second) {
-            strOutput += " (2)"; /* Continued */
+            strOutput += " (2)";
         }
         strOutput += strprintf(" %s\n", i.second.ToString().c_str());
     }
--- src/validation.cpp
+++ src/validation.cpp
@@ -2548,14 +2548,14 @@ void static UpdateTip(const CBlockIndex *pindexNew, const CChainParams& chainPar
             DoWarning(strWarning);
         }
     }
-    std::string strMessage = strprintf("%s: new best=%s height=%d version=0x%08x log2_work=%.8f tx=%lu date='%s' progress=%f cache=%.1fMiB(%utxo)", __func__, /* Continued */
+    std::string strMessage = strprintf("%s: new best=%s height=%d version=0x%08x log2_work=%.8f tx=%lu date='%s' progress=%f cache=%.1fMiB(%utxo)", __func__,
       pindexNew->GetBlockHash().ToString(), pindexNew->nHeight, pindexNew->nVersion,
       log(pindexNew->nChainWork.getdouble())/log(2.0), (unsigned long)pindexNew->nChainTx,
       DateTimeStrFormat("%Y-%m-%d %H:%M:%S", pindexNew->GetBlockTime()),
       GuessVerificationProgress(chainParams.TxData(), pindexNew), pcoinsTip->DynamicMemoryUsage() * (1.0 / (1<<20)), pcoinsTip->GetCacheSize());
     strMessage += strprintf(" evodb_cache=%.1fMiB", evoDb->GetMemoryUsage() * (1.0 / (1<<20)));
     if (!warningMessages.empty())
-        strMessage += strprintf(" warning='%s'", boost::algorithm::join(warningMessages, ", ")); /* Continued */
+        strMessage += strprintf(" warning='%s'", boost::algorithm::join(warningMessages, ", "));
     LogPrintf("%s\n", strMessage);
 }
 
