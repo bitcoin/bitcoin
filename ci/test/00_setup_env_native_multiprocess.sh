fa05f44893d228f672f39436d0cb6b3376f81ac2
MarcoFalke <falke.marco@gmail.com>
2020-06-13 09:15:00 -0400

    ci: Upgrade most ci configs to focal

--- ci/test/00_setup_env_native_multiprocess.sh
+++ ci/test/00_setup_env_native_multiprocess.sh
@@ -7,6 +7,7 @@
 export LC_ALL=C.UTF-8
 
 export CONTAINER_NAME=ci_native_multiprocess
+export DOCKER_NAME_TAG=ubuntu:20.04
 export PACKAGES="cmake python3"
 export DEP_OPTS="MULTIPROCESS=1"
 export GOAL="install"
