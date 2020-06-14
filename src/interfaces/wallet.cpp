a607c1f091a2b522d6d6cd6b840eed0e0ab50bd8
sidhujag <jsidhu@blockchainfoundry.co>
2020-06-13 23:40:00 -0700

    move auxpow back into rpcwallet
    Former-commit-id: cc3223d94c03e0e1240b0a283adb8231063b7eb3

--- src/interfaces/wallet.cpp
+++ src/interfaces/wallet.cpp
@@ -503,13 +503,7 @@ public:
     void registerRpcs() override
     {
         for (const CRPCCommand& command : GetWalletRPCCommands()) {
-            m_rpc_commands.emplace_back(command.category, command.name, [this, &command](const JSONRPCRequest& request, UniValue& result, bool last_handler) {
-                return command.actor({request, m_context}, result, last_handler);
-            }, command.argNames, command.unique_id);
-            m_rpc_handlers.emplace_back(m_context.chain->handleRpc(m_rpc_commands.back()));
-        }
-        for (const CRPCCommand& command : GetAssetWalletRPCCommands()) {
-            m_rpc_commands.emplace_back(command.category, command.name, [this, &command](const JSONRPCRequest& request, UniValue& result, bool last_handler) {
+           m_rpc_commands.emplace_back(command.category, command.name, [this, &command](const JSONRPCRequest& request, UniValue& result, bool last_handler) {
                 /* SYSCOIN Unlike upstream Bitcoin, we need the NodeContext for
                    getauxblock.  Thus we construct a new context that
                    contains both and use that.  */
@@ -517,6 +511,12 @@ public:
                 if (request.context.Has<NodeContext>())
                     extendedCtx.nodeContext = &request.context.Get<NodeContext>();
                 return command.actor({request, extendedCtx}, result, last_handler);
+            }, command.argNames, command.unique_id);
+            m_rpc_handlers.emplace_back(m_context.chain->handleRpc(m_rpc_commands.back()));
+        }
+        for (const CRPCCommand& command : GetAssetWalletRPCCommands()) {
+                        m_rpc_commands.emplace_back(command.category, command.name, [this, &command](const JSONRPCRequest& request, UniValue& result, bool last_handler) {
+                return command.actor({request, m_context}, result, last_handler);
             }, command.argNames, command.unique_id);
             m_rpc_handlers.emplace_back(m_context.chain->handleRpc(m_rpc_commands.back()));
         }
