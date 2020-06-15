980f9b667355a1d3ce7888347ed57ee3d727d964
MarcoFalke <falke.marco@gmail.com>
2020-06-15 06:25:00 -0400

    Merge #19276: ci: Move travis workarounds to .travis.yml
    fa7166759789c1282609ff3ab2e80d4f70910a9f ci: Move travis workarounds to .travis.yml (MarcoFalke)
    
    Pull request description:
    
      It seems odd to have travis related workarounds in the general ci config files. Fix that oddity by moving the travis related workarounds to the travis yaml file.
    
      For unexplained reasons, this should also work around and thus close #19171
    
    ACKs for top commit:
      hebasto:
        ACK fa7166759789c1282609ff3ab2e80d4f70910a9f, I have reviewed the code and it looks OK, I agree it can be merged.
    
    Tree-SHA512: b4419d38e2b41f6e4d6e6b7658f1d972c40c390a49fe78808f8640d28efd84cc6668ce292d45b7c539e65b9e2ecbad10e796cb8f9329a0f1e7d0132ce962d226

--- ci/test/05_before_script.sh
+++ ci/test/05_before_script.sh
@@ -33,9 +33,7 @@ if [ -z "$NO_DEPENDS" ]; then
   else
     SHELL_OPTS="CONFIG_SHELL="
   fi
-  # Temporary workaround for https://github.com/bitcoin/bitcoin/issues/16368
-  python3 -c 'import time; [print(".") or time.sleep(500) for _ in range(4)]' &
-  ( DOCKER_EXEC $SHELL_OPTS make $MAKEJOBS -C depends HOST=$HOST $DEP_OPTS ) &> /dev/null
+  DOCKER_EXEC $SHELL_OPTS make $MAKEJOBS -C depends HOST=$HOST $DEP_OPTS
 fi
 if [ -n "$PREVIOUS_RELEASES_TO_DOWNLOAD" ]; then
   BEGIN_FOLD previous-versions
