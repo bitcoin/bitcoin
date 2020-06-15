313a081b907bf0a5b56af99ec2d42814ef0638b0
wiz <j@wiz.biz>
2020-06-15 18:22:52 +0900

    [net] Add seed.bitcoin.wiz.biz to DNS seeds

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -121,6 +121,7 @@ public:
         vSeeds.emplace_back("seed.btc.petertodd.org"); // Peter Todd, only supports x1, x5, x9, and xd
         vSeeds.emplace_back("seed.bitcoin.sprovoost.nl"); // Sjors Provoost
         vSeeds.emplace_back("dnsseed.emzy.de"); // Stephan Oeste
+        vSeeds.emplace_back("seed.bitcoin.wiz.biz"); // Jason Maurice
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,0);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
