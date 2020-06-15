1b7c2a07c47b44292ba71d9409fd13fdbabb8cd8
Patrick Strateman <patrick.strateman@gmail.com>
2020-06-15 12:30:22 -0400

    Add DecodeGCSFilter and BlockFilterGetHash benchmarks.
    Also standardize on the BASIC filter parameters so we can compare between all the benchmarks.

--- src/bench/gcs_filter.cpp
+++ src/bench/gcs_filter.cpp
@@ -17,7 +17,7 @@ static void ConstructGCSFilter(benchmark::State& state)
 
     uint64_t siphash_k0 = 0;
     while (state.KeepRunning()) {
-        GCSFilter filter({siphash_k0, 0, 20, 1 << 20}, elements);
+        GCSFilter filter({siphash_k0, 0, BASIC_FILTER_P, BASIC_FILTER_M}, elements);
 
         siphash_k0++;
     }
@@ -32,12 +32,48 @@ static void MatchGCSFilter(benchmark::State& state)
         element[1] = static_cast<unsigned char>(i >> 8);
         elements.insert(std::move(element));
     }
-    GCSFilter filter({0, 0, 20, 1 << 20}, elements);
+    GCSFilter filter({0, 0, BASIC_FILTER_P, BASIC_FILTER_M}, elements);
 
     while (state.KeepRunning()) {
         filter.Match(GCSFilter::Element());
     }
 }
 
+static void DecodeGCSFilter(benchmark::State& state)
+{
+    GCSFilter::ElementSet elements;
+    for (int i = 0; i < 10000; ++i) {
+        GCSFilter::Element element(32);
+        element[0] = static_cast<unsigned char>(i);
+        element[1] = static_cast<unsigned char>(i >> 8);
+        elements.insert(std::move(element));
+    }
+    GCSFilter filter({0, 0, BASIC_FILTER_P, BASIC_FILTER_M}, elements);
+    auto encoded = filter.GetEncoded();
+
+    while (state.KeepRunning()) {
+        GCSFilter filter({0, 0, BASIC_FILTER_P, BASIC_FILTER_M}, encoded);
+    }
+}
+
+static void BlockFilterGetHash(benchmark::State& state)
+{
+    GCSFilter::ElementSet elements;
+    for (int i = 0; i < 10000; ++i) {
+        GCSFilter::Element element(32);
+        element[0] = static_cast<unsigned char>(i);
+        element[1] = static_cast<unsigned char>(i >> 8);
+        elements.insert(std::move(element));
+    }
+    GCSFilter filter({0, 0, BASIC_FILTER_P, BASIC_FILTER_M}, elements);
+    BlockFilter block_filter(BlockFilterType::BASIC, {}, filter.GetEncoded());
+
+    while (state.KeepRunning()) {
+        block_filter.GetHash();
+    }
+}
+
+BENCHMARK(BlockFilterGetHash, 10000);
 BENCHMARK(ConstructGCSFilter, 1000);
+BENCHMARK(DecodeGCSFilter, 5000);
 BENCHMARK(MatchGCSFilter, 50 * 1000);
