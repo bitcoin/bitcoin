ed27297c4cb44eb8da299e9ee998c3df90ec0e7b
Cory Fields <cory-nospam-@coryfields.com>
2020-06-16 14:20:19 -0400

    macos: Bump to xcode 11.3.1 and 10.15 SDK
    This gets us a newer SDK with c++17 support and retains 10.12
    back-compat.
    
    Co-authored-by: Carl Dong <contact@carldong.me>
    
    Former-commit-id: 978e4dab6698ef0727419c4c081e84f6b7589eb2

--- depends/hosts/darwin.mk
+++ depends/hosts/darwin.mk
@@ -1,7 +1,7 @@
 OSX_MIN_VERSION=10.12
-OSX_SDK_VERSION=10.14.4
-XCODE_VERSION=10.2.1
-XCODE_BUILD_ID=10E1001
+OSX_SDK_VERSION=10.15.1
+XCODE_VERSION=11.3.1
+XCODE_BUILD_ID=11C505
 OSX_SDK=$(SDK_PATH)/Xcode-$(XCODE_VERSION)-$(XCODE_BUILD_ID)-extracted-SDK-with-libcxx-headers
 darwin_CC=clang -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK)
 darwin_CXX=clang++ -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK) -stdlib=libc++
