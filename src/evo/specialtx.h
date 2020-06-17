c318f511a076c7e15617f1da42d4ccf4299f0d90
sidhujag <jsidhu@blockchainfoundry.co>
2020-06-17 10:29:51 -0700

    fix mn payload get/set
    Former-commit-id: c5a244092f953e7dc1f214d74fe93a7e5150f928

--- src/evo/specialtx.h
+++ src/evo/specialtx.h
@@ -5,9 +5,7 @@
 #ifndef DASH_SPECIALTX_H
 #define DASH_SPECIALTX_H
 
-#include "primitives/transaction.h"
-#include "streams.h"
-#include "version.h"
+#include <primitives/transaction.h>
 
 class CBlock;
 class CBlockIndex;
@@ -16,6 +14,7 @@ class CValidationState;
 bool CheckSpecialTx(const CTransaction& tx, const CBlockIndex* pindexPrev, CValidationState& state);
 bool ProcessSpecialTxsInBlock(const CBlock& block, const CBlockIndex* pindex, CValidationState& state, bool fJustCheck, bool fCheckCbTxMerleRoots);
 bool UndoSpecialTxsInBlock(const CBlock& block, const CBlockIndex* pindex);
+
 
 template <typename T>
 inline bool GetTxPayload(const std::vector<unsigned char>& payload, T& obj)
@@ -31,20 +30,33 @@ inline bool GetTxPayload(const std::vector<unsigned char>& payload, T& obj)
 template <typename T>
 inline bool GetTxPayload(const CMutableTransaction& tx, T& obj)
 {
-    return GetTxPayload(tx.vExtraPayload, obj);
+    return GetTxPayload(CTransaction(tx), obj);
 }
 template <typename T>
 inline bool GetTxPayload(const CTransaction& tx, T& obj)
 {
-    return GetTxPayload(tx.vExtraPayload, obj);
+    std::vector<unsigned char> vchData;
+	int nOut;
+	if (!GetSyscoinData(tx, vchData, nOut))
+		return false;
+    return GetTxPayload(vchData, obj);
 }
 
 template <typename T>
 void SetTxPayload(CMutableTransaction& tx, const T& payload)
 {
+    std::vector<unsigned char> vchData;
+	int nOut;
     CDataStream ds(SER_NETWORK, PROTOCOL_VERSION);
     ds << payload;
-    tx.vExtraPayload.assign(ds.begin(), ds.end());
+    CScript scriptData;
+    scriptData << OP_RETURN << std::vector<unsigned char>(ds.begin(), ds.end());
+    // if opreturn exists update payload
+	if (GetSyscoinData(tx, vchData, nOut))
+        tx.vout[nOut].scriptPubKey = scriptData;
+    // otherwise add a new output with opreturn
+    else
+        tx.vout.push_back(CTxOut(0, scriptData));
 }
 
 uint256 CalcTxInputsHash(const CTransaction& tx);
