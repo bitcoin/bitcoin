0ef97b1b103231db54e04a64bbdb5dcc3f34f482
Pieter Wuille <pieter@wuille.net>
2020-06-18 16:44:32 -0700

    Make MurmurHash3 consume Spans

--- src/hash.h
+++ src/hash.h
@@ -200,7 +200,7 @@ uint256 SerializeHash(const T& obj, int nType=SER_GETHASH, int nVersion=PROTOCOL
     return ss.GetHash();
 }
 
-unsigned int MurmurHash3(unsigned int nHashSeed, const std::vector<unsigned char>& vDataToHash);
+unsigned int MurmurHash3(unsigned int nHashSeed, Span<const unsigned char> vDataToHash);
 
 void BIP32Hash(const ChainCode &chainCode, unsigned int nChild, unsigned char header, const unsigned char data[32], unsigned char output[64]);
 
