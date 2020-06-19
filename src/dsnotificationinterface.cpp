b1d6449a05fd11547540d2f190ae33e31fbc2ee9
sidhujag <jsidhu@blockchainfoundry.co>
2020-06-19 16:34:26 -0700

    update notification interface
    Former-commit-id: 8a0d625c0192856400f2f4e4780b2ee6b4e323be

--- src/dsnotificationinterface.cpp
+++ src/dsnotificationinterface.cpp
@@ -5,20 +5,26 @@
 #include <chainparams.h>
 #include <dsnotificationinterface.h>
 #include <governance/governance.h>
+#include <masternode/masternode-payments.h>
 #include <masternode/masternode-sync.h>
 #include <validation.h>
 
 #include <evo/deterministicmns.h>
 #include <evo/mnauth.h>
 
+#include <llmq/quorums.h>
+#include <llmq/quorums_dkgsessionmgr.h>
+
 void CDSNotificationInterface::InitializeCurrentBlockTip()
 {
     LOCK(cs_main);
-    UpdatedBlockTip(::ChainActive().Tip(), NULL, ::ChainstateActive().IsInitialBlockDownload());
+    SynchronousUpdatedBlockTip(::ChainActive().Tip(), nullptr, IsInitialBlockDownload());
+    UpdatedBlockTip(::ChainActive().Tip(), nullptr, IsInitialBlockDownload());
 }
 
 void CDSNotificationInterface::AcceptedBlockHeader(const CBlockIndex *pindexNew)
 {
+    llmq::chainLocksHandler->AcceptedBlockHeader(pindexNew);
     masternodeSync.AcceptedBlockHeader(pindexNew);
 }
 
@@ -27,21 +33,29 @@ void CDSNotificationInterface::NotifyHeaderTip(const CBlockIndex *pindexNew, boo
     masternodeSync.NotifyHeaderTip(pindexNew, fInitialDownload, connman);
 }
 
-void CDSNotificationInterface::UpdatedBlockTip(const CBlockIndex *pindexNew, const CBlockIndex *pindexFork, bool fInitialDownload)
+void CDSNotificationInterface::SynchronousUpdatedBlockTip(const CBlockIndex *pindexNew, const CBlockIndex *pindexFork, bool fInitialDownload)
 {
     if (pindexNew == pindexFork) // blocks were disconnected without any new ones
         return;
 
     deterministicMNManager->UpdatedBlockTip(pindexNew);
+}
+
+void CDSNotificationInterface::UpdatedBlockTip(const CBlockIndex *pindexNew, const CBlockIndex *pindexFork, bool fInitialDownload)
+{
+    if (pindexNew == pindexFork) // blocks were disconnected without any new ones
+        return;
 
     masternodeSync.UpdatedBlockTip(pindexNew, fInitialDownload, connman);
 
     if (fInitialDownload)
         return;
 
+    llmq::quorumManager->UpdatedBlockTip(pindexNew, fInitialDownload);
+    llmq::quorumDKGSessionManager->UpdatedBlockTip(pindexNew, fInitialDownload);
+
     if (!fLiteMode) governance.UpdatedBlockTip(pindexNew, connman);
 }
-
 
 void CDSNotificationInterface::NotifyMasternodeListChanged(bool undo, const CDeterministicMNList& oldMNList, const CDeterministicMNListDiff& diff)
 {
