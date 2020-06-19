eff89074c42b1a290e53f81fcfdeac41f308a73e
MarcoFalke <falke.marco@gmail.com>
2020-06-19 14:38:10 -0400

    Merge #19309: refactor: Fix link error with --enable-debug
    b83cc0fc94df99f0334430e63e8c9fa6ae3790e1 Fix link error with --enable-debug (Hennadii Stepanov)
    
    Pull request description:
    
      Fixes a link error on master (39bd9ddb8783807b9cde6288233e86ad7c85d61f):
      ```
      $ ./configure --enable-debug
      $ make
      ...
      bitcoin_wallet-bitcoin-wallet.o:(.data.rel.ro+0x0): undefined reference to `InitError(bilingual_str const&)'
      libbitcoin_wallet_tool.a(libbitcoin_wallet_tool_a-wallettool.o):(.data.rel.ro+0x8): undefined reference to `InitError(bilingual_str const&)'
      libbitcoin_wallet.a(libbitcoin_wallet_a-salvage.o):(.data.rel.ro+0x8): undefined reference to `InitError(bilingual_str const&)'
      libbitcoin_wallet.a(libbitcoin_wallet_a-wallet.o):(.data.rel.ro+0x8): undefined reference to `InitError(bilingual_str const&)'
      libbitcoin_wallet.a(libbitcoin_wallet_a-walletdb.o):(.data.rel.ro+0x8): undefined reference to `InitError(bilingual_str const&)'
      libbitcoin_wallet.a(libbitcoin_wallet_a-wallet.o):(.data.rel.ro+0x8): more undefined references to `InitError(bilingual_str const&)' follow
      collect2: error: ld returned 1 exit status
      ```
    
      See:
      - https://github.com/bitcoin/bitcoin/pull/19295#issuecomment-645471771
      - https://github.com/bitcoin/bitcoin/pull/19295#issuecomment-645487182
    
    ACKs for top commit:
      achow101:
        Re-ACK b83cc0fc94df99f0334430e63e8c9fa6ae3790e1
    
    Tree-SHA512: f563d978b6725284049449bb0b3a184d356f32e9b63bcadb0ba71352d3d521af3dbb4a7b4fc0a5a620ed99c357e59f62249c10d0defc0cbe7775f2c06791dabe

--- src/ui_interface.h
+++ src/ui_interface.h
@@ -139,7 +139,7 @@ void InitWarning(const bilingual_str& str);
 
 /** Show error message **/
 bool InitError(const bilingual_str& str);
-constexpr auto AbortError = InitError;
+inline bool AbortError(const bilingual_str& str) { return InitError(str); }
 
 extern CClientUIInterface uiInterface;
 
