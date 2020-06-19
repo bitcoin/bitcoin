fa0f6c58c1c6d10f04c4e65a424cc51ebca50a8c
MarcoFalke <falke.marco@gmail.com>
2020-06-19 16:29:03 -0400

    Revert "Fix link error with --enable-debug"
    This reverts commit b83cc0fc94df99f0334430e63e8c9fa6ae3790e1.

--- src/ui_interface.h
+++ src/ui_interface.h
@@ -122,7 +122,7 @@ void InitWarning(const bilingual_str& str);
 
 /** Show error message **/
 bool InitError(const bilingual_str& str);
-inline bool AbortError(const bilingual_str& str) { return InitError(str); }
+constexpr auto AbortError = InitError;
 
 extern CClientUIInterface uiInterface;
 
