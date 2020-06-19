faa7431fee45b26f7ac2f5fd0b8874cb6afafbd4
MarcoFalke <falke.marco@gmail.com>
2020-06-19 19:19:22 -0400

    refactor: Fix clang compile failure
    script/standard.cpp:278:22: error: default initialization of an object of const type 'const (anonymous namespace)::CScriptVisitor' without a user-provided default constructor
    const CScriptVisitor g_script_visitor;
                         ^
                                         {}
    1 error generated.

--- src/script/standard.cpp
+++ src/script/standard.cpp
@@ -274,14 +274,11 @@ public:
         return CScript() << CScript::EncodeOP_N(id.version) << std::vector<unsigned char>(id.program, id.program + id.length);
     }
 };
-
-const CScriptVisitor g_script_visitor;
-
 } // namespace
 
 CScript GetScriptForDestination(const CTxDestination& dest)
 {
-    return boost::apply_visitor(::g_script_visitor, dest);
+    return boost::apply_visitor(CScriptVisitor{}, dest);
 }
 
 CScript GetScriptForRawPubKey(const CPubKey& pubKey)
