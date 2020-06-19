cb31ee01b42af58c64b5d3c0eabae4bcd7fd67cf
gzhao408 <gzhao408@berkeley.edu>
2020-06-19 09:49:51 -0700

    [test] feefilter during and after IBD
    Co-authored-by: Jon Atack <jon@atack.com>

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -246,6 +246,7 @@ BASE_SCRIPTS = [
     'rpc_help.py',
     'feature_help.py',
     'feature_shutdown.py',
+    'p2p_ibd_txrelay.py',
     # Don't append tests at the end to avoid merge conflicts
     # Put them in a random line within the section that fits their approximate run-time
 ]
