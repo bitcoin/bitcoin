ff39d28dbe7618e63715f13ebfc2a294d98ccd6a
sidhujag <jsidhu@blockchainfoundry.co>
2020-06-20 21:16:25 -0700

    ren
    Former-commit-id: e1d1b506f026ee9ba5aec4a7fe58958c3597ccde

--- src/llmq/quorums_dkgsessionhandler.cpp
+++ src/llmq/quorums_dkgsessionhandler.cpp
@@ -98,7 +98,7 @@ CDKGSessionHandler::CDKGSessionHandler(const Consensus::LLMQParams& _params, ctp
     connman(_connman)
 {
     phaseHandlerThread = std::thread([this] {
-        RenameThread(strprintf("dash-q-phase-%d", (uint8_t)params.type).c_str());
+        RenameThread(strprintf("syscoin-q-phase-%d", (uint8_t)params.type).c_str());
         PhaseHandlerThread();
     });
 }
