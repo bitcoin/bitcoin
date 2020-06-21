d336800e2d4e1a1f9073f1f1e67426fb0e88b62a
sidhujag <jsidhu@blockchainfoundry.co>
2020-06-20 17:09:09 -0700

    in out check
    Former-commit-id: dc70f7b391732efdf64d11b7908624352b964e81

--- src/consensus/tx_check.cpp
+++ src/consensus/tx_check.cpp
@@ -9,11 +9,17 @@
 
 bool CheckTransaction(const CTransaction& tx, TxValidationState& state)
 {
+    // SYSCOIN
+    bool allowEmptyTxInOut = false;
+    if (tx.nVersion == SYSCOIN_TX_VERSION_QUORUM_COMMITMENT) {
+        allowEmptyTxInOut = true;
+    }
+
     // Basic checks that don't depend on any context
-    if (tx.vin.empty())
-        return state.Invalid(TxValidationResult::TX_CONSENSUS, "bad-txns-vin-empty");
-    if (tx.vout.empty())
-        return state.Invalid(TxValidationResult::TX_CONSENSUS, "bad-txns-vout-empty");
+    if (!allowEmptyTxInOut && tx.vin.empty())
+        return state.DoS(10, false, REJECT_INVALID, "bad-txns-vin-empty");
+    if (!allowEmptyTxInOut && tx.vout.empty())
+        return state.DoS(10, false, REJECT_INVALID, "bad-txns-vout-empty");
     // Size limits (this doesn't take the witness into account, as that hasn't been checked for malleability)
     if (::GetSerializeSize(tx, PROTOCOL_VERSION | SERIALIZE_TRANSACTION_NO_WITNESS) * WITNESS_SCALE_FACTOR > MAX_BLOCK_WEIGHT)
         return state.Invalid(TxValidationResult::TX_CONSENSUS, "bad-txns-oversize");
