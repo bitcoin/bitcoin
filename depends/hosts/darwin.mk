670da38f2489346ccb444fb6dc2852286a6b0001
Cory Fields <cory-nospam-@coryfields.com>
2020-06-22 16:46:16 -0400

    darwin: pass mlinker-version so that clang enables new features
    Without this clang fails to add any newly-added linker features.
    
    Removing this in ca5055a5aa07aba81a87cf12f6f0526a63c423b5 was likely a
    regression.
    
    See https://github.com/bitcoin/bitcoin/pull/19240#issuecomment-647764049
    for more discussion.
    
    
    Former-commit-id: 745cef00f3070280acd1813fc33ba52ed6dd7a29

--- depends/hosts/darwin.mk
+++ depends/hosts/darwin.mk
@@ -2,9 +2,11 @@ OSX_MIN_VERSION=10.12
 OSX_SDK_VERSION=10.15.1
 XCODE_VERSION=11.3.1
 XCODE_BUILD_ID=11C505
+LD64_VERSION=530
+
 OSX_SDK=$(SDK_PATH)/Xcode-$(XCODE_VERSION)-$(XCODE_BUILD_ID)-extracted-SDK-with-libcxx-headers
-darwin_CC=clang -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK)
-darwin_CXX=clang++ -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK) -stdlib=libc++
+darwin_CC=clang -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION)
+darwin_CXX=clang++ -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK) -stdlib=libc++ -mlinker-version=$(LD64_VERSION)
 
 darwin_CFLAGS=-pipe
 darwin_CXXFLAGS=$(darwin_CFLAGS)
