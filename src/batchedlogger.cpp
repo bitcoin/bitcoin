cea6bf5bbf3caa1ca7a92998ffe310b2fcfdf715
sidhujag <jsidhu@blockchainfoundry.co>
2020-06-22 10:54:54 -0700

    add batched logger
    Former-commit-id: 4c8bd6ea5972a0e52bca1aab6433ad589a7ce95b

---
+++ src/batchedlogger.cpp
@@ -0,0 +1,25 @@
+// Copyright (c) 2018-2019 The Dash Core developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <batchedlogger.h>
+#include <logging.h>
+
+CBatchedLogger::CBatchedLogger(uint64_t _category, const std::string& _header) :
+    accept(LogAcceptCategory(_category)), header(_header)
+{
+}
+
+CBatchedLogger::~CBatchedLogger()
+{
+    Flush();
+}
+
+void CBatchedLogger::Flush()
+{
+    if (!accept || msg.empty()) {
+        return;
+    }
+    LogPrintStr(strprintf("%s:\n%s", header, msg));
+    msg.clear();
+}
