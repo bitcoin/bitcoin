95df107568fe9c8cebc8e77ffc9db802cb621fca
sidhujag <jsidhu@blockchainfoundry.co>
2020-06-22 13:41:34 -0700

    add ! operator
    Former-commit-id: 46cdd2e0ab3a7737ef0264c5a2a64d5e367363b5

--- src/arith_uint256.h
+++ src/arith_uint256.h
@@ -245,6 +245,13 @@ public:
         return pn[0] | (uint64_t)pn[1] << 32;
     }
     // SYSCOIN
+    bool operator!() const
+    {
+        for (int i = 0; i < WIDTH; i++)
+            if (pn[i] != 0)
+                return false;
+        return true;
+    }
     uint32_t GetLow32() const
     {
         static_assert(WIDTH >= 1, "Assertion WIDTH >= 1 failed (WIDTH = BITS / 32). BITS is a template parameter.");
@@ -259,7 +266,6 @@ public:
     arith_uint256(const base_uint<256>& b) : base_uint<256>(b) {}
     arith_uint256(uint64_t b) : base_uint<256>(b) {}
     explicit arith_uint256(const std::string& str) : base_uint<256>(str) {}
-
     /**
      * The "compact" format is a representation of a whole
      * number N using an unsigned 32bit number similar to a
