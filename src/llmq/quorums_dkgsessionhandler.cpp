7f4433849cb3ac300fb91aa4444535c106dc036f
Alexander Block <ablock84@gmail.com>
2020-06-25 09:47:24 +0200

    Disable new connection handling and concentrated recovery for large LLMQs (#3548)
    * Rename CollectSigSharesToSend to CollectSigSharesToSendConcentrated
    
    * Allow to disable concentrated recovery and new connection handling for large LLMQs
    
    
    Former-commit-id: f410e08a908435e766b41209072553f28881f696

--- src/llmq/quorums_dkgsessionhandler.cpp
+++ src/llmq/quorums_dkgsessionhandler.cpp
@@ -533,7 +533,7 @@ void CDKGSessionHandler::HandleDKGRound()
     });
 
     CLLMQUtils::EnsureQuorumConnections(params.type, pindexQuorum, curSession->myProTxHash, gArgs.GetBoolArg("-watchquorums", DEFAULT_WATCH_QUORUMS), connman);
-    if (curSession->AreWeMember() && sporkManager.IsSporkActive(SPORK_21_QUORUM_ALL_CONNECTED)) {
+    if (curSession->AreWeMember() && CLLMQUtils::IsAllMembersConnectedEnabled(params.type)) {
         CLLMQUtils::AddQuorumProbeConnections(params.type, pindexQuorum, curSession->myProTxHash, connman);
     }
 
