cb8ab1d6b66b71426397d97eaa122d750a2a01fc
fanquake <fanquake@gmail.com>
2020-06-25 10:25:23 +0800

    build: pass _WIN32_WINNT=0x0601 when building libevent for Windows
    This enables of the use of AI_* definitions in the Windows headers,
    specifically AI_ADDRCONFIG, which fixes an issue with libevent and
    ipv6 on Windows.
    
    It also aligns with what we define in configure when building Core.
    
    
    Former-commit-id: 3840427ce4e213b4d654c6379878be0336e3f7ad

--- depends/packages/libevent.mk
+++ depends/packages/libevent.mk
@@ -10,12 +10,16 @@ define $(package)_preprocess_cmds
   ./autogen.sh
 endef
 
+# When building for Windows, we set _WIN32_WINNT to target the same Windows
+# version as we do in configure. Due to quirks in libevents build system, this
+# is also required to enable support for ipv6. See #19375.
 define $(package)_set_vars
   $(package)_config_opts=--disable-shared --disable-openssl --disable-libevent-regress --disable-samples
   $(package)_config_opts += --disable-dependency-tracking --enable-option-checking
   $(package)_config_opts_release=--disable-debug-mode
   $(package)_config_opts_linux=--with-pic
   $(package)_config_opts_android=--with-pic
+  $(package)_cppflags_mingw32=-D_WIN32_WINNT=0x0601
 endef
 
 define $(package)_config_cmds
