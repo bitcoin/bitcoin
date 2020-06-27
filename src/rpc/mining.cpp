afad5a9bd052c42213a8e2ea2df54fdd7a640d3a
sidhujag <jsidhu@blockchainfoundry.co>
2020-06-26 18:56:01 -0700

    compile
    Former-commit-id: 54aac29c4706c852d8c0dbb179142aaa4c5d0b92

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -935,7 +935,7 @@ static UniValue getblocktemplate(const JSONRPCRequest& request)
     result.pushKV("superblocks_enabled", sporkManager.IsSporkActive(SPORK_9_SUPERBLOCKS_ENABLED));
     std::vector<unsigned char> vchData;
 	int nOut;
-	if (GetSyscoinData(*pblock->vtx[0], vchData, nOut, SYSCOIN_TX_VERSION_MN_COINBASE))
+	if (GetSyscoinData(*pblock->vtx[0], vchData, nOut))
         result.pushKV("coinbase_payload", HexStr(vchData));
     return result;
 }
