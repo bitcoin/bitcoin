1d69e67e3cf40eca326db467164a4899d20cd26b
sidhujag <jsidhu@blockchainfoundry.co>
2020-06-28 11:33:13 -0700

    fix test
    Former-commit-id: 203d17c1fe4b192d4fcc1a0532552f1406e815df

--- test/functional/feature_assumevalid.py
+++ test/functional/feature_assumevalid.py
@@ -61,7 +61,7 @@ class AssumeValidTest(SyscoinTestFramework):
         self.num_nodes = 3
         self.rpc_timeout = 120
         # Must set '-dip3params=9000:9000' to create pre-dip3 blocks only
-        self.extra_args = ['-dip3params=9000:9000']
+        self.extra_args = [['-dip3params=9000:9000'],['-dip3params=9000:9000'],['-dip3params=9000:9000']]
 
     def setup_network(self):
         self.add_nodes(3, extra_args=self.extra_args)
