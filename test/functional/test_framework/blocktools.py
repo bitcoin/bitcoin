3e2292b032394282746d92a53b803fc8bd64d2e5
sidhujag <jsidhu@blockchainfoundry.co>
2020-06-27 20:49:51 -0700

    fix createcb
    Former-commit-id: c1a8ec128987857cf3600c22a52c29623e5548a9

--- test/functional/test_framework/blocktools.py
+++ test/functional/test_framework/blocktools.py
@@ -118,7 +118,7 @@ def script_BIP34_coinbase_height(height):
     return CScript([CScriptNum(height)])
 
 # SYSCOIN
-def create_coinbase(height, pubkey=None, dip4_activated=False, witness=witness):
+def create_coinbase(height, pubkey=None, dip4_activated=False, witness=None):
     """Create a coinbase transaction, assuming no miner fees.
 
     If pubkey is passed in, the coinbase output will be a P2PK output;
@@ -138,7 +138,7 @@ def create_coinbase(height, pubkey=None, dip4_activated=False, witness=witness):
     if dip4_activated:
         coinbase.nVersion = SYSCOIN_TX_VERSION_MN_COINBASE
         cbtx_payload = CCbTx(2, height, 0, 0)
-        if witness:
+        if witness is not None:
             coinbase.extraData = cbtx_payload.serialize()
         else:
             coinbase.vout.append(CTxOut(0, CScript([OP_RETURN, cbtx_payload.serialize()])))
