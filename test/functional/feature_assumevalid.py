32dba34849a9ab285b43b8cdd3afd85279525847
sidhujag <jsidhu@blockchainfoundry.co>
2020-06-28 08:46:41 -0700

    move dipactivated into createcb
    Former-commit-id: 5c489e353a49af061f25f747090252bf7bce8e09

--- test/functional/feature_assumevalid.py
+++ test/functional/feature_assumevalid.py
@@ -150,9 +150,7 @@ class AssumeValidTest(SyscoinTestFramework):
 
         # Bury the assumed valid block 2100 deep
         for i in range(2100):
-            # Syscoin
-            dip4_activated = height >= 432
-            block = create_block(self.tip, create_coinbase(height, dip4_activated=dip4_activated), self.block_time)
+            block = create_block(self.tip, create_coinbase(height), self.block_time)
             block.set_base_version(4)
             block.solve()
             self.blocks.append(block)
