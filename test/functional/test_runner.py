d4645aaeb6df72474831ae3c94515a76876184dd
sidhujag <jsidhu@blockchainfoundry.co>
2020-06-29 11:46:43 -0700

    add new tests to runner
    Former-commit-id: 44411f8b130a73b4fe67938e2f71b9e740a50c1a

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -250,6 +250,15 @@ BASE_SCRIPTS = [
     'rpc_help.py',
     'feature_help.py',
     'feature_shutdown.py',
+    'sporks.py',
+    'multikeysporks.py',
+    'llmq-signing.py',
+    'llmq-signing.py --spork21',
+    'llmq-connections.py',
+    'llmq-simplepose.py',
+    'llmq-dkgerrors.py',
+    'dip3-deterministicmns.py',
+    'dip4-coinbasemerkleroots.py',
     # Don't append tests at the end to avoid merge conflicts
     # Put them in a random line within the section that fits their approximate run-time
 
