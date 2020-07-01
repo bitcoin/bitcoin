a3e1f988eba3be54b2f9ca0b0a4b51e3d41dc401
sidhujag <jsidhu@blockchainfoundry.co>
2020-07-01 14:36:50 -0700

    add hash256
    Former-commit-id: 0ea1ddc94337d6455933b19513cfd4ca082a8926

--- test/functional/feature_coinbasemerkleroots.py
+++ test/functional/feature_coinbasemerkleroots.py
@@ -5,7 +5,7 @@
 from test_framework.mininode import *
 from test_framework.test_framework import DashTestFramework
 from test_framework.util import assert_equal
-from test_framework.messages import FromHex, CMerkleBlock, ser_uint256
+from test_framework.messages import FromHex, CMerkleBlock, ser_uint256, hash256
 
 '''
 dip4-coinbasemerkleroots.py
