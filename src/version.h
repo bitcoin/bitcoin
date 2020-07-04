53443460e2d575d08cecc9e71e933d149516a821
sidhujag <jsidhu@blockchainfoundry.co>
2020-07-03 19:20:42 -0700

    create min mn version
    Former-commit-id: 199df1de48b424f74fc321e16ad9037bca3643ad

--- src/version.h
+++ src/version.h
@@ -23,6 +23,10 @@ static const int GETHEADERS_VERSION = 31800;
 //! disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION = GETHEADERS_VERSION;
 
+// SYSCOIN
+//! minimum proto version of masternode to accept in DKGs
+static const int MIN_MASTERNODE_PROTO_VERSION = 70015;
+
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
 static const int CADDR_TIME_VERSION = 31402;
