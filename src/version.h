d1bf2d6fce33198718dad3dd4a22c1ad7c73dc85
sidhujag <jsidhu@blockchainfoundry.co>
2020-07-03 18:53:01 -0700

    update min peer
    Former-commit-id: 2d29acff5e971ed0869aabf33713507de7e7ef0b

--- src/version.h
+++ src/version.h
@@ -21,7 +21,7 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 31800;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = GETHEADERS_VERSION;
+static const int MIN_PEER_PROTO_VERSION = PROTOCOL_VERSION;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
