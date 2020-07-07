c936dfc88680a937a380662ab87bbb1021ead07d
sidhujag <jsidhu@blockchainfoundry.co>
2020-07-07 07:26:19 -0700

    update test
    Former-commit-id: 6133c17a19de8f36968540a685d45ec56f1d45d3

--- test/functional/mempool_compatibility.py
+++ test/functional/mempool_compatibility.py
@@ -15,10 +15,10 @@ Only v0.15.2 is required by this test. The rest is used in other backwards compa
 
 import os
 
-from test_framework.test_framework import BitcoinTestFramework
+from test_framework.test_framework import SyscoinTestFramework
 
 
-class MempoolCompatibilityTest(BitcoinTestFramework):
+class MempoolCompatibilityTest(SyscoinTestFramework):
     def set_test_params(self):
         self.num_nodes = 2
 
