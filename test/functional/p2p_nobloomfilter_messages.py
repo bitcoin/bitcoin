hebasto/bitcoin-qtw

[test] logging and style followups for bloomfilter tests
-Use peer to refer to mininodes instead of node
because they are not bitcoind nodes.
-Use log.debug for logs that give helpful but
not super necessary information.
-Adhere to style guidelines (newlines, capitalization).

test/functional/p2p_nobloomfilter_messages.py
@@ -4,7 +4,7 @@
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 """Test invalid p2p messages for nodes with bloom filters disabled.
 
-Test that, when bloom filters are not enabled, nodes are disconnected if:
+Test that, when bloom filters are not enabled, peers are disconnected if:
 1. They send a p2p mempool message
 2. They send a p2p filterload message
 3. They send a p2p filteradd message
@@ -17,31 +17,32 @@ from test_framework.test_framework import BitcoinTestFramework
 from test_framework.util import assert_equal
 
 
-class P2PNobloomfilterMessages(BitcoinTestFramework):
+class P2PNoBloomFilterMessages(BitcoinTestFramework):
     def set_test_params(self):
         self.setup_clean_chain = True
         self.num_nodes = 1
         self.extra_args = [["-peerbloomfilters=0"]]
 
     def test_message_causes_disconnect(self, message):
-        # Add a p2p connection that sends a message and check that it disconnects
+        """Add a p2p connection that sends a message and check that it disconnects."""
         peer = self.nodes[0].add_p2p_connection(P2PInterface())
         peer.send_message(message)
         peer.wait_for_disconnect()
-        assert_equal(len(self.nodes[0].getpeerinfo()), 0)
+        assert_equal(self.nodes[0].getconnectioncount(), 0)
 
     def run_test(self):
-        self.log.info("Test that node is disconnected if it sends mempool message")
+        self.log.info("Test that peer is disconnected if it sends mempool message")
         self.test_message_causes_disconnect(msg_mempool())
 
-        self.log.info("Test that node is disconnected if it sends filterload message")
+        self.log.info("Test that peer is disconnected if it sends filterload message")
         self.test_message_causes_disconnect(msg_filterload())
 
-        self.log.info("Test that node is disconnected if it sends filteradd message")
+        self.log.info("Test that peer is disconnected if it sends filteradd message")
         self.test_message_causes_disconnect(msg_filteradd(data=b'\xcc'))
 
         self.log.info("Test that peer is disconnected if it sends a filterclear message")
         self.test_message_causes_disconnect(msg_filterclear())
 
+
 if __name__ == '__main__':
-    P2PNobloomfilterMessages().main()
+    P2PNoBloomFilterMessages().main()

bitcoin/bitcoin

Former-commit-id: 33b015ae7a8034b9c45d0e212fd61a0895c3da24

test/functional/p2p_nobloomfilter_messages.py
@@ -4,7 +4,7 @@
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 """Test invalid p2p messages for nodes with bloom filters disabled.
 
-Test that, when bloom filters are not enabled, nodes are disconnected if:
+Test that, when bloom filters are not enabled, peers are disconnected if:
 1. They send a p2p mempool message
 2. They send a p2p filterload message
 3. They send a p2p filteradd message
@@ -17,31 +17,32 @@ from test_framework.test_framework import SyscoinTestFramework
 from test_framework.util import assert_equal
 
 
-class P2PNobloomfilterMessages(SyscoinTestFramework):
+class P2PNoBloomFilterMessages(SyscoinTestFramework):
     def set_test_params(self):
         self.setup_clean_chain = True
         self.num_nodes = 1
         self.extra_args = [["-peerbloomfilters=0"]]
 
     def test_message_causes_disconnect(self, message):
-        # Add a p2p connection that sends a message and check that it disconnects
+        """Add a p2p connection that sends a message and check that it disconnects."""
         peer = self.nodes[0].add_p2p_connection(P2PInterface())
         peer.send_message(message)
         peer.wait_for_disconnect()
-        assert_equal(len(self.nodes[0].getpeerinfo()), 0)
+        assert_equal(self.nodes[0].getconnectioncount(), 0)
 
     def run_test(self):
-        self.log.info("Test that node is disconnected if it sends mempool message")
+        self.log.info("Test that peer is disconnected if it sends mempool message")
         self.test_message_causes_disconnect(msg_mempool())
 
-        self.log.info("Test that node is disconnected if it sends filterload message")
+        self.log.info("Test that peer is disconnected if it sends filterload message")
         self.test_message_causes_disconnect(msg_filterload())
 
-        self.log.info("Test that node is disconnected if it sends filteradd message")
+        self.log.info("Test that peer is disconnected if it sends filteradd message")
         self.test_message_causes_disconnect(msg_filteradd(data=b'\xcc'))
 
         self.log.info("Test that peer is disconnected if it sends a filterclear message")
         self.test_message_causes_disconnect(msg_filterclear())
 
+
 if __name__ == '__main__':
-    P2PNobloomfilterMessages().main()
+    P2PNoBloomFilterMessages().main()