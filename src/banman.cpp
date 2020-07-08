0b8ba84659b56b12d74587ea31b6062fce887ba3
fanquake <fanquake@gmail.com>
2020-07-08 21:44:45 +0800

    banlist: log post-swept banlist size at startup
    We are currently logging the size of the banlist before SweepBanned()
    has been called, meaning the value may be incorrect.

--- src/banman.cpp
+++ src/banman.cpp
@@ -26,7 +26,7 @@ BanMan::BanMan(fs::path ban_file, CClientUIInterface* client_interface, int64_t 
         SweepBanned();            // sweep out unused entries
 
         LogPrint(BCLog::NET, "Loaded %d banned node ips/subnets from banlist.dat  %dms\n",
-            banmap.size(), GetTimeMillis() - n_start);
+            m_banned.size(), GetTimeMillis() - n_start);
     } else {
         LogPrintf("Invalid or missing banlist.dat; recreating\n");
         SetBannedSetDirty(true); // force write
