e959d3801d59e3e7b9a934b22d1e052721c20f03
sidhujag <jsidhu@blockchainfoundry.co>
2020-07-07 18:33:36 -0700

    compile
    Former-commit-id: e580ebc5acadaa28bdc6e129b65ff5605f00b7f4

--- src/bloom.h
+++ src/bloom.h
@@ -14,7 +14,7 @@ class CTransaction;
 class uint256;
 // SYSCOIN
 class CScript;
-class uint160;
+class CKeyID;
 //! 20,000 items with fp rate < 0.1% or 10,000 items and <0.0001%
 static const unsigned int MAX_BLOOM_FILTER_SIZE = 36000; // bytes
 static const unsigned int MAX_HASH_FUNCS = 50;
