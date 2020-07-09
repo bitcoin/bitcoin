67bfce2c89a357e14e721e08147ae3f8cba3f9bf
fanquake <fanquake@gmail.com>
2020-07-09 20:33:52 +0800

    Merge #18896: qt: Reset toolbar after all wallets are closed
    1e9bfd4926a3cbb9db877c773a60b0bbbbe8bde0 qt: Reset toolbar after all wallets are closed (Hennadii Stepanov)
    
    Pull request description:
    
      If the last open wallet is closed from the non-"Overview" tab, that tab remains active when a new wallet is opened:
    
      ![Screenshot from 2020-05-06 09-00-26](https://user-images.githubusercontent.com/32963518/81142394-46821880-8f78-11ea-84b5-1d02f2b7f3f1.png)
    
      This PR fixes this bug.
    
    ACKs for top commit:
      promag:
        Code review ACK 1e9bfd4926a3cbb9db877c773a60b0bbbbe8bde0.
      luke-jr:
        utACK 1e9bfd4926a3cbb9db877c773a60b0bbbbe8bde0
      jonasschnelli:
        utACK 1e9bfd4926a3cbb9db877c773a60b0bbbbe8bde0
    
    Tree-SHA512: a8dfd7591267e9544ad40c87581d554e5cfaad4b2a5bbfdbaf2596dc6869d2ac6cf7877adfef3d528fc61b081d40c6e30d787bbd7280ef7946aa7f7d9bc8b18e
    
    Former-commit-id: eee4cc3d8478e16edb8f3d63d07fc103bc22a31a

--- src/qt/syscoingui.cpp
+++ src/qt/syscoingui.cpp
@@ -718,6 +718,7 @@ void SyscoinGUI::removeWallet(WalletModel* walletModel)
     m_wallet_selector->removeItem(index);
     if (m_wallet_selector->count() == 0) {
         setWalletActionsEnabled(false);
+        overviewAction->setChecked(true);
     } else if (m_wallet_selector->count() == 1) {
         m_wallet_selector_label_action->setVisible(false);
         m_wallet_selector_action->setVisible(false);
