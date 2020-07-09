f53dea9dcd0976b20ed37d91f07d4985245e8507
UdjinM6 <UdjinM6@users.noreply.github.com>
2020-07-09 12:31:01 +0300

    More of 12728
    Former-commit-id: dda6d5f316d6b78e68727a17f2f150d7da78746c

--- test/functional/feature_coinbasemerkleroots.py
+++ test/functional/feature_coinbasemerkleroots.py
@@ -14,7 +14,7 @@ Checks DIP4 merkle roots in coinbases
 
 '''
 
-class TestNode(P2PInterface):
+class TestP2PConn(P2PInterface):
     def __init__(self):
         super().__init__()
         self.last_mnlistdiff = None
@@ -40,7 +40,7 @@ class LLMQCoinbaseCommitmentsTest(DashTestFramework):
         self.set_dash_test_params(4, 3, fast_dip3_enforcement=True)
 
     def run_test(self):
-        self.test_node = self.nodes[0].add_p2p_connection(TestNode())
+        self.test_node = self.nodes[0].add_p2p_connection(TestP2PConn())
         self.nodes[0].p2p.wait_for_verack()
 
         self.confirm_mns()
