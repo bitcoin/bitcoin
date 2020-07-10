c0e489e8f5dbdf11fa18e9085618b5d35c686a42
MarcoFalke <falke.marco@gmail.com>
2020-07-10 16:06:11 +0200

    Merge #18638: net: Use mockable time for ping/pong, add tests
    fa3365430c5fb57d7c0b5f2bce9fbbe290be93c3 net: Use mockable time for ping/pong, add tests (MarcoFalke)
    faab4aaf2fa1153c6d76efc8113fa01b06943ece util: Add count_microseconds helper (MarcoFalke)
    
    Pull request description:
    
      Switch `CNode::m_ping_start` and `CNetMessage::m_time` to mockable time, so that tests can be added.
    
      Mockable time is also type-safe, since it uses `std::chrono`
    
    ACKs for top commit:
      jonatack:
        Code review re-ACK fa33654 re-read code, verified rebase per `git range-diff 4b5c919 fa94d6f fa33654`, previous tested ACKs still valid
      troygiorshev:
        ACK fa3365430c5fb57d7c0b5f2bce9fbbe290be93c3
    
    Tree-SHA512: 7d632bd6019ce7c882029e71b667a61517e783af82755a85dd979ef09380934e172dec8b8f91d57b200a30a6e096aeaf01f19fee7f3aed0e0e871c72eb44d70e
    
    Former-commit-id: c5d9d2b110dcf067a4fe277c5e36d5ece3447adf

--- test/functional/feature_maxuploadtarget.py
+++ test/functional/feature_maxuploadtarget.py
@@ -36,7 +36,11 @@ class MaxUploadTest(SyscoinTestFramework):
         self.setup_clean_chain = True
         self.num_nodes = 1
         # SYSCOIN
-        self.extra_args = [["-maxuploadtarget=32000", "-acceptnonstdtxn=1"]]
+        self.extra_args = [[
+            "-maxuploadtarget=32000",
+            "-acceptnonstdtxn=1",
+            "-peertimeout=9999",  # bump because mocktime might cause a disconnect otherwise
+        ]]
         self.supports_cli = False
 
         # Cache for utxos, as the listunspent may take a long time later in the test
