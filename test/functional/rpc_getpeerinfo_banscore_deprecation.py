7fbc875159108a9b43b2cac309ac0593cfbb39f4
MarcoFalke <falke.marco@gmail.com>
2020-07-10 16:06:11 +0200

    Merge #18638: net: Use mockable time for ping/pong, add tests
    fa3365430c5fb57d7c0b5f2bce9fbbe290be93c3 net: Use mockable time for ping/pong, add tests (MarcoFalke)
    faab4aaf2fa1153c6d76efc8113fa01b06943ece util: Add count_microseconds helper (MarcoFalke)
    
    Pull request description:
    
      Switch `CNode::m_ping_start` and `CNetMessage::m_time` to mockable time, so that tests can be added.
    
      Mockable time is also type-safe, since it uses `std::chrono`
    
    ACKs for top commit:
      jonatack:
        Code review re-ACK fa33654 re-read code, verified rebase per `git range-diff 4b5c919 fa94d6f fa33654`, previous tested ACKs still valid
      troygiorshev:
        ACK fa3365430c5fb57d7c0b5f2bce9fbbe290be93c3
    
    Tree-SHA512: 7d632bd6019ce7c882029e71b667a61517e783af82755a85dd979ef09380934e172dec8b8f91d57b200a30a6e096aeaf01f19fee7f3aed0e0e871c72eb44d70e
    
    Former-commit-id: 7be177178ab03444071ad2b85633cccb4088d684

--- test/functional/rpc_getpeerinfo_banscore_deprecation.py
+++ test/functional/rpc_getpeerinfo_banscore_deprecation.py
@@ -4,10 +4,10 @@
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 """Test deprecation of getpeerinfo RPC banscore field."""
 
-from test_framework.test_framework import BitcoinTestFramework
+from test_framework.test_framework import SyscoinTestFramework
 
 
-class GetpeerinfoBanscoreDeprecationTest(BitcoinTestFramework):
+class GetpeerinfoBanscoreDeprecationTest(SyscoinTestFramework):
     def set_test_params(self):
         self.num_nodes = 2
         self.extra_args = [[], ["-deprecatedrpc=banscore"]]
