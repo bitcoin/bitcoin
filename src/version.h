7bb6f9bfdbd3b0b5594febc9a0ae2f84e6d7add1
John Newbery <john@johnnewbery.com>
2020-07-10 16:52:05 +0100

    [protocol] Remove unused GETHEADERS_VERSION
    GETHEADERS_VERSION is unused. No need to keep it in the codebase
    for just its historic comment.

--- src/version.h
+++ src/version.h
@@ -14,11 +14,8 @@ static const int PROTOCOL_VERSION = 70015;
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
-//! In this version, 'getheaders' was introduced.
-static const int GETHEADERS_VERSION = 31800;
-
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = GETHEADERS_VERSION;
+static const int MIN_PEER_PROTO_VERSION = 31800;
 
 //! BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
