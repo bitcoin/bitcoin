77c4649f5025b5631fbadc065bda723b938390a8
sidhujag <jsidhu@blockchainfoundry.co>
2020-07-11 20:53:46 -0700

    asset_description_too_big test
    Former-commit-id: c2b2e033b2b0bfbf9eae8af9cabcccd14cd706e9

--- test/functional/feature_asset.py
+++ test/functional/feature_asset.py
@@ -16,6 +16,10 @@ class AssetTest(SyscoinTestFramework):
 
     def run_test(self):
         self.nodes[0].generate(200)
+        self.basic_asset()
+        self.asset_description_too_big()
+
+    def basic_asset(self):
         asset = self.nodes[0].assetnew('1', 'TST', 'asset description', '0x', 8, '1000', '10000', 31, {})['asset_guid']
         self.sync_all()
         self.nodes[1].generate(3)
@@ -25,6 +29,25 @@ class AssetTest(SyscoinTestFramework):
         assetInfo = self.nodes[1].assetinfo(asset)
         assert_equal(assetInfo['asset_guid'], asset)
 
+    def asset_description_too_big(self):
+        # 480 bytes long
+        gooddata = "SfsddfdfsdsdffsdfdfsdsfDsdsdsdsfsfsdsfsdsfdsfsdsfdsfsdsfsdSfsdfdfsdsfSfsdfdfsdsfDsdsdsdsfsfsdsfsdsfdsfsdsfdsfsdsfsdSfsdfdfsdsfSfsdfdfsdsfDsdsdsdsfsfsdsfsdsfdsfsdsfdsfsdsfsdSfsdfdfsdsfSfsdfdfsdsfDsdsdsdsfsfsdsfsdsfdsfsdsfdsfsdsfsdSfsdfdfsdsfSfsdfdfsdsDfdfddSfsddfdfsdsfSfsdfdfsdsfDsdsdsdsfsfsdsfsdsfdsfsdsfdsfsdsfsdSfsdfdfsdsfSfsdfdfsdsfDsdsdsdsfsfsdsfsdsfdsfsdsfdsfsdsfsdSfsdfdfsdsfSfsdfdfsdsfDsdsdsdsfsfsdsfsdsfdsfsdsfdsfsdsfsdSfsdfdfsdsfSfsdfdfsdsfDsdsdsdsfsfsdsfsdsfdssfsddSfdd";
+        # 481 bytes long (makes 513 with the pubdata overhead)
+        baddata = gooddata + "a"
+        asset = self.nodes[0].assetnew('1', 'TST', gooddata, '0x', 8, '1000', '10000', 31, {})['asset_guid']
+        asset1 = self.nodes[0].assetnew('1', 'TST', gooddata, '0x', 8, '1000', '10000', 31, {})['asset_guid']
+
+        assetInfo = self.nodes[0].assetinfo(asset)
+        assert_equal(assetInfo['asset_guid'], asset)
+        assetInfo = self.nodes[1].assetinfo(asset)
+        assert_equal(assetInfo['asset_guid'], asset)
+        assetInfo = self.nodes[0].assetinfo(asset1)
+        assert_equal(assetInfo['asset_guid'], asset1)
+        assetInfo = self.nodes[1].assetinfo(asset1)
+        assert_equal(assetInfo['asset_guid'], asset1)
+
+        # data too big
+        assert_raises_rpc_error(-1, "asset-pubdata-too-big", self.nodes[0].assetnew, '1', 'TST', baddata, '0x', 8, '1000', '10000', 31, {})
 
 if __name__ == '__main__':
     AssetTest().main()
