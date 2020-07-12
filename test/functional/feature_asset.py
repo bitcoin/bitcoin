1941b0bbdc356245eff4d66cc44d18a2396a5493
sidhujag <jsidhu@blockchainfoundry.co>
2020-07-11 20:36:32 -0700

    fix test
    Former-commit-id: d994630c383a0c1e7402269afd2fdea496ed7cfc

--- test/functional/feature_asset.py
+++ test/functional/feature_asset.py
@@ -4,8 +4,7 @@
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 from test_framework.test_framework import SyscoinTestFramework
-from test_framework.util import wait_until, get_datadir_path, connect_nodes
-import os
+from test_framework.util import assert_equal
 
 
 class AssetTest(SyscoinTestFramework):
@@ -17,7 +16,6 @@ class AssetTest(SyscoinTestFramework):
 
     def run_test(self):
         self.nodes[0].generate(200)
-        self.sync_all()
         asset = self.nodes[0].assetnew('1', 'TST', 'asset description', '0x', 8, '1000', '10000', 31, {})['asset_guid']
         self.sync_all()
         self.nodes[1].generate(3)
