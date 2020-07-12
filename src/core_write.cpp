61c3b7ebdcc1f58f11b46c9d4f9a4d67ab9d4cb8
sidhujag <jsidhu@blockchainfoundry.co>
2020-07-12 09:39:45 -0700

    output allocations in mintsys json output
    Former-commit-id: 34fd865143c576739bd18401a101a7f76266392d

--- src/core_write.cpp
+++ src/core_write.cpp
@@ -107,7 +107,26 @@ bool AssetMintTxToJson(const CTransaction& tx, const uint256& txHash, const uint
         oSPVProofObj.__pushKV("receiptparentnodes", HexStr(mintSyscoin.vchReceiptParentNodes)); 
         oSPVProofObj.__pushKV("receiptroot", HexStr(mintSyscoin.vchReceiptRoot)); 
         oSPVProofObj.__pushKV("ethblocknumber", mintSyscoin.nBlockNumber); 
-        entry.__pushKV("spv_proof", oSPVProofObj); 
+        entry.__pushKV("spv_proof", oSPVProofObj);
+        UniValue oAssetAllocationReceiversArray(UniValue::VARR);
+        for(const auto &it: mintSyscoin.voutAssets) {
+            CAmount nTotal = 0;
+            UniValue oAssetAllocationReceiversObj(UniValue::VOBJ);
+            const uint32_t &nAsset = it.first;
+            oAssetAllocationReceiversObj.__pushKV("asset_guid", nAsset);
+            UniValue oAssetAllocationReceiverOutputsArray(UniValue::VARR);
+            for(const auto& voutAsset: it.second){
+                nTotal += voutAsset.nValue;
+                UniValue oAssetAllocationReceiverOutputObj(UniValue::VOBJ);
+                oAssetAllocationReceiverOutputObj.__pushKV("n", voutAsset.n);
+                oAssetAllocationReceiverOutputObj.__pushKV("amount", voutAsset.nValue);
+                oAssetAllocationReceiverOutputsArray.push_back(oAssetAllocationReceiverOutputObj);
+            }
+            oAssetAllocationReceiversObj.__pushKV("outputs", oAssetAllocationReceiverOutputsArray); 
+            oAssetAllocationReceiversObj.__pushKV("total", nTotal);
+            oAssetAllocationReceiversArray.push_back(oAssetAllocationReceiversObj);
+        }
+        entry.__pushKV("allocations", oAssetAllocationReceiversArray);
         return true;
     } 
     return false;
