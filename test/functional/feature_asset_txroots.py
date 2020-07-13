cf345822df33d735405591bcb60ddd547505a74b
sidhujag <jsidhu@blockchainfoundry.co>
2020-07-13 12:34:29 -0700

    fix assert
    Former-commit-id: e56b70dd41db033918bff21668c4a6ef72378f1b

--- test/functional/feature_asset_txroots.py
+++ test/functional/feature_asset_txroots.py
@@ -93,7 +93,7 @@ class AssetTxRootsTest(SyscoinTestFramework):
         self.nodes[0].syscoinsetethheaders([[814011,'814011','814010','a','a',0],[700059,'700059','700058','a','a',0],[800022,'800022','800021','a','a',0]])
         r = self.nodes[0].syscoinsetethstatus("synced", 820000)
         missing_blocks = r["missing_blocks"]
-        assert_equal(missing_blocks, False)
+        assert_equal(missing_blocks, [])
 
 
 if __name__ == '__main__':
