f7141967c291a41fc38bfffe29a56cffef50ae1f
sidhujag <jsidhu@blockchainfoundry.co>
2020-07-13 12:06:22 -0700

    fix test use str
    Former-commit-id: 99eb5558aea0cddd59681ee49ea7e49af84a7870

--- test/functional/feature_asset_txroots.py
+++ test/functional/feature_asset_txroots.py
@@ -19,7 +19,7 @@ class AssetTxRootsTest(SyscoinTestFramework):
         self.basic_audittxroot1()
 
     def basic_audittxroot(self):
-        self.nodes[0].syscoinsetethheaders([[709780,709780,709779,'a','a',0],[707780,707780,707779,'a','a',0],[707772,707772,707771,'a','a',0],[707776,707776,707775,'a','a',0],[707770,707770,707769,'a','a',0],[707778,707778,707777,'a','a',0],[707774,707774,707773,'a','a',0]])
+        self.nodes[0].syscoinsetethheaders([[709780,'709780','709779','a','a',0],[707780,'707780','707779','a','a',0],[707772,'707772','707771','a','a',0],[707776,'707776','707775','a','a',0],[707770,'707770','707769','a','a',0],[707778,'707778','707777','a','a',0],[707774,'707774','707773','a','a',0]])
         r = self.nodes[0].syscoinsetethstatus("synced", 709780)
         missing_blocks = r["missing_blocks"]
         # the - MAX_ETHEREUM_TX_ROOTS check to ensure you have at least that many roots stored from the tip
@@ -38,7 +38,7 @@ class AssetTxRootsTest(SyscoinTestFramework):
         assert_equal(missing_blocks[5]["to"] , 707779);
         assert_equal(missing_blocks[6]["from"] , 707781);
         assert_equal(missing_blocks[6]["to"] , 709779);
-        self.nodes[0].syscoinsetethheaders([[707773,707773,707772,'a','a',0],[707775,707775,707774,'a','a',0],[707771,707771,707770,'a','a',0],[707777,707777,707776,'a','a',0],[707779,707779,707778,'a','a',0],[707781,707781,707780,'a','a',0]])
+        self.nodes[0].syscoinsetethheaders([[707773,'707773','707772','a','a',0],[707775,'707775','707774','a','a',0],[707771,'707771','707770','a','a',0],[707777,'707777','707776','a','a',0],[707779,'707779','707778','a','a',0],[707781,'707781','707780','a','a',0]])
         r = self.nodes[0].syscoinsetethstatus("synced", 709780)
         missing_blocks = r["missing_blocks"]
         assert_equal(missing_blocks[0]["from"] , 659780);
@@ -47,7 +47,7 @@ class AssetTxRootsTest(SyscoinTestFramework):
         assert_equal(missing_blocks[1]["to"] , 709779);
         # now fork and check it revalidates chain
         # 707771 (should be 707772) -> 707773 and 707773 (should be 707774) -> 707775
-        self.nodes[0].syscoinsetethheaders([[707773,707773,707771,'a','a',0],[707775,707775,707773,'a','a',0]])
+        self.nodes[0].syscoinsetethheaders([[707773,'707773','707771','a','a',0],[707775,'707775','707773','a','a',0]])
         r = self.nodes[0].syscoinsetethstatus("synced", 709780)
         missing_blocks = r["missing_blocks"]
         # we should still have the missing ranges prior to the forks
@@ -73,7 +73,7 @@ class AssetTxRootsTest(SyscoinTestFramework):
             i = nStartHeight + index
             if i == nMissingRange1 or i == nMissingRange2 or i == nMissingRange3:
                 continue
-            roots += [i,i,i-1,'a','a',0]
+            roots += [i,str(i),str(i-1),'a','a',0]
             if nCount > 0 and (nCount % 400) == 0:
                 self.nodes[0].syscoinsetethheaders([roots])
                 roots = []
@@ -90,7 +90,7 @@ class AssetTxRootsTest(SyscoinTestFramework):
         assert_equal(missing_blocks[1]["to"] , 800022);
         assert_equal(missing_blocks[2]["from"] , 814011);
         assert_equal(missing_blocks[2]["to"] , 814011);
-        self.nodes[0].syscoinsetethheaders([[814011,814011,814010,'a','a',0],[700059,700059,700058,'a','a',0],[800022,800022,800021,'a','a',0]])
+        self.nodes[0].syscoinsetethheaders([[814011,'814011','814010','a','a',0],[700059,'700059','700058','a','a',0],[800022,'800022','800021','a','a',0]])
         missing_blocks = self.nodes[0].syscoinsetethstatus("synced", 820000)
         assert_equal(missing_blocks, False)
 
