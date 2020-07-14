948e3d6d4412ff608fe875a4d14ad61b4c2c490e
sidhujag <jsidhu@blockchainfoundry.co>
2020-07-14 12:12:01 -0700

    fix assrt
    Former-commit-id: 47dd33e36a29272edf5c453a9a8624b6a1479bb1

--- test/functional/feature_asset_burn.py
+++ test/functional/feature_asset_burn.py
@@ -29,7 +29,7 @@ class AssetBurnTest(SyscoinTestFramework):
         out =  self.nodes[1].listunspent(query_options={'assetGuid': self.asset})
         assert_equal(len(out), 1)
         # try to burn more than we own
-        assert_raises_rpc_error(-4, "Insufficient funds", self.nodes[1].assetallocationburn(self.asset, int(0.6*COIN), "0x931d387731bbbc988b312206c74f77d004d6b84b"))
+        assert_raises_rpc_error(-4, "Insufficient funds", self.nodes[1].assetallocationburn, self.asset, int(0.6*COIN), "0x931d387731bbbc988b312206c74f77d004d6b84b")
         self.nodes[1].assetallocationburn(self.asset, int(0.5*COIN), "0x931d387731bbbc988b312206c74f77d004d6b84b")
         self.nodes[0].generate(1)
         self.sync_blocks()
