5c175f865aeaa687325cc752b33f9c45f7122313
MarcoFalke <falke.marco@gmail.com>
2020-07-15 09:03:37 +0200

    Merge #19491: util: Make Assert work with any value
    fa5363538125d996ae5cede55f7f05e88701ace2 util: Make Assert work with any value (MarcoFalke)
    
    Pull request description:
    
      Goal is to avoid compile failures
    
    ACKs for top commit:
      jonatack:
        ACK fa5363538125d996ae5cede55f7f05e88701ace2
      ryanofsky:
        Code review ACK fa5363538125d996ae5cede55f7f05e88701ace2. Looks like if argument is an lvalue this effectively does:
    
    Tree-SHA512: a5cf47a8bb2fa1bd8b8895774f33de50ad803165d6f7b520351be1cfcd5612d5d97c51d118461331d30640186c470879e5ad19e3333e09e72685c5e4e4f23079
    
    Former-commit-id: 73905269f0a77d4232c3d57e58326ab370e7c900

--- src/test/util_tests.cpp
+++ src/test/util_tests.cpp
@@ -41,6 +41,16 @@ namespace BCLog {
 
 BOOST_FIXTURE_TEST_SUITE(util_tests, BasicTestingSetup)
 
+BOOST_AUTO_TEST_CASE(util_check)
+{
+    // Check that Assert can forward
+    const std::unique_ptr<int> p_two = Assert(MakeUnique<int>(2));
+    // Check that Assert works on lvalues and rvalues
+    const int two = *Assert(p_two);
+    Assert(two == 2);
+    Assert(true);
+}
+
 BOOST_AUTO_TEST_CASE(util_criticalsection)
 {
     RecursiveMutex cs;
