534af53999e82e2a187b8668e22d7a2e7416d14f
sidhujag <jsidhu@blockchainfoundry.co>
2020-07-15 12:17:21 -0700

    compile
    Former-commit-id: 11be79b2b00fb5c86a81e6fda03242d2f8d29578

--- src/txmempool.cpp
+++ src/txmempool.cpp
@@ -957,7 +957,7 @@ void CTxMemPool::check(const CCoinsViewCache *pcoins) const
             // Check whether its inputs are marked in mapNextTx.
             auto it3 = mapNextTx.find(txin.prevout);
             auto itConflict = assetAllocationConflicts.find(txin.prevout);
-            auto itConflict1 = assetAllocationConflicts.find(it3->first);
+            auto itConflict1 = assetAllocationConflicts.find(*it3->first);
             assert(it3 != mapNextTx.end());
             if(it3->first != &txin.prevout) {
                 LogPrintf("dblspend mempool check prevout mismatch txin.prevout %s it3->first %s assetAllocationConflict(txin.prevout)? %d assetAllocationConflict(it3->first)? %d it3->second hash %s tx hash %s\n", txin.prevout.ToString(), it3->first->ToString(), itConflict == assetAllocationConflicts.end()? 0: 1, itConflict1 == assetAllocationConflicts.end()? 0: 1,it3->second->GetHash().GetHex(), tx.GetHash().GetHex());
