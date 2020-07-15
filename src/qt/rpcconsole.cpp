784ef8be41c7e5130a6b063b359031ee1ce75aff
Wladimir J. van der Laan <laanwj@protonmail.com>
2020-07-15 22:43:47 +0200

    gui: Show permissions instead of whitelisted
    Show detailed permissions instead of legacy "whitelisted" flag.
    These are formatted with `&` in between just like services flags.
    It reuses the "N/A" translation message if not.
    This removes the one-but-last use of `legacyWhitelisted`.

--- src/qt/rpcconsole.cpp
+++ src/qt/rpcconsole.cpp
@@ -1120,7 +1120,15 @@ void RPCConsole::updateNodeDetail(const CNodeCombinedStats *stats)
     ui->peerSubversion->setText(QString::fromStdString(stats->nodeStats.cleanSubVer));
     ui->peerDirection->setText(stats->nodeStats.fInbound ? tr("Inbound") : tr("Outbound"));
     ui->peerHeight->setText(QString::number(stats->nodeStats.nStartingHeight));
-    ui->peerWhitelisted->setText(stats->nodeStats.m_legacyWhitelisted ? tr("Yes") : tr("No"));
+    if (stats->nodeStats.m_permissionFlags == PF_NONE) {
+        ui->peerPermissions->setText(tr("N/A"));
+    } else {
+        QStringList permissions;
+        for (const auto& permission : NetPermissions::ToStrings(stats->nodeStats.m_permissionFlags)) {
+            permissions.append(QString::fromStdString(permission));
+        }
+        ui->peerPermissions->setText(permissions.join(" & "));
+    }
     ui->peerMappedAS->setText(stats->nodeStats.m_mapped_as != 0 ? QString::number(stats->nodeStats.m_mapped_as) : tr("N/A"));
 
     // This check fails for example if the lock was busy and
