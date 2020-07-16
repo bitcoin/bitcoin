3cba04e6efeb6be1b4a684022cdc5ad9f6bc4d6b
MarcoFalke <falke.marco@gmail.com>
2020-07-16 09:55:12 +0200

    Merge bitcoin-core/gui#14: scripted-diff: rename movie folder to animation
    80968cff68f4d11d98a2a6670846eafbb2803f4f scripted-diff: rename movie folder to animation (Peter Bushnell)
    
    Pull request description:
    
      Rename the movies directory and RES_MOVIES make variable to animation and RES_ANIMATION respectively. Movies is a bit of an unexpected term to be found.
    
    ACKs for top commit:
      MarcoFalke:
        ACK 80968cff68f4d11d98a2a6670846eafbb2803f4f
      hebasto:
        ACK 80968cff68f4d11d98a2a6670846eafbb2803f4f, tested on Linux Mint 20 (Qt 5.12.8).
    
    Tree-SHA512: 6bd31ce36e821f6a1bef8a7972086a2387d6258c48fc9df12d3ffdae07d0237036afbc2dec673384b78d9567b91d6e12eafa59fa2305aa79153dfd9b7c3a8655
    
    Former-commit-id: 4c148018cc133f5f947e95a168d5105c7d93a9a4

--- src/qt/syscoingui.cpp
+++ src/qt/syscoingui.cpp
@@ -1094,7 +1094,7 @@ void SyscoinGUI::setNumBlocks(int count, const QDateTime& blockDate, double nVer
         if(count != prevBlocks)
         {
             labelBlocksIcon->setPixmap(platformStyle->SingleColorIcon(QString(
-                ":/movies/spinner-%1").arg(spinnerFrame, 3, 10, QChar('0')))
+                ":/animation/spinner-%1").arg(spinnerFrame, 3, 10, QChar('0')))
                 .pixmap(STATUSBAR_ICONSIZE, STATUSBAR_ICONSIZE));
             spinnerFrame = (spinnerFrame + 1) % SPINNER_FRAMES;
         }
