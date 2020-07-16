2db3e7249022841190d581673b89fd0b5eebf0a7
sidhujag <jsidhu@blockchainfoundry.co>
2020-07-16 12:46:11 -0700

    import set_node_times
    Former-commit-id: d6023f695bbffba5d4b476cfb253ddd803dcc40f

--- test/functional/feature_asset_zdag.py
+++ test/functional/feature_asset_zdag.py
@@ -4,7 +4,7 @@
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 from test_framework.test_framework import SyscoinTestFramework
-from test_framework.util import assert_equal, assert_raises_rpc_error
+from test_framework.util import assert_equal, assert_raises_rpc_error, set_node_times
 from test_framework.messages import COIN
 ZDAG_NOT_FOUND = -1
 ZDAG_STATUS_OK = 0
