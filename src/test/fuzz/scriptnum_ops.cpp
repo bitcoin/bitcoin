b390205eef49d4f5ca939ae150f45942229393ad
Jon Atack <jon@atack.com>
2020-07-18 20:27:56 +0200

    fuzz: add missing overrides to signature_checker
    and also
    
    - add missing parentheses in fuzz/scriptnum_ops.cpp
    
    - remove useless unsigned int conditional in fuzz/script.cpp
    
    These changes fix 5 compile warnings in gcc 10.
    
    
    Former-commit-id: f9b93baaa8e260982fd430987b7c5b531128b49b

--- src/test/fuzz/scriptnum_ops.cpp
+++ src/test/fuzz/scriptnum_ops.cpp
@@ -33,7 +33,7 @@ void test_one_input(const std::vector<uint8_t>& buffer)
         case 0: {
             const int64_t i = fuzzed_data_provider.ConsumeIntegral<int64_t>();
             assert((script_num == i) != (script_num != i));
-            assert((script_num <= i) != script_num > i);
+            assert((script_num <= i) != (script_num > i));
             assert((script_num >= i) != (script_num < i));
             // Avoid signed integer overflow:
             // script/script.h:264:93: runtime error: signed integer overflow: -2261405121394637306 + -9223372036854775802 cannot be represented in type 'long'
