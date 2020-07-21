fc11231b8d980abebf893af3a73feff18bd5ed9f
MarcoFalke <falke.marco@gmail.com>
2020-07-21 11:28:33 +0200

    Merge #18984: Remove unnecessary input blockfile SetPos
    5fa067a27d709a8a24b798cbd2459bf5b291c885 Remove unnecessary blockfile SetPos (Tom Harding)
    
    Pull request description:
    
      Nothing could have changed the position since we retrieved it a few statements earlier. This dates from commit 16d5194165c8c83492b95f431a664d98c40ff254.
    
    ACKs for top commit:
      LarryRuane:
        ACK 5fa067a27d709a8a24b798cbd2459bf5b291c885
    
    Tree-SHA512: 459cc7226e186c231ffb67f0613f550e8eb940f1b8933c3bc4a4e8dd519c8d5d45884e8cfd9347039dab90a093644bbbb31be063baed1c6fc7984b6cb4f17c9f
    
    Former-commit-id: ebf3974d5ff45eb1e11805085d093e90b3a49309

--- src/validation.cpp
+++ src/validation.cpp
@@ -5048,7 +5048,6 @@ void LoadExternalBlockFile(const CChainParams& chainparams, FILE* fileIn, FlatFi
                 if (dbp)
                     dbp->nPos = nBlockPos;
                 blkdat.SetLimit(nBlockPos + nSize);
-                blkdat.SetPos(nBlockPos);
                 std::shared_ptr<CBlock> pblock = std::make_shared<CBlock>();
                 CBlock& block = *pblock;
                 blkdat >> block;
