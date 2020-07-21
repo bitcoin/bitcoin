a7fb06566058d0f838dd76c9aad7d33f5c190ca4
sidhujag <jsidhu@blockchainfoundry.co>
2020-07-21 09:46:57 -0700

    rmv thread exit msg
    Former-commit-id: d0bf0e51068a515c652458e83e39b7d00a88c8b9

--- src/util/system.h
+++ src/util/system.h
@@ -437,7 +437,6 @@ template <typename Callable> void TraceThread(const char* name,  Callable func)
     {
         LogPrintf("%s thread start\n", name);
         func();
-        LogPrintf("%s thread exit\n", name);
     }
     catch (const boost::thread_interrupted&)
     {
