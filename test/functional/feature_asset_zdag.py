bc18d556e444b8248bb839f0dff055e67986c790
sidhujag <jsidhu@blockchainfoundry.co>
2020-07-22 11:12:01 -0700

    create utxo on node3
    Former-commit-id: eb0700a2d86faac54f39cad5e0e9b81d8549262f

--- test/functional/feature_asset_zdag.py
+++ test/functional/feature_asset_zdag.py
@@ -118,6 +118,7 @@ class AssetZDAGTest(SyscoinTestFramework):
         self.nodes[0].sendtoaddress(useraddress1, 1)
         self.nodes[0].sendtoaddress(useraddress2, 1)
         self.nodes[0].sendtoaddress(useraddress3, 1)
+        self.nodes[0].sendtoaddress(useraddress4, 1)
         self.nodes[3].importprivkey(self.nodes[0].dumpprivkey(useraddress1))
         self.nodes[0].assetsendmany(self.asset,[{'address': useraddress1,'amount':int(1.0*COIN)},{'address': useraddress2,'amount':int(0.4*COIN)},{'address': useraddress3,'amount':int(0.5*COIN)}])
         self.nodes[0].generate(1)
