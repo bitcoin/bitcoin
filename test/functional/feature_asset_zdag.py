1c2e5b73414192878b74f847c904292710a1cadf
sidhujag <jsidhu@blockchainfoundry.co>
2020-07-21 22:21:47 -0700

    remove erroneous asset send
    Former-commit-id: 774283c80bf5352771dad7747e9b0830069530eb

--- test/functional/feature_asset_zdag.py
+++ test/functional/feature_asset_zdag.py
@@ -115,10 +115,11 @@ class AssetZDAGTest(SyscoinTestFramework):
         useraddress4 = self.nodes[3].getnewaddress()
         useraddress1 = self.nodes[0].getnewaddress()
         self.nodes[3].importprivkey(self.nodes[0].dumpprivkey(useraddress1))
-        self.nodes[0].assetsend(self.asset, useraddress1, int(2*COIN))
         self.nodes[0].sendtoaddress(useraddress2, 1)
         self.nodes[0].sendtoaddress(useraddress3, 1)
         self.nodes[0].assetsendmany(self.asset,[{'address': useraddress1,'amount':int(1.5*COIN)},{'address': useraddress2,'amount':int(0.4*COIN)},{'address': useraddress3,'amount':int(0.5*COIN)}])
+        # try to do multiple asset sends in one block
+        assert_raises_rpc_error(-4, 'bad-txns-asset-inputs-missingorspent', self.nodes[0].assetsend, self.asset, useraddress1, int(2*COIN))
         self.nodes[0].generate(1)
         self.sync_blocks()
         self.nodes[0].assetallocationsend(self.asset, useraddress2, int(0.2*COIN))
