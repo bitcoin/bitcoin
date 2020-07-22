8318f198ba8c20bc54c96fd1619a2605ffdefc31
sidhujag <jsidhu@blockchainfoundry.co>
2020-07-22 13:29:11 -0700

    adjus based on usable inputs
    Former-commit-id: 3c5193b04b5e76a081a67abf61cc0215d211e679

--- test/functional/feature_asset_zdag.py
+++ test/functional/feature_asset_zdag.py
@@ -135,11 +135,11 @@ class AssetZDAGTest(SyscoinTestFramework):
         self.nodes[0].assetallocationsend(self.asset, useraddress3, int(0.2*COIN))
         self.nodes[2].assetallocationsend(self.asset, useraddress1, int(0.2*COIN))
         # put all in useraddress1 so node4 can access in dbl spend, its probably in change address prior to this on node0
-        self.nodes[0].assetallocationsend(self.asset, useraddress1, int(1.5*COIN))
+        self.nodes[0].assetallocationsend(self.asset, useraddress1, int(1.1*COIN))
         self.sync_mempools(timeout=30)
-        self.nodes[0].assetallocationburn(self.asset, int(1.5*COIN), "0x931d387731bbbc988b312206c74f77d004d6b84b")
+        self.nodes[0].assetallocationburn(self.asset, int(1.1*COIN), "0x931d387731bbbc988b312206c74f77d004d6b84b")
         # dbl spend
-        txdblspend = self.nodes[3].assetallocationburn(self.asset, int(1.5*COIN), "0x931d387731bbbc988b312206c74f77d004d6b84b")["txid"]
+        txdblspend = self.nodes[3].assetallocationburn(self.asset, int(1.1*COIN), "0x931d387731bbbc988b312206c74f77d004d6b84b")["txid"]
 
         self.nodes[1].assetallocationsend(self.asset, useraddress3, int(0.2*COIN))
         self.nodes[2].assetallocationburn(self.asset, int(0.3*COIN), "0x931d387731bbbc988b312206c74f77d004d6b84b")
