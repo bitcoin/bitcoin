4efbb4142eb572df0ae1cb1e99fa347741683a33
John Newbery <john@johnnewbery.com>
2020-07-23 18:10:35 +0100

    [net] Add addpeeraddress RPC method
    Allows addresses to be added to Address Manager for testing.
    
    
    Former-commit-id: 53915d1cc292e417c44ecad8fa15cf74f30be64e

--- src/net.cpp
+++ src/net.cpp
@@ -2776,14 +2776,10 @@ void CConnman::MarkAddressGood(const CAddress& addr)
 {
     addrman.Good(addr);
 }
-// SYSCOIN
-void CConnman::AddNewAddress(const CAddress& addr, const CAddress& addrFrom, int64_t nTimePenalty)
+
+bool CConnman::AddNewAddresses(const std::vector<CAddress>& vAddr, const CAddress& addrFrom, int64_t nTimePenalty)
 {
-    addrman.Add(addr, addrFrom, nTimePenalty);
-}
-void CConnman::AddNewAddresses(const std::vector<CAddress>& vAddr, const CAddress& addrFrom, int64_t nTimePenalty)
-{
-    addrman.Add(vAddr, addrFrom, nTimePenalty);
+    return addrman.Add(vAddr, addrFrom, nTimePenalty);
 }
 
 std::vector<CAddress> CConnman::GetAddresses(size_t max_addresses, size_t max_pct)
