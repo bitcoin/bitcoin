5e89d5a1bfd2b52fa5e10d8423f2a40a9a87d949
sidhujag <jsidhu@blockchainfoundry.co>
2020-07-23 09:44:37 -0700

    ensure nodes get zdag tx before dbl spend burn
    Former-commit-id: a59545b7a1cbf16f9463754cc33c2aeb9b5c961b

--- test/functional/feature_asset_zdag.py
+++ test/functional/feature_asset_zdag.py
@@ -136,11 +136,12 @@ class AssetZDAGTest(SyscoinTestFramework):
         self.nodes[2].assetallocationsend(self.asset, useraddress1, int(0.2*COIN))
         self.sync_mempools(timeout=30)
         # put all in useraddress1 so node4 can access in dbl spend, its probably in change address prior to this on node0
-        self.nodes[0].assetallocationsend(self.asset, useraddress1, int(1.5*COIN))
-        self.sync_mempools(timeout=30)
-        self.nodes[0].assetallocationsend(self.asset, useraddress2, int(1.5*COIN))
+        txid = self.nodes[0].assetallocationsend(self.asset, useraddress1, int(1.5*COIN))['txid']
+        rawtx = self.nodes[0].getrawtransaction(txid)
         # dbl spend
         txdblspend = self.nodes[3].assetallocationburn(self.asset, int(1.1*COIN), "0x931d387731bbbc988b312206c74f77d004d6b84b")["txid"]
+        self.nodes[1].sendrawtransaction(rawtx)
+        self.nodes[2].sendrawtransaction(rawtx)
 
         self.nodes[1].assetallocationsend(self.asset, useraddress3, int(0.2*COIN))
         self.nodes[2].assetallocationburn(self.asset, int(0.3*COIN), "0x931d387731bbbc988b312206c74f77d004d6b84b")
