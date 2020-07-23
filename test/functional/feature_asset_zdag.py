aac26d2513e9e34fa536ddb3e2d78a54b515c6b2
sidhujag <jsidhu@blockchainfoundry.co>
2020-07-23 09:35:43 -0700

    fix test
    Former-commit-id: 5cb0e466cd34e5e721fc4fea15aeb03693f7104e

--- test/functional/feature_asset_zdag.py
+++ test/functional/feature_asset_zdag.py
@@ -138,13 +138,13 @@ class AssetZDAGTest(SyscoinTestFramework):
         # put all in useraddress1 so node4 can access in dbl spend, its probably in change address prior to this on node0
         self.nodes[0].assetallocationsend(self.asset, useraddress1, int(1.5*COIN))
         self.sync_mempools(timeout=30)
-        self.nodes[0].assetallocationburn(self.asset, int(1.1*COIN), "0x931d387731bbbc988b312206c74f77d004d6b84b")
+        self.nodes[0].assetallocationsend(self.asset, useraddress2, int(1.5*COIN))
         # dbl spend
         txdblspend = self.nodes[3].assetallocationburn(self.asset, int(1.1*COIN), "0x931d387731bbbc988b312206c74f77d004d6b84b")["txid"]
 
         self.nodes[1].assetallocationsend(self.asset, useraddress3, int(0.2*COIN))
         self.nodes[2].assetallocationburn(self.asset, int(0.3*COIN), "0x931d387731bbbc988b312206c74f77d004d6b84b")
-        self.sync_mempools(timeout=30)
+        self.sync_mempools(self.nodes[0:3], timeout=30)
         # node1/node2/node3 shouldn't have dbl spend tx because no RBF and not zdag tx
         assert_raises_rpc_error(-5, 'No such mempool transaction', self.nodes[0].getrawtransaction, txdblspend)
         assert_raises_rpc_error(-5, 'No such mempool transaction', self.nodes[1].getrawtransaction, txdblspend)
