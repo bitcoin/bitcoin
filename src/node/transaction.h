bba7bfbe88714a2af4426430fe884fbffa9e2ea1
Antoine Riard <ariard@student.42.fr>
2020-07-24 13:39:34 -0400

    Add BroadcastPackage

--- src/node/transaction.h
+++ src/node/transaction.h
@@ -10,6 +10,8 @@
 #include <primitives/transaction.h>
 #include <util/error.h>
 
+#include <list>
+
 struct NodeContext;
 
 /** Maximum fee rate for sendrawtransaction and testmempoolaccept RPC calls.
@@ -37,5 +39,23 @@ static const CFeeRate DEFAULT_MAX_RAW_TX_FEE_RATE{COIN / 10};
  * return error
  */
 NODISCARD TransactionError BroadcastTransaction(NodeContext& node, CTransactionRef tx, std::string& err_string, const CAmount& max_tx_fee, bool relay, bool wait_callback);
+
+/**
+ * Submit a package to the mempool and (optionally) relay it to all P2P peers.
+ *
+ * Mempool submission can be synchronous (will await mempool entry notification
+ * over the CValidationInterface) or asynchronous (will submit and not wait for
+ * notification), depending on the value of wait_callback. wait_callback MUST
+ * NOT be set while cs_main, cs_mempool or cs_wallet are held to avoid
+ * deadlock.
+ *
+ * @param[in]  node reference to node context
+ * @param[in]  package the packaged transaction to broadcast
+ * @param[out] err_string reference to std::string to fill with error string if available
+ * @param[in]  relay flag if both mempool insertion and p2p relay are requested
+ * @param[in]  wait_callback wait until callbacks have been processed to avoid stale result due to a sequentially RPC.
+ * return error
+ */
+NODISCARD TransactionError BroadcastPackage(NodeContext& node, std::list<CTransactionRef>& package, std::string& err_string, bool relay, bool wait_callback);
 
 #endif // BITCOIN_NODE_TRANSACTION_H
