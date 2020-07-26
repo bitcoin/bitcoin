fe627aa7df776665e130e50c7bd84c6529748209
sidhujag <jsidhu@blockchainfoundry.co>
2020-07-25 19:25:07 -0700

    fix supply
    Former-commit-id: cc1ad0cf77d04cdcc2e85b765a0303959c8e6d28

--- test/functional/feature_asset.py
+++ test/functional/feature_asset.py
@@ -56,15 +56,15 @@ class AssetTest(SyscoinTestFramework):
         asset = self.nodes[0].assetnew('1', 'TST', gooddata, '0x', 8, 1*COIN, 1*COIN, 31, {})['asset_guid']
         self.nodes[0].generate(1)
         # cannot increase supply
-        self.nodes[0].assetupdate(asset, '', '', 0.1, 31, {})
+        self.nodes[0].assetupdate(asset, '', '', int(0.1*COIN), 31, {})
         asset = self.nodes[0].assetnew('1', 'TST', gooddata, '0x', 8, 1*COIN, 2*COIN, 31, {})['asset_guid']
         self.nodes[0].generate(1)
-        self.nodes[0].assetupdate(asset, '', '', 0.1, 31, {})
+        self.nodes[0].assetupdate(asset, '', '', int(0.1*COIN), 31, {})
         self.nodes[0].generate(1)
-        self.nodes[0].assetupdate(asset, '', '', 0.9, 31, {})
+        self.nodes[0].assetupdate(asset, '', '', int(0.9*COIN), 31, {})
         self.nodes[0].generate(1)
         # would go over 2 coins supply
-        self.nodes[0].assetupdate(asset, '', '', 0.1, 31, {})
+        self.nodes[0].assetupdate(asset, '', '', int(0.1*COIN), 31, {})
         self.nodes[0].generate(1)
         # balance > max supply
         assert_raises_rpc_error(-4, 'asset-invalid-supply', self.nodes[0].assetnew('1', 'TST', gooddata, '0x', 8, 2*COIN, 1*COIN, 31, {})['asset_guid'])
