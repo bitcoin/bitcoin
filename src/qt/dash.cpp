8d99e7836c32f024d506cf7f4f9604787f5aa5f5
UdjinM6 <UdjinM6@users.noreply.github.com>
2020-07-27 10:33:00 +0300

    Fix `-resetguisettings` (#3624)

--- src/qt/dash.cpp
+++ src/qt/dash.cpp
@@ -393,7 +393,7 @@ BitcoinApplication::~BitcoinApplication()
 #endif
     // Delete Qt-settings if user clicked on "Reset Options"
     QSettings settings;
-    if(optionsModel && optionsModel->resetSettings){
+    if(optionsModel && optionsModel->resetSettingsOnShutdown){
         settings.clear();
         settings.sync();
     }
--- src/qt/optionsdialog.cpp
+++ src/qt/optionsdialog.cpp
@@ -254,6 +254,7 @@ void OptionsDialog::on_resetButton_clicked()
 
         /* reset all options and close GUI */
         model->Reset();
+        model->resetSettingsOnShutdown = true;
         QApplication::quit();
     }
 }
