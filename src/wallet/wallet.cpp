60d298376b41965e51dfdfc035eb61920e746c92
UdjinM6 <UdjinM6@users.noreply.github.com>
2020-07-28 23:50:23 +0300

    Fix crash on splash screen when wallet fails to load (#3629)

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -4176,8 +4176,6 @@ DBErrors CWallet::LoadWallet(bool& fFirstRunRet)
     if (nLoadWalletRet != DB_LOAD_OK)
         return nLoadWalletRet;
 
-    uiInterface.LoadWallet(this);
-
     return DB_LOAD_OK;
 }
 
@@ -5232,6 +5230,7 @@ CWallet* CWallet::CreateWalletFromFile(const std::string& name, const fs::path& 
                 InitError(_("Failed to rescan the wallet during initialization"));
                 return nullptr;
             }
+            uiInterface.LoadWallet(walletInstance); // TODO: move it up when backporting 13063
             walletInstance->ScanForWalletTransactions(pindexRescan, nullptr, reserver, true);
         }
         LogPrintf(" rescan      %15dms\n", GetTimeMillis() - nStart);
