dba8196b447b6a85be66890db70928100e867d8b
Antoine Poinsot <darosior@protonmail.com>
2020-07-30 18:33:57 +0200

    policy/fees: correct decay explanation comments
    This was confusing: which one is the good one ? After testing the value
    is right but not the comment, so fix it.
    
    Signed-off-by: Antoine Poinsot <darosior@protonmail.com>

--- src/policy/fees.h
+++ src/policy/fees.h
@@ -138,9 +138,9 @@ private:
 
     /** Decay of .962 is a half-life of 18 blocks or about 3 hours */
     static constexpr double SHORT_DECAY = .962;
-    /** Decay of .998 is a half-life of 144 blocks or about 1 day */
+    /** Decay of .9952 is a half-life of 144 blocks or about 1 day */
     static constexpr double MED_DECAY = .9952;
-    /** Decay of .9995 is a half-life of 1008 blocks or about 1 week */
+    /** Decay of .99931 is a half-life of 1008 blocks or about 1 week */
     static constexpr double LONG_DECAY = .99931;
 
     /** Require greater than 60% of X feerate transactions to be confirmed within Y/2 blocks*/
