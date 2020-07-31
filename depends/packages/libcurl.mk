6932d55bb61dff0b79fe592f758b12863544f5a4
sidhujag <jsidhu@blockchainfoundry.co>
2020-07-30 21:17:40 -0700

    fix dl path
    Former-commit-id: 6e300f3abbe3ee6d1a760e0dc5642575cb914269

--- depends/packages/libcurl.mk
+++ depends/packages/libcurl.mk
@@ -1,6 +1,6 @@
 package=curl
 $(package)_version=7.52.1
-$(package)_download_path=https://curl.haxx.se/download/curl-$($(package)_version).tar.gz
+$(package)_download_path=https://curl.haxx.se/download/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=a8984e8b20880b621f61a62d95ff3c0763a3152093a9f9ce4287cfd614add6ae
 
