3557e7376a8928a40ebd87b71a45ce775c130e5c
fanquake <fanquake@gmail.com>
2020-08-04 21:41:29 +0800

    build: patch around broken pkg-config in libxcb_util_image

--- depends/packages/libxcb_util_image.mk
+++ depends/packages/libxcb_util_image.mk
@@ -4,6 +4,7 @@ $(package)_download_path=https://xcb.freedesktop.org/dist
 $(package)_file_name=xcb-util-image-$($(package)_version).tar.bz2
 $(package)_sha256_hash=2db96a37d78831d643538dd1b595d7d712e04bdccf8896a5e18ce0f398ea2ffc
 $(package)_dependencies=libxcb libxcb_util
+$(package)_patches=missing_xcb_util_pkg_config.patch
 
 define $(package)_set_vars
 $(package)_config_opts=--disable-static --disable-devel-docs --without-doxygen
@@ -11,7 +12,8 @@ $(package)_config_opts+= --disable-dependency-tracking --enable-option-checking
 endef
 
 define $(package)_preprocess_cmds
-  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub .
+  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub . && \
+  patch -p1 < $($(package)_patch_dir)/missing_xcb_util_pkg_config.patch
 endef
 
 define $(package)_config_cmds
