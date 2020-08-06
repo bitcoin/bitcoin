0343a7a83038fb0afdacd598b8969afd05b45be6
sidhujag <jsidhu@blockchainfoundry.co>
2020-08-06 13:50:43 -0700

    fix test
    Former-commit-id: e4f2347b3dfd7695663043cc62efe6f6586e1505

--- test/functional/feature_asset.py
+++ test/functional/feature_asset.py
@@ -65,15 +65,15 @@ class AssetTest(SyscoinTestFramework):
         asset = self.nodes[0].assetnew('1', 'TST', gooddata, '0x', 8, 1, 1, 31, '', {}, {})['asset_guid']
         self.nodes[0].generate(1)
         # cannot increase supply
-        assert_raises_rpc_error(-4, 'asset-amount-outofrange', self.nodes[0].assetupdate, asset, '', '', int(0.1), 31, '', {}, {})
+        assert_raises_rpc_error(-4, 'asset-amount-outofrange', self.nodes[0].assetupdate, asset, '', '', 0.1, 31, '', {}, {})
         asset = self.nodes[0].assetnew('1', 'TST', gooddata, '0x', 8, 1, 2, 31, '', {}, {})['asset_guid']
         self.nodes[0].generate(1)
-        self.nodes[0].assetupdate(asset, '', '', int(0.1), 31, '', {}, {})
+        self.nodes[0].assetupdate(asset, '', '', 0.1, 31, '', {}, {})
         self.nodes[0].generate(1)
-        self.nodes[0].assetupdate(asset, '', '', int(0.9), 31, '', {}, {})
+        self.nodes[0].assetupdate(asset, '', '', 0.9, 31, '', {}, {})
         self.nodes[0].generate(1)
         # would go over 2 coins supply
-        assert_raises_rpc_error(-4, 'asset-amount-outofrange', self.nodes[0].assetupdate, asset, '', '', int(0.1), 31, '', {}, {})
+        assert_raises_rpc_error(-4, 'asset-amount-outofrange', self.nodes[0].assetupdate, asset, '', '', 0.1, 31, '', {}, {})
         self.nodes[0].generate(1)
         # balance > max supply
         assert_raises_rpc_error(-4, 'asset-amount-outofrange', self.nodes[0].assetnew, '1', 'TST', gooddata, '0x', 8, 2, 1, 31, '', {}, {})
