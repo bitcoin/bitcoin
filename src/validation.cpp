49d801890990b4063e1a1219259208c84ae9e651
Bohdan Vanieiev <warchantua@gmail.com>
2020-08-06 17:37:22 +0300

    Fix

--- src/validation.cpp
+++ src/validation.cpp
@@ -1396,6 +1396,11 @@ void CChainState::InvalidBlockFound(CBlockIndex* pindex, const BlockValidationSt
 {
     if (state.GetResult() != BlockValidationResult::BLOCK_MUTATED) {
         pindex->nStatus |= BLOCK_FAILED_VALID;
+
+        VeriBlock::getService<VeriBlock::PopService>()
+            .getAltTree()
+            .invalidateSubtree(pindex->GetBlockHash().asVector(), altintegration::BLOCK_FAILED_BLOCK);
+
         m_blockman.m_failed_blocks.insert(pindex);
         setDirtyBlockIndex.insert(pindex);
         setBlockIndexCandidates.erase(pindex);
