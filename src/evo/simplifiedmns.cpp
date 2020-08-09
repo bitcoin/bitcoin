fce487564a8fbe6ce9442fdda2474afcc681de1f
sidhujag <jsidhu@blockchainfoundry.co>
2020-08-09 10:45:17 -0700

    compile
    Former-commit-id: d2de806afaa9981a98c44e73c020bbbaadf15967

--- src/evo/simplifiedmns.cpp
+++ src/evo/simplifiedmns.cpp
@@ -141,7 +141,7 @@ void CSimplifiedMNListDiff::ToJson(UniValue& obj) const
 
     CDataStream ssCbTxMerkleTree(SER_NETWORK, PROTOCOL_VERSION);
     ssCbTxMerkleTree << cbTxMerkleTree;
-    obj.pushKV("cbTxMerkleTree", HexStr(ssCbTxMerkleTree.begin(), ssCbTxMerkleTree.end()));
+    obj.pushKV("cbTxMerkleTree", HexStr(ssCbTxMerkleTree));
 
     UniValue deletedMNsArr(UniValue::VARR);
     for (const auto& h : deletedMNs) {
