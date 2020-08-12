df091b9b509f0b10e4315c0bfa2da0cc0c31c22f
Amiti Uttarwar <amiti@uttarwar.org>
2020-08-12 15:50:19 -0700

    [refactor] Rename test file to allow any getpeerinfo deprecations.
    Simple rename/restructure to allow for upcoming test additions.

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -251,7 +251,7 @@ BASE_SCRIPTS = [
     'feature_config_args.py',
     'feature_settings.py',
     'rpc_getdescriptorinfo.py',
-    'rpc_getpeerinfo_banscore_deprecation.py',
+    'rpc_getpeerinfo_deprecation.py',
     'rpc_help.py',
     'feature_help.py',
     'feature_shutdown.py',
