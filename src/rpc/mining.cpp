46d89ff20fd2f17bcfc2d89fc9d614717999cd33
Samuel Dobson <dobsonsa68@gmail.com>
2020-08-14 09:57:43 +1200

    Merge #19644: rpc: document returned error fields as optional if applicable
    f110b7c722eb150816a26cab161ac2b8c0f58609 rpc: document returned error fields as optional if applicable (Sebastian Falbesoner)
    
    Pull request description:
    
      The following RPCs return error fields (named `"error"` or `"errors"`) that are optional, but don't show up as optional in the help text yet:
      * `analyzepsbt`
      * `estimatesmartfee`
      * `signrawtransactionwithkey`
      * `signrawtransactionwithwallet`
    
      The following RPC has the errors field already marked as optional, but doesn't match the usual format in the description (like `"if there are any"` in parantheses):
      * `estimaterawfee`
    
      This PR adds the missing optional flags and adapts the description strings. Inspired by a recent PR #19634 by justinmoon.
    
      The instances were found via `git grep "RPCResult.*\"error"`. Note that there is one RPC so far where the return error is not optional (i.e. in case of no error, the field is included in the result, but is just empty), namely `bumpfee`.
    
    ACKs for top commit:
      adaminsky:
        ACK `f110b7c`
      laanwj:
        ACK f110b7c722eb150816a26cab161ac2b8c0f58609, new documentation looks consistent with actual behavior
      achow101:
        ACK f110b7c722eb150816a26cab161ac2b8c0f58609
      meshcollider:
        utACK f110b7c722eb150816a26cab161ac2b8c0f58609
    
    Tree-SHA512: 30c00f78a575b60e32b4536496af986d53a25f33e6ebbf553adcdcf825ad21a44f90267f3d1ea53326dac83bcfa9983fdb3dad6d3126e20f97f3c08ce286e188
    
    Former-commit-id: 166c599bf8351af89b62cc7708fbd7557b2cc2f3

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -1080,7 +1080,7 @@ static UniValue estimatesmartfee(const JSONRPCRequest& request)
                     RPCResult::Type::OBJ, "", "",
                     {
                         {RPCResult::Type::NUM, "feerate", /* optional */ true, "estimate fee rate in " + CURRENCY_UNIT + "/kB (only present if no errors were encountered)"},
-                        {RPCResult::Type::ARR, "errors", "Errors encountered during processing",
+                        {RPCResult::Type::ARR, "errors", /* optional */ true, "Errors encountered during processing (if there are any)",
                             {
                                 {RPCResult::Type::STR, "", "error"},
                             }},
@@ -1159,7 +1159,7 @@ static UniValue estimaterawfee(const JSONRPCRequest& request)
                                 {
                                     {RPCResult::Type::ELISION, "", ""},
                                 }},
-                                {RPCResult::Type::ARR, "errors", /* optional */ true, "Errors encountered during processing",
+                                {RPCResult::Type::ARR, "errors", /* optional */ true, "Errors encountered during processing (if there are any)",
                                 {
                                     {RPCResult::Type::STR, "error", ""},
                                 }},
