1b5e154eb16cdf68facc3bfdd3641d5476297c7c
sidhujag <jsidhu@blockchainfoundry.co>
2020-08-14 20:12:56 -0700

    fix test
    Former-commit-id: 643ccd4ceb62c30f23ddaa8a41ea5108550e16f4

--- test/functional/feature_asset.py
+++ test/functional/feature_asset.py
@@ -73,10 +73,10 @@ class AssetTest(SyscoinTestFramework):
         self.nodes[0].assetupdate(asset, '', '', 0.9, 127, '', '', {}, {})
         self.nodes[0].generate(1)
         # would go over 2 coins supply
-        assert_raises_rpc_error(-4, 'asset-invalid-supply', self.nodes[0].assetupdate, asset, '', '', 0.1, 127, '', {}, {})
+        assert_raises_rpc_error(-4, 'asset-invalid-supply', self.nodes[0].assetupdate, asset, '', '', 0.1, 127, '', '', {}, {})
         self.nodes[0].generate(1)
         # balance > max supply
-        assert_raises_rpc_error(-4, 'asset-invalid-supply', self.nodes[0].assetnew, '1', 'TST', gooddata, '0x', 8, 2, 1, 127, '', {}, {})
+        assert_raises_rpc_error(-4, 'asset-invalid-supply', self.nodes[0].assetnew, '1', 'TST', gooddata, '0x', 8, 2, 1, 127, '', '', {}, {})
         # int64 limits
         # largest decmal amount that we can use, without compression overflow of uint (~1 quintillion)
         # 10^18 - 1
@@ -95,9 +95,9 @@ class AssetTest(SyscoinTestFramework):
         assert_equal(assetInfo['total_supply'], maxUint)
         assert_equal(assetInfo['balance'], maxUint)
         assert_equal(assetInfo['max_supply'], maxUint)
-        assert_raises_rpc_error(-4, 'asset-amount-outofrange', self.nodes[0].assetupdate, asset, '', '', 1, 127, '', {}, {})
-        assert_raises_rpc_error(-4, 'asset-invalid-maxsupply', self.nodes[0].assetnew, '1', 'TST', gooddata, '0x', 0, maxUint, maxUint+1, 127, '', {}, {})
-        assert_raises_rpc_error(-4, 'asset-invalid-maxsupply', self.nodes[0].assetnew, '1', 'TST', gooddata, '0x', 0, maxUint+1, maxUint+1, 127, '', {}, {})
+        assert_raises_rpc_error(-4, 'asset-amount-outofrange', self.nodes[0].assetupdate, asset, '', '', 1, 127, '', '', {}, {})
+        assert_raises_rpc_error(-4, 'asset-invalid-maxsupply', self.nodes[0].assetnew, '1', 'TST', gooddata, '0x', 0, maxUint, maxUint+1, 127, '', '', {}, {})
+        assert_raises_rpc_error(-4, 'asset-invalid-maxsupply', self.nodes[0].assetnew, '1', 'TST', gooddata, '0x', 0, maxUint+1, maxUint+1, 127, '', '', {}, {})
 
     def asset_transfer(self):
         useraddress1 = self.nodes[1].getnewaddress()
@@ -116,13 +116,13 @@ class AssetTest(SyscoinTestFramework):
         self.sync_blocks()
         self.nodes[0].assettransfer(asset0, self.nodes[1].getnewaddress())
         self.nodes[1].assettransfer(asset1, self.nodes[0].getnewaddress())
-        assert_raises_rpc_error(-4, 'No inputs found for this asset', self.nodes[0].assetupdate, asset0, '', '', 0, 127, '', {}, {})
-        assert_raises_rpc_error(-4, 'No inputs found for this asset', self.nodes[1].assetupdate, asset1, '', '', 0, 127, '', {}, {})
+        assert_raises_rpc_error(-4, 'No inputs found for this asset', self.nodes[0].assetupdate, asset0, '', '', 0, 127, '', '', {}, {})
+        assert_raises_rpc_error(-4, 'No inputs found for this asset', self.nodes[1].assetupdate, asset1, '', '', 0, 127, '', '', {}, {})
         self.sync_mempools()
         self.nodes[0].generate(1)
         self.sync_blocks()
-        assert_raises_rpc_error(-4, 'No inputs found for this asset', self.nodes[0].assetupdate, asset0, '', '', 0, 127, '', {}, {})
-        assert_raises_rpc_error(-4, 'No inputs found for this asset', self.nodes[1].assetupdate, asset1, '', '', 0, 127, '', {}, {})
+        assert_raises_rpc_error(-4, 'No inputs found for this asset', self.nodes[0].assetupdate, asset0, '', '', 0, 127, '', '', {}, {})
+        assert_raises_rpc_error(-4, 'No inputs found for this asset', self.nodes[1].assetupdate, asset1, '', '', 0, 127, '', '', {}, {})
         assert_raises_rpc_error(-4, 'No inputs found for this asset', self.nodes[0].assettransfer, asset0, self.nodes[1].getnewaddress())
         assert_raises_rpc_error(-4, 'No inputs found for this asset', self.nodes[1].assettransfer, asset1, self.nodes[0].getnewaddress())
         self.nodes[0].assetupdate(asset1, '', '', 0, 127, '', '', {}, {})
