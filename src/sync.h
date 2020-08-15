fab9c6259ac46a970dcbed771c1f975c243248e1
MarcoFalke <falke.marco@gmail.com>
2020-08-15 13:43:58 +0200

    sync: Add AssertLockHeld(MutexType*) helper

--- src/sync.h
+++ src/sync.h
@@ -74,8 +74,13 @@ inline void AssertLockNotHeldInternal(const char* pszName, const char* pszFile, 
 inline void DeleteLock(void* cs) {}
 inline bool LockStackEmpty() { return true; }
 #endif
-#define AssertLockHeld(cs) AssertLockHeldInternal(#cs, __FILE__, __LINE__, &cs)
+#define AssertLockHeld(cs) AssertLockHeldInternal(#cs, __FILE__, __LINE__, cs)
 #define AssertLockNotHeld(cs) AssertLockNotHeldInternal(#cs, __FILE__, __LINE__, &cs)
+template <typename MutexType>
+void AssertLockHeldInternal(const char* name, const char* file, int line, MutexType& mtx) ASSERT_EXCLUSIVE_LOCK(mtx)
+{
+    AssertLockHeldInternal(name, file, line, &mtx);
+};
 
 /**
  * Template mixin that adds -Wthread-safety locking annotations and lock order
