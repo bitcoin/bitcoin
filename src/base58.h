d3e8adfada889a3c9fba930086eda609509aca07
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2020-08-17 01:43:11 +0200

    util: remove c-string interfaces for DecodeBase58{Check}

--- src/base58.h
+++ src/base58.h
@@ -31,13 +31,6 @@ std::string EncodeBase58(const unsigned char* pbegin, const unsigned char* pend)
 std::string EncodeBase58(const std::vector<unsigned char>& vch);
 
 /**
- * Decode a base58-encoded string (psz) into a byte vector (vchRet).
- * return true if decoding is successful.
- * psz cannot be nullptr.
- */
-NODISCARD bool DecodeBase58(const char* psz, std::vector<unsigned char>& vchRet, int max_ret_len);
-
-/**
  * Decode a base58-encoded string (str) into a byte vector (vchRet).
  * return true if decoding is successful.
  */
@@ -47,12 +40,6 @@ NODISCARD bool DecodeBase58(const std::string& str, std::vector<unsigned char>& 
  * Encode a byte vector into a base58-encoded string, including checksum
  */
 std::string EncodeBase58Check(const std::vector<unsigned char>& vchIn);
-
-/**
- * Decode a base58-encoded string (psz) that includes a checksum into a byte
- * vector (vchRet), return true if decoding is successful
- */
-NODISCARD bool DecodeBase58Check(const char* psz, std::vector<unsigned char>& vchRet, int max_ret_len);
 
 /**
  * Decode a base58-encoded string (str) that includes a checksum into a byte
