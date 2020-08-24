28b7395ec59a2acdb5d7fdf6628379a1ce30de7a
PastaPastaPasta <6443210+PastaPastaPasta@users.noreply.github.com>
2020-08-24 11:15:29 -0400

    trivial: don't mix unsigned int with int (#3664)
    Signed-off-by: pasta <pasta@dashboost.org>

--- src/privatesend/privatesend-client.cpp
+++ src/privatesend/privatesend-client.cpp
@@ -1802,8 +1802,8 @@ void CPrivateSendClientManager::DoMaintenance(CConnman& connman)
 
     if (!masternodeSync.IsBlockchainSynced() || ShutdownRequested()) return;
 
-    static unsigned int nTick = 0;
-    static unsigned int nDoAutoNextRun = nTick + PRIVATESEND_AUTO_TIMEOUT_MIN;
+    static int nTick = 0;
+    static int nDoAutoNextRun = nTick + PRIVATESEND_AUTO_TIMEOUT_MIN;
 
     nTick++;
     CheckTimeout();
