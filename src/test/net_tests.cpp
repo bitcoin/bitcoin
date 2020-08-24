102867c587f5f7954232fb8ed8e85cda78bb4d32
Vasil Dimov <vd@FreeBSD.org>
2020-08-24 21:34:26 +0200

    net: change CNetAddr::ip to have flexible size
    Before this change `CNetAddr::ip` was a fixed-size array of 16 bytes,
    not being able to store larger addresses (e.g. TORv3) and encoded
    smaller ones as 16-byte IPv6 addresses.
    
    Change its type to `prevector`, so that it can hold larger addresses and
    do not disguise non-IPv6 addresses as IPv6. So the IPv4 address
    `1.2.3.4` is now encoded as `01020304` instead of
    `00000000000000000000FFFF01020304`.
    
    Rename `CNetAddr::ip` to `CNetAddr::m_addr` because it is not an "IP" or
    "IP address" (TOR addresses are not IP addresses).
    
    In order to preserve backward compatibility with serialization (where
    e.g. `1.2.3.4` is serialized as `00000000000000000000FFFF01020304`)
    introduce `CNetAddr` dedicated legacy serialize/unserialize methods.
    
    Adjust `CSubNet` accordingly. Still use `CSubNet::netmask[]` of fixed 16
    bytes, but use the first 4 for IPv4 (not the last 4). Only allow
    subnetting for IPv4 and IPv6.
    
    Co-authored-by: Carl Dong <contact@carldong.me>

--- src/test/net_tests.cpp
+++ src/test/net_tests.cpp
@@ -13,8 +13,10 @@
 #include <streams.h>
 #include <test/util/setup_common.h>
 #include <util/memory.h>
+#include <util/strencodings.h>
 #include <util/string.h>
 #include <util/system.h>
+#include <version.h>
 
 #include <boost/test/unit_test.hpp>
 
@@ -191,6 +193,78 @@ BOOST_AUTO_TEST_CASE(cnode_simple_test)
     std::unique_ptr<CNode> pnode2 = MakeUnique<CNode>(id++, NODE_NETWORK, height, hSocket, addr, 1, 1, CAddress(), pszDest, fInboundIn);
     BOOST_CHECK(pnode2->fInbound == true);
     BOOST_CHECK(pnode2->fFeeler == false);
+}
+
+BOOST_AUTO_TEST_CASE(cnetaddr_basic)
+{
+    CNetAddr addr;
+
+    // IPv4, INADDR_ANY
+    BOOST_REQUIRE(LookupHost("0.0.0.0", addr, false));
+    BOOST_REQUIRE(!addr.IsValid());
+    BOOST_REQUIRE(addr.IsIPv4());
+
+    BOOST_CHECK(addr.IsBindAny());
+    BOOST_CHECK_EQUAL(addr.ToString(), "0.0.0.0");
+
+    // IPv4, INADDR_NONE
+    BOOST_REQUIRE(LookupHost("255.255.255.255", addr, false));
+    BOOST_REQUIRE(!addr.IsValid());
+    BOOST_REQUIRE(addr.IsIPv4());
+
+    BOOST_CHECK(!addr.IsBindAny());
+    BOOST_CHECK_EQUAL(addr.ToString(), "255.255.255.255");
+
+    // IPv4, casual
+    BOOST_REQUIRE(LookupHost("12.34.56.78", addr, false));
+    BOOST_REQUIRE(addr.IsValid());
+    BOOST_REQUIRE(addr.IsIPv4());
+
+    BOOST_CHECK(!addr.IsBindAny());
+    BOOST_CHECK_EQUAL(addr.ToString(), "12.34.56.78");
+
+    // IPv6, in6addr_any
+    BOOST_REQUIRE(LookupHost("::", addr, false));
+    BOOST_REQUIRE(!addr.IsValid());
+    BOOST_REQUIRE(addr.IsIPv6());
+
+    BOOST_CHECK(addr.IsBindAny());
+    BOOST_CHECK_EQUAL(addr.ToString(), "::");
+
+    // IPv6, casual
+    BOOST_REQUIRE(LookupHost("1122:3344:5566:7788:9900:aabb:ccdd:eeff", addr, false));
+    BOOST_REQUIRE(addr.IsValid());
+    BOOST_REQUIRE(addr.IsIPv6());
+
+    BOOST_CHECK(!addr.IsBindAny());
+    BOOST_CHECK_EQUAL(addr.ToString(), "1122:3344:5566:7788:9900:aabb:ccdd:eeff");
+
+    // TORv2
+    addr.SetSpecial("6hzph5hv6337r6p2.onion");
+    BOOST_REQUIRE(addr.IsValid());
+    BOOST_REQUIRE(addr.IsTor());
+
+    BOOST_CHECK(!addr.IsBindAny());
+    BOOST_CHECK_EQUAL(addr.ToString(), "6hzph5hv6337r6p2.onion");
+
+    // Internal
+    addr.SetInternal("esffpp");
+    BOOST_REQUIRE(!addr.IsValid()); // "internal" is considered invalid
+    BOOST_REQUIRE(addr.IsInternal());
+
+    BOOST_CHECK(!addr.IsBindAny());
+    BOOST_CHECK_EQUAL(addr.ToString(), "esffpvrt3wpeaygy.internal");
+}
+
+BOOST_AUTO_TEST_CASE(cnetaddr_serialize)
+{
+    CNetAddr addr;
+    CDataStream s(SER_NETWORK, PROTOCOL_VERSION);
+
+    addr.SetInternal("a");
+    s << addr;
+    BOOST_CHECK_EQUAL(HexStr(s), "fd6b88c08724ca978112ca1bbdcafac2");
+    s.clear();
 }
 
 // prior to PR #14728, this test triggers an undefined behavior
