97e14dbed1974d4bf24a8283874a49ff6fb6411b
sidhujag <jsidhu@blockchainfoundry.co>
2020-08-26 11:09:16 -0700

    compile
    Former-commit-id: 06509696a484181201cbba650643d4d35c635733

--- src/services/rpc/wallet/assetwalletrpc.cpp
+++ src/services/rpc/wallet/assetwalletrpc.cpp
@@ -442,7 +442,7 @@ UniValue signhash(const JSONRPCRequest& request)
         LogPrintf("CSporkMessage::Sign -- VerifyHash() failed\n");
         return false;
     }
-    return EncodeBase64(vchSig.data(), vchSig.size());
+    return EncodeBase64(vchSig);
 }
 UniValue syscoinburntoassetallocation(const JSONRPCRequest& request) {
     const UniValue &params = request.params;
