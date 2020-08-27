fa1cd9e1ddc6918c3d600d36eadea71eebb242b6
MarcoFalke <falke.marco@gmail.com>
2020-08-27 11:52:52 +0200

    test: Remove unused lock arg from BitcoinTestFramework.wait_until

--- test/functional/test_framework/test_framework.py
+++ test/functional/test_framework/test_framework.py
@@ -603,8 +603,8 @@ class BitcoinTestFramework(metaclass=BitcoinTestMetaClass):
         self.sync_blocks(nodes)
         self.sync_mempools(nodes)
 
-    def wait_until(self, test_function, timeout=60, lock=None):
-        return wait_until_helper(test_function, timeout=timeout, lock=lock, timeout_factor=self.options.timeout_factor)
+    def wait_until(self, test_function, timeout=60):
+        return wait_until_helper(test_function, timeout=timeout, timeout_factor=self.options.timeout_factor)
 
     # Private helper methods. These should not be accessed by the subclass test scripts.
 
