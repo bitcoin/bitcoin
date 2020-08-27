ba4fde00b7d9b2181a67e69157b47c34743f50da
sidhujag <jsidhu@blockchainfoundry.co>
2020-08-26 20:13:01 -0700

    adjust download roots to about 1 week
    Former-commit-id: fa061db74459553a489727774b7f1aef542b2965

--- src/services/asset.h
+++ src/services/asset.h
@@ -17,8 +17,8 @@ static const unsigned int MAX_AUXFEES = 10;
 static const int64_t MAINNET_MAX_MINT_AGE = 302400; // 0.5 week in seconds, should send to network in half a week or less
 static const int64_t MAINNET_MAX_VALIDATE_AGE = 1512000; // 2.5 weeks in seconds
 static const int64_t MAINNET_MIN_MINT_AGE = 3600; // 1 hr
-static const uint32_t DOWNLOAD_ETHEREUM_TX_ROOTS = 75000; // roughly 7k blocks a day * 1.5 weeks
-static const uint32_t MAX_ETHEREUM_TX_ROOTS = DOWNLOAD_ETHEREUM_TX_ROOTS*2.5;
+static const uint32_t DOWNLOAD_ETHEREUM_TX_ROOTS = 60000; // roughly 1 week
+static const uint32_t MAX_ETHEREUM_TX_ROOTS = DOWNLOAD_ETHEREUM_TX_ROOTS*4;
 std::string stringFromVch(const std::vector<unsigned char> &vch);
 std::vector<unsigned char> vchFromString(const std::string &str);
 uint32_t GenerateSyscoinGuid(const COutPoint& outPoint);
