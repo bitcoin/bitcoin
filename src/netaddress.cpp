d2bb681f96fb327b4c4d5b2b113692ca22fdffbf
Vasil Dimov <vd@FreeBSD.org>
2020-08-31 10:39:00 +0200

    util: move HasPrefix() so it can be reused
    Move the function `HasPrefix()` from `netaddress.cpp` to `util/string.h`
    so it can be reused by `CNetAddr` methods (and possibly others).

--- src/netaddress.cpp
+++ src/netaddress.cpp
@@ -7,8 +7,9 @@
 
 #include <hash.h>
 #include <tinyformat.h>
-#include <util/strencodings.h>
 #include <util/asmap.h>
+#include <util/strencodings.h>
+#include <util/string.h>
 
 #include <algorithm>
 #include <array>
@@ -48,13 +49,6 @@ void CNetAddr::SetIP(const CNetAddr& ipIn)
 
     m_net = ipIn.m_net;
     m_addr = ipIn.m_addr;
-}
-
-template <typename T1, size_t PREFIX_LEN>
-inline bool HasPrefix(const T1& obj, const std::array<uint8_t, PREFIX_LEN>& prefix)
-{
-    return obj.size() >= PREFIX_LEN &&
-           std::equal(std::begin(prefix), std::end(prefix), std::begin(obj));
 }
 
 void CNetAddr::SetLegacyIPv6(Span<const uint8_t> ipv6)
