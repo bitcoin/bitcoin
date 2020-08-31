faf251d854e3a670533ea3e9087e82c92f3ae533
João Barbosa <joao.paulo.barbosa@gmail.com>
2020-08-31 16:14:50 +0200

    test: gettxoutproof duplicate txid

--- test/functional/rpc_txoutproof.py
+++ test/functional/rpc_txoutproof.py
@@ -79,6 +79,8 @@ class MerkleBlockTest(BitcoinTestFramework):
         assert_raises_rpc_error(-5, "Not all transactions found in specified or retrieved block", self.nodes[0].gettxoutproof, [txid1, txid3])
         # Test empty list
         assert_raises_rpc_error(-5, "Transaction not yet in block", self.nodes[0].gettxoutproof, [])
+        # Test duplicate txid
+        assert_raises_rpc_error(-8, 'Invalid parameter, duplicated txid', self.nodes[0].gettxoutproof, [txid1, txid1])
 
         # Now we'll try tweaking a proof.
         proof = self.nodes[1].gettxoutproof([txid1, txid2])
