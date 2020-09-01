9c988d5b5a62aeca7aa400146cd025a573886420
dustinface <35775977+xdustinface@users.noreply.github.com>
2020-09-01 17:27:33 +0200

    privatesend|wallet|qt: Improve calculations in CreateDenominated/MakeCollateralAmounts (#3657)
    * wallet: Add m_dust_feerate to CCoinControl / Use it in CreateTransaction
    
    * privatesend: Introduce CTransactionBuilder/CTransactionBuilderOutput
    
    Builder classes for transactions from type
    
    Inputs: Defined by CompactTallyItem
    Outputs: Simple outputs with lose reserve keys
    
    This takes fully takes care of fee calculations and makes sure calculations are the same like those happening when actually create the transaction with CreateTransaction.
    
    * privatesend: Improve amount/fee calculation in CreateDenominated
    
    * privatesend: Improve amount/fee calculation in MakeCollateralAmounts
    
    * qt: Fix decomposeTransaction's MakeCollateralAmounts detection
    
    Align it with the three cases in CPrivateSendClientSession::MakeCollateralAmounts
    
    * Refactor GetFee
    
    The fee rate is always coinControl.m_feerate, also it's not used outside so should be a private method
    
    * Simplify nBytesOutput calculations
    
    * Drop unused GetBytesOutput()
    
    * Make Clear(), GetBytesTotal() and GetAmountUsed() private
    
    * Drop unused GetCoinControl()
    
    * Make ToString() const
    
    * Refactor `CTransactionBuilder::Commit()`
    
    * Reorder cases in decomposeTransaction
    
    * Fix "finished" conditions in CreateDenominated
    
    * Fix typo
    
    * wallet|privatesend: Refactor CCoinControl's m_dust_feerate -> m_discard_feerate
    
    * privatesend: Drop unused member CTransactionBuilder::dustFeeRate
    
    * privatesend: Improve CTransactionBuilder's readability
    
    * privatesend: Make the static CTransactionBuilder::GetAmountLeft private
    
    * wallet: Recover previous code style
    
    * Update src/privatesend/privatesend-util.cpp
    
    Co-authored-by: UdjinM6 <UdjinM6@users.noreply.github.com>
    
    * Tweak CTransactionBuilder to respect potential compact size diffs
    
    * Tweak GetFee param type
    
    * Trivial log/comments tweaks
    
    * privatesend: Fix countPossibleOutputs
    
    - Fix off by one issue
    - Respect max outputs threshold
    
    * privatesend: Use GetSizeOfCompactSizeDiff in CTransactionBuilder
    
    * Apply suggestions from code review
    
    Co-authored-by: PastaPastaPasta <6443210+PastaPastaPasta@users.noreply.github.com>
    
    * privatesend: Rename TryAdd to CouldAdd in CTransactionBuilder
    
    * wallet: Reset m_discard_feerate in CCoinControl::SetNull
    
    * Respect `-paytxfee` and `settxfee`
    
    * privatesend: Check for denominated amount only where really required
    
    * qt: Remove obsolete IsCollateralAmount() checks
    
    * privatesend: Don't accept negative amounts in CTransactionBuilder
    
    * privatesend: Remove unused CConnman parameter
    
    * use emplace_back instead of push_back
    
    Signed-off-by: pasta <pasta@dashboost.org>
    
    * fix typos
    
    Signed-off-by: pasta <pasta@dashboost.org>
    
    * make GetAmount const
    
    Signed-off-by: pasta <pasta@dashboost.org>
    
    * privatesend: Explicit capture __func__ in needMoreOutputs lambda
    
    * privatesend: Update CTransactionBuilder::UpdateAmount
    
    * remove const on parameter in declaration
    
    Signed-off-by: pasta <pasta@dashboost.org>
    
    * handle unsigned int -> int conversions a bit better
    
    Signed-off-by: pasta <pasta@dashboost.org>
    
    * explicitly cast to int from unsigned int.
    
    estimateSmartFee handles it if negative
    
    Signed-off-by: pasta <pasta@dashboost.org>
    
    * Make CTransactionBuilderOutput::GetScript const
    
    Co-authored-by: UdjinM6 <UdjinM6@users.noreply.github.com>
    
    * privatesend: Update comments to follow doxygen
    
    * privatesend: Add class descriptions
    
    Co-authored-by: UdjinM6 <UdjinM6@users.noreply.github.com>
    Co-authored-by: PastaPastaPasta <6443210+PastaPastaPasta@users.noreply.github.com>
    Co-authored-by: pasta <pasta@dashboost.org>

--- src/privatesend/privatesend-util.h
+++ src/privatesend/privatesend-util.h
@@ -7,6 +7,8 @@
 
 #include <wallet/wallet.h>
 
+class CTransactionBuilder;
+
 class CKeyHolder
 {
 private:
@@ -34,4 +36,105 @@ public:
     void KeepAll();
     void ReturnAll();
 };
+
+/**
+ * @brief Used by CTransactionBuilder to represent its transaction outputs.
+ * It creates a CReserveKey for the given CWallet as destination.
+ */
+class CTransactionBuilderOutput
+{
+    /// Used for amount updates
+    CTransactionBuilder* pTxBuilder{nullptr};
+    /// Reserve key where the amount of this output will end up
+    CReserveKey key;
+    /// Amount this output will receive
+    CAmount nAmount{0};
+    /// ScriptPubKey of this output
+    CScript script;
+
+public:
+    CTransactionBuilderOutput(CTransactionBuilder* pTxBuilderIn, CWallet* pwalletIn, CAmount nAmountIn);
+    CTransactionBuilderOutput(CTransactionBuilderOutput&&) = delete;
+    CTransactionBuilderOutput& operator=(CTransactionBuilderOutput&&) = delete;
+    /// Get the scriptPubKey of this output
+    CScript GetScript() const { return script; }
+    /// Get the amount of this output
+    CAmount GetAmount() const { return nAmount; }
+    /// Try update the amount of this output. Returns true if it was successful and false if not (e.g. insufficient amount left).
+    bool UpdateAmount(CAmount nAmount);
+    /// Tell the wallet to remove the key used by this output from the keypool
+    void KeepKey() { key.KeepKey(); }
+    /// Tell the wallet to return the key used by this output to the keypool
+    void ReturnKey() { key.ReturnKey(); }
+};
+
+/**
+ * @brief Enables simple transaction generation for a given CWallet object. The resulting
+ * transaction's inputs are defined by the given CompactTallyItem. The outputs are
+ * defined by CTransactionBuilderOutput.
+ */
+class CTransactionBuilder
+{
+    /// Wallet the transaction will be build for
+    CWallet* pwallet{nullptr};
+    /// See CTransactionBuilder() for initialization
+    CCoinControl coinControl;
+    /// Dummy since we anyway use tallyItem's destination as change destination in coincontrol.
+    /// Its a member just to make sure ReturnKey can be called in destructor just in case it gets generated/kept
+    /// somewhere in CWallet code.
+    CReserveKey dummyReserveKey;
+    /// Contains all utxos available to generate this transactions. They are all from the same address.
+    CompactTallyItem tallyItem;
+    /// Contains the number of bytes required for a transaction with only the inputs of tallyItems, no outputs
+    int nBytesBase{0};
+    /// Contains the number of bytes required to add one output
+    int nBytesOutput{0};
+    /// Call KeepKey for all keys in destructor if fKeepKeys is true, call ReturnKey for all key if its false.
+    bool fKeepKeys{false};
+    /// Protect vecOutputs
+    mutable CCriticalSection cs_outputs;
+    /// Contains all outputs already added to the transaction
+    std::vector<std::unique_ptr<CTransactionBuilderOutput>> vecOutputs;
+    /// Needed by CTransactionBuilderOutput::UpdateAmount to lock cs_outputs
+    friend class CTransactionBuilderOutput;
+
+public:
+    CTransactionBuilder(CWallet* pwalletIn, const CompactTallyItem& tallyItemIn);
+    ~CTransactionBuilder();
+    /// Check it would be possible to add a single output with the amount nAmount. Returns true if its possible and false if not.
+    bool CouldAddOutput(CAmount nAmountOutput) const;
+    /// Check if its possible to add multiple outputs as vector of amounts. Returns true if its possible to add all of them and false if not.
+    bool CouldAddOutputs(const std::vector<CAmount>& vecOutputAmounts) const;
+    /// Add an output with the amount nAmount. Returns a pointer to the output if it could be added and nullptr if not due to insufficient amount left.
+    CTransactionBuilderOutput* AddOutput(CAmount nAmountOutput = 0);
+    /// Get amount we had available when we started
+    CAmount GetAmountInitial() const { return tallyItem.nAmount; }
+    /// Get the amount currently left to add more outputs. Does respect fees.
+    CAmount GetAmountLeft() const { return GetAmountInitial() - GetAmountUsed() - GetFee(GetBytesTotal()); }
+    /// Check if an amounts should be considered as dust
+    bool IsDust(CAmount nAmount) const;
+    /// Get the total number of added outputs
+    int CountOutputs() const { return vecOutputs.size(); }
+    /// Create and Commit the transaction to the wallet
+    bool Commit(std::string& strResult);
+    /// Convert to a string
+    std::string ToString() const;
+
+private:
+    /// Clear the output vector and keep/return the included keys depending on the value of fKeepKeys
+    void Clear();
+    /// Get the total number of bytes used already by this transaction
+    unsigned int GetBytesTotal() const;
+    /// Helper to calculate static amount left by simply subtracting an used amount and a fee from a provided initial amount.
+    static CAmount GetAmountLeft(CAmount nAmountInitial, CAmount nAmountUsed, CAmount nFee);
+    /// Get the amount currently used by added outputs. Does not include fees.
+    CAmount GetAmountUsed() const;
+    /// Get fees based on the number of bytes and the feerate set in CoinControl.
+    /// NOTE: To get the total transaction fee this should only be called once with the total number of bytes for the transaction to avoid
+    /// calling CFeeRate::GetFee multiple times with subtotals as this may add rounding errors with each further call.
+    CAmount GetFee(unsigned int nBytes) const;
+    /// Helper to get GetSizeOfCompactSizeDiff(vecOutputs.size(), vecOutputs.size() + nAdd)
+    int GetSizeOfCompactSizeDiff(size_t nAdd) const;
+};
+
 #endif //PRIVATESENDUTIL_H
