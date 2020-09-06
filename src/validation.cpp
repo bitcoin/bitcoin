bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

refactor: Prevent double lock in CTxMemPool::check()

src/validation.cpp
@@ -2842,7 +2842,7 @@ bool CChainState::ActivateBestChainStep(BlockValidationState& state, const CChai
         // any disconnected transactions back to the mempool.
         UpdateMempoolForReorg(disconnectpool, true);
     }
-    mempool.check(&CoinsTip());
+    mempool.checkNonLockHelper(&CoinsTip());
 
     // Callbacks/notifications for a new best chain.
     if (fInvalidFound)

bitcoin/bitcoin

src/validation.cpp
@@ -2831,7 +2831,7 @@ bool CChainState::ActivateBestChainStep(BlockValidationState& state, const CChai
         // any disconnected transactions back to the mempool.
         UpdateMempoolForReorg(m_mempool, disconnectpool, true);
     }
-    m_mempool.check(&CoinsTip());
+    m_mempool.checkNonLockHelper(&CoinsTip());
 
     // Callbacks/notifications for a new best chain.
     if (fInvalidFound)