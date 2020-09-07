1727af91b738caa8b86398a5b6f139b3ac74ac6f
dexX7 <dexx@bitwatch.co>
2020-09-07 10:02:46 +0200

    Add new transaction type to embed any data
    The transaction type is optional and not consensus affecting. It does not change the consensus hash.
    
    The new transaction allows to embed arbitrary data and has the tranasaction identifier 200.

--- src/omnicore/rules.h
+++ src/omnicore/rules.h
@@ -130,6 +130,8 @@ public:
     int MSC_BET_BLOCK;
     //! Block to enable cross property STO (v1)
     int MSC_STOV1_BLOCK;
+    //! Block to enable any data payloads
+    int MSC_ANYDATA_BLOCK;
 
     //! Block to deactivate crowdsale participations when "granting tokens"
     int GRANTEFFECTS_FEATURE_BLOCK;
