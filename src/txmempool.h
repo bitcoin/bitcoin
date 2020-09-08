fb2dca415f6ac74d79292296831ccc33c485230b
João Barbosa <joao.paulo.barbosa@gmail.com>
2020-09-08 21:46:56 +0100

    refactor: CTxMemPool::GetUnbroadcastTxs requires lock

--- src/txmempool.h
+++ src/txmempool.h
@@ -764,8 +764,9 @@ public:
     void RemoveUnbroadcastTx(const uint256& txid, const bool unchecked = false) EXCLUSIVE_LOCKS_REQUIRED(cs);
 
     /** Returns transactions in unbroadcast set */
-    std::map<uint256, uint256> GetUnbroadcastTxs() const {
-        LOCK(cs);
+    std::map<uint256, uint256> GetUnbroadcastTxs() const EXCLUSIVE_LOCKS_REQUIRED(cs)
+    {
+        AssertLockHeld(cs);
         return m_unbroadcast_txids;
     }
 
