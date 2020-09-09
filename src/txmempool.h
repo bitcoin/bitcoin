9c670ec80641bb4a8aa3f1c2376218eedba00188
sidhujag <jsidhu@blockchainfoundry.co>
2020-09-09 16:08:30 -0700

    const qualifier
    Former-commit-id: bfa59481d1a18fbfb54618be2bb74c83ae04bade

--- src/txmempool.h
+++ src/txmempool.h
@@ -653,7 +653,7 @@ public:
     void removeProTxKeyChangedConflicts(const CTransaction &tx, const uint256& proTxHash, const uint256& newKeyHash);
     void removeProTxConflicts(const CTransaction &tx);
     bool existsConflicts(const CTransaction& tx) const EXCLUSIVE_LOCKS_REQUIRED(cs);
-    bool isSyscoinConflictIsFirstSeen(const CTransaction &tx) EXCLUSIVE_LOCKS_REQUIRED(cs);
+    bool isSyscoinConflictIsFirstSeen(const CTransaction &tx) const EXCLUSIVE_LOCKS_REQUIRED(cs);
     void removeConflicts(const CTransaction& tx) EXCLUSIVE_LOCKS_REQUIRED(cs);
     // SYSCOIN
     void removeZDAGConflicts(const CTransaction& tx) EXCLUSIVE_LOCKS_REQUIRED(cs);
