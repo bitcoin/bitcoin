25963221090274b55f5181dfd3a37fef1c0e0bcc
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-09-11 01:14:04 +0300

    net: Replace uiInterface global with CConnman::clientInterface member

--- src/net.cpp
+++ src/net.cpp
@@ -2353,8 +2353,9 @@ void CConnman::SetNetworkActive(bool active)
     }
 
     fNetworkActive = active;
-
-    uiInterface.NotifyNetworkActiveChanged(fNetworkActive);
+    if (clientInterface) {
+        clientInterface->NotifyNetworkActiveChanged(fNetworkActive);
+    }
 }
 
 CConnman::CConnman(uint64_t nSeed0In, uint64_t nSeed1In, bool network_active)
@@ -2471,7 +2472,9 @@ bool CConnman::Start(CScheduler& scheduler, const Options& connOptions)
         LogPrintf("%i block-relay-only anchors will be tried for connections.\n", m_anchors.size());
     }
 
-    uiInterface.InitMessage(_("Starting network threads...").translated);
+    if (clientInterface) {
+        clientInterface->InitMessage(_("Starting network threads...").translated);
+    }
 
     fAddressesInitialized = true;
 
