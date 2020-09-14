d032fbd45b3c132a0ceb62995af5d278d600fccc
fanquake <fanquake@gmail.com>
2020-09-14 10:28:45 +0800

    Merge #19944: Update secp256k1 subtree (including BIP340 support)
    b9c1a7648131c5deec9704ee9acd00ec1820b9ce Squashed 'src/secp256k1/' changes from 2ed54da18a..8ab24e8dad (Pieter Wuille)
    
    Pull request description:
    
      This updates our src/secp256k1 subtree to the latest libsecp256k1 upstream version.
    
      As it adds BIP340 support (see https://github.com/bitcoin-core/secp256k1/pull/558), this is a prerequisite for #17977. In particular, it contains:
      * A few generic library improvements
      * Support for x-only public keys as used by BIP340.
      * Support for "key pair" objects, making signing more efficient by using a precomputed public key.
      * Signing support for BIP340 Schnorr (single-party) signatures.
      * Verification support for BIP340 Schnorr signatures.
      * Support for verifying tweaked x-only keys, as used by BIP341's Taproot construction.
    
      Things that are not included:
      * MuSig, nor any kind of multisignatures, threshold signatures, ... on top.
      * Batch verification.
      * Support for variable-length messages in BIP340 (which are still being discussed, but won't affect BIP341, or Bitcoin Core).
      * A few more generic improvements that are still in the pipeline, including faster modular inversions.
    
    ACKs for top commit:
      instagibbs:
        ACK 894fb33f4c1b24667891f7d2aff9f486177b1173
      fanquake:
        ACK 894fb33f4c1b24667891f7d2aff9f486177b1173. Any Valgrind concerns will be addressed upstream, see discussion in https://github.com/bitcoin-core/secp256k1/pull/813, and if necessary, can be pulled into our tree prior to the 0.21.0 branch off. They are not a blocker for merging this PR in it's current state.
      benthecarman:
        ACK `894fb33`
    
    Tree-SHA512: 6dc992f4477069b7fbd223316f1be955750923be1479c38adad2312649fdca1f316edb375c42ef9d97cea2407caaef49fb8c93abd6c037fe1a522910cbbc2479
    
    Former-commit-id: 0c79f84843d02cb35f7c0af0013989a1ae7bd445

--- src/secp256k1/src/valgrind_ctime_test.c
+++ src/secp256k1/src/valgrind_ctime_test.c
@@ -6,6 +6,7 @@
 
 #include <valgrind/memcheck.h>
 #include "include/secp256k1.h"
+#include "assumptions.h"
 #include "util.h"
 
 #if ENABLE_MODULE_ECDH
@@ -14,6 +15,14 @@
 
 #if ENABLE_MODULE_RECOVERY
 # include "include/secp256k1_recovery.h"
+#endif
+
+#if ENABLE_MODULE_EXTRAKEYS
+# include "include/secp256k1_extrakeys.h"
+#endif
+
+#if ENABLE_MODULE_SCHNORRSIG
+#include "include/secp256k1_schnorrsig.h"
 #endif
 
 int main(void) {
@@ -32,6 +41,9 @@ int main(void) {
     secp256k1_ecdsa_recoverable_signature recoverable_signature;
     int recid;
 #endif
+#if ENABLE_MODULE_EXTRAKEYS
+    secp256k1_keypair keypair;
+#endif
 
     if (!RUNNING_ON_VALGRIND) {
         fprintf(stderr, "This test can only usefully be run inside valgrind.\n");
@@ -49,7 +61,9 @@ int main(void) {
         msg[i] = i + 1;
     }
 
-    ctx = secp256k1_context_create(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_DECLASSIFY);
+    ctx = secp256k1_context_create(SECP256K1_CONTEXT_SIGN
+                                   | SECP256K1_CONTEXT_VERIFY
+                                   | SECP256K1_CONTEXT_DECLASSIFY);
 
     /* Test keygen. */
     VALGRIND_MAKE_MEM_UNDEFINED(key, 32);
@@ -113,6 +127,30 @@ int main(void) {
     ret = secp256k1_context_randomize(ctx, key);
     VALGRIND_MAKE_MEM_DEFINED(&ret, sizeof(ret));
     CHECK(ret);
+
+    /* Test keypair_create and keypair_xonly_tweak_add. */
+#if ENABLE_MODULE_EXTRAKEYS
+    VALGRIND_MAKE_MEM_UNDEFINED(key, 32);
+    ret = secp256k1_keypair_create(ctx, &keypair, key);
+    VALGRIND_MAKE_MEM_DEFINED(&ret, sizeof(ret));
+    CHECK(ret == 1);
+
+    /* The tweak is not treated as a secret in keypair_tweak_add */
+    VALGRIND_MAKE_MEM_DEFINED(msg, 32);
+    ret = secp256k1_keypair_xonly_tweak_add(ctx, &keypair, msg);
+    VALGRIND_MAKE_MEM_DEFINED(&ret, sizeof(ret));
+    CHECK(ret == 1);
+#endif
+
+#if ENABLE_MODULE_SCHNORRSIG
+    VALGRIND_MAKE_MEM_UNDEFINED(key, 32);
+    ret = secp256k1_keypair_create(ctx, &keypair, key);
+    VALGRIND_MAKE_MEM_DEFINED(&ret, sizeof(ret));
+    CHECK(ret == 1);
+    ret = secp256k1_schnorrsig_sign(ctx, sig, msg, &keypair, NULL, NULL);
+    VALGRIND_MAKE_MEM_DEFINED(&ret, sizeof(ret));
+    CHECK(ret == 1);
+#endif
 
     secp256k1_context_destroy(ctx);
     return 0;
