c563d8e13f20602d76ad1c7a4fccc5f8743ac8a5
sidhujag <jsidhu@blockchainfoundry.co>
2020-09-13 19:43:55 -0700

    geth testnet for testnet mode by def
    Former-commit-id: 7e5d4b9a5af324281ccd77c574062c8666caef95

--- src/validation.cpp
+++ src/validation.cpp
@@ -5779,7 +5779,7 @@ void DoGethMaintenance() {
         LogPrintf("GETH: Starting Geth and Relayer because PID's were uninitialized\n");
         int wsport = gArgs.GetArg("-gethwebsocketport", 8646);
         int ethrpcport = gArgs.GetArg("-gethrpcport", 8645);
-        bGethTestnet = gArgs.GetBoolArg("-gethtestnet", false);
+        bGethTestnet = gArgs.GetBoolArg("-gethtestnet", gArgs.GetChainName() != CBaseChainParams::MAIN);
         const std::string mode = gArgs.GetArg("-gethsyncmode", "light");
         StartGethNode(exePath, gethPID, wsport, ethrpcport, mode);
         int rpcport = gArgs.GetArg("-rpcport", BaseParams().RPCPort());
