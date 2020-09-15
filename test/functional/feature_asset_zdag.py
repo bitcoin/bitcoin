6c9fb42732f232c123491dc3623613ed59a1cf3d
sidhujag <jsidhu@blockchainfoundry.co>
2020-09-15 11:58:47 -0700

    fix sync mempool in zdag tests
    Former-commit-id: 62cfa553d1ce43b5c71bee2e758ca4c8bc393228

--- test/functional/feature_asset_zdag.py
+++ test/functional/feature_asset_zdag.py
@@ -55,7 +55,7 @@ class AssetZDAGTest(SyscoinTestFramework):
         assert_equal(len(out), 4)
         # this will use 1 sys utxo and 1 asset utxo and send it to change address owned by node2
         self.nodes[1].assetallocationsend(self.asset, newaddress1, 0.4)
-        self.sync_mempools(timeout=30)
+        self.sync_mempools(self.nodes[0:3], timeout=30)
         # node3 should have 2 less utxos because they were sent to change on node2
         out =  self.nodes[3].listunspent(minconf=0)
         assert_equal(len(out), 2)
@@ -136,10 +136,10 @@ class AssetZDAGTest(SyscoinTestFramework):
         self.nodes[1].assetallocationsend(self.asset, useraddress1, 0.2)
         self.nodes[0].assetallocationsend(self.asset, useraddress3, 0.2)
         self.nodes[2].assetallocationsend(self.asset, useraddress1, 0.2)
-        self.sync_mempools(timeout=30)
+        self.sync_mempools(self.nodes[0:3],timeout=30)
         # put all in useraddress1 so node4 can access in dbl spend, its probably in change address prior to this on node0
         self.nodes[0].assetallocationsend(self.asset, useraddress1, 1.5)
-        self.sync_mempools(timeout=30)
+        self.sync_mempools(self.nodes[0:3],timeout=30)
         txid = self.nodes[0].assetallocationsend(self.asset, useraddress1, 1.5)['txid']
         # dbl spend
         txdblspend = self.nodes[3].assetallocationburn(self.asset, 1.1, "0x931d387731bbbc988b312206c74f77d004d6b84b")["txid"]
