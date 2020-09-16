1c76e3e152cd3a8cd401f2d2f982ee8b704ce767
dustinface <35775977+xdustinface@users.noreply.github.com>
2020-09-16 11:14:09 +0200

    qt: Make sure font size in  TransactionDescDialog is adjusted properly (#3707)

--- src/qt/transactiondescdialog.cpp
+++ src/qt/transactiondescdialog.cpp
@@ -17,6 +17,7 @@ TransactionDescDialog::TransactionDescDialog(const QModelIndex &idx, QWidget *pa
     ui(new Ui::TransactionDescDialog)
 {
     ui->setupUi(this);
+    GUIUtil::updateFonts();
     setWindowTitle(tr("Details for %1").arg(idx.data(TransactionTableModel::TxHashRole).toString()));
     QString desc = idx.data(TransactionTableModel::LongDescriptionRole).toString();
     ui->detailText->setHtml(desc);
