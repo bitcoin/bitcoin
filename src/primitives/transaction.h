65e77c1db805667de25b9e286613f2d017a689d2
sidhujag <jsidhu@blockchainfoundry.co>
2020-09-17 16:43:40 -0700

    compile
    Former-commit-id: 59ec92df816db80932518bccf0179337e30561c7

--- src/primitives/transaction.h
+++ src/primitives/transaction.h
@@ -730,8 +730,8 @@ public:
         obj.vchReceiptParentNodes);
     }
 
-    inline void SetNull() { voutAssets.clear(); vchTxValue = 0; vchTxParentNodes.clear(); vchTxPath.clear(); vchReceiptValue = 0; vchReceiptParentNodes.clear(); nBridgeTransferID = 0; nBlockNumber = 0;  }
-    inline bool IsNull() const { return (voutAssets.empty() && vchTxValue === 0 && vchReceiptValue == 0); }
+    inline void SetNull() { voutAssets.clear(); posValue = 0; vchTxParentNodes.clear(); vchTxPath.clear(); posReceipt = 0; vchReceiptParentNodes.clear(); nBridgeTransferID = 0; nBlockNumber = 0;  }
+    inline bool IsNull() const { return (voutAssets.empty() && posValue === 0 && posReceipt == 0); }
     bool UnserializeFromData(const std::vector<unsigned char> &vchData);
     bool UnserializeFromTx(const CTransaction &tx);
     void SerializeData(std::vector<unsigned char>& vchData);
