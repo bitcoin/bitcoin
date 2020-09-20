37df4a287b83f17aa5a4d4300f9c19e0025495ac
sidhujag <jsidhu@blockchainfoundry.co>
2020-09-20 08:41:50 -0700

    add wallet def to dip tests
    Former-commit-id: 7045dfa8fa5aee0a0748017cf5bbbddd84c6ad38

--- test/functional/test_framework/test_framework.py
+++ test/functional/test_framework/test_framework.py
@@ -822,6 +822,8 @@ class DashTestFramework(SyscoinTestFramework):
                 self.extra_args[i].append("-dip3params=50:50")
         for i in range(0, num_nodes):
             self.extra_args[i].append("-mncollateral=100")
+        for i in range(0, num_nodes):
+            self.extra_args[i].append("-wallet=")
         # LLMQ default test params (no need to pass -llmqtestparams)
         self.llmq_size = 3
         self.llmq_threshold = 2
