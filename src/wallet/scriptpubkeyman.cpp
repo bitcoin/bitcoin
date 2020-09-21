95fedd33a23d6cb7542378afef229965f1ebfd68
maskoficarus <bitcoin@maskoficarus.com>
2020-09-20 19:07:28 -0500

    refactor: Clean up -Wlogical-op warning
    This commit fixes #19912 by removing a check that always returned true. That check was causing a -Wlogical-op warning because it treated a constant int as though it were a boolean.

--- src/wallet/scriptpubkeyman.cpp
+++ src/wallet/scriptpubkeyman.cpp
@@ -452,7 +452,7 @@ bool LegacyScriptPubKeyMan::Upgrade(int prev_version, bilingual_str& error)
         hd_upgrade = true;
     }
     // Upgrade to HD chain split if necessary
-    if (m_storage.CanSupportFeature(FEATURE_HD_SPLIT) && CHDChain::VERSION_HD_CHAIN_SPLIT) {
+    if (m_storage.CanSupportFeature(FEATURE_HD_SPLIT)) {
         WalletLogPrintf("Upgrading wallet to use HD chain split\n");
         m_storage.SetMinVersion(FEATURE_PRE_SPLIT_KEYPOOL);
         split_upgrade = FEATURE_HD_SPLIT > prev_version;
