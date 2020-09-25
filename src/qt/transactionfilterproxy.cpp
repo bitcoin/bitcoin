b22aa1813c8cc2d1a1b0aa3d235cd648e23e85d6
dustinface <35775977+xdustinface@users.noreply.github.com>
2020-09-25 18:04:53 +0200

    qt: Finetune OverviewPage (#3715)
    * qt: Adjust "Recent Transactions" in Overview tab
    
    Make sure they follow the same format as the transactions in
    TransactionsView.
    
    - Removed the transaction type representing arrows
    - Apply the same coloring like like in the transaction tab for
    the different transaction types (orange = internal, red = outgoing,
    green = incoming)
    
    * qt: Cleanup layout of OverviewPage in css
    
    * qt: Add three spacer (left, middle, right) and adjust layout stretch.
    
    This allows to have the elements on the screen aligned symetrically
    around the center independent from the window size/resizing.
    
    * qt: Inrease date/amount size for "Recent Transactions" in Overview tab
    
    * qt: Inrease number of "Recent Transactions" displayed in Overview tab
    
    Just to fill the empty space
    
    * qt: Make sure PS elements show as expected and adjust number of recent transactions based on PS
    
    * qt: Adjust transaction entry generation
    
    Co-Authored-By: UdjinM6 <UdjinM6@users.noreply.github.com>
    
    * qt: Adjust warning message box
    
    Co-Authored-By: UdjinM6 <UdjinM6@users.noreply.github.com>
    
    * Move tx list style to css
    
    * Fix tx list style for the traditional theme
    
    * Drop (no longer needed?) min height offset in SetupTransactionList
    
    Can't reproduce the tx list scrolling issue anymore
    
    * Avoid recreating transaction filter from scratch every time SetupTransactionList is called
    
    We call SetupTransactionList every second now (from privateSendStatus()) and this makes GUI unresponsive for huge wallets (I have ~400k txes in my testnet wallet) because of filter recreation/sorting. There is no need to go through all setup steps really, simply updating the limit works just fine and fixes the issue.
    
    * qt: Fix an `if` statement
    
    * qt: Just some refactoring
    
    * fix code style
    
    * bail out if `filter->rowCount() == nNumItems`
    
    * qt: Make sure number of transactions is always correct
    
    * Drop spacer to let recent tx list occupy max height available
    
    * TransactionFilterProxy::setLimit should emit signals to let coresponding layouts update themselves
    
    https://doc.qt.io/qt-5/qabstractitemmodel.html#layoutAboutToBeChanged
    https://doc.qt.io/qt-5/qabstractitemmodel.html#layoutChanged
    
    Co-authored-by: UdjinM6 <UdjinM6@users.noreply.github.com>

--- src/qt/transactionfilterproxy.cpp
+++ src/qt/transactionfilterproxy.cpp
@@ -98,7 +98,9 @@ void TransactionFilterProxy::setWatchOnlyFilter(WatchOnlyFilter filter)
 
 void TransactionFilterProxy::setLimit(int limit)
 {
+    Q_EMIT layoutAboutToBeChanged();
     this->limitRows = limit;
+    Q_EMIT layoutChanged();
 }
 
 void TransactionFilterProxy::setShowInactive(bool _showInactive)
