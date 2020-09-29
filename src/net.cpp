6d953a42f68571d6fa0cebad4de718403eac2cba
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-09-29 18:02:44 +0300

    net, refactor: Move AddLocal call one level up
    This change simplifies the following commit.
    
    
    Former-commit-id: 512dfc7e3afa2f3fa11b221aeb7a086697d52e13

--- src/net.cpp
+++ src/net.cpp
@@ -2493,10 +2493,6 @@ bool CConnman::BindListenPort(const CService& addrBind, bilingual_str& strError,
     }
 
     vhListenSocket.push_back(ListenSocket(hListenSocket, permissions));
-
-    if (addrBind.IsRoutable() && fDiscover && (permissions & PF_NOBAN) == 0)
-        AddLocal(addrBind, LOCAL_BIND);
-
     return true;
 }
 
@@ -2593,6 +2589,11 @@ bool CConnman::Bind(const CService &addr, unsigned int flags, NetPermissionFlags
         }
         return false;
     }
+
+    if (addr.IsRoutable() && fDiscover && (permissions & PF_NOBAN) == 0) {
+        AddLocal(addr, LOCAL_BIND);
+    }
+
     return true;
 }
 
