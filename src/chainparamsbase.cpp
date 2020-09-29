b6783409112fc21768410ab7e77d6c6e6822b2d6
fanquake <fanquake@gmail.com>
2020-09-29 13:29:49 +0800

    Merge #20014: doc: Mention signet in -help output
    b3972bca9f74b3e75918676ef029f1964b0622a1 doc: Mention signet in -help output (Hennadii Stepanov)
    
    Pull request description:
    
      ```
      $ src/bitcoind -help | grep -A 4 -e '-chain=' | head -8
        -chain=<chain>
             Use the chain <chain> (default: main). Allowed values: main, test,
             signet, regtest
    
        -signet
             Use the signet chain. Equivalent to -chain=signet. Note that the network
             is defined by the -signetchallenge parameter
    
      ```
    
      ```
      $ src/bitcoind -help | grep -A 3 -e '-port='
        -port=<port>
             Listen for connections on <port> (default: 8333, testnet: 18333 signet:
             38333, regtest: 18444)
    
      ```
    
      ```
      $ src/bitcoind -help | grep -A 3 -e '-rpcport='
        -rpcport=<port>
             Listen for JSON-RPC connections on <port> (default: 8332, testnet:
             18332, signet: 38332, regtest: 18443)
    
      ```
    
    ACKs for top commit:
      jonatack:
        ACK b3972bca9f74b3e75918676ef029f1964b0622a1
      kallewoof:
        ACK b3972bca9f74b3e75918676ef029f1964b0622a1
      ajtowns:
        ACK b3972bca9f74b3e75918676ef029f1964b0622a1 - skimmed code only, looks fine to me
    
    Tree-SHA512: 66c59cdc3c19e8f8a02d3f3f992ff1db06769df63244d4af62629e18aaf4a12b3b7e75e4a0b9f616033cdc4415da046053ba36fede8be145b2dc695b2aa69a02
    
    Former-commit-id: b6487cf0de7777e8f721b282b0049950ec92f615

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -18,13 +18,13 @@ const std::string CBaseChainParams::REGTEST = "regtest";
 
 void SetupChainParamsBaseOptions(ArgsManager& argsman)
 {
-    argsman.AddArg("-chain=<chain>", "Use the chain <chain> (default: main). Allowed values: main, test, regtest", ArgsManager::ALLOW_ANY, OptionsCategory::CHAINPARAMS);
+    argsman.AddArg("-chain=<chain>", "Use the chain <chain> (default: main). Allowed values: main, test, signet, regtest", ArgsManager::ALLOW_ANY, OptionsCategory::CHAINPARAMS);
     argsman.AddArg("-regtest", "Enter regression test mode, which uses a special chain in which blocks can be solved instantly. "
                  "This is intended for regression testing tools and app development. Equivalent to -chain=regtest.", ArgsManager::ALLOW_ANY | ArgsManager::DEBUG_ONLY, OptionsCategory::CHAINPARAMS);
     argsman.AddArg("-segwitheight=<n>", "Set the activation height of segwit. -1 to disable. (regtest-only)", ArgsManager::ALLOW_ANY | ArgsManager::DEBUG_ONLY, OptionsCategory::DEBUG_TEST);
     argsman.AddArg("-testnet", "Use the test chain. Equivalent to -chain=test.", ArgsManager::ALLOW_ANY, OptionsCategory::CHAINPARAMS);
     argsman.AddArg("-vbparams=deployment:start:end", "Use given start/end times for specified version bits deployment (regtest-only)", ArgsManager::ALLOW_ANY | ArgsManager::DEBUG_ONLY, OptionsCategory::CHAINPARAMS);
-    argsman.AddArg("-signet", "Use the signet chain. Note that the network is defined by the -signetchallenge parameter", ArgsManager::ALLOW_ANY, OptionsCategory::CHAINPARAMS);
+    argsman.AddArg("-signet", "Use the signet chain. Equivalent to -chain=signet. Note that the network is defined by the -signetchallenge parameter", ArgsManager::ALLOW_ANY, OptionsCategory::CHAINPARAMS);
     argsman.AddArg("-signetchallenge", "Blocks must satisfy the given script to be considered valid (only for signet networks; defaults to the global default signet test network challenge)", ArgsManager::ALLOW_STRING, OptionsCategory::CHAINPARAMS);
     argsman.AddArg("-signetseednode", "Specify a seed node for the signet network, in the hostname[:port] format, e.g. sig.net:1234 (may be used multiple times to specify multiple seed nodes; defaults to the global default signet test network seed node(s))", ArgsManager::ALLOW_STRING, OptionsCategory::CHAINPARAMS);
 }
