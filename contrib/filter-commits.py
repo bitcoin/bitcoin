3f022cfbde66412b53e801fb1af1f5660b3dd0e9
Bohdan Vanieiev <warchantua@gmail.com>
2020-09-30 17:53:28 +0300

    Add more to filter

--- contrib/filter-commits.py
+++ contrib/filter-commits.py
@@ -7,9 +7,18 @@ if "INITIAL_COMMIT" in os.environ:
     initialCommit = os.environ["INITIAL_COMMIT"]
 
 filterPaths = [
-    "build_msvc",
-    "contrib/filter-commits.py",
-    ".gitignore"
+    "./build_msvc",
+    ".gitignore",
+    ".dockerignore",
+    ".github",
+    ".gitmodules",
+    "CONTRIBUTING.md",
+    "COPYING",
+    "INSTALL.md",
+    "README.md",
+    "SECURITY.md",
+    "*.Dockerfile",
+    "contrib"
 ]
 
 subprocess.run(["git", "reset", "--soft", initialCommit])
@@ -18,9 +27,9 @@ subprocess.run(["git", "add", "--all", "."])
 for p in filterPaths:
     subprocess.run(["git", "rm", "--cached", "-r", p])
 
-subprocess.run(["git", "commit", "-m", "Auto squash commits from master since " + initialCommit])
+subprocess.run(["git", "commit", "-m", "Features added since " + initialCommit])
 
 for p in filterPaths:
     subprocess.run(["git", "add", p])
 
-subprocess.run(["git", "commit", "-m", "Auxiliary changes"])
+subprocess.run(["git", "commit", "-m", "Branding changes"])
