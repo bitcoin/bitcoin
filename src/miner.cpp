dfdb9eaa9c1ff62696c07cf38f5b7b7efcce8b2a
Carl Dong <contact@carldong.me>
2020-09-30 17:02:51 -0400

    validation: Pass in chainstate to TestBlockValidity
    [META] This commit should be followed up by removing the comments and
           assertions meant only to show that the change is correct.

--- src/miner.cpp
+++ src/miner.cpp
@@ -176,7 +176,7 @@ std::unique_ptr<CBlockTemplate> BlockAssembler::CreateNewBlock(const CScript& sc
     pblocktemplate->vTxSigOpsCost[0] = WITNESS_SCALE_FACTOR * GetLegacySigOpCount(*pblock->vtx[0]);
 
     BlockValidationState state;
-    if (!TestBlockValidity(state, chainparams, *pblock, pindexPrev, false, false)) {
+    if (!TestBlockValidity(state, chainparams, ::ChainstateActive(), *pblock, pindexPrev, false, false)) {
         throw std::runtime_error(strprintf("%s: TestBlockValidity failed: %s", __func__, state.ToString()));
     }
     int64_t nTime2 = GetTimeMicros();
