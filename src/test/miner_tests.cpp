3ddc202aee8a48cb580074cbe0037dc8c0fda483
Wladimir J. van der Laan <laanwj@protonmail.com>
2020-09-30 16:18:57 +0200

    Merge #20004: test: Add signet witness commitment section parse tests
    fa29b5ae666bbb4c19188f0dcf8a1ba738aac624 test: Add signet witness commitment section parse tests (MarcoFalke)
    fa23308e9aad70c99a31f91d8556f1876ea02c04 Remove gArgs global from CreateChainParams to aid testing (MarcoFalke)
    
    Pull request description:
    
    ACKs for top commit:
      laanwj:
        ACK fa29b5ae666bbb4c19188f0dcf8a1ba738aac624
    
    Tree-SHA512: f956407d690decbfb8178bcb8f101d107389fecc3aa7be515f7b0f5ceac26d798c165100f7ddf08cec569beabcc6514862dda23b667cc4fd0a784316784735c2
    
    Former-commit-id: e03b190aedf32e1df88b6810ac74411e1ea8057c

--- src/test/miner_tests.cpp
+++ src/test/miner_tests.cpp
@@ -198,7 +198,7 @@ void MinerTestingSetup::TestPackageSelection(const CChainParams& chainparams, co
 BOOST_AUTO_TEST_CASE(CreateNewBlock_validity)
 {
     // Note that by default, these tests run with size accounting enabled.
-    const auto chainParams = CreateChainParams(CBaseChainParams::MAIN);
+    const auto chainParams = CreateChainParams(*m_node.args, CBaseChainParams::MAIN);
     const CChainParams& chainparams = *chainParams;
     CScript scriptPubKey = CScript() << ParseHex("04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f") << OP_CHECKSIG;
     std::unique_ptr<CBlockTemplate> pblocktemplate;
