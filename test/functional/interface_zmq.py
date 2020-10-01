e29a35a997072064fb58ca0b160da79e68b475d3
Wladimir J. van der Laan <laanwj@protonmail.com>
2020-10-01 17:41:22 +0200

    Merge #18309: zmq: Add support to listen on multiple interfaces
    e66870c5a4c2adbd30dca67d409fd5cd98697587 zmq: Append address to notify log output (nthumann)
    241803da211265444e65f254f24dd184f2457fa9 test: Add zmq test to support multiple interfaces (nthumann)
    a0b2e5cb6aa8db0563fac7d67a949b9baefe3a25 doc: Add release notes to support multiple interfaces (nthumann)
    b1c3f180ecb63f3960506d202feebaa4271058ae doc: Adjust ZMQ usage to support multiple interfaces (nthumann)
    347c94f551c3f144c44e00373e4dd61ff6d908b7 zmq: Add support to listen on multiple interfaces (Nicolas Thumann)
    
    Pull request description:
    
      This PR adds support for ZeroMQ to listen on multiple interfaces, just like the RPC server.
      Currently, if you specify more than one e.g. `zmqpubhashblock` paramter, only the first one will be used. Therefore a user may be forced to listen on all interfaces (e.g. `zmqpubhashblock=0.0.0.0:28332`), which can result in an increased attack surface.
      With this PR a user can specify multiple interfaces to listen on, e.g.
      `-zmqpubhashblock=tcp://127.0.0.1:28332 -zmqpubhashblock=tcp://192.168.1.123:28332`.
    
    ACKs for top commit:
      laanwj:
        Code review ACK e66870c5a4c2adbd30dca67d409fd5cd98697587
      instagibbs:
        reACK https://github.com/bitcoin/bitcoin/pull/18309/commits/e66870c5a4c2adbd30dca67d409fd5cd98697587
    
    Tree-SHA512: f38ab4a6ff00dc821e5f4842508cefadb701e70bb3893992c1b32049be20247c8aa9476a1f886050c5f17fe7f2ce99ee30193ce2c81a7482a5a51f8fc22300c7

--- test/functional/interface_zmq.py
+++ test/functional/interface_zmq.py
@@ -78,6 +78,7 @@ class ZMQTest (BitcoinTestFramework):
     def run_test(self):
         try:
             self._zmq_test()
+            self.test_multiple_interfaces()
         finally:
             # Destroy the ZMQ context.
             self.log.debug("Destroying ZMQ context")
@@ -130,6 +131,30 @@ class ZMQTest (BitcoinTestFramework):
         ])
 
         assert_equal(self.nodes[1].getzmqnotifications(), [])
+
+    def test_multiple_interfaces(self):
+        import zmq
+        # Set up two subscribers with different addresses
+        subscribers = []
+        for i in range(2):
+            address = 'tcp://127.0.0.1:%d' % (28334 + i)
+            socket = self.zmq_context.socket(zmq.SUB)
+            socket.set(zmq.RCVTIMEO, 60000)
+            hashblock = ZMQSubscriber(socket, b"hashblock")
+            socket.connect(address)
+            subscribers.append({'address': address, 'hashblock': hashblock})
+
+        self.restart_node(0, ['-zmqpub%s=%s' % (subscriber['hashblock'].topic.decode(), subscriber['address']) for subscriber in subscribers])
+
+        # Relax so that the subscriber is ready before publishing zmq messages
+        sleep(0.2)
+
+        # Generate 1 block in nodes[0] and receive all notifications
+        self.nodes[0].generatetoaddress(1, ADDRESS_BCRT1_UNSPENDABLE)
+
+        # Should receive the same block hash on both subscribers
+        assert_equal(self.nodes[0].getbestblockhash(), subscribers[0]['hashblock'].receive().hex())
+        assert_equal(self.nodes[0].getbestblockhash(), subscribers[1]['hashblock'].receive().hex())
 
 if __name__ == '__main__':
     ZMQTest().main()
