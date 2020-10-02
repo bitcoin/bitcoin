717a41a5721182bae54ed6613cff6a7b49fe7955
dustinface <35775977+xdustinface@users.noreply.github.com>
2020-10-02 21:50:17 +0200

    qt: Make sure there is a valid theme set in the options (#3755)
    * qt: Add GUIUtil::isValidTheme()
    
    Check if a given string is a valid theme.
    
    * qt: Make sure there is a valid theme set in the options

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -1152,6 +1152,11 @@ const QString getDefaultTheme()
     return defaultTheme;
 }
 
+const bool isValidTheme(const QString& strTheme)
+{
+    return strTheme == defaultTheme || strTheme == darkThemePrefix || strTheme == traditionalTheme;
+}
+
 void loadStyleSheet(QWidget* widget, bool fForceUpdate)
 {
     AssertLockNotHeld(cs_css);
