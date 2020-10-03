8d58fdf1a92e64ff44c7676088b2da95eb283e30
fanquake <fanquake@gmail.com>
2020-10-03 09:56:59 +0800

    Merge #19956: rpc: Improve invalid vout value rpc error message
    f471a3be00c2b6433b8c258b716982c0539da13f scripted diff: Improve invalid vout value rpc error message (Nima Yazdanmehr)
    
    Pull request description:
    
      Since the `vout` value can start at `0`, the error message for *negative* values can be improved to something like: `vout cannot be negative`.
    
    ACKs for top commit:
      fanquake:
        ACK f471a3be00c2b6433b8c258b716982c0539da13f
      promag:
        Code review ACK f471a3be00c2b6433b8c258b716982c0539da13f.
    
    Tree-SHA512: fbdee3d0ddd5b58eb93934a1217b44e125a9ad39e672b1f35c7609c6c5fcf45ae1b731d3d6135b7225d98792dbfc34a50907b8c41274a5b029d7b5c59f886560

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -2321,7 +2321,7 @@ static UniValue lockunspent(const JSONRPCRequest& request)
 
         const int nOutput = find_value(o, "vout").get_int();
         if (nOutput < 0) {
-            throw JSONRPCError(RPC_INVALID_PARAMETER, "Invalid parameter, vout must be positive");
+            throw JSONRPCError(RPC_INVALID_PARAMETER, "Invalid parameter, vout cannot be negative");
         }
 
         const COutPoint outpt(uint256S(txid), nOutput);
