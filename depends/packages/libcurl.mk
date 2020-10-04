0cc86132b7edcd908a1e3bf55c0d5fc4be37ea9d
sidhujag <jsidhu@blockchainfoundry.co>
2020-10-03 19:11:36 -0700

    rmv setvars fn
    Former-commit-id: 114a4a8d3f0940de0dd3d7827304238a4b2aaa6f

--- depends/packages/libcurl.mk
+++ depends/packages/libcurl.mk
@@ -5,23 +5,22 @@ $(package)_download_path=https://curl.haxx.se/download
 $(package)_file_name=curl-$($(package)_version).tar.gz
 $(package)_sha256_hash=d4d5899a3868fbb6ae1856c3e55a32ce35913de3956d1973caccd37bd0174fa2
 
-define $(package)_set_vars
-  # default settings
-  $(package)_config_opts_default=LIBS="-lnettle -lhogweed -lgmp" --without-nghttp2 --disable-shared --enable-static --disable-ftp --without-ssl --with-gnutls="$(PREFIX_DIR)" --disable-ntlm-wb --disable-file --disable-ldap --disable-ldaps --disable-rtsp --disable-dict --disable-telnet --disable-tftp --disable-pop3 --disable-imap --disable-smb --disable-smtp --disable-gopher --enable-proxy --without-ca-path --without-ca-bundle --with-ca-fallback --disable-telnet --enable-threaded-resolver
+# default settings
+$(package)_config_opts_default=LIBS="-lnettle -lhogweed -lgmp" --without-nghttp2 --disable-shared --enable-static --disable-ftp --without-ssl --with-gnutls="$(PREFIX_DIR)" --disable-ntlm-wb --disable-file --disable-ldap --disable-ldaps --disable-rtsp --disable-dict --disable-telnet --disable-tftp --disable-pop3 --disable-imap --disable-smb --disable-smtp --disable-gopher --enable-proxy --without-ca-path --without-ca-bundle --with-ca-fallback --disable-telnet --enable-threaded-resolver
 
-  # mingw specific settings
-  $(package)_config_opts_mingw32=LIBS="-lcrypt32 -lnettle -lhogweed -lgmp" -DCURL_STATIC_LIB -static" --disable-ftp --without-ssl --with-gnutls="$(PREFIX_DIR)" --disable-ntlm-wb --disable-file --disable-ldap --disable-ldaps --disable-rtsp --disable-dict --disable-telnet --disable-tftp --disable-pop3 --disable-imap --disable-smb --disable-smtp --disable-gopher --enable-proxy --without-ca-path --without-ca-bundle --with-ca-fallback --disable-telnet
+# mingw specific settings
+$(package)_config_opts_mingw32=LIBS="-lcrypt32 -lnettle -lhogweed -lgmp" -DCURL_STATIC_LIB -static" --disable-ftp --without-ssl --with-gnutls="$(PREFIX_DIR)" --disable-ntlm-wb --disable-file --disable-ldap --disable-ldaps --disable-rtsp --disable-dict --disable-telnet --disable-tftp --disable-pop3 --disable-imap --disable-smb --disable-smtp --disable-gopher --enable-proxy --without-ca-path --without-ca-bundle --with-ca-fallback --disable-telnet
 
-  $(package)_config_opts_x86_64-w64-mingw32=$($(package)_config_opts_mingw32)
-  $(package)_config_opts_i686-w64-mingw32=$($(package)_config_opts_mingw32)
+$(package)_config_opts_x86_64-w64-mingw32=$($(package)_config_opts_mingw32)
+$(package)_config_opts_i686-w64-mingw32=$($(package)_config_opts_mingw32)
 
-  # darwin specific settings
-  $(package)_config_opts_darwin=--with-sysroot="$(DARWIN_SDK_PATH)" --disable-ftp --without-gnutls --with-darwinssl --without-ssl --disable-ntlm-wb --disable-file --disable-ldap --disable-ldaps --disable-rtsp --disable-dict --disable-telnet --disable-tftp --disable-pop3 --disable-imap --disable-smb --disable-smtp --disable-gopher --enable-proxy --disable-telnet --enable-threaded-resolver
-  $(package)_config_opts_x86_64-apple-darwin11=$($(package)_config_opts_darwin)
+# darwin specific settings
+$(package)_config_opts_darwin=--with-sysroot="$(DARWIN_SDK_PATH)" --disable-ftp --without-gnutls --with-darwinssl --without-ssl --disable-ntlm-wb --disable-file --disable-ldap --disable-ldaps --disable-rtsp --disable-dict --disable-telnet --disable-tftp --disable-pop3 --disable-imap --disable-smb --disable-smtp --disable-gopher --enable-proxy --disable-telnet --enable-threaded-resolver
+$(package)_config_opts_x86_64-apple-darwin11=$($(package)_config_opts_darwin)
 
   # set settings based on host
-  $(package)_config_opts = $(if $($(package)_config_opts_$(HOST)), $($(package)_config_opts_$(HOST)), $($(package)_config_opts_default))
-endef
+$(package)_config_opts = $(if $($(package)_config_opts_$(HOST)), $($(package)_config_opts_$(HOST)), $($(package)_config_opts_default))
+
 define $(package)_config_cmds
   $($(package)_autoconf)
 endef
--- depends/packages/nettle.mk
+++ depends/packages/nettle.mk
@@ -3,12 +3,11 @@ $(package)_version=3.3
 $(package)_download_path=https://ftp.gnu.org/gnu/nettle
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=46942627d5d0ca11720fec18d81fc38f7ef837ea4197c1f630e71ce0d470b11e
-define $(package)_set_vars
-  # default settings  $(package)_config_opts_default=--enable-pic -fPIC -static
+# default settings  $(package)_config_opts_default=--enable-pic -fPIC -static
 
-  # set settings based on host
-  $(package)_config_opts = $(if $($(package)_config_opts_$(HOST)), $($(package)_config_opts_$(HOST)), $($(package)_config_opts_default))
-endef
+# set settings based on host
+$(package)_config_opts = $(if $($(package)_config_opts_$(HOST)), $($(package)_config_opts_$(HOST)), $($(package)_config_opts_default))
+
 define $(package)_config_cmds
   $($(package)_autoconf)
 endef
