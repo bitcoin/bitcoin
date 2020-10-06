1fc96ab02d620ed03bd1072e505995f91c933ae2
sidhujag <jsidhu@blockchainfoundry.co>
2020-10-06 16:11:43 -0700

    update bls
    Former-commit-id: dc41a60e3235e343ecd7ded777d1a88fa5a7a416

--- depends/packages/chia_bls.mk
+++ depends/packages/chia_bls.mk
@@ -1,9 +1,9 @@
 package=chia_bls
-$(package)_version=v20181101
+$(package)_version=v20201006
 # It's actually from https://github.com/Chia-Network/bls-signatures, but we have so many patches atm that it's forked
-$(package)_download_path=https://github.com/codablock/bls-signatures/archive
+$(package)_download_path=https://github.com/syscoin/bls-signatures/archive
 $(package)_file_name=$($(package)_version).tar.gz
-$(package)_sha256_hash=b3ec74a77a7b6795f84b05e051a0824ef8d9e05b04b2993f01040f35689aa87c
+$(package)_sha256_hash=e2da846b832922f90bd05f49d64aa6ad0875b04cc115ebc0dbf58f52aec0597a
 $(package)_dependencies=gmp
 #$(package)_patches=...TODO (when we switch back to https://github.com/Chia-Network/bls-signatures)
 
