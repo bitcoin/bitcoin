788e7520cb8c0ad1941749237adc4f8d34d90be3
sidhujag <jsidhu@blockchainfoundry.co>
2020-10-05 19:58:25 -0700

    Revert "use older version of curl"
    This reverts commit fbaa290b83114045b7a9329e3fc7d77270446f7a [formerly ed8ac25fef267ec18489c7e5c4b31e480f03ff79].
    
    
    Former-commit-id: 56c3aa65c9e6ca49b167b2069e3e3dfbc11398f1

--- depends/packages/libcurl.mk
+++ depends/packages/libcurl.mk
@@ -1,9 +1,9 @@
 package=curl
-$(package)_version=7.52.1
+$(package)_version=7.72.0
 $(package)_dependencies=gnutls
 $(package)_download_path=https://curl.haxx.se/download
 $(package)_file_name=curl-$($(package)_version).tar.gz
-$(package)_sha256_hash=a8984e8b20880b621f61a62d95ff3c0763a3152093a9f9ce4287cfd614add6ae
+$(package)_sha256_hash=d4d5899a3868fbb6ae1856c3e55a32ce35913de3956d1973caccd37bd0174fa2
 
 # default settings
 $(package)_config_opts=LIBS="-lnettle -lhogweed -lgmp" --without-nghttp2 --disable-shared --enable-static --disable-ftp --without-ssl --with-gnutls="$(PREFIX_DIR)" --disable-ntlm-wb --disable-file --disable-ldap --disable-ldaps --disable-rtsp --disable-dict --disable-telnet --disable-tftp --disable-pop3 --disable-imap --disable-smb --disable-smtp --disable-gopher --enable-proxy --without-ca-path --without-ca-bundle --with-ca-fallback --enable-threaded-resolver
