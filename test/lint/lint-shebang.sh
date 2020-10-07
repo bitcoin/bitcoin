31abcbf9606ff883c7f703f497615c4290fccfb6
sidhujag <jsidhu@blockchainfoundry.co>
2020-10-07 16:20:15 -0700

    typo
    Former-commit-id: 3c5be40bd5e23ab863ae057fab3cf028e1eec4e2

--- test/lint/lint-shebang.sh
+++ test/lint/lint-shebang.sh
@@ -7,7 +7,7 @@
 
 export LC_ALL=C
 EXIT_CODE=0
-for PYTHON_FILE in $(git ls-files -- "*.py" | grep -vE "src/(secp256k1|univalue|chiabls)/"); do
+for PYTHON_FILE in $(git ls-files -- "*.py" | grep -vE "src/(chiabls)/"); do
     if [[ $(head -c 2 "${PYTHON_FILE}") == "#!" &&
           $(head -n 1 "${PYTHON_FILE}") != "#!/usr/bin/env python3" ]]; then
         echo "Missing shebang \"#!/usr/bin/env python3\" in ${PYTHON_FILE} (do not use python or python2)"
