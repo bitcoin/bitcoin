7eb726581aa076233e5fbe3fed363ead86609909
sidhujag <jsidhu@blockchainfoundry.co>
2020-10-07 07:53:00 -0700

    build both versions of gmp
    Former-commit-id: 9292088a04d0bcdd1f1bcdbb2c909093cb234802

--- depends/packages/gnutls.mk
+++ depends/packages/gnutls.mk
@@ -7,7 +7,7 @@ $(package)_sha256_hash=0e97f243ae72b70307d684b84c7fe679385aa7a7a0e37e5be810193dc
 
 
 # default settings
-$(package)_config_opts=NETTLE_CFLAGS="-static" GMP_CFLAGS="-static" --disable-shared --enable-static --with-included-libtasn1 --with-included-unistring --enable-local-libopts --disable-non-suiteb-curves --disable-doc --without-p11-kit
+$(package)_config_opts=--disable-shared --enable-static --with-included-libtasn1 --with-included-unistring --enable-local-libopts --disable-non-suiteb-curves --disable-doc --without-p11-kit
 
 # mingw dll specific settings
 ifeq ($(BUILD_DLL), 1)
