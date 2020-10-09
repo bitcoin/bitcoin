bf7d6e31b1062ab5f90e14e83c56309f499fa2e9
Elliott Jin <elliott.jin@gmail.com>
2020-10-09 08:50:44 -0700

    RPC: getblock: tx fee calculation for verbosity 2 via Undo data
    Co-authored-by: Felix Weis <mail@felixweis.com>

--- src/core_io.h
+++ src/core_io.h
@@ -18,6 +18,7 @@ class CTransaction;
 struct CMutableTransaction;
 class uint256;
 class UniValue;
+class CTxUndo;
 
 // core_read.cpp
 CScript ParseScript(const std::string& s);
@@ -45,6 +46,6 @@ std::string EncodeHexTx(const CTransaction& tx, const int serializeFlags = 0);
 std::string SighashToStr(unsigned char sighash_type);
 void ScriptPubKeyToUniv(const CScript& scriptPubKey, UniValue& out, bool fIncludeHex);
 void ScriptToUniv(const CScript& script, UniValue& out, bool include_address);
-void TxToUniv(const CTransaction& tx, const uint256& hashBlock, UniValue& entry, bool include_hex = true, int serialize_flags = 0);
+void TxToUniv(const CTransaction& tx, const uint256& hashBlock, UniValue& entry, bool include_hex = true, int serialize_flags = 0, const CTxUndo* txundo = nullptr);
 
 #endif // BITCOIN_CORE_IO_H
