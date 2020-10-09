e5e12a716ecd52580d3db68bdad497980b7fc14f
sidhujag <jsidhu@blockchainfoundry.co>
2020-10-09 07:52:15 -0700

    disable tests on gnu
    Former-commit-id: f8b78711b56e4cacd748ad3d98ae0659dee7af61

--- depends/packages/gnutls.mk
+++ depends/packages/gnutls.mk
@@ -7,12 +7,7 @@ $(package)_sha256_hash=0ea8c3283de8d8335d7ae338ef27c53a916f15f382753b174c18b45ff
 
 
 # default settings
-$(package)_config_opts=--disable-shared --enable-static --with-included-libtasn1 --with-included-unistring --enable-local-libopts --disable-non-suiteb-curves --disable-doc --without-p11-kit
-
-# mingw dll specific settings
-ifeq ($(BUILD_DLL), 1)
-$(package)_config_opts_x86_64_mingw32=PKG_CONFIG_LIBDIR="$(PREFIX_DIR)/lib/pkgconfig" CFLAGS="-I$(PREFIX_DIR)include -L$(PREFIX_DIR)lib -D_WIN32_WINNT=0x0600 -DNCRYPT_PAD_PKCS1_FLAG=2 -DNCRYPT_SHA1_ALGORITHM=BCRYPT_SHA1_ALGORITHM -DNCRYPT_SHA256_ALGORITHM=BCRYPT_SHA256_ALGORITHM -DCERT_NCRYPT_KEY_HANDLE_TRANSFER_PROP_ID=99"
-endif
+$(package)_config_opts=--disable-shared --enable-static --with-included-libtasn1 --with-included-unistring --enable-local-libopts --disable-non-suiteb-curves --disable-doc --without-p11-kit --disable-tests
 
 define $(package)_config_cmds
   $($(package)_autoconf)
