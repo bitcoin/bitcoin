91c5829db4983e84884088374794e211c1d9dc1c
sidhujag <jsidhu@blockchainfoundry.co>
2020-10-09 19:43:09 -0700

    Revert "try to use autoconf in chia"
    This reverts commit 782afd4030f63e72ed7e1f196e6a6a9174236055 [formerly f820af16b129f282ccfc9e157215830cb5f65ab7].
    
    
    Former-commit-id: e1cb1d9876d221ec4f472c469ee60671d7b0a010

--- depends/packages/chia_bls.mk
+++ depends/packages/chia_bls.mk
@@ -11,9 +11,20 @@ $(package)_dependencies=gmp
 #  for i in $($(package)_patches); do patch -N -p1 < $($(package)_patch_dir)/$$$$i; done
 #endef
 
+define $(package)_set_vars
+  $(package)_cflags=-DSTLIB=ON -DSHLIB=OFF -DSTBIN=ON
+  $(package)_cflags_linux=-DOPSYS=LINUX -DCMAKE_SYSTEM_NAME=Linux
+  $(package)_cflags_darwin=-DOPSYS=MACOSX -DCMAKE_SYSTEM_NAME=Darwin
+  $(package)_cflags_mingw32=-DOPSYS=WINDOWS -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_SHARED_LIBRARY_LINK_C_FLAGS="" -DCMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS=""
+  $(package)_cflags_i686+=-DWSIZE=32
+  $(package)_cflags_x86_64+=-DWSIZE=64
+  $(package)_cflags_arm+=-DWSIZE=32
+  $(package)_cflags_armv7l+=-DWSIZE=32
+endef
+
 define $(package)_config_cmds
   mkdir -p build && cd build && \
-  $($(package)_autoconf)
+  $($(package)_cmake) $($(package)_cflags) ../
 endef
 
 define $(package)_build_cmds
