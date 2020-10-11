463ae30109af36276e5c259149bb2318f0083f05
sidhujag <jsidhu@blockchainfoundry.co>
2020-10-10 20:32:43 -0700

    rmv ossl
    Former-commit-id: c0534e0841a7dd3d6f7fc1f2e9b7b1c1bd9a4006

--- depends/packages/nettle.mk
+++ depends/packages/nettle.mk
@@ -5,7 +5,7 @@ $(package)_download_path=https://ftp.gnu.org/gnu/nettle
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=d24c0d0f2abffbc8f4f34dcf114b0f131ec3774895f3555922fe2f40f3d5e3f1
 # default settings  
-$(package)_config_opts=--disable-shared --enable-static --disable-documentation
+$(package)_config_opts=--disable-shared --enable-static --disable-documentation --disable-openssl
 $(package)_config_opts_linux=--enable-pic
 
 define $(package)_config_cmds
