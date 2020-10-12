42981ae3feaf48cc479cdedcf06178cea40ea0a5
sidhujag <jsidhu@blockchainfoundry.co>
2020-10-11 23:15:19 -0700

    update sha
    Former-commit-id: 845c438967b8b69db4e2dd40d2709aba55d7e77e

--- depends/packages/libcurl.mk
+++ depends/packages/libcurl.mk
@@ -3,7 +3,7 @@ $(package)_version=7.69.1
 $(package)_dependencies=gnutls zlib
 $(package)_download_path=https://curl.haxx.se/download
 $(package)_file_name=curl-$($(package)_version).tar.gz
-$(package)_sha256_hash=ca2feeb8ef13368ce5d5e5849a5fd5e2dd4755fecf7d8f0cc94000a4206fb8e7
+$(package)_sha256_hash=01ae0c123dee45b01bbaef94c0bc00ed2aec89cb2ee0fd598e0d302a6b5e0a98
 
 # default settings
 $(package)_config_opts=--without-nghttp2 --disable-shared --enable-static --disable-ftp --without-ssl --with-gnutls="$(PREFIX_DIR)" --with-zlib="$(PREFIX_DIR)" --disable-ntlm-wb --disable-file --disable-ldap --disable-ldaps --disable-rtsp --disable-dict --disable-telnet --disable-tftp --disable-pop3 --disable-imap --disable-smb --disable-smtp --disable-gopher --enable-proxy --without-ca-path --without-ca-bundle --with-ca-fallback --enable-threaded-resolver
