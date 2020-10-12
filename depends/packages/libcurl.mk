e4fce69559fe80f76347abc99427a8fa038069d0
sidhujag <jsidhu@blockchainfoundry.co>
2020-10-12 12:18:01 -0700

    fix libs
    Former-commit-id: 7bfacb34b712d8ab9f2b46249780adf86e1080c7

--- depends/packages/libcurl.mk
+++ depends/packages/libcurl.mk
@@ -7,13 +7,13 @@ $(package)_sha256_hash=d4d5899a3868fbb6ae1856c3e55a32ce35913de3956d1973caccd37bd
 
 # default settings
 $(package)_config_opts=--with-zlib="$(PREFIX_DIR)" --disable-shared --enable-static --disable-ftp --disable-file --disable-ldap --disable-ldaps --disable-rtsp --disable-dict --disable-telnet --disable-tftp --disable-pop3 --disable-imap --disable-smb --disable-smtp --disable-gopher --enable-proxy --without-ssl --with-gnutls="$(PREFIX_DIR)" --without-ca-path --without-ca-bundle --with-ca-fallback --disable-telnet
-$(package)_config_opts_linux=LIBS="-lcrypt32 -lnettle -lhogweed -lgmp" --without-ssl --with-gnutls="$(PREFIX_DIR)" --enable-threaded-resolver
+$(package)_config_opts_linux=LIBS="-lnettle -lhogweed -lgmp" --without-ssl --with-gnutls="$(PREFIX_DIR)" --enable-threaded-resolver
 
 # mingw specific settings
 $(package)_config_opts_mingw32=LIBS="-lcrypt32 -lnettle -lhogweed -lgmp" --without-ssl --with-gnutls="$(PREFIX_DIR)"
 
 # darwin specific settings
-$(package)_config_opts_darwin=--with-sysroot="$(DARWIN_SDK_PATH)" --with-darwinssl --without-ssl --enable-threaded-resolver
+$(package)_config_opts_darwin=LIBS="-lnettle -lhogweed -lgmp" --with-sysroot="$(DARWIN_SDK_PATH)" --with-darwinssl --without-ssl --enable-threaded-resolver
 
 define $(package)_config_cmds
   $($(package)_autoconf)
