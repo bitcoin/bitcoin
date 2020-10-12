f004ef4faf6112b4b852ba7a10962de3d874b515
sidhujag <jsidhu@blockchainfoundry.co>
2020-10-11 23:25:30 -0700

    use old tls and nettle
    Former-commit-id: d968d8baec27d4110cdd83e9436e083ca610611e

--- depends/packages/gnutls.mk
+++ depends/packages/gnutls.mk
@@ -1,13 +1,28 @@
 package=gnutls
-$(package)_version=3.6.15
-$(package)_dependencies=nettle
-$(package)_download_path=https://www.gnupg.org/ftp/gcrypt/gnutls/v3.6/
+$(package)_version=3.5.8
+$(package)_download_path=https://www.gnupg.org/ftp/gcrypt/gnutls/v3.5/
 $(package)_file_name=$(package)-$($(package)_version).tar.xz
-$(package)_sha256_hash=0ea8c3283de8d8335d7ae338ef27c53a916f15f382753b174c18b45ffd481558
-
+$(package)_sha256_hash=0e97f243ae72b70307d684b84c7fe679385aa7a7a0e37e5be810193dcc17d4ff
 
 # default settings
-$(package)_config_opts=--disable-shared --enable-static --with-included-libtasn1 --with-included-unistring --disable-non-suiteb-curves --disable-doc --disable-tools --without-p11-kit --disable-tests
+$(package)_config_env_default=NETTLE_CFLAGS="-static" GMP_CFLAGS="-static" PKG_CONFIG_LIBDIR="$(PREFIX_DIR)/lib/pkgconfig" CFLAGS="-I$(PREFIX_DIR)include -L$(PREFIX_DIR)lib -static"
+$(package)_config_opts_default=--with-included-libtasn1 --with-included-unistring --enable-local-libopts --disable-non-suiteb-curves --disable-doc --without-p11-kit
+
+# darwin specific settings
+$(package)_config_env_darwin=PKG_CONFIG_LIBDIR="$(PREFIX_DIR)/lib/pkgconfig"
+$(package)_config_env_x86_64-apple-darwin11=$($(package)_config_env_darwin)
+
+# 32 bit linux settings
+$(package)_config_env_i686-pc-linux-gnu=NETTLE_CFLAGS="-static" GMP_CFLAGS="-static" PKG_CONFIG_LIBDIR="$(PREFIX_DIR)/lib/pkgconfig" CFLAGS="-I$(PREFIX_DIR)include -L$(PREFIX_DIR)lib -static -m32" CXXFLAGS="-m32" LDFLAGS="-m32"
+
+# mingw dll specific settings
+ifeq ($(BUILD_DLL), 1)
+$(package)_config_env_x86_64-w64-mingw32=PKG_CONFIG_LIBDIR="$(PREFIX_DIR)/lib/pkgconfig" CFLAGS="-I$(PREFIX_DIR)include -L$(PREFIX_DIR)lib -D_WIN32_WINNT=0x0600 -DNCRYPT_PAD_PKCS1_FLAG=2 -DNCRYPT_SHA1_ALGORITHM=BCRYPT_SHA1_ALGORITHM -DNCRYPT_SHA256_ALGORITHM=BCRYPT_SHA256_ALGORITHM -DCERT_NCRYPT_KEY_HANDLE_TRANSFER_PROP_ID=99"
+endif
+
+# set settings based on host
+$(package)_config_env = $(if $($(package)_config_env_$(HOST)), $($(package)_config_env_$(HOST)), $($(package)_config_env_default))
+$(package)_config_opts = $(if $($(package)_config_opts_$(HOST)), $($(package)_config_opts_$(HOST)), $($(package)_config_opts_default))
 $(package)_config_opts_linux=--with-pic
 
 define $(package)_config_cmds
