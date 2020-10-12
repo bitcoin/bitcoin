b5ec5a5f9a945e3e84d24eafaa30d18a06fd7de4
sidhujag <jsidhu@blockchainfoundry.co>
2020-10-11 23:27:51 -0700

    updatte dep
    Former-commit-id: 89184eb8d6118136b8124e62bcc6bed3e7c77c82

--- depends/packages/nettle.mk
+++ depends/packages/nettle.mk
@@ -1,5 +1,6 @@
 package=nettle
 $(package)_version=3.3
+$(package)_dependencies=gmp
 $(package)_download_path=https://ftp.gnu.org/gnu/nettle
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=46942627d5d0ca11720fec18d81fc38f7ef837ea4197c1f630e71ce0d470b11e
