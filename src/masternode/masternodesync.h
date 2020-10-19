0dfe0c64d641139dfe07c07a8a4ef5e30d0e8cc5
sidhujag <jsidhu@blockchainfoundry.co>
2020-10-19 16:30:41 -0700

    remove redundant locks
    Former-commit-id: 62518b294e2ceddb02eaac6d06a877639838fee9

--- src/masternode/masternodesync.h
+++ src/masternode/masternodesync.h
@@ -65,7 +65,7 @@ public:
     void ProcessMessage(CNode* pfrom, const std::string& strCommand, CDataStream& vRecv) const;
     void ProcessTick(CConnman& connman, const PeerManager& peerman);
 
-    void AcceptedBlockHeader(const CBlockIndex *pindexNew) EXCLUSIVE_LOCKS_REQUIRED(::cs_main);
+    void AcceptedBlockHeader(const CBlockIndex *pindexNew);
     void NotifyHeaderTip(const CBlockIndex *pindexNew, bool fInitialDownload, CConnman& connman);
     void UpdatedBlockTip(const CBlockIndex *pindexNew, bool fInitialDownload, CConnman& connman) EXCLUSIVE_LOCKS_REQUIRED(::cs_main);
 
