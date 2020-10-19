bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

Split CConnman::GetNodeCount() in two functions
This change prevents repetitive traversing vNodes.

src/interfaces/node.h
@@ -6,7 +6,7 @@
 #define BITCOIN_INTERFACES_NODE_H
 
 #include <amount.h>     // For CAmount
-#include <net.h>        // For CConnman::NumConnections
+#include <net.h>        // For ConnectionCounts
 #include <net_types.h>  // For banmap_t
 #include <netaddress.h> // For Network
 #include <support/allocators/secure.h> // For SecureString
@@ -87,8 +87,11 @@ public:
     //! Get proxy.
     virtual bool getProxy(Network net, proxyType& proxy_info) = 0;
 
-    //! Get number of connections.
-    virtual size_t getNodeCount(CConnman::NumConnections flags) = 0;
+    //! Get number of peers.
+    virtual size_t peerCount() = 0;
+
+    //! Get numbers of connections of different types.
+    virtual ConnectionCounts connectionCounts() = 0;
 
     //! Get stats for connected nodes.
     using NodesStats = std::vector<std::tuple<CNodeStats, bool, CNodeStateStats>>;

src/qt/rpcconsole.cpp
@@ -841,9 +841,10 @@ void RPCConsole::message(int category, const QString &message, bool html)
 
 void RPCConsole::updateNetworkState()
 {
-    QString connections = QString::number(clientModel->getNumConnections()) + " (";
-    connections += tr("In:") + " " + QString::number(clientModel->getNumConnections(CONNECTIONS_IN)) + " / ";
-    connections += tr("Out:") + " " + QString::number(clientModel->getNumConnections(CONNECTIONS_OUT)) + ")";
+    const ConnectionCounts conn_counts = m_node.connectionCounts();
+    QString connections = QString::number(conn_counts.all) + " (";
+    connections += tr("In:") + " " + QString::number(conn_counts.in) + " / ";
+    connections += tr("Out:") + " " + QString::number(conn_counts.out) + ")";
 
     if(!clientModel->node().getNetworkActive()) {
         connections += " (" + tr("Network activity disabled") + ")";

src/rpc/net.cpp
@@ -47,7 +47,7 @@ static UniValue getconnectioncount(const JSONRPCRequest& request)
     if(!node.connman)
         throw JSONRPCError(RPC_CLIENT_P2P_DISABLED, "Error: Peer-to-peer functionality missing or disabled");
 
-    return (int)node.connman->GetNodeCount(CConnman::CONNECTIONS_ALL);
+    return (int)node.connman->PeerCount();
 }
 
 static UniValue ping(const JSONRPCRequest& request)
@@ -540,9 +540,10 @@ static UniValue getnetworkinfo(const JSONRPCRequest& request)
     obj.pushKV("timeoffset",    GetTimeOffset());
     if (node.connman) {
         obj.pushKV("networkactive", node.connman->GetNetworkActive());
-        obj.pushKV("connections", (int)node.connman->GetNodeCount(CConnman::CONNECTIONS_ALL));
-        obj.pushKV("connections_in", (int)node.connman->GetNodeCount(CConnman::CONNECTIONS_IN));
-        obj.pushKV("connections_out", (int)node.connman->GetNodeCount(CConnman::CONNECTIONS_OUT));
+        const ConnectionCounts conn_counts = node.connman->ConnectionCounts();
+        obj.pushKV("connections", conn_counts.all);
+        obj.pushKV("connections_in", conn_counts.in);
+        obj.pushKV("connections_out", conn_counts.out);
     }
     obj.pushKV("networks",      GetNetworksInfo());
     obj.pushKV("relayfee",      ValueFromAmount(::minRelayTxFee.GetFeePerK()));

bitcoin/bitcoin

src/interfaces/node.h
@@ -6,7 +6,7 @@
 #define BITCOIN_INTERFACES_NODE_H
 
 #include <amount.h>     // For CAmount
-#include <net.h>        // For CConnman::NumConnections
+#include <net.h>        // For ConnCounts
 #include <net_types.h>  // For banmap_t
 #include <netaddress.h> // For Network
 #include <support/allocators/secure.h> // For SecureString
@@ -87,8 +87,11 @@ public:
     //! Get proxy.
     virtual bool getProxy(Network net, proxyType& proxy_info) = 0;
 
-    //! Get number of connections.
-    virtual size_t getNodeCount(CConnman::NumConnections flags) = 0;
+    //! Get number of peers.
+    virtual size_t peerCount() = 0;
+
+    //! Get numbers of connections of different types.
+    virtual ConnCounts connectionCounts() = 0;
 
     //! Get stats for connected nodes.
     using NodesStats = std::vector<std::tuple<CNodeStats, bool, CNodeStateStats>>;

src/qt/rpcconsole.cpp
@@ -844,9 +844,10 @@ void RPCConsole::message(int category, const QString &message, bool html)
 
 void RPCConsole::updateNetworkState()
 {
-    QString connections = QString::number(clientModel->getNumConnections()) + " (";
-    connections += tr("In:") + " " + QString::number(clientModel->getNumConnections(CONNECTIONS_IN)) + " / ";
-    connections += tr("Out:") + " " + QString::number(clientModel->getNumConnections(CONNECTIONS_OUT)) + ")";
+    const ConnCounts conn_counts = m_node.connectionCounts();
+    QString connections = QString::number(conn_counts.all) + " (";
+    connections += tr("In:") + " " + QString::number(conn_counts.in) + " / ";
+    connections += tr("Out:") + " " + QString::number(conn_counts.out) + ")";
 
     if(!clientModel->node().getNetworkActive()) {
         connections += " (" + tr("Network activity disabled") + ")";

src/rpc/net.cpp
@@ -56,7 +56,7 @@ static RPCHelpMan getconnectioncount()
     if(!node.connman)
         throw JSONRPCError(RPC_CLIENT_P2P_DISABLED, "Error: Peer-to-peer functionality missing or disabled");
 
-    return (int)node.connman->GetNodeCount(CConnman::CONNECTIONS_ALL);
+    return (int)node.connman->PeerCount();
 },
     };
 }
@@ -583,9 +583,10 @@ static RPCHelpMan getnetworkinfo()
     obj.pushKV("timeoffset",    GetTimeOffset());
     if (node.connman) {
         obj.pushKV("networkactive", node.connman->GetNetworkActive());
-        obj.pushKV("connections", (int)node.connman->GetNodeCount(CConnman::CONNECTIONS_ALL));
-        obj.pushKV("connections_in", (int)node.connman->GetNodeCount(CConnman::CONNECTIONS_IN));
-        obj.pushKV("connections_out", (int)node.connman->GetNodeCount(CConnman::CONNECTIONS_OUT));
+        const ConnCounts conn_counts = node.connman->GetConnectionCounts();
+        obj.pushKV("connections", conn_counts.all);
+        obj.pushKV("connections_in", conn_counts.in);
+        obj.pushKV("connections_out", conn_counts.out);
     }
     obj.pushKV("networks",      GetNetworksInfo());
     obj.pushKV("relayfee",      ValueFromAmount(::minRelayTxFee.GetFeePerK()));