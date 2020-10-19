f28ba7b33d004540c97edc08df4602b336b929ef
sidhujag <jsidhu@blockchainfoundry.co>
2020-10-19 08:02:52 -0700

    bump scheduler before mn probe
    Former-commit-id: 360484b8d492da612ba868c779e63602d0a229a2

--- test/functional/feature_llmqconnections.py
+++ test/functional/feature_llmqconnections.py
@@ -47,16 +47,19 @@ class LLMQConnections(DashTestFramework):
         q = self.mine_quorum(expected_connections=4)
 
         self.log.info("checking that all MNs got probed")
+        self.bump_scheduler(5)
         for mn in self.get_quorum_masternodes(q):
             self.wait_until(lambda: self.get_mn_probe_count(mn.node, q, False) == 4)
 
         self.log.info("checking that probes age")
         self.bump_mocktime(60)
+        self.bump_scheduler(5)
         for mn in self.get_quorum_masternodes(q):
             self.wait_until(lambda: self.get_mn_probe_count(mn.node, q, False) == 0)
 
         self.log.info("mine a new quorum and re-check probes")
         q = self.mine_quorum(expected_connections=4)
+        self.bump_scheduler(5)
         for mn in self.get_quorum_masternodes(q):
             self.wait_until(lambda: self.get_mn_probe_count(mn.node, q, True) == 4)
 
