9771b099e3aa65cd83c7399e999be9422ea66fd2
Bohdan Vanieiev <warchantua@gmail.com>
2020-10-26 19:42:39 +0200

    Fix

--- src/bootstraps.cpp
+++ src/bootstraps.cpp
@@ -121,13 +121,13 @@ void selectPopConfig(const ArgsManager& args)
 {
     std::string btcnet = args.GetArg("-popbtcnetwork", "regtest");
     std::string vbknet = args.GetArg("-popvbknetwork", "regtest");
-    bool popautoconfig = args.GetBoolArg("-popautoconfig", true);
+    int popautoconfig = args.GetArg("-popautoconfig", 1);
     int btcstart = args.GetArg("-popbtcstartheight", 0);
     std::string btcblocks = args.GetArg("-popbtcblocks", "");
     int vbkstart = args.GetArg("-popvbkstartheight", 0);
     std::string vbkblocks = args.GetArg("-popvbkblocks", "");
 
-    selectPopConfig(btcnet, vbknet, popautoconfig, btcstart, btcblocks, vbkstart, vbkblocks);
+    selectPopConfig(btcnet, vbknet, (bool)popautoconfig, btcstart, btcblocks, vbkstart, vbkblocks);
 }
 
 
