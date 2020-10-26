f9428d0dee29a8f1c162a35c8f3716ee6db626b0
sidhujag <jsidhu@blockchainfoundry.co>
2020-10-26 09:34:05 -0700

    Revert "rm memcpy alias which was <= 2.14 glibc and now we only support >= 2.17"
    This reverts commit 2f2ed354772ff33c74003357b1910a74e4a55e0b [formerly b63ffac61d82dd9ae0e8311318f1a9b49800368a].
    
    
    Former-commit-id: 32d0448e5ec02df56a75df484e928f21cf3683d6

--- src/compat/glibc_compat.cpp
+++ src/compat/glibc_compat.cpp
@@ -27,6 +27,13 @@
 #include <cstring>
 #define likely(x)      __builtin_expect(!!(x), 1)
 #define unlikely(x)    __builtin_expect(!!(x), 0)
+// Prior to GLIBC_2.14, memcpy was aliased to memmove.
+extern "C" void* memmove(void* a, const void* b, size_t c);
+extern "C" void* memcpy(void* a, const void* b, size_t c)
+{
+    return memmove(a, b, c);
+}
+
 #if defined(__i386__) || defined(__arm__)
 
 extern "C" int64_t __udivmoddi4(uint64_t u, uint64_t v, uint64_t* rp);
