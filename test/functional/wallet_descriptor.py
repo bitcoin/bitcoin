586640381a2c379ce3d6366b1b4534ccc4e8ccf2
Andrew Chow <achow101-github@achow101.com>
2020-10-29 12:28:07 -0400

    Skip --descriptor tests if sqlite is not compiled

--- test/functional/wallet_descriptor.py
+++ test/functional/wallet_descriptor.py
@@ -19,6 +19,7 @@ class WalletDescriptorTest(BitcoinTestFramework):
 
     def skip_test_if_missing_module(self):
         self.skip_if_no_wallet()
+        self.skip_if_no_sqlite()
 
     def run_test(self):
         wallet_info = self.nodes[0].getwalletinfo()
