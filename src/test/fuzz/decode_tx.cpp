d7901ab8d2fdd2f6e68c4fa48078111bf5f0fa73
practicalswift <practicalswift@users.noreply.github.com>
2020-11-04 23:06:25 +0000

    fuzz: Assert expected DecodeHexTx behaviour when using legacy decoding

--- src/test/fuzz/decode_tx.cpp
+++ src/test/fuzz/decode_tx.cpp
@@ -19,12 +19,14 @@ void test_one_input(const std::vector<uint8_t>& buffer)
     const bool result_none = DecodeHexTx(mtx, tx_hex, false, false);
     const bool result_try_witness = DecodeHexTx(mtx, tx_hex, false, true);
     const bool result_try_witness_and_maybe_no_witness = DecodeHexTx(mtx, tx_hex, true, true);
-    const bool result_try_no_witness = DecodeHexTx(mtx, tx_hex, true, false);
+    CMutableTransaction no_witness_mtx;
+    const bool result_try_no_witness = DecodeHexTx(no_witness_mtx, tx_hex, true, false);
     assert(!result_none);
     if (result_try_witness_and_maybe_no_witness) {
         assert(result_try_no_witness || result_try_witness);
     }
     if (result_try_no_witness) {
+        assert(!no_witness_mtx.HasWitness());
         assert(result_try_witness_and_maybe_no_witness);
     }
 }
