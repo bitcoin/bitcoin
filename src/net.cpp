35e231f849a6ac4a385d9430631c598d0f6274e4
MarcoFalke <falke.marco@gmail.com>
2020-11-05 13:00:10 +0100

    Merge #20302: net: Make it easier to reason about node eviction by removing unused NodeEvictionCandidate::addr (CAddress)
    f1f433e8ca014f69925e8d5487877e06ca784ec8 Make it easier to reason about node eviction by removing unused NodeEvictionCandidate::addr (CAddress) (practicalswift)
    
    Pull request description:
    
      Make it easier to reason about node eviction by removing unused `NodeEvictionCandidate::addr` (`CAddress`).
    
    ACKs for top commit:
      jnewbery:
        utACK f1f433e8ca014f69925e8d5487877e06ca784ec8
    
    Tree-SHA512: fef91d7b412b8a4f172370cff6c37eb8c3db0ba618f5daf2dcc8737c8fcef7b9b820d7ee99cd0a9eae7dd653a096cf83d5113776b0d1d9a324147581674e9ede
    
    Former-commit-id: 26d5b65bc839e709ca24c77ff8a7d6d8bdfaafb3

--- src/net.cpp
+++ src/net.cpp
@@ -878,7 +878,6 @@ struct NodeEvictionCandidate
     bool fRelevantServices;
     bool fRelayTxes;
     bool fBloomFilter;
-    CAddress addr;
     uint64_t nKeyedNetGroup;
     bool prefer_evict;
     bool m_is_local;
@@ -989,7 +988,7 @@ bool CConnman::AttemptToEvictConnection()
             NodeEvictionCandidate candidate = {node->GetId(), node->nTimeConnected, node->nMinPingUsecTime,
                                                node->nLastBlockTime, node->nLastTXTime,
                                                HasAllDesirableServiceFlags(node->nServices),
-                                               peer_relay_txes, peer_filter_not_null, node->addr, node->nKeyedNetGroup,
+                                               peer_relay_txes, peer_filter_not_null, node->nKeyedNetGroup,
                                                node->m_prefer_evict, node->addr.IsLocal()};
             vEvictionCandidates.push_back(candidate);
         }
