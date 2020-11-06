cc0ef1d226f00d0575f5e308ec96fc8348281c14
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2020-11-06 08:05:50 -0800

    Update feerate.cpp
    Former-commit-id: 7ee736954aa9c3496663ee352e23c0c6f30560ef

--- src/policy/feerate.cpp
+++ src/policy/feerate.cpp
@@ -13,7 +13,7 @@ CFeeRate::CFeeRate(const CAmount& nFeePaid, size_t nBytes_)
     int64_t nSize = int64_t(nBytes_);
 
     if (nSize > 0)
-        nSatoshisPerK = nFeePaid * 1000 / nSize;
+        nSatoshisPerK = nFeePaid * (1000 / nSize);
     else
         nSatoshisPerK = 0;
 }
@@ -23,7 +23,7 @@ CAmount CFeeRate::GetFee(size_t nBytes_) const
     assert(nBytes_ <= uint64_t(std::numeric_limits<int64_t>::max()));
     int64_t nSize = int64_t(nBytes_);
 
-    CAmount nFee = nSatoshisPerK * nSize / 1000;
+    CAmount nFee = nSatoshisPerK * (nSize / 1000);
 
     if (nFee == 0 && nSize != 0) {
         if (nSatoshisPerK > 0)
