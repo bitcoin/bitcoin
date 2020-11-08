14faa098540d87d641675115a280267a780153c6
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2020-11-08 14:33:37 -0800

    Update util.py
    Former-commit-id: 0e3f7a32336a08f2097d578a22e73017070462ec

--- test/functional/test_framework/util.py
+++ test/functional/test_framework/util.py
@@ -399,7 +399,7 @@ def copy_datadir(from_node, to_node, dirname):
     from_datadir = os.path.join(dirname, "node"+str(from_node), "regtest")
     to_datadir = os.path.join(dirname, "node"+str(to_node), "regtest")
 
-    dirs = ["blocks", "chainstate", "evodb", "llmq", "ethereumminttx", "ethereumtxroots", "assets", "blockindex"]
+    dirs = ["blocks", "chainstate", "evodb", "llmq", "ethereumminttx", "ethereumtxroots", "assets", "dbblockindex"]
     for d in dirs:
         try:
             src = os.path.join(from_datadir, d)
