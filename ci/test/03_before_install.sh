dde5f4a1a57a6fcbbadb47e35b2b409eadac0a2f
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-11-09 22:12:26 +0200

    ci: Use travis_fold on Travis CI only

--- ci/test/03_before_install.sh
+++ ci/test/03_before_install.sh
@@ -6,15 +6,20 @@
 
 export LC_ALL=C.UTF-8
 
+FOLD_COMMAND=""
+if [[ ${TRAVIS} == "true" ]]; then
+  FOLD_COMMAND="travis_fold:"
+fi
+
 BEGIN_FOLD () {
   echo ""
   CURRENT_FOLD_NAME=$1
-  echo "travis_fold:start:${CURRENT_FOLD_NAME}"
+  echo "${FOLD_COMMAND}start:${CURRENT_FOLD_NAME}"
 }
 
 END_FOLD () {
   RET=$?
-  echo "travis_fold:end:${CURRENT_FOLD_NAME}"
+  echo "${FOLD_COMMAND}end:${CURRENT_FOLD_NAME}"
   if [ $RET != 0 ]; then
     echo "${CURRENT_FOLD_NAME} failed with status code ${RET}"
   fi
