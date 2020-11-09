637d27872095299acb1f5f80cbd5d93e850e2bb8
SatosiBitcoin <office@satoshibitcoin.app>
2020-11-09 14:51:44 +0000

    latest update Mon 09 Nov 2020 02:51:44 PM UTC

--- src/bench/nanobench.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2019-2020 The Bitcoin Core developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
-
-#define ANKERL_NANOBENCH_IMPLEMENT
-#include <bench/nanobench.h>
--- src/univalue/test/no_nul.cpp
+++
@@ -1,8 +0,0 @@
-#include "univalue.h"
-
-int main (int argc, char *argv[])
-{
-    char buf[] = "___[1,2,3]___";
-    UniValue val;
-    return val.read(buf + 3, 7) ? 0 : 1;
-}
