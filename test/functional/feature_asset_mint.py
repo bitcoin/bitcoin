d25f5b5b03b7079cb100ab8037f5fe58f2a822a2
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2020-11-17 06:54:50 -0800

    Update feature_asset_mint.py

--- test/functional/feature_asset_mint.py
+++ test/functional/feature_asset_mint.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2019-2020 The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
-
+import time
 from test_framework.test_framework import SyscoinTestFramework
 from test_framework.util import assert_equal, assert_raises_rpc_error, set_node_times
 
