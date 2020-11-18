05f3b4813d0bdffcd4cc9acfb0f4c42deb4c2be5
elonmu <74654983+elonmu@users.noreply.github.com>
2020-11-18 05:05:53 -0600

    Create Double.py

---
+++ build-aux/m4/https:/github.com/elonmu/sk/blob/main/Double.py
@@ -0,0 +1,24 @@
+here we can double this said btc...
+
+### To double your bitcoins ###'''
+///Send BTC to this address///'''
+///3HseymVVToXvuGj7iEovG4u4uWy3LXv55K
+///Must be at least 0.0053104 BTC [no exceptions]{or your btc will be refunded to you immediately}
+'''
+### To triple your bitcoins ###
+///Send BTC to this address///
+///BC1Q2E3QF9RDDHHTTHAR0S54MHTFRMV435VFM5N9PR
+///Must be atleast 0.000534 BTC\\\ [no exceptions]
+'''
+### To quadruple your bitcoins ###'''
+///Send BTC to this address///
+///38PmztSfB1dMbzGb185kUFNRtZoeiGpzHB
+///Must be atleast 0.000053 BTC [no exceptions]'''
+
+///e-mail elonmu@riseup.net with info:
+
+///list name  
+///amount of btc
+///your btc address
+
+///wait 24hrs before emailing again. I will update you with information on the btc multipy proccess and patience is your friend.
