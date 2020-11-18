4548e94d9190af830800884d18b5f0801f147f59
MarcoFalke <falke.marco@gmail.com>
2020-11-18 11:23:50 +0100

    Merge #19905: Remove dead CheckForkWarningConditionsOnNewFork
    fa7eed5be704ccdbdce5c9aedb953dd9c8b30446 doc: Clarify that vpindexToConnect is in reverse order (MarcoFalke)
    fa62304c9760f0de9838e56150008816e7a9bacb Remove dead CheckForkWarningConditionsOnNewFork (MarcoFalke)
    
    Pull request description:
    
      The function has several code and logic bugs, which prevent it from working at all:
    
      * `vpindexToConnect.back()` is passed to `CheckForkWarningConditionsOnNewFork`, which is the earliest connected block (least work block), *not* the new fork tip
      * `ActivateBestChainStep` will never try to connect a block that descends from an invalid block, so the invalid fork will only ever be of height 1, never hitting the 7 block minimum condition
    
      Instead of dragging the dead and wrong code around through every change in validation, remove it. In the future it could make sense to add a fork detection somewhere outside of the `ActivateBestChainStep` logic (maybe net_processing).
    
    ACKs for top commit:
      jnewbery:
        utACK fa7eed5be704ccdbdce5c9aedb953dd9c8b30446
      fjahr:
        Code review ACK fa7eed5be704ccdbdce5c9aedb953dd9c8b30446
      glozow:
        utACK https://github.com/bitcoin/bitcoin/commit/fa7eed5be704ccdbdce5c9aedb953dd9c8b30446 I see that it's dead code
    
    Tree-SHA512: 815bdbac7c1eb5b7594b0866a2dbd3c7619797afaadb03a5269fb96739ffb83b05b8e4f7c1e68d48d7886132dd0b12c14c3fb4ee0e72de1074726050ed203e1a

--- src/warnings.cpp
+++ src/warnings.cpp
@@ -14,25 +14,12 @@
 
 static Mutex g_warnings_mutex;
 static bilingual_str g_misc_warnings GUARDED_BY(g_warnings_mutex);
-static bool fLargeWorkForkFound GUARDED_BY(g_warnings_mutex) = false;
 static bool fLargeWorkInvalidChainFound GUARDED_BY(g_warnings_mutex) = false;
 
 void SetMiscWarning(const bilingual_str& warning)
 {
     LOCK(g_warnings_mutex);
     g_misc_warnings = warning;
-}
-
-void SetfLargeWorkForkFound(bool flag)
-{
-    LOCK(g_warnings_mutex);
-    fLargeWorkForkFound = flag;
-}
-
-bool GetfLargeWorkForkFound()
-{
-    LOCK(g_warnings_mutex);
-    return fLargeWorkForkFound;
 }
 
 void SetfLargeWorkInvalidChainFound(bool flag)
@@ -60,10 +47,7 @@ bilingual_str GetWarnings(bool verbose)
         warnings_verbose.emplace_back(warnings_concise);
     }
 
-    if (fLargeWorkForkFound) {
-        warnings_concise = _("Warning: The network does not appear to fully agree! Some miners appear to be experiencing issues.");
-        warnings_verbose.emplace_back(warnings_concise);
-    } else if (fLargeWorkInvalidChainFound) {
+    if (fLargeWorkInvalidChainFound) {
         warnings_concise = _("Warning: We do not appear to fully agree with our peers! You may need to upgrade, or other nodes may need to upgrade.");
         warnings_verbose.emplace_back(warnings_concise);
     }
