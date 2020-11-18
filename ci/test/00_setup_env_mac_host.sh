8239ab8bc347717e16338493186b281ee5fdf4bb
fanquake <fanquake@gmail.com>
2020-11-18 21:38:21 +0800

    build: set minimum supported macOS to 10.14

--- ci/test/00_setup_env_mac_host.sh
+++ ci/test/00_setup_env_mac_host.sh
@@ -6,7 +6,7 @@
 
 export LC_ALL=C.UTF-8
 
-export HOST=x86_64-apple-darwin16
+export HOST=x86_64-apple-darwin18
 export PIP_PACKAGES="zmq"
 export GOAL="install"
 export BITCOIN_CONFIG="--with-gui --enable-reduce-exports --enable-werror --with-boost-process"
