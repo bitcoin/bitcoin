529bfc16ffb35c5356e27f61d59395fae6707bcd
Jon Atack <jon@atack.com>
2020-11-27 17:36:22 +0100

    test: add setfeerate functional coverage in wallet_create_tx.py

--- test/functional/wallet_create_tx.py
+++ test/functional/wallet_create_tx.py
@@ -3,13 +3,13 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+from decimal import Decimal
+
+from test_framework.blocktools import TIME_GENESIS_BLOCK
 from test_framework.test_framework import BitcoinTestFramework
 from test_framework.util import (
     assert_equal,
     assert_raises_rpc_error,
-)
-from test_framework.blocktools import (
-    TIME_GENESIS_BLOCK,
 )
 
 
@@ -17,6 +17,7 @@ class CreateTxWalletTest(BitcoinTestFramework):
     def set_test_params(self):
         self.setup_clean_chain = True
         self.num_nodes = 1
+        self.wallet_names = ["test_wallet"]
 
     def skip_test_if_missing_module(self):
         self.skip_if_no_wallet()
@@ -29,6 +30,7 @@ class CreateTxWalletTest(BitcoinTestFramework):
 
         self.test_anti_fee_sniping()
         self.test_tx_size_too_large()
+        self.test_setfeerate()
 
     def test_anti_fee_sniping(self):
         self.log.info('Check that we have some (old) blocks and that anti-fee-sniping is disabled')
@@ -76,6 +78,51 @@ class CreateTxWalletTest(BitcoinTestFramework):
             lambda: self.nodes[0].fundrawtransaction(hexstring=raw_tx),
         )
         self.nodes[0].settxfee(0)
+
+    def test_setfeerate(self):
+        self.log.info("Test setfeerate")
+        self.restart_node(0, extra_args=["-mintxfee=0.00003141"])  # 3.141 sat/vB
+        node = self.nodes[0]
+
+        def test_response(*, requested=0, expected=0, error=None, msg):
+            assert_equal(node.setfeerate(requested), {"wallet_name": "test_wallet", "fee_rate": expected, ("error" if error else "result"): msg})
+
+        # Test setfeerate with 10.0001 (CFeeRate rounding), "10.001" and "4" sat/vB
+        test_response(requested=10.0001, expected=10, msg="Fee rate for transactions with this wallet successfully set to 10.000 sat/vB")
+        assert_equal(node.getwalletinfo()["paytxfee"], Decimal("0.00010000"))
+        test_response(requested="10.001", expected=Decimal("10.001"), msg="Fee rate for transactions with this wallet successfully set to 10.001 sat/vB")
+        assert_equal(node.getwalletinfo()["paytxfee"], Decimal("0.00010001"))
+        test_response(requested="4", expected=4, msg="Fee rate for transactions with this wallet successfully set to 4.000 sat/vB")
+        assert_equal(node.getwalletinfo()["paytxfee"], Decimal("0.00004000"))
+
+        # Test setfeerate with too-high/low values returns expected errors
+        test_response(requested=Decimal("10000.001"), expected=4, error=True, msg="The requested fee rate of 10000.001 sat/vB cannot be greater than the wallet max fee rate of 10000.000 sat/vB. The current setting of 4.000 sat/vB for this wallet remains unchanged.")
+        test_response(requested=Decimal("0.999"), expected=4, error=True, msg="The requested fee rate of 0.999 sat/vB cannot be less than the minimum relay fee rate of 1.000 sat/vB. The current setting of 4.000 sat/vB for this wallet remains unchanged.")
+        test_response(requested=Decimal("3.140"), expected=4, error=True, msg="The requested fee rate of 3.140 sat/vB cannot be less than the wallet min fee rate of 3.141 sat/vB. The current setting of 4.000 sat/vB for this wallet remains unchanged.")
+        assert_equal(node.getwalletinfo()["paytxfee"], Decimal("0.00004000"))
+
+        # Test setfeerate to 3.141 sat/vB
+        test_response(requested=3.141, expected=Decimal("3.141"), msg="Fee rate for transactions with this wallet successfully set to 3.141 sat/vB")
+        assert_equal(node.getwalletinfo()["paytxfee"], Decimal("0.00003141"))
+
+        # Test setfeerate with values non-representable by CFeeRate
+        for invalid_value in [0.00000001, 0.0009, 0.00099999]:
+            assert_raises_rpc_error(-3, "Invalid amount", node.setfeerate, amount=invalid_value)
+
+        # Test setfeerate with values rejected by ParseFixedPoint() called in AmountFromValue()
+        for invalid_value in ["", 0.000000001, "1.111111111", 11111111111]:
+            assert_raises_rpc_error(-3, "Invalid amount", node.setfeerate, amount=invalid_value)
+
+        # Test deactivating setfeerate
+        test_response(msg="Fee rate for transactions with this wallet successfully unset. By default, automatic fee selection will be used.")
+        assert_equal(node.getwalletinfo()["paytxfee"], 0)
+
+        # Test currently-unset setfeerate with too-high/low values returns expected errors
+        test_response(requested=Decimal("10000.001"), error=True, msg="The requested fee rate of 10000.001 sat/vB cannot be greater than the wallet max fee rate of 10000.000 sat/vB. The current setting of 0 (unset) for this wallet remains unchanged.")
+        assert_equal(node.getwalletinfo()["paytxfee"], 0)
+        test_response(requested=Decimal("0.999"), error=True, msg="The requested fee rate of 0.999 sat/vB cannot be less than the minimum relay fee rate of 1.000 sat/vB. The current setting of 0 (unset) for this wallet remains unchanged.")
+        test_response(requested=Decimal("3.140"), error=True, msg="The requested fee rate of 3.140 sat/vB cannot be less than the wallet min fee rate of 3.141 sat/vB. The current setting of 0 (unset) for this wallet remains unchanged.")
+        assert_equal(node.getwalletinfo()["paytxfee"], 0)
 
 
 if __name__ == '__main__':
