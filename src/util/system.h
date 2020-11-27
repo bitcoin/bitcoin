ce9dd45422e1f4ecce6df68da086b8bfc2100756
practicalswift <practicalswift@users.noreply.github.com>
2020-11-27 12:41:26 +0000

    Add [[nodiscard]] to RenameOver(...)

--- src/util/system.h
+++ src/util/system.h
@@ -60,7 +60,7 @@ bool FileCommit(FILE *file);
 bool TruncateFile(FILE *file, unsigned int length);
 int RaiseFileDescriptorLimit(int nMinFD);
 void AllocateFileRange(FILE *file, unsigned int offset, unsigned int length);
-bool RenameOver(fs::path src, fs::path dest);
+[[nodiscard]] bool RenameOver(fs::path src, fs::path dest);
 bool LockDirectory(const fs::path& directory, const std::string lockfile_name, bool probe_only=false);
 void UnlockDirectory(const fs::path& directory, const std::string& lockfile_name);
 bool DirIsWritable(const fs::path& directory);
