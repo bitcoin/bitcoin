6a455f1c8a0d95e8f68ac41bddb5d8891dd8c1f3
UdjinM6 <UdjinM6@users.noreply.github.com>
2020-11-28 22:16:31 +0300

    tests: Make sure txes are actually included in blocks we mine in feat…  …ure_llmq_is_retroactive.py

--- test/functional/feature_llmq_is_retroactive.py
+++ test/functional/feature_llmq_is_retroactive.py
@@ -86,6 +86,7 @@ class LLMQ_IS_RetroactiveSigning(DashTestFramework):
         # Make node0 consider the TX as safe
         self.bump_mocktime(10 * 60 + 1)
         block = self.nodes[0].generate(1)[0]
+        assert(txid in self.nodes[0].getblock(block, 1)['tx'])
         self.wait_for_chainlocked_block_all_nodes(block)
 
         self.log.info("testing retroactive signing with partially known TX and all nodes session timeout")
@@ -130,6 +131,7 @@ class LLMQ_IS_RetroactiveSigning(DashTestFramework):
         # Make node 0 consider the TX as safe
         self.bump_mocktime(10 * 60 + 1)
         block = self.nodes[0].generate(1)[0]
+        assert(txid in self.nodes[0].getblock(block, 1)['tx'])
         self.wait_for_chainlocked_block_all_nodes(block)
 
     def test_single_node_session_timeout(self, do_cycle_llmqs):
@@ -159,6 +161,7 @@ class LLMQ_IS_RetroactiveSigning(DashTestFramework):
         # Make node 0 consider the TX as safe
         self.bump_mocktime(10 * 60 + 1)
         block = self.nodes[0].generate(1)[0]
+        assert(txid in self.nodes[0].getblock(block, 1)['tx'])
         self.wait_for_chainlocked_block_all_nodes(block)
 
 if __name__ == '__main__':
