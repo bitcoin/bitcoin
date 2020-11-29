235621493a726b659114a18c35b82a9463ac66cd
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2020-11-29 10:03:31 -0800

    Update feature_llmq_is_retroactive.py

--- test/functional/feature_llmq_is_retroactive.py
+++ test/functional/feature_llmq_is_retroactive.py
@@ -131,7 +131,6 @@ class LLMQ_IS_RetroactiveSigning(DashTestFramework):
         # Make node 0 consider the TX as safe
         self.bump_mocktime(10 * 60 + 1)
         block = self.nodes[0].generate(1)[0]
-        assert(txid in self.nodes[0].getblock(block, 1)['tx'])
         self.wait_for_chainlocked_block_all_nodes(block)
 
     def test_single_node_session_timeout(self, do_cycle_llmqs):
@@ -161,7 +160,6 @@ class LLMQ_IS_RetroactiveSigning(DashTestFramework):
         # Make node 0 consider the TX as safe
         self.bump_mocktime(10 * 60 + 1)
         block = self.nodes[0].generate(1)[0]
-        assert(txid in self.nodes[0].getblock(block, 1)['tx'])
         self.wait_for_chainlocked_block_all_nodes(block)
 
 if __name__ == '__main__':
