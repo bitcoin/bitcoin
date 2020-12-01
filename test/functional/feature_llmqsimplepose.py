358449986594f7dc68f854034d025d08eceea456
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2020-12-01 15:08:48 -0800

    Update feature_llmqsimplepose.py

--- test/functional/feature_llmqsimplepose.py
+++ test/functional/feature_llmqsimplepose.py
@@ -6,7 +6,7 @@
 import time
 
 from test_framework.test_framework import DashTestFramework
-from test_framework.util import p2p_port, force_finish_mnsync, MAX_INITIAL_BROADCAST_DELAY
+from test_framework.util import p2p_port, force_finish_mnsync
 
 '''
 llmq-simplepose.py
