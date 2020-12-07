e381179b35657e2e1ac0a3df2ce8ccd1ff23fc9e
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2020-12-07 08:15:59 -0800

    allow max drift to 12m
    syscoin's blocks are 10x faster so absolute amount of blocks to work is 10x less, to account for it a miner cannot mine ahead as much as bitcoin as there would be less equivalent work. IE: 10 blocks of bitcoin is equivalent to work of 100 blocks in syscoin, thus reduce max drift by 10 times to account for this especially in light client verification schemes

--- src/chain.h
+++ src/chain.h
@@ -20,7 +20,8 @@
  * Maximum amount of time that a block timestamp is allowed to exceed the
  * current network-adjusted time before the block will be accepted.
  */
-static constexpr int64_t MAX_FUTURE_BLOCK_TIME = 2 * 60 * 60;
+// SYSCOIN is 10x faster in blocks so therefor has less threshold for block time descrepency
+static constexpr int64_t MAX_FUTURE_BLOCK_TIME = 2 * 6 * 60;
 
 /**
  * Timestamp window used as a grace period by code that compares external
