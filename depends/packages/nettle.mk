18c72857b64e5e5b62e8a4c82f0096452637b2e4
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2020-12-08 12:31:04 -0800

    stable gnutls/nettle

--- depends/packages/nettle.mk
+++ depends/packages/nettle.mk
@@ -1,9 +1,9 @@
 package=nettle
-$(package)_version=3.5
+$(package)_version=3.3
 $(package)_dependencies=gmp
 $(package)_download_path=https://ftp.gnu.org/gnu/nettle
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=432d98dcd341cbd063a963025524122368c1f2ee9c721bb68d771d5e34674b4f
+$(package)_sha256_hash=46942627d5d0ca11720fec18d81fc38f7ef837ea4197c1f630e71ce0d470b11e
 
 # default settings
 $(package)_config_opts=--libdir=$(host_prefix)/lib --disable-shared --enable-static --disable-documentation --disable-openssl
