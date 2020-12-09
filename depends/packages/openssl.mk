4da6db111885348ccd4c06199401e43ed8130641
Adrian Gallagher <thrasher@addictionsoftware.com>
2020-12-08 20:02:55 -0800

    Revert "build: skip building OpenSSL lib_ssl"
    This reverts commit 2cba35ab38b492768e85bd77442a566dda169466.

--- depends/packages/openssl.mk
+++ depends/packages/openssl.mk
@@ -76,7 +76,7 @@ define $(package)_config_cmds
 endef
 
 define $(package)_build_cmds
-  $(MAKE) -j1 build_crypto libcrypto.pc libssl.pc openssl.pc
+  $(MAKE) -j1 build_libs libcrypto.pc libssl.pc openssl.pc
 endef
 
 define $(package)_stage_cmds
