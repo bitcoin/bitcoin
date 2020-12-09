e765f343212e8fc80099579bfcaac4a9027b0592
UdjinM6 <UdjinM6@users.noreply.github.com>
2020-12-09 23:56:10 +0300

    rpc: Fix filtering in `masternode winners` (#3856)
    Was broken by https://github.com/dashpay/dash/commit/44706dc88acf2eab07695f5093bda31ab278d2bf#diff-244e87df3ed336aadc56ec7c8fc521954751f3c3f66b3eb3832bcbcc87996c51L807

--- src/rpc/masternode.cpp
+++ src/rpc/masternode.cpp
@@ -247,6 +247,7 @@ static RPCHelpMan masternode_winners()
     UniValue obj(UniValue::VOBJ);
     auto mapPayments = GetRequiredPaymentsStrings(nHeight - nLast, nHeight + 20);
     for (const auto &p : mapPayments) {
+        if (strFilter != "" && p.second.find(strFilter) == std::string::npos) continue;
         obj.pushKV(strprintf("%d", p.first), p.second);
     }
 
