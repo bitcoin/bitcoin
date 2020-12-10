a613346909576f7e72ddc07564d57623ad4f0bba
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2020-12-10 07:22:15 -0800

    remove todo
    not an issue on real test/main net

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -2786,13 +2786,12 @@ void PeerManager::ProcessMessage(CNode& pfrom, const std::string& msg_type, CDat
     }
 
     if (msg_type == NetMsgType::SENDADDRV2) {
-        // SYSCOIN TODO: find out why it's getting in here
-        // if (pfrom.fSuccessfullyConnected) {
+         if (pfrom.fSuccessfullyConnected) {
             // Disconnect peers that send SENDADDRV2 message after VERACK; this
             // must be negotiated between VERSION and VERACK.
-            // pfrom.fDisconnect = true;
-            //return;
-        //}
+            pfrom.fDisconnect = true;
+            return;
+        }
         pfrom.m_wants_addrv2 = true;
         return;
     }
