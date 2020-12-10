859a6a63bc60a3d4468b17a1642e4a472bcfef6a
muxator <antonio.muci@bancaditalia.it>
2020-12-10 22:09:31 +0100

    continue-mining-local.sh: script to resume the mining process

---
+++ infra/continue-mining-local.sh
@@ -0,0 +1,92 @@
+#!/usr/bin/env bash
+#
+# ItCoin
+#
+# This script is meant for debugging purposes. Assuming
+# initialize-itcoin-local.sh has been already executed once, restarts the
+# continuous mining process from where it left off. The current datadir is
+# reused: there is no need to delete it.
+#
+# REQUIREMENTS:
+# - jq
+# - the itcoin source must already be built with <basedir>/configure-itcoin.sh
+#   && make && make install
+# - initialize-itcoin-local.sh has been started and stopped already
+#
+# USAGE:
+#     continue-mining-local.sh
+#
+# Author: muxator <antonio.muci@bancaditalia.it>
+
+set -eu
+
+# https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself#246128
+MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# These should probably be taken from cmdline
+DATADIR="${MYDIR}/datadir"
+TARGET_DELAY=60
+WALLET_NAME=itcoin_signer
+
+errecho() {
+    # prints to stderr
+    >&2 echo $@;
+}
+
+cleanup() {
+    # stop the itcoin daemon
+    "${BITCOIN_CLI}" -datadir="${DATADIR}" stop
+}
+
+checkPrerequisites() {
+    if ! command -v jq &> /dev/null; then
+        errecho "Please install jq (https://stedolan.github.io/jq/)"
+        exit 1
+    fi
+}
+
+# Automatically stop the container (wich will also self-remove at script exit
+# or if an error occours
+trap cleanup EXIT
+
+# Do not run if the required packages are not installed
+checkPrerequisites
+
+PATH_TO_BINARIES=$(realpath "${MYDIR}/../target/bin")
+
+BITCOIND="${PATH_TO_BINARIES}/bitcoind"
+BITCOIN_CLI="${PATH_TO_BINARIES}/bitcoin-cli"
+BITCOIN_UTIL="${PATH_TO_BINARIES}/bitcoin-util"
+MINER=$(realpath "${MYDIR}/../contrib/signet/miner")
+
+# Start itcoin daemon
+"${BITCOIND}" -daemon -datadir="${DATADIR}"
+
+# wait until the daemon is fully started
+errecho "ItCoin daemon: waiting (at most 10 seconds) for warmup"
+timeout 10 "${BITCOIN_CLI}" -datadir="${DATADIR}" -rpcwait -rpcclienttimeout=3 uptime >/dev/null
+errecho "ItCoin daemon: warmed up"
+
+# Open the wallet WALLET_NAME
+errecho "Load wallet ${WALLET_NAME}"
+"${BITCOIN_CLI}" -datadir="${DATADIR}" loadwallet "${WALLET_NAME}" >/dev/null
+errecho "Wallet ${WALLET_NAME} loaded"
+
+# Retrieve the address of the first transaction in this blockchain
+errecho "Retrieve the address of the first transaction we find"
+ADDR=$("${BITCOIN_CLI}" -datadir="${DATADIR}" listtransactions | jq --raw-output '.[0].address')
+errecho "Address ${ADDR} retrieved"
+
+# Estimate the value for nbit parameter which is necessary to generate blocks at
+# 5 seconds pace on the hardware we are running now.
+# The NBITS variable will be an empty string if the regex does not match.
+#
+# On my laptop, nbits=1d1ae7b3 for a target of 60 seconds.
+errecho "Calibrating difficulty for target delay ${TARGET_DELAY} seconds"
+NBITS=$("${MINER}" calibrate --grind-cmd="${BITCOIN_UTIL} grind" --seconds "${TARGET_DELAY}" | sed --regexp-extended --quiet 's/nbits=(.*) for (.*)s average mining time/\1/p')
+errecho "Difficulty for target delay ${TARGET_DELAY} seconds calibrated. nbits=${NBITS}"
+
+# Let's start mining continuously. We'll reuse the same ADDR as before.
+errecho "Keep mining eternally"
+"${MINER}" --cli="${BITCOIN_CLI} -datadir=${DATADIR}" generate --address "${ADDR}" --grind-cmd="${BITCOIN_UTIL} grind" --nbits="${NBITS}" --ongoing
+errecho "You should never reach here"
