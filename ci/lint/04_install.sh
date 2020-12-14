2eca48c1caae5398c34020dc2f0bb58d2405d011
Dhruv Mehta <856960+dhruv@users.noreply.github.com>
2020-12-14 14:29:23 -0800

    testing linting travis -> cirrus

--- ci/lint/04_install.sh
+++ ci/lint/04_install.sh
@@ -6,14 +6,14 @@
 
 export LC_ALL=C
 
-travis_retry sudo apt update && sudo apt install -y clang-format-9
-sudo update-alternatives --install /usr/bin/clang-format      clang-format      $(which clang-format-9     ) 100
-sudo update-alternatives --install /usr/bin/clang-format-diff clang-format-diff $(which clang-format-diff-9) 100
+apt update && apt install -y clang-format-9
+update-alternatives --install /usr/bin/clang-format      clang-format      $(which clang-format-9     ) 100
+update-alternatives --install /usr/bin/clang-format-diff clang-format-diff $(which clang-format-diff-9) 100
 
-travis_retry pip3 install codespell==1.17.1
-travis_retry pip3 install flake8==3.8.3
-travis_retry pip3 install yq
-travis_retry pip3 install mypy==0.781
+pip3 install codespell==1.17.1
+pip3 install flake8==3.8.3
+pip3 install yq
+pip3 install mypy==0.781
 
 SHELLCHECK_VERSION=v0.7.1
 curl -sL "https://github.com/koalaman/shellcheck/releases/download/${SHELLCHECK_VERSION}/shellcheck-${SHELLCHECK_VERSION}.linux.x86_64.tar.xz" | tar --xz -xf - --directory /tmp/
