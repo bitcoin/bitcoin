13ed44dbba1c16113817a8042aa773ea7333ffbd
PastaPastaPasta <6443210+PastaPastaPasta@users.noreply.github.com>
2020-12-14 20:26:17 -0600

    fix potential deadlocks in rpcwallet.cpp
    Co-authored-by: UdjinM6 <UdjinM6@users.noreply.github.com>

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -3432,7 +3432,8 @@ UniValue signrawtransactionwithwallet(const JSONRPCRequest& request)
     }
 
     // Sign the transaction
-    LOCK2(cs_main, pwallet->cs_wallet);
+    LOCK2(cs_main, mempool.cs);
+    LOCK(pwallet->cs_wallet);
     return SignTransaction(mtx, request.params[1], pwallet, false, request.params[2]);
 }
 
