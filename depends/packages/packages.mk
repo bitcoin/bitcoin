a409fe578fcb0cfd7f2da9bb46e0811b5b72c9e9
Wladimir J. van der Laan <laanwj@protonmail.com>
2020-12-16 21:03:31 +0100

    Merge #20470: build: Replace genisoimage with xorriso

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -25,5 +25,5 @@ darwin_native_packages = native_ds_store native_mac_alias
 $(host_arch)_$(host_os)_native_packages += native_b2
 
 ifneq ($(build_os),darwin)
-darwin_native_packages += native_cctools native_cdrkit native_libdmg-hfsplus
+darwin_native_packages += native_cctools native_libdmg-hfsplus
 endif
