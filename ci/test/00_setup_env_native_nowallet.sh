b96e01722dd770ab7802179bc9e08e4c3f80c17e
Kiminuo <kiminuo@protonmail.com>
2020-12-16 08:01:16 +0000

    Test with 20.04

--- ci/test/00_setup_env_native_nowallet.sh
+++ ci/test/00_setup_env_native_nowallet.sh
@@ -7,7 +7,7 @@
 export LC_ALL=C.UTF-8
 
 export CONTAINER_NAME=ci_native_nowallet
-export DOCKER_NAME_TAG=ubuntu:18.04  # Use bionic to have one config run the tests in python3.6, see doc/dependencies.md
+export DOCKER_NAME_TAG=ubuntu:20.04  # Use bionic to have one config run the tests in python3.6, see doc/dependencies.md
 export PACKAGES="python3-zmq clang-5.0 llvm-5.0"  # Use clang-5 to test C++17 compatibility, see doc/dependencies.md
 export DEP_OPTS="NO_WALLET=1"
 export GOAL="install"
