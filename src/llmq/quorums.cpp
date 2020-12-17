ad4ca1f2be947137241836d1ba3dce54c461651a
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2020-12-16 20:34:10 -0800

    Update quorums.cpp

--- src/llmq/quorums.cpp
+++ src/llmq/quorums.cpp
@@ -143,8 +143,7 @@ void CQuorum::StartCachePopulatorThread(std::shared_ptr<CQuorum> _this)
 
     // this thread will exit after some time
     // when then later some other thread tries to get keys, it will be much faster
-    _this->cachePopulatorThread = std::thread([_this]() {
-        util::ThreadRename("syscoin-q-cachepop");
+    _this->cachePopulatorThread = std::thread(&TraceThread<std::function<void()>>, "syscoin-q-cachepop", [_this] {
         for (size_t i = 0; i < _this->members.size() && !_this->stopCachePopulatorThread && !ShutdownRequested(); i++) {
             if (_this->qc.validMembers[i]) {
                 _this->GetPubKeyShare(i);
