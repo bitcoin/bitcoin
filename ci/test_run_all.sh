09d105ef0f8b4b06bf248721a1209c9e16e9db75
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-12-17 10:43:05 +0200

    ci: Drop travis_fold feature as Travis CI is no longer used

--- ci/test_run_all.sh
+++ ci/test_run_all.sh
@@ -7,7 +7,6 @@
 export LC_ALL=C.UTF-8
 
 set -o errexit; source ./ci/test/00_setup_env.sh
-set -o errexit; source ./ci/test/03_before_install.sh
 set -o errexit; source ./ci/test/04_install.sh
 set -o errexit; source ./ci/test/05_before_script.sh
 set -o errexit; source ./ci/test/06_script_a.sh
