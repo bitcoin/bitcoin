a0bc41e79fda26e234879f334ff7de7d38dc90cb
MarcoFalke <falke.marco@gmail.com>
2020-12-17 09:04:37 +0100

    Merge #20658: ci: Move linter task to cirrus

--- ci/lint/04_install.sh
+++ ci/lint/04_install.sh
@@ -6,14 +6,14 @@
 
 export LC_ALL=C
 
-travis_retry sudo apt update && sudo apt install -y clang-format-9
-sudo update-alternatives --install /usr/bin/clang-format      clang-format      $(which clang-format-9     ) 100
-sudo update-alternatives --install /usr/bin/clang-format-diff clang-format-diff $(which clang-format-diff-9) 100
+${CI_RETRY_EXE} apt update && apt install -y clang-format-9 python3-pip curl git
+update-alternatives --install /usr/bin/clang-format      clang-format      $(which clang-format-9     ) 100
+update-alternatives --install /usr/bin/clang-format-diff clang-format-diff $(which clang-format-diff-9) 100
 
-travis_retry pip3 install codespell==1.17.1
-travis_retry pip3 install flake8==3.8.3
-travis_retry pip3 install yq
-travis_retry pip3 install mypy==0.781
+${CI_RETRY_EXE} pip3 install codespell==1.17.1
+${CI_RETRY_EXE} pip3 install flake8==3.8.3
+${CI_RETRY_EXE} pip3 install yq
+${CI_RETRY_EXE} pip3 install mypy==0.781
 
 SHELLCHECK_VERSION=v0.7.1
 curl -sL "https://github.com/koalaman/shellcheck/releases/download/${SHELLCHECK_VERSION}/shellcheck-${SHELLCHECK_VERSION}.linux.x86_64.tar.xz" | tar --xz -xf - --directory /tmp/
