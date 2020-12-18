8daef64f04fdb18f39b8f257c17c4dcce3c859cb
MarcoFalke <falke.marco@gmail.com>
2020-12-18 07:31:54 +0100

    Merge #20691: ci, doc: Travis CI features and mentions cleanup
    95487b055328b590ba83f258de9637ab0f9a2f17 doc: Drop mentions of Travis CI as it is no longer used (Hennadii Stepanov)
    09d105ef0f8b4b06bf248721a1209c9e16e9db75 ci: Drop travis_fold feature as Travis CI is no longer used (Hennadii Stepanov)
    
    Pull request description:
    
      As Travis CI is no longer used, this PR:
      - drops `travis_fold` feature
      - drops mentions of Travis CI in docs
    
    ACKs for top commit:
      MarcoFalke:
        ACK 95487b055328b590ba83f258de9637ab0f9a2f17
    
    Tree-SHA512: 2e259bb8b1e37bcefc1251737bb2716f06ddb57c490010b373825c4e70f42ca38efae69a2f63f21f577d7cee3725b94097bdddbd313f8ebf499281cf97c53cef

--- ci/test_run_all.sh
+++ ci/test_run_all.sh
@@ -7,6 +7,5 @@
 export LC_ALL=C.UTF-8
 
 set -o errexit; source ./ci/test/00_setup_env.sh
-set -o errexit; source ./ci/test/03_before_install.sh
 set -o errexit; source ./ci/test/04_install.sh
 set -o errexit; source ./ci/test/05_before_script.sh
