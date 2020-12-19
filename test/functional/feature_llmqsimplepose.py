0d9dbd03e59b7e496d3a878fc156ce58e2baef86
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2020-12-19 08:12:27 -0800

    Update feature_llmqsimplepose.py

--- test/functional/feature_llmqsimplepose.py
+++ test/functional/feature_llmqsimplepose.py
@@ -100,6 +100,7 @@ class LLMQSimplePoSeTest(DashTestFramework):
                 self.bump_mocktime(50 * 60 + 1)
                 # Sleep a couple of seconds to let mn sync tick to happen
                 time.sleep(2)
+                self.nodes[0].generate(1)
                 self.mine_quorum(expected_connections=expected_connections, expected_members=expected_contributors, expected_contributions=expected_contributors, expected_complaints=expected_contributors-1, expected_commitments=expected_contributors, mninfos_online=mninfos_online, mninfos_valid=mninfos_valid)
 
             assert(self.check_banned(mn))
