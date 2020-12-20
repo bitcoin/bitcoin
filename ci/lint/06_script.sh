3c2478c38522c176e81befd4d991a259b09be063
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-12-20 13:47:20 +0200

    ci: Print COMMIT_RANGE to the log as it was in Travis CI

--- ci/lint/06_script.sh
+++ ci/lint/06_script.sh
@@ -6,8 +6,9 @@
 
 export LC_ALL=C
 
+GIT_HEAD=$(git rev-parse HEAD)
 if [ -n "$CIRRUS_PR" ]; then
-  COMMIT_RANGE="$CIRRUS_BASE_SHA..HEAD"
+  COMMIT_RANGE="$CIRRUS_BASE_SHA..$GIT_HEAD"
   test/lint/commit-script-check.sh $COMMIT_RANGE
 fi
 export COMMIT_RANGE
@@ -28,3 +29,6 @@ if [ "$CIRRUS_REPO_FULL_NAME" = "bitcoin/bitcoin" ] && [ -n "$CIRRUS_CRON" ]; th
     ${CI_RETRY_EXE}  gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys $(<contrib/verify-commits/trusted-keys) &&
     ./contrib/verify-commits/verify-commits.py --clean-merge=2;
 fi
+
+echo
+git log --no-merges --oneline $COMMIT_RANGE
