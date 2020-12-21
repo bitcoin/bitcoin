50117e91035e254a2bf22cc37eb50cbd7d3313b4
vbk-deploy <65036814+vbk-deploy@users.noreply.github.com>
2020-12-21 06:07:55 -0600

    Update veriblock-pop-cpp (#410)
    Co-authored-by: vbk-deploy <vbk-deploy@users.noreply.github.com>

--- depends/packages/veriblock-pop-cpp.mk
+++ depends/packages/veriblock-pop-cpp.mk
@@ -1,8 +1,8 @@
 package=veriblock-pop-cpp
-$(package)_version=05087f984a12643d5f28ad99b4d92d74429e3fde
+$(package)_version=4f98d32c381c9228a2d487c0b869ef614e73c0f4
 $(package)_download_path=https://github.com/VeriBlock/alt-integration-cpp/archive/
 $(package)_file_name=$($(package)_version).tar.gz
-$(package)_sha256_hash=861aa7da5443bbc7033c1dd97a768daa43f215c00c71386e375ea96e8e6f80ab
+$(package)_sha256_hash=45eb8439253433d904f6922447e051f8c4d1ef925b65f9ef374c298de98dc41d
 $(package)_build_subdir=build
 $(package)_build_type=$(BUILD_TYPE)
 $(package)_asan=$(ASAN)
