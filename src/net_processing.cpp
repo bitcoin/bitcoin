176325a5a47befe32d480b3dc206dd0e64e04b21
John Newbery <john@johnnewbery.com>
2020-12-22 17:48:31 +0000

    [net processing] Remove dropmessagestest
    -dropmessagestest is a command line option that causes 1 in n received
    messages to be dropped. The Bitcoin P2P protocol is stateful and in
    general cannot handle messages being dropped. Dropped
    version/verack/ping/pong messages will cause the connection to time out
    and be torn down. Other dropped messages may also cause the peer to
    believe that the peer has stalled and tear down the connection.
    
    It seems difficult to uncover any actual issues with -dropmessagestest,
    and any coverage that could be generated would probably be easier to
    trigger with fuzz testing.

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -2272,11 +2272,6 @@ void PeerManager::ProcessMessage(CNode& pfrom, const std::string& msg_type, CDat
                                          const std::atomic<bool>& interruptMsgProc)
 {
     LogPrint(BCLog::NET, "received: %s (%u bytes) peer=%d\n", SanitizeString(msg_type), vRecv.size(), pfrom.GetId());
-    if (gArgs.IsArgSet("-dropmessagestest") && GetRand(gArgs.GetArg("-dropmessagestest", 0)) == 0)
-    {
-        LogPrintf("dropmessagestest DROPPING RECV MESSAGE\n");
-        return;
-    }
 
     PeerRef peer = GetPeerRef(pfrom.GetId());
     if (peer == nullptr) return;
