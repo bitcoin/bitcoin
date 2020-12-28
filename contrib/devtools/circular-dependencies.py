93ce1b2cd9ef9dacdbaa2578206b5ce8b3d1b784
Wladimir J. van der Laan <laanwj@protonmail.com>
2020-12-28 14:18:02 +0100

    Merge #20451: lint: run mypy over contrib/devtools
    1ef2138c0db3bd4f9332c777fa3fb2770dc1b08c lint: run mypy over contrib/devtools (fanquake)
    
    Pull request description:
    
      wumpus mentioned on IRC that we don't currently run `mypy` over the `contrib/devtools` directory, and that it would likely be worthwhile given #20434. This just adds that dir to the linter, as well as some missing annotations to fix existing errors. Note that now we require Python 3.6 we can make use of variable annotations.
    
      master (patched to check contrib devtools):
      ```bash
      test/lint/lint-python.sh
      contrib/devtools/symbol-check.py:154: error: Incompatible types in assignment (expression has type "List[str]", variable has type "str")
      contrib/devtools/circular-dependencies.py:35: error: Need type annotation for 'deps' (hint: "deps: Dict[<type>, <type>] = ...")
      contrib/devtools/circular-dependencies.py:67: error: Need type annotation for 'closure' (hint: "closure: Dict[<type>, <type>] = ...")
      Found 4 errors in 3 files (checked 187 source files)
      ```
    
      I haven't quite gone as far as to add annotations like
      ```python
      CHECKS: Dict[str, List[Tuple[str, Callable[[Any], bool]]]] = {...
      ```
      to `symbol-check.py`.
    
    ACKs for top commit:
      laanwj:
        ACK 1ef2138c0db3bd4f9332c777fa3fb2770dc1b08c
    
    Tree-SHA512: a58c2ece588c640289dc1d35dad5b1b8732788272daa0965d6bf44ee8a7f7c8e8585f94d233ac41c84b9ffcfc97841a00fe2c9acba41f58fd164f01de4b6512b

--- contrib/devtools/circular-dependencies.py
+++ contrib/devtools/circular-dependencies.py
@@ -5,6 +5,7 @@
 
 import sys
 import re
+from typing import Dict, List, Set
 
 MAPPING = {
     'core_read.cpp': 'core_io.cpp',
@@ -32,7 +33,7 @@ def module_name(path):
     return None
 
 files = dict()
-deps = dict()
+deps: Dict[str, Set[str]] = dict()
 
 RE = re.compile("^#include <(.*)>")
 
@@ -59,12 +60,12 @@ for arg in sorted(files.keys()):
                     deps[module].add(included_module)
 
 # Loop to find the shortest (remaining) circular dependency
-have_cycle = False
+have_cycle: bool = False
 while True:
     shortest_cycle = None
     for module in sorted(deps.keys()):
         # Build the transitive closure of dependencies of module
-        closure = dict()
+        closure: Dict[str, List[str]] = dict()
         for dep in deps[module]:
             closure[dep] = []
         while True:
