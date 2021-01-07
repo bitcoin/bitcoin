1b0fc000f25c4e1bcca252e15ccf71237446c0dc
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-01-07 14:55:46 -0800

    Update mapport.h

--- src/mapport.h
+++ src/mapport.h
@@ -2,8 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_MAPPORT_H
-#define BITCOIN_MAPPORT_H
+#ifndef SYSCOIN_MAPPORT_H
+#define SYSCOIN_MAPPORT_H
 
 #ifdef USE_UPNP
 static constexpr bool DEFAULT_UPNP = USE_UPNP;
@@ -27,4 +27,4 @@ void StartMapPort(bool use_upnp, bool use_natpmp);
 void InterruptMapPort();
 void StopMapPort();
 
-#endif // BITCOIN_MAPPORT_H
+#endif // SYSCOIN_MAPPORT_H
