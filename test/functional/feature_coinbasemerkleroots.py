24d3b88add65f05c398d9d62c11e484ef5b57964
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-01-08 13:16:04 -0800

    Update feature_coinbasemerkleroots.py

--- test/functional/feature_coinbasemerkleroots.py
+++ test/functional/feature_coinbasemerkleroots.py
@@ -93,7 +93,7 @@ class LLMQCoinbaseCommitmentsTest(DashTestFramework):
         for n in self.nodes:
             n.invalidateblock(oldhash)
         self.sync_all()
-        first_quorum = self.test_dip8_quorum_merkle_root_activation(False)
+        first_quorum = self.test_dip8_quorum_merkle_root_activation(False, True)
         # Re-enable ChainLocks again
         self.nodes[0].spork("SPORK_19_CHAINLOCKS_ENABLED", 0)
         self.nodes[0].spork("SPORK_17_QUORUM_DKG_ENABLED", 0)
@@ -235,7 +235,7 @@ class LLMQCoinbaseCommitmentsTest(DashTestFramework):
 
         return d
 
-    def test_dip8_quorum_merkle_root_activation(self, with_initial_quorum):
+    def test_dip8_quorum_merkle_root_activation(self, with_initial_quorum, slow_mode=False):
         if with_initial_quorum:
             self.nodes[0].spork("SPORK_17_QUORUM_DKG_ENABLED", 0)
             self.wait_for_sporks_same()
