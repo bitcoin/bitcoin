1294a6883558a2c021be4b2df9f25ce8a523d531
Andrew Chow <achow101-github@achow101.com>
2021-01-11 15:27:38 -0500

    Convert PSBTv0 unsigned tx to PSBTv2 fields
    This is just a convenience and doesn't effect serialization.

--- src/psbt.h
+++ src/psbt.h
@@ -1518,6 +1518,8 @@ struct PartiallySignedTransaction
         if (outputs.size() != output_count) {
             throw std::ios_base::failure("Outputs provided does not match the number of outputs in transaction.");
         }
+
+        CacheUnsignedTxPieces();
     }
 
     template <typename Stream>
