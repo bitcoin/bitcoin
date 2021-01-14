3ee7c291b0686e5117edd41025bc0f7b06ea3c89
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2021-01-14 12:55:06 -0700

    qt: Add masternode info to peer page (#3927)
    * Add masternode info to peer page
    
    * Update src/qt/forms/debugwindow.ui
    
    Co-authored-by: UdjinM6 <UdjinM6@users.noreply.github.com>
    
    * Use verifiedProRegTxHash to check if node is masternode
    
    Co-authored-by: UdjinM6 <UdjinM6@users.noreply.github.com>
    
    Co-authored-by: UdjinM6 <UdjinM6@users.noreply.github.com>

--- src/qt/rpcconsole.cpp
+++ src/qt/rpcconsole.cpp
@@ -1240,6 +1240,14 @@ void RPCConsole::updateNodeDetail(const CNodeCombinedStats *stats)
     ui->peerDirection->setText(stats->nodeStats.fInbound ? tr("Inbound") : tr("Outbound"));
     ui->peerHeight->setText(QString("%1").arg(QString::number(stats->nodeStats.nStartingHeight)));
     ui->peerWhitelisted->setText(stats->nodeStats.fWhitelisted ? tr("Yes") : tr("No"));
+    if (stats->nodeStats.verifiedProRegTxHash.IsNull()) {
+        ui->peerNodeType->setText(tr("Normal"));
+        ui->peerPoSeScore->setText(tr("N/A"));
+    } else {
+        ui->peerNodeType->setText(tr("Masternode"));
+        auto dmn = clientModel->getMasternodeList().GetMNByService(stats->nodeStats.addr);
+        ui->peerPoSeScore->setText(dmn == nullptr ? tr("N/A") : QString::number(dmn->pdmnState->nPoSePenalty));
+    }
 
     // This check fails for example if the lock was busy and
     // nodeStateStats couldn't be fetched.
