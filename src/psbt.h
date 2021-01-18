ca4d341b5736856c62731ff800ddb464c9a4c118
Andrew Chow <achow101-github@achow101.com>
2021-01-18 16:55:20 -0500

    Allow specifying PSBT version in constructor

--- src/psbt.h
+++ src/psbt.h
@@ -1168,7 +1168,7 @@ struct PartiallySignedTransaction
     CMutableTransaction GetUnsignedTx() const;
     uint256 GetUniqueID() const;
     PartiallySignedTransaction() {}
-    explicit PartiallySignedTransaction(const CMutableTransaction& tx);
+    explicit PartiallySignedTransaction(const CMutableTransaction& tx, uint32_t version = 0);
 
     template <typename Stream>
     inline void Serialize(Stream& s) const {
