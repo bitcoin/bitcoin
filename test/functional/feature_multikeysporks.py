e9341da0a7e35e5697042d236b722b031d6ba691
UdjinM6 <UdjinM6@users.noreply.github.com>
2021-01-21 01:27:41 +0300

    spork: Fix a couple of issues with multikey sporks cleanup, add more tests (#3947)
    * Fix a couple of issues with multikey sporks cleanup
    
    1. Should remove sporks with signatures from unknown signers from mapSporksActive
    2. Should advance itSignerPair while doing (1)...
    
    * tests: make sure sporks cleanup works as expected for multikey sporks
    
    * tests: make sure multiple multikey sporks (and their cleanups) work together as expected
    
    * Prettify node extra args

--- test/functional/feature_multikeysporks.py
+++ test/functional/feature_multikeysporks.py
@@ -48,83 +48,88 @@ class MultiKeySporkTest(BitcoinTestFramework):
 
         self.add_nodes(5)
 
-        self.start_node(0, ["-sporkkey=931wyuRNVYvhg18Uu9bky5Qg1z4QbxaJ7fefNBzjBPiLRqcd33F",
-                            "-sporkaddr=ygcG5S2pQz2U1UAaHvU6EznKZW7yapKMA7",
-                            "-sporkaddr=yfLSXFfipnkgYioD6L8aUNyfRgEBuJv48h",
-                            "-sporkaddr=yNsMZhEhYqv14TgdYb1NS2UmNZjE8FSJxa",
-                            "-sporkaddr=ycbRQWbovrhQMTuxg9p4LAuW5SCMAKqPrn",
-                            "-sporkaddr=yc5TGfcHYoLCrcbVy4umsiDjsYUn39vLui",
-                            "-minsporkkeys=3"])
-        self.start_node(1, ["-sporkkey=91vbXGMSWKGHom62986XtL1q2mQDA12ngcuUNNe5NfMSj44j7g3",
-                            "-sporkaddr=ygcG5S2pQz2U1UAaHvU6EznKZW7yapKMA7",
-                            "-sporkaddr=yfLSXFfipnkgYioD6L8aUNyfRgEBuJv48h",
-                            "-sporkaddr=yNsMZhEhYqv14TgdYb1NS2UmNZjE8FSJxa",
-                            "-sporkaddr=ycbRQWbovrhQMTuxg9p4LAuW5SCMAKqPrn",
-                            "-sporkaddr=yc5TGfcHYoLCrcbVy4umsiDjsYUn39vLui",
-                            "-minsporkkeys=3"])
-        self.start_node(2, ["-sporkkey=92bxUjPT5AhgXuXJwfGGXqhomY2SdQ55MYjXyx9DZNxCABCSsRH",
-                            "-sporkaddr=ygcG5S2pQz2U1UAaHvU6EznKZW7yapKMA7",
-                            "-sporkaddr=yfLSXFfipnkgYioD6L8aUNyfRgEBuJv48h",
-                            "-sporkaddr=yNsMZhEhYqv14TgdYb1NS2UmNZjE8FSJxa",
-                            "-sporkaddr=ycbRQWbovrhQMTuxg9p4LAuW5SCMAKqPrn",
-                            "-sporkaddr=yc5TGfcHYoLCrcbVy4umsiDjsYUn39vLui",
-                            "-minsporkkeys=3"])
-        self.start_node(3, ["-sporkkey=934yPXiVGf4RCY2qTs2Bt5k3TEtAiAg12sMxCt8yVWbSU7p3fuD",
-                            "-sporkaddr=ygcG5S2pQz2U1UAaHvU6EznKZW7yapKMA7",
-                            "-sporkaddr=yfLSXFfipnkgYioD6L8aUNyfRgEBuJv48h",
-                            "-sporkaddr=yNsMZhEhYqv14TgdYb1NS2UmNZjE8FSJxa",
-                            "-sporkaddr=ycbRQWbovrhQMTuxg9p4LAuW5SCMAKqPrn",
-                            "-sporkaddr=yc5TGfcHYoLCrcbVy4umsiDjsYUn39vLui",
-                            "-minsporkkeys=3"])
-        self.start_node(4, ["-sporkkey=92Cxwia363Wg2qGF1fE5z4GKi8u7r1nrWQXdtsj2ACZqaDPSihD",
-                            "-sporkaddr=ygcG5S2pQz2U1UAaHvU6EznKZW7yapKMA7",
-                            "-sporkaddr=yfLSXFfipnkgYioD6L8aUNyfRgEBuJv48h",
-                            "-sporkaddr=yNsMZhEhYqv14TgdYb1NS2UmNZjE8FSJxa",
-                            "-sporkaddr=ycbRQWbovrhQMTuxg9p4LAuW5SCMAKqPrn",
-                            "-sporkaddr=yc5TGfcHYoLCrcbVy4umsiDjsYUn39vLui",
-                            "-minsporkkeys=3"])
+        spork_chain_params =   ["-sporkaddr=ygcG5S2pQz2U1UAaHvU6EznKZW7yapKMA7",
+                                "-sporkaddr=yfLSXFfipnkgYioD6L8aUNyfRgEBuJv48h",
+                                "-sporkaddr=yNsMZhEhYqv14TgdYb1NS2UmNZjE8FSJxa",
+                                "-sporkaddr=ycbRQWbovrhQMTuxg9p4LAuW5SCMAKqPrn",
+                                "-sporkaddr=yc5TGfcHYoLCrcbVy4umsiDjsYUn39vLui",
+                                "-minsporkkeys=3"]
+
+        # Node0 extra args to use on normal node restarts
+        self.node0_extra_args = ["-sporkkey=931wyuRNVYvhg18Uu9bky5Qg1z4QbxaJ7fefNBzjBPiLRqcd33F"] + spork_chain_params
+
+        self.start_node(0, self.node0_extra_args)
+        self.start_node(1, ["-sporkkey=91vbXGMSWKGHom62986XtL1q2mQDA12ngcuUNNe5NfMSj44j7g3"] + spork_chain_params)
+        self.start_node(2, ["-sporkkey=92bxUjPT5AhgXuXJwfGGXqhomY2SdQ55MYjXyx9DZNxCABCSsRH"] + spork_chain_params)
+        self.start_node(3, ["-sporkkey=934yPXiVGf4RCY2qTs2Bt5k3TEtAiAg12sMxCt8yVWbSU7p3fuD"] + spork_chain_params)
+        self.start_node(4, ["-sporkkey=92Cxwia363Wg2qGF1fE5z4GKi8u7r1nrWQXdtsj2ACZqaDPSihD"] + spork_chain_params)
+
         # connect nodes at start
         for i in range(0, 5):
             for j in range(i, 5):
                 connect_nodes(self.nodes[i], j)
 
-    def get_test_spork_value(self, node):
+    def get_test_spork_value(self, node, spork_name):
+        self.bump_mocktime(5)  # advance ProcessTick
         info = node.spork('show')
         # use InstantSend spork for tests
-        return info['SPORK_2_INSTANTSEND_ENABLED']
+        return info[spork_name]
 
-    def set_test_spork_value(self, node, value):
-        # use InstantSend spork for tests
-        node.spork('SPORK_2_INSTANTSEND_ENABLED', value)
-
-    def run_test(self):
+    def test_spork(self, spork_name, final_value):
         # check test spork default state
         for node in self.nodes:
-            assert(self.get_test_spork_value(node) == 4070908800)
+            assert(self.get_test_spork_value(node, spork_name) == 4070908800)
 
         self.bump_mocktime(1)
         # first and second signers set spork value
-        self.set_test_spork_value(self.nodes[0], 1)
-        self.set_test_spork_value(self.nodes[1], 1)
+        self.nodes[0].spork(spork_name, 1)
+        self.nodes[1].spork(spork_name, 1)
         # spork change requires at least 3 signers
         time.sleep(10)
         for node in self.nodes:
-            assert(self.get_test_spork_value(node) != 1)
+            assert(self.get_test_spork_value(node, spork_name) != 1)
+
+        # restart with no extra args to trigger CheckAndRemove
+        self.restart_node(0)
+        assert(self.get_test_spork_value(self.nodes[0], spork_name) != 1)
+
+        # restart again with corect_params, should resync spork parts from other nodes
+        self.restart_node(0, self.node0_extra_args)
+        for i in range(1, 5):
+            connect_nodes(self.nodes[0], i)
 
         # third signer set spork value
-        self.set_test_spork_value(self.nodes[2], 1)
+        self.nodes[2].spork(spork_name, 1)
         # now spork state is changed
         for node in self.nodes:
-            wait_until(lambda: self.get_test_spork_value(node) == 1, sleep=0.1, timeout=10)
+            wait_until(lambda: self.get_test_spork_value(node, spork_name) == 1, sleep=0.1, timeout=10)
+
+        # restart with no extra args to trigger CheckAndRemove, should reset the spork back to its default
+        self.restart_node(0)
+        assert(self.get_test_spork_value(self.nodes[0], spork_name) == 4070908800)
+
+        # restart again with corect_params, should resync sporks from other nodes
+        self.restart_node(0, self.node0_extra_args)
+        for i in range(1, 5):
+            connect_nodes(self.nodes[0], i)
+
+        wait_until(lambda: self.get_test_spork_value(self.nodes[0], spork_name) == 1, sleep=0.1, timeout=10)
 
         self.bump_mocktime(1)
         # now set the spork again with other signers to test
         # old and new spork messages interaction
-        self.set_test_spork_value(self.nodes[2], 2)
-        self.set_test_spork_value(self.nodes[3], 2)
-        self.set_test_spork_value(self.nodes[4], 2)
+        self.nodes[2].spork(spork_name, final_value)
+        self.nodes[3].spork(spork_name, final_value)
+        self.nodes[4].spork(spork_name, final_value)
         for node in self.nodes:
-            wait_until(lambda: self.get_test_spork_value(node) == 2, sleep=0.1, timeout=10)
+            wait_until(lambda: self.get_test_spork_value(node, spork_name) == final_value, sleep=0.1, timeout=10)
+
+    def run_test(self):
+        self.test_spork('SPORK_2_INSTANTSEND_ENABLED', 2)
+        self.test_spork('SPORK_3_INSTANTSEND_BLOCK_FILTERING', 3)
+        for node in self.nodes:
+            assert(self.get_test_spork_value(node, 'SPORK_2_INSTANTSEND_ENABLED') == 2)
+            assert(self.get_test_spork_value(node, 'SPORK_3_INSTANTSEND_BLOCK_FILTERING') == 3)
 
 
 if __name__ == '__main__':
