10fa92c64477ffe28871e9c4620b56a2ee8ae1c4
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-01-20 12:16:53 -0800

    Update trace.h

--- src/util/trace.h
+++ src/util/trace.h
@@ -2,8 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_UTIL_TRACE_H
-#define BITCOIN_UTIL_TRACE_H
+#ifndef SYSCOIN_UTIL_TRACE_H
+#define SYSCOIN_UTIL_TRACE_H
 
 #ifdef ENABLE_TRACING
 
@@ -42,4 +42,4 @@
 #endif
 
 
-#endif /* BITCOIN_UTIL_TRACE_H */
+#endif /* SYSCOIN_UTIL_TRACE_H */
