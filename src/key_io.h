1f9d20bd2a6fba930c49ce52bd0cb3f9bd436e59
Samuel Dobson <dobsonsa68@gmail.com>
2021-01-26 12:38:38 +1300

    Merge #20832: rpc: Better error messages for invalid addresses

--- src/key_io.h
+++ src/key_io.h
@@ -23,6 +23,7 @@ std::string EncodeExtPubKey(const CExtPubKey& extpubkey);
 
 std::string EncodeDestination(const CTxDestination& dest);
 CTxDestination DecodeDestination(const std::string& str);
+CTxDestination DecodeDestination(const std::string& str, std::string& error_msg);
 bool IsValidDestinationString(const std::string& str);
 bool IsValidDestinationString(const std::string& str, const CChainParams& params);
 
