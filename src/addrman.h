4676a4fb5be0f6ef0b3f71c1f4361c20f7cb0e0b
John Newbery <john@johnnewbery.com>
2021-01-29 10:29:44 +0000

    [addrman] Don't repeat "Bucketing method was updated" log multiple times
    Thanks to Vasil Dimov <vd@FreeBSD.org> for these suggestions

--- src/addrman.h
+++ src/addrman.h
@@ -528,6 +528,10 @@ public:
         const bool restore_bucketing{nUBuckets == ADDRMAN_NEW_BUCKET_COUNT &&
                                      serialized_asmap_checksum == supplied_asmap_checksum};
 
+        if (!restore_bucketing) {
+            LogPrint(BCLog::ADDRMAN, "Bucketing method was updated, re-bucketing addrman entries from disk\n");
+        }
+
         for (auto bucket_entry : bucket_entries) {
             int bucket{bucket_entry.first};
             const int entry_index{bucket_entry.second};
@@ -546,7 +550,6 @@ public:
             } else {
                 // In case the new table data cannot be used (bucket count wrong or new asmap),
                 // try to give them a reference based on their primary source address.
-                LogPrint(BCLog::ADDRMAN, "Bucketing method was updated, re-bucketing addrman entries from disk\n");
                 bucket = info.GetNewBucket(nKey, m_asmap);
                 bucket_position = info.GetBucketPosition(nKey, true, bucket);
                 if (vvNew[bucket][bucket_position] == -1) {
