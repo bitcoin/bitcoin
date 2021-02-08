723eb4326bac4a3906cbfe278bb6b8bee17e7790
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2021-02-08 15:11:07 +0200

    test: Fix Windows cross build

--- src/test/fuzz/string.cpp
+++ src/test/fuzz/string.cpp
@@ -78,7 +78,9 @@ FUZZ_TARGET(string)
     }
     (void)SanitizeString(random_string_1);
     (void)SanitizeString(random_string_1, fuzzed_data_provider.ConsumeIntegralInRange<int>(0, 3));
+#ifndef WIN32
     (void)ShellEscape(random_string_1);
+#endif // WIN32
     int port_out;
     std::string host_out;
     SplitHostPort(random_string_1, port_out, host_out);
