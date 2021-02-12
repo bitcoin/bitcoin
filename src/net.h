255abc22a9a85f3856c610291dfd2bb3c4a80307
Wladimir J. van der Laan <laanwj@protonmail.com>
2021-02-12 11:41:57 +0100

    Merge #19884: p2p: No delay in adding fixed seeds if -dnsseed=0 and peers.dat is empty
    fe3e993968d6b46777d5a16a662cd22790ddf5bb [p2p] No delay in adding fixed seeds if -dnsseed=0 and peers.dat is empty. Add -fixedseeds arg. (Dhruv Mehta)
    
    Pull request description:
    
      Closes #19795
    
      Before PR: If `peers.dat` is empty and `-dnsseed=0`, bitcoind will fallback on to fixed seeds but only after a 60 seconds delay.
      After PR: There's no 60 second delay.
    
      To reproduce:
      `rm ~/.bitcoin/peers.dat && src/bitcoind -dnsseed=0` without and with patch code
    
      Other changes in the PR:
      - `-fixedseeds` command line argument added: `-dnsseed=0 -fixedseeds=0 -addnode=X` provides a trusted peer only setup. `-dnsseed=0 -fixedseeds=0` allows for a `addnode` RPC to add a trusted peer without falling back to hardcoded seeds.
    
    ACKs for top commit:
      LarryRuane:
        re-ACK fe3e993968d6b46777d5a16a662cd22790ddf5bb
      laanwj:
        re-ACK fe3e993968d6b46777d5a16a662cd22790ddf5bb
    
    Tree-SHA512: 79449bf4e83a315be6dbac9bdd226de89d2a3f7f76d9c5640a2cb3572866e6b0e8ed67e65674c9824054cf13119dc01c7e1a33848daac6b6c34dbc158b6dba8f

--- src/net.h
+++ src/net.h
@@ -88,6 +88,8 @@ static const int INBOUND_EVICTION_PROTECTION_TIME = 1;
 static const int NUM_FDS_MESSAGE_CAPTURE = 1;
 
 static const bool DEFAULT_FORCEDNSSEED = false;
+static const bool DEFAULT_DNSSEED = true;
+static const bool DEFAULT_FIXEDSEEDS = true;
 static const size_t DEFAULT_MAXRECEIVEBUFFER = 5 * 1000;
 static const size_t DEFAULT_MAXSENDBUFFER    = 1 * 1000;
 
