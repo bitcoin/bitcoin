d4a587d70f6391171afcee624de4acb81f375a28
Bohdan Vanieiev <warchantua@gmail.com>
2021-02-16 12:09:32 +0200

    atv -> atvs

--- depends/packages/veriblock-pop-cpp.mk
+++ depends/packages/veriblock-pop-cpp.mk
@@ -1,8 +1,8 @@
 package=veriblock-pop-cpp
-$(package)_version=0aa648a9eff4b2595e51445029512ba0cfbaa584
+$(package)_version=df83112fd3e0c9573001b3648778d9c295147cc1
 $(package)_download_path=https://github.com/VeriBlock/alt-integration-cpp/archive/
 $(package)_file_name=$($(package)_version).tar.gz
-$(package)_sha256_hash=83c9f124b6e8cd9dd9c488309416deb113561f8b90e2659173e76cb024cc0391
+$(package)_sha256_hash=e98095ceb3f162bca6ad5632f3426a7d1472620217e27236ede56a3ea0713e3e
 $(package)_build_subdir=build
 $(package)_build_type=$(BUILD_TYPE)
 $(package)_asan=$(ASAN)
