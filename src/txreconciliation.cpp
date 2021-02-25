70f23791552d3a1e8c2e21b325f4bbf115cadc36
Gleb Naumenko <naumenko.gs@gmail.com>
2021-02-25 14:41:11 +0200

    Add helper to compute sketches for tx reconciliation

--- src/txreconciliation.cpp
+++ src/txreconciliation.cpp
@@ -4,6 +4,8 @@
 
 #include <txreconciliation.h>
 
+#include <minisketch/include/minisketch.h>
+
 #include <unordered_map>
 
 namespace {
@@ -15,6 +17,22 @@ const std::string RECON_STATIC_SALT = "Tx Relay Salting";
 /** Announce transactions via full wtxid to a limited number of inbound and outbound peers. */
 constexpr uint8_t INBOUND_FANOUT_DESTINATIONS = 2;
 constexpr uint8_t OUTBOUND_FANOUT_DESTINATIONS = 2;
+/** The size of the field, used to compute sketches to reconcile transactions (see BIP-330). */
+constexpr unsigned int RECON_FIELD_SIZE = 32;
+/**
+ * Limit sketch capacity to avoid DoS. This applies only to the original sketches,
+ * and implies that extended sketches could be at most twice the size.
+ */
+constexpr uint32_t MAX_SKETCH_CAPACITY = 2 << 12;
+/**
+* It is possible that if sketch encodes more elements than the capacity, or
+* if it is constructed of random bytes, sketch decoding may "succeed",
+* but the result will be nonsense (false-positive decoding).
+* Given this coef, a false positive probability will be of 1 in 2**coef.
+*/
+constexpr unsigned int RECON_FALSE_POSITIVE_COEF = 16;
+static_assert(RECON_FALSE_POSITIVE_COEF <= 256,
+    "Reducing reconciliation false positives beyond 1 in 2**256 is not supported");
 /** Coefficient used to estimate reconciliation set differences. */
 constexpr double RECON_Q = 0.01;
 /**
@@ -66,6 +84,14 @@ struct ReconciliationSet {
     /** Transactions we want to announce to the peer */
     std::set<uint256> m_wtxids;
 
+    /**
+     * Reconciliation sketches are computed over short transaction IDs.
+     * This is a cache of these IDs enabling faster lookups of full wtxids,
+     * useful when peer will ask for missing transactions by short IDs
+     * at the end of a reconciliation round.
+     */
+    std::map<uint32_t, uint256> m_short_id_mapping;
+
     /** Get a number of transactions in the set. */
     size_t GetSize() const {
         return m_wtxids.size();
@@ -74,6 +100,7 @@ struct ReconciliationSet {
     /** This should be called at the end of every reconciliation to avoid unbounded state growth. */
     void Clear() {
         m_wtxids.clear();
+        m_short_id_mapping.clear();
     }
 
 };
@@ -112,6 +139,19 @@ struct ReconciliationInitByThem {
 
     /** Keep track of the reconciliation phase with the peer. */
     Phase m_phase{Phase::NONE};
+
+    /**
+     * Estimate a capacity of a sketch we will send or use locally (to find set difference)
+     * based on the local set size.
+     */
+    uint32_t EstimateSketchCapacity(size_t local_set_size) const
+    {
+        const uint16_t set_size_diff = std::abs(uint16_t(local_set_size) - m_remote_set_size);
+        const uint16_t min_size = std::min(uint16_t(local_set_size), m_remote_set_size);
+        const uint16_t weighted_min_size = m_remote_q * min_size;
+        const uint32_t estimated_diff = 1 + weighted_min_size + set_size_diff;
+        return minisketch_compute_capacity(RECON_FIELD_SIZE, estimated_diff, RECON_FALSE_POSITIVE_COEF);
+    }
 };
 
 /**
@@ -161,6 +201,29 @@ class ReconciliationState {
 
     ReconciliationState(uint64_t k0, uint64_t k1, bool we_initiate) :
         m_k0(k0), m_k1(k1), m_we_initiate(we_initiate) {}
+
+    /**
+     * Reconciliation involves computing a space-efficient representation of transaction identifiers
+     * (a sketch). A sketch has a capacity meaning it allows reconciling at most a certain number
+     * of elements (see BIP-330).
+     */
+    Minisketch ComputeSketch(uint32_t& capacity)
+    {
+        Minisketch sketch;
+        // Avoid serializing/sending an empty sketch.
+        if (capacity == 0) return sketch;
+
+        capacity = std::min(capacity, MAX_SKETCH_CAPACITY);
+        sketch = Minisketch(RECON_FIELD_SIZE, 0, capacity);
+
+        for (const auto& wtxid: m_local_set.m_wtxids) {
+            uint32_t short_txid = ComputeShortID(wtxid);
+            sketch.Add(short_txid);
+            m_local_set.m_short_id_mapping.emplace(short_txid, wtxid);
+        }
+
+        return sketch;
+    }
 };
 
 } // namespace
