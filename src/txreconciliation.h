05d7e5a21f0e64252724c860db6617919fcec1d4
Gleb Naumenko <naumenko.gs@gmail.com>
2021-02-25 15:09:06 +0200

    Add a function to identify local/remote missing txs
    When the sketches from both sides are combined successfully,
    the diff is produced. Then this diff can (together with the local txs)
    be used to identified which transactions are missing locally and remotely.

--- src/txreconciliation.h
+++ src/txreconciliation.h
@@ -328,6 +328,26 @@ public:
         if (clear_local_set) m_local_set.clear();
         ClearState();
     }
+
+    /**
+     * When during reconciliation we find a set difference successfully (by combining sketches),
+     * we want to find which transactions are missing on our and on their side.
+     * For those missing on our side, we may only find short IDs.
+     */
+    std::pair<std::vector<uint32_t>, std::vector<uint256>> GetRelevantIDsFromShortIDs(const std::vector<uint64_t>& diff) const
+    {
+        std::vector<uint32_t> local_missing;
+        std::vector<uint256> remote_missing;
+        for (const auto& diff_short_id: diff) {
+            const auto local_tx = m_local_short_id_mapping.find(diff_short_id);
+            if (local_tx != m_local_short_id_mapping.end()) {
+                remote_missing.push_back(local_tx->second);
+            } else {
+                local_missing.push_back(diff_short_id);
+            }
+        }
+        return std::make_pair(local_missing, remote_missing);
+    }
 };
 
 /**
