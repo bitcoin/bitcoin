aa6a707d7cad10e53df79183bc9887f668d6c486
Luke Dashjr <luke-jr+git@utopios.org>
2021-02-26 03:45:04 +0000

    GUI: Don't force creation of a descriptor wallet, error instead if BDB isn't supported

--- src/qt/createwalletdialog.cpp
+++ src/qt/createwalletdialog.cpp
@@ -12,6 +12,7 @@
 
 #include <qt/guiutil.h>
 
+#include <QMessageBox>
 #include <QPushButton>
 
 CreateWalletDialog::CreateWalletDialog(QWidget* parent) :
@@ -94,9 +95,16 @@ CreateWalletDialog::CreateWalletDialog(QWidget* parent) :
         ui->external_signer_checkbox->setChecked(false);
 #endif
 
-#ifndef USE_BDB
-        ui->descriptor_checkbox->setEnabled(false);
-        ui->descriptor_checkbox->setChecked(true);
+#ifdef USE_BDB
+    connect(ui->buttonBox, &QDialogButtonBox::accepted, this, &QDialog::accept);
+#else
+    connect(ui->buttonBox, &QDialogButtonBox::accepted, [this]() {
+        if (!this->isDescriptorWalletChecked()) {
+            QMessageBox::critical(this, tr("Cannot create wallet"), tr("This build was compiled without BDB support, so only experimental descriptor wallets are supported."));
+            return;
+        }
+        this->accept();
+    });
 #endif
 
 #ifndef ENABLE_EXTERNAL_SIGNER
