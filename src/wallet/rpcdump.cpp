6ee2c7cc59376dce6c0fdd2aa25b12a4d925d4ca
Wladimir J. van der Laan <laanwj@protonmail.com>
2021-02-26 10:07:46 +0100

    Merge #21277: wallet: listdescriptors uses normalized descriptor form
    a69c3b35f8974b378a87a3e42d331bd4147e07df wallet: listdescriptors uses normalized descriptor form (Ivan Metlushko)
    
    Pull request description:
    
      Rationale: show importable descriptors with `listdescriptors` RPC
    
      It uses #19136 to derive xpub at the last hardened step.
    
      **Before**:
      ```
      [
          {
            "desc": "wpkh(tpubD6NzVbkrYhZ4YUQRJL49TWw1VR5v3QKUNYaGGMUfJUm19x5ZqQ2hEiPiYbAQvD2nHoPGQGPg3snLPM8sjmYpvx7XQhkmyfk8xhsUwKbXzyh/84'/1'/0'/0/*)#p4cn3erf",
            "timestamp": 1613982591,
            "active": true,
            "internal": false,
            "range": [
              0,
              999
            ],
            "next": 0
          },
          ...
      ]
      ```
    
      **After**:
      ```
      [
        {
          "desc": "wpkh([d4ade89c/84'/1'/0']tpubDDUEYcVXy6Vh5meHvcXN3sAr4k3fWwLZGpAHbkAHL8EnkDxp4d99CjNhJHfM2fUJicANvAKnCZS6XaVAgwAeKYc1KesGCN5qbQ25qQHrRxM/0/*)#8wq8rcft",
          "timestamp": 1613982591,
          "active": true,
          "internal": false,
          "range": [
            0,
            999
          ],
          "next": 0
        },
        ...
      ]
      ```
    
    ACKs for top commit:
      achow101:
        ACK a69c3b35f8974b378a87a3e42d331bd4147e07df
    
    Tree-SHA512: 4f92e726cb8245aa0b520729cfd272945f0c66830f0555544e0618883aca516318543fa6ab1862058c64b4e4ed54ad1da953e032f4846eef7454122031c1b005

--- src/wallet/rpcdump.cpp
+++ src/wallet/rpcdump.cpp
@@ -1971,6 +1971,8 @@ RPCHelpMan listdescriptors()
         throw JSONRPCError(RPC_WALLET_ERROR, "listdescriptors is not available for non-descriptor wallets");
     }
 
+    EnsureWalletIsUnlocked(wallet.get());
+
     LOCK(wallet->cs_wallet);
 
     UniValue response(UniValue::VARR);
@@ -1983,7 +1985,11 @@ RPCHelpMan listdescriptors()
         UniValue spk(UniValue::VOBJ);
         LOCK(desc_spk_man->cs_desc_man);
         const auto& wallet_descriptor = desc_spk_man->GetWalletDescriptor();
-        spk.pushKV("desc", wallet_descriptor.descriptor->ToString());
+        std::string descriptor;
+        if (!desc_spk_man->GetDescriptorString(descriptor, false)) {
+            throw JSONRPCError(RPC_WALLET_ERROR, "Can't get normalized descriptor string.");
+        }
+        spk.pushKV("desc", descriptor);
         spk.pushKV("timestamp", wallet_descriptor.creation_time);
         const bool active = active_spk_mans.count(desc_spk_man) != 0;
         spk.pushKV("active", active);
