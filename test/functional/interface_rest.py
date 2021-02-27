459104b2aae6eeaadfa5a7e47944f1a34780dacd
fyquah <fyquah@protonmail.com>
2021-02-27 18:18:41 +0000

    rest: Add test for prevout fields in getblock

--- test/functional/interface_rest.py
+++ test/functional/interface_rest.py
@@ -311,6 +311,15 @@ class RESTTest (BitcoinTestFramework):
                             if 'coinbase' not in tx['vin'][0]}
         assert_equal(non_coinbase_txs, set(txs))
 
+        # Verify that the non-coinbase tx has "prevout" key set
+        for tx_obj in json_obj["tx"]:
+            for vin in tx_obj["vin"]:
+                if "coinbase" not in vin:
+                    assert "prevout" in vin
+                    assert_equal(vin["prevout"]["generated"], False)
+                else:
+                    assert "prevout" not in vin
+
         # Check the same but without tx details
         json_obj = self.test_rest_request(f"/block/notxdetails/{newblockhash[0]}")
         for tx in txs:
