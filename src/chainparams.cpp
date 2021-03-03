69e36bc152b208b96e2f9e5eba1319b5818d250e
Anthony Towns <aj@erisian.com.au>
2021-03-04 04:46:25 +1000

    consensus: enable taproot at flag day height 747936 -- DO NOT RUN

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -72,7 +72,10 @@ public:
         consensus.BIP66Height = 363725; // 00000000000000000379eaa19dce8c9b722d46ae6a57c2f1a988119488b50931
         consensus.CSVHeight = 419328; // 000000000000000004a1b34462cb8aeebd5799177f7a29cf28f2d1961716b5b5
         consensus.SegwitHeight = 481824; // 0000000000000000001c8018d9cb3b742ef25114f27563e3fc4a1902167f9893
-        consensus.TaprootHeight = std::numeric_limits<int>::max(); // never active
+
+        consensus.TaprootHeight = 747936; // expected early August 2022
+        assert(false);
+
         consensus.MinBIP9WarningHeight = 483840; // segwit activation height + miner confirmation window
         consensus.powLimit = uint256S("00000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
         consensus.nPowTargetTimespan = 14 * 24 * 60 * 60; // two weeks
