fa59ad5130d732027d01b8aac04b88326b81ac5b
MarcoFalke <falke.marco@gmail.com>
2021-03-04 08:40:11 +0100

    fuzz: Add missing include (test/util/setup_common.h)

--- src/test/fuzz/torcontrol.cpp
+++ src/test/fuzz/torcontrol.cpp
@@ -1,10 +1,11 @@
-// Copyright (c) 2020 The Bitcoin Core developers
+// Copyright (c) 2020-2021 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include <test/fuzz/FuzzedDataProvider.h>
 #include <test/fuzz/fuzz.h>
 #include <test/fuzz/util.h>
+#include <test/util/setup_common.h>
 #include <torcontrol.h>
 
 #include <cstdint>
