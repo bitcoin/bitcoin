d4bb77d5c520a64720aedf74dbf71c988b48dde7
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-03-04 18:40:48 -0800

    Update rpcdump.cpp

--- src/wallet/rpcdump.cpp
+++ src/wallet/rpcdump.cpp
@@ -744,9 +744,7 @@ RPCHelpMan dumpwallet()
     // Make sure the results are valid at least up to the most recent block
     // the user could have gotten from another RPC command prior to now
     wallet.BlockUntilSyncedToCurrentChain();
-    // SYSCOIN
-    LOCK2(wallet.cs_wallet, cs_main);
-    LOCK(spk_man.cs_KeyStore);
+    LOCK2(wallet.cs_wallet, spk_man.cs_KeyStore);
 
     EnsureWalletIsUnlocked(&wallet);
 
