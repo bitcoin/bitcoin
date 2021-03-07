e23a0a11873a8df725a8e273e23f16488e64aed0
MarcoFalke <falke.marco@gmail.com>
2021-03-07 18:56:13 +0100

    Merge bitcoin-core/gui#221: qt, refactor: rpcconsole translatable str…

--- src/qt/rpcconsole.h
+++ src/qt/rpcconsole.h
@@ -145,6 +145,11 @@ Q_SIGNALS:
     /** SYSCOIN Get restart command-line parameters and handle restart */
     void handleRestart(const QStringList &args);
 private:
+    struct TranslatedStrings {
+        const QString yes{tr("Yes")}, no{tr("No")}, to{tr("To")}, from{tr("From")},
+            ban_for{tr("Ban for")}, na{tr("N/A")}, unknown{tr("Unknown")};
+    } const ts;
+
     void startExecutor();
     void setTrafficGraphRange(int mins);
     // SYSCOIN
