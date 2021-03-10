52f0be3a933225429b9b59b58de4fe54c2b20a02
fanquake <fanquake@gmail.com>
2021-03-10 14:31:10 +0800

    compat: remove memcpy -> memmove backwards compatibility alias
    In glib 2.13 memcpy was changed such that the way it copied bytes was reversed.
    This caused all sorts of issues for existing software, which depended on the
    existing behavior (when they should have been using memmove). See:
    https://sourceware.org/bugzilla/show_bug.cgi?id=12518
    https://bugzilla.redhat.com/show_bug.cgi?id=638477
    
    Now that we require glibc 2.17+ (#17538), we should be well clear of having to
    maintain our memcpy ->  memmove aliasing, which was introduced in #4339.

--- src/init.cpp
+++ src/init.cpp
@@ -771,7 +771,7 @@ static bool InitSanityCheck()
         return InitError(Untranslated("Elliptic curve cryptography sanity check failure. Aborting."));
     }
 
-    if (!glibc_sanity_test() || !glibcxx_sanity_test())
+    if (!glibcxx_sanity_test())
         return false;
 
     if (!Random_SanityCheck()) {
