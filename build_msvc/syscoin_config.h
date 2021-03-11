51927eeb04362602ae07ee5cc8ad66099d229e5d
Wladimir J. van der Laan <laanwj@protonmail.com>
2021-03-11 15:23:24 +0100

    Merge #21007: bitcoind: Add -daemonwait option to wait for initialization

--- build_msvc/syscoin_config.h
+++ build_msvc/syscoin_config.h
@@ -92,9 +92,9 @@
    don't. */
 #define HAVE_DECL_BSWAP_64 0
 
-/* Define to 1 if you have the declaration of `daemon', and to 0 if you don't.
+/* Define to 1 if you have the declaration of `fork', and to 0 if you don't.
    */
-#define HAVE_DECL_DAEMON 0
+#define HAVE_DECL_FORK 0
 
 /* Define to 1 if you have the declaration of `htobe16', and to 0 if you
    don't. */
@@ -131,6 +131,10 @@
 /* Define to 1 if you have the declaration of `le64toh', and to 0 if you
    don't. */
 #define HAVE_DECL_LE64TOH 0
+
+/* Define to 1 if you have the declaration of `setsid', and to 0 if you don't.
+   */
+#define HAVE_DECL_SETSID 0
 
 /* Define to 1 if you have the declaration of `strerror_r', and to 0 if you
    don't. */
