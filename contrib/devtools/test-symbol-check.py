0fc0c00f7ab1a90bf673ba1a01b021d3b1fa7df0
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2021-03-13 18:29:08 +0200

    test: Drop unused get_machine function
    The removed get_machine function is no longer used since #21255.

--- contrib/devtools/test-symbol-check.py
+++ contrib/devtools/test-symbol-check.py
@@ -16,10 +16,6 @@ def call_symbol_check(cc, source, executable, options):
     os.remove(executable)
     return (p.returncode, p.stdout.rstrip())
 
-def get_machine(cc):
-    p = subprocess.run([cc,'-dumpmachine'], stdout=subprocess.PIPE, universal_newlines=True)
-    return p.stdout.rstrip()
-
 class TestSymbolChecks(unittest.TestCase):
     def test_ELF(self):
         source = 'test1.c'
