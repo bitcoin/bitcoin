841eabc325f1d3949e8d1135c4dc063d92e6a182
Jon Atack <jon@atack.com>
2021-03-14 18:58:32 +0100

    test: add -daemonwait config arg tests

--- test/functional/feature_daemon_config_args.py
+++ test/functional/feature_daemon_config_args.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2021 The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
-"""Test -daemon config arg options."""
+"""Test -daemon and -daemonwait config arg options."""
 
 from test_framework.test_framework import BitcoinTestFramework
 
@@ -11,7 +11,7 @@ class DaemonConfigArgsTest(BitcoinTestFramework):
     def set_test_params(self):
         self.setup_clean_chain = True
         self.usecli = True
-        self.num_nodes = 3
+        self.num_nodes = 5
 
     def test_daemon(self):
         self.log.info('Test -daemon config arg options')
@@ -42,8 +42,38 @@ class DaemonConfigArgsTest(BitcoinTestFramework):
             self.nodes[2].start(extra_args=["-daemon=1"])
         self.nodes[2].wait_until_stopped(timeout=200)
 
+    def test_daemonwait(self):
+        self.log.info('Test -daemonwait config arg options')
+        self.stop_nodes()
+
+        self.log.debug('Test -daemonwait=0')
+        with self.nodes[0].assert_debug_log(expected_msgs=['Command-line arg: daemonwait="0"\n']):
+            self.start_node(0, extra_args=['-daemonwait=0'])
+        self.stop_node(0)
+
+        self.log.debug('Test -nodaemonwait')
+        with self.nodes[0].assert_debug_log(expected_msgs=['Command-line arg: daemonwait=false\n']):
+            self.start_node(0, extra_args=['-nodaemonwait'])
+        self.stop_node(0)
+
+        self.log.debug('Test -nodaemonwait=1')
+        with self.nodes[0].assert_debug_log(expected_msgs=['Command-line arg: daemonwait=false\n']):
+            self.start_node(0, extra_args=['-nodaemonwait=1'])
+        self.stop_node(0)
+
+        self.log.debug('Test -daemonwait')
+        with self.nodes[3].assert_debug_log(expected_msgs=['Command-line arg: daemonwait=""\n']):
+            self.nodes[3].start(extra_args=["-daemonwait"])
+        self.nodes[3].wait_until_stopped(timeout=200)
+
+        self.log.debug('Test -daemonwait="1"')
+        with self.nodes[4].assert_debug_log(expected_msgs=['Command-line arg: daemonwait="1"\n']):
+            self.nodes[4].start(extra_args=["-daemonwait=1"])
+        self.nodes[4].wait_until_stopped(timeout=200)
+
     def run_test(self):
         self.test_daemon()
+        self.test_daemonwait()
 
 
 if __name__ == '__main__':
