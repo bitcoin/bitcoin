c49daf9885e86ba08acdc8332d2a34bc5951a487
Jeremy Rubin <j@rubin.io>
2021-03-17 15:29:15 -0700

    [TESTS] Increase limitancestorcount in tournament RPC test to showcase improved algorithm

--- test/functional/mempool_updatefromblock.py
+++ test/functional/mempool_updatefromblock.py
@@ -17,7 +17,7 @@ from test_framework.util import assert_equal
 class MempoolUpdateFromBlockTest(BitcoinTestFramework):
     def set_test_params(self):
         self.num_nodes = 1
-        self.extra_args = [['-limitdescendantsize=1000', '-limitancestorsize=1000']]
+        self.extra_args = [['-limitdescendantsize=1000', '-limitancestorsize=1000', '-limitancestorcount=100']]
 
     def skip_test_if_missing_module(self):
         self.skip_if_no_wallet()
