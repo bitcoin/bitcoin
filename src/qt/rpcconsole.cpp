0c32b9c5273a4933bda90aa9eb9b7eace6dcaa14
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2021-03-20 05:06:32 +0200

    qt, rpc: Accept stop RPC even another command is executing
    While here, clean up the command input by calling the trimmed function
    on the input from the command prompt.

--- src/qt/rpcconsole.cpp
+++ src/qt/rpcconsole.cpp
@@ -924,7 +924,7 @@ void RPCConsole::setMempoolSize(long numberOfTxs, size_t dynUsage)
 
 void RPCConsole::on_lineEdit_returnPressed()
 {
-    QString cmd = ui->lineEdit->text();
+    QString cmd = ui->lineEdit->text().trimmed();
 
     if (cmd.isEmpty()) {
         return;
@@ -939,6 +939,13 @@ void RPCConsole::on_lineEdit_returnPressed()
         }
     } catch (const std::exception& e) {
         QMessageBox::critical(this, "Error", QString("Error: ") + QString::fromStdString(e.what()));
+        return;
+    }
+
+    // A special case allows to request shutdown even a long-running command is executed.
+    if (cmd == QLatin1String("stop")) {
+        std::string dummy;
+        RPCExecuteCommandLine(m_node, dummy, cmd.toStdString());
         return;
     }
 
