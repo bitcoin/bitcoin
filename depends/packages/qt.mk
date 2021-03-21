0ab6ab79e4d6d63348d8d6094736073fe20e3163
Kittywhiskers Van Gogh <63189531+kittywhiskers@users.noreply.github.com>
2021-03-21 21:18:53 +0200

    merge bitcoin#21495: Fix static builds on macOS Big Sur

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -10,6 +10,7 @@ $(package)_patches=fix_qt_pkgconfig.patch mac-qmake.conf fix_no_printer.patch no
 $(package)_patches+= fix_android_qmake_conf.patch fix_android_jni_static.patch dont_hardcode_pwd.patch
 $(package)_patches+= drop_lrelease_dependency.patch no_sdk_version_check.patch
 $(package)_patches+= fix_qpainter_non_determinism.patch fix_lib_paths.patch fix_android_pch.patch
+$(package)_patches+= fix_bigsur_drawing.patch
 $(package)_patches+= fix_limits_header.patch
 
 # Update OSX_QT_TRANSLATIONS when this is updated
@@ -221,6 +222,7 @@ define $(package)_preprocess_cmds
   patch -p1 -i $($(package)_patch_dir)/fix_qpainter_non_determinism.patch &&\
   patch -p1 -i $($(package)_patch_dir)/no_sdk_version_check.patch && \
   patch -p1 -i $($(package)_patch_dir)/fix_lib_paths.patch && \
+  patch -p1 -i $($(package)_patch_dir)/fix_bigsur_drawing.patch && \
   patch -p1 -i $($(package)_patch_dir)/fix_limits_header.patch && \
   sed -i.old "s|updateqm.commands = \$$$$\$$$$LRELEASE|updateqm.commands = $($(package)_extract_dir)/qttools/bin/lrelease|" qttranslations/translations/translations.pro && \
   mkdir -p qtbase/mkspecs/macx-clang-linux &&\
