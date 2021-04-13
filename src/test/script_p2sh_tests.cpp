deebd929f6d3f9f7a4f834b3a3eb5e1532c48d6a
fanquake <fanquake@gmail.com>
2021-04-13 09:03:31 +0800

    Merge #21330: Deal with missing data in signature hashes more consistently
    725d7ae0494d4a45f5a840bbbd19c008a7363965 Use PrecomputedTransactionData in signet check (Pieter Wuille)
    497718b467330b2c6bb0d44786020c55f1aa75f9 Treat amount<0 also as missing data for P2WPKH/P2WSH (Pieter Wuille)
    3820090bd619ac85ab35eff376c03136fe4a9f04 Make all SignatureChecker explicit about missing data (Pieter Wuille)
    b77b0cc507bdc716e5236b1d9880e648147e0af9 Add MissingDataBehavior and make TransactionSignatureChecker handle it (Pieter Wuille)
    
    Pull request description:
    
      Currently we have 2 levels of potentially-missing data in the transaction signature hashes:
      * P2WPKH/P2WSH hashes need the spent amount
      * P2TR hashes need all spent outputs (amount + scriptPubKey)
    
      Missing amounts are treated as -1 (thus leading to unexpected signature failures), while missing outputs in P2TR validation cause assertion failure. This is hard to extend for signing support, and also quite ugly in general.
    
      In this PR, an explicit configuration option to {Mutable,}TransactionSignatureChecker is added (MissingDataBehavior enum class) to either select ASSERT_FAIL or FAIL. Validation code passes ASSERT_FAIL (as at validation time all data should always be passed, and anything else is a serious bug in the code), while signing code uses FAIL.
    
      The existence of the ASSERT_FAIL option is really just an abundance of caution. Always using FAIL should be just fine, but if there were for some reason a code path in consensus code was introduced that misses certain data, I think we prefer as assertion failure over silently introducing a consensus change.
    
      Potentially useful follow-ups (not for this PR, in my preference):
      * Having an explicit script validation error code for missing data.
      * Having a MissingDataBehavior::SUCCEED option as well, for use in script/sign.cpp DataFromTransaction (if a signature is present in a witness, and we don't have enough data to fully validate it, we should probably treat it as valid and not touch it).
    
    ACKs for top commit:
      sanket1729:
        reACK 725d7ae0494d4a45f5a840bbbd19c008a7363965
      Sjors:
        ACK 725d7ae0494d4a45f5a840bbbd19c008a7363965
      achow101:
        re-ACK 725d7ae0494d4a45f5a840bbbd19c008a7363965
      benthecarman:
        ACK 725d7ae0494d4a45f5a840bbbd19c008a7363965
      fjahr:
        Code review ACK 725d7ae0494d4a45f5a840bbbd19c008a7363965
    
    Tree-SHA512: d67dc51bae9ca7ef6eb9acccefd682529f397830f77d74cd305500a081ef55aede0e9fa380648c3a8dd4857aa7eeb1ab54fe808979d79db0784ac94ceb31b657

--- src/test/script_p2sh_tests.cpp
+++ src/test/script_p2sh_tests.cpp
@@ -41,7 +41,7 @@ Verify(const CScript& scriptSig, const CScript& scriptPubKey, bool fStrict, Scri
     txTo.vin[0].scriptSig = scriptSig;
     txTo.vout[0].nValue = 1;
 
-    return VerifyScript(scriptSig, scriptPubKey, nullptr, fStrict ? SCRIPT_VERIFY_P2SH : SCRIPT_VERIFY_NONE, MutableTransactionSignatureChecker(&txTo, 0, txFrom.vout[0].nValue), &err);
+    return VerifyScript(scriptSig, scriptPubKey, nullptr, fStrict ? SCRIPT_VERIFY_P2SH : SCRIPT_VERIFY_NONE, MutableTransactionSignatureChecker(&txTo, 0, txFrom.vout[0].nValue, MissingDataBehavior::ASSERT_FAIL), &err);
 }
 
 
