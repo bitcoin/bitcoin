4e05b3cbe8ea97d99140abd9632e25ddafbed68a
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-04-13 09:59:15 -0700

    fix chia build

--- src/chiabls/build/.configure-custom.sh
+++ src/chiabls/build/.configure-custom.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-ENABLE_VARS="natpmp-default suppress-external-warnings experimental module-schnorrsig module-recovery benchmark glibc-back-compat reduce-exports zmq c++17 upnp-default extended-functional-tests fuzz lcov lcov-branch-coverage threadlocal debug gprof determinism werror"
+ENABLE_VARS="natpmp-default enable-external-signer suppress-external-warnings experimental module-schnorrsig module-recovery benchmark glibc-back-compat reduce-exports zmq c++17 upnp-default extended-functional-tests fuzz lcov lcov-branch-coverage threadlocal debug gprof determinism werror"
 DISABLE_VARS="cache-file option-checking shared ccache maintainer-mode dependency-tracking bench gui-tests wallet tests hardening asm zmq man"
-WITH_VARS="natpmp incompatible-bdb pic bignum gui boost-process miniupnpc qrencode system-univalue libmultiprocess mpgen multiprocess sanitizers"
+WITH_VARS="natpmp incompatible-bdb pic bignum gui miniupnpc qrencode system-univalue libmultiprocess mpgen multiprocess sanitizers"
