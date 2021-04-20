e839d06b86978deb5126cd61d10ac1f0dd511690
David <widecoin.official@gmail.com>
2021-04-20 13:42:57 +0700

    Updated block reward

--- src/version.h
+++ src/version.h
@@ -9,7 +9,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70017;
+static const int PROTOCOL_VERSION = 70016;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
