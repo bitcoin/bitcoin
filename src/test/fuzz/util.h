f8bd13f85ae5404adef23a52719d804a5c36b1e8
Vasil Dimov <vd@FreeBSD.org>
2021-04-23 09:43:43 +0200

    net: add new method Sock::Accept() that wraps accept()
    This will help to increase `Sock` usage and make more code mockable.

--- src/test/fuzz/util.h
+++ src/test/fuzz/util.h
@@ -410,6 +410,8 @@ public:
 
     int Connect(const sockaddr*, socklen_t) const override;
 
+    std::unique_ptr<Sock> Accept(sockaddr* addr, socklen_t* addr_len) const override;
+
     int GetSockOpt(int level, int opt_name, void* opt_val, socklen_t* opt_len) const override;
 
     bool Wait(std::chrono::milliseconds timeout, Event requested, Event* occurred = nullptr) const override;
