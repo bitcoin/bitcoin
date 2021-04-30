80d9dfd0f0d75d996552414c723e5b4a5cec20c8
Vasil Dimov <vd@FreeBSD.org>
2021-04-30 17:07:35 +0200

    fuzz: add CConnman::SocketHandler() to the tests

--- src/test/fuzz/connman.cpp
+++ src/test/fuzz/connman.cpp
@@ -189,6 +189,9 @@ FUZZ_TARGET(connman, .init = initialize_connman)
                                       options.onion_binds.empty();
 
                 connman.InitBindsPublic(options);
+            },
+            [&] {
+                connman.SocketHandlerPublic();
             });
     }
     (void)connman.GetAddedNodeInfo();
--- src/test/util/net.h
+++ src/test/util/net.h
@@ -55,6 +55,11 @@ struct ConnmanTestMsg : public CConnman {
         return InitBinds(options);
     }
 
+    void SocketHandlerPublic()
+    {
+        SocketHandler();
+    }
+
     void Handshake(CNode& node,
                    bool successfully_connected,
                    ServiceFlags remote_services,
