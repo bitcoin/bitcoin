a9b2a215e0bed69054f96565f20ae83ee93f2c17
Gapcoin1 <68176698+Gapcoin1@users.noreply.github.com>
2021-05-02 17:42:36 +0000

    Update hmac_sha512.cpp
    Upgrading keys to 256 bits will protect the HMAC up to 128 bits of Quantum Resistance.
    
    The HMAC is the message bus for the Bitcoin Protocol it can easily be upgraded without any issues for the Blockchain.  
    
    The HMAC helps prevent Man-in-the-Middle attacks.  And on the fly attacks against live transactions and the blockchain itself!

--- src/crypto/hmac_sha512.cpp
+++ src/crypto/hmac_sha512.cpp
@@ -8,22 +8,22 @@
 
 CHMAC_SHA512::CHMAC_SHA512(const unsigned char* key, size_t keylen)
 {
-    unsigned char rkey[128];
-    if (keylen <= 128) {
+    unsigned char rkey[256];
+    if (keylen <= 256) {
         memcpy(rkey, key, keylen);
-        memset(rkey + keylen, 0, 128 - keylen);
+        memset(rkey + keylen, 0, 256 - keylen);
     } else {
         CSHA512().Write(key, keylen).Finalize(rkey);
         memset(rkey + 64, 0, 64);
     }
 
-    for (int n = 0; n < 128; n++)
+    for (int n = 0; n < 256; n++)
         rkey[n] ^= 0x5c;
-    outer.Write(rkey, 128);
+    outer.Write(rkey, 256);
 
-    for (int n = 0; n < 128; n++)
+    for (int n = 0; n < 256; n++)
         rkey[n] ^= 0x5c ^ 0x36;
-    inner.Write(rkey, 128);
+    inner.Write(rkey, 256);
 }
 
 void CHMAC_SHA512::Finalize(unsigned char hash[OUTPUT_SIZE])
