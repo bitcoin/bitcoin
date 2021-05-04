9859b9a4c9c11932d7fefa716d0baf779d68e1c1
fanquake <fanquake@gmail.com>
2021-05-04 11:03:01 +0800

    build: copy updated config.guess & config.sub into build-aux
    autoreconf / libtoolize may generate versions of config.guess and config.sub
    that are too old to recognise some triplets, i.e arm64-apple-darwin20, so
    copy over the generated config.guess & config.sub with updated versions.

--- autogen.sh
+++ autogen.sh
@@ -14,3 +14,15 @@ fi
 command -v autoreconf >/dev/null || \
   (echo "configuration failed, please install autoconf first" && exit 1)
 autoreconf --install --force --warnings=all
+
+# autoreconf / libtoolize may generate versions of config.guess and config.sub
+# that are too old to recognise some triplets, i.e arm64-apple-darwin20, so
+# copy over the generated config.guess & config.sub with updated versions.
+cp depends/config.guess build-aux/config.guess
+cp depends/config.sub build-aux/config.sub
+
+cp depends/config.guess src/secp256k1/build-aux/config.guess
+cp depends/config.sub src/secp256k1/build-aux/config.sub
+
+cp depends/config.guess src/univalue/build-aux/config.guess
+cp depends/config.sub src/univalue/build-aux/config.sub
