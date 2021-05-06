8b837dc7780ef50c7a44a94734b3c1b1810ec89a
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-05-06 14:05:27 -0700

    Revert "fix test"
    This reverts commit ef5670b597bdf1643076662f9d7f63a653e3f73a.

--- src/test/amount_tests.cpp
+++ src/test/amount_tests.cpp
@@ -73,7 +73,8 @@ BOOST_AUTO_TEST_CASE(GetFeeTest)
     BOOST_CHECK(CFeeRate(CAmount(0), 0) == CFeeRate(0));
     BOOST_CHECK(CFeeRate(CAmount(1), 0) == CFeeRate(0));
     // default value
-    BOOST_CHECK(CFeeRate(CAmount(-1), 1000) == CFeeRate(-1));
+    // SYSCOIN
+    BOOST_CHECK(CFeeRate(CAmount(-1), 1000) == CFeeRate(0));
     BOOST_CHECK(CFeeRate(CAmount(0), 1000) == CFeeRate(0));
     BOOST_CHECK(CFeeRate(CAmount(1), 1000) == CFeeRate(1));
     // lost precision (can only resolve satoshis per kB)
