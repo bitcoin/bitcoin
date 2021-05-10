3bf004dc81cd9a7546dc6aed464da1bfeb678cc9
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2021-05-11 00:42:48 +0300

    Merge bitcoin-core/gui#293: Enable wordWrap for Services
    a0f797867433b0d3e8d7851ddf05743fe70d320a qt: enable wordWrap for peers-tab detail services (randymcmillan)
    
    Pull request description:
    
      Enable wordWrap for peers-tab detailView Services
    
    ACKs for top commit:
      Talkless:
        tACK a0f797867433b0d3e8d7851ddf05743fe70d320a on same environment as previously.
      hebasto:
        ACK a0f797867433b0d3e8d7851ddf05743fe70d320a, tested on Linux Mint 20.1 (Qt 5.12.8):
      kristapsk:
        re-ACK a0f797867433b0d3e8d7851ddf05743fe70d320a. Tested under Gentoo Linux with Xfce4 (Qt 5.15.2).
    
    Tree-SHA512: 872e511d2ecfa72fea0fd3284a958b45ee8aee138469ce7f9cd853cd9098b9583917909934b0a5c96f9b81ea1567bcea6a037558829bb79f2a3f413a83df06e6

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -709,7 +709,7 @@ QString formatServicesStr(quint64 mask)
     }
 
     if (strList.size())
-        return strList.join(" & ");
+        return strList.join(", ");
     else
         return QObject::tr("None");
 }
