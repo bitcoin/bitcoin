1af208318067843cc1adbc352c3332ed68ebf391
Sjors Provoost <sjors@sprovoost.nl>
2021-05-12 11:49:47 +0200

    wallet: allow external_signer flag toggle

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -55,6 +55,9 @@ const std::map<uint64_t,std::string> WALLET_FLAG_CAVEATS{
         "destinations in the past. Until this is done, some destinations may "
         "be considered unused, even if the opposite is the case."
     },
+    {WALLET_FLAG_EXTERNAL_SIGNER,
+        "The ability to toggle this flag may be removed in a future update."
+    },
 };
 
 bool AddWalletSetting(interfaces::Chain& chain, const std::string& wallet_name)
