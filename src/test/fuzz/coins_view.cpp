ec73d491f985c25f818d37325c8cf15eeaf71cd8
MarcoFalke <falke.marco@gmail.com>
2021-05-19 21:24:26 +0200

    Merge #21970: fuzz: Add missing CheckTransaction before CheckTxInputs

--- src/test/fuzz/coins_view.cpp
+++ src/test/fuzz/coins_view.cpp
@@ -6,6 +6,7 @@
 #include <chainparams.h>
 #include <chainparamsbase.h>
 #include <coins.h>
+#include <consensus/tx_check.h>
 #include <consensus/tx_verify.h>
 #include <consensus/validation.h>
 #include <key.h>
@@ -230,10 +231,15 @@ FUZZ_TARGET_INIT(coins_view, initialize_coins_view)
                     // consensus/tx_verify.cpp:171: bool Consensus::CheckTxInputs(const CTransaction &, TxValidationState &, const CCoinsViewCache &, int, CAmount &): Assertion `!coin.IsSpent()' failed.
                     return;
                 }
-                    // SYSCOIN
-                    CAssetsMap mapAssetIn;
-                    CAssetsMap mapAssetOut;
-                    (void)Consensus::CheckTxInputs(transaction, state, coins_view_cache, fuzzed_data_provider.ConsumeIntegralInRange<int>(0, std::numeric_limits<int>::max()), tx_fee_out, mapAssetIn, mapAssetOut);
+                TxValidationState dummy;
+                if (!CheckTransaction(transaction, dummy)) {
+                    // It is not allowed to call CheckTxInputs if CheckTransaction failed
+                    return;
+                }
+                // SYSCOIN
+                CAssetsMap mapAssetIn;
+                CAssetsMap mapAssetOut;
+                (void)Consensus::CheckTxInputs(transaction, state, coins_view_cache, fuzzed_data_provider.ConsumeIntegralInRange<int>(0, std::numeric_limits<int>::max()), tx_fee_out, mapAssetIn, mapAssetOut);
                 assert(MoneyRange(tx_fee_out));
             },
             [&] {
