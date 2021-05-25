4f4bda0557c25c9c192581d60908b2fecb332304
dustinface <35775977+xdustinface@users.noreply.github.com>
2021-05-25 04:09:48 +0200

    depends: Undefine `BLSALLOC_SODIUM` in `bls-dash.mk` (#4176)

--- depends/packages/bls-dash.mk
+++ depends/packages/bls-dash.mk
@@ -48,6 +48,8 @@ define $(package)_set_vars
     $(package)_config_opts_darwin+= -DCMAKE_AR="$(host_prefix)/native/bin/$($(package)_ar)"
     $(package)_config_opts_darwin+= -DCMAKE_RANLIB="$(host_prefix)/native/bin/$($(package)_ranlib)"
   endif
+
+  $(package)_cppflags+=-UBLSALLOC_SODIUM
 endef
 
 define $(package)_preprocess_cmds
