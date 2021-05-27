4279caaccefde6bddf3c03e710b10fd8a2dbd92d
bram <bram.darras@pandora.be>
2021-05-27 23:04:35 +0200

    Update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Copyright (c) 2013-2019 The Bitcoin Core developers
+# Copyright (c) 2013-2019 The Bitcoin Core developers bram darras
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
