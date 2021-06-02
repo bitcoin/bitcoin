85296954d309a9b1b333113fa205510003cb56e8
strophy <32928115+strophy@users.noreply.github.com>
2021-06-02 19:54:57 +0200

    feat: add tor entrypoint script for use in dashmate (#4182)
    * feat: add entrypoint for tor features in dashmate
    
    * fix: use spaces consistently for indendation
    
    * fix: output blank line after info
    
    * fix: ensure script is executable
    
    * fix: keep linter happy

---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export LC_ALL=C
+set -e
+
+# Get Tor service IP if running
+if [[ "$1" == "dashd" ]]; then
+  # Because dashd only accept torcontrol= host as an ip only, we resolve it here and add to config
+  if [[ "$TOR_CONTROL_HOST" ]] && [[ "$TOR_CONTROL_PORT" ]] && [[ "$TOR_PROXY_PORT" ]]; then
+    TOR_IP=$(getent hosts $TOR_CONTROL_HOST | cut -d ' ' -f 1)
+    echo "proxy=$TOR_IP:$TOR_PROXY_PORT" >> "$HOME/.dashcore/dash.conf"
+    echo "Added "proxy=$TOR_IP:$TOR_PROXY_PORT" to $HOME/.dashcore/dash.conf"
+    echo "torcontrol=$TOR_IP:$TOR_CONTROL_PORT" >> "$HOME/.dashcore/dash.conf"
+    echo "Added "torcontrol=$TOR_IP:$TOR_CONTROL_PORT" to $HOME/.dashcore/dash.conf"
+    echo -e "\n"
+  else
+    echo "Tor control credentials not provided"
+  fi
+fi
+
+exec "$@"
