c6f59c2ec2aee4aaa3a7bbb9b329825e3c12969f
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-06-03 20:18:22 -0700

    Update script_standard_tests.cpp

--- src/test/script_standard_tests.cpp
+++ src/test/script_standard_tests.cpp
@@ -444,7 +444,8 @@ BOOST_AUTO_TEST_CASE(script_standard_taproot_builder)
     BOOST_CHECK(builder.IsValid() && builder.IsComplete());
     builder.Finalize(key_inner);
     BOOST_CHECK(builder.IsValid() && builder.IsComplete());
-    BOOST_CHECK_EQUAL(EncodeDestination(builder.GetOutput()), "bc1pj6gaw944fy0xpmzzu45ugqde4rz7mqj5kj0tg8kmr5f0pjq8vnaqgynnge");
+    // SYSCOIN
+    BOOST_CHECK_EQUAL(EncodeDestination(builder.GetOutput()), "sys1pj6gaw944fy0xpmzzu45ugqde4rz7mqj5kj0tg8kmr5f0pjq8vnaqjvkpnk");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
