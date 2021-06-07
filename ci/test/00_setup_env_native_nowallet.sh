6d5e4aa5147847ca0ec868e3f2733bae3f7a13b4
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-06-06 18:57:28 -0700

    focal fixes

--- ci/test/00_setup_env_native_nowallet.sh
+++ ci/test/00_setup_env_native_nowallet.sh
@@ -7,8 +7,8 @@
 export LC_ALL=C.UTF-8
 
 export CONTAINER_NAME=ci_native_nowallet
-export DOCKER_NAME_TAG=ubuntu:18.04  # Use bionic to have one config run the tests in python3.6, see doc/dependencies.md
-export PACKAGES="python3-zmq clang-5.0 llvm-5.0"  # Use clang-5 to test C++17 compatibility, see doc/dependencies.md
+export DOCKER_NAME_TAG=ubuntu:20.04  # Use focal to have one config run the tests in python3.6, see doc/dependencies.md
+export PACKAGES="python3-zmq clang-7.0 llvm-7.0"  # Use clang-7 to test C++17 compatibility, see doc/dependencies.md
 export DEP_OPTS="NO_WALLET=1"
 export GOAL="install"
-export SYSCOIN_CONFIG="--enable-glibc-back-compat --enable-reduce-exports CC=clang-5.0 CXX=clang++-5.0 --enable-external-signer"
+export SYSCOIN_CONFIG="--enable-glibc-back-compat --enable-reduce-exports CC=clang-7.0 CXX=clang++-7.0 --enable-external-signer"
