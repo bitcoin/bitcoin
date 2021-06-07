3ae503c95bccc4899fc881c603d618a824078e7b
fanquake <fanquake@gmail.com>
2021-06-07 14:35:28 +0800

    refactor: replace QDateTime::toTime_t with QDateTime::toSecsSinceEpoch

--- src/qt/recentrequeststablemodel.h
+++ src/qt/recentrequeststablemodel.h
@@ -7,6 +7,8 @@
 
 #include <qt/sendcoinsrecipient.h>
 
+#include <string>
+
 #include <QAbstractTableModel>
 #include <QStringList>
 #include <QDateTime>
@@ -26,7 +28,7 @@ public:
 
     SERIALIZE_METHODS(RecentRequestEntry, obj) {
         unsigned int date_timet;
-        SER_WRITE(obj, date_timet = obj.date.toTime_t());
+        SER_WRITE(obj, date_timet = obj.date.toSecsSinceEpoch());
         READWRITE(obj.nVersion, obj.id, date_timet, obj.recipient);
         SER_READ(obj, obj.date = QDateTime::fromSecsSinceEpoch(date_timet));
     }
