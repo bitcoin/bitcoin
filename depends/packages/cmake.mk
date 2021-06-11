3f7cd27e16f86437bb20a73fb9b63ab57d75d3c3
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-06-11 13:43:20 -0700

    Update cmake.mk

--- depends/packages/cmake.mk
+++ depends/packages/cmake.mk
@@ -4,7 +4,17 @@ $(package)_download_path=https://cmake.org/files/v3.14/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=9221993e0af3e6d10124d840ff24f5b2f3b884416fca04d3312cb0388dec1385
 
+define $(package)_set_vars
+  $(package)_build_opts=CC="$($(package)_cc)"
+  $(package)_build_env+=CFLAGS="$($(package)_cflags) $($(package)_cppflags)" AR="$($(package)_ar)"
+endef
+
 define $(package)_config_cmds
+  export CC="$($(package)_cc)" && \
+  export CXX="$($(package)_cxx)" && \
+  export CFLAGS="$($(package)_cflags) $($(package)_cppflags)" && \
+  export CXXFLAGS="$($(package)_cxxflags) $($(package)_cppflags)" && \
+  export LDFLAGS="$($(package)_ldflags)" && \
   ./bootstrap --prefix=$(host_prefix)
 endef
 
