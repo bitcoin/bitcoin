2452364607a204055f4b3e9674830b82c7f42094
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-06-11 14:01:14 -0700

    use env var to pass in CC

--- depends/packages/cmake.mk
+++ depends/packages/cmake.mk
@@ -5,12 +5,14 @@ $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=9221993e0af3e6d10124d840ff24f5b2f3b884416fca04d3312cb0388dec1385
 
 define $(package)_set_vars
-  $(package)_build_opts=CC="$($(package)_cc)"
-  $(package)_build_env+=CFLAGS="$($(package)_cflags) $($(package)_cppflags)" AR="$($(package)_ar)"
+  $(package)_config_opts=CC="$($(package)_cc)"
+  $(package)_config_opts+=CFLAGS="$($(package)_cflags) $($(package)_cppflags)" AR="$($(package)_ar)"
 endef
 
+# cmake has its own custom configure script that takes in options like CC,
+# CFLAGS from environment
 define $(package)_config_cmds
-  ./bootstrap --prefix=$(host_prefix)
+  env $($(package)_config_opts) ./bootstrap --prefix=$(host_prefix)
 endef
 
 define $(package)_build_cmds
