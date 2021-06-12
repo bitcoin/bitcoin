21153d7a5ddd202a4d833b7d3c7d1ef2ba05cf26
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-06-11 18:03:23 -0700

    try to fix arm cross compile

--- depends/packages/cmake.mk
+++ depends/packages/cmake.mk
@@ -4,13 +4,8 @@ $(package)_download_path=https://cmake.org/files/v3.14/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=9221993e0af3e6d10124d840ff24f5b2f3b884416fca04d3312cb0388dec1385
 
-define $(package)_set_vars
-  $(package)_config_opts=CC="$($(package)_cc)"
-  $(package)_config_opts+=CFLAGS="$($(package)_cflags) $($(package)_cppflags)"
-endef
-
 define $(package)_config_cmds
-  env $($(package)_config_opts) ./bootstrap --prefix=$(host_prefix)
+  ./bootstrap --prefix=$(host_prefix)
 endef
 
 define $(package)_build_cmds
