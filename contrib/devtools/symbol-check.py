a33381acf5ae2b43616fffaf26b1c8962e8ef0bb
W. J. van der Laan <laanwj@protonmail.com>
2021-06-14 20:28:26 +0200

    devtools: Add xkb version to symbol-check
    xkb versions symbols (using the prefix `V`), as this library is used by
    bitcoin-qt, add it to the valid versions in `symbol-check.py`.

--- contrib/devtools/symbol-check.py
+++ contrib/devtools/symbol-check.py
@@ -42,7 +42,8 @@ import pixie
 MAX_VERSIONS = {
 'GCC':       (4,8,0),
 'GLIBC':     (2,17),
-'LIBATOMIC': (1,0)
+'LIBATOMIC': (1,0),
+'V':         (0,5,0),  # xkb (bitcoin-qt only)
 }
 # See here for a description of _IO_stdin_used:
 # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=634261#109
