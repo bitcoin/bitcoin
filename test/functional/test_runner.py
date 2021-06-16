ead5f6da52166772c75cfd8044bb644aa8e7e53f
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-06-16 16:31:54 -0700

    add disabled test back

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -247,7 +247,7 @@ BASE_SCRIPTS = [
     'rpc_bind.py --ipv6',
     'rpc_bind.py --nonloopback',
     'mining_basic.py',
-    # 'feature_signet.py',
+    'feature_signet.py',
     'wallet_bumpfee.py --legacy-wallet',
     'wallet_bumpfee.py --descriptors',
     'wallet_implicitsegwit.py --legacy-wallet',
