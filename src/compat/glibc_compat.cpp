9922cda0823ac0b5651094f08eb46998c1415d64
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2021-06-19 02:27:17 +0300

    build: Avoid @GLIBC_2.29 libm symbols when --enable-glibc-back-compat

--- src/compat/glibc_compat.cpp
+++ src/compat/glibc_compat.cpp
@@ -38,25 +38,61 @@ extern "C" int64_t __wrap___divmoddi4(int64_t u, int64_t v, int64_t* rp)
 }
 #endif
 
+extern "C" double exp_old(double x);
+extern "C" double log_old(double x);
 extern "C" float log2f_old(float x);
+extern "C" double pow_old(double base, double power);
 #ifdef __i386__
+__asm(".symver exp_old,exp@GLIBC_2.1");
+__asm(".symver log_old,log@GLIBC_2.1");
 __asm(".symver log2f_old,log2f@GLIBC_2.1");
+__asm(".symver pow_old,pow@GLIBC_2.1");
 #elif defined(__amd64__)
+__asm(".symver exp_old,exp@GLIBC_2.2.5");
+__asm(".symver log_old,log@GLIBC_2.2.5");
 __asm(".symver log2f_old,log2f@GLIBC_2.2.5");
+__asm(".symver pow_old,pow@GLIBC_2.2.5");
 #elif defined(__arm__)
+__asm(".symver exp_old,exp@GLIBC_2.4");
+__asm(".symver log_old,log@GLIBC_2.4");
 __asm(".symver log2f_old,log2f@GLIBC_2.4");
+__asm(".symver pow_old,pow@GLIBC_2.4");
 #elif defined(__aarch64__)
+__asm(".symver exp_old,exp@GLIBC_2.17");
+__asm(".symver log_old,log@GLIBC_2.17");
 __asm(".symver log2f_old,log2f@GLIBC_2.17");
+__asm(".symver pow_old,pow@GLIBC_2.17");
 #elif defined(__powerpc64__)
 #  ifdef WORDS_BIGENDIAN
+__asm(".symver exp_old,exp@GLIBC_2.3");
+__asm(".symver log_old,log@GLIBC_2.3");
 __asm(".symver log2f_old,log2f@GLIBC_2.3");
+__asm(".symver pow_old,pow@GLIBC_2.3");
 #  else
+__asm(".symver exp_old,exp@GLIBC_2.17");
+__asm(".symver log_old,log@GLIBC_2.17");
 __asm(".symver log2f_old,log2f@GLIBC_2.17");
+__asm(".symver pow_old,pow@GLIBC_2.17");
 #  endif
 #elif defined(__riscv)
+__asm(".symver exp_old,exp@GLIBC_2.27");
+__asm(".symver log_old,log@GLIBC_2.27");
 __asm(".symver log2f_old,log2f@GLIBC_2.27");
+__asm(".symver pow_old,pow@GLIBC_2.27");
 #endif
+extern "C" double __wrap_exp(double x)
+{
+    return exp_old(x);
+}
+extern "C" double __wrap_log(double x)
+{
+    return log_old(x);
+}
 extern "C" float __wrap_log2f(float x)
 {
     return log2f_old(x);
+}
+extern "C" double __wrap_pow(double base, double power)
+{
+    return pow_old(base, power);
 }
