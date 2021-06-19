743e390070113c8198f0b3464e010e4084cfceb6
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-06-18 20:03:51 -0700

    Update 00_setup_env_native_multiprocess.sh

--- ci/test/00_setup_env_native_multiprocess.sh
+++ ci/test/00_setup_env_native_multiprocess.sh
@@ -8,8 +8,8 @@ export LC_ALL=C.UTF-8
 
 export CONTAINER_NAME=ci_native_multiprocess
 export DOCKER_NAME_TAG=ubuntu:20.04
-export PACKAGES="cmake python3 python3-pip"
-export DEP_OPTS="MULTIPROCESS=1"
+export PACKAGES="cmake python3 python3-pip llvm clang"
+export DEP_OPTS="DEBUG=1 MULTIPROCESS=1"
 export GOAL="install"
 export SYSCOIN_CONFIG="--enable-debug CC=clang CXX=clang++"  # Use clang to avoid OOM
 export TEST_RUNNER_ENV="SYSCOIND=syscoin-node"
