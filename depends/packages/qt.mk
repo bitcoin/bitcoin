35aab4f0c0bf0d1acdcea60a18622ac1df154113
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2021-06-23 04:37:30 +0300

    build, qt: Avoid getentropy@GLIBC_2.25 symbol

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -136,6 +136,7 @@ $(package)_config_opts_linux += -fontconfig
 $(package)_config_opts_linux += -no-opengl
 $(package)_config_opts_linux += -no-feature-vulkan
 $(package)_config_opts_linux += -dbus-runtime
+$(package)_config_opts_linux += -no-feature-getentropy
 $(package)_config_opts_arm_linux += -platform linux-g++ -xplatform bitcoin-linux-g++
 $(package)_config_opts_i686_linux  = -xplatform linux-g++-32
 $(package)_config_opts_x86_64_linux = -xplatform linux-g++-64
