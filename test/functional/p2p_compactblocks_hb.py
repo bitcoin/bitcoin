8092b204f226614641b6e59745b81ca7cb90722b
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-06-23 18:13:16 -0700

    Update p2p_compactblocks_hb.py

--- test/functional/p2p_compactblocks_hb.py
+++ test/functional/p2p_compactblocks_hb.py
@@ -4,11 +4,11 @@
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 """Test compact blocks HB selection logic."""
 
-from test_framework.test_framework import BitcoinTestFramework
+from test_framework.test_framework import SyscoinTestFramework
 from test_framework.util import assert_equal
 
 
-class CompactBlocksConnectionTest(BitcoinTestFramework):
+class CompactBlocksConnectionTest(SyscoinTestFramework):
     """Test class for verifying selection of HB peer connections."""
 
     def set_test_params(self):
