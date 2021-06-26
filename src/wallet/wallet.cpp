4c6fe7b709d215b4f449bd8daaed0692126d6f10
n0feer <76183328+n0feer@users.noreply.github.com>
2021-06-26 21:08:23 +0300

    'internal' loop's variable hides a function's parameter of the same name

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -3206,11 +3206,11 @@ ScriptPubKeyMan* CWallet::AddWalletDescriptor(WalletDescriptor& desc, const Flat
 
         // Remove from maps of active spkMans
         auto old_spk_man_id = old_spk_man->GetID();
-        for (bool internal : {false, true}) {
+        for (bool loopInternal : {false, true}) {
             for (OutputType t : OUTPUT_TYPES) {
-                auto active_spk_man = GetScriptPubKeyMan(t, internal);
+                auto active_spk_man = GetScriptPubKeyMan(t, loopInternal);
                 if (active_spk_man && active_spk_man->GetID() == old_spk_man_id) {
-                    if (internal) {
+                    if (loopInternal) {
                         m_internal_spk_managers.erase(t);
                     } else {
                         m_external_spk_managers.erase(t);
