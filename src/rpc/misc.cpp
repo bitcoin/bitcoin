e480af6868cfb00b59ffe3d41211bafa1741c5bc
Luke Dashjr <luke-jr+git@utopios.org>
2021-06-26 18:27:52 +0000

    RPC/Misc: Include <bech32.h> to avoid conflicts later

--- src/rpc/misc.cpp
+++ src/rpc/misc.cpp
@@ -3,6 +3,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#include <bech32.h>
 #include <httpserver.h>
 #include <index/blockfilterindex.h>
 #include <index/coinstatsindex.h>
