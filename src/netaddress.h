fa0dff59cd9d363222f5c6825a3850f1a3b532d3
MarcoFalke <falke.marco@gmail.com>
2021-06-30 11:50:26 +0200

    Remove unused CSubNet serialize code

--- src/netaddress.h
+++ src/netaddress.h
@@ -534,22 +534,23 @@ class CSubNet
         friend bool operator!=(const CSubNet& a, const CSubNet& b) { return !(a == b); }
         friend bool operator<(const CSubNet& a, const CSubNet& b);
 
-        SERIALIZE_METHODS(CSubNet, obj)
+        template <typename Stream>
+        void Unserialize(Stream& s)
         {
-            READWRITE(obj.network);
-            if (obj.network.IsIPv4()) {
+            s >> network;
+            if (network.IsIPv4()) {
                 // Before commit 102867c587f5f7954232fb8ed8e85cda78bb4d32, CSubNet used the last 4 bytes of netmask
                 // to store the relevant bytes for an IPv4 mask. For compatibility reasons, keep doing so in
                 // serialized form.
                 unsigned char dummy[12] = {0};
-                READWRITE(dummy);
-                READWRITE(MakeSpan(obj.netmask).first(4));
+                s >> dummy;
+                s >> MakeSpan(netmask).first(4);
             } else {
-                READWRITE(obj.netmask);
+                s >> netmask;
             }
-            READWRITE(obj.valid);
+            s >> valid;
             // Mark invalid if the result doesn't pass sanity checking.
-            SER_READ(obj, if (obj.valid) obj.valid = obj.SanityCheck());
+            if (valid) valid = SanityCheck();
         }
 };
 
