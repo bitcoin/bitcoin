ba75fc7775e64f08600b3b2427f3fce4896166cb
dexX7 <dexx@bitwatch.co>
2021-07-01 14:32:19 +0200

    Bump release version to 0.11

--- src/omnicore/test/version_tests.cpp
+++ src/omnicore/test/version_tests.cpp
@@ -23,18 +23,18 @@ BOOST_AUTO_TEST_CASE(version_comparison)
 
 BOOST_AUTO_TEST_CASE(version_string)
 {
-    BOOST_CHECK_EQUAL(OmniCoreVersion(), "0.10.0");
+    BOOST_CHECK_EQUAL(OmniCoreVersion(), "0.11.0");
 }
 
 BOOST_AUTO_TEST_CASE(version_number)
 {
-    BOOST_CHECK_EQUAL(OMNICORE_VERSION, 100000000);
+    BOOST_CHECK_EQUAL(OMNICORE_VERSION, 110000000);
 }
 
 BOOST_AUTO_TEST_CASE(config_package_version)
 {
     // the package version is used in the file names:
-    BOOST_CHECK_EQUAL(PACKAGE_VERSION, "0.10.0");
+    BOOST_CHECK_EQUAL(PACKAGE_VERSION, "0.11.0");
 }
 
 
