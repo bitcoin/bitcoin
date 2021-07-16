5c31c137086f199cc17661031b0631e85ca9e054
Alex Groce <agroce@gmail.com>
2021-07-16 15:27:54 -0700

    Include boost error codes

--- src/test/fuzz/fuzz.cpp
+++ src/test/fuzz/fuzz.cpp
@@ -5,6 +5,7 @@
 #include <test/fuzz/fuzz.h>
 
 #include <boost/filesystem.hpp>
+#include <boost/system/error_code.hpp>
 #include <netaddress.h>
 #include <netbase.h>
 #include <test/util/setup_common.h>
@@ -129,9 +130,9 @@ int main(int argc, char** argv)
     test_one_input(buffer);
 #endif
     if (std::getenv("CLEAN_TMP_AFTER_FUZZ")) {
-        system::error_code ec;
+        boost::system::error_code ec;
         if (!boost::filesystem::remove_all("/tmp/test_common_Bitcoin Core", ec)) {
-            std::cout << ec << std::endl;
+            std::cout << ec.message() << std::endl;
         }
     }
     return 0;
