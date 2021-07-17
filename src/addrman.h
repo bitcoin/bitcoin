74216f0b5896099b485bf6c5436606b65ccd71be
Jon Atack <jon@atack.com>
2021-07-17 16:43:41 +0200

    p2p: log DEBUG_ADDRMAN consistency checks

--- src/addrman.h
+++ src/addrman.h
@@ -732,6 +732,7 @@ private:
     {
 #ifdef DEBUG_ADDRMAN
         AssertLockHeld(cs);
+        LogPrint(BCLog::ADDRMAN, "Check addrman\n");
         const int err = Check_();
         if (err) {
             LogPrintf("ADDRMAN CONSISTENCY CHECK FAILED!!! err=%i\n", err);
