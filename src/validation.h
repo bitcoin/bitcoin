92bfe50955d6f24dd3a88bc2a78a82deec294b98
pasta <pasta@dashboost.org>
2021-07-17 23:20:16 -0500

    partial Merge #16152: (Don't actually) Disable bloom filtering by default.
    This does not disable bloom filters, but does implement a minor refactoring
    
    Signed-off-by: pasta <pasta@dashboost.org>

--- src/validation.h
+++ src/validation.h
@@ -135,8 +135,6 @@ static const unsigned int MAX_BLOCKS_TO_ANNOUNCE = 8;
 /** Maximum number of unconnecting headers announcements before DoS score */
 static const int MAX_UNCONNECTING_HEADERS = 10;
 
-static const bool DEFAULT_PEERBLOOMFILTERS = true;
-
 /** Default for -stopatheight */
 static const int DEFAULT_STOPATHEIGHT = 0;
 
