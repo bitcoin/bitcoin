0db6f1f9528168ef5ed0ed44b829f58527aa8de7
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-07-21 11:47:51 -0700

    Update schemes.hpp

--- src/bls-dash/src/schemes.hpp
+++ src/bls-dash/src/schemes.hpp
@@ -228,6 +228,7 @@ public:
 class LegacySchemeMPL : public CoreMPL {
 
 public:
+    using CoreMPL::Aggregate; // imports both declarations from CoreMPL
     virtual ~LegacySchemeMPL() {};
     LegacySchemeMPL() : CoreMPL(std::string{}) {}
 
@@ -247,7 +248,7 @@ public:
     bool Verify(const Bytes& pubkey, const Bytes& message, const Bytes& signature) final { throw std::runtime_error("Not supported in LegacySchemeMPL"); }
     bool Verify(const G1Element &pubkey, const Bytes& message, const G2Element &signature) final;
 
-    virtual vector<uint8_t> Aggregate(const vector<vector<uint8_t>> &signatures) final { throw std::runtime_error("Not supported in LegacySchemeMPL"); }
+    vector<uint8_t> Aggregate(const vector<vector<uint8_t>> &signatures) final { throw std::runtime_error("Not supported in LegacySchemeMPL"); }
 
     G2Element AggregateSecure(const std::vector<G1Element>& vecPublicKeys,
                               const std::vector<G2Element>& vecSignatures,
