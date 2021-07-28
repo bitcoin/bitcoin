fad9d451f1e17634e07a059ee8ef5093c72066e3
MarcoFalke <falke.marco@gmail.com>
2021-07-28 13:39:38 +0200

    scripted-diff: Bump copyright headers
    The previous diff touched most files in ./test/, so bump the headers to
    avoid having to touch them again for a bump later.
    
    -BEGIN VERIFY SCRIPT-
    ./contrib/devtools/copyright_header.py update ./test/
    -END VERIFY SCRIPT-

--- test/lint/commit-script-check.sh
+++ test/lint/commit-script-check.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Copyright (c) 2017-2020 The Bitcoin Core developers
+# Copyright (c) 2017-2021 The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
