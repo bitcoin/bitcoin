fac23c211407a77af82bb1491c48c8d37022c8b3
MarcoFalke <falke.marco@gmail.com>
2021-07-28 13:57:16 +0200

    scripted-diff: Bump copyright headers
    The previous diff touched most files in ./test/, so bump the headers to
    avoid having to touch them again for a bump later.
    
    -BEGIN VERIFY SCRIPT-
    ./contrib/devtools/copyright_header.py update ./test/
    -END VERIFY SCRIPT-

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (c) 2014-2020 The Bitcoin Core developers
+# Copyright (c) 2014-2021 The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 """Run regression test suite.
