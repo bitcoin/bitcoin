c7ea623849a1172b018b9c6fb49496c2f67ef012
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-07-29 15:01:46 -0700

    fix merge

--- src/test/fuzz/multiplication_overflow.cpp
+++ src/test/fuzz/multiplication_overflow.cpp
@@ -3,7 +3,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #if defined(HAVE_CONFIG_H)
-#include <config/bitcoin-config.h>
+#include <config/syscoin-config.h>
 #endif
 
 #include <test/fuzz/FuzzedDataProvider.h>
