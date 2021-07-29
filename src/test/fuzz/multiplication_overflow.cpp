bb887e3e4d6f87102bbc0cd233156e3789439ac6
fanquake <fanquake@gmail.com>
2021-07-29 20:52:59 +0800

    Merge bitcoin/bitcoin#21882: build: Fix undefined reference to __mulodi4
    e4c8bb62e4a6873c45f42d0d2a24927cb241a0ea build: Fix undefined reference to __mulodi4 (Hennadii Stepanov)
    
    Pull request description:
    
      When compiling with clang on 32-bit systems the `__mulodi4` symbol is defined in compiler-rt only.
    
      Fixes #21294.
    
      See more:
      - https://bugs.llvm.org/show_bug.cgi?id=16404
      - https://bugs.llvm.org/show_bug.cgi?id=28629
    
    ACKs for top commit:
      MarcoFalke:
        tested-only ACK e4c8bb62e4a6873c45f42d0d2a24927cb241a0ea
      luke-jr:
        utACK e4c8bb62e4a6873c45f42d0d2a24927cb241a0ea
      fanquake:
        ACK e4c8bb62e4a6873c45f42d0d2a24927cb241a0ea - it's a bit of an awkward workaround to carry, but at-least it's contained to the fuzzers.
    
    Tree-SHA512: 93edb4ed568027702b1b9aba953ad50889b834ef97fde3cb99d1ce70076d9c00aa13f95c86b12d6f59b24fa90108d93742f920e15119901a2848fb337ab859a1

--- src/test/fuzz/multiplication_overflow.cpp
+++ src/test/fuzz/multiplication_overflow.cpp
@@ -2,6 +2,10 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#if defined(HAVE_CONFIG_H)
+#include <config/bitcoin-config.h>
+#endif
+
 #include <test/fuzz/FuzzedDataProvider.h>
 #include <test/fuzz/fuzz.h>
 #include <test/fuzz/util.h>
@@ -9,14 +13,6 @@
 #include <cstdint>
 #include <string>
 #include <vector>
-
-#if defined(__has_builtin)
-#if __has_builtin(__builtin_mul_overflow)
-#define HAVE_BUILTIN_MUL_OVERFLOW
-#endif
-#elif defined(__GNUC__)
-#define HAVE_BUILTIN_MUL_OVERFLOW
-#endif
 
 namespace {
 template <typename T>
