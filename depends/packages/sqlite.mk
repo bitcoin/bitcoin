1d2b6316d0253165594563097745c04791f6a983
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-08-08 20:19:00 -0700

    rm libcurl dep

--- depends/packages/sqlite.mk
+++ depends/packages/sqlite.mk
@@ -3,7 +3,6 @@ $(package)_version=3320100
 $(package)_download_path=https://sqlite.org/2020/
 $(package)_file_name=sqlite-autoconf-$($(package)_version).tar.gz
 $(package)_sha256_hash=486748abfb16abd8af664e3a5f03b228e5f124682b0c942e157644bf6fff7d10
-$(package)_dependencies=libcurl
 
 define $(package)_set_vars
 $(package)_config_opts=--disable-shared --disable-readline --disable-dynamic-extensions --enable-option-checking
