3d2feeb36d425cd76012db4779ba2cf0e14f4f19
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-08-09 20:35:53 -0700

    Revert "rm libcurl dep"
    This reverts commit 1d2b6316d0253165594563097745c04791f6a983.

--- depends/packages/bdb.mk
+++ depends/packages/bdb.mk
@@ -4,6 +4,7 @@ $(package)_download_path=https://download.oracle.com/berkeley-db
 $(package)_file_name=db-$($(package)_version).NC.tar.gz
 $(package)_sha256_hash=12edc0df75bf9abd7f82f821795bcee50f42cb2e5f76a6a281b85732798364ef
 $(package)_build_subdir=build_unix
+$(package)_dependencies=libcurl
 $(package)_patches=clang_cxx_11.patch
 
 define $(package)_set_vars
