4c43b7d41d11072f382f938379d21cd2e0bcbb47
fanquake <fanquake@gmail.com>
2021-08-12 16:40:34 +0800

    contrib: use hkps://keys.openpgp.org to retrieve builder keys
    hkps://hkps.pool.sks-keyservers.net is essentially no-longer functional,
    and a number of distributions and GPG tools have since switched to using
    this key server as their default.
    
    See this Debian patch for additional context:
    https://salsa.debian.org/debian/gnupg2/-/blob/debian/main/debian/patches/Use-hkps-keys.openpgp.org-as-the-default-keyserver.patch
    
    Switch to using keys.openpgp.org in the CI as well.

--- ci/lint/06_script.sh
+++ ci/lint/06_script.sh
@@ -25,7 +25,7 @@ test/lint/lint-all.sh
 
 if [ "$CIRRUS_REPO_FULL_NAME" = "bitcoin/bitcoin" ] && [ -n "$CIRRUS_CRON" ]; then
     git log --merges --before="2 days ago" -1 --format='%H' > ./contrib/verify-commits/trusted-sha512-root-commit
-    ${CI_RETRY_EXE}  gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys $(<contrib/verify-commits/trusted-keys) &&
+    ${CI_RETRY_EXE}  gpg --keyserver hkps://keys.openpgp.org --recv-keys $(<contrib/verify-commits/trusted-keys) &&
     ./contrib/verify-commits/verify-commits.py --clean-merge=2;
 fi
 
