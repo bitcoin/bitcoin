32e1424f84a30194dc5baa7108cf7d958ea0afcd
Rafael Sadowski <rafael@sizeofvoid.org>
2021-08-16 06:34:02 +0200

    Fix build with Boost 1.77.0
    BOOST_FILESYSTEM_C_STR changed to accept the path as an argument
    
    Github-Pull: bitcoin/bitcoin#22713
    Rebased-From: acb7aad27ec8a184808aa7905887e3b2c5d54e9c

--- src/wallet/test/db_tests.cpp
+++ src/wallet/test/db_tests.cpp
@@ -25,7 +25,11 @@ BOOST_AUTO_TEST_CASE(getwalletenv_file)
     std::string test_name = "test_name.dat";
     const fs::path datadir = gArgs.GetDataDirNet();
     fs::path file_path = datadir / test_name;
+#if BOOST_VERSION >= 107700
+    std::ofstream f(BOOST_FILESYSTEM_C_STR(file_path));
+#else
     std::ofstream f(file_path.BOOST_FILESYSTEM_C_STR);
+#endif // BOOST_VERSION >= 107700
     f.close();
 
     std::string filename;
