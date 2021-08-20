0220b8275f38afa16a5467f15ded317599fdd7f1
glozow <gloriajzhao@gmail.com>
2021-08-20 11:50:10 +0100

    [functional test] deprecated bip125-replaceable field

--- test/functional/rpc_deprecated.py
+++ test/functional/rpc_deprecated.py
@@ -4,12 +4,13 @@
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 """Test deprecation of RPC calls."""
 from test_framework.test_framework import BitcoinTestFramework
+from test_framework.util import assert_equal
+from test_framework.wallet import MiniWallet
 
 class DeprecatedRpcTest(BitcoinTestFramework):
     def set_test_params(self):
         self.num_nodes = 2
-        self.setup_clean_chain = True
-        self.extra_args = [[], ['-deprecatedrpc=bumpfee']]
+        self.extra_args = [[], ['-deprecatedrpc=bip125']]
 
     def run_test(self):
         # This test should be used to verify correct behaviour of deprecated
@@ -23,7 +24,19 @@ class DeprecatedRpcTest(BitcoinTestFramework):
         # assert_raises_rpc_error(-32, 'The wallet generate rpc method is deprecated', self.nodes[0].rpc.generate, 1)
         # self.nodes[1].generate(1)
 
-        self.log.info("No tested deprecated RPC methods")
+        self.log.info("Test deprecated fields for RBF signaling")
+        self.wallet = MiniWallet(self.nodes[0])
+        self.wallet.scan_blocks(start=76, num=1)
+        txid = self.wallet.send_self_transfer(from_node=self.nodes[0])["txid"]
+        self.sync_all()
+        node0_entry = self.nodes[0].getmempoolentry(txid)
+        assert "replaceable" in node0_entry
+        assert "bip125-replaceable" not in node0_entry
+        node1_entry = self.nodes[1].getmempoolentry(txid)
+        assert "replaceable" in node1_entry
+        assert "bip125-replaceable" in node1_entry
+        assert_equal(node1_entry["replaceable"], node1_entry["bip125-replaceable"])
+
 
 if __name__ == '__main__':
     DeprecatedRpcTest().main()
