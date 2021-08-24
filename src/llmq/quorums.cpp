b57970a70de0fa8121d33398ab2f782f0688fc76
UdjinM6 <UdjinM6@users.noreply.github.com>
2021-08-24 19:18:52 +0300

    Fix thread names for threads started via pools (#4349)
    `RenameThreadPool` calls `ThreadRename` which adds `dash-` prefix internally

--- src/llmq/quorums.cpp
+++ src/llmq/quorums.cpp
@@ -183,7 +183,7 @@ void CQuorumManager::Start()
     int workerCount = std::thread::hardware_concurrency() / 2;
     workerCount = std::max(std::min(1, workerCount), 4);
     workerPool.resize(workerCount);
-    RenameThreadPool(workerPool, "dash-q-mngr");
+    RenameThreadPool(workerPool, "q-mngr");
 }
 
 void CQuorumManager::Stop()
