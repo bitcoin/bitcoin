5b8e1b9fd932d04fc18728a182c7b0a658b8151c
Kittywhiskers Van Gogh <63189531+kittywhiskers@users.noreply.github.com>
2021-08-25 20:27:35 +0530

    merge #15601: Switch to python3

--- depends/packages/native_mac_alias.mk
+++ depends/packages/native_mac_alias.mk
@@ -3,7 +3,7 @@ $(package)_version=2.0.7
 $(package)_download_path=https://github.com/al45tair/mac_alias/archive/
 $(package)_file_name=v$($(package)_version).tar.gz
 $(package)_sha256_hash=6f606d3b6bccd2112aeabf1a063f5b5ece87005a5d7e97c8faca23b916e88838
-$(package)_install_libdir=$(build_prefix)/lib/python/dist-packages
+$(package)_install_libdir=$(build_prefix)/lib/python3/dist-packages
 
 define $(package)_build_cmds
     python3 setup.py build
