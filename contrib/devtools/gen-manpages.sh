c1c79f4c816fa1224ad593f2af90752160fabb09
W. J. van der Laan <laanwj@protonmail.com>
2021-08-26 14:49:26 +0200

    doc: Stop nixing `-` in manual pages
    The version replacement here is not working anyway, not just that but it
    is actively harmful as it removes all `-` from the text. So remove that
    line. See discussion in #22681.
    
    Tree-SHA512: 909c6225a50bbda1253bae65c09700a658bd36b5c91c91cea28d1df0aad5dc4adb095e0695259eca377536da34d17741ce453a5c5c50041d1163d94bbbe5355d

--- contrib/devtools/gen-manpages.sh
+++ contrib/devtools/gen-manpages.sh
@@ -47,7 +47,6 @@ $BITCOIND --version | sed -n '1!p' >> footer.h2m
 for cmd in $BITCOIND $BITCOINCLI $BITCOINTX $WALLET_TOOL $BITCOINUTIL $BITCOINQT; do
   cmdname="${cmd##*/}"
   help2man -N --version-string=${BTCVER[0]} --include=footer.h2m -o ${MANDIR}/${cmdname}.1 ${cmd}
-  sed -i "s/\\\-${BTCVER[1]}//g" ${MANDIR}/${cmdname}.1
 done
 
 rm -f footer.h2m
