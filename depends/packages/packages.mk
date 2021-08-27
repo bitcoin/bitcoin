6a367fc1987d449d72a9561aee8e980a544dc9e9
Kittywhiskers Van Gogh <63189531+kittywhiskers@users.noreply.github.com>
2021-08-28 00:42:09 +0530

    merge bitcoin#19764: Split boost into build/host packages + bump + cleanup

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -14,6 +14,8 @@ upnp_packages=miniupnpc
 
 darwin_native_packages = native_biplist native_ds_store native_mac_alias
 
+$(host_arch)_$(host_os)_native_packages += native_b2
+
 ifneq ($(build_os),darwin)
 darwin_native_packages += native_cctools native_cdrkit native_libdmg-hfsplus
 endif
