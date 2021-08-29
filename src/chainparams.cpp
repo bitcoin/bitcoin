5d5cee34273d02d889c3126d877a808eb56b3dbd
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-08-29 14:16:22 -0700

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -371,7 +371,7 @@ public:
         consensus.vchSYSXERC20Manager = ParseHex("A738a563F9ecb55e0b2245D1e9E380f0fE455ea1");
         consensus.vchTokenFreezeMethod = ParseHex("7ca654cf9212e4c3cf0164a529dd6159fc71113f867d0b09fdeb10aa65780732");
         consensus.nBridgeStartBlock = 1000;
-        consensus.nNEVMStartBlock = 545000;
+        consensus.nNEVMStartBlock = 845000;
         consensus.nUTXOAssetsBlock = 1348000;
         consensus.nUTXOAssetsBlockProvisioning = consensus.nUTXOAssetsBlock + 10000;
         consensus.DIP0003Height = 545000;
