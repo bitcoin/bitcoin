9b3836710b8160d212aacd56154938e5bb4b26b7
John Newbery <john@johnnewbery.com>
2021-08-31 13:22:36 +0100

    [build] Add netgroup.cpp|h
    These aren't used yet.

---
+++ src/netgroup.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2021 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_NETGROUP_H
+#define BITCOIN_NETGROUP_H
+
+/**
+ * Netgroup manager
+ */
+class NetGroupManager {};
+
+#endif // BITCOIN_NETGROUP_H
