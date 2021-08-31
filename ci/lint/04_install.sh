065c64a4a06bee3392000e553f508fc7644560c0
josibake <josibake@protonmail.com>
2021-08-31 15:10:37 +0200

    bump pyzmq version to 22.2.1
    mypy stubs were introduced in 21.0.1
    install 22.2.1 (latest as of this commit)

--- ci/lint/04_install.sh
+++ ci/lint/04_install.sh
@@ -16,6 +16,7 @@ ${CI_RETRY_EXE} pip3 install flake8==3.8.3
 ${CI_RETRY_EXE} pip3 install yq
 ${CI_RETRY_EXE} pip3 install mypy==0.781
 ${CI_RETRY_EXE} pip3 install vulture==2.3
+${CI_RETRY_EXE} pip3 install pyzmq==22.2.1
 
 SHELLCHECK_VERSION=v0.7.2
 curl -sL "https://github.com/koalaman/shellcheck/releases/download/${SHELLCHECK_VERSION}/shellcheck-${SHELLCHECK_VERSION}.linux.x86_64.tar.xz" | tar --xz -xf - --directory /tmp/
