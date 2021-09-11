fbd095153abc6ef0ce9e7f7dce7ca7a23242c686
Jon Atack <jon@atack.com>
2021-09-11 20:33:41 +0200

    log: add BLOCK logging category

--- src/logging.cpp
+++ src/logging.cpp
@@ -161,6 +161,7 @@ const CLogCategoryDesc LogCategories[] =
     {BCLog::IPC, "ipc"},
     {BCLog::LOCK, "lock"},
     {BCLog::UTIL, "util"},
+    {BCLog::BLOCK, "block"},
     {BCLog::ALL, "1"},
     {BCLog::ALL, "all"},
 };
