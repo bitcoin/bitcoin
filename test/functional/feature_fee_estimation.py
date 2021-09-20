30b1637727b3b929775194d975d8c89736aef373
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2021-09-20 19:22:03 +0200

    merge bitcoin#23075: Fee estimation functional test cleanups
    excludes:
    - e50213967b6d5dda9c0acc4643c8ec67f9fd7284 (we don't support RBF)
    - 60ae1161a4c415cc73f47df95598f3688e8d34df (see above)

--- test/functional/feature_fee_estimation.py
+++ test/functional/feature_fee_estimation.py
@@ -16,7 +16,6 @@ from test_framework.messages import (
 from test_framework.script import (
     CScript,
     OP_1,
-    OP_2,
     OP_DROP,
     OP_TRUE,
 )
@@ -35,16 +34,14 @@ from test_framework.util import (
 # Construct 2 trivial P2SH's and the ScriptSigs that spend them
 # So we can create many transactions without needing to spend
 # time signing.
-REDEEM_SCRIPT_1 = CScript([OP_1, OP_DROP])
-REDEEM_SCRIPT_2 = CScript([OP_2, OP_DROP])
-P2SH_1 = script_to_p2sh_script(REDEEM_SCRIPT_1)
-P2SH_2 = script_to_p2sh_script(REDEEM_SCRIPT_2)
-
-# Associated ScriptSig's to spend satisfy P2SH_1 and P2SH_2
-SCRIPT_SIG = [CScript([OP_TRUE, REDEEM_SCRIPT_1]), CScript([OP_TRUE, REDEEM_SCRIPT_2])]
+SCRIPT = CScript([OP_1, OP_DROP])
+P2SH = script_to_p2sh_script(SCRIPT)
+REDEEM_SCRIPT = CScript([OP_TRUE, SCRIPT])
 
 
-def small_txpuzzle_randfee(from_node, conflist, unconflist, amount, min_fee, fee_increment):
+def small_txpuzzle_randfee(
+    from_node, conflist, unconflist, amount, min_fee, fee_increment
+):
     """Create and send a transaction with a random fee.
 
     The transaction pays to a trivial P2SH script, and assumes that its inputs
@@ -65,54 +62,20 @@ def small_txpuzzle_randfee(from_node, conflist, unconflist, amount, min_fee, fee
     while total_in <= (amount + fee) and len(conflist) > 0:
         t = conflist.pop(0)
         total_in += t["amount"]
-        tx.vin.append(CTxIn(COutPoint(int(t["txid"], 16), t["vout"]), b""))
+        tx.vin.append(CTxIn(COutPoint(int(t["txid"], 16), t["vout"]), REDEEM_SCRIPT))
+    while total_in <= (amount + fee) and len(unconflist) > 0:
+        t = unconflist.pop(0)
+        total_in += t["amount"]
+        tx.vin.append(CTxIn(COutPoint(int(t["txid"], 16), t["vout"]), REDEEM_SCRIPT))
     if total_in <= amount + fee:
-        while total_in <= (amount + fee) and len(unconflist) > 0:
-            t = unconflist.pop(0)
-            total_in += t["amount"]
-            tx.vin.append(CTxIn(COutPoint(int(t["txid"], 16), t["vout"]), b""))
-        if total_in <= amount + fee:
-            raise RuntimeError(f"Insufficient funds: need {amount + fee}, have {total_in}")
-    tx.vout.append(CTxOut(int((total_in - amount - fee) * COIN), P2SH_1))
-    tx.vout.append(CTxOut(int(amount * COIN), P2SH_2))
-    # These transactions don't need to be signed, but we still have to insert
-    # the ScriptSig that will satisfy the ScriptPubKey.
-    for inp in tx.vin:
-        inp.scriptSig = SCRIPT_SIG[inp.prevout.n]
+        raise RuntimeError(f"Insufficient funds: need {amount + fee}, have {total_in}")
+    tx.vout.append(CTxOut(int((total_in - amount - fee) * COIN), P2SH))
+    tx.vout.append(CTxOut(int(amount * COIN), P2SH))
     txid = from_node.sendrawtransaction(hexstring=tx.serialize().hex(), maxfeerate=0)
     unconflist.append({"txid": txid, "vout": 0, "amount": total_in - amount - fee})
     unconflist.append({"txid": txid, "vout": 1, "amount": amount})
 
     return (tx.serialize().hex(), fee)
-
-
-def split_inputs(from_node, txins, txouts, initial_split=False):
-    """Generate a lot of inputs so we can generate a ton of transactions.
-
-    This function takes an input from txins, and creates and sends a transaction
-    which splits the value into 2 outputs which are appended to txouts.
-    Previously this was designed to be small inputs so they wouldn't have
-    a high coin age when the notion of priority still existed."""
-
-    prevtxout = txins.pop()
-    tx = CTransaction()
-    tx.vin.append(CTxIn(COutPoint(int(prevtxout["txid"], 16), prevtxout["vout"]), b""))
-
-    half_change = satoshi_round(prevtxout["amount"] / 2)
-    rem_change = prevtxout["amount"] - half_change - Decimal("0.00001000")
-    tx.vout.append(CTxOut(int(half_change * COIN), P2SH_1))
-    tx.vout.append(CTxOut(int(rem_change * COIN), P2SH_2))
-
-    # If this is the initial split we actually need to sign the transaction
-    # Otherwise we just need to insert the proper ScriptSig
-    if (initial_split):
-        completetx = from_node.signrawtransactionwithwallet(tx.serialize().hex())["hex"]
-    else:
-        tx.vin[0].scriptSig = SCRIPT_SIG[prevtxout["vout"]]
-        completetx = tx.serialize().hex()
-    txid = from_node.sendrawtransaction(hexstring=completetx, maxfeerate=0)
-    txouts.append({"txid": txid, "vout": 0, "amount": half_change})
-    txouts.append({"txid": txid, "vout": 1, "amount": rem_change})
 
 
 def check_raw_estimates(node, fees_seen):
@@ -125,7 +88,10 @@ def check_raw_estimates(node, fees_seen):
             assert_greater_than(feerate, 0)
 
             if feerate + delta < min(fees_seen) or feerate - delta > max(fees_seen):
-                raise AssertionError(f"Estimated fee ({feerate}) out of range ({min(fees_seen)},{max(fees_seen)})")
+                raise AssertionError(
+                    f"Estimated fee ({feerate}) out of range ({min(fees_seen)},{max(fees_seen)})"
+                )
+
 
 
 def check_smart_estimates(node, fees_seen):
@@ -134,8 +100,8 @@ def check_smart_estimates(node, fees_seen):
     delta = 1.0e-6  # account for rounding error
     last_feerate = float(max(fees_seen))
     all_smart_estimates = [node.estimatesmartfee(i) for i in range(1, 26)]
-    mempoolMinFee = node.getmempoolinfo()['mempoolminfee']
-    minRelaytxFee = node.getmempoolinfo()['minrelaytxfee']
+    mempoolMinFee = node.getmempoolinfo()["mempoolminfee"]
+    minRelaytxFee = node.getmempoolinfo()["minrelaytxfee"]
     for i, e in enumerate(all_smart_estimates):  # estimate is for i+1
         feerate = float(e["feerate"])
         assert_greater_than(feerate, 0)
@@ -143,9 +109,13 @@ def check_smart_estimates(node, fees_seen):
         assert_greater_than_or_equal(feerate, float(minRelaytxFee))
 
         if feerate + delta < min(fees_seen) or feerate - delta > max(fees_seen):
-            raise AssertionError(f"Estimated fee ({feerate}) out of range ({min(fees_seen)},{max(fees_seen)})")
+            raise AssertionError(
+                f"Estimated fee ({feerate}) out of range ({min(fees_seen)},{max(fees_seen)})"
+            )
         if feerate - delta > last_feerate:
-            raise AssertionError(f"Estimated fee ({feerate}) larger than last fee ({last_feerate}) for lower number of confirms")
+            raise AssertionError(
+                f"Estimated fee ({feerate}) larger than last fee ({last_feerate}) for lower number of confirms"
+            )
         last_feerate = feerate
 
         if i == 0:
@@ -161,12 +131,11 @@ def check_estimates(node, fees_seen):
 class EstimateFeeTest(BitcoinTestFramework):
     def set_test_params(self):
         self.num_nodes = 3
-        # mine non-standard txs (e.g. txs with "dust" outputs)
         # Force fSendTrickle to true (via whitelist)
         self.extra_args = [
-            ["-acceptnonstdtxn=1", "-whitelist=noban@127.0.0.1"],
-            ["-acceptnonstdtxn=1", "-whitelist=noban@127.0.0.1", "-blockmaxsize=17000"],
-            ["-acceptnonstdtxn=1", "-whitelist=noban@127.0.0.1", "-blockmaxsize=8000"]
+            ["-whitelist=noban@127.0.0.1"],
+            ["-whitelist=noban@127.0.0.1", "-blockmaxsize=17000"],
+            ["-whitelist=noban@127.0.0.1", "-blockmaxsize=8000"]
         ]
 
     def skip_test_if_missing_module(self):
@@ -199,11 +168,17 @@ class EstimateFeeTest(BitcoinTestFramework):
             random.shuffle(self.confutxo)
             for _ in range(random.randrange(100 - 50, 100 + 50)):
                 from_index = random.randint(1, 2)
-                (txhex, fee) = small_txpuzzle_randfee(self.nodes[from_index], self.confutxo,
-                                                      self.memutxo, Decimal("0.005"), min_fee, min_fee)
+                (txhex, fee) = small_txpuzzle_randfee(
+                    self.nodes[from_index],
+                    self.confutxo,
+                    self.memutxo,
+                    Decimal("0.005"),
+                    min_fee,
+                    min_fee,
+                )
                 tx_kbytes = (len(txhex) // 2) / 1000.0
                 self.fees_per_kb.append(float(fee) / tx_kbytes)
-            self.sync_mempools(wait=.1)
+            self.sync_mempools(wait=0.1)
             mined = mining_node.getblock(self.generate(mining_node, 1)[0], True)["tx"]
             # update which txouts are confirmed
             newmem = []
@@ -216,46 +191,49 @@ class EstimateFeeTest(BitcoinTestFramework):
 
     def initial_split(self, node):
         """Split two coinbase UTxOs into many small coins"""
-        self.txouts = []
-        self.txouts2 = []
-        # Split a coinbase into two transaction puzzle outputs
-        split_inputs(node, node.listunspent(0), self.txouts, True)
+        utxo_count = 2048
+        self.confutxo = []
+        splitted_amount = Decimal("0.04")
+        fee = Decimal("0.0007")
+        # Calculate change from UTXOs instead of relying on hardcoded amounts
+        coinbase_utxos = node.listunspent()[:2]
+        total_input = sum(Decimal(str(cb["amount"])) for cb in coinbase_utxos)
+        change = total_input - splitted_amount * utxo_count - fee
 
-        # Mine
+        tx = CTransaction()
+        tx.vin = [
+            CTxIn(COutPoint(int(cb["txid"], 16), cb["vout"]))
+            for cb in coinbase_utxos
+        ]
+        tx.vout = [CTxOut(int(splitted_amount * COIN), P2SH) for _ in range(utxo_count)]
+        tx.vout.append(CTxOut(int(change * COIN), P2SH))
+        txhex = node.signrawtransactionwithwallet(tx.serialize().hex())["hex"]
+        txid = node.sendrawtransaction(txhex)
+        self.confutxo = [
+            {"txid": txid, "vout": i, "amount": splitted_amount}
+            for i in range(utxo_count)
+        ]
         while len(node.getrawmempool()) > 0:
             self.generate(node, 1, sync_fun=self.no_op)
-
-        # Repeatedly split those 2 outputs, doubling twice for each rep
-        # Use txouts to monitor the available utxo, since these won't be tracked in wallet
-        reps = 0
-        while reps < 5:
-            # Double txouts to txouts2
-            while len(self.txouts) > 0:
-                split_inputs(node, self.txouts, self.txouts2)
-            while len(node.getrawmempool()) > 0:
-                self.generate(node, 1, sync_fun=self.no_op)
-            # Double txouts2 to txouts
-            while len(self.txouts2) > 0:
-                split_inputs(node, self.txouts2, self.txouts)
-            while len(node.getrawmempool()) > 0:
-                self.generate(node, 1, sync_fun=self.no_op)
-            reps += 1
 
     def sanity_check_estimates_range(self):
         """Populate estimation buckets, assert estimates are in a sane range and
         are strictly increasing as the target decreases."""
         self.fees_per_kb = []
         self.memutxo = []
-        self.confutxo = self.txouts  # Start with the set of confirmed txouts after splitting
         self.log.info("Will output estimates for 1/2/3/6/15/25 blocks")
 
         for _ in range(2):
-            self.log.info("Creating transactions and mining them with a block size that can't keep up")
+            self.log.info(
+                "Creating transactions and mining them with a block size that can't keep up"
+            )
             # Create transactions and mine 10 small blocks with node 2, but create txs faster than we can mine
             self.transact_and_mine(10, self.nodes[2])
             check_estimates(self.nodes[1], self.fees_per_kb)
 
-            self.log.info("Creating transactions and mining them at a block size that is just big enough")
+            self.log.info(
+                "Creating transactions and mining them at a block size that is just big enough"
+            )
             # Generate transactions while mining 10 more blocks, this time with node1
             # which mines blocks with capacity just above the rate that transactions are being created
             self.transact_and_mine(10, self.nodes[1])
@@ -264,12 +242,13 @@ class EstimateFeeTest(BitcoinTestFramework):
         # Finish by mining a normal-sized block:
         while len(self.nodes[1].getrawmempool()) > 0:
             self.generate(self.nodes[1], 1)
+
         self.log.info("Final estimates after emptying mempools")
         check_estimates(self.nodes[1], self.fees_per_kb)
 
     def test_feerate_mempoolminfee(self):
-        high_val = 3*self.nodes[1].estimatesmartfee(1)['feerate']
-        self.restart_node(1, extra_args=[f'-minrelaytxfee={high_val}'])
+        high_val = 3 * self.nodes[1].estimatesmartfee(1)["feerate"]
+        self.restart_node(1, extra_args=[f"-minrelaytxfee={high_val}"])
         check_estimates(self.nodes[1], self.fees_per_kb)
         self.stop_node(1, expected_stderr="Warning: -minrelaytxfee is set very high! The wallet will avoid paying less than the minimum relay fee.")
 
@@ -295,14 +274,17 @@ class EstimateFeeTest(BitcoinTestFramework):
         self.sanity_check_estimates_range()
 
         # check that the effective feerate is greater than or equal to the mempoolminfee even for high mempoolminfee
-        self.log.info("Test fee rate estimation after restarting node with high MempoolMinFee")
+        self.log.info(
+            "Test fee rate estimation after restarting node with high MempoolMinFee"
+        )
         self.test_feerate_mempoolminfee()
 
         self.log.info("Testing that fee estimation is disabled in blocksonly.")
         self.restart_node(0, ["-blocksonly"])
-        assert_raises_rpc_error(-32603, "Fee estimation disabled",
-                                self.nodes[0].estimatesmartfee, 2)
+        assert_raises_rpc_error(
+            -32603, "Fee estimation disabled", self.nodes[0].estimatesmartfee, 2
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     EstimateFeeTest().main()
