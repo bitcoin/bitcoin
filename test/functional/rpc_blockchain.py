245e86102307327ab1a833176352022b4e863698
merge-script <falke.marco@gmail.com>
2021-09-21 09:22:30 +0200

    Merge #23017: test: Replace MiniWallet scan_blocks with rescan_utxos

--- test/functional/rpc_blockchain.py
+++ test/functional/rpc_blockchain.py
@@ -404,7 +404,7 @@ class BlockchainTest(SyscoinTestFramework):
         node = self.nodes[0]
 
         miniwallet = MiniWallet(node)
-        miniwallet.scan_blocks(num=5)
+        miniwallet.rescan_utxos()
 
         fee_per_byte = Decimal('0.00000010')
         fee_per_kb = 1000 * fee_per_byte
