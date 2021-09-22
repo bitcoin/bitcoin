b4478b149cc4611f45d888ef0c8ba983f8c297bf
Wahid Tanner <wahidtanner@gmail.com>
2021-09-22 12:13:17 -0500

    Updated txid in test to match new genesis block.

--- src/qt/test/rpcnestedtests.cpp
+++ src/qt/test/rpcnestedtests.cpp
@@ -71,7 +71,7 @@ void RPCNestedTests::rpcNestedTests()
     QVERIFY(result == result2);
 
     RPCConsole::RPCExecuteCommandLine(*node, result, "getblock(getbestblockhash())[tx][0]", &filtered);
-    QVERIFY(result == "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"); //this txid should be the first transaction within the genesis block
+    QVERIFY(result == "172a104def609f48532c3e4afbd9d9f530b7c359d031ac47d63191cb11ae56fe"); //this txid should be the first transaction within the genesis block
     QVERIFY(filtered == "getblock(getbestblockhash())[tx][0]");
 
     RPCConsole::RPCParseCommandLine(nullptr, result, "importprivkey", false, &filtered);
