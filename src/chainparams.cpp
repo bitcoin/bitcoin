fa189621cc79fb72f7e27c76b75ebc00935a751e
MarcoFalke <falke.marco@gmail.com>
2021-09-28 20:18:27 +0200

    doc: Remove un-actionable TODO from chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -282,7 +282,7 @@ public:
             bin = ParseHex("512103ad5e0edad18cb1f0fc0d28a3d4f1f3e445640337489abb10404f2d1e086be430210359ef5021964fe22d6f8e05b2463c9540ce96883fe3b278760f048f5189f2e6c452ae");
             vSeeds.emplace_back("seed.signet.bitcoin.sprovoost.nl");
 
-            // TODO: remove hardcoded nodes once there are more DNS seeds
+            // Hardcoded nodes can be removed once there are more DNS seeds
             vSeeds.emplace_back("178.128.221.177");
             vSeeds.emplace_back("v7ajjeirttkbnt32wpy3c6w3emwnfr3fkla7hpxcfokr3ysd3kqtzmqd.onion:38333");
 
