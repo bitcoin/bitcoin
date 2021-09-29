fa2d611bedc2a755dcf84a82699c70b57b903cf6
MarcoFalke <falke.marco@gmail.com>
2021-09-29 17:09:55 +0200

    style: Sort

--- src/merkleblock.h
+++ src/merkleblock.h
@@ -6,10 +6,10 @@
 #ifndef BITCOIN_MERKLEBLOCK_H
 #define BITCOIN_MERKLEBLOCK_H
 
+#include <common/bloom.h>
+#include <primitives/block.h>
 #include <serialize.h>
 #include <uint256.h>
-#include <primitives/block.h>
-#include <common/bloom.h>
 
 #include <vector>
 
