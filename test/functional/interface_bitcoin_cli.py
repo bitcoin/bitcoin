bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

test: add -getinfo command parsing regression tests
These tests fail without the changes in the first commit of this PR.

Github-Pull: #19089
Rebased-From: 198943934543a85a07bb50cf585adc0042280819

test/functional/interface_bitcoin_cli.py
@@ -49,6 +49,22 @@ class TestBitcoinCli(BitcoinTestFramework):
         self.log.info("Test -getinfo with arguments fails")
         assert_raises_process_error(1, "-getinfo takes no arguments", self.nodes[0].cli('-getinfo').help)
 
+        self.log.info("Test -getinfo command parsing")
+
+        self.log.debug("Test -getinfo=1 and -getinfo=-1 both succeed")
+        for cmd in ['-getinfo=1', '-getinfo=-1']:
+            assert_equal(rpc_response['blocks'], self.nodes[0].cli(cmd).send_cli()['blocks'])
+
+        self.log.debug("Test -getinfo=0 and -nogetinfo both raise 'too few parameters'")
+        err_msg = "error: too few parameters (need at least command)"
+        for cmd in ['-getinfo=0', '-nogetinfo']:
+            assert_raises_process_error(1, err_msg, self.nodes[0].cli(cmd).send_cli)
+
+        self.log.debug("Test -igetinfo and -getinfos both raise 'Invalid parameter'")
+        err_msg = "Error parsing command line arguments: Invalid parameter"
+        for cmd in ['-igetinfo', '-getinfos']:
+            assert_raises_process_error(1, "{} {}".format(err_msg, cmd), self.nodes[0].cli(cmd).send_cli)
+
         self.log.info("Test -getinfo returns expected network and blockchain info")
         if self.is_wallet_compiled():
             self.nodes[0].encryptwallet(password)

bitcoin/bitcoin

test/functional/interface_bitcoin_cli.py
@@ -66,6 +66,22 @@ class TestBitcoinCli(BitcoinTestFramework):
         self.log.info("Test -getinfo with arguments fails")
         assert_raises_process_error(1, "-getinfo takes no arguments", self.nodes[0].cli('-getinfo').help)
 
+        self.log.info("Test -getinfo command parsing")
+
+        self.log.debug("Test -getinfo=1 and -getinfo=-1 both succeed")
+        for cmd in ['-getinfo=1', '-getinfo=-1']:
+            assert_equal(blockchain_info['blocks'], self.nodes[0].cli(cmd).send_cli()['blocks'])
+
+        self.log.debug("Test -getinfo=0 and -nogetinfo both raise 'too few parameters'")
+        err_msg = "error: too few parameters (need at least command)"
+        for cmd in ['-getinfo=0', '-nogetinfo']:
+            assert_raises_process_error(1, err_msg, self.nodes[0].cli(cmd).send_cli)
+
+        self.log.debug("Test -igetinfo and -getinfos both raise 'Invalid parameter'")
+        err_msg = "Error parsing command line arguments: Invalid parameter"
+        for cmd in ['-igetinfo', '-getinfos']:
+            assert_raises_process_error(1, "{} {}".format(err_msg, cmd), self.nodes[0].cli(cmd).send_cli)
+
         self.log.info("Test -getinfo returns expected network and blockchain info")
         if self.is_wallet_compiled():
             self.nodes[0].encryptwallet(password)