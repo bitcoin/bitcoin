31b2b802b5b8f214e91ec47fbc953c6ca95c7cf9
Anthony Towns <aj@erisian.com.au>
2021-10-04 14:44:41 +1000

    blockstorage: use debug log category

--- test/functional/rpc_misc.py
+++ test/functional/rpc_misc.py
@@ -57,7 +57,7 @@ class RpcMiscTest(BitcoinTestFramework):
         self.log.info("test logging rpc and help")
 
         # Test logging RPC returns the expected number of logging categories.
-        assert_equal(len(node.logging()), 26)
+        assert_equal(len(node.logging()), 27)
 
         # Test toggling a logging category on/off/on with the logging RPC.
         assert_equal(node.logging()['qt'], True)
