faa5e171e6bdb8f3b4027a3f06497f0de5abf766
MarcoFalke <falke.marco@gmail.com>
2021-10-05 14:47:12 +0200

    RPCConsole: Throw when overflowing size_t type for array indices

--- src/qt/rpcconsole.cpp
+++ src/qt/rpcconsole.cpp
@@ -247,10 +247,11 @@ bool RPCConsole::RPCParseCommandLine(interfaces::Node* node, std::string &strRes
                                 UniValue subelement;
                                 if (lastResult.isArray())
                                 {
-                                    for(char argch: curarg)
-                                        if (!IsDigit(argch))
-                                            throw std::runtime_error("Invalid result query");
-                                    subelement = lastResult[LocaleIndependentAtoi<int>(curarg)];
+                                    const auto parsed{ToIntegral<size_t>(curarg)};
+                                    if (!parsed) {
+                                        throw std::runtime_error("Invalid result query");
+                                    }
+                                    subelement = lastResult[parsed.value()];
                                 }
                                 else if (lastResult.isObject())
                                     subelement = find_value(lastResult, curarg);
