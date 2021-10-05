75263a9e9ab19be73de9739c4251216a4d082f1e
CallMeMisterOwl <denizhasler@outlook.de>
2021-10-05 22:21:06 +0200

    Actually call the function

--- build_msvc/msvc-autogen.py
+++ build_msvc/msvc-autogen.py
@@ -112,6 +112,8 @@ def main():
             with open(vcxproj_filename, 'w', encoding='utf-8') as vcxproj_file:
                 vcxproj_file.write(vcxproj_in_file.read().replace(
                     '@SOURCE_FILES@\n', content))
+                    
+    parse_config_into_btc_config()
     copyfile(os.path.join(SOURCE_DIR,'../build_msvc/bitcoin_config.h'), os.path.join(SOURCE_DIR, 'config/bitcoin-config.h'))
     copyfile(os.path.join(SOURCE_DIR,'../build_msvc/libsecp256k1_config.h'), os.path.join(SOURCE_DIR, 'secp256k1/src/libsecp256k1-config.h'))
 
