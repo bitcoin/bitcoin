88cc4810926e4f5af6757ee1b0eed61abda3d746
Samuel Dobson <dobsonsa68@gmail.com>
2021-10-07 14:43:10 +1300

    Modify copyright header on Bech32 code

--- src/test/bech32_tests.cpp
+++ src/test/bech32_tests.cpp
@@ -1,4 +1,5 @@
 // Copyright (c) 2017 Pieter Wuille
+// Copyright (c) 2021 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
