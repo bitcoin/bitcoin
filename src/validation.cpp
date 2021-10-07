1b4da8b0472321fb65ecef055450f7befc1b5e1c
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-10-07 15:47:38 -0700

    Update validation.cpp

--- src/validation.cpp
+++ src/validation.cpp
@@ -6165,7 +6165,7 @@ void KillProcess(const pid_t& pid){
             }
             hwnd = ::GetNextWindow(hwnd, GW_HWNDNEXT);
         }
-        ::SendMessage(hwnd, WM_QUIT, 0, 0);
+        ::SendMessage(hwnd, WM_CLOSE, 0, 0);
         CloseHandle(handy);
     #endif  
     #ifndef WIN32
