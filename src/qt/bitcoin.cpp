bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

GUI: Initialise prune setting using rwconf

src/qt/bitcoin.cpp
@@ -283,7 +283,7 @@ void BitcoinApplication::parameterSetup()
 
 void BitcoinApplication::InitPruneSetting(int64_t prune_opt)
 {
-    optionsModel->SetPruneTargetGB(PruneMiBtoGB(prune_opt), true);
+    optionsModel->SetPruneMiB(prune_opt, true);
 }
 
 void BitcoinApplication::requestInitialize()

bitcoin/bitcoin

src/qt/bitcoin.cpp
@@ -321,7 +321,7 @@ void BitcoinApplication::parameterSetup()
 
 void BitcoinApplication::InitPruneSetting(int64_t prune_MiB)
 {
-    optionsModel->SetPruneTargetGB(PruneMiBtoGB(prune_MiB), true);
+    optionsModel->SetPruneMiB(prune_MiB, true);
 }
 
 void BitcoinApplication::requestInitialize()