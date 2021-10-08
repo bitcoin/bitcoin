07632958cbf552340282d7e678979ab4819c36d7
Kittywhiskers Van Gogh <63189531+kittywhiskers@users.noreply.github.com>
2021-10-08 18:24:20 +0530

    merge bitcoin#16277: suppress output in test_bitcoin for expected errors

--- src/wallet/test/init_tests.cpp
+++ src/wallet/test/init_tests.cpp
@@ -4,6 +4,7 @@
 
 #include <boost/test/unit_test.hpp>
 
+#include <noui.h>
 #include <test/test_dash.h>
 #include <wallet/test/init_test_fixture.h>
 
@@ -17,7 +18,9 @@ BOOST_FIXTURE_TEST_SUITE(init_tests, InitWalletDirTestingSetup)
 BOOST_AUTO_TEST_CASE(walletinit_verify_walletdir_default)
 {
     SetWalletDir(m_walletdir_path_cases["default"]);
+    noui_suppress();
     bool result = m_chain_client->verify();
+    noui_reconnect();
     BOOST_CHECK(result == true);
     fs::path walletdir = gArgs.GetArg("-walletdir", "");
     fs::path expected_path = fs::canonical(m_walletdir_path_cases["default"]);
@@ -27,7 +30,9 @@ BOOST_AUTO_TEST_CASE(walletinit_verify_walletdir_default)
 BOOST_AUTO_TEST_CASE(walletinit_verify_walletdir_custom)
 {
     SetWalletDir(m_walletdir_path_cases["custom"]);
+    noui_suppress();
     bool result = m_chain_client->verify();
+    noui_reconnect();
     BOOST_CHECK(result == true);
     fs::path walletdir = gArgs.GetArg("-walletdir", "");
     fs::path expected_path = fs::canonical(m_walletdir_path_cases["custom"]);
@@ -37,28 +42,36 @@ BOOST_AUTO_TEST_CASE(walletinit_verify_walletdir_custom)
 BOOST_AUTO_TEST_CASE(walletinit_verify_walletdir_does_not_exist)
 {
     SetWalletDir(m_walletdir_path_cases["nonexistent"]);
+    noui_suppress();
     bool result = m_chain_client->verify();
+    noui_reconnect();
     BOOST_CHECK(result == false);
 }
 
 BOOST_AUTO_TEST_CASE(walletinit_verify_walletdir_is_not_directory)
 {
     SetWalletDir(m_walletdir_path_cases["file"]);
+    noui_suppress();
     bool result = m_chain_client->verify();
+    noui_reconnect();
     BOOST_CHECK(result == false);
 }
 
 BOOST_AUTO_TEST_CASE(walletinit_verify_walletdir_is_not_relative)
 {
     SetWalletDir(m_walletdir_path_cases["relative"]);
+    noui_suppress();
     bool result = m_chain_client->verify();
+    noui_reconnect();
     BOOST_CHECK(result == false);
 }
 
 BOOST_AUTO_TEST_CASE(walletinit_verify_walletdir_no_trailing)
 {
     SetWalletDir(m_walletdir_path_cases["trailing"]);
+    noui_suppress();
     bool result = m_chain_client->verify();
+    noui_reconnect();
     BOOST_CHECK(result == true);
     fs::path walletdir = gArgs.GetArg("-walletdir", "");
     fs::path expected_path = fs::canonical(m_walletdir_path_cases["default"]);
@@ -68,7 +81,9 @@ BOOST_AUTO_TEST_CASE(walletinit_verify_walletdir_no_trailing)
 BOOST_AUTO_TEST_CASE(walletinit_verify_walletdir_no_trailing2)
 {
     SetWalletDir(m_walletdir_path_cases["trailing2"]);
+    noui_suppress();
     bool result = m_chain_client->verify();
+    noui_reconnect();
     BOOST_CHECK(result == true);
     fs::path walletdir = gArgs.GetArg("-walletdir", "");
     fs::path expected_path = fs::canonical(m_walletdir_path_cases["default"]);
