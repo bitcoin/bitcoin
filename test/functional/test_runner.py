c4d76c6faa3adf06f192649e169ca860ce420d30
Andrew Chow <achow101-github@achow101.com>
2021-10-18 16:24:24 -0400

    tests: Tests for inactive HD chains
    test cases are added for inactive HD chains: a basic case, a case
    where the wallet is encrypted, and a case for the 21605 segfault.

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -279,6 +279,7 @@ BASE_SCRIPTS = [
     'wallet_send.py --descriptors',
     'wallet_create_tx.py --descriptors',
     'wallet_taproot.py',
+    'wallet_inactive_hdchains.py',
     'p2p_fingerprint.py',
     'feature_uacomment.py',
     'feature_init.py',
