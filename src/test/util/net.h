417f64c4920761760b4fd9ee28c875754a8c4808
W. J. van der Laan <laanwj@protonmail.com>
2021-10-21 19:34:55 +0200

    Merge #23218: p2p: Use mocktime for ping timeout

--- src/test/util/net.h
+++ src/test/util/net.h
@@ -17,6 +17,12 @@
 
 struct ConnmanTestMsg : public CConnman {
     using CConnman::CConnman;
+
+    void SetPeerConnectTimeout(int64_t timeout)
+    {
+        m_peer_connect_timeout = timeout;
+    }
+
     void AddTestNode(CNode& node)
     {
         LOCK(cs_vNodes);
