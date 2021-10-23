cd29362eea7e8f53e782f2f951c747ec4739b2b9
PastaPastaPasta <6443210+PastaPastaPasta@users.noreply.github.com>
2021-10-23 12:26:55 -0400

    Merge bitcoin#23181: refactor: remove references to deprecated values under std::allocator (#4534)
    * Merge bitcoin/bitcoin#23181: refactor: remove references to deprecated values under std::allocator
    
    ea4b61a1570178ebe5851b5fb4065222e3926f7e refactor: remove references to deprecated values under std::allocator (Pasta)
    
    Pull request description:
    
      Removes usages of allocator::pointer, allocator::const_pointer, allocator::reference and allocator::const_reference which are deprecated in c++17 and **removed** in c++20. See https://en.cppreference.com/w/cpp/memory/allocator
    
      Also prefers `using` over `typedef` see: https://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines#Rt-using I'll be happy to revert this if requested so
    
    ACKs for top commit:
      laanwj:
        Re-ACK ea4b61a1570178ebe5851b5fb4065222e3926f7e
    
    Tree-SHA512: 9353e47a7de27bcd91b341eb2d832318b51fce9f508fcc791f05c02c5a160f430f4e7214e76f4b3e29639750d311c679789d8b7409255b13637391e4575c9ebe
    
    * extend 23181
    
    Co-authored-by: W. J. van der Laan <laanwj@protonmail.com>

--- src/support/allocators/pooled_secure.h
+++ src/support/allocators/pooled_secure.h
@@ -19,15 +19,13 @@
 //
 template <typename T>
 struct pooled_secure_allocator : public std::allocator<T> {
-    // MSVC8 default copy constructor is broken
-    typedef std::allocator<T> base;
-    typedef typename base::size_type size_type;
-    typedef typename base::difference_type difference_type;
-    typedef typename base::pointer pointer;
-    typedef typename base::const_pointer const_pointer;
-    typedef typename base::reference reference;
-    typedef typename base::const_reference const_reference;
-    typedef typename base::value_type value_type;
+    using base = std::allocator<T>;
+    using traits = std::allocator_traits<base>;
+    using size_type = typename traits::size_type;
+    using difference_type = typename traits::difference_type;
+    using pointer = typename traits::pointer;
+    using const_pointer = typename traits::const_pointer;
+    using value_type = typename traits::value_type;
     pooled_secure_allocator(const size_type nrequested_size = 32,
                             const size_type nnext_size = 32,
                             const size_type nmax_size = 0) noexcept :
