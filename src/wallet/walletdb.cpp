6cba19de61e4b36be356bfae9a2f0b309b95026a
MarcoFalke <falke.marco@gmail.com>
2021-10-25 13:28:35 +0200

    Merge bitcoin/bitcoin#23338: tests: speed up coinselector_tests
    BACKPORT NOTICE
    This PR indeed speeded up unit tests significantly
    
        $ time src/test/test_dash -t '*coinselector*'
        Running 4 test cases...
    
        *** No errors detected
    
        real    0m31,104s
        user    0m30,086s
        sys     0m0,885s
    
        to
    
        real    0m5,663s
        user    0m5,154s
        sys     0m0,385s
    
    a52f1d13409e4ef46277596ec13fa8b421fa1329 walletdb: Use SQLiteDatabase for mock wallet databases (Andrew Chow)
    a78c2298080f173d0266e708267458a72eb2f600 tests: Place into mapWallet in coinselector_tests (Andrew Chow)
    
    Pull request description:
    
      #23288 changed coinselector_tests to use `DescriptorScriptPubKeyMan`, but it also ended up significantly slowing down the test, from 4 seconds to over 1 minute. It appears that the source of this slow down is with `CWallet::AddToWallet`, and primarily due to writing data to the mock wallet database. Because the only thing that is actually needed is for the created transaction to be placed into `CWallet::mapWallet`, this PR removes the call to `AddToWallet` and just places the transaction into `mapWallet` directly. This reduces the test time to 5 seconds.
    
      To speed things up further, `CreateMockWalletDatabase` is changed to make a `SQLiteDatabase` instead of a `BerkeleyDatabase`. This is safe because there are no tests that require a specific mock database type.
    
    ACKs for top commit:
      brunoerg:
        tACK a52f1d13409e4ef46277596ec13fa8b421fa1329
      lsilva01:
        tACK a52f1d1. Performed 74.36% better on Ubuntu 20.04 (VM, 12 MB, 8vCPU).
      glozow:
        utACK a52f1d13409e4ef46277596ec13fa8b421fa1329
    
    Tree-SHA512: da77936bfd2e816d2e71703567b9389d0ee79f3a4a690802ffe3469df5bed371b296cb822b897f625654dab9436d91fd6bc02364a518a47d746e487d70a72595

--- src/wallet/walletdb.cpp
+++ src/wallet/walletdb.cpp
@@ -1186,9 +1186,9 @@ std::unique_ptr<WalletDatabase> CreateDummyWalletDatabase()
 /** Return object for accessing temporary in-memory database. */
 std::unique_ptr<WalletDatabase> CreateMockWalletDatabase()
 {
-#ifdef USE_BDB
-    return std::make_unique<BerkeleyDatabase>(std::make_shared<BerkeleyEnvironment>(), "");
-#elif defined(USE_SQLITE)
+#ifdef USE_SQLITE
     return std::make_unique<SQLiteDatabase>("", "", true);
+#elif defined(USE_BDB)
+    return std::make_unique<BerkeleyDatabase>(std::make_shared<BerkeleyEnvironment>(), "");
 #endif
 }
