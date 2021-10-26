7f561b7b861761708e96c3dff92a9a877f6f1c8f
Denis Ahrens <denis@h3q.com>
2021-10-26 23:00:12 +0200

    util: optimize HexStr() by 27% with a larger table and 16-bit writes
    The old code needed 365ns for 256 byte input on an i7-6920HQ CPU,
    this commit needs 268ns for the same 256 byte input.
    This is a speedup of about 27%.

--- src/util/strencodings.cpp
+++ src/util/strencodings.cpp
@@ -516,13 +516,20 @@ std::string Capitalize(std::string str)
 std::string HexStr(const Span<const uint8_t> s)
 {
     std::string rv(s.size() * 2, '\0');
-    static constexpr char hexmap[16] = { '0', '1', '2', '3', '4', '5', '6', '7',
-                                         '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
-    auto it = rv.begin();
-    for (uint8_t v : s) {
-        *it++ = hexmap[v >> 4];
-        *it++ = hexmap[v & 15];
-    }
-    assert(it == rv.end());
+    static const uint16_t *hexmap = (const uint16_t *)
+        "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f"
+        "202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f"
+        "404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d5e5f"
+        "606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e7f"
+        "808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9f"
+        "a0a1a2a3a4a5a6a7a8a9aaabacadaeafb0b1b2b3b4b5b6b7b8b9babbbcbdbebf"
+        "c0c1c2c3c4c5c6c7c8c9cacbcccdcecfd0d1d2d3d4d5d6d7d8d9dadbdcdddedf"
+        "e0e1e2e3e4e5e6e7e8e9eaebecedeeeff0f1f2f3f4f5f6f7f8f9fafbfcfdfeff";
+    uint16_t *it = (uint16_t *)rv.data();
+
+    for (uint8_t v : s)
+        *it++ = hexmap[v];
+
+    assert(it == (uint16_t *)(rv.data() + s.size() * 2));
     return rv;
 }
