de2bf9999fc28539dc47d960e00c5f4f62309e20
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2021-11-02 15:03:25 +0100

    p2p_getaddr_caching.py: Using test framework utils
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- test/functional/p2p_getaddr_caching.py
+++ test/functional/p2p_getaddr_caching.py
@@ -14,7 +14,10 @@ from test_framework.p2p import (
 from test_framework.test_framework import BitcoinTestFramework
 from test_framework.util import (
     assert_equal,
+    PORT_MIN,
+    PORT_RANGE,
     p2p_port,
+    assert_greater_than,
 )
 
 # As defined in net_processing.
@@ -60,7 +63,7 @@ class AddrTest(BitcoinTestFramework):
 
         # Need to make sure we hit MAX_ADDR_TO_SEND records in the addr response later because
         # only a fraction of all known addresses can be cached and returned.
-        assert(len(self.nodes[0].getnodeaddresses(0)) > int(MAX_ADDR_TO_SEND / (MAX_PCT_ADDR_TO_SEND / 100)))
+        assert_greater_than(len(self.nodes[0].getnodeaddresses(0)), int(MAX_ADDR_TO_SEND / (MAX_PCT_ADDR_TO_SEND / 100)))
 
         last_response_on_local_bind = None
         last_response_on_onion_bind1 = None
