f2c9af9657f6c20bf06ac43304414ff63b5ab1d5
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-11-09 11:47:19 -0800

    Update feature_sporks.py

--- test/functional/feature_sporks.py
+++ test/functional/feature_sporks.py
@@ -55,7 +55,7 @@ class SporkTest(DashTestFramework):
         assert(self.get_test_spork_state(self.nodes[1]) == spork_new_state)
 
         # Generate one block to kick off masternode sync, which also starts sporks syncing for node2
-        self.generate(self.nodes[1], 1)
+        self.generate(self.nodes[1], 1, sync_fun=self.no_op)
 
         # connect new node and check spork propagation after restoring from cache
         self.connect_nodes(1, 2)
