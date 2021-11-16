d8e874af5d5fc7699db256eb1e049d2637ac5d9d
fanquake <fanquake@gmail.com>
2021-11-16 15:51:19 +0800

    Merge #23491: scripted-diff: Move minisketchwrapper to src/node

--- src/node/minisketchwrapper.h
+++ src/node/minisketchwrapper.h
@@ -2,10 +2,11 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef SYSCOIN_MINISKETCHWRAPPER_H
-#define SYSCOIN_MINISKETCHWRAPPER_H
+#ifndef SYSCOIN_NODE_MINISKETCHWRAPPER_H
+#define SYSCOIN_NODE_MINISKETCHWRAPPER_H
 
 #include <minisketch.h>
+
 #include <cstddef>
 #include <cstdint>
 
@@ -14,4 +15,4 @@ Minisketch MakeMinisketch32(size_t capacity);
 /** Wrapper around Minisketch::CreateFP. */
 Minisketch MakeMinisketch32FP(size_t max_elements, uint32_t fpbits);
 
-#endif // SYSCOIN_MINISKETCHWRAPPER_H
+#endif // SYSCOIN_NODE_MINISKETCHWRAPPER_H
