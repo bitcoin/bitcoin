0885e61ec935f9fd0f4dbfc39831982120412945
fanquake <fanquake@gmail.com>
2021-11-16 16:10:47 +0800

    Merge #23504: ci: Replace soon EOL hirsute with jammy

--- ci/test/00_setup_env_native_asan.sh
+++ ci/test/00_setup_env_native_asan.sh
@@ -8,7 +8,7 @@ export LC_ALL=C.UTF-8
 
 export CONTAINER_NAME=ci_native_asan
 export PACKAGES="clang llvm python3-zmq qtbase5-dev qttools5-dev-tools libevent-dev bsdmainutils libboost-dev libboost-system-dev libboost-filesystem-dev libboost-test-dev libdb5.3++-dev libminiupnpc-dev libnatpmp-dev libzmq3-dev libqrencode-dev libsqlite3-dev libgmp3-dev libcurl4-gnutls-dev"
-export DOCKER_NAME_TAG=ubuntu:hirsute
+export DOCKER_NAME_TAG=ubuntu:22.04
 export NO_DEPENDS=1
 export GOAL="install"
 export TEST_RUNNER_EXTRA="--exclude interface_zmq_nevm,feature_deterministicmns,feature_llmq_is_retroactive,feature_llmqdkgerrors,feature_llmqconnections,feature_llmqsigning,feature_llmqsimplepose"
