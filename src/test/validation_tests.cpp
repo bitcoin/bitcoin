1d59b4ffb6e33f47da809a21438bd84b08866ecf
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-11-16 11:17:39 -0800

    Update validation_tests.cpp

--- src/test/validation_tests.cpp
+++ src/test/validation_tests.cpp
@@ -41,7 +41,9 @@ static void TestBlockSubsidyHalvings(int nSubsidyHalvingInterval)
 
 BOOST_AUTO_TEST_CASE(block_subsidy_test)
 {
-    const auto chainParams = CreateChainParams(*m_node.args, CBaseChainParams::MAIN);
+    // SYSCOIN
+    fRegTest = true;
+    const auto chainParams = CreateChainParams(*m_node.args, CBaseChainParams::REGTEST);
     TestBlockSubsidyHalvings(chainParams->GetConsensus()); // As in main
     TestBlockSubsidyHalvings(150); // As in regtest
     TestBlockSubsidyHalvings(1000); // Just another interval
@@ -49,10 +51,13 @@ BOOST_AUTO_TEST_CASE(block_subsidy_test)
 
 BOOST_AUTO_TEST_CASE(subsidy_limit_test)
 {
-    const auto chainParams = CreateChainParams(*m_node.args, CBaseChainParams::MAIN);
+    // SYSCOIN
+    fRegTest = true;
+    Consensus::Params consensusParams;
+    consensusParams.nSubsidyHalvingInterval = 210000;
     CAmount nSum = 0;
     for (int nHeight = 0; nHeight < 14000000; nHeight += 1000) {
-        CAmount nSubsidy = GetBlockSubsidy(nHeight, chainParams->GetConsensus());
+        CAmount nSubsidy = GetBlockSubsidy(nHeight, consensusParams);
         BOOST_CHECK(nSubsidy <= 50 * COIN);
         nSum += nSubsidy * 1000;
         BOOST_CHECK(MoneyRange(nSum));
