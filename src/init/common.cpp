9211d91b226ff45c1eeaa4942a7b5c79f170a78d
Carl Dong <contact@carldong.me>
2021-11-19 16:27:56 -0500

    kernel: Decouple from full init/common.cpp
    ...by splitting the libbitcoinkernel-used portion of init/common.cpp off
       to kernel/init/common.cpp
    
    Notes for reviewers:
        - I strongly recommend reviewing with the following git-diff flags:
              --patience --color-moved=dimmed-zebra
        - See the diff for src/Makefile.am to see the overall effect.

--- src/init/common.cpp
+++ src/init/common.cpp
@@ -8,36 +8,15 @@
 
 #include <clientversion.h>
 #include <compat/sanity.h>
-#include <crypto/sha256.h>
 #include <key.h>
 #include <logging.h>
 #include <node/ui_interface.h>
-#include <pubkey.h>
 #include <random.h>
 #include <util/system.h>
 #include <util/time.h>
 #include <util/translation.h>
 
-#include <memory>
-
-static std::unique_ptr<ECCVerifyHandle> globalVerifyHandle;
-
 namespace init {
-void SetGlobals()
-{
-    std::string sha256_algo = SHA256AutoDetect();
-    LogPrintf("Using the '%s' SHA256 implementation\n", sha256_algo);
-    RandomInit();
-    ECC_Start();
-    globalVerifyHandle.reset(new ECCVerifyHandle());
-}
-
-void UnsetGlobals()
-{
-    globalVerifyHandle.reset();
-    ECC_Stop();
-}
-
 bool SanityChecks()
 {
     if (!ECC_InitSanityCheck()) {
