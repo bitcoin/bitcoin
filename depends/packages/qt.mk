f6e278167595b9f6f7f5d5f8b333d1b6c5f0b40b
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2021-11-24 17:33:56 +0200

    build, qt, macOS: Don't pass -device-option when building natively

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -128,8 +128,10 @@ $(package)_config_opts_darwin += -device-option MAC_TARGET=$(host)
 $(package)_config_opts_darwin += -device-option XCODE_VERSION=$(XCODE_VERSION)
 endif
 
-# for macOS on Apple Silicon (ARM) see https://bugreports.qt.io/browse/QTBUG-85279
+ifneq ($(build_arch),$(host_arch))
 $(package)_config_opts_aarch64_darwin += -device-option QMAKE_APPLE_DEVICE_ARCHS=arm64
+$(package)_config_opts_x86_64_darwin += -device-option QMAKE_APPLE_DEVICE_ARCHS=x86_64
+endif
 
 $(package)_config_opts_linux = -qt-xcb
 $(package)_config_opts_linux += -no-xcb-xlib
