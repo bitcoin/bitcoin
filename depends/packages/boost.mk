8d2b6bb6841723b627121d67033f29ae091b51a1
PastaPastaPasta <6443210+PastaPastaPasta@users.noreply.github.com>
2021-11-29 00:28:09 -0500

    depends: upgrade depends Boost to 1.73 (#4576)

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -1,8 +1,8 @@
 package=boost
-$(package)_version=1_71_0
+$(package)_version=1_73_0
 $(package)_download_path=https://boostorg.jfrog.io/artifactory/main/release/$(subst _,.,$($(package)_version))/source/
 $(package)_file_name=boost_$($(package)_version).tar.bz2
-$(package)_sha256_hash=d73a8da01e8bf8c7eda40b4c84915071a8c8a0df4a6734537ddde4a8580524ee
+$(package)_sha256_hash=4eb3b8d442b426dc35346235c8733b5ae35ba431690e38c6a8263dce9fcbb402
 $(package)_dependencies=native_b2
 
 define $(package)_set_vars
