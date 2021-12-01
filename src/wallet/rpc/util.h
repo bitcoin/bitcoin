7b04a064f6e8ee9d93e5a5ad54dab20b769083f2
Samuel Dobson <dobsonsa68@gmail.com>
2021-12-01 13:10:43 +1300

    Introduce wallet/rpc/util

---
+++ src/wallet/rpc/util.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2017-2021 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_WALLET_RPC_UTIL_H
+#define BITCOIN_WALLET_RPC_UTIL_H
+
+
+
+#endif // BITCOIN_WALLET_RPC_UTIL_H
