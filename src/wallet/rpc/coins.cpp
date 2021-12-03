f336ff7f213564909cf5f9742618cc6ec87600fd
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2021-12-03 17:29:26 +0100

    rpc: avoid expensive `IsMine` calls in `GetReceived` tally

--- src/wallet/rpc/coins.cpp
+++ src/wallet/rpc/coins.cpp
@@ -23,7 +23,10 @@ static CAmount GetReceived(const CWallet& wallet, const UniValue& params, bool b
         // Get the set of addresses assigned to label
         std::string label = LabelFromValue(params[0]);
         for (const auto& address : wallet.GetLabelAddresses(label)) {
-            output_scripts.insert(GetScriptForDestination(address));
+            auto output_script{GetScriptForDestination(address)};
+            if (wallet.IsMine(output_script)) {
+                output_scripts.insert(output_script);
+            }
         }
     } else {
         // Get the address
@@ -67,7 +70,7 @@ static CAmount GetReceived(const CWallet& wallet, const UniValue& params, bool b
         }
 
         for (const CTxOut& txout : wtx.tx->vout) {
-            if (wallet.IsMine(txout.scriptPubKey) && output_scripts.count(txout.scriptPubKey) > 0) {
+            if (output_scripts.count(txout.scriptPubKey) > 0) {
                 amount += txout.nValue;
             }
         }
