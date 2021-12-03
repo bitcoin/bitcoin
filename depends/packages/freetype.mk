03b262fec5a82f064a38c0fc829ed1994feb9308
fanquake <fanquake@gmail.com>
2021-12-03 19:56:21 +0800

    Merge #23489: build: Qt 5.15.2

--- depends/packages/freetype.mk
+++ depends/packages/freetype.mk
@@ -1,12 +1,12 @@
 package=freetype
-$(package)_version=2.7.1
+$(package)_version=2.11.0
 $(package)_download_path=https://download.savannah.gnu.org/releases/$(package)
-$(package)_file_name=$(package)-$($(package)_version).tar.bz2
-$(package)_sha256_hash=3a3bb2c4e15ffb433f2032f50a5b5a92558206822e22bfe8cbe339af4aa82f88
+$(package)_file_name=$(package)-$($(package)_version).tar.xz
+$(package)_sha256_hash=8bee39bd3968c4804b70614a0a3ad597299ad0e824bc8aad5ce8aaf48067bde7
 
 define $(package)_set_vars
   $(package)_config_opts=--without-zlib --without-png --without-harfbuzz --without-bzip2 --disable-static
-  $(package)_config_opts += --enable-option-checking
+  $(package)_config_opts += --enable-option-checking --without-brotli
   $(package)_config_opts_linux=--with-pic
 endef
 
