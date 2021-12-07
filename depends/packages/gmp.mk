06ebacbb9abe499f53b9fbf6a6484589faa5a483
Kittywhiskers Van Gogh <kittywhiskers@kittywhiskers.eu>
2021-12-07 06:05:38 +0530

    build: bump gmp from 6.1.2 to 6.2.1 (#4597)

--- depends/packages/gmp.mk
+++ depends/packages/gmp.mk
@@ -1,12 +1,13 @@
 package=gmp
-$(package)_version=6.1.2
+$(package)_version=6.2.1
 $(package)_download_path=https://gmplib.org/download/gmp
 $(package)_file_name=gmp-$($(package)_version).tar.bz2
-$(package)_sha256_hash=5275bb04f4863a13516b2f39392ac5e272f5e1bb8057b18aec1c9b79d73d8fb2
+$(package)_sha256_hash=eae9326beb4158c386e39a356818031bd28f3124cf915f8c5b1dc4c7a36b4d7c
 
 define $(package)_set_vars
 $(package)_config_opts+=--enable-cxx --enable-fat --with-pic --disable-shared
 $(package)_cflags_armv7l_linux+=-march=armv7-a
+$(package)_cflags_aarch64_darwin+=-march=armv8-a
 endef
 
 define $(package)_config_cmds
