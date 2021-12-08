c4d6e60819e3adb5fee4cc26a1c619823e865a2c
Kittywhiskers Van Gogh <63189531+kittywhiskers@users.noreply.github.com>
2021-12-08 11:54:46 +0530

    merge bitcoin#15895: Avoid re-reading config.ini unnecessarily

--- test/functional/test_framework/test_framework.py
+++ test/functional/test_framework/test_framework.py
@@ -654,24 +654,15 @@ class BitcoinTestFramework(metaclass=BitcoinTestMetaClass):
 
     def is_cli_compiled(self):
         """Checks whether dash-cli was compiled."""
-        config = configparser.ConfigParser()
-        config.read_file(open(self.options.configfile))
-
-        return config["components"].getboolean("ENABLE_CLI")
+        return self.config["components"].getboolean("ENABLE_CLI")
 
     def is_wallet_compiled(self):
         """Checks whether the wallet module was compiled."""
-        config = configparser.ConfigParser()
-        config.read_file(open(self.options.configfile))
-
-        return config["components"].getboolean("ENABLE_WALLET")
+        return self.config["components"].getboolean("ENABLE_WALLET")
 
     def is_zmq_compiled(self):
         """Checks whether the zmq module was compiled."""
-        config = configparser.ConfigParser()
-        config.read_file(open(self.options.configfile))
-
-        return config["components"].getboolean("ENABLE_ZMQ")
+        return self.config["components"].getboolean("ENABLE_ZMQ")
 
 
 MASTERNODE_COLLATERAL = 1000
