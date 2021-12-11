542fbdc54557a00ce950510cb8fb23a38d2e7e5d
MarcoFalke <falke.marco@gmail.com>
2021-12-11 09:23:54 +0100

    Merge bitcoin/bitcoin#23128: doc: Remove TODO 'exclude peers with download permission'
    2f97c1180b7eba2c48683a553ca166eba84e14c6 doc: Remove TODO 'exclude peers with download permission' (Douglas Chimento)
    
    Pull request description:
    
      Following from PR https://github.com/bitcoin/bitcoin/pull/23109
      The [TODO](https://github.com/bitcoin/bitcoin/blob/master/src/net.cpp#L2872)  is no longer necessary.
      Removing it to  prevent future confusion.
    
    Top commit has no ACKs.
    
    Tree-SHA512: c2f4c3eae951d13d623e1b4bd9315804ec33473e501367f89edae80fa446674edc71549e145f058fe7126b8588790dc895d6ea3dfb4347ceeca61f5e5f2f95cc

--- src/net.cpp
+++ src/net.cpp
@@ -3736,7 +3736,6 @@ void CConnman::RecordBytesSent(uint64_t bytes)
         nMaxOutboundTotalBytesSentInCycle = 0;
     }
 
-    // TODO, exclude peers with download permission
     nMaxOutboundTotalBytesSentInCycle += bytes;
 }
 
