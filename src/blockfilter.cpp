df2307cdc3d08233d17beb9a50c144baaef1f44e
fanquake <fanquake@gmail.com>
2021-12-13 20:54:30 +0800

    util: move MapIntoRange() for reuse in fuzz tests

--- src/blockfilter.cpp
+++ src/blockfilter.cpp
@@ -24,37 +24,6 @@ static const std::map<BlockFilterType, std::string> g_filter_types = {
     {BlockFilterType::BASIC, "basic"},
 };
 
-// Map a value x that is uniformly distributed in the range [0, 2^64) to a
-// value uniformly distributed in [0, n) by returning the upper 64 bits of
-// x * n.
-//
-// See: https://lemire.me/blog/2016/06/27/a-fast-alternative-to-the-modulo-reduction/
-static uint64_t MapIntoRange(uint64_t x, uint64_t n)
-{
-#ifdef __SIZEOF_INT128__
-    return (static_cast<unsigned __int128>(x) * static_cast<unsigned __int128>(n)) >> 64;
-#else
-    // To perform the calculation on 64-bit numbers without losing the
-    // result to overflow, split the numbers into the most significant and
-    // least significant 32 bits and perform multiplication piece-wise.
-    //
-    // See: https://stackoverflow.com/a/26855440
-    uint64_t x_hi = x >> 32;
-    uint64_t x_lo = x & 0xFFFFFFFF;
-    uint64_t n_hi = n >> 32;
-    uint64_t n_lo = n & 0xFFFFFFFF;
-
-    uint64_t ac = x_hi * n_hi;
-    uint64_t ad = x_hi * n_lo;
-    uint64_t bc = x_lo * n_hi;
-    uint64_t bd = x_lo * n_lo;
-
-    uint64_t mid34 = (bd >> 32) + (bc & 0xFFFFFFFF) + (ad & 0xFFFFFFFF);
-    uint64_t upper64 = ac + (bc >> 32) + (ad >> 32) + (mid34 >> 32);
-    return upper64;
-#endif
-}
-
 uint64_t GCSFilter::HashToRange(const Element& element) const
 {
     uint64_t hash = CSipHasher(m_params.m_siphash_k0, m_params.m_siphash_k1)
