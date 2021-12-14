e4a8d561edf3cfb326e86c87155fed41a61e7333
Martin Zumsande <mzumsande@gmail.com>
2021-12-14 21:36:00 +0000

    doc: add explanations for assert in index and magic numbers in test
    These were suggested in review of #23365
    
    Co-authored-by: John Newbery <john@johnnewbery.com>

--- src/index/base.cpp
+++ src/index/base.cpp
@@ -96,6 +96,8 @@ bool BaseIndex::Init()
                     prune_violation = false;
                     break;
                 }
+                // block->pprev must exist at this point, since block_to_test is part of the chain
+                // and thus must be encountered when going backwards from the tip
                 assert(block->pprev);
                 block = block->pprev;
             }
