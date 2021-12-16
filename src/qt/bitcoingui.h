e7a3f698b5f3f7dde8632c4911abd4e5bc1f06cb
w0xlt <94266259+w0xlt@users.noreply.github.com>
2021-12-16 18:22:09 -0300

    gui: Add Wallet Restore in the GUI
    Co-authored-by: Shashwat Vangani <85434418+shaavan@users.noreply.github.com>
    Co-authored-by: furszy <matiasfurszyfer@protonmail.com>

--- src/qt/bitcoingui.h
+++ src/qt/bitcoingui.h
@@ -157,6 +157,7 @@ private:
     QAction* m_create_wallet_action{nullptr};
     QAction* m_open_wallet_action{nullptr};
     QMenu* m_open_wallet_menu{nullptr};
+    QAction* m_restore_wallet_action{nullptr};
     QAction* m_close_wallet_action{nullptr};
     QAction* m_close_all_wallets_action{nullptr};
     QAction* m_wallet_selector_label_action = nullptr;
