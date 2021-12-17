a6b71d7e483d5ad172c955abfcbc6b4f667130f3
Giuseppe Galano <giuseppe.galano2@bancaditalia.it>
2021-12-17 10:25:18 +0100

    infra: add configure-itcoin-core-dev.sh
    compile itcoin with support for debug
    infra: remove werror

---
+++ infra/configure-itcoin-core-dev.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# ItCoin
+#
+# This script configures the build for itcoin-core. It can be used both on bare
+# metal and inside a Docker container.
+#
+# Author: Antonio Muci <antonio.muci@bancaditalia.it>
+
+set -eu
+
+# https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself#246128
+MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+INSTALL_DIR=$(realpath "${MY_DIR}/../target")
+
+# autogen always assumes that the user's is calling it from the same directory
+# in which the script is placed. This is not always true (like in our case),
+# but but we have to adapt, so let's cd one level lower.
+cd "${MY_DIR}/.."
+"${MY_DIR}"/../autogen.sh
+
+# Removed flags:
+    # --enable-werror \
+
+"${MY_DIR}"/../configure \
+    --prefix="${INSTALL_DIR}" \
+    --enable-debug \
+    --enable-determinism \
+    --enable-wallet \
+    --enable-c++17 \
+    --disable-gui-tests \
+    --disable-bench \
+    --disable-man \
+    --with-incompatible-bdb \
+    --with-zmq \
+    --without-miniupnpc \
+    CXX="g++-10" \
+    CC="gcc-10"
