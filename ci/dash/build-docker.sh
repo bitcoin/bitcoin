1c188bcd47a23218e43c31727e3fa3f9b4ed47b4
Kittywhiskers Van Gogh <63189531+kittywhiskers@users.noreply.github.com>
2021-12-21 12:40:24 +0530

    build: relocate *net deployment logic to their own directories

---
+++ ci/dash/build-docker.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+export LC_ALL=C
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"/../.. || exit
+
+DOCKER_IMAGE=${DOCKER_IMAGE:-dashpay/dashd-develop}
+DOCKER_TAG=${DOCKER_TAG:-latest}
+DOCKER_RELATIVE_PATH=contrib/containers/deploy
+
+BUILD_DIR=${BUILD_DIR:-.}
+
+
+if [ -d $DOCKER_RELATIVE_PATH/bin ]; then
+    rm $DOCKER_RELATIVE_PATH/bin/*
+fi
+
+mkdir $DOCKER_RELATIVE_PATH/bin
+cp "$BUILD_DIR"/src/dashd    $DOCKER_RELATIVE_PATH/bin/
+cp "$BUILD_DIR"/src/dash-cli $DOCKER_RELATIVE_PATH/bin/
+cp "$BUILD_DIR"/src/dash-tx  $DOCKER_RELATIVE_PATH/bin/
+strip $DOCKER_RELATIVE_PATH/bin/dashd
+strip $DOCKER_RELATIVE_PATH/bin/dash-cli
+strip $DOCKER_RELATIVE_PATH/bin/dash-tx
+
+docker build --pull -t "$DOCKER_IMAGE":"$DOCKER_TAG" -f $DOCKER_RELATIVE_PATH/Dockerfile docker
