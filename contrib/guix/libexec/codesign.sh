b66b56c3019fe7ab0c9f35dd9894c0ded4d9d420
MarcoFalke <falke.marco@gmail.com>
2021-12-31 12:08:39 +0100

    Merge bitcoin/bitcoin#23912: Insert and bump copyright headers
    1362d6173f863428de17c4017f4e3bb6883b0be8 scripted-diff: Insert missed copyright headers (Hennadii Stepanov)
    f47dda2c58b5d8d623e0e7ff4e74bc352dfa83d7 scripted-diff: Bump copyright headers (Hennadii Stepanov)
    c29105efdc3daac0e8c43138f7c0a50535556455 script: Fix copyright_header.py (Hennadii Stepanov)
    
    Pull request description:
    
      This PR is an alternative to #23903.
    
      It bumps the existing copyright headers as we did every year, and adds the missed copyright headers.
    
      A small fix has been applied to the `copyright_header.py` in order to prevent such weird bumping as `2021` --> `2021-2017`.
    
    ACKs for top commit:
      MarcoFalke:
        ACK 1362d6173f863428de17c4017f4e3bb6883b0be8
    
    Tree-SHA512: 204d970fe8c51546b26b8f03fe4297db8a9bef5101df851540b7b9eddbd3a09677ee81fdd882c60937d732407f42c9883165bd978272200cff8f90190f075905

--- contrib/guix/libexec/codesign.sh
+++ contrib/guix/libexec/codesign.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# Copyright (c) 2021 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
 export LC_ALL=C
 set -e -o pipefail
 export TZ=UTC
