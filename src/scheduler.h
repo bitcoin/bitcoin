e116e877cd811e0b009e993b97d34f57d8f3e074
MarcoFalke <falke.marco@gmail.com>
2021-12-31 12:08:39 +0100

    Merge #23912: Insert and bump copyright headers

--- src/scheduler.h
+++ src/scheduler.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2015-2020 The Bitcoin Core developers
+// Copyright (c) 2015-2021 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
--- src/script/syscoinconsensus.h
+++ src/script/syscoinconsensus.h
@@ -1,5 +1,5 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2018 The Bitcoin Core developers
+// Copyright (c) 2009-2020 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
