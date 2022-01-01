2e42050b7fc61201f202438e8cd4383a06eb98d5
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2022-01-01 13:00:07 +0100

    doc: fix undo data filename (s/undo???.dat/rev???.dat/)

--- src/validation.h
+++ src/validation.h
@@ -402,7 +402,7 @@ private:
     void FindFilesToPruneManual(std::set<int>& setFilesToPrune, int nManualPruneHeight, int chain_tip_height);
 
     /**
-     * Prune block and undo files (blk???.dat and undo???.dat) so that the disk space used is less than a user-defined target.
+     * Prune block and undo files (blk???.dat and rev???.dat) so that the disk space used is less than a user-defined target.
      * The user sets the target (in MB) on the command line or in config file.  This will be run on startup and whenever new
      * space is allocated in a block or undo file, staying below the target. Changing back to unpruned requires a reindex
      * (which in this case means the blockchain must be re-downloaded.)
