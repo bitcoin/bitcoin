95cbb40a86b91b425bc17c9910290c7ff16d606d
Niklas Gögge <n.goeggi@gmail.com>
2022-01-03 00:01:57 +0100

    [validation] Exempt UpdateTipLog from rate limiting
    UpdateTipLog logs everytime a new tip is activated. This occurs at an
    increased frequency during IBD and should therefore be exempt from rate
    limiting.

--- src/validation.cpp
+++ src/validation.cpp
@@ -2515,15 +2515,15 @@ static void UpdateTipLog(
 {
 
     AssertLockHeld(::cs_main);
-    LogPrintf("%s%s: new best=%s height=%d version=0x%08x log2_work=%f tx=%lu date='%s' progress=%f cache=%.1fMiB(%utxo)%s\n",
-        prefix, func_name,
-        tip->GetBlockHash().ToString(), tip->nHeight, tip->nVersion,
-        log(tip->nChainWork.getdouble()) / log(2.0), (unsigned long)tip->nChainTx,
-        FormatISO8601DateTime(tip->GetBlockTime()),
-        GuessVerificationProgress(params.TxData(), tip),
-        coins_tip.DynamicMemoryUsage() * (1.0 / (1 << 20)),
-        coins_tip.GetCacheSize(),
-        !warning_messages.empty() ? strprintf(" warning='%s'", warning_messages) : "");
+    LogPrint(BCLog::UNCONDITIONAL_ALWAYS, "%s%s: new best=%s height=%d version=0x%08x log2_work=%f tx=%lu date='%s' progress=%f cache=%.1fMiB(%utxo)%s\n",
+             prefix, func_name,
+             tip->GetBlockHash().ToString(), tip->nHeight, tip->nVersion,
+             log(tip->nChainWork.getdouble()) / log(2.0), (unsigned long)tip->nChainTx,
+             FormatISO8601DateTime(tip->GetBlockTime()),
+             GuessVerificationProgress(params.TxData(), tip),
+             coins_tip.DynamicMemoryUsage() * (1.0 / (1 << 20)),
+             coins_tip.GetCacheSize(),
+             !warning_messages.empty() ? strprintf(" warning='%s'", warning_messages) : "");
 }
 
 void CChainState::UpdateTip(const CBlockIndex* pindexNew)
