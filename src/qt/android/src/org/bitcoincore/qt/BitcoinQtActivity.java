efe815f7ca4eba2bd9f0af6ecbb3430256a051f9
fanquake <fanquake@gmail.com>
2022-01-04 10:08:18 +0800

    Merge bitcoin/bitcoin#23751: build, qt: No need to set inapplicable QPA backend for Android
    29e1794ba5350914ea2be8cba33d8d6d2c99760b build, qt: No need to set inapplicable QPA backend for Android (Hennadii Stepanov)
    
    Pull request description:
    
      The current workflow looks weird. At first, the inapplicable `xcb` QPA backend is set in Qt `configure` options. Then the correct `android` QPA backend is forced via the `QT_QPA_PLATFORM` environment variable.
    
      Using the default QPA backend, which is `android` for Android devices, is just fine.
    
    ACKs for top commit:
      icota:
        re-tACK 29e1794ba5350914ea2be8cba33d8d6d2c99760b
    
    Tree-SHA512: 08ed7d05209c1bedc1a71de5ea3be5d86b40319a164dceb9191f7a4dfe78f2f951778b90421335e73e71a798a57bdf046aa96876762d338b600037bd7ee27b52

--- src/qt/android/src/org/bitcoincore/qt/BitcoinQtActivity.java
+++ src/qt/android/src/org/bitcoincore/qt/BitcoinQtActivity.java
@@ -18,12 +18,6 @@ public class SyscoinQtActivity extends QtActivity
             syscoinDir.mkdir();
         }
 
-        try {
-            Os.setenv("QT_QPA_PLATFORM", "android", true);
-        } catch (ErrnoException e) {
-            e.printStackTrace();
-        }
-
         super.onCreate(savedInstanceState);
     }
 }
