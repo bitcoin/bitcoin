cb428e17700cb8599ecd0a94954ead7e76ebc6e5
MarcoFalke <falke.marco@gmail.com>
2022-01-06 17:15:14 +0100

    Merge bitcoin/bitcoin#23938: ci: Clone entire bitcoin-core/qa-assets repo only when run fuzzing
    0b7c55f157514e1845934fff3f9c66e862c526ca ci: Clone entire bitcoin-core/qa-assets repo only when run fuzzing (Hennadii Stepanov)
    
    Pull request description:
    
      This PR speeds up CI tasks that run unit tests but do not run fuzzing.
    
      On my machine:
      ```
      $ time git clone --depth=1 https://github.com/bitcoin-core/qa-assets
      Cloning into 'qa-assets'...
      remote: Enumerating objects: 289750, done.
      remote: Counting objects: 100% (289750/289750), done.
      remote: Compressing objects: 100% (207687/207687), done.
      remote: Total 289750 (delta 16863), reused 275449 (delta 12092), pack-reused 0
      Receiving objects: 100% (289750/289750), 1.39 GiB | 4.79 MiB/s, done.
      Resolving deltas: 100% (16863/16863), done.
      Updating files: 100% (294515/294515), done.
    
      real7m43,417s
      user2m39,771s
      sys0m43,272s
      ```
    
    ACKs for top commit:
      MarcoFalke:
        cr ACK 0b7c55f157514e1845934fff3f9c66e862c526ca
    
    Tree-SHA512: 8f8dd63e361c3d9c64a2b31f207f59beea7d2e4142363f7762ec724a61152323fda0c6b98a36386e15f2ca4ef3f7412da763baef65e862fc7f48cdeda8c40e09

--- ci/test/04_install.sh
+++ ci/test/04_install.sh
@@ -87,13 +87,17 @@ fi
 DOCKER_EXEC echo "Free disk space:"
 DOCKER_EXEC df -h
 
-if [ "$RUN_FUZZ_TESTS" = "true" ] || [ "$RUN_UNIT_TESTS" = "true" ] || [ "$RUN_UNIT_TESTS_SEQUENTIAL" = "true" ]; then
-  if [ ! -d "${DIR_QA_ASSETS}" ]; then
+if [ "$RUN_FUZZ_TESTS" = "true" ]; then
+  export DIR_FUZZ_IN=${DIR_QA_ASSETS}/fuzz_seed_corpus/
+  if [ ! -d "$DIR_FUZZ_IN" ]; then
     DOCKER_EXEC git clone --depth=1 https://github.com/bitcoin-core/qa-assets "${DIR_QA_ASSETS}"
   fi
-
-  export DIR_FUZZ_IN=${DIR_QA_ASSETS}/fuzz_seed_corpus/
+elif [ "$RUN_UNIT_TESTS" = "true" ] || [ "$RUN_UNIT_TESTS_SEQUENTIAL" = "true" ]; then
   export DIR_UNIT_TEST_DATA=${DIR_QA_ASSETS}/unit_test_data/
+  if [ ! -d "$DIR_UNIT_TEST_DATA" ]; then
+    DOCKER_EXEC mkdir -p "$DIR_UNIT_TEST_DATA"
+    DOCKER_EXEC curl --location --fail https://github.com/bitcoin-core/qa-assets/raw/main/unit_test_data/script_assets_test.json -o "${DIR_UNIT_TEST_DATA}/script_assets_test.json"
+  fi
 fi
 
 DOCKER_EXEC mkdir -p "${BASE_SCRATCH_DIR}/sanitizer-output/"
