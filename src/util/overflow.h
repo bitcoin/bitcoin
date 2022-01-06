5002c483dcb332f7112c48eaba8653a74d23cfdc
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2022-01-05 16:44:24 -0800

    Update overflow.h

--- src/util/overflow.h
+++ src/util/overflow.h
@@ -2,8 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_UTIL_OVERFLOW_H
-#define BITCOIN_UTIL_OVERFLOW_H
+#ifndef SYSCOIN_UTIL_OVERFLOW_H
+#define SYSCOIN_UTIL_OVERFLOW_H
 
 #include <limits>
 #include <type_traits>
@@ -28,4 +28,4 @@ template <class T>
     return i + j;
 }
 
-#endif // BITCOIN_UTIL_OVERFLOW_H
+#endif // SYSCOIN_UTIL_OVERFLOW_H
