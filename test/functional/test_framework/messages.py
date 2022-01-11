f30041c9143d0added18105c9f0c4ae3f340efbc
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2022-01-11 04:02:49 +0100

    test: create txs with current `nVersion` (2) by default
    This enables testing of BIP68 without the need of explicitly
    setting nVersion to 2. This is e.g. useful for transactions
    created with MiniWallet.

--- test/functional/test_framework/messages.py
+++ test/functional/test_framework/messages.py
@@ -509,7 +509,7 @@ class CTransaction:
 
     def __init__(self, tx=None):
         if tx is None:
-            self.nVersion = 1
+            self.nVersion = 2
             self.vin = []
             self.vout = []
             self.wit = CTxWitness()
