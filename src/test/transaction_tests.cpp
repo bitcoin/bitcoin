168a74e6caacbb4b7e98e6a375675a3bb8f6b432
MarcoFalke <falke.marco@gmail.com>
2022-01-20 18:10:47 +0100

    Merge bitcoin/bitcoin#23629: refactor tests to fix ubsan suppressions
    faedb111d2df78eabddaeccaba42f47572388bc7 refactor tests to fix ubsan suppressions (MarcoFalke)
    
    Pull request description:
    
      The ubsan suppressions for test files have several issues:
    
      * They bloat the suppressions file, distracting from real bugs
      * They are file-wide, thus suppressing any other (newly introduced) issues in the same file
      * Some of them are causing compile issues with -Wimplicit-int-conversion
    
      Fix all issues by making the integer truncations or sign changes explicit.
    
      This is a refactor that shouldn't change the test binary unless compiled with sanitizers.
    
    ACKs for top commit:
      jonatack:
        ACK faedb111d2df78eabddaeccaba42f47572388bc7 per `git range-diff 1824644a fa9bab0 faedb11` and clang 13 debug build sanity check
    
    Tree-SHA512: a1a6aaf2cf52562a49220375deb10062b388c1ccb7ddcd9ef2a8e6b9873961c5b285704799589b7dd5579f3db8692ba72a9d622172b4e4d2423534cd550e8859

--- src/test/transaction_tests.cpp
+++ src/test/transaction_tests.cpp
@@ -181,7 +181,7 @@ BOOST_AUTO_TEST_CASE(tx_valid)
                     break;
                 }
 
-                mapprevOutScriptPubKeys[COutPoint(uint256S(vinput[0].get_str()), vinput[1].get_int())] = ParseScript(vinput[2].get_str());
+                mapprevOutScriptPubKeys[COutPoint{uint256S(vinput[0].get_str()), uint32_t(vinput[1].get_int())}] = ParseScript(vinput[2].get_str());
             }
             if (!fValid)
             {
@@ -265,7 +265,7 @@ BOOST_AUTO_TEST_CASE(tx_invalid)
                     break;
                 }
 
-                mapprevOutScriptPubKeys[COutPoint(uint256S(vinput[0].get_str()), vinput[1].get_int())] = ParseScript(vinput[2].get_str());
+                mapprevOutScriptPubKeys[COutPoint{uint256S(vinput[0].get_str()), uint32_t(vinput[1].get_int())}] = ParseScript(vinput[2].get_str());
             }
             if (!fValid)
             {
