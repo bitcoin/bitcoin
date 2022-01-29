a5b66738f19a0ad46d7bc287e0db08552c5a1fec
brunoerg <brunoely.gc@gmail.com>
2022-01-29 15:39:12 -0300

    test: add wtxid in expected_fields for wallet_basic

--- test/functional/wallet_basic.py
+++ test/functional/wallet_basic.py
@@ -668,7 +668,7 @@ class WalletTest(BitcoinTestFramework):
                                  "category": baz["category"],
                                  "vout":     baz["vout"]}
         expected_fields = frozenset({'amount', 'bip125-replaceable', 'confirmations', 'details', 'fee',
-                                     'hex', 'time', 'timereceived', 'trusted', 'txid', 'walletconflicts'})
+                                     'hex', 'time', 'timereceived', 'trusted', 'txid', 'wtxid', 'walletconflicts'})
         verbose_field = "decoded"
         expected_verbose_fields = expected_fields | {verbose_field}
 
