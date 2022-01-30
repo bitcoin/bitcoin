17b65055bad720a443d9d6a39fa6461d6516e5de
David Burkett <davidburkett38@gmail.com>
2022-01-30 01:36:29 -0500

    MWEB: Fixing gitian build

--- depends/packages/libfmt.mk
+++ depends/packages/libfmt.mk
@@ -4,10 +4,6 @@ $(package)_download_path=https://github.com/fmtlib/fmt/archive/
 $(package)_file_name=$($(package)_version).tar.gz
 $(package)_sha256_hash=5cae7072042b3043e12d53d50ef404bbb76949dad1de368d7f993a15c8c05ecc
 
-define $(package)_set_vars
-$(package)_config_opts_x86_64_mingw32=-DCMAKE_TOOLCHAIN_FILE=$(BASEDIR)/cmake/mingw-w64-x86_64.cmake
-endef
-
 define $(package)_config_cmds
   $($(package)_cmake) -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=true
 endef
