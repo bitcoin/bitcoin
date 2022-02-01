fadcd031390dd4588bbb1c07e5020a7131312050
MarcoFalke <falke.marco@gmail.com>
2022-02-01 16:08:57 +0100

    Fix unsigned integer overflow in LoadMempool

--- src/validation.cpp
+++ src/validation.cpp
@@ -4493,7 +4493,8 @@ bool LoadMempool(CTxMemPool& pool, CChainState& active_chainstate, FopenFn mocka
         }
         uint64_t num;
         file >> num;
-        while (num--) {
+        while (num) {
+            --num;
             CTransactionRef tx;
             int64_t nTime;
             int64_t nFeeDelta;
