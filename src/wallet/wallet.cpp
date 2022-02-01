f2c3e4f8ec8d497972280a888a4db53bde40677c
Andrew Chow <achow101-github@achow101.com>
2022-02-01 14:42:02 -0500

    Revert "Add to spends only transcations from me" #24083

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -958,9 +958,7 @@ CWalletTx* CWallet::AddToWallet(CTransactionRef tx, const TxState& state, const 
         wtx.nOrderPos = IncOrderPosNext(&batch);
         wtx.m_it_wtxOrdered = wtxOrdered.insert(std::make_pair(wtx.nOrderPos, &wtx));
         wtx.nTimeSmart = ComputeTimeSmart(wtx, rescanning_old_block);
-        if (IsFromMe(*tx.get())) {
-            AddToSpends(hash);
-        }
+        AddToSpends(hash, &batch);
         // SYSCOIN
         auto mnList = deterministicMNManager->GetListAtChainTip();
         for(unsigned int i = 0; i < wtx.tx->vout.size(); ++i) {
