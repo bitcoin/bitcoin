95a29e2c8b84836633aced859c8afa48241ede8c
MarcoFalke <falke.marco@gmail.com>
2022-02-01 11:28:35 +0100

    Merge bitcoin/bitcoin#24212: ci: Bump CentOS 8 image
    fafc55a489d8f654dd7732b699c9bf51b5136b4b ci: Use dash when building depends in centos build (MarcoFalke)
    fa33236e77974de13a329c8eda34ebc842d1aa01 scripted-diff: Rename DOCKER_EXEC to CI_EXEC (MarcoFalke)
    fa5457e64adfc6060382bd36c45e95be6ea693ef ci: Bump CentOS 8 image (MarcoFalke)
    
    Pull request description:
    
      CentOS 8 was just shut down:
    
      ```
      $ pob centos:8
      [root_e606059da4d9 /]# dnf update
      Failed to set locale, defaulting to C.UTF-8
      CentOS Linux 8 - AppStream
      Error: Failed to download metadata for repo 'appstream': Cannot prepare internal mirrorlist: No URLs in mirrorlist
      ```
    
      Fix that by bumping the image.
    
    ACKs for top commit:
      Sjors:
        utACK fafc55a with the caveat that I've never used CentOS.
    
    Tree-SHA512: 741eb606c9f67cd218e41ce98460606d8594ff98148636c28ac20bae2809e3ee4a18c97aa3c849630f0a6ac30255c7423253b4309ae2d7cdd5c6624cc6836b95

--- ci/test/05_before_script.sh
+++ ci/test/05_before_script.sh
@@ -10,28 +10,28 @@ export LC_ALL=C.UTF-8
 if [ "$CI_OS_NAME" == "macos" ]; then
   echo > "${HOME}/Library/Application Support/DashCore"
 else
-  DOCKER_EXEC echo \> \$HOME/.dashcore
+  CI_EXEC echo \> \$HOME/.dashcore
 fi
 
-DOCKER_EXEC mkdir -p "${DEPENDS_DIR}/SDKs" "${DEPENDS_DIR}/sdk-sources"
+CI_EXEC mkdir -p "${DEPENDS_DIR}/SDKs" "${DEPENDS_DIR}/sdk-sources"
 
 if [ -n "$XCODE_VERSION" ] && [ ! -f "$OSX_SDK_PATH" ]; then
-  DOCKER_EXEC curl --location --fail "${SDK_URL}/${OSX_SDK_BASENAME}" -o "$OSX_SDK_PATH"
+  CI_EXEC curl --location --fail "${SDK_URL}/${OSX_SDK_BASENAME}" -o "$OSX_SDK_PATH"
 fi
 if [ -n "$XCODE_VERSION" ] && [ -f "$OSX_SDK_PATH" ]; then
-  DOCKER_EXEC tar -C "${DEPENDS_DIR}/SDKs" -xf "$OSX_SDK_PATH"
+  CI_EXEC tar -C "${DEPENDS_DIR}/SDKs" -xf "$OSX_SDK_PATH"
 fi
 if [[ $HOST = *-mingw32 ]]; then
-  DOCKER_EXEC update-alternatives --set "${HOST}-g++" \$\(which "${HOST}-g++-posix"\)
+  CI_EXEC update-alternatives --set "${HOST}-g++" \$\(which "${HOST}-g++-posix"\)
 fi
 if [ -z "$NO_DEPENDS" ]; then
-  if [[ $DOCKER_NAME_TAG == centos* ]]; then
-    SHELL_OPTS="CONFIG_SHELL=/bin/bash"
+  if [[ $DOCKER_NAME_TAG == *centos* ]]; then
+    SHELL_OPTS="CONFIG_SHELL=/bin/dash"
   else
     SHELL_OPTS="CONFIG_SHELL="
   fi
-  DOCKER_EXEC "$SHELL_OPTS" make "$MAKEJOBS" -C depends HOST="$HOST" "$DEP_OPTS" LOG=1
+  CI_EXEC "$SHELL_OPTS" make "$MAKEJOBS" -C depends HOST="$HOST" "$DEP_OPTS" LOG=1
 fi
 if [ -n "$PREVIOUS_RELEASES_TO_DOWNLOAD" ]; then
-  DOCKER_EXEC test/get_previous_releases.py -b -t "$PREVIOUS_RELEASES_DIR" "${PREVIOUS_RELEASES_TO_DOWNLOAD}"
+  CI_EXEC test/get_previous_releases.py -b -t "$PREVIOUS_RELEASES_DIR" "${PREVIOUS_RELEASES_TO_DOWNLOAD}"
 fi
