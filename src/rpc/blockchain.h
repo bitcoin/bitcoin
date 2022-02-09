c45cf059a77898bcbbe79951850d51160c9b8632
w0xlt <94266259+w0xlt@users.noreply.github.com>
2022-02-08 22:41:35 -0300

    rpc: allow dumptxoutset to dump human-readable data
    Co-authored-by: Shashwat Vangani <shaavan.github@gmail.com>
    Co-authored-by: Luke Dashjr <luke-jr+git@utopios.org>

--- src/rpc/blockchain.h
+++ src/rpc/blockchain.h
@@ -5,8 +5,10 @@
 #ifndef BITCOIN_RPC_BLOCKCHAIN_H
 #define BITCOIN_RPC_BLOCKCHAIN_H
 
+#include <coins.h>
 #include <consensus/amount.h>
 #include <core_io.h>
+#include <span.h>
 #include <streams.h>
 #include <sync.h>
 #include <util/fs.h>
@@ -26,6 +28,7 @@ struct NodeContext;
 } // namespace node
 
 static constexpr int NUM_GETBLOCKSTATS_PERCENTILES = 5;
+using coinascii_cb_t = std::function<std::string(const COutPoint&, const Coin&)>;
 
 /**
  * Get the difficulty of the net wrt to the given block index.
