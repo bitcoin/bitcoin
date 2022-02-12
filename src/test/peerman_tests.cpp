9c04c32101cd458c336de1d66ad296dc7f1cf35d
John Newbery <john@johnnewbery.com>
2022-02-12 12:12:02 +0000

    [test] Add unit test for ping/pong logic

--- src/test/peerman_tests.cpp
+++ src/test/peerman_tests.cpp
@@ -13,6 +13,7 @@
 #include <script/signingprovider.h>
 #include <script/standard.h>
 #include <serialize.h>
+#include <span.h>
 #include <test/util/net.h>
 #include <test/util/setup_common.h>
 #include <util/string.h>
@@ -22,6 +23,7 @@
 #include <validation.h>
 
 #include <array>
+#include <optional>
 #include <stdint.h>
 
 #include <boost/test/unit_test.hpp>
@@ -31,6 +33,12 @@ static CService ip(uint32_t i)
     struct in_addr s;
     s.s_addr = i;
     return CService(CNetAddr(s), Params().GetDefaultPort());
+}
+
+static void AdvanceMockTime(int64_t delta)
+{
+    auto time_before{GetMockTime()};
+    SetMockTime(time_before + std::chrono::seconds(delta));
 }
 
 /** Mock the connections interface. */
@@ -63,17 +71,30 @@ public:
 
     void NodeReceiveMsgBytes(CNode& node, Span<const uint8_t> msg_bytes, bool& complete) const;
     bool ReceiveMsgFrom(CNode& node, CSerializedNetMsg& ser_msg) const;
+    void OnPing(CDataStream& data);
 
     virtual ~ConnectionsInterfaceMock() {}
 
     /** Count of number of each message type sent */
     std::map<std::string, uint64_t> m_message_types_sent;
+    /** Most recent ping nonce received. */
+    uint64_t m_ping_nonce{0};
+
 };
 
 void ConnectionsInterfaceMock::PushMessage(CNode* pnode, CSerializedNetMsg&& msg)
 {
     BOOST_TEST_MESSAGE(strprintf("sending message %s to peer %d", msg.m_type, pnode->GetId()));
     m_message_types_sent[msg.m_type]++;
+
+    CDataStream data{msg.data, SER_NETWORK, PROTOCOL_VERSION};
+    if (msg.m_type == NetMsgType::PING) OnPing(data);
+}
+
+void ConnectionsInterfaceMock::OnPing(CDataStream& data)
+{
+    data >> m_ping_nonce;
+    BOOST_TEST_MESSAGE(strprintf("received ping. Nonce:%d", m_ping_nonce));
 }
 
 void ConnectionsInterfaceMock::NodeReceiveMsgBytes(CNode& node, Span<const uint8_t> msg_bytes, bool& complete) const
@@ -181,6 +202,104 @@ BOOST_AUTO_TEST_CASE(version_handshake)
     BOOST_CHECK_EQUAL(connman->m_message_types_sent[NetMsgType::SENDCMPCT], 1);
     BOOST_CHECK_EQUAL(connman->m_message_types_sent[NetMsgType::GETHEADERS], 1);
     BOOST_CHECK_EQUAL(connman->m_message_types_sent[NetMsgType::FEEFILTER], 1);
+
+    peerman->FinalizeNode(node);
+}
+
+static void CheckPingTimes(CNode& node, PeerManager& peerman,
+                           std::chrono::microseconds min_ping_time, std::chrono::microseconds last_ping_time, std::chrono::microseconds ping_wait)
+{
+    // Check min and last ping times
+    BOOST_CHECK_EQUAL(node.m_min_ping_time.load().count(), min_ping_time.count());
+    BOOST_CHECK_EQUAL(node.m_last_ping_time.load().count(), last_ping_time.count());
+
+    // Check if and how long current ping has been pending
+    CNodeStateStats stats;
+    peerman.GetNodeStateStats(node.GetId(), stats);
+    BOOST_CHECK_EQUAL(stats.m_ping_wait.count(), ping_wait.count());
+}
+
+static void SendPong(CNode& node, ConnectionsInterfaceMock& connman,
+                     PeerManager& peerman, std::optional<int64_t> nonce = {})
+{
+    const CNetMsgMaker mm{0};
+    CSerializedNetMsg msg_pong = nonce ? mm.Make(NetMsgType::PONG, nonce.value()) : mm.Make(NetMsgType::PONG);
+    SendMessage(connman, peerman, node, msg_pong);
+}
+
+BOOST_AUTO_TEST_CASE(ping)
+{
+    // See PING_INTERVAL in net_processing.cpp
+    constexpr int64_t PING_INTERVAL{2 * 60};
+
+    auto connman = std::make_unique<ConnectionsInterfaceMock>();
+    auto peerman = PeerManager::make(*connman, *m_node.addrman, nullptr,
+                                     *m_node.chainman, *m_node.mempool, false);
+
+    // Mock an outbound peer
+    CNode node{/*id=*/0, /*sock=*/nullptr, CAddress(), /*nKeyedNetGroupIn=*/0, /*nLocalHostNonceIn=*/0, CAddress(),
+               /*addrNameIn=*/"", ConnectionType::OUTBOUND_FULL_RELAY, /*inbound_onion=*/false};
+
+    peerman->InitializeNode(node, /*our_services=*/ServiceFlags(NODE_NETWORK | NODE_WITNESS));
+
+    // Use mock time to control pings from node
+    SetMockTime(GetTime());
+
+    Handshake(*connman, *peerman, node, /*their_services=*/ServiceFlags(NODE_NETWORK | NODE_WITNESS));
+
+    BOOST_TEST_MESSAGE("Advanced mocktime and check that ping is sent after connection is established");
+    AdvanceMockTime(1);
+    BOOST_CHECK_EQUAL(connman->m_message_types_sent[NetMsgType::PING], 1);
+
+    // The ping nonce should be non-zero
+    BOOST_CHECK(connman->m_ping_nonce != 0);
+
+    // No pong response has been received yet, and current ping is outstanding
+    CheckPingTimes(node, *peerman, std::chrono::microseconds::max(), 0us, 1s);
+
+    BOOST_TEST_MESSAGE("Reply without nonce cancels ping");
+    SendPong(node, *connman, *peerman, std::nullopt);
+
+    // Ping metrics have not been updated on the CNode object, and there is no ping outstanding
+    CheckPingTimes(node, *peerman, std::chrono::microseconds::max(), 0us, 0us);
+
+    BOOST_TEST_MESSAGE("Reply without ping");
+    SendPong(node, *connman, *peerman, 0);
+
+    // Ping metrics have not been updated on the CNode object, and there is no ping outstanding
+    CheckPingTimes(node, *peerman, std::chrono::microseconds::max(), 0us, 0us);
+
+    // Wait for next ping
+    AdvanceMockTime(PING_INTERVAL + 1);
+    connman->m_message_types_sent[NetMsgType::PING] = 0;
+    WITH_LOCK(NetEventsInterface::g_msgproc_mutex, peerman->SendMessages(&node));
+    BOOST_CHECK_EQUAL(connman->m_message_types_sent[NetMsgType::PING], 1);
+
+    BOOST_TEST_MESSAGE("Reply with wrong nonce does not cancel ping");
+    SendPong(node, *connman, *peerman, connman->m_ping_nonce + 1);
+    AdvanceMockTime(1);
+
+    // Ping metrics have not been updated on the CNode object, and there is a ping outstanding
+    CheckPingTimes(node, *peerman, std::chrono::microseconds::max(), 0us, 1s);
+
+    BOOST_TEST_MESSAGE("Reply with zero nonce cancels ping");
+    SendPong(node, *connman, *peerman, 0);
+
+    // Ping metrics have not been updated on the CNode object, and there is no ping outstanding
+    CheckPingTimes(node, *peerman, std::chrono::microseconds::max(), 0us, 0us);
+
+    // Wait for next ping
+    AdvanceMockTime(PING_INTERVAL + 1);
+    connman->m_message_types_sent[NetMsgType::PING] = 0;
+    WITH_LOCK(NetEventsInterface::g_msgproc_mutex, peerman->SendMessages(&node));
+    BOOST_CHECK_EQUAL(connman->m_message_types_sent[NetMsgType::PING], 1);
+
+    // Send pong with correct nonce
+    AdvanceMockTime(1);
+    SendPong(node, *connman, *peerman, connman->m_ping_nonce);
+
+    // Ping metrics have been updated on the CNode object, and there is no ping outstanding
+    CheckPingTimes(node, *peerman, 1s, 1s, 0us);
 
     peerman->FinalizeNode(node);
 }
