60b7e35979ceb655a7c76ae69405fa0c2c9eefb0
fanquake <fanquake@gmail.com>
2022-02-14 09:55:37 +0000

    Merge #24301: build: header-only Boost

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -25,8 +25,6 @@ usdt_linux_packages=systemtap
 
 darwin_native_packages = native_ds_store native_mac_alias
 
-$(host_arch)_$(host_os)_native_packages += native_b2
-
 ifneq ($(build_os),darwin)
 darwin_native_packages += native_cctools native_libtapi native_libdmg-hfsplus
 
