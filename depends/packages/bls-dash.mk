0b3582ddd797848c3978e7f0f14d7b9949171209
Kittywhiskers Van Gogh <63189531+kittywhiskers@users.noreply.github.com>
2022-02-15 20:42:11 +0530

    depends: bump bls-dash to 1.2.0, cmake to 3.22.2, relic to aecdcae (#4681)
    * depends: bump cmake to 3.22.2
    
    Contains additional patches to mitigate errors
    
    ```
    -- Could NOT find OpenSSL, try to set the path to OpenSSL root folder in the system variable OPENSSL_ROOT_DIR (missing: OPENSSL_CRYPTO_LIBRARY OPENSSL_INCLUDE_DIR)
    CMake Error at Utilities/cmcurl/CMakeLists.txt:562 (message):
      Could not find OpenSSL.  Install an OpenSSL development package or
      configure CMake with -DCMAKE_USE_OPENSSL=OFF to build without OpenSSL.
    ```
    
    * depends: bump bls-dash to 1.2.0 (https://github.com/kittywhiskers/bls-signatures/commit/979f423234d9d4ace4bf2805ea1a99ba4d021f48)
    
    * depends: bump relic to https://github.com/relic-toolkit/relic/commit/aecdcae7956f542fbee2392c1f0feb0a8ac41dc5

--- depends/packages/bls-dash.mk
+++ depends/packages/bls-dash.mk
@@ -1,20 +1,19 @@
 package=bls-dash
-$(package)_version=1.1.0
+$(package)_version=1.2.0
 $(package)_download_path=https://github.com/dashpay/bls-signatures/archive
 $(package)_download_file=$($(package)_version).tar.gz
 $(package)_file_name=$(package)-$($(package)_download_file)
 $(package)_build_subdir=build
-$(package)_sha256_hash=276c8573104e5f18bb5b9fd3ffd49585dda5ba5f6de2de74759dda8ca5a9deac
+$(package)_sha256_hash=94e49f3eaa29bc1f354cd569c00f4f4314d1c8ab4758527c248b67da9686135a
 $(package)_dependencies=gmp cmake
-$(package)_patches=gcc_alignment_cast.patch
 $(package)_darwin_triplet=x86_64-apple-darwin19
 
-$(package)_relic_version=3a23142be0a5510a3aa93cd6c76fc59d3fc732a5
+$(package)_relic_version=aecdcae7956f542fbee2392c1f0feb0a8ac41dc5
 $(package)_relic_download_path=https://github.com/relic-toolkit/relic/archive
 $(package)_relic_download_file=$($(package)_relic_version).tar.gz
-$(package)_relic_file_name=relic-toolkit-$($(package)_relic_download_file)
+$(package)_relic_file_name=relic-$($(package)_relic_download_file)
 $(package)_relic_build_subdir=relic
-$(package)_relic_sha256_hash=ddad83b1406985a1e4703bd03bdbab89453aa700c0c99567cf8de51c205e5dde
+$(package)_relic_sha256_hash=f2de6ebdc9def7077f56c83c8b06f4da5bacc36b709514bd550a92a149e9fa1d
 
 $(package)_extra_sources=$($(package)_relic_file_name)
 
@@ -56,13 +55,8 @@ define $(package)_set_vars
 endef
 
 define $(package)_preprocess_cmds
-  sed -i.old "s|GIT_REPOSITORY https://github.com/relic-toolkit/relic.git|URL \"../../relic-toolkit-$($(package)_relic_version).tar.gz\"|" src/CMakeLists.txt && \
-  sed -i.old "s|GIT_TAG        .*RELIC_GIT_TAG.*||" src/CMakeLists.txt && \
-  tar xzf relic-toolkit-$($(package)_relic_version).tar.gz relic-$($(package)_relic_version)/src/md/blake2.h && \
-  patch -p1 relic-$($(package)_relic_version)/src/md/blake2.h < $($(package)_patch_dir)/gcc_alignment_cast.patch && \
-  gunzip relic-toolkit-$($(package)_relic_version).tar.gz && \
-  tar rf relic-toolkit-$($(package)_relic_version).tar relic-$($(package)_relic_version)/src/md/blake2.h && \
-  gzip relic-toolkit-$($(package)_relic_version).tar
+  sed -i.old "s|GIT_REPOSITORY https://github.com/Chia-Network/relic.git|URL \"../../relic-$($(package)_relic_version).tar.gz\"|" CMakeLists.txt && \
+  sed -i.old "s|RELIC_GIT_TAG \".*\"|RELIC_GIT_TAG \"\"|" CMakeLists.txt
 endef
 
 define $(package)_config_cmds
