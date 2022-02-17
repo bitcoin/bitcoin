fd145549cb693d28f810df98497e6821943bf4e7
fanquake <fanquake@gmail.com>
2022-02-17 16:28:31 +0000

    Merge bitcoin/bitcoin#24338: util: Work around libstdc++ create_directories issue
    b223c3c21e89f6af76b5401413880923f7c444d6 test: Add functional test for symlinked blocks directory (laanwj)
    ddb75c2e87a60ed24065bdf0c3bfabf4e058cef1 test: Add fs_tests/create_directories unit test (Hennadii Stepanov)
    1f46b6e46e1454b91ff7ceb31853bc440952f8eb util: Work around libstdc++ create_directories issue (laanwj)
    
    Pull request description:
    
      Work around libstdc++ issue [PR101510] with create_directories where the leaf already exists as a symlink. Fixes #24257, introduced by the switch to `std::filesystem`. It is meant to be more thorough than #24266, which worked around one instance of the problem.
    
      The issue was [fixed upstream](https://gcc.gnu.org/git/?p=gcc.git;a=commit;h=124eaa50e0a34f5f89572c1aa812c50979da58fc), but unfortunately we'll have to carry a fix for it for a while.
    
      This introduces a function `fs::create_directories` which wraps
      `std::filesystem::create_directories`. This allows easiliy reverting the
      workaround when it is no longer necessary.
    
    ACKs for top commit:
      jonatack:
        re-ACK b223c3c21e89f6af76b5401413880923f7c444d6 per `git range-diff df08250 67019cd b223c3c`
      hebasto:
        re-ACK b223c3c21e89f6af76b5401413880923f7c444d6
      w0xlt:
        re-ACK b223c3c
      vasild:
        ACK b223c3c21e89f6af76b5401413880923f7c444d6
    
    Tree-SHA512: 028321717c8b10d16185c3711b35da6b05fb7aa31cee1c8c7e754e92bf5a0b02719a3785cd0f6f8bf052b3bd759f644af212320672baabc9e44e0b93ba464abc

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -333,6 +333,7 @@ BASE_SCRIPTS = [
     'rpc_getdescriptorinfo.py',
     'rpc_mempool_entry_fee_fields_deprecation.py',
     'rpc_help.py',
+    'feature_dirsymlinks.py',
     'feature_help.py',
     'feature_shutdown.py',
     'feature_sporks.py',
