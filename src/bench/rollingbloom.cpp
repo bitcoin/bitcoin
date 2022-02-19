e1fcf8dc148615842341726c6aa35bb00c35f1d0
phyBrackets <singh.shivamsingh2003@gmail.com>
2022-02-19 15:12:01 +0530

    Remove the deduplicating of data in rollingbloom.cpp

--- src/bench/rollingbloom.cpp
+++ src/bench/rollingbloom.cpp
@@ -5,24 +5,20 @@
 
 #include <bench/bench.h>
 #include <common/bloom.h>
+#include <crypto/common.h>
 
 static void RollingBloom(benchmark::Bench& bench)
 {
     CRollingBloomFilter filter(120000, 0.000001);
     std::vector<unsigned char> data(32);
+    unsigned char* dataPtr = data.data();
     uint32_t count = 0;
     bench.run([&] {
         count++;
-        data[0] = count & 0xFF;
-        data[1] = (count >> 8) & 0xFF;
-        data[2] = (count >> 16) & 0xFF;
-        data[3] = (count >> 24) & 0xFF;
+        WriteLE32(dataPtr, count);
         filter.insert(data);
 
-        data[0] = (count >> 24) & 0xFF;
-        data[1] = (count >> 16) & 0xFF;
-        data[2] = (count >> 8) & 0xFF;
-        data[3] = count & 0xFF;
+        WriteBE32(dataPtr, count);
         filter.contains(data);
     });
 }
