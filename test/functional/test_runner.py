2b1e71526f538b2174fad263f643121a971aa843
Kittywhiskers Van Gogh <63189531+kittywhiskers@users.noreply.github.com>
2022-02-26 16:17:21 +0530

    merge bitcoin#18378: Bugfix & simplify bn2vch using int.to_bytes

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -237,6 +237,7 @@ BASE_SCRIPTS = [
     'feature_config_args.py',
     'rpc_help.py',
     'feature_help.py',
+    'framework_test_script.py',
     # Don't append tests at the end to avoid merge conflicts
     # Put them in a random line within the section that fits their approximate run-time
 ]
@@ -617,7 +618,7 @@ class TestResult():
 def check_script_prefixes():
     """Check that test scripts start with one of the allowed name prefixes."""
 
-    good_prefixes_re = re.compile("(example|feature|interface|mempool|mining|p2p|rpc|wallet|tool)_")
+    good_prefixes_re = re.compile("^(example|feature|interface|mempool|mining|p2p|rpc|wallet|tool|framework_test)_")
     bad_script_names = [script for script in ALL_SCRIPTS if good_prefixes_re.match(script) is None]
 
     if bad_script_names:
