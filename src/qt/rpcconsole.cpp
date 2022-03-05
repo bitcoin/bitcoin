9879322b9934d739f38a323f0eb1ec24a3eb474f
UdjinM6 <UdjinM6@users.noreply.github.com>
2022-03-05 23:49:55 +0300

    qt: restore "wallet path on repair tab" logic

--- src/qt/rpcconsole.cpp
+++ src/qt/rpcconsole.cpp
@@ -504,6 +504,12 @@ RPCConsole::RPCConsole(interfaces::Node& node, QWidget* parent, Qt::WindowFlags 
     connect(ui->btn_upgradewallet, &QPushButton::clicked, this, &RPCConsole::walletUpgrade);
     connect(ui->btn_reindex, &QPushButton::clicked, this, &RPCConsole::walletReindex);
 
+#ifdef ENABLE_WALLET
+    std::string walletPath = GetDataDir().string();
+    walletPath += QDir::separator().toLatin1() + gArgs.GetArg("-wallet", "wallet.dat");
+    ui->wallet_path->setText(QString::fromStdString(walletPath));
+#endif
+
     // Register RPC timer interface
     rpcTimerInterface = new QtRPCTimerInterface();
     // avoid accidentally overwriting an existing, non QTThread
