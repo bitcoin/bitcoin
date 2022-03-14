038d2a607f93a31be76f3a8210a5385996012783
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2022-03-14 12:21:43 +0100

    test: add test for signet miner script

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -145,6 +145,8 @@ BASE_SCRIPTS = [
     'wallet_txn_doublespend.py --mineblock',
     'tool_wallet.py --legacy-wallet',
     'tool_wallet.py --descriptors',
+    'tool_signet_miner.py --legacy-wallet',
+    'tool_signet_miner.py --descriptors',
     'wallet_txn_clone.py',
     'wallet_txn_clone.py --segwit',
     'rpc_getchaintips.py',
