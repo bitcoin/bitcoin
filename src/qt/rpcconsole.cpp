d8641f04e4350755e5264e55730392730ab7c9ee
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-03-21 10:01:22 +0100

    qt: Use human-readable strings in preference to hard-coded integers
    This is recommended by Qt docs.
    See: https://doc.qt.io/qt-5/qkeysequence.html#details
    
    Also this change avoids -Wdeprecated-enum-enum-conversion warnings.

--- src/qt/rpcconsole.cpp
+++ src/qt/rpcconsole.cpp
@@ -40,6 +40,7 @@
 #include <QDateTime>
 #include <QFont>
 #include <QKeyEvent>
+#include <QKeySequence>
 #include <QLatin1String>
 #include <QLocale>
 #include <QMenu>
@@ -1353,10 +1354,10 @@ QString RPCConsole::tabTitle(TabTypes tab_type) const
 QKeySequence RPCConsole::tabShortcut(TabTypes tab_type) const
 {
     switch (tab_type) {
-    case TabTypes::INFO: return QKeySequence(Qt::CTRL + Qt::Key_I);
-    case TabTypes::CONSOLE: return QKeySequence(Qt::CTRL + Qt::Key_T);
-    case TabTypes::GRAPH: return QKeySequence(Qt::CTRL + Qt::Key_N);
-    case TabTypes::PEERS: return QKeySequence(Qt::CTRL + Qt::Key_P);
+    case TabTypes::INFO: return QKeySequence(tr("Ctrl+I"));
+    case TabTypes::CONSOLE: return QKeySequence(tr("Ctrl+T"));
+    case TabTypes::GRAPH: return QKeySequence(tr("Ctrl+N"));
+    case TabTypes::PEERS: return QKeySequence(tr("Ctrl+P"));
     } // no default case, so the compiler can warn about missing cases
 
     assert(false);
