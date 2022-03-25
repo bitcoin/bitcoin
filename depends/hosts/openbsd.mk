7449f8cabb7e1d914cb9eed5fed19ddec951be25
fanquake <fanquake@gmail.com>
2022-03-25 08:28:58 +0000

    Merge bitcoin/bitcoin#24604: build: fix copypasta in OpenBSD C{XX} flags
    28f17c1a6de7b50482d8f55fe03698126862e8eb build: fix copypasta in OpenBSD C{XX} flags (fanquake)
    
    Pull request description:
    
      Introduced in #23998.
    
    ACKs for top commit:
      hebasto:
        ACK 28f17c1a6de7b50482d8f55fe03698126862e8eb, I have reviewed the code and it looks OK, not tested on OpenBSD though.
    
    Tree-SHA512: d905161534075f518c8924d3c42cca7ff8d4898e559f1daa9bd03dac95b109b2c3e76790fb8bc65b9e45e8a59566825afbf4dc3734ad74617dfdf797430e486b

--- depends/hosts/openbsd.mk
+++ depends/hosts/openbsd.mk
@@ -1,11 +1,11 @@
 openbsd_CFLAGS=-pipe
-openbsd_CFLAGS_CXXFLAGS=$(openbsd_CFLAGS)
+openbsd_CXXFLAGS=$(openbsd_CFLAGS)
 
-openbsd_CFLAGS_release_CFLAGS=-O2
-openbsd_CFLAGS_release_CXXFLAGS=$(openbsd_release_CFLAGS)
+openbsd_release_CFLAGS=-O2
+openbsd_release_CXXFLAGS=$(openbsd_release_CFLAGS)
 
-openbsd_CFLAGS_debug_CFLAGS=-O1
-openbsd_CFLAGS_debug_CXXFLAGS=$(openbsd_debug_CFLAGS)
+openbsd_debug_CFLAGS=-O1
+openbsd_debug_CXXFLAGS=$(openbsd_debug_CFLAGS)
 
 ifeq (86,$(findstring 86,$(build_arch)))
 i686_openbsd_CC=clang -m32
