3dcb4b3a16025543ca719084875676271330dc90
John Oseni <johnoseni26@gmail.com>
2022-03-28 10:56:36 +0100

    added full explanation to the bitcoin core codes

--- src/coins.cpp
+++ src/coins.cpp
@@ -2,6 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+// import other files in the codes
+
 #include <coins.h>
 
 #include <consensus/consensus.h>
