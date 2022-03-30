68a041dd12b3bfee1f8308cd3586dccd3efefed4
Jon Atack <jon@atack.com>
2022-03-31 01:49:09 +0200

    rpc: gettxout fixups

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -972,7 +972,7 @@ static RPCHelpMan gettxout()
                     {RPCResult::Type::STR, "asm", ""},
                     {RPCResult::Type::STR, "desc", "Inferred descriptor for the output"},
                     {RPCResult::Type::STR_HEX, "hex", ""},
-                    {RPCResult::Type::STR, "type", "The type, eg pubkeyhash"},
+                    {RPCResult::Type::STR, "type", "The type (one of: " + GetAllOutputTypes() + ")"},
                     {RPCResult::Type::STR, "address", /*optional=*/true, "The Bitcoin address (only if a well-defined address exists)"},
                 }},
                 {RPCResult::Type::BOOL, "coinbase", "Coinbase or not"},
