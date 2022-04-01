b0740fdcb84120b76b201e6d17859e9a398f93b4
fanquake <fanquake@gmail.com>
2022-04-01 12:57:40 +0100

    ci: use DWARF-4 for Valgrind CI job
    clang-14 defaults to using DWARF-5, which breaks vlagrinds (3.18) ability
    to parse debug info. Valgrind claims to support DWARF-5 from version
    3.18 onwards, but maybe that only works when building with GCC.
    
    Explicitly use DWARF-4 for now. Note that from 11.0 GCC also defaults to
    using DWARF-5.
    
    https://releases.llvm.org/14.0.0/tools/clang/docs/ReleaseNotes.html#dwarf-support-in-clang
    https://www.gnu.org/software/gcc/gcc-11/changes.html
    https://valgrind.org/docs/manual/dist.news.html

--- ci/test/00_setup_env_native_valgrind.sh
+++ ci/test/00_setup_env_native_valgrind.sh
@@ -13,4 +13,5 @@ export USE_VALGRIND=1
 export NO_DEPENDS=1
 export TEST_RUNNER_EXTRA="--nosandbox --exclude feature_init,rpc_bind,feature_bind_extra"  # Excluded for now, see https://github.com/bitcoin/bitcoin/issues/17765#issuecomment-602068547
 export GOAL="install"
-export BITCOIN_CONFIG="--enable-zmq --with-incompatible-bdb --with-gui=no CC=clang CXX=clang++"  # TODO enable GUI
+# Temporarily pin dwarf 4, until valgrind can understand clang's dwarf 5
+export BITCOIN_CONFIG="--enable-zmq --with-incompatible-bdb --with-gui=no CC=clang CXX=clang++ CXXFLAGS='-fdebug-default-version=4'"  # TODO enable GUI
