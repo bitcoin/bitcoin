236dbd12654ef316678756ec79a6cd1480f340eb
Jon Atack <jon@atack.com>
2022-04-01 12:23:01 +0200

    Put lock logging behind DEBUG_LOCKCONTENTION preprocessor directive

--- src/logging.h
+++ src/logging.h
@@ -60,7 +60,9 @@ namespace BCLog {
         VALIDATION  = (1 << 21),
         I2P         = (1 << 22),
         IPC         = (1 << 23),
+#ifdef DEBUG_LOCKCONTENTION
         LOCK        = (1 << 24),
+#endif
         UTIL        = (1 << 25),
         BLOCKSTORE  = (1 << 26),
         ALL         = ~(uint32_t)0,
