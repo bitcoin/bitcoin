bcbf982553aba8107fdb0db413d4b9fe8adc8f17
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-04-10 19:02:29 +0200

    qt, doc: Remove unneeded comments
    Function names are self-described.

--- src/qt/clientmodel.cpp
+++ src/qt/clientmodel.cpp
@@ -240,7 +240,6 @@ void ClientModel::TipChanged(SynchronizationState sync_state, interfaces::BlockT
 
 void ClientModel::subscribeToCoreSignals()
 {
-    // Connect signals to client
     m_handler_show_progress = m_node.handleShowProgress(
         [this](const std::string& title, int progress, [[maybe_unused]] bool resume_possible) {
             Q_EMIT showProgress(QString::fromStdString(title), progress);
@@ -275,7 +274,6 @@ void ClientModel::subscribeToCoreSignals()
 
 void ClientModel::unsubscribeFromCoreSignals()
 {
-    // Disconnect signals from client
     m_handler_show_progress->disconnect();
     m_handler_notify_num_connections_changed->disconnect();
     m_handler_notify_network_active_changed->disconnect();
