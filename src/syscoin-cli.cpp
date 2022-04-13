e77af4bc9e0a7ec1675f3eb2ba30878201f59164
laanwj <126646+laanwj@users.noreply.github.com>
2022-04-13 21:48:18 +0200

    Merge bitcoin/bitcoin#24632: add `(none)` in -getinfo `Warnings:` if no warning returned
    0cea7b10f1180e9993c14473e1a3b6525ef6ba01 print `(none)` if no warnings in -getinfo (/dev/fd0)
    
    Pull request description:
    
      Adds `(none)` in warnings when no warnings returned by -getinfo
    
      Reviewers can test this by making the following change in `/src/warnings.cpp`:
    
      ```diff
      bilingual_str GetWarnings(bool verbose)
      {
          bilingual_str warnings_concise;
          std::vector<bilingual_str> warnings_verbose;
    
          LOCK(g_warnings_mutex);
    
          // Pre-release build warning
          if (!CLIENT_VERSION_IS_RELEASE) {
      -        warnings_concise = _("This is a pre-release test build - use at your own risk - do not use for mining or merchant applications");;
      +        warnings_concise = _("");;
      ```
    
      Before this pull request:
    
      ```
      $ bitcoin-cli -getinfo
      Chain: regtest
      Blocks: 0
      Headers: 0
      Verification progress: 100.0000%
      Difficulty: 4.656542373906925e-10
    
      Network: in 0, out 0, total 0
      Version: 239900
      Time offset (s): 0
      Proxies: n/a
      Min tx relay fee rate (BTC/kvB): 0.00001000
    
      Warnings:
      ```
    
      After this pull request:
    
      ```diff
      $ bitcoin-cli -getinfo
      Chain: regtest
      Blocks: 0
      Headers: 0
      Verification progress: 100.0000%
      Difficulty: 4.656542373906925e-10
    
      Network: in 0, out 0, total 0
      Version: 239900
      Time offset (s): 0
      Proxies: n/a
      Min tx relay fee rate (BTC/kvB): 0.00001000
    
      Warnings: (none)
      ```
    
    ACKs for top commit:
      jonatack:
        ACK 0cea7b10f1180e9993c14473e1a3b6525ef6ba01
      laanwj:
        Tested ACK 0cea7b10f1180e9993c14473e1a3b6525ef6ba01
    
    Tree-SHA512: a12499d11ff84bc954db354f968eb1f5ee4999d8b80581fe0bdf604732b2e2f608cb5c35c4ca8cb5a430f3991954a6207f0758302618662e6b9505044cf2dc95

--- src/syscoin-cli.cpp
+++ src/syscoin-cli.cpp
@@ -1055,7 +1055,9 @@ static void ParseGetInfoResult(UniValue& result)
         result_string += "\n";
     }
 
-    result_string += strprintf("%sWarnings:%s %s", YELLOW, RESET, result["warnings"].getValStr());
+    const std::string warnings{result["warnings"].getValStr()};
+    result_string += strprintf("%sWarnings:%s %s", YELLOW, RESET, warnings.empty() ? "(none)" : warnings);
+
     result.setStr(result_string);
 }
 
