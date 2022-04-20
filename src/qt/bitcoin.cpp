766a07e6d48a8b99391f18ccefa0ac5b5b5d1cbd
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-04-20 09:48:13 +0200

    qt6: Fix type registration
    In Qt 6, registration of `QDataStream` streaming operators is done
    automatically. Consequently, `qRegisterMetaTypeStreamOperators()` does
    no longer exist. Calls to this method have to be removed when porting
    to Qt 6.
    
    See https://doc.qt.io/qt-6/qtcore-changes-qt6.html

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -96,7 +96,11 @@ static void RegisterMetaTypes()
     qRegisterMetaType<QMessageBox::Icon>("QMessageBox::Icon");
     qRegisterMetaType<interfaces::BlockAndHeaderTipInfo>("interfaces::BlockAndHeaderTipInfo");
 
+#if (QT_VERSION >= QT_VERSION_CHECK(6, 0, 0))
+    qRegisterMetaType<BitcoinUnit>("BitcoinUnit");
+#else
     qRegisterMetaTypeStreamOperators<BitcoinUnit>("BitcoinUnit");
+#endif
 }
 
 static QString GetLangTerritory()
