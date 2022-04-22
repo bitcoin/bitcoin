f396b3d3fdecd8b8a6eee0d3d06c9850de860bbc
Ben Woosley <ben.woosley@gmail.com>
2022-04-22 12:08:06 -0500

    refactor: Drop unused logging includes from addrman_impl.h
    These were introduced in #22950, but they're not used in the header,
    rather equivalent includes in addrman.cpp do the work.

--- src/addrman_impl.h
+++ src/addrman_impl.h
@@ -5,8 +5,6 @@
 #ifndef BITCOIN_ADDRMAN_IMPL_H
 #define BITCOIN_ADDRMAN_IMPL_H
 
-#include <logging.h>
-#include <logging/timer.h>
 #include <netaddress.h>
 #include <protocol.h>
 #include <serialize.h>
