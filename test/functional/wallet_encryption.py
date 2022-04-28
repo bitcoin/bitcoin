baa739e84527a75a4f760b80f2d54b6da90e63ad
Jon Atack <jon@atack.com>
2022-04-29 00:29:36 +0200

    Work around intermittent Win failure in walletpassphrase tests
    Co-authored-by: Vasil Dimov <vd@FreeBSD.org>

--- test/functional/wallet_encryption.py
+++ test/functional/wallet_encryption.py
@@ -4,13 +4,12 @@
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 """Test Wallet encryption"""
 
+from math import ceil, floor
 import time
 
 from test_framework.test_framework import BitcoinTestFramework
 from test_framework.util import (
     assert_raises_rpc_error,
-    assert_greater_than,
-    assert_greater_than_or_equal,
 )
 
 
@@ -74,23 +73,25 @@ class WalletEncryptionTest(BitcoinTestFramework):
         # Test timeout bounds
         assert_raises_rpc_error(-8, "Timeout cannot be negative.", self.nodes[0].walletpassphrase, passphrase2, -10)
 
-        self.log.info('Check a timeout less than the limit')
-        MAX_VALUE = 100000000
-        expected_time = int(time.time()) + MAX_VALUE - 600
-        self.nodes[0].walletpassphrase(passphrase2, MAX_VALUE - 600)
-        # give buffer for walletpassphrase, since it iterates over all encrypted keys
-        expected_time_with_buffer = time.time() + MAX_VALUE - 600
-        actual_time = self.nodes[0].getwalletinfo()['unlocked_until']
-        assert_greater_than_or_equal(actual_time, expected_time)
-        assert_greater_than(expected_time_with_buffer, actual_time)
+        # Maximum time to keep the decryption key, in seconds (~3 years); see MAX_SLEEP_TIME
+        timeout_limit = 100_000_000
 
-        self.log.info('Check a timeout greater than the limit')
-        expected_time = int(time.time()) + MAX_VALUE - 1
-        self.nodes[0].walletpassphrase(passphrase2, MAX_VALUE + 1000)
-        expected_time_with_buffer = time.time() + MAX_VALUE
-        actual_time = self.nodes[0].getwalletinfo()['unlocked_until']
-        assert_greater_than_or_equal(actual_time, expected_time)
-        assert_greater_than(expected_time_with_buffer, actual_time)
+        self.log.info("Test walletpassphrase with a timeout less than the limit")
+        timeout = timeout_limit - 600
+        before = floor(time.time()) + timeout
+        self.nodes[0].walletpassphrase(passphrase2, timeout)
+        after = ceil(time.time()) + timeout
+        actual = self.nodes[0].getwalletinfo()['unlocked_until']
+        if not before <= actual <= after:
+            raise AssertionError(f'walletpassphrase "unlocked_until" value of {actual} not in expected range of {before}..{after}')
+
+        self.log.info("Test walletpassphrase with a timeout greater than the limit")
+        before = floor(time.time()) + timeout_limit
+        self.nodes[0].walletpassphrase(passphrase=passphrase2, timeout=timeout_limit + 1000)
+        after = ceil(time.time()) + timeout_limit
+        actual = self.nodes[0].getwalletinfo()['unlocked_until']
+        if not before <= actual <= after:
+            raise AssertionError(f'walletpassphrase "unlocked_until" value of {actual} not in expected range of {before}..{after}')
 
 
 if __name__ == '__main__':
