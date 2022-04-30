766c2c27576c00204bf77f5c0d5cc3fc0e92ccf1
MacroFake <falke.marco@gmail.com>
2022-04-30 09:25:48 +0200

    Merge bitcoin/bitcoin#25034: test: add missing stop_node calls to feature_coinstatsindex and feature_prune
    a3cd7dbfd8200c580aae9ea0f5473d58107dd582 test: stop node before calling assert_start_raises_init_error (Martin Zumsande)
    
    Pull request description:
    
      In #24789, I forgot to stop the node before using `assert_start_raises_init_error` in `feature_coinstatsindex`. This resulted in a bitcoind process that is not being terminated after the test finishes.
      `feature_prune` has the same problem and also creates a zombie bitcoind process.
    
      Also adds an assert to `assert_start_raises_init_error` to make sure the node isn't already running to prevent this sort of mistake in the future.
    
    Top commit has no ACKs.
    
    Tree-SHA512: 902f683ebe7b19ca32ab83ca40d9698e9d91509b1d003f21a7221f79b647e05b6ef5c0c888fbb772cbca5e641d5c9437d522b6671f446c3ab321d79f7c6d0284

--- test/functional/test_framework/test_node.py
+++ test/functional/test_framework/test_node.py
@@ -564,6 +564,7 @@ class TestNode():
 
         Will throw if dashd starts without an error.
         Will throw if an expected_msg is provided and it does not match dashd's stdout."""
+        assert not self.running
         with tempfile.NamedTemporaryFile(dir=self.stderr_dir, delete=False) as log_stderr, \
              tempfile.NamedTemporaryFile(dir=self.stdout_dir, delete=False) as log_stdout:
             try:
