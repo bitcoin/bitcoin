d5654db27e4668e6a7cf607cfe3babad272d349a
MacroFake <falke.marco@gmail.com>
2022-05-06 11:37:58 +0200

    partial Merge bitcoin/bitcoin#25063: test: previous releases: add v23.0
    dba123167236a172d2d33861d58aa94a19729671 test: previous releases: add v23.0 (Sjors Provoost)
    
    Pull request description:
    
      Follows the same pattern as d8b705f1caeb3b4a6790cb26e4e5584ca791d965 (v22.0) and 8a57a06a5062dd8dfdefca4e404d0ddbd2a3da1d (v0.21.0).
    
      Starting from v23.0 there is a separate macOS release for x86_64 and aarch64.
    
    ACKs for top commit:
      prusnak:
        Approach ACK dba123167236a172d2d33861d58aa94a19729671
    
    Tree-SHA512: 249aeddd5e80e163578581e5c8e9b6579f3694abc3d1fb68dddb7b42d75021ad85266688ec4a365a6631d82a65a19873aff7ba61c0ea59d21f8adbe4b772dc16

--- test/get_previous_releases.py
+++ test/get_previous_releases.py
@@ -105,8 +105,11 @@ def download_binary(tag, args) -> int:
     if match:
         bin_path = 'releases/download/test.{}'.format(
             match.group(1), match.group(2))
+    platform = args.platform
+    if tag < "v20" and platform in ["x86_64-apple-darwin", "aarch64-apple-darwin"]:
+        platform = "osx64"
     tarball = 'dashcore-{tag}-{platform}.tar.gz'.format(
-        tag=tag[1:], platform=args.platform)
+        tag=tag[1:], platform=platform)
     tarballUrl = 'https://github.com/dashpay/dash/{bin_path}/{tarball}'.format(
         bin_path=bin_path, tarball=tarball)
 
@@ -212,8 +215,8 @@ def check_host(args) -> int:
         platforms = {
             'aarch64-*-linux*': 'aarch64-linux-gnu',
             'x86_64-*-linux*': 'x86_64-linux-gnu',
-            'x86_64-apple-darwin*': 'osx64',
-            'aarch64-apple-darwin*': 'osx64',
+            'x86_64-apple-darwin*': 'x86_64-apple-darwin',
+            'aarch64-apple-darwin*': 'aarch64-apple-darwin',
         }
         args.platform = ''
         for pattern, target in platforms.items():
