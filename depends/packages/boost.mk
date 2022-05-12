d9eb2a1405eba8fc374b07af7a57d2eee23bb1e7
Luke Dashjr <luke-jr+git@utopios.org>
2022-05-12 03:01:08 +0000

    depends: Patch boost to remove filesystem module and use std::filesystem instead (for Boost.Process)

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -4,6 +4,11 @@ $(package)_download_path=https://boostorg.jfrog.io/artifactory/main/release/$($(
 $(package)_file_name=boost_$(subst .,_,$($(package)_version)).tar.bz2
 $(package)_sha256_hash=fc9f85fc030e233142908241af7a846e60630aa7388de9a5fafb1f3a26840854
 
+define $(package)_preprocess_cmds
+  rm boost/filesystem* -rf &&\
+  sed -i.old 's/\(#include <\)boost\/filesystem[^>]*>/\1filesystem>/; s/boost\(::filesystem\)/std\1/g' `find boost/process -type f`
+endef
+
 define $(package)_stage_cmds
   mkdir -p $($(package)_staging_prefix_dir)/include && \
   cp -r boost $($(package)_staging_prefix_dir)/include
