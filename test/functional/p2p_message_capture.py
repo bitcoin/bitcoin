237448ba7fe9edae7bf22cc5091f38c6ae5cf779
MacroFake <falke.marco@gmail.com>
2022-05-13 07:49:20 +0200

    Merge #25117: test: Check msg type in msg capture is followed by zeros

--- test/functional/p2p_message_capture.py
+++ test/functional/p2p_message_capture.py
@@ -43,15 +43,8 @@ def mini_parser(dat_file):
                 break
             tmp_header = BytesIO(tmp_header_raw)
             tmp_header.read(TIME_SIZE) # skip the timestamp field
-            raw_msgtype = tmp_header.read(MSGTYPE_SIZE)
-            raw_msgtype_split = raw_msgtype.split(b'\x00', 1)
-            msgtype: bytes = raw_msgtype_split[0]
-            # SYSCOIN
-            if len(raw_msgtype_split) > 1:
-                remainder =  raw_msgtype_split[1]
-            assert(len(msgtype) > 0)
+            msgtype = tmp_header.read(MSGTYPE_SIZE).rstrip(b'\x00')
             assert(msgtype in MESSAGEMAP)
-            assert(len(remainder) == 0 or not remainder.decode().isprintable())
             length: int = int.from_bytes(tmp_header.read(LENGTH_SIZE), "little")
             data = f_in.read(length)
             assert_equal(len(data), length)
