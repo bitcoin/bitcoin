e519628c8787594a684e4f5be1daebbafe5c47c4
MacroFake <falke.marco@gmail.com>
2022-05-16 10:49:40 +0200

    Merge #25067: validationinterface: make MainSignalsInstance() a class…

--- src/validationinterface.h
+++ src/validationinterface.h
@@ -18,7 +18,6 @@ class CBlock;
 class CBlockIndex;
 struct CBlockLocator;
 class CValidationInterface;
-class uint256;
 class CScheduler;
 // SYSCOIN
 class ChainstateManager;
@@ -197,10 +196,10 @@ protected:
     friend class ValidationInterfaceTest;
 };
 
-struct MainSignalsInstance;
+class MainSignalsImpl;
 class CMainSignals {
 private:
-    std::unique_ptr<MainSignalsInstance> m_internals;
+    std::unique_ptr<MainSignalsImpl> m_internals;
 
     friend void ::RegisterSharedValidationInterface(std::shared_ptr<CValidationInterface>);
     friend void ::UnregisterValidationInterface(CValidationInterface*);
