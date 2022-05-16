f9226184b0331f0ecbee517a9fcf3db479237924
Luke Dashjr <luke-jr+git@utopios.org>
2022-05-16 07:44:32 +0000

    GUI: Options: Replace addresstype radioboxes with a combobox to match Receive dialog

--- src/qt/optionsdialog.cpp
+++ src/qt/optionsdialog.cpp
@@ -17,6 +17,7 @@
 #include <interfaces/node.h>
 #include <node/chainstatemanager_args.h>
 #include <netbase.h>
+#include <outputtype.h>
 #include <txdb.h>
 #include <util/strencodings.h>
 
@@ -151,6 +152,15 @@ OptionsDialog::OptionsDialog(QWidget* parent, bool enableWallet)
         ui->tabWidget->removeTab(ui->tabWidget->indexOf(ui->tabWallet));
         ui->thirdPartyTxUrlsLabel->setVisible(false);
         ui->thirdPartyTxUrls->setVisible(false);
+    } else {
+        for (OutputType type : OUTPUT_TYPES) {
+            const QString& val = QString::fromStdString(FormatOutputType(type));
+            const auto [text, tooltip] = GetOutputTypeDescription(type);
+
+            const auto index = ui->addressType->count();
+            ui->addressType->addItem(text, val);
+            ui->addressType->setItemData(index, tooltip, Qt::ToolTipRole);
+        }
     }
 
 #ifdef ENABLE_EXTERNAL_SIGNER
@@ -312,23 +322,12 @@ void OptionsDialog::setMapper()
     mapper->addMapping(ui->pruneSizeMiB, OptionsModel::PruneSizeMiB);
 
     /* Wallet */
+    mapper->addMapping(ui->addressType, OptionsModel::addresstype);
     mapper->addMapping(ui->spendZeroConfChange, OptionsModel::SpendZeroConfChange);
     mapper->addMapping(ui->coinControlFeatures, OptionsModel::CoinControlFeatures);
     mapper->addMapping(ui->subFeeFromAmount, OptionsModel::SubFeeFromAmount);
     mapper->addMapping(ui->externalSignerPath, OptionsModel::ExternalSignerPath);
     mapper->addMapping(ui->m_enable_psbt_controls, OptionsModel::EnablePSBTControls);
-
-    {
-        QString radio_name_lower = "addresstype" + model->data(model->index(OptionsModel::addresstype, 0), Qt::EditRole).toString().toLower();
-        radio_name_lower.replace("-", "_");
-        for (int i = ui->layoutAddressType->count(); i--; ) {
-            QRadioButton * const radio = qobject_cast<QRadioButton*>(ui->layoutAddressType->itemAt(i)->widget());
-            if (!radio) {
-                continue;
-            }
-            radio->setChecked(radio->objectName().toLower() == radio_name_lower);
-        }
-    }
 
     /* Network */
     mapper->addMapping(ui->networkPort, OptionsModel::NetworkPort);
@@ -468,20 +467,6 @@ void OptionsDialog::on_okButton_clicked()
 
     model->setData(model->index(OptionsModel::FontForMoney, 0), ui->moneyFont->itemData(ui->moneyFont->currentIndex()));
     model->setData(model->index(OptionsModel::FontForQRCodes, 0), ui->qrFont->itemData(ui->qrFont->currentIndex()));
-
-    {
-        QString new_addresstype;
-        for (int i = ui->layoutAddressType->count(); i--; ) {
-            QRadioButton * const radio = qobject_cast<QRadioButton*>(ui->layoutAddressType->itemAt(i)->widget());
-            if (!(radio && radio->objectName().startsWith("addressType") && radio->isChecked())) {
-                continue;
-            }
-            new_addresstype = radio->objectName().mid(11).toLower();
-            new_addresstype.replace("_", "-");
-            break;
-        }
-        model->setData(model->index(OptionsModel::addresstype, 0), new_addresstype);
-    }
 
     if (ui->maxuploadtargetCheckbox->isChecked()) {
         model->setData(model->index(OptionsModel::maxuploadtarget, 0), ui->maxuploadtarget->value());
