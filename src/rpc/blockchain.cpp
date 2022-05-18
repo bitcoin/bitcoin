3b226806d439c1ab897fd1d782fd2537b3009136
Luke Dashjr <luke-jr+git@utopios.org>
2022-05-18 22:17:53 +0000

    Diff-minimise

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -2797,9 +2797,9 @@ UniValue CreateUTXOSnapshot(
         fs::PathToString(path), fs::PathToString(temppath)));
 
     if (!is_human_readable) {
-        SnapshotMetadata metadata{chainstate.m_chainman.GetParams().MessageStart(), tip->GetBlockHash(), maybe_stats->coins_count};
+    SnapshotMetadata metadata{chainstate.m_chainman.GetParams().MessageStart(), tip->GetBlockHash(), maybe_stats->coins_count};
 
-        afile << metadata;
+    afile << metadata;
     } else if (show_header) {
         afile.write(MakeByteSpan("#(blockhash " + tip->GetBlockHash().ToString() + " ) "));
         for (auto it = std::begin(requested); it != std::end(requested); ++it) {
@@ -2842,12 +2842,12 @@ UniValue CreateUTXOSnapshot(
         ++iter;
         if (pcursor->GetKey(key) && pcursor->GetValue(coin)) {
             if (!is_human_readable) {
-                if (key.hash != last_hash) {
-                    write_coins_to_file(afile, last_hash, coins, written_coins_count);
-                    last_hash = key.hash;
-                    coins.clear();
-                }
-                coins.emplace_back(key.n, coin);
+            if (key.hash != last_hash) {
+                write_coins_to_file(afile, last_hash, coins, written_coins_count);
+                last_hash = key.hash;
+                coins.clear();
+            }
+            coins.emplace_back(key.n, coin);
             } else {
                 for (auto it = std::begin(requested); it != std::end(requested); ++it) {
                     if (it != std::begin(requested))
