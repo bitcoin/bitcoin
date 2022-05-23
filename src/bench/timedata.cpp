5e82ba19ce9995f0bd46df9b1a713fa4f0e579b7
Antoine Desbois <antoine.desbois@hotmail.ca>
2022-05-22 19:33:51 -0700

    Add benchmarks for CMedianFilter
    This will allow us to measure any potential performance improvement in
    the CMedianFilter algorithm

---
+++ src/bench/timedata.cpp
@@ -0,0 +1,72 @@
+// Copyright (c) 2020-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <timedata.h>
+
+#include <bench/bench.h>
+
+// 256 element max
+// 1000 iteration
+static void ComputeMedian256_1000(benchmark::Bench& bench)
+{
+    //Precompute random numbers
+    std::vector<int> rand_num;
+    rand_num.reserve(1000);
+    for (size_t i = 0; i < 1000; ++i) {
+        rand_num.emplace_back(rand());
+    }
+
+    bench.run([&rand_num] {
+        CMedianFilter<int> median_filter(256, 15);
+
+        for (size_t i = 0; i < 1000; ++i) {
+            median_filter.input(rand_num[i]);
+        }
+    });
+}
+
+// 20 element max
+// 1000 iteration
+static void ComputeMedian20_1000(benchmark::Bench& bench)
+{
+    //Precompute random numbers
+    std::vector<int> rand_num;
+    rand_num.reserve(1000);
+    for (size_t i = 0; i < 1000; ++i) {
+        rand_num.emplace_back(rand());
+    }
+
+    bench.run([&rand_num] {
+        CMedianFilter<int> median_filter(20,15);
+
+        for (size_t i = 0; i < 1000; ++i) {
+            median_filter.input(rand_num[i]);
+        }
+    });
+}
+
+// 10 element max
+// 1000 iteration
+static void ComputeMedian10_1000(benchmark::Bench& bench)
+{
+    //Precompute random numbers
+    std::vector<int> rand_num;
+    rand_num.reserve(1000);
+    for (size_t i = 0; i < 1000; ++i) {
+        rand_num.emplace_back(rand());
+    }
+
+    bench.run([&rand_num] {
+        CMedianFilter<int> median_filter(10, 15);
+
+        for (size_t i = 0; i < 1000; ++i) {
+            median_filter.input(rand_num[i]);
+        }
+    });
+}
+
+BENCHMARK(ComputeMedian256_1000);
+BENCHMARK(ComputeMedian20_1000);
+BENCHMARK(ComputeMedian10_1000);
+
