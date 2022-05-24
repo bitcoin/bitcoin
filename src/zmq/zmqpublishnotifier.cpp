422d99c33820ee52f782bd0c8d4ab5ca89534474
Luke Dashjr <luke-jr+git@utopios.org>
2022-05-24 23:27:02 +0000

    Bugfix: zmq: Fix implicit-integer-sign-change in CZMQPublishHashTransactionNotifier

--- src/zmq/zmqpublishnotifier.cpp
+++ src/zmq/zmqpublishnotifier.cpp
@@ -246,7 +246,7 @@ bool CZMQPublishHashTransactionNotifier::NotifyTransaction(const CTransaction &t
 bool CZMQPublishHashWalletTransactionNotifier::NotifyWalletTransaction(const CTransaction &transaction, const uint256 &hashBlock){
     uint256 hash = transaction.GetHash();
     LogPrint(BCLog::ZMQ, "zmq: Publish hashwallettx %s to %s\n", hash.GetHex(), this->address);
-    char data[32];
+    uint8_t data[32];
     for (unsigned int i = 0; i < 32; i++)
         data[31 - i] = hash.begin()[i];
 
