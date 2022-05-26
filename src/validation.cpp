797b88a2d50784d41bebb19a94d3084cb6d3b899
laanwj <126646+laanwj@users.noreply.github.com>
2022-05-26 15:12:44 +0200

    Merge #25210: doc: remove misleading AreInputsStandard() comment

--- src/validation.cpp
+++ src/validation.cpp
@@ -873,7 +873,7 @@ bool MemPoolAccept::PreChecks(ATMPArgs& args, Workspace& ws)
             return false; // state filled in by CheckSyscoinInputs
         } 
     }  
-    // Check for non-standard pay-to-script-hash in inputs
+
     if (fRequireStandard && !AreInputsStandard(tx, m_view)) {
         return state.Invalid(TxValidationResult::TX_INPUTS_NOT_STANDARD, "bad-txns-nonstandard-inputs");
     }
