d496df033f3ffe330b21e0cdf678694769a6de4e
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2022-05-30 17:41:37 +0200

    merge bitcoin-core/gui#612: Drop unused `QFrame`s in `SendCoinsEntry`

--- src/qt/sendcoinsentry.cpp
+++ src/qt/sendcoinsentry.cpp
@@ -20,15 +20,13 @@
 #include <QClipboard>
 
 SendCoinsEntry::SendCoinsEntry(QWidget* parent) :
-    QStackedWidget(parent),
+    QWidget(parent),
     ui(new Ui::SendCoinsEntry),
     model(nullptr)
 {
     ui->setupUi(this);
 
     GUIUtil::disableMacFocusRect(this);
-
-    setCurrentWidget(ui->SendCoins);
 
     setButtonIcons();
 
@@ -46,8 +44,6 @@ SendCoinsEntry::SendCoinsEntry(QWidget* parent) :
     connect(ui->payAmount, &BitcoinAmountField::valueChanged, this, &SendCoinsEntry::payAmountChanged);
     connect(ui->checkboxSubtractFeeFromAmount, &QCheckBox::toggled, this, &SendCoinsEntry::subtractFeeFromAmountChanged);
     connect(ui->deleteButton, &QPushButton::clicked, this, &SendCoinsEntry::deleteClicked);
-    connect(ui->deleteButton_is, &QPushButton::clicked, this, &SendCoinsEntry::deleteClicked);
-    connect(ui->deleteButton_s, &QPushButton::clicked, this, &SendCoinsEntry::deleteClicked);
     connect(ui->useAvailableBalanceButton, &QPushButton::clicked, this, &SendCoinsEntry::useAvailableBalanceClicked);
 }
 
@@ -109,14 +105,6 @@ void SendCoinsEntry::clear()
     ui->messageTextLabel->clear();
     ui->messageTextLabel->hide();
     ui->messageLabel->hide();
-    // clear UI elements for unauthenticated payment request
-    ui->payTo_is->clear();
-    ui->memoTextLabel_is->clear();
-    ui->payAmount_is->clear();
-    // clear UI elements for authenticated payment request
-    ui->payTo_s->clear();
-    ui->memoTextLabel_s->clear();
-    ui->payAmount_s->clear();
 
     // update the display unit, to not use the default ("BTC")
     updateDisplayUnit();
@@ -226,7 +214,7 @@ void SendCoinsEntry::setAmount(const CAmount &amount)
 
 bool SendCoinsEntry::isClear()
 {
-    return ui->payTo->text().isEmpty() && ui->payTo_is->text().isEmpty() && ui->payTo_s->text().isEmpty();
+    return ui->payTo->text().isEmpty();
 }
 
 void SendCoinsEntry::setFocus()
@@ -236,18 +224,14 @@ void SendCoinsEntry::setFocus()
 
 void SendCoinsEntry::updateDisplayUnit()
 {
-    if(model && model->getOptionsModel())
-    {
-        // Update payAmount with the current unit
+    if (model && model->getOptionsModel()) {
         ui->payAmount->setDisplayUnit(model->getOptionsModel()->getDisplayUnit());
-        ui->payAmount_is->setDisplayUnit(model->getOptionsModel()->getDisplayUnit());
-        ui->payAmount_s->setDisplayUnit(model->getOptionsModel()->getDisplayUnit());
     }
 }
 
 void SendCoinsEntry::changeEvent(QEvent* e)
 {
-    QStackedWidget::changeEvent(e);
+    QWidget::changeEvent(e);
     if (e->type() == QEvent::StyleChange) {
         // Adjust button icon colors on theme changes
         setButtonIcons();
@@ -259,8 +243,6 @@ void SendCoinsEntry::setButtonIcons()
     GUIUtil::setIcon(ui->addressBookButton, "address-book");
     GUIUtil::setIcon(ui->pasteButton, "editpaste");
     GUIUtil::setIcon(ui->deleteButton, "remove", GUIUtil::ThemedColor::RED);
-    GUIUtil::setIcon(ui->deleteButton_is, "remove", GUIUtil::ThemedColor::RED);
-    GUIUtil::setIcon(ui->deleteButton_s, "remove", GUIUtil::ThemedColor::RED);
 }
 
 bool SendCoinsEntry::updateLabel(const QString &address)
