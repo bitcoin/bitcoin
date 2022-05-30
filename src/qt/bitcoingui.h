Bitcoin-ABC/bitcoin-abc

Merge #13248: [gui] Make proxy icon from statusbar clickable
Summary:
6d5fcad576962e5950641f7e7b113a6ac6f397e5 [gui] Make proxy icon from statusbar clickable (Cristian Mircea Messel)

Pull request description:

  Clicking on the proxy icon will open settings showing the network tab

  https://github.com/bitcoin/bitcoin/pull/11491#issuecomment-336685303

Tree-SHA512: c3549749296918818694a371326d1a3b1075478918aaee940b5c7119a7e2cb991dcfda78f20d44d6d001157b9b82951f0d5157b17f4f0d1a0a242795efade036

Backport of Core PR13248
https://github.com/bitcoin/bitcoin/pull/13248/

Depends on D4269

Test Plan:
  make check
  ./bitcoin-qt -proxy=<valid proxy>
Clicking the `P` icon in the bottom right corner should open up the `network` tab in the `options` menu.

Reviewers: deadalnix, Fabien, jasonbcox, O1 Bitcoin ABC, #bitcoin_abc

Reviewed By: deadalnix, Fabien, O1 Bitcoin ABC, #bitcoin_abc

Differential Revision: https://reviews.bitcoinabc.org/D4270

src/qt/bitcoingui.h
@@ -9,6 +9,8 @@
 #include <config/bitcoin-config.h>
 #endif
 
+#include <qt/optionsdialog.h>
+
 #include <amount.h>
 
 #include <QLabel>
@@ -46,6 +48,10 @@ class QComboBox;
 class QProgressBar;
 class QProgressDialog;
 QT_END_NAMESPACE
+
+namespace GUIUtil {
+class ClickableLabel;
+}
 
 /**
  * Bitcoin GUI main class. This class represents the main window of the Bitcoin
@@ -100,8 +106,8 @@ private:
     UnitDisplayStatusBarControl *unitDisplayControl = nullptr;
     QLabel *labelWalletEncryptionIcon = nullptr;
     QLabel *labelWalletHDStatusIcon = nullptr;
-    QLabel *labelProxyIcon = nullptr;
-    QLabel *connectionsControl = nullptr;
+    GUIUtil::ClickableLabel *labelProxyIcon = nullptr;
+    GUIUtil::ClickableLabel *connectionsControl = nullptr;
     QLabel *labelBlocksIcon = nullptr;
     QLabel *progressBarLabel = nullptr;
     QProgressBar *progressBar = nullptr;
@@ -173,6 +179,9 @@ private:
     void updateNetworkState();
 
     void updateHeadersSyncProgressLabel();
+
+    /** Open the OptionsDialog on the specified tab index */
+    void openOptionsDialogWithTab(OptionsDialog::Tab tab);
 
 Q_SIGNALS:
     /** Signal raised when a URI was entered or dragged to the GUI */
@@ -282,9 +291,6 @@ private Q_SLOTS:
     /** When hideTrayIcon setting is changed in OptionsModel hide or show the
      * icon accordingly. */
     void setTrayIconVisible(bool);
-
-    /** Toggle networking */
-    void toggleNetworkActive();
 
     void showModalOverlay();
 };

bitcoin/bitcoin

              Clicking on the proxy icon will open settings showing the network tab
              Create enum Tab in OptionsModel
              Use new connect syntax
              Use lambda for private slots

src/qt/bitcoingui.h
@@ -9,6 +9,8 @@
 #include <config/dash-config.h>
 #endif
 
+#include <qt/optionsdialog.h>
+
 #include <amount.h>
 
 #include <QLabel>
@@ -118,8 +120,8 @@ private:
     UnitDisplayStatusBarControl* unitDisplayControl = nullptr;
     QLabel* labelWalletEncryptionIcon = nullptr;
     QLabel* labelWalletHDStatusIcon = nullptr;
-    QLabel* labelProxyIcon = nullptr;
     GUIUtil::ClickableLabel* labelConnectionsIcon = nullptr;
+    GUIUtil::ClickableLabel* labelProxyIcon = nullptr;
     GUIUtil::ClickableLabel* labelBlocksIcon = nullptr;
     QLabel* progressBarLabel = nullptr;
     GUIUtil::ClickableProgressBar* progressBar = nullptr;
@@ -236,6 +238,9 @@ private:
     void updateHeadersSyncProgressLabel();
 
     void updateProgressBarVisibility();
+
+    /** Open the OptionsDialog on the specified tab index */
+    void openOptionsDialogWithTab(OptionsDialog::Tab tab);
 
 Q_SIGNALS:
     /** Signal raised when a URI was entered or dragged to the GUI */
@@ -373,9 +378,6 @@ public Q_SLOTS:
 
     /** When hideTrayIcon setting is changed in OptionsModel hide or show the icon accordingly. */
     void setTrayIconVisible(bool);
-
-    /** Toggle networking */
-    void toggleNetworkActive();
 
     void showModalOverlay();
 