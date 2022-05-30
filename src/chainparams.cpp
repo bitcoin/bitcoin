8dc9bc09aee10315679c6b6b1ef55881c57d0e34
David Burkett <davidburkett38@gmail.com>
2022-05-30 12:50:18 -0400

    Add testnet checkpoint

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -260,6 +260,7 @@ public:
             {
                 {300, uint256S("54e6075affe658d6574e04c9245a7920ad94dc5af8f5b37fd9a094e317769740")},
                 {2056, uint256S("17748a31ba97afdc9a4f86837a39d287e3e7c7290a08a1d816c5969c78a83289")},
+                {2352616, uint256S("7540437e7bf7831fa872ba8cfae85951a1e5dbb04c201b6f5def934d9299f3c2")}
             }
         };
 
