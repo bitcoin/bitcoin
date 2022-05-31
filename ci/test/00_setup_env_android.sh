ca0c3e50779771832dd8db419940fde877d9b691
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-05-31 12:50:19 +0200

    ci, android: Update Command-line Tools from 2.1 up to 7.0

--- ci/test/00_setup_env_android.sh
+++ ci/test/00_setup_env_android.sh
@@ -17,7 +17,7 @@ export RUN_FUNCTIONAL_TESTS=false
 export ANDROID_API_LEVEL=28
 export ANDROID_BUILD_TOOLS_VERSION=28.0.3
 export ANDROID_NDK_VERSION=23.1.7779620
-export ANDROID_TOOLS_URL=https://dl.google.com/android/repository/commandlinetools-linux-6609375_latest.zip
+export ANDROID_TOOLS_URL=https://dl.google.com/android/repository/commandlinetools-linux-8512546_latest.zip
 export ANDROID_HOME="${DEPENDS_DIR}/SDKs/android"
 export ANDROID_NDK_HOME="${ANDROID_HOME}/ndk/${ANDROID_NDK_VERSION}"
 export DEP_OPTS="ANDROID_SDK=${ANDROID_HOME} ANDROID_NDK=${ANDROID_NDK_HOME} ANDROID_API_LEVEL=${ANDROID_API_LEVEL} ANDROID_TOOLCHAIN_BIN=${ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/linux-x86_64/bin/"
