e36d02059750a9482022f695fb0ab064ad4f9cdf
fanquake <fanquake@gmail.com>
2022-06-01 13:56:54 +0100

    scripted-diff: use static constexpr in version.h
    -BEGIN VERIFY SCRIPT-
    sed -i -e "s/static const/static constexpr/" src/version.h
    -END VERIFY SCRIPT-

--- src/version.h
+++ src/version.h
@@ -9,34 +9,34 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70016;
+static constexpr int PROTOCOL_VERSION = 70016;
 
 //! initial proto version, to be increased after version/verack negotiation
-static const int INIT_PROTO_VERSION = 209;
+static constexpr int INIT_PROTO_VERSION = 209;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 31800;
+static constexpr int MIN_PEER_PROTO_VERSION = 31800;
 
 //! BIP 0031, pong message, is enabled for all versions AFTER this one
-static const int BIP0031_VERSION = 60000;
+static constexpr int BIP0031_VERSION = 60000;
 
 //! "filter*" commands are disabled without NODE_BLOOM after and including this version
-static const int NO_BLOOM_VERSION = 70011;
+static constexpr int NO_BLOOM_VERSION = 70011;
 
 //! "sendheaders" command and announcing blocks with headers starts with this version
-static const int SENDHEADERS_VERSION = 70012;
+static constexpr int SENDHEADERS_VERSION = 70012;
 
 //! "feefilter" tells peers to filter invs to you by fee starts with this version
-static const int FEEFILTER_VERSION = 70013;
+static constexpr int FEEFILTER_VERSION = 70013;
 
 //! short-id-based block download starts with this version
-static const int SHORT_IDS_BLOCKS_VERSION = 70014;
+static constexpr int SHORT_IDS_BLOCKS_VERSION = 70014;
 
 //! not banning for invalid compact blocks starts with this version
-static const int INVALID_CB_NO_BAN_VERSION = 70015;
+static constexpr int INVALID_CB_NO_BAN_VERSION = 70015;
 
 //! "wtxidrelay" command for wtxid-based relay starts with this version
-static const int WTXID_RELAY_VERSION = 70016;
+static constexpr int WTXID_RELAY_VERSION = 70016;
 
 // Make sure that none of the values above collide with
 // `SERIALIZE_TRANSACTION_NO_WITNESS` or `ADDRV2_FORMAT`.
