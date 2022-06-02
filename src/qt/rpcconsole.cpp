f0e87778dcf708d51a99059e2fe7b92f4b48ffa7
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-06-02 19:14:16 +0200

    Merge bitcoin-core/gui#613: Remove unnecessary wallet includes from rpcconsole.cpp
    09942736495502cedcef4b097dde6abd9327fbdb qt: Remove unnecessary wallet includes from rpcconsole.cpp (laanwj)
    
    Pull request description:
    
      Fixes bitcoin/bitcoin#25266
    
    ACKs for top commit:
      MarcoFalke:
        cr ACK 09942736495502cedcef4b097dde6abd9327fbdb
      hebasto:
        ACK 09942736495502cedcef4b097dde6abd9327fbdb.
    
    Tree-SHA512: 02cef4a1f3522c4cd662853eb930dfdf0866a5bd959a00f4c42d8c741b0751df4cf2e14c304b93ca3ce699c0e9730caf3d6fa2053009af312c60e861b0f79179

--- src/qt/rpcconsole.cpp
+++ src/qt/rpcconsole.cpp
@@ -29,14 +29,6 @@
 
 #include <univalue.h>
 
-#ifdef ENABLE_WALLET
-#ifdef USE_BDB
-#include <wallet/bdb.h>
-#endif
-#include <wallet/db.h>
-#include <wallet/wallet.h>
-#endif
-
 #include <QAbstractButton>
 #include <QAbstractItemModel>
 #include <QDateTime>
