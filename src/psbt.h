241c4d047ee658a976625c39c6acd04dd7b071ef
Ben Woosley <ben.woosley@gmail.com>
2022-06-07 15:49:03 -0500

    doc: Correct comment describing value of MAX_FILE_SIZE_PSBT as in MiB
    100 MiB is 100 * (10224 ** 2), or 107,374,182,400 bytes.

--- src/psbt.h
+++ src/psbt.h
@@ -54,7 +54,7 @@ static constexpr uint8_t PSBT_SEPARATOR = 0x00;
 
 // BIP 174 does not specify a maximum file size, but we set a limit anyway
 // to prevent reading a stream indefinitely and running out of memory.
-const std::streamsize MAX_FILE_SIZE_PSBT = 100000000; // 100 MiB
+const std::streamsize MAX_FILE_SIZE_PSBT = 100000000; // 100 MB
 
 // PSBT version number
 static constexpr uint32_t PSBT_HIGHEST_VERSION = 0;
