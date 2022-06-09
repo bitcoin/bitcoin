8b8edc25c13a3e613770bf38b21a2556192e6315
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-06-09 15:49:54 +0200

    build: Specify native binaries explicitly when building `capnp` package
    From `configure --help`:
      --with-external-capnp   use the system capnp binary (or the one specified
                              with $CAPNP) instead of compiling a new one (useful
                              for cross-compiling)

--- depends/packages/capnp.mk
+++ depends/packages/capnp.mk
@@ -8,6 +8,8 @@ $(package)_dependencies=native_$(package)
 
 define $(package)_set_vars :=
 $(package)_config_opts := --with-external-capnp
+$(package)_config_opts += CAPNP="$$(native_capnp_prefixbin)/capnp"
+$(package)_config_opts += CAPNP_CXX="$$(native_capnp_prefixbin)/capnp-c++"
 $(package)_config_opts_android := --disable-shared
 endef
 
