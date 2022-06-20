76e6825101cf7e27af0fea5db219de59fd1d29c1
mxaddict <mxaddict@codedmaster.com>
2022-06-20 20:29:19 +0800

    More branding changes

--- src/qt/intro.cpp
+++ src/qt/intro.cpp
@@ -389,7 +389,7 @@ void Intro::UpdatePruneLabels(bool prune_checked)
         //: Explanatory text on the capability of the current prune target.
         tr("(sufficient to restore backups %n day(s) old)", "", expected_backup_days));
     ui->sizeWarningLabel->setText(
-        tr("%1 will download and store a copy of the Bitcoin block chain.").arg(PACKAGE_NAME) + " " +
+        tr("%1 will download and store a copy of the Navcoin block chain.").arg(PACKAGE_NAME) + " " +
         storageRequiresMsg.arg(m_required_space_gb) + " " +
         tr("The wallet will also be stored in this directory.")
     );
