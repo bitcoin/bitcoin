51fcdf5749a187f0f02f6ea8c518977079bd6b42
laanwj <126646+laanwj@users.noreply.github.com>
2022-06-22 12:19:57 +0200

    Merge bitcoin/bitcoin#25443: test: Fail if connect_nodes fails
    faee330c7bde4d52dc18cf55166b5975b26bdecd test: Fail if connect_nodes fails (MacroFake)
    
    Pull request description:
    
      Currently, `connect_nodes` will return silently when the connection is disconnected while connecting. This is confusing, so fix it.
    
      Can be tested by reverting the signet test change and observing the failure when running the test.
    
    ACKs for top commit:
      laanwj:
        Tested ACK faee330c7bde4d52dc18cf55166b5975b26bdecd
    
    Tree-SHA512: 641ca8adcb9f5ff33239b143573bddc0dfde41dbd103751ee870f1572ca2469f6a0d4bab6693102454cd3e270ef8251d87fbfac48f6d8adac70d2d6bbffaae56

--- test/functional/test_framework/test_framework.py
+++ test/functional/test_framework/test_framework.py
@@ -586,6 +586,8 @@ class SyscoinTestFramework(metaclass=SyscoinTestMetaClass):
     def connect_nodes(self, a, b):
         from_connection = self.nodes[a]
         to_connection = self.nodes[b]
+        from_num_peers = 1 + len(from_connection.getpeerinfo())
+        to_num_peers = 1 + len(to_connection.getpeerinfo())
         ip_port = "127.0.0.1:" + str(p2p_port(b))
         from_connection.addnode(ip_port, "onetry")
         # poll until version handshake complete to avoid race conditions
@@ -593,10 +595,10 @@ class SyscoinTestFramework(metaclass=SyscoinTestMetaClass):
         # See comments in net_processing:
         # * Must have a version message before anything else
         # * Must have a verack message before anything else
-        wait_until_helper(lambda: all(peer['version'] != 0 for peer in from_connection.getpeerinfo()))
-        wait_until_helper(lambda: all(peer['version'] != 0 for peer in to_connection.getpeerinfo()))
-        wait_until_helper(lambda: all(peer['bytesrecv_per_msg'].pop('verack', 0) == 24 for peer in from_connection.getpeerinfo()))
-        wait_until_helper(lambda: all(peer['bytesrecv_per_msg'].pop('verack', 0) == 24 for peer in to_connection.getpeerinfo()))
+        self.wait_until(lambda: sum(peer['version'] != 0 for peer in from_connection.getpeerinfo()) == from_num_peers)
+        self.wait_until(lambda: sum(peer['version'] != 0 for peer in to_connection.getpeerinfo()) == to_num_peers)
+        self.wait_until(lambda: sum(peer['bytesrecv_per_msg'].pop('verack', 0) == 24 for peer in from_connection.getpeerinfo()) == from_num_peers)
+        self.wait_until(lambda: sum(peer['bytesrecv_per_msg'].pop('verack', 0) == 24 for peer in to_connection.getpeerinfo()) == to_num_peers)
 
     def disconnect_nodes(self, a, b):
         def disconnect_nodes_helper(from_connection, node_num):
@@ -625,7 +627,7 @@ class SyscoinTestFramework(metaclass=SyscoinTestMetaClass):
                         raise
 
             # wait to disconnect
-            wait_until_helper(lambda: not get_peer_ids(), timeout=5)
+            self.wait_until(lambda: not get_peer_ids(), timeout=5)
 
         disconnect_nodes_helper(self.nodes[a], b)
 
