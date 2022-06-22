88f0782ba8241c8867b837d612b76803103234bb
mxaddict <mxaddict@codedmaster.com>
2022-06-22 12:45:35 +0800

    Fixed some failing tests on Message Sign/Verify suite

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -5,7 +5,7 @@
 /**
  * See https://www.boost.org/doc/libs/1_78_0/libs/test/doc/html/boost_test/adv_scenarios/single_header_customizations/multiple_translation_units.html
  */
-#define BOOST_TEST_MODULE Bitcoin Core Test Suite
+#define BOOST_TEST_MODULE Navcoin Core Test Suite
 
 #include <boost/test/included/unit_test.hpp>
 
