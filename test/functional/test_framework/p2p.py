8d883286f037eef1da0901eeaf1f61b8552a52d9
mxaddict <mxaddict@codedmaster.com>
2022-06-28 19:42:15 +0800

    Updated MAGIC BYTES in tests

--- test/functional/test_framework/p2p.py
+++ test/functional/test_framework/p2p.py
@@ -127,9 +127,9 @@ MESSAGEMAP = {
 }
 
 MAGIC_BYTES = {
-    "mainnet": b"\xf9\xbe\xb4\xd9",   # mainnet
-    "testnet3": b"\x0b\x11\x09\x07",  # testnet3
-    "regtest": b"\xfa\xbf\xb5\xda",   # regtest
+    "mainnet": b"\xdb\xd2\xb1\xac",   # mainnet
+    "testnet3": b"\xe2\xbe\x8d\xb7",  # testnet3
+    "regtest": b"\xfd\xbf\x9f\xfb",   # regtest
     "signet": b"\x0a\x03\xcf\x40",    # signet
 }
 
