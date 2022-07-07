c50d5ecf778c56774c749340375bb775e8f41862
Carl Dong <contact@carldong.me>
2022-07-06 23:00:19 -0400

    Use CDBWrapper::Options ctor for test callers
    Also use scopes for freeing temporary CDBWrappers within tests.

--- src/test/dbwrapper_tests.cpp
+++ src/test/dbwrapper_tests.cpp
@@ -27,7 +27,13 @@ BOOST_AUTO_TEST_CASE(dbwrapper)
     // Perform tests both obfuscated and non-obfuscated.
     for (const bool obfuscate : {false, true}) {
         fs::path ph = m_args.GetDataDirBase() / (obfuscate ? "dbwrapper_obfuscate_true" : "dbwrapper_obfuscate_false");
-        CDBWrapper dbw(ph, (1 << 20), true, false, obfuscate);
+        CDBWrapper dbw{{
+            .db_path = ph,
+            .cache_size = (1 << 20),
+            .in_memory = true,
+            .wipe_existing = false,
+            .obfuscate_data = obfuscate,
+        }};
         uint8_t key{'k'};
         uint256 in = InsecureRand256();
         uint256 res;
@@ -46,7 +52,13 @@ BOOST_AUTO_TEST_CASE(dbwrapper_basic_data)
     // Perform tests both obfuscated and non-obfuscated.
     for (bool obfuscate : {false, true}) {
         fs::path ph = m_args.GetDataDirBase() / (obfuscate ? "dbwrapper_1_obfuscate_true" : "dbwrapper_1_obfuscate_false");
-        CDBWrapper dbw(ph, (1 << 20), false, true, obfuscate);
+        CDBWrapper dbw{{
+            .db_path = ph,
+            .cache_size = 1 << 20,
+            .in_memory = false,
+            .wipe_existing = true,
+            .obfuscate_data = obfuscate,
+        }};
 
         uint256 res;
         uint32_t res_uint_32;
@@ -127,7 +139,13 @@ BOOST_AUTO_TEST_CASE(dbwrapper_batch)
     // Perform tests both obfuscated and non-obfuscated.
     for (const bool obfuscate : {false, true}) {
         fs::path ph = m_args.GetDataDirBase() / (obfuscate ? "dbwrapper_batch_obfuscate_true" : "dbwrapper_batch_obfuscate_false");
-        CDBWrapper dbw(ph, (1 << 20), true, false, obfuscate);
+        CDBWrapper dbw{{
+            .db_path = ph,
+            .cache_size = 1 << 20,
+            .in_memory = true,
+            .wipe_existing = false,
+            .obfuscate_data = obfuscate,
+        }};
 
         uint8_t key{'i'};
         uint256 in = InsecureRand256();
@@ -163,7 +181,13 @@ BOOST_AUTO_TEST_CASE(dbwrapper_iterator)
     // Perform tests both obfuscated and non-obfuscated.
     for (const bool obfuscate : {false, true}) {
         fs::path ph = m_args.GetDataDirBase() / (obfuscate ? "dbwrapper_iterator_obfuscate_true" : "dbwrapper_iterator_obfuscate_false");
-        CDBWrapper dbw(ph, (1 << 20), true, false, obfuscate);
+        CDBWrapper dbw{{
+            .db_path = ph,
+            .cache_size = 1 << 20,
+            .in_memory = true,
+            .wipe_existing = false,
+            .obfuscate_data = obfuscate,
+        }};
 
         // The two keys are intentionally chosen for ordering
         uint8_t key{'j'};
@@ -205,21 +229,33 @@ BOOST_AUTO_TEST_CASE(existing_data_no_obfuscate)
     fs::path ph = m_args.GetDataDirBase() / "existing_data_no_obfuscate";
     fs::create_directories(ph);
 
-    // Set up a non-obfuscated wrapper to write some initial data.
-    std::unique_ptr<CDBWrapper> dbw = std::make_unique<CDBWrapper>(ph, (1 << 10), false, false, false);
     uint8_t key{'k'};
     uint256 in = InsecureRand256();
-    uint256 res;
 
-    BOOST_CHECK(dbw->Write(key, in));
-    BOOST_CHECK(dbw->Read(key, res));
-    BOOST_CHECK_EQUAL(res.ToString(), in.ToString());
+    {
+        // Set up a non-obfuscated wrapper to write some initial data.
+        CDBWrapper dbw{{
+            .db_path = ph,
+            .cache_size = 1 << 10,
+            .in_memory = false,
+            .wipe_existing = false,
+            .obfuscate_data = false,
+        }};
+        uint256 res;
 
-    // Call the destructor to free leveldb LOCK
-    dbw.reset();
+        BOOST_CHECK(dbw.Write(key, in));
+        BOOST_CHECK(dbw.Read(key, res));
+        BOOST_CHECK_EQUAL(res.ToString(), in.ToString());
+    }
 
     // Now, set up another wrapper that wants to obfuscate the same directory
-    CDBWrapper odbw(ph, (1 << 10), false, false, true);
+    CDBWrapper odbw{{
+        .db_path = ph,
+        .cache_size = 1 << 10,
+        .in_memory = false,
+        .wipe_existing = false,
+        .obfuscate_data = true,
+    }};
 
     // Check that the key/val we wrote with unobfuscated wrapper exists and
     // is readable.
@@ -246,21 +282,33 @@ BOOST_AUTO_TEST_CASE(existing_data_reindex)
     fs::path ph = m_args.GetDataDirBase() / "existing_data_reindex";
     fs::create_directories(ph);
 
-    // Set up a non-obfuscated wrapper to write some initial data.
-    std::unique_ptr<CDBWrapper> dbw = std::make_unique<CDBWrapper>(ph, (1 << 10), false, false, false);
     uint8_t key{'k'};
     uint256 in = InsecureRand256();
-    uint256 res;
 
-    BOOST_CHECK(dbw->Write(key, in));
-    BOOST_CHECK(dbw->Read(key, res));
-    BOOST_CHECK_EQUAL(res.ToString(), in.ToString());
+    {
+        // Set up a non-obfuscated wrapper to write some initial data.
+        CDBWrapper dbw{{
+            .db_path = ph,
+            .cache_size = 1 << 10,
+            .in_memory = false,
+            .wipe_existing = false,
+            .obfuscate_data = false,
+        }};
+        uint256 res;
 
-    // Call the destructor to free leveldb LOCK
-    dbw.reset();
+        BOOST_CHECK(dbw.Write(key, in));
+        BOOST_CHECK(dbw.Read(key, res));
+        BOOST_CHECK_EQUAL(res.ToString(), in.ToString());
+    }
 
     // Simulate a -reindex by wiping the existing data store
-    CDBWrapper odbw(ph, (1 << 10), false, true, true);
+    CDBWrapper odbw{{
+        .db_path = ph,
+        .cache_size = 1 << 10,
+        .in_memory = false,
+        .wipe_existing = true,
+        .obfuscate_data = true,
+    }};
 
     // Check that the key/val we wrote with unobfuscated wrapper doesn't exist
     uint256 res2;
@@ -279,7 +327,13 @@ BOOST_AUTO_TEST_CASE(existing_data_reindex)
 BOOST_AUTO_TEST_CASE(iterator_ordering)
 {
     fs::path ph = m_args.GetDataDirBase() / "iterator_ordering";
-    CDBWrapper dbw(ph, (1 << 20), true, false, false);
+    CDBWrapper dbw{{
+        .db_path = ph,
+        .cache_size = 1 << 20,
+        .in_memory = true,
+        .wipe_existing = false,
+        .obfuscate_data = false,
+    }};
     for (int x=0x00; x<256; ++x) {
         uint8_t key = x;
         uint32_t value = x*x;
@@ -359,7 +413,13 @@ BOOST_AUTO_TEST_CASE(iterator_string_ordering)
     char buf[10];
 
     fs::path ph = m_args.GetDataDirBase() / "iterator_string_ordering";
-    CDBWrapper dbw(ph, (1 << 20), true, false, false);
+    CDBWrapper dbw{{
+        .db_path = ph,
+        .cache_size = 1 << 20,
+        .in_memory = true,
+        .wipe_existing = false,
+        .obfuscate_data = false,
+    }};
     for (int x=0x00; x<10; ++x) {
         for (int y = 0; y < 10; y++) {
             snprintf(buf, sizeof(buf), "%d", x);
@@ -405,7 +465,10 @@ BOOST_AUTO_TEST_CASE(unicodepath)
     // the ANSI CreateDirectoryA call and the code page isn't UTF8.
     // It will succeed if created with CreateDirectoryW.
     fs::path ph = m_args.GetDataDirBase() / "test_runner_₿_🏃_20191128_104644";
-    CDBWrapper dbw(ph, (1 << 20));
+    CDBWrapper dbw{{
+        .db_path = ph,
+        .cache_size = 1 << 20,
+    }};
 
     fs::path lockPath = ph / "LOCK";
     BOOST_CHECK(fs::exists(lockPath));
