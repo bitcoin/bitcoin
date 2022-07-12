fa97a528d6382a0163d5aa7d37ecbf93579b8186
MacroFake <falke.marco@gmail.com>
2022-07-12 17:49:21 +0200

    Fix UB/data-race in RPCNotifyBlockChange
    ActiveTip() is *not* thread-safe, as the required ::cs_main lock will be
    released as ActiveChainstate() returns.
    
    ActiveTip() is an alias for ActiveChainstate().m_chain.Tip(), so m_chain
    may be involved in a data-race (UB).

--- src/init.cpp
+++ src/init.cpp
@@ -1751,12 +1751,12 @@ bool AppInitMain(NodeContext& node, interfaces::BlockAndHeaderTipInfo* tip_info)
     // At this point, the RPC is "started", but still in warmup, which means it
     // cannot yet be called. Before we make it callable, we need to make sure
     // that the RPC's view of the best block is valid and consistent with
-    // ChainstateManager's ActiveTip.
+    // ChainstateManager's active tip.
     //
     // If we do not do this, RPC's view of the best block will be height=0 and
     // hash=0x0. This will lead to erroroneous responses for things like
     // waitforblockheight.
-    RPCNotifyBlockChange(chainman.ActiveTip());
+    RPCNotifyBlockChange(WITH_LOCK(chainman.GetMutex(), return chainman.ActiveTip()));
     SetRPCWarmupFinished();
 
     uiInterface.InitMessage(_("Done loading").translated);
