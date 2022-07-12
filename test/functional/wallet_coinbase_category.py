098748ac6ac29018fbee21dab4bd1afaf44b44fe
MacroFake <falke.marco@gmail.com>
2022-07-12 08:04:51 +0200

    Merge bitcoin/bitcoin#25589: test: speedup wallet_coinbase_category.py
    76a84c0a6cac3df711b1ed907a46c905cb85c485 test: speedup wallet_coinbase_category.py (furszy)
    
    Pull request description:
    
      No need to create a chain (200 extra blocks), nor use the cache, for it.
    
    Top commit has no ACKs.
    
    Tree-SHA512: beec64ba6c580d475e19700371ae155f4f3d74325879802da83d02f4153a752d5829b8a4ed77e0c893e79cbc26f66389eed90ac2e8b03a59f6c630ee9333355c

--- test/functional/wallet_coinbase_category.py
+++ test/functional/wallet_coinbase_category.py
@@ -15,6 +15,7 @@ from test_framework.util import (
 class CoinbaseCategoryTest(BitcoinTestFramework):
     def set_test_params(self):
         self.num_nodes = 1
+        self.setup_clean_chain = True
 
     def skip_test_if_missing_module(self):
         self.skip_if_no_wallet()
