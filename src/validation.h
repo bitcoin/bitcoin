38377002671d038aadb01e1521ea95c97838cedc
Carl Dong <contact@carldong.me>
2022-07-13 14:15:11 -0400

    Move ChainstateManagerOpts into kernel:: namespace
    It should have been there in the first place.

--- src/validation.h
+++ src/validation.h
@@ -834,7 +834,7 @@ private:
     friend CChainState;
 
 public:
-    using Options = ChainstateManagerOpts;
+    using Options = kernel::ChainstateManagerOpts;
 
     explicit ChainstateManager(const Options& opts)
         : m_chainparams{opts.chainparams},
