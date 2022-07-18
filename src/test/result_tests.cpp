93cee983b051175b3ffdc727b1a9a621d02f9a4c
Ryan Ofsky <ryan@ofsky.org>
2022-07-18 15:48:04 -0400

    refactor: Add util::Result failure values
    Add util::Result support for returning more error information. For better error
    handling, adds the ability to return a value on failure, not just a value on
    success. This is a key missing feature that made the result class not useful
    for functions like LoadChainstate() which produce different errors that need to
    be handled differently.
    
    This change also makes some more minor improvements:
    
    - Smaller type size. util::Result<int> is 16 bytes, and util::Result<void> is 8
      bytes. Previously util::Result<int> and util::Result<void> were 72 bytes.
    
    - More documentation and tests.

--- src/test/result_tests.cpp
+++ src/test/result_tests.cpp
@@ -2,6 +2,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#include <util/check.h>
 #include <util/result.h>
 
 #include <boost/test/unit_test.hpp>
@@ -33,6 +34,34 @@ std::ostream& operator<<(std::ostream& os, const NoCopy& o)
     return os << "NoCopy(" << *o.m_n << ")";
 }
 
+struct NoCopyNoMove {
+    NoCopyNoMove(int n) : m_n{n} {}
+    NoCopyNoMove(const NoCopyNoMove&) = delete;
+    NoCopyNoMove(NoCopyNoMove&&) = delete;
+    int m_n;
+};
+
+bool operator==(const NoCopyNoMove& a, const NoCopyNoMove& b)
+{
+    return a.m_n == b.m_n;
+}
+
+std::ostream& operator<<(std::ostream& os, const NoCopyNoMove& o)
+{
+    os << "NoCopyNoMove(" << o.m_n << ")";
+    return os;
+}
+
+util::Result<void> VoidSuccessFn()
+{
+    return {};
+}
+
+util::Result<void> VoidFailFn()
+{
+    return util::Error{Untranslated("void fail.")};
+}
+
 util::Result<int> IntFn(int i, bool success)
 {
     if (success) return i;
@@ -45,21 +74,46 @@ util::Result<bilingual_str> StrFn(bilingual_str s, bool success)
     return util::Error{strprintf(Untranslated("str %s error."), s.original)};
 }
 
-util::Result<NoCopy> NoCopyFn(int i, bool success)
+util::Result<NoCopy, NoCopy> NoCopyFn(int i, bool success)
 {
     if (success) return {i};
-    return util::Error{Untranslated(strprintf("nocopy %i error.", i))};
+    return {util::Error{Untranslated(strprintf("nocopy %i error.", i))}, i};
 }
 
-template <typename T>
-void ExpectResult(const util::Result<T>& result, bool success, const bilingual_str& str)
+util::Result<NoCopyNoMove, NoCopyNoMove> NoCopyNoMoveFn(int i, bool success)
+{
+    if (success) return {i};
+    return {util::Error{Untranslated(strprintf("nocopynomove %i error.", i))}, i};
+}
+
+enum FnError { ERR1, ERR2 };
+
+util::Result<int, FnError> IntFailFn(int i, bool success)
+{
+    if (success) return i;
+    return {util::Error{Untranslated(strprintf("int %i error.", i))}, i % 2 ? ERR1 : ERR2};
+}
+
+util::Result<NoCopyNoMove, FnError> EnumFailFn(FnError ret)
+{
+    return {util::Error{Untranslated("enum fail.")}, ret};
+}
+
+util::Result<int, int> TruthyFalsyFn(int i, bool success)
+{
+    if (success) return i;
+    return {util::Error{Untranslated(strprintf("failure value %i.", i))}, i};
+}
+
+template <typename T, typename F>
+void ExpectResult(const util::Result<T, F>& result, bool success, const bilingual_str& str)
 {
     BOOST_CHECK_EQUAL(bool(result), success);
     BOOST_CHECK_EQUAL(util::ErrorString(result), str);
 }
 
-template <typename T, typename... Args>
-void ExpectSuccess(const util::Result<T>& result, const bilingual_str& str, Args&&... args)
+template <typename T, typename F, typename... Args>
+void ExpectSuccess(const util::Result<T, F>& result, const bilingual_str& str, Args&&... args)
 {
     ExpectResult(result, true, str);
     BOOST_CHECK_EQUAL(result.has_value(), true);
@@ -67,40 +121,61 @@ void ExpectSuccess(const util::Result<T>& result, const bilingual_str& str, Args
     BOOST_CHECK_EQUAL(&result.value(), &*result);
 }
 
-template <typename T, typename... Args>
-void ExpectFail(const util::Result<T>& result, const bilingual_str& str)
+template <typename T, typename F, typename... Args>
+void ExpectFail(const util::Result<T, F>& result, bilingual_str str, Args&&... args)
 {
     ExpectResult(result, false, str);
+    BOOST_CHECK_EQUAL(result.GetFailure(), F{std::forward<Args>(args)...});
 }
 
 BOOST_AUTO_TEST_CASE(check_returned)
 {
+    ExpectResult(VoidSuccessFn(), true, {});
+    ExpectResult(VoidFailFn(), false, Untranslated("void fail."));
     ExpectSuccess(IntFn(5, true), {}, 5);
-    ExpectFail(IntFn(5, false), Untranslated("int 5 error."));
+    ExpectResult(IntFn(5, false), false, Untranslated("int 5 error."));
     ExpectSuccess(NoCopyFn(5, true), {}, 5);
-    ExpectFail(NoCopyFn(5, false), Untranslated("nocopy 5 error."));
+    ExpectFail(NoCopyFn(5, false), Untranslated("nocopy 5 error."), 5);
+    ExpectSuccess(NoCopyNoMoveFn(5, true), {}, 5);
+    ExpectFail(NoCopyNoMoveFn(5, false), Untranslated("nocopynomove 5 error."), 5);
     ExpectSuccess(StrFn(Untranslated("S"), true), {}, Untranslated("S"));
-    ExpectFail(StrFn(Untranslated("S"), false), Untranslated("str S error."));
+    ExpectResult(StrFn(Untranslated("S"), false), false, Untranslated("str S error."));
+    ExpectFail(EnumFailFn(ERR2), Untranslated("enum fail."), ERR2);
+    ExpectSuccess(TruthyFalsyFn(0, true), {}, 0);
+    ExpectFail(TruthyFalsyFn(0, false), Untranslated("failure value 0."), 0);
+    ExpectSuccess(TruthyFalsyFn(1, true), {}, 1);
+    ExpectFail(TruthyFalsyFn(1, false), Untranslated("failure value 1."), 1);
 }
 
 BOOST_AUTO_TEST_CASE(check_set)
 {
     // Test using Set method to change a result value from success -> failure,
     // and failure->success.
-    util::Result<int> result{0};
+    util::Result<int, FnError> result;
     ExpectSuccess(result, {}, 0);
-    result.Set({util::Error{Untranslated("error")}});
-    ExpectFail(result, Untranslated("error"));
+    result.Set({util::Error{Untranslated("error")}, ERR1});
+    ExpectFail(result, Untranslated("error"), ERR1);
     result.Set(2);
     ExpectSuccess(result, Untranslated(""), 2);
 
     // Test the same thing but with non-copyable success and failure types.
-    util::Result<NoCopy> result2{0};
+    util::Result<NoCopy, NoCopy> result2{0};
     ExpectSuccess(result2, {}, 0);
-    result2.Set({util::Error{Untranslated("error")}});
-    ExpectFail(result2, Untranslated("error"));
-    result2.Set(util::Result<NoCopy>{4});
+    result2.Set({util::Error{Untranslated("error")}, 3});
+    ExpectFail(result2, Untranslated("error"), 3);
+    result2.Set(4);
     ExpectSuccess(result2, Untranslated(""), 4);
+}
+
+BOOST_AUTO_TEST_CASE(check_dereference_operators)
+{
+    util::Result<std::pair<int, std::string>> mutable_result;
+    const auto& const_result{mutable_result};
+    mutable_result.value() = {1, "23"};
+    BOOST_CHECK_EQUAL(mutable_result->first, 1);
+    BOOST_CHECK_EQUAL(const_result->second, "23");
+    (*mutable_result).first = 5;
+    BOOST_CHECK_EQUAL((*const_result).first, 5);
 }
 
 BOOST_AUTO_TEST_CASE(check_value_or)
@@ -111,6 +186,20 @@ BOOST_AUTO_TEST_CASE(check_value_or)
     BOOST_CHECK_EQUAL(NoCopyFn(10, false).value_or(20), 20);
     BOOST_CHECK_EQUAL(StrFn(Untranslated("A"), true).value_or(Untranslated("B")), Untranslated("A"));
     BOOST_CHECK_EQUAL(StrFn(Untranslated("A"), false).value_or(Untranslated("B")), Untranslated("B"));
+}
+
+struct Derived : NoCopyNoMove {
+    using NoCopyNoMove::NoCopyNoMove;
+};
+
+util::Result<std::unique_ptr<NoCopyNoMove>> DerivedToBaseFn(util::Result<std::unique_ptr<Derived>> derived)
+{
+    return derived;
+}
+
+BOOST_AUTO_TEST_CASE(derived_to_base)
+{
+    BOOST_CHECK_EQUAL(*Assert(*Assert(DerivedToBaseFn(std::make_unique<Derived>(5)))), 5);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
--- src/util/result.h
+++ src/util/result.h
@@ -8,68 +8,188 @@
 #include <attributes.h>
 #include <util/translation.h>
 
-#include <variant>
+#include <cassert>
+#include <memory>
+#include <new>
+#include <optional>
+#include <type_traits>
+#include <utility>
+#include <vector>
 
 namespace util {
 
+//! The `Result` class provides a standard way for functions to return an error
+//! string in addition to optional result values.
+//!
+//! The easiest way to understand `Result<T>` is to think of it as a substitute
+//! for `std::optional<T>`, that just contains an error string in
+//! addition to the optional return value. For example:
+//!
+//!    util::Result<int> AddNumbers(int a, int b)
+//!    {
+//!        if (b == 0) return util::Error{_("Not adding 0, that's dumb.")};
+//!        return a + b;
+//!    }
+//!
+//!    void TryAddNumbers(int a, int b)
+//!    {
+//!        if (auto result = AddNumbers(a, b)) {
+//!            LogPrintf("%i + %i = %i\n", a, b, *result);
+//!        } else {
+//!            LogPrintf("Error: %s\n", util::ErrorString(result).translated);
+//!        }
+//!    }
+//!
+//! The `Result` class is intended to be used for high-level functions that need
+//! to report error messages to end users. Low-level functions that don't need
+//! error-reporting and only need error-handling should avoid `Result` and
+//! instead use standard classes like `std::optional`, `std::variant`,
+//! `std::expected`, and `std::tuple`, or custom structs and enum types to
+//! return function results.
+//!
+//! Usage examples can be found in \example ../test/result_tests.cpp, but in
+//! general code using `Result<T>` return values is similar to code using
+//! `std::optional<T>` return values. Existing functions returning
+//! `std::optional<T>` can be updated to return `Result<T>` usually just
+//! replacing `return std::nullopt;` with `return util::Error{error_string};`.
+//!
+//! An optional failure type `F` can be passed as a `Result<T, F>` template
+//! argument. The default type `F = void` is sufficient for most code that just
+//! needs an error description without more complicated error handling. But code
+//! that does need more fine-grained failure information can set custom error
+//! values of type `F` and retrieve them with the `GetFailure()` method.
+template <typename T, typename F = void>
+class Result;
+
+//! Wrapper type to pass error strings to Result constructors.
 struct Error {
     bilingual_str message;
 };
 
-//! The util::Result class provides a standard way for functions to return
-//! either error messages or result values.
-//!
-//! It is intended for high-level functions that need to report error strings to
-//! end users. Lower-level functions that don't need this error-reporting and
-//! only need error-handling should avoid util::Result and instead use standard
-//! classes like std::optional, std::variant, and std::tuple, or custom structs
-//! and enum types to return function results.
-//!
-//! Usage examples can be found in \example ../test/result_tests.cpp, but in
-//! general code returning `util::Result<T>` values is very similar to code
-//! returning `std::optional<T>` values. Existing functions returning
-//! `std::optional<T>` can be updated to return `util::Result<T>` and return
-//! error strings usually just replacing `return std::nullopt;` with `return
-//! util::Error{error_string};`.
-template <class M>
-class Result
-{
-private:
-    using T = std::conditional_t<std::is_same_v<M, void>, std::monostate, M>;
+namespace detail {
+//! Substitute for std::monostate that doesn't depend on std::variant.
+struct MonoState{};
 
-    std::variant<bilingual_str, T> m_variant;
+//! Error information only allocated on failure.
+template <typename F>
+struct ErrorInfo {
+    std::conditional_t<std::is_same_v<F, void>, MonoState, F> failure;
+    bilingual_str error;
+};
+
+//! Result base class which is inherited by Result<T, F>.
+//! T is the type of the success return value, or void if there is none.
+//! F is the type of the failure return value, or void if there is none.
+template <typename T, typename F>
+class ResultBase;
+
+//! Result base specialization for empty (T=void) value type. Holds error
+//! information and provides accessor methods.
+template <typename F>
+class ResultBase<void, F>
+{
+protected:
+    std::unique_ptr<ErrorInfo<F>> m_info;
+
+    //! Value setter methods which do nothing because this ResultBase class is
+    //! specialized for type T=void, so there is no result value for it to hold.
+    //! The other ResultBase specialization below for non-void T overrides these
+    //! methods to manage the result value it contains.
+    void ConstructValue() {}
+    template <typename O>
+    void MoveValue(O&& other) {}
+    void DestroyValue() {}
+
+    //! Helper function to construct a new Result success value or failure value
+    //! using the arguments provided.
+    template <bool Failure, typename Result, typename... Args>
+    static void ConstructResult(Result& result, Args&&... args)
+    {
+        if constexpr (Failure) {
+            static_assert(sizeof...(args) > 0 || !std::is_scalar_v<F>,
+                "Refusing to default-construct failure value with int, float, enum, or pointer type, please specify an explicit failure value.");
+            result.m_info.reset(new detail::ErrorInfo<F>{.failure{std::forward<Args>(args)...}, .error{}});
+        } else {
+            result.ConstructValue(std::forward<Args>(args)...);
+        }
+    }
+
+    //! ConstructResult() overload peeling off a util::Error constructor argument.
+    template <bool Failure, typename Result, typename... Args>
+    static void ConstructResult(Result& result, util::Error error, Args&&... args)
+    {
+        ConstructResult</*Failure=*/true>(result, std::forward<Args>(args)...);
+        result.m_info->error = std::move(error.message);
+    }
+
+    //! Helper function to move success or failure values and any error
+    //! message from one result object to another. The two result
+    //! objects must have compatible success and failure types.
+    template <bool Constructed, typename DstResult, typename SrcResult>
+    static void MoveResult(DstResult& dst, SrcResult&& src)
+    {
+        if constexpr (Constructed) {
+            if (dst) {
+                dst.DestroyValue();
+            } else {
+                dst.m_info.reset();
+            }
+        }
+        if (src) {
+            dst.MoveValue(std::move(src));
+        } else {
+            dst.m_info.reset(new detail::ErrorInfo<F>{std::move(*src.m_info)});
+        }
+    }
 
     //! Disallow operator= and require explicit Result::Set() calls to avoid
     //! confusion in the future when the Result class gains support for richer
     //! error reporting, and callers should have ability to set a new result
     //! value without clearing existing error messages.
-    Result& operator=(const Result&) = default;
-    Result& operator=(Result&&) = default;
+    template <typename Result>
+    ResultBase& operator=(Result&&) = delete;
 
-    template <typename FT>
-    friend bilingual_str ErrorString(const Result<FT>& result);
+    template <typename, typename>
+    friend class ResultBase;
 
 public:
-    Result() : m_variant{std::in_place_index_t<1>{}, std::monostate{}} {}  // constructor for void
-    Result(T obj) : m_variant{std::in_place_index_t<1>{}, std::move(obj)} {}
-    Result(Error error) : m_variant{std::in_place_index_t<0>{}, std::move(error.message)} {}
-    Result(const Result&) = default;
-    Result(Result&&) = default;
-    ~Result() = default;
+    //! Success check.
+    explicit operator bool() const { return !m_info; }
 
+    //! Error retrieval.
+    const auto& GetFailure() const LIFETIMEBOUND { assert(!*this); return m_info->failure; }
+
+    static constexpr bool is_result{true};
+};
+
+//! Result base class for T value type. Holds value and provides accessor methods.
+template <typename T, typename F>
+class ResultBase : public ResultBase<void, F>
+{
+protected:
+    //! Result success value. Uses anonymous union so success value is never
+    //! constructed in failure case.
+    union { T m_value; };
+
+    template <typename... Args>
+    void ConstructValue(Args&&... args) { new (&m_value) T{std::forward<Args>(args)...}; }
+    template <typename O>
+    void MoveValue(O&& other) { new (&m_value) T{std::move(other.m_value)}; }
+    void DestroyValue() { m_value.~T(); }
+
+    //! Empty constructor that needs to be declared because the class contains a union.
+    ResultBase() {}
+    ~ResultBase() { if (*this) DestroyValue(); }
+
+    template <typename, typename>
+    friend class ResultBase;
+
+public:
     //! std::optional methods, so functions returning optional<T> can change to
     //! return Result<T> with minimal changes to existing code, and vice versa.
-    bool has_value() const noexcept { return m_variant.index() == 1; }
-    const T& value() const LIFETIMEBOUND
-    {
-        assert(has_value());
-        return std::get<1>(m_variant);
-    }
-    T& value() LIFETIMEBOUND
-    {
-        assert(has_value());
-        return std::get<1>(m_variant);
-    }
+    bool has_value() const { return bool{*this}; }
+    const T& value() const LIFETIMEBOUND { assert(has_value()); return m_value; }
+    T& value() LIFETIMEBOUND { assert(has_value()); return m_value; }
     template <class U>
     T value_or(U&& default_value) const&
     {
@@ -80,21 +200,57 @@ public:
     {
         return has_value() ? std::move(value()) : std::forward<U>(default_value);
     }
-    explicit operator bool() const noexcept { return has_value(); }
     const T* operator->() const LIFETIMEBOUND { return &value(); }
     const T& operator*() const LIFETIMEBOUND { return value(); }
     T* operator->() LIFETIMEBOUND { return &value(); }
     T& operator*() LIFETIMEBOUND { return value(); }
+};
+} // namespace detail
 
-    //! Assign success or failure value from another result object.
-    Result& Set(Result&& other) LIFETIMEBOUND { return *this = std::move(other); }
+template <typename T, typename F>
+class Result : public detail::ResultBase<T, F>
+{
+protected:
+    using Base = detail::ResultBase<T, F>;
+
+public:
+    //! Construct a Result object setting a success or failure value. An
+    //! optional util::Error argument is processed first to set an error
+    //! message. Then, any remaining arguments are passed to the type `T`
+    //! constructor and used to construct a success value in the success case.
+    //! In the failure case, any remaining arguments are passed to the type `F`
+    //! constructor and used to construct a failure value.
+    template <typename... Args>
+    Result(Args&&... args)
+    {
+        Base::template ConstructResult</*Failure=*/false>(*this, std::forward<Args>(args)...);
+    }
+
+    //! Move-construct a Result object from another Result object, moving the
+    //! success or failure value and any error message.
+    template <typename O>
+    requires (std::decay_t<O>::is_result)
+    Result(O&& other)
+    {
+        Base::template MoveResult</*Constructed=*/false>(*this, std::move(other));
+    }
+
+    //! Move success or failure values and any error message from another Result
+    //! object to this object.
+    Result& Set(Result&& other) LIFETIMEBOUND
+    {
+        Base::template MoveResult</*Constructed=*/true>(*this, std::move(other));
+        return *this;
+    }
+
+    inline friend bilingual_str _ErrorString(const Result& result)
+    {
+        return result ? bilingual_str{} : result.m_info->error;
+    }
 };
 
-template <typename T>
-bilingual_str ErrorString(const Result<T>& result)
-{
-    return result ? bilingual_str{} : std::get<0>(result.m_variant);
-}
+template <typename T, typename F>
+bilingual_str ErrorString(const Result<T, F>& result) { return _ErrorString(result); }
 } // namespace util
 
 #endif // BITCOIN_UTIL_RESULT_H
