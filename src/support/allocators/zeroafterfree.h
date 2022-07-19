6143406022a4c51c561d49a93d17f9914d073917
jerrymae1999 <jggabutan@up.edu.ph>
2022-07-19 17:47:15 +0800

    First commit on editting src_support'

--- src/support/allocators/zeroafterfree.h
+++ src/support/allocators/zeroafterfree.h
@@ -1,10 +1,10 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2021 The Bitcoin Core developers
+// Copyright (c) 2009-2021 The Revolt Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_SUPPORT_ALLOCATORS_ZEROAFTERFREE_H
-#define BITCOIN_SUPPORT_ALLOCATORS_ZEROAFTERFREE_H
+#ifndef REVOLT_SUPPORT_ALLOCATORS_ZEROAFTERFREE_H
+#define REVOLT_SUPPORT_ALLOCATORS_ZEROAFTERFREE_H
 
 #include <support/cleanse.h>
 
@@ -43,4 +43,4 @@ struct zero_after_free_allocator : public std::allocator<T> {
 /** Byte-vector that clears its contents before deletion. */
 using SerializeData = std::vector<std::byte, zero_after_free_allocator<std::byte>>;
 
-#endif // BITCOIN_SUPPORT_ALLOCATORS_ZEROAFTERFREE_H
+#endif // REVOLT_SUPPORT_ALLOCATORS_ZEROAFTERFREE_H
