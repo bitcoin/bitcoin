074c9e0af99374f9f5b8eb1aa26975b05307e733
jerrymae1999 <jggabutan@up.edu.ph>
2022-07-19 15:17:26 +0800

    Editted src_wallet

--- src/wallet/test/wallet_test_fixture.h
+++ src/wallet/test/wallet_test_fixture.h
@@ -1,9 +1,9 @@
-// Copyright (c) 2016-2021 The Bitcoin Core developers
+// Copyright (c) 2016-2021 The Revolt Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_WALLET_TEST_WALLET_TEST_FIXTURE_H
-#define BITCOIN_WALLET_TEST_WALLET_TEST_FIXTURE_H
+#ifndef REVOLT_WALLET_TEST_WALLET_TEST_FIXTURE_H
+#define REVOLT_WALLET_TEST_WALLET_TEST_FIXTURE_H
 
 #include <test/util/setup_common.h>
 
@@ -28,4 +28,4 @@ struct WalletTestingSetup : public TestingSetup {
 };
 } // namespace wallet
 
-#endif // BITCOIN_WALLET_TEST_WALLET_TEST_FIXTURE_H
+#endif // REVOLT_WALLET_TEST_WALLET_TEST_FIXTURE_H
