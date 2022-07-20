a25a5d3e00beafff0d1f7e28764fcc2d6861aef2
jerrymae1999 <jggabutan@up.edu.ph>
2022-07-20 15:46:56 +0800

    First commit on src_rpc

--- src/rpc/rawtransaction_util.h
+++ src/rpc/rawtransaction_util.h
@@ -1,9 +1,9 @@
-// Copyright (c) 2017-2021 The Bitcoin Core developers
+// Copyright (c) 2017-2021 The Revolt Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_RPC_RAWTRANSACTION_UTIL_H
-#define BITCOIN_RPC_RAWTRANSACTION_UTIL_H
+#ifndef REVOLT_RPC_RAWTRANSACTION_UTIL_H
+#define REVOLT_RPC_RAWTRANSACTION_UTIL_H
 
 #include <map>
 #include <string>
@@ -40,4 +40,4 @@ void ParsePrevouts(const UniValue& prevTxsUnival, FillableSigningProvider* keyst
 /** Create a transaction from univalue parameters */
 CMutableTransaction ConstructTransaction(const UniValue& inputs_in, const UniValue& outputs_in, const UniValue& locktime, bool rbf);
 
-#endif // BITCOIN_RPC_RAWTRANSACTION_UTIL_H
+#endif // REVOLT_RPC_RAWTRANSACTION_UTIL_H
