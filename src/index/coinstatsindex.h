b588c9ea963c13d9c50165c3e60347cb7289a814
jerrymae1999 <jggabutan@up.edu.ph>
2022-07-20 18:29:34 +0800

    Editted src_index

--- src/index/coinstatsindex.h
+++ src/index/coinstatsindex.h
@@ -1,9 +1,9 @@
-// Copyright (c) 2020-2021 The Bitcoin Core developers
+// Copyright (c) 2020-2021 The Revolt Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_INDEX_COINSTATSINDEX_H
-#define BITCOIN_INDEX_COINSTATSINDEX_H
+#ifndef REVOLT_INDEX_COINSTATSINDEX_H
+#define REVOLT_INDEX_COINSTATSINDEX_H
 
 #include <chain.h>
 #include <crypto/muhash.h>
@@ -62,4 +62,4 @@ public:
 /// The global UTXO set hash object.
 extern std::unique_ptr<CoinStatsIndex> g_coin_stats_index;
 
-#endif // BITCOIN_INDEX_COINSTATSINDEX_H
+#endif // REVOLT_INDEX_COINSTATSINDEX_H
