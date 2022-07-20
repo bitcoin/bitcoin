e5634ad3103510e495cb3d402041a205cceb5e3b
jerrymae1999 <jggabutan@up.edu.ph>
2022-07-20 18:16:15 +0800

    Editted src_ipc

--- src/ipc/exception.h
+++ src/ipc/exception.h
@@ -1,9 +1,9 @@
-// Copyright (c) 2021 The Bitcoin Core developers
+// Copyright (c) 2021 The Revolt Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_IPC_EXCEPTION_H
-#define BITCOIN_IPC_EXCEPTION_H
+#ifndef REVOLT_IPC_EXCEPTION_H
+#define REVOLT_IPC_EXCEPTION_H
 
 #include <stdexcept>
 
@@ -17,4 +17,4 @@ public:
 };
 } // namespace ipc
 
-#endif // BITCOIN_IPC_EXCEPTION_H
+#endif // REVOLT_IPC_EXCEPTION_H
