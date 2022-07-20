5960977152692f0b54f11604ba1491e3ce7f20ed
jerrymae1999 <jggabutan@up.edu.ph>
2022-07-20 18:20:24 +0800

    Editted src_interfaces

--- src/interfaces/ipc.h
+++ src/interfaces/ipc.h
@@ -1,9 +1,9 @@
-// Copyright (c) 2021 The Bitcoin Core developers
+// Copyright (c) 2021 The Revolt Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_INTERFACES_IPC_H
-#define BITCOIN_INTERFACES_IPC_H
+#ifndef REVOLT_INTERFACES_IPC_H
+#define REVOLT_INTERFACES_IPC_H
 
 #include <functional>
 #include <memory>
@@ -75,4 +75,4 @@ protected:
 std::unique_ptr<Ipc> MakeIpc(const char* exe_name, const char* process_argv0, Init& init);
 } // namespace interfaces
 
-#endif // BITCOIN_INTERFACES_IPC_H
+#endif // REVOLT_INTERFACES_IPC_H
