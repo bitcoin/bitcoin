df6dccde8e1cfc498700b3e10ab3b873156a3aed
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-07-20 16:41:56 +0100

    build: Make packages related variables simply expanded ones

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -6,8 +6,8 @@ $(package)_file_name := qtbase-$($(package)_suffix)
 $(package)_sha256_hash := 26394ec9375d52c1592bd7b689b1619c6b8dbe9b6f91fdd5c355589787f3a0b6
 $(package)_linux_dependencies := freetype fontconfig libxcb libxkbcommon libxcb_util libxcb_util_render libxcb_util_keysyms libxcb_util_image libxcb_util_wm
 $(package)_qt_libs := corelib network widgets gui plugins testlib
-$(package)_linguist_tools = lrelease lupdate lconvert
-$(package)_patches = qt.pro
+$(package)_linguist_tools := lrelease lupdate lconvert
+$(package)_patches := qt.pro
 $(package)_patches += qttools_src.pro
 $(package)_patches += mac-qmake.conf
 $(package)_patches += fix_qt_pkgconfig.patch
@@ -29,7 +29,7 @@ $(package)_qttranslations_sha256_hash := 5d7869f670a135ad0986e266813b9dd5bbae2b0
 $(package)_qttools_file_name := qttools-$($(package)_suffix)
 $(package)_qttools_sha256_hash := 463b2fe71a085e7ab4e39333ae360ab0ec857b966d7a08f752c427e5df55f90d
 
-$(package)_extra_sources  = $($(package)_qttranslations_file_name)
+$(package)_extra_sources := $($(package)_qttranslations_file_name)
 $(package)_extra_sources += $($(package)_qttools_file_name)
 
 define $(package)_set_vars
