32fccfebcdb9a0311515d5aea112430e4536987c
Ryan Ofsky <ryan@ofsky.org>
2022-07-25 17:40:49 -0400

    refactor: Use util::Result class in wallet/sqlite

--- src/wallet/test/db_tests.cpp
+++ src/wallet/test/db_tests.cpp
@@ -64,10 +64,8 @@ static std::vector<std::unique_ptr<WalletDatabase>> TestDatabases(const fs::path
 {
     std::vector<std::unique_ptr<WalletDatabase>> dbs;
     DatabaseOptions options;
-    DatabaseStatus status;
-    bilingual_str error;
     // Unable to test BerkeleyRO since we cannot create a new BDB database to open
-    dbs.emplace_back(MakeSQLiteDatabase(path_root / "sqlite", options, status, error));
+    dbs.emplace_back(std::move(MakeSQLiteDatabase(path_root / "sqlite", options).value()));
     dbs.emplace_back(CreateMockableWalletDatabase());
     return dbs;
 }
@@ -227,9 +225,7 @@ BOOST_AUTO_TEST_CASE(txn_close_failure_dangling_txn)
     // Verifies that there is no active dangling, to-be-reversed db txn
     // after the batch object that initiated it is destroyed.
     DatabaseOptions options;
-    DatabaseStatus status;
-    bilingual_str error;
-    std::unique_ptr<SQLiteDatabase> database = MakeSQLiteDatabase(m_path_root / "sqlite", options, status, error);
+    auto database = MakeSQLiteDatabase(m_path_root / "sqlite", options);
 
     std::string key = "key";
     std::string value = "value";
@@ -262,9 +258,7 @@ BOOST_AUTO_TEST_CASE(concurrent_txn_dont_interfere)
     std::string value2 = "value_2";
 
     DatabaseOptions options;
-    DatabaseStatus status;
-    bilingual_str error;
-    const auto& database = MakeSQLiteDatabase(m_path_root / "sqlite", options, status, error);
+    auto database = MakeSQLiteDatabase(m_path_root / "sqlite", options);
 
     std::unique_ptr<DatabaseBatch> handler = Assert(database)->MakeBatch();
 
