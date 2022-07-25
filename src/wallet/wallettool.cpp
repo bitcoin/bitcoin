f7aca5fd1d241e34070af235b14f266f264a26a2
Ryan Ofsky <ryan@ofsky.org>
2022-07-25 17:40:49 -0400

    refactor: Use util::Result class in wallet/wallettool

--- src/wallet/wallettool.cpp
+++ src/wallet/wallettool.cpp
@@ -47,16 +47,14 @@ static void WalletCreate(CWallet* wallet_instance, uint64_t wallet_creation_flag
 
 static std::shared_ptr<CWallet> MakeWallet(const std::string& name, const fs::path& path, DatabaseOptions options)
 {
-    DatabaseStatus status;
-    bilingual_str error;
-    std::unique_ptr<WalletDatabase> database = ResultExtract(MakeDatabase(path, options), &status, &error);
+    auto database{MakeDatabase(path, options)};
     if (!database) {
-        tfm::format(std::cerr, "%s\n", error.original);
+        tfm::format(std::cerr, "%s\n", util::ErrorString(database).original);
         return nullptr;
     }
 
     // dummy chain interface
-    std::shared_ptr<CWallet> wallet_instance{new CWallet(/*chain=*/nullptr, name, std::move(database)), WalletToolReleaseWallet};
+    std::shared_ptr<CWallet> wallet_instance{new CWallet(/*chain=*/nullptr, name, std::move(database.value())), WalletToolReleaseWallet};
     DBErrors load_wallet_ret;
     try {
         load_wallet_ret = wallet_instance->LoadWallet();
@@ -171,18 +169,16 @@ bool ExecuteWalletToolFunc(const ArgsManager& args, const std::string& command)
         wallet_instance->Close();
     } else if (command == "salvage") {
 #ifdef USE_BDB
-        bilingual_str error;
-        std::vector<bilingual_str> warnings;
-        bool ret = ResultExtract(RecoverDatabaseFile(args, path), nullptr, &error, &warnings);
+        auto ret{RecoverDatabaseFile(args, path)};
         if (!ret) {
-            for (const auto& warning : warnings) {
+            for (const auto& warning : ret.GetMessages()->warnings) {
                 tfm::format(std::cerr, "%s\n", warning.original);
             }
-            if (!error.empty()) {
+            for (const auto& error : ret.GetMessages()->errors) {
                 tfm::format(std::cerr, "%s\n", error.original);
             }
         }
-        return ret;
+        return bool{ret};
 #else
         tfm::format(std::cerr, "Salvage command is not available as BDB support is not compiled");
         return false;
@@ -191,37 +187,33 @@ bool ExecuteWalletToolFunc(const ArgsManager& args, const std::string& command)
         DatabaseOptions options;
         ReadDatabaseArgs(args, options);
         options.require_existing = true;
-        DatabaseStatus status;
-
         if (args.GetBoolArg("-withinternalbdb", false) && IsBDBFile(BDBDataFile(path))) {
             options.require_format = DatabaseFormat::BERKELEY_RO;
         }
-
-        bilingual_str error;
-        std::unique_ptr<WalletDatabase> database = ResultExtract(MakeDatabase(path, options), &status, &error);
+        auto database{MakeDatabase(path, options)};
         if (!database) {
-            tfm::format(std::cerr, "%s\n", error.original);
+            tfm::format(std::cerr, "%s\n", util::ErrorString(database).original);
             return false;
         }
 
-        bool ret = ResultExtract(DumpWallet(args, *database), nullptr, &error);
-        if (!ret && !error.empty()) {
-            tfm::format(std::cerr, "%s\n", error.original);
-            return ret;
+        auto ret{DumpWallet(args, *database)};
+        if (!ret) {
+            tfm::format(std::cerr, "%s\n", util::ErrorString(ret).original);
+            return false;
         }
         tfm::format(std::cout, "The dumpfile may contain private keys. To ensure the safety of your Bitcoin, do not share the dumpfile.\n");
-        return ret;
+        return bool{ret};
     } else if (command == "createfromdump") {
-        bilingual_str error;
-        std::vector<bilingual_str> warnings;
-        bool ret = ResultExtract(CreateFromDump(args, name, path), nullptr, &error, &warnings);
-        for (const auto& warning : warnings) {
-            tfm::format(std::cout, "%s\n", warning.original);
+        auto ret{CreateFromDump(args, name, path)};
+        if (ret.GetMessages()) {
+            for (const auto& warning : ret.GetMessages()->warnings) {
+                tfm::format(std::cout, "%s\n", warning.original);
+            }
+            for (const auto& error : ret.GetMessages()->errors) {
+                tfm::format(std::cerr, "%s\n", error.original);
+            }
         }
-        if (!ret && !error.empty()) {
-            tfm::format(std::cerr, "%s\n", error.original);
-        }
-        return ret;
+        return bool{ret};
     } else {
         tfm::format(std::cerr, "Invalid command: %s\n", command);
         return false;
