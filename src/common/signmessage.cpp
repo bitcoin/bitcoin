3d056b0314ef95e8c8a03385cb1f8a8ca49817fd
Karl-Johan Alm <karljohan-alm@garage.co.jp>
2022-07-29 14:29:05 +0900

    message: add BIP322Txs class for preparing BIP-322 transactions
    Github-Pull: #24058
    Rebased-From: 3b00ed9a52d91864665f074eadaafc179344ba56

--- src/common/signmessage.cpp
+++ src/common/signmessage.cpp
@@ -4,11 +4,13 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include <common/signmessage.h>
+#include <core_io.h>
 #include <hash.h>
 #include <key.h>
 #include <key_io.h>
 #include <outputtype.h>
 #include <pubkey.h>
+#include <streams.h>
 #include <uint256.h>
 #include <util/strencodings.h>
 
@@ -133,4 +135,66 @@ std::string SigningResultString(const SigningResult res)
         // no default case, so the compiler can warn about missing cases
     }
     assert(false);
+}
+
+std::optional<BIP322Txs> BIP322Txs::Create(const CTxDestination& destination, const std::string& message, MessageVerificationResult& result, std::optional<const std::vector<unsigned char>> signature)
+{
+    // attempt to get script pub key for destination
+    CScript message_challenge = GetScriptForDestination(destination);
+    if (message_challenge.size() == 0) {
+        // NoDestination; failure
+        // (use legacy result)
+        return std::nullopt;
+    }
+
+    // prepare message hash
+    uint256 message_hash = MessageHash(message, MessageSignatureFormat::SIMPLE);
+    std::vector<unsigned char> message_hash_vec(message_hash.begin(), message_hash.end());
+
+    // generate to_spend transaction
+    CMutableTransaction to_spend;
+    to_spend.version = 0;
+    to_spend.nLockTime = 0;
+    to_spend.vin.emplace_back(COutPoint(Txid::FromUint256(uint256::ZERO), 0xFFFFFFFF), (CScript() << OP_0 << message_hash_vec), 0);
+    to_spend.vout.emplace_back(0, message_challenge);
+
+    CMutableTransaction to_sign;
+    if (signature.has_value() && DecodeTx(to_sign, signature.value(), /* try_no_witness= */ true, /* try_witness= */ true)) {
+        // validate decoded transaction
+        // multiple inputs (proof of funds) are not supported as we do not have UTXO set access
+        if (to_sign.vin.size() > 1) {
+            result = MessageVerificationResult::ERR_POF;
+            return std::nullopt;
+        }
+        if ((to_sign.vin.size() == 0 || to_sign.vin[0].prevout.hash != to_spend.GetHash()) ||
+            (to_sign.vin[0].prevout.n != 0) ||
+            (to_sign.vout.size() != 1) ||
+            (to_sign.vout[0].nValue != 0) ||
+            (to_sign.vout[0].scriptPubKey != (CScript() << OP_RETURN))) {
+            result = MessageVerificationResult::ERR_INVALID;
+            return std::nullopt;
+        }
+    } else {
+        // signature is missing, or a witness stack only
+        to_sign.version = 0;
+        to_sign.nLockTime = 0;
+        to_sign.vin.emplace_back(COutPoint(to_spend.GetHash(), 0), CScript(), 0);
+        if (signature.has_value()) {
+            try {
+                DataStream ds(signature.value());
+                ds >> to_sign.vin[0].scriptWitness.stack;
+                if (!ds.empty()) {
+                    result = MessageVerificationResult::ERR_INVALID;
+                    return std::nullopt;
+                }
+            } catch (...) {
+                // not a script witness either; fall back to legacy error
+                // (use legacy result)
+                return std::nullopt;
+            }
+        }
+        to_sign.vout.emplace_back(0, CScript() << OP_RETURN);
+    }
+
+    return BIP322Txs{to_spend, to_sign};
 }
