b46c6ec52e1501b8c4337cead0301ef2c3777dd6
fanquake <fanquake@gmail.com>
2022-08-01 12:06:07 +0100

    bdb: disable Werror for format-security
    This is causing build failures in some build environments, like NixOS.
    I don't think we are going to patch bdb at this point, and this warning
    has existed for as long as we've used bdb.
    
    Fixes #25211.
    
    Tested (in Docker) with:
    ```bash
    docker run -it nixos/nix
    nix-shell -p gitMinimal gcc12 libtool pkg-config curl gnumake patch autoconf automake
    git clone https://github.com/bitcoin/bitcoin
    make -C bitcoin/depends bdb
    ```
    
    Co-authored-by: Ryan Ofsky <ryan@ofsky.org>

--- depends/packages/bdb.mk
+++ depends/packages/bdb.mk
@@ -14,7 +14,7 @@ $(package)_config_opts_freebsd=--with-pic
 $(package)_config_opts_netbsd=--with-pic
 $(package)_config_opts_openbsd=--with-pic
 $(package)_config_opts_android=--with-pic
-$(package)_cflags+=-Wno-error=implicit-function-declaration
+$(package)_cflags+=-Wno-error=implicit-function-declaration -Wno-error=format-security
 $(package)_cppflags_mingw32=-DUNICODE -D_UNICODE
 endef
 
