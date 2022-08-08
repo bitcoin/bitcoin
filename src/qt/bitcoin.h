fc1853605375eecf842a5c10faaed05fffe51fe3
Kittywhiskers Van Gogh <63189531+kittywhiskers@users.noreply.github.com>
2022-08-08 20:23:26 +0530

    trivial: revert dashification of header guards

--- src/qt/bitcoin.h
+++ src/qt/bitcoin.h
@@ -2,8 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_QT_DASH_H
-#define BITCOIN_QT_DASH_H
+#ifndef BITCOIN_QT_BITCOIN_H
+#define BITCOIN_QT_BITCOIN_H
 
 #if defined(HAVE_CONFIG_H)
 #include <config/bitcoin-config.h>
@@ -115,4 +115,4 @@ private:
 
 int GuiMain(int argc, char* argv[]);
 
-#endif // BITCOIN_QT_DASH_H
+#endif // BITCOIN_QT_BITCOIN_H
