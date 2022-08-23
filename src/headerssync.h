09c7d0f703067d455aae4ee458ba7953c29d72fb
Pieter Wuille <pieter@wuille.net>
2022-08-23 17:29:37 -0400

    refactor: Make ProcessNextHeaders use the headers argument as in/out
    This allows reusing the same vector storage for input and output headers to
    HeadersSync::ProcessNextHeaders. It's also natural in the sense that this argument
    just represents the headers-to-be-processed, both in the caller and the callee, and
    both before and after the calls.
    
    Co-Authored-By: Daniela Brozzoni <danielabrozzoni@protonmail.com>

--- src/headerssync.h
+++ src/headerssync.h
@@ -140,33 +140,30 @@ public:
 
     /** Result data structure for ProcessNextHeaders. */
     struct ProcessingResult {
-        std::vector<CBlockHeader> pow_validated_headers;
         bool success{false};
         bool request_more{false};
     };
 
     /** Process a batch of headers, once a sync via this mechanism has started
      *
-     * received_headers: headers that were received over the network for processing.
+     * headers: headers that were received over the network for processing.
      *                   Assumes the caller has already verified the headers
      *                   are continuous, and has checked that each header
      *                   satisfies the proof-of-work target included in the
      *                   header (but not necessarily verified that the
      *                   proof-of-work target is correct and passes consensus
      *                   rules).
+     *                   On output, this will be replaced with headers the caller
+     *                   can fully process and validate now (because these
+     *                   returned headers are on a chain with sufficient work).
      * full_headers_message: true if the message was at max capacity,
      *                       indicating more headers may be available
-     * ProcessingResult.pow_validated_headers: will be filled in with any
-     *                       headers that the caller can fully process and
-     *                       validate now (because these returned headers are
-     *                       on a chain with sufficient work)
      * ProcessingResult.success: set to false if an error is detected and the sync is
      *                       aborted; true otherwise.
      * ProcessingResult.request_more: if true, the caller is suggested to call
      *                       NextHeadersRequestLocator and send a getheaders message using it.
      */
-    ProcessingResult ProcessNextHeaders(const std::vector<CBlockHeader>&
-            received_headers, bool full_headers_message);
+    ProcessingResult ProcessNextHeaders(std::vector<CBlockHeader>& headers, bool full_headers_message);
 
     /** Issue the next GETHEADERS message to our peer.
      *
@@ -204,8 +201,8 @@ private:
      * buffer for later processing */
     bool ValidateAndStoreRedownloadedHeader(const CBlockHeader& header);
 
-    /** Return a set of headers that satisfy our proof-of-work threshold */
-    std::vector<CBlockHeader> PopHeadersReadyForAcceptance();
+    /** Populate the given vector with headers that satisfy our proof-of-work threshold */
+    void PopHeadersReadyForAcceptance(std::vector<CBlockHeader>& headers);
 
 private:
     /** NodeId of the peer (used for log messages) **/
