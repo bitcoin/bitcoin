cca4f82b828669ae23f6ac64fb83e068b81ae189
brunoerg <brunoely.gc@gmail.com>
2022-08-26 15:23:21 -0300

    test: add coverage for rpc error when trying to rescan beyond pruned data

--- test/functional/feature_pruning.py
+++ test/functional/feature_pruning.py
@@ -143,6 +143,10 @@ class PruneTest(BitcoinTestFramework):
             extra_args=['-prune=550', '-reindex-chainstate'],
         )
 
+    def test_rescan_blockchain(self):
+        self.restart_node(0, ["-prune=550"])
+        assert_raises_rpc_error(-1, "Can't rescan beyond pruned data. Use RPC call getblockchaininfo to determine your pruned height.", self.nodes[0].rescanblockchain)
+
     def test_height_min(self):
         assert os.path.isfile(os.path.join(self.prunedir, "blk00000.dat")), "blk00000.dat is missing, pruning too early"
         self.log.info("Success")
@@ -476,6 +480,9 @@ class PruneTest(BitcoinTestFramework):
         if self.is_wallet_compiled():
             self.log.info("Test wallet re-scan")
             self.wallet_test()
+
+            self.log.info("Test it's not possible to rescan beyond pruned data")
+            self.test_rescan_blockchain()
 
         self.log.info("Test invalid pruning command line options")
         self.test_invalid_command_line_options()
