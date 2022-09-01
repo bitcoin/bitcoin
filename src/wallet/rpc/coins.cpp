d09a74c7e4f5022eaa0ba8b22d40a65bbe60f217
Luke Dashjr <luke-jr+git@utopios.org>
2022-09-01 20:36:09 +0000

    RPC/Wallet: Hacky fix for getbalance bugs

--- src/wallet/rpc/coins.cpp
+++ src/wallet/rpc/coins.cpp
@@ -167,8 +167,8 @@ RPCHelpMan getbalance()
                 "The available balance is what the wallet considers currently spendable, and is\n"
                 "thus affected by options which limit spendability such as -spendzeroconfchange.\n",
                 {
-                    {"dummy", RPCArg::Type::STR, RPCArg::Optional::OMITTED, "Remains for backward compatibility. Must be excluded or set to \"*\"."},
-                    {"minconf", RPCArg::Type::NUM, RPCArg::Default{0}, "Only include transactions confirmed at least this many times."},
+                    {"dummy|account", RPCArg::Type::STR, RPCArg::Optional::OMITTED, "Remains for backward compatibility. Must be excluded or set to \"*\"."},
+                    {"minconf", RPCArg::Type::NUM, RPCArg::Default{1}, "Only include transactions confirmed at least this many times. (Requires dummy=\"*\")"},
                     {"include_watchonly", RPCArg::Type::BOOL, RPCArg::DefaultHint{"true for watch-only wallets, otherwise false"}, "Also include balance in watch-only addresses (see 'importaddress')"},
                     {"avoid_reuse", RPCArg::Type::BOOL, RPCArg::Default{true}, "(only available if avoid_reuse wallet flag is set) Do not include balance in dirty outputs; addresses are considered dirty if they have previously been used in a transaction."},
                 },
@@ -199,7 +199,7 @@ RPCHelpMan getbalance()
         throw JSONRPCError(RPC_METHOD_DEPRECATED, "dummy first argument must be excluded or set to \"*\".");
     }
 
-    const auto min_depth{self.Arg<int>("minconf")};
+    const auto min_depth{dummy_value ? self.Arg<int>("minconf") : 0};
 
     bool include_watchonly = ParseIncludeWatchonly(request.params[2], *pwallet);
 
@@ -207,6 +207,15 @@ RPCHelpMan getbalance()
 
     const auto bal = GetBalance(*pwallet, min_depth, avoid_reuse);
 
+    if (dummy_value) {
+        isminefilter filter = ISMINE_SPENDABLE;
+        if (include_watchonly) filter = filter | ISMINE_WATCH_ONLY;
+        return ValueFromAmount(pwallet->GetLegacyBalance(filter, min_depth));
+    }
+
+    if (!request.params[1].isNull()) {
+        throw JSONRPCError(RPC_INVALID_PARAMETER, "getbalance minconf option is only currently supported if dummy is set to \"*\"");
+    }
     return ValueFromAmount(bal.m_mine_trusted + (include_watchonly ? bal.m_watchonly_trusted : 0));
 },
     };
