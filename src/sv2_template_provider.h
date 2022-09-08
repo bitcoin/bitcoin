8d5e6a2fe968a4d4ec80117c984a4c9ccf21a53f
Christopher Coverdale <chris.coverdale24@gmail.com>
2022-09-08 13:24:31 +0100

    sv2: add base class Sv2Msg for all stratum v2 messages

--- src/sv2_template_provider.h
+++ src/sv2_template_provider.h
@@ -1,4 +1,26 @@
 #ifndef SV2_TEMPLATE_PROVIDER_H
 #define SV2_TEMPLATE_PROVIDER_H
 
+#include <streams.h>
+
+/**
+ * Base class for all stratum v2 messages.
+ */
+class Sv2Msg
+{
+public:
+    void ReadSTR0_255(CDataStream& stream, std::string& output)
+    {
+        uint8_t len;
+        stream >> len;
+
+        for (auto i = 0; i < len; ++i) {
+            uint8_t b;
+            stream >> b;
+
+            output.push_back(b);
+        }
+    }
+};
+
 #endif // SV2_TEMPLATE_PROVIDER_H
