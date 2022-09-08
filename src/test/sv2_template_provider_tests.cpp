f2d7eb2eb11c68d1ab74d06660bfad744ca29027
Christopher Coverdale <chris.coverdale24@gmail.com>
2022-09-08 13:38:24 +0100

    build: add sv2_template_provider_tests unit tests

---
+++ src/test/sv2_template_provider_tests.cpp
@@ -0,0 +1,8 @@
+#include <sv2_template_provider.h>
+#include <test/util/setup_common.h>
+#include <util/strencodings.h>
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_FIXTURE_TEST_SUITE(sv2_template_provider_tests, BasicTestingSetup)
+BOOST_AUTO_TEST_SUITE_END()
