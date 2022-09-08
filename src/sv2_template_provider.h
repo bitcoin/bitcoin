2d354c690c3e6b10e37b9abc0d1aed29e4690c2d
Christopher Coverdale <chris.coverdale24@gmail.com>
2022-09-08 13:45:26 +0100

    sv2: add SetupConnection message and test

--- src/sv2_template_provider.h
+++ src/sv2_template_provider.h
@@ -23,4 +23,79 @@ public:
     }
 };
 
+/**
+ * The first message sent by the client to the server to establish a connection
+ * and specifies the subprotocol (Template Provider).
+ */
+class SetupConnection : Sv2Msg
+{
+public:
+    /**
+     * Specifies the subprotocol for the new conncetion. It will always be TemplateDistribution
+     * (0x02).
+     */
+    uint8_t m_protocol;
+
+    /**
+     * The minimum protocol version the client supports (currently must be 2).
+     */
+    uint16_t m_min_version;
+
+    /**
+     * The maximum protocol version the client supports (currently must be 2).
+     */
+    uint16_t m_max_version;
+
+    /**
+     * Flags indicating optional protocol features the client supports. Each protocol 
+     * from protocol field has its own values/flags.
+     */
+    uint32_t m_flags;
+
+    /**
+     * ASCII text indicating the hostname or IP address.
+     */
+    std::string m_endpoint_host;
+
+    /**
+     * Connecting port value.
+     */
+    uint16_t m_endpoint_port;
+
+    /**
+     * Vendor name of the connecting device.
+     */
+    std::string m_vendor;
+
+    /**
+     * Hardware version of the connecting device.
+     */
+    std::string m_hardware_version;
+
+    /**
+     * Firmware of the connecting device.
+     */
+    std::string m_firmware;
+
+    /**
+     * Unique identifier of the device as defined by the vendor.
+     */
+    std::string m_device_id;
+
+    template <typename Stream>
+    void Unserialize(Stream& s) {
+        s >> m_protocol
+          >> m_min_version
+          >> m_max_version
+          >> m_flags;
+
+        ReadSTR0_255(s, m_endpoint_host);
+        s >> m_endpoint_port;
+        ReadSTR0_255(s, m_vendor);
+        ReadSTR0_255(s, m_hardware_version);
+        ReadSTR0_255(s, m_firmware);
+        ReadSTR0_255(s, m_device_id);
+    }
+};
+
 #endif // SV2_TEMPLATE_PROVIDER_H
