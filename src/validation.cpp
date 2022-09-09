c0674fadbdc1e7b5262472a10822a9023a470b49
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2022-09-08 19:50:19 -0700

    add benchmark around blob verification

--- src/validation.cpp
+++ src/validation.cpp
@@ -142,7 +142,6 @@ static constexpr int PRUNE_LOCK_BUFFER{10};
  * chainstate at the same time.
  */
 RecursiveMutex cs_main;
-
 GlobalMutex g_best_block_mutex;
 std::condition_variable g_best_block_cv;
 uint256 g_best_block;
@@ -2180,8 +2179,12 @@ bool ProcessNEVMDataHelper(const BlockManager& blockman, std::vector<CNEVMDataPr
     if(fNEVMConnection && nMedianTime > 0 && !vecNEVMDataToProcess.empty()) {
         // process new vector in batch checking the blobs
         BlockValidationState state;
+        int64_t nTimeStart = GetTimeMicros();
         // if not in DB then we need to verify it via Geth KZG blob verification
         GetMainSignals().NotifyCheckNEVMBlobs(vecNEVMDataToProcess, state);
+        int64_t nTimeDiff = GetTimeMicros() - nTimeStart;
+        LogPrint(BCLog::BENCHMARK, "ProcessNEVMDataHelper: verified %d blobs in %.2fs (%.2fms/blob)\n", nTimeDiff * MICRO, nTimeDiff * MILLI / vecNEVMDataToProcess.size());
+
         if(state.IsInvalid()) {
             LogPrint(BCLog::SYS, "ProcessNEVMDataHelper: Invalid blob %s\n", state.ToString());
             return false;
