fcea94bd3e236d0fb8b427289d46f9b02c875786
MacroFake <falke.marco@gmail.com>
2022-09-28 19:58:13 +0200

    ci: Use git2.34 for lint task

--- ci/lint/04_install.sh
+++ ci/lint/04_install.sh
@@ -8,6 +8,13 @@ export LC_ALL=C
 
 ${CI_RETRY_EXE} apt-get update
 ${CI_RETRY_EXE} apt-get install -y clang-format-9 python3-pip curl git gawk jq
+(
+  # Temporary workaround for https://github.com/bitcoin/bitcoin/pull/26130#issuecomment-1260499544
+  # Can be removed once the underlying image is bumped to something that includes git2.34 or later
+  sed -i -e 's/bionic/jammy/g' /etc/apt/sources.list
+  ${CI_RETRY_EXE} apt-get update
+  ${CI_RETRY_EXE} apt-get install -y --reinstall git
+)
 update-alternatives --install /usr/bin/clang-format      clang-format      "$(which clang-format-9     )" 100
 update-alternatives --install /usr/bin/clang-format-diff clang-format-diff "$(which clang-format-diff-9)" 100
 
