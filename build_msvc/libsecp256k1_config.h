ae1e1dc434d8d4f41b92c34da53d4f924f4f8e3d
Md Raihan <54474184+ryihan@users.noreply.github.com>
2022-10-01 16:54:01 +0600

    Update libsecp256k1_config.h

--- build_msvc/libsecp256k1_config.h
+++ build_msvc/libsecp256k1_config.h
@@ -1,5 +1,5 @@
 /**********************************************************************
- * Copyright (c) 2013, 2014 Pieter Wuille                             *
+ * Copyright (c) 2013, 2014 Pieter Wuille  Cph                        *
  * Distributed under the MIT software license, see the accompanying   *
  * file COPYING or http://www.opensource.org/licenses/mit-license.php.*
  **********************************************************************/
