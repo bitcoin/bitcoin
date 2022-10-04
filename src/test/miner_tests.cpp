5572370e0b963c46134c048e265661b9ef40c823
fanquake <fanquake@gmail.com>
2022-10-04 21:13:42 +0100

    Merge bitcoin/bitcoin#26243: test: Remove unused fCheckpointsEnabled from miner_tests
    fa9436e90859c31d9aa7bc0b7f223ab93ec14bdc test: Remove unused fCheckpointsEnabled from miner_tests (MacroFake)
    
    Pull request description:
    
      The earliest checkpoint is at height 11111, so this can't possibly have any impact on this test.
    
    ACKs for top commit:
      fanquake:
        ACK fa9436e90859c31d9aa7bc0b7f223ab93ec14bdc - given the low number of blocks, having the additional check in `ContextualCheckBlockHeader()` enabled should be a no-op, so disabling and re-enabling is dead code.
    
    Tree-SHA512: 7d1b952c297c915e9588761f82f5006cf5186b7ff30e8a1c702302e0b44afe536bde9eda8acf2995825ae01d2ad9d2393ae2feefb29f15676aaf71881941579b

--- src/test/miner_tests.cpp
+++ src/test/miner_tests.cpp
@@ -558,8 +558,6 @@ BOOST_AUTO_TEST_CASE(CreateNewBlock_validity)
     CScript scriptPubKey = CScript() << ParseHex("04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f") << OP_CHECKSIG;
     std::unique_ptr<CBlockTemplate> pblocktemplate;
 
-    fCheckpointsEnabled = false;
-
     // Simple block creation, nothing special yet:
     BOOST_CHECK(pblocktemplate = AssemblerForTest(chainparams).CreateNewBlock(scriptPubKey));
 
@@ -608,8 +606,6 @@ BOOST_AUTO_TEST_CASE(CreateNewBlock_validity)
     m_node.mempool->clear();
 
     TestPrioritisedMining(chainparams, scriptPubKey, txFirst);
-
-    fCheckpointsEnabled = true;
 }
 
 BOOST_AUTO_TEST_SUITE_END()
