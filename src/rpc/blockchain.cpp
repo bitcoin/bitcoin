a5f458e721a0514acfc455a5ebf4a2543a5c30ee
thephez <thephez@users.noreply.github.com>
2022-10-04 18:46:04 -0400

    chore(rpc): add conflicting to getchaintips help (#5032)

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -1623,7 +1623,8 @@ static UniValue getchaintips(const JSONRPCRequest& request)
     "2.  \"headers-only\"          Not all blocks for this branch are available, but the headers are valid\n"
     "3.  \"valid-headers\"         All blocks are available for this branch, but they were never fully validated\n"
     "4.  \"valid-fork\"            This branch is not part of the active chain, but is fully validated\n"
-    "5.  \"active\"                This is the tip of the active main chain, which is certainly valid"},
+    "5.  \"active\"                This is the tip of the active main chain, which is certainly valid\n",
+    "6.  \"conflicting\"           This block or one of its ancestors is conflicting with ChainLocks."},
                 }}}},
         RPCExamples{
             HelpExampleCli("getchaintips", "")
