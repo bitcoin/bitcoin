2dce1a820a0edd3ced68d45c95c5f4dd242ea752
MacroFake <falke.marco@gmail.com>
2022-10-05 11:52:42 +0200

    Merge bitcoin/bitcoin#26256: ci: Remove clang-format from lint task
    fa0437655417a9179a25e7717fdc1506f915bb08 Remove clang-format from lint task (MacroFake)
    
    Pull request description:
    
      clang-format could be used in scripted diffs, but remained largely unused.
    
      So remove the install bloat, as it is unlikely to be used in the future.
    
    ACKs for top commit:
      fanquake:
        ACK fa0437655417a9179a25e7717fdc1506f915bb08
      hebasto:
        ACK fa0437655417a9179a25e7717fdc1506f915bb08
    
    Tree-SHA512: e0a3ee47d2aa2565dd34676914c558c985eaeb522a05f10bcaac115871edcf0d7f101b517e4d452ca5223c40b18ad02883c31e2da3d1f4ff86464a9af0097b11

--- ci/lint/04_install.sh
+++ ci/lint/04_install.sh
@@ -7,7 +7,7 @@
 export LC_ALL=C
 
 ${CI_RETRY_EXE} apt-get update
-${CI_RETRY_EXE} apt-get install -y clang-format-9 python3-pip curl git gawk jq
+${CI_RETRY_EXE} apt-get install -y python3-pip curl git gawk jq
 (
   # Temporary workaround for https://github.com/bitcoin/bitcoin/pull/26130#issuecomment-1260499544
   # Can be removed once the underlying image is bumped to something that includes git2.34 or later
@@ -15,8 +15,6 @@ ${CI_RETRY_EXE} apt-get install -y clang-format-9 python3-pip curl git gawk jq
   ${CI_RETRY_EXE} apt-get update
   ${CI_RETRY_EXE} apt-get install -y --reinstall git
 )
-update-alternatives --install /usr/bin/clang-format      clang-format      "$(which clang-format-9     )" 100
-update-alternatives --install /usr/bin/clang-format-diff clang-format-diff "$(which clang-format-diff-9)" 100
 
 ${CI_RETRY_EXE} pip3 install codespell==2.2.1
 ${CI_RETRY_EXE} pip3 install flake8==4.0.1
