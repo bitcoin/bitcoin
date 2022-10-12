bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

Optimized siphash implementation
Github-Pull: #18014
Rebased-From: 81e0f144216f5021851c30df349e521b0b43935c

src/crypto/siphash.h
@@ -14,8 +14,8 @@ class CSipHasher
 {
 private:
     uint64_t v[4];
-    uint64_t tmp;
-    int count;
+    size_t count;  // total amount of bytes inputted.
+    uint64_t tail; // bytes that weren't processed yet.
 
 public:
     /** Construct a SipHash calculator initialized with 128-bit key (k0, k1) */

bitcoin/bitcoin

src/crypto/siphash.h
@@ -14,8 +14,8 @@ class CSipHasher
 {
 private:
     uint64_t v[4];
-    uint64_t tmp;
-    uint8_t count; // Only the low 8 bits of the input size matter.
+    uint64_t tail; // bytes that weren't processed yet.
+    uint8_t count;  // total amount of bytes inputted.
 
 public:
     /** Construct a SipHash calculator initialized with 128-bit key (k0, k1) */