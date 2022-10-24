b90d35b09b9056838f8576b6e4f1a3f5f81bfca3
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-10-24 07:47:38 +0600

    Update banman.cpp

--- src/banman.cpp
+++ src/banman.cpp
@@ -189,8 +189,7 @@ void BanMan::SweepBanned()
         CBanEntry ban_entry = (*it).second;
         if (!sub_net.IsValid() || now > ban_entry.nBanUntil) {
             m_banned.erase(it++);
-            m_is_dirty = true;
-            notify_ui = true;
+            m_is_dirty = notify_ui = true;
             LogPrint(BCLog::NET, "Removed banned node address/subnet: %s\n", sub_net.ToString());
         } else {
             ++it;
