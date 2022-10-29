bf243acf4d9698ef8891f4b5c0d829f0c152ab14
MacroFake <falke.marco@gmail.com>
2022-10-29 09:59:18 +0200

    Merge bitcoin/bitcoin#26408: test: Remove spam from debug log
    ef97b89902f03d710f6111a1d8deaaa87d92a817 Exclude rand from debug log (Jeff Ruane)
    
    Pull request description:
    
      Currently, `debug.log` is spammed with messages like this from `random.cpp` when functional tests are run.
    
      ```
      2022-10-25T19:24:34.787663Z [scheduler] [random.cpp:519] [SeedPeriodic] [rand] Feeding 36565 bytes of dynamic environment data into RNG
      ```
    
      These logs are not useful for debugging and decrease the signal-to-noise ratio of the logs, so they should be suppressed by excluding the `rand` category, as the `libevent` and `leveldb` categories currently are.
    
    ACKs for top commit:
      LarryRuane:
        ACK ef97b89902f03d710f6111a1d8deaaa87d92a817
      kouloumos:
         ACK ef97b89902f03d710f6111a1d8deaaa87d92a817, confirmed that this log level is only used in `random.cpp` and indeed it seems that it doesn't add any value to the debug.log during functional tests.
      satsie:
        ACK ef97b89902f03d710f6111a1d8deaaa87d92a817
      theStack:
        ACK ef97b89902f03d710f6111a1d8deaaa87d92a817
    
    Tree-SHA512: 5cea384a3197f0ec77efa9efc77822914450ecf5546606568bbd432c3536040c772c57aef58d3bb083a2e5e756f690766fa1fb382ab1973748db238108a58746

--- test/functional/test_framework/test_node.py
+++ test/functional/test_framework/test_node.py
@@ -102,6 +102,7 @@ class TestNode():
             "-debug",
             "-debugexclude=libevent",
             "-debugexclude=leveldb",
+            "-debugexclude=rand",
             "-uacomment=testnode%d" % i,
         ]
         if use_valgrind:
