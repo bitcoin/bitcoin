71dc7f44f4920598e0a22dbf806ae57c7f841ff3
James O'Beirne <james.obeirne@pm.me>
2022-11-10 16:10:35 -0500

    net_processing: validationinterface: ignore some events for bg chain

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -1876,8 +1876,14 @@ void PeerManagerImpl::BlockConnected(
     const std::shared_ptr<const CBlock>& pblock,
     const CBlockIndex* pindex)
 {
-    m_orphanage.EraseForBlock(*pblock);
+    // Update this for all chainstate roles so that we don't mistakenly see peers
+    // helping us do background IBD as having a stale tip.
     m_last_tip_update = GetTime<std::chrono::seconds>();
+
+    if (role == ChainstateRole::BACKGROUND) {
+        return;
+    }
+    m_orphanage.EraseForBlock(*pblock);
 
     {
         LOCK(m_recent_confirmed_transactions_mutex);
@@ -1979,6 +1985,9 @@ void PeerManagerImpl::NewPoWValidBlock(
  */
 void PeerManagerImpl::UpdatedBlockTip(const ChainstateRole role, const CBlockIndex *pindexNew, const CBlockIndex *pindexFork, bool fInitialDownload)
 {
+    if (role == ChainstateRole::BACKGROUND) {
+        return;
+    }
     SetBestHeight(pindexNew->nHeight);
     SetServiceFlagsIBDCache(!fInitialDownload);
 
