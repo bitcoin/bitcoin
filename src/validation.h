1d87137227ee125018006935d55164a72265c935
dergoegge <n.goeggi@gmail.com>
2022-11-14 11:01:48 +0000

    [validation] Annotate ChainstateManager::m_best_header as guarded by cs_main

--- src/validation.h
+++ src/validation.h
@@ -997,7 +997,7 @@ public:
     std::set<CBlockIndex*> m_failed_blocks;
 
     /** Best header we've seen so far (used for getheaders queries' starting points). */
-    CBlockIndex* m_best_header = nullptr;
+    CBlockIndex* m_best_header GUARDED_BY(::cs_main){nullptr};
 
     //! The total number of bytes available for us to use across all in-memory
     //! coins caches. This will be split somehow across chainstates.
