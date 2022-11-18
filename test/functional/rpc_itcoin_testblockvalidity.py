b1dda1f268843e28803deebd35bf27d1e583a6e4
muxator <antonio.muci@bancaditalia.it>
2022-11-18 02:09:12 +0100

    test: fix rpc_itcoin_testblockvalidity.py description

--- test/functional/rpc_itcoin_testblockvalidity.py
+++ test/functional/rpc_itcoin_testblockvalidity.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2020-2021 The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
-'''Test generateblock rpc.'''
+'''Test testblockvalidity rpc.'''
 import os
 import subprocess
 
