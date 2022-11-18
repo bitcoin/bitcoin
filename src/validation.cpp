88db13160a7c6d05bb8a9316a395ac170073a951
MacroFake <falke.marco@gmail.com>
2022-11-18 09:54:21 +0100

    Merge bitcoin/bitcoin#26487: log: improve some validation log messages to include hashPrevBlock
    ac410e6fc0fceb568d4fb3d398724ca859a8be25 log: improve some validation log messages to include hashPrevBlock (Skuli Dulfari)
    
    Pull request description:
    
      When there is an issue with a previous block the current log messages do not indicate hashPrevBlock. Adding it makes debugging easier.
    
    ACKs for top commit:
      stickies-v:
        ACK ac410e6fc0fceb568d4fb3d398724ca859a8be25
      aureleoules:
        reACK ac410e6fc0fceb568d4fb3d398724ca859a8be25
      theStack:
        ACK ac410e6fc0fceb568d4fb3d398724ca859a8be25
    
    Tree-SHA512: d91481321f4474bb4fdf6ad55d1c897437b631b0a12308815c4ac5b053c8a76726e2d93f2aa0701e8cfd48fba7fad19ef5ffca3c67d3aa973dc593df806f1757

--- src/validation.cpp
+++ src/validation.cpp
@@ -4063,14 +4063,14 @@ bool ChainstateManager::AcceptBlockHeader(const CBlockHeader& block, BlockValida
         CBlockIndex* pindexPrev = nullptr;
         BlockMap::iterator mi{m_blockman.m_block_index.find(block.hashPrevBlock)};
         if (mi == m_blockman.m_block_index.end()) {
-            LogPrint(BCLog::VALIDATION, "%s: %s prev block not found\n", __func__, hash.ToString());
+            LogPrint(BCLog::VALIDATION, "header %s has prev block not found: %s\n", hash.ToString(), block.hashPrevBlock.ToString());
             return state.Invalid(BlockValidationResult::BLOCK_MISSING_PREV, "prev-blk-not-found");
         }
         pindexPrev = &((*mi).second);
         assert(pindexPrev);
 
         if (pindexPrev->nStatus & BLOCK_FAILED_MASK) {
-            LogPrint(BCLog::VALIDATION, "%s: %s prev block invalid\n", __func__, hash.ToString());
+            LogPrint(BCLog::VALIDATION, "header %s has prev block invalid: %s\n", hash.ToString(), block.hashPrevBlock.ToString());
             return state.Invalid(BlockValidationResult::BLOCK_INVALID_PREV, "bad-prevblk");
         }
 
@@ -4118,7 +4118,7 @@ bool ChainstateManager::AcceptBlockHeader(const CBlockHeader& block, BlockValida
                         m_blockman.m_dirty_blockindex.insert(invalid_walk);
                         invalid_walk = invalid_walk->pprev;
                     }
-                    LogPrint(BCLog::VALIDATION, "%s: %s prev block invalid\n", __func__, hash.ToString());
+                    LogPrint(BCLog::VALIDATION, "header %s has prev block invalid: %s\n", hash.ToString(), block.hashPrevBlock.ToString());
                     return state.Invalid(BlockValidationResult::BLOCK_INVALID_PREV, "bad-prevblk");
                 }
             }
