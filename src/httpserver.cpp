50439383a24fb7096e100ee6383da76db99426d5
JoaoAJMatos <joaoafonsoj2004@gmail.com>
2022-11-24 21:13:04 +0000

    Deleted unreachable code in httpserver.cpp

--- src/httpserver.cpp
+++ src/httpserver.cpp
@@ -192,16 +192,12 @@ std::string RequestMethodString(HTTPRequest::RequestMethod m)
     switch (m) {
     case HTTPRequest::GET:
         return "GET";
-        break;
     case HTTPRequest::POST:
         return "POST";
-        break;
     case HTTPRequest::HEAD:
         return "HEAD";
-        break;
     case HTTPRequest::PUT:
         return "PUT";
-        break;
     default:
         return "unknown";
     }
