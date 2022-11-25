8357d1e281f4f0946a52e7c9de12ca3fc47c873b
robigan <35210888+robigan@users.noreply.github.com>
2022-11-25 10:46:34 +0100

    Use pyenv and switch to python 3.6.5
    - Install build environment dependencies for pyenv (https://github.com/pyenv/pyenv#install-python-build-dependencies)
    - Curl https://pyenv.run installer and pipe it to bash
    - Add pyenv's bin directory to path
    - Run pyenv's init script (https://github.com/pyenv/pyenv#set-up-your-shell-environment-for-pyenv)s
    - Use pyenv's CLI to install python 3.6.15
    - Use python cache in Cirrus
    - Use intermediary directory for interacting with Cirrus's caching functions
    - Assign more resources to the cirrus task to compile python 3.6.15

--- ci/lint/04_install.sh
+++ ci/lint/04_install.sh
@@ -8,6 +8,19 @@ export LC_ALL=C
 
 ${CI_RETRY_EXE} apt-get update
 ${CI_RETRY_EXE} apt-get install -y curl git gawk jq
+${CI_RETRY_EXE} apt-get install -y make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+${CI_RETRY_EXE} curl https://pyenv.run | bash
+eval "$(pyenv init -)"
+
+[[ -d /tmp/python-build ]] && ${CI_RETRY_EXE} mv /tmp/python-build $PYENV_ROOT/versions/$(cat .python-version) # Pyenv doesn't like .pyenv being already present, so an intermediary directory is used to interface with Cirrus's CI Caching features. Uses atomic swap (mv) to speed up CI instead of cp.
+${CI_RETRY_EXE} pyenv install # Uses python version found in CWD's .python-version file, or searches parent directories until root for it
+${CI_RETRY_EXE} pyenv local
+[[ ! -d /tmp/python-build ]] && ${CI_RETRY_EXE} mkdir -p /tmp/python-build && cp -r $PYENV_ROOT/versions/$(cat .python-version)/* /tmp/python-build # Copy built python to intermediary directory. Do not use atomic swap as python needs it.
 
 ${CI_RETRY_EXE} pip3 install codespell==2.2.1
 ${CI_RETRY_EXE} pip3 install flake8==4.0.1
