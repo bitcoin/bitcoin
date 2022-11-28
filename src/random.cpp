cf96097de3a865f78ecca1906124b1e19e9ca9d4
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-11-28 14:25:26 +0000

    Add MSVC implementation of GetCPUID()

--- src/random.cpp
+++ src/random.cpp
@@ -68,7 +68,7 @@ static inline int64_t GetPerformanceCounter() noexcept
 #endif
 }
 
-#ifdef HAVE_GETCPUID
+#if defined(HAVE_GETCPUID) && !defined(_MSC_VER)
 static bool g_rdrand_supported = false;
 static bool g_rdseed_supported = false;
 static constexpr uint32_t CPUID_F1_ECX_RDRAND = 0x40000000;
