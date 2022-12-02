5d332da2cfdc67d69a165119c6ff598e29b28afa
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-12-02 15:44:34 +0000

    doc: Drop no longer relevant comment
    The comment was introduced in 4cf3411056f6a59fc5fe07784b6b6a512d76b046,
    and since 7e4bd19785ff9120b7242ff9f15231868aaf7db4 it has been no longer
    relevant.

--- src/qt/test/test_main.cpp
+++ src/qt/test/test_main.cpp
@@ -79,8 +79,6 @@ int main(int argc, char* argv[])
         setenv("QT_QPA_PLATFORM", "minimal", 0 /* overwrite */);
     #endif
 
-    // Don't remove this, it's needed to access
-    // QApplication:: and QCoreApplication:: in the tests
     BitcoinApplication app;
     app.setApplicationName("Bitcoin-Qt-test");
     app.createNode(*init);
