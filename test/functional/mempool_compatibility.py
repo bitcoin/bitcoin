2c1e298ccfc68deade4afc156bad3726ee19049e
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2022-12-05 12:40:47 +0100

    merge bitcoin#26640: Run mempool_compatibility.py with MiniWallet

--- test/functional/mempool_compatibility.py
+++ test/functional/mempool_compatibility.py
@@ -20,7 +20,6 @@ from test_framework.wallet import MiniWallet
 class MempoolCompatibilityTest(BitcoinTestFramework):
     def set_test_params(self):
         self.num_nodes = 2
-        self.wallet_names = [None]
 
     def skip_test_if_missing_module(self):
         self.skip_if_no_previous_releases()
@@ -38,7 +37,6 @@ class MempoolCompatibilityTest(BitcoinTestFramework):
         # make sense of our current database formats
         shutil.rmtree(os.path.join(self.nodes[0].datadir, self.chain))
         self.start_nodes()
-        self.import_deterministic_coinbase_privkeys()
 
     def run_test(self):
         self.log.info("Test that mempool.dat is compatible between versions")
@@ -52,11 +50,10 @@ class MempoolCompatibilityTest(BitcoinTestFramework):
         # unbroadcasted_tx won't pass old_node's `MemPoolAccept::PreChecks`.
         self.connect_nodes(0, 1)
         self.sync_blocks()
-        recipient = old_node.getnewaddress()
         self.stop_node(1)
 
         self.log.info("Add a transaction to mempool on old node and shutdown")
-        old_tx_hash = old_node.sendtoaddress(recipient, 0.0001)
+        old_tx_hash = new_wallet.send_self_transfer(from_node=old_node)["txid"]
         assert old_tx_hash in old_node.getrawmempool()
         self.stop_node(0)
 
