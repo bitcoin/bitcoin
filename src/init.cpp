7ffb8caaedd95f9615d3132ceffc96d96128f698
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2022-12-08 12:37:46 -0800

    Update init.cpp

--- src/init.cpp
+++ src/init.cpp
@@ -1750,7 +1750,7 @@ bool AppInitMain(NodeContext& node, interfaces::BlockAndHeaderTipInfo* tip_info)
             try {
                 return f();
             } catch (const std::exception& e) {
-                LogPrintf("%s\n", e.what());
+                printf("%s\n", e.what());
                 return std::make_tuple(node::ChainstateLoadStatus::FAILURE, _("Error opening block database"));
             }
         };
