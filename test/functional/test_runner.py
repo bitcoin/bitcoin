bc9283c4415a932ec1eeb70ca2aa4399c80437b3
stratospher <44024636+stratospher@users.noreply.github.com>
2022-12-10 10:38:31 +0530

    [test] Add functional test to test early key response behaviour in BIP 324
    - A node initiates a v2 connection by sending 64 bytes ellswift
    - In BIP 324 "The responder waits until one byte is received which does not match the
      V1_PREFIX (16 bytes consisting of the network magic followed by "version\x00\x00\x00\x00\x00".)"
    - It's possible that the 64 bytes ellswift sent by an initiator starts with a prefix of V1_PREFIX
    - Example form of 64 bytes ellswift could be:
    	4 bytes network magic + 60 bytes which aren't prefixed with remaining V1_PREFIX
    - We test this behaviour:
    	- when responder receives 4 byte network magic -> no response received by initiator
    	- when first mismatch happens -> response received by initiator

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -260,6 +260,7 @@ BASE_SCRIPTS = [
     'p2p_invalid_tx.py --v2transport',
     'p2p_v2_transport.py',
     'p2p_v2_encrypted.py',
+    'p2p_v2_earlykeyresponse.py',
     'example_test.py',
     'wallet_txn_doublespend.py --legacy-wallet',
     'wallet_multisig_descriptor_psbt.py --descriptors',
