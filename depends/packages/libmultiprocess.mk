ad0c279ac805f310fb7fe1814945eecae228f607
fanquake <fanquake@gmail.com>
2022-12-10 10:25:21 +0000

    Merge bitcoin/bitcoin#26672: build: Update libmultiprocess library
    1986f129c6af7644d238c76f032713731d7afcac build: Update `libmultiprocess` library (Hennadii Stepanov)
    
    Pull request description:
    
      This update in particular includes:
      - https://github.com/chaincodelabs/libmultiprocess/pull/78 which is [required](https://github.com/bitcoin/bitcoin/pull/25972#issuecomment-1246812573) for https://github.com/bitcoin/bitcoin/pull/25972
      - https://github.com/chaincodelabs/libmultiprocess/pull/74
      - https://github.com/chaincodelabs/libmultiprocess/pull/70
    
    ACKs for top commit:
      ryanofsky:
        Code review ACK 1986f129c6af7644d238c76f032713731d7afcac
    
    Tree-SHA512: 2d9fa72df5de7d5be37d77d479702cba36c45e9fa9d9fc27e58aac0437c39e4a1054c0ac53b612cb43e830982e444d98c7d3e651d093ac68344e66f4734227bb

--- depends/packages/libmultiprocess.mk
+++ depends/packages/libmultiprocess.mk
@@ -24,5 +24,5 @@ define $(package)_build_cmds
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install-lib
 endef
--- depends/packages/native_libmultiprocess.mk
+++ depends/packages/native_libmultiprocess.mk
@@ -1,8 +1,8 @@
 package=native_libmultiprocess
-$(package)_version=d576d975debdc9090bd2582f83f49c76c0061698
+$(package)_version=1af83d15239ccfa7e47b8764029320953dd7fdf1
 $(package)_download_path=https://github.com/chaincodelabs/libmultiprocess/archive
 $(package)_file_name=$($(package)_version).tar.gz
-$(package)_sha256_hash=9f8b055c8bba755dc32fe799b67c20b91e7b13e67cadafbc54c0f1def057a370
+$(package)_sha256_hash=e5587d3feedc7f8473f178a89b94163a11076629825d664964799bbbd5844da5
 $(package)_dependencies=native_capnp
 
 define $(package)_config_cmds
@@ -14,5 +14,5 @@ define $(package)_build_cmds
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install-bin
 endef
