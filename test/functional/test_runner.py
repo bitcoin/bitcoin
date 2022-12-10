ba737358a37438c18f0fba723eab10ccfd9aae9b
stratospher <44024636+stratospher@users.noreply.github.com>
2022-12-10 10:37:51 +0530

    [test] Add functional tests to test v2 P2P behaviour

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -259,6 +259,7 @@ BASE_SCRIPTS = [
     'p2p_invalid_tx.py',
     'p2p_invalid_tx.py --v2transport',
     'p2p_v2_transport.py',
+    'p2p_v2_encrypted.py',
     'example_test.py',
     'wallet_txn_doublespend.py --legacy-wallet',
     'wallet_multisig_descriptor_psbt.py --descriptors',
