c39619eeb4e615eb991143edb99e031dd562d0f6
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-12-15 21:24:14 +0000

    clang-tidy: Fix `readability-redundant-string-init` in headers
    See https://clang.llvm.org/extra/clang-tidy/checks/readability/redundant-string-init.html

--- src/qt/transactionrecord.h
+++ src/qt/transactionrecord.h
@@ -76,12 +76,12 @@ public:
     static const int RecommendedNumConfirmations = 6;
 
     TransactionRecord():
-            hash(), time(0), type(Other), address(""), debit(0), credit(0), idx(0)
+            hash(), time(0), type(Other), debit(0), credit(0), idx(0)
     {
     }
 
     TransactionRecord(uint256 _hash, qint64 _time):
-            hash(_hash), time(_time), type(Other), address(""), debit(0),
+            hash(_hash), time(_time), type(Other), debit(0),
             credit(0), idx(0)
     {
     }
