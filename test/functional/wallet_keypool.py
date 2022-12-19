5d2a74dddb5388b23f050fe2143ce250e3baec27
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2022-12-19 15:17:41 +0100

    Merge #26723: test: call keypoolrefill with priv key disabled shoul…

--- test/functional/wallet_keypool.py
+++ test/functional/wallet_keypool.py
@@ -209,6 +209,10 @@ class KeyPoolTest(SyscoinTestFramework):
         res = w2.walletcreatefundedpsbt(inputs=[], outputs=[{destination: 0.00010000}], options={"subtractFeeFromOutputs": [0], "feeRate": 0.00010, "changeAddress": addr.pop()})
         assert_equal("psbt" in res, True)
 
+        if not self.options.descriptors:
+            msg = "Error: Private keys are disabled for this wallet"
+            assert_raises_rpc_error(-4, msg, w2.keypoolrefill, 100)
+
 def test_auxpow(self, nodes):
     """
     Test behaviour of getauxpow.  Calling getauxpow should reserve
