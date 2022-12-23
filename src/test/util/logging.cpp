e4a5cee48dc25e6f28c0e26c37f4fc847a596c60
Vasil Dimov <vd@FreeBSD.org>
2022-12-23 10:03:26 +0100

    test: don't throw from the destructor of DebugLogHelper
    Throwing an exception from the destructor of a class is a bad practice
    because the destructor will be called when an object of that type is
    alive on the stack and another exception is thrown, which will result
    in "exception during the exception". This would terminate the program
    without any messages.
    
    Instead print the message to the standard error output and call
    `std::abort()`.

--- src/test/util/logging.cpp
+++ src/test/util/logging.cpp
@@ -7,7 +7,9 @@
 #include <logging.h>
 #include <noui.h>
 #include <tinyformat.h>
+#include <util/check.h>
 
+#include <iostream>
 #include <stdexcept>
 
 DebugLogHelper::DebugLogHelper(std::string message, MatchFn match)
@@ -19,13 +21,23 @@ DebugLogHelper::DebugLogHelper(std::string message, MatchFn match)
             m_found = s.find(m_message) != std::string::npos && m_match(&s);
         });
     noui_test_redirect();
+    m_receiving_log = true;
 }
 
-void DebugLogHelper::check_found()
+DebugLogHelper::~DebugLogHelper()
 {
-    noui_reconnect();
-    LogInstance().DeleteCallback(m_print_connection);
+    StopReceivingLog();
     if (!m_found && m_match(nullptr)) {
-        throw std::runtime_error(strprintf("'%s' not found in debug log\n", m_message));
+        std::cerr << "Fatal error: expected message not found in the debug log: " << m_message << "\n";
+        std::abort();
+    }
+}
+
+void DebugLogHelper::StopReceivingLog()
+{
+    if (m_receiving_log) {
+        noui_reconnect();
+        LogInstance().DeleteCallback(m_print_connection);
+        m_receiving_log = false;
     }
 }
