306ccd4927a2efe325c8d84be1bdb79edeb29b04
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-12-24 23:49:50 +0000

    scripted-diff: Bump copyright headers
    -BEGIN VERIFY SCRIPT-
    ./contrib/devtools/copyright_header.py update ./
    -END VERIFY SCRIPT-
    
    Commits of previous years:
    - 2021: f47dda2c58b5d8d623e0e7ff4e74bc352dfa83d7
    - 2020: fa0074e2d82928016a43ca408717154a1c70a4db
    - 2019: aaaaad6ac95b402fe18d019d67897ced6b316ee0

--- test/functional/feature_syscall_sandbox.py
+++ test/functional/feature_syscall_sandbox.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (c) 2021 The Bitcoin Core developers
+# Copyright (c) 2021-2022 The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 """Test bitcoind aborts if a disallowed syscall is used when compiled with the syscall sandbox."""
