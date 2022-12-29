9b2513857667ea1063300d9a710271fa8620943d
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2022-12-29 20:49:18 +0100

    Merge #26770: ci: Move --enable-c++20 from "tidy" task back to "ASa…

--- ci/test/00_setup_env_native_asan.sh
+++ ci/test/00_setup_env_native_asan.sh
@@ -24,4 +24,4 @@ export DOCKER_NAME_TAG=ubuntu:22.04
 export NO_DEPENDS=1
 export GOAL="install"
 export TEST_RUNNER_EXTRA="--exclude interface_zmq_nevm"
-export SYSCOIN_CONFIG="--enable-usdt --enable-zmq --with-incompatible-bdb --with-gui=qt5 CPPFLAGS='-DARENA_DEBUG -DDEBUG_LOCKORDER' --with-sanitizers=address,integer,undefined CC=clang CXX=clang++"
+export SYSCOIN_CONFIG="--enable-c++20 --enable-usdt --enable-zmq --with-incompatible-bdb --with-gui=qt5 CPPFLAGS='-DARENA_DEBUG -DDEBUG_LOCKORDER' --with-sanitizers=address,integer,undefined CC=clang CXX=clang++"
