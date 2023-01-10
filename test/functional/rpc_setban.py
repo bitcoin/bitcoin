54d0e3219336864499e58b6b0265bc94c850eadd
brunoerg <brunoely.gc@gmail.com>
2023-01-10 09:53:57 -0300

    test: merge `setban` test from `p2p_disconnect_ban` to `rpc_setban`
    Sometimes both ones are testing same stuff related to banning, it makes sense to merge them to avoid repetition.

--- test/functional/rpc_setban.py
+++ test/functional/rpc_setban.py
@@ -3,12 +3,15 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 """Test the setban rpc call."""
+import time
+from pathlib import Path
 
 from contextlib import ExitStack
 from test_framework.test_framework import BitcoinTestFramework
 from test_framework.util import (
-    p2p_port,
     assert_equal,
+    assert_raises_rpc_error,
+    p2p_port
 )
 
 class SetBanTests(BitcoinTestFramework):
@@ -17,11 +20,12 @@ class SetBanTests(BitcoinTestFramework):
         self.setup_clean_chain = True
         self.extra_args = [[],[]]
 
-    def is_banned(self, node, addr):
-        return any(e['address'] == addr for e in node.listbanned())
-
     def run_test(self):
-        # Node 0 connects to Node 1, check that the noban permission is not granted
+        self.log.info("Connect nodes both way")
+        # By default, the test framework sets up an addnode connection from
+        # node 1 --> node0. By connecting node0 --> node 1, we're left with
+        # the two nodes being connected both ways.
+        # Topology will look like: node0 <--> node1
         self.connect_nodes(0, 1)
         peerinfo = self.nodes[1].getpeerinfo()[0]
         assert not "noban" in peerinfo["permissions"]
@@ -40,47 +44,95 @@ class SetBanTests(BitcoinTestFramework):
             self.restart_node(1, [])
             self.nodes[0].addnode("127.0.0.1:" + str(p2p_port(1)), "onetry")
 
-        # However, node 0 should be able to reconnect if it has noban permission
-        self.restart_node(1, ['-whitelist=127.0.0.1'])
-        self.connect_nodes(0, 1)
-        peerinfo = self.nodes[1].getpeerinfo()[0]
-        assert "noban" in peerinfo["permissions"]
+        self.log.info("clearbanned: successfully clear ban list")
+        self.nodes[1].clearbanned()
+        assert_equal(len(self.nodes[1].listbanned()), 0)
 
-        # If we remove the ban, Node 0 should be able to reconnect even without noban permission
-        self.nodes[1].setban("127.0.0.1", "remove")
-        self.restart_node(1, [])
-        self.connect_nodes(0, 1)
-        peerinfo = self.nodes[1].getpeerinfo()[0]
-        assert not "noban" in peerinfo["permissions"]
+        self.log.info('Test banlist database recreation')
+        self.stop_node(1)
+        target_file = self.nodes[1].chain_path / "banlist.json"
+        Path.unlink(target_file)
+        with self.nodes[1].assert_debug_log(["Recreating the banlist database"]):
+            self.start_node(1)
+
+        assert Path.exists(target_file)
+        assert_equal(self.nodes[1].listbanned(), [])
+
+        self.nodes[1].setban("127.0.0.0/24", "add")
+
+        self.log.info("setban: fail to ban an already banned subnet")
+        assert_equal(len(self.nodes[1].listbanned()), 1)
+        assert_raises_rpc_error(-23, "IP/Subnet already banned", self.nodes[1].setban, "127.0.0.1", "add")
+
+        self.log.info("setban: fail to ban an invalid subnet")
+        assert_raises_rpc_error(-30, "Error: Invalid IP/Subnet", self.nodes[1].setban, "127.0.0.1/42", "add")
+        assert_equal(len(self.nodes[1].listbanned()), 1)  # still only one banned ip because 127.0.0.1 is within the range of 127.0.0.0/24
+
+        self.log.info("setban: fail to ban with past absolute timestamp")
+        assert_raises_rpc_error(-8, "Error: Absolute timestamp is in the past", self.nodes[1].setban, "127.27.0.1", "add", 123, True)
+
+        self.log.info("setban remove: fail to unban a non-banned subnet")
+        assert_raises_rpc_error(-30, "Error: Unban failed", self.nodes[1].setban, "127.0.0.1", "remove")
+        assert_equal(len(self.nodes[1].listbanned()), 1)
+
+        self.log.info("setban remove: successfully unban subnet")
+        self.nodes[1].setban("127.0.0.0/24", "remove")
+        assert_equal(len(self.nodes[1].listbanned()), 0)
+        self.nodes[1].clearbanned()
+        assert_equal(len(self.nodes[1].listbanned()), 0)
+
+        self.log.info("setban: test persistence across node restart")
+        # Set the mocktime so we can control when bans expire
+        old_time = int(time.time())
+        self.nodes[1].setmocktime(old_time)
+        self.nodes[1].setban("127.0.0.0/32", "add")
+        self.nodes[1].setban("127.0.0.0/24", "add")
+        self.nodes[1].setban("192.168.0.1", "add", 1)  # ban for 1 seconds
+        self.nodes[1].setban("2001:4d48:ac57:400:cacf:e9ff:fe1d:9c63/19", "add", 1000)  # ban for 1000 seconds
+        assert any(e['address'] == "192.168.0.1/32" for e in self.nodes[1].listbanned())
+
+        self.log.info("setban: test banning with absolute timestamp")
+        self.nodes[1].setban("192.168.0.2", "add", old_time + 120, True)
+
+        # Move time forward by 3 seconds so the third ban has expired
+        self.nodes[1].setmocktime(old_time + 3)
+        assert_equal(len(self.nodes[1].listbanned()), 4)
+
+        self.log.info("Test ban_duration and time_remaining")
+        for ban in self.nodes[1].listbanned():
+            if ban["address"] in ["127.0.0.0/32", "127.0.0.0/24"]:
+                assert_equal(ban["ban_duration"], 86400)
+                assert_equal(ban["time_remaining"], 86397)
+            elif ban["address"] == "2001:4d48:ac57:400:cacf:e9ff:fe1d:9c63/19":
+                assert_equal(ban["ban_duration"], 1000)
+                assert_equal(ban["time_remaining"], 997)
+            elif ban["address"] == "192.168.0.2/32":
+                assert_equal(ban["ban_duration"], 120)
+                assert_equal(ban["time_remaining"], 117)
+
+        self.restart_node(1)
+
+        listAfterShutdown = self.nodes[1].listbanned()
+        assert_equal("127.0.0.0/24", listAfterShutdown[0]['address'])
+        assert_equal("127.0.0.0/32", listAfterShutdown[1]['address'])
+        assert_equal("192.168.0.2/32", listAfterShutdown[2]['address'])
+        assert_equal("/19" in listAfterShutdown[3]['address'], True)
 
         self.log.info("Test that a non-IP address can be banned/unbanned")
-        node = self.nodes[1]
         tor_addr = "pg6mmjiyjmcrsslvykfwnntlaru7p5svn6y2ymmju6nubxndf4pscryd.onion"
         ip_addr = "1.2.3.4"
-        assert not self.is_banned(node, tor_addr)
-        assert not self.is_banned(node, ip_addr)
+        list_banned = [addr["address"] for addr in self.nodes[1].listbanned()]
+        assert not tor_addr in list_banned
+        assert not ip_addr in list_banned
 
-        node.setban(tor_addr, "add")
-        assert self.is_banned(node, tor_addr)
-        assert not self.is_banned(node, ip_addr)
-
-        self.log.info("Test the ban list is preserved through restart")
-
-        self.restart_node(1)
-        assert self.is_banned(node, tor_addr)
-        assert not self.is_banned(node, ip_addr)
-
-        node.setban(tor_addr, "remove")
-        assert not self.is_banned(self.nodes[1], tor_addr)
-        assert not self.is_banned(node, ip_addr)
-
-        self.restart_node(1)
-        assert not self.is_banned(node, tor_addr)
-        assert not self.is_banned(node, ip_addr)
+        self.nodes[1].setban(tor_addr, "add")
+        list_banned = [addr["address"] for addr in self.nodes[1].listbanned()]
+        assert tor_addr in list_banned
+        assert not ip_addr in list_banned
 
         self.log.info("Test -bantime")
         self.restart_node(1, ["-bantime=1234"])
-        self.nodes[1].setban("127.0.0.1", "add")
+        self.nodes[1].setban("1.2.3.5", "add")
         banned = self.nodes[1].listbanned()[0]
         assert_equal(banned['ban_duration'], 1234)
 
