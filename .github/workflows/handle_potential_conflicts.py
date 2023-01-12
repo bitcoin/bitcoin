6293e722732050ec9f075dd45a8d8f2813e51ab2
UdjinM6 <UdjinM6@users.noreply.github.com>
2023-01-13 00:51:59 +0300

    Merge pull request #5160 from UdjinM6/bump_year_2023
    chore: bump copyright year to 2023, add missing copyrights

--- .github/workflows/handle_potential_conflicts.py
+++ .github/workflows/handle_potential_conflicts.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2022 The Dash Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
 import sys
 import requests
 
