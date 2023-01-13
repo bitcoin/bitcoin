25a0e8ba0b31d8bd265df0589fe49241a60d0fc2
fanquake <fanquake@gmail.com>
2023-01-13 15:29:52 +0000

    Remove configure-time setting of DEFAULT_UPNP
    Default to false.

--- src/mapport.h
+++ src/mapport.h
@@ -5,11 +5,7 @@
 #ifndef BITCOIN_MAPPORT_H
 #define BITCOIN_MAPPORT_H
 
-#ifdef USE_UPNP
-static constexpr bool DEFAULT_UPNP = USE_UPNP;
-#else
 static constexpr bool DEFAULT_UPNP = false;
-#endif // USE_UPNP
 
 static constexpr bool DEFAULT_NATPMP = false;
 
