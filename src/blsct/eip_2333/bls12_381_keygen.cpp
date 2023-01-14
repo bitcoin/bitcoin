27aae9d63fd29c9916d4306a4cbffd4d245d38a4
gogoex <110195520+gogoex@users.noreply.github.com>
2023-01-14 10:33:30 +0900

    replace K with DigestSize. clean up code

--- src/blsct/eip_2333/bls12_381_keygen.cpp
+++ src/blsct/eip_2333/bls12_381_keygen.cpp
@@ -6,7 +6,7 @@
 #include <crypto/sha256.h>
 #include <tinyformat.h>
 
-std::array<uint8_t,CHMAC_SHA256::OUTPUT_SIZE> BLS12_381_KeyGen::HKDF_Extract(const std::vector<uint8_t>& salt, const std::vector<uint8_t>& IKM)
+std::array<uint8_t,BLS12_381_KeyGen::DigestSize> BLS12_381_KeyGen::HKDF_Extract(const std::vector<uint8_t>& salt, const std::vector<uint8_t>& IKM)
 {
     auto hmac_sha256 = CHMAC_SHA256(&salt[0], salt.size());
     hmac_sha256.Write(&IKM[0], IKM.size());
@@ -18,13 +18,13 @@ std::array<uint8_t,CHMAC_SHA256::OUTPUT_SIZE> BLS12_381_KeyGen::HKDF_Extract(con
 }
 
 template <size_t L>
-std::array<uint8_t,L> BLS12_381_KeyGen::HKDF_Expand(const std::array<uint8_t,BLS12_381_KeyGen::K>& PRK, const std::vector<uint8_t>& info)
+std::array<uint8_t,L> BLS12_381_KeyGen::HKDF_Expand(const std::array<uint8_t,BLS12_381_KeyGen::DigestSize>& PRK, const std::vector<uint8_t>& info)
 {
-    std::array<uint8_t,K> prev;
+    std::array<uint8_t,DigestSize> prev;
     std::array<uint8_t,L> output;
     uint8_t n;
 
-    const size_t num_loops = std::ceil(static_cast<float>(L) / BLS12_381_KeyGen::K);
+    const size_t num_loops = std::ceil(static_cast<float>(L) / DigestSize);
     auto output_it = output.begin();
 
     for (size_t i=1; i<=num_loops; ++i) {
@@ -37,16 +37,16 @@ std::array<uint8_t,L> BLS12_381_KeyGen::HKDF_Expand(const std::array<uint8_t,BLS
         hmac_sha256.Write(&n, 1);
         hmac_sha256.Finalize(&prev[0]);
 
-        // if less than K bytes are remaining to copy, copy only the remaining bytes. otherwiese copy K bytes.
-        size_t bytes2copy = i * K > L ? L - (i - 1) * K : K;
+        // if less than DigestSize bytes are remaining to copy, copy only the remaining bytes. otherwiese copy DigestSize bytes.
+        size_t bytes2copy = i * DigestSize > L ? L - (i - 1) * DigestSize : DigestSize;
         std::copy_n(prev.cbegin(), bytes2copy, output_it);
 
-        std::advance(output_it, K);
+        std::advance(output_it, DigestSize);
     }
     return output;
 }
-template std::array<uint8_t,8160ul> BLS12_381_KeyGen::HKDF_Expand<8160ul>(const std::array<uint8_t,BLS12_381_KeyGen::K>& PRK, const std::vector<uint8_t>& info);
-template std::array<uint8_t,48ul> BLS12_381_KeyGen::HKDF_Expand<48ul>(const std::array<uint8_t,BLS12_381_KeyGen::K>& PRK, const std::vector<uint8_t>& info);
+template std::array<uint8_t,8160ul> BLS12_381_KeyGen::HKDF_Expand<8160ul>(const std::array<uint8_t,BLS12_381_KeyGen::DigestSize>& PRK, const std::vector<uint8_t>& info);
+template std::array<uint8_t,48ul> BLS12_381_KeyGen::HKDF_Expand<48ul>(const std::array<uint8_t,BLS12_381_KeyGen::DigestSize>& PRK, const std::vector<uint8_t>& info);
 
 std::vector<uint8_t> BLS12_381_KeyGen::I2OSP(const MclScalar& x, const size_t& xLen)
 {
@@ -84,16 +84,16 @@ std::vector<uint8_t> BLS12_381_KeyGen::flip_bits(const std::vector<uint8_t>& vec
 
 BLS12_381_KeyGen::LamportChunks BLS12_381_KeyGen::bytes_split(const std::array<uint8_t,8160>& octet_string)
 {
-    std::array<std::array<uint8_t,K>,N> ret;
+    std::array<std::array<uint8_t,DigestSize>,NumLamportChunks> ret;
     size_t i = 0;
 
-    for (auto it = octet_string.cbegin(); it != octet_string.cend(); std::advance(it, BLS12_381_KeyGen::K), ++i) {
-        std::copy(it, it + K, ret[i].begin());
+    for (auto it = octet_string.cbegin(); it != octet_string.cend(); std::advance(it, DigestSize), ++i) {
+        std::copy(it, it + DigestSize, ret[i].begin());
     }
     return ret;
 }
 
-MclScalar BLS12_381_KeyGen::HKDF_mod_r(const std::vector<uint8_t>& IKM, const std::vector<uint8_t>& key_info)
+MclScalar BLS12_381_KeyGen::HKDF_mod_r(const std::vector<uint8_t>& IKM)
 {
     const auto L = 48;  // `ceil((3 * ceil(log2(r))) / 16)`, where `r` is the order of the BLS 12-381 curve
     const std::string salt_str = "BLS-SIG-KEYGEN-SALT-";
@@ -101,7 +101,7 @@ MclScalar BLS12_381_KeyGen::HKDF_mod_r(const std::vector<uint8_t>& IKM, const st
 
     std::vector<uint8_t> salt(salt_str.cbegin(), salt_str.cend());
     CSHA256 sha256;
-    std::array<uint8_t, CSHA256::OUTPUT_SIZE> hash;
+    std::array<uint8_t, DigestSize> hash;
     MclScalar SK;
 
     while (true) {
@@ -113,7 +113,8 @@ MclScalar BLS12_381_KeyGen::HKDF_mod_r(const std::vector<uint8_t>& IKM, const st
         auto i2osp_zero = I2OSP(0, 1);
         IKM_zero.insert(IKM_zero.end(), i2osp_zero.cbegin(), i2osp_zero.cend());
         auto PRK = HKDF_Extract(salt, IKM_zero);
-        auto OKM = HKDF_Expand<L>(PRK, key_info);
+        auto info = I2OSP(L, 2);
+        auto OKM = HKDF_Expand<L>(PRK, info);
 
         SK = OS2IP(OKM);
         if (!SK.IsZero()) return SK;
@@ -121,11 +122,12 @@ MclScalar BLS12_381_KeyGen::HKDF_mod_r(const std::vector<uint8_t>& IKM, const st
     }
 }
 
-BLS12_381_KeyGen::LamportChunks BLS12_381_KeyGen::IKM_to_lamport_SK(const std::vector<uint8_t>& IKM, const std::vector<uint8_t>& salt)
+BLS12_381_KeyGen::LamportChunks
+BLS12_381_KeyGen::IKM_to_lamport_SK(const std::vector<uint8_t>& IKM, const std::vector<uint8_t>& salt)
 {
     auto PRK = HKDF_Extract(salt, IKM);
 
-    const auto L = N * K;
+    const auto L = NumLamportChunks * DigestSize;
     std::vector<uint8_t> info(0);
     auto OKM = HKDF_Expand<L>(PRK, info);
 
@@ -134,7 +136,8 @@ BLS12_381_KeyGen::LamportChunks BLS12_381_KeyGen::IKM_to_lamport_SK(const std::v
     return lamport_SK;
 }
 
-std::array<uint8_t,BLS12_381_KeyGen::K> BLS12_381_KeyGen::parent_SK_to_lamport_PK(const MclScalar& parent_SK, const uint32_t& index)
+std::array<uint8_t,BLS12_381_KeyGen::DigestSize>
+BLS12_381_KeyGen::parent_SK_to_lamport_PK(const MclScalar& parent_SK, const uint32_t& index)
 {
     auto salt = I2OSP(index, 4);
     auto IKM = I2OSP(parent_SK, 32);
@@ -145,12 +148,12 @@ std::array<uint8_t,BLS12_381_KeyGen::K> BLS12_381_KeyGen::parent_SK_to_lamport_P
 
     std::vector<uint8_t> lamport_PK;
     CSHA256 sha256;
-    std::array<uint8_t,K> hash;
+    std::array<uint8_t,DigestSize> hash;
 
     for (auto lamport_chunks : std::vector<LamportChunks> {lamport_0, lamport_1}) {
         for (size_t i=1; i<=255; ++i) {
             auto chunk = lamport_chunks[i-1];
-            sha256.Write(&chunk[0], K);
+            sha256.Write(&chunk[0], DigestSize);
             sha256.Finalize(&hash[0]);
             sha256.Reset();
             lamport_PK.insert(lamport_PK.end(), hash.cbegin(), hash.cend());
@@ -167,15 +170,13 @@ MclScalar BLS12_381_KeyGen::derive_master_SK(const std::vector<uint8_t>& seed)
     if (seed.size() < 32) {
         throw std::runtime_error("seed must be an 256-bit octet string or larger");
     }
-    const auto L = 48;
-    auto key_info = I2OSP(L, 2);
-    auto SK = HKDF_mod_r(seed, key_info);
+    auto SK = HKDF_mod_r(seed);
     return SK;
 }
 
 MclScalar BLS12_381_KeyGen::derive_child_SK(const MclScalar& parent_SK, const uint32_t& index)
 {
-    auto compressed_lamport_PK = parent_SK_to_lamport_PK(parent_SK, index);
-    auto SK = HKDF_mod_r(std::vector<uint8_t>(compressed_lamport_PK.cbegin(), compressed_lamport_PK.cend()), {});
+    auto comp_PK = parent_SK_to_lamport_PK(parent_SK, index);
+    auto SK = HKDF_mod_r(std::vector<uint8_t>(comp_PK.cbegin(), comp_PK.cend()));
     return SK;
 }
--- src/blsct/eip_2333/bls12_381_keygen.h
+++ src/blsct/eip_2333/bls12_381_keygen.h
@@ -11,98 +11,27 @@
 class BLS12_381_KeyGen
 {
 public:
-    // Inputs
-    // - seed, the source entropy for the entire tree, a octet string >= 256 bits in length
-    // Outputs
-    // - SK, the secret key of master node within the tree, a big endian encoded integer
     static MclScalar derive_master_SK(const std::vector<uint8_t>& seed);
-
-    // Inputs
-    // - parent_SK, the secret key of the parent node, a big endian encoded integer
-    // - index, the index of the desired child node, an integer 0 <= index < 2^32
-    // Outputs
-    // - child_SK, the secret key of the child node, a big endian encoded integer
     static MclScalar derive_child_SK(const MclScalar& parent_SK, const uint32_t& index);
 
 #ifndef BOOST_UNIT_TEST
 private:
 #endif
-    inline static const uint32_t K = CSHA256::OUTPUT_SIZE;  // digest size
-    inline static const uint32_t N = 255;  // ? TODO give a better name
+    inline static const uint32_t DigestSize = CSHA256::OUTPUT_SIZE;
+    inline static const uint32_t NumLamportChunks = 255;
 
-    using LamportChunks = std::array<std::array<uint8_t,K>,N>;
+    using LamportChunks = std::array<std::array<uint8_t,DigestSize>,NumLamportChunks>;
 
-    // TODO check if this remains the same in v5
-    inline static std::string r = "52435875175126190479447740508185965837690552500527637822603658699938581184513";
+    static std::array<uint8_t,DigestSize> HKDF_Extract(const std::vector<uint8_t>& salt, const std::vector<uint8_t>& IKM);
 
-    // // TODO check this remains the same in v5 environment as well
-    // inline static const uint32_t HKDF_mod_r_L = 48; // ceil((3 * ceil(log2(r))) / 16).(L=48)
-
-    // HKDF-Extract is as defined in RFC5869, instantiated with SHA256
-    // Inputs:
-    // - salt, optional salt value (a non-secret random value); if not provided, it is set to a string of HashLen zeros.
-    // - IKM, input keying material
-    // Output:
-    // - PRK, a pseudorandom key (of HashLen octets)
-    static std::array<uint8_t,K> HKDF_Extract(const std::vector<uint8_t>& salt, const std::vector<uint8_t>& IKM);
-
-    // HKDF-Expand is as defined in RFC5869, instantiated with SHA256
-    // Inputs:
-    // - PRK, a pseudorandom key of at least HashLen octets (usually, the output from the extract step) <- in bls12-381 keygen, fixed to 32
-    // - info, optional context and application specific information (can be a zero-length string)
-    // - L, length of output keying material in octets (<= 255*HashLen)  <- added as type argument
-    // Output:
-    // - OKM, output keying material (of L octets) <- OutSize octets are returned instead
     template <size_t L>
-    static std::array<uint8_t,L> HKDF_Expand(const std::array<uint8_t,K>& PRK, const std::vector<uint8_t>& info);
+    static std::array<uint8_t,L> HKDF_Expand(const std::array<uint8_t,DigestSize>& PRK, const std::vector<uint8_t>& info);
 
-    // I2OSP converts a nonnegative integer to an octet string of a specified length
-    // as defined in RFC3447 (Big endian decoding)
-    //
-    // I2OSP (x, xLen)
-    // Input:
-    // - x, nonnegative integer to be converted
-    // - xLen, intended length of the resulting octet string
-    // Output:
-    // - X, corresponding octet string of length xLen
     static std::vector<uint8_t> I2OSP(const MclScalar& x, const size_t& xLen);
-
-    // OS2IP converts an octet string to a nonnegative integer as defined in RFC3447
-    // (Big endian encoding)
-    // OS2IP (X)
-    // Input:
-    // - X, octet string to be converted <- size is fixed to 48
-    // Output:
-    // - x, corresponding nonnegative integer
     static MclScalar OS2IP(const std::array<uint8_t,48>& X);
-
-    // flip_bits is a function that returns the bitwise negation of its input
     static std::vector<uint8_t> flip_bits(const std::vector<uint8_t>& vec);
-
-    // a function that takes in an octet string and splits it into K-byte chunks which are returned as an array
-    // expected length of octet string is N * K
     static LamportChunks bytes_split(const std::array<uint8_t,8160>& octet_string);
-
-    // Inputs
-    // - IKM, a secret octet string >= 256 bits in length
-    // - key_info, an optional octet string (default="", the empty string)
-    // Outputs
-    // - SK, the corresponding secret key, an integer 0 <= SK < r.
-    static MclScalar HKDF_mod_r(const std::vector<uint8_t>& IKM, const std::vector<uint8_t>& key_info);
-
-    // Inputs
-    // - IKM, a secret octet string
-    // - salt, an octet string
-    //
-    // Outputs
-    // - lamport_SK, an array of 255 32-octet strings
+    static MclScalar HKDF_mod_r(const std::vector<uint8_t>& IKM);
     static LamportChunks IKM_to_lamport_SK(const std::vector<uint8_t>& IKM, const std::vector<uint8_t>& salt);
-
-    // Inputs
-    // - parent_SK, the BLS Secret Key of the parent node
-    // - index, the index of the desired child node, an integer 0 <= index < 2^32
-    //
-    // Outputs
-    // - lamport_PK, the compressed lamport PK, a 32 octet string Inputs
-    static std::array<uint8_t,BLS12_381_KeyGen::K> parent_SK_to_lamport_PK(const MclScalar& parent_SK, const uint32_t& index);
+    static std::array<uint8_t,DigestSize> parent_SK_to_lamport_PK(const MclScalar& parent_SK, const uint32_t& index);
 };
