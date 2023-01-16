021761aeacbaee15677030bf91280a2ec64be64b
Andrew Chow <github@achow101.com>
2023-01-16 17:32:35 -0500

    Merge #26325: rpc: Return accurate results for scanblocks

--- test/functional/feature_pruning.py
+++ test/functional/feature_pruning.py
@@ -85,7 +85,7 @@ class PruneTest(SyscoinTestFramework):
             ["-dip3params=2000:2000","-maxreceivebuffer=20000", "-prune=550"],
             ["-dip3params=2000:2000","-maxreceivebuffer=20000"],
             ["-dip3params=2000:2000","-maxreceivebuffer=20000"],
-            ["-dip3params=2000:2000","-prune=550"],
+            ["-dip3params=2000:2000","-prune=550", "-blockfilterindex=1"],
         ]
         self.rpc_timeout = 120
 
@@ -357,7 +357,7 @@ class PruneTest(SyscoinTestFramework):
         self.connect_nodes(0, 5)
         nds = [self.nodes[0], self.nodes[5]]
         self.sync_blocks(nds, wait=5, timeout=300)
-        self.restart_node(5, extra_args=["-prune=550"]) # restart to trigger rescan
+        self.restart_node(5, extra_args=["-prune=550", "-blockfilterindex=1"]) # restart to trigger rescan
         self.log.info("Success")
 
     def run_test(self):
@@ -473,7 +473,20 @@ class PruneTest(SyscoinTestFramework):
         self.log.info("Test invalid pruning command line options")
         self.test_invalid_command_line_options()
 
+        self.test_scanblocks_pruned()
+
         self.log.info("Done")
+
+    def test_scanblocks_pruned(self):
+        node = self.nodes[5]
+        genesis_blockhash = node.getblockhash(0)
+        false_positive_spk = bytes.fromhex("001400000000000000000000000000000000000cadcb")
+
+        assert genesis_blockhash in node.scanblocks(
+            "start", [{"desc": f"raw({false_positive_spk.hex()})"}], 0, 0)['relevant_blocks']
+
+        assert_raises_rpc_error(-1, "Block not available (pruned data)", node.scanblocks,
+            "start", [{"desc": f"raw({false_positive_spk.hex()})"}], 0, 0, "basic", {"filter_false_positives": True})
 
 if __name__ == '__main__':
     PruneTest().main()
