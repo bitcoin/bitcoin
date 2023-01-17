dddd462137a85225955d4c2bcdb52e1e4235bff0
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2023-01-17 21:55:37 +0100

    Bump minimum python version to 3.7

--- ci/test/00_setup_env_native_nowallet_libbitcoinkernel.sh
+++ ci/test/00_setup_env_native_nowallet_libbitcoinkernel.sh
@@ -7,8 +7,10 @@
 export LC_ALL=C.UTF-8
 
 export CONTAINER_NAME=ci_native_nowallet_libbitcoinkernel
-export CI_IMAGE_NAME_TAG=ubuntu:18.04  # Use bionic to have one config run the tests in python3.6, see doc/dependencies.md
-export PACKAGES="python3-zmq clang-8 llvm-8 libc++abi-8-dev libc++-8-dev"  # Use clang-8 to test C++17 compatibility, see doc/dependencies.md
+export CI_IMAGE_NAME_TAG=debian:buster
+# Use minimum supported python3.7 and clang-8, see doc/dependencies.md
+export PACKAGES="-t buster-backports python3-zmq clang-8 llvm-8 libc++abi-8-dev libc++-8-dev"
+export APPEND_APT_SOURCES_LIST="deb http://deb.debian.org/debian buster-backports main"
 export DEP_OPTS="NO_WALLET=1 CC=clang-8 CXX='clang++-8 -stdlib=libc++'"
 export GOAL="install"
 export BITCOIN_CONFIG="--enable-reduce-exports CC=clang-8 CXX='clang++-8 -stdlib=libc++' --enable-experimental-util-chainstate --with-experimental-kernel-lib --enable-shared"
