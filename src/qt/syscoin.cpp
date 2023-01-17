48cde8f2cbc6d2cea7c3ba68e34f1813c273e3b1
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2023-01-17 10:33:46 -0800

    Update syscoin.cpp

--- src/qt/syscoin.cpp
+++ src/qt/syscoin.cpp
@@ -227,7 +227,7 @@ void DebugMessageHandler(QtMsgType type, const QMessageLogContext& context, cons
 static int qt_argc = 1;
 static const char* qt_argv = "syscoin-qt";
 
-BitcoinApplication::SyscoinApplication()
+SyscoinApplication::SyscoinApplication()
     : QApplication(qt_argc, const_cast<char**>(&qt_argv))
 {
     // Qt runs setlocale(LC_ALL, "") on initialization.
