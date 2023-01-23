9c5224c54406dc1b0474179279e85cce6a44ce9b
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2023-01-23 09:23:35 +0100

    Merge bitcoin/bitcoin#26944: depends: fix systemtap download URL
    d81ca6619a5d05472af7f59e36cd100dd04a3a01 depends: fix systemtap download URL (fanquake)
    
    Pull request description:
    
      The URL has changed, and the current one 404s.
    
    ACKs for top commit:
      hebasto:
        ACK d81ca6619a5d05472af7f59e36cd100dd04a3a01, verified each link: the old one returns 404, the new one is OK.
      theStack:
        ACK d81ca6619a5d05472af7f59e36cd100dd04a3a01
      jarolrod:
        ACK d81ca6619a5d05472af7f59e36cd100dd04a3a01
    
    Tree-SHA512: e3240efd97003b4063c84bf72638d005f1629d0753359520353e249745fde185ef8e23fcd504037486bce4c4453dcb86f972e33111486ace8ad65746636e1499

--- depends/packages/systemtap.mk
+++ depends/packages/systemtap.mk
@@ -1,6 +1,6 @@
 package=systemtap
 $(package)_version=4.7
-$(package)_download_path=https://sourceware.org/systemtap/ftp/releases/
+$(package)_download_path=https://sourceware.org/ftp/systemtap/releases/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=43a0a3db91aa4d41e28015b39a65e62059551f3cc7377ebf3a3a5ca7339e7b1f
 $(package)_patches=remove_SDT_ASM_SECTION_AUTOGROUP_SUPPORT_check.patch
