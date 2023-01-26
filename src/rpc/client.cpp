d703fb01117ff3ed68cf1052c9b1ee05b746ccf3
muxator <antonio.muci@bancaditalia.it>
2023-01-26 14:02:56 +0100

    rpc: fix CRPCConvertParam table in client.cpp. Fixes failure of p2p_blockfilters.py
    This change should have been part of commit fc85aea0398e ("rpc: add
    'check_signet_solution' parameter to testblockvalidity").
    
    Before this change:
    $ cd test/functional
    $ ./test_runner.py rpc_help.py
    Temporary test directory at /tmp/test_runner_₿_🏃_20230126_110139
    Running Unit Tests for Test Framework Modules
    ..........
    ----------------------------------------------------------------------
    Ran 10 tests in 0.648s
    
    OK
    Remaining jobs: [rpc_help.py]
    1/1 - rpc_help.py failed, Duration: 2 s
    
    stdout:
    2023-01-26T10:01:40.180000Z TestFramework (INFO): Initializing test directory /tmp/test_runner_₿_🏃_20230126_110139/rpc_help_0
    2023-01-26T10:01:41.939000Z TestFramework (ERROR): Assertion failed
    Traceback (most recent call last):
      File "<base>/test/functional/test_framework/test_framework.py", line 132, in main
        self.run_test()
      File "<base>/test/functional/rpc_help.py", line 51, in run_test
        self.test_client_conversion_table()
      File "<base>/test/functional/rpc_help.py", line 69, in test_client_conversion_table
        raise AssertionError("RPC client conversion table ({}) and RPC server named arguments mismatch!\n{}".format(
    AssertionError: RPC client conversion table (<base>/src/rpc/client.cpp) and RPC server named arguments mismatch!
    {('testblockvalidity', 1, 'check_signet_solution')}
    [...]

--- src/rpc/client.cpp
+++ src/rpc/client.cpp
@@ -175,6 +175,7 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "logging", 1, "exclude" },
     { "disconnectnode", 1, "nodeid" },
     { "upgradewallet", 0, "version" },
+    { "testblockvalidity", 1, "check_signet_solution"}, // ITCOIN_SPECIFIC
     // Echo with conversion (For testing only)
     { "echojson", 0, "arg0" },
     { "echojson", 1, "arg1" },
