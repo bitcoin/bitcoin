8f36092b7a275fd8a35c3625e01f23fe7fb1a3b4
fanquake <fanquake@gmail.com>
2023-01-31 17:55:37 +0000

    Merge bitcoin/bitcoin#23670: build: Build minisketch test in `make check`, not in `make`
    6d58117a31a88eec3f0a103f9d1fc26cf0b48348 build: Build minisketch test in `make check`, not in `make` (Hennadii Stepanov)
    
    Pull request description:
    
      On master (d1e42659bbdd8da170542d8c638242cd94f71a7d):
      ```
      $ ./autogen.sh && ./configure --without-gui --disable-wallet && make clean
      $ make 2>&1 | grep LD | grep -v .la
        CXXLD    bitcoind
        CXXLD    bitcoin-cli
        CXXLD    bitcoin-tx
        CXXLD    bitcoin-util
        CXXLD    test/test_bitcoin
        CXXLD    bench/bench_bitcoin
        CXXLD    minisketch/test
        CXXLD    test/fuzz/fuzz
        CXXLD    univalue/test/object
        CXXLD    univalue/test/unitester
      $ make check 2>&1 | grep LD
        CCLD     exhaustive_tests
        CCLD     tests
      ```
    
      With this PR:
      ```
      $ ./autogen.sh && ./configure --without-gui --disable-wallet && make clean
      $ make 2>&1 | grep LD | grep -v .la
        CXXLD    bitcoind
        CXXLD    bitcoin-cli
        CXXLD    bitcoin-tx
        CXXLD    bitcoin-util
        CXXLD    test/test_bitcoin
        CXXLD    bench/bench_bitcoin
        CXXLD    test/fuzz/fuzz
        CXXLD    univalue/test/object
        CXXLD    univalue/test/unitester
      $ make check 2>&1 | grep LD
        CXXLD    minisketch/test
        CCLD     exhaustive_tests
        CCLD     tests
      ```
    
      In fact, this PR restores behavior that was before bitcoin/bitcoin#22646, and that behavior looks more optimal.
    
      As an outcome, the `contrib/guix/libexec/build.sh` does not spend resources to build binaries which are not a part of the release package.
    
    ACKs for top commit:
      TheCharlatan:
        ACK 6d58117a31a88eec3f0a103f9d1fc26cf0b48348
    
    Tree-SHA512: 4957c8f88a01aca005813bf4c1c26f433756bf68ea0c958481c638ead229fa8e23ecae3a8ac31ea555876ba6f2cc10ecd91caf2e2f664de5cb529ec05fb38fa7

--- ci/test/06_script_b.sh
+++ ci/test/06_script_b.sh
@@ -9,12 +9,14 @@ export LC_ALL=C.UTF-8
 if [[ $HOST = *-mingw32 ]]; then
   # Generate all binaries, so that they can be wrapped
   CI_EXEC make "$MAKEJOBS" -C src/secp256k1 VERBOSE=1
+  CI_EXEC make "$MAKEJOBS" -C src minisketch/test.exe VERBOSE=1
   CI_EXEC "${BASE_ROOT_DIR}/ci/test/wrap-wine.sh"
 fi
 
 if [ -n "$QEMU_USER_CMD" ]; then
   # Generate all binaries, so that they can be wrapped
   CI_EXEC make "$MAKEJOBS" -C src/secp256k1 VERBOSE=1
+  CI_EXEC make "$MAKEJOBS" -C src minisketch/test VERBOSE=1
   CI_EXEC "${BASE_ROOT_DIR}/ci/test/wrap-qemu.sh"
 fi
 
