e1e1fab923940f5266738e36ef6553128e17d7e9
glozow <gloriajzhao@gmail.com>
2023-02-01 11:46:10 +0000

    Merge bitcoin/bitcoin#26991: doc: followups to #26471
    47c174d8ce4c5f36c41203aedde86c5f0da90217 doc: NetPermissionFlags for tx relay in blocksonly (willcl-ark)
    e325e0fccba4981d28053b79473ddaa44355e6e8 doc: Fix comment syntax error (willcl-ark)
    
    Pull request description:
    
      Fix syntax error and specify `NetPermissionFlags` for whitelisted tx relay
    
    ACKs for top commit:
      w0xlt:
        ACK https://github.com/bitcoin/bitcoin/pull/26991/commits/47c174d8ce4c5f36c41203aedde86c5f0da90217
    
    Tree-SHA512: eb579dc599a96a3ea79c01ac3e76160ec59cf71c2486c9401da8fbbd96ae756ba647aa9ba874835946bc76ba02782729da788617f982ae5a852139e10e7dfd75

--- src/kernel/mempool_options.h
+++ src/kernel/mempool_options.h
@@ -18,7 +18,7 @@ class CBlockPolicyEstimator;
 
 /** Default for -maxmempool, maximum megabytes of mempool memory usage */
 static constexpr unsigned int DEFAULT_MAX_MEMPOOL_SIZE_MB{300};
-//** Default for -maxmempool when blocksonly is set */
+/** Default for -maxmempool when blocksonly is set */
 static constexpr unsigned int DEFAULT_BLOCKSONLY_MAX_MEMPOOL_SIZE_MB{5};
 /** Default for -mempoolexpiry, expiration time for mempool transactions in hours */
 static constexpr unsigned int DEFAULT_MEMPOOL_EXPIRY_HOURS{336};
