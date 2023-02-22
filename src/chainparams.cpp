4600d6c25696b55f6adc2ad20ab8844dff3dae2e
PastaPastaPasta <6443210+PastaPastaPasta@users.noreply.github.com>
2023-02-22 11:08:57 -0600

    chore: set the nStartTime for v19 deployment Fed 22 and March 31st respectively (#5218)
    ## Issue being fixed or feature implemented
    
    ## What was done?
    
    ## How Has This Been Tested?
    
    ## Breaking Changes
    
    ## Checklist:
    - [x] I have performed a self-review of my own code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have added or updated relevant unit/integration/functional/e2e
    tests
    - [ ] I have made corresponding changes to the documentation
    
    **For repository code-owners and collaborators only**
    - [x] I have assigned this pull request to a milestone

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -248,8 +248,8 @@ public:
 
         // Deployment of Deployment of Basic BLS, AssetLocks, EHF
         consensus.vDeployments[Consensus::DEPLOYMENT_V19].bit = 8;
-        consensus.vDeployments[Consensus::DEPLOYMENT_V19].nStartTime = 19999999999;     // TODO: To be determined later
-        consensus.vDeployments[Consensus::DEPLOYMENT_V19].nTimeout = 999999999999ULL;
+        consensus.vDeployments[Consensus::DEPLOYMENT_V19].nStartTime = 1680220800;     // Fri, Mar 31, 2023 0:00:00
+        consensus.vDeployments[Consensus::DEPLOYMENT_V19].nTimeout = 1711843200;     // Sun, Mar 31, 2024 00:00:00
         consensus.vDeployments[Consensus::DEPLOYMENT_V19].nWindowSize = 4032;
         consensus.vDeployments[Consensus::DEPLOYMENT_V19].nThresholdStart = 3226;       // 80% of 4032
         consensus.vDeployments[Consensus::DEPLOYMENT_V19].nThresholdMin = 2420;         // 60% of 4032
@@ -486,7 +486,7 @@ public:
 
         // Deployment of Deployment of Basic BLS, AssetLocks, EHF
         consensus.vDeployments[Consensus::DEPLOYMENT_V19].bit = 8;
-        consensus.vDeployments[Consensus::DEPLOYMENT_V19].nStartTime = 19999999999;     // TODO: To be determined later
+        consensus.vDeployments[Consensus::DEPLOYMENT_V19].nStartTime = 1677024000;     // Wed, Feb 22, 2023 0:00:00
         consensus.vDeployments[Consensus::DEPLOYMENT_V19].nTimeout = 999999999999ULL;
         consensus.vDeployments[Consensus::DEPLOYMENT_V19].nWindowSize = 100;
         consensus.vDeployments[Consensus::DEPLOYMENT_V19].nThresholdStart = 80;         // 80% of 100
