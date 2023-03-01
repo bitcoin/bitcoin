2d60375c2283a022daadb98d83ba53f339ac4747
Odysseas Gabrielides <odysseas.gabrielides@gmail.com>
2023-03-01 19:42:33 +0200

    feat(llmq): llmq_25_67 for Platform (Testnet only) (#5225)
    ## Issue being fixed or feature implemented
    
    ## What was done?
    - Added new LLMQ type `llmq_25_67`
    - The above LLMQ is added only for Testnet and it is activated with v19
    fork.
    
    ## How Has This Been Tested?
    
    ## Breaking Changes
    
    ## Checklist:
    - [x] I have performed a self-review of my own code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have added or updated relevant unit/integration/functional/e2e
    tests
    - [ ] I have made corresponding changes to the documentation
    
    **For repository code-owners and collaborators only**
    - [x] I have assigned this pull request to a milestone
    
    ---------
    
    Co-authored-by: pasta <pasta@dashboost.org>

--- src/llmq/params.h
+++ src/llmq/params.h
@@ -19,6 +19,7 @@ enum class LLMQType : uint8_t {
     LLMQ_400_85 = 3, // 400 members, 340 (85%) threshold, one every 24 hours
     LLMQ_100_67 = 4, // 100 members, 67 (67%) threshold, one per hour
     LLMQ_60_75 = 5,  // 60 members, 45 (75%) threshold, one every 12 hours
+    LLMQ_25_67 = 6, // 25 members, 67 (67%) threshold, one per hour
 
     // for testing only
     LLMQ_TEST = 100, // 3 members, 2 (66%) threshold, one per hour. Params might differ when -llmqtestparams is used
@@ -108,7 +109,7 @@ struct LLMQParams {
 };
 
 
-static constexpr std::array<LLMQParams, 13> available_llmqs = {
+static constexpr std::array<LLMQParams, 14> available_llmqs = {
 
     /**
      * llmq_test
@@ -438,6 +439,33 @@ static constexpr std::array<LLMQParams, 13> available_llmqs = {
 
         .keepOldConnections = 25,
         .recoveryMembers = 50,
+    },
+
+    /**
+     * llmq_25_67
+     * This quorum is deployed on Testnet and requires
+     * 25 participants
+     *
+     * Used by Dash Platform
+     */
+    LLMQParams{
+        .type = LLMQType::LLMQ_25_67,
+        .name = "llmq_25_67",
+        .useRotation = false,
+        .size = 25,
+        .minSize = 22,
+        .threshold = 17,
+
+        .dkgInterval = 24, // one DKG per hour
+        .dkgPhaseBlocks = 2,
+        .dkgMiningWindowStart = 10, // dkgPhaseBlocks * 5 = after finalization
+        .dkgMiningWindowEnd = 18,
+        .dkgBadVotesThreshold = 22,
+
+        .signingActiveQuorumCount = 24, // a full day worth of LLMQs
+
+        .keepOldConnections = 25,
+        .recoveryMembers = 12,
     },
 
 }; // available_llmqs
