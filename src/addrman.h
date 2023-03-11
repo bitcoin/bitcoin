17e705428ddf80c7a7f31fe5430d966cf08a37d6
Amiti Uttarwar <amiti@uttarwar.org>
2023-03-10 18:25:59 -0800

    doc: clarify new_only param for Select function
    Co-authored-by: Martin Zumsande <mzumsande@gmail.com>

--- src/addrman.h
+++ src/addrman.h
@@ -146,7 +146,9 @@ public:
     /**
      * Choose an address to connect to.
      *
-     * @param[in] new_only Whether to only select addresses from the new table.
+     * @param[in] new_only Whether to only select addresses from the new table. Passing `true` returns
+     *                     an address from the new table or an empty pair. Passing `false` will return an
+     *                     address from either the new or tried table (it does not guarantee a tried entry).
      * @param[in] network  Select only addresses of this network (nullopt = all)
      * @return    CAddress The record for the selected peer.
      *            seconds  The last time we attempted to connect to that peer.
