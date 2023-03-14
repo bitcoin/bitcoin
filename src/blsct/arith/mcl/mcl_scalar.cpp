674bc160a01a321690c110f7d577802d27f9d090
alex v <alex@encrypt-s.com>
2023-03-14 18:57:33 +0100

    Update mcl_scalar.cpp

--- src/blsct/arith/mcl/mcl_scalar.cpp
+++ src/blsct/arith/mcl/mcl_scalar.cpp
@@ -287,9 +287,7 @@ std::vector<uint8_t> MclScalar::GetVch(const bool trim_preceeding_zeros) const
     if (mclBnFr_serialize(&vec[0], seri_size, &m_fr) == 0) {
         // We avoid throwing an exception as the fuzzer would crash when injecting random data
         // through a transaction in one of the mcl fields.
-        
         // The default value is the same of the constructor (0)
-        
         MclScalar ret;
         return ret.GetVch();
     }
