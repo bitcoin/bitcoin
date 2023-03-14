a9a35db766807c275b2df44fc63a5b10ab9bc25f
mxaddict <mxaddict@codedmaster.com>
2023-03-15 04:16:27 +0800

    Fixed missing 'bool extendedRangeProof' para in TxToUniv function declaration

--- src/core_io.h
+++ src/core_io.h
@@ -57,6 +57,6 @@ std::string EncodeHexTx(const CTransaction& tx, const int serializeFlags = 0);
 std::string SighashToStr(unsigned char sighash_type);
 void RangeProofToUniv(const RangeProof<Mcl>& rp, UniValue& entry, const bool& extended = false);
 void ScriptToUniv(const CScript& script, UniValue& out, bool include_hex = true, bool include_address = false, const SigningProvider* provider = nullptr);
-void TxToUniv(const CTransaction& tx, const uint256& block_hash, UniValue& entry, bool include_hex = true, int serialize_flags = 0, const CTxUndo* txundo = nullptr, TxVerbosity verbosity = TxVerbosity::SHOW_DETAILS);
+void TxToUniv(const CTransaction& tx, const uint256& block_hash, UniValue& entry, bool include_hex = true, int serialize_flags = 0, const CTxUndo* txundo = nullptr, TxVerbosity verbosity = TxVerbosity::SHOW_DETAILS, bool extendedRangeProof = false);
 
 #endif // BITCOIN_CORE_IO_H
