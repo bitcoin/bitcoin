bbab474c92d9d4c55631c107659d83e8121ae0d7
PastaPastaPasta <6443210+PastaPastaPasta@users.noreply.github.com>
2023-03-17 10:40:05 -0500

    Merge pull request #5258 from kittywhiskers/update_dashbls
    depends: update 'src/dashbls' to dashpay/bls-signatures@9329803 as c1992c1

--- src/dashbls/js-bindings/jsbindings.cpp
+++ src/dashbls/js-bindings/jsbindings.cpp
@@ -22,92 +22,88 @@ using namespace emscripten;
 namespace js_wrappers {
 EMSCRIPTEN_BINDINGS(blsjs) {
     class_<AugSchemeMPLWrapper>("AugSchemeMPL")
-        .class_function("sk_to_g1", &AugSchemeMPLWrapper::SkToG1)
-        .class_function("key_gen", &AugSchemeMPLWrapper::KeyGen)
+        .class_function("skToG1", &AugSchemeMPLWrapper::SkToG1)
+        .class_function("keyGen", &AugSchemeMPLWrapper::KeyGen)
         .class_function("sign", &AugSchemeMPLWrapper::Sign)
-        .class_function("sign_prepend", &AugSchemeMPLWrapper::SignPrepend)
+        .class_function("signPrepend", &AugSchemeMPLWrapper::SignPrepend)
         .class_function("verify", &AugSchemeMPLWrapper::Verify)
         .class_function("aggregate", &AugSchemeMPLWrapper::Aggregate)
-        .class_function("aggregate_verify", &AugSchemeMPLWrapper::AggregateVerify)
-        .class_function("derive_child_sk", &AugSchemeMPLWrapper::DeriveChildSk)
-        .class_function("derive_child_sk_unhardened", &AugSchemeMPLWrapper::DeriveChildSkUnhardened)
-        .class_function("derive_child_pk_unhardened", &AugSchemeMPLWrapper::DeriveChildPkUnhardened);
+        .class_function("aggregateVerify", &AugSchemeMPLWrapper::AggregateVerify)
+        .class_function("deriveChildSk", &AugSchemeMPLWrapper::DeriveChildSk)
+        .class_function("deriveChildSkUnhardened", &AugSchemeMPLWrapper::DeriveChildSkUnhardened)
+        .class_function("deriveChildPkUnhardened", &AugSchemeMPLWrapper::DeriveChildPkUnhardened);
 
     class_<SchemeMPLWrapper<BasicSchemeMPL>>("BasicSchemeMPL")
-        .class_function("sk_to_g1", &SchemeMPLWrapper<BasicSchemeMPL>::SkToG1)
-        .class_function("key_gen", &SchemeMPLWrapper<BasicSchemeMPL>::KeyGen)
+        .class_function("skToG1", &SchemeMPLWrapper<BasicSchemeMPL>::SkToG1)
+        .class_function("keyGen", &SchemeMPLWrapper<BasicSchemeMPL>::KeyGen)
         .class_function("sign", &SchemeMPLWrapper<BasicSchemeMPL>::Sign)
         .class_function("verify", &SchemeMPLWrapper<BasicSchemeMPL>::Verify)
         .class_function("aggregate", &SchemeMPLWrapper<BasicSchemeMPL>::Aggregate)
-        .class_function("aggregate_verify", &SchemeMPLWrapper<BasicSchemeMPL>::AggregateVerify)
-        .class_function("derive_child_sk", &SchemeMPLWrapper<BasicSchemeMPL>::DeriveChildSk)
-        .class_function("derive_child_sk_unhardened", &SchemeMPLWrapper<BasicSchemeMPL>::DeriveChildSkUnhardened)
-        .class_function("derive_child_pk_unhardened", &SchemeMPLWrapper<BasicSchemeMPL>::DeriveChildPkUnhardened);
+        .class_function("aggregateVerify", &SchemeMPLWrapper<BasicSchemeMPL>::AggregateVerify)
+        .class_function("deriveChildSk", &SchemeMPLWrapper<BasicSchemeMPL>::DeriveChildSk)
+        .class_function("deriveChildSkUnhardened", &SchemeMPLWrapper<BasicSchemeMPL>::DeriveChildSkUnhardened)
+        .class_function("deriveChildPkUnhardened", &SchemeMPLWrapper<BasicSchemeMPL>::DeriveChildPkUnhardened);
 
     class_<PopSchemeMPLWrapper>("PopSchemeMPL")
-        .class_function("sk_to_g1", &PopSchemeMPLWrapper::SkToG1)
-        .class_function("key_gen", &PopSchemeMPLWrapper::KeyGen)
+        .class_function("skToG1", &PopSchemeMPLWrapper::SkToG1)
+        .class_function("keyGen", &PopSchemeMPLWrapper::KeyGen)
         .class_function("sign", &PopSchemeMPLWrapper::Sign)
         .class_function("verify", &PopSchemeMPLWrapper::Verify)
         .class_function("aggregate", &PopSchemeMPLWrapper::Aggregate)
-        .class_function("aggregate_verify", &PopSchemeMPLWrapper::AggregateVerify)
-        .class_function("derive_child_sk", &PopSchemeMPLWrapper::DeriveChildSk)
-        .class_function("derive_child_sk_unhardened", &PopSchemeMPLWrapper::DeriveChildSkUnhardened)
-        .class_function("derive_child_pk_unhardened", &PopSchemeMPLWrapper::DeriveChildPkUnhardened)
-        .class_function("pop_prove", &PopSchemeMPLWrapper::PopProve)
-        .class_function("pop_verify", &PopSchemeMPLWrapper::PopVerify)
-        .class_function("fast_aggregate_verify", &PopSchemeMPLWrapper::FastAggregateVerify);
+        .class_function("aggregateVerify", &PopSchemeMPLWrapper::AggregateVerify)
+        .class_function("deriveChildSk", &PopSchemeMPLWrapper::DeriveChildSk)
+        .class_function("deriveChildSkUnhardened", &PopSchemeMPLWrapper::DeriveChildSkUnhardened)
+        .class_function("deriveChildPkUnhardened", &PopSchemeMPLWrapper::DeriveChildPkUnhardened)
+        .class_function("popProve", &PopSchemeMPLWrapper::PopProve)
+        .class_function("popVerify", &PopSchemeMPLWrapper::PopVerify)
+        .class_function("fastAggregateVerify", &PopSchemeMPLWrapper::FastAggregateVerify);
 
 
     class_<G1ElementWrapper>("G1Element")
         .class_property("SIZE", &G1ElementWrapper::SIZE)
         .constructor<>()
-        .class_function("fromBytes", &G1ElementWrapper::FromBytes) // Not removing this for compatibility
-        .class_function("from_bytes", &G1ElementWrapper::FromBytes)
+        .class_function("fromBytes", &G1ElementWrapper::FromBytes)
         .class_function("generator", &G2ElementWrapper::Generator)
         .function("serialize", &G1ElementWrapper::Serialize)
         .function("negate", &G1ElementWrapper::Negate)
         .function("deepcopy", &G1ElementWrapper::Deepcopy)
-        .function("get_fingerprint", &G1ElementWrapper::GetFingerprint)
+        .function("getFingerprint", &G1ElementWrapper::GetFingerprint)
         .function("add", &G1ElementWrapper::Add)
         .function("mul", &G1ElementWrapper::Mul)
-        .function("equal_to", &G1ElementWrapper::EqualTo);
+        .function("equalTo", &G1ElementWrapper::EqualTo);
 
     class_<G2ElementWrapper>("G2Element")
         .class_property("SIZE", &G2ElementWrapper::SIZE)
         .constructor<>()
-        .class_function("fromBytes", &G2ElementWrapper::FromBytes) // Not removing this for compatibility
-        .class_function("from_bytes", &G2ElementWrapper::FromBytes)
-        .class_function("from_g2", &G2ElementWrapper::FromG2Element)
-        .class_function("aggregate_sigs", &G2ElementWrapper::AggregateSigs)
+        .class_function("fromBytes", &G2ElementWrapper::FromBytes)
+        .class_function("fromG2", &G2ElementWrapper::FromG2Element)
+        .class_function("aggregateSigs", &G2ElementWrapper::AggregateSigs)
         .class_function("generator", &G2ElementWrapper::Generator)
         .function("serialize", &G2ElementWrapper::Serialize)
         .function("negate", &G2ElementWrapper::Negate)
         .function("deepcopy", &G2ElementWrapper::Deepcopy)
         .function("add", &G2ElementWrapper::Add)
         .function("mul", &G2ElementWrapper::Mul)
-        .function("equal_to", &G2ElementWrapper::EqualTo);
+        .function("equalTo", &G2ElementWrapper::EqualTo);
 
     class_<PrivateKeyWrapper>("PrivateKey")
         .class_property("PRIVATE_KEY_SIZE", &PrivateKeyWrapper::PRIVATE_KEY_SIZE)
-        .class_function("fromBytes", &PrivateKeyWrapper::FromBytes) // Not removing this for compatibility
-        .class_function("from_bytes", &PrivateKeyWrapper::FromBytes)
+        .class_function("fromBytes", &PrivateKeyWrapper::FromBytes)
         .class_function("aggregate", &PrivateKeyWrapper::Aggregate)
         .function("deepcopy", &PrivateKeyWrapper::Deepcopy)
         .function("serialize", &PrivateKeyWrapper::Serialize)
-        .function("get_g1", &PrivateKeyWrapper::GetG1)
-        .function("get_g2", &PrivateKeyWrapper::GetG2)
-        .function("mul_g1", &PrivateKeyWrapper::MulG1)
-        .function("mul_g2", &PrivateKeyWrapper::MulG2)
-        .function("equal_to", &PrivateKeyWrapper::EqualTo);
+        .function("getG1", &PrivateKeyWrapper::GetG1)
+        .function("getG2", &PrivateKeyWrapper::GetG2)
+        .function("mulG1", &PrivateKeyWrapper::MulG1)
+        .function("mulG2", &PrivateKeyWrapper::MulG2)
+        .function("equalTo", &PrivateKeyWrapper::EqualTo);
 
     class_<BignumWrapper>("Bignum")
-        .class_function("fromString", &BignumWrapper::FromString) // Not removing this for compatibility
-        .class_function("from_string", &BignumWrapper::FromString)
+        .class_function("fromString", &BignumWrapper::FromString)
         .function("toString", &BignumWrapper::ToString);
 
     class_<UtilWrapper>("Util")
         .class_function("hash256", &UtilWrapper::Hash256)
-        .class_function("hex_str", &UtilWrapper::HexStr);
+        .class_function("hexStr", &UtilWrapper::HexStr);
 };
 }  // namespace js_wrappers
