b4bed5c1f98c0eed18f52fdcea11a420c10ed98d
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2023-03-21 13:04:35 +0000

    refactor: Drop no longer used `CScriptCheck()` default constructor

--- src/validation.h
+++ src/validation.h
@@ -318,7 +318,6 @@ private:
     PrecomputedTransactionData *txdata;
 
 public:
-    CScriptCheck(): ptxTo(nullptr), nIn(0), nFlags(0), cacheStore(false), error(SCRIPT_ERR_UNKNOWN_ERROR) {}
     CScriptCheck(const CTxOut& outIn, const CTransaction& txToIn, unsigned int nInIn, unsigned int nFlagsIn, bool cacheIn, PrecomputedTransactionData* txdataIn) :
         m_tx_out(outIn), ptxTo(&txToIn), nIn(nInIn), nFlags(nFlagsIn), cacheStore(cacheIn), error(SCRIPT_ERR_UNKNOWN_ERROR), txdata(txdataIn) { }
 
