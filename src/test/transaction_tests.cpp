06820032142a75cc3c5b832045058bc6f6f74786
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2023-03-21 13:03:30 +0000

    test, refactor: Avoid `CScriptCheck::swap` in `transaction_tests`

--- src/test/transaction_tests.cpp
+++ src/test/transaction_tests.cpp
@@ -547,9 +547,7 @@ BOOST_AUTO_TEST_CASE(test_big_witness_transaction)
 
     for(uint32_t i = 0; i < mtx.vin.size(); i++) {
         std::vector<CScriptCheck> vChecks;
-        CScriptCheck check(coins[tx.vin[i].prevout.n].out, tx, i, SCRIPT_VERIFY_P2SH | SCRIPT_VERIFY_WITNESS, false, &txdata);
-        vChecks.push_back(CScriptCheck());
-        check.swap(vChecks.back());
+        vChecks.emplace_back(coins[tx.vin[i].prevout.n].out, tx, i, SCRIPT_VERIFY_P2SH | SCRIPT_VERIFY_WITNESS, false, &txdata);
         control.Add(vChecks);
     }
 
