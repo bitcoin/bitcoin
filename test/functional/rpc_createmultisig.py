4e19d00773624fc71a4cb1a7fd56399d671ec922
Konstantin Akimov <knstqq@gmail.com>
2023-03-24 16:50:28 +0700

    backport: follow-up new rpc_createmultisig.py (bitcoin#13072) - changes from bitcoin#17675

--- test/functional/rpc_createmultisig.py
+++ test/functional/rpc_createmultisig.py
@@ -22,6 +22,7 @@ class RpcCreateMultiSigTest(BitcoinTestFramework):
     def set_test_params(self):
         self.setup_clean_chain = True
         self.num_nodes = 3
+        self.supports_cli = False
 
     def skip_test_if_missing_module(self):
         self.skip_if_no_wallet()
