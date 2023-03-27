46b0e700deb378829fced05f13b63de137b1d3c5
Andrew Poelstra <apoelstra@wpsoftware.net>
2023-03-27 23:32:23 +0000

    codex32: add functional test for seed import

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -273,6 +273,7 @@ BASE_SCRIPTS = [
     'feature_nulldummy.py',
     'mempool_accept.py',
     'mempool_expiry.py',
+    'wallet_addhdkey.py',
     'wallet_importdescriptors.py',
     'wallet_crosschain.py',
     'mining_basic.py',
