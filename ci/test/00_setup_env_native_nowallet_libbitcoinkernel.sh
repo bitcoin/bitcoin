fa4a46de0b3c1a5895e95dba7e95278932fbfc2c
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2023-03-29 15:51:03 +0200

    ci: Bump nowallet_libbitcoinkernel task to ubuntu:focal
    This is needed to work around
    https://github.com/bitcoin/bitcoin/pull/27340#issuecomment-1484988445
    
    The only change should be that python3.7 is bumped to 3.8, but this is
    fine because ci/test/00_setup_env_native_qt5.sh still checks for
    python3.7 compatibility.

--- ci/test/00_setup_env_native_nowallet_libbitcoinkernel.sh
+++ ci/test/00_setup_env_native_nowallet_libbitcoinkernel.sh
@@ -7,10 +7,9 @@
 export LC_ALL=C.UTF-8
 
 export CONTAINER_NAME=ci_native_nowallet_libbitcoinkernel
-export CI_IMAGE_NAME_TAG=debian:buster
-# Use minimum supported python3.7 and clang-8, see doc/dependencies.md
-export PACKAGES="-t buster-backports python3-zmq clang-8 llvm-8 libc++abi-8-dev libc++-8-dev"
-export APPEND_APT_SOURCES_LIST="deb http://deb.debian.org/debian buster-backports main"
+export CI_IMAGE_NAME_TAG=ubuntu:focal
+# Use minimum supported python3.7 (or python3.8, as best-effort) and clang-8, see doc/dependencies.md
+export PACKAGES="python3-zmq clang-8 llvm-8 libc++abi-8-dev libc++-8-dev"
 export DEP_OPTS="NO_WALLET=1 CC=clang-8 CXX='clang++-8 -stdlib=libc++'"
 export GOAL="install"
 export NO_WERROR=1
