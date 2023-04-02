syscoin-core/syscoin

Merge #18952: test: avoid os-dependant path
8a22fd01140bd957036fc00419b147e4268ae9b1 avoided os-dependant path (Ferdinando M. Ametrano)

Pull request description:

  The current code fails on windows because of the forward slashes; using os.path.join solves the problem and it is in general more robust

ACKs for top commit:
  MarcoFalke:
    ACK 8a22fd01140bd957036fc00419b147e4268ae9b1

Tree-SHA512: 813f27aea33f97c8afac52e716a55fc5d7fb69621023aba99d40df7e1d145e0ec8d1eee49ddd403b219bf0e0e168e0e987b05c78eaef611f744d99bf2fc8bc91

test/functional/test_framework/test_framework.py
@@ -185,8 +185,18 @@ class SyscoinTestFramework(metaclass=SyscoinTestMetaClass):
         config = configparser.ConfigParser()
         config.read_file(open(self.options.configfile))
         self.config = config
-        self.options.syscoind = os.getenv("SYSCOIND", default=config["environment"]["BUILDDIR"] + '/src/syscoind' + config["environment"]["EXEEXT"])
-        self.options.syscoincli = os.getenv("SYSCOINCLI", default=config["environment"]["BUILDDIR"] + '/src/syscoin-cli' + config["environment"]["EXEEXT"])
+        fname_syscoind = os.path.join(
+            config["environment"]["BUILDDIR"],
+            "src",
+            "syscoind" + config["environment"]["EXEEXT"]
+        )
+        fname_syscoincli = os.path.join(
+            config["environment"]["BUILDDIR"],
+            "src",
+            "syscoin-cli" + config["environment"]["EXEEXT"]
+        )
+        self.options.syscoind = os.getenv("SYSCOIND", default=fname_syscoind)
+        self.options.syscoincli = os.getenv("SYSCOINCLI", default=fname_syscoincli)
 
         self.options.previous_releases_path = os.getenv("PREVIOUS_RELEASES_DIR") or os.getcwd() + "/releases"
 

bitcoin/bitcoin

test/functional/test_framework/test_framework.py
@@ -238,8 +238,18 @@ class BitcoinTestFramework(metaclass=BitcoinTestMetaClass):
 
         config = self.config
 
-        self.options.bitcoind = os.getenv("BITCOIND", default=config["environment"]["BUILDDIR"] + '/src/dashd' + config["environment"]["EXEEXT"])
-        self.options.bitcoincli = os.getenv("BITCOINCLI", default=config["environment"]["BUILDDIR"] + '/src/dash-cli' + config["environment"]["EXEEXT"])
+        fname_bitcoind = os.path.join(
+            config["environment"]["BUILDDIR"],
+            "src",
+            "dashd" + config["environment"]["EXEEXT"]
+        )
+        fname_bitcoincli = os.path.join(
+            config["environment"]["BUILDDIR"],
+            "src",
+            "dash-cli" + config["environment"]["EXEEXT"]
+        )
+        self.options.bitcoind = os.getenv("BITCOIND", default=fname_bitcoind)
+        self.options.bitcoincli = os.getenv("BITCOINCLI", default=fname_bitcoincli)
 
         self.extra_args_from_options = self.options.dashd_extra_args
 