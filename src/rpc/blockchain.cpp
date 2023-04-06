Bitcoin-ABC/bitcoin-abc

Merge #13424: Consistently validate txid / blockhash length and encoding in rpc calls
Summary:
5eb20f81d9 Consistently use ParseHashV to validate hash inputs in rpc (Ben Woosley)

Pull request description:

  ParseHashV validates the length and encoding of the string and throws
  an informative RPC error on failure, which is as good or better than
  these alternative calls.

  Note I switched ParseHashV to check string length first, because
  IsHex tests that the length is even, and an error like:
  "must be of length 64 (not 63, for X)" is much more informative than
  "must be hexadecimal string (not X)" in that case.

  Split from #13420

Tree-SHA512: f0786b41c0d7793ff76e4b2bb35547873070bbf7561d510029e8edb93f59176277efcd4d183b3185532ea69fc0bbbf3dbe9e19362e8017007ae9d51266cd78ae

Backport of Core [[https://github.com/bitcoin/bitcoin/pull/13424 | PR13424]]
https://github.com/bitcoin/bitcoin/pull/13424/

Depends on D5292

Test Plan:
  ninja check
  ninja check-functional

Reviewers: O1 Bitcoin ABC, #bitcoin_abc, Fabien

Reviewed By: O1 Bitcoin ABC, #bitcoin_abc, Fabien

Subscribers: Fabien

Differential Revision: https://reviews.bitcoinabc.org/D5294

src/rpc/blockchain.cpp
@@ -294,7 +294,7 @@ static UniValue waitforblock(const Config &config,
 
     int timeout = 0;
 
-    uint256 hash = uint256S(request.params[0].get_str());
+    BlockHash hash(ParseHashV(request.params[0], "blockhash"));
 
     if (!request.params[1].isNull()) {
         timeout = request.params[1].get_int();
@@ -820,8 +820,7 @@ static UniValue getblockheader(const Config &config,
                                              "\""));
     }
 
-    std::string strHash = request.params[0].get_str();
-    BlockHash hash(uint256S(strHash));
+    BlockHash hash(ParseHashV(request.params[0], "hash"));
 
     bool fVerbose = true;
     if (!request.params[1].isNull()) {
@@ -939,8 +938,7 @@ static UniValue getblock(const Config &config, const JSONRPCRequest &request) {
 
     LOCK(cs_main);
 
-    std::string strHash = request.params[0].get_str();
-    BlockHash hash(uint256S(strHash));
+    BlockHash hash(ParseHashV(request.params[0], "blockhash"));
 
     int verbosity = 1;
     if (!request.params[1].isNull()) {
@@ -1202,8 +1200,7 @@ UniValue gettxout(const Config &config, const JSONRPCRequest &request) {
 
     UniValue ret(UniValue::VOBJ);
 
-    std::string strTxId = request.params[0].get_str();
-    TxId txid(uint256S(strTxId));
+    TxId txid(ParseHashV(request.params[0], "txid"));
     int n = request.params[1].get_int();
     COutPoint out(txid, n);
     bool fMempool = true;
@@ -1634,8 +1631,7 @@ static UniValue preciousblock(const Config &config,
             HelpExampleRpc("preciousblock", "\"blockhash\""));
     }
 
-    std::string strHash = request.params[0].get_str();
-    BlockHash hash(uint256S(strHash));
+    BlockHash hash(ParseHashV(request.params[0], "blockhash"));
     CBlockIndex *pblockindex;
 
     {
@@ -1712,8 +1708,7 @@ static UniValue invalidateblock(const Config &config,
             HelpExampleRpc("invalidateblock", "\"blockhash\""));
     }
 
-    const std::string strHash = request.params[0].get_str();
-    const BlockHash hash(uint256S(strHash));
+    const BlockHash hash(ParseHashV(request.params[0], "blockhash"));
     CValidationState state;
 
     CBlockIndex *pblockindex;
@@ -1793,8 +1788,7 @@ static UniValue reconsiderblock(const Config &config,
             HelpExampleRpc("reconsiderblock", "\"blockhash\""));
     }
 
-    const std::string strHash = request.params[0].get_str();
-    const BlockHash hash(uint256S(strHash));
+    const BlockHash hash(ParseHashV(request.params[0], "blockhash"));
 
     {
         LOCK(cs_main);
@@ -1902,7 +1896,7 @@ static UniValue getchaintxstats(const Config &config,
         LOCK(cs_main);
         pindex = chainActive.Tip();
     } else {
-        BlockHash hash(uint256S(request.params[1].get_str()));
+        BlockHash hash(ParseHashV(request.params[1], "blockhash"));
         LOCK(cs_main);
         pindex = LookupBlockIndex(hash);
         if (!pindex) {
@@ -2077,8 +2071,7 @@ static UniValue getblockstats(const Config &config,
 
         pindex = chainActive[height];
     } else {
-        const std::string strHash = request.params[0].get_str();
-        const BlockHash hash(uint256S(strHash));
+        const BlockHash hash(ParseHashV(request.params[0], "hash_or_height"));
         pindex = LookupBlockIndex(hash);
         if (!pindex) {
             throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Block not found");

src/wallet/rpcwallet.cpp
@@ -2726,7 +2726,8 @@ static UniValue listsinceblock(const Config &config,
     isminefilter filter = ISMINE_SPENDABLE;
 
     if (!request.params[0].isNull() && !request.params[0].get_str().empty()) {
-        BlockHash blockId(uint256S(request.params[0].get_str()));
+        BlockHash blockId(ParseHashV(request.params[0], "blockhash"));
+
         paltindex = pindex = LookupBlockIndex(blockId);
         if (!pindex) {
             throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Block not found");
@@ -2908,8 +2909,7 @@ static UniValue gettransaction(const Config &config,
     auto locked_chain = pwallet->chain().lock();
     LOCK(pwallet->cs_wallet);
 
-    TxId txid;
-    txid.SetHex(request.params[0].get_str());
+    TxId txid(ParseHashV(request.params[0], "txid"));
 
     isminefilter filter = ISMINE_SPENDABLE;
     if (!request.params[1].isNull() && request.params[1].get_bool()) {
@@ -2987,8 +2987,7 @@ static UniValue abandontransaction(const Config &config,
     auto locked_chain = pwallet->chain().lock();
     LOCK(pwallet->cs_wallet);
 
-    TxId txid;
-    txid.SetHex(request.params[0].get_str());
+    TxId txid(ParseHashV(request.params[0], "txid"));
 
     if (!pwallet->mapWallet.count(txid)) {
         throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY,
@@ -3493,19 +3492,13 @@ static UniValue lockunspent(const Config &config,
                                {"vout", UniValueType(UniValue::VNUM)},
                            });
 
-        const std::string &strTxId = find_value(o, "txid").get_str();
-        if (!IsHex(strTxId)) {
-            throw JSONRPCError(RPC_INVALID_PARAMETER,
-                               "Invalid parameter, expected hex txid");
-        }
-
         const int nOutput = find_value(o, "vout").get_int();
         if (nOutput < 0) {
             throw JSONRPCError(RPC_INVALID_PARAMETER,
                                "Invalid parameter, vout must be positive");
         }
 
-        const TxId txid(uint256S(strTxId));
+        const TxId txid(ParseHashO(o, "txid"));
         const auto it = pwallet->mapWallet.find(txid);
         if (it == pwallet->mapWallet.end()) {
             throw JSONRPCError(RPC_INVALID_PARAMETER,

bitcoin/bitcoin

src/rpc/blockchain.cpp
@@ -374,7 +374,7 @@ static UniValue waitforblock(const JSONRPCRequest& request)
     }.Check(request);
     int timeout = 0;
 
-    uint256 hash = uint256S(request.params[0].get_str());
+    uint256 hash(ParseHashV(request.params[0], "blockhash"));
 
     if (!request.params[1].isNull())
         timeout = request.params[1].get_int();
@@ -634,7 +634,7 @@ static UniValue getmempoolancestors(const JSONRPCRequest& request)
     if (!request.params[1].isNull())
         fVerbose = request.params[1].get_bool();
 
-    uint256 hash = ParseHashV(request.params[0], "parameter 1");
+    uint256 hash(ParseHashV(request.params[0], "parameter 1"));
 
     const CTxMemPool& mempool = EnsureMemPool(request.context);
     LOCK(mempool.cs);
@@ -698,7 +698,7 @@ static UniValue getmempooldescendants(const JSONRPCRequest& request)
     if (!request.params[1].isNull())
         fVerbose = request.params[1].get_bool();
 
-    uint256 hash = ParseHashV(request.params[0], "parameter 1");
+    uint256 hash(ParseHashV(request.params[0], "parameter 1"));
 
     const CTxMemPool& mempool = EnsureMemPool(request.context);
     LOCK(mempool.cs);
@@ -749,7 +749,7 @@ static UniValue getmempoolentry(const JSONRPCRequest& request)
         },
     }.Check(request);
 
-    uint256 hash = ParseHashV(request.params[0], "parameter 1");
+    uint256 hash(ParseHashV(request.params[0], "parameter 1"));
 
     const CTxMemPool& mempool = EnsureMemPool(request.context);
     LOCK(mempool.cs);
@@ -862,8 +862,7 @@ static UniValue getblockheader(const JSONRPCRequest& request)
         },
     }.Check(request);
 
-    std::string strHash = request.params[0].get_str();
-    uint256 hash(uint256S(strHash));
+    uint256 hash(ParseHashV(request.params[0], "hash"));
 
     bool fVerbose = true;
     if (!request.params[1].isNull())
@@ -936,8 +935,7 @@ static UniValue getblockheaders(const JSONRPCRequest& request)
         },
     }.Check(request);
 
-    std::string strHash = request.params[0].get_str();
-    uint256 hash(uint256S(strHash));
+    uint256 hash(ParseHashV(request.params[0], "blockhash"));
 
     const CBlockIndex* pblockindex;
     const CBlockIndex* tip;
@@ -1048,8 +1046,7 @@ static UniValue getmerkleblocks(const JSONRPCRequest& request)
         throw JSONRPCError(RPC_INVALID_PARAMETER, "Filter is not within size constraints");
     }
 
-    std::string strHash = request.params[1].get_str();
-    uint256 hash(uint256S(strHash));
+    uint256 hash(ParseHashV(request.params[1], "blockhash"));
 
     const CBlockIndex* pblockindex = g_chainman.m_blockman.LookupBlockIndex(hash);
     if (!pblockindex) {
@@ -1154,8 +1151,7 @@ static UniValue getblock(const JSONRPCRequest& request)
                 },
     }.Check(request);
 
-    std::string strHash = request.params[0].get_str();
-    uint256 hash(uint256S(strHash));
+    uint256 hash(ParseHashV(request.params[0], "blockhash"));
 
     int verbosity = 1;
     if (!request.params[1].isNull()) {
@@ -1356,8 +1352,7 @@ static UniValue gettxout(const JSONRPCRequest& request)
 
     UniValue ret(UniValue::VOBJ);
 
-    std::string strHash = request.params[0].get_str();
-    uint256 hash(uint256S(strHash));
+    uint256 hash(ParseHashV(request.params[0], "txid"));
     int n = request.params[1].get_int();
     COutPoint out(hash, n);
     bool fMempool = true;
@@ -1805,8 +1800,7 @@ static UniValue preciousblock(const JSONRPCRequest& request)
         },
     }.Check(request);
 
-    std::string strHash = request.params[0].get_str();
-    uint256 hash(uint256S(strHash));
+    uint256 hash(ParseHashV(request.params[0], "blockhash"));
     CBlockIndex* pblockindex;
 
     {
@@ -1841,8 +1835,7 @@ static UniValue invalidateblock(const JSONRPCRequest& request)
         },
     }.Check(request);
 
-    std::string strHash = request.params[0].get_str();
-    uint256 hash(uint256S(strHash));
+    uint256 hash(ParseHashV(request.params[0], "blockhash"));
     CValidationState state;
 
     CBlockIndex* pblockindex;
@@ -1881,8 +1874,7 @@ static UniValue reconsiderblock(const JSONRPCRequest& request)
         },
     }.Check(request);
 
-    std::string strHash = request.params[0].get_str();
-    uint256 hash(uint256S(strHash));
+    uint256 hash(ParseHashV(request.params[0], "blockhash"));
 
     {
         LOCK(cs_main);
@@ -1937,7 +1929,7 @@ static UniValue getchaintxstats(const JSONRPCRequest& request)
         LOCK(cs_main);
         pindex = ::ChainActive().Tip();
     } else {
-        uint256 hash = uint256S(request.params[1].get_str());
+        uint256 hash(ParseHashV(request.params[1], "blockhash"));
         LOCK(cs_main);
         pindex = g_chainman.m_blockman.LookupBlockIndex(hash);
         if (!pindex) {
@@ -2114,8 +2106,7 @@ static UniValue getblockstats(const JSONRPCRequest& request)
 
         pindex = ::ChainActive()[height];
     } else {
-        const uint256 hash = ParseHashV(request.params[0], "parameter 1");
-
+        const uint256 hash(ParseHashV(request.params[0], "hash_or_height"));
         pindex = g_chainman.m_blockman.LookupBlockIndex(hash);
         if (!pindex) {
             throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Block not found");
@@ -2307,8 +2298,7 @@ static UniValue getspecialtxes(const JSONRPCRequest& request)
 
     LOCK(cs_main);
 
-    std::string strHash = request.params[0].get_str();
-    uint256 hash(uint256S(strHash));
+    uint256 hash(ParseHashV(request.params[0], "blockhash"));
 
     int nTxType = -1;
     if (!request.params[1].isNull()) {
@@ -2637,7 +2627,7 @@ static UniValue getblockfilter(const JSONRPCRequest& request)
         },
     }.Check(request);
 
-    uint256 block_hash = ParseHashV(request.params[0], "blockhash");
+    uint256 block_hash(ParseHashV(request.params[0], "blockhash"));
     std::string filtertype_name = "basic";
     if (!request.params[1].isNull()) {
         filtertype_name = request.params[1].get_str();

src/wallet/rpcwallet.cpp
@@ -1582,7 +1582,7 @@ static UniValue listsinceblock(const JSONRPCRequest& request)
 
     uint256 blockId;
     if (!request.params[0].isNull() && !request.params[0].get_str().empty()) {
-        blockId.SetHex(request.params[0].get_str());
+        blockId = ParseHashV(request.params[0], "blockhash");
         height = pwallet->chain().findFork(blockId, &altheight);
 
         if (!height) {
@@ -1707,8 +1707,7 @@ static UniValue gettransaction(const JSONRPCRequest& request)
 
     LOCK(pwallet->cs_wallet);
 
-    uint256 hash;
-    hash.SetHex(request.params[0].get_str());
+    uint256 hash(ParseHashV(request.params[0], "txid"));
 
     isminefilter filter = ISMINE_SPENDABLE;
 
@@ -1772,8 +1771,7 @@ static UniValue abandontransaction(const JSONRPCRequest& request)
 
     LOCK(pwallet->cs_wallet);
 
-    uint256 hash;
-    hash.SetHex(request.params[0].get_str());
+    uint256 hash(ParseHashV(request.params[0], "txid"));
 
     if (!pwallet->mapWallet.count(hash)) {
         throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid or non-wallet transaction id");
@@ -2176,17 +2174,13 @@ static UniValue lockunspent(const JSONRPCRequest& request)
                 {"vout", UniValueType(UniValue::VNUM)},
             });
 
-        const std::string& txid = find_value(o, "txid").get_str();
-        if (!IsHex(txid)) {
-            throw JSONRPCError(RPC_INVALID_PARAMETER, "Invalid parameter, expected hex txid");
-        }
-
+        const uint256 txid(ParseHashO(o, "txid"));
         const int nOutput = find_value(o, "vout").get_int();
         if (nOutput < 0) {
             throw JSONRPCError(RPC_INVALID_PARAMETER, "Invalid parameter, vout cannot be negative");
         }
 
-        const COutPoint outpt(uint256S(txid), nOutput);
+        const COutPoint outpt(txid, nOutput);
 
         const auto it = pwallet->mapWallet.find(outpt.hash);
         if (it == pwallet->mapWallet.end()) {