c40b1da2fd64bb10f120f85966b44f0d2bb315f8
fanquake <fanquake@gmail.com>
2023-04-14 10:45:34 +0100

    depends: fix compiling bdb with clang-16 on aarch64
    Compiling bdb with clang-16 on aarch64 (hardware) currently fails:
    ```bash
    make -C depends/ bdb CC=clang CXX=clang++
    ...
    checking for mutexes... UNIX/fcntl
    configure: WARNING: NO SHARED LATCH IMPLEMENTATION FOUND FOR THIS PLATFORM.
    configure: error: Unable to find a mutex implementation
    ```
    
    Looking at config.log we've got:
    ```bash
    configure:18704: checking for mutexes
    configure:18815: clang -o conftest -pipe -std=c11 -O2 -Wno-error=implicit-function-declaration -Wno-error=format-security    -I/bitcoin/depends/aarch64-unknown-linux-gnu/include -D_GNU_SOURCE -D_REENTRANT   -L/bitcoin/depends/aarch64-unknown-linux-gnu/lib conftest.c  -lpthread >&5
    conftest.c:45:1: error: type specifier missing, defaults to 'int'; ISO C99 and later do not support implicit int [-Wimplicit-int]
    main() {
    ^
    int
    conftest.c:50:2: warning: call to undeclared library function 'exit' with type 'void (int) __attribute__((noreturn))'; ISO C99 and later do not support implicit function declarations [-Wimplicit-function-declaration]
            exit (
            ^
    conftest.c:50:2: note: include the header <stdlib.h> or explicitly provide a declaration for 'exit'
    1 warning and 1 error generated.
    ```
    
    Clang-16 changed `-Wimplicit-function-declaration` and `-Wimplicit-int`
    warnings into errors, see:
    https://releases.llvm.org/16.0.0/tools/clang/docs/ReleaseNotes.html#potentially-breaking-changes.
    
    > The -Wimplicit-function-declaration and -Wimplicit-int warnings now
    > default to an error in C99, C11, and C17. As of C2x, support for implicit
    > function declarations and implicit int has been removed, and the
    > warning options will have no effect. Specifying -Wimplicit-int in
    > C89 mode will now issue warnings instead of being a noop.
    
    Github-Pull: #27462
    Rebased-From: f8b8458276983f8fc1e2a47c4d00c1e30633067d

--- depends/packages/bdb.mk
+++ depends/packages/bdb.mk
@@ -14,7 +14,7 @@ $(package)_config_opts_freebsd=--with-pic
 $(package)_config_opts_netbsd=--with-pic
 $(package)_config_opts_openbsd=--with-pic
 $(package)_config_opts_android=--with-pic
-$(package)_cflags+=-Wno-error=implicit-function-declaration -Wno-error=format-security
+$(package)_cflags+=-Wno-error=implicit-function-declaration -Wno-error=format-security -Wno-error=implicit-int
 $(package)_cppflags_mingw32=-DUNICODE -D_UNICODE
 endef
 
