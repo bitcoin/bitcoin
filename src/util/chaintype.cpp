bfc21c31b2186f7d30fc9a9ca7d6887ab61c6fb9
TheCharlatan <seb.kung@gmail.com>
2023-04-17 21:55:17 +0200

    refactor: Create chaintype files
    This is the first of a number of commits with the goal of moving the
    chain type definitions out of chainparamsbase to their own file and
    implementing them as enums instead of constant strings. The goal is to
    allow the kernel chainparams to no longer include chainparamsbase.
    
    The commit is part of an ongoing effort to decouple the libbitcoinkernel
    library from the ArgsManager and other functionality that should not be
    part of the kernel library.

---
+++ src/util/chaintype.cpp
@@ -0,0 +1,39 @@
+// Copyright (c) 2023 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <util/chaintype.h>
+
+#include <cassert>
+#include <optional>
+#include <string>
+
+std::string ChainTypeToString(ChainType chain)
+{
+    switch (chain) {
+    case ChainType::MAIN:
+        return "main";
+    case ChainType::TESTNET:
+        return "test";
+    case ChainType::SIGNET:
+        return "signet";
+    case ChainType::REGTEST:
+        return "regtest";
+    }
+    assert(false);
+}
+
+std::optional<ChainType> ChainTypeFromString(std::string_view chain)
+{
+    if (chain == "main") {
+        return ChainType::MAIN;
+    } else if (chain == "test") {
+        return ChainType::TESTNET;
+    } else if (chain == "signet") {
+        return ChainType::SIGNET;
+    } else if (chain == "regtest") {
+        return ChainType::REGTEST;
+    } else {
+        return std::nullopt;
+    }
+}
