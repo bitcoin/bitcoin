9a77d84a56906c80412d60d3eb7065b1f436fc94
glozow <gloriajzhao@gmail.com>
2023-04-18 16:02:58 +0100

    [txorphanage] support multiple announcers
    Add ability to add and track multiple announcers per orphan transaction,
    erasing announcers but not the entire orphan.
    
    The tx creation code in orphanage_tests needs to be updated so that each
    tx is unique, because the CountOrphans() check assumes that calling
    EraseForPeer necessarily means its orphans are deleted.
    
    Unused for now.

--- src/test/orphanage_tests.cpp
+++ src/test/orphanage_tests.cpp
@@ -133,7 +133,7 @@ BOOST_AUTO_TEST_CASE(DoS_mapOrphans)
         tx.vin[0].prevout.hash = Txid::FromUint256(m_rng.rand256());
         tx.vin[0].scriptSig << OP_1;
         tx.vout.resize(1);
-        tx.vout[0].nValue = 1*CENT;
+        tx.vout[0].nValue = i*CENT;
         tx.vout[0].scriptPubKey = GetScriptForDestination(PKHash(key.GetPubKey()));
 
         AddToOrphanage(orphanage, MakeTransactionRef(tx), i);
@@ -149,7 +149,7 @@ BOOST_AUTO_TEST_CASE(DoS_mapOrphans)
         tx.vin[0].prevout.n = 0;
         tx.vin[0].prevout.hash = txPrev->GetHash();
         tx.vout.resize(1);
-        tx.vout[0].nValue = 1*CENT;
+        tx.vout[0].nValue = i*CENT;
         tx.vout[0].scriptPubKey = GetScriptForDestination(PKHash(key.GetPubKey()));
         SignatureData empty;
         BOOST_CHECK(SignSignature(keystore, *txPrev, tx, 0, SIGHASH_ALL, empty));
