88881cf7ac029aea660c2413ca8e2a5136fcd41b
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2023-04-18 14:12:35 +0200

    Bump python minimum version to 3.8
    Also, switch ci_native_qt5 to g++-9 (from g++-8) to work around bugs.
    This should be fine, because the i686_centos task still checks for g++-8
    compatibility.
    
    See
    https://github.com/bitcoin/bitcoin/pull/27483#issuecomment-1513477050
    for the list of bugs.

--- ci/test/00_setup_env_native_nowallet_libbitcoinkernel.sh
+++ ci/test/00_setup_env_native_nowallet_libbitcoinkernel.sh
@@ -7,8 +7,8 @@
 export LC_ALL=C.UTF-8
 
 export CONTAINER_NAME=ci_native_nowallet_libbitcoinkernel
-export CI_IMAGE_NAME_TAG=ubuntu:focal
-# Use minimum supported python3.7 (or python3.8, as best-effort) and clang-8, see doc/dependencies.md
+export CI_IMAGE_NAME_TAG="ubuntu:20.04"
+# Use minimum supported python3.8 and clang-8, see doc/dependencies.md
 export PACKAGES="python3-zmq clang-8 llvm-8 libc++abi-8-dev libc++-8-dev"
 export DEP_OPTS="NO_WALLET=1 CC=clang-8 CXX='clang++-8 -stdlib=libc++'"
 export GOAL="install"
