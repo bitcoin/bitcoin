3168b08043546cd248a81563e21ff096019f1521
Pieter Wuille <pieter@wuille.net>
2023-04-18 12:16:45 -0400

    Bench test for EllSwift ECDH
    Co-authored-by: Dhruv Mehta <856960+dhruv@users.noreply.github.com>

--- src/random.cpp
+++ src/random.cpp
@@ -599,6 +599,12 @@ std::vector<unsigned char> FastRandomContext::randbytes(size_t len)
     return ret;
 }
 
+void FastRandomContext::fillrand(Span<std::byte> output)
+{
+    if (requires_seed) RandomSeed();
+    rng.Keystream(UCharCast(output.data()), output.size());
+}
+
 FastRandomContext::FastRandomContext(const uint256& seed) noexcept : requires_seed(false), bitbuf_size(0)
 {
     rng.SetKey32(seed.begin());
