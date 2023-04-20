0e7f457fa6ee7d61e9c88a5cfb7dcba36f29a3bf
Martin Zumsande <mzumsande@gmail.com>
2023-04-20 14:41:45 -0400

    net, refactor: Move DNS seed querying logic into its own function

--- src/net.h
+++ src/net.h
@@ -1324,6 +1324,7 @@ private:
 
     void ThreadSocketHandler() EXCLUSIVE_LOCKS_REQUIRED(!m_total_bytes_sent_mutex, !mutexMsgProc, !m_nodes_mutex, !m_reconnections_mutex);
     void ThreadAddressSeed() EXCLUSIVE_LOCKS_REQUIRED(!m_addr_fetches_mutex, !m_nodes_mutex);
+    void QueryDNSSeeds() EXCLUSIVE_LOCKS_REQUIRED(!m_addr_fetches_mutex, !m_nodes_mutex);
 
     uint64_t CalculateKeyedNetGroup(const CAddress& ad) const;
 
