furszy/bitcoin-core

wallet: pass 'WalletBatch' to 'CWallet::LoadToWallet'
So we stop creating and re-creating internal wallet batch objects
every time that a new transaction is loaded into the wallet.

src/wallet/salvage.cpp
@@ -136,6 +136,7 @@ bool RecoverDatabaseFile(const ArgsManager& args, const fs::path& file_path, bil
 
     DbTxn* ptxn = env->TxnBegin();
     CWallet dummyWallet(nullptr, "", gArgs, CreateDummyWalletDatabase());
+    std::unique_ptr<WalletBatch> batch = std::make_unique<WalletBatch>(dummyWallet.GetDatabase());
     for (KeyValPair& row : salvagedData)
     {
         /* Filter for only private key type KV pairs to be added to the salvaged wallet */
@@ -146,7 +147,7 @@ bool RecoverDatabaseFile(const ArgsManager& args, const fs::path& file_path, bil
         {
             // Required in LoadKeyMetadata():
             LOCK(dummyWallet.cs_wallet);
-            fReadOK = ReadKeyValue(&dummyWallet, ssKey, ssValue, strType, strErr, KeyFilter);
+            fReadOK = ReadKeyValue(&dummyWallet, *batch, ssKey, ssValue, strType, strErr, KeyFilter);
         }
         if (!KeyFilter(strType)) {
             continue;
@@ -162,6 +163,7 @@ bool RecoverDatabaseFile(const ArgsManager& args, const fs::path& file_path, bil
         if (ret2 > 0)
             fSuccess = false;
     }
+    batch.release();
     ptxn->commit(0);
     pdbCopy->close(0);
 

src/wallet/walletdb.cpp
@@ -318,7 +318,7 @@ public:
 };
 
 static bool
-ReadKeyValue(CWallet* pwallet, CDataStream& ssKey, CDataStream& ssValue,
+ReadKeyValue(CWallet* pwallet, WalletBatch& batch, CDataStream& ssKey, CDataStream& ssValue,
              CWalletScanState &wss, std::string& strType, std::string& strErr, const KeyFilterFn& filter_fn = nullptr) EXCLUSIVE_LOCKS_REQUIRED(pwallet->cs_wallet)
 {
     try {
@@ -383,7 +383,7 @@ ReadKeyValue(CWallet* pwallet, CDataStream& ssKey, CDataStream& ssValue,
 
                 return true;
             };
-            if (!pwallet->LoadToWallet(hash, fill_wtx)) {
+            if (!pwallet->LoadToWallet(batch, hash, fill_wtx)) {
                 return false;
             }
         } else if (strType == DBKeys::WATCHS) {
@@ -745,11 +745,11 @@ ReadKeyValue(CWallet* pwallet, CDataStream& ssKey, CDataStream& ssValue,
     return true;
 }
 
-bool ReadKeyValue(CWallet* pwallet, CDataStream& ssKey, CDataStream& ssValue, std::string& strType, std::string& strErr, const KeyFilterFn& filter_fn)
+bool ReadKeyValue(CWallet* pwallet, WalletBatch& batch, CDataStream& ssKey, CDataStream& ssValue, std::string& strType, std::string& strErr, const KeyFilterFn& filter_fn)
 {
     CWalletScanState dummy_wss;
     LOCK(pwallet->cs_wallet);
-    return ReadKeyValue(pwallet, ssKey, ssValue, dummy_wss, strType, strErr, filter_fn);
+    return ReadKeyValue(pwallet, batch, ssKey, ssValue, dummy_wss, strType, strErr, filter_fn);
 }
 
 bool WalletBatch::IsKeyType(const std::string& strType)
@@ -817,7 +817,7 @@ DBErrors WalletBatch::LoadWallet(CWallet* pwallet)
 
             // Try to be tolerant of single corrupt records:
             std::string strType, strErr;
-            if (!ReadKeyValue(pwallet, ssKey, ssValue, wss, strType, strErr))
+            if (!ReadKeyValue(pwallet, *this, ssKey, ssValue, wss, strType, strErr))
             {
                 // losing keys is considered a catastrophic error, anything else
                 // we assume the user can live with:

src/wallet/walletdb.h
@@ -295,7 +295,7 @@ void MaybeCompactWalletDB(WalletContext& context);
 using KeyFilterFn = std::function<bool(const std::string&)>;
 
 //! Unserialize a given Key-Value pair and load it into the wallet
-bool ReadKeyValue(CWallet* pwallet, CDataStream& ssKey, CDataStream& ssValue, std::string& strType, std::string& strErr, const KeyFilterFn& filter_fn = nullptr);
+bool ReadKeyValue(CWallet* pwallet, WalletBatch& batch, CDataStream& ssKey, CDataStream& ssValue, std::string& strType, std::string& strErr, const KeyFilterFn& filter_fn = nullptr);
 
 /** Return object for accessing dummy database with no read/write capabilities. */
 std::unique_ptr<WalletDatabase> CreateDummyWalletDatabase();

bitcoin/bitcoin

src/wallet/salvage.cpp
@@ -136,6 +136,7 @@ bool RecoverDatabaseFile(const ArgsManager& args, const fs::path& file_path, bil
 
     DbTxn* ptxn = env->TxnBegin();
     CWallet dummyWallet(nullptr, "", CreateDummyWalletDatabase());
+    std::unique_ptr<WalletBatch> batch = std::make_unique<WalletBatch>(dummyWallet.GetDatabase());
     for (KeyValPair& row : salvagedData)
     {
         /* Filter for only private key type KV pairs to be added to the salvaged wallet */
@@ -146,7 +147,7 @@ bool RecoverDatabaseFile(const ArgsManager& args, const fs::path& file_path, bil
         {
             // Required in LoadKeyMetadata():
             LOCK(dummyWallet.cs_wallet);
-            fReadOK = ReadKeyValue(&dummyWallet, ssKey, ssValue, strType, strErr, KeyFilter);
+            fReadOK = ReadKeyValue(&dummyWallet, *batch, ssKey, ssValue, strType, strErr, KeyFilter);
         }
         if (!KeyFilter(strType)) {
             continue;
@@ -162,6 +163,7 @@ bool RecoverDatabaseFile(const ArgsManager& args, const fs::path& file_path, bil
         if (ret2 > 0)
             fSuccess = false;
     }
+    batch.reset();
     ptxn->commit(0);
     pdbCopy->close(0);
 

src/wallet/walletdb.cpp
@@ -321,7 +321,7 @@ public:
 };
 
 static bool
-ReadKeyValue(CWallet* pwallet, DataStream& ssKey, CDataStream& ssValue,
+ReadKeyValue(CWallet* pwallet, WalletBatch& batch, DataStream& ssKey, CDataStream& ssValue,
              CWalletScanState &wss, std::string& strType, std::string& strErr, const KeyFilterFn& filter_fn = nullptr) EXCLUSIVE_LOCKS_REQUIRED(pwallet->cs_wallet)
 {
     try {
@@ -397,7 +397,7 @@ ReadKeyValue(CWallet* pwallet, DataStream& ssKey, CDataStream& ssValue,
 
                 return true;
             };
-            if (!pwallet->LoadToWallet(hash, fill_wtx)) {
+            if (!pwallet->LoadToWallet(batch, hash, fill_wtx)) {
                 return false;
             }
         } else if (strType == DBKeys::WATCHS) {
@@ -765,11 +765,11 @@ ReadKeyValue(CWallet* pwallet, DataStream& ssKey, CDataStream& ssValue,
     return true;
 }
 
-bool ReadKeyValue(CWallet* pwallet, DataStream& ssKey, CDataStream& ssValue, std::string& strType, std::string& strErr, const KeyFilterFn& filter_fn)
+bool ReadKeyValue(CWallet* pwallet, WalletBatch& batch, DataStream& ssKey, CDataStream& ssValue, std::string& strType, std::string& strErr, const KeyFilterFn& filter_fn)
 {
     CWalletScanState dummy_wss;
     LOCK(pwallet->cs_wallet);
-    return ReadKeyValue(pwallet, ssKey, ssValue, dummy_wss, strType, strErr, filter_fn);
+    return ReadKeyValue(pwallet, batch, ssKey, ssValue, dummy_wss, strType, strErr, filter_fn);
 }
 
 bool WalletBatch::IsKeyType(const std::string& strType)
@@ -841,7 +841,7 @@ DBErrors WalletBatch::LoadWallet(CWallet* pwallet)
 
             // Try to be tolerant of single corrupt records:
             std::string strType, strErr;
-            if (!ReadKeyValue(pwallet, ssKey, ssValue, wss, strType, strErr))
+            if (!ReadKeyValue(pwallet, *this, ssKey, ssValue, wss, strType, strErr))
             {
                 if (wss.unexpected_legacy_entry) {
                     strErr = strprintf("Error: Unexpected legacy entry found in descriptor wallet %s. ", pwallet->GetName());

src/wallet/walletdb.h
@@ -303,7 +303,7 @@ void MaybeCompactWalletDB(WalletContext& context);
 using KeyFilterFn = std::function<bool(const std::string&)>;
 
 //! Unserialize a given Key-Value pair and load it into the wallet
-bool ReadKeyValue(CWallet* pwallet, DataStream& ssKey, CDataStream& ssValue, std::string& strType, std::string& strErr, const KeyFilterFn& filter_fn = nullptr);
+bool ReadKeyValue(CWallet* pwallet, WalletBatch& batch, DataStream& ssKey, CDataStream& ssValue, std::string& strType, std::string& strErr, const KeyFilterFn& filter_fn = nullptr);
 
 /** Return object for accessing dummy database with no read/write capabilities. */
 std::unique_ptr<WalletDatabase> CreateDummyWalletDatabase();