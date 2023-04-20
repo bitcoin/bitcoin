furszy/bitcoin-core

wallet: re-use existent WalletBatch for 'AddWalletFlags' and 'SetMinVersion'

src/wallet/wallet.cpp
@@ -1495,12 +1495,12 @@ bool CWallet::LoadWalletFlags(uint64_t flags)
     return true;
 }
 
-bool CWallet::AddWalletFlags(uint64_t flags)
+bool CWallet::AddWalletFlags(uint64_t flags, WalletBatch& batch)
 {
     LOCK(cs_wallet);
     // We should never be writing unknown non-tolerable wallet flags
     assert(((flags & KNOWN_WALLET_FLAGS) >> 32) == (flags >> 32));
-    if (!WalletBatch(GetDatabase()).WriteWalletFlags(flags)) {
+    if (!batch.WriteWalletFlags(flags)) {
         throw std::runtime_error(std::string(__func__) + ": writing wallet flags failed");
     }
 
@@ -2738,10 +2738,11 @@ std::shared_ptr<CWallet> CWallet::Create(WalletContext& context, const std::stri
                      !walletInstance->IsWalletFlagSet(WALLET_FLAG_BLANK_WALLET);
     if (fFirstRun)
     {
-        // ensure this wallet.dat can only be opened by clients supporting HD with chain split and expects no default key
-        walletInstance->SetMinVersion(FEATURE_LATEST);
+        WalletBatch batch(walletInstance->GetDatabase());
 
-        walletInstance->AddWalletFlags(wallet_creation_flags);
+        // ensure this wallet.dat can only be opened by clients supporting HD with chain split and expects no default key
+        walletInstance->SetMinVersion(FEATURE_LATEST, &batch);
+        walletInstance->AddWalletFlags(wallet_creation_flags, batch);
 
         // Only create LegacyScriptPubKeyMan when not descriptor wallet
         if (!walletInstance->IsWalletFlagSet(WALLET_FLAG_DESCRIPTORS)) {

src/wallet/wallettool.cpp
@@ -33,8 +33,9 @@ static void WalletCreate(CWallet* wallet_instance, uint64_t wallet_creation_flag
 {
     LOCK(wallet_instance->cs_wallet);
 
-    wallet_instance->SetMinVersion(FEATURE_LATEST);
-    wallet_instance->AddWalletFlags(wallet_creation_flags);
+    WalletBatch batch(wallet_instance->GetDatabase());
+    wallet_instance->SetMinVersion(FEATURE_LATEST, &batch);
+    wallet_instance->AddWalletFlags(wallet_creation_flags, batch);
 
     if (!wallet_instance->IsWalletFlagSet(WALLET_FLAG_DESCRIPTORS)) {
         auto spk_man = wallet_instance->GetOrCreateLegacyScriptPubKeyMan();

bitcoin/bitcoin

src/wallet/wallet.cpp
@@ -1613,7 +1613,7 @@ bool CWallet::LoadWalletFlags(uint64_t flags)
     return true;
 }
 
-void CWallet::InitWalletFlags(uint64_t flags)
+void CWallet::InitWalletFlags(uint64_t flags, WalletBatch& batch)
 {
     LOCK(cs_wallet);
 
@@ -1622,7 +1622,7 @@ void CWallet::InitWalletFlags(uint64_t flags)
     // This should only be used once, when creating a new wallet - so current flags are expected to be blank
     assert(m_wallet_flags == 0);
 
-    if (!WalletBatch(GetDatabase()).WriteWalletFlags(flags)) {
+    if (!batch.WriteWalletFlags(flags)) {
         throw std::runtime_error(std::string(__func__) + ": writing wallet flags failed");
     }
 
@@ -2964,10 +2964,12 @@ std::shared_ptr<CWallet> CWallet::Create(WalletContext& context, const std::stri
                      !walletInstance->IsWalletFlagSet(WALLET_FLAG_BLANK_WALLET);
     if (fFirstRun)
     {
-        // ensure this wallet.dat can only be opened by clients supporting HD with chain split and expects no default key
-        walletInstance->SetMinVersion(FEATURE_LATEST);
+        WalletBatch batch(walletInstance->GetDatabase());
 
-        walletInstance->InitWalletFlags(wallet_creation_flags);
+        // ensure this wallet.dat can only be opened by clients supporting HD with chain split and expects no default key
+        walletInstance->SetMinVersion(FEATURE_LATEST, &batch);
+
+        walletInstance->InitWalletFlags(wallet_creation_flags, batch);
 
         // Only create LegacyScriptPubKeyMan when not descriptor wallet
         if (!walletInstance->IsWalletFlagSet(WALLET_FLAG_DESCRIPTORS)) {

src/wallet/wallettool.cpp
@@ -33,8 +33,9 @@ static void WalletCreate(CWallet* wallet_instance, uint64_t wallet_creation_flag
 {
     LOCK(wallet_instance->cs_wallet);
 
-    wallet_instance->SetMinVersion(FEATURE_LATEST);
-    wallet_instance->InitWalletFlags(wallet_creation_flags);
+    WalletBatch batch(wallet_instance->GetDatabase());
+    wallet_instance->SetMinVersion(FEATURE_LATEST, &batch);
+    wallet_instance->InitWalletFlags(wallet_creation_flags, batch);
 
     if (!wallet_instance->IsWalletFlagSet(WALLET_FLAG_DESCRIPTORS)) {
         auto spk_man = wallet_instance->GetOrCreateLegacyScriptPubKeyMan();