bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

validation: add ChainstateManager::GetAllForBlockDownload()
For use in subsequent changes to net_processing.

src/validation.cpp
@@ -4842,6 +4842,28 @@ std::vector<CChainState*> ChainstateManager::GetAll()
     return out;
 }
 
+std::vector<CChainState*> ChainstateManager::GetAllForBlockDownload()
+{
+    AssertLockHeld(::cs_main);
+    std::vector<CChainState*> out;
+
+    bool snapshot_in_ibd{
+        m_snapshot_chainstate && m_snapshot_chainstate->IsInitialBlockDownload()};
+
+    if (m_snapshot_chainstate) {
+        out.push_back(m_snapshot_chainstate.get());
+    }
+    // Exclude the ibd chainstate (which is in the background if we have a snapshot
+    // chainstate active) if the snapshot chainstate is in IBD because the main
+    // priority is getting the active chain to network tip.
+    if (!IsSnapshotValidated() && !snapshot_in_ibd && m_ibd_chainstate) {
+        out.push_back(m_ibd_chainstate.get());
+    }
+
+    assert(out.size() > 0);
+    return out;
+}
+
 CChainState& ChainstateManager::InitializeChainstate(
     CTxMemPool* mempool, const std::optional<uint256>& snapshot_blockhash)
 {

bitcoin/bitcoin

src/validation.cpp
@@ -5010,6 +5010,22 @@ std::vector<Chainstate*> ChainstateManager::GetAll()
     return out;
 }
 
+std::vector<Chainstate*> ChainstateManager::GetAllForBlockDownload() const
+{
+    AssertLockHeld(::cs_main);
+    std::vector<Chainstate*> out;
+
+    if (IsUsable(m_snapshot_chainstate.get())) {
+        out.push_back(m_snapshot_chainstate.get());
+    }
+    if (!IsSnapshotValidated() && m_ibd_chainstate) {
+        out.push_back(m_ibd_chainstate.get());
+    }
+
+    assert(out.size() > 0);
+    return out;
+}
+
 Chainstate& ChainstateManager::InitializeChainstate(CTxMemPool* mempool)
 {
     AssertLockHeld(::cs_main);