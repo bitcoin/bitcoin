fb61bc0c022cc0fff290b94ee4f9cf9f4160efe2
Cory Fields <cory-nospam-@coryfields.com>
2023-05-10 22:34:06 +0000

    depends: Bump MacOS minimum runtime requirement to 11.0
    This is necessary as the new fixup_chains linker behavior is only valid
    when the runtime target is >=11.0.

--- contrib/devtools/symbol-check.py
+++ contrib/devtools/symbol-check.py
@@ -232,7 +232,7 @@ def check_MACHO_libraries(binary) -> bool:
     return ok
 
 def check_MACHO_min_os(binary) -> bool:
-    if binary.build_version.minos == [10,15,0]:
+    if binary.build_version.minos == [11,0,0]:
         return True
     return False
 
