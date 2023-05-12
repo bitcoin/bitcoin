1dd62c5295ca319c94f7233bcb2e11f9d37a33f1
Jon Atack <jon@atack.com>
2023-05-11 18:35:54 -0700

    refactor: move GetServicesNames from rpc/util.{h,cpp} to rpc/net.cpp
    as it is only called from that compilation unit.
    
    This avoids needlessly compiling GetServicesNames() in the 35 other files that
    include rpc/util.h.

--- src/rpc/util.h
+++ src/rpc/util.h
@@ -132,9 +132,6 @@ std::pair<int64_t, int64_t> ParseDescriptorRange(const UniValue& value);
 /** Evaluate a descriptor given as a string, or as a {"desc":...,"range":...} object, with default range of 1000. */
 std::vector<CScript> EvalDescriptorStringOrObject(const UniValue& scanobject, FlatSigningProvider& provider, const bool expand_priv = false);
 
-/** Returns, given services flags, a list of humanly readable (known) network services */
-UniValue GetServicesNames(ServiceFlags services);
-
 /**
  * Serializing JSON objects depends on the outer type. Only arrays and
  * dictionaries can be nested in json. The top-level outer type is "NONE".
