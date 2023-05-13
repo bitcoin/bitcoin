ef65f610bb3718ac3bdcc61cbd1fed4684e6caf4
Kittywhiskers Van Gogh <63189531+kittywhiskers@users.noreply.github.com>
2023-05-13 15:32:47 +0000

    merge bitcoin#21871: add checks for minimum required OS versions

--- contrib/guix/manifest.scm
+++ contrib/guix/manifest.scm
@@ -583,7 +583,7 @@ inspecting signatures in Mach-O binaries.")
 (define-public lief
   (package
    (name "python-lief")
-   (version "0.11.4")
+   (version "0.11.5")
    (source
     (origin
      (method git-fetch)
@@ -593,7 +593,7 @@ inspecting signatures in Mach-O binaries.")
      (file-name (git-file-name name version))
      (sha256
       (base32
-       "0h4kcwr9z478almjqhmils8imfpflzk0r7d05g4xbkdyknn162qf"))))
+       "0qahjfg1n0x76ps2mbyljvws1l3qhkqvmxqbahps4qgywl2hbdkj"))))
    (build-system python-build-system)
    (native-inputs
     `(("cmake" ,cmake)))
