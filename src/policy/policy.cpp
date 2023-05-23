66d8be9f0d85843be98dc51b58a9d85d474ff03b
Jonathan Bier <42411042+jonathanbier@users.noreply.github.com>
2023-05-23 10:56:24 +0100

    doc: update comment in policy.cpp to refer to vbytes
    doc: update comment in policy.cpp to refer to virtual bytes

--- src/policy/policy.cpp
+++ src/policy/policy.cpp
@@ -35,7 +35,7 @@ CAmount GetDustThreshold(const CTxOut& txout, const CFeeRate& dustRelayFeeIn)
     // 182*dustRelayFee/1000 (in satoshis).
     // 546 satoshis at the default rate of 3000 sat/kvB.
     // A typical spendable segwit P2WPKH txout is 31 bytes big, and will
-    // need a CTxIn of at least 67 bytes to spend:
+    // need a CTxIn of at least 67 vbytes to spend:
     // so dust is a spendable txout less than
     // 98*dustRelayFee/1000 (in satoshis).
     // 294 satoshis at the default rate of 3000 sat/kvB.
