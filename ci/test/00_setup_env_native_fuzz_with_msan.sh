d3cbcbf62693ad7394b3f8693b1c08d4271903fa
fanquake <fanquake@gmail.com>
2023-05-23 18:09:32 +0100

    ci: compile clang and compiler-rt in MSAN jobs
    This works around https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=1005341.

--- ci/test/00_setup_env_native_fuzz_with_msan.sh
+++ ci/test/00_setup_env_native_fuzz_with_msan.sh
@@ -7,13 +7,13 @@
 export LC_ALL=C.UTF-8
 
 export CI_IMAGE_NAME_TAG="ubuntu:23.04" # Version 23.04 will reach EOL in Jan 2024, and can be replaced by "ubuntu:24.04" (or anything else that ships the wanted clang version).
-LIBCXX_DIR="${BASE_SCRATCH_DIR}/msan/build/"
+LIBCXX_DIR="${BASE_SCRATCH_DIR}/msan/cxx_build/"
 export MSAN_FLAGS="-fsanitize=memory -fsanitize-memory-track-origins=2 -fno-omit-frame-pointer -g -O1 -fno-optimize-sibling-calls"
 LIBCXX_FLAGS="-nostdinc++ -nostdlib++ -isystem ${LIBCXX_DIR}include/c++/v1 -L${LIBCXX_DIR}lib -Wl,-rpath,${LIBCXX_DIR}lib -lc++ -lc++abi -lpthread -Wno-unused-command-line-argument"
 export MSAN_AND_LIBCXX_FLAGS="${MSAN_FLAGS} ${LIBCXX_FLAGS}"
 
 export CONTAINER_NAME="ci_native_fuzz_msan"
-export PACKAGES="clang-16 llvm-16 libclang-rt-16-dev cmake"
+export PACKAGES="cmake ninja-build"
 # BDB generates false-positives and will be removed in future
 export DEP_OPTS="NO_BDB=1 NO_QT=1 CC=clang CXX=clang++ CFLAGS='${MSAN_FLAGS}' CXXFLAGS='${MSAN_AND_LIBCXX_FLAGS}'"
 export GOAL="install"
