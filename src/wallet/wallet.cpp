d2bdc7bd4edcf6d86d88de69274d975405ba9fa3
alex v <alex@nav.community>
2023-05-26 15:08:13 +0200

    m_blsct_key_manager

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -3549,8 +3549,7 @@ void CWallet::SetupBLSCTKeyMan()
     if (m_blsct_key_manager != nullptr) {
         return;
     }
-    auto spk_manager = std::unique_ptr<blsct::KeyMan>(new blsct::KeyMan(*this));
-    m_blsct_key_manager = std::move(spk_manager.get());
+    m_blsct_key_manager = std::move(new blsct::KeyMan(*this));
 }
 
 const CKeyingMaterial& CWallet::GetEncryptionKey() const
