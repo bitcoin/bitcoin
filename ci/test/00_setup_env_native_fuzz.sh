fa3ab4520317f48d4700b81dab023c4e639bbd68
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2023-05-30 12:04:22 +0200

    ci: Enable float-divide-by-zero check

--- ci/test/00_setup_env_native_fuzz.sh
+++ ci/test/00_setup_env_native_fuzz.sh
@@ -14,5 +14,6 @@ export RUN_UNIT_TESTS=false
 export RUN_FUNCTIONAL_TESTS=false
 export RUN_FUZZ_TESTS=true
 export GOAL="install"
-export BITCOIN_CONFIG="--enable-fuzz --with-sanitizers=fuzzer,address,undefined,integer CC='clang-16 -ftrivial-auto-var-init=pattern' CXX='clang++-16 -ftrivial-auto-var-init=pattern'"
+export BITCOIN_CONFIG="--enable-fuzz --with-sanitizers=fuzzer,address,undefined,float-divide-by-zero,integer \
+CC='clang-16 -ftrivial-auto-var-init=pattern' CXX='clang++-16 -ftrivial-auto-var-init=pattern'"
 export CCACHE_SIZE=200M
