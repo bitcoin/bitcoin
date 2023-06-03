101cb674336740ad6f106a7a1a38d586b7fc921f
UdjinM6 <UdjinM6@users.noreply.github.com>
2023-06-03 14:32:01 +0300

    fix: move test-security-check call

--- ci/dash/test_unittests.sh
+++ ci/dash/test_unittests.sh
@@ -11,10 +11,6 @@ set -e
 
 source ./ci/dash/matrix.sh
 
-if [ "$RUN_SECURITY_TESTS" = "true" ]; then
-  make test-security-check
-fi
-
 if [ "$RUN_UNIT_TESTS" != "true" ] && [ "$RUN_UNIT_TESTS_SEQUENTIAL" != "true" ]; then
   echo "Skipping unit tests"
   exit 0
