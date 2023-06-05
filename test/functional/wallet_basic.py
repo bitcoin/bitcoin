9f79664efe4f963fe8bf1584cbee09f6787824af
alex v <alex@encrypt-s.com>
2023-06-05 22:15:15 +0200

    fix wallet_basic

--- test/functional/wallet_basic.py
+++ test/functional/wallet_basic.py
@@ -29,7 +29,7 @@ class WalletTest(BitcoinTestFramework):
     def set_test_params(self):
         self.num_nodes = 4
         self.extra_args = [[
-            "-dustrelayfee=0", "-walletrejectlongchains=0", "-whitelist=noban@127.0.0.1"
+            "-dustrelayfee=0", "-walletrejectlongchains=0", "-whitelist=noban@127.0.0.1", "-addresstype=bech32"
         ]] * self.num_nodes
         self.setup_clean_chain = True
         self.supports_cli = False
