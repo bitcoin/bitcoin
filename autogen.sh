264441ac39ede255cfb0e4bc3932112a139abbd9
romanornr <romanornr@gmail.com>
2023-06-14 17:50:29 +0200

    autogen.sh: refactor

--- autogen.sh
+++ autogen.sh
@@ -3,14 +3,64 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-export LC_ALL=C
+# Set error handling
 set -e
-srcdir="$(dirname "$0")"
-cd "$srcdir"
-if [ -z "${LIBTOOLIZE}" ] && GLIBTOOLIZE="$(command -v glibtoolize)"; then
-  LIBTOOLIZE="${GLIBTOOLIZE}"
-  export LIBTOOLIZE
-fi
-command -v autoreconf >/dev/null || \
-  (echo "configuration failed, please install autoconf first" && exit 1)
-autoreconf --install --force --warnings=all
+
+# If verbose is non-zero, print messages
+verbose=1
+
+# Sets environment variables and navigates to the source directory
+set_environment_variables() {
+  export LC_ALL=C
+  srcdir="$(dirname "$0")"
+  cd "$srcdir"
+}
+
+# Check for libtoolize and set LIBTOOLIZE variable
+check_libtoolize() {
+  if [ -z "${LIBTOOLIZE}" ] && LIBTOOLIZE="$(command -v libtoolize)"; then
+    LIBTOOLIZE="${LIBTOOLIZE}"
+    export LIBTOOLIZE
+  fi
+}
+
+# Run autoreconf
+run_autoreconf() {
+  command -V autoreconf >/dev/null || { echo "Error: autoreconf not found."; exit 1; }
+  autoreconf --install --force --warnings=all || { echo "Error: autoreconf failed"; exit 1; }
+}
+
+# Copy file if newer
+copy_if_newer() {
+  local src=$1 dst=$2
+
+  if [ ! -f "$src" ]; then
+    echo "Source file $src does not exist. Skipping copy operation."
+    return
+  fi
+
+  if [ "$verbose" -ne 0 ]; then
+    echo "Checking if $src is newer than $dst..."
+  fi
+  
+  if [ "$src" -nt "$dst" ]; then
+    cp "$src" "$dst" || { echo "Error: Failed to copy $src to $dst."; exit 1; }
+    if [ "$verbose" -ne 0 ]; then
+      echo "Copied $src to $dst"
+    fi
+  else
+    if [ "$verbose" -ne 0 ]; then
+      echo "$src is not newer than $dst. Skipping copy operation."
+    fi
+  fi
+}
+
+# Call functions
+set_environment_variables
+check_libtoolize
+run_autoreconf
+
+copy_if_newer 'depends/config.guess' 'build-aux/config.guess'
+copy_if_newer 'depends/config.guess' 'src/secp256k1/build-aux/config.guess'
+copy_if_newer 'depends/config.sub' 'build-aux/config.sub'
+copy_if_newer 'depends/config.sub' 'src/secp256k1/build-aux/config.sub'
