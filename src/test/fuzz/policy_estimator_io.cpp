cf219f29f3c5b41070eaab9a549a476f01990f3a
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2023-06-14 22:39:26 +0100

    tx fees, policy: read stale fee estimates with a regtest-only option
    If -acceptstalefeeestimates option is passed stale fee estimates can now
    be read when operating in regtest environments.
    
    Additionally, this commit updates all declarations of the CBlockPolicyEstimator
    class to include a the second constructor variable.

--- src/test/fuzz/policy_estimator_io.cpp
+++ src/test/fuzz/policy_estimator_io.cpp
@@ -28,7 +28,7 @@ FUZZ_TARGET_INIT(policy_estimator_io, initialize_policy_estimator_io)
     FuzzedAutoFileProvider fuzzed_auto_file_provider = ConsumeAutoFile(fuzzed_data_provider);
     AutoFile fuzzed_auto_file{fuzzed_auto_file_provider.open()};
     // Re-using block_policy_estimator across runs to avoid costly creation of CBlockPolicyEstimator object.
-    static CBlockPolicyEstimator block_policy_estimator{FeeestPath(*g_setup->m_node.args)};
+    static CBlockPolicyEstimator block_policy_estimator{FeeestPath(*g_setup->m_node.args), DEFAULT_ACCEPT_STALE_FEE_ESTIMATES};
     if (block_policy_estimator.Read(fuzzed_auto_file)) {
         block_policy_estimator.Write(fuzzed_auto_file);
     }
