b8ffea6cc34cc55e1cf11226131d0834400c3f05
Kittywhiskers Van Gogh <63189531+kittywhiskers@users.noreply.github.com>
2023-06-20 14:43:56 +0000

    revert: Avoid @GLIBC_2.25 symbols for compatibility
    This reverts commit 6de96a86bfe51fe974f756c4d8070865d2efc2f2.

--- depends/packages/libevent.mk
+++ depends/packages/libevent.mk
@@ -3,7 +3,6 @@ $(package)_version=2.1.12-stable
 $(package)_download_path=https://github.com/libevent/libevent/releases/download/release-$($(package)_version)/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=92e6de1be9ec176428fd2367677e61ceffc2ee1cb119035037a27d346b0403bb
-$(package)_patches = glibc_compatibility.patch
 
 # When building for Windows, we set _WIN32_WINNT to target the same Windows
 # version as we do in configure. Due to quirks in libevents build system, this
@@ -18,8 +17,7 @@ define $(package)_set_vars
 endef
 
 define $(package)_preprocess_cmds
-  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub build-aux && \
-  patch -p1 -i $($(package)_patch_dir)/glibc_compatibility.patch
+  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub build-aux
 endef
 
 define $(package)_config_cmds
