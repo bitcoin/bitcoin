f16a29a864618cb0eb1ab968c1edbf233bcb2174
Kittywhiskers Van Gogh <63189531+kittywhiskers@users.noreply.github.com>
2023-06-20 13:29:58 +0000

    partial bitcoin#22318: Avoid @GLIBC_2.25 symbols for compatibility
    contains:
     - a4b0b340df22dd0e11096c94ebe669dc68f4a05d

--- depends/packages/libevent.mk
+++ depends/packages/libevent.mk
@@ -3,6 +3,7 @@ $(package)_version=2.1.12-stable
 $(package)_download_path=https://github.com/libevent/libevent/releases/download/release-$($(package)_version)/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=92e6de1be9ec176428fd2367677e61ceffc2ee1cb119035037a27d346b0403bb
+$(package)_patches = glibc_compatibility.patch
 
 # When building for Windows, we set _WIN32_WINNT to target the same Windows
 # version as we do in configure. Due to quirks in libevents build system, this
@@ -14,6 +15,10 @@ define $(package)_set_vars
   $(package)_config_opts_linux=--with-pic
   $(package)_config_opts_android=--with-pic
   $(package)_cppflags_mingw32=-D_WIN32_WINNT=0x0601
+endef
+
+define $(package)_preprocess_cmds
+  patch -p1 -i $($(package)_patch_dir)/glibc_compatibility.patch
 endef
 
 define $(package)_config_cmds
