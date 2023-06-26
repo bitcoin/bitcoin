fa8c250c2f6b43f828020a35a8ee4262755c39ed
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2023-06-26 15:41:27 +0200

    ci: Limit scope of some env vars
    No need to have a larger scope than needed.
    
    Can be reviewed with --color-moved=dimmed-zebra

--- ci/test/06_script_b.sh
+++ ci/test/06_script_b.sh
@@ -8,6 +8,11 @@ export LC_ALL=C.UTF-8
 
 set -ex
 
+export ASAN_OPTIONS="detect_stack_use_after_return=1:check_initialization_order=1:strict_init_order=1"
+export LSAN_OPTIONS="suppressions=${BASE_ROOT_DIR}/test/sanitizer_suppressions/lsan"
+export TSAN_OPTIONS="suppressions=${BASE_ROOT_DIR}/test/sanitizer_suppressions/tsan:halt_on_error=1:log_path=${BASE_SCRATCH_DIR}/sanitizer-output/tsan"
+export UBSAN_OPTIONS="suppressions=${BASE_ROOT_DIR}/test/sanitizer_suppressions/ubsan:print_stacktrace=1:halt_on_error=1:report_error_type=1"
+
 if [ "$CI_OS_NAME" == "macos" ]; then
   top -l 1 -s 0 | awk ' /PhysMem/ {print}'
   echo "Number of CPUs: $(sysctl -n hw.logicalcpu)"
