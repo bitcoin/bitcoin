167ebe320c6846c188ead78ddd860d78c2e53bc9
jagdeep sidhu <sidhujag@syscoin.org>
2023-07-05 18:52:33 -0700

    Update feerate.cpp

--- src/policy/feerate.cpp
+++ src/policy/feerate.cpp
@@ -14,7 +14,8 @@ CFeeRate::CFeeRate(const CAmount& nFeePaid, uint32_t num_bytes)
     const int64_t nSize{num_bytes};
 
     if (nSize > 0) {
-        nSatoshisPerK = nFeePaid * 1000 / nSize;
+        // SYSCOIN
+        nSatoshisPerK = std::max(CAmount(0), nFeePaid) * 1000 / nSize;
     } else {
         nSatoshisPerK = 0;
     }
