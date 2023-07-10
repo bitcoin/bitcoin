50269b391fa18556bad72dc8c2fb4e2493a6a054
Pieter Wuille <pieter@wuille.net>
2023-07-10 14:02:14 -0400

    crypto: switch poly1305 to incremental implementation
    This code is taken from poly1305-donna-32.h, poly1305-donna.h, poly1305-donna.c
    from https://github.com/floodyberry/poly1305-donna, commit
    e6ad6e091d30d7f4ec2d4f978be1fcfcbce72781, with the following modifications:
    
    * Coding style (braces around one-line indented if/for loops).
    * Rename unsigned long (long) to uint32_t and uint64_t.
    * Rename poly1305_block_size to POLY1305_BLOCK_SIZE.
    * Adding noexcept to functions.
    * Merging poly1305_state_internal_t and poly1305_context types.
    * Merging code from multiple files.
    * Place all imported code in the poly1305_donna namespace.

--- src/crypto/poly1305.h
+++ src/crypto/poly1305.h
@@ -10,6 +10,27 @@
 
 #define POLY1305_KEYLEN 32
 #define POLY1305_TAGLEN 16
+#define POLY1305_BLOCK_SIZE 16
+
+namespace poly1305_donna {
+
+// Based on the public domain implementation by Andrew Moon
+// poly1305-donna-32.h from https://github.com/floodyberry/poly1305-donna
+
+typedef struct {
+    uint32_t r[5];
+    uint32_t h[5];
+    uint32_t pad[4];
+    size_t leftover;
+    unsigned char buffer[POLY1305_BLOCK_SIZE];
+    unsigned char final;
+} poly1305_context;
+
+void poly1305_init(poly1305_context *st, const unsigned char key[32]) noexcept;
+void poly1305_update(poly1305_context *st, const unsigned char *m, size_t bytes) noexcept;
+void poly1305_finish(poly1305_context *st, unsigned char mac[16]) noexcept;
+
+}  // namespace poly1305_donna
 
 void poly1305_auth(unsigned char out[POLY1305_TAGLEN], const unsigned char *m, size_t inlen,
     const unsigned char key[POLY1305_KEYLEN]);
