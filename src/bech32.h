5676aec1e1a6d2c6fd3099e120e263a0a7def089
josibake <josibake@protonmail.com>
2023-07-18 22:22:26 +0200

    refactor: Model the bech32 charlimit as an Enum
    Bech32(m) was defined with a 90 character limit so that certain
    guarantees for error detection could be made for segwit addresses.
    However, there is nothing about the encoding scheme itself that requires
    a limit and in practice bech32(m) has been used without the 90 char
    limit (e.g. lightning invoices).
    
    Further, increasing the character limit doesn't do away with error
    detection, it simply lessons the guarantees.
    
    Model charlimit as an Enum, so that if a different address scheme is
    using bech32(m), the character limit for that address scheme can be
    used, rather than always using the 90 charlimit defined for segwit
    addresses.
    
    upate comment

--- src/bech32.h
+++ src/bech32.h
@@ -28,6 +28,14 @@ enum class Encoding {
     BECH32M, //!< Bech32m encoding as defined in BIP350
 };
 
+/** Character limits for Bech32(m) encoded strings. Character limits are how we provide error location guarantees.
+ *  These values should never exceed 2^31 - 1 (max value for a 32-bit int), since there are places where we may need to
+ *  convert the CharLimit::VALUE to an int. In practice, this should never happen since this CharLimit applies to an address encoding
+ *  and we would never encode an address with such a massive value */
+enum CharLimit : size_t {
+    BECH32 = 90,            //!< BIP173/350 imposed character limit for Bech32(m) encoded addresses. This guarantees finding up to 4 errors.
+};
+
 /** Encode a Bech32 or Bech32m string. If hrp contains uppercase characters, this will cause an
  *  assertion error. Encoding must be one of BECH32 or BECH32M. */
 std::string Encode(Encoding encoding, const std::string& hrp, const std::vector<uint8_t>& values);
@@ -43,10 +51,10 @@ struct DecodeResult
 };
 
 /** Decode a Bech32 or Bech32m string. */
-DecodeResult Decode(const std::string& str);
+DecodeResult Decode(const std::string& str, CharLimit limit = CharLimit::BECH32);
 
 /** Return the positions of errors in a Bech32 string. */
-std::pair<std::string, std::vector<int>> LocateErrors(const std::string& str);
+std::pair<std::string, std::vector<int>> LocateErrors(const std::string& str, CharLimit limit = CharLimit::BECH32);
 
 } // namespace bech32
 
