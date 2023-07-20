632295996c2d86aab854913831ae6c74f755f8a6
fanquake <fanquake@gmail.com>
2023-07-20 16:57:53 +0100

    Merge bitcoin/bitcoin#28103: test: Add missing `set -ex` to ci/lint/06_script.sh
    ffff4b5dc57c32bf759b705530c1368de4aa787e lint: Add missing `set -ex` to ci/lint/06_script.sh (MarcoFalke)
    fadc5232f49ebe0e23599cf76d6aadbd63e575b9 doc: Add doc comment to ci/test_imagefile (MarcoFalke)
    
    Pull request description:
    
      Requested in https://github.com/bitcoin/bitcoin/pull/28083#pullrequestreview-1535304219.
    
      Also, one doc commit.
    
    ACKs for top commit:
      fanquake:
        ACK ffff4b5dc57c32bf759b705530c1368de4aa787e
      jamesob:
        ACK ffff4b5dc57c32bf759b705530c1368de4aa787e ([`jamesob/ackr/28103.1.MarcoFalke.test_add_missing_set_ex`](https://github.com/jamesob/bitcoin/tree/ackr/28103.1.MarcoFalke.test_add_missing_set_ex))
    
    Tree-SHA512: 99e67aeaae460319c2c428eab5297dbe1f1dc7f162f6592380bc5d2005308300c391cc187959cb2ace486dfe3411a8b0477f703ff11b5fe33944942c210a2d32

--- ci/lint/container-entrypoint.sh
+++ ci/lint/container-entrypoint.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+#
+# Copyright (c) The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or https://opensource.org/license/mit/.
+
 export LC_ALL=C
 
 # Fixes permission issues when there is a container UID/GID mismatch with the owner
