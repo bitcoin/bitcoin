65d4f214fd3be19456571161d0d34abecc9e017f
josibake <josibake@protonmail.com>
2023-07-23 18:57:27 +0200

    wallet: create new type OutputType::SILENT_PAYMENT

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -3544,6 +3544,7 @@ void CWallet::SetupDescriptorScriptPubKeyMans(const CExtKey& master_key)
 
     for (bool internal : {false, true}) {
         for (OutputType t : OUTPUT_TYPES) {
+            if (t == OutputType::SILENT_PAYMENT) continue;
             auto spk_manager = std::unique_ptr<DescriptorScriptPubKeyMan>(new DescriptorScriptPubKeyMan(*this, m_keypool_size));
             if (IsCrypted()) {
                 if (IsLocked()) {
