e477fa665dff37a44a3ea727ad38bc618769af4f
Sergi Delgado Segura <sergi.delgado.s@gmail.com>
2023-07-25 15:29:00 -0400

    rpc: Prevents adding the same ip more than once when formatted differently
    Currently it is possible to add the same node twice when formatting IPs in
    different, yet equivalent, manner. This applies to both ipv4 and ipv6, e.g:
    
    127.0.0.1 = 127.1 | [::1] = [0:0:0:0:0:0:0:1]
    
    `addnode` will accept both and display both as connected (given they translate to
    the same IP). This will not result in multiple connections to the same node, but
    will report redundant info when querying `getaddednodeinfo` and populate `m_added_nodes`
    with redundant data.
    
    This can be avoided performing comparing the contents of `m_added_addr` and the address
    to be added as `CServices` instead of as strings.

--- src/net.cpp
+++ src/net.cpp
@@ -3430,9 +3430,10 @@ std::vector<CAddress> CConnman::GetAddresses(CNode& requestor, size_t max_addres
 
 bool CConnman::AddNode(const AddedNodeParams& add)
 {
+    CService resolved(LookupNumeric(add.m_added_node, GetDefaultPort(add.m_added_node)));
     LOCK(m_added_nodes_mutex);
     for (const auto& it : m_added_node_params) {
-        if (add.m_added_node == it.m_added_node) return false;
+        if (add.m_added_node == it.m_added_node || (resolved.IsValid() && resolved == LookupNumeric(it.m_added_node, GetDefaultPort(it.m_added_node)))) return false;
     }
 
     m_added_node_params.push_back(add);
