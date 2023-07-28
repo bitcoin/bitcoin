519e5b169fe2e506a937ba8c2ca94832e6d3e059
josibake <josibake@protonmail.com>
2023-07-28 15:24:38 +0200

    tests: add sending functional tests

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -148,6 +148,7 @@ BASE_SCRIPTS = [
     'wallet_listtransactions.py --legacy-wallet',
     'wallet_listtransactions.py --descriptors',
     # vv Tests less than 30s vv
+    'wallet_silentpayments_sending.py',
     'p2p_invalid_messages.py',
     'rpc_createmultisig.py',
     'p2p_timeouts.py',
