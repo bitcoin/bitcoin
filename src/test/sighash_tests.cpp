0b17a2d1870d4279d0c2f7e2e32f7aef481f3eda
fanquake <fanquake@gmail.com>
2023-07-28 11:48:07 +0100

    Merge bitcoin/bitcoin#28168: refactor: Remove unused raw-pointer read helper from univalue
    fa940f41eaffa4b2a28c465a10a4c12d4b8976b8 Remove unused raw-pointer read helper from univalue (MarcoFalke)
    
    Pull request description:
    
      The helpers are unused outside of tests and redundant with the existing `bool read(std::string_view raw);`.
    
      Fix both issues by removing them.
    
      Also, simplify the tests code by removing a `std::string` constructor where possible.
    
    ACKs for top commit:
      stickies-v:
        utACK fa940f41eaffa4b2a28c465a10a4c12d4b8976b8
      TheCharlatan:
        tACK fa940f41eaffa4b2a28c465a10a4c12d4b8976b8
    
    Tree-SHA512: 60c154c1046f01551335af79bf820a6104844f63e89977271b4336b3cd06ac3bab1379e18b7bc61d12bef7446029e91c16541ddecf9e88bc8bc897fc1f6ee2c8

--- src/test/sighash_tests.cpp
+++ src/test/sighash_tests.cpp
@@ -163,7 +163,7 @@ BOOST_AUTO_TEST_CASE(sighash_test)
 // SYSCOIN
 /*BOOST_AUTO_TEST_CASE(sighash_from_data)
 {
-    UniValue tests = read_json(std::string(json_tests::sighash, json_tests::sighash + sizeof(json_tests::sighash)));
+    UniValue tests = read_json(json_tests::sighash);
 
     for (unsigned int idx = 0; idx < tests.size(); idx++) {
         const UniValue& test = tests[idx];
