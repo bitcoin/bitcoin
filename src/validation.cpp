65cb8609da388a9a0b47da79bfb725f6b91bb2a7
Luke Dashjr <luke-jr+git@utopios.org>
2023-08-01 18:12:43 +0000

    Ability to ignore AreInputsStandard rejection reasons

--- src/validation.cpp
+++ src/validation.cpp
@@ -896,7 +896,7 @@ bool MemPoolAccept::PreChecks(ATMPArgs& args, Workspace& ws)
         return false; // state filled in by CheckTxInputs
     }
 
-    if (m_pool.m_opts.require_standard && !AreInputsStandard(tx, m_view, "bad-txns-input-", reason)) {
+    if (m_pool.m_opts.require_standard && !AreInputsStandard(tx, m_view, "bad-txns-input-", reason, ignore_rejects)) {
         return state.Invalid(TxValidationResult::TX_INPUTS_NOT_STANDARD, reason);
     }
 
