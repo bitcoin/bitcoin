fae379b6b1c9d434821acc348f010d67c4fee927
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2023-08-03 14:22:15 +0200

    build: Bump minimum supported Clang to clang-13

--- src/net.cpp
+++ src/net.cpp
@@ -1415,7 +1415,7 @@ std::optional<std::string> V2Transport::GetMessageType(Span<const uint8_t>& cont
     }
     // Strip message type bytes of contents.
     contents = contents.subspan(CMessageHeader::COMMAND_SIZE);
-    return {std::move(ret)};
+    return ret;
 }
 
 CNetMessage V2Transport::GetReceivedMessage(std::chrono::microseconds time, bool& reject_message) noexcept
--- src/wallet/interfaces.cpp
+++ src/wallet/interfaces.cpp
@@ -602,7 +602,7 @@ public:
         bilingual_str error;
         std::unique_ptr<Wallet> wallet{MakeWallet(m_context, CreateWallet(m_context, name, /*load_on_start=*/true, options, status, error, warnings))};
         if (wallet) {
-            return {std::move(wallet)};
+            return wallet;
         } else {
             return util::Error{error};
         }
@@ -616,7 +616,7 @@ public:
         bilingual_str error;
         std::unique_ptr<Wallet> wallet{MakeWallet(m_context, LoadWallet(m_context, name, /*load_on_start=*/true, options, status, error, warnings))};
         if (wallet) {
-            return {std::move(wallet)};
+            return wallet;
         } else {
             return util::Error{error};
         }
@@ -627,7 +627,7 @@ public:
         bilingual_str error;
         std::unique_ptr<Wallet> wallet{MakeWallet(m_context, RestoreWallet(m_context, backup_file, wallet_name, /*load_on_start=*/true, status, error, warnings))};
         if (wallet) {
-            return {std::move(wallet)};
+            return wallet;
         } else {
             return util::Error{error};
         }
@@ -642,7 +642,7 @@ public:
             .solvables_wallet_name = res->solvables_wallet ? std::make_optional(res->solvables_wallet->GetName()) : std::nullopt,
             .backup_path = res->backup_path,
         };
-        return {std::move(out)}; // std::move to work around clang bug
+        return out;
     }
     std::string getWalletDir() override
     {
