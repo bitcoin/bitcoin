34088d6c9ed4ed99bb6b7fc83795da01ec9f3c97
glozow <gloriajzhao@gmail.com>
2023-08-08 18:25:40 +0100

    [test util] CheckPackageMempoolAcceptResult for sanity-checking results

--- src/test/util/txmempool.h
+++ src/test/util/txmempool.h
@@ -5,12 +5,14 @@
 #ifndef BITCOIN_TEST_UTIL_TXMEMPOOL_H
 #define BITCOIN_TEST_UTIL_TXMEMPOOL_H
 
+#include <policy/packages.h>
 #include <txmempool.h>
 #include <util/time.h>
 
 namespace node {
 struct NodeContext;
 }
+struct PackageMempoolAcceptResult;
 
 CTxMemPool::Options MemPoolOptionsForTest(const node::NodeContext& node);
 
@@ -36,4 +38,12 @@ struct TestMemPoolEntryHelper {
     TestMemPoolEntryHelper& SigOpsCost(unsigned int _sigopsCost) { sigOpCost = _sigopsCost; return *this; }
 };
 
+/** Check expected properties for every PackageMempoolAcceptResult, regardless of value. Returns
+ * a string if an error occurs with error populated, nullopt otherwise. If mempool is provided,
+ * checks that the expected transactions are in mempool (this should be set to nullptr for a test_accept).
+*/
+std::optional<std::string>  CheckPackageMempoolAcceptResult(const Package& txns,
+                                                            const PackageMempoolAcceptResult& result,
+                                                            bool expect_valid,
+                                                            const CTxMemPool* mempool);
 #endif // BITCOIN_TEST_UTIL_TXMEMPOOL_H
