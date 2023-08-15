a9a1d69391596f57851f545fae12f8851149d2c3
Martin Zumsande <mzumsande@gmail.com>
2023-08-15 12:02:14 -0400

    rpc: add test-only sendmsgtopeer rpc
    This rpc can be used when we want a node to send a message, but
    cannot use a python P2P object, for example for testing of low-level
    net transport behavior.

--- src/test/fuzz/rpc.cpp
+++ src/test/fuzz/rpc.cpp
@@ -158,6 +158,7 @@ const std::vector<std::string> RPC_COMMANDS_SAFE_FOR_FUZZING{
     "reconsiderblock",
     "scanblocks",
     "scantxoutset",
+    "sendmsgtopeer", // when no peers are connected, no p2p message is sent
     "sendrawtransaction",
     "setmocktime",
     "setnetworkactive",
