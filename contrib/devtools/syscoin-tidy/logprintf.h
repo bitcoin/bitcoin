cb8844e5aff225e05db7a904e912e12f79b2ee3b
fanquake <fanquake@gmail.com>
2023-08-18 11:21:52 +0100

    Merge #28237: refactor: Enforce C-str fmt strings in WalletLogPrintf()

--- contrib/devtools/syscoin-tidy/logprintf.h
+++ contrib/devtools/syscoin-tidy/logprintf.h
@@ -9,6 +9,7 @@
 
 namespace syscoin {
 
+// Warn about any use of LogPrintf that does not end with a newline.
 class LogPrintfCheck final : public clang::tidy::ClangTidyCheck
 {
 public:
--- src/wallet/scriptpubkeyman.h
+++ src/wallet/scriptpubkeyman.h
@@ -250,9 +250,10 @@ public:
     virtual std::unordered_set<CScript, SaltedSipHasher> GetScriptPubKeys() const { return {}; };
 
     /** Prepends the wallet name in logging output to ease debugging in multi-wallet use cases */
-    template<typename... Params>
-    void WalletLogPrintf(std::string fmt, Params... parameters) const {
-        LogPrintf(("%s " + fmt).c_str(), m_storage.GetDisplayName(), parameters...);
+    template <typename... Params>
+    void WalletLogPrintf(const char* fmt, Params... parameters) const
+    {
+        LogPrintf(("%s " + std::string{fmt}).c_str(), m_storage.GetDisplayName(), parameters...);
     };
 
     /** Watch-only address added */
