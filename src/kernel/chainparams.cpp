27b4084e16a1cb210ce27119416ee34625781052
Tim Neubauer <timnbr99@gmail.com>
2023-08-31 18:59:43 +0200

    Refactor: Remove m_is_test_chain

--- src/kernel/chainparams.cpp
+++ src/kernel/chainparams.cpp
@@ -151,7 +151,6 @@ public:
         vFixedSeeds = std::vector<uint8_t>(std::begin(chainparams_seed_main), std::end(chainparams_seed_main));
 
         fDefaultConsistencyChecks = false;
-        m_is_test_chain = false;
         m_is_mockable_chain = false;
 
         checkpointData = {
@@ -258,7 +257,6 @@ public:
         vFixedSeeds = std::vector<uint8_t>(std::begin(chainparams_seed_test), std::end(chainparams_seed_test));
 
         fDefaultConsistencyChecks = false;
-        m_is_test_chain = true;
         m_is_mockable_chain = false;
 
         checkpointData = {
@@ -380,7 +378,6 @@ public:
         bech32_hrp = "tb";
 
         fDefaultConsistencyChecks = false;
-        m_is_test_chain = true;
         m_is_mockable_chain = false;
     }
 };
@@ -471,7 +468,6 @@ public:
         vSeeds.emplace_back("dummySeed.invalid.");
 
         fDefaultConsistencyChecks = true;
-        m_is_test_chain = true;
         m_is_mockable_chain = true;
 
         checkpointData = {
