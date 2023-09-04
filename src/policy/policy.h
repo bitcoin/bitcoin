95f706615deb416a4690503e31b5b0010f75bafd
Luke Dashjr <luke-jr+git@utopios.org>
2023-09-04 22:49:52 +0000

    Add -datacarriercost option to adjust policy vsize of datacarrier bytes

--- src/policy/policy.h
+++ src/policy/policy.h
@@ -57,6 +57,8 @@ static constexpr unsigned int DEFAULT_INCREMENTAL_RELAY_FEE{1000};
 static constexpr unsigned int DEFAULT_BYTES_PER_SIGOP{20};
 /** Default for -bytespersigopstrict */
 static constexpr unsigned int DEFAULT_BYTES_PER_SIGOP_STRICT{20};
+/** Default for -datacarriercost (multiplied by WITNESS_SCALE_FACTOR) */
+static constexpr unsigned int DEFAULT_WEIGHT_PER_DATA_BYTE{1};
 /** Default for -permitbarepubkey */
 static constexpr bool DEFAULT_PERMIT_BAREPUBKEY{true};
 /** Default for -permitbaremultisig */
