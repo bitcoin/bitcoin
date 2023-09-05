04c6ba58e09c4cd5ac6ec815afeabffefacb1ae5
Luke Dashjr <luke-jr+git@utopios.org>
2023-09-05 21:56:19 +0000

    txdb: Increase max dbcache size to 1 TB

--- src/txdb.h
+++ src/txdb.h
@@ -24,7 +24,7 @@ class uint256;
 //! -dbcache default (MiB)
 static const int64_t nDefaultDbCache = 300;
 //! max. -dbcache (MiB)
-static const int64_t nMaxDbCache = sizeof(void*) > 4 ? 16384 : 1024;
+static constexpr int64_t nMaxDbCache = sizeof(void*) > 4 ? 1048576 : 1024;
 //! min. -dbcache (MiB)
 static const int64_t nMinDbCache = 4;
 //! Max memory allocated to block tree DB specific cache, if no -txindex (MiB)
