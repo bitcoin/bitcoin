bf94df6e024eeae43b8969b0b2f022a5f8387c69
Martin Zumsande <mzumsande@gmail.com>
2023-09-10 11:45:11 -0400

    p2p: trigger possible eviction if we support bloom filters and change a peer to tx relay
    Co-authored-by: Amiti Uttarwar <amiti@uttarwar.org>

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -4842,6 +4842,11 @@ void PeerManagerImpl::ProcessMessage(CNode& pfrom, const std::string& msg_type, 
             }
             pfrom.m_bloom_filter_loaded = true;
             pfrom.m_relays_txs = true;
+            if (pfrom.IsInboundConn() && !m_connman.EvictTxPeerIfFull()) {
+                // We don't have room for another tx-relay peer, disconnect
+                LogDebug(BCLog::NET, "filterload received, but no capacity for tx-relay and no other peer to evict. disconnecting peer=%d\n", pfrom.GetId());
+                pfrom.fDisconnect = true;
+            };
         }
         return;
     }
@@ -4890,6 +4895,11 @@ void PeerManagerImpl::ProcessMessage(CNode& pfrom, const std::string& msg_type, 
         }
         pfrom.m_bloom_filter_loaded = false;
         pfrom.m_relays_txs = true;
+        if (pfrom.IsInboundConn() && !m_connman.EvictTxPeerIfFull()) {
+            // We don't have room for another tx-relay peer, disconnect
+            LogDebug(BCLog::NET, "filterclear received, but no capacity for tx-relay and no other peer to evict. disconnecting peer=%d\n", pfrom.GetId());
+            pfrom.fDisconnect = true;
+        };
         return;
     }
 
