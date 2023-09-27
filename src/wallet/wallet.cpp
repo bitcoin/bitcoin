8a553c94098c96cb3679468c2b460be145a0eabf
Ryan Ofsky <ryan@ofsky.org>
2023-09-27 10:35:45 -0400

    wallet: Add TxStateString function for debugging and logging
    Co-authored-by: furszy <matiasfurszyfer@protonmail.com>

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -1129,7 +1129,7 @@ CWalletTx* CWallet::AddToWallet(CTransactionRef tx, const TxState& state, const 
     }
 
     //// debug print
-    WalletLogPrintf("AddToWallet %s  %s%s\n", hash.ToString(), (fInsertedNew ? "new" : ""), (fUpdated ? "update" : ""));
+    WalletLogPrintf("AddToWallet %s  %s%s %s\n", hash.ToString(), (fInsertedNew ? "new" : ""), (fUpdated ? "update" : ""), TxStateString(state));
 
     // Write to disk
     if (fInsertedNew || fUpdated)
