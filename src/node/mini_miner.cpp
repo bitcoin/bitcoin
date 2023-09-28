c896823af1553be2d596f2589bd6c36f6f934775
Suhas Daftuar <sdaftuar@chaincode.com>
2023-09-28 17:55:16 -0400

    Remove dependency on cached ancestor data in mini-miner

--- src/node/mini_miner.cpp
+++ src/node/mini_miner.cpp
@@ -77,12 +77,11 @@ MiniMiner::MiniMiner(const CTxMemPool& mempool, const std::vector<COutPoint>& ou
     // Add every entry to m_entries_by_txid and m_entries, except the ones that will be replaced.
     for (const auto& txiter : cluster) {
         if (!m_to_be_replaced.count(txiter->GetTx().GetHash())) {
-            auto [mapiter, success] = m_entries_by_txid.emplace(txiter->GetTx().GetHash(),
-                MiniMinerMempoolEntry{/*tx_in=*/txiter->GetSharedTx(),
-                                      /*vsize_self=*/txiter->GetTxSize(),
-                                      /*vsize_ancestor=*/txiter->GetSizeWithAncestors(),
-                                      /*fee_self=*/txiter->GetModifiedFee(),
-                                      /*fee_ancestor=*/txiter->GetModFeesWithAncestors()});
+            size_t ancestor_count{0};
+            size_t ancestor_size{0};
+            CAmount ancestor_fee{0};
+            mempool.CalculateAncestorData(*txiter, ancestor_count, ancestor_size, ancestor_fee);
+            auto [mapiter, success] = m_entries_by_txid.emplace(txiter->GetTx().GetHash(), MiniMinerMempoolEntry(txiter->GetSharedTx(), txiter->GetTxSize(), int64_t(ancestor_size), txiter->GetModifiedFee(), ancestor_fee));
             m_entries.push_back(mapiter);
         } else {
             auto outpoints_it = m_requested_outpoints_by_txid.find(txiter->GetTx().GetHash());
