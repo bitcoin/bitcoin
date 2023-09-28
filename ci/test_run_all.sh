fac9abbf475a1de6f9f39ddede9a6a59bbd1cff4
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2023-09-28 11:05:28 +0200

    ci: Rename 04_install to 02_run_container
    This reflects what the script does (docker run ...).

--- ci/test_run_all.sh
+++ ci/test_run_all.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 #
-# Copyright (c) 2019-2020 The Bitcoin Core developers
+# Copyright (c) 2019-present The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 export LC_ALL=C.UTF-8
 
 set -o errexit; source ./ci/test/00_setup_env.sh
-set -o errexit; source ./ci/test/04_install.sh
+set -o errexit; source "./ci/test/02_run_container.sh"
 set -o errexit
 CI_EXEC "${BASE_ROOT_DIR}/ci/test/06_script_b.sh"
 
