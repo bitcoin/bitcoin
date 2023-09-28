64b80d5c5bc1311cd2fb188f2d84aff6704e27d7
Fabian Jahr <fjahr@protonmail.com>
2023-09-28 13:58:18 +0200

    test: simplify feature_init

--- test/functional/feature_init.py
+++ test/functional/feature_init.py
@@ -133,15 +133,12 @@ class InitStressTest(BitcoinTestFramework):
 
             for target_file in target_files:
                 self.log.info(f"Perturbing file to ensure failure {target_file}")
-                with open(target_file, "rb") as tf_read:
-                    contents = tf_read.read()
-                    tweaked_contents = bytearray(contents)
+                with open(target_file, "r+b") as tf:
                     # Since the genesis block is not checked by -checkblocks, the
                     # perturbation window must be chosen such that a higher block
                     # in blk*.dat is affected.
-                    tweaked_contents[150:350] = b'1' * 200
-                with open(target_file, "wb") as tf_write:
-                    tf_write.write(bytes(tweaked_contents))
+                    tf.seek(150)
+                    tf.write(b'1' * 200)
 
             start_expecting_error(err_fragment)
 
