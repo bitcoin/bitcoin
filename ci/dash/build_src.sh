00b3dd669014dab9256d1101d814e7e17fc31ef3
Kittywhiskers Van Gogh <63189531+kittywhiskers@users.noreply.github.com>
2023-09-28 13:37:09 +0530

    ci: don't re-use config.cache when running configure script

--- ci/dash/build_src.sh
+++ ci/dash/build_src.sh
@@ -45,11 +45,11 @@ rm -rf build-ci
 mkdir build-ci
 cd build-ci
 
-../configure --cache-file=config.cache $BITCOIN_CONFIG_ALL $BITCOIN_CONFIG || ( cat config.log && false)
+../configure $BITCOIN_CONFIG_ALL $BITCOIN_CONFIG || ( cat config.log && false)
 make distdir VERSION=$BUILD_TARGET
 
 cd dashcore-$BUILD_TARGET
-./configure --cache-file=../config.cache $BITCOIN_CONFIG_ALL $BITCOIN_CONFIG || ( cat config.log && false)
+./configure $BITCOIN_CONFIG_ALL $BITCOIN_CONFIG || ( cat config.log && false)
 
 make $MAKEJOBS $GOAL || ( echo "Build failure. Verbose build follows." && make $GOAL V=1 ; false )
 
