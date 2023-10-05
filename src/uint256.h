a2c3031dcc60f8305245245e8daf92222ea36e91
pasta <pasta@dashboost.org>
2023-10-05 08:24:16 -0500

    refactor: extend changes to uint512

--- src/uint256.h
+++ src/uint256.h
@@ -137,9 +137,9 @@ inline uint256 uint256S(const std::string& str)
 /** 512-bit unsigned big integer. */
 class uint512 : public base_blob<512> {
 public:
-    uint512() {}
-    uint512(const base_blob<512>& b) : base_blob<512>(b) {}
-    explicit uint512(const std::vector<unsigned char>& vch) : base_blob<512>(vch) {}
+    constexpr uint512() = default;
+    constexpr uint512(const base_blob<512>& b) : base_blob<512>(b) {}
+    constexpr explicit uint512(Span<unsigned char> vch) : base_blob<512>(vch) {}
 
     uint256 trim256() const
     {
