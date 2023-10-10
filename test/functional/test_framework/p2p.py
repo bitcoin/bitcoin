35253cdd15c3b9b0e7d3e327c5b4c0474d4f2290
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2023-10-10 12:11:00 -0400

    merge bitcoin#28632: make python p2p not send getaddr on incoming connections

--- test/functional/test_framework/p2p.py
+++ test/functional/test_framework/p2p.py
@@ -490,7 +490,8 @@ class P2PInterface(P2PConnection):
             self.send_message(msg_sendaddrv2())
         self.send_message(msg_verack())
         self.nServices = message.nServices
-        self.send_message(msg_getaddr())
+        if self.p2p_connected_to_node:
+            self.send_message(msg_getaddr())
 
     # Connection helper methods
 
