3a6b676e4a51dd4ff4ab33276e2221a498684e38
Suhas Daftuar <sdaftuar@chaincode.com>
2023-10-11 11:34:16 -0400

    bench: add more mempool benchmarks
    Add benchmarks for:
    
      - mempool update time when blocks are found
      - adding a transaction
      - performing the mempool's RBF calculation
      - calculating mempool ancestors/descendants

--- src/bench/mempool_stress.cpp
+++ src/bench/mempool_stress.cpp
@@ -21,7 +21,7 @@
 
 class CCoinsViewCache;
 
-static void AddTx(const CTransactionRef& tx, CTxMemPool& pool) EXCLUSIVE_LOCKS_REQUIRED(cs_main, pool.cs)
+static void AddTx(const CTransactionRef& tx, CTxMemPool& pool, FastRandomContext& det_rand) EXCLUSIVE_LOCKS_REQUIRED(cs_main, pool.cs)
 {
     int64_t nTime = 0;
     unsigned int nHeight = 1;
@@ -29,7 +29,7 @@ static void AddTx(const CTransactionRef& tx, CTxMemPool& pool) EXCLUSIVE_LOCKS_R
     bool spendsCoinbase = false;
     unsigned int sigOpCost = 4;
     LockPoints lp;
-    AddToMempool(pool, CTxMemPoolEntry(TxGraph::Ref(), tx, 1000, nTime, nHeight, sequence, spendsCoinbase, sigOpCost, lp));
+    AddToMempool(pool, CTxMemPoolEntry(TxGraph::Ref(), tx, det_rand.randrange(10000)+1000, nTime, nHeight, sequence, spendsCoinbase, sigOpCost, lp));
 }
 
 struct Available {
@@ -93,6 +93,33 @@ static std::vector<CTransactionRef> CreateCoinCluster(FastRandomContext& det_ran
     return ordered_coins;
 }
 
+static void MemPoolAddTransactions(benchmark::Bench& bench)
+{
+    FastRandomContext det_rand{true};
+    int childTxs = 50;
+    if (bench.complexityN() > 1) {
+        childTxs = static_cast<int>(bench.complexityN());
+    }
+    const auto testing_setup = MakeNoLogFileContext<const TestingSetup>(ChainType::MAIN);
+    CTxMemPool& pool = *testing_setup.get()->m_node.mempool;
+
+    std::vector<CTransactionRef> transactions;
+    // Create 1000 clusters of 100 transactions each
+    for (int i=0; i<100; i++) {
+        auto new_txs = CreateCoinCluster(det_rand, childTxs, /*min_ancestors*/ 1);
+        transactions.insert(transactions.end(), new_txs.begin(), new_txs.end());
+    }
+
+    LOCK2(cs_main, pool.cs);
+
+    bench.run([&]() NO_THREAD_SAFETY_ANALYSIS {
+        for (auto& tx : transactions) {
+            AddTx(tx, pool, det_rand);
+        }
+        pool.TrimToSize(0, nullptr);
+    });
+}
+
 static void ComplexMemPool(benchmark::Bench& bench)
 {
     FastRandomContext det_rand{true};
@@ -121,7 +148,7 @@ static void ComplexMemPool(benchmark::Bench& bench)
                 ++tx_count;
                 hashes_remove_for_block.emplace_back(tx->GetHash());
             }
-            AddTx(tx, pool);
+            AddTx(tx, pool, det_rand);
         }
     }
 
@@ -131,11 +158,40 @@ static void ComplexMemPool(benchmark::Bench& bench)
     bench.run([&]() NO_THREAD_SAFETY_ANALYSIS {
         pool.removeForBlock(tx_remove_for_block, /*nBlockHeight*/100);
         for (auto& tx: tx_remove_for_block) {
-            AddTx(tx, pool);
+            AddTx(tx, pool, det_rand);
         }
         pool.UpdateTransactionsFromBlock(hashes_remove_for_block);
     });
 }
+
+static void MemPoolAncestorsDescendants(benchmark::Bench& bench)
+{
+    FastRandomContext det_rand{true};
+    int childTxs = 50;
+    if (bench.complexityN() > 1) {
+        childTxs = static_cast<int>(bench.complexityN());
+    }
+    const auto testing_setup = MakeNoLogFileContext<const TestingSetup>(ChainType::MAIN);
+    CTxMemPool& pool = *testing_setup.get()->m_node.mempool;
+
+    LOCK2(cs_main, pool.cs);
+
+    std::vector<CTransactionRef> transactions = CreateCoinCluster(det_rand, childTxs, /*min_ancestors=*/1);
+    for (auto& tx : transactions) {
+        AddTx(tx, pool, det_rand);
+    }
+
+    CTxMemPool::txiter first_tx = *pool.GetIter(transactions[0]->GetHash());
+    CTxMemPool::txiter last_tx = *pool.GetIter(transactions.back()->GetHash());
+
+    bench.run([&]() NO_THREAD_SAFETY_ANALYSIS {
+        CTxMemPool::setEntries dummy;
+        ankerl::nanobench::doNotOptimizeAway(dummy);
+        pool.CalculateDescendants({first_tx}, dummy);
+        ankerl::nanobench::doNotOptimizeAway(pool.CalculateMemPoolAncestors(*last_tx));
+    });
+}
+
 
 static void MempoolCheck(benchmark::Bench& bench)
 {
@@ -152,5 +208,65 @@ static void MempoolCheck(benchmark::Bench& bench)
     });
 }
 
+#if 0
+static void MemPoolMiningScoreCheck(benchmark::Bench& bench)
+{
+    // Default test: each cluster is of size 20, and we'll try to RBF with a
+    // transaction that merges 10 clusters, evicting 10 transactions from each.
+
+    FastRandomContext det_rand{true};
+    int childTxs = 10;
+    if (bench.complexityN() > 1) {
+        childTxs = static_cast<int>(bench.complexityN());
+    }
+    const auto testing_setup = MakeNoLogFileContext<const TestingSetup>(ChainType::MAIN);
+    CTxMemPool& pool = *testing_setup.get()->m_node.mempool;
+
+    LOCK2(cs_main, pool.cs);
+
+    std::vector<CTransactionRef> parent_txs_for_rbf;
+    std::set<uint256> child_txs_to_conflict_with;
+
+    for (int i=0; i<10; i++) {
+        std::vector<CTransactionRef> transactions = CreateCoinCluster(det_rand, childTxs, /*min_ancestors=*/1);
+        parent_txs_for_rbf.push_back(transactions[0]);
+        // Conflict with everything after the first 10 transactions
+        for (size_t j=10; j<transactions.size(); ++j) {
+            child_txs_to_conflict_with.insert(transactions[j]->GetHash());
+        }
+
+        // Add all transactions to the mempool.
+        for (auto& tx : transactions) {
+            AddTx(tx, pool, det_rand);
+        }
+    }
+
+    // Construct a transaction that spends from each of the parent transactions
+    // selected.
+    CMutableTransaction tx = CMutableTransaction();
+    tx.vin.resize(10);
+    for (size_t i=0; i<parent_txs_for_rbf.size(); ++i) {
+        tx.vin[i].prevout = COutPoint(parent_txs_for_rbf[i]->GetHash(), 0);
+        tx.vin[i].scriptSig = CScript() << i;
+    }
+    tx.vout.resize(1);
+    for (auto& out : tx.vout) {
+        out.scriptPubKey = CScript() << CScriptNum(det_rand.randrange(19)+1) << OP_EQUAL;
+        out.nValue = 10 * COIN;
+    }
+
+    CTxMemPool::setEntries all_conflicts = pool.GetIterSet(child_txs_to_conflict_with);
+    CTxMemPoolEntry entry(MakeTransactionRef(tx), det_rand.randrange(10000)+1000, 0, 1, 0, false, 4, LockPoints());
+
+    bench.run([&]() NO_THREAD_SAFETY_ANALYSIS {
+        CTxMemPool::Limits limits(pool.m_limits);
+        pool.CalculateMiningScoreOfReplacementTx(entry, det_rand.randrange(30000)+1000, all_conflicts, limits);
+    });
+}
+#endif
+
+BENCHMARK(MemPoolAncestorsDescendants, benchmark::PriorityLevel::HIGH);
+BENCHMARK(MemPoolAddTransactions, benchmark::PriorityLevel::HIGH);
 BENCHMARK(ComplexMemPool, benchmark::PriorityLevel::HIGH);
 BENCHMARK(MempoolCheck, benchmark::PriorityLevel::HIGH);
+//BENCHMARK(MemPoolMiningScoreCheck, benchmark::PriorityLevel::HIGH);
