66c11e6277c70fa785290656cee308bd3929ddb9
Luke Dashjr <luke-jr+git@utopios.org>
2023-10-12 00:49:38 +0000

    QA: Properly initialize coin-age priority on transactions made by TestChain100Setup::PopulateMempool

--- src/test/util/setup_common.cpp
+++ src/test/util/setup_common.cpp
@@ -28,6 +28,7 @@
 #include <node/peerman_args.h>
 #include <node/warnings.h>
 #include <noui.h>
+#include <policy/coin_age_priority.h>
 #include <policy/fees.h>
 #include <pow.h>
 #include <random.h>
@@ -507,6 +508,8 @@ CMutableTransaction TestChain100Setup::CreateValidMempoolTransaction(CTransactio
 
 std::vector<CTransactionRef> TestChain100Setup::PopulateMempool(FastRandomContext& det_rand, size_t num_transactions, bool submit)
 {
+    auto& active_chainstate = m_node.chainman->ActiveChainstate();
+    const auto height = active_chainstate.m_chain.Height();
     std::vector<CTransactionRef> mempool_transactions;
     std::deque<std::pair<COutPoint, CAmount>> unspent_prevouts;
     std::transform(m_coinbase_txns.begin(), m_coinbase_txns.end(), std::back_inserter(unspent_prevouts),
@@ -543,11 +546,13 @@ std::vector<CTransactionRef> TestChain100Setup::PopulateMempool(FastRandomContex
         }
         if (submit) {
             LOCK2(cs_main, m_node.mempool->cs);
+            CAmount in_chain_input_value;
+            double dPriority = GetPriority(*ptx, active_chainstate.CoinsTip(), height + 1, in_chain_input_value);
             LockPoints lp;
             auto changeset = m_node.mempool->GetChangeSet();
             changeset->StageAddition(ptx, /*fee=*/(total_in - num_outputs * amount_per_output),
-                    /*time=*/0, /*entry_priority=*/0, /*entry_height=*/1, /*entry_sequence=*/0,
-                    /*in_chain_input_value=*/0,
+                    /*time=*/0, /*entry_priority=*/ dPriority, /*entry_height=*/ height, /*entry_sequence=*/0,
+                    /*in_chain_input_value=*/ in_chain_input_value,
                     /*spends_coinbase=*/false, /*sigops_cost=*/4, lp);
             changeset->Apply();
         }
