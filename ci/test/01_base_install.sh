1c2132ddd97dbf8fc908b0d1113bbb70e1a6a64e
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2023-10-12 14:58:49 +0100

    Revert "ci: Upgrading pip version in macos environment"
    This reverts commit 057750c09d0a8331c33966d2cc2285ef82f08af8.
    
    It is not needed anymore in the GHA CI.
    This change will make the code much simpler in the following commit.

--- ci/test/01_base_install.sh
+++ ci/test/01_base_install.sh
@@ -32,7 +32,6 @@ fi
 
 if [ -n "$PIP_PACKAGES" ]; then
   if [ "$CI_OS_NAME" == "macos" ]; then
-    sudo -H pip3 install --upgrade pip
     # shellcheck disable=SC2086
     IN_GETOPT_BIN="$(brew --prefix gnu-getopt)/bin/getopt" ${CI_RETRY_EXE} pip3 install --user $PIP_PACKAGES
   else
