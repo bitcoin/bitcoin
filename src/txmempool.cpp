72e74e0d42284c712529bf3c619b1b740c070f1b
Suhas Daftuar <sdaftuar@gmail.com>
2023-10-12 10:48:55 -0400

    fuzz: try to add more code coverage for mempool fuzzing
    Including test coverage for mempool eviction and expiry

--- src/txmempool.cpp
+++ src/txmempool.cpp
@@ -418,6 +418,7 @@ void CTxMemPool::check(const CCoinsViewCache& active_coins_tip, int64_t spendhei
     uint64_t innerUsage = 0;
 
     assert(!m_txgraph->IsOversized(TxGraph::Level::MAIN));
+    m_txgraph->SanityCheck();
 
     CCoinsViewCache mempoolDuplicate(const_cast<CCoinsViewCache*>(&active_coins_tip));
 
