c2139c3f98d3aef57d0a6afa54c67c86ca002bd4
Murch <murch@murch.one>
2023-10-19 14:44:35 -0400

    Move simple BnB failure tests

--- src/wallet/test/coinselection_tests.cpp
+++ src/wallet/test/coinselection_tests.cpp
@@ -122,9 +122,18 @@ static void TestBnBSuccess(std::string test_title, std::vector<COutput>& utxo_po
     expected_result.Clear();
 }
 
+static void TestBnBFail(std::string test_title, std::vector<COutput>& utxo_pool, const CAmount& selection_target)
+{
+    BOOST_CHECK_MESSAGE(!SelectCoinsBnB(GroupCoins(utxo_pool), selection_target, /*cost_of_change=*/ default_cs_params.m_cost_of_change, /*max_weight=*/MAX_STANDARD_TX_WEIGHT), "BnB-Fail: " + test_title);
+}
+
 BOOST_AUTO_TEST_CASE(bnb_test)
 {
     std::vector<COutput> utxo_pool;
+
+    // Fail for empty UTXO pool
+    TestBnBFail("Empty UTXO pool", utxo_pool, /*selection_target=*/ 1 * CENT);
+
     AddCoins(utxo_pool, {1 * CENT, 3 * CENT, 5 * CENT});
 
     // Simple success cases
@@ -136,6 +145,13 @@ BOOST_AUTO_TEST_CASE(bnb_test)
 
     // BnB finds changeless solution while overshooting by up to cost_of_change
     TestBnBSuccess("Select upper bound", utxo_pool, /*selection_target=*/ 9 * CENT - default_cs_params.m_cost_of_change, /*expected_input_amounts=*/ {1 * CENT, 3 * CENT, 5 * CENT});
+    // BnB fails to find changeless solution when overshooting by cost_of_change + 1 sat
+    TestBnBFail("Overshoot upper bound", utxo_pool, /*selection_target=*/ 9 * CENT - default_cs_params.m_cost_of_change - 1);
+
+    // Simple cases without BnB solution
+    TestBnBFail("Smallest combination too big", utxo_pool, /*selection_target=*/ 0.5 * CENT);
+    TestBnBFail("No UTXO combination in target window", utxo_pool, /*selection_target=*/ 7 * CENT);
+    TestBnBFail("Select more than available", utxo_pool, /*selection_target=*/ 10 * CENT);
 
     // Test skipping of equivalent input sets
     std::vector<COutput> clone_pool;
