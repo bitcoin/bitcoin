f1684bb88a878eb3f54e945db39ea69b14256eef
Andrew Chow <github@achow101.com>
2023-10-19 09:43:00 -0400

    rpc: mention that migratewallet can take a while

--- src/wallet/rpc/wallet.cpp
+++ src/wallet/rpc/wallet.cpp
@@ -744,7 +744,8 @@ static RPCHelpMan migratewallet()
         "\nThe migration process will create a backup of the wallet before migrating. This backup\n"
         "file will be named <wallet name>-<timestamp>.legacy.bak and can be found in the directory\n"
         "for this wallet. In the event of an incorrect migration, the backup can be restored using restorewallet."
-        "\nEncrypted wallets must have the passphrase provided as an argument to this call.",
+        "\nEncrypted wallets must have the passphrase provided as an argument to this call.\n"
+        "\nThis RPC may take a long time to complete. Increasing the RPC client timeout is recommended.",
         {
             {"wallet_name", RPCArg::Type::STR, RPCArg::DefaultHint{"the wallet name from the RPC endpoint"}, "The name of the wallet to migrate. If provided both here and in the RPC endpoint, the two must be identical."},
             {"passphrase", RPCArg::Type::STR, RPCArg::Optional::OMITTED, "The wallet passphrase"},
