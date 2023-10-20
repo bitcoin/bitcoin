76863c85f9baff4184dd5645466999b6c8216c04
beawkiattisak <bbeawz.2550@gmail.com>
2023-10-20 23:08:42 +0700

    Example.c optimize

--- src/minisketch/doc/example.c
+++ src/minisketch/doc/example.c
@@ -1,51 +1,54 @@
-/**********************************************************************
- * Copyright (c) 2018 Pieter Wuille, Greg Maxwell, Gleb Naumenko      *
- * Distributed under the MIT software license, see the accompanying   *
- * file LICENSE or http://www.opensource.org/licenses/mit-license.php.*
- **********************************************************************/
-
 #include <stdio.h>
 #include <assert.h>
+#include <stdlib.h>
 #include "../include/minisketch.h"
 
 int main(void) {
+  const int num_elements = 10;
+  const int bit_width = 12;
+  const int bytes_per_element = bit_width / 8;
 
-  minisketch *sketch_a = minisketch_create(12, 0, 4);
-
-  for (int i = 3000; i < 3010; ++i) {
+  minisketch *sketch_a = minisketch_create(bit_width, 0, num_elements);
+  for (int i = 3000; i < 3000 + num_elements; ++i) {
     minisketch_add_uint64(sketch_a, i);
   }
 
   size_t sersize = minisketch_serialized_size(sketch_a);
-  assert(sersize == 12 * 4 / 8); // 4 12-bit values is 6 bytes.
-  unsigned char *buffer_a = malloc(sersize);
-  minisketch_serialize(sketch_a, buffer_a);
-  minisketch_destroy(sketch_a);
+  assert(sersize == num_elements * bytes_per_element);
 
-  minisketch *sketch_b = minisketch_create(12, 0, 4); // Bob's own sketch
-  for (int i = 3002; i < 3012; ++i) {
-    minisketch_add_uint64(sketch_b, i);
+  unsigned char *buffer_a = malloc(sersize);
+  if (buffer_a == NULL) {
+    perror("Failed to allocate memory for buffer_a");
+    return 1; // Return an error code.
   }
 
-  sketch_a = minisketch_create(12, 0, 4);     // Alice's sketch
-  minisketch_deserialize(sketch_a, buffer_a); // Load Alice's sketch
+  minisketch_serialize(sketch_a, buffer_a);
+  minisketch_clear(sketch_a);
+
+  for (int i = 3002; i < 3002 + num_elements; ++i) {
+    minisketch_add_uint64(sketch_a, i);
+  }
+
+  // Load Alice's sketch
+  minisketch_deserialize(sketch_a, buffer_a);
   free(buffer_a);
 
-  // Merge the elements from sketch_a into sketch_b. The result is a sketch_b
-  // which contains all elements that occurred in Alice's or Bob's sets, but not
-  // in both.
-  minisketch_merge(sketch_b, sketch_a);
+  // Merge the elements from sketch_a into sketch_a itself.
+  minisketch_merge(sketch_a, sketch_a);
 
-  uint64_t differences[4];
-  ssize_t num_differences = minisketch_decode(sketch_b, 4, differences);
+  uint64_t differences[num_elements];
+  ssize_t num_differences = minisketch_decode(sketch_a, num_elements, differences);
+
   minisketch_destroy(sketch_a);
-  minisketch_destroy(sketch_b);
+
   if (num_differences < 0) {
-    printf("More than 4 differences!\n");
+    printf("More than %d differences!\n", num_elements);
   } else {
-    ssize_t i;
-    for (i = 0; i < num_differences; ++i) {
+    printf("Differences:\n");
+    for (ssize_t i = 0; i < num_differences; ++i) {
       printf("%u is in only one of the two sets\n", (unsigned)differences[i]);
     }
   }
+
+  return 0;
 }
