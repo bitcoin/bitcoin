1251e00370e9709532d0acc37a4f1701a53dc96b
alex v <alex@encrypt-s.com>
2023-10-22 11:12:26 +0200

    trigger ci

--- src/test/coins_tests.cpp
+++ src/test/coins_tests.cpp
@@ -101,7 +101,6 @@ BOOST_FIXTURE_TEST_SUITE(coins_tests, BasicTestingSetup)
 
 static const unsigned int NUM_SIMULATION_ITERATIONS = 40000;
 
-
 // This is a large randomized insert/remove simulation test on a variable-size
 // stack of caches on top of CCoinsViewTest.
 //
