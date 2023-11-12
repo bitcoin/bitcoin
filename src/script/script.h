726b66b69158117744fda60550d2aabda9f9acbc
Steven Roose <steven@stevenroose.org>
2023-11-12 10:02:29 +0000

    script: Add CScript::IsPayToTaproot fast getter

--- src/script/script.h
+++ src/script/script.h
@@ -535,6 +535,7 @@ public:
 
     bool IsPayToScriptHash() const;
     bool IsPayToWitnessScriptHash() const;
+    bool IsPayToTaproot() const;
     bool IsWitnessProgram(int& version, std::vector<unsigned char>& program) const;
 
     /** Called by IsStandardTx and P2SH/BIP62 VerifyScript (which makes it consensus-critical). */
