7fe698da3fc0b7ead9757a8f8e74d33efd97478b
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2023-11-23 23:22:08 +0100

    policy: update `AreInputsStandard` to return error string
    This commit renames AreInputsStandard to ValidateInputsStandardness.
    
    ValidateInputsStandardness now returns valid TxValidationState if all inputs
    (scriptSigs) use only standard transaction forms else returns invalid
    TxValidationState which states why an input is not standard.

--- src/test/fuzz/transaction.cpp
+++ src/test/fuzz/transaction.cpp
@@ -89,7 +89,7 @@ FUZZ_TARGET(transaction, .init = initialize_transaction)
 
     CCoinsView coins_view;
     const CCoinsViewCache coins_view_cache(&coins_view);
-    (void)AreInputsStandard(tx, coins_view_cache);
+    (void)ValidateInputsStandardness(tx, coins_view_cache);
     (void)IsWitnessStandard(tx, coins_view_cache);
 
     if (tx.GetTotalSize() < 250'000) { // Avoid high memory usage (with msan) due to json encoding
