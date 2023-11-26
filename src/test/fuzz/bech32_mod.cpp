f3bae813775db67f063f6db5f5fff8f56d1a9d58
gogoex <110195520+gogoex@users.noreply.github.com>
2023-11-26 19:56:47 +0900

    fix fuzz bech32

--- src/test/fuzz/bech32_mod.cpp
+++ src/test/fuzz/bech32_mod.cpp
@@ -7,15 +7,32 @@
 #include <test/util/str.h>
 #include <util/strencodings.h>
 
+#include <algorithm>
 #include <cassert>
 #include <cstdint>
+#include <random>
 #include <string>
 #include <utility>
 #include <vector>
 
 FUZZ_TARGET(bech32_mod)
 {
-    const std::string random_string(buffer.begin(), buffer.end());
+    // create 154-byte buf from the given buffer
+    std::vector<uint8_t> buf(buffer.begin(), buffer.end());
+
+    if (buf.size() > 154) {
+        buf.resize(154);
+    } else if (buf.size() < 154) {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<uint8_t> dist(0, 255);
+
+        while (buf.size() < 154) {
+            buf.push_back(dist(gen));
+        }
+    }
+
+    const std::string random_string(buf.begin(), buf.end());
     const auto r1 = bech32_mod::Decode(random_string);
     if (r1.hrp.empty()) {
         assert(r1.encoding == bech32_mod::Encoding::INVALID);
@@ -26,18 +43,17 @@ FUZZ_TARGET(bech32_mod)
         assert(CaseInsensitiveEqual(random_string, reencoded));
     }
 
+    // make the buf valid for encoding
+    buf.resize(96);
     std::vector<unsigned char> input;
-    ConvertBits<8, 5, true>([&](unsigned char c) { input.push_back(c); }, buffer.begin(), buffer.end());
+    ConvertBits<8, 5, true>([&](unsigned char c) { input.push_back(c); }, buf.begin(), buf.end());
 
-    if (input.size() + 3 + 6 <= 90) {
-        // If it's possible to encode input in Bech32(m) without exceeding the 90-character limit:
-        for (auto encoding : {bech32_mod::Encoding::BECH32, bech32_mod::Encoding::BECH32M}) {
-            const std::string encoded = bech32_mod::Encode(encoding, "bc", input);
-            assert(!encoded.empty());
-            const auto r2 = bech32_mod::Decode(encoded);
-            assert(r2.encoding == encoding);
-            assert(r2.hrp == "bc");
-            assert(r2.data == input);
-        }
+    for (auto encoding : {bech32_mod::Encoding::BECH32, bech32_mod::Encoding::BECH32M}) {
+        const std::string encoded = bech32_mod::Encode(encoding, "nv", input);
+        assert(!encoded.empty());
+        const auto r2 = bech32_mod::Decode(encoded);
+        assert(r2.encoding == encoding);
+        assert(r2.hrp == "nv");
+        assert(r2.data == input);
     }
 }
