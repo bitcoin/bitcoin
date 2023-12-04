69e53d1e47613efb831d01e446b790d21c35ba4b
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2023-12-04 09:44:21 +0100

    ci: Use Ubuntu 24.04 Noble for tsan,tidy,fuzz
    Github-Pull: #28992
    Rebased-From: fa83b65ef8934b44fbac02da8dbc27fc0bc230e6

--- ci/test/00_setup_env_native_tsan.sh
+++ ci/test/00_setup_env_native_tsan.sh
@@ -7,7 +7,7 @@
 export LC_ALL=C.UTF-8
 
 export CONTAINER_NAME=ci_native_tsan
-export CI_IMAGE_NAME_TAG="docker.io/ubuntu:23.10"  # This version will reach EOL in Jul 2024, and can be replaced by "ubuntu:24.04" (or anything else that ships the wanted clang version).
+export CI_IMAGE_NAME_TAG="docker.io/ubuntu:24.04"
 export PACKAGES="clang-17 llvm-17 libclang-rt-17-dev libc++abi-17-dev libc++-17-dev python3-zmq"
 export DEP_OPTS="CC=clang-17 CXX='clang++-17 -stdlib=libc++'"
 export GOAL="install"
