fa87f8feb76da42eeb5c4d32ee7be070b2bd559f
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2023-12-09 14:55:01 +0100

    doc: Clarify C++20 comments

--- src/test/fuzz/fuzz.cpp
+++ src/test/fuzz/fuzz.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2009-2022 The Bitcoin Core developers
+// Copyright (c) 2009-present The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -71,7 +71,7 @@ auto& FuzzTargets()
 
 void FuzzFrameworkRegisterTarget(std::string_view name, TypeTestOneInput target, FuzzTargetOptions opts)
 {
-    const auto it_ins{FuzzTargets().try_emplace(name, FuzzTarget /* temporary can be dropped in C++20 */ {std::move(target), std::move(opts)})};
+    const auto it_ins{FuzzTargets().try_emplace(name, FuzzTarget /* temporary can be dropped after clang-16 */ {std::move(target), std::move(opts)})};
     Assert(it_ins.second);
 }
 
--- src/util/types.h
+++ src/util/types.h
@@ -5,6 +5,7 @@
 #ifndef BITCOIN_UTIL_TYPES_H
 #define BITCOIN_UTIL_TYPES_H
 
+// Not needed after C++23 (DR, https://cplusplus.github.io/CWG/issues/2518.html)
 template <class>
 inline constexpr bool ALWAYS_FALSE{false};
 
