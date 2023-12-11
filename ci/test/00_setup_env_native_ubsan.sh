4a753efa223a7e5517d23784ffd319ee6786861f
Konstantin Akimov <knstqq@gmail.com>
2023-12-12 03:24:17 +0700

    partial Merge #20182: ci: Build with --enable-werror by default, and document exceptions
    It does not includes changes that actually enable werror by default.
    But this backport is necessary to prevent compilation error on mac such as this one:
    
        In file included from mapport.cpp:26:
        In file included from /builds/dashpay/dash/depends/x86_64-apple-darwin/include/miniupnpc/miniupnpc.h:14:
        /builds/dashpay/dash/depends/x86_64-apple-darwin/include/miniupnpc/upnpdev.h:27:14: error: zero size arrays are an extension [-Werror,-Wzero-length-array]
                char buffer[0];

--- ci/test/00_setup_env_native_ubsan.sh
+++ ci/test/00_setup_env_native_ubsan.sh
@@ -11,6 +11,6 @@ export CONTAINER_NAME=ci_native_ubsan
 export PACKAGES="clang-15 llvm-15 python3-zmq qtbase5-dev qttools5-dev-tools libevent-dev bsdmainutils libboost-filesystem-dev libboost-test-dev libboost-thread-dev libdb5.3++-dev libminiupnpc-dev libzmq3-dev libqrencode-dev"
 export DEP_OPTS="NO_UPNP=1 DEBUG=1"
 export GOAL="install"
-export BITCOIN_CONFIG="--enable-zmq --enable-reduce-exports --enable-crash-hooks --with-sanitizers=undefined --enable-suppress-external-warnings CC=clang-15 CXX=clang++-15"
+export BITCOIN_CONFIG="--enable-zmq --enable-reduce-exports --enable-crash-hooks --with-sanitizers=undefined CC=clang-15 CXX=clang++-15"
 export PYZMQ=true
 export RUN_SYMBOL_TESTS=false
