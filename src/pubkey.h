999962d68d47e1e630d689aca880f41635c004cb
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2023-12-22 15:45:33 +0100

    Add missing XOnlyPubKey::data() to get mutable data
    This is needed for consistency, and also to allow std::span construction
    from XOnlyPubKey.

--- src/pubkey.h
+++ src/pubkey.h
@@ -285,10 +285,11 @@ public:
     CPubKey GetEvenCorrespondingCPubKey() const;
 
     const unsigned char& operator[](int pos) const { return *(m_keydata.begin() + pos); }
-    const unsigned char* data() const { return m_keydata.begin(); }
     static constexpr size_t size() { return decltype(m_keydata)::size(); }
+    const unsigned char* data() const { return m_keydata.begin(); }
     const unsigned char* begin() const { return m_keydata.begin(); }
     const unsigned char* end() const { return m_keydata.end(); }
+    unsigned char* data() { return m_keydata.begin(); }
     unsigned char* begin() { return m_keydata.begin(); }
     unsigned char* end() { return m_keydata.end(); }
     bool operator==(const XOnlyPubKey& other) const { return m_keydata == other.m_keydata; }
