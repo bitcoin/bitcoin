081fa0e3d39cd4b79669255eb70b145518b7e369
mxaddict <mxaddict@codedmaster.com>
2023-12-24 04:45:01 +0800

    Updated src/test/blsct/building_block/imp_inner_prod_arg_tests.cpp to use emplace_back instead of push_back

--- src/test/blsct/building_block/imp_inner_prod_arg_tests.cpp
+++ src/test/blsct/building_block/imp_inner_prod_arg_tests.cpp
@@ -56,9 +56,9 @@ BOOST_AUTO_TEST_CASE(test_exec_ypow_loop)
     std::vector<Scalar> act;
     ImpInnerProdArg::LoopWithYPows<Mcl>(3, y,
         [&](const size_t& i, const Scalar& y_pow, const Scalar& y_inv_pow) {
-            act.push_back(Scalar(i));
-            act.push_back(y_pow);
-            act.push_back(y_inv_pow);
+            act.emplace_back(Scalar(i));
+            act.emplace_back(y_pow);
+            act.emplace_back(y_inv_pow);
         }
     );
     Scalar zero = Scalar(0);
