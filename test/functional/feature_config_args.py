157a11d2b341ba4eb3e833c267e6e5ee5fb2f4c4
mxaddict <mxaddict@codedmaster.com>
2023-12-24 02:12:08 +0800

    Fixed linting issue with test/functional/feature_config_args.py

--- test/functional/feature_config_args.py
+++ test/functional/feature_config_args.py
@@ -413,7 +413,7 @@ class ConfArgsTest(BitcoinTestFramework):
 
         self.nodes[0].assert_start_raises_init_error([f'-conf={conf_file}'], f'Error: Error reading configuration file: specified data directory "{new_data_dir}" does not exist.')
 
-        navheck that an explicitly specified config file that cannot be opened fails
+        # Check that an explicitly specified config file that cannot be opened fails
         none_existent_conf_file = default_data_dir / "none_existent_navcoin.conf"
         self.nodes[0].assert_start_raises_init_error(['-conf=' + f'{none_existent_conf_file}'], 'Error: Error reading configuration file: specified config file "' + f'{none_existent_conf_file}' + '" could not be opened.')
 
