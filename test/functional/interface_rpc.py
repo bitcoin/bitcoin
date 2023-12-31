92c651394bcb1c476cc62989936edd621b5d4aa5
Matthew Zipkin <pinheadmz@gmail.com>
2023-12-31 07:55:03 -0500

    test: cover JSONRPC 2.0 requests, batches, and notifications

--- test/functional/interface_rpc.py
+++ test/functional/interface_rpc.py
@@ -11,21 +11,28 @@ from test_framework.util import assert_equal, assert_greater_than_or_equal
 from threading import Thread
 import subprocess
 
-RPC_INVALID_PARAMETER = -8
-RPC_METHOD_NOT_FOUND  = -32601
-RPC_INVALID_REQUEST   = -32600
-RPC_PARSE_ERROR       = -32700
+RPC_INVALID_ADDRESS_OR_KEY = -5
+RPC_INVALID_PARAMETER      = -8
+RPC_METHOD_NOT_FOUND       = -32601
+RPC_INVALID_REQUEST        = -32600
+RPC_PARSE_ERROR            = -32700
 
 
-class Format:
-    ID_COUNT = 0
+class RequestBuilder:
+    def __init__(self):
+        self.id_count = 0
 
-    @staticmethod
-    def rpc_request(body):
-        req = body
-        req["version"] = "1.1"
-        req["id"] = Format.ID_COUNT
-        Format.ID_COUNT += 1
+    def rpc_request(self, fields, version=None, notification=False):
+        req = dict(**fields)
+        if not version:
+            req["version"] = "1.1"
+        if version == 1:
+            req["jsonrpc"] = "1.0"
+        if version == 2:
+            req["jsonrpc"] = "2.0"
+        if not notification:
+            req["id"] = self.id_count
+            self.id_count += 1
         return req
 
 
@@ -38,8 +45,8 @@ def send_json_rpc(node, body: object) -> tuple[object, int]:
     return send_raw_rpc(node, raw)
 
 
-def expect_http_rpc_status(expected_http_status, expected_rpc_error_code, node, method, params):
-    req = Format.rpc_request({"method": method, "params": params})
+def expect_http_rpc_status(expected_http_status, expected_rpc_error_code, node, method, params, version=1, notification=False):
+    req = RequestBuilder().rpc_request({"method": method, "params": params}, version, notification)
     response, status = send_json_rpc(node, req)
 
     if expected_rpc_error_code is not None:
@@ -75,8 +82,6 @@ class RPCInterfaceTest(BitcoinTestFramework):
         assert_equal(info['logpath'], os.path.join(self.nodes[0].chain_path, 'debug.log'))
 
     def test_batch_request(self):
-        self.log.info("Testing basic JSON-RPC batch request...")
-
         commands = [
             # A basic request that will work fine.
             {"method": "getblockcount"},
@@ -87,13 +92,24 @@ class RPCInterfaceTest(BitcoinTestFramework):
             # Invalid request format
             {"pizza": "sausage"}
         ]
+        builder = RequestBuilder()
 
+        self.log.info("Testing empty batch request...")
         body = []
-        for cmd in commands:
-            body.append(Format.rpc_request(cmd))
-
         response, status = send_json_rpc(self.nodes[0], body)
         assert_equal(status, 200)
+        assert_equal(
+            response,
+            []
+        )
+
+        self.log.info("Testing default JSON-RPC 1.1 batch request...")
+        body = []
+        for cmd in commands:
+            body.append(builder.rpc_request(cmd))
+        response, status = send_json_rpc(self.nodes[0], body)
+        assert_equal(status, 200)
+        # JSON 1.1: Every response has a "result" and an "error"
         assert_equal(
             response,
             [
@@ -104,15 +120,132 @@ class RPCInterfaceTest(BitcoinTestFramework):
             ]
         )
 
+        self.log.info("Testing basic JSON-RPC 1.0 batch request...")
+        body = []
+        for cmd in commands:
+            body.append(builder.rpc_request(cmd, version=1))
+        response, status = send_json_rpc(self.nodes[0], body)
+        assert_equal(status, 200)
+        # JSON 1.1: Every response has a "result" and an "error"
+        assert_equal(
+            response,
+            [
+                {"result": 0, "error": None, "id": 4},
+                {"result": None, "error": {"code": RPC_METHOD_NOT_FOUND, "message": "Method not found"}, "id": 5},
+                {"result": "0f9188f13cb7b2c71f2a335e3a4fc328bf5beb436012afca590b1a11466e2206", "error": None, "id": 6},
+                {"result": None, "error": {"code": RPC_INVALID_REQUEST, "message": "Missing method"}, "id": 7}
+            ]
+        )
+
+        self.log.info("Testing basic JSON-RPC 2.0 batch request...")
+        body = []
+        for cmd in commands:
+            body.append(builder.rpc_request(cmd, version=2))
+        response, status = send_json_rpc(self.nodes[0], body)
+        assert_equal(status, 200)
+        # JSON 2.0: Each response has either a "result" or an "error"
+        assert_equal(
+            response,
+            [
+                {"jsonrpc": "2.0", "result": 0, "id": 8},
+                {"jsonrpc": "2.0", "error": {"code": RPC_METHOD_NOT_FOUND, "message": "Method not found"}, "id": 9},
+                {"jsonrpc": "2.0", "result": "0f9188f13cb7b2c71f2a335e3a4fc328bf5beb436012afca590b1a11466e2206", "id": 10},
+                {"jsonrpc": "2.0", "error": {"code": RPC_INVALID_REQUEST, "message": "Missing method"}, "id": 11}
+            ]
+        )
+
+        self.log.info("Testing mixed JSON-RPC 1.1/2.0 batch request...")
+        body = []
+        version = 1
+        for cmd in commands:
+            body.append(builder.rpc_request(cmd, version=version))
+            version = 2 if version == 1 else 1
+        response, status = send_json_rpc(self.nodes[0], body)
+        assert_equal(status, 200)
+        # Responses respect the version of the individual request in the batch
+        assert_equal(
+            response,
+            [
+                {"result": 0, "error": None, "id": 12},
+                {"jsonrpc": "2.0", "error": {"code": RPC_METHOD_NOT_FOUND, "message": "Method not found"}, "id": 13},
+                {"result": "0f9188f13cb7b2c71f2a335e3a4fc328bf5beb436012afca590b1a11466e2206", "error": None, "id": 14},
+                {"jsonrpc": "2.0", "error": {"code": RPC_INVALID_REQUEST, "message": "Missing method"}, "id": 15}
+            ]
+        )
+
+        self.log.info("Testing JSON-RPC 2.0 batch with notifications...")
+        body = []
+        notification = False
+        for cmd in commands:
+            body.append(builder.rpc_request(cmd, version=2, notification=notification))
+            notification = True
+        response, status = send_json_rpc(self.nodes[0], body)
+        assert_equal(status, 200)
+        # Notifications are never responded to, even if there is an error
+        assert_equal(
+            response,
+            [
+                {"jsonrpc": "2.0", "result": 0, "id": 16}
+            ]
+        )
+
+        self.log.info("Testing JSON-RPC 2.0 batch of ALL notifications...")
+        body = []
+        for cmd in commands:
+            body.append(builder.rpc_request(cmd, version=2, notification=True))
+        response, status = send_json_rpc(self.nodes[0], body)
+        assert_equal(status, 204) # HTTP_NO_CONTENT
+        assert_equal(response, None) # not even an empty array
+
     def test_http_status_codes(self):
-        self.log.info("Testing HTTP status codes for JSON-RPC requests...")
+        self.log.info("Testing HTTP status codes for JSON-RPC 1.1 requests...")
+        # OK
+        expect_http_rpc_status(200, None,                  self.nodes[0], "getblockhash", [0])
+        # Errors
         expect_http_rpc_status(404, RPC_METHOD_NOT_FOUND,  self.nodes[0], "invalidmethod", [])
         expect_http_rpc_status(500, RPC_INVALID_PARAMETER, self.nodes[0], "getblockhash", [42])
-        expect_http_rpc_status(200, None,                  self.nodes[0], "getblockhash", [0])
         # force-send invalidly formatted request
         response, status = send_raw_rpc(self.nodes[0], b"this is bad")
+        assert_equal(response["id"], None)
         assert_equal(response["error"]["code"], RPC_PARSE_ERROR)
         assert_equal(status, 500)
+
+        self.log.info("Testing HTTP status codes for JSON-RPC 2.0 requests...")
+        # OK
+        expect_http_rpc_status(200, None,                   self.nodes[0], "getblockhash", [0],  2, False)
+        # RPC errors but not HTTP errors
+        expect_http_rpc_status(200, RPC_METHOD_NOT_FOUND,   self.nodes[0], "invalidmethod", [],  2, False)
+        expect_http_rpc_status(200, RPC_INVALID_PARAMETER,  self.nodes[0], "getblockhash", [42], 2, False)
+        # force-send invalidly formatted requests
+        response, status = send_json_rpc(self.nodes[0], {"jsonrpc": 2, "method": "getblockcount"})
+        assert_equal(response["id"], None)
+        assert_equal(response["error"]["code"], RPC_INVALID_REQUEST)
+        assert_equal(response["error"]["message"], "jsonrpc field must be a string")
+        assert_equal(status, 400)
+        response, status = send_json_rpc(self.nodes[0], {"jsonrpc": "3.0", "method": "getblockcount"})
+        assert_equal(response["id"], None)
+        assert_equal(response["error"]["code"], RPC_INVALID_REQUEST)
+        assert_equal(response["error"]["message"], "JSON-RPC version not supported")
+        assert_equal(status, 400)
+
+        self.log.info("Testing HTTP status codes for JSON-RPC 2.0 notifications...")
+        # Not notification: id exists
+        response, status = send_json_rpc(self.nodes[0], {"jsonrpc": "2.0", "id": None, "method": "getblockcount"})
+        assert_equal(response["result"], 0)
+        assert_equal(status, 200)
+        # Not notification: JSON 1.1
+        expect_http_rpc_status(200, None,                   self.nodes[0], "getblockcount", [],  1)
+        # Not notification: has "id" field
+        expect_http_rpc_status(200, None,                   self.nodes[0], "getblockcount", [],  2, False)
+        block_count = self.nodes[0].getblockcount()
+        # Notification response status code: HTTP_NO_CONTENT
+        expect_http_rpc_status(204, None,                   self.nodes[0], "generatetoaddress", [1, "bcrt1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqdku202"],  2, True)
+        # The command worked even though there was no response
+        assert_equal(block_count + 1, self.nodes[0].getblockcount())
+        # No error response for notifications even if they are invalid
+        expect_http_rpc_status(204, None,                   self.nodes[0], "generatetoaddress", [1, "invalid_address"], 2, True)
+        # Sanity check: command was not executed
+        assert_equal(block_count + 1, self.nodes[0].getblockcount())
 
     def test_work_queue_exceeded(self):
         self.log.info("Testing work queue exceeded...")
