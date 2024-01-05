0b3f06ff8cdfad225de756ebdbaef35925257582
Ava Chow <github@achow101.com>
2024-01-05 18:42:04 -0500

    test: remove legacy wallet functional tests
    Removes all legacy wallet specific functional tests.
    
    Also removes the --descriptor and --legacy-wallet options as these are
    no longer necessary with the legacy wallet removed.

--- test/functional/rpc_help.py
+++ test/functional/rpc_help.py
@@ -43,12 +43,10 @@ def process_mapping(fname):
 
 
 class HelpRpcTest(BitcoinTestFramework):
-    def add_options(self, parser):
-        self.add_wallet_options(parser)
-
     def set_test_params(self):
         self.num_nodes = 1
         self.supports_cli = False
+        self.uses_wallet = None
 
     def run_test(self):
         self.test_client_conversion_table()
