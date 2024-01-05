62d900267ecdc7ab57dc27c393a2186db415f8ea
fanquake <fanquake@gmail.com>
2024-01-05 15:52:57 +0000

    build: use system Clang & LLD when compiling for macOS
    Use LLD when compiling for macOS.
    Always use the system Clang when compiling for macOS (remove
    FORCE_USE_SYSTEM_CLANG).

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -31,9 +31,4 @@ darwin_native_packages =
 
 ifneq ($(build_os),darwin)
 darwin_native_packages += native_cctools native_libtapi
-
-ifeq ($(strip $(FORCE_USE_SYSTEM_CLANG)),)
-darwin_native_packages+= native_llvm
-endif
-
 endif
