aaaace2fd1299939c755c281b787df0bbf1747a0
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2024-01-09 16:47:36 +0100

    fuzz: Assume presence of __builtin_*_overflow, without checks

--- src/test/fuzz/multiplication_overflow.cpp
+++ src/test/fuzz/multiplication_overflow.cpp
@@ -10,14 +10,6 @@
 #include <string>
 #include <vector>
 
-#if defined(__has_builtin)
-#if __has_builtin(__builtin_mul_overflow)
-#define HAVE_BUILTIN_MUL_OVERFLOW
-#endif
-#elif defined(__GNUC__)
-#define HAVE_BUILTIN_MUL_OVERFLOW
-#endif
-
 namespace {
 template <typename T>
 void TestMultiplicationOverflow(FuzzedDataProvider& fuzzed_data_provider)
@@ -25,18 +17,12 @@ void TestMultiplicationOverflow(FuzzedDataProvider& fuzzed_data_provider)
     const T i = fuzzed_data_provider.ConsumeIntegral<T>();
     const T j = fuzzed_data_provider.ConsumeIntegral<T>();
     const bool is_multiplication_overflow_custom = MultiplicationOverflow(i, j);
-#if defined(HAVE_BUILTIN_MUL_OVERFLOW)
     T result_builtin;
     const bool is_multiplication_overflow_builtin = __builtin_mul_overflow(i, j, &result_builtin);
     assert(is_multiplication_overflow_custom == is_multiplication_overflow_builtin);
     if (!is_multiplication_overflow_custom) {
         assert(i * j == result_builtin);
     }
-#else
-    if (!is_multiplication_overflow_custom) {
-        (void)(i * j);
-    }
-#endif
 }
 } // namespace
 
