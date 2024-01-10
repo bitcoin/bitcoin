a28c6bbde0bcd064869cf861d92a6e23df4a7b73
Cory Fields <cory-nospam-@coryfields.com>
2024-01-10 19:01:49 +0000

    depends: remove (darwin) libtool now that it's no longer used
    Note that this is completely unrelated to gnu usage of libtool.

--- depends/builders/darwin.mk
+++ depends/builders/darwin.mk
@@ -16,7 +16,6 @@ darwin_CXX:=$(shell xcrun -f clang++) -mmacosx-version-min=$(OSX_MIN_VERSION) -s
 darwin_AR:=$(shell xcrun -f ar)
 darwin_RANLIB:=$(shell xcrun -f ranlib)
 darwin_STRIP:=$(shell xcrun -f strip)
-darwin_LIBTOOL:=$(shell xcrun -f libtool)
 darwin_OTOOL:=$(shell xcrun -f otool)
 darwin_NM:=$(shell xcrun -f nm)
 darwin_INSTALL_NAME_TOOL:=$(shell xcrun -f install_name_tool)
