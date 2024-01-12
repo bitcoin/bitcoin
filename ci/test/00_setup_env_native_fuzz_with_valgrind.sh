8085729789b597a38bbfaf4d185f5ecc15ba04d3
Konstantin Akimov <knstqq@gmail.com>
2024-01-13 02:34:20 +0700

    chore: dashification: drop DOCKER_NAME_TAG
    We have only one docker image for all functional test and this option is confusing

--- ci/test/00_setup_env_native_fuzz_with_valgrind.sh
+++ ci/test/00_setup_env_native_fuzz_with_valgrind.sh
@@ -6,7 +6,6 @@
 
 export LC_ALL=C.UTF-8
 
-export DOCKER_NAME_TAG="ubuntu:20.04"
 export CONTAINER_NAME=ci_native_fuzz_valgrind
 export PACKAGES="clang llvm python3 libevent-dev bsdmainutils libboost-system-dev libboost-filesystem-dev libboost-test-dev valgrind"
 export NO_DEPENDS=1
