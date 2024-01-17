3ce35e43bca575ffb1cc61182577dae8dadcf04f
Gleb Naumenko <naumenko.gs@gmail.com>
2024-01-17 11:28:02 +0200

    p2p: Cache inbound reconciling peers count
    It helps to avoid recomputing every time we consider
    a transaction for fanout/reconciliation.
    
    Co-authored-by: Sergi Delgado Segura <sergi.delgado.s@gmail.com>

--- src/node/txreconciliation_impl.cpp
+++ src/node/txreconciliation_impl.cpp
@@ -29,6 +29,9 @@ private:
      */
     std::unordered_map<NodeId, std::variant<uint64_t, TxReconciliationState>> m_states GUARDED_BY(m_txreconciliation_mutex);
 
+    /** Keeps track of how many of the registered peers are inbound. Updated on registering or forgetting peers. */
+    size_t m_inbounds_count GUARDED_BY(m_txreconciliation_mutex){0};
+
     TxReconciliationState* GetRegisteredPeerState(NodeId peer_id) EXCLUSIVE_LOCKS_REQUIRED(m_txreconciliation_mutex)
     {
         AssertLockHeld(m_txreconciliation_mutex);
@@ -88,6 +91,9 @@ public:
         bool emplaced = m_states.emplace(peer_id, std::move(new_state)).second;
         Assume(emplaced);
 
+        if (is_peer_inbound && m_inbounds_count < std::numeric_limits<size_t>::max()) {
+            ++m_inbounds_count;
+        }
         return std::nullopt;
     }
 
@@ -105,6 +111,15 @@ public:
     {
         AssertLockNotHeld(m_txreconciliation_mutex);
         LOCK(m_txreconciliation_mutex);
+        const auto peer = m_states.find(peer_id);
+        if (peer == m_states.end()) return;
+
+        const auto registered = std::get_if<TxReconciliationState>(&peer->second);
+        if (registered && !registered->m_we_initiate) {
+            Assert(m_inbounds_count > 0);
+            --m_inbounds_count;
+        }
+
         if (m_states.erase(peer_id)) {
             LogDebug(BCLog::TXRECONCILIATION, "Forget txreconciliation state of peer=%d.\n", peer_id);
         }
