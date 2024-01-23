ebf8ff2a341fa1af474d29e669311792ba0baa0f
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2024-01-23 21:19:16 +0000

    merge bitcoin#29298: patch libtool out of libnatpmp/miniupnpc

--- depends/packages/libnatpmp.mk
+++ depends/packages/libnatpmp.mk
@@ -3,12 +3,16 @@ $(package)_version=07004b97cf691774efebe70404cf22201e4d330d
 $(package)_download_path=https://github.com/miniupnp/libnatpmp/archive
 $(package)_file_name=$($(package)_version).tar.gz
 $(package)_sha256_hash=9321953ceb39d07c25463e266e50d0ae7b64676bb3a986d932b18881ed94f1fb
+$(package)_patches=no_libtool.patch
 
 define $(package)_set_vars
   $(package)_build_opts=CC="$($(package)_cc)"
   $(package)_build_opts_mingw32=CPPFLAGS=-DNATPMP_STATICLIB
-  $(package)_build_opts_darwin=LIBTOOL="$($(package)_libtool)"
   $(package)_build_env+=CFLAGS="$($(package)_cflags) $($(package)_cppflags)" AR="$($(package)_ar)"
+endef
+
+define $(package)_preprocess_cmds
+  patch -p1 < $($(package)_patch_dir)/no_libtool.patch
 endef
 
 define $(package)_build_cmds
