99b79389ff43dd0d640be6db19b5279d5ca36d8f
Vasil Dimov <vd@FreeBSD.org>
2024-01-30 18:25:23 +0100

    net_processing: retry private broadcast
    Periodically check for stale transactions in peerman and if found,
    reschedule new connections to be opened by connman for broadcasting
    them.

--- src/private_broadcast.h
+++ src/private_broadcast.h
@@ -82,6 +82,12 @@ public:
     bool DidNodeConfirmReception(const NodeId& nodeid)
         EXCLUSIVE_LOCKS_REQUIRED(!m_mutex);
 
+    /**
+     * Get the transactions that have not been broadcast recently.
+     */
+    std::vector<CTransactionRef> GetStale() const
+        EXCLUSIVE_LOCKS_REQUIRED(!m_mutex);
+
 private:
     /// Status of a transaction sent to a given node.
     struct SendStatus {
