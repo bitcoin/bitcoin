8b9ba608123dc05b54c83a009a658e5d33fe6b67
Vasil Dimov <vd@FreeBSD.org>
2024-01-30 18:51:23 +0100

    net_processing: stop private broadcast of a transaction after round-trip
    Remove the transaction from the list of transactions to broadcast after
    we receive it from the network.
    
    Only remove the transaction if it is the same as the one we sent: both
    txid and wtxid match. Don't remove transactions that have the same txid
    and different wxtid. Such transactions show that some of the private
    broadcast recipients malleated the witness and the transaction made it
    back to us. The witness could be either:
    * invalid, in which case the transaction will not be accepted in
      anybody's pool; or
    * valid, in which case either the original or the malleated transaction
      will make it to nodes' mempools and eventually be mined. Our response
      is to keep broadcasting the original. If the malleated transaction
      wins then we will eventually stop broadcasting the original when it
      gets stale and gets removed from the "to broadcast" storage cause it
      is not acceptable in our mempool.

--- src/private_broadcast.cpp
+++ src/private_broadcast.cpp
@@ -16,6 +16,19 @@ bool PrivateBroadcast::Add(const CTransactionRef& tx) EXCLUSIVE_LOCKS_REQUIRED(!
     return inserted;
 }
 
+std::optional<size_t> PrivateBroadcast::Remove(const CTransactionRef& tx) EXCLUSIVE_LOCKS_REQUIRED(!m_mutex)
+{
+    LOCK(m_mutex);
+    auto iters = Find(tx->GetHash());
+    if (!iters || iters->by_txid->second.tx->GetWitnessHash() != tx->GetWitnessHash()) {
+        return std::nullopt;
+    }
+    const size_t num_broadcasted{iters->by_priority->first.num_broadcasted};
+    m_by_priority.erase(iters->by_priority);
+    m_by_txid.erase(iters->by_txid);
+    return num_broadcasted;
+}
+
 std::optional<CTransactionRef> PrivateBroadcast::GetTxForBroadcast() EXCLUSIVE_LOCKS_REQUIRED(!m_mutex)
 {
     LOCK(m_mutex);
