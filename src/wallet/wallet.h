b217f576a5f284ac7261986ee6ec9334bbbdbbc6
Ava Chow <github@achow101.com>
2024-02-20 11:49:58 -0500

    wallet: Change balance calculation to use m_txos
    Since we track the outputs owned by the wallet with m_txos, we can now
    calculate the balance of the wallet by iterating m_txos and summing up
    the amounts of the unspent txos.

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -507,6 +507,8 @@ public:
 
     std::set<uint256> GetTxConflicts(const CWalletTx& wtx) const EXCLUSIVE_LOCKS_REQUIRED(cs_wallet);
 
+    const std::unordered_map<COutPoint, WalletTXO, SaltedOutpointHasher>& GetTXOs() const EXCLUSIVE_LOCKS_REQUIRED(cs_wallet) { AssertLockHeld(cs_wallet); return m_txos; };
+
     void RefreshWalletTxTXOs(const CWalletTx& wtx) EXCLUSIVE_LOCKS_REQUIRED(cs_wallet);
 
     /**
