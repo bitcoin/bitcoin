1c083188666bab6edac4e522a297f41f907de819
Ava Chow <github@achow101.com>
2024-02-20 11:54:40 -0500

    wallet: Have WalletTXOs also store parent tx time
    WalletTXOs need to know their parent tx's timestamp for AvailableCoins
    to work.

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -4532,7 +4532,7 @@ void CWallet::RefreshTXOsFromTx(const CWalletTx& wtx)
             it->second.SetState(wtx.m_state);
             it->second.SetTxFromMe(*wtx.m_from_me);
         } else {
-            m_txos.emplace(outpoint, WalletTXO{wtx, txout, wtx.m_state, wtx.IsCoinBase(), *wtx.m_from_me});
+            m_txos.emplace(outpoint, WalletTXO{wtx, txout, wtx.m_state, wtx.IsCoinBase(), *wtx.m_from_me, wtx.GetTxTime()});
         }
     }
 }
