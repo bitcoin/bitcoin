2dd38182d558a1763679affe543cd14011fd2160
Lőrinc <pap.lorinc@gmail.com>
2024-03-06 18:49:04 +0100

    Speed up Base58 decoding with 64-bit packing
    > make && ./src/bench/bench_bitcoin --filter=Base58Decode --min-time=1000
    
    After:
    ```
    |             ns/byte |              byte/s |    err% |     total | benchmark
    |--------------------:|--------------------:|--------:|----------:|:----------
    |                8.79 |      113,767,685.06 |    0.1% |      1.10 | `Base58Decode`
    |                8.78 |      113,831,528.33 |    0.0% |      1.10 | `Base58Decode`
    |                8.80 |      113,607,470.35 |    0.2% |      1.10 | `Base58Decode`
    ```

--- src/base58.cpp
+++ src/base58.cpp
@@ -43,11 +43,13 @@ static constexpr int8_t mapBase58[256] = {
 
 static constexpr int base{58};
 static constexpr int64_t baseScale{1000LL};
-static constexpr int64_t log58_256Ratio =  733LL;  // Approximation of log(base)/log(256), scaled by baseScale.
+static constexpr int64_t log58_256Ratio =  733LL; // Approximation of log(base)/log(256), scaled by baseScale.
 static constexpr int64_t log256_58Ratio = 1366LL; // Approximation of log(256)/log(base), scaled by baseScale.
 
 // Defines the size of groups that fit into 64 bit batches, processed together for encoding and decoding efficiency.
 static constexpr int encodingBatch = 7;
+static constexpr int decodingBatch = 9;
+static constexpr int64_t decodingPowerOf58 = static_cast<int64_t>(base)*base*base*base*base*base*base*base*base; // pow(base, decodingBatch)
 
 [[nodiscard]] static bool DecodeBase58(const char* input, std::vector<unsigned char>& result, const int max_ret_len)
 {
@@ -66,12 +68,14 @@ static constexpr int encodingBatch = 7;
     result.reserve(leading + size);
     result.assign(leading, 0x00);
 
-    std::vector<uint8_t> inputBatched;
-    inputBatched.reserve(effectiveLength);
-    for (; *input && !IsSpace(*input); ++input) {
+    // Convert the Base58 string to a 64 bit representation for faster manipulation.
+    std::vector<int64_t> inputBatched((effectiveLength + decodingBatch - 1) / decodingBatch, 0);
+    const size_t groupOffset = (decodingBatch - (effectiveLength % decodingBatch)) % decodingBatch;
+    for (size_t i{0}; *input && !IsSpace(*input); ++input, ++i) {
         const int8_t digit = mapBase58[static_cast<uint8_t>(*input)];
-        if (digit == -1) [[unlikely]]  return false;
-        inputBatched.push_back(digit);
+        if (digit == -1) [[unlikely]] return false;
+        const size_t index = (groupOffset + i) / decodingBatch;
+        inputBatched[index] = (inputBatched[index] * base) + digit;
     }
     for (; *input; ++input)
         if (!IsSpace(*input)) [[unlikely]] return false; // Ensure no non-space characters after processing.
@@ -80,7 +84,7 @@ static constexpr int encodingBatch = 7;
     for (size_t i{0}; i < inputBatched.size(); ++resultLength) {
         int64_t remainder = 0;
         for (size_t j{i}; j < inputBatched.size(); ++j) { // Calculate next digit, dividing inputBatched
-            const int64_t accumulator = (remainder * base) + inputBatched[j];
+            const int64_t accumulator = (remainder * decodingPowerOf58) + inputBatched[j];
             inputBatched[j] = accumulator / 256;
             remainder = accumulator % 256;
         }
