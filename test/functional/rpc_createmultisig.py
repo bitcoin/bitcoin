syscoin-core/syscoin

Merge #19239: tests: move generate_wif_key to wallet_util.py
3a83a01694160f2e722e1bc90a328bd569b8e109 [tests] move generate_wif_key to wallet_util.py (John Newbery)
b216b0b71f7853d747af8b712fc250c699f3c320 [tests] sort imports in rpc_createmultisig.py (John Newbery)
e38081846d889fcbbbc6ca4a4d3bca26807fde2f Revert "[TESTS] Move base58 to own module to break circular dependency" (John Newbery)

Pull request description:

  generate_wif_key is a wallet utility function. Move it from the EC key module to the wallet util module.

  This fixes the circular dependency issue in #17977

ACKs for top commit:
  MarcoFalke:
    ACK 3a83a01694160f2e722e1bc90a328bd569b8e109 🍪

Tree-SHA512: 24985dffb75202721ccc0c6c5b52f1fa5d1ce7963bccde24389feb913cab4dad0c265274ca67892c46c8b64e6a065a0f23263a89be4fb9134dfefbdbe5c7238a

test/functional/rpc_createmultisig.py
@@ -3,21 +3,21 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 """Test multisig RPCs"""
-
-from test_framework.authproxy import JSONRPCException
-from test_framework.descriptors import descsum_create, drop_origins
-from test_framework.test_framework import SyscoinTestFramework
-from test_framework.util import (
-    assert_raises_rpc_error,
-    assert_equal,
-)
-from test_framework.key import ECPubKey, ECKey, bytes_to_wif
-
 import binascii
 import decimal
 import itertools
 import json
 import os
+
+from test_framework.authproxy import JSONRPCException
+from test_framework.descriptors import descsum_create, drop_origins
+from test_framework.key import ECPubKey, ECKey
+from test_framework.test_framework import SyscoinTestFramework
+from test_framework.util import (
+    assert_raises_rpc_error,
+    assert_equal,
+)
+from test_framework.wallet_util import bytes_to_wif
 
 class RpcCreateMultiSigTest(SyscoinTestFramework):
     def set_test_params(self):

bitcoin/bitcoin

test/functional/rpc_createmultisig.py
@@ -3,21 +3,21 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 """Test multisig RPCs"""
-
-from test_framework.authproxy import JSONRPCException
-from test_framework.descriptors import descsum_create, drop_origins
-from test_framework.test_framework import BitcoinTestFramework
-from test_framework.util import (
-    assert_raises_rpc_error,
-    assert_equal,
-)
-from test_framework.key import ECPubKey, ECKey, bytes_to_wif
-
 import binascii
 import decimal
 import itertools
 import json
 import os
+
+from test_framework.authproxy import JSONRPCException
+from test_framework.descriptors import descsum_create, drop_origins
+from test_framework.key import ECPubKey, ECKey
+from test_framework.test_framework import BitcoinTestFramework
+from test_framework.util import (
+    assert_raises_rpc_error,
+    assert_equal,
+)
+from test_framework.wallet_util import bytes_to_wif
 
 class RpcCreateMultiSigTest(BitcoinTestFramework):
     def set_test_params(self):