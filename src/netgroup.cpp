1636d177113ec941f0981a710f828d9957619e72
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2024-03-12 20:57:45 +0100

    scripted-diff: LogPrintf -> LogInfo
    -BEGIN VERIFY SCRIPT-
     sed -i 's/\<LogPrintf\>/LogInfo/g' $( git grep -l '\<LogPrintf\>' -- ./contrib/ ./src/ ./test/ ':(exclude)src/logging.h' )
    -END VERIFY SCRIPT-

--- src/netgroup.cpp
+++ src/netgroup.cpp
@@ -124,7 +124,7 @@ void NetGroupManager::ASMapHealthCheck(const std::vector<CNetAddr>& clearnet_add
         clearnet_asns.insert(asn);
     }
 
-    LogPrintf("ASMap Health Check: %i clearnet peers are mapped to %i ASNs with %i peers being unmapped\n", clearnet_addrs.size(), clearnet_asns.size(), unmapped_count);
+    LogInfo("ASMap Health Check: %i clearnet peers are mapped to %i ASNs with %i peers being unmapped\n", clearnet_addrs.size(), clearnet_asns.size(), unmapped_count);
 }
 
 bool NetGroupManager::UsingASMap() const {
