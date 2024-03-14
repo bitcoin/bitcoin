fa5844f06d74b35cd27c1927e2250ebb494578e9
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2024-03-14 15:57:30 +0100

    Remove unused g++-10 workaround
    This reverts d4999d40b9bd04dc20111aaaa6ed2d3db1a5caf9

--- src/util/fs_helpers.cpp
+++ src/util/fs_helpers.cpp
@@ -249,20 +249,9 @@ fs::path GetSpecialFolderPath(int nFolder, bool fCreate)
 
 bool RenameOver(fs::path src, fs::path dest)
 {
-#ifdef __MINGW64__
-    // This is a workaround for a bug in libstdc++ which
-    // implements fs::rename with _wrename function.
-    // This bug has been fixed in upstream:
-    //  - GCC 10.3: 8dd1c1085587c9f8a21bb5e588dfe1e8cdbba79e
-    //  - GCC 11.1: 1dfd95f0a0ca1d9e6cbc00e6cbfd1fa20a98f312
-    // For more details see the commits mentioned above.
-    return MoveFileExW(src.wstring().c_str(), dest.wstring().c_str(),
-                       MOVEFILE_REPLACE_EXISTING) != 0;
-#else
     std::error_code error;
     fs::rename(src, dest, error);
     return !error;
-#endif
 }
 
 /**
