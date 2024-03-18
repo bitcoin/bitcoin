dfddfd09a43f880949bda9158c13d4c9f14a58d8
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2024-03-18 09:09:53 +0000

    trivial: remove unneeded header, enumerate circular dependencies
    As found in a review comment in #5908[1], validation.h is not needed for
    specialtxman.cpp and removing the header uncovers other circular
    dependencies that were obscured by the shortest circular path[2].
    
    [1] - https://github.com/dashpay/dash/pull/5929#discussion_r1526663050
    [2] - https://github.com/dashpay/dash/pull/5929#discussion_r1527594636

--- src/evo/specialtxman.cpp
+++ src/evo/specialtxman.cpp
@@ -17,7 +17,6 @@
 #include <llmq/blockprocessor.h>
 #include <llmq/commitment.h>
 #include <primitives/block.h>
-#include <validation.h>
 
 static bool CheckSpecialTxInner(const CTransaction& tx, const CBlockIndex* pindexPrev, const CCoinsViewCache& view, const std::optional<CRangesSet>& indexes, bool check_sigs, TxValidationState& state)
 {
