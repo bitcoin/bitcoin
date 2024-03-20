47b32c886dc55d448e618cc74b8487c0c5d85211
Ryan Ofsky <ryan@ofsky.org>
2024-03-20 17:12:09 -0400

    refactor, validation: Return fatal errors from activate best chain functions
    Return fatal errors from ActivateBestChain, ActivateBestChainStep, and
    PreciousBlock functions.  Also add nodiscard annotations so callers handle the
    result values. Two callers, ProcessNewBlock and LoadExternalBlockFile, will be
    updated in an upcoming commits to bubble results up to its callers.

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -1628,7 +1628,7 @@ static RPCHelpMan preciousblock()
     }
 
     BlockValidationState state;
-    chainman.ActiveChainstate().PreciousBlock(state, pblockindex);
+    (void)chainman.ActiveChainstate().PreciousBlock(state, pblockindex);
 
     if (!state.IsValid()) {
         throw JSONRPCError(RPC_DATABASE_ERROR, state.ToString());
@@ -1652,7 +1652,7 @@ void InvalidateBlock(ChainstateManager& chainman, const uint256 block_hash) {
     (void)chainman.ActiveChainstate().InvalidateBlock(state, pblockindex);
 
     if (state.IsValid()) {
-        chainman.ActiveChainstate().ActivateBestChain(state);
+        (void)chainman.ActiveChainstate().ActivateBestChain(state);
     }
 
     if (!state.IsValid()) {
@@ -1698,7 +1698,7 @@ void ReconsiderBlock(ChainstateManager& chainman, uint256 block_hash) {
     }
 
     BlockValidationState state;
-    chainman.ActiveChainstate().ActivateBestChain(state);
+    (void)chainman.ActiveChainstate().ActivateBestChain(state);
 
     if (!state.IsValid()) {
         throw JSONRPCError(RPC_DATABASE_ERROR, state.ToString());
