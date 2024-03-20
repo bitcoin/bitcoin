23299eacce4fc9a8b9192fbcc7afaa68f7a9f981
Ryan Ofsky <ryan@ofsky.org>
2024-03-20 17:12:09 -0400

    refactor, validation: Return fatal errors from activate best chain functions
    Return fatal errors from ActivateBestChain, ActivateBestChainStep, and
    PreciousBlock functions.  Also add nodiscard annotations so callers handle the
    result values. Two callers, ProcessNewBlock and LoadExternalBlockFile, will be
    updated in an upcoming commits to bubble results up to its callers.

--- src/node/blockstorage.cpp
+++ src/node/blockstorage.cpp
@@ -1319,6 +1319,7 @@ public:
 
 void ImportBlocks(ChainstateManager& chainman, std::span<const fs::path> import_paths)
 {
+    FlushResult<InterruptResult, AbortFailure> result; // TODO Return this result!
     ImportingNow imp{chainman.m_blockman.m_importing};
 
     // -reindex
@@ -1368,8 +1369,9 @@ void ImportBlocks(ChainstateManager& chainman, std::span<const fs::path> import_
     }
 
     // scan for better chains in the block chain database, that are not yet connected in the active best chain
-    if (auto result = chainman.ActivateBestChains(); !result) {
-        chainman.GetNotifications().fatalError(util::ErrorString(result));
+    if (auto activate_result = chainman.ActivateBestChains(); !activate_result) {
+        chainman.GetNotifications().fatalError(util::ErrorString(activate_result));
+        activate_result >> result;
     }
     // End scope of ImportingNow
 }
