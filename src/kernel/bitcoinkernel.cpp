4d896fe05c476d99f2d8d493a5ae6bafd357919b
Ryan Ofsky <ryan@ofsky.org>
2024-03-20 17:12:09 -0400

    refactor, validation: Return fatal errors from FlushStateToDisk
    Return fatal errors from the Chainstate::FlushStateToDisk method and several
    small, related methods which wrap it: ForceFlushStateToDisk, PruneAndFlush,
    ResizeCoinsCaches, and MaybeRebalanceCaches.
    
    Also add nodiscard annotations so callers do not accidentally ignore the result
    values. Callers in init and rpc files are updated to explicitly ignore the
    flush results, and other callers (AcceptToMemoryPool, ProcessNewPackage,
    DisconnectTip, ConnectTip, ActivateBestChainStep, ActivateSnapshot,
    MaybeCompleteSnapshotValidation) are updated to store the results in this
    commit, and will be updated in upcoming commits to bubble results up to their
    callers.

--- src/kernel/bitcoinkernel.cpp
+++ src/kernel/bitcoinkernel.cpp
@@ -1014,7 +1014,8 @@ void btck_chainstate_manager_destroy(btck_ChainstateManager* chainman)
         LOCK(btck_ChainstateManager::get(chainman).m_chainman->GetMutex());
         for (Chainstate* chainstate : btck_ChainstateManager::get(chainman).m_chainman->GetAll()) {
             if (chainstate->CanFlushToDisk()) {
-                chainstate->ForceFlushStateToDisk();
+                auto flush_result{chainstate->ForceFlushStateToDisk()};
+                if (!flush_result) LogError("%s", util::ErrorString(flush_result).original);
                 chainstate->ResetCoinsViews();
             }
         }
