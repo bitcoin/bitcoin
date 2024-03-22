6622c094cc32a1949ab7f0bcd9c0a0a7f4a830e5
fanquake <fanquake@gmail.com>
2024-03-22 17:29:22 +0000

    depends: libnatpmp f2433bec24ca3d3f22a8a7840728a3ac177f94ba
    This includes once CMake related change I upstreamed:
    https://github.com/miniupnp/libnatpmp/pull/43.

--- depends/packages/libnatpmp.mk
+++ depends/packages/libnatpmp.mk
@@ -1,8 +1,8 @@
 package=libnatpmp
-$(package)_version=07004b97cf691774efebe70404cf22201e4d330d
+$(package)_version=f2433bec24ca3d3f22a8a7840728a3ac177f94ba
 $(package)_download_path=https://github.com/miniupnp/libnatpmp/archive
 $(package)_file_name=$($(package)_version).tar.gz
-$(package)_sha256_hash=9321953ceb39d07c25463e266e50d0ae7b64676bb3a986d932b18881ed94f1fb
+$(package)_sha256_hash=ef84979950dfb3556705b63c9cd6c95501b75e887fba466234b187f3c9029669
 $(package)_patches=no_libtool.patch
 
 define $(package)_set_vars
