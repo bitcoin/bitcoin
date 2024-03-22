3e319e5a67a114c73cb685896c5d61e15ac78dc7
Drew Roberts <github@drewroberts.com>
2024-03-22 19:35:24 -0400

    Extend 2023 copyrights for bitcoin developers

--- contrib/devtools/bitcoin-tidy/logprintf.h
+++ contrib/devtools/bitcoin-tidy/logprintf.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2023 Bitcoin Developers
+// Copyright (c) 2023-2024 Bitcoin Developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
