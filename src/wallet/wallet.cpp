91e065ec175e3d40439230d0a70b2c2c73ff024b
furszy <matiasfurszyfer@protonmail.com>
2024-03-24 01:03:51 -0300

    wallet: remove post-migration signals connection
    The wallet is isolated during migration and reloaded at the end
    of the process. There is no benefit on connecting the signals
    few lines before unloading the wallet.

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -4275,10 +4275,6 @@ bool CWallet::ApplyMigrationData(MigrationData& data, bilingual_str& error)
     }
     local_wallet_batch.TxnCommit();
 
-    // Connect the SPKM signals
-    ConnectScriptPubKeyManNotifiers();
-    NotifyCanGetAddressesChanged();
-
     WalletLogPrintf("Wallet migration complete.\n");
 
     return true;
