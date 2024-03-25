2f1f312856e2f78809739d7003bf7a757e319db1
fanquake <fanquake@gmail.com>
2024-03-25 12:26:19 +0000

    depends: patches to fixup CMake build
    Some of these have been upstreamed. Some, i.e changing minimum versions
    are unlikely to be accepted, so have not and exist here to suppress
    output.

--- depends/packages/zeromq.mk
+++ depends/packages/zeromq.mk
@@ -3,7 +3,7 @@ $(package)_version=4.3.5
 $(package)_download_path=https://github.com/zeromq/libzmq/releases/download/v$($(package)_version)/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=6653ef5910f17954861fe72332e68b03ca6e4d9c7160eb3a8de5a5a913bfab43
-$(package)_patches=remove_libstd_link.patch
+$(package)_patches=remove_libstd_link.patch fix_cmake_build.patch
 
 define $(package)_set_vars
   $(package)_config_opts = --without-docs --disable-shared --disable-valgrind
@@ -14,7 +14,8 @@ define $(package)_set_vars
 endef
 
 define $(package)_preprocess_cmds
-  patch -p1 < $($(package)_patch_dir)/remove_libstd_link.patch
+  patch -p1 < $($(package)_patch_dir)/remove_libstd_link.patch && \
+  patch -p1 < $($(package)_patch_dir)/fix_cmake_build.patch
 endef
 
 define $(package)_config_cmds
