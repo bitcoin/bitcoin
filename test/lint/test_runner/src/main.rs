a1b5e663a1543becd2b287bf510a5122109a4091
David Gumberg <davidzgumberg@gmail.com>
2024-03-26 17:44:32 -0400

    Rewrite spelling linter in rust

--- test/lint/test_runner/src/main.rs
+++ test/lint/test_runner/src/main.rs
@@ -5,6 +5,7 @@
 use std::collections::HashSet;
 use std::env;
 use std::fs;
+use std::io;
 use std::path::{Path, PathBuf};
 use std::process::Command;
 use std::process::ExitCode;
@@ -12,6 +13,8 @@ use std::process::ExitCode;
 type LintError = String;
 type LintResult = Result<(), LintError>;
 type LintFn = fn() -> LintResult;
+
+const SPELLING_IGNORE_WORDS_FILE:&str = "test/lint/spelling.ignore-words.txt";
 
 /// Return the git command
 fn git() -> Command {
@@ -535,6 +538,75 @@ fn lint_doc() -> LintResult {
     }
 }
 
+/// Return the pathspecs for spelling check related excludes
+fn get_pathspecs_exclude_spelling() -> Vec<String> {
+    let mut list = get_pathspecs_exclude_subtrees();
+    list.extend(
+        [
+            "build-aux/m4/",
+            "contrib/seeds/*.txt",
+            "depends/",
+            "doc/release-notes/",
+            "src/qt/locale/",
+            "src/qt/*.qrc",
+            "contrib/guix/patches",
+        ]
+        .iter()
+        .map(|s| format!(":(exclude){}", s)),
+    );
+    list
+}
+
+// Prints warnings for spelling errors.
+// Note: Will always return Ok(()), even if there are spelling errors.
+fn lint_spelling() -> LintResult {
+    // Check that we have codespell installed
+    let codespell_check = Command::new("codespell")
+        .arg("--version")
+        .output();
+
+    match codespell_check {
+        Ok(_) => (),
+        Err(err) => {
+            if err.kind() == io::ErrorKind::NotFound {
+                println!("Skipping spell check linting since codespell is not installed.");
+                return Ok(())
+            } else {
+                panic!("command error")
+            }
+        }
+    }
+
+    let files_list_string = String::from_utf8(
+        git()
+            .args(["ls-files", "--"])
+            .args(get_pathspecs_exclude_spelling())
+            .output()
+            .expect("command error")
+            .stdout
+    ).expect("error reading stdout");
+
+    let files_list:Vec<&str> = files_list_string
+        .split('\n')
+        .collect::<Vec<_>>();
+
+    // Using Command::status() here let's codespell's output pass through to the user.
+    let codespell_result = Command::new("codespell")
+        .args(["--check-filenames", "--disable-colors", "--quiet-level=7"])
+        .args([format!("--ignore-words={SPELLING_IGNORE_WORDS_FILE}")])
+        .args(files_list)
+        .status()
+        .expect("command error");
+
+    if !codespell_result.success() {
+        println!(r#"
+^^^ Warning: `codespell` identified likely spelling errors. Any false positives? Add them to the list of ignored words in {SPELLING_IGNORE_WORDS_FILE}
+        "#);
+    }
+
+    Ok(())
+}
+
 fn lint_all() -> LintResult {
     let mut good = true;
     let lint_dir = get_git_root().join("test/lint");
@@ -570,6 +642,7 @@ fn main() -> ExitCode {
         ("includes check", lint_includes),
         ("build config includes check", lint_includes_build_config),
         ("-help=1 documentation check", lint_doc),
+        ("spelling check", lint_spelling),
         ("lint-*.py scripts", lint_all),
     ];
 
