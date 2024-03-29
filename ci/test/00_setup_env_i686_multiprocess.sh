fad23a06469607689c4f637bb407c96af4902a27
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2024-03-29 15:20:32 +0100

    ci: Bump clang+llvm in i686_multiprocess task

--- ci/test/00_setup_env_i686_multiprocess.sh
+++ ci/test/00_setup_env_i686_multiprocess.sh
@@ -8,7 +8,7 @@ export LC_ALL=C.UTF-8
 
 export HOST=i686-pc-linux-gnu
 export CONTAINER_NAME=ci_i686_multiprocess
-export CI_IMAGE_NAME_TAG="docker.io/amd64/ubuntu:22.04"
+export CI_IMAGE_NAME_TAG="docker.io/amd64/ubuntu:24.04"
 export PACKAGES="llvm clang g++-multilib"
 export DEP_OPTS="DEBUG=1 MULTIPROCESS=1"
 export GOAL="install"
