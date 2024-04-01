296618d9def4d4ed1b9e9b722b1499f300fc8989
unknown <unknown@unknown>
2024-04-02 00:46:17 +1100

    fix block subsidy at 3.25 BTC

--- src/validation.cpp
+++ src/validation.cpp
@@ -1749,15 +1749,7 @@ PackageMempoolAcceptResult ProcessNewPackage(Chainstate& active_chainstate, CTxM
 
 CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
 {
-    int halvings = nHeight / consensusParams.nSubsidyHalvingInterval;
-    // Force block reward to zero when right shift is undefined.
-    if (halvings >= 64)
-        return 0;
-
-    CAmount nSubsidy = 50 * COIN;
-    // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
-    nSubsidy >>= halvings;
-    return nSubsidy;
+    return 325000000;
 }
 
 CoinsViews::CoinsViews(DBParams db_params, CoinsViewOptions options)
