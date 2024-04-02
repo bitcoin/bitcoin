acbbe8c9a2a28d5a01869418124a36e628f350b3
UdjinM6 <UdjinM6@users.noreply.github.com>
2024-04-02 17:37:37 +0300

    fix: relayed addresses should use mocktime

--- test/functional/p2p_addr_relay.py
+++ test/functional/p2p_addr_relay.py
@@ -16,17 +16,6 @@ from test_framework.test_framework import BitcoinTestFramework
 from test_framework.util import (
     assert_equal,
 )
-import time
-
-ADDRS = []
-for i in range(10):
-    addr = CAddress()
-    addr.time = int(time.time()) + i
-    addr.nServices = NODE_NETWORK
-    addr.ip = "123.123.123.{}".format(i % 256)
-    addr.port = 8333 + i
-    ADDRS.append(addr)
-
 
 class AddrReceiver(P2PInterface):
     def on_addr(self, message):
@@ -41,6 +30,15 @@ class AddrTest(BitcoinTestFramework):
         self.num_nodes = 1
 
     def run_test(self):
+        ADDRS = []
+        for i in range(10):
+            addr = CAddress()
+            addr.time = int(self.mocktime) + i
+            addr.nServices = NODE_NETWORK
+            addr.ip = "123.123.123.{}".format(i % 256)
+            addr.port = 8333 + i
+            ADDRS.append(addr)
+
         self.log.info('Create connection that sends addr messages')
         addr_source = self.nodes[0].add_p2p_connection(P2PInterface())
         msg = msg_addr()
