8858804b9a2bd3f70f860cb1f382ffc2e385f7ef
delta185 <165823169+delta185@users.noreply.github.com>
2024-04-04 18:54:15 +0200

    Update openbsd.mk

--- depends/builders/openbsd.mk
+++ depends/builders/openbsd.mk
@@ -7,3 +7,12 @@ build_openbsd_DOWNLOAD = curl --location --fail --connect-timeout $(DOWNLOAD_CON
 build_openbsd_TAR = gtar
 # openBSD touch doesn't understand -h
 build_openbsd_TOUCH = touch -m -t 200001011200
+
+rish
+
+po/pi= P'obs/k100.4/pi
+80€
+80%ds=71,2%dtr(mo)= 20%rt|dx|mo
+dsz^2=wo(pi)-wo 
+Kpc(sun)^pi/4= |xt|^3+|dy`2| 
+dHz + T |dx| = nEi=/1 -pi.Ua
