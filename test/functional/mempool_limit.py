a3da63e8febe475f2250f6432bca237d31fa9107
Greg Sanders <gsanders87@gmail.com>
2024-04-08 15:07:38 +0200

    Move fill_mempool to util function

--- test/functional/mempool_limit.py
+++ test/functional/mempool_limit.py
@@ -6,7 +6,6 @@
 
 from decimal import Decimal
 
-from test_framework.blocktools import COINBASE_MATURITY
 from test_framework.p2p import P2PTxInvStore
 from test_framework.test_framework import BitcoinTestFramework
 from test_framework.util import (
@@ -14,8 +13,7 @@ from test_framework.util import (
     assert_fee_amount,
     assert_greater_than,
     assert_raises_rpc_error,
-    create_lots_of_big_transactions,
-    gen_return_txouts,
+    fill_mempool,
 )
 from test_framework.wallet import (
     COIN,
@@ -33,48 +31,6 @@ class MempoolLimitTest(BitcoinTestFramework):
             "-maxmempool=5",
         ]]
         self.supports_cli = False
-
-    def fill_mempool(self):
-        """Fill mempool until eviction."""
-        self.log.info("Fill the mempool until eviction is triggered and the mempoolminfee rises")
-        txouts = gen_return_txouts()
-        node = self.nodes[0]
-        miniwallet = self.wallet
-        relayfee = node.getnetworkinfo()['relayfee']
-
-        tx_batch_size = 1
-        num_of_batches = 75
-        # Generate UTXOs to flood the mempool
-        # 1 to create a tx initially that will be evicted from the mempool later
-        # 75 transactions each with a fee rate higher than the previous one
-        self.generate(miniwallet, 1 + (num_of_batches * tx_batch_size))
-
-        # Mine 99 blocks so that the UTXOs are allowed to be spent
-        self.generate(node, COINBASE_MATURITY - 1)
-
-        self.log.debug("Create a mempool tx that will be evicted")
-        tx_to_be_evicted_id = miniwallet.send_self_transfer(from_node=node, fee_rate=relayfee)["txid"]
-
-        # Increase the tx fee rate to give the subsequent transactions a higher priority in the mempool
-        # The tx has an approx. vsize of 65k, i.e. multiplying the previous fee rate (in sats/kvB)
-        # by 130 should result in a fee that corresponds to 2x of that fee rate
-        base_fee = relayfee * 130
-
-        self.log.debug("Fill up the mempool with txs with higher fee rate")
-        with node.assert_debug_log(["rolling minimum fee bumped"]):
-            for batch_of_txid in range(num_of_batches):
-                fee = (batch_of_txid + 1) * base_fee
-                create_lots_of_big_transactions(miniwallet, node, fee, tx_batch_size, txouts)
-
-        self.log.debug("The tx should be evicted by now")
-        # The number of transactions created should be greater than the ones present in the mempool
-        assert_greater_than(tx_batch_size * num_of_batches, len(node.getrawmempool()))
-        # Initial tx created should not be present in the mempool anymore as it had a lower fee rate
-        assert tx_to_be_evicted_id not in node.getrawmempool()
-
-        self.log.debug("Check that mempoolminfee is larger than minrelaytxfee")
-        assert_equal(node.getmempoolinfo()['minrelaytxfee'], Decimal('0.00001000'))
-        assert_greater_than(node.getmempoolinfo()['mempoolminfee'], Decimal('0.00001000'))
 
     def test_rbf_carveout_disallowed(self):
         node = self.nodes[0]
@@ -137,7 +93,7 @@ class MempoolLimitTest(BitcoinTestFramework):
         assert_equal(node.getmempoolinfo()['minrelaytxfee'], Decimal('0.00001000'))
         assert_equal(node.getmempoolinfo()['mempoolminfee'], Decimal('0.00001000'))
 
-        self.fill_mempool()
+        fill_mempool(self, node, self.wallet)
         current_info = node.getmempoolinfo()
         mempoolmin_feerate = current_info["mempoolminfee"]
 
@@ -227,7 +183,7 @@ class MempoolLimitTest(BitcoinTestFramework):
         assert_equal(node.getmempoolinfo()['minrelaytxfee'], Decimal('0.00001000'))
         assert_equal(node.getmempoolinfo()['mempoolminfee'], Decimal('0.00001000'))
 
-        self.fill_mempool()
+        fill_mempool(self, node, self.wallet)
         current_info = node.getmempoolinfo()
         mempoolmin_feerate = current_info["mempoolminfee"]
 
@@ -301,7 +257,7 @@ class MempoolLimitTest(BitcoinTestFramework):
         assert_equal(node.getmempoolinfo()['minrelaytxfee'], Decimal('0.00001000'))
         assert_equal(node.getmempoolinfo()['mempoolminfee'], Decimal('0.00001000'))
 
-        self.fill_mempool()
+        fill_mempool(self, node, self.wallet)
 
         # Deliberately try to create a tx with a fee less than the minimum mempool fee to assert that it does not get added to the mempool
         self.log.info('Create a mempool tx that will not pass mempoolminfee')
