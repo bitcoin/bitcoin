91d75d94978c59a9682d83aa793b73624aae0d90
Suhas Daftuar <sdaftuar@gmail.com>
2024-04-16 13:53:12 -0400

    Rewrite removeForReorg to use a vector instead of a set

--- src/txmempool.cpp
+++ src/txmempool.cpp
@@ -445,14 +445,15 @@ void CTxMemPool::removeForReorg(CChain& chain, std::function<bool(txiter)> check
     AssertLockHeld(::cs_main);
     Assume(!m_have_changeset);
 
-    setEntries txToRemove;
+    Entries txToRemove;
     for (indexed_transaction_set::const_iterator it = mapTx.begin(); it != mapTx.end(); it++) {
-        if (check_final_and_mature(it)) txToRemove.insert(it);
+        if (check_final_and_mature(it)) txToRemove.push_back(it);
     }
+    auto descendants = CalculateDescendants(txToRemove);
+
     setEntries setAllRemoves;
-    for (txiter it : txToRemove) {
-        CalculateDescendants(it, setAllRemoves);
-    }
+    setAllRemoves.insert(descendants.begin(), descendants.end());
+
     RemoveStaged(setAllRemoves, MemPoolRemovalReason::REORG);
     for (indexed_transaction_set::const_iterator it = mapTx.begin(); it != mapTx.end(); it++) {
         assert(TestLockPointValidity(chain, it->GetLockPoints()));
