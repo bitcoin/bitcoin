58d9f8307989bce3695dd9bdc291244d48452f75
glozow <gloriajzhao@gmail.com>
2024-04-16 17:06:56 +0100

    [refactor] move AlreadyHaveTx to TxDownload
    This is move-only.
    Also delete external RecentConfirmedTransactionsFilter() access since it
    is no longer necessary.

--- src/node/txdownloadman_impl.cpp
+++ src/node/txdownloadman_impl.cpp
@@ -34,10 +34,6 @@ CRollingBloomFilter& TxDownloadManager::RecentRejectsReconsiderableFilter()
 {
     return m_impl->RecentRejectsReconsiderableFilter();
 }
-CRollingBloomFilter& TxDownloadManager::RecentConfirmedTransactionsFilter()
-{
-    return m_impl->RecentConfirmedTransactionsFilter();
-}
 void TxDownloadManager::ActiveTipChange()
 {
     m_impl->ActiveTipChange();
@@ -49,6 +45,10 @@ void TxDownloadManager::BlockConnected(const std::shared_ptr<const CBlock>& pblo
 void TxDownloadManager::BlockDisconnected()
 {
     m_impl->BlockDisconnected();
+}
+bool TxDownloadManager::AlreadyHaveTx(const GenTxid& gtxid, bool include_reconsiderable)
+{
+    return m_impl->AlreadyHaveTx(gtxid, include_reconsiderable);
 }
 
 // TxDownloadManagerImpl
@@ -83,5 +83,34 @@ void TxDownloadManagerImpl::BlockDisconnected()
     // presumably the most common case of relaying a confirmed transaction
     // should be just after a new block containing it is found.
     RecentConfirmedTransactionsFilter().reset();
+}
+
+bool TxDownloadManagerImpl::AlreadyHaveTx(const GenTxid& gtxid, bool include_reconsiderable)
+{
+    const uint256& hash = gtxid.GetHash();
+
+    if (gtxid.IsWtxid()) {
+        // Normal query by wtxid.
+        if (m_orphanage.HaveTx(Wtxid::FromUint256(hash))) return true;
+    } else {
+        // Never query by txid: it is possible that the transaction in the orphanage has the same
+        // txid but a different witness, which would give us a false positive result. If we decided
+        // not to request the transaction based on this result, an attacker could prevent us from
+        // downloading a transaction by intentionally creating a malleated version of it.  While
+        // only one (or none!) of these transactions can ultimately be confirmed, we have no way of
+        // discerning which one that is, so the orphanage can store multiple transactions with the
+        // same txid.
+        //
+        // While we won't query by txid, we can try to "guess" what the wtxid is based on the txid.
+        // A non-segwit transaction's txid == wtxid. Query this txid "casted" to a wtxid. This will
+        // help us find non-segwit transactions, saving bandwidth, and should have no false positives.
+        if (m_orphanage.HaveTx(Wtxid::FromUint256(hash))) return true;
+    }
+
+    if (include_reconsiderable && RecentRejectsReconsiderableFilter().contains(hash)) return true;
+
+    if (RecentConfirmedTransactionsFilter().contains(hash)) return true;
+
+    return RecentRejectsFilter().contains(hash) || m_opts.m_mempool.exists(gtxid);
 }
 } // namespace node
