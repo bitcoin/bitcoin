e63a8c64564c8b92c83790b8dd06840b8126c016
laanwj <126646+laanwj@users.noreply.github.com>
2024-04-25 09:37:33 +0200

    depends: Change qtsowrap to wayland branch

--- depends/packages/qtsowrap.mk
+++ depends/packages/qtsowrap.mk
@@ -1,9 +1,8 @@
 package=qtsowrap
 $(package)_version=0.0.1
 $(package)_download_path=https://github.com/laanwj/qtsowrap/archive/
-$(package)_file_name=474a0d7b0aded621d10ebe6ffd0e69622ec5337c.tar.gz
-#$(package)_file_name=$(package)-$($(package)_version).tar.xz
-$(package)_sha256_hash=d0ebb0794fddf1213eca54e1b51dba1aae024bd557cc53c73963744f6791d0ce
+$(package)_file_name=8825b14e8db51a1ae707c58448ec36dfa7ab7be2.tar.gz
+$(package)_sha256_hash=26f99e32d0b9f84f588d5fd6e129812418759d0fd551f6a35367d61974ea424a
 $(package)_dependencies=
 $(package)_patches =
 
