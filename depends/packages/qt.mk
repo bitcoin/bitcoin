a995902d604c701be4f46087057b907de9a0ecca
laanwj <126646+laanwj@users.noreply.github.com>
2024-04-28 13:40:22 +0200

    depends: Fix build of Qt for 32-bit platforms
    The 32 to 64-bit time_t transition causes a build failure in the built-in
    zlib about conflicting _TIME_BITS and _FILE_OFFSET_BITS.
    
    Note that zlib doesn't use time_t at all, so it is a false alarm.
    
    Take the following patch from upstream zlib:
    https://github.com/madler/zlib/commit/a566e156b3fa07b566ddbf6801b517a9dba04fa3.patch
    
    Closes #29980.
    
    Github-Pull: #29985
    Rebased-From: 2e266f33b5d2be5c233c2c692481f75785714fa1

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -24,6 +24,7 @@ $(package)_patches += fix-macos-linker.patch
 $(package)_patches += memory_resource.patch
 $(package)_patches += utc_from_string_no_optimize.patch
 $(package)_patches += windows_lto.patch
+$(package)_patches += zlib-timebits64.patch
 
 $(package)_qttranslations_file_name=qttranslations-$($(package)_suffix)
 $(package)_qttranslations_sha256_hash=a31785948c640b7c66d9fe2db4993728ca07f64e41c560b3625ad191b276ff20
@@ -255,6 +256,7 @@ define $(package)_preprocess_cmds
   patch -p1 -i $($(package)_patch_dir)/fast_fixed_dtoa_no_optimize.patch && \
   patch -p1 -i $($(package)_patch_dir)/guix_cross_lib_path.patch && \
   patch -p1 -i $($(package)_patch_dir)/windows_lto.patch && \
+  patch -p1 -i $($(package)_patch_dir)/zlib-timebits64.patch && \
   mkdir -p qtbase/mkspecs/macx-clang-linux &&\
   cp -f qtbase/mkspecs/macx-clang/qplatformdefs.h qtbase/mkspecs/macx-clang-linux/ &&\
   cp -f $($(package)_patch_dir)/mac-qmake.conf qtbase/mkspecs/macx-clang-linux/qmake.conf && \
