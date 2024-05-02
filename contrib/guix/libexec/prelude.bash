a870a28706dcfc3bdc488380d31ee34f7859cb11
fanquake <fanquake@gmail.com>
2024-05-02 15:56:26 +0800

    guix: bump time-machine to 1fa1325c0b4bb5c28e564526f387d82083733104
    Includes LLVM 18.1.6.
    https://git.savannah.gnu.org/cgit/guix.git/commit/?id=1fa1325c0b4bb5c28e564526f387d82083733104.

--- contrib/guix/libexec/prelude.bash
+++ contrib/guix/libexec/prelude.bash
@@ -51,7 +51,7 @@ fi
 time-machine() {
     # shellcheck disable=SC2086
     guix time-machine --url=https://git.savannah.gnu.org/git/guix.git \
-                      --commit=dc4842797bfdc5f9f3f5f725bf189c2b68bd6b5a \
+                      --commit=1fa1325c0b4bb5c28e564526f387d82083733104 \
                       --cores="$JOBS" \
                       --keep-failed \
                       --fallback \
