4d021e6226f7def5f3503977804bf4018357e322
Léo Haf <leohaf@orangepill.ovh>
2024-05-06 18:13:51 +0200

    add `-rejectparasites` option
    Github-Pull: knots#78
    Rebased-From: 18cd7b0ef6eaeacd06678c6d192b6cacc9d7eee5

--- src/kernel/mempool_options.h
+++ src/kernel/mempool_options.h
@@ -63,6 +63,7 @@ struct MemPoolOptions {
     bool datacarrier_fullcount{DEFAULT_DATACARRIER_FULLCOUNT};
     bool permit_bare_pubkey{DEFAULT_PERMIT_BAREPUBKEY};
     bool permit_bare_multisig{DEFAULT_PERMIT_BAREMULTISIG};
+    bool reject_parasites{DEFAULT_REJECT_PARASITES};
     bool reject_tokens{DEFAULT_REJECT_TOKENS};
     bool accept_non_std_datacarrier{DEFAULT_ACCEPT_NON_STD_DATACARRIER};
     bool require_standard{true};
