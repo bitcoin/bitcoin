5229faad2716e6eda52bf6742c6b50cb484cf57f
Jon Atack <jon@atack.com>
2024-05-10 18:34:39 -0600

    ci: update dependency versions

--- ci/lint/04_install.sh
+++ ci/lint/04_install.sh
@@ -47,11 +47,11 @@ fi
 
 ${CI_RETRY_EXE} pip3 install \
   codespell==2.2.6 \
-  flake8==6.1.0 \
+  flake8==7.0.0 \
   lief==0.13.2 \
-  mypy==1.4.1 \
-  pyzmq==25.1.0 \
-  vulture==2.6
+  mypy==1.10.0 \
+  pyzmq==26.0.3 \
+  vulture==2.11
 
 SHELLCHECK_VERSION=v0.8.0
 curl -sL "https://github.com/koalaman/shellcheck/releases/download/${SHELLCHECK_VERSION}/shellcheck-${SHELLCHECK_VERSION}.linux.x86_64.tar.xz" | \
