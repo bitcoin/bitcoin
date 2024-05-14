3070c3e388daaa00bbfb873820957c105f3e6998
merge-script <fanquake@gmail.com>
2024-05-14 11:47:09 +0800

    Merge bitcoin/bitcoin#30078: depends: set AR & RANLIB for CMake
    019ad7327c397094d7648b55503bf5373b108a57 depends: set RANLIB for CMake (fanquake)
    43cfb428cba04b8db98d4d0d56ffe28ad686e58c depends: set NM for CMake (fanquake)
    1e4412b317f74dd64069309544fe73c95e2c10e7 depends: set AR for CMake (fanquake)
    
    Pull request description:
    
      Needed for #21778. Should be more correct in any case.
    
    ACKs for top commit:
      theuni:
        utACK 019ad7327c397094d7648b55503bf5373b108a57. I didn't test, but I tried this approach on a few deps and it seemed to work as expected.
      TheCharlatan:
        ACK 019ad7327c397094d7648b55503bf5373b108a57
    
    Tree-SHA512: 78cc8981456f7476cafca0e40fcc569e474b92004c8024d1c4268b6aab53175074a06ab17ebded8d706bf0a7f77401642dd38bb7ce2e4b04abdcd149d3d69969

--- depends/funcs.mk
+++ depends/funcs.mk
@@ -181,6 +181,9 @@ $(1)_cmake=env CC="$$($(1)_cc)" \
                CXXFLAGS="$$($(1)_cppflags) $$($(1)_cxxflags)" \
                LDFLAGS="$$($(1)_ldflags)" \
                cmake -DCMAKE_INSTALL_PREFIX:PATH="$$($($(1)_type)_prefix)" \
+               -DCMAKE_AR=`which $$($(1)_ar)` \
+               -DCMAKE_NM=`which $$($(1)_nm)` \
+               -DCMAKE_RANLIB=`which $$($(1)_ranlib)` \
                -DCMAKE_INSTALL_LIBDIR=lib/ \
                -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
                $$($(1)_config_opts)
