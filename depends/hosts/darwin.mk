91a799247dc5e4627e6b2f221669c8ff9238bc8d
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2024-05-15 11:01:31 +0100

    depends: Add host-specific `cmake_system_version` variables

--- depends/hosts/darwin.mk
+++ depends/hosts/darwin.mk
@@ -76,3 +76,6 @@ darwin_debug_CFLAGS=-O1 -g
 darwin_debug_CXXFLAGS=$(darwin_debug_CFLAGS)
 
 darwin_cmake_system_name=Darwin
+# Darwin version, which corresponds to OSX_MIN_VERSION.
+# See https://en.wikipedia.org/wiki/Darwin_(operating_system)
+darwin_cmake_system_version=20.1
