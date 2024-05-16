e02030432b77abbf27bb4f67d879d3ad6d6366e6
laanwj <126646+laanwj@users.noreply.github.com>
2024-05-16 10:16:41 +0200

    net: Add netif utility
    This adds an utility header with two functions that will be needed for
    PCP, `QueryDefaultGateway` and `GetLocalAddresses`.
    
    Co-authored-by: Vasil Dimov <vd@FreeBSD.org>

---
+++ src/util/netif.h
@@ -0,0 +1,20 @@
+// Copyright (c) 2024 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or https://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_UTIL_NETIF_H
+#define BITCOIN_UTIL_NETIF_H
+
+#include <netaddress.h>
+
+#include <optional>
+
+//! Query the OS for the default gateway for `network`. This only makes sense for NET_IPV4 and NET_IPV6.
+//! Returns std::nullopt if it cannot be found, or there is no support for this OS.
+std::optional<CNetAddr> QueryDefaultGateway(Network network);
+
+// TODO share with Discover()
+//! Return all local non-loopback IPv4 and IPv6 network addresses.
+std::vector<CNetAddr> GetLocalAddresses();
+
+#endif // BITCOIN_UTIL_NETIF_H
