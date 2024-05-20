2eeabf617530393469f61097c2613a02e827a55e
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2024-05-20 10:46:50 +0100

    [fees]: add `GetMiniMinerInput` to `fees_util`
    - This method takes removed mempool transaction and generates
      an output that can be linearized by mini miner.
    
    Co-authored-by: willcl-ark <will@256k1.dev>

--- src/policy/fees_util.cpp
+++ src/policy/fees_util.cpp
@@ -3,6 +3,8 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 
+#include <node/mini_miner.h>
+#include <policy/feerate.h>
 #include <policy/fees_util.h>
 #include <primitives/transaction.h>
 
@@ -27,4 +29,32 @@ TxAncestorsAndDescendants GetTxAncestorsAndDescendants(const std::vector<Removed
         }
     }
     return visited_txs;
+}
+
+MiniMinerInput GetMiniMinerInput(const std::vector<RemovedMempoolTransactionInfo>& txs_removed_for_block)
+{
+    // Cache all the transactions for efficient lookup
+    std::map<Txid, TransactionInfo> tx_caches;
+    for (const auto& tx : txs_removed_for_block) {
+        tx_caches.emplace(tx.info.m_tx->GetHash(), TransactionInfo(tx.info.m_tx, tx.info.m_fee, tx.info.m_virtual_transaction_size, tx.info.txHeight));
+    }
+    const auto& txAncestorsAndDescendants = GetTxAncestorsAndDescendants(txs_removed_for_block);
+    std::vector<node::MiniMinerMempoolEntry> transactions;
+    std::map<Txid, std::set<Txid>> descendant_caches;
+    transactions.reserve(txAncestorsAndDescendants.size());
+    for (const auto& transaction : txAncestorsAndDescendants) {
+        const auto& txid = transaction.first;
+        const auto& [ancestors, descendants] = transaction.second;
+        int64_t vsize_ancestor = 0;
+        CAmount fee_with_ancestors = 0;
+        for (auto& ancestor_id : ancestors) {
+            const auto& ancestor = tx_caches.find(ancestor_id)->second;
+            vsize_ancestor += ancestor.m_virtual_transaction_size;
+            fee_with_ancestors += ancestor.m_fee;
+        }
+        descendant_caches.emplace(txid, descendants);
+        auto tx_info = tx_caches.find(txid)->second;
+        transactions.emplace_back(tx_info.m_tx, tx_info.m_virtual_transaction_size, vsize_ancestor, tx_info.m_fee, fee_with_ancestors);
+    }
+    return std::make_tuple(transactions, descendant_caches);
 }
