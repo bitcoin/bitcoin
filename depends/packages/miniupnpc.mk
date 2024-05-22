77b2321ca03dcbd5f77060510dc8a19e7f4fdfa2
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2024-05-22 08:54:41 +0100

    depends: Fetch miniupnpc sources from an alternative website
    The https://miniupnp.tuxfamily.org website is unavailable now.
    
    Github-Pull: #30151
    Rebased-From: 21b8a14d37c19ce292d5529597e0d52338db48a9

--- depends/packages/miniupnpc.mk
+++ depends/packages/miniupnpc.mk
@@ -1,6 +1,6 @@
 package=miniupnpc
 $(package)_version=2.2.2
-$(package)_download_path=https://miniupnp.tuxfamily.org/files/
+$(package)_download_path=http://miniupnp.free.fr/files/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=888fb0976ba61518276fe1eda988589c700a3f2a69d71089260d75562afd3687
 $(package)_patches=dont_leak_info.patch respect_mingw_cflags.patch no_libtool.patch
