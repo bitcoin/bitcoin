a5f361c5e59a721b97e458159b82d5b2a198c490
fanquake <fanquake@gmail.com>
2024-05-23 10:07:23 +0100

    [WIP] ci: use GCC 13 for some jobs

--- ci/test/00_setup_env_win64.sh
+++ ci/test/00_setup_env_win64.sh
@@ -7,13 +7,10 @@
 export LC_ALL=C.UTF-8
 
 export CONTAINER_NAME=ci_win64
-export CI_IMAGE_NAME_TAG="docker.io/amd64/debian:bookworm"  # Check that https://packages.debian.org/bookworm/g++-mingw-w64-x86-64-posix (version 12.2, similar to guix) can cross-compile
+export CI_IMAGE_NAME_TAG="docker.io/amd64/debian:trixie"  # Check that https://packages.debian.org/trixie/g++-mingw-w64-x86-64-posix (version 13.2, similar to guix) can cross-compile
 export HOST=x86_64-w64-mingw32
 export DPKG_ADD_ARCH="i386"
 export PACKAGES="nsis g++-mingw-w64-x86-64-posix wine-binfmt wine64 wine32 file"
 export RUN_FUNCTIONAL_TESTS=false
 export GOAL="deploy"
-# Prior to 11.0.0, the mingw-w64 headers were missing noreturn attributes, causing warnings when
-# cross-compiling for Windows. https://sourceforge.net/p/mingw-w64/bugs/306/
-# https://github.com/mingw-w64/mingw-w64/commit/1690994f515910a31b9fb7c7bd3a52d4ba987abe
-export BITCOIN_CONFIG="--enable-reduce-exports --disable-gui-tests CXXFLAGS='-Wno-return-type -Wno-error=maybe-uninitialized -Wno-error=array-bounds'"
+export BITCOIN_CONFIG="--enable-reduce-exports --disable-gui-tests CXXFLAGS='-Wno-error=maybe-uninitialized'"
