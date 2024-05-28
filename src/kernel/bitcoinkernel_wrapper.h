65571c36a265ec340343b555d1537c58ab335538
TheCharlatan <seb.kung@gmail.com>
2024-05-28 20:58:48 +0200

    kernel: Add chainstate manager object to C header
    This is the main driver class for anything validation related, so expose
    it here.
    
    Creating the chainstate manager options will currently also trigger the
    creation of their respectively configured directories.
    
    The chainstate manager and block manager options are consolidated into a
    single object. The kernel might eventually introduce a separate block
    manager object for the purposes of being a light-weight block store
    reader.
    
    The chainstate manager will associate with the context with which it was
    created for the duration of its lifetime and it keeps it in memory with
    a shared pointer.
    
    The tests now also create dedicated temporary directories. This is
    similar to the behaviour in the existing unit test framework.
    
    Co-authored-by: stickies-v <stickies-v@protonmail.com>

--- src/kernel/bitcoinkernel_wrapper.h
+++ src/kernel/bitcoinkernel_wrapper.h
@@ -11,6 +11,7 @@
 #include <memory>
 #include <span>
 #include <stdexcept>
+#include <string>
 #include <string_view>
 #include <type_traits>
 #include <utility>
@@ -618,6 +619,24 @@ public:
 
     Context()
         : Handle{btck_context_create(ContextOptions{}.get())} {}
+};
+
+class ChainstateManagerOptions : public UniqueHandle<btck_ChainstateManagerOptions, btck_chainstate_manager_options_destroy>
+{
+public:
+    ChainstateManagerOptions(const Context& context, const std::string& data_dir, const std::string& blocks_dir)
+        : UniqueHandle{btck_chainstate_manager_options_create(context.get(), data_dir.c_str(), data_dir.length(), blocks_dir.c_str(), blocks_dir.length())}
+    {
+    }
+};
+
+class ChainMan : UniqueHandle<btck_ChainstateManager, btck_chainstate_manager_destroy>
+{
+public:
+    ChainMan(const Context& context, const ChainstateManagerOptions& chainman_opts)
+        : UniqueHandle{btck_chainstate_manager_create(chainman_opts.get())}
+    {
+    }
 };
 
 } // namespace btck
