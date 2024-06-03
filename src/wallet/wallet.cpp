3c951f7774157d494c4f09d9ed099ade84d8ae8e
Ava Chow <github@achow101.com>
2024-06-03 16:24:13 -0400

    wallet: Replace chainStateFlushed in loading with WriteBestBlock
    The only reason to call chainStateFlushed during wallet loading is to
    ensure that the best block is written. Do these writes explicitly to
    prepare for removing chainStateFlushed.

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -3087,7 +3087,8 @@ std::shared_ptr<CWallet> CWallet::Create(WalletContext& context, const std::stri
         }
 
         if (chain) {
-            walletInstance->chainStateFlushed(ChainstateRole::NORMAL, chain->getTipLocator());
+            WalletBatch batch(walletInstance->GetDatabase());
+            batch.WriteBestBlock(chain->getTipLocator());
         }
     } else if (wallet_creation_flags & WALLET_FLAG_DISABLE_PRIVATE_KEYS) {
         // Make it impossible to disable private keys after creation
@@ -3374,7 +3375,8 @@ bool CWallet::AttachChain(const std::shared_ptr<CWallet>& walletInstance, interf
             }
         }
         walletInstance->m_attaching_chain = false;
-        walletInstance->chainStateFlushed(ChainstateRole::NORMAL, chain.getTipLocator());
+        WalletBatch batch(walletInstance->GetDatabase());
+        batch.WriteBestBlock(chain.getTipLocator());
         walletInstance->GetDatabase().IncrementUpdateCounter();
     }
     walletInstance->m_attaching_chain = false;
