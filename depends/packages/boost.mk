f2e05cd2a9bdc42fa45d284a5162f1f670d07ab6
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2024-06-03 09:49:34 +0100

    depends: Update Boost download link
    See: https://github.com/boostorg/boost-tasks/pull/3
    
    Github-Pull: #30217
    Rebased-From: ffbc173ca1ed6b93de8bf3f88b8aed0743f4916c

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -1,6 +1,6 @@
 package=boost
 $(package)_version=1.81.0
-$(package)_download_path=https://boostorg.jfrog.io/artifactory/main/release/$($(package)_version)/source/
+$(package)_download_path=https://archives.boost.io/release/$($(package)_version)/source/
 $(package)_file_name=boost_$(subst .,_,$($(package)_version)).tar.bz2
 $(package)_sha256_hash=71feeed900fbccca04a3b4f2f84a7c217186f28a940ed8b7ed4725986baf99fa
 $(package)_patches=process_macos_sdk.patch
