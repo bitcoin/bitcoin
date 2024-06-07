22e980219a3ca8d8bc9e9bff1e7da600788dd212
Gleb Naumenko <naumenko.gs@gmail.com>
2024-06-07 15:01:58 -0400

    p2p: Add peers to reconciliation queue on negotiation
    When we're finalizing negotiation, we should add the peers
    for which we will initiate reconciliations to the queue.
    
    Co-authored-by: Sergi Delgado Segura <sergi.delgado.s@gmail.com>

--- src/node/txreconciliation.cpp
+++ src/node/txreconciliation.cpp
@@ -107,9 +107,16 @@ private:
     size_t m_inbounds_count GUARDED_BY(m_txreconciliation_mutex){0};
 
     /**
+     * Maintains a queue of reconciliations we should initiate. To achieve higher bandwidth
+     * conservation and avoid overflows, we should reconcile in the same order, because then it’s
+     * easier to estimate set difference size.
+     */
+    std::deque<NodeId> m_queue GUARDED_BY(m_txreconciliation_mutex);
+
+    /*
      * Collection of inbound peers selected for fanout. Should get periodically rotated using RotateInboundFanoutTargets.
      */
-    std::unordered_set<NodeId> m_inbound_fanout_targets;
+    std::unordered_set<NodeId> m_inbound_fanout_targets GUARDED_BY(m_txreconciliation_mutex);
 
     /**
      * Next time m_inbound_fanout_targets need to be rotated.
@@ -200,6 +207,14 @@ public:
                 }
             }
         }
+        if (!is_peer_inbound) {
+            // If this is the first outbound peer registered for reconciliation, don't bother instantly requesting reconciliation.
+            // Set the next request one RECON_REQUEST_INTERVAL in the future so we have time to gather some transactions
+            if (m_queue.empty()) {
+                m_next_recon_request = GetTime<std::chrono::microseconds>() + RECON_REQUEST_INTERVAL;
+            }
+            m_queue.push_back(peer_id);
+        }
         return ReconciliationRegisterResult::SUCCESS;
     }
 
@@ -280,10 +295,14 @@ public:
         if (peer == m_states.end()) return;
 
         const auto registered = std::get_if<TxReconciliationState>(&peer->second);
-        if (registered && !registered->m_we_initiate) {
-            Assert(m_inbounds_count > 0);
-            --m_inbounds_count;
-            m_inbound_fanout_targets.erase(peer_id);
+        if (registered) {
+            if (registered->m_we_initiate) {
+                m_queue.erase(std::remove(m_queue.begin(), m_queue.end(), peer_id), m_queue.end());
+            } else {
+                Assume(m_inbounds_count > 0);
+                --m_inbounds_count;
+                m_inbound_fanout_targets.erase(peer_id);
+            }
         }
 
         if (m_states.erase(peer_id)) {
