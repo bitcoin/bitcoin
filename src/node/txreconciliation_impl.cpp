7c4f38f127888f813e89dad58ce50958c8cffdc4
Sergi Delgado Segura <sergi.delgado.s@gmail.com>
2024-06-07 15:01:58 -0400

    p2p: Add peers to reconciliation queue on negotiation
    When we're finalizing negotiation, we should add the peers
    for which we will initiate reconciliations to the queue.
    
    Co-authored-by: Gleb Naumenko <naumenko.gs@gmail.com>

--- src/node/txreconciliation_impl.cpp
+++ src/node/txreconciliation_impl.cpp
@@ -53,6 +53,12 @@ private:
     /** Keeps track of how many of the registered peers are inbound. Updated on registering or forgetting peers. */
     size_t m_inbounds_count GUARDED_BY(m_txreconciliation_mutex){0};
 
+    /**
+     * Maintains a queue of reconciliations we should initiate. To achieve higher bandwidth conservation and avoid overflows,
+     * we should reconcile in the same order, because then it’s easier to estimate set difference size.
+     */
+    std::deque<NodeId> m_queue GUARDED_BY(m_txreconciliation_mutex);
+
     /** Collection of inbound peers selected for fanout. Should get periodically rotated using RotateInboundFanoutTargets. */
     std::unordered_set<NodeId> m_inbound_fanout_targets GUARDED_BY(m_txreconciliation_mutex);
 
@@ -123,6 +129,9 @@ public:
                 m_inbound_fanout_targets.insert(peer_id);
             }
         }
+        if (!is_peer_inbound) {
+            m_queue.push_back(peer_id);
+        }
         return std::nullopt;
     }
 
@@ -142,9 +151,14 @@ public:
         LOCK(m_txreconciliation_mutex);
 
         auto peer_state = GetRegisteredPeerState(peer_id);
-        if (peer_state && !peer_state->m_we_initiate) {
-            Assert(m_inbounds_count > 0);
-            --m_inbounds_count;
+        if (peer_state) {
+            if (peer_state->m_we_initiate) {
+                m_queue.erase(std::remove(m_queue.begin(), m_queue.end(), peer_id), m_queue.end());
+            } else {
+                Assume(m_inbounds_count > 0);
+                --m_inbounds_count;
+                m_inbound_fanout_targets.erase(peer_id);
+            }
         }
 
         if (m_states.erase(peer_id)) {
