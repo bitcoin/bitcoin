f3906e86b1e99666836595706bca0beb2dfeab0c
Sergi Delgado Segura <sergi.delgado.s@gmail.com>
2024-06-07 13:54:52 -0400

    p2p: Add PeerManager method to count the amount of inbound/outbounds fanout peers

--- src/net_processing.h
+++ src/net_processing.h
@@ -108,6 +108,9 @@ public:
     /** Relay transaction to all peers. */
     virtual void RelayTransaction(const uint256& txid, const uint256& wtxid) = 0;
 
+    /** Get the amount of inbounds (first) and outbounds fanout peers (second). */
+    virtual std::pair<size_t, size_t> GetFanoutPeersCount() = 0;
+
     /** Send ping message to all peers */
     virtual void SendPings() = 0;
 
