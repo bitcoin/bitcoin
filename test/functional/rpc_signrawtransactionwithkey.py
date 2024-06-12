e2779ce98b39e14cada08a654928e798436f5a46
brunoerg <brunoely.gc@gmail.com>
2024-06-12 11:11:32 -0300

    test: cover more errors for `signrawtransactionwithkey` RPC
    * Invalid private key
    * TX decode failed

--- test/functional/rpc_signrawtransactionwithkey.py
+++ test/functional/rpc_signrawtransactionwithkey.py
@@ -126,10 +126,20 @@ class SignRawTransactionWithKeyTest(BitcoinTestFramework):
         privkeys = [self.nodes[0].get_deterministic_priv_key().key]
         assert_raises_rpc_error(-8, "'all' is not a valid sighash parameter.", self.nodes[0].signrawtransactionwithkey, tx, privkeys, sighashtype="all")
 
+    def invalid_private_key_and_tx(self):
+        self.log.info("Test signing transaction with an invalid private key")
+        tx = self.nodes[0].createrawtransaction(INPUTS, OUTPUTS)
+        privkeys = ["123"]
+        assert_raises_rpc_error(-5, "Invalid private key", self.nodes[0].signrawtransactionwithkey, tx, privkeys)
+        self.log.info("Test signing transaction with an invalid tx hex")
+        privkeys = [self.nodes[0].get_deterministic_priv_key().key]
+        assert_raises_rpc_error(-22, "TX decode failed. Make sure the tx has at least one input.", self.nodes[0].signrawtransactionwithkey, tx + "00", privkeys)
+
     def run_test(self):
         self.successful_signing_test()
         self.witness_script_test()
         self.invalid_sighashtype_test()
+        self.invalid_private_key_and_tx()
 
 
 if __name__ == '__main__':
