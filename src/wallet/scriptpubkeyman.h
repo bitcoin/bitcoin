574b840c42d7ad5e53fe5c400581561912d1504f
Ava Chow <github@achow101.com>
2024-06-24 12:33:59 -0400

    Revert "wallet: Move LegacyScriptPubKeyMan::IsMine to LegacyDataSPKM"
    This reverts commit bbb1d51e1240da61db2ca1036f9ec91fd2f36f2d.

--- src/wallet/scriptpubkeyman.h
+++ src/wallet/scriptpubkeyman.h
@@ -314,8 +314,6 @@ public:
     std::unordered_set<CScript, SaltedSipHasher> GetScriptPubKeys() const override;
     std::unique_ptr<SigningProvider> GetSolvingProvider(const CScript& script) const override;
     uint256 GetID() const override { return uint256::ONE; }
-    // TODO: Remove IsMine when deleting LegacyScriptPubKeyMan
-    isminetype IsMine(const CScript& script) const override;
 
     // FillableSigningProvider overrides
     bool HaveKey(const CKeyID &address) const override;
@@ -449,6 +447,7 @@ public:
     LegacyScriptPubKeyMan(WalletStorage& storage, int64_t keypool_size) : LegacyDataSPKM(storage), m_keypool_size(keypool_size) {}
 
     util::Result<CTxDestination> GetNewDestination(const OutputType type) override;
+    isminetype IsMine(const CScript& script) const override;
 
     bool Encrypt(const CKeyingMaterial& master_key, WalletBatch* batch) override;
 
