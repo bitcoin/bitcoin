b66b885186da2d3815db2ac2d7e540c865009598
Elias Rad <146735585+nnsW3@users.noreply.github.com>
2024-06-25 19:04:16 +0300

    fix 00_setup_env.sh

--- ci/test/00_setup_env.sh
+++ ci/test/00_setup_env.sh
@@ -8,7 +8,7 @@ export LC_ALL=C.UTF-8
 
 set -ex
 
-# The source root dir, usually from git, usually read-only.
+# The source root dir, usually from git, is usually read-only.
 # The ci system copies this folder.
 BASE_READ_ONLY_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )"/../../ >/dev/null 2>&1 && pwd )
 export BASE_READ_ONLY_DIR
@@ -16,7 +16,7 @@ export BASE_READ_ONLY_DIR
 # This folder will also hold any SDKs.
 # This folder only exists on the ci guest and will be a copy of BASE_READ_ONLY_DIR
 export BASE_ROOT_DIR="${BASE_ROOT_DIR:-/ci_container_base}"
-# The depends dir.
+# The depends on dir.
 # This folder exists only on the ci guest, and on the ci host as a volume.
 export DEPENDS_DIR=${DEPENDS_DIR:-$BASE_ROOT_DIR/depends}
 # A folder for the ci system to put temporary files (build result, datadirs for tests, ...)
