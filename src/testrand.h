1408944d2ec9f78e62bf91a5e5a50317ba3060c5
fanquake <fanquake@gmail.com>
2024-06-25 15:01:00 +0100

    Squashed 'src/secp256k1/' changes from 06bff6dec8..4af241b320
    4af241b320 Merge bitcoin-core/secp256k1#1535: build: Replace hardcoded "auto" value with default one
    f473c959f0 Merge bitcoin-core/secp256k1#1543: cmake: Do not modify build types when integrating by downstream project
    d403eea484 Merge bitcoin-core/secp256k1#1546: cmake: Rename `SECP256K1_LATE_CFLAGS` and switch to Bitcoin Core's approach
    d7ae25ce6f Merge bitcoin-core/secp256k1#1550: fix: typos in secp256k1.c
    0e2fadb20c fix: typos in secp256k1.c
    69b2192ad4 Merge bitcoin-core/secp256k1#1545: cmake: Do not set `CTEST_TEST_TARGET_ALIAS`
    5dd637f3cf Merge bitcoin-core/secp256k1#1548: README: mention ellswift module
    7454a53736 README: mention ellswift module
    4706be2cd0 cmake: Reimplement `SECP256K1_APPEND_CFLAGS` using Bitcoin Core approach
    c2764dbb99 cmake: Rename `SECP256K1_LATE_CFLAGS` to `SECP256K1_APPEND_CFLAGS`
    f87a3589f4 cmake: Do not set `CTEST_TEST_TARGET_ALIAS`
    158f9e5eae cmake: Do not modify build types when integrating by downstream project
    35c0fdc86b Merge bitcoin-core/secp256k1#1529: cmake: Fix cache issue when integrating by downstream project
    4392f0f717 Merge bitcoin-core/secp256k1#1533: tests: refactor: tidy up util functions (#1491)
    bedffd53d8 Merge bitcoin-core/secp256k1#1488: ci: Add native macOS arm64 job
    4b8d5eeacf Merge bitcoin-core/secp256k1#1532: cmake: Disable eager MSan in ctime_tests
    f55703ba49 autotools: Delete unneeded compiler test
    396e885886 autotools: Align MSan checking code with CMake's implementation
    abde59f52d cmake: Report more compiler details in summary
    7abf979a43 cmake: Disable `ctime_tests` if build with `-fsanitize=memory`
    4d9645bee0 cmake: Remove "AUTO" value of `SECP256K1_ECMULT_GEN_KB` option
    a06805ee74 cmake: Remove "AUTO" value of `SECP256K1_ECMULT_WINDOW_SIZE` option
    1791f6fce4 Merge bitcoin-core/secp256k1#1517: autotools: Disable eager MSan in ctime_tests
    26b94ee92a autotools: Remove "auto" value of `--with-ecmult-gen-kb` option
    122dbaeb37 autotools: Remove "auto" value of `--with-ecmult-window` option
    e73f6f8fd9 tests: refactor: drop `secp256k1_` prefix from testrand.h functions
    0ee7453a99 tests: refactor: add `testutil_` prefix to testutil.h functions
    0c6bc76dcd tests: refactor: move `random_` helpers from tests.c to testutil.h
    0fef8479be tests: refactor: rename `random_field_element_magnitude` -> `random_fe_magnitude`
    59db007f0f tests: refactor: rename `random_group_element_...` -> `random_ge_...`
    ebfb82ee2f ci: Add job with -fsanitize-memory-param-retval
    e1bef0961c configure: Move "experimental" warning to bottom
    55e5d975db autotools: Disable eager MSan in ctime_tests
    ec4c002faa cmake: Simplify `PROJECT_IS_TOP_LEVEL` emulation
    cae9a7ad14 cmake: Do not set emulated PROJECT_IS_TOP_LEVEL as cache variable
    218f0cc93b ci: Add native macOS arm64 job
    
    git-subtree-dir: src/secp256k1
    git-subtree-split: 4af241b32099067464e015fa66daac5096206dea

--- src/testrand.h
+++ src/testrand.h
@@ -12,37 +12,37 @@
 /* A non-cryptographic RNG used only for test infrastructure. */
 
 /** Seed the pseudorandom number generator for testing. */
-SECP256K1_INLINE static void secp256k1_testrand_seed(const unsigned char *seed16);
+SECP256K1_INLINE static void testrand_seed(const unsigned char *seed16);
 
 /** Generate a pseudorandom number in the range [0..2**32-1]. */
-SECP256K1_INLINE static uint32_t secp256k1_testrand32(void);
+SECP256K1_INLINE static uint32_t testrand32(void);
 
 /** Generate a pseudorandom number in the range [0..2**64-1]. */
-SECP256K1_INLINE static uint64_t secp256k1_testrand64(void);
+SECP256K1_INLINE static uint64_t testrand64(void);
 
 /** Generate a pseudorandom number in the range [0..2**bits-1]. Bits must be 1 or
  *  more. */
-SECP256K1_INLINE static uint64_t secp256k1_testrand_bits(int bits);
+SECP256K1_INLINE static uint64_t testrand_bits(int bits);
 
 /** Generate a pseudorandom number in the range [0..range-1]. */
-static uint32_t secp256k1_testrand_int(uint32_t range);
+static uint32_t testrand_int(uint32_t range);
 
 /** Generate a pseudorandom 32-byte array. */
-static void secp256k1_testrand256(unsigned char *b32);
+static void testrand256(unsigned char *b32);
 
 /** Generate a pseudorandom 32-byte array with long sequences of zero and one bits. */
-static void secp256k1_testrand256_test(unsigned char *b32);
+static void testrand256_test(unsigned char *b32);
 
 /** Generate pseudorandom bytes with long sequences of zero and one bits. */
-static void secp256k1_testrand_bytes_test(unsigned char *bytes, size_t len);
+static void testrand_bytes_test(unsigned char *bytes, size_t len);
 
 /** Flip a single random bit in a byte array */
-static void secp256k1_testrand_flip(unsigned char *b, size_t len);
+static void testrand_flip(unsigned char *b, size_t len);
 
 /** Initialize the test RNG using (hex encoded) array up to 16 bytes, or randomly if hexseed is NULL. */
-static void secp256k1_testrand_init(const char* hexseed);
+static void testrand_init(const char* hexseed);
 
 /** Print final test information. */
-static void secp256k1_testrand_finish(void);
+static void testrand_finish(void);
 
 #endif /* SECP256K1_TESTRAND_H */
