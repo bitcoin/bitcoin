916b3f00414ff96e32d241e321889ace6ab6b5c8
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2024-07-04 12:39:00 +0000

    merge bitcoin#24917: Make BlockManager::LoadBlockIndex private

--- src/validation.h
+++ src/validation.h
@@ -845,7 +845,6 @@ private:
     bool m_snapshot_validated{false};
 
     CBlockIndex* m_best_invalid;
-    friend bool BlockManager::LoadBlockIndex(const Consensus::Params&);
 
     //! Internal helper for ActivateSnapshot().
     [[nodiscard]] bool PopulateAndValidateSnapshot(
