15777f2e09f983c0c6cf1767d809b70887eaf6d7
alex v <alex@encrypt-s.com>
2024-07-05 21:15:07 +0200

    fix sigs

--- src/test/util_tests.cpp
+++ src/test/util_tests.cpp
@@ -1593,7 +1593,7 @@ BOOST_AUTO_TEST_CASE(message_sign)
     const std::string message = "Trust no one";
 
     const std::string expected_signature =
-        "IMP+uZWPFRS8hi0wTcc1kf5y0CVtXxSbI+EPXEucnKNHSfolrbv9doLbYRVsPcCRf1i384QdULL2o5Xc0l2bTrc=";
+        "HwLT3I4xgQaSZDhjg3QHTYp149isz26W2vDjURdBDlmECI1PiMOQDs9LgrtPYuyXZTBconIyD0/qQbZvkarYMtw=";
 
     CKey privkey;
     std::string generated_signature;
@@ -1655,7 +1655,7 @@ BOOST_AUTO_TEST_CASE(message_verify)
     BOOST_CHECK_EQUAL(
         MessageVerify(
             "15CRxFdyRpGZLW9w8HnHvVduizdL5jKNbs",
-            "IMP+uZWPFRS8hi0wTcc1kf5y0CVtXxSbI+EPXEucnKNHSfolrbv9doLbYRVsPcCRf1i384QdULL2o5Xc0l2bTrc=",
+            "HwLT3I4xgQaSZDhjg3QHTYp149isz26W2vDjURdBDlmECI1PiMOQDs9LgrtPYuyXZTBconIyD0/qQbZvkarYMtw=",
             "Trust no one"),
         MessageVerificationResult::OK);
 
