6c2538d5bfeafe6f234e5382c151d173826d78f0
Cory Fields <cory-nospam-@coryfields.com>
2024-07-11 18:10:39 +0000

    depends: Bump boost to 1.88.0 and use new CMake buildsystem
    This has a few advantages over the old method of simply copying headers:
    - Installs proper cmake files which can be picked up by our buildsystem
    - Only installs necessary headers, not all of boost
    
    Co-authored-by: Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -1,10 +1,29 @@
 package=boost
-$(package)_version=1.81.0
-$(package)_download_path=https://archives.boost.io/release/$($(package)_version)/source/
-$(package)_file_name=boost_$(subst .,_,$($(package)_version)).tar.gz
-$(package)_sha256_hash=205666dea9f6a7cfed87c7a6dfbeb52a2c1b9de55712c9c1a87735d7181452b6
+$(package)_version = 1.88.0
+$(package)_download_path = https://github.com/boostorg/boost/releases/download/boost-$($(package)_version)
+$(package)_file_name = boost-$($(package)_version)-cmake.tar.gz
+$(package)_sha256_hash = dcea50f40ba1ecfc448fdf886c0165cf3e525fef2c9e3e080b9804e8117b9694
+$(package)_patches = skip_compiled_targets.patch
+$(package)_build_subdir = build
+
+define $(package)_set_vars
+  $(package)_config_opts = -DBOOST_INCLUDE_LIBRARIES="multi_index;signals2;test"
+  $(package)_config_opts += -DBOOST_TEST_HEADERS_ONLY=ON
+  $(package)_config_opts += -DBOOST_ENABLE_MPI=OFF
+  $(package)_config_opts += -DBOOST_ENABLE_PYTHON=OFF
+  $(package)_config_opts += -DBOOST_INSTALL_LAYOUT=system
+  $(package)_config_opts += -DBUILD_TESTING=OFF
+  $(package)_config_opts += -DCMAKE_DISABLE_FIND_PACKAGE_ICU=ON
+endef
+
+define $(package)_preprocess_cmds
+  patch -p1 < $($(package)_patch_dir)/skip_compiled_targets.patch
+endef
+
+define $(package)_config_cmds
+  $($(package)_cmake) -S .. -B .
+endef
 
 define $(package)_stage_cmds
-  mkdir -p $($(package)_staging_prefix_dir)/include && \
-  cp -r boost $($(package)_staging_prefix_dir)/include
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install
 endef
