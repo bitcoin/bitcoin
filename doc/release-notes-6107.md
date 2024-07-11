89ade3e340225ccaa612eea652fd5916fbd80447
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2024-07-11 11:17:18 +0000

    rpc: disallow `coinjoin stop` if there's no CoinJoin session to stop

---
+++ doc/release-notes-6107.md
@@ -0,0 +1,4 @@
+RPC changes
+-----------
+
+- `coinjoin stop` will now return an error if there is no CoinJoin mixing session to stop
--- src/rpc/coinjoin.cpp
+++ src/rpc/coinjoin.cpp
@@ -172,6 +172,9 @@ static RPCHelpMan coinjoin_stop()
     auto cj_clientman = node.coinjoin_loader->walletman().Get(wallet->GetName());
 
     CHECK_NONFATAL(cj_clientman);
+    if (!cj_clientman->IsMixing()) {
+        throw JSONRPCError(RPC_INTERNAL_ERROR, "No mix session to stop");
+    }
     cj_clientman->StopMixing();
 
     return "Mixing was stopped";
