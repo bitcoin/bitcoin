04a3f65032fd95ae1523c515ccae0707a0894656
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2024-07-20 18:06:17 +0000

    merge bitcoin#23721: Move restorewallet() logic to the wallet section

--- src/wallet/interfaces.cpp
+++ src/wallet/interfaces.cpp
@@ -607,6 +607,12 @@ public:
         assert(m_context.m_coinjoin_loader);
         return MakeWallet(LoadWallet(*m_context.chain, *m_context.m_coinjoin_loader, name, true /* load_on_start */, options, status, error, warnings));
     }
+    std::unique_ptr<Wallet> restoreWallet(const std::string& backup_file, const std::string& wallet_name, bilingual_str& error, std::vector<bilingual_str>& warnings) override
+    {
+        DatabaseStatus status;
+        assert(m_context.m_coinjoin_loader);
+        return MakeWallet(RestoreWallet(*m_context.chain, *m_context.m_coinjoin_loader, backup_file, wallet_name, /*load_on_start=*/true, status, error, warnings));
+    }
     std::string getWalletDir() override
     {
         return GetWalletDir().string();
