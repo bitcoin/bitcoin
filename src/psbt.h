84306916496449b1cc0653d887c863e5331ea8f5
Ava Chow <github@achow101.com>
2024-07-22 17:14:53 -0400

    psbt: Change default psbt version to 2

--- src/psbt.h
+++ src/psbt.h
@@ -1353,7 +1353,7 @@ struct PartiallySignedTransaction
     CMutableTransaction GetUnsignedTx() const;
     Txid GetUniqueID() const;
     PartiallySignedTransaction() = default;
-    explicit PartiallySignedTransaction(const CMutableTransaction& tx, uint32_t version = 0);
+    explicit PartiallySignedTransaction(const CMutableTransaction& tx, uint32_t version = 2);
 
     template <typename Stream>
     inline void Serialize(Stream& s) const {
