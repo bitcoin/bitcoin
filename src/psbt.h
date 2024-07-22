28066c39f15ffdc8986521c788625b6b4372a7f3
Ava Chow <github@achow101.com>
2024-07-22 17:14:21 -0400

    Change PSBT::AddInput to take just PSBTInput

--- src/psbt.h
+++ src/psbt.h
@@ -1163,7 +1163,7 @@ struct PartiallySignedTransaction
     /** Merge psbt into this. The two psbts must have the same underlying CTransaction (i.e. the
       * same actual Bitcoin transaction.) Returns true if the merge succeeded, false otherwise. */
     [[nodiscard]] bool Merge(const PartiallySignedTransaction& psbt);
-    bool AddInput(const CTxIn& txin, PSBTInput& psbtin);
+    bool AddInput(PSBTInput& psbtin);
     bool AddOutput(const CTxOut& txout, const PSBTOutput& psbtout);
     void SetupFromTx(const CMutableTransaction& tx);
     void CacheUnsignedTxPieces();
