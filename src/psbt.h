db723739570edd63959befa6cc466f6d3f13355b
Ava Chow <github@achow101.com>
2024-07-22 17:14:17 -0400

    Add PSBT::ComputeLockTime()
    Function to compute the lock time for the transaction

--- src/psbt.h
+++ src/psbt.h
@@ -1335,6 +1335,7 @@ struct PartiallySignedTransaction
     bool AddOutput(const CTxOut& txout, const PSBTOutput& psbtout);
     void SetupFromTx(const CMutableTransaction& tx);
     void CacheUnsignedTxPieces();
+    bool ComputeTimeLock(uint32_t& locktime) const;
     PartiallySignedTransaction() = default;
     explicit PartiallySignedTransaction(const CMutableTransaction& tx);
 
