9e18381527996e1bd4c5a8b2fe0483fad8574d73
Ava Chow <github@achow101.com>
2024-07-22 17:14:18 -0400

    Add PSBT::GetUnsignedTx
    A helper function for getting the unsigned transaction regardless of
    psbt version.

--- src/psbt.h
+++ src/psbt.h
@@ -1342,6 +1342,7 @@ struct PartiallySignedTransaction
     void SetupFromTx(const CMutableTransaction& tx);
     void CacheUnsignedTxPieces();
     bool ComputeTimeLock(uint32_t& locktime) const;
+    CMutableTransaction GetUnsignedTx() const;
     PartiallySignedTransaction() = default;
     explicit PartiallySignedTransaction(const CMutableTransaction& tx);
 
