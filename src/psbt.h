92ea5722421dc75a1619fe648486a28490500a3e
Ava Chow <github@achow101.com>
2024-07-22 17:14:20 -0400

    Add PSBT::GetUniqueID
    The unique ID for PSBTv2 is different from v0. Use this function to get
    the ID without requiring the caller to know the version number.

--- src/psbt.h
+++ src/psbt.h
@@ -1337,6 +1337,7 @@ struct PartiallySignedTransaction
     void CacheUnsignedTxPieces();
     bool ComputeTimeLock(uint32_t& locktime) const;
     CMutableTransaction GetUnsignedTx() const;
+    uint256 GetUniqueID() const;
     PartiallySignedTransaction() = default;
     explicit PartiallySignedTransaction(const CMutableTransaction& tx);
 
