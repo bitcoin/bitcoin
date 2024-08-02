5e87f30f7c9f6b276ccace88a55b7f0abced591b
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2024-08-02 20:13:07 +0200

    test: check that keyless P2A 'signing' via `signrawtransactionwithkey` succeeds

--- test/functional/test_framework/address.py
+++ test/functional/test_framework/address.py
@@ -155,6 +155,9 @@ def output_key_to_p2tr(key, main=False):
     assert len(key) == 32
     return program_to_witness(1, key, main)
 
+def p2a(main=False):
+    return program_to_witness(1, "4e73", main)
+
 def check_key(key):
     if (type(key) is str):
         key = bytes.fromhex(key)  # Assuming this is hex string
