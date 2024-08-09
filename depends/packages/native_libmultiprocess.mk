4da20434d4d68b7933e39aca36faa6fd2264cc45
Ryan Ofsky <ryan@ofsky.org>
2024-08-09 11:35:17 -0400

    depends: Update libmultiprocess library for CustomMessage function and ThreadContext bugfix
    The CustomMessage functions allow simplifying custom IPC type code, and the
    bugfix is needed to prevent in a crash in a new test which creates and destroys
    connections in a loop. Upstream PRs are:
    
    https://github.com/chaincodelabs/libmultiprocess/pull/105 types: Add Custom{Build,Read,Pass}Message hooks
    https://github.com/chaincodelabs/libmultiprocess/pull/106 Bugfix: Clean up ThreadContext pointers when Connection is destroyed

--- depends/packages/native_libmultiprocess.mk
+++ depends/packages/native_libmultiprocess.mk
@@ -1,8 +1,8 @@
 package=native_libmultiprocess
-$(package)_version=6aca5f389bacf2942394b8738bbe15d6c9edfb9b
+$(package)_version=c1b4ab4eb897d3af09bc9b3cc30e2e6fff87f3e2
 $(package)_download_path=https://github.com/chaincodelabs/libmultiprocess/archive
 $(package)_file_name=$($(package)_version).tar.gz
-$(package)_sha256_hash=2efeed53542bc1d8af3291f2b6f0e5d430d86a5e04e415ce33c136f2c226a51d
+$(package)_sha256_hash=6edf5ad239ca9963c78f7878486fb41411efc9927c6073928a7d6edf947cac4a
 $(package)_dependencies=native_capnp
 
 define $(package)_config_cmds
