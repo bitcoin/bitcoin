a0eaa4749fe0f755e113eee70dee1989bdc07ad5
marcofleon <marleo23@proton.me>
2024-08-13 11:42:59 +0100

    Add FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION in PoW check
    To avoid PoW being a blocker for fuzz tests,
    `FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION` is used in fuzz builds to
    bypass the actual PoW validation in `CheckProofOfWork`. It's
    replaced with a check on the last byte of the hash, which allows the
    fuzzer to quickly generate (in)valid blocks by checking a single bit,
    rather than performing the full PoW computation.
    
    If PoW is the target of a fuzz test, then it should call
    `CheckProofOfWorkImpl`.

--- src/pow.h
+++ src/pow.h
@@ -19,6 +19,7 @@ unsigned int CalculateNextWorkRequired(const CBlockIndex* pindexLast, int64_t nF
 
 /** Check whether a block hash satisfies the proof-of-work requirement specified by nBits */
 bool CheckProofOfWork(uint256 hash, unsigned int nBits, const Consensus::Params&);
+bool CheckProofOfWorkImpl(uint256 hash, unsigned int nBits, const Consensus::Params&);
 
 /**
  * Return false if the proof-of-work requirement specified by new_nbits at a
