8dec4e1294a6c6895b4fe23f38cd067dc8f2352f
brunoerg <brunoely.gc@gmail.com>
2024-08-27 11:17:16 -0300

    fuzz: fix timeout in `crypto_fschacha20poly1305`

--- src/test/fuzz/crypto_chacha20poly1305.cpp
+++ src/test/fuzz/crypto_chacha20poly1305.cpp
@@ -130,7 +130,7 @@ FUZZ_TARGET(crypto_fschacha20poly1305)
     // data).
     InsecureRandomContext rng(provider.ConsumeIntegral<uint64_t>());
 
-    LIMITED_WHILE(provider.ConsumeBool(), 10000)
+    LIMITED_WHILE(provider.ConsumeBool(), 100)
     {
         // Mode:
         // - Bit 0: whether to use single-plain Encrypt/Decrypt; otherwise use a split at prefix.
