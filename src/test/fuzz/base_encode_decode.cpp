ad917fb105b68fb369455ec7c42292a7673db1ad
Lőrinc <pap.lorinc@gmail.com>
2024-08-29 19:03:40 +0200

    test: Fuzz Base32/Base58/Base64 roundtrips both ways
    A few other minor refactors were also done:
    * Spit out each base to separate fuzz target
    * Added symmetric encode-decode roundtrips all bases
    * Removed trim testing of encoded_string since base58 doesn't have whitespace padding
    * made the comparison stricter by removing unnecessary lowercase conversions for bases that have both cases in their alphabet

--- src/test/fuzz/base_encode_decode.cpp
+++ src/test/fuzz/base_encode_decode.cpp
@@ -10,45 +10,86 @@
 #include <util/string.h>
 
 #include <cassert>
-#include <cstdint>
 #include <string>
 #include <vector>
 
 using util::TrimString;
-using util::TrimStringView;
 
-FUZZ_TARGET(base_encode_decode)
+FUZZ_TARGET(base58_encode_decode)
 {
-    const std::string random_encoded_string(buffer.begin(), buffer.end());
+    std::string random_string(buffer.begin(), buffer.end());
+    std::vector<unsigned char> random_data{ToByteVector(random_string)};
 
+    // Decode/Encode roundtrip
     std::vector<unsigned char> decoded;
-    if (DecodeBase58(random_encoded_string, decoded, 100)) {
-        const std::string encoded_string = EncodeBase58(decoded);
-        assert(encoded_string == TrimStringView(encoded_string));
-        assert(ToLower(encoded_string) == ToLower(TrimString(random_encoded_string)));
+    if (DecodeBase58(random_string, decoded, 100)) {
+        auto encoded_string{EncodeBase58(decoded)};
+        assert(encoded_string == TrimString(random_string));
+        assert(encoded_string.empty() || !DecodeBase58(encoded_string, decoded, decoded.size() - 1));
     }
+    // Encode/Decode roundtrip
+    auto encoded{EncodeBase58(random_data)};
+    std::vector<unsigned char> roundtrip_decoded;
+    assert(DecodeBase58(encoded, roundtrip_decoded, random_string.size())
+        && roundtrip_decoded == random_data);
+}
 
-    if (DecodeBase58Check(random_encoded_string, decoded, 100)) {
-        const std::string encoded_string = EncodeBase58Check(decoded);
-        assert(encoded_string == TrimString(encoded_string));
-        assert(ToLower(encoded_string) == ToLower(TrimString(random_encoded_string)));
-    }
+FUZZ_TARGET(base58check_encode_decode)
+{
+    std::string random_string(buffer.begin(), buffer.end());
+    std::vector<unsigned char> random_data{ToByteVector(random_string)};
 
-    auto result = DecodeBase32(random_encoded_string);
-    if (result) {
-        const std::string encoded_string = EncodeBase32(*result);
-        assert(encoded_string == TrimStringView(encoded_string));
-        assert(ToLower(encoded_string) == ToLower(TrimString(random_encoded_string)));
+    // Decode/Encode roundtrip
+    std::vector<unsigned char> decoded;
+    if (DecodeBase58Check(random_string, decoded, 100)) {
+        auto encoded_string{EncodeBase58Check(decoded)};
+        assert(encoded_string == TrimString(random_string));
+        assert(encoded_string.empty() || !DecodeBase58Check(encoded_string, decoded, decoded.size() - 1));
     }
+    // Encode/Decode roundtrip
+    auto encoded{EncodeBase58Check(random_data)};
+    std::vector<unsigned char> roundtrip_decoded;
+    assert(DecodeBase58Check(encoded, roundtrip_decoded, random_string.size())
+        && roundtrip_decoded == random_data);
+}
 
-    result = DecodeBase64(random_encoded_string);
-    if (result) {
-        const std::string encoded_string = EncodeBase64(*result);
-        assert(encoded_string == TrimString(encoded_string));
-        assert(ToLower(encoded_string) == ToLower(TrimString(random_encoded_string)));
+FUZZ_TARGET(base32_encode_decode)
+{
+    std::string random_string(buffer.begin(), buffer.end());
+    std::vector<unsigned char> random_data{ToByteVector(random_string)};
+
+    // Decode/Encode roundtrip
+    if (auto result{DecodeBase32(random_string)}) {
+        auto encoded_string{EncodeBase32(*result)};
+        assert(encoded_string == ToLower(TrimString(random_string)));
     }
+    // Encode/Decode roundtrip
+    auto encoded{EncodeBase32(random_data)};
+    auto decoded{DecodeBase32(encoded)};
+    assert(decoded && random_data == *decoded);
+}
+
+FUZZ_TARGET(base64_encode_decode)
+{
+    std::string random_string(buffer.begin(), buffer.end());
+    std::vector<unsigned char> random_data{ToByteVector(random_string)};
+
+    // Decode/Encode roundtrip
+    if (auto result{DecodeBase64(random_string)}) {
+        auto encoded_string{EncodeBase64(*result)};
+        assert(encoded_string == TrimString(random_string));
+    }
+    // Encode/Decode roundtrip
+    auto encoded{EncodeBase64(random_data)};
+    auto decoded{DecodeBase64(encoded)};
+    assert(decoded && random_data == *decoded);
+}
+
+FUZZ_TARGET(psbt_base64_decode)
+{
+    std::string random_string(buffer.begin(), buffer.end());
 
     PartiallySignedTransaction psbt;
     std::string error;
-    (void)DecodeBase64PSBT(psbt, random_encoded_string, error);
+    (void) DecodeBase64PSBT(psbt, random_string, error);
 }
