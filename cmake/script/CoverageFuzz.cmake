d9fcbfc3727e06e6f57d4ab09861f3212d558426
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2024-09-04 06:29:37 +0100

    build: Add `JOBS` variable support to `CoverageFuzz.cmake` script

--- cmake/script/CoverageFuzz.cmake
+++ cmake/script/CoverageFuzz.cmake
@@ -8,8 +8,13 @@ if(NOT DEFINED FUZZ_CORPORA_DIR)
   set(FUZZ_CORPORA_DIR ${CMAKE_CURRENT_SOURCE_DIR}/qa-assets/fuzz_corpora)
 endif()
 
+set(fuzz_test_runner test/fuzz/test_runner.py ${FUZZ_CORPORA_DIR})
+if(DEFINED JOBS)
+  list(APPEND fuzz_test_runner -j ${JOBS})
+endif()
+
 execute_process(
-  COMMAND test/fuzz/test_runner.py ${FUZZ_CORPORA_DIR} --loglevel DEBUG
+  COMMAND ${fuzz_test_runner} --loglevel DEBUG
   WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
   COMMAND_ERROR_IS_FATAL ANY
 )
