788c1324f3d840f7a39b8bc3537dcff26ca0b552
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2024-09-04 07:52:02 +0100

    build: Unify `-logsourcelocations` format

--- cmake/ccache.cmake
+++ cmake/ccache.cmake
@@ -23,14 +23,6 @@ if(NOT MSVC)
   else()
     set(WITH_CCACHE OFF)
   endif()
-  if(WITH_CCACHE)
-    try_append_cxx_flags("-fdebug-prefix-map=A=B" TARGET core_interface SKIP_LINK
-      IF_CHECK_PASSED "-fdebug-prefix-map=${PROJECT_SOURCE_DIR}=."
-    )
-    try_append_cxx_flags("-fmacro-prefix-map=A=B" TARGET core_interface SKIP_LINK
-      IF_CHECK_PASSED "-fmacro-prefix-map=${PROJECT_SOURCE_DIR}=."
-    )
-  endif()
 endif()
 
 mark_as_advanced(CCACHE_EXECUTABLE)
