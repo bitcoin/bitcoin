bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

net: Apply outgoing connection permissions to automatic outgoing connections

src/net.cpp
@@ -515,8 +515,7 @@ CNode* CConnman::ConnectNode(CAddress addrConnect, const char *pszDest, bool fCo
     }
 
     NetPermissionFlags permission_flags = NetPermissionFlags::None;
-    std::vector<NetWhitelistPermissions> whitelist_permissions = conn_type == ConnectionType::MANUAL ? vWhitelistedRangeOutgoing : std::vector<NetWhitelistPermissions>{};
-    AddWhitelistPermissionFlags(permission_flags, addrConnect, whitelist_permissions);
+    AddWhitelistPermissionFlags(permission_flags, addrConnect, vWhitelistedRangeOutgoing);
 
     // Add node
     NodeId id = GetNewNodeId();

bitcoin/bitcoin

src/net.cpp
@@ -511,8 +511,7 @@ CNode* CConnman::ConnectNode(CAddress addrConnect, const char *pszDest, bool fCo
         }
 
         NetPermissionFlags permission_flags = NetPermissionFlags::None;
-        std::vector<NetWhitelistPermissions> whitelist_permissions = conn_type == ConnectionType::MANUAL ? vWhitelistedRangeOutgoing : std::vector<NetWhitelistPermissions>{};
-        AddWhitelistPermissionFlags(permission_flags, target_addr, whitelist_permissions);
+        AddWhitelistPermissionFlags(permission_flags, target_addr, vWhitelistedRangeOutgoing);
 
         // Add node
         NodeId id = GetNewNodeId();