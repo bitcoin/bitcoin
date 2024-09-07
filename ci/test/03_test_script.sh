c45186ca548362b75a6640393ccf79b11ff727da
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2024-09-07 14:28:28 +0100

    ci: Switch from `make` to `cmake --build`

--- ci/test/03_test_script.sh
+++ ci/test/03_test_script.sh
@@ -123,7 +123,7 @@ fi
 
 bash -c "cmake -S $BASE_ROOT_DIR $BITCOIN_CONFIG_ALL $BITCOIN_CONFIG || ( (cat $(cmake -P "${BASE_ROOT_DIR}/ci/test/GetCMakeLogFiles.cmake")) && false)"
 
-bash -c "make $MAKEJOBS all $GOAL" || ( echo "Build failure. Verbose build follows." && make all "$GOAL" V=1 ; false )
+bash -c "cmake --build . $MAKEJOBS --target all $GOAL" || ( echo "Build failure. Verbose build follows." && cmake --build . --target all "$GOAL" --verbose ; false )
 
 bash -c "${PRINT_CCACHE_STATISTICS}"
 du -sh "${DEPENDS_DIR}"/*/
