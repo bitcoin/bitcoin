503612f5a34051f81d33060228deef6d3c3cab4b
Bitcoil <ujahmichael323@gmail.com>
2024-09-16 15:49:43 +0100

    Update net.cpp

--- src/rpc/net.cpp
+++ src/rpc/net.cpp
@@ -492,7 +492,7 @@ static RPCHelpMan getaddednodeinfo()
                             {
                                 {RPCResult::Type::OBJ, "", "",
                                 {
-                                    {RPCResult::Type::STR, "address", "The bitcoin server IP and port we're connected to"},
+                                    {RPCResult::Type::STR, "address", "The bitcoil server IP and port we're connected to"},
                                     {RPCResult::Type::STR, "connected", "connection, inbound or outbound"},
                                 }},
                             }},
