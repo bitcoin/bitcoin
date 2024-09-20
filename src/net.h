058a055b9630f2c776177a0fa1cfc0dc4676b11d
Vasil Dimov <vd@FreeBSD.org>
2024-09-20 13:27:28 +0200

    net: index nodes in CConnman by id
    Change `CConnman::m_nodes` from `std::vector<CNode*>` to
    `std::unordered_map<NodeId, CNode*>` because interaction
    between `CConnman` and `SockMan` is going to be based on
    `NodeId` and finding a node by its id would better be fast.
    
    Change `PeerManagerImpl::EvictExtraOutboundPeers()` to account for nodes
    no longer always being in order of id. The old code would have failed to
    update `next_youngest_peer` correctly if `CConnman::m_nodes` hadn't
    always had nodes in ascending order of id.
    
    During fuzzing make sure that we don't generate duplicate `CNode` ids.
    The easiest way to do that is to use sequential ids.
    
    As a nice side effect the existent search-by-id operations in
    `CConnman::AttemptToEvictConnection()`,
    `CConnman::DisconnectNode()` and
    `CConnman::ForNode()` now become `O(1)` (were `O(number of nodes)`),
    as well as the erase in `CConnman::DisconnectNodes()`.

--- src/net.h
+++ src/net.h
@@ -45,6 +45,7 @@
 #include <optional>
 #include <queue>
 #include <thread>
+#include <unordered_map>
 #include <unordered_set>
 #include <vector>
 
@@ -1153,7 +1154,7 @@ public:
     void ForEachNode(const NodeFn& func)
     {
         LOCK(m_nodes_mutex);
-        for (auto&& node : m_nodes) {
+        for (auto& [_, node] : m_nodes) {
             if (NodeFullyConnected(node))
                 func(node);
         }
@@ -1162,7 +1163,7 @@ public:
     void ForEachNode(const NodeFn& func) const
     {
         LOCK(m_nodes_mutex);
-        for (auto&& node : m_nodes) {
+        for (auto& [_, node] : m_nodes) {
             if (NodeFullyConnected(node))
                 func(node);
         }
@@ -1432,7 +1433,7 @@ private:
     std::vector<AddedNodeParams> m_added_node_params GUARDED_BY(m_added_nodes_mutex);
 
     mutable Mutex m_added_nodes_mutex;
-    std::vector<CNode*> m_nodes GUARDED_BY(m_nodes_mutex);
+    std::unordered_map<NodeId, CNode*> m_nodes GUARDED_BY(m_nodes_mutex);
     std::list<CNode*> m_nodes_disconnected;
     mutable RecursiveMutex m_nodes_mutex;
     unsigned int nPrevNodeCount{0};
@@ -1618,8 +1619,9 @@ private:
         {
             {
                 LOCK(connman.m_nodes_mutex);
-                m_nodes_copy = connman.m_nodes;
-                for (auto& node : m_nodes_copy) {
+                m_nodes_copy.reserve(connman.m_nodes.size());
+                for (auto& [_, node] : connman.m_nodes) {
+                    m_nodes_copy.push_back(node);
                     node->AddRef();
                 }
             }
