d549f01caab659f1c431ccabf0f5ff1800debcfe
Matthew Zipkin <pinheadmz@pm.me>
2024-09-30 13:22:29 -0400

    http: Implement HTTPHeaders class
    see:
    https://www.rfc-editor.org/rfc/rfc2616#section-4.2
    https://www.rfc-editor.org/rfc/rfc7231#section-5
    https://www.rfc-editor.org/rfc/rfc7231#section-7
    https://httpwg.org/specs/rfc9111.html#header.field.definitions

--- src/httpserver.h
+++ src/httpserver.h
@@ -6,9 +6,13 @@
 #define BITCOIN_HTTPSERVER_H
 
 #include <functional>
+#include <map>
 #include <optional>
 #include <span>
 #include <string>
+
+#include <util/strencodings.h>
+#include <util/string.h>
 
 namespace util {
 class SignalInterrupt;
@@ -195,5 +199,27 @@ public:
 private:
     struct event* ev;
 };
+
+namespace http_bitcoin {
+class HTTPHeaders
+{
+public:
+    std::optional<std::string> Find(const std::string& key) const;
+    void Write(const std::string& key, const std::string& value);
+    void Remove(const std::string& key);
+    /*
+     * @returns false if LineReader hits the end of the buffer before reading an
+     *                \n, meaning that we are still waiting on more data from the client.
+     *          true  after reading an entire HTTP headers section, terminated
+     *                by an empty line and \n.
+     * @throws on exceeded read limit and on bad headers syntax (e.g. no ":" in a line)
+     */
+    bool Read(util::LineReader& reader);
+    std::string Stringify() const;
+
+private:
+    std::unordered_map<std::string, std::string, util::AsciiCaseInsensitiveHash, util::AsciiCaseInsensitiveKeyEqual> m_map;
+};
+} // namespace http_bitcoin
 
 #endif // BITCOIN_HTTPSERVER_H
