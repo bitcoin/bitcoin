5b709e434de0c095ded4885ea6b7621085a698c5
Cory Fields <cory-nospam-@coryfields.com>
2024-10-07 21:05:08 +0000

    build: support ccache + pre-compiled headers when possible
    ccache supports setting key=value options as of v4.8. Prior to that, setting
    sloppiness would've required substantial hacking.
    
    For 4.8+:
      - Set the required sloppiness options
      - Set "-Xclang -fno-pch-timestamp" for clang
    
    Both options are documented here: https://ccache.dev/manual/latest.html

--- cmake/ccache.cmake
+++ cmake/ccache.cmake
@@ -24,6 +24,23 @@ if(NOT MSVC)
     set(WITH_CCACHE OFF)
   endif()
   if(WITH_CCACHE)
+    if(NOT CMAKE_DISABLE_PRECOMPILE_HEADERS)
+      # ccache only allows us to reasonable set the necessary pch option as of version 4.8.
+      # Rather than attempting to parse the version, try running with the necessary options and a dummy compile.
+      execute_process(COMMAND "${CMAKE_CXX_COMPILER_LAUNCHER}" sloppiness=pch_defines,time_macros ${CMAKE_CXX_COMPILER} --version
+        RESULT_VARIABLE CCACHE_TEST_RESULT
+        OUTPUT_QUIET
+        ERROR_QUIET
+      )
+      if(NOT CCACHE_TEST_RESULT)
+        list(APPEND CMAKE_CXX_COMPILER_LAUNCHER sloppiness=pch_defines,time_macros)
+        # According to the ccache docs clang requires this option.
+        try_append_cxx_flags("-Xclang -fno-pch-timestamp" TARGET core_interface SKIP_LINK)
+      else()
+        list(APPEND configure_warnings "For pre-compiled headers support with ccache, version 4.8+ is required. Disabling pch.")
+        set(CMAKE_DISABLE_PRECOMPILE_HEADERS ON)
+      endif()
+    endif()
     try_append_cxx_flags("-fdebug-prefix-map=A=B" TARGET core_interface SKIP_LINK
       IF_CHECK_PASSED "-fdebug-prefix-map=${PROJECT_SOURCE_DIR}=."
     )
