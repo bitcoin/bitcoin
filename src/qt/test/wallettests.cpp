furszy/bitcoin-core

wallet: add 'internal' field to WalletDescriptor
To detect whether active and non-active descriptors are internal or not.

src/qt/test/wallettests.cpp
@@ -171,7 +171,7 @@ void TestGUI(interfaces::Node& node)
         std::string error;
         std::unique_ptr<Descriptor> desc = Parse("combo(" + EncodeSecret(test.coinbaseKey) + ")", provider, error, /* require_checksum=*/ false);
         assert(desc);
-        WalletDescriptor w_desc(std::move(desc), 0, 0, 1, 1);
+        WalletDescriptor w_desc(std::move(desc), 0, 0, 1, 1, /*internal=*/false);
         if (!wallet->AddWalletDescriptor(w_desc, provider, "", false)) assert(false);
         CTxDestination dest = GetDestinationForKey(test.coinbaseKey.GetPubKey(), wallet->m_default_address_type);
         wallet->SetAddressBook(dest, "", "receive");

src/wallet/rpc/backup.cpp
@@ -1553,7 +1553,7 @@ static UniValue ProcessDescriptorImport(CWallet& wallet, const UniValue& data, c
            }
         }
 
-        WalletDescriptor w_desc(std::move(parsed_desc), timestamp, range_start, range_end, next_index);
+        WalletDescriptor w_desc(std::move(parsed_desc), timestamp, range_start, range_end, next_index, internal);
 
         // Check if the wallet already contains the descriptor
         auto existing_spk_manager = wallet.GetDescriptorScriptPubKeyMan(w_desc);

src/wallet/test/ismine_tests.cpp
@@ -29,7 +29,7 @@ wallet::ScriptPubKeyMan* CreateDescriptor(CWallet& keystore, const std::string& 
 
     const int64_t range_start = 0, range_end = 1, next_index = 0, timestamp = 1;
 
-    WalletDescriptor w_desc(std::move(parsed_desc), timestamp, range_start, range_end, next_index);
+    WalletDescriptor w_desc(std::move(parsed_desc), timestamp, range_start, range_end, next_index, /*internal=*/false);
 
     LOCK(keystore.cs_wallet);
 

src/wallet/test/psbt_wallet_tests.cpp
@@ -22,7 +22,7 @@ static void import_descriptor(CWallet& wallet, const std::string& descriptor)
     std::string error;
     std::unique_ptr<Descriptor> desc = Parse(descriptor, provider, error, /* require_checksum=*/ false);
     assert(desc);
-    WalletDescriptor w_desc(std::move(desc), 0, 0, 10, 0);
+    WalletDescriptor w_desc(std::move(desc), 0, 0, 10, 0, /*internal=*/false);
     wallet.AddWalletDescriptor(w_desc, provider, "", false);
 }
 

src/wallet/test/util.cpp
@@ -31,7 +31,7 @@ std::unique_ptr<CWallet> CreateSyncedWallet(interfaces::Chain& chain, CChain& cc
         std::string error;
         std::unique_ptr<Descriptor> desc = Parse("combo(" + EncodeSecret(key) + ")", provider, error, /* require_checksum=*/ false);
         assert(desc);
-        WalletDescriptor w_desc(std::move(desc), 0, 0, 1, 1);
+        WalletDescriptor w_desc(std::move(desc), 0, 0, 1, 1, /*internal=*/false);
         if (!wallet->AddWalletDescriptor(w_desc, provider, "", false)) assert(false);
     }
     WalletRescanReserver reserver(*wallet);

src/wallet/test/wallet_tests.cpp
@@ -84,7 +84,7 @@ static void AddKey(CWallet& wallet, const CKey& key)
     std::string error;
     std::unique_ptr<Descriptor> desc = Parse("combo(" + EncodeSecret(key) + ")", provider, error, /* require_checksum=*/ false);
     assert(desc);
-    WalletDescriptor w_desc(std::move(desc), 0, 0, 1, 1);
+    WalletDescriptor w_desc(std::move(desc), 0, 0, 1, 1, /*internal=*/false);
     if (!wallet.AddWalletDescriptor(w_desc, provider, "", false)) assert(false);
 }
 

bitcoin/bitcoin

src/qt/test/wallettests.cpp
@@ -222,7 +222,7 @@ std::shared_ptr<CWallet> SetupDescriptorsWallet(interfaces::Node& node, TestChai
     assert(!descs.empty());
     assert(descs.size() == 1);
     auto& desc = descs.at(0);
-    WalletDescriptor w_desc(std::move(desc), 0, 0, 1, 1);
+    WalletDescriptor w_desc(std::move(desc), 0, 0, 1, 1, /*_internal=*/false);
     if (!wallet->AddWalletDescriptor(w_desc, provider, "", false)) assert(false);
     CTxDestination dest = GetDestinationForKey(test.coinbaseKey.GetPubKey(), wallet->m_default_address_type);
     wallet->SetAddressBook(dest, "", wallet::AddressPurpose::RECEIVE);

src/wallet/rpc/backup.cpp
@@ -1573,7 +1573,7 @@ static UniValue ProcessDescriptorImport(CWallet& wallet, const UniValue& data, c
                }
             }
 
-            WalletDescriptor w_desc(std::move(parsed_desc), timestamp, range_start, range_end, next_index);
+            WalletDescriptor w_desc(std::move(parsed_desc), timestamp, range_start, range_end, next_index, desc_internal);
 
             // Check if the wallet already contains the descriptor
             auto existing_spk_manager = wallet.GetDescriptorScriptPubKeyMan(w_desc);

src/wallet/test/ismine_tests.cpp
@@ -33,7 +33,7 @@ wallet::ScriptPubKeyMan* CreateDescriptor(CWallet& keystore, const std::string& 
 
     const int64_t range_start = 0, range_end = 1, next_index = 0, timestamp = 1;
 
-    WalletDescriptor w_desc(std::move(desc), timestamp, range_start, range_end, next_index);
+    WalletDescriptor w_desc(std::move(desc), timestamp, range_start, range_end, next_index, /*_internal=*/false);
 
     LOCK(keystore.cs_wallet);
 

src/wallet/test/psbt_wallet_tests.cpp
@@ -26,7 +26,7 @@ static void import_descriptor(CWallet& wallet, const std::string& descriptor)
     auto descs = Parse(descriptor, provider, error, /* require_checksum=*/ false);
     assert(descs.size() == 1);
     auto& desc = descs.at(0);
-    WalletDescriptor w_desc(std::move(desc), 0, 0, 10, 0);
+    WalletDescriptor w_desc(std::move(desc), 0, 0, 10, 0, /*_internal=*/false);
     wallet.AddWalletDescriptor(w_desc, provider, "", false);
 }
 

src/wallet/test/util.cpp
@@ -34,7 +34,7 @@ std::unique_ptr<CWallet> CreateSyncedWallet(interfaces::Chain& chain, CChain& cc
         auto descs = Parse("combo(" + EncodeSecret(key) + ")", provider, error, /* require_checksum=*/ false);
         assert(descs.size() == 1);
         auto& desc = descs.at(0);
-        WalletDescriptor w_desc(std::move(desc), 0, 0, 1, 1);
+        WalletDescriptor w_desc(std::move(desc), 0, 0, 1, 1, /*_internal=*/false);
         if (!wallet->AddWalletDescriptor(w_desc, provider, "", false)) assert(false);
     }
     WalletRescanReserver reserver(*wallet);

src/wallet/test/wallet_tests.cpp
@@ -68,7 +68,7 @@ static void AddKey(CWallet& wallet, const CKey& key)
     auto descs = Parse("combo(" + EncodeSecret(key) + ")", provider, error, /* require_checksum=*/ false);
     assert(descs.size() == 1);
     auto& desc = descs.at(0);
-    WalletDescriptor w_desc(std::move(desc), 0, 0, 1, 1);
+    WalletDescriptor w_desc(std::move(desc), 0, 0, 1, 1, /*_internal=*/false);
     if (!wallet.AddWalletDescriptor(w_desc, provider, "", false)) assert(false);
 }
 