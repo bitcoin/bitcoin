c0b3062215f66cdf5f26daf8af268583be9bf735
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2024-10-14 20:53:38 +0000

    merge bitcoin#28782: Add missing sync on send_version in peer_connect

--- test/functional/test_framework/p2p.py
+++ test/functional/test_framework/p2p.py
@@ -392,8 +392,8 @@ class P2PInterface(P2PConnection):
         vt.strSubVer = self.strSubVer
         self.on_connection_send_msg = vt  # Will be sent soon after connection_made
 
-    def peer_connect(self, *args, services=P2P_SERVICES, send_version=True, **kwargs):
-        create_conn = super().peer_connect(*args, **kwargs)
+    def peer_connect(self, *, services=P2P_SERVICES, send_version, **kwargs):
+        create_conn = super().peer_connect(**kwargs)
 
         if send_version:
             self.peer_connect_send_version(services)
