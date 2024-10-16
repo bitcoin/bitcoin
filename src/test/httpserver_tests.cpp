8fcb3ee42c06c1dc228c48ccb8e2b4f86aad963d
Matthew Zipkin <pinheadmz@pm.me>
2024-10-16 10:57:06 -0400

    http: Implement HTTPResponse class
    HTTP Response message:
    https://datatracker.ietf.org/doc/html/rfc1945#section-6
    
    Status line (first line of response):
    https://datatracker.ietf.org/doc/html/rfc1945#section-6.1
    
    Status code definitions:
    https://datatracker.ietf.org/doc/html/rfc1945#section-9
    
    This commit also moves StringToBuffer to test/util/str
    since it is now used in more than one test.

--- src/test/httpserver_tests.cpp
+++ src/test/httpserver_tests.cpp
@@ -3,12 +3,14 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include <httpserver.h>
+#include <rpc/protocol.h>
 #include <test/util/setup_common.h>
-#include <util/strencodings.h>
+#include <test/util/str.h>
 
 #include <boost/test/unit_test.hpp>
 
 using http_bitcoin::HTTPHeaders;
+using http_bitcoin::HTTPResponse;
 
 BOOST_FIXTURE_TEST_SUITE(httpserver_tests, BasicTestingSetup)
 
@@ -95,5 +97,28 @@ BOOST_AUTO_TEST_CASE(http_headers_tests)
         BOOST_CHECK_EQUAL(headers.Find("Content-Length").value(), "46");
         BOOST_CHECK(!headers.Find("Pizza"));
     }
+}
+
+BOOST_AUTO_TEST_CASE(http_response_tests)
+{
+    // Typical HTTP 1.1 response headers
+    HTTPHeaders headers{};
+    headers.Write("Content-Length", "41");
+    // Response points to headers which already exist because some of them
+    // are set before we even know what the response will be.
+    HTTPResponse res;
+    res.m_version_major = 1;
+    res.m_version_minor = 1;
+    res.m_status = HTTP_OK;
+    res.m_reason = HTTPReason.find(res.m_status)->second;
+    res.m_body = StringToBuffer("{\"result\":865793,\"error\":null,\"id\":null\"}");
+    res.m_headers = std::move(headers);
+    // Only one header means we don't need to worry about unordered_map,
+    // this should always be the same:
+    BOOST_CHECK_EQUAL(
+        res.StringifyHeaders(),
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Length: 41\r\n"
+        "\r\n");
 }
 BOOST_AUTO_TEST_SUITE_END()
