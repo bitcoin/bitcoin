49ef53ce0091f195f60c6cc2259fbd01f49fa951
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2024-10-19 12:13:01 +0000

    merge bitcoin#23491: Move minisketchwrapper to src/node

--- src/node/minisketchwrapper.h
+++ src/node/minisketchwrapper.h
@@ -2,8 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_MINISKETCHWRAPPER_H
-#define BITCOIN_MINISKETCHWRAPPER_H
+#ifndef BITCOIN_NODE_MINISKETCHWRAPPER_H
+#define BITCOIN_NODE_MINISKETCHWRAPPER_H
 
 #include <minisketch.h>
 #include <cstddef>
@@ -14,4 +14,4 @@ Minisketch MakeMinisketch32(size_t capacity);
 /** Wrapper around Minisketch::CreateFP. */
 Minisketch MakeMinisketch32FP(size_t max_elements, uint32_t fpbits);
 
-#endif // BITCOIN_DBWRAPPER_H
+#endif // BITCOIN_NODE_MINISKETCHWRAPPER_H
