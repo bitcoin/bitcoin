bb97b1ffa9f02bf9c05f653602cfb1cf48efb7fa
Martin Zumsande <mzumsande@gmail.com>
2024-10-23 14:20:12 -0400

    test: fix intermittent timeout in p2p_seednodes.py
    On some CI runs, the timer in ThreadOpenConnection was only started *after*
    the mocktime was set. Fix this by waiting for the first connection attempt,
    which happens after the timer was started.
    
    Also convert some comments into log messages/add a log, so that the test
    isn't completely silent.

--- test/functional/p2p_seednode.py
+++ test/functional/p2p_seednode.py
@@ -20,17 +20,18 @@ class P2PSeedNodes(BitcoinTestFramework):
         self.disable_autoconnect = False
 
     def test_no_seednode(self):
-        # Check that if no seednode is provided, the node proceeds as usual (without waiting)
+        self.log.info("Check that if no seednode is provided, the node proceeds as usual (without waiting)")
         with self.nodes[0].assert_debug_log(expected_msgs=[], unexpected_msgs=["Empty addrman, adding seednode", f"Couldn't connect to peers from addrman after {ADD_NEXT_SEEDNODE} seconds. Adding seednode"], timeout=ADD_NEXT_SEEDNODE):
             self.restart_node(0)
 
     def test_seednode_empty_addrman(self):
         seed_node = "0.0.0.1"
-        # Check that the seednode is added to m_addr_fetches on bootstrap on an empty addrman
+        self.log.info("Check that the seednode is immediately added on bootstrap on an empty addrman")
         with self.nodes[0].assert_debug_log(expected_msgs=[f"Empty addrman, adding seednode ({seed_node}) to addrfetch"], timeout=ADD_NEXT_SEEDNODE):
             self.restart_node(0, extra_args=[f'-seednode={seed_node}'])
 
     def test_seednode_addrman_unreachable_peers(self):
+        self.log.info("Check that if addrman is non-empty, seednodes are queried with a delay")
         seed_node = "0.0.0.2"
         node = self.nodes[0]
         # Fill the addrman with unreachable nodes
@@ -40,8 +41,10 @@ class P2PSeedNodes(BitcoinTestFramework):
             node.addpeeraddress(ip, port)
 
         # Restart the node so seednode is processed again
-        with node.assert_debug_log(expected_msgs=[f"Couldn't connect to peers from addrman after {ADD_NEXT_SEEDNODE} seconds. Adding seednode ({seed_node}) to addrfetch"], unexpected_msgs=["Empty addrman, adding seednode"], timeout=ADD_NEXT_SEEDNODE * 1.5):
+        with node.assert_debug_log(expected_msgs=["trying v1 connection"], timeout=ADD_NEXT_SEEDNODE):
             self.restart_node(0, extra_args=[f'-seednode={seed_node}'])
+
+        with node.assert_debug_log(expected_msgs=[f"Couldn't connect to peers from addrman after {ADD_NEXT_SEEDNODE} seconds. Adding seednode ({seed_node}) to addrfetch"], unexpected_msgs=["Empty addrman, adding seednode"], timeout=ADD_NEXT_SEEDNODE * 1.5):
             node.setmocktime(int(time.time()) + ADD_NEXT_SEEDNODE + 1)
 
     def run_test(self):
@@ -52,4 +55,3 @@ class P2PSeedNodes(BitcoinTestFramework):
 
 if __name__ == '__main__':
     P2PSeedNodes(__file__).main()
-
