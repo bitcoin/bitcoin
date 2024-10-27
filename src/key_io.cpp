f998ac628685c6459d5de3bd7469f9046901fc47
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2024-10-27 15:31:48 +0100

    key: clear out secret data in `DecodeExtKey`
    Same as in `DecodeSecret`, we should also clear out the secret data from
    the vector resulting from the Base58Check parsing for xprv keys. Note
    that the if condition is needed in order to avoid UB, see #14242 (commit
    d855e4cac8303ad4e34ac31cfa7634286589ce99).
    
    Github-Pull: #31166
    Rebased-From: 559a8dd9c0aafcecf00f9ccd9aabe5720bcebe8c

--- src/key_io.cpp
+++ src/key_io.cpp
@@ -274,6 +274,9 @@ CExtKey DecodeExtKey(const std::string& str)
             key.Decode(data.data() + prefix.size());
         }
     }
+    if (!data.empty()) {
+        memory_cleanse(data.data(), data.size());
+    }
     return key;
 }
 
