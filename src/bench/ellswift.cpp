97f3c6deaf5ce40731746368df9ad7cd2357e15d
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2024-11-01 20:54:03 +0000

    merge bitcoin#24989: rename BytePtr to AsBytePtr

--- src/bench/ellswift.cpp
+++ src/bench/ellswift.cpp
@@ -22,7 +22,7 @@ static void EllSwiftCreate(benchmark::Bench& bench)
         key.Set(UCharCast(ret.data()), UCharCast(ret.data()) + 32, true);
         assert(key.IsValid());
         /* Use the last 32 bytes of the ellswift encoded public key as next entropy. */
-        std::copy(ret.begin() + 32, ret.begin() + 64, BytePtr(entropy.data()));
+        std::copy(ret.begin() + 32, ret.begin() + 64, AsBytePtr(entropy.data()));
     });
 
     ECC_Stop();
