092822d131fa48aba8ee2efff772b59513d35b6b
Ryan Ofsky <ryan@ofsky.org>
2024-11-05 14:33:09 -0500

    move-only: move AddArg default values to headers
    Move constant declarations referenced in AddArg calls to headers so AddArg
    calls be moved to header files in an upcoming scripted-diff.

---
+++ src/test/util/setup_common_settings.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2024 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_TEST_UTIL_SETUP_COMMON_SETTINGS_H
+#define BITCOIN_TEST_UTIL_SETUP_COMMON_SETTINGS_H
+
+constexpr inline auto TEST_DIR_PATH_ELEMENT{"test_common bitcoin"}; // Includes a space to catch possible path escape issues.
+
+#endif // BITCOIN_TEST_UTIL_SETUP_COMMON_SETTINGS_H
