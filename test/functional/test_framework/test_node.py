5321eb089cd1b39160038050a6ef6f27fd6f4ef9
Hodlinator <172445034+hodlinator@users.noreply.github.com>
2024-11-08 11:38:41 +0100

    test refactor: wait_for_rpc_connection - Treat OSErrors the same
    ConnectionResetError is an OSError as well (ECONNRESET), no reason to have a separate except-block for it.
    
    Also improves comments for other exceptions and make condition above more Pythonic.

--- test/functional/test_framework/test_node.py
+++ test/functional/test_framework/test_node.py
@@ -311,23 +311,22 @@ class TestNode():
                 self.rpc_connected = True
                 self.url = self.rpc.rpc_url
                 return
-            except JSONRPCException as e:  # Initialization phase
+            except JSONRPCException as e:
+                # Suppress these in favor of a later outcome (success, FailedToStartError, or timeout).
                 # -28 RPC in warmup
-                # -342 Service unavailable, RPC server started but is shutting down due to error
-                if e.error['code'] != -28 and e.error['code'] != -342:
+                # -342 Service unavailable, could be starting up or shutting down
+                if e.error['code'] not in [-28, -342]:
                     raise  # unknown JSON RPC exception
-            except ConnectionResetError:
-                # This might happen when the RPC server is in warmup, but shut down before the call to getblockcount
-                # succeeds. Try again to properly raise the FailedToStartError
-                pass
             except OSError as e:
-                if e.errno == errno.ETIMEDOUT:
-                    pass  # Treat identical to ConnectionResetError
-                elif e.errno == errno.ECONNREFUSED:
-                    pass  # Port not yet open?
-                else:
+                # Suppress similarly to the above JSONRPCException errors:
+                # ECONNRESET   - This might happen when the RPC server is in warmup, but shut down before the call to
+                #                getblockcount succeeds.
+                # ETIMEDOUT    - Treat identical to ECONNRESET
+                # ECONNREFUSED - Port not yet open?
+                if e.errno not in [errno.ECONNRESET, errno.ETIMEDOUT, errno.ECONNREFUSED]:
                     raise  # unknown OS error
-            except ValueError as e:  # cookie file not found and no rpcuser or rpcpassword; bitcoind is still starting
+            except ValueError as e:
+                # Suppress if cookie file is missing and no rpcuser or rpcpassword; bitcoind may be starting
                 if "No RPC credentials" not in str(e):
                     raise
             time.sleep(1.0 / poll_per_s)
