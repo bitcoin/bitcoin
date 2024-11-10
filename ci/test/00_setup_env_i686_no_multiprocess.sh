5c349adee008229e0a2f1899f89775f3e7d178e2
Sjors Provoost <sjors@sprovoost.nl>
2024-11-10 14:35:22 -0500

    build: depends makes libmultiprocess by default
    Except for Windows.
    
    Co-Authored-By: fanquake <fanquake@gmail.com>

--- ci/test/00_setup_env_i686_no_multiprocess.sh
+++ ci/test/00_setup_env_i686_no_multiprocess.sh
@@ -7,10 +7,10 @@
 export LC_ALL=C.UTF-8
 
 export HOST=i686-pc-linux-gnu
-export CONTAINER_NAME=ci_i686_multiprocess
+export CONTAINER_NAME=ci_i686_no_multiprocess
 export CI_IMAGE_NAME_TAG="docker.io/amd64/ubuntu:24.04"
 export PACKAGES="llvm clang g++-multilib"
-export DEP_OPTS="DEBUG=1 MULTIPROCESS=1"
+export DEP_OPTS="DEBUG=1 NO_MULTIPROCESS=1"
 export GOAL="install"
 export TEST_RUNNER_EXTRA="--v2transport"
 export BITCOIN_CONFIG="\
@@ -20,4 +20,3 @@ export BITCOIN_CONFIG="\
  -DCMAKE_CXX_FLAGS='-Wno-error=documentation' \
  -DAPPEND_CPPFLAGS='-DBOOST_MULTI_INDEX_ENABLE_SAFE_MODE' \
 "
-export BITCOIND=bitcoin-node  # Used in functional tests
