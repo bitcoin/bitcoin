9d89f515f33d29fb5abd7605f1a4ea57e264d24c
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2024-11-13 09:41:50 -0500

    fees: add forecast utility structure

---
+++ src/policy/fees/forecaster_util.h
@@ -0,0 +1,59 @@
+// Copyright (c) 2024 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_POLICY_FEES_FORECASTER_UTIL_H
+#define BITCOIN_POLICY_FEES_FORECASTER_UTIL_H
+
+#include <util/feefrac.h>
+
+#include <optional>
+#include <string>
+
+struct ForecastResponse {
+    FeeFrac low_priority;
+    FeeFrac high_priority;
+    unsigned int current_block_height{0};
+};
+
+class ForecastResult
+{
+public:
+    ForecastResult(ForecastResponse response, std::optional<std::string> error = std::nullopt)
+        : m_response(std::move(response)), m_error(std::move(error)) {}
+
+    bool empty() const
+    {
+        return m_response.low_priority.IsEmpty() && m_response.high_priority.IsEmpty();
+    }
+
+    bool operator<(const ForecastResult& other) const
+    {
+        return m_response.high_priority < other.m_response.high_priority;
+    }
+
+    const ForecastResponse& GetResponse() const
+    {
+        return m_response;
+    }
+
+    const std::optional<std::string>& GetError() const
+    {
+        return m_error;
+    }
+
+private:
+    ForecastResponse m_response;
+    std::optional<std::string> m_error;
+};
+
+enum class ConfirmationTargetType {
+    BLOCKS,
+};
+
+struct ConfirmationTarget {
+    unsigned int value;
+    ConfirmationTargetType type;
+};
+
+#endif // BITCOIN_POLICY_FEES_FORECASTER_UTIL_H
