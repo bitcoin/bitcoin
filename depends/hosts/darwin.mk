0f8c420f2ca7f826232651a4ddc3d5435fd183ec
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2024-11-14 18:06:51 +0000

    merge bitcoin#29890: remove some tools when cross-compiling for macOS

--- depends/hosts/darwin.mk
+++ depends/hosts/darwin.mk
@@ -39,7 +39,7 @@ llvm_config_prog=$(shell $(SHELL) $(.SHELLFLAGS) "command -v llvm-config")
 llvm_lib_dir=$(shell $(llvm_config_prog) --libdir)
 endif
 
-cctools_TOOLS=AR RANLIB STRIP NM OTOOL INSTALL_NAME_TOOL
+cctools_TOOLS=AR RANLIB STRIP NM OTOOL
 
 # Make-only lowercase function
 lc = $(subst A,a,$(subst B,b,$(subst C,c,$(subst D,d,$(subst E,e,$(subst F,f,$(subst G,g,$(subst H,h,$(subst I,i,$(subst J,j,$(subst K,k,$(subst L,l,$(subst M,m,$(subst N,n,$(subst O,o,$(subst P,p,$(subst Q,q,$(subst R,r,$(subst S,s,$(subst T,t,$(subst U,u,$(subst V,v,$(subst W,w,$(subst X,x,$(subst Y,y,$(subst Z,z,$1))))))))))))))))))))))))))
