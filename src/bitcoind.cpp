1f322a09e3dee3bbd0cde89dec922d3a101bbef2
Luke Dashjr <luke-jr+git@utopios.org>
2024-11-14 21:01:28 +0000

    bitcoind: Keep "cutting-edge transaction services" for wallet description

--- src/bitcoind.cpp
+++ src/bitcoind.cpp
@@ -143,7 +143,7 @@ static bool ProcessInitCommands(ArgsManager& args)
             strUsage += "\n"
                 "The " PACKAGE_NAME " daemon (bitcoind) is a headless program that connects to the Bitcoin network to validate and relay transactions and blocks, as well as relaying addresses.\n\n"
                 "It provides the backbone of the Bitcoin network and its RPC, REST and ZMQ services can provide various transaction, block and address-related services.\n\n"
-                "There is an optional wallet component which provides transaction services.\n\n"
+                "There is an optional wallet component which provides cutting-edge transaction services.\n\n"
                 "It can be used in a headless environment or as part of a server setup.\n"
                 "\n"
                 "Usage: bitcoind [options]\n"
