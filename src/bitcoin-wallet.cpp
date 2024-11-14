0cef7f9cd80fa07c3cb28209c1bf1486ee53b9f3
Luke Dashjr <luke-jr+git@utopios.org>
2024-11-14 20:59:08 +0000

    bitcoin-wallet: Mention -testnet4 option in help

--- src/bitcoin-wallet.cpp
+++ src/bitcoin-wallet.cpp
@@ -69,7 +69,7 @@ static std::optional<int> WalletAppInit(ArgsManager& args, int argc, char* argv[
             strUsage += "\n"
                 "bitcoin-wallet is an offline tool for creating and interacting with " PACKAGE_NAME " wallet files.\n\n"
                 "By default bitcoin-wallet will act on wallets in the default mainnet wallet directory in the datadir.\n\n"
-                "To change the target wallet, use the -datadir, -wallet and -regtest / -signet / -testnet arguments.\n"
+                "To change the target wallet, use the -datadir, -wallet and -regtest / -signet / -testnet / -testnet4 arguments.\n"
                 "\n"
                 "Usage: bitcoin-wallet [options] <command>\n"
                 "\n";
