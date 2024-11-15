ee59e03d34d8bd85ee546c58bdd7cf25849e98bb
Pieter Wuille <pieter@wuille.net>
2024-11-14 22:45:46 -0500

    txgraph: (tests) add internal sanity check function
    To make testing more powerful, expose a function to perform an internal sanity
    check on the state of a TxGraph. This is especially important as TxGraphImpl
    contains many redundantly represented pieces of information:
    
    * graph contains clusters, which refer to entries, but the entries refer back
    * graph maintains pointers to Ref objects, which point back to the graph.
    
    This lets us make sure they are always in sync.

--- src/test/fuzz/txgraph.cpp
+++ src/test/fuzz/txgraph.cpp
@@ -356,6 +356,11 @@ FUZZ_TARGET(txgraph)
             }
         }
     }
+
+    // After running all modifications, perform an internal sanity check (before invoking
+    // inspectors that may modify the internal state).
+    real->SanityCheck();
+
     // Compare simple properties of the graph with the simulation.
     assert(real->GetTransactionCount() == sim.GetTransactionCount());
 
@@ -411,4 +416,7 @@ FUZZ_TARGET(txgraph)
             }
         }
     }
+
+    // Sanity check again (because invoking inspectors may modify internal unobservable state).
+    real->SanityCheck();
 }
