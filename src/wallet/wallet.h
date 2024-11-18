db225cea56b0531cc42d4b89dc61b02890f432ff
Ryan Ofsky <ryan@ofsky.org>
2024-11-18 10:29:55 -0500

    wallet, refactor: Replace GetDisplayName() with LogName()
    The GetDisplayName() method name was confusing because it suggested the return
    value could be used for display, while documentation and implementation
    indicated it only meant to be used for logging. Also the name didn't suggest
    that it was formatting the wallet names, which made it harder understand how
    messages were formatted in the places it was called. Fix these issues by
    splitting up the GetDisplayName() method and replacing it with LogName() /
    DisplayName() methods.
    
    This commit is a refactoring that does not change any behavior.

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -905,14 +905,14 @@ public:
     /** Loads the flags into the wallet. (used by LoadWallet) */
     bool LoadWalletFlags(uint64_t flags);
 
-    /** Returns a bracketed wallet name for displaying in logs, will return [default wallet] if the wallet has no name */
-    std::string GetDisplayName() const override
+    /** Return wallet name for use in logs, will return "default wallet" if the wallet has no name. */
+    std::string LogName() const override
     {
-        std::string wallet_name = GetName().length() == 0 ? "default wallet" : GetName();
-        return strprintf("[%s]", wallet_name);
+        std::string name{GetName()};
+        return name.empty() ? "default wallet" : name;
     };
 
-    /** Return wallet name for display, translating "default wallet" string if returned. */
+    /** Return wallet name for display, like LogName() but translates "default wallet" string. */
     std::string DisplayName() const
     {
         std::string name{GetName()};
@@ -923,7 +923,7 @@ public:
     template <typename... Params>
     void WalletLogPrintf(util::ConstevalFormatString<sizeof...(Params)> wallet_fmt, const Params&... params) const
     {
-        LogInfo("%s %s", GetDisplayName(), tfm::format(wallet_fmt, params...));
+        LogInfo("[%s] %s", LogName(), tfm::format(wallet_fmt, params...));
     };
 
     /** Upgrade the wallet */
