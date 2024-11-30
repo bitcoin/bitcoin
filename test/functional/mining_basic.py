a350a6796091030f90e485da97fa9e7b11023ccf
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2024-11-30 11:45:24 -0500

    miner: bugfix: remove duplicate space reservation for coinbase tx

--- test/functional/mining_basic.py
+++ test/functional/mining_basic.py
@@ -22,6 +22,7 @@ from test_framework.messages import (
     CBlock,
     CBlockHeader,
     COIN,
+    DEFAULT_COINBASE_TX_WEIGHT,
     ser_uint256,
 )
 from test_framework.p2p import P2PDataStore
@@ -73,7 +74,7 @@ class MiningTest(BitcoinTestFramework):
         mining_info = self.nodes[0].getmininginfo()
         assert_equal(mining_info['blocks'], 200)
         assert_equal(mining_info['currentblocktx'], 0)
-        assert_equal(mining_info['currentblockweight'], 4000)
+        assert_equal(mining_info['currentblockweight'], DEFAULT_COINBASE_TX_WEIGHT)
 
         self.log.info('test blockversion')
         self.restart_node(0, extra_args=[f'-mocktime={t}', '-blockversion=1337'])
