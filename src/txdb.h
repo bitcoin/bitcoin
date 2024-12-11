5db7d4d3d28bd1269a09955b4695135c86c4827d
TheCharlatan <seb.kung@gmail.com>
2024-12-11 22:15:22 +0100

    doc: Correct docstring describing max block tree db cache
    It is always applied in the same way, no matter how the txindex is
    setup. This was no longer accurate after 8181db8, where their
    initialization was made independent.

--- src/txdb.h
+++ src/txdb.h
@@ -27,11 +27,11 @@ static const int64_t nDefaultDbCache = 450;
 static const int64_t nDefaultDbBatchSize = 16 << 20;
 //! min. -dbcache (MiB)
 static const int64_t nMinDbCache = 4;
-//! Max memory allocated to block tree DB specific cache, if no -txindex (MiB)
+//! Max memory allocated to block tree DB specific cache (MiB)
 static const int64_t nMaxBlockDBCache = 2;
-//! Max memory allocated to block tree DB specific cache, if -txindex (MiB)
 // Unlike for the UTXO database, for the txindex scenario the leveldb cache make
 // a meaningful difference: https://github.com/bitcoin/bitcoin/pull/8273#issuecomment-229601991
+//! Max memory allocated to tx index DB specific cache in MiB.
 static const int64_t nMaxTxIndexCache = 1024;
 //! Max memory allocated to all block filter index caches combined in MiB.
 static const int64_t max_filter_index_cache = 1024;
