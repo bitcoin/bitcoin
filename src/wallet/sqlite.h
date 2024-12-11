4df5e62fef2522829eee09a28df398e60ed25fee
furszy <matiasfurszyfer@protonmail.com>
2024-12-11 13:10:01 -0500

    wallet: introduce method to return all db created files

--- src/wallet/sqlite.h
+++ src/wallet/sqlite.h
@@ -166,6 +166,14 @@ public:
     void IncrementUpdateCounter() override { ++nUpdateCounter; }
 
     std::string Filename() override { return m_file_path; }
+    /** Return paths to all database created files */
+    std::vector<fs::path> Files() override
+    {
+        std::vector<fs::path> files;
+        files.emplace_back(m_dir_path / fs::PathFromString(m_file_path));
+        files.emplace_back(m_dir_path / fs::PathFromString(m_file_path + "-journal"));
+        return files;
+    }
     std::string Format() override { return "sqlite"; }
 
     /** Make a SQLiteBatch connected to this database */
