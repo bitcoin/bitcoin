fa4085e4df78c58f0dcbbb66f3f8b28cb6d59fd5
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2024-12-17 19:52:16 +0100

    refactor: Return std::span from MakeByteSpan
    In theory this commit should only touch the span.h header, because
    std::span can implicilty convert into Span in most places, if needed.
    
    However, at least when using the clang compiler, there are some
    false-positive lifetimebound warnings and some implicit conversions can
    not be resolved.
    
    Thus, this refactoring commit also changed the affected places to
    replace Span with std::span.

--- src/pubkey.cpp
+++ src/pubkey.cpp
@@ -353,7 +353,7 @@ bool CPubKey::Derive(CPubKey& pubkeyChild, ChainCode &ccChild, unsigned int nChi
     return true;
 }
 
-EllSwiftPubKey::EllSwiftPubKey(Span<const std::byte> ellswift) noexcept
+EllSwiftPubKey::EllSwiftPubKey(std::span<const std::byte> ellswift) noexcept
 {
     assert(ellswift.size() == SIZE);
     std::copy(ellswift.begin(), ellswift.end(), m_pubkey.begin());
