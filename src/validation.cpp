5709718b830161b7c2ba0db545ef0cfa98423597
Lőrinc <pap.lorinc@gmail.com>
2024-12-18 19:37:33 +0100

    coins: warn on shutdown for big UTXO set flushes
    Setting a large `-dbcache` size postpones the index writes until the coins cache size exceeds the specified limit.
    This causes the final flush after manual termination to seemingly hang forever (e.g. tens of minutes for 20 GiB);
    Now that the `dbcache` upper cap has been lifted, this will become even more apparent, so a warning will be shown when large UTXO sets are flushed (currently >1 GiB), such as:
    > 2024-12-18T18:25:03Z Flushed fee estimates to fee_estimates.dat.
    > 2024-12-18T18:25:03Z [warning] Flushing large (1 GiB) UTXO set to disk, it may take several minutes
    > 2024-12-18T18:25:09Z Shutdown: done
    
    Note that the related BCLog::BENCH units were also converted to `KiB` from `kB` to unify the bases.
    
    Co-authored-by: Cory Fields <cory-nospam-@coryfields.com>

--- src/validation.cpp
+++ src/validation.cpp
@@ -88,6 +88,8 @@ using node::CBlockIndexHeightOnlyComparator;
 using node::CBlockIndexWorkComparator;
 using node::SnapshotMetadata;
 
+/** Size threshold for warning about slow UTXO set flush to disk. */
+static constexpr size_t WARN_FLUSH_COINS_SIZE = 1 << 30; // 1 GiB
 /** Time to wait between writing blocks/block index to disk. */
 static constexpr std::chrono::hours DATABASE_WRITE_INTERVAL{1};
 /** Time to wait between flushing chainstate to disk. */
@@ -2929,8 +2931,9 @@ bool Chainstate::FlushStateToDisk(
         }
         // Flush best chain related state. This can only be done if the blocks / block index write was also done.
         if (fDoFullFlush && !CoinsTip().GetBestBlock().IsNull()) {
-            LOG_TIME_MILLIS_WITH_CATEGORY(strprintf("write coins cache to disk (%d coins, %.2fkB)",
-                coins_count, coins_mem_usage / 1000), BCLog::BENCH);
+            if (coins_mem_usage >= WARN_FLUSH_COINS_SIZE) LogWarning("Flushing large (%d GiB) UTXO set to disk, it may take several minutes", coins_mem_usage >> 30);
+            LOG_TIME_MILLIS_WITH_CATEGORY(strprintf("write coins cache to disk (%d coins, %.2fKiB)",
+                coins_count, coins_mem_usage >> 10), BCLog::BENCH);
 
             // Typical Coin structures on disk are around 48 bytes in size.
             // Pushing a new one to the database can cause it to be written
