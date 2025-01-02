faaabfaea768deb7767c489d32fd2097fd180872
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2025-01-02 14:21:07 +0100

    ci: Bump centos stream 10

--- ci/test/00_setup_env_native_centos.sh
+++ ci/test/00_setup_env_native_centos.sh
@@ -7,9 +7,8 @@
 export LC_ALL=C.UTF-8
 
 export CONTAINER_NAME=ci_native_centos
-export CI_IMAGE_NAME_TAG="quay.io/centos/centos:stream9"
-export STREAM_GCC_V="12"
-export CI_BASE_PACKAGES="gcc-toolset-${STREAM_GCC_V}-gcc-c++ glibc-devel gcc-toolset-${STREAM_GCC_V}-libstdc++-devel ccache make git python3 python3-pip which patch xz procps-ng dash rsync coreutils bison e2fsprogs cmake"
+export CI_IMAGE_NAME_TAG="quay.io/centos/centos:stream10"
+export CI_BASE_PACKAGES="gcc-c++ glibc-devel libstdc++-devel ccache make git python3 python3-pip which patch xz procps-ng ksh rsync coreutils bison e2fsprogs cmake"
 export PIP_PACKAGES="pyzmq"
 export GOAL="install"
 export BITCOIN_CONFIG="-DWITH_ZMQ=ON -DBUILD_GUI=ON -DREDUCE_EXPORTS=ON"
