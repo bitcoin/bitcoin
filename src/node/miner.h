071354813783768e3dec3b209b539e3d0fd7a1a0
Sjors Provoost <sjors@sprovoost.nl>
2025-01-03 15:23:39 +0100

    refactor: add GetMinimumTime() helper
    Before bip94 there was an assumption that the minimum permitted
    timestamp is GetMedianTimePast() + 1.
    
    This commit splits a helper function out of UpdateTime() to
    obtain the minimum time in a way that takes the
    timewarp attack rule into account.

--- src/node/miner.h
+++ src/node/miner.h
@@ -211,6 +211,13 @@ private:
     void SortForBlock(const CTxMemPool::setEntries& package, std::vector<CTxMemPool::txiter>& sortedEntries);
 };
 
+/**
+ * Get the minimum time a miner should use in the next block. This always
+ * accounts for the BIP94 timewarp rule, so does not necessarily reflect the
+ * consensus limit.
+ */
+int64_t GetMinimumTime(const CBlockIndex* pindexPrev, const int64_t difficulty_adjustment_interval);
+
 int64_t UpdateTime(CBlockHeader* pblock, const Consensus::Params& consensusParams, const CBlockIndex* pindexPrev);
 
 /** Update an old GenerateCoinbaseCommitment from CreateNewBlock after the block txs have changed */
