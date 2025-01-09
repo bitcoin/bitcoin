a2e33262b9f2fcef6b12e5a35fa736fc3a5c458d
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2025-01-09 14:55:59 -0500

    fees: add Forecaster abstract class
    - This commit implements `Forecaster` abstract class
      as the base class of fee rate forecasters.
    
    - Derived classes must provide concrete implementation
      of the virtual methods.
    
    Co-authored-by: willcl-ark <will@256k1.dev>

---
+++ src/policy/fees/forecaster.h
@@ -0,0 +1,31 @@
+// Copyright (c) 2025 The Bitcoin Core developers
+// Distributed under the MIT software license. See the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_POLICY_FEES_FORECASTER_H
+#define BITCOIN_POLICY_FEES_FORECASTER_H
+
+#include <policy/fees/forecaster_util.h>
+
+/** \class Forecaster
+ * Abstract base class for fee rate forecasters.
+ */
+class Forecaster
+{
+public:
+    Forecaster() = default;
+
+    /**
+     * Forecast the fee rate required for package to confirm within
+     * a specified target.
+     *
+     * This pure virtual function must be overridden by derived classes.
+     * @param target The confirmation target for which to forecast the feerate for.
+     * @return ForecastResult containing the forecasted fee rate.
+     */
+    virtual ForecastResult EstimateFee(ConfirmationTarget& target) = 0;
+
+    virtual ~Forecaster() = default;
+};
+
+#endif // BITCOIN_POLICY_FEES_FORECASTER_H
