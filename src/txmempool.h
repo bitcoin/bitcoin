eba685c49f22889e0b816a6c04b2edd85d434801
Suhas Daftuar <sdaftuar@chaincode.com>
2025-01-09 11:04:14 -0500

    Create a txgraph inside CTxMemPool

--- src/txmempool.h
+++ src/txmempool.h
@@ -18,6 +18,7 @@
 #include <policy/packages.h>
 #include <primitives/transaction.h>
 #include <sync.h>
+#include <txgraph.h>
 #include <util/epochguard.h>
 #include <util/feefrac.h>
 #include <util/hasher.h>
@@ -376,6 +377,7 @@ public:
 
     uint64_t CalculateDescendantMaximum(txiter entry) const EXCLUSIVE_LOCKS_REQUIRED(cs);
 private:
+    std::unique_ptr<TxGraph> m_txgraph GUARDED_BY(cs);
     typedef std::map<txiter, setEntries, CompareIteratorByHash> cacheMap;
 
 
