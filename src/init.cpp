5cadb81081179cd7e2d82da2f2d8b94752723878
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2025-01-09 17:31:05 -0500

    fees: refactor: rename fees to block_policy_estimator
    - Also move it to policy/fees and update the includes

--- src/init.cpp
+++ src/init.cpp
@@ -58,7 +58,7 @@
 #include <node/miner.h>
 #include <node/peerman_args.h>
 #include <policy/feerate.h>
-#include <policy/fees.h>
+#include <policy/fees/block_policy_estimator.h>
 #include <policy/fees_args.h>
 #include <policy/policy.h>
 #include <policy/settings.h>
