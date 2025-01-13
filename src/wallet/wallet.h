99999352f98b01af6b54a9ebd5842909f3004f6c
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2025-01-13 21:34:08 +0100

    refactor: Avoid copies by using const references or by move-construction

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -785,7 +785,7 @@ public:
      */
     void MarkDestinationsDirty(const std::set<CTxDestination>& destinations) EXCLUSIVE_LOCKS_REQUIRED(cs_wallet);
 
-    util::Result<CTxDestination> GetNewDestination(const OutputType type, const std::string label);
+    util::Result<CTxDestination> GetNewDestination(const OutputType type, const std::string& label);
     util::Result<CTxDestination> GetNewChangeDestination(const OutputType type);
 
     isminetype IsMine(const CTxDestination& dest) const EXCLUSIVE_LOCKS_REQUIRED(cs_wallet);
