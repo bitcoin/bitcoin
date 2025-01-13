85f4f714712884d2ff7935808daceb6e802dfa38
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2025-01-13 17:59:06 -0500

    fees: make block_policy_estimator a forecaster

--- src/policy/fees/block_policy_estimator.cpp
+++ src/policy/fees/block_policy_estimator.cpp
@@ -10,6 +10,7 @@
 #include <kernel/mempool_entry.h>
 #include <logging.h>
 #include <policy/feerate.h>
+#include <policy/fees/forecaster_util.h>
 #include <primitives/transaction.h>
 #include <random.h>
 #include <serialize.h>
@@ -541,7 +542,7 @@ bool CBlockPolicyEstimator::_removeTx(const Txid& hash, bool inBlock)
 }
 
 CBlockPolicyEstimator::CBlockPolicyEstimator(const fs::path& estimation_filepath, const bool read_stale_estimates)
-    : m_estimation_filepath{estimation_filepath}
+    : Forecaster(ForecastType::BLOCK_POLICY), m_estimation_filepath{estimation_filepath}
 {
     static_assert(MIN_BUCKET_FEERATE > 0, "Min feerate must be nonzero");
     size_t bucketIndex = 0;
@@ -722,6 +723,29 @@ CFeeRate CBlockPolicyEstimator::estimateFee(int confTarget) const
         return CFeeRate(0);
 
     return estimateRawFee(confTarget, DOUBLE_SUCCESS_PCT, FeeEstimateHorizon::MED_HALFLIFE);
+}
+
+ForecastResult CBlockPolicyEstimator::ForecastFeeRate(int target, bool conservative) const
+{
+    ForecastResult result;
+    result.forecaster = ForecastType::BLOCK_POLICY;
+    FeeCalculation fee_calculation_result;
+    CFeeRate feerate{estimateSmartFee(target, &fee_calculation_result, conservative)};
+    result.current_block_height = fee_calculation_result.best_height;
+    if (feerate == CFeeRate(0)) {
+        result.error = "Insufficient data or no feerate found";
+        return result;
+    }
+    // Note: size can be any positive non-zero integer; the evaluated fee/size will result in the same fee rate,
+    // and we only care that the fee rate remains consistent.
+    int32_t size = 1000;
+    result.feerate = FeeFrac(feerate.GetFee(size), size);
+    return result;
+}
+
+unsigned int CBlockPolicyEstimator::MaximumTarget() const
+{
+    return HighestTargetTracked(FeeEstimateHorizon::LONG_HALFLIFE);
 }
 
 CFeeRate CBlockPolicyEstimator::estimateRawFee(int confTarget, double successThreshold, FeeEstimateHorizon horizon, EstimationResult* result) const
