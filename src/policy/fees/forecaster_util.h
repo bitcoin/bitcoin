655ba6768c78728a1d831c76c523238f0eec6ca0
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2025-01-15 11:26:09 -0500

    fees: add `CalculatePercentiles` function
    - The CalculatePercentiles function, given
      a vector of feerates in the order they were added
      to the block, will return the 25th, 50th, 75th,
      and 95th percentile feerates.
    
    - Also add a unit test for this function.

--- src/policy/fees/forecaster_util.h
+++ src/policy/fees/forecaster_util.h
@@ -45,6 +45,36 @@ struct ForecastResult {
     }
 };
 
+// Block percentiles fee rate (in BTC/vB).
+struct Percentiles {
+    FeeFrac p25;
+    FeeFrac p50;
+    FeeFrac p75;
+    FeeFrac p95;
+
+    Percentiles() = default;
+
+    bool empty() const
+    {
+        return p25.IsEmpty() && p50.IsEmpty() && p75.IsEmpty() && p95.IsEmpty();
+    }
+};
+
+/**
+ * Calculates the percentile fee rates from a given vector of fee rates.
+ *
+ * This function assumes that the fee rates in the input vector are sorted in descending order
+ * based on mining score priority. It ensures that the calculated percentile fee rates
+ * are monotonically decreasing by filtering out outliers. Outliers can occur when
+ * the mining score of a transaction increases due to the inclusion of its ancestors
+ * in different transaction packages.
+ *
+ * @param[in] package_feerates A vector containing packages fee rates.
+ * @param[in] total_weight The total weight units to use for percentile fee rate calculations.
+ * @return Percentiles object containing the calculated percentile fee rates.
+ */
+Percentiles CalculatePercentiles(const std::vector<FeeFrac>& package_feerates, const int32_t total_weight);
+
 std::string forecastTypeToString(ForecastType forecastType);
 
 #endif // BITCOIN_POLICY_FEES_FORECASTER_UTIL_H
