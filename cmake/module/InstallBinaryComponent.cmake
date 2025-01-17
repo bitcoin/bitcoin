cd97905ebc564b8b095099a28d1d5437951927c4
Ryan Ofsky <ryan@ofsky.org>
2025-01-17 11:44:46 -0500

    cmake: Move internal binaries from bin/ to libexec/
    This change moves binaries that are not typically invoked directly by users
    from the `bin/` directory to the `libexec/` directory in CMake installs and
    binary releases. The goal is to simplify the contents of `bin/` for end users
    while still making all binaries available when needed. After this change, the
    binaries remaining in `bin/` are:
    
    - bitcoin
    - bitcoin-cli
    - bitcoind
    - bitcoin-qt
    - bitcoin-tx
    - bitcoin-util
    - bitcoin-wallet
    
    And the binaries that are moved to `libexec/` are:
    
    - bench_bitcoin
    - bitcoin-chainstate(*)
    - bitcoin-gui(***)
    - bitcoin-node(***)
    - test_bitcoin(**)
    - test_bitcoin-qt
    
    (*) bitcoin-chainstate was previously missing an install rule and was actually
    not installed even when it was enabled.
    
    (**) test_bitcoin is the only libexec/ binary that is currently included in
    bitcoin binary releases. The others are only installed when building from
    source with relevant cmake options enabled.
    
    (***) bitcoin-node and bitcoin-gui are not currently built by default or
    included in binary releases but both of these changes are planned and
    implemented in #31802

--- cmake/module/InstallBinaryComponent.cmake
+++ cmake/module/InstallBinaryComponent.cmake
@@ -7,14 +7,19 @@ include(GNUInstallDirs)
 
 function(install_binary_component component)
   cmake_parse_arguments(PARSE_ARGV 1
-    IC                # prefix
-    "HAS_MANPAGE"     # options
-    ""                # one_value_keywords
-    ""                # multi_value_keywords
+    IC                          # prefix
+    "HAS_MANPAGE;INTERNAL"      # options
+    ""                          # one_value_keywords
+    ""                          # multi_value_keywords
   )
   set(target_name ${component})
+  if(IC_INTERNAL)
+    set(runtime_dest ${CMAKE_INSTALL_LIBEXECDIR})
+  else()
+    set(runtime_dest ${CMAKE_INSTALL_BINDIR})
+  endif()
   install(TARGETS ${target_name}
-    RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
+    RUNTIME DESTINATION ${runtime_dest}
     COMPONENT ${component}
   )
   if(INSTALL_MAN AND IC_HAS_MANPAGE)
