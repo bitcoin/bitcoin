1cbf964113ab62a6922ec118852aee8a7b726e68
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2025-01-23 12:11:35 -0500

    [wallet]: add `maxfeerate` wallet startup option
    - The commits adds a new wallet startup option `-maxfeerate`
    
    - The value will be used as the upper limit of wallet transaction fee rate.
    
    - The commit updates all instances where `-maxtxfee` value is utilized to check
      wallet transactions fee rate upper limit to now use `-maxfeerate` value.
    
    - `settxfee` RPC now only check if the fee rate user set is less `minrelaytxfee`,
       less than wallet min fee, or whether it exceed `maxfeerate`.
       We should not check whether `settxfee` RPC respects `maxtxfee` limit in
       in the functional test because its not enforced.
       We now only check if `settxfee` respects `maxfeerate` limit.

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -3102,13 +3102,25 @@ std::shared_ptr<CWallet> CWallet::Create(WalletContext& context, const std::stri
             warnings.push_back(strprintf(_("%s is set very high! Fees this large could be paid on a single transaction."), "-maxtxfee"));
         }
 
-        if (chain && CFeeRate{max_tx_fee.value(), 1000} < chain->relayMinFee()) {
-            error = strprintf(_("Invalid amount for %s=<amount>: '%s' (must be at least the minrelay fee of %s to prevent stuck transactions)"),
-                "-maxtxfee", *arg, chain->relayMinFee().ToString());
+        walletInstance->m_max_tx_fee = max_tx_fee.value();
+    }
+
+    if (const auto arg{args.GetArg("-maxfeerate")}) {
+        std::optional<CAmount> max_tx_fee_rate = ParseMoney(*arg);
+        if (!max_tx_fee_rate) {
+            error = AmountErrMsg("maxfeerate", args.GetArg("-maxfeerate", ""));
             return nullptr;
         }
+        if (chain && CFeeRate(*max_tx_fee_rate) < chain->relayMinFee()) {
+            error = strprintf(_("Invalid amount for %s=<amount>: '%s' (must be at least the minrelay fee of %s to prevent stuck transactions)"),
+                              "-maxfeerate", *arg, chain->relayMinFee().ToString());
+            return nullptr;
+        }
+        if (CFeeRate(*max_tx_fee_rate) > HIGH_MAX_TX_FEERATE) {
+            warnings.push_back(strprintf(_("%s is set very high! Fee rate this large could be paid on a single transaction."), "-maxfeerate"));
+        }
 
-        walletInstance->m_max_tx_fee = max_tx_fee.value();
+        walletInstance->m_max_tx_fee_rate = CFeeRate(*max_tx_fee_rate);
     }
 
     if (const auto arg{args.GetArg("-consolidatefeerate")}) {
