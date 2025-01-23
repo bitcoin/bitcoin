66d40a288c7cfe4efa6cbb154fda67c170fb4511
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2025-01-23 14:16:27 -0500

    [wallet]: warn user if 1kvb tx with `-maxtxfee` base fee has fee rate less than `minrelaytxfee`
    - Wallet prevents creating transactions with fee rates lower than `-minrelaytxfee`.
      Also prevents creating a transaction with a base fee above `-maxtxfee`.
    
    - Warn user if a 1kvb transaction, with a base fee set to `-maxtxfee`, has a fee rate less than
      `-minrelaytxfee`. It is likely that some transactions with fee rates greater than or equal
      to the `-minrelaytxfee` will also likely exceed `-maxtxfee`, the wallet won't be able to create
      transactions.

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -3034,7 +3034,16 @@ bool CWallet::LoadWalletArgs(std::shared_ptr<CWallet> wallet, const WalletContex
             return false;
         } else if (max_tx_fee.value() > HIGH_MAX_TX_FEE) {
             warnings.push_back(strprintf(_("%s is set very high! Fees this large could be paid on a single transaction."), "-maxtxfee"));
+        } else if (chain && CFeeRate(max_tx_fee.value(), 1000) < chain->relayMinFee()) {
+            // Wallet prevents creating transactions with fee rates lower than minrelaytxfee.
+            // Also the wallet prevents creating transactions with base fee above maxtxfee.
+            // Warn when a 1kvb transaction, with a base fee set to maxtxfee, has a fee rate less than minrelaytxfee.
+            // It is likely that some transactions with fee rates greater than or equal to the minrelaytxfee will exceed maxtxfee.
+            // In such cases, the wallet won't be able to create transactions. Therefore, warn the user.
+            warnings.push_back(strprintf(_("Invalid amount for %s=<amount>: '%s' conflicts with the minimum relay transaction feerate %s. Please set a higher %s or lower %s"),
+                                         "-maxtxfee", args.GetArg("-maxtxfee", ""), chain->relayMinFee().ToString(), "-maxtxfee", "-minrelaytxfee"));
         }
+
         wallet->m_max_tx_fee = max_tx_fee.value();
     }
 
