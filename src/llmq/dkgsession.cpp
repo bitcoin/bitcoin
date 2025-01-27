darkcoin/darkcoin

perf: introduce ToBytes in bls.h and use it in all spots trivially possible, convert vecBytes to std::array
this results in an improved reindex speed as previously roughly 2% of total cycles were just in allocating the vector.

src/llmq/dkgsession.cpp
@@ -1016,12 +1016,12 @@ void CDKGSession::SendCommitment(CDKGPendingMessages& pendingMessages, PeerManag
 
     if (lieType == 3) {
         const bool is_bls_legacy = bls::bls_legacy_scheme.load();
-        std::vector<uint8_t> buf = qc.sig.ToByteVector(is_bls_legacy);
+        auto buf = qc.sig.ToBytes(is_bls_legacy);
         buf[5]++;
         qc.sig.SetBytes(buf, is_bls_legacy);
     } else if (lieType == 4) {
         const bool is_bls_legacy = bls::bls_legacy_scheme.load();
-        std::vector<uint8_t> buf = qc.quorumSig.ToByteVector(is_bls_legacy);
+        auto buf = qc.quorumSig.ToBytes(is_bls_legacy);
         buf[5]++;
         qc.quorumSig.SetBytes(buf, is_bls_legacy);
     }

bitcoin/bitcoin

src/llmq/dkgsession.cpp
@@ -1019,11 +1019,11 @@ void CDKGSession::SendCommitment(CDKGPendingMessages& pendingMessages, PeerManag
     qc.quorumSig = skShare.Sign(commitmentHash, m_use_legacy_bls);
 
     if (lieType == 3) {
-        std::vector<uint8_t> buf = qc.sig.ToByteVector(m_use_legacy_bls);
+        auto buf = qc.sig.ToBytes(m_use_legacy_bls);
         buf[5]++;
         qc.sig.SetBytes(buf, m_use_legacy_bls);
     } else if (lieType == 4) {
-        std::vector<uint8_t> buf = qc.quorumSig.ToByteVector(m_use_legacy_bls);
+        auto buf = qc.quorumSig.ToBytes(m_use_legacy_bls);
         buf[5]++;
         qc.quorumSig.SetBytes(buf, m_use_legacy_bls);
     }