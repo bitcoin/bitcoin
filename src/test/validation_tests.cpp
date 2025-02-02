bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

QA: Check that a few defined mainchain checkpoints are working
This partially reverts dce8360e44d5330cc9f9d09c9b09ac9237237204.

src/test/validation_tests.cpp
@@ -37,6 +37,25 @@ static void TestBlockSubsidyHalvings(int nSubsidyHalvingInterval)
     TestBlockSubsidyHalvings(consensusParams);
 }
 
+BOOST_AUTO_TEST_CASE(checkpoint_sanity)
+{
+    const auto chainParams = CreateChainParams(*m_node.args, CBaseChainParams::MAIN);
+    const auto& checkpoints = chainParams->Checkpoints();
+
+    uint256 p11111 = uint256S("0x0000000069e244f73d78e8fd29ba2fd2ed618bd6fa2ee92559f542fdb26e7c1d");
+    uint256 p134444 = uint256S("0x00000000000005b12ffd4cd315cd34ffd4a594f430ac814c91184a0d42d2b0fe");
+    BOOST_CHECK(checkpoints.CheckBlock(11111, p11111));
+    BOOST_CHECK(checkpoints.CheckBlock(134444, p134444));
+
+    // Wrong hashes at checkpoints should fail:
+    BOOST_CHECK(!checkpoints.CheckBlock(11111, p134444));
+    BOOST_CHECK(!checkpoints.CheckBlock(134444, p11111));
+
+    // ... but any hash not at a checkpoint should succeed:
+    BOOST_CHECK(checkpoints.CheckBlock(11111+1, p134444));
+    BOOST_CHECK(checkpoints.CheckBlock(134444+1, p11111));
+}
+
 BOOST_AUTO_TEST_CASE(block_subsidy_test)
 {
     const auto chainParams = CreateChainParams(*m_node.args, CBaseChainParams::MAIN);

bitcoin/bitcoin

src/test/validation_tests.cpp
@@ -45,6 +45,25 @@ static void TestBlockSubsidyHalvings(int nSubsidyHalvingInterval)
     TestBlockSubsidyHalvings(consensusParams);
 }
 
+BOOST_AUTO_TEST_CASE(checkpoint_sanity)
+{
+    const auto chainParams = CreateChainParams(*m_node.args, ChainType::MAIN);
+    const auto& checkpoints = chainParams->Checkpoints();
+
+    uint256 p11111 = uint256S("0x0000000069e244f73d78e8fd29ba2fd2ed618bd6fa2ee92559f542fdb26e7c1d");
+    uint256 p134444 = uint256S("0x00000000000005b12ffd4cd315cd34ffd4a594f430ac814c91184a0d42d2b0fe");
+    BOOST_CHECK(checkpoints.CheckBlock(11111, p11111));
+    BOOST_CHECK(checkpoints.CheckBlock(134444, p134444));
+
+    // Wrong hashes at checkpoints should fail:
+    BOOST_CHECK(!checkpoints.CheckBlock(11111, p134444));
+    BOOST_CHECK(!checkpoints.CheckBlock(134444, p11111));
+
+    // ... but any hash not at a checkpoint should succeed:
+    BOOST_CHECK(checkpoints.CheckBlock(11111+1, p134444));
+    BOOST_CHECK(checkpoints.CheckBlock(134444+1, p11111));
+}
+
 BOOST_AUTO_TEST_CASE(block_subsidy_test)
 {
     const auto chainParams = CreateChainParams(*m_node.args, ChainType::MAIN);