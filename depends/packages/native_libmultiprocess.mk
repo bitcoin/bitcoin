1746618e081d582673b458091c46bb3f71ba3a56
Ryan Ofsky <ryan@ofsky.org>
2025-02-03 09:23:54 -0500

    depends: Update libmultiprocess library before converting to subtree
    Bring in a few cmake changes to improve cmake support and fix compile/lint/tidy warnings.
    
    https://github.com/chaincodelabs/libmultiprocess/pull/140 build: don't clobber user/superproject c++ version
    https://github.com/chaincodelabs/libmultiprocess/pull/142 build: add option for external mpgen binary
    https://github.com/chaincodelabs/libmultiprocess/pull/143 cleanup: initialize vars in the EventLoop constructor in the correct order
    https://github.com/chaincodelabs/libmultiprocess/pull/146 cmake: Suppress compiler warnings from capnproto headers
    https://github.com/chaincodelabs/libmultiprocess/pull/147 cmake: EXTERNAL_MPGEN cleanups
    https://github.com/chaincodelabs/libmultiprocess/pull/148 util: fix -Wpessimizing-move warning
    https://github.com/chaincodelabs/libmultiprocess/pull/145 CTest: Module must be included at the top level
    https://github.com/chaincodelabs/libmultiprocess/pull/149 Avoid `-Wundef` compiler warnings
    https://github.com/chaincodelabs/libmultiprocess/pull/152 refactor: Fix compiler and clang-tidy warnings
    https://github.com/chaincodelabs/libmultiprocess/pull/155 scripted-diff: s/Libmultiprocess_EXTERNAL_MPGEN/MPGEN_EXECUTABLE/g
    https://github.com/chaincodelabs/libmultiprocess/pull/156 refactor: Remove locale-dependent function calls
    https://github.com/chaincodelabs/libmultiprocess/pull/157 refactor: Avoid using std::format
    https://github.com/chaincodelabs/libmultiprocess/pull/159 bugfix: Do not lock EventLoop::mutex after EventLoop is done
    https://github.com/chaincodelabs/libmultiprocess/pull/161 cmake: Avoid including CTest if not top level project

--- depends/packages/native_libmultiprocess.mk
+++ depends/packages/native_libmultiprocess.mk
@@ -1,8 +1,8 @@
 package=native_libmultiprocess
-$(package)_version=07c917f7ca910d66abc6d3873162fc9061704074
+$(package)_version=011fc53aeaf2b8bfceb8e738aa9bf512a240496e
 $(package)_download_path=https://github.com/chaincodelabs/libmultiprocess/archive
 $(package)_file_name=$($(package)_version).tar.gz
-$(package)_sha256_hash=ac9db311e3b22aac3c7b7b7b3f6b7fee5cf3043ebb3c3bf412049e8b17166de8
+$(package)_sha256_hash=43737480f59fbad16db33dd08790fd245bb6660491a9a41f060406c26d1a23d4
 $(package)_dependencies=native_capnp
 
 define $(package)_config_cmds
