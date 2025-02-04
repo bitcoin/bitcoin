0551bcd0ff9f219229a782a3280a42b39b25f681
Luke Dashjr <luke-jr+git@utopios.org>
2025-02-04 00:08:02 +0000

    Qt/Options: Configure spkreuse using rwconf

--- src/qt/optionsdialog.h
+++ src/qt/optionsdialog.h
@@ -106,6 +106,7 @@ private:
     QSpinBox *mempoolexpiry;
 
     QCheckBox *rejectunknownscripts;
+    QCheckBox *rejectspkreuse;
     BitcoinAmountField *minrelaytxfee;
     QSpinBox *bytespersigop, *bytespersigopstrict;
     QSpinBox *limitancestorcount;
