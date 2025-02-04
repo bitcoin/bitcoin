20e42a4a7423d7bc62326cdb6405bc6d5900953e
Suhas Daftuar <sdaftuar@chaincode.com>
2025-02-04 16:54:20 -0500

    scripted-diff: rename AddToMempool -> TryAddToMempool
    -BEGIN VERIFY SCRIPT-
    find src/test -type f -exec sed -i 's/AddToMempool/TryAddToMempool/g' {} +
    find src/bench -type f -exec sed -i 's/AddToMempool/TryAddToMempool/g' {} +
    -END VERIFY SCRIPT-

--- src/test/util/txmempool.h
+++ src/test/util/txmempool.h
@@ -65,6 +65,6 @@ void CheckMempoolTRUCInvariants(const CTxMemPool& tx_pool);
 
 /** One-line wrapper for creating a mempool changeset with a single transaction
  *  and applying it if the policy limits are respected. */
-void AddToMempool(CTxMemPool& tx_pool, const CTxMemPoolEntry& entry);
+void TryAddToMempool(CTxMemPool& tx_pool, const CTxMemPoolEntry& entry);
 
 #endif // BITCOIN_TEST_UTIL_TXMEMPOOL_H
