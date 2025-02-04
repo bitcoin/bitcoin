57fadfa0360d079bfefd401d0a2873f336fe6674
Luke Dashjr <luke-jr+git@utopios.org>
2025-02-04 01:53:39 +0000

    Set maxscriptsize policy option default to 1650 (like MAX_STANDARD_SCRIPTSIG_SIZE)

--- src/policy/policy.h
+++ src/policy/policy.h
@@ -45,7 +45,7 @@ static constexpr unsigned int MAX_STANDARD_TX_SIGOPS_COST{MAX_BLOCK_SIGOPS_COST/
 /** Default for -incrementalrelayfee, which sets the minimum feerate increase for mempool limiting or replacement **/
 static constexpr unsigned int DEFAULT_INCREMENTAL_RELAY_FEE{1000};
 /** Default for -maxscriptsize */
-static constexpr unsigned int DEFAULT_SCRIPT_SIZE_POLICY_LIMIT{std::numeric_limits<unsigned int>::max()};
+static constexpr unsigned int DEFAULT_SCRIPT_SIZE_POLICY_LIMIT{1650};
 /** Default for -bytespersigop */
 static constexpr unsigned int DEFAULT_BYTES_PER_SIGOP{20};
 /** Default for -bytespersigopstrict */
