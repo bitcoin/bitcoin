06db700a1315bb655ac7fa12578c626990a22ea5
stratospher <44024636+stratospher@users.noreply.github.com>
2025-02-07 12:56:09 +0530

    validation: reset BLOCK_FAILED_CHILD to BLOCK_FAILED_VALID when loading from disk
    - there maybe existing block indexes stored in disk with
      BLOCK_FAILED_CHILD
    - since they don't exist anymore, clean up block index entries with
      BLOCK_FAILED_CHILD and reset it to BLOCK_FAILED_VALID.

--- src/node/blockstorage.cpp
+++ src/node/blockstorage.cpp
@@ -464,7 +464,9 @@ bool BlockManager::LoadBlockIndex(const std::optional<uint256>& snapshot_blockha
             }
         }
         if (!(pindex->nStatus & BLOCK_FAILED_VALID) && pindex->pprev && (pindex->pprev->nStatus & BLOCK_FAILED_VALID)) {
-            pindex->nStatus |= BLOCK_FAILED_VALID;
+            // if BLOCK_FAILED_CHILD already exists in disk, clear it
+            // and reset it to BLOCK_FAILED_VALID which is used everywhere now
+            pindex->nStatus = (pindex->nStatus & ~BLOCK_FAILED_CHILD) | BLOCK_FAILED_VALID;
             m_dirty_blockindex.insert(pindex);
         }
         if (pindex->pprev) {
