fa9048636ba35e833c3e0305852b02e7ce1b90c5
Luke Dashjr <luke-jr+git@utopios.org>
2025-02-10 17:50:01 +0000

    add basic BIP-322 message signing support
    Github-Pull: #24058
    Rebased-From: 69bd3c4fe8586cf5b61a7df25dc73d4d473a1868

--- src/wallet/test/fuzz/scriptpubkeyman.cpp
+++ src/wallet/test/fuzz/scriptpubkeyman.cpp
@@ -140,7 +140,7 @@ FUZZ_TARGET(scriptpubkeyman, .init = initialize_spkm)
                                            *std::get_if<PKHash>(&dest) :
                                            PKHash{ConsumeUInt160(fuzzed_data_provider)}};
                         std::string str_sig;
-                        (void)spk_manager->SignMessage(msg, pk_hash, str_sig);
+                        (void)spk_manager->SignMessage(MessageSignatureFormat::LEGACY, msg, pk_hash, str_sig);
                         (void)spk_manager->GetMetadata(dest);
                     }
                 }
