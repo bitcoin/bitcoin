f41efa4c2c3e2c626820c987983935a672bb2e22
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2025-02-10 20:46:24 +0000

    ci: source target triple from `matrix.sh`

--- ci/test/00_setup_env_native_qt5.sh
+++ ci/test/00_setup_env_native_qt5.sh
@@ -7,6 +7,7 @@
 export LC_ALL=C.UTF-8
 
 export CONTAINER_NAME=ci_native_qt5
+export HOST=x86_64-pc-linux-gnu
 export PACKAGES="python3-zmq qtbase5-dev qttools5-dev-tools libdbus-1-dev libharfbuzz-dev"
 export DEP_OPTS="NO_UPNP=1 DEBUG=1"
 export TEST_RUNNER_EXTRA="--previous-releases --coverage --extended --exclude feature_pruning,feature_dbcrash"  # Run extended tests so that coverage does not fail, but exclude the very slow dbcrash
