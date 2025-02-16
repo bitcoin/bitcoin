f419ecdd4730f79b6c87cd585b6ca196b9685e79
Jon Atack <jon@atack.com>
2025-02-16 09:22:46 -0600

    netinfo: return shortened services, if peers list requested
    When the detailed peers list is requested, return the shortened services in the
    -netinfo header in the same format as the "serv" column, instead of the full names
    list in the report.
    
    Github-Pull: #31886
    Rebased-From: 8f195ac358959125a4c652e6e13723210798ad46

--- src/bitcoin-cli.cpp
+++ src/bitcoin-cli.cpp
@@ -583,7 +583,8 @@ public:
         }
 
         // Generate report header.
-        std::string result{strprintf("%s client %s%s - server %i%s\n\n", CLIENT_NAME, FormatFullVersion(), ChainToString(), networkinfo["protocolversion"].getInt<int>(), networkinfo["subversion"].get_str())};
+        const std::string services{DetailsRequested() ? strprintf(" - services %s", FormatServices(networkinfo["localservicesnames"])) : ""};
+        std::string result{strprintf("%s client %s%s - server %i%s%s\n\n", CLIENT_NAME, FormatFullVersion(), ChainToString(), networkinfo["protocolversion"].getInt<int>(), networkinfo["subversion"].get_str(), services)};
 
         // Report detailed peer connections list sorted by direction and minimum ping time.
         if (DetailsRequested() && !m_peers.empty()) {
@@ -665,7 +666,9 @@ public:
         }
 
         // Report local addresses, ports, and scores.
-        result += strprintf("\n\nLocal services: %s", ServicesList(networkinfo["localservicesnames"]));
+        if (!DetailsRequested()) {
+            result += strprintf("\n\nLocal services: %s", ServicesList(networkinfo["localservicesnames"]));
+        }
         result += "\n\nLocal addresses";
         const std::vector<UniValue>& local_addrs{networkinfo["localaddresses"].getValues()};
         if (local_addrs.empty()) {
