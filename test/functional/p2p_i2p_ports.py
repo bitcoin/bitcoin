fa780b31ab54febd7e56a0fc165683e056fc2500
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2025-02-19 14:12:34 +0100

    test: Assert unused port to debug intermittent issue

--- test/functional/p2p_i2p_ports.py
+++ test/functional/p2p_i2p_ports.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
-# Copyright (c) 2021-2021 The Bitcoin Core developers
+# Copyright (c) 2021-present The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 """
 Test ports handling for I2P hosts
 """
 
-
+import subprocess
+import platform
+from test_framework.util import assert_equal
 from test_framework.test_framework import BitcoinTestFramework
 
 PROXY = "127.0.0.1:60000"
+
 
 class I2PPorts(BitcoinTestFramework):
     def set_test_params(self):
@@ -27,9 +30,17 @@ class I2PPorts(BitcoinTestFramework):
 
         self.log.info("Ensure we try to connect if port=0 and get an error due to missing I2P proxy")
         addr = "h3r6bkn46qxftwja53pxiykntegfyfjqtnzbm6iv6r5mungmqgmq.b32.i2p:0"
+        if platform.system() == "Linux":
+            port_used = subprocess.run(
+                ["lsof", "-i", ":60000"],
+                stdout=subprocess.PIPE,
+                text=True,
+            ).stdout
+            # Port must not be used. Added to debug https://github.com/bitcoin/bitcoin/issues/30030
+            assert_equal(port_used, "")
         with node.assert_debug_log(expected_msgs=[f"Error connecting to {addr}: Cannot connect to {PROXY}"]):
             node.addnode(node=addr, command="onetry")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     I2PPorts(__file__).main()
