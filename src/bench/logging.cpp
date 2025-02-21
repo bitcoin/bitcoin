a445c688ab0740495a4565496703ed14aa77ed52
Lőrinc <pap.lorinc@gmail.com>
2025-02-21 11:35:27 +0100

    bench: measure LogEscapeMessage in normal & suspicious cases
    > build/src/bench/bench_bitcoin -filter='LogEscapeMessage.+' --min-time=10000
    
    > C compiler ............................ AppleClang 16.0.0.16000026
    
    |               ns/op |                op/s |    err% |     total | benchmark
    |--------------------:|--------------------:|--------:|----------:|:----------
    |              216.10 |        4,627,429.03 |    0.2% |     10.81 | `LogEscapeMessageNormal`
    |              328.77 |        3,041,669.54 |    0.7% |     10.97 | `LogEscapeMessageSuspicious`

--- src/bench/logging.cpp
+++ src/bench/logging.cpp
@@ -9,6 +9,8 @@
 #include <functional>
 #include <vector>
 
+using namespace std::literals;
+
 // All but 2 of the benchmarks should have roughly similar performance:
 //
 // LogWithoutDebug should be ~3 orders of magnitude faster, as nothing is logged.
@@ -57,8 +59,47 @@ static void LogWithoutWriteToFile(benchmark::Bench& bench)
     });
 }
 
+namespace BCLog {
+    std::string LogEscapeMessage(std::string_view str);
+}
+
+static void LogEscapeMessageNormal(benchmark::Bench& bench) {
+    const std::vector<std::string> normal_logs{
+        "UpdateTip: new best=000000000000000000003c4c32d94a9363241a84d42cbbc1ec9f5f12f84f4feb height=875371 version=0x20000004 log2_work=95.334853 tx=1133590694 date='2024-12-19T01:57:26Z' progress=0.993026 cache=8.3MiB(56574txo)"s,
+        "received: headers (162 bytes) peer=15"s,
+        "Config file arg: datadir=/Users/bitcoin/data"s,
+        "Verified block header at height 12345 hash: 你好 🔤"s
+    };
+
+    bench.batch(normal_logs.size()).run([&] {
+        for (const auto& msg : normal_logs) {
+            const auto& res{BCLog::LogEscapeMessage(msg)};
+            ankerl::nanobench::doNotOptimizeAway(res);
+        }
+    });
+}
+
+static void LogEscapeMessageSuspicious(benchmark::Bench& bench) {
+    // Based on the test cases
+    const std::vector<std::string> suspicious_logs{
+        "Received strange message\0from peer=12"s,
+        "Got malformed packet\x01\x02\x03\x7F from peer=13"s,
+        "Peer=14 sent:\x0D\x0Econtent-length: 100\x0D\x0A"s,
+        "Validation failed on\x1B[31mERROR\x1B[0m block=123456"s
+    };
+
+   bench.batch(suspicious_logs.size()).run([&] {
+        for (const auto& msg : suspicious_logs) {
+            const auto& res{BCLog::LogEscapeMessage(msg)};
+            ankerl::nanobench::doNotOptimizeAway(res);
+        }
+    });
+}
+
 BENCHMARK(LogWithDebug, benchmark::PriorityLevel::HIGH);
 BENCHMARK(LogWithoutDebug, benchmark::PriorityLevel::HIGH);
 BENCHMARK(LogWithThreadNames, benchmark::PriorityLevel::HIGH);
 BENCHMARK(LogWithoutThreadNames, benchmark::PriorityLevel::HIGH);
 BENCHMARK(LogWithoutWriteToFile, benchmark::PriorityLevel::HIGH);
+BENCHMARK(LogEscapeMessageNormal, benchmark::PriorityLevel::LOW);
+BENCHMARK(LogEscapeMessageSuspicious, benchmark::PriorityLevel::LOW);
