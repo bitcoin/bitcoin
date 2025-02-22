knst/dash

Merge bitcoin/bitcoin#23014: ci: Bump distro version, disable feature_bind_extra for two configurations
fa660de2ac28628e22ee0c18e04e520a41b3584e ci: Update valgrind config (MarcoFalke)
fad5dbc13c26901928e5b71bff047fe273f72a45 ci: Update s390x config (MarcoFalke)

Pull request description:

  (See commit messages)

ACKs for top commit:
  fanquake:
    ACK fa660de2ac28628e22ee0c18e04e520a41b3584e

Tree-SHA512: b5bf7c89042d5d8f49a92c59b934185447ae42bd055ccb1c6edb98fe104a35eb2d6c26d810bec9f52d545d09599f3d33fc8606fbfb793862610d276b2a4bac6c

ci/test/00_setup_env_native_valgrind.sh
@@ -9,6 +9,6 @@ export LC_ALL=C.UTF-8
 export PACKAGES="valgrind clang llvm python3-zmq libevent-dev bsdmainutils libboost-dev libdb5.3++-dev libminiupnpc-dev libzmq3-dev"
 export USE_VALGRIND=1
 export NO_DEPENDS=1
-export TEST_RUNNER_EXTRA="--exclude rpc_bind --timeout-factor=4"  # Excluded for now, see https://github.com/bitcoin/bitcoin/issues/17765#issuecomment-602068547
+export TEST_RUNNER_EXTRA="--exclude rpc_bind,fature_bind_extra --timeout-factor=4"  # Excluded for now, see https://github.com/bitcoin/bitcoin/issues/17765#issuecomment-602068547
 export GOAL="install"
 export BITCOIN_CONFIG="--enable-zmq --with-incompatible-bdb --with-gui=no --enable-suppress-external-warnings CC=clang-18 CXX=clang++-18"  # TODO enable GUI

bitcoin/bitcoin

ci/test/00_setup_env_native_valgrind.sh
@@ -9,6 +9,6 @@ export LC_ALL=C.UTF-8
 export PACKAGES="valgrind clang llvm python3-zmq libevent-dev bsdmainutils libboost-dev libdb5.3++-dev libminiupnpc-dev libzmq3-dev"
 export USE_VALGRIND=1
 export NO_DEPENDS=1
-export TEST_RUNNER_EXTRA="--exclude rpc_bind --timeout-factor=4"  # Excluded for now, see https://github.com/bitcoin/bitcoin/issues/17765#issuecomment-602068547
+export TEST_RUNNER_EXTRA="--exclude rpc_bind,feature_bind_extra --timeout-factor=4"  # Excluded for now, see https://github.com/bitcoin/bitcoin/issues/17765#issuecomment-602068547
 export GOAL="install"
 export BITCOIN_CONFIG="--enable-zmq --with-incompatible-bdb --with-gui=no --enable-suppress-external-warnings CC=clang-18 CXX=clang++-18"  # TODO enable GUI