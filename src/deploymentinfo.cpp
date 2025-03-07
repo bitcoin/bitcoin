1e9c19e778292a69b25fd1872a750a24092227a3
Sjors Provoost <sjors@sprovoost.nl>
2025-03-07 04:18:27 -0500

    Remove Taproot activation height
    Drop DEPLOYMENT_TAPROOT from consensus.vDeployments.
    
    Bump MinBIP9WarningHeight.
    
    Clarify what is considered a BuriedDeployment and
    drop taproot from getdeploymentinfo RPC.
    
    Add a test to getblocktemplate to ensure the taproot
    rule is still set.
    
    Co-Authored-By: MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>

--- src/deploymentinfo.cpp
+++ src/deploymentinfo.cpp
@@ -13,10 +13,6 @@ const std::array<VBDeploymentInfo,Consensus::MAX_VERSION_BITS_DEPLOYMENTS> Versi
         .name = "testdummy",
         .gbt_force = true,
     },
-    VBDeploymentInfo{
-        .name = "taproot",
-        .gbt_force = true,
-    },
 };
 
 std::string DeploymentName(Consensus::BuriedDeployment dep)
