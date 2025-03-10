6faf34d4dda7c003f3c22f08a932222fa765df96
UdjinM6 <UdjinM6@users.noreply.github.com>
2025-03-10 21:41:03 +0300

    chore: some cleanups

--- src/test/evo_islock_tests.cpp
+++ src/test/evo_islock_tests.cpp
@@ -1,4 +1,3 @@
-// instantsend_tests.cpp
 #include <boost/test/unit_test.hpp>
 #include <hash.h>
 #include <llmq/instantsend.h>
@@ -10,7 +9,7 @@
 // For constructing dummy outpoints using uint256S.
 #include <util/strencodings.h>
 
-BOOST_AUTO_TEST_SUITE(instantsend_tests)
+BOOST_AUTO_TEST_SUITE(evo_islock_tests)
 
 uint256 CalculateRequestId(const std::vector<COutPoint>& inputs)
 {
@@ -52,12 +51,11 @@ BOOST_AUTO_TEST_CASE(deserialize_instantlock_from_realdata2)
     // Expected values from the provided getislocks output:
     const std::string_view expectedTxidStr = "7b33968effa613e8ea9c1b5734c9bbbe467ff4650f8060caf8a5c213c6059d5b";
     const std::string_view expectedCycleHashStr = "000000000000000bbd0b1bb95540351e7ee99c5b08efde076b3d712a57ea74d6";
-    const std::string_view expectedSignature = "997d0b36738a9eef46ceeb4405998ff7235317708f277402799ffe05258015cae9b6bae"
+    const std::string_view expectedSignatureStr = "997d0b36738a9eef46ceeb4405998ff7235317708f277402799ffe05258015cae9b6bae"
                                                "43683f992b2f50f70f8f0cb9c0f26af340b00903e93995c1345d1b2c5b697ebecdbe581"
                                                "1dd112e11889101dcb4553b2bc206ab304026b96c07dec4f24";
-    const std::string_view cycleHash = "000000000000000bbd0b1bb95540351e7ee99c5b08efde076b3d712a57ea74d6";
-    const std::string quorumHash = "0000000000000019756ecc9c9c5f476d3f66876b1dcfa5dde1ea82f0d99334a2";
-    const std::string_view expectedSignHash = "6a3c37bc610c4efd5babd8941068a8eca9e7bec942fe175b8ca9cae31b67e838";
+    const std::string quorumHashStr = "0000000000000019756ecc9c9c5f476d3f66876b1dcfa5dde1ea82f0d99334a2";
+    const std::string_view expectedSignHashStr = "6a3c37bc610c4efd5babd8941068a8eca9e7bec942fe175b8ca9cae31b67e838";
     // The serialized InstantSend lock from the "hex" field of getislocks:
     const std::string_view islockHex =
         "0101497915895c30eebfad0c5fcfb9e0e72308c7e92cd3749be2fd49c8320c4c58b6010000005b9d05c613c2a5f8ca60800f65f47f46be"
@@ -75,9 +73,9 @@ BOOST_AUTO_TEST_CASE(deserialize_instantlock_from_realdata2)
     ss >> islock;
 
     // Verify the calculated signHash
-    auto signHash = llmq::BuildSignHash(Consensus::LLMQType::LLMQ_60_75, uint256S(quorumHash), islock.GetRequestId(),
+    auto signHash = llmq::BuildSignHash(Consensus::LLMQType::LLMQ_60_75, uint256S(quorumHashStr), islock.GetRequestId(),
                                         islock.txid);
-    BOOST_CHECK_EQUAL(signHash.ToString(), expectedSignHash);
+    BOOST_CHECK_EQUAL(signHash.ToString(), expectedSignHashStr);
 
     // Verify the txid field.
     BOOST_CHECK_EQUAL(islock.txid.ToString(), expectedTxidStr);
@@ -98,7 +96,7 @@ BOOST_AUTO_TEST_CASE(deserialize_instantlock_from_realdata2)
     BOOST_CHECK_EQUAL(islock.GetRequestId().ToString(), expectedRequestId.ToString());
 
     // Verify the signature field.
-    BOOST_CHECK_EQUAL(islock.sig.Get().ToString(), expectedSignature);
+    BOOST_CHECK_EQUAL(islock.sig.Get().ToString(), expectedSignatureStr);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
