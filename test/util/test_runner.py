c64c5313885af6a39a446749e059bc7dc6464c57
GarmashAlex <garmasholeksii@gmail.com>
2025-03-11 20:27:36 +0300

    Update test_runner.py

--- test/util/test_runner.py
+++ test/util/test_runner.py
@@ -156,7 +156,6 @@ def bctest(testDir, testObj, buildenv):
     if "error_txt" in testObj:
         want_error = testObj["error_txt"]
         # Compare error text
-        # TODO: ideally, we'd compare the strings exactly and also assert
         # That stderr is empty if no errors are expected. However, bitcoin-tx
         # emits DISPLAY errors when running as a windows application on
         # linux through wine. Just assert that the expected error text appears
