29ca24f362f0800dbe4c837ccc19538a6fb5b623
VolodymyrBg <aqdrgg19@gmail.com>
2025-03-11 19:36:37 +0200

    Update sendcoinsrecipient.h

--- src/qt/sendcoinsrecipient.h
+++ src/qt/sendcoinsrecipient.h
@@ -23,8 +23,12 @@ public:
     // the addresses, e.g. address-A<br />address-B<br />address-C.
     // Info: As we don't need to process addresses in here when using
     // payment requests, we can abuse it for displaying an address list.
-    // Todo: This is a hack, should be replaced with a cleaner solution!
     QString address;
+
+    // Separate field for storing multiple addresses from unauthenticated payment requests
+    // This replaces the hack mentioned in the TODO comment
+    QString addressList;
+
     QString label;
     CAmount amount;
     // If from a payment request, this is used for storing the memo
@@ -42,19 +46,21 @@ public:
 
     SERIALIZE_METHODS(SendCoinsRecipient, obj)
     {
-        std::string address_str, label_str, message_str, auth_merchant_str;
+        std::string address_str, label_str, message_str, auth_merchant_str, address_list_str;
 
         SER_WRITE(obj, address_str = obj.address.toStdString());
         SER_WRITE(obj, label_str = obj.label.toStdString());
         SER_WRITE(obj, message_str = obj.message.toStdString());
         SER_WRITE(obj, auth_merchant_str = obj.authenticatedMerchant.toStdString());
+        SER_WRITE(obj, address_list_str = obj.addressList.toStdString());
 
-        READWRITE(obj.nVersion, address_str, label_str, obj.amount, message_str, obj.sPaymentRequest, auth_merchant_str);
+        READWRITE(obj.nVersion, address_str, label_str, obj.amount, message_str, obj.sPaymentRequest, auth_merchant_str, address_list_str);
 
         SER_READ(obj, obj.address = QString::fromStdString(address_str));
         SER_READ(obj, obj.label = QString::fromStdString(label_str));
         SER_READ(obj, obj.message = QString::fromStdString(message_str));
         SER_READ(obj, obj.authenticatedMerchant = QString::fromStdString(auth_merchant_str));
+        SER_READ(obj, obj.addressList = QString::fromStdString(address_list_str));
     }
 };
 
