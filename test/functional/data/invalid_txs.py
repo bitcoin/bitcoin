9342bd4e21d543a5d16def60fd1ca2a5035ab5df
Antoine Poinsot <darosior@protonmail.com>
2025-03-11 15:25:51 -0400

    validation: make 64-byte transactions invalid
    64-byte transactions are also now treated as a consensus failure in
    PreChecks, like BIP54-sigops check failures. Note this only changes the
    RPC error, and not the disconnection behaviour in P2P since
    266dd0e10d08c0bfde63205db15d6c210a021b90.

--- test/functional/data/invalid_txs.py
+++ test/functional/data/invalid_txs.py
@@ -116,8 +116,9 @@ class InputMissing(BadTxTemplate):
 # The following check prevents exploit of lack of merkle
 # tree depth commitment (CVE-2017-12842)
 class SizeExactly64(BadTxTemplate):
-    reject_reason = "tx-size-small"
-    valid_in_block = True
+    reject_reason = "txn-size-64"
+    valid_in_block = False
+    block_reject_reason = "bad-txns-size"
 
     def get_tx(self):
         tx = CTransaction()
