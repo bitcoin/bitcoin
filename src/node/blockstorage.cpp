a4de16049222d0a0f5530f4e366254478a21ab44
Lőrinc <pap.lorinc@gmail.com>
2025-03-14 22:09:38 +0100

    scripted-diff: shorten BLOCK_SERIALIZATION_HEADER_SIZE constant
    Renames the constant to be less verbose and better reflect its purpose:
    it represents the size of the storage header that precedes serialized block data on disk,
    not to be confused with a block's own header.
    
    -BEGIN VERIFY SCRIPT-
    git grep -q "STORAGE_HEADER_BYTES" $(git ls-files) && echo "Error: Target name STORAGE_HEADER_BYTES already exists in the codebase" && exit 1
    sed -i 's/BLOCK_SERIALIZATION_HEADER_SIZE/STORAGE_HEADER_BYTES/g' $(git grep -l 'BLOCK_SERIALIZATION_HEADER_SIZE')
    -END VERIFY SCRIPT-

--- src/node/blockstorage.cpp
+++ src/node/blockstorage.cpp
@@ -949,7 +949,7 @@ bool BlockManager::WriteBlockUndo(const CBlockUndo& blockundo, BlockValidationSt
 
             // Write index header
             fileout << GetParams().MessageStart() << blockundo_size;
-            pos.nPos += BLOCK_SERIALIZATION_HEADER_SIZE;
+            pos.nPos += STORAGE_HEADER_BYTES;
             {
                 // Calculate checksum
                 HashWriter hasher{};
@@ -1087,7 +1087,7 @@ bool BlockManager::ReadRawBlock(std::vector<uint8_t>& block, const FlatFilePos& 
 FlatFilePos BlockManager::WriteBlock(const CBlock& block, int nHeight)
 {
     const unsigned int block_size{static_cast<unsigned int>(GetSerializeSize(TX_WITH_WITNESS(block)))};
-    FlatFilePos pos{FindNextBlockPos(block_size + BLOCK_SERIALIZATION_HEADER_SIZE, nHeight, block.GetBlockTime())};
+    FlatFilePos pos{FindNextBlockPos(block_size + STORAGE_HEADER_BYTES, nHeight, block.GetBlockTime())};
     if (pos.IsNull()) {
         LogError("FindNextBlockPos failed");
         return FlatFilePos();
@@ -1101,7 +1101,7 @@ FlatFilePos BlockManager::WriteBlock(const CBlock& block, int nHeight)
 
     // Write index header
     fileout << GetParams().MessageStart() << block_size;
-    pos.nPos += BLOCK_SERIALIZATION_HEADER_SIZE;
+    pos.nPos += STORAGE_HEADER_BYTES;
     // Write block
     fileout << TX_WITH_WITNESS(block);
     return pos;
