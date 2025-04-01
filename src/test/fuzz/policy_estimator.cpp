339685e1abc57b6576dc4b05fed7eb134c103dbd
marcofleon <marleo23@proton.me>
2025-04-01 15:26:25 +0100

    Convert all policy from uint256 to Txid

--- src/test/fuzz/policy_estimator.cpp
+++ src/test/fuzz/policy_estimator.cpp
@@ -78,7 +78,7 @@ FUZZ_TARGET(policy_estimator, .init = initialize_policy_estimator)
                 block_policy_estimator.processBlock(txs, fuzzed_data_provider.ConsumeIntegral<unsigned int>());
             },
             [&] {
-                (void)block_policy_estimator.removeTx(ConsumeUInt256(fuzzed_data_provider));
+                (void)block_policy_estimator.removeTx(Txid::FromUint256(ConsumeUInt256(fuzzed_data_provider)));
             },
             [&] {
                 block_policy_estimator.FlushUnconfirmed();
