03c15a3c1dd311dbde58648c00de9b9edc2cebc3
pasta <pasta@dashboost.org>
2025-04-02 10:14:00 -0500

    fix: actually bump protocol version

--- src/version.h
+++ src/version.h
@@ -11,7 +11,7 @@
  */
 
 
-static const int PROTOCOL_VERSION = 70235;
+static const int PROTOCOL_VERSION = 70236;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
