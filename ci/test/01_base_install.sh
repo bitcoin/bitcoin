30dd1f1644e0441b5310f1eceecfd6a5abc45f68
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2025-04-02 09:15:25 +0100

    ci: Update for Qt 6

--- ci/test/01_base_install.sh
+++ ci/test/01_base_install.sh
@@ -34,7 +34,8 @@ fi
 
 if [[ $CI_IMAGE_NAME_TAG == *centos* ]]; then
   bash -c "dnf -y install epel-release"
-  bash -c "dnf -y --allowerasing install $CI_BASE_PACKAGES $PACKAGES"
+  # The ninja-build package is available in the CRB repository.
+  bash -c "dnf -y --allowerasing --enablerepo crb install $CI_BASE_PACKAGES $PACKAGES"
 elif [ "$CI_OS_NAME" != "macos" ]; then
   if [[ -n "${APPEND_APT_SOURCES_LIST}" ]]; then
     echo "${APPEND_APT_SOURCES_LIST}" >> /etc/apt/sources.list
