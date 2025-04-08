52db62f6fa49d5624cb560aa386505838ae2d3e7
Luke Dashjr <luke-jr+git@utopios.org>
2025-04-08 20:24:16 +0000

    RPC/Mempool: Deny importmempool usage for wallet-restricted RPC users

--- src/rpc/mempool.cpp
+++ src/rpc/mempool.cpp
@@ -750,6 +750,8 @@ static RPCHelpMan importmempool()
         RPCResult{RPCResult::Type::OBJ, "", "", std::vector<RPCResult>{}},
         RPCExamples{HelpExampleCli("importmempool", "/path/to/mempool.dat") + HelpExampleRpc("importmempool", "/path/to/mempool.dat")},
         [&](const RPCHelpMan& self, const JSONRPCRequest& request) -> UniValue {
+            EnsureNotWalletRestricted(request);
+
             const NodeContext& node{EnsureAnyNodeContext(request.context)};
 
             CTxMemPool& mempool{EnsureMemPool(node)};
