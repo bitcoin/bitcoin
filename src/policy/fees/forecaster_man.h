8284454fddcb01cf6025eaebb25cc6dde8e17501
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2025-04-10 09:35:23 +0100

    fees: add ForecasterMan class
    - Its a module for managing and utilising multiple
      fee rate forecasters to provide fee rate forecast.
    
    - The ForecasterManager class allows for the registration of
      multiple fee rate forecasters.
    
    Co-authored-by: willcl-ark <will@256k1.dev>

---
+++ src/policy/fees/forecaster_man.h
@@ -0,0 +1,33 @@
+// Copyright (c) 2025 The Bitcoin Core developers
+// Distributed under the MIT software license. See the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_POLICY_FEES_FORECASTER_MAN_H
+#define BITCOIN_POLICY_FEES_FORECASTER_MAN_H
+
+#include <memory>
+#include <unordered_map>
+
+class Forecaster;
+struct ForecastResult;
+
+enum class ForecastType;
+
+/** \class FeeRateForecasterManager
+ * Manages multiple fee rate forecasters.
+ */
+class FeeRateForecasterManager
+{
+private:
+    //! Map of all registered forecasters to their shared pointers.
+    std::unordered_map<ForecastType, std::shared_ptr<Forecaster>> forecasters;
+
+public:
+    /**
+     * Register a forecaster.
+     * @param[in] forecaster shared pointer to a Forecaster instance.
+     */
+    void RegisterForecaster(std::shared_ptr<Forecaster> forecaster);
+};
+
+#endif // BITCOIN_POLICY_FEES_FORECASTER_MAN_H
