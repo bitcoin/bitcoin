f12941032d08a48dcd57f511f09a8f5f9f0d8dc3
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2025-04-10 09:54:14 +0100

    fees: add forecaster structures
    i  - Forecaster abstract class is the base class of fee rate forecasters.
         Derived classes must provide concrete implementation
         of the virtual methods.
    
    ii - ForecastResult struct represent the response returned by
         a fee rate forecaster.
    
    iii - ForecastType will be used to identify forecasters.
          Each time a new forecaster is added, a corresponding
          enum value should be added to ForecastType.
    
    Co-authored-by: willcl-ark <will@256k1.dev>

---
+++ src/policy/fees/forecaster_util.h
@@ -0,0 +1,47 @@
+// Copyright (c) 2025 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_POLICY_FEES_FORECASTER_UTIL_H
+#define BITCOIN_POLICY_FEES_FORECASTER_UTIL_H
+
+#include <util/feefrac.h>
+
+#include <optional>
+#include <string>
+
+/**
+ * @enum ForecastType
+ * Identifier for fee rate forecasters.
+ */
+enum class ForecastType {};
+
+/**
+ * @struct ForecastResult
+ * Represents the response returned by a fee rate forecaster.
+ */
+struct ForecastResult {
+    //! This identifies which forecaster is providing this feerate forecast
+    std::optional<ForecastType> forecaster;
+
+    //! Fee rate sufficient to confirm a package within target.
+    FeeFrac feerate;
+
+    //! The chain tip at which the forecast was made.
+    unsigned int current_block_height{0};
+
+    std::optional<std::string> error; ///< Optional error message.
+
+    /**
+     * Overloaded less than operator which compares two ForecastResult objects based on
+     * their fee rate.
+     * @param other The other ForecastResult object to compare with.
+     * @return true if the current object's fee rate is less than the other, false otherwise.
+     */
+    bool operator<(const ForecastResult& other) const
+    {
+        return feerate << other.feerate;
+    }
+};
+
+#endif // BITCOIN_POLICY_FEES_FORECASTER_UTIL_H
