0de5277e2f6ab72a5b83b87b757a536fb7e151cc
Sjors Provoost <sjors@sprovoost.nl>
2025-04-17 14:40:29 +0200

    build: depends makes libmultiprocess by default
    This causes IPC binaries (bitcoin-node, bitcoin-gui) to be included
    in releases.
    
    The effect on CI is that this causes more depends builds to build IPC
    binaries, but still the only build running functional tests with them
    is the i686_multiprocess one.
    
    Except for Windows.

--- ci/test/00_setup_env_i686_multiprocess.sh
+++ ci/test/00_setup_env_i686_multiprocess.sh
@@ -11,7 +11,7 @@ export CONTAINER_NAME=ci_i686_multiprocess
 export CI_IMAGE_NAME_TAG="mirror.gcr.io/ubuntu:24.04"
 export CI_IMAGE_PLATFORM="linux/amd64"
 export PACKAGES="llvm clang g++-multilib"
-export DEP_OPTS="DEBUG=1 MULTIPROCESS=1"
+export DEP_OPTS="DEBUG=1"
 export GOAL="install"
 export TEST_RUNNER_EXTRA="--v2transport --usecli"
 export BITCOIN_CONFIG="\
