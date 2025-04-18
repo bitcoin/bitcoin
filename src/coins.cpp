edbdb0a3057feedc1f9d9e72d8f1b8b4df795236
Lőrinc <pap.lorinc@gmail.com>
2025-04-18 11:23:27 +0200

    coins: reset `cachedCoinsUsage` in Flush() only after the map is cleared
    Prevents the counter from dropping to 0 while cacheCoins still holds objects

--- src/coins.cpp
+++ src/coins.cpp
@@ -254,8 +254,8 @@ bool CCoinsViewCache::Flush() {
     if (fOk) {
         cacheCoins.clear();
         ReallocateCache();
+        cachedCoinsUsage = 0;
     }
-    cachedCoinsUsage = 0;
     return fOk;
 }
 
