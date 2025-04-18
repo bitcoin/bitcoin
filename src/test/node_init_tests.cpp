f91406953299811cfcacb5976945137a7563d84c
Ryan Ofsky <ryan@ofsky.org>
2025-04-18 18:12:46 -0400

    test: Add unit test coverage for Init and Shutdown code
    Currently this code is not called in unit tests. Calling should make it
    possible to write tests for things like IPC exceptions being thrown during
    shutdown.

---
+++ src/test/node_init_tests.cpp
@@ -0,0 +1,47 @@
+// Copyright (c) 2025 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <init.h>
+#include <interfaces/init.h>
+#include <rpc/server.h>
+
+#include <boost/test/unit_test.hpp>
+#include <test/util/setup_common.h>
+
+using node::NodeContext;
+
+BOOST_FIXTURE_TEST_SUITE(node_init_tests, BasicTestingSetup)
+
+//! Custom implementation of interfaces::Init for testing.
+class TestInit : public interfaces::Init
+{
+public:
+    TestInit(NodeContext& node) : m_node(node)
+    {
+        InitContext(m_node);
+        m_node.init = this;
+    }
+    std::unique_ptr<interfaces::Chain> makeChain() override { return interfaces::MakeChain(m_node); }
+    std::unique_ptr<interfaces::WalletLoader> makeWalletLoader(interfaces::Chain& chain) override
+    {
+        return MakeWalletLoader(chain, *Assert(m_node.args));
+    }
+    NodeContext& m_node;
+};
+
+BOOST_AUTO_TEST_CASE(init_test)
+{
+    // Reset logging and config file path to avoid errors in AppInitMain
+    LogInstance().DisconnectTestLogger();
+    m_node.args->SetConfigFilePath({});
+
+    // Run through initialization and shutdown code.
+    TestInit init{m_node};
+    BOOST_CHECK(AppInitInterfaces(m_node));
+    BOOST_CHECK(AppInitMain(m_node));
+    Interrupt(m_node);
+    Shutdown(m_node);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
