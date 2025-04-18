948ab343209eec832d7354d60b9220629f61390a
Ryan Ofsky <ryan@ofsky.org>
2025-04-18 18:12:46 -0400

    test: Add unit test coverage for Init and Shutdown code
    Currently this code is not called in unit tests. Calling should make it
    possible to write tests for things like IPC exceptions being thrown during
    shutdown.

--- src/rpc/server.cpp
+++ src/rpc/server.cpp
@@ -327,6 +327,12 @@ void SetRPCWarmupStatus(const std::string& newStatus)
     rpcWarmupStatus = newStatus;
 }
 
+void SetRPCWarmupStarting()
+{
+    LOCK(g_rpc_warmup_mutex);
+    fRPCInWarmup = true;
+}
+
 void SetRPCWarmupFinished()
 {
     LOCK(g_rpc_warmup_mutex);
