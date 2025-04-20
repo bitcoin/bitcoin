29090a045925bdcdae114cfbaf6fccdfd7ff00d1
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2025-04-20 22:46:55 +0000

    merge bitcoin#25972: no-longer disable WARN_CXXFLAGS when CXXFLAGS is set

--- ci/test/00_setup_env_native_multiprocess.sh
+++ ci/test/00_setup_env_native_multiprocess.sh
@@ -15,5 +15,5 @@ export GOAL="install"
 export TEST_RUNNER_EXTRA="--v2transport"
 export BITCOIN_CONFIG="--with-boost-process --enable-debug CC=clang-18 CXX=clang++-18" # Use clang to avoid OOM
 # Additional flags for RUN_TIDY
-export BITCOIN_CONFIG="${BITCOIN_CONFIG} --disable-hardening CFLAGS='-O0 -g0' CXXFLAGS='-O0 -g0'"
+export BITCOIN_CONFIG="${BITCOIN_CONFIG} --disable-hardening CFLAGS='-O0 -g0' CXXFLAGS='-O0 -g0 -Wno-error=documentation'"
 export BITCOIND=dash-node  # Used in functional tests
