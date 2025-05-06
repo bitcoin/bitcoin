faec029c619937081d1c928c9b6496ebd512cd85
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2025-05-06 20:02:14 +0200

    fuzz: Use ElapseTime

--- src/test/fuzz/p2p_headers_presync.cpp
+++ src/test/fuzz/p2p_headers_presync.cpp
@@ -176,7 +176,7 @@ FUZZ_TARGET(p2p_headers_presync, .init = initialize)
 
     ChainstateManager& chainman = *g_testing_setup->m_node.chainman;
     CBlockHeader base{chainman.GetParams().GenesisBlock()};
-    SetMockTime(base.nTime);
+    const ElapseTime elapse_time{base.Time()};
 
     LOCK(NetEventsInterface::g_msgproc_mutex);
 
