fa3662d41bf3794642bc48c4c3ff836ae19af795
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2025-05-07 09:40:15 +0200

    test: Avoid resetting mocktime in testing setup
    This allows to set the mocktime before the testing setup.
    
    Also, in some fuzz tests the mocktime was reset to 0 before this change,
    so set it.

--- src/test/fuzz/utxo_total_supply.cpp
+++ src/test/fuzz/utxo_total_supply.cpp
@@ -24,14 +24,13 @@ FUZZ_TARGET(utxo_total_supply)
 {
     SeedRandomStateForTest(SeedRand::ZEROS);
     FuzzedDataProvider fuzzed_data_provider(buffer.data(), buffer.size());
-    const auto mock_time{ConsumeTime(fuzzed_data_provider, /*min=*/1296688602)}; // regtest genesis block timestamp
+    SetMockTime(ConsumeTime(fuzzed_data_provider, /*min=*/1296688602)); // regtest genesis block timestamp
     /** The testing setup that creates a chainman only (no chainstate) */
     ChainTestingSetup test_setup{
         ChainType::REGTEST,
         {
             .extra_args = {
                 "-testactivationheight=bip34@2",
-                strprintf("-mocktime=%d", mock_time).c_str()
             },
         },
     };
