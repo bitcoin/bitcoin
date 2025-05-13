2fa5be42a651608ebaaca316e530ee699bb57c0b
Ava Chow <github@achow101.com>
2025-05-13 14:36:04 -0700

    wallet: Write new descriptor's cache in AddWalletDescriptor
    If a new WalletDescriptor is provided to us with a cache, write the
    cache to disk as well.

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -3760,6 +3760,12 @@ util::Result<std::reference_wrapper<DescriptorScriptPubKeyMan>> CWallet::AddWall
         // Save the descriptor to memory
         uint256 id = new_spk_man->GetID();
         AddScriptPubKeyMan(id, std::move(new_spk_man));
+
+        // Write the existing cache to disk
+        WalletBatch batch(GetDatabase());
+        if (!batch.WriteDescriptorCacheItems(id, desc.cache)) {
+            return util::Error{_("Unable to write descriptor cache")};
+        }
     }
 
     // Add the private keys to the descriptor
