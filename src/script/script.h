038104fd1ba65765c92c27ec53f16c714a57b472
Lőrinc <pap.lorinc@gmail.com>
2025-05-14 16:05:05 +0200

    refactor: rename `GetSigOpCount` to `GetLegacySigOpCount`
    Also added primitive argument name reminder to the call sites to highlight the meaning of the call.

--- src/script/script.h
+++ src/script/script.h
@@ -536,7 +536,7 @@ public:
      * counted more accurately, assuming they are of the form
      *  ... OP_N CHECKMULTISIG ...
      */
-    unsigned int GetSigOpCount(bool fAccurate) const;
+    unsigned int GetLegacySigOpCount(bool fAccurate) const;
 
     /**
      * Accurately count sigOps, including sigOps in
