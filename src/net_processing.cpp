58b5735f5abd6a0c874a2a691a4e173faadda8e3
glozow <gloriajzhao@gmail.com>
2025-05-16 10:36:05 -0400

    [prep/test] have TxOrphanage remember its own limits in LimitOrphans
    Move towards a model where TxOrphanage is initialized with limits that
    it remembers throughout its lifetime.
    Remove the param. Limiting by number of unique orphans will be removed
    in a later commit.
    Now that -maxorphantx is gone, this does not change the node behavior.
    The parameter is only used in tests.

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -1925,7 +1925,7 @@ PeerManagerImpl::PeerManagerImpl(CConnman& connman, AddrMan& addrman,
       m_banman(banman),
       m_chainman(chainman),
       m_mempool(pool),
-      m_txdownloadman(node::TxDownloadOptions{pool, m_rng, opts.max_orphan_txs, opts.deterministic_rng}),
+      m_txdownloadman(node::TxDownloadOptions{pool, m_rng, opts.deterministic_rng}),
       m_warnings{warnings},
       m_opts{opts}
 {
