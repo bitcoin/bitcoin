7ad2b243684d2c1b8fd040a129066d7576a9695d
Pieter Wuille <pieter@wuille.net>
2025-05-16 23:04:44 -0400

    clusterlin: replace cluster linearization with SFL implementation (feature)
    This replaces the existing LIMO linearization algorithm (which internally uses
    ancestor set finding and candidate set finding) with the much more performant
    spanning-forest linearization algorithm.
    
    See https://delvingbitcoin.org/t/spanning-forest-cluster-linearization/1419

--- src/test/fuzz/cluster_linearize.cpp
+++ src/test/fuzz/cluster_linearize.cpp
@@ -25,13 +25,13 @@
  *   <<---: The right side is implemented using the left side.
  *
  *   +-----------------------+
- *   | SearchCandidateFinder | <<---------------------\
- *   +-----------------------+                        |
+ *   | SearchCandidateFinder |
+ *   +-----------------------+
  *     |                                            +-----------+
  *     |                                            | Linearize |
  *     |                                            +-----------+
- *     |        +-------------------------+           |  |
- *     |        | AncestorCandidateFinder | <<--------/  |
+ *     |        +-------------------------+              |
+ *     |        | AncestorCandidateFinder |              |
  *     |        +-------------------------+              |
  *     |          |                     ^                |        ^^  PRODUCTION CODE
  *     |          |                     |                |        ||
@@ -1155,7 +1155,6 @@ FUZZ_TARGET(clusterlin_linearize)
     // Invoke Linearize().
     iter_count &= 0x7ffff;
     auto [linearization, optimal, cost] = Linearize(depgraph, iter_count, rng_seed, old_linearization);
-    assert(cost <= iter_count);
     SanityCheck(depgraph, linearization);
     auto chunking = ChunkLinearization(depgraph, linearization);
 
@@ -1182,9 +1181,13 @@ FUZZ_TARGET(clusterlin_linearize)
         // If SimpleLinearize finds the optimal result too, they must be equal (if not,
         // SimpleLinearize is broken).
         if (simple_optimal) assert(cmp == 0);
-        // If simple_chunking is diagram-optimal, it cannot have more chunks than chunking (as
-        // chunking is claimed to be optimal, which implies minimal chunks).
-        if (cmp == 0) assert(chunking.size() >= simple_chunking.size());
+
+        // Temporarily disabled, as Linearize() currently does not guarantee minimal chunks, even
+        // when it reports an optimal result. This will be re-introduced in a later commit.
+        //
+        // // If simple_chunking is diagram-optimal, it cannot have more chunks than chunking (as
+        // // chunking is claimed to be optimal, which implies minimal chunks).
+        // if (cmp == 0) assert(chunking.size() >= simple_chunking.size());
 
         // Compare with a linearization read from the fuzz input.
         auto read = ReadLinearization(depgraph, reader);
