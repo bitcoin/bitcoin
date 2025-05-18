419db246bb669bb8f37615c3fea22013591c89bc
Luke Dashjr <luke-jr+git@utopios.org>
2025-05-18 01:02:27 +0000

    Recognise service bit 29 as NODE_MALICIOUS (aka NODE_LIBRE)

--- src/protocol.cpp
+++ src/protocol.cpp
@@ -99,6 +99,7 @@ static std::string serviceFlagToStr(size_t bit)
     case NODE_COMPACT_FILTERS: return "COMPACT_FILTERS";
     case NODE_NETWORK_LIMITED: return "NETWORK_LIMITED";
     case NODE_P2P_V2:          return "P2P_V2";
+    case NODE_MALICIOUS:       return "MALICIOUS?";
     // Not using default, so we get warned when a case is missing
     }
 
--- src/protocol.h
+++ src/protocol.h
@@ -336,6 +336,8 @@ enum ServiceFlags : uint64_t {
     // collisions and other cases where nodes may be advertising a service they
     // do not actually support. Other service bits should be allocated via the
     // BIP process.
+
+    NODE_MALICIOUS = (1 << 29),
 };
 
 /**
