c3905e8f3a7712b90c73c7db391f64f670afc13f
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2025-05-19 09:25:04 +0200

    try without DEBUG=1

--- ci/test/00_setup_env_native_previous_releases.sh
+++ ci/test/00_setup_env_native_previous_releases.sh
@@ -9,7 +9,7 @@ export LC_ALL=C.UTF-8
 export CONTAINER_NAME=ci_native_previous_releases
 export CI_IMAGE_NAME_TAG="mirror.gcr.io/ubuntu:24.04"
 export PACKAGES="gcc-13 g++-13 python3-zmq gdb"
-export DEP_OPTS="DEBUG=1 CC=gcc-13 CXX=g++-13 NO_QT=1"
+export DEP_OPTS="CC=gcc-13 CXX=g++-13 NO_QT=1"
 export TEST_RUNNER_EXTRA="--previous-releases --coverage --extended --exclude feature_dbcrash"  # Run extended tests so that coverage does not fail, but exclude the very slow dbcrash
 export RUN_UNIT_TESTS="false"
 export GOAL="install"
@@ -20,6 +20,6 @@ export BITCOIN_CONFIG="\
  -DCMAKE_C_FLAGS='-funsigned-char' \
  -DCMAKE_C_FLAGS_DEBUG='-g2 -O2' \
  -DCMAKE_CXX_FLAGS='-funsigned-char' \
- -DCMAKE_CXX_FLAGS_DEBUG='-g2 -O2' \
+ -DCMAKE_CXX_FLAGS_DEBUG='-g2 -O2 -Wno-error=array-bounds' \
  -DAPPEND_CPPFLAGS='-DBOOST_MULTI_INDEX_ENABLE_SAFE_MODE' \
 "
