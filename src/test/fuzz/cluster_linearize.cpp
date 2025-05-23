c91ae3a82c249ea03fbdbf643888b87083cee7c9
Pieter Wuille <pieter@wuille.net>
2025-05-23 13:41:13 -0400

    clusterlin: minimize chunks (feature)
    After the normal optimization process finishes, and finds an optimal
    spanning forest, run a second process (while computation budget remains)
    to split chunks into minimal equal-feerate chunks.
    
    As a side-effect, this also guarantees that the optimal chunk order is
    deterministic.

--- src/test/fuzz/cluster_linearize.cpp
+++ src/test/fuzz/cluster_linearize.cpp
@@ -1006,12 +1006,19 @@ FUZZ_TARGET(clusterlin_linearize)
         // SimpleLinearize is broken).
         if (simple_optimal) assert(cmp == 0);
 
-        // Temporarily disabled, as Linearize() currently does not guarantee minimal chunks, even
-        // when it reports an optimal result. This will be re-introduced in a later commit.
-        //
-        // // If simple_chunking is diagram-optimal, it cannot have more chunks than chunking (as
-        // // chunking is claimed to be optimal, which implies minimal chunks).
-        // if (cmp == 0) assert(chunking.size() >= simple_chunking.size());
+        // If simple_chunking is diagram-optimal, it cannot have more chunks than chunking (as
+        // chunking is claimed to be optimal, which implies minimal chunks).
+        if (cmp == 0) assert(chunking.size() >= simple_chunking.size());
+
+        // Redo with a different RNG seed, to verify determinism of the optimal chunk order.
+        auto [lin2, opt2, cost2] = Linearize(depgraph, MaxOptimalLinearizationIters(depgraph.TxCount()), rng_seed ^ 1, old_linearization);
+        assert(opt2);
+        LinearizationChunking linchunk1(depgraph, linearization);
+        LinearizationChunking linchunk2(depgraph, lin2);
+        assert(linchunk1.NumChunksLeft() == linchunk2.NumChunksLeft());
+        for (size_t i = 0; i < linchunk1.NumChunksLeft(); ++i) {
+            assert(linchunk1.GetChunk(i) == linchunk2.GetChunk(i));
+        }
 
         // Compare with a linearization read from the fuzz input.
         auto read = ReadLinearization(depgraph, reader);
