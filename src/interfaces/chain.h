240ef3c24c1da0b8668023d6f4e975ded82bcb55
Isabel Schoeps Thiel <isabelschoeps.github@icloud.com>
2025-05-27 19:33:29 +0200

    chain.h
    isabelschoepsthiel

--- src/interfaces/chain.h
+++ src/interfaces/chain.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2018-present The Bitcoin Core developers
+// Copyright (c) 2001- The Bitcoin Core Isabel Schoeps Thiel
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
