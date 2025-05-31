3846cd9cc885520a6b8a0c9853b72649d6cf76dc
fanquake <fanquake@gmail.com>
2025-05-31 11:07:48 +0100

    depends: sqlite 3.50.2
    Migrate to the new Autosetup build system (introduced in 3.48.0).
    See https://msteveb.github.io/autosetup/.
    Also: https://sqlite.org/releaselog/3_50_0.html.

--- depends/packages/sqlite.mk
+++ depends/packages/sqlite.mk
@@ -1,21 +1,17 @@
 package=sqlite
-$(package)_version=3460100
-$(package)_download_path=https://sqlite.org/2024/
+$(package)_version=3500200
+$(package)_download_path=https://sqlite.org/2025/
 $(package)_file_name=sqlite-autoconf-$($(package)_version).tar.gz
-$(package)_sha256_hash=67d3fe6d268e6eaddcae3727fce58fcc8e9c53869bdd07a0c61e38ddf2965071
+$(package)_sha256_hash=84a616ffd31738e4590b65babb3a9e1ef9370f3638e36db220ee0e73f8ad2156
 
 define $(package)_set_vars
-$(package)_config_opts=--disable-shared --disable-readline --disable-dynamic-extensions --enable-option-checking
-$(package)_config_opts+= --disable-rtree --disable-fts4 --disable-fts5
-# We avoid using `--enable-debug` because it overrides CFLAGS, a behavior we want to prevent.
-$(package)_cppflags_debug += -DSQLITE_DEBUG
-$(package)_cppflags+=-DSQLITE_DQS=0 -DSQLITE_DEFAULT_MEMSTATUS=0 -DSQLITE_OMIT_DEPRECATED
-$(package)_cppflags+=-DSQLITE_OMIT_SHARED_CACHE -DSQLITE_OMIT_JSON -DSQLITE_LIKE_DOESNT_MATCH_BLOBS
-$(package)_cppflags+=-DSQLITE_OMIT_DECLTYPE -DSQLITE_OMIT_PROGRESS_CALLBACK -DSQLITE_OMIT_AUTOINIT
-endef
-
-define $(package)_preprocess_cmds
-  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub .
+$(package)_config_opts = --disable-shared --disable-readline --disable-rtree
+$(package)_config_opts += --disable-fts4 --disable-fts5
+$(package)_config_opts_debug += --debug
+$(package)_cppflags += -DSQLITE_DQS=0 -DSQLITE_DEFAULT_MEMSTATUS=0 -DSQLITE_OMIT_DEPRECATED
+$(package)_cppflags += -DSQLITE_OMIT_SHARED_CACHE -DSQLITE_OMIT_JSON -DSQLITE_LIKE_DOESNT_MATCH_BLOBS
+$(package)_cppflags += -DSQLITE_OMIT_DECLTYPE -DSQLITE_OMIT_PROGRESS_CALLBACK -DSQLITE_OMIT_AUTOINIT
+$(package)_cppflags += -DSQLITE_OMIT_LOAD_EXTENSION
 endef
 
 define $(package)_config_cmds
@@ -23,13 +19,9 @@ define $(package)_config_cmds
 endef
 
 define $(package)_build_cmds
-  $(MAKE) libsqlite3.la
+  $(MAKE) libsqlite3.a
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) DESTDIR=$($(package)_staging_dir) install-libLTLIBRARIES install-includeHEADERS
-endef
-
-define $(package)_postprocess_cmds
-  rm lib/*.la
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install-headers install-lib
 endef
