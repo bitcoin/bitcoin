095b2d8a4e18b75b5d5075fd8d9ce71167345053
Konstantin Akimov <knstqq@gmail.com>
2025-06-03 01:51:06 +0700

    refactor: use if statement feature
    Co-authored-by: PastaPastaPasta <6443210+PastaPastaPasta@users.noreply.github.com>

--- src/evo/creditpool.cpp
+++ src/evo/creditpool.cpp
@@ -67,11 +67,8 @@ static std::optional<CreditPoolDataPerBlock> GetCreditDataFromBlock(const gsl::n
     static Mutex cache_mutex;
     static unordered_lru_cache<uint256, CreditPoolDataPerBlock, StaticSaltedHasher> block_data_cache GUARDED_BY(
         cache_mutex){static_cast<size_t>(Params().CreditPoolPeriodBlocks()) * 2};
-    {
-        LOCK(cache_mutex);
-        if (block_data_cache.get(block_index->GetBlockHash(), blockData)) {
-            return blockData;
-        }
+    if (LOCK(cache_mutex); block_data_cache.get(block_index->GetBlockHash(), blockData)) {
+        return blockData;
     }
 
     CBlock block;
