e3b7f8c93f630ef6406b64276e17bb97fa08d054
Matthew Zipkin <pinheadmz@pm.me>
2025-06-04 13:52:56 -0400

    SockMan: introduce class and implement binding to listening socket
    Introduce a new low-level socket managing class `SockMan`.
    
    BindListenPort() is copied from CConnMan in net.cpp and will
    be modernized in the next commit.
    
    Unit-test it with a new class `SocketTestingSetup` which mocks
    `CreateSock()` and will enable mock client I/O in future commits.
    
    `SockMan` and `SocketTestingSetup` are designed to be generic and
    reusbale for higher-level network protocol implementation and testing.
    
    Co-authored-by: Vasil Dimov <vd@FreeBSD.org>

---
+++ src/test/sockman_tests.cpp
@@ -0,0 +1,44 @@
+// Copyright (c) The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <common/sockman.h>
+#include <test/util/setup_common.h>
+#include <util/translation.h>
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_FIXTURE_TEST_SUITE(sockman_tests, SocketTestingSetup)
+
+class TestSockMan : public SockMan
+{
+public:
+    size_t GetListeningSocketCount() { return m_listen.size(); };
+};
+
+BOOST_AUTO_TEST_CASE(test_sockman)
+{
+    TestSockMan sockman;
+
+    {
+        // We can only bind to NET_IPV4 and NET_IPV6
+        CService onion_address{Lookup("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaam2dqd.onion", 0, false).value()};
+        bilingual_str str_error;
+        auto result{sockman.BindListenPort(onion_address, str_error)};
+        BOOST_REQUIRE(!result);
+        BOOST_CHECK_EQUAL(str_error.original, "Bind address family for aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaam2dqd.onion:0 not supported");
+    }
+
+    // This VALID address won't actually get used because we stubbed CreateSock()
+    CService addr_bind{Lookup("0.0.0.0", 0, false).value()};
+
+    // Init state
+    BOOST_REQUIRE_EQUAL(sockman.GetListeningSocketCount(), 0);
+    // Bind to mock Listening Socket
+    bilingual_str str_error;
+    BOOST_REQUIRE(sockman.BindListenPort(addr_bind, str_error));
+    // We are bound and listening
+    BOOST_REQUIRE_EQUAL(sockman.GetListeningSocketCount(), 1);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
--- src/test/util/setup_common.cpp
+++ src/test/util/setup_common.cpp
@@ -582,6 +582,23 @@ std::vector<CTransactionRef> TestChain100Setup::PopulateMempool(FastRandomContex
     return mempool_transactions;
 }
 
+SocketTestingSetup::SocketTestingSetup()
+    : m_create_sock_orig{CreateSock}
+{
+    CreateSock = [this](int, int, int) {
+        // This is a mock Listening Socket that a server can "bind" to and
+        // listen to for incoming connections. We won't need to access its I/O
+        // pipes because we don't read or write directly to it. It will return
+        // Connected Sockets from the queue via its Accept() method.
+        return std::make_unique<DynSock>(std::make_shared<DynSock::Pipes>(), m_accepted_sockets);
+    };
+};
+
+SocketTestingSetup::~SocketTestingSetup()
+{
+    CreateSock = m_create_sock_orig;
+}
+
 /**
  * @returns a real block (0000000000013b8ab2cd513b0261a14096412195a72a0c4827d229dcc7e0f7af)
  *      with 9 txs.
