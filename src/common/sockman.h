735a12a1a90d040bc66f8d7a05ac561a43fbaba2
Matthew Zipkin <pinheadmz@pm.me>
2025-06-04 14:14:13 -0400

    SockMan: implement and test AcceptConnection()
    AcceptConnection() is mostly copied from CConmann in net.cpp
    and will be modernized in the following commit.
    
    Co-authored-by: Vasil Dimov <vd@FreeBSD.org>

--- src/common/sockman.h
+++ src/common/sockman.h
@@ -18,6 +18,7 @@
  * To use this class, inherit from it and implement the pure virtual methods.
  * Handled operations:
  * - binding and listening on sockets
+ * - accepting incoming connections
  */
 class SockMan
 {
@@ -35,6 +36,14 @@ public:
     void StopListening();
 
 protected:
+    /**
+     * Accept a connection.
+     * @param[in] listen_sock Socket on which to accept the connection.
+     * @param[out] addr Address of the peer that was accepted.
+     * @return Newly created socket for the accepted connection.
+     */
+    std::unique_ptr<Sock> AcceptConnection(const Sock& listen_sock, CService& addr);
+
     /**
      * List of listening sockets.
      */
