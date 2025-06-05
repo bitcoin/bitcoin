8713e8060d504f561fed705b4aa5af7b96c36e75
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2025-06-06 01:48:41 +0200

    depends: fix SHA256SUM command on OpenBSD (use GNU mode output)
    On OpenBSD, the `sha256` command by default outputs hashsums on files in
    "BSD" mode, looking like this:
    
    $ sha256 ~/.vimrc
    SHA256 (/home/thestack/.vimrc) = 6ba69d100e8c5ca0488ded6293d4e5f740a6a5d5ace96cbcf0599c18d27389e4
    
    This is not compatible with our depends commands, which expect the
    hashes to be on the first column (to be extracted via `cut -d" " -f1`).
    Fix this by switching to GNU mode output, looking like this:
    
    $ sha256 -r ~/.vimrc
    6ba69d100e8c5ca0488ded6293d4e5f740a6a5d5ace96cbcf0599c18d27389e4 /home/thestack/.vimrc
    
    Without this change, the multiprocess depends build fails with the following output:
    
    $ gmake -C depends MULTIPROCESS=1 NO_BOOST=1 NO_LIBEVENT=1 NO_QT=1 NO_QR=1 NO_WALLET=1 NO_ZMQ=1 NO_USDT=1
    [ ..... ]
    Extracting native_libmultiprocess...
    sha256: /home/thestack/bitcoin/depends/work/build/x86_64-unknown-openbsd7.7/native_libmultiprocess/-2bc902f4693/.src-ipc-libmultiprocess.tar.hash: no properly formatted checksum lines found
    gmake: *** [funcs.mk:342: /home/thestack/bitcoin/depends/work/build/x86_64-unknown-openbsd7.7/native_libmultiprocess/-2bc902f4693/.stamp_extracted] Error 1

--- depends/builders/openbsd.mk
+++ depends/builders/openbsd.mk
@@ -1,7 +1,7 @@
 build_openbsd_CC = clang
 build_openbsd_CXX = clang++
 
-build_openbsd_SHA256SUM = sha256
+build_openbsd_SHA256SUM = sha256 -r
 build_openbsd_DOWNLOAD = curl --location --fail --connect-timeout $(DOWNLOAD_CONNECT_TIMEOUT) --retry $(DOWNLOAD_RETRIES) -o
 
 build_openbsd_TAR = gtar
