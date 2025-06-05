2d938720bd67cf4259bcc934025e7af17f5b4b2b
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2025-06-06 01:27:48 +0200

    depends: add patch to fix capnp build on OpenBSD
    see https://github.com/capnproto/capnproto/pull/2308

--- depends/packages/native_capnp.mk
+++ depends/packages/native_capnp.mk
@@ -4,11 +4,16 @@ $(package)_download_path=https://capnproto.org/
 $(package)_download_file=capnproto-c++-$($(package)_version).tar.gz
 $(package)_file_name=capnproto-cxx-$($(package)_version).tar.gz
 $(package)_sha256_hash=07167580e563f5e821e3b2af1c238c16ec7181612650c5901330fa9a0da50939
+$(package)_patches=fix_openbsd_build.patch
 
 define $(package)_set_vars
   $(package)_config_opts := -DBUILD_TESTING=OFF
   $(package)_config_opts += -DWITH_OPENSSL=OFF
   $(package)_config_opts += -DWITH_ZLIB=OFF
+endef
+
+define $(package)_preprocess_cmds
+  patch -p2 < $($(package)_patch_dir)/fix_openbsd_build.patch
 endef
 
 define $(package)_config_cmds
