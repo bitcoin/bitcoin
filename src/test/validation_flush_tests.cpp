24630069ad2f322b14a20509d832ff7d09ef8d56
Larry Ruane <larryruane@gmail.com>
2025-06-08 10:13:59 -0600

    improve MallocUsage() accuracy
    Co-authored-by: Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
    Co-authored-by: Pieter Wuille <pieter@wuille.net>
    Co-authored-by: Martin Leitner-Ankerl <martin.ankerl@gmail.com>
    Co-authored-by: l0rinc <pap.lorinc@gmail.com>
    
    # Changes to test/functional/test_framework/mempool_util.py:
    
    ## fill_mempool() should call sync_mempools() before returning
    
    We saw a case where a test (p2p_1p1c_network.py) called
    raise_network_minfee(), which called fill_mempool() using node 0.
    
    Then raise_network_minfee() returned, and the test called rescan_utxos(),
    which called getrawmempool() using a different node (node 1) followed by
    getrawtransaction() on each returned transaction, and the test asserted
    because a transaction was not found.
    
    This was caused by the timing window between the call to getrawmempool()
    and fetching the individual transactions; the transactions were still
    being propagated on the P2P network. During this window, a transaction
    (returned by getrawmempool()) was evicted (the mempool is close to full
    during this test), and did not exist in the mempool by the time it was
    attempted to be fetched.
    
    It might make more sense for rescan_utxos() to call sync_mempools() just
    before calling getrawmempool(), but it can't because rescan_utxos() is
    part of the MiniWallet class, which doesn't have access to test_framework
    (but that could probably be changed).
    
    ## ensure that `fill_mempool` leaves some room in mempool
    
    Without this change, fill_mempool() may leave the mempool very close
    to its memory size limit (-maxmempool). This can cause tests to
    incorrectly fail when they submit another transaction expecting it
    to succeed. Note that without this change, the same test that fails on
    one platform may succeed on another, because their memory allocation
    accounting algorithms (how they calculate memory usage, that is,
    MallocUsage()) may be slightly different.

--- src/test/validation_flush_tests.cpp
+++ src/test/validation_flush_tests.cpp
@@ -21,115 +21,113 @@ BOOST_AUTO_TEST_CASE(getcoinscachesizestate)
 {
     Chainstate& chainstate{m_node.chainman->ActiveChainstate()};
 
-    constexpr bool is_64_bit = sizeof(void*) == 8;
-
     LOCK(::cs_main);
     auto& view = chainstate.CoinsTip();
 
-    // The number of bytes consumed by coin's heap data, i.e. CScript
-    // (prevector<28, unsigned char>) when assigned 56 bytes of data per above.
-    //
-    // See also: Coin::DynamicMemoryUsage().
-    constexpr unsigned int COIN_SIZE = is_64_bit ? 80 : 64;
+    // An empty coins cache still allocates one PoolResource 'chunk', which is 256 KiB, and there
+    // is some overhead. As a sanity check, an empty coins cache should be only slightly larger.
+    BOOST_CHECK_LT(view.DynamicMemoryUsage(), 256 * 1024 * 100 / 98);
 
-    auto print_view_mem_usage = [](CCoinsViewCache& view) {
-        BOOST_TEST_MESSAGE("CCoinsViewCache memory usage: " << view.DynamicMemoryUsage());
-    };
+    // PoolResource defaults to 256 KiB that will be allocated, so we need to test with a size
+    // significantly larger that size so that fragmentation effects are minimal.
+    // But this size can still be much smaller than the default coins cache size, 450 MB.
+    constexpr size_t MAX_COINS_CACHE_BYTES{8'000'000};
 
-    // PoolResource defaults to 256 KiB that will be allocated, so we'll take that and make it a bit larger.
-    constexpr size_t MAX_COINS_CACHE_BYTES = 262144 + 512;
+    // The coins cache can also use the mempool.
+    constexpr size_t MAX_MEMPOOL_CACHE_BYTES{4'000'000};
+
+    // Check GetCoinsCacheSizeState() with an empty coins cache.
+    const CoinsCacheSizeState empty_cache{chainstate.GetCoinsCacheSizeState(MAX_COINS_CACHE_BYTES, /*max_mempool_size_bytes=*/0)};
 
     // Without any coins in the cache, we shouldn't need to flush.
-    BOOST_TEST(
-        chainstate.GetCoinsCacheSizeState(MAX_COINS_CACHE_BYTES, /*max_mempool_size_bytes=*/ 0) != CoinsCacheSizeState::CRITICAL);
+    BOOST_CHECK_NE(empty_cache, CoinsCacheSizeState::CRITICAL);
 
-    // If the initial memory allocations of cacheCoins don't match these common
-    // cases, we can't really continue to make assertions about memory usage.
-    // End the test early.
-    if (view.DynamicMemoryUsage() != 32 && view.DynamicMemoryUsage() != 16) {
-        // Add a bunch of coins to see that we at least flip over to CRITICAL.
+    // And we shouldn't be within 10% of full.
+    BOOST_CHECK_NE(empty_cache, CoinsCacheSizeState::LARGE);
 
-        for (int i{0}; i < 1000; ++i) {
-            const COutPoint res = AddTestCoin(m_rng, view);
-            BOOST_CHECK_EQUAL(view.AccessCoin(res).DynamicMemoryUsage(), COIN_SIZE);
-        }
+    // There should be plenty of space.
+    BOOST_CHECK_EQUAL(empty_cache, CoinsCacheSizeState::OK);
 
-        BOOST_CHECK_EQUAL(
-            chainstate.GetCoinsCacheSizeState(MAX_COINS_CACHE_BYTES, /*max_mempool_size_bytes=*/0),
-            CoinsCacheSizeState::CRITICAL);
-
-        BOOST_TEST_MESSAGE("Exiting cache flush tests early due to unsupported arch");
-        return;
-    }
-
-    print_view_mem_usage(view);
-    BOOST_CHECK_EQUAL(view.DynamicMemoryUsage(), is_64_bit ? 32U : 16U);
-
-    // We should be able to add COINS_UNTIL_CRITICAL coins to the cache before going CRITICAL.
-    // This is contingent not only on the dynamic memory usage of the Coins
-    // that we're adding (COIN_SIZE bytes per), but also on how much memory the
-    // cacheCoins (unordered_map) preallocates.
-    constexpr int COINS_UNTIL_CRITICAL{3};
-
-    // no coin added, so we have plenty of space left.
-    BOOST_CHECK_EQUAL(
-        chainstate.GetCoinsCacheSizeState(MAX_COINS_CACHE_BYTES, /*max_mempool_size_bytes*/ 0),
-        CoinsCacheSizeState::OK);
-
-    for (int i{0}; i < COINS_UNTIL_CRITICAL; ++i) {
-        const COutPoint res = AddTestCoin(m_rng, view);
-        print_view_mem_usage(view);
-        BOOST_CHECK_EQUAL(view.AccessCoin(res).DynamicMemoryUsage(), COIN_SIZE);
-
-        // adding first coin causes the MemoryResource to allocate one 256 KiB chunk of memory,
-        // pushing us immediately over to LARGE
-        BOOST_CHECK_EQUAL(
-            chainstate.GetCoinsCacheSizeState(MAX_COINS_CACHE_BYTES, /*max_mempool_size_bytes=*/ 0),
-            CoinsCacheSizeState::LARGE);
-    }
-
-    // Adding some additional coins will push us over the edge to CRITICAL.
-    for (int i{0}; i < 4; ++i) {
+    // Add coins until the cache is just under 90% full; the returned state should
+    // be OK until 90%. The loop counter prevents a runaway test.
+    size_t i;
+    for (i = 0; i < 80'000; ++i) {
         AddTestCoin(m_rng, view);
-        print_view_mem_usage(view);
-        if (chainstate.GetCoinsCacheSizeState(MAX_COINS_CACHE_BYTES, /*max_mempool_size_bytes=*/0) ==
-            CoinsCacheSizeState::CRITICAL) {
-            break;
-        }
-    }
 
+        // Fill to about 89% (below 90%) of the cache size limit.
+        if (view.DynamicMemoryUsage() >= MAX_COINS_CACHE_BYTES * 89 / 100) break;
+
+        BOOST_REQUIRE_EQUAL(
+            chainstate.GetCoinsCacheSizeState(MAX_COINS_CACHE_BYTES, /*max_mempool_size_bytes=*/0),
+            CoinsCacheSizeState::OK);
+    }
+    BOOST_CHECK_LT(i, 80'000);
+
+    // This loop transitions the cache state from OK (< 90%) to LARGE (between 90% and 100%).
+    for (i = 0; i < 80'000; ++i) {
+        AddTestCoin(m_rng, view);
+
+        // Fill to about 95% of the cache size limit.
+        if (view.DynamicMemoryUsage() >= MAX_COINS_CACHE_BYTES * 95 / 100) break;
+    }
+    BOOST_CHECK_LT(i, 80'000);
+    BOOST_CHECK_EQUAL(
+        chainstate.GetCoinsCacheSizeState(MAX_COINS_CACHE_BYTES, /*max_mempool_size_bytes=*/0),
+        CoinsCacheSizeState::LARGE);
+
+    // This loop moves the cache state from LARGE (90% to 100%) to CRITICAL (over 100%).
+    for (i = 0; i < 80'000; ++i) {
+        AddTestCoin(m_rng, view);
+
+        // Fill to just beyojnd the cache size limit.
+        if (view.DynamicMemoryUsage() > MAX_COINS_CACHE_BYTES) break;
+    }
+    BOOST_CHECK_LT(i, 80'000);
     BOOST_CHECK_EQUAL(
         chainstate.GetCoinsCacheSizeState(MAX_COINS_CACHE_BYTES, /*max_mempool_size_bytes=*/0),
         CoinsCacheSizeState::CRITICAL);
 
-    // Passing non-zero max mempool usage (512 KiB) should allow us more headroom.
+    // Repeat (continuing with the existing cache) but with a non-zero max mempool;
+    // this allows more headroom.
     BOOST_CHECK_EQUAL(
-        chainstate.GetCoinsCacheSizeState(MAX_COINS_CACHE_BYTES, /*max_mempool_size_bytes=*/ 1 << 19),
+        chainstate.GetCoinsCacheSizeState(MAX_COINS_CACHE_BYTES, /*max_mempool_size_bytes=*/MAX_MEMPOOL_CACHE_BYTES),
         CoinsCacheSizeState::OK);
 
-    for (int i{0}; i < 3; ++i) {
+    for (i = 0; i < 80'000; ++i) {
         AddTestCoin(m_rng, view);
-        print_view_mem_usage(view);
-        BOOST_CHECK_EQUAL(
-            chainstate.GetCoinsCacheSizeState(MAX_COINS_CACHE_BYTES, /*max_mempool_size_bytes=*/ 1 << 19),
+        // Fill to about 89% (below 90%) of the cache size limit.
+        if (view.DynamicMemoryUsage() >= (MAX_COINS_CACHE_BYTES + MAX_MEMPOOL_CACHE_BYTES) * 89 / 100) break;
+
+        BOOST_REQUIRE_EQUAL(
+            chainstate.GetCoinsCacheSizeState(MAX_COINS_CACHE_BYTES, /*max_mempool_size_bytes=*/MAX_MEMPOOL_CACHE_BYTES),
             CoinsCacheSizeState::OK);
     }
+    BOOST_CHECK_LT(i, 80'000);
 
-    // Adding another coin with the additional mempool room will put us >90%
-    // but not yet critical.
-    AddTestCoin(m_rng, view);
-    print_view_mem_usage(view);
+    // This loop transitions the cache state from OK (< 90%) to LARGE (between 90% and 100%).
+    for (i = 0; i < 80'000; ++i) {
+        AddTestCoin(m_rng, view);
 
-    // Only perform these checks on 64 bit hosts; I haven't done the math for 32.
-    if (is_64_bit) {
-        float usage_percentage = (float)view.DynamicMemoryUsage() / (MAX_COINS_CACHE_BYTES + (1 << 10));
-        BOOST_TEST_MESSAGE("CoinsTip usage percentage: " << usage_percentage);
-        BOOST_CHECK(usage_percentage >= 0.9);
-        BOOST_CHECK(usage_percentage < 1);
-        BOOST_CHECK_EQUAL(
-            chainstate.GetCoinsCacheSizeState(MAX_COINS_CACHE_BYTES, /*max_mempool_size_bytes*/ 1 << 10), // 1024
-            CoinsCacheSizeState::LARGE);
+        // Fill to about 95% of the cache size limit..
+        if (view.DynamicMemoryUsage() >= (MAX_COINS_CACHE_BYTES + MAX_MEMPOOL_CACHE_BYTES) * 95 / 100) break;
     }
+    BOOST_CHECK_LT(i, 80'000);
+    BOOST_CHECK_EQUAL(
+        chainstate.GetCoinsCacheSizeState(MAX_COINS_CACHE_BYTES, /*max_mempool_size_bytes=*/MAX_MEMPOOL_CACHE_BYTES),
+        CoinsCacheSizeState::LARGE);
+
+    // This loop moves the cache state from LARGE (90% to 100%) to CRITICAL (over 100%).
+    for (i = 0; i < 80'000; ++i) {
+        AddTestCoin(m_rng, view);
+
+        // Fill to just beyojnd the cache size limit.
+        if (view.DynamicMemoryUsage() > MAX_COINS_CACHE_BYTES + MAX_MEMPOOL_CACHE_BYTES) break;
+    }
+    BOOST_CHECK_LT(i, 80'000);
+    BOOST_CHECK_EQUAL(
+        chainstate.GetCoinsCacheSizeState(MAX_COINS_CACHE_BYTES, /*max_mempool_size_bytes=*/MAX_MEMPOOL_CACHE_BYTES),
+        CoinsCacheSizeState::CRITICAL);
+
 
     // Using the default max_* values permits way more coins to be added.
     for (int i{0}; i < 1000; ++i) {
@@ -140,14 +138,12 @@ BOOST_AUTO_TEST_CASE(getcoinscachesizestate)
     }
 
     // Flushing the view does take us back to OK because ReallocateCache() is called
-
     BOOST_CHECK_EQUAL(
         chainstate.GetCoinsCacheSizeState(MAX_COINS_CACHE_BYTES, 0),
         CoinsCacheSizeState::CRITICAL);
 
     view.SetBestBlock(m_rng.rand256());
     BOOST_CHECK(view.Flush());
-    print_view_mem_usage(view);
 
     BOOST_CHECK_EQUAL(
         chainstate.GetCoinsCacheSizeState(MAX_COINS_CACHE_BYTES, 0),
