af1c1df25a083faa35a54a25e097f1cf90bd6afc
WakeTrainDev <175499942+waketraindev@users.noreply.github.com>
2025-06-12 20:56:57 +0300

    rpc: add optional "peer_id" filter to getpeerinfo with test coverage

--- src/rpc/util.cpp
+++ src/rpc/util.cpp
@@ -724,6 +724,7 @@ static void CheckRequiredOrDefault(const RPCArg& param)
 TMPL_INST(nullptr, const UniValue*, maybe_arg;);
 TMPL_INST(nullptr, std::optional<double>, maybe_arg ? std::optional{maybe_arg->get_real()} : std::nullopt;);
 TMPL_INST(nullptr, std::optional<bool>, maybe_arg ? std::optional{maybe_arg->get_bool()} : std::nullopt;);
+TMPL_INST(nullptr, std::optional<int64_t>, maybe_arg ? std::optional{maybe_arg->getInt<int64_t>()} : std::nullopt;);
 TMPL_INST(nullptr, const std::string*, maybe_arg ? &maybe_arg->get_str() : nullptr;);
 
 // Required arg or optional arg with default value.
