a488c8d3123ffa6cc7e4dc3fc7d4760dbbf1c5ed
Konstantin Akimov <knstqq@gmail.com>
2025-06-13 14:47:24 +0700

    fix: add missing mutex to operator== for bls wrapper

--- src/bls/bls.h
+++ src/bls/bls.h
@@ -509,19 +509,23 @@ public:
 
     bool operator==(const CBLSLazyWrapper& r) const
     {
-        // If neither bufValid or objInitialized are set, then the object is the default object.
-        const bool is_default{!bufValid && !objInitialized};
-        const bool r_is_default{!r.bufValid && !r.objInitialized};
-        // If both are default; they are equal.
-        if (is_default && r_is_default) return true;
-        // If one is default and the other isn't, we are not equal
-        if (is_default != r_is_default) return false;
+        if (&r == this) return true;
+        {
+            std::scoped_lock lock(mutex, r.mutex);
+            // If neither bufValid or objInitialized are set, then the object is the default object.
+            const bool is_default{!bufValid && !objInitialized};
+            const bool r_is_default{!r.bufValid && !r.objInitialized};
+            // If both are default; they are equal.
+            if (is_default && r_is_default) return true;
+            // If one is default and the other isn't, we are not equal
+            if (is_default != r_is_default) return false;
 
-        if (bufValid && r.bufValid && bufLegacyScheme == r.bufLegacyScheme) {
-            return vecBytes == r.vecBytes;
-        }
-        if (objInitialized && r.objInitialized) {
-            return obj == r.obj;
+            if (bufValid && r.bufValid && bufLegacyScheme == r.bufLegacyScheme) {
+                return vecBytes == r.vecBytes;
+            }
+            if (objInitialized && r.objInitialized) {
+                return obj == r.obj;
+            }
         }
         return Get() == r.Get();
     }
