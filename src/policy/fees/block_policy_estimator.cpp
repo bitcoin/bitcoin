099cfc5fb8a8d3596cdec109bfaa345c7760acac
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2025-06-14 06:40:54 +0100

    fees: make flushes log debug only
    - Also log the full file path of fee_estimates.dat consistently
      not just the filename.

--- src/policy/fees/block_policy_estimator.cpp
+++ src/policy/fees/block_policy_estimator.cpp
@@ -561,18 +561,18 @@ CBlockPolicyEstimator::CBlockPolicyEstimator(const fs::path& estimation_filepath
     AutoFile est_file{fsbridge::fopen(m_estimation_filepath, "rb")};
 
     if (est_file.IsNull()) {
-        LogInfo("%s is not found. Continue anyway.", fs::PathToString(m_estimation_filepath));
+        LogInfo("%s is not found. Continue anyway.\n", fs::PathToString(m_estimation_filepath));
         return;
     }
 
     std::chrono::hours file_age = GetFeeEstimatorFileAge();
     if (file_age > MAX_FILE_AGE && !read_stale_estimates) {
-        LogPrintf("Fee estimation file %s too old (age=%lld > %lld hours) and will not be used to avoid serving stale estimates.\n", fs::PathToString(m_estimation_filepath), Ticks<std::chrono::hours>(file_age), Ticks<std::chrono::hours>(MAX_FILE_AGE));
+        LogInfo("Fee estimation file %s too old (age=%lld > %lld hours) and will not be used to avoid serving stale estimates.\n", fs::PathToString(m_estimation_filepath), Ticks<std::chrono::hours>(file_age), Ticks<std::chrono::hours>(MAX_FILE_AGE));
         return;
     }
 
     if (!Read(est_file)) {
-        LogPrintf("Failed to read fee estimates from %s. Continue anyway.\n", fs::PathToString(m_estimation_filepath));
+        LogInfo("Failed to read fee estimates from %s. Continue anyway.\n", fs::PathToString(m_estimation_filepath));
     }
 }
 
@@ -975,7 +975,7 @@ void CBlockPolicyEstimator::FlushFeeEstimates()
 
     AutoFile est_file{fsbridge::fopen(m_estimation_filepath, "wb")};
     if (est_file.IsNull() || !Write(est_file)) {
-        LogPrintf("Failed to write fee estimates to %s. Continue anyway.\n", fs::PathToString(m_estimation_filepath));
+        LogDebug(BCLog::ESTIMATEFEE, "Failed to write fee estimates to %s. Continue anyway.\n", fs::PathToString(m_estimation_filepath));
         (void)est_file.fclose();
         return;
     }
@@ -983,7 +983,7 @@ void CBlockPolicyEstimator::FlushFeeEstimates()
         LogError("Failed to close fee estimates file %s: %s. Continuing anyway.", fs::PathToString(m_estimation_filepath), SysErrorString(errno));
         return;
     }
-    LogInfo("Flushed fee estimates to %s.", fs::PathToString(m_estimation_filepath.filename()));
+    LogDebug(BCLog::ESTIMATEFEE, "Flushed fee estimates to %s.\n", fs::PathToString(m_estimation_filepath));
 }
 
 bool CBlockPolicyEstimator::Write(AutoFile& fileout) const
--- test/functional/feature_fee_estimation.py
+++ test/functional/feature_fee_estimation.py
@@ -332,7 +332,8 @@ class EstimateFeeTest(BitcoinTestFramework):
 
         # Verify if the string "Flushed fee estimates to fee_estimates.dat." is present in the debug log file.
         # If present, it indicates that fee estimates have been successfully flushed to disk.
-        with self.nodes[0].assert_debug_log(expected_msgs=["Flushed fee estimates to fee_estimates.dat."], timeout=1):
+        expected_message = f"Flushed fee estimates to {fee_dat}."
+        with self.nodes[0].assert_debug_log(expected_msgs=[expected_message], timeout=1):
             # Mock the scheduler for an hour to flush fee estimates to fee_estimates.dat
             self.nodes[0].mockscheduler(SECONDS_PER_HOUR)
 
@@ -342,7 +343,7 @@ class EstimateFeeTest(BitcoinTestFramework):
         # Verify that the estimates remain the same if there are no blocks in the flush interval
         block_hash_before = self.nodes[0].getbestblockhash()
         fee_dat_initial_content = open(fee_dat, "rb").read()
-        with self.nodes[0].assert_debug_log(expected_msgs=["Flushed fee estimates to fee_estimates.dat."], timeout=1):
+        with self.nodes[0].assert_debug_log(expected_msgs=[expected_message], timeout=1):
             # Mock the scheduler for an hour to flush fee estimates to fee_estimates.dat
             self.nodes[0].mockscheduler(SECONDS_PER_HOUR)
 
@@ -358,7 +359,7 @@ class EstimateFeeTest(BitcoinTestFramework):
         assert_equal(fee_dat_current_content, fee_dat_initial_content)
 
         # Verify that the estimates are not the same if new blocks were produced in the flush interval
-        with self.nodes[0].assert_debug_log(expected_msgs=["Flushed fee estimates to fee_estimates.dat."], timeout=1):
+        with self.nodes[0].assert_debug_log(expected_msgs=[expected_message], timeout=1):
             # Mock the scheduler for an hour to flush fee estimates to fee_estimates.dat
             self.generate(self.nodes[0], 5, sync_fun=self.no_op)
             self.nodes[0].mockscheduler(SECONDS_PER_HOUR)
