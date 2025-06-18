a21dfd272415835eb49753cdef7557a22f5cffc1
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2025-06-18 08:26:08 +0000

    partial bitcoin#24587: use MiniWallet for rpc_createmultisig.py
    includes:
    - 2726b60a3ac098b44f2970bed21147b70e12a1c2 (only wallet.py)

--- test/functional/test_framework/wallet.py
+++ test/functional/test_framework/wallet.py
@@ -89,6 +89,9 @@ class MiniWallet:
             self._address = ADDRESS_BCRT1_P2SH_OP_TRUE
             self._scriptPubKey = bytes.fromhex(self._test_node.validateaddress(self._address)['scriptPubKey'])
 
+    def get_balance(self):
+        return sum(u['value'] for u in self._utxos)
+
     def rescan_utxos(self):
         """Drop all utxos and rescan the utxo set"""
         self._utxos = []
