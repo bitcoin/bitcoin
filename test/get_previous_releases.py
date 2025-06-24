1026f111081d45d7489bdaa8b90c46fe7f0273bb
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2025-06-24 12:59:54 +0000

    fix: make `get_previous_release.py` translate triples for pre-v0.12.2.x
    Relevant for the upcoming backport, which relies on a version of Dash
    before v0.12.2.2 for validating migration code handling databases
    created before bitcoin#10195 (which is included in v0.12.2.2, so we must
    use one patch version earlier, i.e. v0.12.1.x)

--- test/get_previous_releases.py
+++ test/get_previous_releases.py
@@ -124,6 +124,15 @@ def download_binary(tag, args) -> int:
         platform = "win64"
     elif tag < "v20" and platform in ["x86_64-apple-darwin", "arm64-apple-darwin"]:
         platform = "osx64"
+    elif tag < "v0.12.2.3":
+        if platform in ["arm-linux-gnueabihf"]:
+            platform = "RPi2"
+        elif platform in ["x86_64-apple-darwin", "arm64-apple-darwin"]:
+            platform = "osx"
+        elif platform in ["i686-pc-linux-gnu"]:
+            platform = "linux32"
+        elif platform in ["x86_64-linux-gnu"]:
+            platform = "linux64"
     tarball = 'dashcore-{tag}-{platform}.tar.gz'.format(
         tag=tag[1:], platform=platform)
     tarballUrl = 'https://github.com/dashpay/dash/{bin_path}/{tarball}'.format(
