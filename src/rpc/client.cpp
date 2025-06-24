8bfb104b5861f6473c5d516c04021bf428210233
Novo <eunovo9@gmail.com>
2025-06-24 09:55:30 +0100

    wallet/rpc: add create silent-payments wallet option
    Co-authored-by: Ava Chow <github@achow101.com>

--- src/rpc/client.cpp
+++ src/rpc/client.cpp
@@ -354,6 +354,7 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "createwallet", 5, "descriptors"},
     { "createwallet", 6, "load_on_startup"},
     { "createwallet", 7, "external_signer"},
+    { "createwallet", 8, "silent_payments" },
     { "restorewallet", 0, "wallet_name", ParamFormat::STRING },
     { "restorewallet", 1, "backup_file", ParamFormat::STRING },
     { "restorewallet", 2, "load_on_startup"},
