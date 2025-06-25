51780cd410b2eba2b9d77c39a9c8919ed8ef97c0
Novo <eunovo9@gmail.com>
2025-06-25 13:26:56 +0100

    wallet/rpc: add sp-address support to getaddressinfo

--- src/rpc/util.cpp
+++ src/rpc/util.cpp
@@ -280,7 +280,9 @@ public:
 
     UniValue operator()(const V0SilentPaymentDestination& dest) const
     {
-        return UniValue(UniValue::VOBJ);
+        UniValue obj(UniValue::VOBJ);
+        obj.pushKV("iswitness", false);
+        return obj;
     }
 
     UniValue operator()(const PubKeyDestination& dest) const
