fa3f100010f1a0273891ed0d14ddb3c5ccacbd4b
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2025-06-30 14:50:32 +0200

    test: Use msg_generic in p2p_ping.py

--- test/functional/test_framework/messages.py
+++ test/functional/test_framework/messages.py
@@ -1374,8 +1374,8 @@ class msg_block:
         return "msg_block(block=%s)" % (repr(self.block))
 
 
-# for cases where a user needs tighter control over what is sent over the wire
-# note that the user must supply the name of the msgtype, and the data
+# Generic type to control the raw bytes sent over the wire.
+# The msgtype and the data must be provided.
 class msg_generic:
     __slots__ = ("msgtype", "data")
 
