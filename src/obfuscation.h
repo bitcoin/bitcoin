342bb224bb96b09d9950a144e62e60ede8710191
Lőrinc <pap.lorinc@gmail.com>
2025-07-02 11:24:21 +0200

    optimization: migrate fixed-size obfuscation from `std::vector<std::byte>` to `uint64_t`
    Migrated the obfuscation key end‑to‑end to `uint64_t` keys instead of array/vector of bytes/chars - from use sites in util::Obfuscation up through streams, LevelDB wrappers, and disk (de)serialization.
    All former `std::vector<std::byte>` keys were replaced with `uint64_t` (we still serialize them as vectors but convert immediately to `uint64_t` on load).
    This is why tests still generate vector keys and convert them to `uint64_t` later instead of generating them directly.
    We also short‑circuit `Obfuscation` calls when the key is zero to avoid unnecessary calculations (e.g., `MakeWritableByteSpan`).
    
    In `Obfuscation::Unserialize` we can safely throw an `std::logic_error` since during mempool fuzzing `mempool_persist.cpp#L141` catches and ignored these errors safely.
    
    >  C++ compiler .......................... GNU 14.2.0
    
    |             ns/byte |              byte/s |    err% |        ins/byte |        cyc/byte |    IPC |       bra/byte |   miss% |     total | benchmark
    |--------------------:|--------------------:|--------:|----------------:|----------------:|-------:|---------------:|--------:|----------:|:----------
    |                0.04 |   28,365,698,819.44 |    0.0% |            0.34 |            0.13 |  2.714 |           0.07 |    0.0% |      5.33 | `ObfuscationBench`
    
    > C++ compiler .......................... Clang 20.1.7
    
    |             ns/byte |              byte/s |    err% |        ins/byte |        cyc/byte |    IPC |       bra/byte |   miss% |     total | benchmark
    |--------------------:|--------------------:|--------:|----------------:|----------------:|-------:|---------------:|--------:|----------:|:----------
    |                0.08 |   13,012,464,203.00 |    0.0% |            0.65 |            0.28 |  2.338 |           0.13 |    0.8% |      5.50 | `ObfuscationBench`
    
    Co-authored-by: Hodlinator <172445034+hodlinator@users.noreply.github.com>

--- src/obfuscation.h
+++ src/obfuscation.h
@@ -8,6 +8,9 @@
 #include <span.h>
 #include <tinyformat.h>
 
+#include <array>
+#include <bit>
+#include <climits>
 #include <stdexcept>
 
 class Obfuscation
@@ -15,53 +18,72 @@ class Obfuscation
 public:
     static constexpr size_t KEY_SIZE{sizeof(uint64_t)};
 
-    Obfuscation() : m_key{KEY_SIZE, std::byte{0}} {}
+    Obfuscation() { SetRotations(0); }
     explicit Obfuscation(std::span<const std::byte, KEY_SIZE> key_bytes)
     {
-        m_key = {key_bytes.begin(), key_bytes.end()};
+        SetRotations(ToUint64(key_bytes));
     }
 
-    uint64_t Key() const { return ToUint64(); }
+    uint64_t Key() const { return m_rotations[0]; }
     operator bool() const { return Key() != 0; }
 
-    void operator()(std::span<std::byte> write, size_t key_offset = 0) const
+    void operator()(std::span<std::byte> target, size_t key_offset = 0) const
     {
-        assert(m_key.size() == KEY_SIZE);
-        key_offset %= KEY_SIZE;
+        if (!*this) return;
 
-        for (size_t i = 0, j = key_offset; i != write.size(); i++) {
-            write[i] ^= m_key[j++];
-
-            // This potentially acts on very many bytes of data, so it's
-            // important that we calculate `j`, i.e. the `key` index in this
-            // way instead of doing a %, which would effectively be a division
-            // for each byte Xor'd -- much slower than need be.
-            if (j == KEY_SIZE)
-                j = 0;
+        const uint64_t rot_key{m_rotations[key_offset % KEY_SIZE]}; // Continue obfuscation from where we left off
+        for (; target.size() >= KEY_SIZE; target = target.subspan(KEY_SIZE)) { // Process multiple bytes at a time
+            Xor(target, rot_key, KEY_SIZE);
         }
+        Xor(target, rot_key, target.size());
     }
 
     template <typename Stream>
     void Serialize(Stream& s) const
     {
-        s << m_key;
+        // Use vector serialization for convenient compact size prefix.
+        std::vector<std::byte> bytes{KEY_SIZE};
+        std::memcpy(bytes.data(), &m_rotations[0], KEY_SIZE);
+        s << bytes;
     }
 
     template <typename Stream>
     void Unserialize(Stream& s)
     {
-        s >> m_key;
-        if (m_key.size() != KEY_SIZE) throw std::logic_error(strprintf("Obfuscation key size should be exactly %s bytes long", KEY_SIZE));
+        std::vector<std::byte> bytes{KEY_SIZE};
+        s >> bytes;
+        if (bytes.size() != KEY_SIZE) throw std::logic_error(strprintf("Obfuscation key size should be exactly %s bytes long", KEY_SIZE));
+        SetRotations(ToUint64(std::span<std::byte, KEY_SIZE>(bytes)));
     }
 
 private:
-    std::vector<std::byte> m_key;
+    // Cached key rotations for different offsets.
+    std::array<uint64_t, KEY_SIZE> m_rotations;
 
-    uint64_t ToUint64() const
+    void SetRotations(const uint64_t key)
+    {
+        for (size_t i{0}; i < KEY_SIZE; ++i) {
+            size_t key_rotation_bits{CHAR_BIT * i};
+            if constexpr (std::endian::native == std::endian::big) key_rotation_bits *= -1;
+            m_rotations[i] = std::rotr(key, key_rotation_bits);
+        }
+    }
+
+    static uint64_t ToUint64(const std::span<const std::byte, KEY_SIZE> key_span)
     {
         uint64_t key{};
-        std::memcpy(&key, m_key.data(), KEY_SIZE);
+        std::memcpy(&key, key_span.data(), KEY_SIZE);
         return key;
+    }
+
+    static void Xor(std::span<std::byte> target, const uint64_t key, const size_t size)
+    {
+        if (target.empty() || !size) return;
+        assert(size <= target.size());
+        uint64_t raw{};
+        std::memcpy(&raw, target.data(), size);
+        raw ^= key;
+        std::memcpy(target.data(), &raw, size);
     }
 };
 
