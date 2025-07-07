5d367db7c1d2e49d38cb76685a8658fbd2407ff7
Ava Chow <github@achow101.com>
2025-07-07 12:45:09 -0700

    wallet: Record the version of the last client to decrypt a wallet

--- src/wallet/walletdb.cpp
+++ src/wallet/walletdb.cpp
@@ -42,6 +42,7 @@ const std::string FLAGS{"flags"};
 const std::string HDCHAIN{"hdchain"};
 const std::string KEYMETA{"keymeta"};
 const std::string KEY{"key"};
+const std::string LAST_DECRYPTED_FEATURES{"lastdecryptedfeatures"};
 const std::string LAST_OPENED_FEATURES{"lastopenedfeatures"};
 const std::string LOCKED_UTXO{"lockedutxo"};
 const std::string MASTER_KEY{"mkey"};
@@ -1143,6 +1144,11 @@ DBErrors WalletBatch::LoadWallet(CWallet* pwallet)
         if (uint64_t features; m_batch->Read(DBKeys::LAST_OPENED_FEATURES, features)) {
             last_client_features = features;
         }
+
+        // Features of last client to decrypt this wallet
+        if (uint64_t last_decrypted; m_batch->Read(DBKeys::LAST_DECRYPTED_FEATURES, last_decrypted)) {
+            pwallet->SetLastDecryptedFeatures(last_decrypted);
+        }
     }
 
     try {
@@ -1293,6 +1299,11 @@ bool WalletBatch::EraseAddressData(const CTxDestination& dest)
 bool WalletBatch::WriteWalletFlags(const uint64_t flags)
 {
     return WriteIC(DBKeys::FLAGS, flags);
+}
+
+bool WalletBatch::WriteLastDecryptedFeatures()
+{
+    return WriteIC(DBKeys::LAST_DECRYPTED_FEATURES, WALLET_CLIENT_FEATURES);
 }
 
 bool WalletBatch::EraseRecords(const std::unordered_set<std::string>& types)
