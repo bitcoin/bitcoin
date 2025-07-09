fa8862723c14cdd471bbffc7a4897ece2e6d8a7f
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2025-07-09 14:25:07 +0200

    fuzz: CheckGlobals in init

--- src/test/fuzz/fuzz.cpp
+++ src/test/fuzz/fuzz.cpp
@@ -94,6 +94,7 @@ const std::function<std::string()> G_TEST_GET_FULL_NAME{[]{
 
 static void initialize()
 {
+    CheckGlobals check{};
     // By default, make the RNG deterministic with a fixed seed. This will affect all
     // randomness during the fuzz test, except:
     // - GetStrongRandBytes(), which is used for the creation of private key material.
