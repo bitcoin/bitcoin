kwvg/dash

rpc: add new key "addresses" to allow reporting multiple entries

test/functional/feature_dip3_deterministicmns.py
@@ -300,8 +300,8 @@ class DIP3Test(BitcoinTestFramework):
         for node in self.nodes:
             protx_info = node.protx('info', mn.protx_hash)
             mn_list = node.masternode('list')
-            assert_equal(protx_info['state']['service'], '127.0.0.2:%d' % mn.p2p_port)
-            assert_equal(mn_list['%s-%d' % (mn.collateral_txid, mn.collateral_vout)]['address'], '127.0.0.2:%d' % mn.p2p_port)
+            assert_equal(protx_info['state']['addresses'][0], '127.0.0.2:%d' % mn.p2p_port)
+            assert_equal(mn_list['%s-%d' % (mn.collateral_txid, mn.collateral_vout)]['addresses'][0], '127.0.0.2:%d' % mn.p2p_port)
 
         # undo
         self.nodes[0].protx('update_service', mn.protx_hash, '127.0.0.1:%d' % mn.p2p_port, mn.blsMnkey, "", mn.fundsAddr)

bitcoin/bitcoin

test/functional/feature_dip3_deterministicmns.py
@@ -277,8 +277,8 @@ class DIP3Test(BitcoinTestFramework):
         for node in self.nodes:
             protx_info = node.protx('info', mn.proTxHash)
             mn_list = node.masternode('list')
-            assert_equal(protx_info['state']['service'], '127.0.0.2:%d' % mn.nodePort)
-            assert_equal(mn_list['%s-%d' % (mn.collateral_txid, mn.collateral_vout)]['address'], '127.0.0.2:%d' % mn.nodePort)
+            assert_equal(protx_info['state']['addresses'][0], '127.0.0.2:%d' % mn.nodePort)
+            assert_equal(mn_list['%s-%d' % (mn.collateral_txid, mn.collateral_vout)]['addresses'][0], '127.0.0.2:%d' % mn.nodePort)
 
         # undo
         mn.update_service(self.nodes[0], submit=True)