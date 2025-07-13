7f2e35a0d25fb69ca8cb3a9e31426e772822b0a8
robinlinus <RobinLinus@users.noreply.github.com>
2025-07-13 18:13:08 +0200

    Reduce minrelaytxfee to 100 sats/kvB
    test: Update p2p_ibd_tx_relay.py to support minrelaytxfee as low as 100 sats/kvB.

--- src/policy/policy.h
+++ src/policy/policy.h
@@ -61,7 +61,7 @@ static constexpr unsigned int MAX_STANDARD_SCRIPTSIG_SIZE{1650};
  * outputs below the new threshold */
 static constexpr unsigned int DUST_RELAY_TX_FEE{3000};
 /** Default for -minrelaytxfee, minimum relay fee for transactions */
-static constexpr unsigned int DEFAULT_MIN_RELAY_TX_FEE{1000};
+static constexpr unsigned int DEFAULT_MIN_RELAY_TX_FEE{100};
 /** Default for -limitancestorcount, max number of in-mempool ancestors */
 static constexpr unsigned int DEFAULT_ANCESTOR_LIMIT{25};
 /** Default for -limitancestorsize, maximum kilobytes of tx + all in-mempool ancestors */
