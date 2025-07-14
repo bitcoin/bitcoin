dead0ec246ff430b32e90f907a492467c9cdb305
Ava Chow <github@achow101.com>
2025-07-14 14:35:39 -0700

    sqlite: Refactor ReadPragmaInteger to use SQLiteStatement

--- src/wallet/sqlite.cpp
+++ src/wallet/sqlite.cpp
@@ -142,21 +142,13 @@ public:
 static std::optional<int> ReadPragmaInteger(sqlite3* db, const std::string& key, const std::string& description, bilingual_str& error)
 {
     std::string stmt_text = strprintf("PRAGMA %s", key);
-    sqlite3_stmt* pragma_read_stmt{nullptr};
-    int ret = sqlite3_prepare_v2(db, stmt_text.c_str(), -1, &pragma_read_stmt, nullptr);
-    if (ret != SQLITE_OK) {
-        sqlite3_finalize(pragma_read_stmt);
-        error = Untranslated(strprintf("SQLiteDatabase: Failed to prepare the statement to fetch %s: %s", description, sqlite3_errstr(ret)));
-        return std::nullopt;
-    }
-    ret = sqlite3_step(pragma_read_stmt);
+    SQLiteStatement pragma_read_stmt(db, stmt_text);
+    int ret = pragma_read_stmt.Step();
     if (ret != SQLITE_ROW) {
-        sqlite3_finalize(pragma_read_stmt);
         error = Untranslated(strprintf("SQLiteDatabase: Failed to fetch %s: %s", description, sqlite3_errstr(ret)));
         return std::nullopt;
     }
-    int result = sqlite3_column_int(pragma_read_stmt, 0);
-    sqlite3_finalize(pragma_read_stmt);
+    int result = pragma_read_stmt.Column<int>(0);
     return result;
 }
 
