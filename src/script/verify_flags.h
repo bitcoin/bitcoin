b96c1b9be1e72722b9b81fa6346ad46d3de3244e
Anthony Towns <aj@erisian.com.au>
2025-07-16 16:02:52 +1000

    script/verify_flags: extend script_verify_flags to 64 bits

--- src/script/verify_flags.h
+++ src/script/verify_flags.h
@@ -14,7 +14,7 @@ enum class script_verify_flag_name : uint8_t;
 class script_verify_flags
 {
 public:
-    using value_type = uint32_t;
+    using value_type = uint64_t;
 
     consteval script_verify_flags() = default;
 
