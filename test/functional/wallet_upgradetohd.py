0267fb72d1d2253ac8cb2ffc62d222169a4ec8ce
Konstantin Akimov <knstqq@gmail.com>
2025-07-17 15:30:31 +0700

    fix: copyright for wallet_upgradetohd.py

--- test/functional/wallet_upgradetohd.py
+++ test/functional/wallet_upgradetohd.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (c) 2016 The Bitcoin Core developers
+# Copyright (c) 2016-2025 The Dash Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 """
