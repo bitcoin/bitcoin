94b39ce73831acc4c94c7f0d1347d5991b27ef0b
marcofleon <marleo23@proton.me>
2025-07-18 21:11:21 +0100

    refactor: Change `m_tx_inventory_to_send` from `std::set<GenTxid>` to `std::set<Wtxid>`
    Simplifies `m_tx_inventory_to_send` a bit by making it a set of Wtxids.
    Wtxid relay is prevalent throughout the network, so the complexity of
    dealing with a GenTxid in this data structure isn't necessary.
    
    For peers that aren't wtxid relay, the txid is now retrieved from our
    mempool entry when the inv is constructed.

--- src/txmempool.cpp
+++ src/txmempool.cpp
@@ -794,7 +794,7 @@ void CTxMemPool::check(const CCoinsViewCache& active_coins_tip, int64_t spendhei
     assert(innerUsage == cachedInnerUsage);
 }
 
-bool CTxMemPool::CompareDepthAndScore(const GenTxid& hasha, const GenTxid& hashb) const
+bool CTxMemPool::CompareDepthAndScore(const Wtxid& hasha, const Wtxid& hashb) const
 {
     /* Return `true` if hasha should be considered sooner than hashb. Namely when:
      *   a is not in the mempool, but b is
@@ -802,9 +802,9 @@ bool CTxMemPool::CompareDepthAndScore(const GenTxid& hasha, const GenTxid& hashb
      *   both are in the mempool and a has a higher score than b
      */
     LOCK(cs);
-    auto j{std::visit([&](const auto& id) EXCLUSIVE_LOCKS_REQUIRED(cs) { return GetIter(id); }, hashb)};
+    auto j{GetIter(hashb)};
     if (!j.has_value()) return false;
-    auto i{std::visit([&](const auto& id) EXCLUSIVE_LOCKS_REQUIRED(cs) { return GetIter(id); }, hasha)};
+    auto i{GetIter(hasha)};
     if (!i.has_value()) return true;
     uint64_t counta = i.value()->GetCountWithAncestors();
     uint64_t countb = j.value()->GetCountWithAncestors();
