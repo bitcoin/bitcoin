4f651d60395f945420ae073f8de415664bf1cdf4
Novo <eunovo9@gmail.com>
2025-07-20 05:35:09 +0100

    descriptor: implement sp(scan, spend) desc
    sp() expects a scan private key and a spend key which could be a private key
    or a public key.
    The scan key is really a "scan entropy" and will not be treated as an actual
    private key.

--- src/script/descriptor.cpp
+++ src/script/descriptor.cpp
@@ -1653,6 +1653,59 @@ public:
     }
 };
 
+/** A parsed sp(...) descriptor */
+class SpDescriptor final : public DescriptorImpl
+{
+protected:
+    std::vector<CScript> MakeScripts(const std::vector<CPubKey>&, std::span<const CScript>, FlatSigningProvider&) const override { return std::vector<CScript>(); }
+
+public:
+    SpDescriptor(std::unique_ptr<PubkeyProvider> scan_pubkey, std::unique_ptr<PubkeyProvider> spend_key) : DescriptorImpl(Vector(std::move(scan_pubkey), std::move(spend_key)), "sp") {};
+
+    std::optional<OutputType> GetOutputType() const override { return OutputType::SILENT_PAYMENTS; }
+
+    bool IsSingleType() const final { return true; }
+
+    bool IsSolvable() const override { return false; }
+
+    bool ToStringHelper(const SigningProvider* arg, std::string& out, const StringType type, const DescriptorCache* cache = nullptr) const override
+    {
+        FlatSigningProvider dummy;
+        std::string scan_key;
+        // This should never fail, since the ScankeyPubkeyProvider will always contain
+        // the scan key.
+
+        // The scan key is stored at m_pubkey_args[0], the spend key at m_pubkey_args[1]
+        bool has_scan_key{m_pubkey_args.at(0)->ToPrivateString(dummy, scan_key)};
+        assert(has_scan_key);
+
+        std::string scan_key_str{m_name + "(" + scan_key + ","};
+        auto& spend_pubkey{m_pubkey_args.at(1)};
+        std::string spend_key_str;
+        switch (type) {
+        case StringType::NORMALIZED:
+            if (!spend_pubkey->ToNormalizedString(*arg, spend_key_str, cache)) return false;
+            break;
+        case StringType::PRIVATE:
+            if (!spend_pubkey->ToPrivateString(*arg, spend_key_str)) return false;
+            break;
+        case StringType::PUBLIC:
+            spend_key_str = spend_pubkey->ToString();
+            break;
+        case StringType::COMPAT:
+            spend_key_str= spend_pubkey->ToString(PubkeyProvider::StringType::COMPAT);
+            break;
+        }
+        out = std::move(scan_key_str) + std::move(spend_key_str) + ")";
+        return true;
+    }
+
+    std::unique_ptr<DescriptorImpl> Clone() const override
+    {
+        return std::make_unique<SpDescriptor>(m_pubkey_args.at(0)->Clone(), m_pubkey_args.at(1)->Clone());
+    }
+};
+
 ////////////////////////////////////////////////////////////////////////////
 // Parser                                                                 //
 ////////////////////////////////////////////////////////////////////////////
@@ -1664,6 +1717,7 @@ enum class ParseScriptContext {
     P2WSH,   //!< Inside wsh() (script becomes v0 witness script)
     P2TR,    //!< Inside tr() (either internal key, or BIP342 script leaf)
     MUSIG,   //!< Inside musig() (implies P2TR, cannot have nested musig())
+    SP,      //!< Inside sp()
 };
 
 std::optional<uint32_t> ParseKeyPathNum(std::span<const char> elem, bool& apostrophe, std::string& error, bool& has_hardened)
@@ -1792,6 +1846,7 @@ std::vector<std::unique_ptr<PubkeyProvider>> ParsePubkeyInner(uint32_t key_exp_i
 {
     std::vector<std::unique_ptr<PubkeyProvider>> ret;
     bool permit_uncompressed = ctx == ParseScriptContext::TOP || ctx == ParseScriptContext::P2SH;
+    bool is_scan_key{ctx == ParseScriptContext::SP && key_exp_index == 0}; // The first key under sp() is the scan key
     auto split = Split(sp, '/');
     std::string str(split[0].begin(), split[0].end());
     if (str.size() == 0) {
@@ -1803,7 +1858,7 @@ std::vector<std::unique_ptr<PubkeyProvider>> ParsePubkeyInner(uint32_t key_exp_i
         return {};
     }
     if (split.size() == 1) {
-        if (IsHex(str)) {
+        if (IsHex(str) && !is_scan_key) { // Scan key cannot be a public key
             std::vector<unsigned char> data = ParseHex(str);
             CPubKey pubkey(data);
             if (pubkey.IsValid() && !pubkey.IsValidNonHybrid()) {
@@ -1834,8 +1889,15 @@ std::vector<std::unique_ptr<PubkeyProvider>> ParsePubkeyInner(uint32_t key_exp_i
         if (key.IsValid()) {
             if (permit_uncompressed || key.IsCompressed()) {
                 CPubKey pubkey = key.GetPubKey();
+                auto provider{std::make_unique<ConstPubkeyProvider>(key_exp_index, pubkey, ctx == ParseScriptContext::P2TR)};
+
+                if (is_scan_key) {
+                    ret.emplace_back(std::make_unique<ScankeyPubkeyProvider>(key_exp_index, key, std::move(provider)));
+                    return ret;
+                }
+
                 out.keys.emplace(pubkey.GetID(), key);
-                ret.emplace_back(std::make_unique<ConstPubkeyProvider>(key_exp_index, pubkey, ctx == ParseScriptContext::P2TR));
+                ret.push_back(std::move(provider));
                 return ret;
             } else {
                 error = "Uncompressed keys are not allowed";
@@ -1852,6 +1914,32 @@ std::vector<std::unique_ptr<PubkeyProvider>> ParsePubkeyInner(uint32_t key_exp_i
     std::vector<KeyPath> paths;
     DeriveType type = ParseDeriveType(split, apostrophe);
     if (!ParseKeyPath(split, paths, apostrophe, error, /*allow_multipath=*/true)) return {};
+    if (is_scan_key && extkey.key.IsValid()) {
+        for (const auto& path : paths) {
+            KeyOriginInfo origin;
+            CKeyID id{extkey.key.GetPubKey().GetID()};
+            std::copy(id.begin(), id.begin() + 4, origin.fingerprint);
+            auto tmp_extkey{extkey};
+
+            for (auto entry : path) {
+                origin.path.push_back(entry);
+                if (!tmp_extkey.Derive(tmp_extkey, entry)) {
+                    error = strprintf("key '%s' is not valid", str);
+                    return {};
+                }
+            }
+
+            auto tmp_extpubkey{tmp_extkey.Neuter()};
+            auto pubkey{std::make_unique<ConstPubkeyProvider>(key_exp_index, tmp_extpubkey.pubkey, false)};
+            auto origin_pubkey{std::make_unique<OriginPubkeyProvider>(key_exp_index, origin, std::move(pubkey), apostrophe)};
+            ret.emplace_back(std::make_unique<ScankeyPubkeyProvider>(key_exp_index, tmp_extkey.key, std::move(origin_pubkey)));
+        }
+        return ret;
+    } else if (is_scan_key) {
+        error = strprintf("key '%s' is not valid", str);
+        return {};
+    }
+
     if (extkey.key.IsValid()) {
         extpubkey = extkey.Neuter();
         out.keys.emplace(extpubkey.pubkey.GetID(), extkey.key);
@@ -2199,6 +2287,36 @@ std::vector<std::unique_ptr<DescriptorImpl>> ParseScript(uint32_t& key_exp_index
             ret.emplace_back(std::make_unique<PKDescriptor>(std::move(pubkey), ctx == ParseScriptContext::P2TR));
         }
         return ret;
+    }
+    if (ctx == ParseScriptContext::TOP && Func("sp", expr)) {
+        auto scan_pubkeys{ParsePubkey(key_exp_index, Expr(expr), ParseScriptContext::SP, out, error)};
+        if (scan_pubkeys.empty()) {
+            error = strprintf("sp(): %s", error);
+            return {};
+        }
+        if (!Const(",", expr)) {
+            error = strprintf("sp(): expected ',', got '%c'", expr.size() > 0 ? expr[0] : ')');
+            return {};
+        }
+        ++key_exp_index;
+
+        auto spend_pubkeys{ParsePubkey(key_exp_index, Expr(expr), ParseScriptContext::SP, out, error)};
+        if (spend_pubkeys.empty()) {
+            error = strprintf("sp(): %s", error);
+            return {};
+        }
+
+        for (auto& scan_pubkey : scan_pubkeys) {
+            for (auto& spend_pubkey : spend_pubkeys) {
+                ret.emplace_back(std::make_unique<SpDescriptor>(std::move(scan_pubkey), std::move(spend_pubkey)));
+            }
+        }
+
+        ++key_exp_index;
+        return ret;
+    } else if (Func("sp", expr)) {
+        error = "Can only have sp() at top level";
+        return {};
     }
     if ((ctx == ParseScriptContext::TOP || ctx == ParseScriptContext::P2SH || ctx == ParseScriptContext::P2WSH) && Func("pkh", expr)) {
         auto pubkeys = ParsePubkey(key_exp_index, expr, ctx, out, error);
