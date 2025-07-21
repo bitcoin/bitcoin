65ac7f6d4d1e26b7c065ed3dc0a17db822b08d71
Lőrinc <pap.lorinc@gmail.com>
2025-07-21 10:54:33 -0700

    refactor: modernize `CScriptBase` definition

--- src/script/script.h
+++ src/script/script.h
@@ -406,7 +406,7 @@ private:
  * Tests in October 2015 showed use of this reduced dbcache memory usage by 23%
  *  and made an initial sync 13% faster.
  */
-typedef prevector<28, unsigned char> CScriptBase;
+using CScriptBase = prevector<28, uint8_t>;
 
 bool GetScriptOp(CScriptBase::const_iterator& pc, CScriptBase::const_iterator end, opcodetype& opcodeRet, std::vector<unsigned char>* pvchRet);
 
