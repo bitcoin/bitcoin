fb5ee445b758820055513382d2ed3418061e3e35
Ava Chow <github@achow101.com>
2025-07-24 11:31:47 -0700

    wallet: Make Mockable{Database,Batch} subclasses of SQLite classes
    The mocking functionality of MockableDatabase, MockableBatch, and
    MockableCursor was not really being used. These are changed to be
    subclasses of their respective SQLite* classes and will use in-memory
    SQLite databases so that the tests are more representative of actual
    database behavior.
    
    MockableCursor is removed as there are no overrides needed in
    SQLiteCursor for the tests.

--- src/wallet/test/util.h
+++ src/wallet/test/util.h
@@ -8,6 +8,7 @@
 #include <addresstype.h>
 #include <wallet/db.h>
 #include <wallet/scriptpubkeyman.h>
+#include <wallet/sqlite.h>
 
 #include <memory>
 
@@ -46,76 +47,31 @@ CTxDestination getNewDestination(CWallet& w, OutputType output_type);
 
 using MockableData = std::map<SerializeData, SerializeData, std::less<>>;
 
-class MockableCursor: public DatabaseCursor
+
+class MockableSQLiteBatch : public SQLiteBatch
 {
 public:
-    MockableData::const_iterator m_cursor;
-    MockableData::const_iterator m_cursor_end;
-    bool m_pass;
-
-    explicit MockableCursor(const MockableData& records, bool pass) : m_cursor(records.begin()), m_cursor_end(records.end()), m_pass(pass) {}
-    MockableCursor(const MockableData& records, bool pass, std::span<const std::byte> prefix);
-    ~MockableCursor() = default;
-
-    Status Next(DataStream& key, DataStream& value) override;
-};
-
-class MockableBatch : public DatabaseBatch
-{
-private:
-    MockableData& m_records;
-    bool m_pass;
-
-public:
-    explicit MockableBatch(MockableData& records, bool pass) : m_records(records), m_pass(pass) {}
-    ~MockableBatch() = default;
-
-    void Close() override {}
-
-    bool ReadKey(DataStream&& key, DataStream& value) override;
-    bool WriteKey(DataStream&& key, DataStream&& value, bool overwrite=true) override;
-    bool EraseKey(DataStream&& key) override;
-    bool HasKey(DataStream&& key) override;
-    bool ErasePrefix(std::span<const std::byte> prefix) override;
-
-    std::unique_ptr<DatabaseCursor> GetNewCursor() override
-    {
-        return std::make_unique<MockableCursor>(m_records, m_pass);
-    }
-    std::unique_ptr<DatabaseCursor> GetNewPrefixCursor(std::span<const std::byte> prefix) override {
-        return std::make_unique<MockableCursor>(m_records, m_pass, prefix);
-    }
-    bool TxnBegin() override { return m_pass; }
-    bool TxnCommit() override { return m_pass; }
-    bool TxnAbort() override { return m_pass; }
-    bool HasActiveTxn() override { return false; }
+    using SQLiteBatch::SQLiteBatch;
+    using SQLiteBatch::WriteKey;
 };
 
 /** A WalletDatabase whose contents and return values can be modified as needed for testing
  **/
-class MockableDatabase : public WalletDatabase
+class MockableSQLiteDatabase : public SQLiteDatabase
 {
 public:
-    MockableData m_records;
-    bool m_pass{true};
+    MockableSQLiteDatabase();
 
-    MockableDatabase() : WalletDatabase() {}
-    ~MockableDatabase() = default;
-
-    void Open() override {}
-
-    bool Rewrite() override { return m_pass; }
-    bool Backup(const std::string& strDest) const override { return m_pass; }
-    void Close() override {}
+    bool Backup(const std::string& strDest) const override { return true; }
 
     std::string Filename() override { return "mockable"; }
     std::vector<fs::path> Files() override { return {}; }
-    std::string Format() override { return "mock"; }
-    std::unique_ptr<DatabaseBatch> MakeBatch() override { return std::make_unique<MockableBatch>(m_records, m_pass); }
+    std::string Format() override { return "sqlite-mock"; }
+    std::unique_ptr<DatabaseBatch> MakeBatch() override { return std::make_unique<MockableSQLiteBatch>(*this); }
 };
 
 std::unique_ptr<WalletDatabase> CreateMockableWalletDatabase();
-MockableDatabase& GetMockableDatabase(CWallet& wallet);
+MockableSQLiteDatabase& GetMockableDatabase(CWallet& wallet);
 
 DescriptorScriptPubKeyMan* CreateDescriptor(CWallet& keystore, const std::string& desc_str, const bool success);
 } // namespace wallet
