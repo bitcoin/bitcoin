d6f4d3ed475cedabf27bfb12fa47982d8463bb60
Anthony Towns <aj@erisian.com.au>
2025-08-01 18:40:17 +1000

    node/miner: Log via MINER debug category
    This removes "CreateNewBlock" messages from logs by default. Also changes
    "-printpriority" argument to automatically enable -debug=miner logging.

--- src/logging.cpp
+++ src/logging.cpp
@@ -201,6 +201,7 @@ static const std::map<std::string, BCLog::LogFlags, std::less<>> LOG_CATEGORIES_
     {"txreconciliation", BCLog::TXRECONCILIATION},
     {"scan", BCLog::SCAN},
     {"txpackages", BCLog::TXPACKAGES},
+    {"miner", BCLog::MINER},
 };
 
 static const std::unordered_map<BCLog::LogFlags, std::string> LOG_CATEGORIES_BY_FLAG{
