a20724d926d5844168c6a13fa8293df8c8927efe
marcofleon <marleo23@proton.me>
2025-08-01 13:29:27 +0100

    refactor: Move `transaction_identifier.h` to primitives
    Moves the file from `src/util` to `src/primitives`. Now that the
    refactor is complete, Txid and Wtxid are fundamental types, so it
    makes sense for them to reside in `src/primitives`.

--- src/primitives/transaction_identifier.h
+++ src/primitives/transaction_identifier.h
@@ -2,8 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or https://opensource.org/license/mit.
 
-#ifndef BITCOIN_UTIL_TRANSACTION_IDENTIFIER_H
-#define BITCOIN_UTIL_TRANSACTION_IDENTIFIER_H
+#ifndef BITCOIN_PRIMITIVES_TRANSACTION_IDENTIFIER_H
+#define BITCOIN_PRIMITIVES_TRANSACTION_IDENTIFIER_H
 
 #include <attributes.h>
 #include <uint256.h>
@@ -91,4 +91,4 @@ public:
     }
 };
 
-#endif // BITCOIN_UTIL_TRANSACTION_IDENTIFIER_H
+#endif // BITCOIN_PRIMITIVES_TRANSACTION_IDENTIFIER_H
