155ee86ebe9a1b43f5f926510d84d37d7f889760
vanity <maelokvevos@gmail.com>
2025-08-04 23:20:27 +0100

    Apply all recent BitGold edits (block limits, MAX_MONEY, maturity)

--- src/consensus/amount.h
+++ src/consensus/amount.h
@@ -23,7 +23,7 @@ static constexpr CAmount COIN = 100000000;
  * critical; in unusual circumstances like a(nother) overflow bug that allowed
  * for the creation of coins out of thin air modification could lead to a fork.
  * */
-static constexpr CAmount MAX_MONEY = 21000000 * COIN;
+static constexpr CAmount MAX_MONEY = 8000000 * COIN;
 inline bool MoneyRange(const CAmount& nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 
 #endif // BITCOIN_CONSENSUS_AMOUNT_H
