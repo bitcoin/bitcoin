db7f83a71e41f217bfea0bf719ea144dfb7e12f6
Pieter Wuille <pieter@wuille.net>
2025-08-04 17:18:09 -0400

    txgraph: avoid holes in DepGraph positions (mem optimization)

--- src/txgraph.cpp
+++ src/txgraph.cpp
@@ -1001,8 +1001,10 @@ bool Cluster::Split(TxGraphImpl& graph, int level) noexcept
     while (todo.Any()) {
         auto component = m_depgraph.FindConnectedComponent(todo);
         auto split_quality = component.Count() <= 2 ? QualityLevel::OPTIMAL : new_quality;
-        if (first && component == todo) {
-            // The existing Cluster is an entire component. Leave it be, but update its quality.
+        if (first && component == todo && SetType::Fill(component.Count()) == component) {
+            // The existing Cluster is an entire component, without holes. Leave it be, but update
+            // its quality. If there are holes, we continue, so that the Cluster is reconstructed
+            // without holes, reducing memory usage.
             Assume(todo == m_depgraph.Positions());
             graph.SetClusterQuality(level, m_quality, m_setindex, split_quality);
             // If this made the quality ACCEPTABLE or OPTIMAL, we need to compute and cache its
@@ -2292,6 +2294,10 @@ void Cluster::SanityCheck(const TxGraphImpl& graph, int level) const
     // exceed this limit are marked oversized, which means they are never applied.
     assert(m_quality == QualityLevel::OVERSIZED_SINGLETON || GetTotalTxSize() <= graph.m_max_cluster_size);
     // m_quality and m_setindex are checked in TxGraphImpl::SanityCheck.
+    // Unless a split is to be applied, the cluster cannot have any holes.
+    if (!NeedsSplitting()) {
+        assert(m_depgraph.Positions() == SetType::Fill(m_depgraph.TxCount()));
+    }
 
     // OVERSIZED clusters are singletons.
     assert(m_quality != QualityLevel::OVERSIZED_SINGLETON || m_linearization.size() == 1);
