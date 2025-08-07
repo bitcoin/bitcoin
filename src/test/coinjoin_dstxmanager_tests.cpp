4b00a0edfa33dbb223206d7c4dc52a0b6487c1a2
pasta <pasta@dashboost.org>
2025-08-07 17:14:08 -0500

    test: add CoinJoin DSTX and inouts tests
    This commit introduces two new test files: `coinjoin_dstxmanager_tests.cpp` and `coinjoin_inouts_tests.cpp`. The `coinjoin_dstxmanager_tests` file includes tests for managing CoinJoin broadcast transactions, while the `coinjoin_inouts_tests` file focuses on validating the structure and behavior of CoinJoin transactions. Both files are added to the test suite in the Makefile for comprehensive testing coverage.

---
+++ src/test/coinjoin_dstxmanager_tests.cpp
@@ -0,0 +1,73 @@
+// Copyright (c) 2025 The Dash Core developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <test/util/setup_common.h>
+
+#include <array>
+#include <algorithm>
+#include <vector>
+
+#include <coinjoin/coinjoin.h>
+#include <coinjoin/common.h>
+#include <coinjoin/context.h>
+#include <chain.h>
+#include <script/script.h>
+#include <uint256.h>
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_FIXTURE_TEST_SUITE(coinjoin_dstxmanager_tests, TestingSetup)
+
+static CCoinJoinBroadcastTx MakeDSTX(int vin_vout_count = 3)
+{
+    CCoinJoinBroadcastTx dstx;
+    CMutableTransaction mtx;
+    const int count = std::max(vin_vout_count, 1);
+    for (int i = 0; i < count; ++i) {
+        mtx.vin.emplace_back(COutPoint(uint256S("0a"), i));
+        // Use denominated P2PKH outputs
+    std::array<unsigned char, 20> h{}; h.fill(static_cast<unsigned char>(i));
+    CScript spk; spk << OP_DUP << OP_HASH160 << std::vector<unsigned char>(h.begin(), h.end()) << OP_EQUALVERIFY << OP_CHECKSIG;
+        mtx.vout.emplace_back(CoinJoin::GetSmallestDenomination(), spk);
+    }
+    dstx.tx = MakeTransactionRef(mtx);
+    dstx.m_protxHash = uint256::ONE;
+    return dstx;
+}
+
+BOOST_AUTO_TEST_CASE(add_get_dstx)
+{
+    CCoinJoinBroadcastTx dstx = MakeDSTX();
+    auto& man = *Assert(Assert(m_node.cj_ctx)->dstxman);
+    // Not present initially
+    BOOST_CHECK(!man.GetDSTX(dstx.tx->GetHash()));
+    // Add
+    man.AddDSTX(dstx);
+    // Fetch back
+    auto got = man.GetDSTX(dstx.tx->GetHash());
+    BOOST_CHECK(static_cast<bool>(got));
+    BOOST_CHECK_EQUAL(got.tx->GetHash().ToString(), dstx.tx->GetHash().ToString());
+}
+
+BOOST_AUTO_TEST_CASE(update_heights_block_connect_disconnect)
+{
+    CCoinJoinBroadcastTx dstx = MakeDSTX();
+    auto& man = *Assert(Assert(m_node.cj_ctx)->dstxman);
+    man.AddDSTX(dstx);
+
+    // Create a fake block containing the tx
+    auto block = std::make_shared<CBlock>();
+    block->vtx.push_back(dstx.tx);
+    CBlockIndex index; index.nHeight = 100; uint256 bh = uint256S("0b"); index.phashBlock = &bh;
+
+    // Height should set to 100 on connect
+    man.BlockConnected(block, &index);
+
+    // Height should clear on disconnect
+    man.BlockDisconnected(block, nullptr);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
