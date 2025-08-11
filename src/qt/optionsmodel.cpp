cd2191797c4935bfb33090b5dcc82d8828ae064d
Luke Dashjr <luke-jr+git@utopios.org>
2025-08-11 06:28:34 +0000

    Bugfix: GUI/OptionsModel: Add missing locks for reducing mempool size/expiry

--- src/qt/optionsmodel.cpp
+++ src/qt/optionsmodel.cpp
@@ -1176,6 +1176,7 @@ bool OptionsModel::setOption(OptionID option, const QVariant& value, const std::
                 auto node_ctx = node().context();
                 assert(node_ctx && node_ctx->mempool && node_ctx->chainman);
                 auto& active_chainstate = node_ctx->chainman->ActiveChainstate();
+                LOCK(node_ctx->mempool->cs);
                 LimitMempoolSize(*node_ctx->mempool, active_chainstate.CoinsTip());
             }
         }
@@ -1202,6 +1203,7 @@ bool OptionsModel::setOption(OptionID option, const QVariant& value, const std::
                 auto node_ctx = node().context();
                 assert(node_ctx && node_ctx->mempool && node_ctx->chainman);
                 auto& active_chainstate = node_ctx->chainman->ActiveChainstate();
+                LOCK(node_ctx->mempool->cs);
                 LimitMempoolSize(*node_ctx->mempool, active_chainstate.CoinsTip());
             }
         }
