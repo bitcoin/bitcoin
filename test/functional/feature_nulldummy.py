c24c9ea37522103bc5d4146cc1257ff70c6f5ade
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2025-08-17 22:33:00 +0000

    merge bitcoin#25364: remove wallet dependency from feature_nulldummy.py

--- test/functional/feature_nulldummy.py
+++ test/functional/feature_nulldummy.py
@@ -17,9 +17,11 @@ from test_framework.blocktools import (
     COINBASE_MATURITY,
     NORMAL_GBT_REQUEST_PARAMS,
     create_block,
-    create_transaction,
 )
-from test_framework.messages import CTransaction
+from test_framework.messages import (
+    CTransaction,
+    tx_from_hex,
+)
 from test_framework.script import (
     OP_0,
     OP_TRUE,
@@ -29,6 +31,9 @@ from test_framework.util import (
     assert_equal,
     assert_raises_rpc_error,
 )
+from test_framework.wallet import getnewdestination
+from test_framework.key import ECKey
+from test_framework.wallet_util import bytes_to_wif
 
 NULLDUMMY_ERROR = "non-mandatory-script-verify-flag (Dummy CHECKMULTISIG argument must be zero)"
 
@@ -52,19 +57,24 @@ class NULLDUMMYTest(BitcoinTestFramework):
             '-par=1',  # Use only one script thread to get the exact reject reason for testing
         ]]
 
-    def skip_test_if_missing_module(self):
-        self.skip_if_no_wallet()
+    def create_transaction(self, *, txid, input_details=None, addr, amount, privkey):
+        input = {"txid": txid, "vout": 0}
+        output = {addr: amount}
+        rawtx = self.nodes[0].createrawtransaction([input], output)
+        # Details only needed for scripthash or witness spends
+        input = None if not input_details else [{**input, **input_details}]
+        signedtx = self.nodes[0].signrawtransactionwithkey(rawtx, [privkey], input)
+        return tx_from_hex(signedtx["hex"])
 
     def run_test(self):
-        self.nodes[0].createwallet(wallet_name='wmulti', disable_private_keys=True)
-        wmulti = self.nodes[0].get_wallet_rpc('wmulti')
-        w0 = self.nodes[0].get_wallet_rpc(self.default_wallet_name)
-        self.address = w0.getnewaddress()
-        self.pubkey = w0.getaddressinfo(self.address)['pubkey']
-        self.ms_address = wmulti.addmultisigaddress(1, [self.pubkey])['address']
-        if not self.options.descriptors:
-            # Legacy wallets need to import these so that they are watched by the wallet. This is unnecessary (and does not need to be tested) for descriptor wallets
-            wmulti.importaddress(self.ms_address)
+        eckey = ECKey()
+        eckey.generate()
+        self.privkey = bytes_to_wif(eckey.get_bytes())
+        self.pubkey = eckey.get_pubkey().get_bytes().hex()
+        cms = self.nodes[0].createmultisig(1, [self.pubkey])
+        self.ms_address = cms["address"]
+        ms_unlock_details = {"scriptPubKey": self.nodes[0].validateaddress(self.ms_address)["scriptPubKey"],
+                             "redeemScript": cms["redeemScript"]}
 
         self.coinbase_blocks = self.generate(self.nodes[0], 2)  # block height = 2
         coinbase_txid = []
@@ -76,14 +86,19 @@ class NULLDUMMYTest(BitcoinTestFramework):
         self.lastblocktime = self.mocktime + self.lastblockheight
 
         self.log.info(f"Test 1: NULLDUMMY compliant base transactions should be accepted to mempool and mined before activation [{COINBASE_MATURITY + 3}]")
-        test1txs = [create_transaction(self.nodes[0], coinbase_txid[0], self.ms_address, amount=49)]
+        test1txs = [self.create_transaction(txid=coinbase_txid[0], addr=self.ms_address, amount=49,
+                                            privkey=self.nodes[0].get_deterministic_priv_key().key)]
         txid1 = self.nodes[0].sendrawtransaction(test1txs[0].serialize().hex(), 0)
-        test1txs.append(create_transaction(self.nodes[0], txid1, self.ms_address, amount=48))
+        test1txs.append(self.create_transaction(txid=txid1, input_details=ms_unlock_details,
+                                                addr=self.ms_address, amount=48,
+                                                privkey=self.privkey))
         txid2 = self.nodes[0].sendrawtransaction(test1txs[1].serialize().hex(), 0)
         self.block_submit(self.nodes[0], test1txs, accept=True)
 
         self.log.info("Test 2: Non-NULLDUMMY base multisig transaction should not be accepted to mempool before activation")
-        test2tx = create_transaction(self.nodes[0], txid2, self.ms_address, amount=47)
+        test2tx = self.create_transaction(txid=txid2, input_details=ms_unlock_details,
+                                          addr=self.ms_address, amount=47,
+                                          privkey=self.privkey)
         invalidate_nulldummy_tx(test2tx)
         assert_raises_rpc_error(-26, NULLDUMMY_ERROR, self.nodes[0].sendrawtransaction, test2tx.serialize().hex(), 0)
 
@@ -91,8 +106,10 @@ class NULLDUMMYTest(BitcoinTestFramework):
         self.block_submit(self.nodes[0], [test2tx], accept=True)
 
         self.log.info("Test 4: Non-NULLDUMMY base multisig transaction is invalid after activation")
-        test4tx = create_transaction(self.nodes[0], test2tx.hash, self.address, amount=46)
-        test6txs=[CTransaction(test4tx)]
+        test4tx = self.create_transaction(txid=test2tx.hash, input_details=ms_unlock_details,
+                                          addr=getnewdestination()[2], amount=46,
+                                          privkey=self.privkey)
+        test6txs = [CTransaction(test4tx)]
         invalidate_nulldummy_tx(test4tx)
         assert_raises_rpc_error(-26, NULLDUMMY_ERROR, self.nodes[0].sendrawtransaction, test4tx.serialize().hex(), 0)
         self.block_submit(self.nodes[0], [test4tx], accept=False)
