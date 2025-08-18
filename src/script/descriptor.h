f1cc37f6ff9c036d82382e45cd5d9aaa461e273b
Novo <eunovo9@gmail.com>
2025-08-18 17:16:15 +0200

    descriptor: add SPDescriptor public interface
    Create an SPDescriptor interface that will be used by the
    `SilentPaymentDescriptorScriptPubKeyMan`.
    
    The interface only needs to provide a way to get the scan key,
    the spend key can be obtained through the normal Descriptor methods.
    
    While the scan key is represented as a `CKey`, it will not be used
    in the signing process. This creates an argument against putting this
    in a signing provider.

--- src/script/descriptor.h
+++ src/script/descriptor.h
@@ -183,6 +183,15 @@ struct Descriptor {
     virtual std::vector<std::string> Warnings() const = 0;
 };
 
+/** Interface for parsed Silent Payments Descriptor objects.
+ *
+ */
+struct SpDescriptor {
+    /** Return the scankey for this descriptor.
+     */
+    virtual CKey GetScanKey() const = 0;
+};
+
 /** Parse a `descriptor` string. Included private keys are put in `out`.
  *
  * If the descriptor has a checksum, it must be valid. If `require_checksum`
