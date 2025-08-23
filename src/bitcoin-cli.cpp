RandyMcMillan/dotfiles

Recognise service bit 24 as NODE_UTREEXO_TMP / "UTREEXO_TMP?"

src/bitcoin-cli.cpp
@@ -481,6 +481,8 @@ private:
                 str += 'l';
             } else if (s == "P2P_V2") {
                 str += '2';
+            } else if (s == "UTREEXO_TMP?") {
+                str += 'T';
             } else {
                 str += ToLower(s[0]);
             }
@@ -719,6 +721,7 @@ public:
         "           \"c\" - COMPACT_FILTERS: peer can handle basic block filter requests (see BIPs 157 and 158)\n"
         "           \"l\" - NETWORK_LIMITED: peer limited to serving only the last 288 blocks (~2 days)\n"
         "           \"2\" - P2P_V2: peer supports version 2 P2P transport protocol, as defined in BIP 324\n"
+        "           \"T\" - UTREEXO_TMP? peer can handle Utreexo proof requests\n"
         "           \"m\" - MALICIOUS? peer openly seeks to aid in bypassing network policy/spam filters (OR to sabotage nodes that seek to)\n"
         "           \"u\" - UNKNOWN: unrecognized bit flag\n"
         "  v        Version of transport protocol used for the connection\n"

bitcoin/bitcoin

src/bitcoin-cli.cpp
@@ -481,6 +481,8 @@ private:
                 str += 'l';
             } else if (s == "P2P_V2") {
                 str += '2';
+            } else if (s == "UTREEXO_TMP?") {
+                str += 'y';
             } else {
                 str += ToLower(s[0]);
             }
@@ -719,6 +721,7 @@ public:
         "           \"c\" - COMPACT_FILTERS: peer can handle basic block filter requests (see BIPs 157 and 158)\n"
         "           \"l\" - NETWORK_LIMITED: peer limited to serving only the last 288 blocks (~2 days)\n"
         "           \"2\" - P2P_V2: peer supports version 2 P2P transport protocol, as defined in BIP 324\n"
+        "           \"y\" - UTREEXO_TMP? peer can handle Utreexo proof requests\n"
         "           \"m\" - MALICIOUS? peer openly seeks to aid in bypassing network policy/spam filters (OR to sabotage nodes that seek to)\n"
         "           \"u\" - UNKNOWN: unrecognized bit flag\n"
         "  v        Version of transport protocol used for the connection\n"