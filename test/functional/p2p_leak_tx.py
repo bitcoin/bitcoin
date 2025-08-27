e14825ace2a8c6abcf11fdfb5057e969cd64b146
David Gumberg <davidzgumberg@gmail.com>
2025-08-27 15:31:32 -0700

    test: make notfound_on_unannounced more reliable
    By using mocktime, we will always hit both the notfound
    branch and the tx sent branch.
    The previous version didn't achieve that due to timing
    issues.
    
    Co-authored-by: Martin Zumsande <mzumsande@gmail.com>

--- test/functional/p2p_leak_tx.py
+++ test/functional/p2p_leak_tx.py
@@ -4,6 +4,8 @@
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 """Test transaction upload"""
 
+import time
+
 from test_framework.messages import msg_getdata, CInv, MSG_TX, MSG_WTX
 from test_framework.p2p import p2p_lock, P2PDataStore, P2PTxInvStore
 from test_framework.test_framework import BitcoinTestFramework
@@ -79,27 +81,32 @@ class P2PLeakTxTest(BitcoinTestFramework):
         self.gen_node.disconnect_p2ps()
         inbound_peer = self.gen_node.add_p2p_connection(P2PNode())  # An "attacking" inbound peer
 
-        MAX_REPEATS = 100
-        self.log.info("Running test up to {} times.".format(MAX_REPEATS))
-        for i in range(MAX_REPEATS):
-            self.log.info('Run repeat {}'.format(i + 1))
-            wtxid = self.miniwallet.send_self_transfer(from_node=self.gen_node)["wtxid"]
+        # Set a mock time so that time does not pass, and gen_node never announces the transaction
+        self.gen_node.setmocktime(int(time.time()))
+        wtxid = int(self.miniwallet.send_self_transfer(from_node=self.gen_node)["wtxid"], 16)
 
-            want_tx = msg_getdata()
-            want_tx.inv.append(CInv(t=MSG_WTX, h=int(wtxid, 16)))
-            with p2p_lock:
-                inbound_peer.last_message.pop('notfound', None)
-            inbound_peer.send_and_ping(want_tx)
-            if inbound_peer.last_message.get('notfound'):
-                self.log.debug('tx {} was not yet announced to us.'.format(wtxid))
-                self.log.debug("node has responded with a notfound message. End test.")
-                assert_equal(inbound_peer.last_message['notfound'].vec[0].hash, int(wtxid, 16))
-                with p2p_lock:
-                    inbound_peer.last_message.pop('notfound')
-                break
-            else:
-                self.log.debug('tx {} was already announced to us. Try test again.'.format(wtxid))
-                assert int(wtxid, 16) in [inv.hash for inv in inbound_peer.last_message['inv'].inv]
+        want_tx = msg_getdata()
+        want_tx.inv.append(CInv(t=MSG_WTX, h=wtxid))
+        with p2p_lock:
+            inbound_peer.last_message.pop('notfound', None)
+        inbound_peer.send_and_ping(want_tx)
+        inbound_peer.wait_until(lambda: "notfound" in inbound_peer.last_message)
+        with p2p_lock:
+            assert_equal(inbound_peer.last_message.get("notfound").vec[0].hash, wtxid)
+            inbound_peer.last_message.pop('notfound')
+
+        # Disable mocktime and wait for the announcement.
+        inbound_peer.last_message.pop('inv', None)
+        self.gen_node.setmocktime(0)
+
+        self.wait_until(lambda: "inv" in inbound_peer.last_message, timeout=120)
+        assert wtxid in [inv.hash for inv in inbound_peer.last_message['inv'].inv]
+
+        # Send the getdata again, this time the node should send us a TX message.
+        inbound_peer.last_message.pop('tx', None)
+        inbound_peer.send_and_ping(want_tx)
+        self.wait_until(lambda: "tx" in inbound_peer.last_message)
+        assert_equal(wtxid, int(inbound_peer.last_message["tx"].tx.wtxid_hex, 16))
 
 
 if __name__ == '__main__':
