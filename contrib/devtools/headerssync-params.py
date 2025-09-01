b73fe786a118427842ab0453ecc6a64a2689d59a
fanquake <fanquake@gmail.com>
2025-09-01 11:57:42 +0100

    kernel: update headersync params
    Github-Pull: #33274
    Rebased-From: 943de66b5043e46f84ccc65039842be34cf42133

--- contrib/devtools/headerssync-params.py
+++ contrib/devtools/headerssync-params.py
@@ -12,13 +12,13 @@ import random
 # Parameters:
 
 # Aim for still working fine at some point in the future. [datetime]
-TIME = datetime(2027, 10, 6)
+TIME = datetime(2028, 4, 2)
 
 # Expected block interval. [timedelta]
 BLOCK_INTERVAL = timedelta(seconds=600)
 
 # The number of headers corresponding to the minchainwork parameter. [headers]
-MINCHAINWORK_HEADERS = 886157
+MINCHAINWORK_HEADERS = 912683
 
 # Combined processing bandwidth from all attackers to one victim. [bit/s]
 # 6 Gbit/s is approximately the speed at which a single thread of a Ryzen 5950X CPU thread can hash
