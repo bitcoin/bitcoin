2327b2b0db084fdaefa0a8e9d666fa3a4bc90c07
Martin Zumsande <mzumsande@gmail.com>
2025-09-12 17:29:04 -0400

    net: Do not apply whitelist permission to onion inbounds
    Tor inbound connections do not reveal the peer's actual network address.
    Therefore do not apply whitelist permissions to them.
    
    Co-authored-by: Vasil Dimov <vd@FreeBSD.org>
    
    Github-Pull: #33395
    Rebased-From: f563ce90818d486d2a199439d2f6ba39cd106352

--- src/net.h
+++ src/net.h
@@ -1377,7 +1377,7 @@ private:
 
     bool AttemptToEvictConnection();
     CNode* ConnectNode(CAddress addrConnect, const char *pszDest, bool fCountFailure, ConnectionType conn_type, bool use_v2transport) EXCLUSIVE_LOCKS_REQUIRED(!m_unused_i2p_sessions_mutex);
-    void AddWhitelistPermissionFlags(NetPermissionFlags& flags, const CNetAddr &addr, const std::vector<NetWhitelistPermissions>& ranges) const;
+    void AddWhitelistPermissionFlags(NetPermissionFlags& flags, std::optional<CNetAddr> addr, const std::vector<NetWhitelistPermissions>& ranges) const;
 
     void DeleteNode(CNode* pnode);
 
