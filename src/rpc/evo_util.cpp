dashpay/dash

refactor(rpc): `platform{HTTP,P2P}Port` > `platform{HTTPS,P2P}Addrs`
The current name is now a misnomer as we accept (multiple) addr:port
pairs, the name has been updated to reflect that. We still support
submitting only ports but that is transitional support that may be
removed in a future release.

src/rpc/evo_util.cpp
@@ -167,8 +167,8 @@ void ProcessNetInfoPlatform(T1& ptx, const UniValue& input_p2p, const UniValue& 
         // Invalid port
         throw JSONRPCError(RPC_INVALID_PARAMETER, strprintf("%s must be a valid port [1-65535]", field_name));
     };
-    process_field(ptx.platformP2PPort, input_p2p, Purpose::PLATFORM_P2P, "platformP2PPort");
-    process_field(ptx.platformHTTPPort, input_http, Purpose::PLATFORM_HTTPS, "platformHTTPPort");
+    process_field(ptx.platformP2PPort, input_p2p, Purpose::PLATFORM_P2P, "platformP2PAddrs");
+    process_field(ptx.platformHTTPPort, input_http, Purpose::PLATFORM_HTTPS, "platformHTTPSAddrs");
 }
 template void ProcessNetInfoPlatform(CProRegTx& ptx, const UniValue& input_p2p, const UniValue& input_http, const bool optional);
 template void ProcessNetInfoPlatform(CProUpServTx& ptx, const UniValue& input_p2p, const UniValue& input_http, const bool optional);

bitcoin/bitcoin

src/rpc/evo_util.cpp
@@ -134,8 +134,8 @@ void ProcessNetInfoPlatform(ProTx& ptx, const UniValue& input_p2p, const UniValu
             }
         }
     };
-    process_field(ptx.platformP2PPort, input_p2p, NetInfoPurpose::PLATFORM_P2P, "platformP2PPort");
-    process_field(ptx.platformHTTPPort, input_http, NetInfoPurpose::PLATFORM_HTTPS, "platformHTTPPort");
+    process_field(ptx.platformP2PPort, input_p2p, NetInfoPurpose::PLATFORM_P2P, "platformP2PAddrs");
+    process_field(ptx.platformHTTPPort, input_http, NetInfoPurpose::PLATFORM_HTTPS, "platformHTTPSAddrs");
 }
 template void ProcessNetInfoPlatform(CProRegTx& ptx, const UniValue& input_p2p, const UniValue& input_http, const bool optional);
 template void ProcessNetInfoPlatform(CProUpServTx& ptx, const UniValue& input_p2p, const UniValue& input_http, const bool optional);