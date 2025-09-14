69c06c8d117336b21ac5d920c14d591d3f0cf328
Musa Haruna <hmusa3962@gmail.com>
2025-09-14 12:04:16 +0100

    node: add and implement `FindCoinsByScript()` for UTXO-set scan by script
    Add `FindCoinsByScript()` which implements a  DB-backed scan
    that iterates the on-disk UTXO DB with a `CCoinViewCursor` and wire it into the node
    interface as `Node::getCoinsByScript()`.

--- src/node/coin.cpp
+++ src/node/coin.cpp
@@ -24,4 +24,33 @@ void FindCoins(const NodeContext& node, std::map<COutPoint, Coin>& coins)
         }
     }
 }
+
+void FindCoinsByScript(const NodeContext& node, std::set<CScript>& output_scripts, std::map<COutPoint, Coin>& coins)
+{
+    assert(node.chainman);
+    std::unique_ptr<CCoinsViewCursor> cursor;
+    {
+        LOCK(cs_main);
+        Chainstate& active_chainstate = node.chainman->ActiveChainstate();
+        // Ensure on-disk coins DB is up-to-date so the cursor will see recent coins.
+        active_chainstate.ForceFlushStateToDisk();
+        cursor = active_chainstate.CoinsDB().Cursor();
+    }
+
+    if (!cursor) {
+        return;
+    }
+
+    // Iterate the cursor.
+    while (cursor->Valid()) {
+        COutPoint key;
+        Coin coin;
+        if (cursor->GetKey(key) && cursor->GetValue(coin)) {
+            if (output_scripts.count(coin.out.scriptPubKey)) {
+                coins.emplace(key, coin);
+            }
+        }
+        cursor->Next();
+    }
+}
 } // namespace node
