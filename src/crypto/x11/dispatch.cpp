1f2eb21d7608e89de1ea9d0c5209115f61c5463b
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2025-09-22 09:37:52 +0000

    crypto: implement naive AES-NI backend for simple rounds

--- src/crypto/x11/dispatch.cpp
+++ src/crypto/x11/dispatch.cpp
@@ -2,11 +2,31 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#if defined(HAVE_CONFIG_H)
+#include <config/bitcoin-config.h>
+#endif
+
 #include <crypto/x11/dispatch.h>
+
+#if !defined(DISABLE_OPTIMIZED_SHA256)
+#include <compat/cpuid.h>
+#endif // !DISABLE_OPTIMIZED_SHA256
 
 #include <cstdint>
 
 namespace sapphire {
+#if !defined(DISABLE_OPTIMIZED_SHA256)
+#if defined(ENABLE_SSE41) && defined(ENABLE_X86_AESNI)
+namespace x86_aesni_aes {
+void Round(uint32_t x0, uint32_t x1, uint32_t x2, uint32_t x3,
+           uint32_t k0, uint32_t k1, uint32_t k2, uint32_t k3,
+           uint32_t& y0, uint32_t& y1, uint32_t& y2, uint32_t& y3);
+void RoundKeyless(uint32_t x0, uint32_t x1, uint32_t x2, uint32_t x3,
+                  uint32_t& y0, uint32_t& y1, uint32_t& y2, uint32_t& y3);
+} // namespace x86_aesni_aes
+#endif // ENABLE_SSE41 && ENABLE_X86_AESNI
+#endif // !DISABLE_OPTIMIZED_SHA256
+
 namespace soft_aes {
 void Round(uint32_t x0, uint32_t x1, uint32_t x2, uint32_t x3,
            uint32_t k0, uint32_t k1, uint32_t k2, uint32_t k3,
@@ -16,6 +36,19 @@ void RoundKeyless(uint32_t x0, uint32_t x1, uint32_t x2, uint32_t x3,
 } // namespace soft_aes
 } // namespace sapphire
 
+namespace {
+[[maybe_unused]] bool use_aes_ni = []() {
+#if !defined(DISABLE_OPTIMIZED_SHA256) && defined(HAVE_GETCPUID)
+    uint32_t eax, ebx, ecx, edx;
+    GetCPUID(1, 0, eax, ebx, ecx, edx);
+    return (/*has_sse4_1=*/((ecx >> 19) & 1) &&
+            /*has_aes_ni=*/((ecx >> 25) & 1));
+#else
+    return false;
+#endif // !DISABLE_OPTIMIZED_SHA256 && HAVE_GETCPUID
+}();
+} // anonymous namespace
+
 extern sapphire::dispatch::AESRoundFn aes_round;
 extern sapphire::dispatch::AESRoundFnNk aes_round_nk;
 
@@ -23,4 +56,13 @@ void SapphireAutoDetect()
 {
     aes_round = sapphire::soft_aes::Round;
     aes_round_nk = sapphire::soft_aes::RoundKeyless;
+
+#if !defined(DISABLE_OPTIMIZED_SHA256)
+#if defined(ENABLE_SSE41) && defined(ENABLE_X86_AESNI)
+    if (use_aes_ni) {
+        aes_round = sapphire::x86_aesni_aes::Round;
+        aes_round_nk = sapphire::x86_aesni_aes::RoundKeyless;
+    }
+#endif // ENABLE_SSE41 && ENABLE_X86_AESNI
+#endif // !DISABLE_OPTIMIZED_SHA256
 }
--- src/crypto/x11/util/util.hpp
+++ src/crypto/x11/util/util.hpp
@@ -7,6 +7,14 @@
 
 #include <cstdint>
 
+#if !defined(DISABLE_OPTIMIZED_SHA256)
+#include <attributes.h>
+
+#if defined(ENABLE_SSE41) && defined(ENABLE_X86_AESNI)
+#include <immintrin.h>
+#endif // ENABLE_SSE41 && ENABLE_X86_AESNI
+#endif // !DISABLE_OPTIMIZED_SHA256
+
 namespace sapphire {
 namespace util {
 constexpr inline uint32_t pack_le(uint8_t b3, uint8_t b2, uint8_t b1, uint8_t b0) noexcept
@@ -16,6 +24,25 @@ constexpr inline uint32_t pack_le(uint8_t b3, uint8_t b2, uint8_t b1, uint8_t b0
            (static_cast<uint32_t>(b1) <<  8) |
            (static_cast<uint32_t>(b0));
 }
+
+#if !defined(DISABLE_OPTIMIZED_SHA256)
+#if defined(ENABLE_SSE41) && defined(ENABLE_X86_AESNI)
+__m128i ALWAYS_INLINE aes_round(const __m128i& input, const __m128i& key) { return _mm_aesenc_si128(input, key); }
+
+__m128i ALWAYS_INLINE pack_le(const uint32_t& w0, const uint32_t& w1, const uint32_t& w2, const uint32_t& w3)
+{
+    return _mm_set_epi32(w3, w2, w1, w0);
+}
+
+void ALWAYS_INLINE unpack_le(const __m128i& i, uint32_t& w0, uint32_t& w1, uint32_t& w2, uint32_t& w3)
+{
+    w0 = _mm_extract_epi32(i, 0);
+    w1 = _mm_extract_epi32(i, 1);
+    w2 = _mm_extract_epi32(i, 2);
+    w3 = _mm_extract_epi32(i, 3);
+}
+#endif // ENABLE_SSE41 && ENABLE_X86_AESNI
+#endif // !DISABLE_OPTIMIZED_SHA256
 } // namespace util
 } // namespace sapphire
 
