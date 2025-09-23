f046286c0c5e1fef8b61da548cb97506f7a8b047
Fabian Jahr <fjahr@protonmail.com>
2025-09-23 23:04:47 +0200

    rpc: Don't invalidate blocks in dumptxoutset
    Instead this new approach uses a temporary coins db to roll back the
    UTXO set.

--- test/functional/rpc_dumptxoutset.py
+++ test/functional/rpc_dumptxoutset.py
@@ -19,17 +19,6 @@ class DumptxoutsetTest(BitcoinTestFramework):
         self.setup_clean_chain = True
         self.num_nodes = 1
 
-    def check_expected_network(self, node, active):
-        rev_file = node.blocks_path / "rev00000.dat"
-        bogus_file = node.blocks_path / "bogus.dat"
-        rev_file.rename(bogus_file)
-        assert_raises_rpc_error(
-            -1, 'Could not roll back to requested height.', node.dumptxoutset, 'utxos.dat', rollback=99)
-        assert_equal(node.getnetworkinfo()['networkactive'], active)
-
-        # Cleanup
-        bogus_file.rename(rev_file)
-
     def run_test(self):
         """Test a trivial usage of the dumptxoutset RPC command."""
         node = self.nodes[0]
@@ -70,14 +59,6 @@ class DumptxoutsetTest(BitcoinTestFramework):
         self.log.info("Test that dumptxoutset with unknown dump type fails")
         assert_raises_rpc_error(
             -8, 'Invalid snapshot type "bogus" specified. Please specify "rollback" or "latest"', node.dumptxoutset, 'utxos.dat', "bogus")
-
-        self.log.info("Test that dumptxoutset failure does not leave the network activity suspended when it was on previously")
-        self.check_expected_network(node, True)
-
-        self.log.info("Test that dumptxoutset failure leaves the network activity suspended when it was off")
-        node.setnetworkactive(False)
-        self.check_expected_network(node, False)
-        node.setnetworkactive(True)
 
 
 if __name__ == '__main__':
