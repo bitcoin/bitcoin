e80a52732384748d194bf95c3a1436df2079578d
pablomartin4btc <pablomartin4btc@gmail.com>
2025-09-30 12:20:32 -0300

    argsman, refactor: Factor out ProcessOptionKey from ParseParameters
    - Centralizes normalization of option keys in `ProcessOptionKey`.
      This will help in next commit to reuse this function avoiding code
      duplication.
    - Improves readability and makes behavior easier to reason about.
    
    Functional and unit tests confirm no behavior change.

--- src/common/args.cpp
+++ src/common/args.cpp
@@ -174,6 +174,37 @@ void ArgsManager::SelectConfigNetwork(const std::string& network)
     m_network = network;
 }
 
+bool ArgsManager::ProcessOptionKey(std::string& key, std::optional<std::string>& val, std::string& error) {
+
+    std::string original_input = key; // Capture the original key
+    if (val) original_input += "=" + *val; // Append =value if it exists
+
+    // Transform --foo to -foo
+    if (key.length() > 1 && key[1] == '-')
+        key.erase(0, 1);
+
+    // Transform -foo to foo
+    key.erase(0, 1);
+
+    KeyInfo keyinfo = InterpretKey(key);
+    std::optional<unsigned int> flags = GetArgFlags('-' + keyinfo.name);
+    // Unknown command line options and command line options with dot characters
+    // (which are returned from InterpretKey with nonempty section strings)are not valid.
+    if (!flags || !keyinfo.section.empty()) {
+        error = strprintf("Invalid parameter %s", original_input);
+        return false;
+    }
+
+    std::optional<common::SettingsValue> value = InterpretValue(keyinfo, val ? &*val : nullptr, *flags, error);
+    if (!value) return false;
+
+    // Store the option
+    LOCK(cs_args);
+    m_settings.command_line_options[keyinfo.name].push_back(*value);
+
+    return true;
+}
+
 bool ArgsManager::ParseParameters(int argc, const char* const argv[], std::string& error)
 {
     LOCK(cs_args);
@@ -220,27 +251,9 @@ bool ArgsManager::ParseParameters(int argc, const char* const argv[], std::strin
             break;
         }
 
-        // Transform --foo to -foo
-        if (key.length() > 1 && key[1] == '-')
-            key.erase(0, 1);
-
-        // Transform -foo to foo
-        key.erase(0, 1);
-        KeyInfo keyinfo = InterpretKey(key);
-        std::optional<unsigned int> flags = GetArgFlags('-' + keyinfo.name);
-
-        // Unknown command line options and command line options with dot
-        // characters (which are returned from InterpretKey with nonempty
-        // section strings) are not valid.
-        if (!flags || !keyinfo.section.empty()) {
-            error = strprintf("Invalid parameter %s", argv[i]);
+        if (!ProcessOptionKey(key, val, error)) {
             return false;
         }
-
-        std::optional<common::SettingsValue> value = InterpretValue(keyinfo, val ? &*val : nullptr, *flags, error);
-        if (!value) return false;
-
-        m_settings.command_line_options[keyinfo.name].push_back(*value);
     }
 
     // we do not allow -includeconf from command line, only -noincludeconf
