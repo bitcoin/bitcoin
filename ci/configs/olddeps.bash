abcd4c4ff928a7af280ca271f8a939367fc4e09a
Ryan Ofsky <ryan@ofsky.org>
2025-10-07 10:12:08 -0400

    Squashed 'src/ipc/libmultiprocess/' changes from 13424cf2ecc1..a4f929696490
    a4f929696490 Merge bitcoin-core/libmultiprocess#224: doc: fix typos
    f4344ae87da0 Merge bitcoin-core/libmultiprocess#222: test, ci: Fix threadsanitizer errors in mptest
    1434642b3804 doc: fix typos
    73d22ba2e930 test: Fix tsan race in thread busy test
    b74e1bba014d ci: Use tsan-instrumented cap'n proto in sanitizers job
    c332774409ad test: Fix failing exception check in new thread busy test
    ca3c05d56709 test: Use KJ_LOG instead of std::cout for logging
    7eb1da120ab6 ci: Use tsan-instrumented libcxx in sanitizers job
    ec86e4336e98 Merge bitcoin-core/libmultiprocess#220: Add log levels and advertise them to users via logging callback
    515ce93ad349 Logging: Pass LogData struct to logging callback
    213574ccc43d Logging: reclassify remaining log messages
    e4de0412b430 Logging: Break out expensive log messages and classify them as Trace
    408874a78fdc Logging: Use new logging macros
    67b092d835cd Logging: Disable logging if messsage level is less than the requested level
    d0a1ba7ebf21 Logging: add log levels to mirror Core's
    463a8296d188 Logging: Disable moving or copying Logger
    83a2e10c0b03 Logging: Add an EventLoop constructor to allow for user-specified log options
    58cf47a7fc8c Merge bitcoin-core/libmultiprocess#221: test default PassField impl handles output parameters
    db03a663f514 Merge bitcoin-core/libmultiprocess#214: Fix crash on simultaneous IPC calls using the same thread
    afcc40b0f1e8 Merge bitcoin-core/libmultiprocess#213: util+doc: Clearer errors when attempting to run examples + polished docs
    6db669628387 test In|Out parameter
    29cf2ada75ea test default PassField impl handles output parameters
    1238170f68e8 test: simultaneous IPC calls using same thread
    eb069ab75d83 Fix crash on simultaneous IPC calls using the same thread
    ec03a9639ab5 doc: Precision and typos
    2b4348193551 doc: Where possible, remove links to ryanofsky/bitcoin/
    286fe469c9c9 util: Add helpful error message when failing to execute file
    47d79db8a552 Merge bitcoin-core/libmultiprocess#201: bug: fix mptest hang, ProxyClient<Thread> deadlock in disconnect handler
    f15ae9c9b9fb Merge bitcoin-core/libmultiprocess#211: Add .gitignore
    4a269b21b8c8 bug: fix ProxyClient<Thread> deadlock if disconnected as IPC call is returning
    85df96482c49 Use try_emplace in SetThread instead of threads.find
    ca9b380ea91a Use std::optional in ConnThreads to allow shortening locks
    9b0799113557 doc: describe ThreadContext struct and synchronization requirements
    d60db601ed9b proxy-io.h: add Waiter::m_mutex thread safety annotations
    4e365b019a9f ci: Use -Wthread-safety not -Wthread-safety-analysis
    15d7bafbb001 Add .gitignore
    fe1cd8c76131 Merge bitcoin-core/libmultiprocess#208: ci: Test minimum cmake version in olddeps job
    b713a0b7bfbc Merge bitcoin-core/libmultiprocess#207: ci: output CMake version in CI script
    0f580397c913 ci: Test minimum cmake version in olddeps job
    d603dcc0eef0 ci: output CMake version in CI script
    
    git-subtree-dir: src/ipc/libmultiprocess
    git-subtree-split: a4f92969649018ca70f949a09148bccfeaecd99a

--- ci/configs/olddeps.bash
+++ ci/configs/olddeps.bash
@@ -1,5 +1,5 @@
-CI_DESC="CI job using old Cap'n Proto version"
+CI_DESC="CI job using old Cap'n Proto and cmake versions"
 CI_DIR=build-olddeps
 export CXXFLAGS="-Werror -Wall -Wextra -Wpedantic -Wno-unused-parameter -Wno-error=array-bounds"
-NIX_ARGS=(--argstr capnprotoVersion "0.7.1")
+NIX_ARGS=(--argstr capnprotoVersion "0.7.1" --argstr cmakeVersion "3.12.4")
 BUILD_ARGS=(-k)
