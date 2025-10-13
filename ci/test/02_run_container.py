3d17293bc2b991e2bde6097988a8935a4da6c9b1
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2025-10-13 11:02:01 +0200

    ci: Move buildx command to python script
    This has a few benefits:
    
    * The raw python3 -c "..." command to calculate --cpuset-cpus lives in a
      Python context.
    * The shellcheck SC2086 warning is disabled for the whole command, but
      is only needed for the DOCKER_BUILD_CACHE_ARG env var.  So in Python,
      only pass this one env var to shlex.split() for proper word splitting.
    
    The comments are moved, which can be checked via the git options:
    --color-moved=dimmed-zebra --color-moved-ws=ignore-all-space

--- ci/test/02_run_container.py
+++ ci/test/02_run_container.py
@@ -4,6 +4,7 @@
 # file COPYING or https://opensource.org/license/mit/.
 
 import os
+import random
 import shlex
 import subprocess
 import sys
@@ -15,6 +16,18 @@ def run(cmd, **kwargs):
         return subprocess.run(cmd, check=True, **kwargs)
     except Exception as e:
         sys.exit(e)
+
+
+def maybe_cpuset():
+    # Env vars during the build can not be changed. For example, a modified
+    # $MAKEJOBS is ignored in the build process. Use --cpuset-cpus as an
+    # approximation to respect $MAKEJOBS somewhat, if cpuset is available.
+    if os.getenv("HAVE_CGROUP_CPUSET"):
+        P = int(run(['nproc'], stdout=subprocess.PIPE).stdout)
+        M = min(P, int(os.getenv("MAKEJOBS").lstrip("-j")))
+        cpus = sorted(random.sample(range(P), M))
+        return [f"--cpuset-cpus={','.join(map(str, cpus))}"]
+    return []
 
 
 def main():
@@ -44,6 +57,30 @@ def main():
             if k in settings:
                 file.write(f"{k}={v}\n")
     run(["cat", env_file])
+
+    if not os.getenv("DANGER_RUN_CI_ON_HOST"):
+        CI_IMAGE_LABEL = "bitcoin-ci-test"
+
+        maybe_cache_arg = shlex.split(os.getenv('DOCKER_BUILD_CACHE_ARG'))
+
+        # Use buildx unconditionally
+        # Using buildx is required to properly load the correct driver, for use with registry caching. Neither build, nor BUILDKIT=1 currently do this properly
+        cmd_build = ["docker", "buildx", "build"]
+        cmd_build += [
+            f"--file={os.getenv('BASE_READ_ONLY_DIR')}/ci/test_imagefile",
+            f"--build-arg=CI_IMAGE_NAME_TAG={os.getenv('CI_IMAGE_NAME_TAG')}",
+            f"--build-arg=FILE_ENV={os.getenv('FILE_ENV')}",
+            f"--build-arg=BASE_ROOT_DIR={os.getenv('BASE_ROOT_DIR')}",
+            f"--platform={os.getenv('CI_IMAGE_PLATFORM')}",
+            f"--label={CI_IMAGE_LABEL}",
+            f"--tag={os.getenv('CONTAINER_NAME')}",
+        ]
+        cmd_build += maybe_cpuset()
+        cmd_build += maybe_cache_arg
+        cmd_build += [os.getenv('BASE_READ_ONLY_DIR')]
+
+        print(f"Building {os.getenv('CI_IMAGE_NAME_TAG')} image to run in")
+        run(cmd_build)
 
     run(["./ci/test/02_run_container.sh"])  # run the remainder
 
