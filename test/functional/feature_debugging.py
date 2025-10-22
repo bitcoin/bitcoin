f0419b823f066f20c399f7c21bdaf3ded084c346
Hodlinator <172445034+hodlinator@users.noreply.github.com>
2025-10-22 23:52:36 +0200

    node: Add -waitfordebugger
    Makes bitcoind spin during startup, waiting for a debugger to be attached. Once a debugger is detected, execution will continue.

--- test/functional/feature_debugging.py
+++ test/functional/feature_debugging.py
@@ -5,6 +5,7 @@
 """Verify that debugging bitcoind is not allowed
 
 This is important when wallets hold private keys in memory.
+Also verifies that debugging is allowed when using -waitfordebugger.
 """
 from test_framework.test_framework import BitcoinTestFramework, SkipTest
 from test_framework.util import (
@@ -53,11 +54,22 @@ class DebuggingDisallowedTest(BitcoinTestFramework):
             else:
                 assert_not_equal(exit_code, 0)
 
+        # When expecting success we are also running the node with
+        # -waitfordebugger and need to require reaching a breakpoint in
+        # something like SocketHandler as it is repeatedly called once node is
+        # initialized and responding to RPCs.
+        # Had we just detached without continuing to the breakpoint, while
+        # inside WaitForDebugger(), it would result no TracerPid being detected,
+        # so the node would not leave that function and start responding to RPCs
+        # this would mean wait_for_rpc_connection() during the test would not
+        # complete.
         if self.gdb:
             verify_attach("GDB", [self.gdb, '-p', str(pid), '--batch'] +
+                                 (['-ex', 'b SocketHandler', '-ex', 'continue'] if expect_success else []) +
                                  ['--ex', 'detach'], expect_success)
         if self.lldb:
             verify_attach("LLDB", [self.lldb, '-p', str(pid), '--batch'] +
+                                  (['-o', 'b SocketHandler', '-o', 'continue'] if expect_success else []) +
                                   ['-o', 'detach -s false'], expect_success)
 
     def run_test(self):
@@ -80,6 +92,37 @@ class DebuggingDisallowedTest(BitcoinTestFramework):
 
         self.attempt_attaching(node_pid, expect_success=False)
         self.stop_node(0)
+
+        wait_for_debugger = None
+        try:
+            with open(os.path.abspath(__file__ + '/../../../CMakeCache.txt'), encoding='utf8') as f:
+                for line in f.readlines():
+                    if line == 'WAIT_FOR_DEBUGGER:BOOL=ON\n':
+                        wait_for_debugger = True
+                    elif line == 'WAIT_FOR_DEBUGGER:BOOL=OFF\n':
+                        wait_for_debugger = False
+        except FileNotFoundError:
+            self.log.warning("Failed opening CMakeCache.txt to detect WAIT_FOR_DEBUGGER, assuming it's off")
+            wait_for_debugger = False
+
+        if wait_for_debugger is None:
+            self.log.warning("Failed detecting WAIT_FOR_DEBUGGER status from CMakeCache.txt, assuming it's off")
+            wait_for_debugger = False
+
+        if wait_for_debugger:
+            self.log.info("Verifying WAIT_FOR_DEBUGGER enables attaching")
+            # Using TestNode.start() over BitcoinTestFramework.start_node() here
+            # as the former does not block waiting for the RPC connection.
+            # -waitfordebugger means we will not have an RPC connection until
+            # a debugger has been attached.
+            self.nodes[0].start(['-waitfordebugger'])
+            self.attempt_attaching(self.nodes[0].process.pid, expect_success=True)
+            self.log.debug("Ensure node is fully connected before teardown")
+            self.nodes[0].wait_for_rpc_connection()
+        else:
+            self.nodes[0].assert_start_raises_init_error(
+                ['-waitfordebugger'],
+                expected_msg="Error: -waitfordebugger is unavailable in this build. Rebuild with -DWAIT_FOR_DEBUGGER=ON.")
 
 
 if __name__ == '__main__':
