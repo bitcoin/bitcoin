85cff48b782538ed5a1c980015dd01cb32986b66
Pieter Wuille <pieter@wuille.net>
2025-10-23 19:15:21 -0400

    clusterlin: replace cluster linearization with SFL implementation (feature)
    This replaces the existing LIMO linearization algorithm (which internally uses
    ancestor set finding and candidate set finding) with the much more performant
    spanning-forest linearization algorithm.

--- src/test/util/cluster_linearize.h
+++ src/test/util/cluster_linearize.h
@@ -396,25 +396,24 @@ void SanityCheck(const DepGraph<SetType>& depgraph, std::span<const DepGraphInde
 
 inline uint64_t MaxOptimalLinearizationIters(DepGraphIndex cluster_count)
 {
-    // We assume sqrt(2^k)+1 candidate-finding iterations per candidate to be found, plus ceil(k/4)
-    // startup cost when up to k unlinearization transactions remain, plus ceil(n^2/64) overall
-    // startup cost in Linearize. Thus, we can compute the upper bound for a whole linearization
-    // (summing for k=1..n) using the Python expression:
-    //
-    //   [sum((k+3)//4 + math.isqrt(2**k) + 1 for k in range(1, n + 1)) + (n**2 + 63) // 64 for n in range(0, 65)]
-    //
-    // Note that these are just assumptions, as the proven upper bound grows with 2^k, not
-    // sqrt(2^k).
-    static constexpr uint64_t MAX_OPTIMAL_ITERS[65] = {
-        0, 4, 8, 12, 18, 26, 37, 51, 70, 97, 133, 182, 251, 346, 480, 666, 927, 1296, 1815, 2545,
-        3576, 5031, 7087, 9991, 14094, 19895, 28096, 39690, 56083, 79263, 112041, 158391, 223936,
-        316629, 447712, 633086, 895241, 1265980, 1790280, 2531747, 3580335, 5063259, 7160424,
-        10126257, 14320575, 20252230, 28640853, 40504150, 57281380, 81007962, 114562410, 162015557,
-        229124437, 324030718, 458248463, 648061011, 916496483, 1296121563, 1832992493, 2592242635,
-        3665984477, 5184484745, 7331968412, 10368968930, 14663936244
+    // These are the largest numbers seen returned as cost by Linearize(), in a large randomized
+    // trial. There exist almost certainly far worse cases, but they are unlikely to be
+    // encountered in randomized tests. The purpose of these numbers is guaranteeing that for
+    // *some* reasonable cost bound, optimal linearizations are always found.
+    static constexpr uint64_t ITERS[65] = {
+        0,
+        0, 1, 3, 6, 11, 19, 28, 32,
+        40, 48, 53, 61, 70, 84, 95, 100,
+        110, 156, 145, 160, 189, 219, 206, 241,
+        273, 264, 288, 338, 300, 429, 369, 403,
+        440, 436, 505, 490, 524, 490, 628, 640,
+        632, 839, 807, 746, 881, 1011, 1048, 905,
+        1010, 1007, 974, 1065, 1062, 1317, 1178, 1535,
+        1298, 1425, 1850, 1453, 1564, 1561, 1575, 1687
     };
-    assert(cluster_count < sizeof(MAX_OPTIMAL_ITERS) / sizeof(MAX_OPTIMAL_ITERS[0]));
-    return MAX_OPTIMAL_ITERS[cluster_count];
+    assert(cluster_count < std::size(ITERS));
+    // Multiply the table number by two, to account for the fact that they are not absolutes.
+    return ITERS[cluster_count] * 2;
 }
 
 } // namespace
