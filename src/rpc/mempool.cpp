5f244f61ca7ced9c72aca3673e57a529b43e3b7d
Pol Espinasa <pol.espinasa@uab.cat>
2025-10-30 00:13:33 +0100

    rpc: let MempoolInfoToJSON to return feerates in sat/vb
    This commit also includes mempool.h, which was missing and was causing function definition issues.

--- src/rpc/mempool.cpp
+++ src/rpc/mempool.cpp
@@ -9,7 +9,6 @@
 #include <common/args.h>
 #include <consensus/amount.h>
 #include <consensus/validation.h>
-#include <core_io.h>
 #include <index/txospenderindex.h>
 #include <net.h>
 #include <net_processing.h>
@@ -1103,7 +1102,7 @@ static RPCMethod gettxspendingprevout()
     };
 }
 
-UniValue MempoolInfoToJSON(const CTxMemPool& pool)
+UniValue MempoolInfoToJSON(const CTxMemPool& pool, FeeRateUnit feerate_units)
 {
     // Make sure this call is atomic in the pool.
     LOCK(pool.cs);
@@ -1114,9 +1113,9 @@ UniValue MempoolInfoToJSON(const CTxMemPool& pool)
     ret.pushKV("usage", pool.DynamicMemoryUsage());
     ret.pushKV("total_fee", ValueFromAmount(pool.GetTotalFee()));
     ret.pushKV("maxmempool", pool.m_opts.max_size_bytes);
-    ret.pushKV("mempoolminfee", ValueFromAmount(std::max(pool.GetMinFee(), pool.m_opts.min_relay_feerate).GetFeePerK()));
-    ret.pushKV("minrelaytxfee", ValueFromAmount(pool.m_opts.min_relay_feerate.GetFeePerK()));
-    ret.pushKV("incrementalrelayfee", ValueFromAmount(pool.m_opts.incremental_relay_feerate.GetFeePerK()));
+    ret.pushKV("mempoolminfee", ValueFromFeeRate(std::max(pool.GetMinFee(), pool.m_opts.min_relay_feerate), feerate_units));
+    ret.pushKV("minrelaytxfee", ValueFromFeeRate(pool.m_opts.min_relay_feerate, feerate_units));
+    ret.pushKV("incrementalrelayfee", ValueFromFeeRate(pool.m_opts.incremental_relay_feerate, feerate_units));
     ret.pushKV("unbroadcastcount", pool.GetUnbroadcastTxs().size());
     ret.pushKV("permitbaremultisig", pool.m_opts.permit_bare_multisig);
     ret.pushKV("maxdatacarriersize", pool.m_opts.max_datacarrier_bytes.value_or(0));
