f2d27e67e02df21071a43aa20c272b503a8fdebe
fanquake <fanquake@gmail.com>
2025-10-29 15:59:52 +0000

    guix: build aarch64-linux bitcoind fully statically

--- contrib/guix/libexec/build.sh
+++ contrib/guix/libexec/build.sh
@@ -143,11 +143,11 @@ export GUIX_LD_WRAPPER_DISABLE_RPATH=yes
 # Determine the correct value for -Wl,--dynamic-linker for the current $HOST
 case "$HOST" in
     x86_64-linux-gnu) ;;
+    aarch64-linux-gnu) ;;
     *linux*)
         glibc_dynamic_linker=$(
             case "$HOST" in
                 arm-linux-gnueabihf)   echo /lib/ld-linux-armhf.so.3 ;;
-                aarch64-linux-gnu)     echo /lib/ld-linux-aarch64.so.1 ;;
                 riscv64-linux-gnu)     echo /lib/ld-linux-riscv64-lp64d.so.1 ;;
                 powerpc64-linux-gnu)   echo /lib64/ld64.so.1;;
                 powerpc64le-linux-gnu) echo /lib64/ld64.so.2;;
@@ -231,6 +231,7 @@ esac
 
 # EXE FLAGS
 case "$HOST" in
+    aarch64-linux-gnu) CMAKE_EXE_LINKER_FLAGS="-DCMAKE_EXE_LINKER_FLAGS=-static-pie -static-libgcc -Wl,-O2" ;;
     x86_64-linux-gnu) CMAKE_EXE_LINKER_FLAGS="-DCMAKE_EXE_LINKER_FLAGS=-static-pie -static-libgcc -Wl,-O2" ;;
     *linux*)  CMAKE_EXE_LINKER_FLAGS="-DCMAKE_EXE_LINKER_FLAGS=${HOST_LDFLAGS} -static-libstdc++ -static-libgcc" ;;
 esac
