d32130a052af854625f3d3a92c71e1b5c361e758
Anthony Towns <aj@erisian.com.au>
2025-11-01 04:46:17 +1000

    [doc] Update comments on dummy extraNonces in tests

--- src/test/util/mining.cpp
+++ src/test/util/mining.cpp
@@ -49,6 +49,7 @@ std::vector<std::shared_ptr<CBlock>> CreateBlockChain(size_t total_height, const
         coinbase_tx.vout.resize(1);
         coinbase_tx.vout[0].scriptPubKey = P2WSH_OP_TRUE;
         coinbase_tx.vout[0].nValue = GetBlockSubsidy(height + 1, params.GetConsensus());
+        // Always include OP_0 as a dummy extraNonce.
         coinbase_tx.vin[0].scriptSig = CScript() << (height + 1) << OP_0;
         block.vtx = {MakeTransactionRef(std::move(coinbase_tx))};
 
