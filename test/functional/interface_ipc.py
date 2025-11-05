ef268ee288c04a31dd802c4bb729a0c2e6e64786
kevkevinpal <oapallikunnel@gmail.com>
2025-11-05 18:01:17 -0500

    test: Ignore error message give from python because of GIL

--- test/functional/interface_ipc.py
+++ test/functional/interface_ipc.py
@@ -7,13 +7,22 @@ import asyncio
 from io import BytesIO
 from pathlib import Path
 import shutil
+import warnings
 from test_framework.messages import (CBlock, CTransaction, ser_uint256, COIN)
 from test_framework.test_framework import (BitcoinTestFramework, assert_equal)
 from test_framework.wallet import MiniWallet
 
 # Test may be skipped and not have capnp installed
 try:
-    import capnp  # type: ignore[import] # noqa: F401
+    # TODO: Remove this warning catch once capnp can run safely without the GIL
+    with warnings.catch_warnings():
+        warnings.filterwarnings(
+            "ignore",
+            message=r"The global interpreter lock \(GIL\) has been enabled to load module \'capnp.lib.capnp\', which has not declared that it can run safely without the GIL. To override this behavior and keep the GIL disabled \(at your own risk\)\, run with PYTHON_GIL\=0 or \-Xgil\=0\.",
+            category=RuntimeWarning,
+            module=r"importlib\._bootstrap",
+        )
+        import capnp  # type: ignore[import] # noqa: F401
 except ImportError:
     pass
 
