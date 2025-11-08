fadb67b4b4e106cc1078172c5996fd6e8d93b4e2
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2025-11-08 11:45:33 +0100

    ci: [refactor] Base nowallet task on --preset=dev-mode
    This makes it clearer what pieces are disabled over the full dev-mode.
    
    The wallet remains explicitly disabled.

--- ci/test/00_setup_env_native_nowallet.sh
+++ ci/test/00_setup_env_native_nowallet.sh
@@ -6,11 +6,15 @@
 
 export LC_ALL=C.UTF-8
 
-export CONTAINER_NAME=ci_native_nowallet_libbitcoinkernel
+export CONTAINER_NAME=ci_native_nowallet
 export CI_IMAGE_NAME_TAG="mirror.gcr.io/ubuntu:24.04"
 # Use minimum supported python3.10 (or best-effort 3.12) and clang-17, see doc/dependencies.md
 export PACKAGES="python3-zmq python3-pip clang-17 llvm-17 libc++abi-17-dev libc++-17-dev"
 export PIP_PACKAGES="--break-system-packages pycapnp"
 export DEP_OPTS="NO_WALLET=1 CC=clang-17 CXX='clang++-17 -stdlib=libc++'"
 export GOAL="install"
-export BITCOIN_CONFIG="-DREDUCE_EXPORTS=ON -DBUILD_UTIL_CHAINSTATE=ON -DBUILD_KERNEL_LIB=ON -DBUILD_KERNEL_TEST=ON -DBUILD_SHARED_LIBS=ON"
+export BITCOIN_CONFIG="\
+  --preset=dev-mode \
+  -DREDUCE_EXPORTS=ON \
+  -DENABLE_WALLET=OFF \
+"
