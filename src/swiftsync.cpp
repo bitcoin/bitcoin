f51d1c58e697d50ae80938b43c1dc26397f6d7d0
rustaceanrob <rob.netzke@gmail.com>
2025-11-12 10:05:25 +0000

    swiftsync: Add hintfile reader/writer
    The set of all inputs ever created and the set of all outputs are not
    equivalent sets. Naturally, some outputs will be unspent. To update the
    aggregate correctly, we require "hints" as to what outputs will remain
    in the UTXO set at a particular height in the chain. These hints allow a
    client to verify `inputs = outputs - UTXOs` for a terminal block height.
    
    The hints are encoded by enumerating each output in the block, and
    recording if an output is in the final UTXO set. Instead of recording
    the literal index in each block, the difference between the last element
    in the UTXO set and the next one is recorded to the file. This is akin
    to run-length encoding.
    
    A header section is added so multiple blocks may be encoded
    concurrently. The header section denotes the block height and file
    position.

--- src/swiftsync.cpp
+++ src/swiftsync.cpp
@@ -3,9 +3,16 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #include <primitives/transaction.h>
 #include <random.h>
+#include <serialize.h>
+#include <streams.h>
 #include <swiftsync.h>
 #include <uint256.h>
+#include <array>
+#include <cstdint>
+#include <cstdio>
+#include <ios>
 #include <span>
+#include <vector>
 
 using namespace swiftsync;
 
@@ -32,4 +39,70 @@ void Aggregate::Spend(const COutPoint& outpoint)
     m_limb1 -= hash.GetUint64(1);
     m_limb2 -= hash.GetUint64(2);
     m_limb3 -= hash.GetUint64(3);
+}
+
+HintsfileWriter::HintsfileWriter(AutoFile& file, const uint32_t& preallocate) : m_file(file.release())
+{
+    uint64_t dummy_file_pos{0};
+    m_file << FILE_MAGIC;
+    m_file << FILE_VERSION;
+    m_file << preallocate;
+    for (uint32_t height = 1; height <= preallocate; height++) {
+        m_file << height;
+        m_file << dummy_file_pos;
+    }
+}
+
+bool HintsfileWriter::WriteNextUnspents(const std::vector<uint64_t>& unspent_offsets, const uint32_t& height)
+{
+    // First write the current file position for the current height in the header section.
+    uint64_t curr_pos = m_file.size();
+    uint64_t cursor = FILE_HEADER_LEN + (m_index * (sizeof(uint64_t) + sizeof(uint32_t)));
+    m_file.seek(cursor, SEEK_SET);
+    m_file << height;
+    m_file << curr_pos;
+    // Next append the positions of the unspent offsets in the block at this height.
+    m_file.seek(curr_pos, SEEK_SET);
+    WriteCompactSize(m_file, unspent_offsets.size());
+    for (const auto& offset : unspent_offsets) {
+        WriteCompactSize(m_file, offset);
+    }
+    m_index++;
+    return m_file.Commit();
+}
+
+HintsfileReader::HintsfileReader(AutoFile& file) : m_file(file.release())
+{
+    std::array<uint8_t, 4> magic{};
+    m_file >> magic;
+    if (magic != FILE_MAGIC) {
+        throw std::ios_base::failure("HintsfileReader: This is not a hint file.");
+    }
+    uint8_t version{};
+    m_file >> version;
+    if (version != FILE_VERSION) {
+        throw std::ios_base::failure("HintsfileReader: Unsupported file version.");
+    }
+    m_file >> m_stop_height;
+    for (uint32_t index = 1; index <= m_stop_height; index++) {
+        uint32_t height;
+        uint64_t file_pos;
+        m_file >> height;
+        m_file >> file_pos;
+        m_height_to_file_pos.emplace(height, file_pos);
+    }
+}
+HintsfileReader::HintsfileReader(HintsfileReader&& o) noexcept : m_file{o.m_file.release()}, m_stop_height{o.m_stop_height}, m_height_to_file_pos{std::move(o.m_height_to_file_pos)} {};
+
+std::vector<uint64_t> HintsfileReader::ReadBlock(const uint32_t& height)
+{
+    uint64_t file_pos = m_height_to_file_pos.at(height);
+    m_file.seek(file_pos, SEEK_SET);
+    uint64_t num_unspents = ReadCompactSize(m_file);
+    std::vector<uint64_t> offsets{};
+    offsets.reserve(num_unspents);
+    for (uint64_t i = 0; i < num_unspents; i++) {
+        offsets.push_back(ReadCompactSize(m_file));
+    }
+    return offsets;
 }
--- src/swiftsync.h
+++ src/swiftsync.h
@@ -5,9 +5,18 @@
 #define BITCOIN_SWIFTSYNC_H
 #include <hash.h>
 #include <primitives/transaction.h>
+#include <streams.h>
+#include <uint256.h>
+#include <array>
 #include <cstdint>
+#include <unordered_map>
+#include <vector>
 
 namespace swiftsync {
+inline constexpr std::array<uint8_t, 4> FILE_MAGIC = {0x55, 0x54, 0x58, 0x4f};
+const uint8_t FILE_VERSION = 0x00;
+// file magic length + version + height
+const uint64_t FILE_HEADER_LEN = 9;
 /** An aggregate for the SwiftSync protocol.
  * This class is intentionally left opaque, as internal changes may occur,
  * but all aggregates will have the concept of "adding" and "spending" an
@@ -31,6 +40,51 @@ public:
     void Add(const COutPoint& outpoint);
     /** Spend an outpoint used in a block. */
     void Spend(const COutPoint& outpoint);
+};
+
+/**
+ * Create a new hint file for writing.
+ */
+class HintsfileWriter
+{
+private:
+    AutoFile m_file;
+    uint32_t m_index{};
+
+public:
+    // Create a new hint file writer that will encode `preallocate` number of blocks.
+    HintsfileWriter(AutoFile& file, const uint32_t& preallocate);
+    ~HintsfileWriter()
+    {
+        std::ignore = m_file.fclose();
+    }
+    // Write the next hints to file.
+    bool WriteNextUnspents(const std::vector<uint64_t>& unspent_offsets, const uint32_t& height);
+    // Close the underlying file.
+    int Close() { return m_file.fclose(); };
+};
+
+/**
+ * A file that encodes the UTXO set state at a particular block height.
+ */
+class HintsfileReader
+{
+private:
+    AutoFile m_file;
+    uint32_t m_stop_height;
+    std::unordered_map<uint32_t, uint64_t> m_height_to_file_pos;
+
+public:
+    HintsfileReader(AutoFile& file);
+    HintsfileReader(HintsfileReader&& o) noexcept;
+    ~HintsfileReader()
+    {
+        std::ignore = m_file.fclose();
+    }
+    /** Read the hints for the specified block height. */
+    std::vector<uint64_t> ReadBlock(const uint32_t& height);
+    /** The height this file encodes up to. */
+    uint32_t StopHeight() { return m_stop_height; };
 };
 } // namespace swiftsync
 #endif // BITCOIN_SWIFTSYNC_H
