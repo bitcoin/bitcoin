c1b1ef37bb89f682648d1ef2dd7853bd7b2afac8
rustaceanrob <rob.netzke@gmail.com>
2025-11-12 10:05:25 +0000

    swiftsync: Add hintfile reader/writer
    The set of all inputs ever created and the set of all outputs are not
    equivalent sets. Naturally, some outputs will be unspent. To update the
    aggregate correctly, we require "hints" as to what outputs will remain
    in the UTXO set at a particular height in the chain. These hints allow a
    client to verify `inputs = outputs - UTXOs` for a terminal block height.
    
    The hints are encoded by enumerating each output in the block, and
    recording if an output is in the final UTXO set. Instead of recording
    the literal index in each block, the difference between the last element
    in the UTXO set and the next one is recorded to the file. This is akin
    to run-length encoding.
    
    A header section is added so multiple blocks may be encoded
    concurrently. The header section denotes the block height and file
    position.

--- src/swiftsync.h
+++ src/swiftsync.h
@@ -5,9 +5,19 @@
 #define BITCOIN_SWIFTSYNC_H
 #include <arith_uint256.h>
 #include <primitives/transaction.h>
+#include <streams.h>
+#include <uint256.h>
 #include <util/hasher.h>
+#include <array>
+#include <cstdint>
+#include <unordered_map>
+#include <vector>
 
 namespace swiftsync {
+inline constexpr std::array<uint8_t, 4> FILE_MAGIC = {0x55, 0x54, 0x58, 0x4f};
+const uint8_t FILE_VERSION = 0x00;
+// file magic length + version + height
+const uint64_t FILE_HEADER_LEN = 9;
 /** An aggregate for the SwiftSync protocol.
  * This class is intentionally left opaque, as internal changes may occur,
  * but all aggregates will have the concept of "create" and "spending" an
@@ -26,6 +36,51 @@ public:
     bool IsBalanced() const { return m_created == m_spent; }
     void Create(const COutPoint& outpoint) { m_created += m_hasher(outpoint); }
     void Spend(const COutPoint& outpoint) { m_spent += m_hasher(outpoint); }
+};
+
+/**
+ * Create a new hint file for writing.
+ */
+class HintsfileWriter
+{
+private:
+    AutoFile m_file;
+    uint32_t m_index{};
+
+public:
+    // Create a new hint file writer that will encode `preallocate` number of blocks.
+    HintsfileWriter(AutoFile& file, const uint32_t& preallocate);
+    ~HintsfileWriter()
+    {
+        (void)m_file.fclose();
+    }
+    // Write the next hints to file.
+    bool WriteNextUnspents(const std::vector<uint64_t>& unspent_offsets, const uint32_t& height);
+    // Close the underlying file.
+    int Close() { return m_file.fclose(); };
+};
+
+/**
+ * A file that encodes the UTXO set state at a particular block height.
+ */
+class HintsfileReader
+{
+private:
+    AutoFile m_file;
+    uint32_t m_stop_height;
+    std::unordered_map<uint32_t, uint64_t> m_height_to_file_pos;
+
+public:
+    HintsfileReader(AutoFile& file);
+    HintsfileReader(HintsfileReader&& o) noexcept;
+    ~HintsfileReader()
+    {
+        (void)m_file.fclose();
+    }
+    /** Read the hints for the specified block height. */
+    std::vector<uint64_t> ReadBlock(const uint32_t& height);
+    /** The height this file encodes up to. */
+    uint32_t StopHeight() { return m_stop_height; };
 };
 } // namespace swiftsync
 #endif // BITCOIN_SWIFTSYNC_H
