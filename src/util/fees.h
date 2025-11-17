08f4c7d9c42bbe68034857a9ecfebd141137de8e
ismaelsadeeq <abubakarsadiqismail@proton.me>
2025-11-17 14:19:57 +0000

    fees: add abstract fee estimator
    i  - FeeRateEstimator abstract class is the base class of fee rate estimators
         Derived classes must provide concrete implementation
         of the virtual methods.
    
    ii - FeeRateEstimatorResult struct represent the response returned by
         a fee rate estimator.
    
    iii - FeeRateEstimatorType will be used to identify fee estimators.
          Each time a new fee estimator is added, a corresponding
          enum value should be added.
    
    Co-authored-by: willcl-ark <will@256k1.dev>

--- src/util/fees.h
+++ src/util/fees.h
@@ -5,11 +5,17 @@
 #ifndef BITCOIN_UTIL_FEES_H
 #define BITCOIN_UTIL_FEES_H
 
+#include <util/feefrac.h>
+
+#include <compare>
+#include <string>
+#include <vector>
+
 /* Used to determine type of fee estimation requested */
 enum class FeeEstimateMode {
     UNSET,        //!< Use default settings based on other criteria
-    ECONOMICAL,   //!< Force estimateSmartFee to use non-conservative estimates
-    CONSERVATIVE, //!< Force estimateSmartFee to use conservative estimates
+    ECONOMICAL,   //!< Force FeeRateEstimator to return non-conservative estimates
+    CONSERVATIVE, //!< Force FeeRateEstimator to return conservative estimates
 };
 
 enum class FeeSource {
@@ -18,6 +24,44 @@ enum class FeeSource {
     PAYTXFEE,
     FALLBACK,
     REQUIRED,
+};
+
+/**
+ * @enum FeeRateEstimatorType
+ * Identifier for fee rate estimator.
+ */
+enum class FeeRateEstimatorType {};
+
+/**
+ * @struct FeeRateEstimatorResult
+ * Represents the response returned by a fee rate estimator.
+ */
+struct FeeRateEstimatorResult {
+    //! This identifies which fee rate estimator is providing this feerate estimate
+    FeeRateEstimatorType feerate_estimator;
+
+    //! Fee rate sufficient to confirm a package within target
+    FeePerVSize feerate{0, 0};
+
+    //! The block height at which the fee rate estimator was made
+    unsigned int current_block_height{0};
+
+    //! The returned target at which the package is likely to confirm within
+    int returned_target{0};
+
+    std::vector<std::string> errors{}; // error messages encountered
+
+    /**
+     * Compare two FeeRateEstimatorResult objects based on fee rate
+     * @param other The other FeeRateEstimatorResult object to compare with
+     * @return strong ordering of either less, greater or equal; based on strict feerate comparison.
+     */
+    auto operator<=>(const FeeRateEstimatorResult& other) const
+    {
+        if (feerate << other.feerate) return std::strong_ordering::less;
+        if (feerate >> other.feerate) return std::strong_ordering::greater;
+        return std::strong_ordering::equal;
+    }
 };
 
 #endif // BITCOIN_UTIL_FEES_H
