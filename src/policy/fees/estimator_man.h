a60b8b031fc26d83b7b866e8db1cbd883c57e78f
ismaelsadeeq <abubakarsadiqismail@proton.me>
2025-11-17 15:28:42 +0000

    fees: add typed accessor for FeeRateEstimator

--- src/policy/fees/estimator_man.h
+++ src/policy/fees/estimator_man.h
@@ -23,6 +23,17 @@ private:
     //! Map of all registered estimators to their unique pointers.
     std::unordered_map<FeeRateEstimatorType, std::unique_ptr<FeeRateEstimator>> estimators;
 
+    //! Return a pointer to a fee rate estimator of the given type, or nullptr if not found.
+    template <class T>
+    T* GetFeeRateEstimator(FeeRateEstimatorType estimator_type)
+    {
+        auto it = estimators.find(estimator_type);
+        if (it == estimators.end()) {
+            return nullptr;
+        }
+        return dynamic_cast<T*>(it->second.get());
+    }
+
 public:
     /**
      * Register a fee rate estimator.
