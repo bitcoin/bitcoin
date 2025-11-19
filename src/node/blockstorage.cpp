c7f8ca71c8930da7828eeebe409924e9948e88cf
Novo <eunovo9@gmail.com>
2025-11-19 12:12:23 +0100

    init: keep bg init thread until headers sync is done

--- src/node/blockstorage.cpp
+++ src/node/blockstorage.cpp
@@ -1201,27 +1201,8 @@ BlockManager::BlockManager(const util::SignalInterrupt& interrupt, Options opts)
     }
 }
 
-class ImportingNow
-{
-    std::atomic<bool>& m_importing;
-
-public:
-    ImportingNow(std::atomic<bool>& importing) : m_importing{importing}
-    {
-        assert(m_importing == false);
-        m_importing = true;
-    }
-    ~ImportingNow()
-    {
-        assert(m_importing == true);
-        m_importing = false;
-    }
-};
-
 void ImportBlocks(ChainstateManager& chainman, std::span<const fs::path> import_paths, bool force_activation)
 {
-    ImportingNow imp{chainman.m_blockman.m_importing};
-
     bool reindexed = false;
     // -reindex
     if (!chainman.m_blockman.m_blockfiles_indexed) {
@@ -1304,7 +1285,6 @@ void ImportBlocks(ChainstateManager& chainman, std::span<const fs::path> import_
             return;
         }
     }
-    // End scope of ImportingNow
 }
 
 std::ostream& operator<<(std::ostream& os, const BlockfileType& type) {
