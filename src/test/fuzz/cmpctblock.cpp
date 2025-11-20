62f2b9e4601ba34e504171f87577f315af51f3c9
Eugene Siegel <elzeigel@gmail.com>
2025-11-20 14:58:58 -0500

    net, fuzz: move CMPCTBLOCK_VERSION to header, use in cmpctblock harness
    The cmpctblock harness now adds peers and processes the sendcmpct message.

--- src/test/fuzz/cmpctblock.cpp
+++ src/test/fuzz/cmpctblock.cpp
@@ -9,6 +9,8 @@
 #include <test/fuzz/FuzzedDataProvider.h>
 #include <test/fuzz/fuzz.h>
 #include <test/fuzz/util.h>
+#include <test/fuzz/util/net.h>
+#include <test/util/net.h>
 #include <test/util/setup_common.h>
 #include <test/util/validation.h>
 #include <util/fs.h>
@@ -84,10 +86,29 @@ FUZZ_TARGET(cmpctblock, .init=initialize_cmpctblock)
     auto& chainman = static_cast<TestChainstateManager&>(*setup->m_node.chainman);
     chainman.ResetIbd();
 
+    LOCK(NetEventsInterface::g_msgproc_mutex);
+
+    std::vector<CNode*> peers;
+    auto& connman = *static_cast<ConnmanTestMsg*>(setup->m_node.connman.get());
+    for (int i = 0; i < 4; ++i) {
+        peers.push_back(ConsumeNodeAsUniquePtr(fuzzed_data_provider, i).release());
+        CNode& p2p_node = *peers.back();
+        FillNode(fuzzed_data_provider, connman, p2p_node);
+        connman.AddTestNode(p2p_node);
+    }
+
     LIMITED_WHILE(fuzzed_data_provider.ConsumeBool(), 1000)
     {
+        CSerializedNetMsg net_msg;
+        bool set_net_msg = true;
+
         CallOneOf(
             fuzzed_data_provider,
+            [&]() {
+                // Send a sendcmpct message, optionally setting hb mode.
+                bool hb = fuzzed_data_provider.ConsumeBool();
+                net_msg = NetMsg::Make(NetMsgType::SENDCMPCT, /*high_bandwidth=*/hb, /*version=*/CMPCTBLOCKS_VERSION);
+            },
             [&]() {
                 // Set mock time randomly or to tip's time.
                 if (fuzzed_data_provider.ConsumeBool()) {
@@ -96,7 +117,25 @@ FUZZ_TARGET(cmpctblock, .init=initialize_cmpctblock)
                     const uint64_t tip_time = WITH_LOCK(::cs_main, return setup->m_node.chainman->ActiveChain().Tip()->GetBlockTime());
                     SetMockTime(tip_time);
                 }
+
+                set_net_msg = false;
             });
+
+        if (!set_net_msg) {
+            continue;
+        }
+
+        CNode& random_node = *PickValue(fuzzed_data_provider, peers);
+        connman.FlushSendBuffer(random_node);
+        (void)connman.ReceiveMsgFrom(random_node, std::move(net_msg));
+
+        bool more_work{true};
+        while (more_work) {
+            random_node.fPauseSend = false;
+
+            more_work = connman.ProcessMessagesOnce(random_node);
+            setup->m_node.peerman->SendMessages(&random_node);
+        }
     }
 
     setup->m_node.validation_signals->SyncWithValidationInterfaceQueue();
