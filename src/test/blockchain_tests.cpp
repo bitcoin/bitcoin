fa7e222a23266e258d82e085f62cbf89d20dc8f3
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2025-11-20 16:44:05 +0100

    clang-format: Set Bitcoin Core IncludeCategories
    Replace the default llvm include categories with the ones specific to
    Bitcoin Core.

--- src/test/blockchain_tests.cpp
+++ src/test/blockchain_tests.cpp
@@ -2,14 +2,14 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <boost/test/unit_test.hpp>
-
 #include <chain.h>
 #include <node/blockstorage.h>
 #include <rpc/blockchain.h>
 #include <sync.h>
 #include <test/util/setup_common.h>
 #include <util/string.h>
+
+#include <boost/test/unit_test.hpp>
 
 #include <cstdlib>
 
