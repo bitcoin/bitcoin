9c667c362a1639b48113a3657882b751f475082c
Sjors Provoost <sjors@sprovoost.nl>
2025-11-21 16:12:27 +0100

    mining: track non-mempool memory usage
    IPC clients can hold on to block templates indefinately, which has the
    same impact as when the node holds a shared pointer to the
    CBlockTemplate. Because each template in turn tracks CTransactionRefs,
    transactions that are removed from the mempool will have not have
    their memory cleared.
    
    This commit adds bookkeeping to the block template constructor and
    destructor that will let us track the resulting memory footprint.

--- src/node/context.h
+++ src/node/context.h
@@ -5,6 +5,8 @@
 #ifndef BITCOIN_NODE_CONTEXT_H
 #define BITCOIN_NODE_CONTEXT_H
 
+#include <node/types.h>
+
 #include <atomic>
 #include <cstdlib>
 #include <functional>
@@ -67,7 +69,11 @@ struct NodeContext {
     std::unique_ptr<AddrMan> addrman;
     std::unique_ptr<CConnman> connman;
     std::unique_ptr<CTxMemPool> mempool;
-    //! Cache latest getblocktemplate result for BIP 22 long polling
+    //! Track how many templates (which we hold on to on behalf of connected IPC
+    //! clients) are referencing each transaction.
+    TxTemplateMap template_tx_refs;
+    //! Cache latest getblocktemplate result for BIP 22 long polling. Must be cleared
+    //! before template_tx_refs.
     std::unique_ptr<interfaces::BlockTemplate> gbt_template;
     std::unique_ptr<const NetGroupManager> netgroupman;
     std::unique_ptr<CBlockPolicyEstimator> fee_estimator;
