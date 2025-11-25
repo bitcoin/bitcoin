8e4294a7abd0ac30699be269c9dd3070cf4f21d8
Sjors Provoost <sjors@sprovoost.nl>
2025-11-25 18:16:43 +0100

    rpc: move static block_template to node context
    The getblocktemplate RPC uses a static BlockTemplate, which goes out
    of scope only after the node completed its shutdown sequence.
    
    This becomes a problem when a later commit implements a destructor
    that uses m_node.

--- src/node/context.h
+++ src/node/context.h
@@ -27,6 +27,7 @@ class NetGroupManager;
 class PeerManager;
 class TorController;
 namespace interfaces {
+class BlockTemplate;
 class Chain;
 class ChainClient;
 class Mining;
@@ -67,6 +68,8 @@ struct NodeContext {
     std::unique_ptr<AddrMan> addrman;
     std::unique_ptr<CConnman> connman;
     std::unique_ptr<CTxMemPool> mempool;
+    //! Cache latest getblocktemplate result for BIP 22 long polling
+    std::unique_ptr<interfaces::BlockTemplate> gbt_result;
     std::unique_ptr<const NetGroupManager> netgroupman;
     std::unique_ptr<CBlockPolicyEstimator> fee_estimator;
     std::unique_ptr<PeerManager> peerman;
