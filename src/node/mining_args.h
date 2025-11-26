ee2738cb385fa3f5722ccb6804e238af0f06ffc3
Sjors Provoost <sjors@sprovoost.nl>
2025-11-26 18:19:26 +0100

    mining: parse block creation args in mining_args
    Move the argument parsing for -blockmaxweight, -blockreservedweight,
    -blockmintxfee and -blockversion out of init.cpp to a dedicated
    mining_args.cpp.

---
+++ src/node/mining_args.h
@@ -0,0 +1,16 @@
+// Copyright (c) 2025 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_NODE_MINING_ARGS_H
+#define BITCOIN_NODE_MINING_ARGS_H
+
+#include <util/result.h>
+
+class ArgsManager;
+
+namespace node {
+[[nodiscard]] util::Result<void> ApplyArgsManOptions(const ArgsManager& args);
+} // namespace node
+
+#endif // BITCOIN_NODE_MINING_ARGS_H
