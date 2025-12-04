1bd6cae51fedeff62755e03ddcb9346b868f6d83
Sjors Provoost <sjors@sprovoost.nl>
2025-12-04 09:19:50 +0100

    mining: add new getCoinbaseTx() returning a struct
    Introduce a new method intended to replace getCoinbaseRawTx(), which
    provides a struct with everything clients need to construct a coinbase.
    This is safer than providing a raw dummy coinbase that clients then have
    to manipulate.
    
    The CoinbaseTxTemplate data is populated during the dummy transaction
    generation and stored in struct CBlockTemplate.
    
    Expand the interface_ipc.py functional test to document its usage
    and ensure equivalence.

--- src/node/interfaces.cpp
+++ src/node/interfaces.cpp
@@ -83,6 +83,7 @@ using interfaces::Node;
 using interfaces::WalletLoader;
 using node::BlockAssembler;
 using node::BlockWaitOptions;
+using node::CoinbaseTxTemplate;
 using util::Join;
 
 namespace node {
@@ -888,6 +889,11 @@ public:
     CTransactionRef getCoinbaseRawTx() override
     {
         return m_block_template->block.vtx[0];
+    }
+
+    CoinbaseTxTemplate getCoinbaseTx() override
+    {
+        return m_block_template->m_coinbase_tx_template;
     }
 
     std::vector<unsigned char> getCoinbaseCommitment() override
