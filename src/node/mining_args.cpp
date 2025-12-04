e0d0a575836336fb3924c864cdf940220384ad09
Sjors Provoost <sjors@sprovoost.nl>
2025-12-04 14:26:37 +0100

    mining: store mining args in NodeContext
    Instead of parsing arguments like -blockmaxweight each time a block
    template is generated, do it once in ApplyArgsManOptions().
    
    These arguments are stored in a new struct MiningArgs.
    
    The BlockAssembler::Options struct is removed in favor of passing
    both MiningArgs and BlockCreateOptions.
    
    This disentangles node configuration from client (or test) options.

--- src/node/mining_args.cpp
+++ src/node/mining_args.cpp
@@ -16,26 +16,30 @@ using common::AmountErrMsg;
 
 namespace node {
 
-util::Result<void> ApplyArgsManOptions(const ArgsManager& args)
+util::Result<void> ApplyArgsManOptions(const ArgsManager& args, MiningArgs& mining_args)
 {
     if (const auto arg{args.GetArg("-blockmintxfee")}) {
-        if (!ParseMoney(*arg)) {
-            return util::Error{AmountErrMsg("blockmintxfee", *arg)};
-        }
+        std::optional<CAmount> block_min_tx_fee{ParseMoney(*arg)};
+        if (!block_min_tx_fee) return util::Error{AmountErrMsg("blockmintxfee", *arg)};
+        mining_args.blockMinFeeRate = CFeeRate{*block_min_tx_fee};
     }
 
-    const auto max_block_weight = args.GetIntArg("-blockmaxweight", DEFAULT_BLOCK_MAX_WEIGHT);
+    const size_t max_block_weight = args.GetIntArg("-blockmaxweight", DEFAULT_BLOCK_MAX_WEIGHT);
     if (max_block_weight > MAX_BLOCK_WEIGHT) {
         return util::Error{strprintf(_("Specified -blockmaxweight (%d) exceeds consensus maximum block weight (%d)"), max_block_weight, MAX_BLOCK_WEIGHT)};
     }
+    mining_args.default_block_max_weight = max_block_weight;
 
-    const auto block_reserved_weight = args.GetIntArg("-blockreservedweight", DEFAULT_BLOCK_RESERVED_WEIGHT);
+    const size_t block_reserved_weight = args.GetIntArg("-blockreservedweight", DEFAULT_BLOCK_RESERVED_WEIGHT);
     if (block_reserved_weight > MAX_BLOCK_WEIGHT) {
         return util::Error{strprintf(_("Specified -blockreservedweight (%d) exceeds consensus maximum block weight (%d)"), block_reserved_weight, MAX_BLOCK_WEIGHT)};
     }
     if (block_reserved_weight < MINIMUM_BLOCK_RESERVED_WEIGHT) {
         return util::Error{strprintf(_("Specified -blockreservedweight (%d) is lower than minimum safety value of (%d)"), block_reserved_weight, MINIMUM_BLOCK_RESERVED_WEIGHT)};
     }
+    mining_args.default_block_reserved_weight = block_reserved_weight;
+
+    mining_args.print_modified_fee = args.GetBoolArg("-printpriority", mining_args.print_modified_fee);
 
     return {};
 }
