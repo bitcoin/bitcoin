9b86140164a04e6f4cace16dec2109fcfbda5e41
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2025-12-09 01:00:28 +0000

    guix: Build for macOS using LLVM toolchain only

--- contrib/guix/manifest.scm
+++ contrib/guix/manifest.scm
@@ -620,7 +620,6 @@ inspecting signatures in Mach-O binaries.")
         gzip
         xz
         ;; Build tools
-        gcc-toolchain-14
         cmake-minimal
         gnu-make
         ninja
@@ -633,6 +632,7 @@ inspecting signatures in Mach-O binaries.")
   (let ((target (getenv "HOST")))
     (cond ((string-suffix? "-mingw32" target)
            (list zip
+                 gcc-toolchain-14
                  (make-mingw-pthreads-cross-toolchain "x86_64-w64-mingw32")
                  nsis-x86_64
                  nss-certs
@@ -640,6 +640,7 @@ inspecting signatures in Mach-O binaries.")
           ((string-contains target "-linux-")
            (list bison
                  pkg-config
+                 gcc-toolchain-14
                  (list gcc-toolchain-14 "static")
                  (make-bitcoin-cross-toolchain target)))
           ((string-contains target "darwin")
