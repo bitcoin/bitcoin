faad5ffdfdc284c38d6b6e76d8a3a68990025a1b
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2025-12-12 15:11:24 +0100

    refactor: Use NodeClock::time_point for m_addr_token_timestamp
    This refactor makes the field a bit more type-safe.

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -377,7 +377,7 @@ struct Peer {
      *  permit self-announcement. */
     double m_addr_token_bucket GUARDED_BY(NetEventsInterface::g_msgproc_mutex){1.0};
     /** When m_addr_token_bucket was last updated */
-    std::chrono::microseconds m_addr_token_timestamp GUARDED_BY(NetEventsInterface::g_msgproc_mutex){GetTime<std::chrono::microseconds>()};
+    NodeClock::time_point m_addr_token_timestamp GUARDED_BY(NetEventsInterface::g_msgproc_mutex){NodeClock::now()};
     /** Total number of addresses that were dropped due to rate limiting. */
     std::atomic<uint64_t> m_addr_rate_limited{0};
     /** Total number of addresses that were processed (excludes rate-limited ones). */
@@ -5415,10 +5415,10 @@ void PeerManagerImpl::ProcessAddrs(std::string_view msg_type, CNode& pfrom, Peer
     const auto current_a_time{Now<NodeSeconds>()};
 
     // Update/increment addr rate limiting bucket.
-    const auto current_time{GetTime<std::chrono::microseconds>()};
+    const auto current_time{NodeClock::now()};
     if (peer.m_addr_token_bucket < MAX_ADDR_PROCESSING_TOKEN_BUCKET) {
         // Don't increment bucket if it's already full
-        const auto time_diff = std::max(current_time - peer.m_addr_token_timestamp, 0us);
+        const auto time_diff{std::max<NodeClock::duration>(current_time - peer.m_addr_token_timestamp, 0us)};
         const double increment = Ticks<SecondsDouble>(time_diff) * MAX_ADDR_RATE_PER_SECOND;
         peer.m_addr_token_bucket = std::min<double>(peer.m_addr_token_bucket + increment, MAX_ADDR_PROCESSING_TOKEN_BUCKET);
     }
