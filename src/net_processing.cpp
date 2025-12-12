fa4263a0500283ffce9189edce016f74a42741f7
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2025-12-12 15:26:07 +0100

    refactor: Rename current_a_time to now_seconds
    This better reflects the meaning and use.

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -5412,7 +5412,6 @@ void PeerManagerImpl::ProcessAddrs(std::string_view msg_type, CNode& pfrom, Peer
 
     // Store the new addresses
     std::vector<CAddress> vAddrOk;
-    const auto current_a_time{Now<NodeSeconds>()};
 
     // Update/increment addr rate limiting bucket.
     const auto current_time{NodeClock::now()};
@@ -5427,6 +5426,7 @@ void PeerManagerImpl::ProcessAddrs(std::string_view msg_type, CNode& pfrom, Peer
     const bool rate_limited = !pfrom.HasPermission(NetPermissionFlags::Addr);
     uint64_t num_proc = 0;
     uint64_t num_rate_limit = 0;
+    const auto now_seconds{std::chrono::time_point_cast<std::chrono::seconds>(current_time)};
     std::shuffle(vAddr.begin(), vAddr.end(), m_rng);
     for (CAddress& addr : vAddr)
     {
@@ -5448,8 +5448,8 @@ void PeerManagerImpl::ProcessAddrs(std::string_view msg_type, CNode& pfrom, Peer
         if (!MayHaveUsefulAddressDB(addr.nServices) && !HasAllDesirableServiceFlags(addr.nServices))
             continue;
 
-        if (addr.nTime <= NodeSeconds{100000000s} || addr.nTime > current_a_time + 10min) {
-            addr.nTime = current_a_time - 5 * 24h;
+        if (addr.nTime <= NodeSeconds{100000000s} || addr.nTime > now_seconds + 10min) {
+            addr.nTime = now_seconds - 5 * 24h;
         }
         AddAddressKnown(peer, addr);
         if (m_banman && (m_banman->IsDiscouraged(addr) || m_banman->IsBanned(addr))) {
@@ -5458,7 +5458,7 @@ void PeerManagerImpl::ProcessAddrs(std::string_view msg_type, CNode& pfrom, Peer
         }
         ++num_proc;
         const bool reachable{g_reachable_nets.Contains(addr)};
-        if (addr.nTime > current_a_time - 10min && !peer.m_getaddr_sent && vAddr.size() <= 10 && addr.IsRoutable()) {
+        if (addr.nTime > now_seconds - 10min && !peer.m_getaddr_sent && vAddr.size() <= 10 && addr.IsRoutable()) {
             // Relay to a limited number of other nodes
             RelayAddress(pfrom.GetId(), addr, reachable);
         }
