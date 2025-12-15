7b2d46a69de5eb63997cb6d071fcf571c215ffea
Hodlinator <172445034+hodlinator@users.noreply.github.com>
2025-12-15 11:24:16 +0100

    qa: Add --debug_cmd
    Enables automated attaching of debuggers.
    
    The special identifier "$PID$" is substituted by the actual bitcoind process id number.
    
    Attaching with LLDB in Tmux pane:
    $ ./build/test/functional/feature_loadblock.py --debug_runs 2 --debug_cmd "tmux split-window -h lldb -p \$PID\$"
    
    Attaching with Sublime Text Debugger:
    $ ./build/test/functional/feature_loadblock.py --debug_runs 2 --debug_cmd "subl --command \"debugger {\\\"action\\\": \\\"start\\\", \\\"configuration\\\": {\\\"name\\\": \\\"foo\\\", \\\"type\\\": \\\"lldb\\\", \\\"request\\\": \\\"attach\\\", \\\"program\\\": \\\"dummyprogram\\\", \\\"pid\\\": \\\"\$PID\$\\\"}}\""

--- test/functional/test_framework/test_node.py
+++ test/functional/test_framework/test_node.py
@@ -93,6 +93,7 @@ class TestNode():
 
     run = 0
     debug_runs = None
+    debug_cmd = ""
 
     def __init__(self, i, datadir_path, *, chain, rpchost, timewait, timeout_factor, binaries, coverage_dir, cwd, extra_conf=None, extra_args=None, use_cli=False, start_perf=False, use_valgrind=False, version=None, v2transport=False, uses_wallet=False, ipcbind=False):
         """
@@ -302,7 +303,9 @@ class TestNode():
         if wait_for_debugger:
             # Note that it is the bitcoind process which is waiting, while the functional test
             # will continue executing, but probably spin at wait_for_rpc_connection().
-            self.log.info(f"bitcoind started (run #{TestNode.run}, node #{self.index}), waiting for debugger, PID: {self.process.pid}")
+            self.log.info(f"bitcoind started (run #{TestNode.run}, node #{self.index}), {'attaching debugger' if TestNode.debug_cmd else 'waiting for debugger'}, PID: {self.process.pid}")
+            if TestNode.debug_cmd:
+                subprocess.Popen(shlex.split(TestNode.debug_cmd.replace("$PID$", str(self.process.pid))), cwd=cwd)
         else:
             self.log.debug(f"bitcoind started (run #{TestNode.run}, node #{self.index}), waiting for RPC to come up")
         TestNode.run += 1
