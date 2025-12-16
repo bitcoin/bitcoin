66088e10d5842096495ad5c691002c9ec967bbcd
stickies-v <stickies-v@protonmail.com>
2025-12-16 15:37:21 +0000

    move-only: Move SourceLocation to util/log.h
    Introduce util/log.h as a lightweight header for code that only needs to emit
    log messages. Move SourceLocation into this header so log-emitting code no
    longer needs to include logging.h and depend on the full log management API.
    
    This is a move-only change and the first change of several changes that
    separate log generation from log handling. It also applies clang-format
    suggestions to the moved code.
    
    Review with --color-moved=dimmed-zebra --color-moved-ws=ignore-all-space
    
    Co-authored-by: Ryan Ofsky <ryan@ofsky.org>

--- src/logging.h
+++ src/logging.h
@@ -6,11 +6,11 @@
 #ifndef BITCOIN_LOGGING_H
 #define BITCOIN_LOGGING_H
 
-#include <crypto/siphash.h>
 #include <threadsafety.h>
 #include <tinyformat.h>
 #include <util/check.h>
 #include <util/fs.h>
+#include <util/log.h> // IWYU pragma: export
 #include <util/string.h>
 #include <util/time.h>
 
@@ -36,44 +36,6 @@ static constexpr bool DEFAULT_LOGLEVELALWAYS = false;
 extern const char * const DEFAULT_DEBUGLOGFILE;
 
 extern bool fLogIPs;
-
-/// Like std::source_location, but allowing to override the function name.
-class SourceLocation
-{
-public:
-    /// The func argument must be constructed from the C++11 __func__ macro.
-    /// Ref: https://en.cppreference.com/w/cpp/language/function.html#func
-    /// Non-static string literals are not supported.
-    SourceLocation(const char* func,
-                   std::source_location loc = std::source_location::current())
-        : m_func{func}, m_loc{loc} {}
-
-    std::string_view file_name() const { return m_loc.file_name(); }
-    std::uint_least32_t line() const { return m_loc.line(); }
-    std::string_view function_name_short() const { return m_func; }
-
-private:
-    std::string_view m_func;
-    std::source_location m_loc;
-};
-
-struct SourceLocationEqual {
-    bool operator()(const SourceLocation& lhs, const SourceLocation& rhs) const noexcept
-    {
-        return lhs.line() == rhs.line() && std::string_view(lhs.file_name()) == std::string_view(rhs.file_name());
-    }
-};
-
-struct SourceLocationHasher {
-    size_t operator()(const SourceLocation& s) const noexcept
-    {
-        // Use CSipHasher(0, 0) as a simple way to get uniform distribution.
-        return size_t(CSipHasher(0, 0)
-                      .Write(s.line())
-                      .Write(MakeUCharSpan(std::string_view{s.file_name()}))
-                      .Finalize());
-    }
-};
 
 struct LogCategory {
     std::string category;
---
+++ src/util/log.h
@@ -0,0 +1,53 @@
+// Copyright (c) The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_UTIL_LOG_H
+#define BITCOIN_UTIL_LOG_H
+
+#include <crypto/siphash.h>
+#include <span.h>
+
+#include <cstdint>
+#include <source_location>
+#include <string_view>
+
+/// Like std::source_location, but allowing to override the function name.
+class SourceLocation
+{
+public:
+    /// The func argument must be constructed from the C++11 __func__ macro.
+    /// Ref: https://en.cppreference.com/w/cpp/language/function.html#func
+    /// Non-static string literals are not supported.
+    SourceLocation(const char* func,
+                   std::source_location loc = std::source_location::current())
+        : m_func{func}, m_loc{loc} {}
+
+    std::string_view file_name() const { return m_loc.file_name(); }
+    std::uint_least32_t line() const { return m_loc.line(); }
+    std::string_view function_name_short() const { return m_func; }
+
+private:
+    std::string_view m_func;
+    std::source_location m_loc;
+};
+
+struct SourceLocationEqual {
+    bool operator()(const SourceLocation& lhs, const SourceLocation& rhs) const noexcept
+    {
+        return lhs.line() == rhs.line() && std::string_view(lhs.file_name()) == std::string_view(rhs.file_name());
+    }
+};
+
+struct SourceLocationHasher {
+    size_t operator()(const SourceLocation& s) const noexcept
+    {
+        // Use CSipHasher(0, 0) as a simple way to get uniform distribution.
+        return size_t(CSipHasher(0, 0)
+                          .Write(s.line())
+                          .Write(MakeUCharSpan(std::string_view{s.file_name()}))
+                          .Finalize());
+    }
+};
+
+#endif // BITCOIN_UTIL_LOG_H
