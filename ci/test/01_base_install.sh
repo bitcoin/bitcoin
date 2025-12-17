7a65437e23706e4820392dc456c3acccbf196dd6
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2025-12-17 20:28:42 +0000

    iwyu: Add patch to prefer angled brackets over quotes for includes

--- ci/test/01_base_install.sh
+++ ci/test/01_base_install.sh
@@ -81,6 +81,7 @@ fi
 
 if [[ "${RUN_TIDY}" == "true" ]]; then
   ${CI_RETRY_EXE} git clone --depth=1 https://github.com/include-what-you-use/include-what-you-use -b clang_"${TIDY_LLVM_V}" /include-what-you-use
+  (cd /include-what-you-use && patch -p1 < /ci_container_base/ci/test/01_iwyu.patch)
   cmake -B /iwyu-build/ -G 'Unix Makefiles' -DCMAKE_PREFIX_PATH=/usr/lib/llvm-"${TIDY_LLVM_V}" -S /include-what-you-use
   make -C /iwyu-build/ install "$MAKEJOBS"
 fi
