6fb639a089d83a2e4e4eeca26f3041d2da15d0c7
Sjors Provoost <sjors@sprovoost.nl>
2025-12-19 18:23:59 -0600

    test: cover feeThreshold = MAX_MONEY

--- test/functional/interface_ipc.py
+++ test/functional/interface_ipc.py
@@ -19,6 +19,7 @@ from test_framework.messages import (
     CTxInWitness,
     ser_uint256,
     COIN,
+    MAX_MONEY,
 )
 from test_framework.test_framework import BitcoinTestFramework
 from test_framework.util import (
@@ -288,12 +289,15 @@ class IPCInterfaceTest(BitcoinTestFramework):
                 coinbase.deserialize(coinbase_data)
                 assert_equal(coinbase.vin[0].prevout.hash, 0)
 
-                self.log.debug("Wait for a new template")
+                self.log.debug("Wait for a new template, get one after the tip updates")
                 waitoptions = self.capnp_modules['mining'].BlockWaitOptions()
                 waitoptions.timeout = timeout
-                waitoptions.feeThreshold = 1
+                # Ignore fee increases, wait only for the tip update
+                waitoptions.feeThreshold = MAX_MONEY
+                miniwallet.send_self_transfer(fee_rate=10, from_node=self.nodes[0])
                 template2 = await wait_and_do(
                     wait_next_template(template, stack, ctx, waitoptions),
+                    # This mines the transaction, so it won't be in the next template
                     lambda: self.generate(self.nodes[0], 1))
                 block2 = await self.parse_and_deserialize_block(template2, ctx)
                 assert_equal(len(block2.vtx), 1)
@@ -303,6 +307,7 @@ class IPCInterfaceTest(BitcoinTestFramework):
                 assert_equal(template3._has("result"), False)
 
                 self.log.debug("Wait for another, get one after increase in fees in the mempool")
+                waitoptions.feeThreshold = 1
                 template4 = await wait_and_do(
                     wait_next_template(template2, stack, ctx, waitoptions),
                     lambda: miniwallet.send_self_transfer(fee_rate=10, from_node=self.nodes[0]))
