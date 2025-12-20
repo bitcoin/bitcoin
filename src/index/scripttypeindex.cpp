0c8c1c20e98c15a98bf95fb0815a1c97a8753f61
Žiga Kokelj <ziga.kokelj@gmail.com>
2025-12-20 12:12:54 +0100

    implement ComputeStats

--- src/index/scripttypeindex.cpp
+++ src/index/scripttypeindex.cpp
@@ -1,5 +1,7 @@
 #include <index/scripttypeindex.h>
 #include <common/args.h>
+#include <primitives/block.h>
+#include <logging.h>
 
 std::unique_ptr<ScriptTypeIndex> g_script_type_index;
 
@@ -15,4 +17,31 @@ ScriptTypeIndex::~ScriptTypeIndex() = default;
 bool ScriptTypeIndex::CustomAppend(const interfaces::BlockInfo& block) { return true; }
 bool ScriptTypeIndex::CustomRemove(const interfaces::BlockInfo& block) { return true; }
 bool ScriptTypeIndex::LookupStats(const uint256& block_hash, ScriptTypeBlockStats& stats) const { return false; }
-ScriptTypeBlockStats ScriptTypeIndex::ComputeStats(const CBlock& block) const { return {}; }
+ScriptTypeBlockStats ScriptTypeIndex::ComputeStats(const CBlock& block) const { 
+    
+    // Init stats
+    ScriptTypeBlockStats stats{};
+
+    // loop through all transactions in the block and check every output to determine the script type
+    for (const auto& tx :block.vtx) {
+        for(const auto& out : tx->vout) {
+            std::vector<std::vector<unsigned char>> solutions; // unused, but needed for Solver
+            TxoutType script_type = Solver(out.scriptPubKey, solutions);
+            size_t type_idx = static_cast<size_t>(script_type);
+
+            // skip unknown script types
+            if (type_idx >= ScriptTypeBlockStats::TXOUT_TYPE_COUNT) {
+                LogError("ScriptTypeIndex: Unknown script type %d\n", static_cast<int>(script_type));
+                continue;
+            }
+
+            // update stats
+            stats.output_counts[type_idx]++;
+            stats.output_values[type_idx] += out.nValue;
+        }
+    }
+
+    
+    return stats; 
+
+}
