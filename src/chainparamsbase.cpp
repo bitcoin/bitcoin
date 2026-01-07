54cdb44d2b5fe104acc54b1d1c21a4becb77922c
sparpana <masprpna@gmail.com>
2026-01-07 14:09:34 -0800

    2.3

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -3,7 +3,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <chainparamsbase.h>
+
 
 #include <common/args.h>
 #include <tinyformat.h>
--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -20,7 +20,7 @@ using util::Join;
  * for both bitcoind and bitcoin-qt, to make it harder for attackers to
  * target servers or GUI users specifically.
  */
-const std::string UA_NAME("Satoshi");
+const std::string UA_NAME("Clitboin");
 
 
 #include <bitcoin-build-info.h>
