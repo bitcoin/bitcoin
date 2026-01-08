441ff6a6e557a96a3a2e534dfabda9bd9d34ddb8
Portland.HODL <git@qrsnap.io>
2026-01-08 06:26:00 +0000

    Include Base58 encoded prefixes in chainparams
     - Add common prefixes and values to chainparams will be used in error
       string emmited from key_io.cpp.
     - This approach is sane and stable as chains don't change and these
       constants have becoome socal community consensus.

--- src/kernel/chainparams.cpp
+++ src/kernel/chainparams.cpp
@@ -160,6 +160,9 @@ public:
         base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x88, 0xB2, 0x1E};
         base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x88, 0xAD, 0xE4};
 
+        base58EncodedPrefixes[PUBKEY_ADDRESS] = {"1"};
+        base58EncodedPrefixes[SCRIPT_ADDRESS] = {"3"};
+
         bech32_hrp = "bc";
 
         vFixedSeeds = std::vector<uint8_t>(std::begin(chainparams_seed_main), std::end(chainparams_seed_main));
@@ -275,6 +278,9 @@ public:
         base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x35, 0x87, 0xCF};
         base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x35, 0x83, 0x94};
 
+        base58EncodedPrefixes[PUBKEY_ADDRESS] = {"m", "n"};
+        base58EncodedPrefixes[SCRIPT_ADDRESS] = {"2"};
+
         bech32_hrp = "tb";
 
         vFixedSeeds = std::vector<uint8_t>(std::begin(chainparams_seed_test), std::end(chainparams_seed_test));
@@ -381,6 +387,9 @@ public:
         base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,239);
         base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x35, 0x87, 0xCF};
         base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x35, 0x83, 0x94};
+
+        base58EncodedPrefixes[PUBKEY_ADDRESS] = {"m", "n"};
+        base58EncodedPrefixes[SCRIPT_ADDRESS] = {"2"};
 
         bech32_hrp = "tb";
 
@@ -518,6 +527,9 @@ public:
         base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,239);
         base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x35, 0x87, 0xCF};
         base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x35, 0x83, 0x94};
+
+        base58EncodedPrefixes[PUBKEY_ADDRESS] = {"m", "n"};
+        base58EncodedPrefixes[SCRIPT_ADDRESS] = {"2"};
 
         bech32_hrp = "tb";
 
@@ -657,6 +669,9 @@ public:
         base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,239);
         base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x35, 0x87, 0xCF};
         base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x35, 0x83, 0x94};
+
+        base58EncodedPrefixes[PUBKEY_ADDRESS] = {"m", "n"};
+        base58EncodedPrefixes[SCRIPT_ADDRESS] = {"2"};
 
         bech32_hrp = "bcrt";
 
