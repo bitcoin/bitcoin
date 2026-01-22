6f7b4323cb46687ba9df7073a9cde427985d2dfc
Bruno Garcia <brunoely.gc@gmail.com>
2026-01-22 11:01:01 -0300

    test: remove UNKNOWN_ERROR from script_tests

--- src/test/script_tests.cpp
+++ src/test/script_tests.cpp
@@ -52,7 +52,6 @@ struct ScriptErrorDesc
 
 static ScriptErrorDesc script_errors[]={
     {SCRIPT_ERR_OK, "OK"},
-    {SCRIPT_ERR_UNKNOWN_ERROR, "UNKNOWN_ERROR"},
     {SCRIPT_ERR_EVAL_FALSE, "EVAL_FALSE"},
     {SCRIPT_ERR_OP_RETURN, "OP_RETURN"},
     {SCRIPT_ERR_SCRIPT_SIZE, "SCRIPT_SIZE"},
