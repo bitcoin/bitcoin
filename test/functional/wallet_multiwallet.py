4d0b3b88c8fd45ecbec7c29ff549e1445f9429d6
Hodlinator <172445034+hodlinator@users.noreply.github.com>
2026-01-26 11:49:03 +0100

    refactor(qa): Remove unused option
    Last use was removed in 0d32d661481f099af572e7a08a50e17bcc165c44.

--- test/functional/wallet_multiwallet.py
+++ test/functional/wallet_multiwallet.py
@@ -51,13 +51,6 @@ class MultiWalletTest(BitcoinTestFramework):
     def skip_test_if_missing_module(self):
         self.skip_if_no_wallet()
 
-    def add_options(self, parser):
-        parser.add_argument(
-            '--data_wallets_dir',
-            default=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data/wallets/'),
-            help='Test data with wallet directories (default: %(default)s)',
-        )
-
     def run_test(self):
         node = self.nodes[0]
 
