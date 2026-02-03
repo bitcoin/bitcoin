b757c3a1bc847bc75df6cce7f8232e109df33dcc
Ryan Ofsky <ryan@ofsky.org>
2026-02-03 16:29:56 -0500

    test: prevent race condition in feature_init.py
    The next commit changes index code to avoid spawning sync threads with
    no syncing is required. This means 'txindex thread start', 'block filter
    index thread start', 'coinstatsindex thread start' messages will not be
    logged every startup, only when syncing is required.
    
    In current the feature_init.py test, this change could a introduce a
    race condition if the test is not able to kill the node fast enough for
    syncing to be necessary on the next restart, so 'thread start' messages
    would not show up.
    
    Fix this by treating 'thread start' logs differently and checking for
    them in earlier restarts, not just in the latest restart.

--- test/functional/feature_init.py
+++ test/functional/feature_init.py
@@ -61,7 +61,12 @@ class InitTest(BitcoinTestFramework):
                 os.kill(node.process.pid, signal.CTRL_BREAK_EVENT)
             else:
                 node.process.terminate()
-            assert_equal(0, node.process.wait())
+            # Don't check exit code returned by wait(). Normally exit code will
+            # be 0, but if the process is killed early, before the bitcoind
+            # signal handler is installed, it will exit with other values like
+            # SIGTERM (-15) on linux or STATUS_DLL_INIT_FAILED (0xc0000142) on
+            # windows.
+            node.process.wait()
 
         lines_to_terminate_after = [
             b'Validating signatures for all blocks',
@@ -88,9 +93,24 @@ class InitTest(BitcoinTestFramework):
         if self.is_wallet_compiled():
             lines_to_terminate_after.append(b'Verifying wallet')
 
+        index_threads = [
+            b'txindex thread start',
+            b'block filter index thread start',
+            b'coinstatsindex thread start',
+            b'txospenderindex thread start',
+        ]
+        log_start_byte = node.debug_log_size(mode="rb")
         for terminate_line in lines_to_terminate_after:
             self.log.info(f"Starting node and will terminate after line {terminate_line}")
-            with node.busy_wait_for_debug_log([terminate_line]):
+            # Index thread start messages are only printed if index is behind
+            # the current chain tip at startup and needs to sync. If the index
+            # is already in sync during startup no messages will be printed.
+            # Set `start_byte` below for this reason to look for the index
+            # thread start messages, starting earlier, from the begining of this
+            # test, in case the node was not killed quickly enough and an index
+            # was already synced by the time this test was looking for its
+            # thread start message.
+            with node.busy_wait_for_debug_log([terminate_line], start_byte=log_start_byte if terminate_line in index_threads else None):
                 if platform.system() == 'Windows':
                     # CREATE_NEW_PROCESS_GROUP is required in order to be able
                     # to terminate the child without terminating the test.
