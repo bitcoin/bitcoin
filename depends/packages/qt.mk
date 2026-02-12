d7e972a90d59bbf705bd06f5d2606cccd1dcdcf9
Cory Fields <cory-nospam-@coryfields.com>
2026-02-12 18:30:47 +0000

    qt: add patch to fix build with gcc16
    Backported from:
    https://github.com/qt/qtbase/commit/7fccc79dd5744ea837ffe200bbfc9f2756870220

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -19,6 +19,7 @@ $(package)_patches += qtbase_skip_tools.patch
 $(package)_patches += rcc_hardcode_timestamp.patch
 $(package)_patches += qttools_skip_dependencies.patch
 $(package)_patches += static_fixes.patch
+$(package)_patches += fix-gcc16-qcompare.patch
 
 $(package)_qttranslations_file_name=$(qt_details_qttranslations_file_name)
 $(package)_qttranslations_sha256_hash=$(qt_details_qttranslations_sha256_hash)
@@ -268,7 +269,8 @@ define $(package)_preprocess_cmds
   patch -p1 -i $($(package)_patch_dir)/qtbase_plugins_cocoa.patch && \
   patch -p1 -i $($(package)_patch_dir)/qtbase_skip_tools.patch && \
   patch -p1 -i $($(package)_patch_dir)/rcc_hardcode_timestamp.patch && \
-  patch -p1 -i $($(package)_patch_dir)/static_fixes.patch
+  patch -p1 -i $($(package)_patch_dir)/static_fixes.patch && \
+  patch -p1 -i $($(package)_patch_dir)/fix-gcc16-qcompare.patch
 endef
 ifeq ($(host),$(build))
   $(package)_preprocess_cmds += && patch -p1 -i $($(package)_patch_dir)/qttools_skip_dependencies.patch
