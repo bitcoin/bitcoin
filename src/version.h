0d89cc9176d9489a00432f510ab42f5d068686ae
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2026-02-13 21:00:44 +0530

    chore: update minimum masternode peer version

--- src/version.h
+++ src/version.h
@@ -20,7 +20,7 @@ static const int INIT_PROTO_VERSION = 209;
 static const int MIN_PEER_PROTO_VERSION = 70221;
 
 //! minimum proto version of masternode to accept in DKGs
-static const int MIN_MASTERNODE_PROTO_VERSION = 70238;
+static const int MIN_MASTERNODE_PROTO_VERSION = 70240;
 
 //! ADDRV2 was introduced in this version
 static const int ADDRV2_PROTO_VERSION = 70223;
