b74b54953cba4be5b21671a811021c972454b666
pasta <pasta@dashboost.org>
2026-03-14 12:54:18 -0500

    Merge #7221: refactor: rename bitcoin-util manpage and test references to dash-util
    c9af7810367edd3b26ebbb18e32e6135b279ed44 refactor: rename bitcoin-util manpage and test references to dash-util (UdjinM6)
    
    Pull request description:
    
      ## Issue being fixed or feature implemented
      Rename bitcoin-util.1 placeholder to dash-util.1 with regenerated content for v23.1.2. Update Makefile.am, test config, and test framework to use dash-util naming consistently. The original bitcoin-util references were missed during the backport in #7054.
    
      Consider backporting in #7214
    
      ## What was done?
    
      ## How Has This Been Tested?
    
      ## Breaking Changes
      n/a
    
      ## Checklist:
      - [ ] I have performed a self-review of my own code
      - [ ] I have commented my code, particularly in hard-to-understand areas
      - [ ] I have added or updated relevant unit/integration/functional/e2e tests
      - [ ] I have made corresponding changes to the documentation
      - [ ] I have assigned this pull request to a milestone _(for repository code-owners and collaborators only)_
    
    Top commit has no ACKs.
    
    Tree-SHA512: 9218d5812f5272167a6a705c04cf85a0be236bd2e0a1217a3b706dad4759ab4f45c61fb2d3a9044b247a181b1c1019305acee217d3710b86e9a182a1e1eec83e

--- test/functional/test_framework/test_framework.py
+++ test/functional/test_framework/test_framework.py
@@ -1089,9 +1089,9 @@ class BitcoinTestFramework(metaclass=BitcoinTestMetaClass):
             raise SkipTest("dash-wallet has not been compiled")
 
     def skip_if_no_bitcoin_util(self):
-        """Skip the running test if bitcoin-util has not been compiled."""
+        """Skip the running test if dash-util has not been compiled."""
         if not self.is_bitcoin_util_compiled():
-            raise SkipTest("bitcoin-util has not been compiled")
+            raise SkipTest("dash-util has not been compiled")
 
     def skip_if_no_cli(self):
         """Skip the running test if dash-cli has not been compiled."""
@@ -1141,8 +1141,8 @@ class BitcoinTestFramework(metaclass=BitcoinTestMetaClass):
         return self.config["components"].getboolean("ENABLE_WALLET_TOOL")
 
     def is_bitcoin_util_compiled(self):
-        """Checks whether bitcoin-util was compiled."""
-        return self.config["components"].getboolean("ENABLE_BITCOIN_UTIL")
+        """Checks whether dash-util was compiled."""
+        return self.config["components"].getboolean("ENABLE_UTIL_TOOL")
 
     def is_zmq_compiled(self):
         """Checks whether the zmq module was compiled."""
