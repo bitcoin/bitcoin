ffa1dc21040238ebfeea759c1172203a518111f1
jagdeep sidhu <sidhujag@syscoin.org>
2026-03-14 13:53:49 -0700

    Update deterministicmns.h

--- src/evo/deterministicmns.h
+++ src/evo/deterministicmns.h
@@ -480,10 +480,10 @@ public:
 };
 class CDeterministicMNManager
 {
+public:
     static constexpr int DISK_SNAPSHOT_PERIOD = 576; // once per day
     static constexpr int DISK_SNAPSHOTS = 3; // keep cache for 3 disk snapshots to have 2 full days covered
     static constexpr int LIST_CACHE_SIZE = DISK_SNAPSHOT_PERIOD * DISK_SNAPSHOTS;
-
 private:
     Mutex cs;
     // Main thread has indicated we should perform cleanup up to this height
