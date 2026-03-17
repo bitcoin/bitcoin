fa2e546978b5dc719a7e6b4afe5fae34293f27d3
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2026-03-17 20:10:56 +0100

    refactor: Use util::NotNull<std::unique_ptr<LevelDBContext>> m_db_context
    This allows to drop the runtime Assert every time the context is
    accessed, because the type is known to be not null at compile-time.
    
    Note there is still the runtime Assert inside the util::NotNull
    constructor itself. However, this is called at most once, with the
    possibility of the compiler being able to optimize it away.

--- src/dbwrapper.h
+++ src/dbwrapper.h
@@ -12,6 +12,7 @@
 #include <util/byte_units.h>
 #include <util/check.h>
 #include <util/fs.h>
+#include <util/not_null.h>
 #include <util/obfuscation.h>
 
 #include <cstddef>
@@ -193,7 +194,7 @@ class CDBWrapper
     friend const Obfuscation& dbwrapper_private::GetObfuscation(const CDBWrapper&);
 private:
     //! holds all leveldb-specific fields of this class
-    std::unique_ptr<LevelDBContext> m_db_context;
+    util::NotNullUniquePtr<LevelDBContext> m_db_context;
 
     //! the name of this database
     std::string m_name;
@@ -207,7 +208,7 @@ private:
     std::optional<std::string> ReadImpl(std::span<const std::byte> key) const;
     bool ExistsImpl(std::span<const std::byte> key) const;
     size_t EstimateSizeImpl(std::span<const std::byte> key1, std::span<const std::byte> key2) const;
-    auto& DBContext() const LIFETIMEBOUND { return *Assert(m_db_context); }
+    auto& DBContext() const LIFETIMEBOUND { return *m_db_context; }
 
 public:
     CDBWrapper(const DBParams& params);
