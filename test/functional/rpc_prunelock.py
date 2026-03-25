9465be6fd01138246337ec91745d08a65f7bea66
Fabian Jahr <fjahr@protonmail.com>
2026-03-25 14:22:49 +0100

    init: Add -prunelockheight init option
    This allows to start a node with a prune lock already set in order to avoid racing with any unwanted pruning.

--- test/functional/rpc_prunelock.py
+++ test/functional/rpc_prunelock.py
@@ -2,7 +2,7 @@
 # Copyright (c) The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
-"""Test the listprunelocks and setprunelock RPCs."""
+"""Test the listprunelocks and setprunelock RPCs, and the -prunelockheight startup option."""
 
 import os
 
@@ -32,6 +32,10 @@ class PruneLockTest(BitcoinTestFramework):
         return os.path.isfile(filepath)
 
     def run_test(self):
+        self.test_prune_lock_rpc()
+        self.test_prune_lock_init()
+
+    def test_prune_lock_rpc(self):
         node = self.nodes[0]
 
         self.log.info("Test empty listprunelocks")
@@ -132,6 +136,55 @@ class PruneLockTest(BitcoinTestFramework):
                             {"id": "rpc:default_height"},
                             {"height": 0})
         node.setprunelock("default_height", "remove")
+
+
+    def test_prune_lock_init(self):
+        node = self.nodes[0]
+
+        self.log.info("Test -prunelockheight with explicit height")
+        self.restart_node(0, extra_args=["-prune=1", "-prunelockheight=500"])
+        assert_array_result(node.listprunelocks()["prune_locks"],
+                            {"id": "init"},
+                            {"height": 500})
+
+        self.log.info("Test -prunelockheight can coexist with RPC locks")
+        node.setprunelock("mylock", "add", 100)
+        locks = node.listprunelocks()["prune_locks"]
+        assert_array_result(locks, {"id": "init"}, {"height": 500})
+        assert_array_result(locks, {"id": "rpc:mylock"}, {"height": 100})
+        node.setprunelock("mylock", "remove")
+
+        self.log.info("Test restarting without -prunelockheight removes the startup lock")
+        self.restart_node(0, extra_args=["-prune=1"])
+        assert_array_result(node.listprunelocks()["prune_locks"],
+                            {"id": "init"}, {},
+                            should_not_find=True)
+
+        self.log.info("Test -prunelockheight=0 locks at tip")
+        tip_height = node.getblockcount()
+        self.restart_node(0, extra_args=["-prune=1", "-prunelockheight=0"])
+        assert_array_result(node.listprunelocks()["prune_locks"],
+                            {"id": "init"},
+                            {"height": tip_height})
+
+        self.log.info("Test -prunelockheight without value locks at tip")
+        self.restart_node(0, extra_args=["-prune=1", "-prunelockheight"])
+        assert_array_result(node.listprunelocks()["prune_locks"],
+                            {"id": "init"},
+                            {"height": tip_height})
+
+        self.log.info("Test -prunelockheight without -prune fails")
+        self.stop_node(0)
+        node.assert_start_raises_init_error(
+            extra_args=["-prunelockheight=100"],
+            expected_msg="Error: -prunelockheight is only possible in prune mode. Set -prune to enable.",
+        )
+
+        self.log.info("Test -prunelockheight with negative value fails")
+        node.assert_start_raises_init_error(
+            extra_args=["-prune=1", "-prunelockheight=-5"],
+            expected_msg='Error: -prunelockheight can not be negative',
+        )
 
 
 if __name__ == '__main__':
