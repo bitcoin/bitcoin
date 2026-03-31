a0908660895-stack/bitcoin1

Change CChain::FindFork() to take ref

src/test/chain_tests.cpp
@@ -151,17 +151,17 @@ BOOST_AUTO_TEST_CASE(cchain_findfork_tests)
 
         // Test the blocks in the common part -> result should be the same
         for(auto& b : b_common) {
-            auto fork{cl.FindFork(b.get())};
+            auto fork{cl.FindFork(*b)};
             BOOST_CHECK_EQUAL(fork, b.get());
         }
         // Test the blocks on the longer fork -> result should be the same
         for(auto& b : b_longer) {
-            auto fork{cl.FindFork(b.get())};
+            auto fork{cl.FindFork(*b)};
             BOOST_CHECK_EQUAL(fork, b.get());
         }
         // Test the blocks on the other shorter fork -> result should be the fork point
         for(auto& b : b_shorter) {
-            auto fork{cl.FindFork(b.get())};
+            auto fork{cl.FindFork(*b)};
             BOOST_CHECK_EQUAL(fork, b_common.back().get());
         }
     }
@@ -179,17 +179,17 @@ BOOST_AUTO_TEST_CASE(cchain_findfork_tests)
 
         // Test the blocks in the common part -> result should be the same
         for(auto& b : b_common) {
-            auto fork{cs.FindFork(b.get())};
+            auto fork{cs.FindFork(*b)};
             BOOST_CHECK_EQUAL(fork, b.get());
         }
         // Test the blocks on the shorter fork -> result should be the same
         for(auto& b : b_shorter) {
-            auto fork{cs.FindFork(b.get())};
+            auto fork{cs.FindFork(*b)};
             BOOST_CHECK_EQUAL(fork, b.get());
         }
         // Test the blocks on the other longer fork -> result should be the fork point
         for(auto& b : b_longer) {
-            auto fork{cs.FindFork(b.get())};
+            auto fork{cs.FindFork(*b)};
             BOOST_CHECK_EQUAL(fork, b_common.back().get());
         }
     }

bitcoin/bitcoin

src/test/chain_tests.cpp
@@ -130,17 +130,17 @@ BOOST_AUTO_TEST_CASE(cchain_findfork_tests)
 
         // Test the blocks in the common part -> result should be the same
         for (auto& b : blocks_common) {
-            auto fork{chain_long.FindFork(b.get())};
+            auto fork{chain_long.FindFork(*b)};
             BOOST_CHECK_EQUAL(fork, b.get());
         }
         // Test the blocks on the longer fork -> result should be the same
         for (auto& b : blocks_long) {
-            auto fork{chain_long.FindFork(b.get())};
+            auto fork{chain_long.FindFork(*b)};
             BOOST_CHECK_EQUAL(fork, b.get());
         }
         // Test the blocks on the other shorter fork -> result should be the fork point
         for (auto& b : blocks_short) {
-            auto fork{chain_long.FindFork(b.get())};
+            auto fork{chain_long.FindFork(*b)};
             BOOST_CHECK_EQUAL(fork, blocks_common.back().get());
         }
     }
@@ -158,17 +158,17 @@ BOOST_AUTO_TEST_CASE(cchain_findfork_tests)
 
         // Test the blocks in the common part -> result should be the same
         for (auto& b : blocks_common) {
-            auto fork{chain_short.FindFork(b.get())};
+            auto fork{chain_short.FindFork(*b)};
             BOOST_CHECK_EQUAL(fork, b.get());
         }
         // Test the blocks on the shorter fork -> result should be the same
         for (auto& b : blocks_short) {
-            auto fork{chain_short.FindFork(b.get())};
+            auto fork{chain_short.FindFork(*b)};
             BOOST_CHECK_EQUAL(fork, b.get());
         }
         // Test the blocks on the other longer fork -> result should be the fork point
         for (auto& b : blocks_long) {
-            auto fork{chain_short.FindFork(b.get())};
+            auto fork{chain_short.FindFork(*b)};
             BOOST_CHECK_EQUAL(fork, blocks_common.back().get());
         }
     }