7bf1c69b69bfc40a1c61fe755a8367073f7f6f74
fanquake <fanquake@gmail.com>
2026-04-08 12:53:42 +0800

    guix: drop -Werror=dev
    Workaround CMake and static-pie.

--- contrib/guix/libexec/build.sh
+++ contrib/guix/libexec/build.sh
@@ -251,7 +251,6 @@ mkdir -p "$DISTSRC"
     cmake -S . -B build \
           --toolchain "${BASEPREFIX}/${HOST}/toolchain.cmake" \
           -DWITH_CCACHE=OFF \
-          -Werror=dev \
           ${CONFIGFLAGS} \
           "${CMAKE_EXE_LINKER_FLAGS}"
 
