46bb20e1b524acdf4277834f549b02f2d7c5f400
Pol Espinasa <pol.espinasa@uab.cat>
2026-04-14 20:23:29 +0200

    wallet: Add ImportDescriptorRequest and ImportDescriptorResult structs
    Add two new structs used for creating requests and getting response when importing descriptors.
    
    These structs replace the UniValue-based interface of ProcessDescriptorImport()
    in a subsequent commit.

---
+++ src/wallet/imports.h
@@ -0,0 +1,45 @@
+// Copyright (c) 2026-present The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_WALLET_IMPORTS_H
+#define BITCOIN_WALLET_IMPORTS_H
+
+#include <wallet/wallet.h>
+
+namespace wallet{
+
+    struct ImportDescriptorResult {
+        enum class FailureReason {
+            NONE,
+            INVALID_DESCRIPTOR,
+            INVALID_PARAMETER,
+            WALLET_ERROR,
+            MISC_ERROR
+        };
+        bool success{false};
+        bool used_default_range{false};
+        FailureReason reason{FailureReason::NONE};
+        std::string error;
+        std::vector<std::string> warnings;
+
+        ImportDescriptorResult& Error(FailureReason r, std::string e) {
+            reason = r;
+            error = std::move(e);
+            return *this;
+        }
+    };
+
+    //! Information about a descriptor to be imported.
+    struct ImportDescriptorRequest {
+        std::string descriptor;
+        std::optional<std::string> label;
+        std::optional<int64_t> timestamp;
+        std::optional<bool> active;
+        std::optional<bool> internal;
+        std::optional<std::pair<int64_t,int64_t>> range;
+        std::optional<int64_t> next_index;
+    };
+}
+
+#endif // BITCOIN_WALLET_IMPORTS_H
