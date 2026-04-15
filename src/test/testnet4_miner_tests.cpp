78689e141fd7f70f007eeeafd517ecfabdcbaf30
angelos <bymet4@protonmail.com>
2026-04-15 11:30:37 +0300

    test: add unit tests for testnet4 softfork

--- src/test/testnet4_miner_tests.cpp
+++ src/test/testnet4_miner_tests.cpp
@@ -2,7 +2,11 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#include <arith_uint256.h>
+#include <chain.h>
+#include <chainparams.h>
 #include <common/system.h>
+#include <util/chaintype.h>
 #include <interfaces/mining.h>
 #include <node/miner.h>
 #include <test/util/common.h>
@@ -13,10 +17,14 @@
 
 #include <boost/test/unit_test.hpp>
 
+#include <limits>
+
 using interfaces::BlockTemplate;
 using interfaces::Mining;
 using node::BlockAssembler;
 using node::BlockWaitOptions;
+using node::GetMaximumTime;
+using node::UpdateTime;
 
 namespace testnet4_miner_tests {
 
@@ -66,6 +74,119 @@ BOOST_AUTO_TEST_CASE(MiningInterface)
     clock_ctx += 1s;
     block_template = block_template->waitNext(wait_options);
     BOOST_REQUIRE(block_template);
+}
+
+BOOST_AUTO_TEST_CASE(MinDifficultyBlocksFixMaxTimeBeforeFork)
+{
+    const auto& consensus = Params().GetConsensus();
+    BOOST_REQUIRE_EQUAL(consensus.min_difficulty_blocks_fix_height, 151200);
+
+    // pindexPrev at height 151198 -> next block height 151199, still pre-fork.
+    CBlockIndex pindexPrev;
+    pindexPrev.nHeight = 151198;
+    pindexPrev.nTime = 1700000000;
+
+    BOOST_CHECK_EQUAL(GetMaximumTime(&pindexPrev, consensus),
+                      std::numeric_limits<int64_t>::max());
+}
+
+BOOST_AUTO_TEST_CASE(MinDifficultyBlocksFixMaxTimeAtFork)
+{
+    const auto& consensus = Params().GetConsensus();
+
+    // pindexPrev at height 151199 -> next block height 151200 is the first
+    // block subject to the rule. Max allowed timestamp = prev + 2 * spacing.
+    CBlockIndex pindexPrev;
+    pindexPrev.nHeight = 151199;
+    pindexPrev.nTime = 1700000000;
+
+    BOOST_CHECK_EQUAL(GetMaximumTime(&pindexPrev, consensus),
+                      1700000000 + consensus.nPowTargetSpacing * 2);
+}
+
+BOOST_AUTO_TEST_CASE(MinDifficultyBlocksFixMaxTimeAfterFork)
+{
+    const auto& consensus = Params().GetConsensus();
+
+    CBlockIndex pindexPrev;
+    pindexPrev.nHeight = 200000;
+    pindexPrev.nTime = 1800000000;
+
+    BOOST_CHECK_EQUAL(GetMaximumTime(&pindexPrev, consensus),
+                      1800000000 + consensus.nPowTargetSpacing * 2);
+}
+
+BOOST_AUTO_TEST_CASE(MinDifficultyBlocksFixMaxTimeDisabledOnOtherChains)
+{
+    // Chains that do not set min_difficulty_blocks_fix_height (default 0)
+    // must not have any upper bound on the block timestamp.
+    const auto mainnet_params = CreateChainParams(*m_node.args, ChainType::MAIN);
+    const auto& consensus = mainnet_params->GetConsensus();
+    BOOST_REQUIRE_EQUAL(consensus.min_difficulty_blocks_fix_height, 0);
+
+    CBlockIndex pindexPrev;
+    pindexPrev.nHeight = 500000;
+    pindexPrev.nTime = 1700000000;
+
+    BOOST_CHECK_EQUAL(GetMaximumTime(&pindexPrev, consensus),
+                      std::numeric_limits<int64_t>::max());
+}
+
+BOOST_AUTO_TEST_CASE(MinDifficultyBlocksFixUpdateTimeClampsWhenNowPastCap)
+{
+    const auto& consensus = Params().GetConsensus();
+    // Height 151201 is post-fork and not a retarget boundary, so
+    // GetNextWorkRequired does not need to walk the pprev chain.
+    CBlockIndex pindexPrev;
+    pindexPrev.nHeight = 151201;
+    pindexPrev.nTime = 1700000000;
+    pindexPrev.nBits = UintToArith256(consensus.powLimit).GetCompact();
+
+    // Wall-clock 3000s past pindexPrev is well above the 1200s cap.
+    NodeClockContext clock_ctx{std::chrono::seconds{1700000000 + 3000}};
+
+    CBlockHeader header;
+    header.nTime = 0;
+    UpdateTime(&header, consensus, &pindexPrev);
+
+    BOOST_CHECK_EQUAL(header.nTime, 1700000000 + consensus.nPowTargetSpacing * 2);
+}
+
+BOOST_AUTO_TEST_CASE(MinDifficultyBlocksFixUpdateTimeUsesNowWhenBelowCap)
+{
+    const auto& consensus = Params().GetConsensus();
+    CBlockIndex pindexPrev;
+    pindexPrev.nHeight = 151201;
+    pindexPrev.nTime = 1700000000;
+    pindexPrev.nBits = UintToArith256(consensus.powLimit).GetCompact();
+
+    // Wall-clock 500s past pindexPrev is below the 1200s cap.
+    NodeClockContext clock_ctx{std::chrono::seconds{1700000000 + 500}};
+
+    CBlockHeader header;
+    header.nTime = 0;
+    UpdateTime(&header, consensus, &pindexPrev);
+
+    BOOST_CHECK_EQUAL(header.nTime, 1700000000 + 500);
+}
+
+BOOST_AUTO_TEST_CASE(MinDifficultyBlocksFixUpdateTimeExactlyAtCap)
+{
+    const auto& consensus = Params().GetConsensus();
+    CBlockIndex pindexPrev;
+    pindexPrev.nHeight = 151201;
+    pindexPrev.nTime = 1700000000;
+    pindexPrev.nBits = UintToArith256(consensus.powLimit).GetCompact();
+
+    // Wall-clock exactly at the cap: must be accepted verbatim.
+    const int64_t cap_time{1700000000 + consensus.nPowTargetSpacing * 2};
+    NodeClockContext clock_ctx{std::chrono::seconds{cap_time}};
+
+    CBlockHeader header;
+    header.nTime = 0;
+    UpdateTime(&header, consensus, &pindexPrev);
+
+    BOOST_CHECK_EQUAL(header.nTime, cap_time);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
