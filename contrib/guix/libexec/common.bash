6dd115af20540855d5c14284ab5b542aa3fb5390
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2026-04-21 15:03:34 +0100

    guix, refactor: Add `BASE` argument to `{distsrc,outdir}_for_host`

--- contrib/guix/libexec/common.bash
+++ contrib/guix/libexec/common.bash
@@ -2,20 +2,22 @@
 export LC_ALL=C
 set -o errexit -o pipefail
 
-# Usage: distsrc_for_host HOST [SUFFIX]
+# Usage: distsrc_for_host HOST [SUFFIX] [BASE]
 #
 #   HOST: The current platform triple we're building for
+#   BASE: Optional. If provided, replaces ${DISTSRC_BASE}
 #
 distsrc_for_host() {
-    echo "${DISTSRC_BASE}/distsrc-${VERSION}-${1}${2:+-${2}}"
+    echo "${3:-${DISTSRC_BASE}}/distsrc-${VERSION}-${1}${2:+-${2}}"
 }
 
-# Usage: outdir_for_host HOST [SUFFIX]
+# Usage: outdir_for_host HOST [SUFFIX] [BASE]
 #
 #   HOST: The current platform triple we're building for
+#   BASE: Optional. If provided, replaces ${OUTDIR_BASE}
 #
 outdir_for_host() {
-    echo "${OUTDIR_BASE}/${1}${2:+-${2}}"
+    echo "${3:-${OUTDIR_BASE}}/${1}${2:+-${2}}"
 }
 
 # Usage: profiledir_for_host HOST [SUFFIX]
