913520ac1340730340050481e031d83ceb9148b1
Antoine Poinsot <mail@antoinep.com>
2026-04-30 17:11:12 -0400

    chainparams: add versionbits deployment for BIP 54

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -1537,6 +1537,7 @@ UniValue DeploymentInfo(const CBlockIndex* blockindex, const ChainstateManager& 
     SoftForkDescPushBack(blockindex, softforks, chainman, Consensus::DEPLOYMENT_CSV);
     SoftForkDescPushBack(blockindex, softforks, chainman, Consensus::DEPLOYMENT_SEGWIT);
     SoftForkDescPushBack(blockindex, softforks, chainman, Consensus::DEPLOYMENT_TESTDUMMY);
+    SoftForkDescPushBack(blockindex, softforks, chainman, Consensus::DEPLOYMENT_CONSENSUSCLEANUP);
     return softforks;
 }
 } // anon namespace
