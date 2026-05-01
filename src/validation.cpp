20b873ed1962cda390f1e8b26b9f9895171f3021
optout21 <13562139+optout21@users.noreply.github.com>
2026-05-01 22:48:32 +0200

    Refactor CheckBlockHeader signature
    Change the (internal) method `CheckBlockHeader` to return the validation
    result in the return value instead of an output parameter.

--- src/validation.cpp
+++ src/validation.cpp
@@ -3841,13 +3841,13 @@ void ChainstateManager::ReceivedBlockTransactions(const CBlock& block, CBlockInd
     }
 }
 
-static bool CheckBlockHeader(const CBlockHeader& block, BlockValidationState& state, const Consensus::Params& consensusParams, bool fCheckPOW = true)
+[[nodiscard]] static BlockValidationState CheckBlockHeader(const CBlockHeader& block, const Consensus::Params& consensusParams, bool fCheckPOW = true)
 {
     // Check proof of work matches claimed amount
-    if (fCheckPOW && !CheckProofOfWork(block.GetHash(), block.nBits, consensusParams))
-        return state.Invalid(BlockValidationResult::BLOCK_INVALID_HEADER, "high-hash", "proof of work failed");
-
-    return true;
+    if (fCheckPOW && !CheckProofOfWork(block.GetHash(), block.nBits, consensusParams)) {
+        return BlockValidationState::InvalidState(BlockValidationResult::BLOCK_INVALID_HEADER, "high-hash", "proof of work failed");
+    }
+    return BlockValidationState{};
 }
 
 static bool CheckMerkleRoot(const CBlock& block, BlockValidationState& state)
@@ -3940,7 +3940,8 @@ bool CheckBlock(const CBlock& block, BlockValidationState& state, const Consensu
 
     // Check that the header is valid (particularly PoW).  This is mostly
     // redundant with the call in AcceptBlockHeader.
-    if (!CheckBlockHeader(block, state, consensusParams, fCheckPOW))
+    state = CheckBlockHeader(block, consensusParams, fCheckPOW);
+    if (!state.IsValid())
         return false;
 
     // Signet only: check block solution
@@ -4220,7 +4221,8 @@ bool ChainstateManager::AcceptBlockHeader(const CBlockHeader& block, BlockValida
             return true;
         }
 
-        if (!CheckBlockHeader(block, state, GetConsensus())) {
+        state = CheckBlockHeader(block, GetConsensus());
+        if (!state.IsValid()) {
             LogDebug(BCLog::VALIDATION, "%s: Consensus::CheckBlockHeader: %s, %s\n", __func__, hash.ToString(), state.ToString());
             return false;
         }
