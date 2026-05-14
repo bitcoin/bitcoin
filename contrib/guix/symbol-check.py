5f63279caf7a4adf49e2bca8f436cce3969f3d00
fanquake <fanquake@gmail.com>
2026-05-14 12:11:17 +0100

    guix: build x86_64-linux bitcoind & utils statically
    -static-pie
    Produce a static position independent executable on targets that support it.
    A static position independent executable is similar to a static executable,
    but can be loaded at any address without a dynamic linker.
    
    See https://gcc.gnu.org/onlinedocs/gcc/Link-Options.html

--- contrib/guix/symbol-check.py
+++ contrib/guix/symbol-check.py
@@ -95,7 +95,6 @@ ELF_ALLOWED_LIBRARIES = {
 'libc.so.6', # C library
 'libpthread.so.0', # threading
 'libm.so.6', # math library
-'ld-linux-x86-64.so.2', # 64-bit dynamic linker
 'ld-linux.so.2', # 32-bit dynamic linker
 'ld-linux-aarch64.so.1', # 64-bit ARM dynamic linker
 'ld-linux-armhf.so.3', # 32-bit ARM dynamic linker
@@ -103,6 +102,7 @@ ELF_ALLOWED_LIBRARIES = {
 'ld64.so.2', # POWER64 ABIv2 dynamic linker
 'ld-linux-riscv64-lp64d.so.1', # 64-bit RISC-V dynamic linker
 # bitcoin-qt only
+'ld-linux-x86-64.so.2', # 64-bit dynamic linker
 'libfontconfig.so.1', # font support
 'libfreetype.so.6', # font parsing
 'libdl.so.2', # programming interface to dynamic linker
