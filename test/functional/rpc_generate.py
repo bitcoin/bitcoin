cd4a7a0dc1960177d307ea9069d4251aba2315cf
Pol Espinasa <pol.espinasa@uab.cat>
2026-05-19 15:32:37 +0200

    test: expand generateblock test coverage

--- test/functional/rpc_generate.py
+++ test/functional/rpc_generate.py
@@ -65,6 +65,14 @@ class RPCGenerateTest(BitcoinTestFramework):
         assert_equal(len(block['tx']), 1)
         assert_equal(block['tx'][0]['vout'][0]['scriptPubKey']['address'], combo_address)
 
+        # Generate some extra mempool transactions to verify that they are mined if no txs are provided
+        for _ in range(10):
+            miniwallet.send_self_transfer(from_node=node)
+        self.log.info("Generate a block with mempool txs")
+        hash = self.generateblock(node, address)['hash']
+        block = node.getblock(hash, 1)
+        assert_equal(len(block['tx']), 11)
+
         # Generate some extra mempool transactions to verify they don't get mined
         for _ in range(10):
             miniwallet.send_self_transfer(from_node=node)
@@ -76,14 +84,39 @@ class RPCGenerateTest(BitcoinTestFramework):
         assert_equal(len(block['tx']), 2)
         assert_equal(block['tx'][1], txid)
 
+        self.log.info('Generate block with multiple txid')
+        # Generate some transactions to get mined
+        txs = []
+        for _ in range(3):
+            txs.append(miniwallet.send_self_transfer(from_node=node)['txid'])
+        # Generate some extra mempool transactions to verify they don't get mined
+        for _ in range(10):
+            miniwallet.send_self_transfer(from_node=node)
+        hash = self.generateblock(node, address, txs)['hash']
+        block = node.getblock(hash, 1)
+        assert_equal(len(block['tx']), 4)
+        for i, tx in enumerate(txs):
+            assert_equal(block['tx'][i+1], tx)
+        # Clean the mempool for next test by mining a block
+        self.generateblock(node, address)
+
         self.log.info('Generate block with raw tx')
         rawtx = miniwallet.create_self_transfer()['hex']
         hash = self.generateblock(node, address, [rawtx])['hash']
-
         block = node.getblock(hash, 1)
         assert_equal(len(block['tx']), 2)
         txid = block['tx'][1]
         assert_equal(node.getrawtransaction(txid=txid, verbose=False, blockhash=hash), rawtx)
+
+        self.log.info('Generate empty block')
+        # put some txs into the mempool
+        for _ in range(10):
+            miniwallet.send_self_transfer(from_node=node)
+        hash = self.generateblock(node, address, [])['hash']
+        block = node.getblock(hash, 1)
+        assert_equal(len(block['tx']), 1) # it contains the coinbase tx
+        # Clean the mempool for next test by mining a block
+        self.generateblock(node, address)
 
         # Ensure that generateblock can be called concurrently by many threads.
         self.log.info('Generate blocks in parallel')
