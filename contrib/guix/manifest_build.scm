e8d3c6e11197273c2e5c1834fbec1d9c5c0c236e
fanquake <fanquake@gmail.com>
2026-06-18 16:36:31 +0200

    guix: binutils 2.46.1

--- contrib/guix/manifest_build.scm
+++ contrib/guix/manifest_build.scm
@@ -28,14 +28,14 @@ FILE-NAME found in ./patches relative to the current file."
 (define (base-binutils target)
   (package
     (inherit (cross-binutils target)) ;; 2.44
-    (version "2.46.0")
+    (version "2.46.1")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://gnu/binutils/binutils-"
                           version ".tar.bz2"))
               (sha256
                (base32
-                "04nd9vl7c1pxjbc9wh3ckddzhz5g82xyjqq9y9kf171a59im4c8g"))))
+                "0fd45n9rrymplx41vk32676d6pqnvfjlq42xma7a4cr6v85d8kij"))))
     (arguments
       (substitute-keyword-arguments (package-arguments (cross-binutils target))
         ((#:configure-flags flags)
