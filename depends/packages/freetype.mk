8f8616b2b9ee13d12cb14b868172072961fe36ba
pasta <pasta@dashboost.org>
2026-06-26 11:08:35 +0100

    Merge #7372: backport: bitcoin/bitcoin#32693: depends: fix cmake compatibility error for freetype
    4a8a5a323870f33f590418a690af4985ffaa71f6 Merge bitcoin/bitcoin#32693: depends: fix cmake compatibility error for freetype (Konstantin Akimov)
    
    Pull request description:
    
      ## Issue being fixed or feature implemented
      It fixes freetype dependency build on Kubuntu 26.04 which provide cmake-4 by default.
    
      ## What was done?
      Backport bitcoin#32693
    
      ## How Has This Been Tested?
      Build succeed
    
      ## Breaking Changes
      N/A
    
      ## Checklist:
      - [x] I have performed a self-review of my own code
      - [ ] I have commented my code, particularly in hard-to-understand areas
      - [ ] I have added or updated relevant unit/integration/functional/e2e tests
      - [ ] I have made corresponding changes to the documentation
      - [x] I have assigned this pull request to a milestone _(for repository code-owners and collaborators only)_
    
    ACKs for top commit:
      PastaPastaPasta:
        utACK 4a8a5a323870f33f590418a690af4985ffaa71f6
    
    Tree-SHA512: b9b796ef3a6e39a1acd58bee1f990d7a5c1b9bcc1b75bc5cb8f9e00ad9cc57cd85c5fd62f585ba7cb57493e55236d8d95069a739acad271cfd01dc6da23065ab
    (cherry picked from commit b2810413e08eff9424c65676e94e92d3223ddb5c)

--- depends/packages/freetype.mk
+++ depends/packages/freetype.mk
@@ -4,12 +4,17 @@ $(package)_download_path=https://download.savannah.gnu.org/releases/$(package)
 $(package)_file_name=$(package)-$($(package)_version).tar.xz
 $(package)_sha256_hash=8bee39bd3968c4804b70614a0a3ad597299ad0e824bc8aad5ce8aaf48067bde7
 $(package)_build_subdir=build
+$(package)_patches += cmake_minimum.patch
 
 define $(package)_set_vars
   $(package)_config_opts := -DCMAKE_BUILD_TYPE=None -DBUILD_SHARED_LIBS=TRUE
   $(package)_config_opts += -DCMAKE_DISABLE_FIND_PACKAGE_ZLIB=TRUE -DCMAKE_DISABLE_FIND_PACKAGE_PNG=TRUE
   $(package)_config_opts += -DCMAKE_DISABLE_FIND_PACKAGE_HarfBuzz=TRUE -DCMAKE_DISABLE_FIND_PACKAGE_BZip2=TRUE
   $(package)_config_opts += -DCMAKE_DISABLE_FIND_PACKAGE_BrotliDec=TRUE
+endef
+
+define $(package)_preprocess_cmds
+  patch -p1 < $($(package)_patch_dir)/cmake_minimum.patch
 endef
 
 define $(package)_config_cmds
