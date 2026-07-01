37f3b857103e5b95c4c3b895d1b03f45419f5278
sedited <seb.kung@gmail.com>
2026-07-01 13:45:04 +0200

    kernel: Add script tracer
    This adds a callback hook to the kernel library C header for getting the
    current script execution state on each script evaluation step. This can
    be used for debugging scripts and gives external applications a view
    into how Bitcoin Core does script evaluation.
    
    This feature is gated behind the ENABLE_SCRIPT_TRACE flag. To compile
    with the feature enabled, pass `-DENABLE_SCRIPT_TRACE=ON`. If the feature
    is not manually enabled, the trace hooks are stubbed out.
    
    The traces are divided into separate frames. RAII is used to guarantee
    that every script execution has at least its start and end state
    captured. Each instruction generates a unique frame as well.
    
    The ScriptError in the VerifyScript invocation run when calling
    script_pubkey_verify is added in order to be able to surface the error
    code in the trace frames.

--- src/test/kernel/test_kernel.cpp
+++ src/test/kernel/test_kernel.cpp
@@ -392,6 +392,113 @@ void CheckRange(const RangeType& range, size_t expected_size)
     BOOST_CHECK(it2 == it + 1);
 }
 
+struct ScriptTracer {
+    struct Snapshot {
+        ScriptTraceFrameKind m_kind;
+        std::optional<int32_t> m_script_error;
+        bool m_exec;
+    };
+    std::vector<Snapshot>& m_state;
+
+    void ScriptTrace(const ScriptTraceFrameView& frame)
+    {
+        m_state.push_back({frame.Kind(), frame.GetScriptError(), frame.Exec()});
+    }
+};
+
+BOOST_AUTO_TEST_CASE(btck_script_trace_tests)
+{
+    std::vector<ScriptTracer::Snapshot> states;
+#ifndef ENABLE_SCRIPT_TRACE
+    BOOST_CHECK_EXCEPTION(
+        ScriptTraceSetCallback(std::make_unique<ScriptTracer>(states)),
+        std::runtime_error,
+        HasReason("Script Tracing is not available. Compile bitcoin kernel with ENABLE_SCRIPT_TRACE"));
+    // Should be no-op
+    ScriptTraceUnsetCallback();
+#else
+    ScriptTraceSetCallback(std::make_unique<ScriptTracer>(states));
+
+    auto legacy_spent_script_pubkey{ScriptPubkey{hex_string_to_byte_vec("76a9144bfbaf6afb76cc5771bc6404810d1cc041a6933988ac")}};
+    auto legacy_spending_tx{Transaction{hex_string_to_byte_vec("02000000013f7cebd65c27431a90bba7f796914fe8cc2ddfc3f2cbd6f7e5f2fc854534da95000000006b483045022100de1ac3bcdfb0332207c4a91f3832bd2c2915840165f876ab47c5f8996b971c3602201c6c053d750fadde599e6f5c4e1963df0f01fc0d97815e8157e3d59fe09ca30d012103699b464d1d8bc9e47d4fb1cdaa89a1c5783d68363c4dbc4b524ed3d857148617feffffff02836d3c01000000001976a914fc25d6d5c94003bf5b0c7b640a248e2c637fcfb088ac7ada8202000000001976a914fbed3d9b11183209a57999d54d59f67c019e756c88ac6acb0700")}};
+    auto status{ScriptVerifyStatus::OK};
+    BOOST_CHECK(legacy_spent_script_pubkey.Verify(
+        /*amount=*/0,
+        /*tx_to=*/legacy_spending_tx,
+        /*precomputed_txdata=*/nullptr,
+        /*input_index=*/0,
+        VERIFY_ALL_PRE_TAPROOT,
+        status));
+    BOOST_CHECK(status == ScriptVerifyStatus::OK);
+
+    const auto frame_count = states.size();
+    ScriptTraceUnsetCallback();
+
+    BOOST_CHECK(legacy_spent_script_pubkey.Verify(
+        /*amount=*/0,
+        /*tx_to=*/legacy_spending_tx,
+        /*precomputed_txdata=*/nullptr,
+        /*input_index=*/0,
+        VERIFY_ALL_PRE_TAPROOT,
+        status));
+    BOOST_CHECK_EQUAL(states.size(), frame_count);
+
+    BOOST_REQUIRE_EQUAL(states.size(), 11);
+    BOOST_CHECK(states[0].m_kind == ScriptTraceFrameKind::BEGIN);
+    BOOST_CHECK(states[3].m_kind == ScriptTraceFrameKind::END);
+    BOOST_CHECK(states[4].m_kind == ScriptTraceFrameKind::BEGIN);
+    BOOST_CHECK(states[10].m_kind == ScriptTraceFrameKind::END);
+    for (int i : {1, 2, 5, 6, 7, 8, 9}) {
+        BOOST_CHECK(states[i].m_kind == ScriptTraceFrameKind::STEP);
+    }
+    BOOST_CHECK_EQUAL(states[3].m_script_error, 0);
+    BOOST_CHECK_EQUAL(states[10].m_script_error, 0);
+
+    ScriptTraceSetCallback(std::make_unique<ScriptTracer>(states));
+    BOOST_CHECK(legacy_spent_script_pubkey.Verify(
+        /*amount=*/0,
+        /*tx_to=*/legacy_spending_tx,
+        /*precomputed_txdata=*/nullptr,
+        /*input_index=*/0,
+        VERIFY_ALL_PRE_TAPROOT,
+        status));
+    BOOST_CHECK_EQUAL(states.size(), 22);
+    ScriptTraceUnsetCallback();
+
+    // Checking a signet block invokes script evaluation without communicating an error code.
+    states.clear();
+    ChainParams signet_params{ChainType::SIGNET};
+    Block signet_block{hex_string_to_byte_vec(
+        "00000020f61eee3b63a380a477a063af32b2bbc97c9ff9f01f2c4225e973988108000000f575c83235984e7dc4afc1f30944c170462e84437ab6f2d52e16878a79e4678bd1914d5fae77031eccf4070001010000000001010000000000000000000000000000000000000000000000000000000000000000ffffffff025151feffffff0200f2052a010000001600149243f727dd5343293eb83174324019ec16c2630f0000000000000000776a24aa21a9ede2f61c3f71d1defd3fa999dfa36953755c690689799962b48bebd836974e8cf94c4fecc7daa2490047304402205e423a8754336ca99dbe16509b877ef1bf98d008836c725005b3c787c41ebe46022047246e4467ad7cc7f1ad98662afcaf14c115e0095a227c7b05c5182591c23e7e01000120000000000000000000000000000000000000000000000000000000000000000000000000")};
+    BlockValidationState validation_state;
+    ScriptTraceSetCallback(std::make_unique<ScriptTracer>(states));
+    const bool valid{signet_block.Check(signet_params.GetConsensusParams(), BlockCheckFlags::ALL, validation_state)};
+    ScriptTraceUnsetCallback();
+    BOOST_CHECK(valid);
+    BOOST_REQUIRE(!states.empty());
+    BOOST_CHECK(!states.back().m_script_error);
+
+    ScriptPubkey script_pubkey{hex_string_to_byte_vec("00636451675168675168")};
+    std::vector<bool> expected_exec{true, true, false, false, false, false, false, false, true, true};
+    const Transaction exec_tx{hex_string_to_byte_vec("020000000100000000000000000000000000000000000000000000000000000000000000000000000000ffffffff0100000000000000000000000000")};
+    states.clear();
+    ScriptTraceSetCallback(std::make_unique<ScriptTracer>(states));
+    BOOST_CHECK(script_pubkey.Verify(
+        /*amount=*/0,
+        /*tx_to=*/exec_tx,
+        /*precomputed_txdata=*/nullptr,
+        /*input_index=*/0,
+        VERIFY_ALL_PRE_TAPROOT,
+        status));
+    ScriptTraceUnsetCallback();
+    std::vector<bool> exec;
+    for (const auto& s : states) {
+        if (s.m_kind == ScriptTraceFrameKind::STEP) exec.push_back(s.m_exec);
+    }
+    BOOST_CHECK_EQUAL_COLLECTIONS(exec.begin(), exec.end(), expected_exec.begin(), expected_exec.end());
+#endif
+}
+
 BOOST_AUTO_TEST_CASE(btck_transaction_tests)
 {
     auto tx_data{hex_string_to_byte_vec("02000000013f7cebd65c27431a90bba7f796914fe8cc2ddfc3f2cbd6f7e5f2fc854534da95000000006b483045022100de1ac3bcdfb0332207c4a91f3832bd2c2915840165f876ab47c5f8996b971c3602201c6c053d750fadde599e6f5c4e1963df0f01fc0d97815e8157e3d59fe09ca30d012103699b464d1d8bc9e47d4fb1cdaa89a1c5783d68363c4dbc4b524ed3d857148617feffffff02836d3c01000000001976a914fc25d6d5c94003bf5b0c7b640a248e2c637fcfb088ac7ada8202000000001976a914fbed3d9b11183209a57999d54d59f67c019e756c88ac6acb0700")};
