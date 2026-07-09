cc2acebefb049043bfb571832a86718877a9f328
Matthew Zipkin <pinheadmz@pm.me>
2026-07-09 11:18:20 -0400

    http: configure simultaneous connection limit with -rpcmaxconnections

--- src/httpserver.h
+++ src/httpserver.h
@@ -44,7 +44,7 @@ inline constexpr int DEFAULT_HTTP_SERVER_TIMEOUT=30;
 /**
  * Maximum number of connected HTTP clients
  */
-inline constexpr int MAX_HTTP_CONNECTIONS = 16;
+inline constexpr int DEFAULT_MAX_HTTP_CONNECTIONS = 16;
 
 enum class HTTPRequestMethod {
     UNKNOWN,
@@ -317,6 +317,11 @@ public:
     void SetServerTimeout(std::chrono::seconds seconds) { m_rpcservertimeout = seconds; }
 
     /**
+     * Set the maximum amount of connected HTTPClients (-rpcmaxconnections)
+     */
+    void SetMaxConnections(int max_conn) { m_rpcmaxconnections = max_conn; }
+
+    /**
      * Force-remove all remaining clients from m_connected without waiting for
      * graceful disconnection. Must only be called after JoinSocketsThreads().
      */
@@ -420,6 +425,11 @@ private:
      * Check an incoming connection's source IP against the allow list
      */
     bool ClientAllowed(const CNetAddr& netaddr) const;
+
+    /**
+     * Maximum amount of concurrent connections
+     */
+    int m_rpcmaxconnections{DEFAULT_MAX_HTTP_CONNECTIONS};
 
     /**
      * Accept a connection.
