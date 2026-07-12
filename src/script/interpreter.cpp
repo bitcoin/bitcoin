7cda56d3a2c2c16e6c098b4e79e7e3be5696b729
Fabian Jahr <fjahr@protonmail.com>
2026-07-12 13:33:13 +0200

    consensus: Add CISA deployment and script flag

--- src/script/interpreter.cpp
+++ src/script/interpreter.cpp
@@ -2200,6 +2200,7 @@ const std::map<std::string, script_verify_flag_name>& ScriptFlagNamesToEnum()
         FLAG_NAME(DISCOURAGE_UPGRADABLE_PUBKEYTYPE),
         FLAG_NAME(DISCOURAGE_OP_SUCCESS),
         FLAG_NAME(DISCOURAGE_UPGRADABLE_TAPROOT_VERSION),
+        FLAG_NAME(WITNESS_V2),
     };
 #undef FLAG_NAME
     return g_names_to_enum;
