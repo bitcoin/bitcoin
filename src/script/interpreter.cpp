7712d21b668ef122055a772d653cb3cb5c2784f8
Fabian Jahr <fjahr@protonmail.com>
2026-07-12 13:40:00 +0200

    script: Add witness v2 keypath signature message

--- src/script/interpreter.cpp
+++ src/script/interpreter.cpp
@@ -408,7 +408,8 @@ static bool EvalChecksig(const valtype& sig, const valtype& pubkey, CScript::con
     case SigVersion::TAPSCRIPT:
         return EvalChecksigTapscript(sig, pubkey, execdata, flags, checker, sigversion, serror, success);
     case SigVersion::TAPROOT:
-        // Key path spending in Taproot has no script, so this is unreachable.
+    case SigVersion::WITNESS_V2_KEYPATH:
+        // Key path spending has no script, so this is unreachable.
         break;
     }
     assert(false);
@@ -1495,6 +1496,7 @@ bool SignatureHashSchnorr(uint256& hash_out, ScriptExecutionData& execdata, cons
     uint8_t ext_flag, key_version;
     switch (sigversion) {
     case SigVersion::TAPROOT:
+    case SigVersion::WITNESS_V2_KEYPATH:
         ext_flag = 0;
         // key_version is not used and left uninitialized.
         break;
@@ -1517,8 +1519,15 @@ bool SignatureHashSchnorr(uint256& hash_out, ScriptExecutionData& execdata, cons
     HashWriter ss{HASHER_TAPSIGHASH};
 
     // Epoch
-    static constexpr uint8_t EPOCH = 0;
-    ss << EPOCH;
+    if (sigversion == SigVersion::WITNESS_V2_KEYPATH) {
+        // BIP460: hash_TapSighash(0x01 || agg_mode || SigMsg(hash_type, 0))
+        static constexpr uint8_t CISA_EPOCH = 1;
+        ss << CISA_EPOCH;
+        ss << execdata.m_cisa_agg_mode;
+    } else {
+        static constexpr uint8_t EPOCH = 0;
+        ss << EPOCH;
+    }
 
     // Hash type
     const uint8_t output_type = (hash_type == SIGHASH_DEFAULT) ? SIGHASH_ALL : (hash_type & SIGHASH_OUTPUT_MASK); // Default (no sighash byte) is equivalent to SIGHASH_ALL
