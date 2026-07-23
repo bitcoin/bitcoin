2cb3bfa8df7cab0635be221af1c8754dbbaff335
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2026-07-23 12:31:31 +0100

    scripted-diff: Use long form of shell options in Guix scripts
    This improves readability and consistency across all Guix scripts.
    
    -BEGIN VERIFY SCRIPT-
    
    sed -i "s/^set -e\>/set -o errexit/g" \
     $( git grep -l "set -e" ./contrib/guix )
    
    -END VERIFY SCRIPT-

--- contrib/guix/libexec/codesign.sh
+++ contrib/guix/libexec/codesign.sh
@@ -3,7 +3,7 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or https://opensource.org/license/mit.
 export LC_ALL=C.UTF-8
-set -e -o pipefail
+set -o errexit -o pipefail
 
 # Environment variables for determinism
 export TAR_OPTIONS="--owner=0 --group=0 --numeric-owner --mtime='@${SOURCE_DATE_EPOCH}' --sort=name"
--- contrib/guix/libexec/package.sh
+++ contrib/guix/libexec/package.sh
@@ -3,7 +3,7 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or https://opensource.org/license/mit.
 export LC_ALL=C.UTF-8
-set -e -o pipefail
+set -o errexit -o pipefail
 
 (
     cd "$DISTSRC"
