2646bf8bcacc206e0b9a9394b16a1a6b78b9b4f0
Lőrinc <pap.lorinc@gmail.com>
2026-07-28 17:11:53 -0700

    qt: type nice time offsets
    Make `formatNiceTimeOffset` accept seconds so its comparisons and divisions retain units.
    Callers now construct durations from raw values provided by Qt or consensus code.
    
    Co-authored-by: MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>

--- src/qt/sendcoinsdialog.cpp
+++ src/qt/sendcoinsdialog.cpp
@@ -171,7 +171,7 @@ void SendCoinsDialog::setModel(WalletModel *_model)
 
         // fee section
         for (const int n : confTargets) {
-            ui->confTargetSelector->addItem(tr("%1 (%2 blocks)").arg(GUIUtil::formatNiceTimeOffset(n*Params().GetConsensus().nPowTargetSpacing)).arg(n));
+            ui->confTargetSelector->addItem(tr("%1 (%2 blocks)").arg(GUIUtil::formatNiceTimeOffset(n * Params().GetConsensus().PowTargetSpacing())).arg(n));
         }
         connect(ui->confTargetSelector, qOverload<int>(&QComboBox::currentIndexChanged), this, &SendCoinsDialog::updateSmartFeeLabel);
         connect(ui->confTargetSelector, qOverload<int>(&QComboBox::currentIndexChanged), this, &SendCoinsDialog::coinControlUpdateLabels);
