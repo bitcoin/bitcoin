861d3345fdbbf2ec8990a07e596312cc821ad3fd
Lőrinc <pap.lorinc@gmail.com>
2026-07-28 17:13:33 -0700

    validation: type future block time limit
    Store the two-hour allowance as a duration and use it directly in time-point comparisons.
    Convert it only where existing interfaces still operate on integer seconds.
    
    The `chain_tests/basic_tests` assertion pins the duration to 7,200 seconds.

--- src/chain.h
+++ src/chain.h
@@ -26,7 +26,7 @@
  * Maximum amount of time that a block timestamp is allowed to exceed the
  * current time before the block will be accepted.
  */
-static constexpr int64_t MAX_FUTURE_BLOCK_TIME = 2 * 60 * 60;
+static constexpr auto MAX_FUTURE_BLOCK_TIME{2h};
 
 /**
  * Timestamp window used as a grace period by code that compares external
@@ -34,7 +34,7 @@ static constexpr int64_t MAX_FUTURE_BLOCK_TIME = 2 * 60 * 60;
  * to block timestamps. This should be set at least as high as
  * MAX_FUTURE_BLOCK_TIME.
  */
-static constexpr int64_t TIMESTAMP_WINDOW = MAX_FUTURE_BLOCK_TIME;
+static constexpr int64_t TIMESTAMP_WINDOW{MAX_FUTURE_BLOCK_TIME / 1s};
 //! Init values for CBlockIndex nSequenceId when loaded from disk
 static constexpr int32_t SEQ_ID_BEST_CHAIN_FROM_DISK = 0;
 static constexpr int32_t SEQ_ID_INIT_FROM_DISK = 1;
