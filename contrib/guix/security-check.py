594a02c3ae05ae4985f6647c7ffb94d615b7a8a1
fanquake <fanquake@gmail.com>
2026-08-03 11:24:11 +0100

    lint: re-add guix scripts to mypy linting
    These were no-longer being linted after #32458.
    
    suppress `[union-attr]` warning. i.e:
    ```bash
    contrib/guix/symbol-check.py:309: error: Item "None" of "lief.PE.Binary | lief.ELF.Binary | lief.MachO.Binary | lief.COFF.Binary | None" has no attribute "format"  [union-attr]
    contrib/guix/security-check.py:284: error: Item "lief.COFF.Binary" of "lief.PE.Binary | lief.ELF.Binary | lief.MachO.Binary | lief.COFF.Binary | None" has no attribute "abstract"  [union-attr]
    ```

--- contrib/guix/security-check.py
+++ contrib/guix/security-check.py
@@ -280,8 +280,8 @@ if __name__ == '__main__':
     for filename in sys.argv[1:]:
         binary = lief.parse(filename)
 
-        etype = binary.format
-        arch = binary.abstract.header.architecture
+        etype = binary.format # type: ignore[union-attr]
+        arch = binary.abstract.header.architecture # type: ignore[union-attr]
 
         failed: list[str] = []
         for (name, func) in CHECKS[etype][arch]:
