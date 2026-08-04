384c985d6030897cf1a05dbd00ed0644eda33d15
Lőrinc <pap.lorinc@gmail.com>
2026-08-04 13:11:53 -0700

    test: characterize BIP35 relay capacity

--- test/functional/p2p_connection_limits.py
+++ test/functional/p2p_connection_limits.py
@@ -98,6 +98,14 @@ class P2PConnectionLimits(BitcoinTestFramework):
         bip35_peer.sync_with_ping()
         assert int(tx['wtxid'], 16) not in bip35_peer.get_invs()
 
+        self.log.info('Check BIP35 requests against inbound transaction-relay capacity')
+        # NODE_BLOOM permits BIP35, while zero capacity makes any counted requester exceed the limit.
+        self.restart_node(0, ['-maxconnections=13', '-peerbloomfilters', '-inboundrelaypercent=0'])
+        peer1 = self.add_relay_disabled_peer(P2PInterface())
+        with node.assert_debug_log(['received: mempool'], timeout=2):
+            peer1.send_without_ping(msg_mempool())
+            peer1.sync_with_ping()  # TODO: Account BIP35 service against inbound tx-relay capacity
+
         self.log.info('Test different values of inboundrelaypercent')
         self.restart_node(0, ['-maxconnections=13', '-inboundrelaypercent=0'])
         with node.assert_debug_log(['failed to find a tx-relaying eviction candidate - connection dropped'], timeout=2):
