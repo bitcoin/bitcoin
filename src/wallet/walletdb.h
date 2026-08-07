1548d15de61886e0b8e67801146e9c168d9d4a66
Ava Chow <github@achow101.com>
2026-08-07 15:16:25 -0700

    walletdb: Rename WriteTx to WriteFullTx
    Clarify in the name that this function writes the entire metadata and
    all of the witness tx variants.

--- src/wallet/walletdb.h
+++ src/wallet/walletdb.h
@@ -230,7 +230,8 @@ public:
     bool WritePurpose(const std::string& strAddress, const std::string& purpose);
     bool ErasePurpose(const std::string& strAddress);
 
-    bool WriteTx(const CWalletTx& wtx);
+    // Write a CWalletTx and all variant witness txs (single tx record and multiple wtxvariant records)
+    bool WriteFullTx(const CWalletTx& wtx);
     bool EraseTx(Txid hash);
     // Write a single witness variant of CWalletTx (single wtxvariant record)
     bool WriteWtxVariant(const Txid& txid, const CTransactionRef& tx);
