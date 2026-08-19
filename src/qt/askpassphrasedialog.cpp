3eed7f994c0b0be799ca64d6759526648cbe3ebd
Ava Chow <github@achow101.com>
2026-08-19 15:58:25 -0700

    wallet: deduplicate wallet unlock and passphrase change errors
    Unlocking and changing the passphrase of a wallet may result in errors.
    These functions should produce specific errors with codes and messages
    so that callers can handle the errors appropriately.
    
    Additionally, doing so allows us to deduplicate some passphrase error
    messages.

--- src/qt/askpassphrasedialog.cpp
+++ src/qt/askpassphrasedialog.cpp
@@ -158,20 +158,9 @@ void AskPassphraseDialog::accept()
     } break;
     case Unlock:
         try {
-            if (!model->unlockWallet(oldpass)) {
-                // Check if the passphrase has a null character (see #27067 for details)
-                if (oldpass.find('\0') == std::string::npos) {
-                    QMessageBox::critical(this, tr("Wallet unlock failed"),
-                                          tr("The passphrase entered for the wallet decryption was incorrect."));
-                } else {
-                    QMessageBox::critical(this, tr("Wallet unlock failed"),
-                                          tr("The passphrase entered for the wallet decryption is incorrect. "
-                                             "It contains a null character (ie - a zero byte). "
-                                             "If the passphrase was set with a version of this software prior to 25.0, "
-                                             "please try again with only the characters up to — but not including — "
-                                             "the first null character. If this is successful, please set a new "
-                                             "passphrase to avoid this issue in the future."));
-                }
+            auto unlocked{model->unlockWallet(oldpass)};
+            if (!unlocked) {
+                QMessageBox::critical(this, tr("Wallet unlock failed"), QString::fromStdString(unlocked.error().message.translated));
             } else {
                 if (m_passphrase_out) {
                     m_passphrase_out->assign(oldpass);
@@ -189,26 +178,13 @@ void AskPassphraseDialog::accept()
     case ChangePass:
         if(newpass1 == newpass2)
         {
-            if(model->changePassphrase(oldpass, newpass1))
-            {
+            auto changed{model->changePassphrase(oldpass, newpass1)};
+            if (changed) {
                 QMessageBox::information(this, tr("Wallet encrypted"),
                                      tr("Wallet passphrase was successfully changed."));
                 QDialog::accept(); // Success
-            }
-            else
-            {
-                // Check if the old passphrase had a null character (see #27067 for details)
-                if (oldpass.find('\0') == std::string::npos) {
-                    QMessageBox::critical(this, tr("Passphrase change failed"),
-                                          tr("The passphrase entered for the wallet decryption was incorrect."));
-                } else {
-                    QMessageBox::critical(this, tr("Passphrase change failed"),
-                                          tr("The old passphrase entered for the wallet decryption is incorrect. "
-                                             "It contains a null character (ie - a zero byte). "
-                                             "If the passphrase was set with a version of this software prior to 25.0, "
-                                             "please try again with only the characters up to — but not including — "
-                                             "the first null character."));
-                }
+            } else {
+                QMessageBox::critical(this, tr("Passphrase change failed"), QString::fromStdString(changed.error().message.translated));
             }
         }
         else
