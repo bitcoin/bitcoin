32337a58f1d221ed340888eea7c450825d9123ca
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2026-08-20 09:33:40 +0200

    build: Require C++23 compiler

--- ci/test/00_setup_env_native_asan.sh
+++ ci/test/00_setup_env_native_asan.sh
@@ -33,6 +33,6 @@ export BITCOIN_CONFIG="\
  -DCMAKE_C_FLAGS='-ftrivial-auto-var-init=pattern' \
  -DCMAKE_CXX_FLAGS='-ftrivial-auto-var-init=pattern' \
  -DCMAKE_EXE_LINKER_FLAGS='-fuse-ld=mold' \
- -DAPPEND_CXXFLAGS='-std=c++23' \
+ -DAPPEND_CXXFLAGS='-std=c++26'  `# Use the next std version to catch deprecated code early` \
  -DAPPEND_CPPFLAGS='-DARENA_DEBUG -DDEBUG_LOCKORDER' \
 "
