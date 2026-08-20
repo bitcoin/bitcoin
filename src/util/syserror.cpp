63bc501a0ca800ba504ef0856a575fb20cbc5ba6
fanquake <fanquake@gmail.com>
2026-08-20 15:11:38 +0100

    util: remove usage of strerror_s/r
    Use std::system_category().message(err), which internally dispatches to
    strerror_r/s, and should handle unknown errors.
    
    See: https://github.com/llvm/llvm-project/blob/4e9e83400a5702059c8a18e2fb6174038875e8a6/libcxx/src/system_error.cpp#L231

--- src/util/syserror.cpp
+++ src/util/syserror.cpp
@@ -2,14 +2,12 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <bitcoin-build-config.h> // IWYU pragma: keep
-
 #include <util/syserror.h>
 
 #include <tinyformat.h>
 
-#include <cstring>
 #include <string>
+#include <system_error>
 
 #if defined(WIN32)
 #include <windows.h>
@@ -17,24 +15,7 @@
 
 std::string SysErrorString(int err)
 {
-    char buf[1024];
-    /* Too bad there are three incompatible implementations of the
-     * thread-safe strerror. */
-    const char *s = nullptr;
-#ifdef WIN32
-    if (strerror_s(buf, sizeof(buf), err) == 0) s = buf;
-#else
-#ifdef STRERROR_R_CHAR_P /* GNU variant can return a pointer outside the passed buffer */
-    s = strerror_r(err, buf, sizeof(buf));
-#else /* POSIX variant always returns message in buffer */
-    if (strerror_r(err, buf, sizeof(buf)) == 0) s = buf;
-#endif
-#endif
-    if (s != nullptr) {
-        return strprintf("%s (%d)", s, err);
-    } else {
-        return strprintf("Unknown error (%d)", err);
-    }
+    return strprintf("%s (%d)", std::system_category().message(err), err);
 }
 
 #if defined(WIN32)
