05bd1513be2d9aaeb0a94671024f585f0d3cca15
rustaceanrob <rob.netzke@gmail.com>
2026-08-25 16:03:00 +0100

    test: Add display `operator<<` overloads to `common.{h,cpp}`
    If the `<<` operator exists, boost can use this as a debug output when a
    check fails (besides `BOOST_CHECK`). This adds `<<` for types commonly
    used across the unit tests that do not yet have a display output.

--- src/test/util/common.h
+++ src/test/util/common.h
@@ -5,10 +5,25 @@
 #ifndef BITCOIN_TEST_UTIL_COMMON_H
 #define BITCOIN_TEST_UTIL_COMMON_H
 
+#include <addresstype.h>
+#include <util/feefrac.h>
+
 #include <chrono>
 #include <optional>
 #include <ostream>
 #include <string>
+#include <utility>
+
+struct AddressPosition;
+class CAddress;
+struct CExtKey;
+struct CExtPubKey;
+class CPubKey;
+class CService;
+struct KeyOriginInfo;
+namespace txindex {
+struct BlockTxPosition;
+} // namespace txindex
 
 /**
  * BOOST_CHECK_EXCEPTION predicates to check the specific validation error.
@@ -56,6 +71,37 @@ inline std::ostream& operator<<(std::ostream& os, const T& obj)
 {
     return os << obj.ToString();
 }
+
+std::ostream& operator<<(std::ostream& os, const AddressPosition& pos);
+std::ostream& operator<<(std::ostream& os, const CService& service);
+std::ostream& operator<<(std::ostream& os, const CAddress& addr);
+std::ostream& operator<<(std::ostream& os, const CExtKey& k);
+std::ostream& operator<<(std::ostream& os, const CExtPubKey& k);
+std::ostream& operator<<(std::ostream& os, const FeeFrac& ff);
+std::ostream& operator<<(std::ostream& os, const CNoDestination& dest);
+std::ostream& operator<<(std::ostream& os, const PubKeyDestination& dest);
+std::ostream& operator<<(std::ostream& os, const PKHash& h);
+std::ostream& operator<<(std::ostream& os, const ScriptHash& h);
+std::ostream& operator<<(std::ostream& os, const WitnessV0KeyHash& h);
+std::ostream& operator<<(std::ostream& os, const WitnessV0ScriptHash& h);
+std::ostream& operator<<(std::ostream& os, const WitnessUnknown& w);
+std::ostream& operator<<(std::ostream& os, const PayToAnchor& p);
+std::ostream& operator<<(std::ostream& os, const WitnessV1Taproot& t);
+std::ostream& operator<<(std::ostream& os, const CTxDestination& dest);
+std::ostream& operator<<(std::ostream& os, const CPubKey& pk);
+std::ostream& operator<<(std::ostream& os, const KeyOriginInfo& info);
+std::ostream& operator<<(std::ostream& os, const ByRatioNegSize<FeeFrac>& b);
+
+template <typename T, typename U>
+    requires requires(std::ostream& os, const T& t, const U& u) { os << t; os << u; }
+inline std::ostream& operator<<(std::ostream& os, const std::pair<T, U>& p)
+{
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+
+namespace txindex {
+std::ostream& operator<<(std::ostream& os, const BlockTxPosition& pos);
+} // namespace txindex
 
 // @}
 
