40a31d97ab118cb030af7d560aedbade3ae4cd36
Martin Zumsande <mzumsande@gmail.com>
2026-08-31 16:51:37 +0200

    fuzz: allow to prune out-of-chain blocks in block_index_tree
    After the issues with m_blocks_unlinked and
    setBlockIndexCandidates have been resolved in previous commits,
    the previous restriction to the fuzz test does no longer apply,
    and we can also prune out-of-chain blocks.

--- src/test/fuzz/block_index_tree.cpp
+++ src/test/fuzz/block_index_tree.cpp
@@ -152,13 +152,9 @@ FUZZ_TARGET(block_index_tree, .init = initialize_block_index_tree)
             [&] {
                 // Prune chain - dealing with block files is beyond the scope of this test, so just prune random blocks, making no assumptions
                 // about what blocks are pruned together because they are in the same block file.
-                // Also don't prune blocks outside of the chain for now - this would make the fuzzer crash because of the problem described in
-                // https://github.com/bitcoin/bitcoin/issues/31512
                 LOCK(cs_main);
-                auto& chain = chainman.ActiveChain();
-                int prune_height = fuzzed_data_provider.ConsumeIntegralInRange<int>(0, chain.Height());
-                CBlockIndex* prune_block{chain[prune_height]};
-                if (prune_block != chain.Tip() && (prune_block->nStatus & BLOCK_HAVE_DATA)) {
+                CBlockIndex* prune_block{PickValue(fuzzed_data_provider, blocks)};
+                if (prune_block != chainman.ActiveChain().Tip() && (prune_block->nStatus & BLOCK_HAVE_DATA)) {
                     blockman.m_have_pruned = true;
                     prune_block->nStatus &= ~BLOCK_HAVE_DATA;
                     prune_block->nStatus &= ~BLOCK_HAVE_UNDO;
