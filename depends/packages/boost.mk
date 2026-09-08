999ec480b81e5bc5b566f266a863e800f3623d20
fanquake <fanquake@gmail.com>
2026-09-08 14:17:16 +0100

    depends: patch around Boost Test std::wcrtomb usage
    If libstdc++ is built with --disable-wchar_t, test_bitcoin will fail to
    compile.

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -4,6 +4,11 @@ $(package)_download_path = https://github.com/boostorg/boost/releases/download/b
 $(package)_file_name = boost-$($(package)_version)-cmake.tar.gz
 $(package)_sha256_hash = f51707c27359a0df0cac1beada86de31bb5eed5e8285592dadec384df99c2984
 $(package)_build_subdir = build
+$(package)_patches += drop_wcrtomb_wchar_t.patch
+
+define $(package)_preprocess_cmds
+  patch -p1 < $($(package)_patch_dir)/drop_wcrtomb_wchar_t.patch
+endef
 
 define $(package)_set_vars
   $(package)_config_opts = -DBOOST_INCLUDE_LIBRARIES="multi_index;test"
