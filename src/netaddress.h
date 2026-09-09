e6c775c6d63a1683c9b2ab68d91b335f17e65d55
Fabian Jahr <fjahr@protonmail.com>
2026-09-09 09:45:19 +0200

    net: treat RFC 9637 new IPv6 documentation range as invalid

--- src/netaddress.h
+++ src/netaddress.h
@@ -162,6 +162,7 @@ public:
     bool IsRFC6598() const; // IPv4 ISP-level NAT (100.64.0.0/10)
     bool IsRFC5737() const; // IPv4 documentation addresses (192.0.2.0/24, 198.51.100.0/24, 203.0.113.0/24)
     bool IsRFC3849() const; // IPv6 documentation address (2001:0DB8::/32)
+    bool IsRFC9637() const; // IPv6 documentation address (3FFF::/20)
     bool IsRFC3927() const; // IPv4 autoconfig (169.254.0.0/16)
     bool IsRFC3964() const; // IPv6 6to4 tunnelling (2002::/16)
     bool IsRFC4193() const; // IPv6 unique local (FC00::/7)
--- src/test/fuzz/netaddress.cpp
+++ src/test/fuzz/netaddress.cpp
@@ -55,7 +55,7 @@ FUZZ_TARGET(netaddress)
         assert(net_addr.IsIPv4());
     }
     (void)net_addr.IsRFC2544();
-    if (net_addr.IsRFC3849() || net_addr.IsRFC3964() || net_addr.IsRFC4380() || net_addr.IsRFC4843() || net_addr.IsRFC7343() || net_addr.IsRFC4862() || net_addr.IsRFC6052() || net_addr.IsRFC6145()) {
+    if (net_addr.IsRFC3849() || net_addr.IsRFC3964() || net_addr.IsRFC4380() || net_addr.IsRFC4843() || net_addr.IsRFC7343() || net_addr.IsRFC4862() || net_addr.IsRFC6052() || net_addr.IsRFC6145() || net_addr.IsRFC9637()) {
         assert(net_addr.IsIPv6());
     }
     (void)net_addr.IsRFC3927();
@@ -71,6 +71,7 @@ FUZZ_TARGET(netaddress)
     (void)net_addr.IsRFC6145();
     (void)net_addr.IsRFC6598();
     (void)net_addr.IsRFC7343();
+    (void)net_addr.IsRFC9637();
     if (!net_addr.IsRoutable()) {
         assert(net_addr.GetNetwork() == Network::NET_UNROUTABLE || net_addr.GetNetwork() == Network::NET_INTERNAL);
     }
