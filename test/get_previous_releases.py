61edcf92532502e91c2e17b8c2a23b45f5773936
Ava Chow <github@achow101.com>
2026-09-11 16:36:29 -0700

    test: Add 31.1 to wallet back compat

--- test/get_previous_releases.py
+++ test/get_previous_releases.py
@@ -109,6 +109,15 @@ SHA256_SUMS = {
     "56824dd705bc2a3b22d42e8aa02ed53498d491ff7c2c8aa96831333871887ead": {"tag": "v31.0", "archive": "bitcoin-31.0-x86_64-apple-darwin.tar.gz"},
     "d3e4c58a35b1d0a97a457462c94f55501ad167c660c245cb1ffa565641c65074": {"tag": "v31.0", "archive": "bitcoin-31.0-x86_64-linux-gnu.tar.gz"},
     "82fd2c504a0f20a31d4d13bd407783d6fc7bf17622d0ce85228a9b92694e03f0": {"tag": "v31.0", "archive": "bitcoin-31.0-win64.zip"},
+
+    "dcf1873f2208ba4f962f3398d47e154c39c0084be8f4553e05c940d0ace3d004": {"tag": "v31.1", "archive": "bitcoin-31.1-aarch64-linux-gnu.tar.gz"},
+    "66b2b45359efa161031a49898f96aa7cf1455db46ca6102acd16a7197dc3b96f": {"tag": "v31.1", "archive": "bitcoin-31.1-arm-linux-gnueabihf.tar.gz"},
+    "16a097c09fbd7eb78b240ce1dae123663ea2e5e377cfd6a951e71e227e23cf2f": {"tag": "v31.1", "archive": "bitcoin-31.1-arm64-apple-darwin.tar.gz"},
+    "f81dd017a551c5af7fa2d6fa67b885077a8353322a8019e8fd538366bae1eff7": {"tag": "v31.1", "archive": "bitcoin-31.1-powerpc64-linux-gnu.tar.gz"},
+    "8a9213348a111438472653b8bd46c12184c60cc35ce0c2af02b853de4297cf94": {"tag": "v31.1", "archive": "bitcoin-31.1-riscv64-linux-gnu.tar.gz"},
+    "bc506958d0f387c1ea770bdc7c7192a505fa645ff62cabcc7761fa7eb89e867e": {"tag": "v31.1", "archive": "bitcoin-31.1-x86_64-apple-darwin.tar.gz"},
+    "b80d9c3e04da78fb6f0569685673418cf686fadba9042d926d13fb87ff503f9e": {"tag": "v31.1", "archive": "bitcoin-31.1-x86_64-linux-gnu.tar.gz"},
+    "c99ef173471c58e6766d9eebd12e6c35349082eeed3939bc99eed58ef57db587": {"tag": "v31.1", "archive": "bitcoin-31.1-win64.zip"},
 }
 
 
