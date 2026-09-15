339d46347425f78e589fb5448a30fdc9f056692b
fanquake <fanquake@gmail.com>
2026-09-15 12:28:41 +0100

    guix: skip psutil tests in python-lief build

--- contrib/guix/manifest_gui.scm
+++ contrib/guix/manifest_gui.scm
@@ -5,7 +5,24 @@
              ((gnu packages installers) #:select (nsis-x86_64))
              (gnu packages pkg-config)
              ((gnu packages python) #:select (python-minimal))
-             ((gnu packages python-xyz) #:select (python-lief)))
+             ((gnu packages python-xyz) #:select (python-lief python-psutil))
+             ((guix utils) #:select (substitute-keyword-arguments))
+             (guix packages))
+
+;; python-lief transitively pulls in python-psutil, which has
+;; tests that fail when builing natively for riscv64.
+;; See <https://codeberg.org/guix/guix/issues/10128>.
+(define python-lief-no-psutil-tests
+  ((package-mapping
+    (lambda (p)
+      (if (eq? p python-psutil)
+          (package
+            (inherit p)
+            (arguments
+             (substitute-keyword-arguments (package-arguments p)
+               ((#:tests? _ #t) #f))))
+          p)))
+   python-lief))
 
 (packages->manifest
  (append
@@ -16,7 +33,7 @@
         ;; Packaging scripts
         python-minimal ;; (3.12)
         ;; Tests
-        python-lief)
+        python-lief-no-psutil-tests)
   (let ((target (getenv "HOST")))
     (cond ((string-suffix? "-mingw32" target)
            (list zip
