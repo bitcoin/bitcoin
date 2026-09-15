484d327563b581a2cb34bf8773223a676e2daf5b
Ava Chow <github@achow101.com>
2026-09-14 18:38:20 -0700

    guix: Validate codesigned windows binaries
    `osslsigncode attach-signature` doesnot validate the signature at all,
    but we should be doing a signature check after applying to ensure that
    the signature itself is also valid.
    
    To avoid requiring the container to have network access, CRL checks are
    disabled.
    
    The package `nss-certs` is required so that a root CA file is available.

--- contrib/guix/libexec/codesign.sh
+++ contrib/guix/libexec/codesign.sh
@@ -86,6 +86,11 @@ mkdir -p "$DISTSRC"
                                  -in "${bin}" \
                                  -out "${WORKDIR}/${bin_base/-unsigned}" \
                                  -sigin codesignatures/win/"${bin_base}".pem
+                osslsigncode verify \
+                                 -CAfile "$GUIX_ENVIRONMENT/etc/ssl/certs/ca-certificates.crt" \
+                                 -ignore-crl \
+                                 -ignore-cdp \
+                                 -in "${WORKDIR}/${bin_base/-unsigned}"
             done
 
             # Move installer to outdir
