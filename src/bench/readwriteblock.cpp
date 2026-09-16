3e6d69ad7ab474238570e7232b172ef628bca653
yuvicc <yuvichh01@gmail.com>
2026-09-16 10:22:04 +0200

    refactor: Make BlockManager use FatalError type
    Co-authored-by: Hodlinator <172445034+hodlinator@users.noreply.github.com>

--- src/bench/readwriteblock.cpp
+++ src/bench/readwriteblock.cpp
@@ -13,6 +13,7 @@
 #include <sync.h>
 #include <test/util/setup_common.h>
 #include <util/check.h>
+#include <util/expected.h>
 #include <validation.h>
 
 #include <memory>
@@ -33,7 +34,7 @@ static void WriteBlockBench(benchmark::Bench& bench)
     bench.run([&] {
         LOCK(::cs_main);
         const auto pos{blockman.WriteBlock(block, 413'567)};
-        assert(!pos.IsNull());
+        assert(pos && !pos->IsNull());
     });
 }
 
@@ -43,7 +44,7 @@ static void ReadBlockBench(benchmark::Bench& bench)
     auto& blockman{testing_setup->m_node.chainman->m_blockman};
     const auto& test_block{CreateTestBlock()};
     const auto& expected_hash{test_block.GetHash()};
-    const auto& pos{WITH_LOCK(::cs_main, return blockman.WriteBlock(test_block, 413'567))};
+    const auto pos{WITH_LOCK(::cs_main, return blockman.WriteBlock(test_block, 413'567)).value()};
     bench.run([&] {
         CBlock block;
         const auto success{blockman.ReadBlock(block, pos, expected_hash)};
@@ -55,7 +56,7 @@ static void ReadRawBlockBench(benchmark::Bench& bench)
 {
     const auto testing_setup{MakeNoLogFileContext<const TestingSetup>(ChainType::MAIN)};
     auto& blockman{testing_setup->m_node.chainman->m_blockman};
-    const auto pos{WITH_LOCK(::cs_main, return blockman.WriteBlock(CreateTestBlock(), 413'567))};
+    const auto pos{WITH_LOCK(::cs_main, return blockman.WriteBlock(CreateTestBlock(), 413'567)).value()};
     bench.run([&] {
         const auto res{blockman.ReadRawBlock(pos)};
         assert(res);
--- src/validation.cpp
+++ src/validation.cpp
@@ -2632,7 +2632,8 @@ bool Chainstate::ConnectBlock(const CBlock& block, BlockValidationState& state, 
         return true;
     }
 
-    if (!m_blockman.WriteBlockUndo(blockundo, state, *pindex)) {
+    if (auto res{m_blockman.WriteBlockUndo(blockundo, *pindex)}; !res) {
+        state.Error(res.error().message());
         return false;
     }
 
@@ -4378,11 +4379,13 @@ bool ChainstateManager::AcceptBlock(const std::shared_ptr<const CBlock>& pblock,
             blockPos = *dbp;
             m_blockman.UpdateBlockInfo(block, pindex->nHeight, blockPos);
         } else {
-            blockPos = m_blockman.WriteBlock(block, pindex->nHeight);
-            if (blockPos.IsNull()) {
-                state.Error(strprintf("%s: Failed to find position to write new block to disk", __func__));
+            auto res{m_blockman.WriteBlock(block, pindex->nHeight)};
+            if (!res) {
+                state.Error(strprintf("%s: Failed to find position to write new block to disk: %s", __func__, res.error().message()));
                 return false;
             }
+            assert(!res->IsNull());
+            blockPos = *res;
         }
         ReceivedBlockTransactions(block, pindex, blockPos);
     } catch (const std::runtime_error& e) {
@@ -4957,13 +4960,13 @@ bool ChainstateManager::LoadGenesisBlock()
     }
 
     try {
-        FlatFilePos blockPos{m_blockman.WriteBlock(genesis_block, 0)};
-        if (blockPos.IsNull()) {
-            LogError("Writing genesis block to disk failed");
+        auto blockPos{m_blockman.WriteBlock(genesis_block, 0)};
+        if (!blockPos) {
+            LogError("Writing genesis block to disk failed (%s)", blockPos.error().message());
             return false;
         }
         CBlockIndex* pindex{m_blockman.AddToBlockIndex(genesis_block, m_best_header)};
-        ReceivedBlockTransactions(genesis_block, pindex, blockPos);
+        ReceivedBlockTransactions(genesis_block, pindex, *blockPos);
     } catch (const std::runtime_error& e) {
         LogError("Failed to write genesis block: %s", e.what());
         return false;
