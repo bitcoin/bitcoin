c40c410f11e7a04dd468476e59d6e814daf2c2da
fanquake <fanquake@gmail.com>
2026-09-16 14:50:40 +0100

    nix: drop MSVCRT build

--- contrib/devtools/shell-win64-cross.nix
+++ contrib/devtools/shell-win64-cross.nix
@@ -8,12 +8,7 @@
   }) {} }:
 
 let
-  host = builtins.getEnv "HOST";
-  baseCrossPkgs = if host == "x86_64-w64-mingw32ucrt"
-    then pkgs.pkgsCross.ucrt64
-    else if host == "x86_64-w64-mingw32"
-      then pkgs.pkgsCross.mingwW64
-      else throw "Unsupported HOST: ${host}";
+  baseCrossPkgs = pkgs.pkgsCross.ucrt64;
   crossPkgs = baseCrossPkgs.extend (_: _: {
     threads = {
       model = "posix";
