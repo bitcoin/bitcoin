52d79b01d3cc6d9f0885a05754300e50adcee4bc
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2026-09-25 12:07:30 +0100

    guix: Update time-machine to `60f6956aeffa7f30285745bd0ea615e9acfc74f8`
    This is necessary to unblock building on RISC-V systems.
    
    Fixes https://github.com/bitcoin/bitcoin/issues/36232.

--- contrib/guix/manifest_gui.scm
+++ contrib/guix/manifest_gui.scm
@@ -44,9 +44,9 @@
         ;; Build tools
         ninja
         ;; Packaging scripts
-        python-minimal ;; (3.11)
+        python-minimal ;; 3.12
         ;; Tests
-        python-lief-no-riscv64-failing-tests)
+        python-lief-no-riscv64-failing-tests) ;; 0.17.6
   (let ((target (getenv "HOST")))
     (cond ((string-suffix? "-mingw32" target)
            (list nsis-x86_64-no-riscv64-failing-tests
